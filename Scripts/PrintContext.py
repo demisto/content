575ce151f0696b2515464433a21f3c978e90352c
liorkol <lior@demisto.com>
2016-08-28 16:27:39 +0300

    PrintContext multiple output formats with sensible default

--- Scripts/PrintContext.py
+++ Scripts/PrintContext.py
@@ -1,3 +1,13 @@
-md = "**Context data**:\n```\n" + json.dumps(demisto.context(), indent=4) + '\n```'
-demisto.results({'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': md})
+fmt = demisto.get(demisto.args(), 'outputformat')
+ctx = demisto.context()
+if ctx:
+    if fmt == 'table':
+        demisto.results({'ContentsFormat': formats['table'], 'Type': entryTypes['note'], 'Contents': [{'Context key': d, 'Value': formatCell(ctx[d])} for d in ctx]})
+    elif fmt == 'json':
+        demisto.results(ctx)
+    else:
+        md = "**Context data**:\n```\n" + json.dumps(demisto.context(), indent=4) + '\n```'
+        demisto.results({'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': md})
 
+else:
+    demisto.results('Context empty.')
