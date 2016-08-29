ecdb14201ca2b053d8be9b23701feea2c485a2ee
liorkol <lior@demisto.com>
2016-08-29 16:01:31 +0300

    Using ctx in all subcases.

--- Scripts/PrintContext.py
+++ Scripts/PrintContext.py
@@ -6,7 +6,7 @@ if ctx:
     elif fmt == 'json':
         demisto.results(ctx)
     else:
-        md = "**Context data**:\n```\n" + json.dumps(demisto.context(), indent=4) + '\n```'
+        md = "**Context data**:\n```\n" + json.dumps(ctx, indent=4) + '\n```'
         demisto.results({'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': md})
 
 else:
