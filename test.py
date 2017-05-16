b9e0162c47eaaf63c11ba75ab2b1b7dc7c083182
asafshen <asaf@demisto.com>
2017-05-16 18:55:00 +0300

    install demisto

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
+import demisto
+
 def main():
+    client = demisto.DemistoClient('<your-api-key-goes-here>', 'https://localhost:8443')
+    print client
     print 'hello world'
 
 if __name__ == '__main__':
