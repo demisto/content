f2b62a5250d36af1a40a3628a28d25c4958b1781
asafshen <asaf@demisto.com>
2017-05-16 21:01:05 +0300

    use env var

--- Tests/test.py
+++ Tests/test.py
@@ -1,9 +1,13 @@
 import demisto
+import os
 
 def main():
-    client = demisto.DemistoClient('<your-api-key-goes-here>', 'https://localhost:8443')
+    #client = demisto.DemistoClient('<your-api-key-goes-here>', 'https://localhost:8443')
     print client
+    print os.environ.get('keyname')
     print 'hello world'
+    print os.environ.get('KEY_THAT_MIGHT_EXIST')
 
 if __name__ == '__main__':
     main()
+
