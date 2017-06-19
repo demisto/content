cc4d10e7a600ce6b2a6902e3642d240a3a916325
asafshen <asaf@demisto.com>
2017-06-19 15:29:38 +0300

    get server artifact

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 ACCEPT_TYPE="Accept: application/json"
 SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
@@ -10,17 +8,11 @@ echo "Getting latest build num"
 
 ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR" | jq '.[0].build_num')
 
-echo "###### before"
-ls
-
-echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
-
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep server.tar.gz | grep /0/)
 
+echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
 curl ${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}
 
-echo "###### after"
 ls
-echo "Getting tom artifact"
 
 echo "Done!"
