f3194882705cb265eb4bf83c83a6668c2b22e959
asafshen <asaf@demisto.com>
2017-06-19 17:09:45 +0300

    get server artifact

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -3,10 +3,11 @@
 ACCEPT_TYPE="Accept: application/json"
 SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
+TOKEN_ATTR1="circle-token=blabla"
 
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR})
 echo "#####"
-
+echo "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR1}
 echo "SERVER_DOWNLOAD_LINK: ${SERVER_DOWNLOAD_LINK}"
 echo "#####"
 
