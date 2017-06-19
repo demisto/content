f6b793a8db16b96540c59fad7bac696d76133aa1
asafshen <asaf@demisto.com>
2017-06-19 15:05:03 +0300

    get server artifact

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -10,12 +10,13 @@ echo "Getting latest build num"
 
 ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR" | jq '.[0].build_num')
 
-echo ARTIFACT_BUILD_NUM=${ARTIFACT_BUILD_NUM}
-
-SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep server.tar.gz | grep /0/)
 echo "###### before"
 ls
+
 echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
+
+SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep server.tar.gz | grep /0/)
+
 curl ${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}
 
 echo "###### after"
