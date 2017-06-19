e7c883f0db9550926472b8e5ab75f21f0bb8ea60
asafshen <asaf@demisto.com>
2017-06-19 14:54:37 +0300

    get server artifact

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -10,7 +10,9 @@ echo "Getting latest build num"
 
 ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR" | jq '.[0].build_num')
 
-SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver*.sh | grep /0/)
+echo ARTIFACT_BUILD_NUM=${ARTIFACT_BUILD_NUM}
+
+SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep "demistoserver*.sh" | grep /0/)
 echo "###### before"
 ls
 echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
