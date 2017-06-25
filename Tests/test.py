ef8593cd66bdf71b3ce6cd3ce658c73aa5877120
asafshen <asaf@demisto.com>
2017-06-25 16:22:23 +0300

    test print error

--- Tests/test.py
+++ Tests/test.py
@@ -1,12 +1,16 @@
 import demisto
 import os
+import sys
+
+def print_error(error_str):
+    sys.stderr.write(error_str + '\n')
 
 def main():
     #client = demisto.DemistoClient('<your-api-key-goes-here>', 'https://localhost:8443')
     #print client
     print os.environ.get('testname') # define in circle-ci settings
     print 'hello world'
-    print os.environ.get('KEY_THAT_MIGHT_EXIST')
+    print_error('helele')
 
 if __name__ == '__main__':
     main()
