2a1024f505a356cdf28fbd1223211fc586ffcf5e
asafshen <asaf@demisto.com>
2017-06-26 11:23:51 +0300

    download artifact

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -6,13 +6,10 @@ TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
 TOKEN_ATTR1="circle-token=blabla"
 
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR})
-echo "#####"
-echo "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR1}
-echo "SERVER_DOWNLOAD_LINK: ${SERVER_DOWNLOAD_LINK}"
-echo "#####"
 
-exit 0
 curl ${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR} -o demistoserver.sh
+
+echo "#####"
 
 ls
 
