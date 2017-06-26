536ad768fbf7691081674815ff2d672c64810e55
asafshen <asaf@demisto.com>
2017-06-26 19:19:47 +0300

    exp-file-config

--- Tests/scripts/configure_installer_commands.sh
+++ Tests/scripts/configure_installer_commands.sh
@@ -2,8 +2,15 @@
 set -e
 
 echo "start"
-#ADMIN_CREDENTIALS=$(cat ./conf.json | jq '.admin')
-echo "ADMIN_CREDENTIALS = ${ADMIN_CREDENTIALS}"
+ADMIN_CRED=$(cat ./conf.json | jq '.admin')
+echo "ADMIN_CREDENTIALS = ${ADMIN_CRED}"
 
 ADMIN_EXP=$(echo $ADMIN_CREDENTIALS | sed -e 's/\(.\)/send -- "\1"\nexpect -exact "*"\n/g')
 echo "ADMIN_EXP = ${ADMIN_EXP}"
+
+echo "BEFORE"
+cat ./Tests/scripts/installer_commands-centos.exp
+sed -i 's/<PASSWORD>/${ADMIN_EXP}/g' ./Tests/scripts/installer_commands-centos.exp
+
+echo "AFTER"
+cat ./Tests/scripts/installer_commands-centos.exp
