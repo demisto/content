a77b2f56000073d5717522cb2960b2c7b2f70ec2
asafshen <asaf@demisto.com>
2017-06-26 19:51:53 +0300

    installer command

--- Tests/scripts/configure_installer_commands.sh
+++ Tests/scripts/configure_installer_commands.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-cat ./conf.json
-
-echo cat ./conf.json | jq '.admin'
-
-exit 0
+echo "start"
+ADMIN_CREDENTIALS=$(cat ./conf.json | jq '.admin')
+echo "ADMIN_CREDENTIALS = ${ADMIN_CREDENTIALS}"
 
 ADMIN_EXP=$(echo $ADMIN_CREDENTIALS | sed -e 's/\(.\)/send -- "\1"\nexpect -exact "*"\n/g')
 echo "ADMIN_EXP = ${ADMIN_EXP}"
 
 echo "BEFORE"
 cat ./Tests/scripts/installer_commands-centos.exp
-sed -i 's/<PASSWORD>/${ADMIN_EXP}/g' ./Tests/scripts/installer_commands-centos.exp
+sed -i 's/original/new/g' ./Tests/scripts/installer_commands-centos.exp
 
 echo "AFTER"
 cat ./Tests/scripts/installer_commands-centos.exp
