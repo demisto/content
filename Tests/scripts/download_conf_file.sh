5debfdf9bcf197d8117ee8fe4c75d54990559e82
asafshen <asaf@demisto.com>
2017-06-26 18:41:43 +0300

    exp-file-config

--- Tests/scripts/download_conf_file.sh
+++ Tests/scripts/download_conf_file.sh
@@ -2,4 +2,6 @@
 curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
         --remote-name --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json"
 
+ls
+
 echo "Successfully downloaded configuration file"
