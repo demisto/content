bd6be66215ec1ac53de2f47857d22bc957be3cd7
asafshen <asaf@demisto.com>
2017-06-26 19:08:35 +0300

    exp-file-config

--- Tests/scripts/configure_installer_commands.sh
+++ Tests/scripts/configure_installer_commands.sh
@@ -2,8 +2,8 @@
 set -e
 
 echo "start"
-#ADMIN_CREDENTIALS=$(cat ./conf.json | jq '.admin')
-#echo "ADMIN_CREDENTIALS = ${ADMIN_CREDENTIALS}"
+ADMIN_CREDENTIALS=$(cat ./conf.json | jq '.admin')
+echo "ADMIN_CREDENTIALS = ${ADMIN_CREDENTIALS}"
 
-ADMIN_EXP=$(echo "hello world" | sed -e 's/\(.\)/send -- "\1"\nexpect -exact "*"\n/g')
+ADMIN_EXP=$(echo $ADMIN_CREDENTIALS | sed -e 's/\(.\)/send -- "\1"\nexpect -exact "*"\n/g')
 echo "ADMIN_EXP = ${ADMIN_EXP}"
