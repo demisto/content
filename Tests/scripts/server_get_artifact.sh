d3914f7ceabb066718a1138631208875338b5959
asafshen <asaf@demisto.com>
2017-06-26 11:32:26 +0300

    download artifact

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-
 ACCEPT_TYPE="Accept: application/json"
 SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
-TOKEN_ATTR1="circle-token=blabla"
 
-SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR})
+echo "Getting latest build num"
 
-curl ${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR} -o demistoserver.sh
+ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR" | jq '.[0].build_num')
 
-echo "#####"
+echo "ARTIFACT_BUILD_NUM -  $ARTIFACT_BUILD_NUM"
+exit 0
 
-ls
+SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep server.tar.gz | grep /0/)
+
+echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
+curl ${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR} | tar -xz
+rm -f server_darwin_amd64 server_windows_amd64.exe
+
+echo "Getting tom artifact"
+curl -o tom ${TOM_DOWNLOAD_LINK}?${TOKEN_ATTR}
+chmod +x ./tom
 
 echo "Done!"
