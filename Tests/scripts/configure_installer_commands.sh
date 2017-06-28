1ab669b3469247111d9a4a6c151a40102d720016
asafshen <asaf@demisto.com>
2017-06-28 14:49:20 +0300

    run tests against instance

--- Tests/scripts/configure_installer_commands.sh
+++ Tests/scripts/configure_installer_commands.sh
@@ -11,6 +11,6 @@ ADMIN_CREDENTIALS=$temp
 # create exp of password
 ADMIN_EXP=$(echo $ADMIN_CREDENTIALS | sed -e 's/\(.\)/send -- "\1"\nexpect -exact "*"\n/g' | sed ':a $!{N; ba}; s/\n/\\n/g')
 
-sed -i "s/<PASSWORD>/$ADMIN_EXP/g" ./Tests/scripts/installer_commands-centos.exp
+sed -i "s/<PASSWORD>/$ADMIN_EXP/g" ./Tests/scripts/installer_commands.exp
 
 echo "Done!"
