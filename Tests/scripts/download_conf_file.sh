349b2b4c4c7fe640c55f1d063812f949f61b78e4
asafshen <asaf@demisto.com>
2017-06-28 12:11:20 +0300

    conf file from branch

--- Tests/scripts/download_conf_file.sh
+++ Tests/scripts/download_conf_file.sh
@@ -1,16 +1,16 @@
 # download configuration file from github repo
 
-echo "$CIRCLE_BRANCH"
+echo "Getting conf from branch $CIRCLE_BRANCH (fallback to master)"
 
 curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-        --remote-name --location "https://api.github.com/repos/demisto/content-test-conf/$CIRCLE_BRANCH/contents/conf.json"
+      --location "https://api.github.com/repos/demisto/content-test-conf/contents/$CIRCLE_BRANCH/conf.json" -o conf.json
 
 echo "MIDDLE"
 cat ./conf.json
 echo "#################"
 
 curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-        --remote-name --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json"
+      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json" -o conf.json
 
 echo "END"
 cat ./conf.json
