a2d3a8be2571b56c67193da6539b346d61e4415a
asafshen <asaf@demisto.com>
2017-06-28 11:24:08 +0300

    use branch artifact

---
+++ Tests/scripts/create_content_artifact.sh
@@ -0,0 +1,16 @@
+mkdir bundle
+
+# move content to bundle dir
+cd Tools/ && for i in */ ; do zip -jr "../bundle/tools-${i%/}.zip" "$i"; done
+cp Integrations/* bundle/
+cp Misc/* bundle/
+cp Playbooks/* bundle/
+cp Reports/* bundle/
+
+# move test playbooks if not master branch
+if [ "$CIRCLE_BRANCH" != "master" ]; then cp TestPlaybooks/* bundle/ ; fi
+
+# create zip & move to artifacts
+cp $(find Scripts -type f -print) bundle/
+cd bundle/ && zip ../content.zip *
+cp content.zip $CIRCLE_ARTIFACTS/content.zip
