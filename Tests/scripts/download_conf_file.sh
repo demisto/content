a7e29163df3fe44c67939fe8e1fcb52b18d6fb3a
asafshen <asaf@demisto.com>
2017-06-28 11:33:37 +0300

    use branch artifact

--- Tests/scripts/download_conf_file.sh
+++ Tests/scripts/download_conf_file.sh
@@ -1,4 +1,12 @@
 # download configuration file from github repo
+
+if [ "$CIRCLE_BRANCH" != "master" ];
+
+curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
+        --remote-name --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json"
+
+ls
+
 curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
         --remote-name --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json"
 
