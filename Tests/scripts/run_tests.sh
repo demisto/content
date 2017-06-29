058cea3399d625c5e2d9a81cca9e37dd82714005
asafshen <asaf@demisto.com>
2017-06-29 20:04:44 +0300

    run tests against instance

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -3,7 +3,7 @@ echo "start content tests"
 USERNAME="admin"
 PASSWORD=$(cat admin_password)
 SERVER_IP=$(cat public_ip)
-SERVER_URL="https://$SERVER_IP:8443"
+SERVER_URL="https://$SERVER_IP"
 CONF_PATH=$(cat conf_path)
 echo "paramters: USERNAME=$USERNAME, PASSWORD=$PASSWORD, SERVER_URL=$SERVER_URL, USERNAME=$CONF_PATH"
 python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH"
