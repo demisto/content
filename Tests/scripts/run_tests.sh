2a27ccbf0d2288741b96fbfa8a40105c3e42edbf
asafshen <asaf@demisto.com>
2017-06-29 21:24:21 +0300

    run test on server

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -6,4 +6,4 @@ SERVER_IP=$(cat public_ip)
 SERVER_URL="https://$SERVER_IP"
 CONF_PATH=$(cat conf_path)
 echo "paramters: USERNAME=$USERNAME, PASSWORD=$PASSWORD, SERVER_URL=$SERVER_URL, USERNAME=$CONF_PATH"
-python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH"
+python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD"ssh -s "$SERVER_URL" -c "$CONF_PATH"
