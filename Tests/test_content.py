d2c0ff5ffb554afed3e31564357ddd3ea91e2863
asafshen <asaf@demisto.com>
2017-06-30 14:55:57 +0300

    run tests against instance

--- Tests/test_content.py
+++ Tests/test_content.py
@@ -29,7 +29,7 @@ def main():
     c = demisto.DemistoClient(None, server, username, password)
     res = c.Login()
     if res.status_code is not 200:
-        raise ValueError("Login has failed")
+        raise ValueError("Login has failed with status code " + str(res.status_code))
 
     with open(conf) as data_file:
         conf = json.load(data_file)
