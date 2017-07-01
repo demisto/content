3b576ca962248a1ee1251533e388890b497fdc05
asafshen <asaf@demisto.com>
2017-07-02 00:11:43 +0300

    postagreSQL

--- Tests/scripts/wait_until_server_ready.sh
+++ Tests/scripts/wait_until_server_ready.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "wait for 90 seconds until server is ready"
+sleep 90s
+
 SERVER_IP=$(cat public_ip)
 SERVER_URL="https://$SERVER_IP"
 GET_HTTP_CODE_COMMAND="curl --write-out %{http_code} --silent --output /dev/null $SERVER_URL/user -k"
@@ -11,10 +14,10 @@ HTTP_CODE=$($GET_HTTP_CODE_COMMAND)
 MAX_TRIES=5
 TRY_COUNT=1
 until [ $HTTP_CODE != 433 ] || [ $TRY_COUNT = $MAX_TRIES ]; do
-    echo "server is not ready - wait another 1 minute"
-   sleep 1m
-   ((TRY_COUNT++))
-   HTTP_CODE=$($GET_HTTP_CODE_COMMAND)
+    echo "server is not yet ready - wait another 90 seconds"
+    sleep 90s
+    ((TRY_COUNT++))
+    HTTP_CODE=$($GET_HTTP_CODE_COMMAND)
 done
 
 if [ $HTTP_CODE = 433 ]
