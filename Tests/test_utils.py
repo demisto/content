a133d93b3aaa6780a644d67bd98ddf65cde8bbd5
asafshen <asaf@demisto.com>
2017-07-02 15:48:05 +0300

    preetify logs & summary

---
+++ Tests/test_utils.py
@@ -0,0 +1,15 @@
+class LOG_COLORS:
+    NATIVE = '\033[m'
+    RED = '\033[01;31m'
+    GREEN = '\033[01;32m'
+
+
+# print srt in the given color
+def print_color(str, color):
+    print(color + str + LOG_COLORS.NATIVE)
+
+
+def print_error(error_str):
+    print_color(error_str, LOG_COLORS.GREEN)
+
+
