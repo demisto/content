9728df90ab788da9b68101b469c4246dbe62c9d7
asafshen <asaf@demisto.com>
2017-07-02 18:44:43 +0300

    separate conf file

--- Tests/scripts/download_secret_conf_file.sh
+++ Tests/scripts/download_secret_conf_file.sh
@@ -4,11 +4,11 @@ set -e
 # download configuration file from github repo
 echo "Getting conf from branch $CIRCLE_BRANCH (fallback to master)"
 
-CONF_PATH="./conf.json"
-echo ${CONF_PATH} > conf_path
+SECRET_CONF_PATH="./conf_secret.json"
+echo ${SECRET_CONF_PATH} > secret_conf_path
 
 curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json?ref=$CIRCLE_BRANCH" -o "$CONF_PATH"
+      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json?ref=$CIRCLE_BRANCH" -o "$SECRET_CONF_PATH"
 
 NOT_FOUND_MESSAGE=$(cat ./conf.json | jq '.message')
 
@@ -18,9 +18,7 @@ if [ ! -z $NOT_FOUND_MESSAGE ] && [ $NOT_FOUND_MESSAGE -ne 'null' ]
     echo "Got message from github=$NOT_FOUND_MESSAGE"
 
     curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json" -o "$CONF_PATH"
+      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json" -o "$SECRET_CONF_PATH"
 fi
 
-cat "$CONF_PATH"
-
-echo "Successfully downloaded configuration file"
+echo "Successfully downloaded secret configuration file to $SECRET_CONF_PATH"
