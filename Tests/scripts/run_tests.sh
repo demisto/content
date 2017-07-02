adad92b7f83cfe013a4789b481ad4958ac6fae54
asafshen <asaf@demisto.com>
2017-07-02 19:06:36 +0300

    separate conf file

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -4,7 +4,7 @@ USERNAME="admin"
 PASSWORD=$(cat admin_password)
 SERVER_IP=$(cat public_ip)
 SERVER_URL="https://$SERVER_IP"
-CONF_PATH="./conf.json"
+CONF_PATH="./Tests/conf.json"
 SECRET_CONF_PATH=$(cat secret_conf_path)
 
 echo "Starts tests with server url - $SERVER_URL"
