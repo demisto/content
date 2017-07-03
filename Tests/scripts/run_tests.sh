e457579f93e74f76d8289edcc29d1bd21116efce
asafshen <asaf@demisto.com>
2017-07-03 13:08:05 +0300

    fix admin pass

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -1,15 +1,16 @@
 echo "start content tests"
 
 SECRET_CONF_PATH=$(cat secret_conf_path)
-USERNAME="admin"
-PASSWORD=$(cat $SECRET_CONF_PATH | jq '.adminPassword')
 SERVER_IP=$(cat public_ip)
 SERVER_URL="https://$SERVER_IP"
 CONF_PATH="./Tests/conf.json"
+USERNAME="admin"
+PASSWORD=$(cat $SECRET_CONF_PATH | jq '.adminPassword')
 
-echo "PASSWORD - $PASSWORD"
+# remove quotes from password
+temp="${ADMIN_PASSWORD%\"}"
+temp="${temp#\"}"
+ADMIN_PASSWORD=$temp
 
-echo "Starts tests with server url - $SERVER_URL\n"
+echo "Starts tests with server url - $SERVER_URL"
 python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH"
-
-sleep 9m
