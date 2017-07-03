7bde6523e7639d0528f5d7e47982cb45b7c32e21
asafshen <asaf@demisto.com>
2017-07-03 12:25:05 +0300

    get secret conf

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -7,5 +7,9 @@ SERVER_IP=$(cat public_ip)
 SERVER_URL="https://$SERVER_IP"
 CONF_PATH="./Tests/conf.json"
 
+echo "SECRET_CONF_PATH - $SECRET_CONF_PATH"
+echo "PASSWORD - $PASSWORD"
+cat $SECRET_CONF_PATH
+
 echo "Starts tests with server url - $SERVER_URL\n"
 python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH"
