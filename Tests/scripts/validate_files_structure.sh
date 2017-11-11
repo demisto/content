6c6aa1fc3515f70b768791e3cb7d92d7ef4ba574
asafshen <asaf@demisto.com>
2017-11-11 19:44:28 +0200

    scripts fixes

--- Tests/scripts/validate_files_structure.sh
+++ Tests/scripts/validate_files_structure.sh
@@ -44,8 +44,8 @@ validateFilesStructure() {
 
 validateFilesStructure Integrations integration- .yml integration
 validateFilesStructure Playbooks playbook- .yml playbook
-validateFilesStructure Reports report- .json "" report
-validateFilesStructure Scripts script- .yml "" script
+validateFilesStructure Reports report- .json report
+validateFilesStructure Scripts script- .yml script
 validateFilesStructure Misc reputations .json
 
 if [ "$foundWrongName" = true ] || [ "$foundMissingField" = true ]
