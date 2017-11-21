837632ef499e9a295bc1dec46cfe880f0c91ebc6
asafshen <asaf@demisto.com>
2017-11-21 13:21:40 +0200

    move lic to server

--- Tests/scripts/download_demisto_conf.sh
+++ Tests/scripts/download_demisto_conf.sh
@@ -7,11 +7,11 @@ echo "Getting conf from branch $CIRCLE_BRANCH (fallback to master)"
 SECRET_CONF_PATH="./conf_secret.json"
 echo ${SECRET_CONF_PATH} > secret_conf_path
 
-curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json?ref=$CIRCLE_BRANCH" -o "$SECRET_CONF_PATH"
-
 DEMISTO_LIC_PATH="./demisto.lic"
 echo ${DEMISTO_LIC_PATH} > demisto_lic_path
+
+curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
+      --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json?ref=$CIRCLE_BRANCH" -o "$SECRET_CONF_PATH"
 
 NOT_FOUND_MESSAGE=$(cat $SECRET_CONF_PATH | jq '.message')
 
