0a977e92363534968e227efed050dfba7d75d583
asafshen <asaf@demisto.com>
2018-01-27 14:03:33 +0200

    test against transformer args

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -7,7 +7,7 @@ TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
 
 echo "Getting latest build num"
 
-ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=1&filter=successful&$TOKEN_ATTR" | jq '.[0].build_num')
+ARTIFACT_BUILD_NUM=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/transformer-args?limit=1&$TOKEN_ATTR" | jq '.[0].build_num')
 
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/)
 
