d5a42bb0e0cd04f2887e4a7557fe06344c903d96
asafshen <asaf@demisto.com>
2018-02-25 19:51:38 +0200

    add print

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -18,6 +18,7 @@ PASSWORD=$temp
 
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 
+echo "SECRET_CONF_PATH - $SECRET_CONF_PATH"
 
 echo "Starts tests with server url - $SERVER_URL"
 python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY
