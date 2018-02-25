d8a1674d122b56ee9b3031ce7351381d4d7bd3c4
asafshen <asaf@demisto.com>
2018-02-25 14:54:34 +0200

    refactor extra functions

--- Tests/scripts/download_demisto_conf.sh
+++ Tests/scripts/download_demisto_conf.sh
@@ -15,6 +15,19 @@ curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: 
 
 NOT_FOUND_MESSAGE=$(cat $SECRET_CONF_PATH | jq '.message')
 
+download_extra_files() {
+    BRANCH="$1"
+
+    echo "Start downloading extra files from branch $BRANCH.."
+
+    echo "Downloading license file..."
+    curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
+      --location "https://api.github.com/repos/demisto/content-test-conf/contents/demisto.lic?ref=$BRANCH" -o "$DEMISTO_LIC_PATH"
+
+    echo "Finished downloading extra files from branch $BRANCH"
+}
+
+
 if [ "$NOT_FOUND_MESSAGE" != 'null' ]
   then
     echo "Branch $CIRCLE_BRANCH does not exists in content-test-conf repo - downloading from master"
@@ -23,15 +36,10 @@ if [ "$NOT_FOUND_MESSAGE" != 'null' ]
     curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
       --location "https://api.github.com/repos/demisto/content-test-conf/contents/conf.json" -o "$SECRET_CONF_PATH"
 
-    echo "Downloading license file..."
-    curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-      --location "https://api.github.com/repos/demisto/content-test-conf/contents/demisto.lic" -o "$DEMISTO_LIC_PATH"
+    download_extra_files "master"
 
   else
-    echo "Downloading license file..."
-    curl  --header "Accept: application/vnd.github.v3.raw" --header "Authorization: token $GITHUB_TOKEN"  \
-      --location "https://api.github.com/repos/demisto/content-test-conf/contents/demisto.lic?ref=$CIRCLE_BRANCH" -o "$DEMISTO_LIC_PATH"
-
+    download_extra_files $CIRCLE_BRANCH
 fi
 
 echo "Successfully downloaded configuration files"
