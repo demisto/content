08db6ebac1c73fd6926a4690eaf7fa8356edd330
Shani Acht <shaniacht1@gmail.com>
2018-02-26 10:42:44 +0200

    delete most of commonServerJS

--- content_creator.py
+++ content_creator.py
@@ -90,6 +90,8 @@ def main(circle_artifacts):
     shutil.make_archive(ZIP_PRE, 'zip', BUNDLE_PRE)
     shutil.copyfile(ZIP_PRE + '.zip', os.path.join(circle_artifacts, ZIP_PRE + '.zip'))
     shutil.copyfile(ZIP_POST + '.zip', os.path.join(circle_artifacts, ZIP_POST + '.zip'))
+    shutil.copyfile('commonServerPyDoc.json', os.path.join(circle_artifacts, 'commonServerPyDoc.json'))
+    shutil.copyfile('commonServerJsDoc.json', os.path.join(circle_artifacts, 'commonServerJsDoc.json'))
     shutil.copyfile('release-notes.txt', os.path.join(circle_artifacts, 'release-notes.txt'))
 
     print 'finished create content artifact'
