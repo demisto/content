30b926f28601de9a666cea25ecf2f83f6a9bbd72
Shani Acht <shaniacht1@gmail.com>
2018-02-26 13:16:07 +0200

    delete most of commonServerJS

--- commonServerDocs.sh
+++ commonServerDocs.sh
@@ -1,3 +1,3 @@
 python common_server_docs.py
 jsdoc2md --json ./commonServerJsDoc.js > commonServerJsDoc.json
-
+python format_js_common_server.py
---
+++ format_js_common_server.py
@@ -0,0 +1,46 @@
+import abc
+import datetime
+import json
+import sys
+import yaml
+import inspect
+from parinx import parser
+import inspect
+import json
+
+import types
+
+contentLibPath = "./"
+limitedVersion = False
+
+def readFile(filepath):
+    with open(filepath, 'r') as f:
+        out = json.load(f)
+        return out
+    return []
+
+def reformatPythonOutput(output):
+    return output
+
+def main(argv):
+    commonServer = readFile('./commonServerJsDoc.json')
+    x = []
+    for a in commonServer:
+        print a
+        y = {}
+        y["name"] = a.get("name", "")
+        y["description"] = a.get("description", "")
+        returns = a.get("returns", {})[0]
+        y["return"] = {"description" : returns.get("description"), "type":  " or ".join(returns.get("type", {}).get("names", []) ) }
+        for arg in a.get("params", {}):
+            arg["type"] = " or ".join(arg.get("type", {}).get("names", []))
+        y["arguments"] = a.get("params", {})
+
+        x.append(y)
+
+    with open('commonServerJsDoc.json', 'w') as fp:
+        json.dump(x, fp)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
