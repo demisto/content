6379f427779140a5e21e610cfa45b63f1f41654e
Shani Acht <shaniacht1@gmail.com>
2018-03-07 18:39:58 +0200

    typo fix

--- Docs/commonServerDocs.sh
+++ Docs/commonServerDocs.sh
@@ -1,4 +1,4 @@
-python ./Docs/extract_common_Server_js.py
+python ./Docs/extract_common_server_js.py
 sleep 5
 jsdoc2md --json ./Docs/commonServerJsDoc.js > ./Docs/commonServerJsDoc.json
 python ./Docs/common_server_docs.py
