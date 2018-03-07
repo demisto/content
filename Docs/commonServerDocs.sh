e225d79e77d4430a2d58b2d565f5204f4df6809d
Shani Acht <shaniacht1@gmail.com>
2018-03-07 16:55:02 +0200

    added validation for commonserverjs doc output

--- Docs/commonServerDocs.sh
+++ Docs/commonServerDocs.sh
@@ -1,3 +1,3 @@
-python ./Docs/common_server_docs.py
+python ./Docs/extract_common_Server_js.py
 jsdoc2md --json ./Docs/commonServerJsDoc.js > ./Docs/commonServerJsDoc.json
-python ./Docs/format_js_common_server.py
+python ./Docs/common_server_docs.py
