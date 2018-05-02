63a9b245999379018c53ad919d536f9dc66160d9
bars <bar@demisto.com>
2018-05-02 16:49:09 +0300

    get more data

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -8,6 +8,7 @@ TOKEN_ATTR="circle-token=$SERVER_CI_TOKEN"
 echo "Getting latest build num"
 TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=5&filter=successful&$TOKEN_ATTR")
 
+echo $SERVER_API_URI/tree/master?limit=5&filter=successful&$TOKEN_ATTR
 echo $TEMP
 ARTIFACT_BUILD_NUM=
 for i in `seq 0 4`; do
