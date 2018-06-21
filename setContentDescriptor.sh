7a091e989185afe3084f13c33b3c03033ef77648
asafshen <asaf@demisto.com>
2018-06-21 12:13:12 +0300

    add debugging

--- setContentDescriptor.sh
+++ setContentDescriptor.sh
@@ -1,8 +1,13 @@
 ### this script populate content descriptor with correct dates and assetId
 
-git diff --name-status $2 > changelog.txt
-git diff  --diff-filter=D $2 > delete-changelog.txt
+echo "got 2 - $2"
 
+echo "before 1"
+git diff --name-status $2 > changelog.txt
+
+echo "before 2"
+git diff  --diff-filter=D $2 > delete-changelog.txt
+echo "after 2"
 ASSETID=$1
 VERSION=$3
 
