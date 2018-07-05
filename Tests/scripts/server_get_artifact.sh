52023c5504256a69602b472daa31239b2df99374
bars92 <bar@demisto.com>
2018-07-05 11:25:55 +0300

    try fix curl bad characters

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -19,7 +19,8 @@ if [[ "$ARTIFACT_BUILD_NUM" = "" ]]; then
 fi
 
 SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/)
-SERVER_DOWNLOAD_LINK=${${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}%$'\r'}
+TEMP_LINK = ${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}
+SERVER_DOWNLOAD_LINK=${TEMP_LINK%$'\r'}
 
 echo "Getting server artifact for build: ${ARTIFACT_BUILD_NUM}"
 echo "curl to ${SERVER_DOWNLOAD_LINK}"
