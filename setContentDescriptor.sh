2929b02e604ebb1aedd05cfb3af51ebbc948f2f6
asafshen <asaf@demisto.com>
2018-07-09 10:44:43 +0300

    print commands outputs

--- setContentDescriptor.sh
+++ setContentDescriptor.sh
@@ -1,8 +1,13 @@
 ### this script populate content descriptor with correct dates and assetId
 
-git diff --name-status $2 > changelog.txt
-git diff  --diff-filter=D $2 > delete-changelog.txt
 
+output=$(git diff --name-status $2 > changelog.txt)
+echo "1 - $output"
+output=$(git diff  --diff-filter=D $2 > delete-changelog.txt)
+echo "2 - $output"
+
+output=$(ls)
+echo "3 - $output"
 ASSETID=$1
 VERSION=$3
 
