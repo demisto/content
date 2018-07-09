b24847cf095fb399a8efe055002fcfbbaeba3ccf
asafshen <asaf@demisto.com>
2018-07-09 12:17:27 +0300

    remove prints

--- setContentDescriptor.sh
+++ setContentDescriptor.sh
@@ -4,12 +4,10 @@ git diff --name-status $2 &> changelog.txt
 git diff  --diff-filter=D $2 &> delete-changelog.txt
 
 if grep -q "fatal: bad object" changelog.txt || grep -q "fatal: bad object" delete-changelog.txt; then
+    # if someone has deleted the branch of the compared git hash - git diff will fail silently
     echo "diff operation failed. make sure the compared branch is exists"
     exit 1
 fi
-
-echo "###"
-cat changelog.txt
 
 ASSETID=$1
 VERSION=$3
