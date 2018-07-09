efdcd4a73a14c3cd99cb5f62ab1f027f1ef55280
asafshen <asaf@demisto.com>
2018-07-09 10:54:08 +0300

    check if files exist

--- setContentDescriptor.sh
+++ setContentDescriptor.sh
@@ -1,13 +1,13 @@
 ### this script populate content descriptor with correct dates and assetId
 
+git diff --name-status $2 > changelog.txt
+git diff  --diff-filter=D $2 > delete-changelog.txt
+if [ ! -f changelog.txt ] || [ ! -f delete-changelog.txt ]
+then
+    echo "Change log files are not exist"
+    exit 1
+fi
 
-output=$(git diff --name-status $2 > changelog.txt)
-echo "1 - $output"
-output=$(git diff  --diff-filter=D $2 > delete-changelog.txt)
-echo "2 - $output"
-
-output=$(ls)
-echo "3 - $output"
 ASSETID=$1
 VERSION=$3
 
