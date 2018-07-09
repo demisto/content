0c6454f6e7347dcd79f690c934c37b9553b6a430
asafshen <asaf@demisto.com>
2018-07-09 10:56:05 +0300

    refactor

--- setContentDescriptor.sh
+++ setContentDescriptor.sh
@@ -1,8 +1,10 @@
 ### this script populate content descriptor with correct dates and assetId
 
-git diff --name-status $2 > changelog.txt
-git diff  --diff-filter=D $2 > delete-changelog.txt
-if [ ! -f changelog.txt ] || [ ! -f delete-changelog.txt ]
+CHANGE_LOG="changelog.txt"
+DELETE_CHANGE_LOG="delete-changelog.txt"
+git diff --name-status $2 > "$CHANGE_LOG"
+git diff  --diff-filter=D $2 > "$DELETE_CHANGE_LOG"
+if [ ! -f "$CHANGE_LOG" ] || [ ! -f "$DELETE_CHANGE_LOG" ]
 then
     echo "Change log files are not exist"
     exit 1
@@ -11,4 +13,4 @@ fi
 ASSETID=$1
 VERSION=$3
 
-python release_notes.py  $VERSION changelog.txt delete-changelog.txt $ASSETID
+python release_notes.py  $VERSION "$CHANGE_LOG" "$DELETE_CHANGE_LOG" $ASSETID
