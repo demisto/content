e338793e1bacffe7e9ce3b9e68687e582ac0782f
shaiyaakovi <yaakovi@demisto.com>
2018-08-22 09:47:59 +0300

    added missing fields

--- Tests/scripts/validate_files_structure.py
+++ Tests/scripts/validate_files_structure.py
@@ -73,7 +73,7 @@ class LOG_COLORS:
 
 # print srt in the given color
 def print_color(str, color):
-    print(color + str + LOG_COLORS.NATIVE)
+    print('%s%s%s' % (color, str, LOG_COLORS.NATIVE, ))
 
 
 def print_error(error_str):
