7a32f2775de469f5b8c5cefaac85a697c2d8bc52
hirshbergshachar <shachar@demisto.com>
2018-08-27 15:47:27 +0300

    fix error color (#2030)

--- Tests/scripts/validate_files_structure.py
+++ Tests/scripts/validate_files_structure.py
@@ -76,8 +76,8 @@ class LOG_COLORS:
     GREEN = '\033[01;32m'
 
 # print srt in the given color
-def print_color(str, color):
-    print(color + str + LOG_COLORS.NATIVE)
+def print_color(msg, color):
+    print(str(color) +str(msg) + LOG_COLORS.NATIVE)
 
 def print_error(error_str):
     print_color(error_str, LOG_COLORS.RED)
