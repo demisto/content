67623886877c5cd16224e78588f952d02b5a4fa7
ronykoz <ronykoz@gmail.com>
2018-10-05 16:01:43 +0300

    slack notifier

--- Tests/scripts/slack_notifier.sh
+++ Tests/scripts/slack_notifier.sh
@@ -6,4 +6,4 @@ PRIVATE_CONF_PATH=$(cat private_conf_path)
 
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 
-python ./Tests/scripts/slack_notifier.py -n $IS_NIGHTLY -u $CIRCLE_BUILD_URL -b $CIRCLE_BUILD_NUM -i $CIRCLE_USERNAME -s $PRIVATE_CONF_PATH
+python ./Tests/scripts/slack_notifier.py -n $IS_NIGHTLY -u "$CIRCLE_BUILD_URL" -b "$CIRCLE_BUILD_NUM" -i "$CIRCLE_USERNAME" -s "$PRIVATE_CONF_PATH"
