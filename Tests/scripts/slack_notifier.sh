9354d7bc3f0abbeecca657aa8598e5454abc0bbb
ronykoz <ronykoz@gmail.com>
2018-10-05 15:28:52 +0300

    fixing slack_notifier calling

---
+++ Tests/scripts/slack_notifier.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "start slack notifier"
+
+PRIVATE_CONF_PATH=$(cat private_conf_path)
+
+[ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
+
+python ./Tests/scripts/slack_notifier.py -n $IS_NIGHTLY -l $CIRCLE_BUILD_URL -b $CIRCLE_BUILD_NUM -i $CIRCLE_USERNAME -s $PRIVATE_CONF_PATH
