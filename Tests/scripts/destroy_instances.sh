ba80338fd68572e09769b60292413b032ef5cd51
ronykoz <ronykoz@gmail.com>
2018-10-16 13:13:01 +0300

    Automatic filter mechanism for content build

--- Tests/scripts/destroy_instances.sh
+++ Tests/scripts/destroy_instances.sh
@@ -10,7 +10,7 @@ else
 
    # collect log file to artifacts
    PUBLIC_IP=$(cat public_ip)
-   scp ${USER}@${PUBLIC_IP}:/var/log/demisto/server.log $1
+   scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${USER}@${PUBLIC_IP}:/var/log/demisto/server.log $1
 
    #destroy instance
    echo "Terminating instance: ${INSTANCE_ID}"
