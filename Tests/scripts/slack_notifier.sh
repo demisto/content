4a8b85f2e9f1c44b4f147b8e4f4835b42485e4bd
ronykoz <ronykoz@gmail.com>
2018-10-17 14:54:52 +0300

    Fixing configure_tests

--- Tests/scripts/slack_notifier.sh
+++ Tests/scripts/slack_notifier.sh
@@ -8,8 +8,6 @@ PASSWORD=$(cat $SECRET_CONF_PATH | jq '.adminPassword')
 SECRET_CONF_PATH=$(cat secret_conf_path)
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 
-python ./Tests/scripts/slack_notifier.py -n $IS_NIGHTLY -u "$CIRCLE_BUILD_URL" -b "$CIRCLE_BUILD_NUM" -s "$SLACK_TOKEN" -c "$CIRCLECI_TOKEN"
-
 python ./Tests/scripts/instance_notifier.py -n $IS_NIGHTLY -s "$SLACK_TOKEN" -c "$CIRCLECI_TOKEN" -e "$SECRET_CONF_PATH" -u "$USERNAME" -p "$PASSWORD"
 
 echo "Finished slack notifier execution"
