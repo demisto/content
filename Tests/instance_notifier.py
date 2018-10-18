e790b18f488587781d63620dfbdab31d65fc18eb
ronykoz <ronykoz@gmail.com>
2018-10-18 13:26:45 +0300

    Update slack channel

--- Tests/instance_notifier.py
+++ Tests/instance_notifier.py
@@ -130,25 +130,24 @@ def slack_notifier(slack_token, secret_conf_path, server, user, password):
     branch_name_reg = re.search("\* (.*)", branches)
     branch_name = branch_name_reg.group(1)
 
-    # if branch_name == 'master':
-    print_color("Starting Slack notifications about instances", LOG_COLORS.GREEN)
-    attachments, integrations_counter = get_attachments(secret_conf_path, server, user, password)
+    if branch_name == 'master':
+        print_color("Starting Slack notifications about instances", LOG_COLORS.GREEN)
+        attachments, integrations_counter = get_attachments(secret_conf_path, server, user, password)
 
-    sc = SlackClient(slack_token)
-    sc.api_call(
-        "chat.postMessage",
-        channel="test_slack",
-        username="Instances nightly report",
-        as_user="False",
-        attachments=attachments,
-        text="You have {0} instances configurations".format(integrations_counter)
-    )
+        sc = SlackClient(slack_token)
+        sc.api_call(
+            "chat.postMessage",
+            channel="devops-events",
+            username="Instances nightly report",
+            as_user="False",
+            attachments=attachments,
+            text="You have {0} instances configurations".format(integrations_counter)
+        )
 
 
 if __name__ == "__main__":
     options = options_handler()
-    slack_notifier(options.slack, options.secret, options.server, options.user, options.password)
-    # if options.nightly:
-    #     pass
-    # else:
-    #     print_color("Not nightly build, stopping Slack Notifications about instances", LOG_COLORS.RED)
+    if options.nightly:
+        pass
+    else:
+        print_color("Not nightly build, stopping Slack Notifications about instances", LOG_COLORS.RED)
