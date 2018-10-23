bbc4809b63b4c78ca4908bff55b290ba23dee04f
ronykoz <ronykoz@gmail.com>
2018-10-23 23:51:22 +0300

    Testing content version for AMI

--- Tests/scripts/run_installer_on_instance.sh
+++ Tests/scripts/run_installer_on_instance.sh
@@ -29,8 +29,9 @@ scp content_new.zip ${USER}@${PUBLIC_IP}:~/content
 scp -r ./TestPlaybooks/* ${USER}@${PUBLIC_IP}:~/TestPlaybooks
 
 # override exiting content with current
-COPY_CONTENT_COMMAND="sudo unzip -o ~/content/content_new.zip -d /usr/local/demisto/res \
-    && sudo cp ~/TestPlaybooks/* /usr/local/demisto/res"
+#COPY_CONTENT_COMMAND="sudo unzip -o ~/content/content_new.zip -d /usr/local/demisto/res \
+#    && sudo cp ~/TestPlaybooks/* /usr/local/demisto/res"
+COPY_CONTENT_COMMAND="sudo cp ~/TestPlaybooks/* /usr/local/demisto/res"
 ssh -t ${USER}@${PUBLIC_IP} ${COPY_CONTENT_COMMAND}
 
 echo "start server"
