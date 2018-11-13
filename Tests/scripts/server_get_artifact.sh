a64d3cb9955cc833160bea82e4d32d6cb2625001
yuvalbenshalom <yuval@demisto.com>
2018-11-13 14:31:07 +0200

    Update server_get_artifact.sh

--- Tests/scripts/server_get_artifact.sh
+++ Tests/scripts/server_get_artifact.sh
@@ -3,23 +3,16 @@ SERVER_API_URI="https://circleci.com/api/v1/project/demisto/server"
 TOKEN_ATTR="circle-token=$1"
 
 echo "Getting latest build num"
-TEMP=$(curl -s -H "$ACCEPT_TYPE" "$SERVER_API_URI/tree/master?limit=10&filter=successful&$TOKEN_ATTR")
 
-ARTIFACT_BUILD_NUM=
-for i in `seq 0 9`; do
-    if [[ $(echo "$TEMP" | jq ".[$i].build_parameters") == "null" ]]; then
-        ARTIFACT_BUILD_NUM=$(echo "$TEMP" | jq ".[$i].build_num")
-        break
-    fi
-done
 
-ARTIFACT_BUILD_NUM="32097"
+ARTIFACT_BUILD_NUM=32097
 if [[ "$ARTIFACT_BUILD_NUM" = "" ]]; then
     echo "couldn't find successful build"
     exit 1
 fi
 
-SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url' -r | grep demistoserver | grep /0/ | head -n 1)
+SERVER_DOWNLOAD_LINK=$(curl -s -H "$ACCEPT_TYPE" ${SERVER_API_URI}/${ARTIFACT_BUILD_NUM}/artifacts?${TOKEN_ATTR} | jq '.[].url'-r | grep demistoserver | grep /0/ | head -n 1)
+echo $SERVER_DOWNLOAD_LINK
 TEMP_LINK=${SERVER_DOWNLOAD_LINK}?${TOKEN_ATTR}
 SERVER_DOWNLOAD_LINK=${TEMP_LINK%$'\r'}
 
