6746a488c9ec0f650b52bfdd295fe447d4baa38e
ronykoz <ronykoz@gmail.com>
2018-11-17 19:29:42 +0200

    Fixing regex

--- Tests/scripts/validate_files_structure.py
+++ Tests/scripts/validate_files_structure.py
@@ -176,7 +176,7 @@ def validate_schema(file_path, matching_regex=None):
 
 def changed_id(file_path):
     change_string = run_git_command("git diff HEAD {0}".format(file_path))
-    if re.search("[+-]( )?id: .*", change_string):
+    if re.search("[+-](  )?id: .*", change_string):
         print_error("You've changed the ID of the file {0} please undo.".format(file_path))
         return True
 
