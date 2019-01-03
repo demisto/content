c7b17c0bf1d52d40e1db12ffbb0c718f7c0562b7
Lior Blob <liorb@demisto.com>
2019-01-03 17:14:10 +0200

    indent

--- Tests/scripts/configure_tests.py
+++ Tests/scripts/configure_tests.py
@@ -111,7 +111,7 @@ def get_modified_files(files_string):
             elif re.match(CONF_REGEX, file_path, re.IGNORECASE):
                 is_conf_json = True
             elif file_status.lower() == 'm' and 'id_set.json' not in file_path:
-                           all_tests.append(file_path)
+                all_tests.append(file_path)
 
     return modified_files_list, modified_tests_list, all_tests, is_conf_json
 
