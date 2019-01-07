dcb3194eab783c8dbe62c7ff9a3f11f0ead75032
bars92 <bar@demisto.com>
2019-01-07 18:30:39 +0200

    try wait for server using health

--- Tests/scripts/wait_until_server_ready.sh
+++ Tests/scripts/wait_until_server_ready.sh
@@ -3,24 +3,21 @@ set -e
 
 SERVER_IP=$(cat public_ip)
 SERVER_URL="https://$SERVER_IP"
-GET_HTTP_CODE_COMMAND="curl --write-out %{http_code} --silent --output /dev/null $SERVER_URL/user -k"
 
-NEXT_WAIT_TIME=0
-HTTP_CODE=$($GET_HTTP_CODE_COMMAND)
-
-MAX_TRIES=20
-TRY_COUNT=1
-until [ $HTTP_CODE != 433 ] || [ $TRY_COUNT = $MAX_TRIES ]; do
-    echo "server is not yet ready - wait another 45 seconds"
-    sleep 45s
-    ((TRY_COUNT++))
-    HTTP_CODE=$($GET_HTTP_CODE_COMMAND)
+CHECKURL="https://$SERVER_IP:443/health/server"
+count=0
+stcode=$(curl -i --write-out "%{http_code}" --silent --insecure --output /dev/null "$CHECKURL")
+while [ $stcode -ne "200" ]
+do
+    echo "Sending to $CHECKURL, response code: $stcode"
+    printf 'Waiting for server to start...\n'
+    sleep 2
+    if [ $count -eq 1200 ]
+    then
+        printf 'Gave up, server is down...\n'
+        exit 1
+    fi;
+    stcode=$(curl -i --write-out "%{http_code}" --silent --insecure --output /dev/null "$CHECKURL")
+    ((count++))
 done
-
-if [ $HTTP_CODE = 433 ]
-then
-    echo "Server is not ready :("
-    exit 1
-fi
-
-echo "Server is ready :)"
+exit 0
