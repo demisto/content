3fb0410c5e3fb8fff225d50daddc8c632f21fc79
Rony Kozakish <37589583+ronykoz@users.noreply.github.com>
2019-01-22 17:41:17 +0200

    Add integration filtering (#2867)
    * updating test filter
    
    * updating id_set creation
    
    * update id_set
    
    * update id_set
    
    * fixing lint
    
    * PR notes

--- Tests/scripts/update_id_set.py
+++ Tests/scripts/update_id_set.py
@@ -139,7 +139,7 @@ def get_task_ids_from_playbook(param_to_enrich_by, data_dict):
 
 
 def get_commmands_from_playbook(data_dict):
-    commands = set([])
+    command_to_integration = {}
     tasks = data_dict.get('tasks', [])
 
     for task in tasks.values():
@@ -147,10 +147,12 @@ def get_commmands_from_playbook(data_dict):
 
         command = task_details.get('script')
         if command:
-            command = command.split('|')[-1]
-            commands.add(command)
+            splitted_cmd = command.split('|')
 
-    return list(commands)
+            if 'Builtin' not in command:
+                command_to_integration[splitted_cmd[-1]] = splitted_cmd[0]
+
+    return command_to_integration
 
 
 def get_integration_data(file_path):
@@ -189,7 +191,7 @@ def get_playbook_data(file_path):
     fromversion = data_dictionary.get('fromversion')
     implementing_scripts = get_task_ids_from_playbook('scriptName', data_dictionary)
     implementing_playbooks = get_task_ids_from_playbook('playbookName', data_dictionary)
-    implementing_commands = get_commmands_from_playbook(data_dictionary)
+    command_to_integration = get_commmands_from_playbook(data_dictionary)
 
     playbook_data['name'] = name
     if toversion:
@@ -200,8 +202,8 @@ def get_playbook_data(file_path):
         playbook_data['implementing_scripts'] = implementing_scripts
     if implementing_playbooks:
         playbook_data['implementing_playbooks'] = implementing_playbooks
-    if implementing_commands:
-        playbook_data['implementing_commands'] = implementing_commands
+    if command_to_integration:
+        playbook_data['command_to_integration'] = command_to_integration
     if tests:
         playbook_data['tests'] = tests
 
