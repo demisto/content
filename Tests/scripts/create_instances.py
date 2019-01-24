ca3f9423032f9edc8456d7bde76903df2e554753
ronykoz <ronykoz@gmail.com>
2019-01-24 11:49:26 +0200

    temp commit

--- Tests/scripts/create_instances.py
+++ Tests/scripts/create_instances.py
@@ -61,9 +61,9 @@ def is_nightly_build():
 
 def create_instance(ami_name):
     print "Creating instance from the AMI image for {}".format(AMI_NAME_TO_READABLE[ami_name])
-    _ = run_bash_command("ami_name=\"{}\"".format(ami_name), is_shell=True)  # noqa
+    _ = run_bash_command("AMI_NAME=\"{}\"".format(ami_name), is_shell=True)  # noqa
     _ = run_bash_command("./Tests/scripts/create_instance.sh instance.json {}".format(ami_name))  # noqa
-    # instance_id = run_bash_command("echo ${ami_name}")
+    instance_id = run_bash_command("echo $AMI_NAME")
     return instance_id
 
 
