1db22eabcd4a3f94e6045cb860b7055736de093c
yardensade <yardenkasade@gmail.com>
2019-01-28 13:28:10 +0200

    Added a secret detection in file validation

--- Tests/scripts/validate_files_structure.py
+++ Tests/scripts/validate_files_structure.py
@@ -25,6 +25,9 @@ except ImportError:
 import re
 import os
 import json
+import io
+import math
+import string
 import argparse
 from subprocess import Popen, PIPE
 from distutils.version import LooseVersion
@@ -96,6 +99,10 @@ SCHEMAS_PATH = "Tests/schemas/"
 
 DIRS = [INTEGRATIONS_DIR, SCRIPTS_DIR, PLAYBOOKS_DIR, REPORTS_DIR, DASHBOARDS_DIR, WIDGETS_DIR, INCIDENT_FIELDS_DIR,
         LAYOUTS_DIR, CLASSIFIERS_DIR, MISC_DIR]
+
+# secrets white list
+ENTROPY_THRESHOLD = 3.4
+SECRETS_WHITE_LIST_FILE = 'secrets_white_list'
 
 
 class LOG_COLORS:
@@ -464,6 +471,20 @@ def integration_valid_in_id_set(file_path, integration_set):
 
 
 def validate_committed_files(branch_name, is_circle):
+
+    # Get all files that have been modified & added to repo
+    secrets_file_paths = get_all_diff_text_files(branch_name, is_circle)
+    secrets_found = search_potential_secrets(secrets_file_paths)
+
+    if secrets_found:
+        print_error('Secrets were found in the following files:\n')
+        for file_name in secrets_found:
+            print_error('\nFile Name: ' + file_name)
+            print_error(json.dumps(secrets_found[file_name], indent=4))
+        print_error('Remove or whitelist secrets in order to proceed, then re-commit\n')
+
+        sys.exit(1)
+
     modified_files, added_files = get_modified_and_added_files(branch_name, is_circle)
     with open('./Tests/id_set.json', 'r') as id_set_file:
         id_set = json.load(id_set_file)
@@ -680,6 +701,222 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def is_text_file(file_path):
+    file_extension = os.path.splitext(file_path)[1]
+    text_file_types = set(['.yml', '.py', '.json', '.md', '.txt', '.sh', '.ini', '.eml', '', '.csv'])
+    if file_extension in text_file_types:
+            return True
+    return False
+
+
+def get_diff_text_files(files_string):
+    accepted_file_statuses = ['M', 'A']
+    all_files = files_string.split('\n')
+    text_files_list = set([])
+    for file_name in all_files:
+        file_data = file_name.split()
+        if not file_data:
+            continue
+
+        file_status = file_data[0]
+        file_path = file_data[1]
+
+        if file_status.upper() in accepted_file_statuses and is_text_file(file_path) and SECRETS_WHITE_LIST_FILE not in file_path:
+            text_files_list.add(file_path)
+
+    return text_files_list
+
+
+def get_all_diff_text_files(branch_name, is_circle):
+
+    if is_circle:
+        branch_changed_files_string = run_git_command("git diff --name-status origin/master...{}".format(branch_name))
+        text_files_list = get_diff_text_files(branch_changed_files_string)
+
+    else:
+        local_changed_files_string = run_git_command("git diff --name-status --no-merges HEAD")
+        text_files_list = get_diff_text_files(local_changed_files_string)
+
+    return text_files_list
+
+
+def validate_committed_files(branch_name, is_circle):
+
+    # secrets validation starts
+    # Get all files that have been modified & added to repo
+    secrets_file_paths = get_all_diff_text_files(branch_name, is_circle)
+    secrets_found = search_potential_secrets(secrets_file_paths)
+
+    if secrets_found:
+        print_error('Secrets were found in the following files:\n')
+        for file in secrets_found:
+            print_error('\nFile Name: ' + file)
+            print_error(json.dumps(secrets_found[file], indent=4))
+        print_error('Remove or whitelist secrets in order to proceed, then re-commit\n')
+
+        sys.exit(1)
+    # secrets validation ends
+
+    modified_files, added_files = get_modified_and_added_files(branch_name, is_circle)
+
+    with open('./Tests/id_set.json', 'r') as id_set_file:
+        id_set = json.load(id_set_file)
+
+    script_set = id_set['scripts']
+    playbook_set = id_set['playbooks']
+    integration_set = id_set['integrations']
+    test_playbook_set = id_set['TestPlaybooks']
+
+    has_schema_problem = validate_modified_files(integration_set, modified_files,
+                                                 playbook_set, script_set, test_playbook_set, is_circle)
+
+    has_schema_problem = validate_added_files(added_files, integration_set, playbook_set,
+                                              script_set, test_playbook_set, is_circle) or has_schema_problem
+
+    if has_schema_problem:
+        sys.exit(1)
+
+
+def search_potential_secrets(secrets_file_paths):
+    """
+
+    :param secrets_file_paths: paths of files that are being commited to git repo
+    :return: dictionary of strings sorted by file name for secrets found in files
+    """
+    # Get generic white list set
+    with io.open('./Tests/secrets_white_list.json', mode="r", encoding="utf-8") as secrets_white_list_file:
+        secrets_white_list = set(json.load(secrets_white_list_file))
+
+    secrets_found = {}
+
+    for file_path in secrets_file_paths:
+        file_name = file_path.split('/')[-1]
+        high_entropy_strings = []
+        regex_secrets = []
+
+        # if py file, search for yml in order to retrieve temp white list
+        file_path_temp, file_extension = os.path.splitext(file_path)
+        if file_extension == '.py':
+            matching_yml_file_contents = retrieve_related_yml(file_path_temp)
+
+        # Open each file, read its contents in UTF-8 encoding to avoid unicode characters
+        with io.open('./' + file_path, mode="r", encoding="utf-8") as file:
+            file_contents = file.read()
+
+            # Add all context output paths keywords to whitelist temporary
+            temp_white_list = create_temp_white_list(matching_yml_file_contents if matching_yml_file_contents else file_contents)
+            secrets_white_list = secrets_white_list.union(temp_white_list)
+
+            # Search by lines after strings with high entropy as possibly suspicious
+            for line in set(file_contents.split('\n')):
+
+                # if detected disable-secrets comment, skip the line
+                if bool(re.findall(r'(disable-secrets-detection)', line)):
+                    file_contents = file_contents.replace(line, '')
+                    continue
+
+                # REGEX scanning for IOCs and false positive groups
+                potential_secrets, false_positives = regex_for_secrets(line)
+                for potential_secret in potential_secrets:
+                    if not any(white_list_string in potential_secret for white_list_string in secrets_white_list):
+                        regex_secrets.append(potential_secret)
+                # added false positives into white list array before testing the strings in line
+                secrets_white_list = secrets_white_list.union(false_positives)
+
+                # calculate entropy for each string in the file
+                for string in line.split():
+                    string = string.strip("\"()[],'><:;\\")
+                    string_lower = string.lower()
+                    # compare the lower case of the string against both generic whitelist & temp white list
+                    if not any(white_list_string in string_lower for white_list_string in secrets_white_list):
+                        entropy = calculate_shannon_entropy(string)
+                        if entropy >= ENTROPY_THRESHOLD:
+                            high_entropy_strings.append(string)
+
+        if high_entropy_strings or regex_secrets:
+            # uniquify identical matches between lists
+            file_secrets = list(set(high_entropy_strings + regex_secrets))
+            secrets_found[file_name] = file_secrets
+
+    return secrets_found
+
+
+def create_temp_white_list(file_contents):
+    temp_white_list = set([])
+    context_paths = re.findall(r'contextPath: (\S+\.+\S+)', file_contents)
+    for context_path in context_paths:
+        context_path = context_path.split('.')
+        context_path = [white_item.lower() for white_item in context_path]
+        temp_white_list = temp_white_list.union(context_path)
+
+    return temp_white_list
+
+
+def retrieve_related_yml(file_path_temp):
+    matching_yml_file_contents = None
+    yml_file = file_path_temp + '.yml'
+    if os.path.exists(yml_file):
+        with io.open('./' + yml_file, mode="r", encoding="utf-8") as matching_yml_file:
+            matching_yml_file_contents = matching_yml_file.read()
+    return matching_yml_file_contents
+
+
+def regex_for_secrets(file_contents):
+    """
+    Scans for IOCs with potentially low entropy score
+    :param file_contents: file to test as string representation (string)
+    :return  potential_secrets (list) IOCs found via regex, false_positives (list) Non secrets with high entropy
+    """
+    potential_secrets = []
+    false_positives = []
+
+    # URL REGEX
+    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', file_contents)
+    if urls:
+        potential_secrets += urls
+    # EMAIL REGEX
+    emails = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', file_contents)
+    if emails:
+        potential_secrets += emails
+    # IPV6 REGEX
+    ipv6_list = re.findall(r'(?:(?:[0-9A-Fa-f]{1,4}:){6}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|::(?:[0-9A-Fa-f]{1,4}:){5}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){4}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){3}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,2}[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){2}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,3}[0-9A-Fa-f]{1,4})?::[0-9A-Fa-f]{1,4}:(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,4}[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,5}[0-9A-Fa-f]{1,4})?::[0-9A-Fa-f]{1,4}|(?:(?:[0-9A-Fa-f]{1,4}:){,6}[0-9A-Fa-f]{1,4})?::)', file_contents)
+    if ipv6_list:
+        for ipv6 in ipv6_list:
+            if ipv6 != '::':
+                potential_secrets.append(ipv6)
+    # IPV4 REGEX
+    ipv4_list = re.findall(r'(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', file_contents)
+    if ipv4_list:
+        potential_secrets += ipv4_list
+    # Dates REGEX for false positive preventing since they have high entropy
+    dates = re.findall(r'((\d{4}[/.-]\d{2}[/.-]\d{2})[T\s](\d{2}:?\d{2}:?\d{2}:?(\.\d{5,6})?([+-]\d{2}:?\d{2})?Z?)?)', file_contents)
+    if dates:
+        false_positives += [date[0] for date in dates]
+
+    if potential_secrets and potential_secrets[0] == '::':
+        raise Exception(file_contents, potential_secrets)
+    return potential_secrets, false_positives
+
+
+def calculate_shannon_entropy(data):
+    """
+    Algorithm to determine the randomness of a given data.
+    :param data: could be either a list/dict or a string.
+    :return: entropy score, higher is more random/complex, most English words will yield result of around 3+-
+    """
+    if not data:
+        return 0
+    entropy = 0
+    # each unicode code representation of all characters which are considered printable
+    for x in (ord(c) for c in string.printable):
+        # probability of event X
+        px = float(data.count(chr(x)))/len(data)
+        if px > 0:
+            # the information in every possible news, in bits
+            entropy += - px * math.log(px, 2)
+    return entropy
 
 
 def main():
