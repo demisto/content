1135a2ff48a9734f51469e3b26ceef584636f93c
benparadise <benjoparadise@gmail.com>
2019-01-31 14:09:00 +0200

    Fixed github connection.

--- Tests/scripts/clone_mocks.sh
+++ Tests/scripts/clone_mocks.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-cat > ~/.ssh/config << EOF
-host github.com
- HostName github.com
- IdentityFile $1
- User git
-EOF
-
-chmod 600 ~/.ssh/config
+ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 git clone git@github.com:demisto/content-test-data.git
