9a079f66f7d37219a884a213e53fa28ca753327c
ronykoz <ronykoz@gmail.com>
2019-02-15 15:45:16 +0200

    fixing lint

--- Tests/scripts/run_content_installation.py
+++ Tests/scripts/run_content_installation.py
@@ -36,16 +36,23 @@ def main():
         ami_instances = instance_file.readlines()
         ami_instances = [line.strip('\n').split(":") for line in ami_instances]
 
+    id_to_ip = {}
     for ami_instance_name, ami_instance_id in ami_instances:
-        print "running content installation for ami instance: {}".format(ami_instance_name)
+        print "Validating ami instance: {}".format(ami_instance_name)
         run_bash_command("./Tests/scripts/get_instance_ip.sh {}".format(ami_instance_id))
         with open('./Tests/instance_ips.txt', 'r') as instance_file:
             instance_ip = instance_file.read()
 
-        print("Waiting 90 Seconds for SSH to start")
-        sleep(90)
-        run_bash_command("./Tests/scripts/copy_content_data.sh {}".format(instance_ip))
-        instance_ips.append("{}:{}".format(ami_instance_name, instance_ip))
+        print("The ip is {} ".format(instance_ip))
+        id_to_ip[ami_instance_id] = instance_ip
+
+    print("Waiting 90 Seconds for SSH to start")
+    sleep(90)
+
+    for ami_instance_name, ami_instance_id in ami_instances:
+        print "Running content installation for ami instance: {}".format(ami_instance_name)
+        run_bash_command("./Tests/scripts/copy_content_data.sh {}".format(id_to_ip[ami_instance_id]))
+        instance_ips.append("{}:{}".format(ami_instance_name, id_to_ip[ami_instance_id]))
 
     with open('./Tests/instance_ips.txt', 'w') as instance_file:
         instance_file.write('\n'.join(instance_ips))
