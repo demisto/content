c135c6e7827f9a0e2b93f3c2d7124b1ee216cdda
ronykoz <ronykoz@gmail.com>
2019-02-15 13:06:44 +0200

    fixing server installtion

--- Tests/scripts/create_instances.py
+++ Tests/scripts/create_instances.py
@@ -62,7 +62,7 @@ def create_instance(ami_name):
     print "Creating instance from the AMI image for {}".format(AMI_NAME_TO_READABLE[ami_name])
     run_bash_command("AMI_NAME=\"{}\"".format(ami_name), is_shell=True)  # noqa
     run_bash_command("./Tests/scripts/create_instance.sh instance.json {}".format(ami_name))  # noqa
-    instance_id = run_bash_command("echo $AMI_NAME")
+    instance_id = run_bash_command("echo ${AMI_NAME}")
     return instance_id
 
 
--- Tests/scripts/run_content_installation.py
+++ Tests/scripts/run_content_installation.py
@@ -37,7 +37,7 @@ def main():
 
     for ami_instance_name, ami_instance_id in ami_instances:
         print "running content installation for ami instance: {}".format(ami_instance_name)
-        _ = run_bash_command("./Tests/scripts/run_installer_on_instance.sh {}".format(ami_instance_id))  # noqa
+        run_bash_command("./Tests/scripts/run_installer_on_instance.sh {}".format(ami_instance_id))  # noqa
         instance_ips.append("{}:{}".format(ami_instance_name, run_bash_command("echo ${PUBLIC_IP}")))
 
     with open('./Tests/instance_ips.txt', 'w') as instance_file:
