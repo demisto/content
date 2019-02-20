17038f97a6c96992d61ee7cf2bdace8d5ef17eb6
benparadise <benjoparadise@gmail.com>
2019-02-20 17:12:29 +0200

    Added missing files from branch mock-instances

---
+++ Tests/scripts/upload_mocks.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+cat >~/.gitconfig << EOF
+[user]
+       name = EC2 Default User
+       email = ec2-user@$HOSTNAME
+EOF
+cd content-test-data
+git add *
+git commit -m "Updated mock files from content branch $1 build $2"
+git push --force
