1eb3f953aa9ddced95e7c59b77f269e26392fcb5
benparadise <benjoparadise@gmail.com>
2019-03-07 19:54:27 +0200

    Fixed issue with nothing to commit

--- Tests/scripts/upload_mocks.sh
+++ Tests/scripts/upload_mocks.sh
@@ -7,4 +7,4 @@ cat >~/.gitconfig << EOF
 EOF
 cd content-test-data
 git add * >/dev/null
-git commit -m "Updated mock files from content branch '$1' build number - $2" >/dev/null && git push --force >/dev/null
+git commit -m "Updated mock files from content branch '$1' build number - $2" && git push --force || :
