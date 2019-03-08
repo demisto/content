7f6379d6acaa939d95d70020af13a1bb0a0ff366
benparadise <benjoparadise@gmail.com>
2019-03-08 16:59:23 +0200

    Fix spammy output

--- Tests/scripts/run_installer_on_instance.sh
+++ Tests/scripts/run_installer_on_instance.sh
@@ -23,8 +23,8 @@ echo "add instance to known hosts"
 ssh-keyscan -H ${PUBLIC_IP} >> ~/.ssh/known_hosts
 
 # install mitmproxy
-ssh ${USER}@${PUBLIC_IP} 'wget https://snapshots.mitmproxy.org/4.0.4/mitmproxy-4.0.4-linux.tar.gz >/dev/null'
-ssh ${USER}@${PUBLIC_IP} 'tar -xzvf mitmproxy-4.0.4-linux.tar.gz >/dev/null'
+ssh ${USER}@${PUBLIC_IP} 'wget --quiet https://snapshots.mitmproxy.org/4.0.4/mitmproxy-4.0.4-linux.tar.gz'
+ssh ${USER}@${PUBLIC_IP} 'tar -xzf mitmproxy-4.0.4-linux.tar.gz'
 ssh ${USER}@${PUBLIC_IP} 'rm mitmproxy-4.0.4-linux.tar.gz'
 ssh ${USER}@${PUBLIC_IP} 'sudo chmod +x mitm*'
 ssh ${USER}@${PUBLIC_IP} 'sudo mv mitm* /usr/local/bin'
