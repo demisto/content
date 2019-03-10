98f7cf920fe52fa62ac90d61a2682cf0ebaaa81d
Rony Kozakish <37589583+ronykoz@users.noreply.github.com>
2019-03-10 10:56:23 +0200

    Content build supported releases (#3021)
    * adding GA tests for content
    
    * updating tests
    
    * some python fixes
    
    * some python fixes
    
    * some python fixes
    
    * temp commit
    
    * temp commit
    
    * Add more Run tests stages to content build
    
    * Add more Run tests stages to content build
    
    * Add more Run tests stages to content build
    
    * Add more Run tests stages to content build
    
    * Add more Run tests stages to content build
    
    * fixing create_instance.sh
    
    * fixing create_instance.sh
    
    * fixing create_instance.sh
    
    * fixing server installtion
    
    * fixing server installtion
    
    * fixing server installation
    
    * fixing server installation
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * fixing lint
    
    * returning content creator
    
    * returning content creator
    
    * returning content creator
    
    * returning content creator
    
    * returning content creator
    
    * Updating destroy instances
    
    * debugging
    
    * debugging
    
    * debugging
    
    * removing debug messages
    
    * updating test_content
    
    * fixing lint
    
    * always running the tests
    
    * testing server installation
    
    * temp commit
    
    * temp commit
    
    * fixing http request
    
    * fixing http request
    
    * fixing http request
    
    * updating test messages
    
    * updating test messages
    
    * adding fromversion to TestPlaybook to avoid internal server error in Demisto
    
    * updating test run
    
    * fixing flake8
    
    * removing 3.6.0 support
    
    * fixing Server installation
    
    * fixing Server installation
    
    * temp commit
    
    * removing redundant code + updating the create_instance mech
    
    * renaming file
    
    * renaming file
    
    * adding pip installation
    
    * fixing flake8
    
    * updating content installation
    
    * updating content installation
    
    * updating content installation
    
    * updating content installation
    
    * updating testing during infra changes
    
    * fixing destroy instances
    
    * fixing destroy instances
    
    * fixing destroy instances
    
    * fixing bash script

--- Tests/scripts/copy_content_data.sh
+++ Tests/scripts/copy_content_data.sh
@@ -1,33 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-INSTANCE_ID=$1
-
-echo "Making sure instance started"
-aws ec2 wait instance-exists --instance-ids ${INSTANCE_ID}
-aws ec2 wait instance-running --instance-ids ${INSTANCE_ID}
-echo "Instance started. fetching IP"
-
-PUBLIC_IP=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} \
-    --query 'Reservations[0].Instances[0].PublicIpAddress' | tr -d '"')
-echo "Instance public IP is: $PUBLIC_IP"
-
-echo ${PUBLIC_IP} > public_ip
-
-echo "wait 90 seconds to ensure server is ready for ssh"
-sleep 90s
+PUBLIC_IP=$1
 
 USER="ec2-user"
 
 echo "add instance to known hosts"
 ssh-keyscan -H ${PUBLIC_IP} >> ~/.ssh/known_hosts
-
-# install mitmproxy
-ssh ${USER}@${PUBLIC_IP} 'wget https://snapshots.mitmproxy.org/4.0.4/mitmproxy-4.0.4-linux.tar.gz'
-ssh ${USER}@${PUBLIC_IP} 'tar -xzvf mitmproxy-4.0.4-linux.tar.gz'
-ssh ${USER}@${PUBLIC_IP} 'rm mitmproxy-4.0.4-linux.tar.gz'
-ssh ${USER}@${PUBLIC_IP} 'sudo chmod +x mitm*'
-ssh ${USER}@${PUBLIC_IP} 'sudo mv mitm* /usr/local/bin'
 
 ssh ${USER}@${PUBLIC_IP} 'mkdir ~/content'
 ssh ${USER}@${PUBLIC_IP} 'mkdir ~/TestPlaybooks'
@@ -39,7 +18,7 @@ scp -r ./Beta_Integrations/* ${USER}@${PUBLIC_IP}:~/Beta_Integrations
 
 # override exiting content with current
 COPY_CONTENT_COMMAND="sudo unzip -o ~/content/content_new.zip -d /usr/local/demisto/res \
-    && sudo unzip -o ~/content/content_test.zip -d /usr/local/demisto/res && sudo cp -r ~/Beta_Integrations/* /usr/local/demisto/res"
+    && sudo unzip -o ~/content/content_test.zip -d /usr/local/demisto/res && sudo cp ~/Beta_Integrations/* /usr/local/demisto/res"
 ssh -t ${USER}@${PUBLIC_IP} ${COPY_CONTENT_COMMAND}
 
 echo "start server"
