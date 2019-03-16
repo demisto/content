3f12b13d5e5bf912c38cd7c951d1e150adec3ce9
idobloch <idoprv@gmail.com>
2019-03-17 00:31:19 +0200

    start custom indicator in STIX 2.0

--- Scripts/script-StixExporter2.py
+++ Scripts/script-StixExporter2.py
@@ -1,6 +1,14 @@
 from stix2 import Indicator
 
+type ="File MD5"
+
+sourceSystem = "DBot"
+
+value = "ABC"
+
 indicator = Indicator(name="File hash for malware variant",
                       labels="label-kof",
-                      pattern="[file:hashes.md5 = 'd41d8cd98f00b204e9800998ecf8427e']")
+                      pattern="[file:hashes.md5 = '" + value + "']",
+                      source=sourceSystem,
+                      allow_custom=True)
 print(indicator)
