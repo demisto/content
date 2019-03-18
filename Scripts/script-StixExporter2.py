28b40b945b342827454a0b0c670078fec8b6c428
idobloch <idoprv@gmail.com>
2019-03-18 12:02:48 +0200

    fix

--- Scripts/script-StixExporter2.py
+++ Scripts/script-StixExporter2.py
@@ -1,27 +1,42 @@
+
 from stix2 import Indicator, Bundle
 
-demisto_indicator_type = demisto.args().get('indicator_type', '')
-value = demisto.args().get('value', '')
-source_System = demisto.args().get('source', '')
-demisto_Score = demisto.args().get('score', '')
-first_seen = demisto.args().get('firstSeen', '')
-demisto_created = demisto.args().get('timestamp', '')
-last_seen = demisto.args().get('lastSeen', '')
+demisto_indicator_type = demisto.args().get('indicator_type','')
+value = demisto.args().get('value','')
+source_System = demisto.args().get('source','')
+demisto_Score = demisto.args().get('score','')
+first_seen = demisto.args().get('firstSeen','')
+demisto_created = demisto.args().get('timestamp','')
+last_seen = demisto.args().get('lastSeen','')
+createBundle = demisto.args().get('createBundle','False')
 
 stix_type_and_value = ""
 
-if demisto_indicator_type.lower() == "FileMD5".lower():
+if demisto_indicator_type.lower() == "File MD5".lower():
     stix_type_and_value = "[file:hashes.md5 = '" + value + "']"
-if demisto_indicator_type.lower() == "File SHA1".lower():
-    stix_type_and_value = "[file:hashes.md5 = '" + value + "']"
+if demisto_indicator_type.lower() == "File SHA-1".lower():
+    stix_type_and_value = "[file:hashes.sha-1 = '" + value + "']"
+if demisto_indicator_type.lower() == "File SHA-256".lower():
+    stix_type_and_value = "[file:hashes.sha-256 = '" + value + "']"
+if demisto_indicator_type.lower() == "IP".lower():
+    stix_type_and_value = "[ipv4-addr:value = '" + value + "']"
+if demisto_indicator_type.lower() == "URL".lower():
+    stix_type_and_value = "[url:value = '" + value + "']"
+else:
+    stix_type_and_value = "[" + demisto_indicator_type.lower() +":value = '" + value + "']"
 
 indicator = Indicator(labels=demisto_indicator_type,
                       pattern=stix_type_and_value,
                       source=source_System,
-                      created=demisto_created,  # created - timestamp
-                      modified=last_seen,  # modified - lastSeen
-                      firstSeen=first_seen,  # first_seen - firstSeen
-                      score=demisto_Score,  # Reputation
+                      created=demisto_created,
+                      modified=last_seen,
+                      firstSeen=first_seen,
+                      score=demisto_Score,
                       allow_custom=True)
-bundle = Bundle(indicator)
-print(bundle)
+
+if createBundle.lower() == "true":
+    bundle = Bundle(indicator)
+    print(bundle)
+else:
+    print(indicator)
+
