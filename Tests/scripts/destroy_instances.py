5f1d218c1a944fb283362504833f6189d6c516de
yaakovi <yaakovi@demisto.com>
2019-03-24 19:08:41 +0200

    fixing flake8

--- Tests/scripts/destroy_instances.py
+++ Tests/scripts/destroy_instances.py
@@ -16,11 +16,12 @@ def main():
     for ami_instance_name, ami_instance_ip in instance_ips:
         for ami_instance_name_second, ami_instance_id in instance_ids:
             if ami_instance_name == ami_instance_name_second:
-                run_command("./Tests/scripts/destroy_instances.sh {} {} {} ./Tests/is_build_failed_{}.txt".format(
-                    circle_aritfact,
-                    ami_instance_id,
-                    ami_instance_ip,
-                    ami_instance_name.replace(' ', '')
+                run_command(
+                    "./Tests/scripts/destroy_instances.sh {} {} {} ./Tests/is_build_failed_{}.txt".format(
+                        circle_aritfact,
+                        ami_instance_id,
+                        ami_instance_ip,
+                        ami_instance_name.replace(' ', '')
                     ), is_silenced=False)
 
 
