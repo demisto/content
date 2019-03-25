bb43ceec149531c32c75f0b8c75fde500eed73dc
Eric <vector@tmhk.it>
2019-03-25 08:58:23 -0400

    Fixed error when specifying a port (#3214)
    Because the port is given to the ldap3 library as a string, a "port must be an integer" error is thrown when you specify a port in the instance configuration. Casting this parameter to an integer resolves this issue.

--- Integrations/Active_Directory_Query/Active_Directory_Query.py
+++ Integrations/Active_Directory_Query/Active_Directory_Query.py
@@ -882,7 +882,7 @@ def main():
     DEFAULT_PAGE_SIZE = int(demisto.params().get('page_size'))
     NTLM_AUTH = demisto.params().get('ntlm')
     UNSECURE = demisto.params().get('unsecure', False)
-    PORT = demisto.params().get('port')
+    PORT = int(demisto.params().get('port'))
 
     try:
         server = initialize_server(SERVER_IP, PORT, SECURE_CONNECTION, UNSECURE)
