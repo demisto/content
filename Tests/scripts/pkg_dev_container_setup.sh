3c1223aeaa78e6273d6cc85e7ab01e1b843f439e
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-03-27 14:47:49 +0200

    added comment about stdin and removed unnecessary pip install

--- Tests/scripts/pkg_dev_container_setup.sh
+++ Tests/scripts/pkg_dev_container_setup.sh
@@ -4,9 +4,9 @@
 #
 # In alpine will install necessary python dev tools and then use pip to install
 
+# /dev/stdin is coming in in the format of a requirements file as pip install expects to receive
+# see code at pgk_dev_tasks_in_docker.py (method: docker_image_create)
 cp /dev/stdin /pkg-dev-test-requirements.txt
-
-pip install -r /pkg-dev-test-requirements.txt
 
 # for alpine we install dev dependencies to support dependencies that need native code (seen with typed_ast)
 if [ -f "/etc/alpine-release" ]; then
