933bc6464e058d8f7d0630ce67f7ea1432cdeda2
deanarbel <deanarbel@demisto.com>
2019-03-31 16:41:53 +0300

    updated file structure

---
+++ Integrations/CarbonBlackProtect/CarbonBlackProtect.py
@@ -0,0 +1,240 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+''' IMPORTS '''
+
+import json
+import requests
+from distutils.util import strtobool
+
+# Disable insecure warnings
+requests.packages.urllib3.disable_warnings()
+
+''' GLOBALS/PARAMS '''
+
+TOKEN = demisto.params().get('token')
+# Remove trailing slash to prevent wrong URL path to service
+SERVER = demisto.params()['url'][:-1] if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
+# Should we use SSL
+USE_SSL = not demisto.params().get('unsecure', False)
+# Service base URL
+BASE_URL = SERVER + '/api/v2.0/'
+# Headers to be sent in requests
+HEADERS = {
+    'Authorization': 'Token ' + TOKEN,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+# Remove proxy if not set to true in params
+if not demisto.params().get('proxy'):
+    del os.environ['HTTP_PROXY']
+    del os.environ['HTTPS_PROXY']
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
+
+
+''' HELPER FUNCTIONS '''
+
+
+def http_request(method, url_suffix, params=None, data=None, headers=HEADERS, safe=False):
+    """
+        A wrapper for requests lib to send our requests and handle requests and responses better.
+
+        :type method: ``str``
+        :param method: HTTP method for the request.
+
+        :type url_suffix: ``str``
+        :param url_suffix: The suffix of the URL (endpoint)
+
+        :type params: ``dict``
+        :param params: The URL params to be passed.
+
+        :type data: ``str``
+        :param data: The body data of the request.
+
+        :type headers: ``dict``
+        :param headers: Request headers
+
+        :type safe: ``bool``
+        :param safe: If set to true will return None in case of error
+
+        :return: Returns the http request response json
+        :rtype: ``dict``
+    """
+    url = SERVER + url_suffix
+    try:
+        res = requests.request(
+            method,
+            url,
+            verify=USE_SSL,
+            params=params,
+            data=data,
+            headers=headers,
+        )
+    except requests.exceptions.RequestException as e:
+        return_error('Error in connection to the server. Please make sure you entered the URL correctly.')
+    # Handle error responses gracefully
+    if res.status_code not in {200, 201}:
+        if safe:
+            return None
+        return_error('Error in API call [{0}] - {1}'.format(res.status_code, res.reason))
+    return res.json()
+
+
+def create_entry_object(contents='', ec=None, hr=''):
+    """
+        Creates an entry object
+
+        :type contents: ``dict``
+        :param contents: Raw response to output
+
+        :type ec: ``dict``
+        :param ec: Entry context of the entry object
+
+        :type hr: ``str``
+        :param hr: Human readable
+
+        :return: Entry object
+        :rtype: ``dict``
+    """
+    return {
+        'Type': entryTypes['note'],
+        'Contents': contents,
+        'ContentsFormat': formats['json'],
+        'ReadableContentsFormat': formats['markdown'],
+        'HumanReadable': hr,
+        'EntryContext': ec
+    }
+
+
+''' COMMANDS + REQUESTS FUNCTIONS '''
+
+
+def test_module():
+    """
+    Performs basic get request to get item samples
+    """
+    samples = http_request('GET', 'items/samples')
+
+
+def get_items_command():
+    """
+    Gets details about a items using IDs or some other filters
+    """
+    # Init main vars
+    headers = []
+    contents = []
+    context = {}
+    context_entries = []
+    title = ''
+    # Get arguments from user
+    item_ids = argToList(demisto.args().get('item_ids', []))
+    is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
+    limit = int(demisto.args().get('limit', 10))
+    # Make request and get raw response
+    items = get_items_request(item_ids, is_active)
+    # Parse response into context & content entries
+    if items:
+        if limit:
+            items = items[:limit]
+        title = 'Example - Getting Items Details'
+
+        for item in items:
+            contents.append({
+                'ID': item.get('id'),
+                'Description': item.get('description'),
+                'Name': item.get('name'),
+                'Created Date': item.get('createdDate')
+            })
+            context_entries.append({
+                'ID': item.get('id'),
+                'Description': item.get('description'),
+                'Name': item.get('name'),
+                'CreatedDate': item.get('createdDate')
+            })
+
+        context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
+
+    demisto.results({
+        'Type': entryTypes['note'],
+        'ContentsFormat': formats['json'],
+        'Contents': contents,
+        'ReadableContentsFormat': formats['markdown'],
+        'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
+        'EntryContext': context
+    })
+
+
+def get_items_request(item_ids, is_active):
+    # The service endpoint to request from
+    endpoint_url = 'items'
+    # Dictionary of params for the request
+    params = {
+        'ids': item_ids,
+        'isActive': is_active
+    }
+    # Send a request using our http_request wrapper
+    response = http_request('GET', endpoint_url, params)
+    # Check if response contains errors
+    if response.get('errors'):
+        return_error(response.get('errors'))
+    # Check if response contains any data to parse
+    if 'data' in response:
+        return response.get('data')
+    # If neither was found, return back empty results
+    return {}
+
+
+def fetch_incidents():
+    last_run = demisto.getLastRun()
+    # Get the last fetch time, if exists
+    last_fetch = last_run.get('time')
+
+    # Handle first time fetch, fetch incidents retroactively
+    if last_fetch is None:
+        last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
+
+    incidents = []
+    items = get_items_request()
+    for item in items:
+        incident = item_to_incident(item)
+        incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        # Update last run and add incident if the incident is newer than last fetch
+        if incident_date > last_fetch:
+            last_fetch = incident_date
+            incidents.append(incident)
+
+    demisto.setLastRun({'time' : last_fetch})
+    demisto.incidents(incidents)
+
+
+''' COMMANDS MANAGER / SWITCH PANEL '''
+
+LOG('Command being called is {}'.format(demisto.command()))
+
+
+def search_file_catalog_command():
+    args = demisto.args()
+    group = args.get('group')
+    headers = args.get('headers')
+    limit = args.get('limit')
+    offset = args.get('offset')
+    query = args.get('query')
+    sort = args.get('sort')
+    raw_res = search_file_catalog(group, headers, limit, offset, query, sort)
+    demisto.results(fileResult(raw_res.get('name???'), raw_res.get('data???')))
+
+try:
+    if demisto.command() == 'test-module':
+        # This is the call made when pressing the integration test button.
+        test_module()
+        demisto.results('ok')
+    elif demisto.command() == 'cbp-fileCatalog-search':
+        # An example command
+        search_file_catalog_command()
+
+# Log exceptions
+except Exception, e:
+    LOG(e.message)
+    LOG.print_log()
+    raise
---
+++ Integrations/CarbonBlackProtect/CarbonBlackProtect_description.md
@@ -0,0 +1,1 @@
+Use these detailed instructions in order to retrieve the API key
