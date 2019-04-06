40c8da042baed8e56ea8592c5f89490d2af8df84
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-04-06 17:11:18 +0300

    unit test to check the fix

--- Tests/scripts/test_configure_tests.py
+++ Tests/scripts/test_configure_tests.py
@@ -167,5 +167,18 @@ class TestConfigureTests_AllTesting(unittest.TestCase):
         os.remove(FILTER_CONF)
 
 
+class TestConfigureTests_PackageFilesModified(unittest.TestCase):
+
+    def test_package_modification(self):
+        active_dir_mod_git = """M       Integrations/Active_Directory_Query/Active_Directory_Query.py
+A       Integrations/Active_Directory_Query/cert.pem
+M       Integrations/Active_Directory_Query/connection_test.py
+A       Integrations/Active_Directory_Query/key.pem
+"""
+        files_list, tests_list, all_tests, is_conf_json, sample_tests = get_modified_files(active_dir_mod_git)
+        self.assertEquals(len(sample_tests), 0)
+        self.assertIn('Integrations/Active_Directory_Query/Active_Directory_Query.yml', files_list)
+
+
 if __name__ == '__main__':
     unittest.main()
