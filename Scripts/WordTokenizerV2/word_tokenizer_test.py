32e7e130a393f78f4da7d3e5202b1e193e45d66d
eharush <erez@demisto.com>
2019-04-15 16:26:24 +0300

    fix UT

--- Scripts/WordTokenizerV2/word_tokenizer_test.py
+++ Scripts/WordTokenizerV2/word_tokenizer_test.py
@@ -35,6 +35,6 @@ def test_tokenize_text():
 def test_word_tokenize():
     text = "test@demisto.com is 100 going to http://google.com bla bla"
     entry = word_tokenize(text)
-    assert "EMAIL_PATTERN NUMBER_PATTERN go URL_PATTERN bla bla" == entry['Contents'][0]['tokenizedText']
-    assert "2074773130 1320446219 5863419 1810208405 193487380 193487380" == entry['Contents'][0][
+    assert "EMAIL_PATTERN NUMBER_PATTERN go URL_PATTERN bla bla" == entry['Contents']['tokenizedText']
+    assert "2074773130 1320446219 5863419 1810208405 193487380 193487380" == entry['Contents'][
         'hashedTokenizedText']
