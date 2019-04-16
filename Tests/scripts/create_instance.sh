268cc29524badcfa1c62970b0f4eec070fa8195a
Andrew Shamah <andrew@demisto.com>
2019-04-16 14:52:41 +0300

    Revert to master build config

--- Tests/scripts/create_instance.sh
+++ Tests/scripts/create_instance.sh
@@ -10,9 +10,11 @@ CONFFILE=$1
 AMI_NAME=$2
 
 #Get nightly image of the server
-aws ec2 describe-images --filters 'Name=name,Values=$AMI_NAME' --owners 676921422616 --query 'Images[*].[ImageId,Name,CreationDate]' --output text | sort -k2 -r | head -n1 > image_id.txt
+IMAGE_ID=$(aws ec2 describe-images \
+    --filters Name=name,Values=$AMI_NAME \
+    --query 'Images[*].[ImageId,Name,CreationDate]' --output text | sort -k2 -r | head -n1)
 
-#echo $IMAGE_ID > image_id.txt
+echo $IMAGE_ID > image_id.txt
 
 python ./Tests/scripts/update_image_id.py -i image_id.txt -c $CONFFILE
 
