d7474e4455c96639d6b1129ae36d3fb8a6e55998
yaakovi <yaakovi@demisto.com>
2019-04-17 11:07:54 +0300

    fixed print

--- release_notes_clear.py
+++ release_notes_clear.py
@@ -76,8 +76,8 @@ def json_remove_releaseNote_record(file_path, current_server_version):
             v = line.strip()[len(version_key[0]) + 1:]
             # compare server versions
             if server_version_compare(current_server_version, v) < 0:
-                print "keeping release notes for (%s)\nto be published on %s version release " \
-                      % (file_path, current_server_version)
+                print("keeping release notes for (%s)\nto be published on %s version release ".format(
+                      file_path, current_server_version))
                 clear_release_notes = False
                 break
 
