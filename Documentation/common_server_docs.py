f3d53c0fb27216bcff65c4c74fbfa7583bb468d1
ronykoz <rkozakish@paloaltonetworks.com>
2019-04-17 16:00:21 +0300

    updating doc creation

--- Documentation/common_server_docs.py
+++ Documentation/common_server_docs.py
@@ -4,8 +4,6 @@ import sys
 import yaml
 from parinx import parser
 
-from package_creator import clean_python_code
-
 jsPrivateFuncs = ["dqQueryBuilder", "toArray", "indent", "formatTableValuesRecursive", "string_to_array",
                   "array_to_hex_string", "SHA256_init", "SHA256_write", "SHA256_finalize", "SHA256_hash",
                   "HMAC_SHA256_init", "HMAC_SHA256_write", "HMAC_SHA256_finalize", "HMAC_SHA256_MAC"]
@@ -46,7 +44,7 @@ def reformatPythonOutput(output, origin, language):
             continue
 
         if a.get("description", "") == "":
-            print "Description is missing for Python function", a["name"]
+            print("Description is missing for Python function", a["name"])
             isError = True
 
         # format arguments
@@ -60,7 +58,7 @@ def reformatPythonOutput(output, origin, language):
                 argInfo["type"] = argInfo["type_name"]
                 if argInfo.get("description", "") == "":
                     isError = True
-                    print "Missing description for argument", argName, "in python function", a["name"]
+                    print("Missing description for argument", argName, "in python function", a["name"])
                 del argInfo["type_name"]
                 z.append(argInfo)
 
@@ -91,11 +89,11 @@ def createJsDocumentation(path, origin, language):
         y = {}
         y["name"] = a.get("name", "")
         if y["name"] == "":
-            print "Error extracting function name for JS fucntion with the following data:\n", a
+            print("Error extracting function name for JS fucntion with the following data:\n", a)
             isError = True
         y["description"] = a.get("description", "")
         if y["description"] == "":
-            print "Description is missing for JS function", y["name"]
+            print("Description is missing for JS function", y["name"])
             isError = True
 
         for arg in a.get("params", []):
@@ -106,8 +104,8 @@ def createJsDocumentation(path, origin, language):
                 del arg["optional"]
             if arg.get("name", "") == "" or arg.get("description", "") == "":
                 isError = True
-                print "Missing name/description for argument in JS function", y["name"], ".\n Arg name is", \
-                    arg.get("name", ""), ", args description is", arg.get("description", "")
+                print("Missing name/description for argument in JS function", y["name"], ".\n Arg name is", \
+                      arg.get("name", ""), ", args description is", arg.get("description", ""))
         y["arguments"] = a.get("params", [])
 
         returns = a.get("returns", None)[0]
@@ -127,14 +125,13 @@ def createJsDocumentation(path, origin, language):
 def createPyDocumentation(path, origin, language):
     isErrorPy = False
 
-    with open(path, 'r') as file:
-        pyScript = file.read()
-
-    pyScript = clean_python_code(pyScript)
+    # create commonServerPy json doc
+    commonServerPython = readYmlFile(path)
+    pyScript = commonServerPython.get("script", "")
 
     code = compile(pyScript, '<string>', 'exec')
     ns = {}
-    exec code in ns
+    exec(code, ns)
 
     x = []
 
@@ -142,7 +139,7 @@ def createPyDocumentation(path, origin, language):
         if callable(ns.get(a)) and a not in pyPrivateFuncs:
             docstring = inspect.getdoc(ns.get(a))
             if not docstring:
-                print "docstring for function " + a + " is empty"
+                print("docstring for function " + a + " is empty")
                 isErrorPy = True
             else:
                 y = parser.parse_docstring(docstring)
@@ -158,8 +155,8 @@ def createPyDocumentation(path, origin, language):
 
 
 def main(argv):
-    jsDoc, isErrorJS = createJsDocumentation('./Documentation/commonServerJsDoc.json', 'CommonServerJs', 'javascript')
-    pyDoc, isErrorPy = createPyDocumentation('./Scripts/CommonServerPython/CommonServerPython.py',
+    # jsDoc, isErrorJS = createJsDocumentation('./Documentation/commonServerJsDoc.json', 'CommonServerJs', 'javascript')
+    pyDoc, isErrorPy = createPyDocumentation('./Scripts/script-CommonServerPython.yml',
                                              'CommonServerPython', 'python')
     finalDoc = readJsonFile('./Documentation/commonServerConstants.json')
 
