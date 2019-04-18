e662db1824878e15ce76b2d5640e933284c39af3
yaakovi <yaakovi@demisto.com>
2019-04-18 17:09:03 +0300

    fixed UT

--- Integrations/FidelisElevateNetwork/FidelisElevateNetwork_test.py
+++ Integrations/FidelisElevateNetwork/FidelisElevateNetwork_test.py
@@ -9,7 +9,8 @@ def test_get_ioc_filter():
     f = get_ioc_filter('c9a31ea148232b201fe7cb7db5c75f5e')
     assert f.get('simple', {}).get('column') == 'MD5'
 
-    # assert f.get('simple', {}).get('column') == 'SHA1_HASH'
+    f = get_ioc_filter('2F6C57D8CB43AA5C0153CD3A06E4A783B5BB7BC1')
+    assert f.get('simple', {}).get('column') == 'SHA1_HASH'
 
     f = get_ioc_filter('9d88425e266b3a74045186837fbd71de657b47d11efefcf8b3cd185a884b5306')
     assert f.get('simple', {}).get('column') == 'SHA256'
@@ -20,7 +21,7 @@ def test_get_ioc_filter():
 
 def test_to_fidelis_time_format():
     from FidelisElevateNetwork import to_fidelis_time_format
-    fidelis_time = re.compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')
+    fidelis_time = re.compile(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')
 
     dt = datetime.now()
     assert fidelis_time.match(to_fidelis_time_format(dt)) is not None
