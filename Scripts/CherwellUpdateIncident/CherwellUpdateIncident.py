6b4eff2b12428eefd7847ed89e40d1801b626fe4
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-04-22 17:16:06 +0300

    Changed fetch advanced query to correlate with the last created time

--- Scripts/CherwellUpdateIncident/CherwellUpdateIncident.py
+++ Scripts/CherwellUpdateIncident/CherwellUpdateIncident.py
@@ -2,11 +2,6 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
-
-import demistomock as demisto
-from CommonServerPython import *
-from CommonServerUserPython import *
-
 args = demisto.args()
 
 # ####################################################################################
@@ -22,16 +17,18 @@ BUSINESS_OBJECT_TYPE = 'Incident'
 
 """
  `FIELDS` contains all the fields you wish to include in your business object.
- In order to update a field in a business object you will need to have a corresponding argument in the script arguments.
- After you added the argument to the script arguments you will need to add its value to this `FIELDS` object by using
- `args.get('argument_name').
+ In order to update a field in a business object you will need to have a corresponding argument in the script arguments 
+ (found in the script settings). After you added the argument to the script arguments you will need to add its value to 
+ this `FIELDS` object by using `args.get('argument_name').
  Make sure the key name is identical to the field name in Cherwell system.
+ 
  For example: we wished to update the requesting customer of some incident, thus, we added a customer_display_name
  argument to this script, we pulled the value of the argument by using`args.get('customer_display_name')` and we set
  the key name to be `CustomerDisplayName` - as it is appear in Cherwell system.
 
- Expert tip: if you don't want to keep Demisto's arguments naming convention, you can name your arguments exactly as
- they appear in Cherwell system, this will shorten the `FIELDS` constant to be `FIELDS = args`
+ Expert tip: if you don't want to keep Demisto's arguments naming convention (where variables are written in snake case
+ ), you can name your arguments exactly as they appear in Cherwell system, this will shorten the `FIELDS` constant to be 
+ `FIELDS = args`
 """
 FIELDS = {
     'Description': args.get('description'),
@@ -40,6 +37,7 @@ FIELDS = {
     'OwnedBy': args.get('owned_by'),
     'Service': args.get('service')
 }
+
 
 # ####################################################################################
 # ############################## EXECUTION PART ######################################
@@ -53,7 +51,8 @@ def build_arguments():
         'id_value': args.get('id_value'),
         # As `owned_by` and `service` arguments are not mandatory we make sure to remove them by using the createContext
         # function with removeNull flag set to true.
-        # If all arguments are mandatory it would be sufficient to use `json: FIELDS`
+        # If all script arguments are mandatory, it would be sufficient to use `json: FIELDS` instead of
+        # `createContext(FIELDS, removeNull=True)`
         'json': createContext(FIELDS, removeNull=True)
     }
     return arguments
