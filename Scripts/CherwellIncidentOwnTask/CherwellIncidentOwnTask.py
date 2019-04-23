cf93fe6a061e923b7dd7838aeee13d4f8a79033b
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-04-23 22:18:46 +0300

    Cleaned the integration

---
+++ Scripts/CherwellIncidentOwnTask/CherwellIncidentOwnTask.py
@@ -0,0 +1,29 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+args = demisto.args()
+
+
+"""
+`RELATIONSHIP_ID` is the id of the relationship you wish to base your link on. In this case we wanted to create script 
+for incidents to own tasks, thus, we used the  "Incident Owns Task" relationship id.
+"""
+RELATIONSHIP_ID = '9369187528b417b4a17aaa4646b7f7a78b3c821be9'
+
+
+"""
+ `PARENT_BUSINESS_OBJECT_TYPE` is the name of parent business object and `CHILD_BUSINESS_OBJECT_TYPE` is the name of the 
+ child business object. In this case, as we wanted to create a script for incidents to own tasks, we set 
+ `PARENT_BUSINESS_OBJECT_TYPE` to be 'Incident' and `CHILD_BUSINESS_OBJECT_TYPE` to be `Task`.  
+"""
+PARENT_BUSINESS_OBJECT_TYPE = 'Incident'
+CHILD_BUSINESS_OBJECT_TYPE = 'Task'
+
+
+"""
+In order to create your own link script you can just modify the arguments inside the integration settings to correspond 
+to the business objects you wish to link.
+For example if you wish to create a link between configuration item and an incident when 
+"""
+parent_record_id = args.get('incident_record_id')
+child_record_id = args.get('task_record_id')
