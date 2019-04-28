bb9aea623d7eb35055867f73c557240df54f8325
bark@demisto.com <bkatzir@paloaltonetworks.com>
2019-04-28 17:10:37 +0300

    tests 103

--- Integrations/Palo_Alto_Networks_WildFire_v2/Palo_Alto_Networks_WildFire_v2_test.py
+++ Integrations/Palo_Alto_Networks_WildFire_v2/Palo_Alto_Networks_WildFire_v2_test.py
@@ -1,4 +1,5 @@
-from Palo_Alto_Networks_WildFire_v2 import prettify_upload
+from Palo_Alto_Networks_WildFire_v2 import prettify_upload, prettify_report_entry, prettify_verdict, \
+    create_dbot_score_from_verdict, prettify_verdicts
 
 
 def test_will_return_ok():
@@ -6,46 +7,39 @@ def test_will_return_ok():
 
 
 def test_prettify_upload():
-    expected_json = dict({
+    expected_upload_dict = dict({
         'MD5': "md5_hash", 'SHA256': "sha256_hash", 'FileType': "pdf", 'Size': 5, 'Status': "Pending"})
-    prettify_upload_res = prettify_upload({'md5': "md5_hash", 'sha256': "sha256_hash", 'filetype': "pdf", 'size': 5})
-    assert expected_json == prettify_upload_res
+    prettify_upload_res = prettify_upload(
+        {'md5': "md5_hash", 'sha256': "sha256_hash", 'filetype': "pdf", 'size': 5})
+    assert expected_upload_dict == prettify_upload_res
 
-    # 2nd usecase
 
-# def test_get_start_time(mocker):
-#     integration_context = {
-#         'stored': int((datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds()),
-#         'access_token': 'dummy'
-#     }
-#     mocker.patch.object(demisto, 'getIntegrationContext', return_value=integration_context)
-#
-#     from PaloAltoNetworksCortex import get_start_time
-#
-#     five_minutes_start_time = get_start_time('minutes', 5)
-#     expected_response = datetime.now() - timedelta(minutes=5)
-#     assert five_minutes_start_time.replace(microsecond=0) == expected_response.replace(microsecond=0)
-#
-#     ten_days_start_time = get_start_time('days', 10)
-#     expected_response = datetime.now() - timedelta(days=10)
-#     assert ten_days_start_time.replace(microsecond=0) == expected_response.replace(microsecond=0)
-#
-#     four_weeks_start_time = get_start_time('weeks', 4)
-#     expected_response = datetime.now() - timedelta(weeks=4)
-#     assert four_weeks_start_time.replace(microsecond=0) == expected_response.replace(microsecond=0)
-#
-#
-# def test_process_incident_pairs():
-#     from PaloAltoNetworksCortex import process_incident_pairs
-#     incident_pairs = [
-#         (1, datetime.fromtimestamp(1)),
-#         (3, datetime.fromtimestamp(3)),
-#         (2, datetime.fromtimestamp(2)),
-#     ]
-#     incidents, max_ts = process_incident_pairs(incident_pairs, 3)
-#     assert incidents[2] == 3
-#     assert max_ts == datetime.fromtimestamp(3)
-#     incidents, max_ts = process_incident_pairs(incident_pairs, 2)
-#     assert incidents[1] == 2
-#     assert len(incidents) == 2
-#     assert max_ts == datetime.fromtimestamp(2)
+def test_prettify_report_entry():
+    expected_report_dict = dict({
+        'MD5': "md5_hash", 'SHA256': "sha256_hash", 'FileType': "pdf", 'Size': 5, 'Status': "Completed"})
+    prettify_report_entry_res = prettify_report_entry(
+        {'md5': "md5_hash", 'sha256': "sha256_hash", 'filetype': "pdf", 'size': 5})
+    assert expected_report_dict == prettify_report_entry_res
+
+
+def test_prettify_verdict():
+    expected_verdict_dict = dict({
+        'MD5': "md5_hash", 'SHA256': "sha256_hash", 'Verdict': "1", 'VerdictDescription': 'desc'})
+    prettify_verdict_res = prettify_verdict(
+        {'md5': "md5_hash", 'sha256': "sha256_hash", 'verdict': "1"})
+    assert expected_verdict_dict == prettify_verdict_res
+
+
+def test_create_dbot_score_from_verdict():
+    expected_dbot_score = list(dict({
+        'Indicator': "sha256_hash", 'Type': "hash", 'Vendor': "WildFire", 'Score': 3}))
+    dbot_score_dict = create_dbot_score_from_verdict(list({'SHA256': "sha256_hash", 'Verdict': "1"}))
+    assert expected_dbot_score == dbot_score_dict
+
+
+def test_prettify_verdicts():
+    expected_verdicts_dict = dict({
+        'MD5': "md5_hash", 'SHA256': "sha256_hash", 'Verdict': "1", 'VerdictDescription': 'desc'})
+    prettify_verdicts_res = prettify_verdicts(list(
+        {'md5': "md5_hash", 'sha256': "sha256_hash", 'verdict': "1"}))
+    assert expected_verdicts_dict == prettify_verdicts_res
