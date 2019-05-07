793b64f4e38bf470ca4da7391fb84704a57a0ce2
Itay Keren <ikeren@paloaltonetworks.com>
2019-05-07 13:49:09 +0300

    fix rasterize mypy lint (#3511)

--- Integrations/rasterize/rasterize.py
+++ Integrations/rasterize/rasterize.py
@@ -20,9 +20,8 @@ return_code = 0
 error_message = ''
 
 
-def rasterize_email_request(html, friendlyName):
-    global return_code
-    global error_message
+def rasterize_email_request(html, friendly_name):
+    global return_code, error_message
 
     f = open('htmlBody.html', 'w')
     f.write('<html style="background:white";>' + html + '</html>')
@@ -33,7 +32,7 @@ def rasterize_email_request(html, friendlyName):
         proxy_flag = "--proxy=" + http_proxy
     demisto.debug('rasterize proxy settings: ' + proxy_flag)
 
-    command = ['phantomjs', proxy_flag, '/usr/local/bin/rasterize.js', 'htmlBody.html', friendlyName]
+    command = ['phantomjs', proxy_flag, '/usr/local/bin/rasterize.js', 'htmlBody.html', friendly_name]
     if demisto.get(demisto.args(), 'width') and demisto.get(demisto.args(), 'height'):
         command.append(demisto.get(demisto.args(), 'width') + '*' + demisto.get(demisto.args(), 'height'))
     try:
@@ -44,15 +43,15 @@ def rasterize_email_request(html, friendlyName):
 
 
 def rasterize():
-    global return_code, error_message, friendlyName, command, file
+    global return_code, error_message
     return_code = 0
     error_message = ''
     url = demisto.args()['url']
     if not (url.startswith("http")):
         url = "http://" + url
-    friendlyName = 'url.png'
+    friendly_name = 'url.png'
     if demisto.get(demisto.args(), 'type') == 'pdf':
-        friendlyName = 'url.pdf'
+        friendly_name = 'url.pdf'
     proxy_flag = ""
     if proxy:
         if url.startswith("https"):
@@ -60,7 +59,7 @@ def rasterize():
         else:
             proxy_flag = "--proxy=" + http_proxy
     demisto.debug('rasterize proxy settings: ' + proxy_flag)
-    command = ['phantomjs', proxy_flag, '/usr/local/bin/rasterize.js', url, friendlyName]
+    command = ['phantomjs', proxy_flag, '/usr/local/bin/rasterize.js', url, friendly_name]
     if demisto.get(demisto.args(), 'width') and demisto.get(demisto.args(), 'height'):
         command.append(demisto.get(demisto.args(), 'width') + '*' + demisto.get(demisto.args(), 'height'))
     try:
@@ -69,7 +68,7 @@ def rasterize():
         return_code = -1
         error_message = "Can't access the URL. It might be malicious, or unreachable for one of several reasons."
     if return_code == 0:
-        file = file_result_existing_file(friendlyName)
+        file = file_result_existing_file(friendly_name)
         file['Type'] = entryTypes['image']
         demisto.results(file)
     else:
@@ -78,19 +77,18 @@ def rasterize():
 
 
 def rasterize_image():
-    global html, return_code, error_message, friendlyName, command, file
+    global return_code, error_message
     res = demisto.getFilePath(demisto.args()['EntryID'])
     with open(res['path'], 'r') as f:
         data = f.read()
     b64 = base64.b64encode(data)
     html = '<img src="data:image/png;base64, ' + b64 + '">'
     return_code = 0
-    error_message = ''
-    friendlyName = 'image.png'
+    friendly_name = 'image.png'
     f = open('htmlImage.html', 'w')
     f.write('<html style="background:white;"><body>' + html + '</body></html>')
     f.close()
-    command = ['phantomjs', '/usr/local/bin/rasterize.js', 'htmlImage.html', friendlyName]
+    command = ['phantomjs', '/usr/local/bin/rasterize.js', 'htmlImage.html', friendly_name]
     if demisto.get(demisto.args(), 'width') and demisto.get(demisto.args(), 'height'):
         command.append(demisto.get(demisto.args(), 'width') + '*' + demisto.get(demisto.args(), 'height'))
     try:
@@ -99,7 +97,7 @@ def rasterize_image():
         return_code = -1
         error_message = e.message
     if return_code == 0:
-        file = file_result_existing_file(friendlyName)
+        file = file_result_existing_file(friendly_name)
         file['Type'] = entryTypes['image']
         demisto.results(file)
 
@@ -109,14 +107,13 @@ def rasterize_image():
 
 
 def rasterize_email_command():
-    global html, friendlyName, file
     html = demisto.args()['htmlBody']
-    friendlyName = 'email.png'
+    friendly_name = 'email.png'
     if demisto.get(demisto.args(), 'type') == 'pdf':
-        friendlyName = 'email.pdf'
-    rasterize_email_request(html, friendlyName)
+        friendly_name = 'email.pdf'
+    rasterize_email_request(html, friendly_name)
     if return_code == 0:
-        file = file_result_existing_file(friendlyName)
+        file = file_result_existing_file(friendly_name)
         file['Type'] = entryTypes['image']
         demisto.results(file)
     else:
