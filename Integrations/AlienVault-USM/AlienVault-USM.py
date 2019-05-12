599729845eada4b8eb976883e2460d934eeb4632
yaakovi <syaakovi@paloaltonetworks.com>
2019-05-12 07:21:37 +0300

    new integration

--- Integrations/AlienVault-USM/AlienVault-USM.py
+++ Integrations/AlienVault-USM/AlienVault-USM.py
@@ -12,30 +12,37 @@ requests.packages.urllib3.disable_warnings()
 
 ''' GLOBALS/PARAMS '''
 
-USERNAME = demisto.params().get('credentials').get('identifier')
-PASSWORD = demisto.params().get('credentials').get('password')
-TOKEN = demisto.params().get('token')
+CLIENT_ID = demisto.params().get('client_id')
+CLIENT_SECRET = demisto.params().get('client_secret')
 # Remove trailing slash to prevent wrong URL path to service
 SERVER = demisto.params()['url'][:-1] \
     if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
+API_VERSION = demisto.params().get('api_version')
+
+TIME_ZONE = demisto.params().get('time_zone', '+0000')
 # Should we use SSL
-USE_SSL = not demisto.params().get('unsecure', False)
+USE_SSL = not demisto.params().get('insecure', False)
 # How many time before the first fetch to retrieve incidents
 FETCH_TIME = demisto.params().get('fetch_time', '3 days')
 # Service base URL
-BASE_URL = SERVER + '/api/v2.0/'
+BASE_URL = SERVER + '/api/{}'.format(API_VERSION)
 # Headers to be sent in requests
 HEADERS = {
-    'Authorization': 'Token ' + TOKEN + ':' + USERNAME + PASSWORD,
     'Content-Type': 'application/json',
     'Accept': 'application/json'
 }
+AUTH_TOKEN = ''
 
 
 ''' HELPER FUNCTIONS '''
 
 
-def http_request(method, url_suffix, params=None, data=None):
+@logger
+def http_request(method, url_suffix, params=None, headers=None, data=None, **kwargs):
+    data = data if data else {}
+    if not headers:
+        headers = HEADERS
+        headers['Authorization'] = 'Bearer ' + AUTH_TOKEN
     # A wrapper for requests lib to send our requests and handle requests and responses better
     res = requests.request(
         method,
@@ -43,7 +50,8 @@ def http_request(method, url_suffix, params=None, data=None):
         verify=USE_SSL,
         params=params,
         data=data,
-        headers=HEADERS
+        headers=headers,
+        **kwargs
     )
     # Handle error responses gracefully
     if res.status_code not in {200}:
@@ -51,15 +59,72 @@ def http_request(method, url_suffix, params=None, data=None):
 
     return res.json()
 
+@logger
+def get_token():
+    basic_auth_credentials = (CLIENT_ID, CLIENT_SECRET)
+
+    res = http_request('POST', '/oauth/token',
+                       params={'grant_type': 'client_credentials'},
+                       headers={'Content-Type': 'application/www-form-urlencoded'},
+                       auth=basic_auth_credentials)
+
+    return res['access_token']
+
+
+def parse_alarm(alarm_data):
+    return {
+        'ID': alarm_data['uuid'],
+        'Priority': alarm_data['priority_label'],
+        'DestinationAsset': alarm_data['destinations'][0]['address'],
+        'RuleAttackId': alarm_data['rule_attack_id'],
+        'RuleAttackTactic': alarm_data['rule_attack_tactic'][0],
+        'RuleAttackTechnique': alarm_data['rule_attack_technique'],
+        'Sensor': alarm_data['events'][0]['received_from'],
+        'Source': {
+            'IpAddress': alarm_data['destinations'][0]['address'],
+            'Organization': alarm_data['sources'][0]['organisation'],
+            'Country': alarm_data['sources'][0]['country'],
+        },
+        'Destination': {
+            'IpAddress': alarm_data['destinations'][0]['address'],
+            'FQDN': alarm_data['destinations'][0]['fqdn']
+        }
+    }
+
+
+def parse_alarms(alarms_data):
+    alarms = []
+    for alarm in alarms_data:
+        alarms.append({
+            'ID': alarm['uuid'],
+            'Priority': alarm['priority_label'],
+            'DestinationAsset': alarm['events'][0]['message']['destination_address'],
+            'RuleAttackId': alarm['rule_attack_id'],
+            'RuleAttackTactic': alarm['rule_attack_tactic'][0],
+            'RuleAttackTechnique': alarm['rule_attack_technique'],
+            'Sensor': alarm['events'][0]['message']['received_from'],
+            'Source': {
+                'IpAddress': alarm['events'][0]['message']['source_address'],
+                'Organization': alarm['events'][0]['message']['source_organisation'],
+                'Country': alarm['events'][0]['message']['source_country'],
+            },
+            'Destination': {
+                'IpAddress': alarm['events'][0]['message']['destination_address'],
+                'FQDN': alarm['events'][0]['message']['destination_fqdn'],
+            }
+        })
+
+    return alarms
+
 
 def item_to_incident(item):
-    incident = {}
-    # Incident Title
-    incident['name'] = 'Example Incident: ' + item.get('name')
-    # Incident occurrence time, usually item creation date in service
-    incident['occurred'] = item.get('createdDate')
-    # The raw response from the service, providing full info regarding the item
-    incident['rawJSON'] = json.dumps(item)
+    incident = {
+        'Type': 'AlienVault USM',
+        'name': 'Example Incident: ' + item.get('name'),
+        'occurred': item.get('createdDate'),
+        'rawJSON': json.dumps(item),
+    }
+
     return incident
 
 
@@ -70,75 +135,71 @@ def test_module():
     """
     Performs basic get request to get item samples
     """
-    samples = http_request('GET', 'items/samples')
+    http_request('GET', '')
+    demisto.results('ok')
 
 
-def get_items_command():
+def get_alarm_command():
     """
-    Gets details about a items using IDs or some other filters
+    Gets alarm details by ID
     """
-    # Init main vars
-    headers = []
-    contents = []
-    context = {}
-    context_entries = []
-    title = ''
-    # Get arguments from user
-    item_ids = argToList(demisto.args().get('item_ids', []))
-    is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
-    limit = int(demisto.args().get('limit', 10))
+    args = demisto.args()
+    alarm_id = args['id']
+
     # Make request and get raw response
-    items = get_items_request(item_ids, is_active)
+    response = get_alarm(alarm_id)
+
     # Parse response into context & content entries
-    if items:
-        if limit:
-            items = items[:limit]
-        title = 'Example - Getting Items Details'
+    alarm_details = parse_alarm(response)
 
-        for item in items:
-            contents.append({
-                'ID': item.get('id'),
-                'Description': item.get('description'),
-                'Name': item.get('name'),
-                'Created Date': item.get('createdDate')
-            })
-            context_entries.append({
-                'ID': item.get('id'),
-                'Description': item.get('description'),
-                'Name': item.get('name'),
-                'CreatedDate': item.get('createdDate')
-            })
-
-        context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
-
-    demisto.results({
-        'Type': entryTypes['note'],
-        'ContentsFormat': formats['json'],
-        'Contents': contents,
-        'ReadableContentsFormat': formats['markdown'],
-        'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
-        'EntryContext': context
-    })
+    return_outputs(tableToMarkdown('Alarm {}'.format(alarm_id), alarm_details), alarm_details, response)
 
 
-def get_items_request(item_ids, is_active):
-    # The service endpoint to request from
-    endpoint_url = 'items'
-    # Dictionary of params for the request
-    params = {
-        'ids': item_ids,
-        'isActive': is_active
+
+def get_alarm(alarm_id):
+    res = http_request('GET', '/alarms/' + alarm_id)
+
+    return res
+
+
+def search_alerts_command():
+    args = demisto.args()
+    start_time = args.get('start_time', 'now-7d')
+    end_time = args.get('end_time', 'now')
+    show_suppressed = args.get('show_suppressed', 'false')
+    limit = int(args.get('limit', 100))
+
+    result = search_alerts(start_time, end_time, show_suppressed, limit)
+    alarms = parse_alarms(result)
+
+    return_outputs(tableToMarkdown('Alarms:', alarms),
+                   {'AlienVault.Alarm(val.ID && val.ID == obj.ID': alarms}, result)
+
+
+@logger
+def search_alerts(start_time, end_time, show_suppressed, limit):
+    data = {
+        "page": 1,
+        "size": limit,
+        "find": {
+            "alarm.suppressed": [
+                show_suppressed
+            ]
+        },
+        "sort": {
+            "alarm.timestamp_occured": "desc"
+        },
+        "range": {
+            "alarm.timestamp_occured": {
+                "gte": start_time,
+                "lte": end_time,
+                "timeZone": TIME_ZONE
+            }
+        }
     }
-    # Send a request using our http_request wrapper
-    response = http_request('GET', endpoint_url, params)
-    # Check if response contains errors
-    if response.get('errors'):
-        return_error(response.get('errors'))
-    # Check if response contains any data to parse
-    if 'data' in response:
-        return response.get('data')
-    # If neither was found, return back empty results
-    return {}
+    res = http_request('GET', '/alarms/', data=data)
+
+    return res['_embedded']['alarms']
 
 
 def fetch_incidents():
@@ -151,7 +212,7 @@ def fetch_incidents():
         last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
 
     incidents = []
-    items = get_items_request()
+    items = search_alerts()
     for item in items:
         incident = item_to_incident(item)
         incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
@@ -165,150 +226,36 @@ def fetch_incidents():
 
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
-
-LOG('Command being called is %s' % (demisto.command()))
-
-try:
-    handle_proxy()
-    if demisto.command() == 'test-module':
-        # This is the call made when pressing the integration test button.
-        test_module()
-        demisto.results('ok')
-    elif demisto.command() == 'fetch-incidents':
-        # Set and define the fetch incidents command to run after activated via integration settings.
-        fetch_incidents()
-    elif demisto.command() == 'example-get-items':
-        # An example command
-        get_items_command()
-
-# Log exceptions
-except Exception as e:
-    LOG(e.message)
-    LOG.print_log()
-    raise
+COMMANDS = {
+    'test-module': test_module,
+    'fetch-incidents': fetch_incidents,
+    'alienvault-search-alerts': search_alerts_command,
+    'alienvault-get-alarm': get_alarm_command,
+}
 
 
-# ''' IMPORTS '''
-# import requests
-#
-# import demistomock as demisto
-# from CommonServerPython import *
-#
-# ''' GLOBAL VARS '''
-# CLIENT_ID = demisto.params().get('clientid')
-# SECRET = demisto.params().get('secret')
-# HOST = demisto.params().get('host')
-#
-#
-# API_VERSION = '2.0'
-# URL = HOST + '/api/' + API_VERSION
-#
-#
-# def get_token():
-#     basicauth_credentials = (CLIENT_ID, SECRET)
-#
-#     try:
-#         response = requests.post(URL + '/oauth/token?grant_type=client_credentials',
-#                                  params={'grant_type': 'client_credentials'},
-#                                  auth=basicauth_credentials)
-#     except Exception:
-#         raise Exception('request failed')
-#
-#     if response.status_code == 200:
-#         res = json.loads(response.text)
-#         return res['access_token']
-#
-#
-# def parse_alarm(alarm_data):
-#     return {
-#         'Alarm.ID': alarm_data['uuid'],
-#         'Alarm.Priority': alarm_data['priority_label'],
-#         'Alarm.DestinationAsset': alarm_data['destinations'][0]['address'],
-#         'Alarm.RuleAttackId': alarm_data['rule_attack_id'],
-#         'Alarm.RuleAttackTactic': alarm_data['rule_attack_tactic'][0],
-#         'Alarm.RuleAttackTechnique': alarm_data['rule_attack_technique'],
-#         "Alarm.Sensor": alarm_data['events'][0]['received_from'],
-#         'Alarm.Source.IpAddress': alarm_data['destinations'][0]['address'],
-#         'Alarm.Source.Organization': alarm_data['sources'][0]['organisation'],
-#         'Alarm.Source.Country': alarm_data['sources'][0]['country'],
-#         'Alarm.Destination.IpAddress': alarm_data['destinations'][0]['address'],
-#         'Alarm.Destination.FQDN': alarm_data['destinations'][0]['fqdn']
-#     }
-#
-#
-# def parse_alarms(alarms_data):
-#     alarms = []
-#     for alarm in alarms_data['_embedded']['alarms']:
-#         tmp = {
-#             'Alarm.ID': alarm['uuid'],
-#             'Alarm.Priority': alarm['priority_label'],
-#             'Alarm.DestinationAsset': alarm['events'][0]['message']['destination_address'],
-#             'Alarm.RuleAttackId': alarm['rule_attack_id'],
-#             'Alarm.RuleAttackTactic': alarm['rule_attack_tactic'][0],
-#             'Alarm.RuleAttackTechnique': alarm['rule_attack_technique'],
-#             "Alarm.Sensor": alarm['events'][0]['message']['received_from'],
-#             'Alarm.Source.IpAddress': alarm['events'][0]['message']['source_address'],
-#             'Alarm.Source.Organization': alarm['events'][0]['message']['source_organisation'],
-#             'Alarm.Source.Country': alarm['events'][0]['message']['source_country'],
-#             'Alarm.Destination.IpAddress': alarm['events'][0]['message']['destination_address'],
-#             'Alarm.Destination.FQDN': alarm['events'][0]['message']['destination_fqdn'],
-#         }
-#         alarms.append(tmp)
-#
-#     return alarms
-#
-#
-# def get_alarm_by_id(alarm_id):
-#     auth_token = get_token()
-#     hed = {'Authorization': 'Bearer ' + auth_token}
-#     url = URL + '/alarms/' + alarm_id
-#
-#     try:
-#         response = requests.get(url, headers=hed)
-#     except Exception:
-#         raise Exception('request failed')
-#
-#     if response.status_code == 401:
-#         raise Exception('invalid token')
-#
-#     if response.status_code == 404:
-#         raise Exception('alarm could not be found')
-#
-#     if response.status_code == 200:
-#         res = json.loads(response.text)
-#
-#         alarm_context = parse_alarm(res)
-#
-#         print alarm_context
-#     return alarm_context
-#
-#
-# def get_alarms():
-#     auth_token = get_token()
-#     hed = {'Authorization': 'Bearer ' + auth_token}
-#     url = URL + '/alarms/'
-#
-#     try:
-#         response = requests.get(url, headers=hed)
-#     except Exception:
-#         raise Exception('request failed')
-#
-#     if response.status_code == 401:
-#         raise Exception('invalid token')
-#
-#     if response.status_code == 404:
-#         raise Exception('alarm could not be found')
-#
-#     if response.status_code == 200:
-#         res = json.loads(response.text)
-#
-#         alarm_context = parse_alarms(res)
-#
-#         print alarm_context
-#     return alarm_context
-#
-#
-# get_alarm_by_id('d8689007-30b1-ae32-f4b2-f5f2b553ac14')
-# get_alarms()
-#
-# sys.exit(0)
+def main():
+    global AUTH_TOKEN
+    cmd = demisto.command()
+    LOG('Command being called is %s' % (cmd))
+
+    try:
+        AUTH_TOKEN = get_token()
+        handle_proxy()
+
+        if cmd in COMMANDS:
+            COMMANDS[cmd]()
+
+    # Log exceptions
+    except Exception as e:
+        if demisto.command() == 'fetch-incidents':
+            LOG(e.message)
+            LOG.print_log()
+            raise
+        else:
+            return_error('An error occurred: {}'.format(str(e)))
+
+
+# python2 uses __builtin__ python3 uses builtins
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
