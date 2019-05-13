258e2efc3895a24ba17d250c592b525bb19cc4f8
guykeller <g12k34ppp>
2019-05-13 07:41:33 +0300

    Improved test error message and unit test naming

--- Integrations/GoogleBigQuery/google_bigquery_test.py
+++ Integrations/GoogleBigQuery/google_bigquery_test.py
@@ -3,7 +3,9 @@ from GoogleBigQuery import convert_to_string_if_datetime
 
 
 def test_convert_to_string_if_datetime():
-    test1 = convert_to_string_if_datetime(None)
-    assert test1 is None
+    test_conversion_for_none = convert_to_string_if_datetime(None)
+    assert test_conversion_for_none is None
     now = datetime.datetime.now()
     convert_to_string_if_datetime(now)
+    test_conversion_for_empty_string = convert_to_string_if_datetime("")
+    assert test_conversion_for_empty_string is ""
