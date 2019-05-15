0072bf6f13d60745f4f783e2fb11d09e18fe53df
Dean Arbel <darbel@paloaltonetworks.com>
2019-05-15 14:56:50 +0300

    Implemented tests

--- Scripts/ReadPDFFile/ReadPDFFile.py
+++ Scripts/ReadPDFFile/ReadPDFFile.py
@@ -8,10 +8,10 @@ import errno
 import shutil
 
 ROOT_PATH = os.getcwd()
-USER_PASSWORD = demisto.getArg('userPassword')
+USER_PASSWORD = demisto.args().get('userPassword')
 
 
-def mark_suspicious(suspicious_reason):
+def mark_suspicious(suspicious_reason, entry_id):
     """Missing EOF, file may be corrupted or suspicious file"""
 
     dbot = {
@@ -23,14 +23,12 @@ def mark_suspicious(suspicious_reason):
                 "Score": 2
             }
     }
-
     human_readable = "{}, file marked as suspicious for entry id: {}".format(suspicious_reason, entry_id)
-
     return_outputs(human_readable, dbot, {})
 
 
-def run_shell_command(command, arg1, *args):
-    cmd = [command, arg1] + list(args)
+def run_shell_command(command, *args):
+    cmd = [command] + list(args)
     proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     o, e = proc.communicate()
     return o.decode('utf8'), e.decode('utf8')
@@ -47,7 +45,7 @@ def get_files_names_in_path(path, name_of_file, full_path=False):
     return res
 
 
-def get_images_names_in_path(path):
+def get_images_paths_in_path(path):
     res = []
     res.extend(get_files_names_in_path(path, "*.ppm", True))
     res.extend(get_files_names_in_path(path, "*.bpm", True))
@@ -57,10 +55,13 @@ def get_images_names_in_path(path):
 
 
 def get_pdf_metadata(file_path):
+    """Gets the metadata from the pdf as a dictionary"""
     if USER_PASSWORD:
         metadata_txt, e = run_shell_command('pdfinfo', '-upw', USER_PASSWORD, file_path)
     else:
         metadata_txt, e = run_shell_command('pdfinfo', file_path)
+    if e:
+        raise TypeError(e)
     metadata = {}
     for line in metadata_txt.split('\n'):
         # split to [key, value...]
@@ -78,12 +79,13 @@ def get_pdf_metadata(file_path):
 
 
 def get_pdf_text(file_path, pdf_text_output_path):
+    """Creates a txt file from the pdf in the pdf_text_output_path and returns the content of the txt file"""
     if USER_PASSWORD:
         o, e = run_shell_command('pdftotext', '-upw', USER_PASSWORD, file_path, pdf_text_output_path)
     else:
         o, e = run_shell_command('pdftotext', file_path, pdf_text_output_path)
     if e:
-        raise Exception(e)
+        raise TypeError(e)
     text = ''
     with open(pdf_text_output_path, 'rb') as f:
         for line in f:
@@ -92,13 +94,14 @@ def get_pdf_text(file_path, pdf_text_output_path):
 
 
 def get_pdf_htmls_content(pdf_path, output_folder):
+    """Creates an html file and images from the pdf in output_folder and returns the text content of the html files"""
     pdf_html_output_path = f'{output_folder}/PDF.html'
     if USER_PASSWORD:
         o, e = run_shell_command('pdftohtml', '-upw', USER_PASSWORD, pdf_path, pdf_html_output_path)
     else:
         o, e = run_shell_command('pdftohtml', pdf_path, pdf_html_output_path)
     if e:
-        raise Exception(e)
+        raise TypeError(e)
     html_file_names = get_files_names_in_path(output_folder, '*.html')
     html_content = ''
     for file_name in html_file_names:
@@ -108,113 +111,122 @@ def get_pdf_htmls_content(pdf_path, output_folder):
     return html_content
 
 
-entry_id = demisto.args()["entryID"]
-# File entity
-pdf_file = {
-    "EntryID": entry_id
-}
+def main():
+    entry_id = demisto.args()["entryID"]
+    # File entity
+    pdf_file = {
+        "EntryID": entry_id
+    }
 
-# URLS
-URLs = []
-folders_to_remove = []
-try:
-    path = demisto.getFilePath(entry_id).get('path')
-    if path:
-        try:
-            output_folder = "ReadPDF"
-            os.makedirs(output_folder)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
+    # URLS
+    URLs = []
+    folders_to_remove = []
+    try:
+        path = demisto.getFilePath(entry_id).get('path')
+        if path:
+            try:
+                output_folder = "ReadPDF"
+                os.makedirs(output_folder)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise e
+                pass
+            try:
+                folders_to_remove.append(output_folder)
+                cpy_file_path = f'{output_folder}/ReadPDF.pdf'
+                shutil.copy(path, cpy_file_path)
+
+                # Get metadata:
+                metadata = get_pdf_metadata(cpy_file_path)
+
+                # Get text:
+                pdf_text_output_path = f'{output_folder}/PDFText.txt'
+                text = get_pdf_text(cpy_file_path, pdf_text_output_path)
+
+                # Get URLS + Images:
+                pdf_html_content = get_pdf_htmls_content(cpy_file_path, output_folder)
+                urls = re.findall(urlRegex, pdf_html_content)
+                urls = set(urls)
+                # this url is always generated with the pdf html file
+                urls.remove('http://www.w3.org/1999/xhtml')
+                for url in urls:
+                    URLs.append({"Data": url})
+                images = get_images_paths_in_path(output_folder)
+
+            except Exception as e:
+                demisto.results({
+                    "Type": entryTypes["error"],
+                    "ContentsFormat": formats["text"],
+                    "Contents": "Could not load pdf file in EntryID {0}\nError: {1}".format(entry_id, str(e))
+                })
                 raise e
-            pass
-        try:
-            folders_to_remove.append(output_folder)
-            cpy_file_path = f'{output_folder}/ReadPDF.pdf'
-            shutil.copy(path, cpy_file_path)
 
-            # Get metadata:
-            metadata = get_pdf_metadata(cpy_file_path)
+            # Add Text to file entity
+            pdf_file["Text"] = text
 
-            # Get text:
-            pdf_text_output_path = f'{output_folder}/PDFText.txt'
-            text = get_pdf_text(cpy_file_path, pdf_text_output_path)
+            # Add Metadata to file entity
+            for k in metadata.keys():
+                pdf_file[k] = metadata[k]
 
-            # Get URLS + Images:
-            pdf_html_content = get_pdf_htmls_content(cpy_file_path, output_folder)
-            urls = re.findall(urlRegex, pdf_html_content)
-            urls = set(urls)
-            # this url is always generated with the pdf html file
-            urls.remove('http://www.w3.org/1999/xhtml')
-            for url in urls:
-                URLs.append({"Data": url})
-            images = get_images_names_in_path(output_folder)
+            md = "### Metadata\n"
+            md += "* " if metadata else ""
+            md += "\n* ".join(["{0}: {1}".format(k,v) for k,v in metadata.items()])
 
-        except Exception as e:
+            md += "\n### URLs\n"
+            md += "* " if URLs else ""
+            md += "\n* ".join(["{0}".format(str(k["Data"])) for k in URLs])
+
+            md += "\n### Text"
+            md += "\n{0}".format(text)
+            results = [{"Type" : entryTypes["note"],
+                        "ContentsFormat" : formats["markdown"],
+                        "Contents" : md,
+                        "HumanReadable": md,
+                        "EntryContext": {"File(val.EntryID == obj.EntryID)": pdf_file, "URL": URLs}
+                        }]
+            if images:
+                results[0]['HumanReadable'] += '\n### Images'
+                os.chdir(ROOT_PATH)
+                for img in images:
+                    file = file_result_existing_file(img)
+                    results.append(file)
+            demisto.results(results)
+            all_pdf_data = ""
+            if metadata:
+                for k,v in metadata.items():
+                    all_pdf_data += str(v)
+            if text:
+                all_pdf_data += text
+            if URLs:
+                for u in URLs:
+                    u = u["Data"] + " "
+                    all_pdf_data += u
+
+            # Extract indicators (omitting context output, letting auto-extract work)
+            indicators_hr = demisto.executeCommand("extractIndicators", {
+                "text": all_pdf_data})[0][u"Contents"]
             demisto.results({
-                "Type": entryTypes["error"],
-                "ContentsFormat": formats["text"],
-                "Contents" : "Could not load pdf file in EntryID {0}\nError: {1}".format(entry_id, str(e))
+                "Type": entryTypes["note"],
+                "ContentsFormat": formats["json"],
+                "Contents": indicators_hr,
+                "HumanReadable": indicators_hr
             })
-            raise e
+        else:
+            demisto.results({
+                "Type" : entryTypes["error"],
+                "ContentsFormat" : formats["text"],
+                "Contents": "EntryID {0} path could not be found".format(entry_id)
+            })
+    except Exception as e:
+        mark_suspicious(f'The script failed read PDF file due to an error: {str(e)}', entry_id)
+    finally:
+        os.chdir(ROOT_PATH)
+        for folder in folders_to_remove:
+            shutil.rmtree(folder)
 
-        # Add Text to file entity
-        pdf_file["Text"] = text
 
-        # Add Metadata to file entity
-        for k in metadata.keys():
-            pdf_file[k] = metadata[k]
+if __name__ == "__builtin__":
+    main()
 
-        md = "### Metadata\n"
-        md += "* " if metadata else ""
-        md += "\n* ".join(["{0}: {1}".format(k,v) for k,v in metadata.items()])
-
-        md += "\n### URLs\n"
-        md += "* " if URLs else ""
-        md += "\n* ".join(["{0}".format(str(k["Data"])) for k in URLs])
-
-        md += "\n### Text"
-        md += "\n{0}".format(text)
-        results = [{"Type" : entryTypes["note"],
-                    "ContentsFormat" : formats["markdown"],
-                    "Contents" : md,
-                    "HumanReadable": md,
-                    "EntryContext": {"File(val.EntryID == obj.EntryID)": pdf_file, "URL": URLs}
-                    }]
-        if images:
-            results[0]['HumanReadable'] += '\n### Images'
-            os.chdir(ROOT_PATH)
-            for img in images:
-                file = file_result_existing_file(img)
-                # file['Type'] = entryTypes['image']
-                results.append(file)
-        demisto.results(results)
-        all_pdf_data = ""
-        if metadata:
-            for k,v in metadata.items():
-                all_pdf_data += str(v)
-        if text:
-            all_pdf_data += text
-        if URLs:
-            for u in URLs:
-                u = u["Data"] + " "
-                all_pdf_data += u
-
-        # Extract indicators (omitting context output, letting auto-extract work)
-        indicators_hr = demisto.executeCommand("extractIndicators", {
-            "text": all_pdf_data})[0][u"Contents"]
-        demisto.results({
-            "Type": entryTypes["note"],
-            "ContentsFormat": formats["json"],
-            "Contents": indicators_hr,
-            "HumanReadable": indicators_hr
-        })
-    else:
-        demisto.results({
-            "Type" : entryTypes["error"],
-            "ContentsFormat" : formats["text"],
-            "Contents": "EntryID {0} path could not be found".format(entry_id)
-        })
-finally:
-    os.chdir(ROOT_PATH)
-    for folder in folders_to_remove:
-        shutil.rmtree(folder)
+if __name__ == '__main__':
+    main()
---
+++ Scripts/ReadPDFFile/ReadPDFFile_test.py
@@ -0,0 +1,142 @@
+# import ReadPDFFile
+from CommonServerPython import *
+import demistomock as demisto
+
+
+def test_get_files_names_in_path():
+    from ReadPDFFile import get_files_names_in_path
+    pdf_file_names = get_files_names_in_path('test_data', '*.pdf')
+    assert 'scanned.pdf' in pdf_file_names
+
+    pdf_file_names = get_files_names_in_path('test_data', '*.pdf', full_path=True)
+    assert 'test_data/text-only.pdf' in pdf_file_names
+
+
+def test_get_images_paths_in_path():
+    from ReadPDFFile import  get_images_paths_in_path
+    img_file_paths = get_images_paths_in_path('test_data')
+    assert 'test_data/test1.png' in img_file_paths
+    assert 'test_data/test2.jpg' in img_file_paths
+    assert 'test_data/scanned.pdf' not in img_file_paths
+
+
+def test_get_pdf_metadata_with_encrypted(mocker):
+    mocker.patch.object(demisto, 'args', return_value={'userPassword': '1234'})
+    from ReadPDFFile import get_pdf_metadata
+    metadata = get_pdf_metadata('test_data/encrypted.pdf')
+    expected = {
+        'Title': 'sample1.pdf',
+        'Keywords': '',
+        'Creator': 'Preview',
+        'Producer': 'macOS Version 10.14.4 (Build 18E226) Quartz PDFContext',
+        'CreationDate': 'Wed May 15 11:18:48 2019 IDT',
+        'ModDate': 'Wed May 15 11:18:48 2019 IDT',
+        'Tagged': 'no',
+        'UserProperties': 'no',
+        'Suspects': 'no', 'Form': 'none',
+        'JavaScript': 'no',
+        'Pages': '2',
+        'Encrypted': 'yes (print:yes copy:yes change:yes addNotes:yes algorithm:AES)',
+        'Page size': '595 x 842 pts (A4)',
+        'Page rot': '0',
+        'File size': '71085 bytes',
+        'Optimized': 'no',
+        'PDF version': '1.6'
+    }
+    assert expected == metadata
+
+
+def test_get_metadata_without_encrypted():
+    from ReadPDFFile import get_pdf_metadata
+    try:
+        get_pdf_metadata('test_data/encrypted.pdf')
+        raise Exception("Incorrect password exception should've been thrown")
+    except TypeError as e:
+        assert 'Command Line Error: Incorrect password\n' == str(e)
+
+    metadata = get_pdf_metadata('test_data/text-only.pdf')
+    print(metadata)
+    expected = {
+        'Title': 'Microsoft Word - Document1',
+        'Keywords': '',
+        'Creator': 'Word',
+        'Producer': 'macOS Version 10.14.4 (Build 18E226) Quartz PDFContext',
+        'CreationDate': 'Wed May 15 14:47:28 2019 IDT',
+        'ModDate': 'Wed May 15 14:47:28 2019 IDT',
+        'Tagged': 'no',
+        'UserProperties': 'no',
+        'Suspects': 'no',
+        'Form': 'none',
+        'JavaScript': 'no',
+        'Pages': '1',
+        'Encrypted': 'no',
+        'Page size': '595 x 842 pts (A4)',
+        'Page rot': '0',
+        'File size': '18920 bytes',
+        'Optimized': 'no',
+        'PDF version': '1.3'
+    }
+
+    assert expected == metadata
+
+
+def test_get_pdf_text_with_encrypted(mocker, tmp_path):
+    mocker.patch.object(demisto, 'args', return_value={'userPassword': '1234'})
+    from ReadPDFFile import get_pdf_text
+    text = get_pdf_text('test_data/encrypted.pdf', f'{tmp_path}/encrypted.txt')
+    expected = "XSL FO Sample Copyright © 2002-2005 Antenna House, Inc. All rights reserved.\n\n" \
+               "Links in PDF\nPDF link is classified into two parts, link to the specified position in the PDF " \
+               "document, and link to the external document.\n" \
+               "The internal-destination property of fo:basic-link indicates to link to the position in the same" \
+               " document. The externaldestination property indicates to link to external document. " \
+               "Below shows the example.\n\nExample of a link to internal destination\nRefer to Purchasing " \
+               "Assistance to get more information.\nExample of a link to external destination\nRefer to Purchasing " \
+               "Assistance to get more information."
+    assert text.startswith(expected)
+
+
+def test_get_pdf_text_without_encrypted(tmp_path):
+    from ReadPDFFile import get_pdf_text
+    try:
+        get_pdf_text('test_data/encrypted.pdf', f'{tmp_path}/encrypted.txt')
+        raise Exception("Incorrect password exception should've been thrown")
+    except TypeError as e:
+        assert 'Command Line Error: Incorrect password\n' == str(e)
+
+    text = get_pdf_text('test_data/text-only.pdf', f'{tmp_path}/text-only.txt')
+    expected = "עברית"
+    assert expected in text
+    assert text.startswith('This is a pdf document with a text line within it.')
+
+    text = get_pdf_text('test_data/text-with-images.pdf', f'{tmp_path}/text-with-images.txt')
+    expected = 'Create an ETD Using Adobe Acrobat'
+    assert text.startswith(expected)
+
+    text = get_pdf_text('test_data/scanned.pdf', f'{tmp_path}/scanned.txt')
+    expected = '\x0c'
+    assert expected == text
+
+
+def test_get_pdf_htmls_content_with_encrypted(mocker, tmp_path):
+    mocker.patch.object(demisto, 'args', return_value={'userPassword': '1234'})
+    from ReadPDFFile import get_pdf_htmls_content
+    from ReadPDFFile import get_images_paths_in_path
+    html_text = get_pdf_htmls_content('test_data/encrypted.pdf', tmp_path)
+    expected = 'If you are end user who wishes to use XSL Formatter yourself, you may purchase ' \
+               'from our Reseller or direct from Antenna<br/>House.<br/>'
+    assert len(get_images_paths_in_path(tmp_path)) != 0, 'Failed to get images from html'
+    assert expected in html_text
+
+
+def test_get_pdf_htmls_content_without_encrypted(tmp_path):
+    from ReadPDFFile import get_pdf_htmls_content
+    from ReadPDFFile import get_images_paths_in_path
+    try:
+        get_pdf_htmls_content('test_data/encrypted.pdf', tmp_path)
+        raise Exception("Incorrect password exception should've been thrown")
+    except TypeError as e:
+        assert 'Command Line Error: Incorrect password\n' == str(e)
+
+    html_text = get_pdf_htmls_content('test_data/hyperlinks.pdf', tmp_path)
+    assert 'http://www.antennahouse.com/purchase.htm' in html_text
+    assert len(get_images_paths_in_path(tmp_path)) != 0, 'Failed to get images from html'
