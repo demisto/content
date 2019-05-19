b073ca03c684faae8aa18a946ddd7e205b304a62
bark@demisto.com <bkatzir@paloaltonetworks.com>
2019-05-19 18:54:00 +0300

    more unit tests

--- Integrations/PaloAltoNetworks_PAN_OS/PaloAltoNetworks_PAN_OS_test.py
+++ Integrations/PaloAltoNetworks_PAN_OS/PaloAltoNetworks_PAN_OS_test.py
@@ -1,5 +1,6 @@
 from PaloAltoNetworks_PAN_OS import add_argument_list, add_argument, add_argument_yes_no, \
-    add_argument_target, prettify_addresses_arr
+    add_argument_target, prettify_addresses_arr, prettify_address, prettify_address_group, \
+    prettify_service, prettify_service_group, prettify_custom_url_category
 
 
 def test_add_argument_list():
@@ -53,4 +54,42 @@ def test_prettify_addresses_arr():
     response = prettify_addresses_arr(addresses_arr)
     expected = [{'Name': 'my_name', 'FQDN': 'a.com'},
                 {'Name': 'my_name2', 'FQDN': 'b.com'}]
+    assert response == expected
+
+
+def test_prettify_address():
+    address = {'@name': 'my_name', 'ip-netmask': '1.1.1.1', 'description': 'lala'}
+    response = prettify_address(address)
+    expected = {'Name': 'my_name', 'IP_Netmask': '1.1.1.1', 'Description': 'lala'}
+    assert response == expected
+
+
+def test_prettify_address_group():
+    address_group_static = {'@name': 'foo', 'static': {'member': 'address object'}}
+    expected_address_group_static = {'Name': 'foo', 'Type': 'static', 'Addresses': 'address object'}
+    assert address_group_static == expected_address_group_static
+
+    address_group_dynamic = {'@name': 'foo', 'dynamic': {'filter': '1.1.1.1 and 2.2.2.2'}}
+    expected_address_group_dynamic = {'Name': 'foo', 'Type': 'Dynamic', 'Match': '1.1.1.1 and 2.2.2.2'}
+    assert address_group_dynamic == expected_address_group_dynamic
+
+
+def test_prettify_service():
+    service = {'@name': 'service_name', 'description': 'foo', 'protocol': {'tcp': {'port': '443'}}}
+    response = prettify_service(service)
+    expected = {'Name': 'service_name', 'Description': 'foo', 'Protocol': 'tcp', 'DestinationPort': '443'}
+    assert response == expected
+
+
+def test_prettify_service_group():
+    service_group = {'@name': 'sg', 'members': {'member': ['service1', 'service2']}}
+    response = prettify_service_group(service_group)
+    expected = {'Name': 'sg', 'Services': ['service1', 'service2']}
+    assert response == expected
+
+
+def test_prettify_custom_url_category():
+    custom_url_category = {'@name': 'foo', 'list': {'member': ['a', 'b', 'c']}}
+    response = prettify_custom_url_category(custom_url_category)
+    expected = {'Name': 'foo', 'Sites': ['a', 'b', 'c']}
     assert response == expected
