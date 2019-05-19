7a6f7a08835c1598632aa158b54a6f077ee9c1f8
ronykoz <rkozakish@paloaltonetworks.com>
2019-05-19 14:54:45 +0300

    Adding md support

--- Tests/scripts/spell_checker.py
+++ Tests/scripts/spell_checker.py
@@ -16,19 +16,11 @@ DISPLAYABLE_LINES = [
 ]
 
 
-def is_displayable_line(line):
-    for line_type in DISPLAYABLE_LINES:
-        if re.match("(.*)?{}: .*".format(line_type), line):
-            return True
-
-    return False
-
-
 def check_yaml(yml_info, unknown_words):
     for key, value in yml_info.items():
         if key in DISPLAYABLE_LINES:
             for word in value.split():
-                if ENGLISH.check(word) is False and re.match('^[a-zA-Z ]*$', word):
+                if word.isalpha() and not ENGLISH.check(word):
                     unknown_words.add(word)
 
         else:
@@ -40,17 +32,32 @@ def check_yaml(yml_info, unknown_words):
                         check_yaml(sub_list, unknown_words)
 
 
+def check_md_file(md_data, unknown_words):
+    for line in md_data:
+        for word in line.split():
+            if word.isalpha() and not ENGLISH.check(word):
+                unknown_words.add(word)
+
+
 def spell_checker():
     description = """Run spell check on a given yml file. """
     parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("-p", "--path", help="Specify path of yml", required=True)
-
-    args = parser.parse_args()
-    with open(args.path, 'r') as yaml_file:
-        yml_info = yaml.safe_load(yaml_file)
+    parser.add_argument("-p", "--path", help="Specify path of yml/md file", required=True)
+    parser.add_argument("-i", "--isMD", help="Whether the path is to a yml file or an md.", action='store_true')
 
     unknown_words = set([])
-    check_yaml(yml_info, unknown_words)
+    args = parser.parse_args()
+
+    if args.isMD:
+        with open(args.path, 'r') as md_file:
+            md_data = md_file.readlines()
+
+        check_md_file(md_data, unknown_words)
+    else:
+        with open(args.path, 'r') as yaml_file:
+            yml_info = yaml.safe_load(yaml_file)
+
+        check_yaml(yml_info, unknown_words)
 
     if unknown_words:
         print_error(u"Found the problematic words:\n{}".format('\n'.join(unknown_words)))
