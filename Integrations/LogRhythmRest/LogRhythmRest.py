9048a3c0187f947f92e1591605c06537ed85ea50
adi88d <adi88daud@gmail.com>
2019-05-25 13:54:50 +0300

    fix formatting errors

--- Integrations/LogRhythmRest/LogRhythmRest.py
+++ Integrations/LogRhythmRest/LogRhythmRest.py
@@ -14,11 +14,12 @@ import xml.etree.ElementTree as ET
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
-
 ''' GLOBALS/PARAMS '''
 
 TOKEN = demisto.params().get('token')
-BASE_URL = demisto.params()['url'][:-1] if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
+BASE_URL = demisto.params()['url'][:-1] if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) \
+    else demisto.params()['url']
+
 CLUSTER_ID = demisto.params().get('cluster-id')
 
 # Headers to be sent in requests
@@ -26,11 +27,12 @@ HEADERS = {
     'Authorization': 'Bearer ' + TOKEN
 }
 
-HOSTS_HEADERS = ['id','name','entity','os','hostStatus','location','riskLevel','threatLevel','dateUpdated','hostZone']
-LOGS_HEASERS = ['Level','Computer','Channel','Keywords','EventData']
-
+HOSTS_HEADERS = ['id', 'name', 'entity', 'os', 'hostStatus', 'location', 'riskLevel', 'threatLevel', 'dateUpdated',
+                 'hostZone']
+LOGS_HEASERS = ['Level', 'Computer', 'Channel', 'Keywords', 'EventData']
 
 ''' HELPER FUNCTIONS '''
+
 
 def fix_hosts_response(hosts):
     for item in hosts:
@@ -49,11 +51,11 @@ def get_time_frame(time_frame, start_arg, end_arg):
     if time_frame == 'Today':
         start = datetime.datetime(end.year, end.month, end.day)
     elif time_frame == 'Last2Days':
-        start = end - timedelta(days = 2)
+        start = end - timedelta(days=2)
     elif time_frame == 'LastWeek':
-        start = end - timedelta(days = 7)
+        start = end - timedelta(days=7)
     elif time_frame == 'LastMonth':
-        start = end - timedelta(days = 30)
+        start = end - timedelta(days=30)
     elif time_frame == 'Custom':
         if not start_arg:
             return_error('start-date argument is missing')
@@ -65,14 +67,14 @@ def get_time_frame(time_frame, start_arg, end_arg):
     return start, end
 
 
-def http_request(method, url_suffix, data = None, headers = HEADERS):
+def http_request(method, url_suffix, data=None, headers=HEADERS):
     try:
         res = requests.request(
             method,
             BASE_URL + '/' + url_suffix,
-            headers = headers,
-            verify = False,
-            data = data
+            headers=headers,
+            verify=False,
+            data=data
         )
     except Exception as e:
         return_error(e)
@@ -82,7 +84,9 @@ def http_request(method, url_suffix, data = None, headers = HEADERS):
         return_error('invalid url or port: ' + BASE_URL)
 
     if res.status_code not in {200, 201, 207}:
-        return_error('Error in API call to {}, status code: {}, reason: {}'.format(BASE_URL + '/' + url_suffix, res.status_code, res.json()['message']))
+        return_error(
+            'Error in API call to {}, status code: {}, reason: {}'.format(BASE_URL + '/' + url_suffix, res.status_code,
+                                                                          res.json()['message']))
 
     return res.json()
 
@@ -94,6 +98,7 @@ def get_host_by_id(host_id):
 
 ''' COMMANDS + REQUESTS FUNCTIONS '''
 
+
 def test_module():
     http_request('GET', 'lr-admin-api/hosts')
     demisto.results('ok')
@@ -101,49 +106,50 @@ def test_module():
 
 def add_host(dataArgs):
     data = {
-            "id": -1,
-            "entity": {
-                "id": dataArgs.get('entity-id'),
-                "name": dataArgs.get('entity-name')
-            },
-            "name": dataArgs.get('name'),
-            "shortDesc": dataArgs.get('short-description'),
-            "longDesc": dataArgs.get('long-description'),
-            "riskLevel": dataArgs.get('risk-level'),
-            "threatLevel": dataArgs.get('threat-level'),
-            "threatLevelComments": dataArgs.get('threat-level-comments'),
-            "recordStatusName": dataArgs.get('host-status'),
-            "hostZone": dataArgs.get('host-zone'),
-            "os": dataArgs.get('os'),
-            "useEventlogCredentials": dataArgs.get('use-eventlog-credentials'),
-            "osType": dataArgs.get('os-type')
-        }
+        "id": -1,
+        "entity": {
+            "id": dataArgs.get('entity-id'),
+            "name": dataArgs.get('entity-name')
+        },
+        "name": dataArgs.get('name'),
+        "shortDesc": dataArgs.get('short-description'),
+        "longDesc": dataArgs.get('long-description'),
+        "riskLevel": dataArgs.get('risk-level'),
+        "threatLevel": dataArgs.get('threat-level'),
+        "threatLevelComments": dataArgs.get('threat-level-comments'),
+        "recordStatusName": dataArgs.get('host-status'),
+        "hostZone": dataArgs.get('host-zone'),
+        "os": dataArgs.get('os'),
+        "useEventlogCredentials": dataArgs.get('use-eventlog-credentials'),
+        "osType": dataArgs.get('os-type')
+    }
 
-    res = http_request('POST', 'lr-admin-api/hosts/',json.dumps(data))
-    return_outputs(readable_output = dataArgs.get('name') + " added successfully to " + dataArgs.get('entity-name'), outputs = None, raw_response = res)
+    res = http_request('POST', 'lr-admin-api/hosts/', json.dumps(data))
+    return_outputs(readable_output=dataArgs.get('name') + " added successfully to " + dataArgs.get('entity-name'),
+                   outputs=None, raw_response=res)
 
 
 def get_hosts(dataArgs):
     res = http_request('GET', 'lr-admin-api/hosts?entity=' + dataArgs['entity-name'] + '&count=' + dataArgs['count'])
     res = fix_hosts_response(res)
-    context = createContext(res, removeNull = True)
+    context = createContext(res, removeNull=True)
     human_readable = tableToMarkdown('Hosts for ' + dataArgs.get('entity-name'), res, HOSTS_HEADERS)
     outputs = {'Logrhythm.Hosts(val.Name && val.id === obj.id)': context}
-    return_outputs(readable_output = human_readable, outputs = outputs, raw_response = res)
+    return_outputs(readable_output=human_readable, outputs=outputs, raw_response=res)
 
 
 def change_status(dataArgs):
-    data =[{
+    data = [{
         "hostId": int(dataArgs.get('host-id')),
         "status": dataArgs.get('status')
     }]
 
-    res = http_request('PUT', 'lr-admin-api/hosts/status',json.dumps(data))
+    res = http_request('PUT', 'lr-admin-api/hosts/status', json.dumps(data))
 
-    host_info =  get_host_by_id(dataArgs.get('host-id'))
-    context = createContext(host_info, removeNull = True)
+    host_info = get_host_by_id(dataArgs.get('host-id'))
+    context = createContext(host_info, removeNull=True)
     outputs = {'Logrhythm.Hosts(val.id === obj.id)': context}
-    return_outputs(readable_output = 'Status updated to ' + dataArgs.get('status'), outputs = outputs, raw_response = res)
+    return_outputs(readable_output='Status updated to ' + dataArgs.get('status'), outputs=outputs, raw_response=res)
 
 
 def execute_query(dataArgs):
@@ -153,28 +159,28 @@ def execute_query(dataArgs):
     dates = []
 
     for i in range(delta.days + 1):
-        dates.append((start + datetime.timedelta(days = i)).strftime("logs-%Y-%m-%d"))
+        dates.append((start + datetime.timedelta(days=i)).strftime("logs-%Y-%m-%d"))
 
     data = {
-      "indices": dates,
-      "searchType": "DFS_QUERY_THEN_FETCH",
-      "source": {
-          "size": dataArgs.get('page-size'),
-          "query": {
-              "query_string": {
-                  "default_field": "logMessage",
-                  "query": dataArgs.get('keyword')
-              }
-          },
-          "stored_fields": "logMessage",
-          "sort": [
-              {
-                  "normalDate": {
-                      "order": "asc"
-                  }
-              }
-          ]
-      }
+        "indices": dates,
+        "searchType": "DFS_QUERY_THEN_FETCH",
+        "source": {
+            "size": dataArgs.get('page-size'),
+            "query": {
+                "query_string": {
+                    "default_field": "logMessage",
+                    "query": dataArgs.get('keyword')
+                }
+            },
+            "stored_fields": "logMessage",
+            "sort": [
+                {
+                    "normalDate": {
+                        "order": "asc"
+                    }
+                }
+            ]
+        }
     }
 
     headers = HEADERS
@@ -213,10 +219,10 @@ def execute_query(dataArgs):
         except:
             continue
 
-    context = createContext(logs_response, removeNull = True)
+    context = createContext(logs_response, removeNull=True)
     human_readable = tableToMarkdown('logs results', logs_response, LOGS_HEASERS)
     outputs = {'Logrhythm.logs': context}
-    return_outputs(readable_output = human_readable, outputs = outputs, raw_response = logs_response)
+    return_outputs(readable_output=human_readable, outputs=outputs, raw_response=logs_response)
 
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
@@ -235,5 +241,5 @@ try:
         execute_query(demisto.args())
     elif demisto.command() == 'lr-update-alarm-status':
         change_status(demisto.args())
-except Exception, e:
+except Exception as e:
     raise
