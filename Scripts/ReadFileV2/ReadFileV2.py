b115a22b7fa2fe0b11e2a27cbb991343098c5a6e
Andrew Shamah <andrew@demisto.com>
2019-05-28 14:49:01 +0300

    Fixes

--- Scripts/ReadFileV2/ReadFileV2.py
+++ Scripts/ReadFileV2/ReadFileV2.py
@@ -5,44 +5,41 @@ import chardet
 
 
 def get_file_encoding(filepath):
-    raw_data = open(filepath, "r").read()  # chardet is not compatible with context manager
+    raw_data = open(filepath, "rb").read()  # chardet is not compatible with context manager
     result = chardet.detect(raw_data)
-    if result.get('confidence') > .5:
+    if result.get('confidence'):
         char_enc = result.get('encoding')
     else:
         char_enc = 'utf-8'
     return char_enc
 
 
-def main():
-    maxFileSize = demisto.get(demisto.args(), 'maxFileSize')
-    if maxFileSize:
-        maxFileSize = int(maxFileSize)
-    else:
-        maxFileSize = 1024 ** 2
-    res = demisto.executeCommand('getFilePath', {'id': demisto.args()['entryID']})
-    file_path = res[0]['Contents']['path']
-    if not demisto.args().get('encoding'):
-        char_enc = get_file_encoding(file_path)
-    else:
-        char_enc = 'utf-8'
-    with open(file_path, 'r') as f:
-        data = f.read(maxFileSize)
-        data = data.decode(char_enc, errors="replace")
-        data = data.encode('utf-8')
+maxFileSize = demisto.get(demisto.args(), 'maxFileSize')
+if maxFileSize:
+    maxFileSize = int(maxFileSize)
+else:
+    maxFileSize = 1024 ** 2
+res = demisto.executeCommand('getFilePath', {'id': demisto.args()['entryID']})
+file_path = res[0]['Contents']['path']
+if not demisto.args().get('encoding'):
+    char_enc = get_file_encoding(file_path)
+else:
+    char_enc = demisto.args().get('encoding')
+with open(file_path, 'rb') as f:
+    data = f.read(maxFileSize)
+    demisto.info(char_enc)
+    try:
+        data = str(data.decode(char_enc))  # type: ignore
+    except UnicodeDecodeError:
+        return_error('Unable to read file using {}. Please try another encoding type.'.format(char_enc))
 
-    if data:
-        message = 'Read %d bytes from file.' % (len(data))
-        result = {"Type": entryTypes["note"],
-                  "ContentsFormat": formats["text"],
-                  "Contents": {"FileData": data},
-                  "HumanReadable": message,
-                  "EntryContext": {"FileData": data}
-                  }
-    else:
-        result = {"Type": entryTypes["error"], "ContentsFormat": formats["text"], "Contents": 'No data could be read.'}
-    return result
-
-
-if __name__ == "__builtin__" or __name__ == '__main__':
-    demisto.results(main())
+if data:
+    message = 'Read {} bytes from file.'.format(len(data))
+    demisto.results({"Type": entryTypes["note"],
+              "ContentsFormat": formats["text"],
+              "Contents": {"FileData": data},
+              "HumanReadable": message,
+              "EntryContext": {"FileData": data}
+              })
+else:
+    return_error('No data could be read.')
