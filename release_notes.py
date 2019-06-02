cd866bad48ae3ee26bd33acd810d1a86c0c5e38e
yaakovi <syaakovi@paloaltonetworks.com>
2019-06-02 07:45:38 +0300

    updated release_notes.py args updated hook validations

--- release_notes.py
+++ release_notes.py
@@ -7,8 +7,9 @@ import os
 import requests
 import argparse
 
-from Tests.test_utils import print_error, print_warning
-from Tests.test_utils import server_version_compare
+from Tests.test_utils import print_error, print_warning, \
+    run_command, server_version_compare, get_release_notes_file_path
+from Tests.scripts.validate_files import FilesValidator
 
 contentLibPath = "./"
 limitedVersion = False
@@ -56,8 +57,8 @@ def release_notes_item(header, body):
     return '- __' + header + '__\n' + add_dot(body) + '\n'
 
 
-class Content:
-    __metaclass__ = abc.ABCMeta
+class Content(object):
+    # __metaclass__ = abc.ABCMeta
 
     def __init__(self):
         self.modified_store = []  # holds modified file paths
@@ -83,9 +84,8 @@ class Content:
     def added_release_notes(self, data):
         return
 
-    @abc.abstractmethod
     def modified_release_notes(self, file_path, data):
-        return
+        return ''
 
     @abc.abstractmethod
     def load_data(self, data):
@@ -311,9 +311,6 @@ class WidgetContent(Content):
         return res
 
 
-# Content.register(WidgetContent)
-
-
 class IncidentFieldContent(Content):
 
     def __init__(self):
@@ -342,9 +339,6 @@ class IncidentFieldContent(Content):
         if rn != '-':
             res = add_dot(rn) + "\n"
         return res
-
-
-# Content.register(IncidentFieldContent)
 
 
 class LayoutContent(Content):
@@ -389,9 +383,6 @@ class LayoutContent(Content):
         return LayoutContent.get_release_notes(cnt)
 
 
-# Content.register(LayoutContent)
-
-
 class ClassifierContent(Content):
     def load_data(self, data):
         return json.loads(data)
@@ -428,9 +419,6 @@ class ClassifierContent(Content):
         return ClassifierContent.get_release_notes(cnt)
 
 
-# Content.register(ClassifierContent)
-
-
 class ReputationContent(Content):
     def __init__(self):
         super(ReputationContent, self).__init__()
@@ -457,9 +445,6 @@ class ReputationContent(Content):
         return res
 
 
-# Content.register(ReputationContent)
-
-
 class IntegrationContent(Content):
     def load_data(self, data):
         return yaml.safe_load(data)
@@ -471,14 +456,12 @@ class IntegrationContent(Content):
         return release_notes_item(cnt["display"], cnt["description"])
 
     def modified_release_notes(self, file_path, cnt):
-        file_name = os.path.basename(file_path)
-        rn_path = os.path.join('Releases', 'LatestRelease', os.path.splitext(file_name)[0] + '.md')
+        rn_path = get_release_notes_file_path(file_path)
 
         if not os.path.isfile(rn_path):
             print_error('no releaseNotes were provided for {}'.format(file_path))
             return None
 
-        # rn = cnt.get("releaseNotes", "")
         with open(rn_path) as f:
             rn = f.read()
 
@@ -486,14 +469,14 @@ class IntegrationContent(Content):
             print_error('no releaseNotes were provided for {}'.format(file_path))
             return None
 
+        super(IntegrationContent, self).modified_release_notes(file_path, cnt)
+
         res = ""
         if rn != '-':
             res = release_notes_item(cnt["display"], rn)
 
         return res
 
-
-# Content.register(IntegrationContent)
 
 release_note_generator = {
     INTEGRATIONS_DIR: IntegrationContent(),
@@ -524,45 +507,39 @@ def get_deleted_content(full_file_name, data):
     return full_file_name
 
 
-def handle_deleted_file(delete_file_path, full_file_name):
-    with open(delete_file_path, 'r') as f:
-        data = f.read()
-        if "/" in full_file_name:
-            file_type = full_file_name.split("/")[0]
-            file_type_mapping = release_note_generator.get(file_type)
-            deleted_content = get_deleted_content(full_file_name, data)
-            if file_type_mapping is not None:
-                file_type_mapping.add("D", deleted_content)
-
-
-def create_file_release_notes(file_name, delete_file_path):
-    if len(file_name) > 0:
-        names = file_name.split("\t")
-        change_type = names[0]
-        full_file_name = names[1]
-
-        if "/" not in full_file_name:
-            return
-
+def handle_deleted_file(deleted_data, full_file_name):
+    if "/" in full_file_name:
         file_type = full_file_name.split("/")[0]
-        file_suffix = None
-        base_name = os.path.basename(full_file_name)
-        if '.' in base_name:
-            file_suffix = base_name.split(".")[-1]
         file_type_mapping = release_note_generator.get(file_type)
-        if file_type_mapping is None or file_suffix not in CONTENT_FILE_SUFFIXES:
-            print("Unsupported file type: {}".format(full_file_name))
-            return
+        deleted_content = get_deleted_content(full_file_name, deleted_data)
+        if file_type_mapping is not None:
+            file_type_mapping.add("D", deleted_content)
 
-        if change_type == "D":
-            handle_deleted_file(delete_file_path, full_file_name)
-        elif change_type != "R100" and change_type != "R094":
-            if change_type == "R093" or change_type == "R098" or change_type == "R078":
-                # handle the same as modified
-                full_file_name = names[2]
-                change_type = 'M'
 
-            file_type_mapping.add(change_type, contentLibPath + full_file_name)
+def create_file_release_notes(change_type, full_file_name, deleted_data):
+    if isinstance(full_file_name, tuple):
+        old_file_path, full_file_name = full_file_name
+
+    if "/" not in full_file_name:
+        return
+
+    file_type = full_file_name.split("/")[0]
+    base_name = os.path.basename(full_file_name)
+    file_suffix = os.path.splitext(base_name)[-1]
+    file_type_mapping = release_note_generator.get(file_type)
+
+    if file_type_mapping is None or file_suffix not in CONTENT_FILE_SUFFIXES:
+        print("Unsupported file type: {}".format(full_file_name))
+        return
+
+    if change_type == "D":
+        handle_deleted_file(deleted_data, full_file_name)
+    elif change_type != "R100":  # only file name has changed (no actual data was modified
+        if 'R' in change_type:
+            # handle the same as modified
+            change_type = 'M'
+
+        file_type_mapping.add(change_type, contentLibPath + full_file_name)
 
 
 def get_release_notes_draft(github_token):
@@ -617,17 +594,26 @@ def create_content_descriptor(version, asset_id, res, github_token):
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('version', help='Release version')
-    arg_parser.add_argument('change_log', help='File with the full list of changes')
-    arg_parser.add_argument('deleted_files', help='Complete diff file for deleted files')
+    arg_parser.add_argument('git_sha1', help='commit sha1 to compare changes with')
+    # arg_parser.add_argument('change_log', help='File with the full list of changes')
+    # arg_parser.add_argument('deleted_files', help='Complete diff file for deleted files')
     arg_parser.add_argument('asset_id', help='Asset ID')
     arg_parser.add_argument('server_version', help='Server version')
     arg_parser.add_argument('github_token', help='Github token')
     args = arg_parser.parse_args()
 
-    files = parse_change_list(args.change_log)
+    fv = FilesValidator()
+    change_log = run_command('git diff --name-status {}'.format(args.git_sha1))
+    modified_files, added_files, _ = fv.get_modified_files(change_log)
+    deleted_files = run_command('git diff --diff-filter=D {}'.format(args.git_sha1))
 
-    for file_ in files:
-        create_file_release_notes(file_, args.deleted_files)
+    # files = parse_change_list(args.change_log)
+
+    for file_ in added_files:
+        create_file_release_notes('A', file_, deleted_files)
+
+    for file_ in modified_files:
+        create_file_release_notes('M', file_, deleted_files)
 
     res = []
     missing_release_notes = False
