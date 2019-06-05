fecd03f72c6c40858ea55e4523e3ebe9d6c8a628
yaakovi <syaakovi@paloaltonetworks.com>
2019-06-05 13:30:03 +0300

    added py file

---
+++ Scripts/FormattedDateToEpoch/FormattedDateToEpoch.py
@@ -0,0 +1,11 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+from datetime import datetime
+
+date_value = demisto.args()['value']
+formatter = demisto.args()['formatter']
+
+date_obj = datetime.strptime(date_value, formatter)
+
+demisto.results(int(date_obj.strftime('%s')))
