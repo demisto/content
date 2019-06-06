c513246031acc53c2b2cd20262997f310cef17bb
Shelly Berman <sberman@paloaltonetworks.com>
2019-06-06 10:17:31 +0300

    Fixing notes, Adding the get-credentials command, adding docstrings

--- Integrations/BeyondTrust_Password_Safe/BeyondTrust_Password_Safe.py
+++ Integrations/BeyondTrust_Password_Safe/BeyondTrust_Password_Safe.py
@@ -1,20 +1,19 @@
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
+
 ''' IMPORTS '''
 
 import json
 import requests
-from requests import Request, Session
-
 
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
 ''' GLOBALS/PARAMS '''
 
-USERNAME = demisto.params().get('credentials').get('identifier')
-PASSWORD = demisto.params().get('credentials').get('password')
+USERNAME = demisto.params().get('credentials', {}).get('identifier')
+PASSWORD = demisto.params().get('credentials', {}).get('password')
 API_KEY = demisto.params().get('key')
 # Remove trailing slash to prevent wrong URL path to service
 SERVER = demisto.params()['url'][:-1] \
@@ -27,36 +26,27 @@ BASE_URL = SERVER + '/BeyondTrust/api/public/v3'
 HEADERS = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Authorization': 'PS-Auth key=' + API_KEY + '; ' + 'runas=' + USERNAME + '; ' + 'pwd=[' + PASSWORD + '];'
+    'Authorization': f'PS-Auth key={API_KEY}; runas={USERNAME}; pwd=[{PASSWORD}];'
 }
 
 SESSION = requests.session()
-# Remove proxy if not set to true in params
-if not demisto.params().get('proxy'):
-    del os.environ['HTTP_PROXY']
-    del os.environ['HTTPS_PROXY']
-    del os.environ['http_proxy']
-    del os.environ['https_proxy']
-
 
 ''' HELPER FUNCTIONS '''
 
 
-def http_request(method, suffix_url, params=None, data=None):
-
+def http_request(method, suffix_url, data=None):
     url = BASE_URL + suffix_url
 
     res = SESSION.request(
         method,
         url,
         verify=USE_SSL,
-        params=params,
         data=data,
         headers=HEADERS
     )
     # Handle error responses gracefully
     if res.status_code not in {200, 201, 204}:
-        return_error('Error in API call to BeyondSafe Integration [%d] - %s' % (res.status_code, res.content))
+        return_error(f'Error in API call to BeyondSafe Integration [{res.status_code}] - {res.content})')
 
     try:
         return res.json()
@@ -65,15 +55,13 @@ def http_request(method, suffix_url, params=None, data=None):
 
 
 def signin():
-
     suffix_url = '/Auth/SignAppin'
-    header = {'Authorization': 'PS-Auth key=' + API_KEY + '; ' + 'runas=' + USERNAME + '; ' + 'pwd=[' + PASSWORD + '];'}
+    header = {'Authorization': f'PS-Auth key={API_KEY}; runas={USERNAME}; pwd=[{PASSWORD}];'}
     SESSION.headers.update(header)
-    response = SESSION.post(BASE_URL + suffix_url,verify=USE_SSL)
+    response = SESSION.post(BASE_URL + suffix_url, verify=USE_SSL)
 
 
 def signout():
-
     suffix_url = '/auth/signout'
     response = SESSION.post(BASE_URL + suffix_url)
 
@@ -89,7 +77,6 @@ def test_module():
 
 
 def get_managed_accounts_request():
-
     suffix_url = '/managedaccounts'
     response = http_request('GET', suffix_url)
 
@@ -105,20 +92,20 @@ def get_managed_accounts():
     for account in managed_accounts:
         data.append({
             'PlatformID': account.get('PlatformID'),
-            'SystemId': account.get('SystemId'),
+            'SystemID': account.get('SystemId'),
             'SystemName': account.get('SystemName'),
             'DomainName': account.get('DomainName'),
-            'AccountId': account.get('AccountId'),
+            'AccountID': account.get('AccountId'),
             'AccountName': account.get('AccountName')
         })
 
-    ec = {'BeyondTrust.Account(val.AccountId === obj.AccountId)': managed_accounts}
+    entry_context = {'BeyondTrust.Account(val.AccountId === obj.AccountId)': managed_accounts}
 
-    return_outputs(tableToMarkdown('BeyondTrust Managed Accounts', data, removeNull=True), ec, managed_accounts)
+    return_outputs(tableToMarkdown('BeyondTrust Managed Accounts', data, removeNull=True), entry_context,
+                   managed_accounts)
 
 
 def get_managed_systems_request():
-
     suffix_url = '/managedsystems'
     response = http_request('GET', suffix_url)
 
@@ -143,13 +130,13 @@ def get_managed_systems():
             'Port': managed_system.get('Port')
         })
 
-    ec = {'BeyondTrust.System(val.ManagedSystemID === obj.ManagedSystemID)': managed_systems}
+    entry_context = {'BeyondTrust.System(val.ManagedSystemID === obj.ManagedSystemID)': managed_systems}
 
-    return_outputs(tableToMarkdown('BeyondTrust Managed Systems', data, removeNull=True), ec, managed_systems)
+    return_outputs(tableToMarkdown('BeyondTrust Managed Systems', data, removeNull=True), entry_context,
+                   managed_systems)
 
 
 def create_release_request(data):
-
     suffix_url = '/requests'
     response = http_request('POST', suffix_url, data=data)
 
@@ -160,11 +147,33 @@ def create_release():
     """
     Creates a new release request.
     Retrieves the credentials for an approved and active (not expired) credentials release request.
+
+    demisto parameter: (string) access_type
+        The type of access requested (View, RDP, SSH). Defualt is "View".
+
+    demisto parameter: (int) system_id
+        ID of the Managed System to request.
+
+    demisto parameter: (int) account_id
+        ID of the Managed Account to request.
+
+    demisto parameter: (int) duration_minutes
+        The request duration (in minutes).
+
+    demisto parameter: (string) reason
+        The reason for the request.
+
+    demisto parameter: (int) access_policy_schedule_id
+        The Schedule ID of an Access Policy to use for the request. If omitted, automatically selects the best schedule.
+
+    demisto parameter: (bool) conflict_option
+        The conflict resolution option to use if an existing request is found for the same user,
+        system, and account ("reuse" or "renew").
     """
     access_type = demisto.args().get('access_type')
-    system_id = demisto.args().get('system_id').encode('utf-8')
-    account_id = demisto.args().get('account_id').encode('utf-8')
-    duration_minutes = demisto.args().get('duration_minutes').encode('utf-8')
+    system_id = demisto.args().get('system_id')
+    account_id = demisto.args().get('account_id')
+    duration_minutes = demisto.args().get('duration_minutes')
     reason = demisto.args().get('reason')
     access_policy_schedule_id = demisto.args().get('access_policy_schedule_id')
     conflict_option = demisto.args().get('conflict_option')
@@ -197,34 +206,34 @@ def create_release():
         'Credentials': credentials
     }
 
-    ec = {'BeyondTrust.Request(val.AccountId === obj.AccountId)':createContext(response)}
-    return_outputs(tableToMarkdown('The request has been Successful', response), ec, response)
+    entry_context = {'BeyondTrust.Request(val.AccountId === obj.AccountId)': createContext(response)}
+    return_outputs(tableToMarkdown('The request has been Successful', response), entry_context, response)
 
 
 def get_credentials_request(request_id):
-
     suffix_url = '/credentials/' + request_id
     response = http_request('GET', suffix_url)
 
     return response
 
 
-# def get_credentials():
-#     """
-#     Retrieves the credentials for an approved and active (not expired) credentials release request.
-#     """
+def get_credentials():
+    """
+    Retrieves the credentials for an approved and active (not expired) credentials release request.
 
-#     request_id = demisto.args().get('request_id')
-#     response = get_credentials_request(request_id)
+    demisto parameter: (int) request_id
+        ID of the Request for which to retrieve the credentials
+    """
 
-#     ec = {'BeyondTrust.Request(val.request_id === obj.request_id)': response}
+    request_id = demisto.args().get('request_id')
+    request = str(request_id)
+    response = get_credentials_request(request)
 
-#     return_outputs(tableToMarkdown('BeyondTrust Release Request', response, removeNull=True), ec, response)
+    demisto.results('The credentials for BeyondTrust request: ' + response)
 
 
 def check_in_credentials_request(request_id, data):
-
-    suffix_url = '/Requests/' + request_id + '/Checkin'
+    suffix_url = f'/Requests/{request_id}/Checkin'
     response = http_request('PUT', suffix_url, data=json.dumps(data))
 
     return response
@@ -233,6 +242,13 @@ def check_in_credentials_request(request_id, data):
 def check_in_credentials():
     """
     Checks-in/releases a request before it has expired.
+
+    demisto parameter: (int) request_id
+        ID of the request to release.
+
+    demisto parameter: (string) reason
+        A reason or comment why the request is being released.
+
     """
     request_id = demisto.args().get('request_id')
     reason = demisto.args().get('reason')
@@ -244,44 +260,62 @@ def check_in_credentials():
     demisto.results('The release was successfully checked-in/released')
 
 
-def change_credentials_request(account_id, data):
-
-    suffix_url = '/ManagedAccounts/' + account_id + '/Credentials'
-    response = http_request('PUT', suffix_url, data=json.dumps(data))
-
-    return response
-
-
-def change_credentials():
-    """
-    Updates the credentials for a Managed Account, optionally applying the change to the Managed System.
-    """
-    account_id = demisto.args().get('account_id')
-    password = demisto.args().get('password')
-    public_key = demisto.args().get('public_key')
-    private_key = demisto.args().get('private_key')
-    pass_phrase = demisto.args().get('pass_phrase')
-    update_system = demisto.args().get('update_system')
-
-    data = {
-        'AccountId': account_id
-    }
-
-    if password:
-        data['Password'] = password
-
-    if private_key:
-        if public_key and update_system == True:
-            data['PrivateKey'] = private_key
-            data['PublicKey'] = public_key
-        else:
-            demisto.results('Please provide public key')
-
-    if pass_phrase:
-        data['Passphrase'] = pass_phrase
-    change_request = change_credentials_request(account_id, data)
-
-    demisto.results('The password has been changed')
+# def change_credentials_request(account_id, data):
+#     suffix_url = f'/ManagedAccounts/{account_id}/Credentials'
+#     response = http_request('PUT', suffix_url, data=json.dumps(data))
+#
+#     return response
+#
+#
+# def change_credentials():
+#     """
+#     Updates the credentials for a Managed Account, optionally applying the change to the Managed System.
+#
+#     demisto parameter: (int) account_id
+#         ID of the account for which to set the credentials.
+#
+#     demisto parameter: (string) password
+#         The new password to set. If not given, generates a new, random password.
+#
+#     demisto parameter: (string) public_key
+#         The new public key to set on the host. This is required if PrivateKey is given and updateSystem=true.
+#
+#     demisto parameter: (string) private_key
+#         The private key to set (provide Passphrase if encrypted).
+#
+#     demisto parameter: (string) pass_phrase
+#         The passphrase to use for an encrypted private key.
+#
+#     demisto parameter: (bool) update_system
+#         Whether to update the credentials on the referenced system.
+#
+#     """
+#     account_id = demisto.args().get('account_id')
+#     password = demisto.args().get('password')
+#     public_key = demisto.args().get('public_key')
+#     private_key = demisto.args().get('private_key')
+#     pass_phrase = demisto.args().get('pass_phrase')
+#     update_system = demisto.args().get('update_system')
+#
+#     data = {
+#         'AccountId': account_id
+#     }
+#
+#     if password:
+#         data['Password'] = password
+#
+#     if private_key:
+#         if public_key and update_system == True:
+#             data['PrivateKey'] = private_key
+#             data['PublicKey'] = public_key
+#         else:
+#             return_error('Missing public key')
+#
+#     if pass_phrase:
+#         data['Passphrase'] = pass_phrase
+#     change_request = change_credentials_request(account_id, data)
+#
+#     demisto.results('The password has been changed')
 
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
@@ -289,6 +323,7 @@ def change_credentials():
 LOG('Command being called is %s' % (demisto.command()))
 
 try:
+    handle_proxy()
     signin()
     if demisto.command() == 'test-module':
         # This is the call made when pressing the integration test button.
@@ -300,6 +335,8 @@ try:
         get_managed_systems()
     elif demisto.command() == 'beyondtrust-create-release-request':
         create_release()
+    elif demisto.command() == 'beyondtrust-get-credentials':
+        get_credentials()
     elif demisto.command() == 'beyondtrust-check-in-credentials':
         check_in_credentials()
     elif demisto.command() == 'beyondtrust-change-credentials':
@@ -308,7 +345,7 @@ try:
         fetch_credentials()
 
 # Log exceptions
-except Exception, e:
+except Exception as e:
     LOG(e.message)
     LOG.print_log()
     raise
