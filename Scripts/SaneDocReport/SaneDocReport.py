d3d797b5bf5ec1cba75bfedd2797f22ca3f5d1ed
Agam More <agam.more@demisto.com>
2019-06-10 16:05:13 +0300

    Remove unnecessary lines and seprecate so users wont see, fix parameter name convention

--- Scripts/SaneDocReport/SaneDocReport.py
+++ Scripts/SaneDocReport/SaneDocReport.py
@@ -5,7 +5,7 @@ import base64
 from sane_doc_reports.main import run
 
 try:
-    sane_json_b64 = demisto.args().get('saneJsonBase64', '').encode("utf-8")
+    sane_json_b64 = demisto.args().get('sane_docx_report_base64', '').encode("utf-8")
     with open('sane.json', 'wb') as f:
         f.write(base64.b64decode(sane_json_b64))
 
@@ -13,9 +13,6 @@ try:
 
     with open('out.docx', 'rb') as f:
         encoded = base64.b64encode(f.read()).decode("utf-8", "ignore")
-        out = {'data': 'encoded'}
-        ec_data = createContext(data=out)
-        ec = {'saneDocReport.data': ec_data}
         return_outputs(readable_output='Successfully generated docx',
                        outputs={}, raw_response={'data': encoded})
 except Exception as e:
