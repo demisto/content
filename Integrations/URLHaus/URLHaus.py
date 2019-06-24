29ce9078a14e44481c1cc7605050295dc82f6803
shakedp <pol.shaked@gmail.com>
2019-06-24 18:48:25 +0300

    Added ip and domain commands

--- Integrations/URLHaus/URLHaus.py
+++ Integrations/URLHaus/URLHaus.py
@@ -4,7 +4,6 @@ from CommonServerUserPython import *
 
 ''' IMPORTS '''
 import traceback
-import json
 import requests
 from datetime import datetime as dt
 
@@ -62,7 +61,7 @@ def query_url_information(url):
 
 def query_host_information(host):
     return http_request('POST',
-                        'https://urlhaus-api.abuse.ch/v1/host/',  # disable-secrets-detection
+                        'host',
                         f'host={host}')
 
 
@@ -90,17 +89,6 @@ def download_malware_sample(sha256, dest):
         malware_sample.write(res.content)
 
 
-def item_to_incident(item):
-    incident = {}
-    # Incident Title
-    incident['name'] = 'Example Incident: ' + item.get('name')
-    # Incident occurrence time, usually item creation date in service
-    incident['occurred'] = item.get('createdDate')
-    # The raw response from the service, providing full info regarding the item
-    incident['rawJSON'] = json.dumps(item)
-    return incident
-
-
 ''' COMMANDS + REQUESTS FUNCTIONS '''
 
 
@@ -109,6 +97,32 @@ def test_module():
     Performs basic get request to get item samples
     """
     http_request('POST', 'url')
+
+
+def calculate_dbot_score(blacklists, threshold, compromised_is_malicious):
+    blacklist_appearances = []
+    for blacklist, status in blacklists.items():
+        if blacklist == 'spamhaus_dbl':
+            if status.endswith('domain') or (status.startswith('abused') and compromised_is_malicious):
+                blacklist_appearances.append((blacklist, status))
+        elif status == 'listed':
+            blacklist_appearances.append((blacklist,))
+
+    if len(blacklist_appearances) >= threshold:
+        description = ''
+        for appearance in blacklist_appearances:
+            if len(appearance) == 1:
+                description += f'Listed in {appearance[0]}. '
+            elif len(appearance) == 2:
+                description += f'Listed as {appearance[1]} in {appearance[0]}. '
+            else:
+                raise Exception('Unknown blacklist format in the response')
+
+        return 3, description
+    elif len(blacklist_appearances) > 0:
+        return 2, None
+    else:
+        return 1, None
 
 
 def url_command():
@@ -153,34 +167,15 @@ def url_command():
             urlhaus_data['Payloads'] = payloads
 
             # DBot score calculation
-            blacklist_appearances = []
-            for blacklist, status in url_information.get('blacklists', {}).items():
-                if blacklist == 'spamhaus_dbl':
-                    if status.endswith('domain') or (status.startswith('abused') and COMPROMISED_IS_MALICIOUS):
-                        blacklist_appearances.append((blacklist, status))
-                elif status == 'listed':
-                    blacklist_appearances.append((blacklist,))
+            dbot_score, description = calculate_dbot_score(url_information.get('blacklists', {}), THRESHOLD,
+                                                           COMPROMISED_IS_MALICIOUS)
 
-            if len(blacklist_appearances) >= THRESHOLD:
-                ec['DBotScore']['Score'] = 3
+            ec['DBotScore']['Score'] = dbot_score
+            if dbot_score == 3:
                 ec['URL']['Malicious'] = {
-                    'Vendor': 'URLhaus'
+                    'Vendor': 'URLhaus',
+                    'Description': description
                 }
-
-                description = ''
-                for appearance in blacklist_appearances:
-                    if len(appearance) == 1:
-                        description += f'Listed in {appearance[0]}. '
-                    elif len(appearance) == 2:
-                        description += f'Listed as {appearance[1]} in {appearance[0]}. '
-                    else:
-                        raise Exception('Unknown blacklist format in the response')
-
-                ec['URL']['Malicious']['Description'] = description
-            elif len(blacklist_appearances) > 0:
-                ec['DBotScore']['Score'] = 2
-            else:
-                ec['DBotScore']['Score'] = 1
 
             ec['URLhaus.URL(val.ID && val.ID === obj.ID)'] = urlhaus_data
 
@@ -234,96 +229,187 @@ def url_command():
         demisto.debug(traceback.format_exc())
         return_error('Failed getting url data, please verify the arguments and parameters')
 
-# def get_items_command():
-#     """
-#     Gets details about a items using IDs or some other filters
-#     """
-#     # Init main vars
-#     headers = []
-#     contents = []
-#     context = {}
-#     context_entries = []
-#     title = ''
-#     # Get arguments from user
-#     item_ids = argToList(demisto.args().get('item_ids', []))
-#     is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
-#     limit = int(demisto.args().get('limit', 10))
-#     # Make request and get raw response
-#     items = get_items_request(item_ids, is_active)
-#     # Parse response into context & content entries
-#     if items:
-#         if limit:
-#             items = items[:limit]
-#         title = 'Example - Getting Items Details'
-#
-#         for item in items:
-#             contents.append({
-#                 'ID': item.get('id'),
-#                 'Description': item.get('description'),
-#                 'Name': item.get('name'),
-#                 'Created Date': item.get('createdDate')
-#             })
-#             context_entries.append({
-#                 'ID': item.get('id'),
-#                 'Description': item.get('description'),
-#                 'Name': item.get('name'),
-#                 'CreatedDate': item.get('createdDate')
-#             })
-#
-#         context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
-#
-#     demisto.results({
-#         'Type': entryTypes['note'],
-#         'ContentsFormat': formats['json'],
-#         'Contents': contents,
-#         'ReadableContentsFormat': formats['markdown'],
-#         'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
-#         'EntryContext': context
-#     })
-#
-#
-# def get_items_request(item_ids, is_active):
-#     # The service endpoint to request from
-#     endpoint_url = 'items'
-#     # Dictionary of params for the request
-#     params = {
-#         'ids': item_ids,
-#         'isActive': is_active
-#     }
-#     # Send a request using our http_request wrapper
-#     response = http_request('GET', endpoint_url, params)
-#     # Check if response contains errors
-#     if response.get('errors'):
-#         return_error(response.get('errors'))
-#     # Check if response contains any data to parse
-#     if 'data' in response:
-#         return response.get('data')
-#     # If neither was found, return back empty results
-#     return {}
-#
-#
-# def fetch_incidents():
-#     last_run = demisto.getLastRun()
-#     # Get the last fetch time, if exists
-#     last_fetch = last_run.get('time')
-#
-#     # Handle first time fetch, fetch incidents retroactively
-#     if last_fetch is None:
-#         last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
-#
-#     incidents = []
-#     items = get_items_request()
-#     for item in items:
-#         incident = item_to_incident(item)
-#         incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
-#         # Update last run and add incident if the incident is newer than last fetch
-#         if incident_date > last_fetch:
-#             last_fetch = incident_date
-#             incidents.append(incident)
-#
-#     demisto.setLastRun({'time' : last_fetch})
-#     demisto.incidents(incidents)
 
+def ip_command():
+    ip = demisto.args()['ip']
+
+    try:
+        ip_information = query_host_information(ip).json()
+
+        ec = {
+            'URL': {
+                'Data': ip
+            },
+            'DBotScore': {
+                'Type': 'ip',
+                'Vendor': 'URLhaus',
+                'Indicator': ip
+            }
+        }
+
+        if ip_information['query_status'] == 'ok':
+            # URLHaus output
+            first_seen = dt.strptime(ip_information.get('date_added', '1970-01-01 00:00:00 UTC'),
+                                     '%Y-%m-%d %H:%M:%S UTC').strftime('%Y-%m-%dT%H:%M:%S')
+
+            urlhaus_data = {
+                'FirstSeen': first_seen,
+                'Blacklist': ip_information.get('blacklists', {}),
+                'URLs': ip_information.get('urls', [])
+            }
+
+            # DBot score calculation
+            dbot_score, description = calculate_dbot_score(ip_information.get('blacklists', {}), THRESHOLD,
+                                                           COMPROMISED_IS_MALICIOUS)
+
+            ec['DBotScore']['Score'] = dbot_score
+            if dbot_score == 3:
+                ec['IP']['Malicious'] = {
+                    'Vendor': 'URLhaus',
+                    'Description': description
+                }
+
+            ec['URLhaus.IP(val.Address && val.Address === obj.Address)'] = urlhaus_data
+
+            human_readable = f'## URLhaus reputation for {ip}\n' \
+                f'URLhaus link: {ip_information.get("urlhaus_reference", "None")}\n' \
+                f'First seen: {first_seen}'
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'ContentsFormat': formats['json'],
+                'Contents': ip_information,
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+        elif ip_information['query_status'] == 'no_results':
+            ec['DBotScore']['Score'] = 0
+
+            human_readable = f'## URLhaus reputation for {ip}\n' \
+                f'No results!'
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'ContentsFormat': formats['json'],
+                'Contents': ip_information,
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+        elif ip_information['query_status'] == 'invalid_host':
+            human_readable = f'## URLhaus reputation for {ip}\n' \
+                f'Invalid IP!'
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'ContentsFormat': formats['json'],
+                'Contents': ip_information,
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+        else:
+            demisto.results({
+                'Type': entryTypes['error'],
+                'ContentsFormat': formats['text'],
+                'Contents': f'Query results = {ip_information["query_status"]}'
+            })
+
+    except Exception:
+        demisto.debug(traceback.format_exc())
+        return_error('Failed getting IP data, please verify the arguments and parameters')
+
+
+def domain_command():
+    domain = demisto.args()['domain']
+
+    try:
+        domain_information = query_host_information(domain).json()
+
+        ec = {
+            'Domain': {
+                'Name': domain
+            },
+            'DBotScore': {
+                'Type': 'domain',
+                'Vendor': 'URLhaus',
+                'Indicator': domain
+            }
+        }
+
+        if domain_information['query_status'] == 'ok':
+            # URLHaus output
+            first_seen = dt.strptime(domain_information.get('firstseen', '1970-01-01 00:00:00 UTC'),
+                                     '%Y-%m-%d %H:%M:%S UTC').strftime('%Y-%m-%dT%H:%M:%S')
+
+            urlhaus_data = {
+                'FirstSeen': first_seen,
+                'Blacklist': domain_information.get('blacklists', {}),
+                'URLs': domain_information.get('urls', [])
+            }
+
+            # DBot score calculation
+            dbot_score, description = calculate_dbot_score(domain_information.get('blacklists', {}), THRESHOLD,
+                                                           COMPROMISED_IS_MALICIOUS)
+
+            ec['DBotScore']['Score'] = dbot_score
+            if dbot_score == 3:
+                ec['domain']['Malicious'] = {
+                    'Vendor': 'URLhaus',
+                    'Description': description
+                }
+
+            ec['URLhaus.Domain(val.Name && val.Name === obj.Name)'] = urlhaus_data
+
+            human_readable = f'## URLhaus reputation for {domain}\n' \
+                f'URLhaus link: {domain_information.get("urlhaus_reference", "None")}\n' \
+                f'First seen: {first_seen}'
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'ContentsFormat': formats['json'],
+                'Contents': domain_information,
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+        elif domain_information['query_status'] == 'no_results':
+            ec['DBotScore']['Score'] = 0
+
+            human_readable = f'## URLhaus reputation for {domain}\n' \
+                f'No results!'
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'ContentsFormat': formats['json'],
+                'Contents': domain_information,
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+        elif domain_information['query_status'] == 'invalid_host':
+            human_readable = f'## URLhaus reputation for {domain}\n' \
+                f'Invalid domain!'
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'ContentsFormat': formats['json'],
+                'Contents': domain_information,
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+        else:
+            demisto.results({
+                'Type': entryTypes['error'],
+                'ContentsFormat': formats['text'],
+                'Contents': f'Query results = {domain_information["query_status"]}'
+            })
+
+    except Exception:
+        demisto.debug(traceback.format_exc())
+        return_error('Failed getting domain data, please verify the arguments and parameters')
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
 
@@ -336,6 +422,10 @@ try:
         demisto.results('ok')
     elif demisto.command() == 'url':
         url_command()
+    elif demisto.command() == 'ip':
+        ip_command()
+    elif demisto.command() == 'domain':
+        domain_command()
 
 # Log exceptions
 except Exception as e:
