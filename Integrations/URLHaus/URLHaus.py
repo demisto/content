3ee79d4c74441695f8ac06481dbe6ff5ca43f1a8
shakedp <pol.shaked@gmail.com>
2019-06-24 15:28:20 +0300

    Added url command

--- Integrations/URLHaus/URLHaus.py
+++ Integrations/URLHaus/URLHaus.py
@@ -1,11 +1,11 @@
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
-''' IMPORTS '''
 
+''' IMPORTS '''
+import traceback
 import json
 import requests
-from distutils.util import strtobool
 
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
@@ -25,14 +25,15 @@ if not demisto.params().get('proxy'):
     del os.environ['http_proxy']
     del os.environ['https_proxy']
 
-THRESHOLD = demisto.params().get('threshold')
+THRESHOLD = int(demisto.params().get('threshold', 1))
+
+COMPROMISED_IS_MALICIOUS = demisto.params().get('compropmised_is_malicious', False)
 
 # Headers to be sent in requests
 HEADERS = {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json'
 }
-
 
 ''' HELPER FUNCTIONS '''
 
@@ -54,38 +55,38 @@ def http_request(method, command, data=None):
 
 def query_url_information(url):
     return http_request('POST',
-                         'https://urlhaus-api.abuse.ch/v1/url/',  # disable-secrets-detection
+                        'url',
                         f'url={url}')
 
 
 def query_host_information(host):
     return http_request('POST',
-                                'https://urlhaus-api.abuse.ch/v1/host/',  # disable-secrets-detection
+                        'https://urlhaus-api.abuse.ch/v1/host/',  # disable-secrets-detection
                         f'host={host}')
 
 
 def query_payload_information(hash_type, hash):
     return http_request('POST',
-                                'https://urlhaus-api.abuse.ch/v1/payload/',  # disable-secrets-detection
+                        'https://urlhaus-api.abuse.ch/v1/payload/',  # disable-secrets-detection
                         f'{hash_type}_hash={hash}')
 
 
 def query_tag_information(tag):
     return http_request('POST',
-                                'https://urlhaus-api.abuse.ch/v1/tag/',  # disable-secrets-detection
+                        'https://urlhaus-api.abuse.ch/v1/tag/',  # disable-secrets-detection
                         f'tag={tag}')
 
 
 def query_signature_information(signature):
     return http_request('POST',
-                                'https://urlhaus-api.abuse.ch/v1/signature/',  # disable-secrets-detection
+                        'https://urlhaus-api.abuse.ch/v1/signature/',  # disable-secrets-detection
                         f'signature={signature}')
 
+
 def download_malware_sample(sha256, dest):
-    res = requests.get(f'https://urlhaus-api.abuse.ch/v1/download/{sha256}/')# disable-secrets-detection
+    res = requests.get(f'https://urlhaus-api.abuse.ch/v1/download/{sha256}/')  # disable-secrets-detection
     with open(dest, 'wb') as malware_sample:
         malware_sample.write(res.content)
-
 
 
 def item_to_incident(item):
@@ -107,6 +108,98 @@ def test_module():
     Performs basic get request to get item samples
     """
     http_request('POST', 'url')
+
+
+def url_command():
+    url = demisto.args().get('url')
+    try:
+        url_information = query_url_information(url).json()
+
+        ec = {
+            'URL': {
+                'Data': url
+            },
+            'DBotScore': {
+                'Type': 'URL',
+                'Vendor': 'URLhaus',
+                'Indicator': url
+            }
+        }
+
+        if url_information['query_status'] == 'ok':
+            # URLHaus output
+            urlhaus_data = {
+                'ID': url_information['id'],
+                'Status': url_information['url_status'],
+                'Host': url_information['host'],
+                'DateAdded': url_information['date_added'],  # TODO - is UTC ok?
+                'Threat': url_information['threat'],
+                'Blacklist': url_information['blacklists'],
+                'Tags': url_information['tags']
+            }
+
+            payloads = []
+            for payload in url_information['payloads']:
+                payloads.append({
+                    'Name': payload['filename'] if payload['filename'] else 'unknown',
+                    'Type': payload['file_type'],
+                    'MD5': payload['response_md5'],
+                    'VT': payload['virustotal']
+                })
+
+            urlhaus_data['Payloads'] = payloads
+
+            # DBot score calculation
+            blacklist_appearances = []
+            for blacklist, status in url_information['blacklists'].items():
+                if blacklist == 'spamhaus_dbl':
+                    if status.endswith('domain') or (status.startswith('abused') and COMPROMISED_IS_MALICIOUS):
+                        blacklist_appearances.append((blacklist, status))
+                elif status == 'listed':
+                    blacklist_appearances.append((blacklist,))
+
+            if len(blacklist_appearances) >= THRESHOLD:
+                ec['DBotScore']['Score'] = 3
+                ec['URL']['Malicious'] = {
+                    'Vendor': 'URLhaus'
+                }
+
+                description = ''
+                for appearance in blacklist_appearances:
+                    if len(appearance) == 1:
+                        description += f'Listed in {appearance[0]}. '
+                    elif len(appearance) == 2:
+                        description += f'Listed as {appearance[1]} in {appearance[0]}. '
+                    else:
+                        raise Exception('Unknown blacklist format in the response')
+                ec['URL']['Malicious']['Description'] = description
+            elif len(blacklist_appearances) > 0:
+                ec['DBotScore']['Score'] = 2
+            else:
+                ec['DBotScore']['Score'] = 1
+
+            ec['URLhaus.URL(val.ID && val.ID === obj.ID)'] = urlhaus_data
+
+        elif url_information['query_status'] == 'no_results':
+            ec['DBotScore']['Score'] = 0
+        else:
+            demisto.results({
+                'Type': entryTypes['error'],
+                'ContentsFormat': formats['text'],
+                'Contents': f'Query results = {url_information["query_status"]}'
+            })
+
+        demisto.results({
+            'Type': entryTypes['note'],
+            'ContentsFormat': formats['json'],
+            'Contents': url_information,
+            'EntryContext': ec
+        })
+
+        demisto.results(url_information)
+    except Exception as e:
+        demisto.debug(traceback.format_exc())
+        return_error(f'Failed getting url data, please verify the arguments and parameters')
 
 
 # def get_items_command():
@@ -204,21 +297,23 @@ def test_module():
 
 LOG('Command being called is %s' % (demisto.command()))
 
-#print(query_url_information('http://sskymedia.com/VMYB-ht_JAQo-gi/INV/99401FORPO/20673114777/US/Outstanding-Invoices/'))
-#print(query_host_information('vektorex.com'))
-#print(query_payload_information('md5', '12c8aec5766ac3e6f26f2505e2f4a8f2'))
-#print(query_tag_information('Retefe'))
-#print(query_signature_information('Gozi'))
-#print(download_malware_sample('254ca6a7a7ef7f17d9884c4a86f88b5d5fd8fe5341c0996eaaf1d4bcb3b2337b', 'here.zip'))
+# print(query_url_information('http://sskymedia.com/VMYB-ht_JAQo-gi/INV/99401FORPO/20673114777/US/Outstanding-Invoices/'))
+# print(query_host_information('vektorex.com'))
+# print(query_payload_information('md5', '12c8aec5766ac3e6f26f2505e2f4a8f2'))
+# print(query_tag_information('Retefe'))
+# print(query_signature_information('Gozi'))
+# print(download_malware_sample('254ca6a7a7ef7f17d9884c4a86f88b5d5fd8fe5341c0996eaaf1d4bcb3b2337b', 'here.zip'))
 
 try:
     if demisto.command() == 'test-module':
         # This is the call made when pressing the integration test button.
         test_module()
         demisto.results('ok')
+    elif demisto.command() == 'url':
+        url_command()
 
 # Log exceptions
 except Exception as e:
-    LOG(e.message)
+    LOG(str(e))
     LOG.print_log()
     raise
