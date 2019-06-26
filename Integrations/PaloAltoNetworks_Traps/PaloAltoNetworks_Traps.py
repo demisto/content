9c53d2fd1e57156936035198dc3b314480ccbef9
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-06-26 22:15:00 +0300

    Added get endpoint command

--- Integrations/PaloAltoNetworks_Traps/PaloAltoNetworks_Traps.py
+++ Integrations/PaloAltoNetworks_Traps/PaloAltoNetworks_Traps.py
@@ -1,3 +1,5 @@
+import inspect
+
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
@@ -38,10 +40,30 @@ HEADERS = {
 # Remove proxy if not set to true in params
 handle_proxy()
 
+OUTPUTS = {
+    'get_endpoint_by_id': {
+        'ID': 'guid',
+        'name': 'name',
+        'domain': 'domain',
+        'platform': 'platform',
+        'status': 'status',
+        'ip': 'ip',
+        'computer_sid': 'computerSid',
+        'is_compromised': 'compromised',
+        'os_version': 'osVersion',
+        'os_product_type': 'osProductType',
+        'os_product_name': 'osProductName',
+        'is_64': 'is64',
+        'last_seen': 'lastSeen',
+        'last_user': 'lastUser'
+    }
+
+}
+
 ''' HELPER FUNCTIONS '''
 
 
-def http_request(method, url_suffix, test=False, params=None, data=None, operation=None):
+def http_request(method, url_suffix, test=False, params=None, data=None, operation_err=None):
     # A wrapper for requests lib to send our requests and handle requests and responses better
     try:
         res = requests.request(
@@ -54,17 +76,17 @@ def http_request(method, url_suffix, test=False, params=None, data=None, operati
         )
     except requests.exceptions.ConnectionError:
         return_error(f'Invalid server address')
-    return parse_http_response(res, operation, test)
+    return parse_http_response(res, operation_err, test)
 
 
-def parse_http_response(resp, operation, test=False):
+def parse_http_response(resp, operation_err_message, test=False):
     try:
         resp.raise_for_status()
         return resp.json() if not test else resp.text
     # except requests.exceptions.HTTPError as err:
     # Handle error responses gracefully
     except Exception as err:
-        return_error(f'Error in {operation}: {err}')
+        return_error(f'{operation_err_message}: {err}')
 
 
 # def item_to_incident(item):
@@ -88,6 +110,22 @@ def health_check():
         return_error(f'Server health-check failed. Status returned was: {server_status} ')
 
 
+def parse_data_from_responce(endpoint_data):
+    new_endpoint_data = {}  # type: dict
+    operation_name = inspect.stack()[1].function  # Get the caller function mame
+    outputs_obj = OUTPUTS[operation_name]
+    for key, val in outputs_obj.items():
+        new_endpoint_data[key] = endpoint_data.get(val)
+
+    return new_endpoint_data
+
+
+def get_endpoint_by_id(endpoint_id):
+    path = f'agents/{endpoint_id}'
+    endpoint_data = http_request('GET', path, operation_err=f'Get endpoint {endpoint_id} failed')
+    return parse_data_from_responce(endpoint_data), endpoint_data
+
+
 ''' COMMANDS + REQUESTS FUNCTIONS '''
 
 
@@ -95,14 +133,17 @@ def test_module_command():
     health_check()
     return
 
-    # demisto.results({
-    #     'Type': entryTypes['note'],
-    #     'ContentsFormat': formats['json'],
-    #     'Contents': contents,
-    #     'ReadableContentsFormat': formats['markdown'],
-    #     'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
-    #     'EntryContext': context
-    # })
+
+def get_endpoint_by_id_command():
+    args = demisto.args()
+    endpoint_id = args.get('endpoint_id')
+    endpoint_data, raw_data = get_endpoint_by_id(endpoint_id)
+    endpoint = {'Name': endpoint_data.get('name'),
+                'Data': createContext(endpoint_data, keyTransform=string_to_context_key())}
+    md = tableToMarkdown(f'Endpoint {endpoint_id} data:', endpoint_data, headerTransform=string_to_table_header())
+    context = {'Traps.Endpoints(val.Data.ID == obj.Data.ID)': endpoint}
+    return_outputs(md, context, raw_response=raw_data)
+
 
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
@@ -114,9 +155,9 @@ try:
         # This is the call made when pressing the integration test button.
         test_module_command()
         demisto.results('ok')
-    # elif demisto.command() == 'traps-get-endpoint-by-id':
-    #     # An example command
-    #     get_items_command()
+    elif demisto.command() == 'traps-get-endpoint-by-id':
+        # An example command
+        get_endpoint_by_id_command()
 
 # Log exceptions
 except Exception as e:
