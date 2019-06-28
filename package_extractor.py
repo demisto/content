391fc90fab0481df2899315d369b7307f5e8c16b
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-06-28 11:57:10 +0300

    start of migrate option

--- package_extractor.py
+++ package_extractor.py
@@ -2,6 +2,7 @@
 
 import yaml
 import argparse
+import os
 from io import open
 
 INTEGRATION = 'integration'
@@ -40,11 +41,20 @@ def str2bool(val):
     return val.lower() in {'yes', 'true', 't', '1', 'y'}
 
 
+def migrate(yml_path, output_path, demisto_mock, commonserver=None, yml_type=None):
+    print("Starting migration of: {} to dir: {}".format(yml_path, output_path))
+    output_path = os.path.abspath(output_path)
+    base_name = os.path.basename(output_path)
+    extract_code(yml_path, "{}/{}.py".format(output_path, base_name), demisto_mock, commonserver, yml_type)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Extract code file from a demmisto integration or script yaml file',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--infile", help="The yml file to extract from", required=True)
-    parser.add_argument("-o", "--outfile", help="The output file to write the code to", required=True)
+    parser.add_argument("-o", "--outfile", help="The output file or dir (if doing migrate) to write the code to", required=True)
+    parser.add_argument("-m", "--migrate", action='store_true',
+                        help="Migrate an integration to package format. Pass to -o option a directory in this case.")
     parser.add_argument("-t", "--type", help="Yaml type. If not specified will try to determine type based upon path.",
                         choices=[SCRIPT, INTEGRATION], default=None)
     parser.add_argument("-d", "--demistomock", help="Add an import for demisto mock",
@@ -54,7 +64,10 @@ def main():
                               + " If not specified will import unless this is CommonServerPython"),
                         choices=[True, False], type=str2bool, default=None)
     args = parser.parse_args()
-    extract_code(args.infile, args.outfile, args.demistomock, args.commonserver, args.type)
+    if args.migrate:
+        migrate(args.infile, args.outfile, args.demistomock, args.commonserver, args.type)
+    else:
+        extract_code(args.infile, args.outfile, args.demistomock, args.commonserver, args.type)
 
 
 if __name__ == "__main__":
