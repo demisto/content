1b630551f09463024551738ff67c7334ef850ed3
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-06-29 21:00:16 +0300

    revert debug print stuff

--- Tests/scripts/run_dev_tasks.sh
+++ Tests/scripts/run_dev_tasks.sh
@@ -27,10 +27,6 @@ fi
 
 if [ -z "${PYTEST_SKIP}" -a -z "${PYTEST_FAIL_NO_TESTS}" ]; then
     echo "collecting tests..."
-    ls -la 
-    ls -ld
-    touch .test-this
-    ls -la
     collect_res=$(python -m pytest --collect-only 2>&1)
     case "$collect_res" in
         *"errors"*)
