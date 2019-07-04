c399136cf0363a603c9f66b1eea31db7c4e01875
shakedp <pol.shaked@gmail.com>
2019-07-04 17:14:24 +0300

    Initial commit with yml

---
+++ Integrations/VulnDB/VulnDB.py
@@ -0,0 +1,193 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+''' IMPORTS '''
+
+import json
+import requests
+from distutils.util import strtobool
+
+# Disable insecure warnings
+requests.packages.urllib3.disable_warnings()
+
+''' GLOBALS/PARAMS '''
+
+USERNAME = demisto.params().get('credentials').get('identifier')
+PASSWORD = demisto.params().get('credentials').get('password')
+TOKEN = demisto.params().get('token')
+# Remove trailing slash to prevent wrong URL path to service
+SERVER = demisto.params()['url'][:-1] \
+    if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
+# Should we use SSL
+USE_SSL = not demisto.params().get('insecure', False)
+# How many time before the first fetch to retrieve incidents
+FETCH_TIME = demisto.params().get('fetch_time', '3 days')
+# Service base URL
+BASE_URL = SERVER + '/api/v2.0/'
+# Headers to be sent in requests
+HEADERS = {
+    'Authorization': 'Token ' + TOKEN + ':' + USERNAME + PASSWORD,
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+# Remove proxy if not set to true in params
+if not demisto.params().get('proxy'):
+    del os.environ['HTTP_PROXY']
+    del os.environ['HTTPS_PROXY']
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
+
+
+''' HELPER FUNCTIONS '''
+
+
+def http_request(method, url_suffix, params=None, data=None):
+    # A wrapper for requests lib to send our requests and handle requests and responses better
+    res = requests.request(
+        method,
+        BASE_URL + url_suffix,
+        verify=USE_SSL,
+        params=params,
+        data=data,
+        headers=HEADERS
+    )
+    # Handle error responses gracefully
+    if res.status_code not in {200}:
+        return_error('Error in API call to Example Integration [%d] - %s' % (res.status_code, res.reason))
+
+    return res.json()
+
+
+def item_to_incident(item):
+    incident = {}
+    # Incident Title
+    incident['name'] = 'Example Incident: ' + item.get('name')
+    # Incident occurrence time, usually item creation date in service
+    incident['occurred'] = item.get('createdDate')
+    # The raw response from the service, providing full info regarding the item
+    incident['rawJSON'] = json.dumps(item)
+    return incident
+
+
+''' COMMANDS + REQUESTS FUNCTIONS '''
+
+
+def test_module():
+    """
+    Performs basic get request to get item samples
+    """
+    samples = http_request('GET', 'items/samples')
+
+
+def get_items_command():
+    """
+    Gets details about a items using IDs or some other filters
+    """
+    # Init main vars
+    headers = []
+    contents = []
+    context = {}
+    context_entries = []
+    title = ''
+    # Get arguments from user
+    item_ids = argToList(demisto.args().get('item_ids', []))
+    is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
+    limit = int(demisto.args().get('limit', 10))
+    # Make request and get raw response
+    items = get_items_request(item_ids, is_active)
+    # Parse response into context & content entries
+    if items:
+        if limit:
+            items = items[:limit]
+        title = 'Example - Getting Items Details'
+
+        for item in items:
+            contents.append({
+                'ID': item.get('id'),
+                'Description': item.get('description'),
+                'Name': item.get('name'),
+                'Created Date': item.get('createdDate')
+            })
+            context_entries.append({
+                'ID': item.get('id'),
+                'Description': item.get('description'),
+                'Name': item.get('name'),
+                'CreatedDate': item.get('createdDate')
+            })
+
+        context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
+
+    demisto.results({
+        'Type': entryTypes['note'],
+        'ContentsFormat': formats['json'],
+        'Contents': contents,
+        'ReadableContentsFormat': formats['markdown'],
+        'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
+        'EntryContext': context
+    })
+
+
+def get_items_request(item_ids, is_active):
+    # The service endpoint to request from
+    endpoint_url = 'items'
+    # Dictionary of params for the request
+    params = {
+        'ids': item_ids,
+        'isActive': is_active
+    }
+    # Send a request using our http_request wrapper
+    response = http_request('GET', endpoint_url, params)
+    # Check if response contains errors
+    if response.get('errors'):
+        return_error(response.get('errors'))
+    # Check if response contains any data to parse
+    if 'data' in response:
+        return response.get('data')
+    # If neither was found, return back empty results
+    return {}
+
+
+def fetch_incidents():
+    last_run = demisto.getLastRun()
+    # Get the last fetch time, if exists
+    last_fetch = last_run.get('time')
+
+    # Handle first time fetch, fetch incidents retroactively
+    if last_fetch is None:
+        last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
+
+    incidents = []
+    items = get_items_request()
+    for item in items:
+        incident = item_to_incident(item)
+        incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        # Update last run and add incident if the incident is newer than last fetch
+        if incident_date > last_fetch:
+            last_fetch = incident_date
+            incidents.append(incident)
+
+    demisto.setLastRun({'time' : last_fetch})
+    demisto.incidents(incidents)
+
+
+''' COMMANDS MANAGER / SWITCH PANEL '''
+
+LOG('Command being called is %s' % (demisto.command()))
+
+try:
+    if demisto.command() == 'test-module':
+        # This is the call made when pressing the integration test button.
+        test_module()
+        demisto.results('ok')
+    elif demisto.command() == 'fetch-incidents':
+        # Set and define the fetch incidents command to run after activated via integration settings.
+        fetch_incidents()
+    elif demisto.command() == 'example-get-items':
+        # An example command
+        get_items_command()
+
+# Log exceptions
+except Exception, e:
+    LOG(e.message)
+    LOG.print_log()
+    raise
