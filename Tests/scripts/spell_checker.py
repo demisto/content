c0b5aa862687e221ea156294fbcb637bbe1f8cac
ronykoz <rkozakish@paloaltonetworks.com>
2019-07-05 11:29:21 +0300

    remove proxy config

--- Tests/scripts/spell_checker.py
+++ Tests/scripts/spell_checker.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import yaml
-import enchant
 import argparse
+
+from spellchecker import SpellChecker
 
 from Tests.test_utils import print_error
 
@@ -15,42 +16,43 @@ DISPLAYABLE_LINES = [
 ]
 
 
-def check_yaml(yml_info, unknown_words):
+def check_yaml(spellchecker, yml_info, unknown_words):
     for key, value in yml_info.items():
         if key in DISPLAYABLE_LINES:
             for word in value.split():
-                if word.isalpha() and not ENGLISH.check(word):
+                if word.isalpha() and spellchecker.unknown([word]):
                     unknown_words.add(word)
 
         else:
             if isinstance(value, dict):
-                check_yaml(value, unknown_words)
+                check_yaml(spellchecker, value, unknown_words)
             elif isinstance(value, list):
                 for sub_list in value:
                     if isinstance(sub_list, dict):
-                        check_yaml(sub_list, unknown_words)
+                        check_yaml(spellchecker, sub_list, unknown_words)
 
 
 def check_md_file(md_data, unknown_words):
     for line in md_data:
         for word in line.split():
-            if word.isalpha() and not ENGLISH.check(word):
+            if word.isalpha() and spellchecker.unknown([word]):
                 unknown_words.add(word)
 
 
 def spell_checker(path, is_md=False):
     unknown_words = set([])
+    spellchecker = SpellChecker()
 
     if is_md:
         with open(path, 'r') as md_file:
             md_data = md_file.readlines()
 
-        check_md_file(md_data, unknown_words)
+        check_md_file(spellchecker, md_data, unknown_words)
     else:
         with open(path, 'r') as yaml_file:
             yml_info = yaml.safe_load(yaml_file)
 
-        check_yaml(yml_info, unknown_words)
+        check_yaml(spellchecker, yml_info, unknown_words)
 
     if unknown_words:
         print_error(u"Found the problematic words:\n{}".format('\n'.join(unknown_words)))
