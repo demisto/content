7cd9e46c86194168e09a2d7b3d38e9d5c93142bd
shakedp <pol.shaked@gmail.com>
2019-07-07 18:47:28 +0300

    Added vulndb-get-vuln-by-id command

--- Integrations/VulnDB/VulnDB.py
+++ Integrations/VulnDB/VulnDB.py
@@ -1,72 +1,50 @@
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
+
 ''' IMPORTS '''
 
-import json
 import requests
-from distutils.util import strtobool
+import urllib.parse
 
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
 ''' GLOBALS/PARAMS '''
 
-USERNAME = demisto.params().get('credentials').get('identifier')
-PASSWORD = demisto.params().get('credentials').get('password')
-TOKEN = demisto.params().get('token')
 # Remove trailing slash to prevent wrong URL path to service
-SERVER = demisto.params()['url'][:-1] \
-    if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
+API_URL = demisto.params()['api_url'].rstrip('/')
+
 # Should we use SSL
 USE_SSL = not demisto.params().get('insecure', False)
-# How many time before the first fetch to retrieve incidents
-FETCH_TIME = demisto.params().get('fetch_time', '3 days')
-# Service base URL
-BASE_URL = SERVER + '/api/v2.0/'
-# Headers to be sent in requests
-HEADERS = {
-    'Authorization': 'Token ' + TOKEN + ':' + USERNAME + PASSWORD,
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-}
+
 # Remove proxy if not set to true in params
 if not demisto.params().get('proxy'):
-    del os.environ['HTTP_PROXY']
-    del os.environ['HTTPS_PROXY']
-    del os.environ['http_proxy']
-    del os.environ['https_proxy']
+    os.environ.pop('HTTP_PROXY', None)
+    os.environ.pop('HTTPS_PROXY', None)
+    os.environ.pop('http_proxy', None)
+    os.environ.pop('https_proxy', None)
 
+CLIENT_ID = demisto.params()['client_id']
+
+CLIENT_SECRET = demisto.params()['client_secret']
 
 ''' HELPER FUNCTIONS '''
 
 
-def http_request(method, url_suffix, params=None, data=None):
-    # A wrapper for requests lib to send our requests and handle requests and responses better
-    res = requests.request(
-        method,
-        BASE_URL + url_suffix,
-        verify=USE_SSL,
-        params=params,
-        data=data,
-        headers=HEADERS
-    )
-    # Handle error responses gracefully
-    if res.status_code not in {200}:
-        return_error('Error in API call to Example Integration [%d] - %s' % (res.status_code, res.reason))
+def get_oath_toekn():
+    # Workaround ParseResult immutability
+    parse_result = list(urllib.parse.urlparse(API_URL))
+    parse_result[2] = '/oauth/token'
+    oath_url = urllib.parse.urlunparse(parse_result)
 
-    return res.json()
-
-
-def item_to_incident(item):
-    incident = {}
-    # Incident Title
-    incident['name'] = 'Example Incident: ' + item.get('name')
-    # Incident occurrence time, usually item creation date in service
-    incident['occurred'] = item.get('createdDate')
-    # The raw response from the service, providing full info regarding the item
-    incident['rawJSON'] = json.dumps(item)
-    return incident
+    return requests.post(oath_url,
+                         verify=USE_SSL,
+                         data={
+                             'client_id': CLIENT_ID,
+                             'client_secret': CLIENT_SECRET,
+                             'grant_type': 'client_credentials'
+                         }).json()['access_token']
 
 
 ''' COMMANDS + REQUESTS FUNCTIONS '''
@@ -76,118 +54,102 @@ def test_module():
     """
     Performs basic get request to get item samples
     """
-    samples = http_request('GET', 'items/samples')
+    get_oath_toekn()
 
 
-def get_items_command():
-    """
-    Gets details about a items using IDs or some other filters
-    """
-    # Init main vars
-    headers = []
-    contents = []
-    context = {}
-    context_entries = []
-    title = ''
-    # Get arguments from user
-    item_ids = argToList(demisto.args().get('item_ids', []))
-    is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
-    limit = int(demisto.args().get('limit', 10))
-    # Make request and get raw response
-    items = get_items_request(item_ids, is_active)
-    # Parse response into context & content entries
-    if items:
-        if limit:
-            items = items[:limit]
-        title = 'Example - Getting Items Details'
+def vulndb_get_vuln_by_id_command():
+    vulndb_id = demisto.args()['vuln_id']
 
-        for item in items:
-            contents.append({
-                'ID': item.get('id'),
-                'Description': item.get('description'),
-                'Name': item.get('name'),
-                'Created Date': item.get('createdDate')
+    res = requests.get(f'{API_URL}/vulnerabilities/{vulndb_id}',
+                       verify=USE_SSL,
+                       headers={'Authorization': f'Bearer {get_oath_toekn()}'}
+                       ).json()
+
+    if 'error' in res:
+        return_error(res['error'])
+    else:
+        vulnerability_data = res['vulnerability']
+        vulnerability_details = {
+            'Title': vulnerability_data.get('title', ''),
+            'Description': vulnerability_data.get('description', ''),
+            'Keywords': vulnerability_data.get('keywords', ''),
+            'PublishedDate': vulnerability_data.get('vulndb_published_date', ''),
+            'TDescription': vulnerability_data.get('t_description', ''),
+            'SolutionDate': vulnerability_data.get('solution_date', ''),
+            'DiscoveryDate': vulnerability_data.get('disclosure_date', ''),
+            'ExploitPublishDate': vulnerability_data.get('exploit_publish_date', ''),
+        }
+
+        cve_ext_reference_values = [ext_reference['value'] for ext_reference in
+                                    vulnerability_data.get('ext_references', [])]
+
+        cvss_metrics_details = [{
+            'Id': cvss_metrics_data.get('id', 0),
+            'AccessVector': cvss_metrics_data.get('access_vector', ''),
+            'AccessComplexity': cvss_metrics_data.get('access_complexity', ''),
+            'Authentication': cvss_metrics_data.get('authentication', ''),
+            'ConfidentialityImpact': cvss_metrics_data.get('confidentiality_impact', ''),
+            'IntegrityImpact': cvss_metrics_data.get('integrity_impact', ''),
+            'AvailabilityImpact': cvss_metrics_data.get('availability_impact', ''),
+            'GeneratedOn': cvss_metrics_data.get('generated_on', ''),
+            'Score': cvss_metrics_data.get('score', 0),
+        } for cvss_metrics_data in vulnerability_data['cvss_metrics']]
+
+        vendor_details = [{'Id': vendor.get('vendor', {'id': 0})['id'], 'Name': vendor.get('vendor', {'name': ''})['name']}
+                          for vendor in vulnerability_data['vendors']]
+
+        product_details = []
+        for product in vulnerability_data['products']:
+            product_versions = [{'Id': version.get('id', ''), 'Name': version.get('name', '')} for version in
+                                product.get('versions', [])]
+            product_details.append({
+                'Id': product.get('id', ''),
+                'Name': product.get('name', ''),
+                'Versions': product_versions
             })
-            context_entries.append({
-                'ID': item.get('id'),
-                'Description': item.get('description'),
-                'Name': item.get('name'),
-                'CreatedDate': item.get('createdDate')
-            })
 
-        context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
+        default_classification = {'longname': '', 'description': ''}
+        classification_details = [{'Longname': classification.get('classification', default_classification)['longname'],
+                                   'Description': classification.get('classification', default_classification)['description']}
+                                  for classification in vulnerability_data['classifications']]
 
-    demisto.results({
-        'Type': entryTypes['note'],
-        'ContentsFormat': formats['json'],
-        'Contents': contents,
-        'ReadableContentsFormat': formats['markdown'],
-        'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
-        'EntryContext': context
-    })
+        ec = {
+            'VulnDB': {
+                'Vulnerability': vulnerability_details,
+                'CVE-ExtReference': {
+                    'Value': cve_ext_reference_values
+                },
+                'CvssMetrics': cvss_metrics_details,
+                'Vendor': vendor_details,
+                'Products': product_details,
+                'Classification': classification_details
+            }
+        }
 
+        human_readable = tableToMarkdown(f'Result for vulnerability ID: {vulndb_id}', {
+            'Title': ec['VulnDB']['Vulnerability']['Title'],
+            'Description': ec['VulnDB']['Vulnerability']['Description'],
+            'Publish Date': ec['VulnDB']['Vulnerability']['PublishedDate'],
+            'Solution Date': ec['VulnDB']['Vulnerability']['SolutionDate']
+        })
 
-def get_items_request(item_ids, is_active):
-    # The service endpoint to request from
-    endpoint_url = 'items'
-    # Dictionary of params for the request
-    params = {
-        'ids': item_ids,
-        'isActive': is_active
-    }
-    # Send a request using our http_request wrapper
-    response = http_request('GET', endpoint_url, params)
-    # Check if response contains errors
-    if response.get('errors'):
-        return_error(response.get('errors'))
-    # Check if response contains any data to parse
-    if 'data' in response:
-        return response.get('data')
-    # If neither was found, return back empty results
-    return {}
-
-
-def fetch_incidents():
-    last_run = demisto.getLastRun()
-    # Get the last fetch time, if exists
-    last_fetch = last_run.get('time')
-
-    # Handle first time fetch, fetch incidents retroactively
-    if last_fetch is None:
-        last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
-
-    incidents = []
-    items = get_items_request()
-    for item in items:
-        incident = item_to_incident(item)
-        incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        # Update last run and add incident if the incident is newer than last fetch
-        if incident_date > last_fetch:
-            last_fetch = incident_date
-            incidents.append(incident)
-
-    demisto.setLastRun({'time' : last_fetch})
-    demisto.incidents(incidents)
+        demisto.results({
+            'Type': entryTypes['note'],
+            'Contents': res,
+            'ContentsFormat': formats['json'],
+            'HumanReadable': human_readable,
+            'HumanReadableFormat': formats['markdown'],
+            'EntryContext': ec
+        })
 
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
 
 LOG('Command being called is %s' % (demisto.command()))
 
-try:
-    if demisto.command() == 'test-module':
-        # This is the call made when pressing the integration test button.
-        test_module()
-        demisto.results('ok')
-    elif demisto.command() == 'fetch-incidents':
-        # Set and define the fetch incidents command to run after activated via integration settings.
-        fetch_incidents()
-    elif demisto.command() == 'example-get-items':
-        # An example command
-        get_items_command()
-
-# Log exceptions
-except Exception, e:
-    LOG(e.message)
-    LOG.print_log()
-    raise
+if demisto.command() == 'test-module':
+    # This is the call made when pressing the integration test button.
+    test_module()
+    demisto.results('ok')
+if demisto.command() == 'vulndb-get-vuln-by-id':
+    vulndb_get_vuln_by_id_command()
