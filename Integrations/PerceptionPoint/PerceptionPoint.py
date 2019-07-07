75b3fc7cb0a362c18b9621ab39da45771952c612
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-07-07 14:01:41 +0300

    Added code review fixes

--- Integrations/PerceptionPoint/PerceptionPoint.py
+++ Integrations/PerceptionPoint/PerceptionPoint.py
@@ -23,7 +23,7 @@ try:
     API_MAX_LOOPS = int(USER_PARAMS.get('api_loops', 1))
 except Exception:
     API_MAX_LOOPS = 1
-HEADER = {'Authorization': 'Token {}'.format(PP_TOKEN)}
+HEADER = {'Authorization': f'Token {PP_TOKEN}'}
 
 ''' CONSTANTS '''
 RELEASE = 'release'
@@ -51,8 +51,13 @@ FETCH_INCIDENTS_TYPE = [{'demisto_param': 'fetch_malicious',
 ''' HELPER FUNCTIONS '''
 
 
-def build_fetch_incident_types(fetch_select):
+def build_fetch_incident_types(fetch_blocked, fetch_malicious, fetch_spam):
     fetch_type_dict = defaultdict(list)  # type: ignore
+    fetch_select = {
+        'fetch_blocked': fetch_blocked,
+        'fetch_malicious': fetch_malicious,
+        'fetch_spam': fetch_spam
+    }
     for darg in FETCH_INCIDENTS_TYPE:
         darg_input = fetch_select.get(darg['demisto_param'])
         if darg_input:
@@ -66,16 +71,16 @@ def create_incident(record):
     return record
 
 
-def collect_incidents(**kwargs):
+def collect_incidents(params):
     list_url = build_request_url(LIST)
-    api_res = get_pp_api_result(url=list_url, **kwargs)
+    api_res = get_pp_api_result(list_url, params)
     num_of_results = api_res.get('count')
     incidents = []  # type: list
     api_loops = 0
     while num_of_results and api_loops < API_MAX_LOOPS:
         incidents += map(create_incident, api_res.get('results'))
         if api_res.get('next'):
-            api_res = get_pp_api_result(url=api_res.get('next'))
+            api_res = get_pp_api_result(api_res.get('next'), {})
             num_of_results = api_res.get('count')
         api_loops += 1
     return incidents
@@ -85,10 +90,10 @@ def report_incidents(incidents_list):
     demisto.incidents(incidents_list)
 
 
-def get_pp_api_result(url, **kwargs):
+def get_pp_api_result(url, params):
     try:
         res = requests.get(url=url,
-                           params=kwargs,
+                           params=params,
                            headers=HEADER,
                            verify=SECURED)
         res.raise_for_status()
@@ -112,30 +117,27 @@ def build_request_url(api_action):
 
 def command_fetch_incidents():
     try:
-        args_dict = demisto.args()
-        user_params_copy = copy.deepcopy(USER_PARAMS)
-        user_params_copy.pop('insecure')
-        user_params_copy.pop('pp_token')
-        user_params_copy.pop('api_loops')
-        args_dict.update(user_params_copy)
-        req_args = build_fetch_incident_types(args_dict)
+        fetch_blocked = USER_PARAMS.get('fetch_blocked')
+        fetch_spam = USER_PARAMS.get('fetch_spam')
+        fetch_malicious = USER_PARAMS.get('fetch_malicious')
+        req_args = build_fetch_incident_types(fetch_blocked, fetch_malicious, fetch_spam)
         last_run_id = int(demisto.getLastRun().get('scan_id', 0))
         req_args[API_CURSOR_ARG] = last_run_id
-        incidents_list = collect_incidents(**req_args)
+        incidents_list = collect_incidents(req_args)
         report_incidents(incidents_list)
         if incidents_list:
             last_run_id = max(last_run_id, int(incidents_list[-1].get('Scan Id')))
             demisto.setLastRun({'scan_id': int(last_run_id)})
-    except Exception:
-        return_error(
-            'An error occurred while trying to fetch new incidents. Please contact PerceptionPoint support')
+    except Exception as err:
+        return_error(f'An error occurred while trying to fetch new incidents. '
+                     f'Please contact PerceptionPoint support for more info. {err}')
 
 
 def release_email_and_get_message(scan_id_to_release):
     try:
         release_url = build_request_url(RELEASE).format(id_=scan_id_to_release)
-        _ = get_pp_api_result(url=release_url)
-        return 'Email with id {} was released Successfully!'.format(scan_id_to_release)
+        _ = get_pp_api_result(release_url, {})
+        return f'Email with id {scan_id_to_release} was released Successfully!'
     except Exception:
         raise
 
@@ -153,25 +155,29 @@ def command_release_email():
                       'EntryContext': {'PP.Released': scan_id_to_release}}
                      )
         demisto.results(entry)
-    except Exception:
-        return_error(
-            'An error occurred while trying to release email. Please contact PerceptionPoint support')
+    except Exception as err:
+        return_error(f'An error occurred while trying to release email. '
+                     f'Please contact PerceptionPoint support for more info\n. {err}')
+
+
+def test_command():
+    list_url = build_request_url(LIST)
+    if get_pp_api_result(list_url, {}):
+        demisto.results('ok')
 
 
 ''' COMMAND CLASSIFIER'''
 try:
+    handle_proxy()
     if demisto.command() == 'test-module':
-        list_url = build_request_url(LIST)
-        if get_pp_api_result(url=list_url):
-            demisto.results('ok')
-        sys.exit(0)
+        test_command()
     if demisto.command() == 'fetch-incidents':
         command_fetch_incidents()
     if demisto.command() == 'pp-release-email':
         command_release_email()
 except Exception as e:
     LOG(str(e))
-    message = f'Unexpected error: {e}, traceback: \n'
+    message = f'Unexpected error: {e} \n'
     LOG(message)
     LOG.print_log()
     return_error(message)
