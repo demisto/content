2ac83bcf200c5cb5c0a2539dc4cd4e94fdedca8b
shakedp <pol.shaked@gmail.com>
2019-07-07 19:26:16 +0300

    Added vulndb-get-vuln-by-vendor-and-product-name command

--- Integrations/VulnDB/VulnDB.py
+++ Integrations/VulnDB/VulnDB.py
@@ -47,6 +47,65 @@ def get_oath_toekn():
                          }).json()['access_token']
 
 
+def vulndb_vulnerability_to_entry(vuln):
+    vulnerability_details = {
+        'ID': vuln.get('vulndb_id', 0),
+        'Title': vuln.get('title', ''),
+        'Description': vuln.get('description', '').rstrip('Z'),
+        'Keywords': vuln.get('keywords', ''),
+        'PublishedDate': vuln.get('vulndb_published_date', '').rstrip('Z'),
+        'TDescription': vuln.get('t_description', ''),
+        'SolutionDate': vuln.get('solution_date', '').rstrip('Z'),
+        'DiscoveryDate': vuln.get('disclosure_date', '').rstrip('Z'),
+        'ExploitPublishDate': vuln.get('exploit_publish_date', '').rstrip('Z'),
+    }
+
+    cve_ext_reference_values = [ext_reference['value'] for ext_reference in
+                                vuln.get('ext_references', [])]
+
+    cvss_metrics_details = [{
+        'Id': cvss_metrics_data.get('id', 0),
+        'AccessVector': cvss_metrics_data.get('access_vector', ''),
+        'AccessComplexity': cvss_metrics_data.get('access_complexity', ''),
+        'Authentication': cvss_metrics_data.get('authentication', ''),
+        'ConfidentialityImpact': cvss_metrics_data.get('confidentiality_impact', ''),
+        'IntegrityImpact': cvss_metrics_data.get('integrity_impact', ''),
+        'AvailabilityImpact': cvss_metrics_data.get('availability_impact', ''),
+        'GeneratedOn': cvss_metrics_data.get('generated_on', ''),
+        'Score': cvss_metrics_data.get('score', 0),
+    } for cvss_metrics_data in vuln['cvss_metrics']]
+
+    vendor_details = [{'Id': vendor.get('vendor', {'id': 0})['id'], 'Name': vendor.get('vendor', {'name': ''})['name']}
+                      for vendor in vuln['vendors']]
+
+    product_details = []
+    for product in vuln['products']:
+        product_versions = [{'Id': version.get('id', ''), 'Name': version.get('name', '')} for version in
+                            product.get('versions', [])]
+        product_details.append({
+            'Id': product.get('id', ''),
+            'Name': product.get('name', ''),
+            'Versions': product_versions
+        })
+
+    default_classification = {'longname': '', 'description': ''}
+    classification_details = [{'Longname': classification.get('classification', default_classification)['longname'],
+                               'Description': classification.get('classification', default_classification)[
+                                   'description']}
+                              for classification in vuln['classifications']]
+
+    return {
+            'Vulnerability': vulnerability_details,
+            'CVE-ExtReference': {
+                'Value': cve_ext_reference_values
+            },
+            'CvssMetrics': cvss_metrics_details,
+            'Vendor': vendor_details,
+            'Products': product_details,
+            'Classification': classification_details
+        }
+
+
 ''' COMMANDS + REQUESTS FUNCTIONS '''
 
 
@@ -69,61 +128,9 @@ def vulndb_get_vuln_by_id_command():
         return_error(res['error'])
     else:
         vulnerability_data = res['vulnerability']
-        vulnerability_details = {
-            'Title': vulnerability_data.get('title', ''),
-            'Description': vulnerability_data.get('description', ''),
-            'Keywords': vulnerability_data.get('keywords', ''),
-            'PublishedDate': vulnerability_data.get('vulndb_published_date', ''),
-            'TDescription': vulnerability_data.get('t_description', ''),
-            'SolutionDate': vulnerability_data.get('solution_date', ''),
-            'DiscoveryDate': vulnerability_data.get('disclosure_date', ''),
-            'ExploitPublishDate': vulnerability_data.get('exploit_publish_date', ''),
-        }
-
-        cve_ext_reference_values = [ext_reference['value'] for ext_reference in
-                                    vulnerability_data.get('ext_references', [])]
-
-        cvss_metrics_details = [{
-            'Id': cvss_metrics_data.get('id', 0),
-            'AccessVector': cvss_metrics_data.get('access_vector', ''),
-            'AccessComplexity': cvss_metrics_data.get('access_complexity', ''),
-            'Authentication': cvss_metrics_data.get('authentication', ''),
-            'ConfidentialityImpact': cvss_metrics_data.get('confidentiality_impact', ''),
-            'IntegrityImpact': cvss_metrics_data.get('integrity_impact', ''),
-            'AvailabilityImpact': cvss_metrics_data.get('availability_impact', ''),
-            'GeneratedOn': cvss_metrics_data.get('generated_on', ''),
-            'Score': cvss_metrics_data.get('score', 0),
-        } for cvss_metrics_data in vulnerability_data['cvss_metrics']]
-
-        vendor_details = [{'Id': vendor.get('vendor', {'id': 0})['id'], 'Name': vendor.get('vendor', {'name': ''})['name']}
-                          for vendor in vulnerability_data['vendors']]
-
-        product_details = []
-        for product in vulnerability_data['products']:
-            product_versions = [{'Id': version.get('id', ''), 'Name': version.get('name', '')} for version in
-                                product.get('versions', [])]
-            product_details.append({
-                'Id': product.get('id', ''),
-                'Name': product.get('name', ''),
-                'Versions': product_versions
-            })
-
-        default_classification = {'longname': '', 'description': ''}
-        classification_details = [{'Longname': classification.get('classification', default_classification)['longname'],
-                                   'Description': classification.get('classification', default_classification)['description']}
-                                  for classification in vulnerability_data['classifications']]
 
         ec = {
-            'VulnDB': {
-                'Vulnerability': vulnerability_details,
-                'CVE-ExtReference': {
-                    'Value': cve_ext_reference_values
-                },
-                'CvssMetrics': cvss_metrics_details,
-                'Vendor': vendor_details,
-                'Products': product_details,
-                'Classification': classification_details
-            }
+            'VulnDB': vulndb_vulnerability_to_entry(vulnerability_data)
         }
 
         human_readable = tableToMarkdown(f'Result for vulnerability ID: {vulndb_id}', {
@@ -143,6 +150,40 @@ def vulndb_get_vuln_by_id_command():
         })
 
 
+def vulndb_get_vuln_by_vendor_and_product_name_command():
+    vendor_name = demisto.args()['vendor_name']
+    product_name = demisto.args()['product_name']
+
+    res = requests.get(f'{API_URL}/vulnerabilities/find_by_vendor_and_product_name?vendor_name={vendor_name}&product_name={product_name}',
+                       verify=USE_SSL,
+                       headers={'Authorization': f'Bearer {get_oath_toekn()}'}
+                       ).json()
+
+    if 'error' in res:
+        return_error(res['error'])
+    else:
+        results = res['results']
+        for result in results:
+            ec = {
+                'VulnDB': vulndb_vulnerability_to_entry(result)
+            }
+
+            human_readable = tableToMarkdown(f'Result for vulnerability ID: {ec["VulnDB"]["Vulnerability"]["ID"]}', {
+                'Title': ec['VulnDB']['Vulnerability']['Title'],
+                'Description': ec['VulnDB']['Vulnerability']['Description'],
+                'Publish Date': ec['VulnDB']['Vulnerability']['PublishedDate'],
+                'Solution Date': ec['VulnDB']['Vulnerability']['SolutionDate']
+            })
+
+            demisto.results({
+                'Type': entryTypes['note'],
+                'Contents': res,
+                'ContentsFormat': formats['json'],
+                'HumanReadable': human_readable,
+                'HumanReadableFormat': formats['markdown'],
+                'EntryContext': ec
+            })
+
 ''' COMMANDS MANAGER / SWITCH PANEL '''
 
 LOG('Command being called is %s' % (demisto.command()))
@@ -153,3 +194,5 @@ if demisto.command() == 'test-module':
     demisto.results('ok')
 if demisto.command() == 'vulndb-get-vuln-by-id':
     vulndb_get_vuln_by_id_command()
+elif demisto.command() == 'vulndb-get-vuln-by-vendor-and-product-name':
+    vulndb_get_vuln_by_vendor_and_product_name_command()
