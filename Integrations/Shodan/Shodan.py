785ed12845974fd394914750b124635126e93629
shakedp <pol.shaked@gmail.com>
2019-07-11 14:16:46 +0300

    Initial commit - yaml from content

---
+++ Integrations/Shodan/Shodan.py
@@ -0,0 +1,161 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+''' IMPORTS '''
+
+import json
+import requests
+from distutils.util import strtobool
+
+# Disable insecure warnings
+requests.packages.urllib3.disable_warnings()
+
+''' GLOBALS/PARAMS '''
+
+API_KEY = demisto.params()['api_key']
+
+# Remove trailing slash to prevent wrong URL path to service
+API_URL = demisto.params()['api_url'].rstrip('/')
+
+# Should we use SSL
+USE_SSL = not demisto.params().get('insecure', False)
+
+# Remove proxy if not set to true in params
+if not demisto.params().get('proxy'):
+    os.environ.pop('HTTP_PROXY', None)
+    os.environ.pop('HTTPS_PROXY', None)
+    os.environ.pop('http_proxy', None)
+    os.environ.pop('https_proxy', None)
+
+
+''' HELPER FUNCTIONS '''
+
+
+def http_request(method, url_suffix, params=None, data=None):
+    # A wrapper for requests lib to send our requests and handle requests and responses better
+    res = requests.request(
+        method,
+        BASE_URL + url_suffix,
+        verify=USE_SSL,
+        params=params,
+        data=data,
+        headers=HEADERS
+    )
+    # Handle error responses gracefully
+    if res.status_code not in {200}:
+        return_error('Error in API call to Example Integration [%d] - %s' % (res.status_code, res.reason))
+
+    return res.json()
+
+
+''' COMMANDS + REQUESTS FUNCTIONS '''
+
+
+def test_module():
+    """
+    Performs basic get request to get item samples
+    """
+    samples = http_request('GET', 'items/samples')
+
+
+def get_items_command():
+    """
+    Gets details about a items using IDs or some other filters
+    """
+    # Init main vars
+    headers = []
+    contents = []
+    context = {}
+    context_entries = []
+    title = ''
+    # Get arguments from user
+    item_ids = argToList(demisto.args().get('item_ids', []))
+    is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
+    limit = int(demisto.args().get('limit', 10))
+    # Make request and get raw response
+    items = get_items_request(item_ids, is_active)
+    # Parse response into context & content entries
+    if items:
+        if limit:
+            items = items[:limit]
+        title = 'Example - Getting Items Details'
+
+        for item in items:
+            contents.append({
+                'ID': item.get('id'),
+                'Description': item.get('description'),
+                'Name': item.get('name'),
+                'Created Date': item.get('createdDate')
+            })
+            context_entries.append({
+                'ID': item.get('id'),
+                'Description': item.get('description'),
+                'Name': item.get('name'),
+                'CreatedDate': item.get('createdDate')
+            })
+
+        context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
+
+    demisto.results({
+        'Type': entryTypes['note'],
+        'ContentsFormat': formats['json'],
+        'Contents': contents,
+        'ReadableContentsFormat': formats['markdown'],
+        'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
+        'EntryContext': context
+    })
+
+
+def get_items_request(item_ids, is_active):
+    # The service endpoint to request from
+    endpoint_url = 'items'
+    # Dictionary of params for the request
+    params = {
+        'ids': item_ids,
+        'isActive': is_active
+    }
+    # Send a request using our http_request wrapper
+    response = http_request('GET', endpoint_url, params)
+    # Check if response contains errors
+    if response.get('errors'):
+        return_error(response.get('errors'))
+    # Check if response contains any data to parse
+    if 'data' in response:
+        return response.get('data')
+    # If neither was found, return back empty results
+    return {}
+
+
+def fetch_incidents():
+    last_run = demisto.getLastRun()
+    # Get the last fetch time, if exists
+    last_fetch = last_run.get('time')
+
+    # Handle first time fetch, fetch incidents retroactively
+    if last_fetch is None:
+        last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
+
+    incidents = []
+    items = get_items_request()
+    for item in items:
+        incident = item_to_incident(item)
+        incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        # Update last run and add incident if the incident is newer than last fetch
+        if incident_date > last_fetch:
+            last_fetch = incident_date
+            incidents.append(incident)
+
+    demisto.setLastRun({'time' : last_fetch})
+    demisto.incidents(incidents)
+
+
+''' COMMANDS MANAGER / SWITCH PANEL '''
+
+if demisto.command() == 'test-module':
+    # This is the call made when pressing the integration test button.
+    test_module()
+    demisto.results('ok')
+elif demisto.command() == 'search':
+    search_command()
+elif demisto.command() == 'ip':
+    get_items_command()
---
+++ Integrations/Shodan/Shodan_description.md
@@ -0,0 +1,1 @@
+search engine for Internet-connected devices 
