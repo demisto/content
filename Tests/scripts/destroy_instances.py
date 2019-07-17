c4094be75613938611c150157be68e4382f7a73f
Nir Ben David <nir@demisto.com>
2019-07-17 11:58:08 +0300

    refactor destroy instances.py

--- Tests/scripts/destroy_instances.py
+++ Tests/scripts/destroy_instances.py
@@ -1,28 +1,43 @@
 import sys
 import os
+import subprocess
 from Tests.awsinstancetool import aws_fuctions
-from threading import Thread
+# from threading import Thread
 
 from Tests.test_utils import run_command, run_threads_list
 
 
 def main():
     circle_aritfact = sys.argv[1]
-    with open('./Tests/instance_ips.txt', 'r') as instance_file:
-        instance_ips = instance_file.readlines()
-        instance_ips = [line.strip('\n').split(":") for line in instance_ips if line.strip() != '']
 
-    with open('./Tests/instance_ids.txt', 'r') as instance_file:
-        instance_ids = instance_file.readlines()
-        instance_ids = [line.strip('\n').split(":") for line in instance_ids if line.strip() != '']
-    threads_list = []
-    for ami_instance_name, ami_instance_ip in instance_ips:
-        for ami_instance_name_second, ami_instance_id in instance_ids:
-            if ami_instance_name == ami_instance_name_second:
-                if os.path.isfile("./Tests/is_build_failed_{}.txt".format(ami_instance_name.replace(' ', ''))):
-                    rminstance = aws_fuctions.destroy_instance("us-west-2",ami_instance_id)
-                    if aws_fuctions.isError(rminstance):
-                        print (rminstance)
+    with open('./env_results.json', 'r') as json_file:
+        env_results = json.load(json_file)
+
+    for env in env_results:
+        if os.path.isfile("./Tests/is_build_failed_{}.txt".format(env["Role"].replace(' ', ''))):
+            subprocess.check_output("ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {}@${} \"sudo chmod -R 755 /var/log/demisto\"".format(env["SSHuser"],env["InstanceDNS"]), shell=True)
+            subprocess.check_output("scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {}@{}:/var/log/demisto/server.log {} || echo \"WARN: Failed downloading server.log\"".format(env["SSHuser"],env["InstanceDNS"],circle_aritfact +
+            "/server_{}.log".format(env["Role"].replace(' ', ''))) ,shell=True)
+            rminstance = aws_fuctions.destroy_instance(env["Region"],env["InstanceID"])
+            if aws_fuctions.isError(rminstance):
+                print (ValueError(rminstance))
+
+
+    # with open('./Tests/instance_ips.txt', 'r') as instance_file:
+    #     instance_ips = instance_file.readlines()
+    #     instance_ips = [line.strip('\n').split(":") for line in instance_ips if line.strip() != '']
+    #
+    # with open('./Tests/instance_ids.txt', 'r') as instance_file:
+    #     instance_ids = instance_file.readlines()
+    #     instance_ids = [line.strip('\n').split(":") for line in instance_ids if line.strip() != '']
+    # threads_list = []
+    # for ami_instance_name, ami_instance_ip in instance_ips:
+    #     for ami_instance_name_second, ami_instance_id in instance_ids:
+    #         if ami_instance_name == ami_instance_name_second:
+    #             if os.path.isfile("./Tests/is_build_failed_{}.txt".format(ami_instance_name.replace(' ', ''))):
+    #                 rminstance = aws_fuctions.destroy_instance("us-west-2",ami_instance_id)
+    #                 if aws_fuctions.isError(rminstance):
+    #                     print (rminstance)
                 # t = Thread(target=run_command,
                 #            args=("./Tests/scripts/destroy_instances.sh {} {} {} ./Tests/is_build_failed_{}.txt".format(
                 #                circle_aritfact,
