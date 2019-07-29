7fe5a98adb5faa3c1662ec0131e78d39a7af829c
bark@demisto.com <bkatzir@paloaltonetworks.com>
2019-07-29 10:06:15 +0300

    no entries -> indicative msg

--- Integrations/AutofocusV2/AutofocusV2.py
+++ Integrations/AutofocusV2/AutofocusV2.py
@@ -575,7 +575,10 @@ def samples_search_results_command():
     args = demisto.args()
     af_cookie = args.get('af_cookie')
     results, status = get_search_results('samples', af_cookie)
-    md = tableToMarkdown(f'Search Samples Results is {status}', results)
+    if len(results) < 1:
+        md = results = 'No entries found that match the query'
+    else:
+        md = tableToMarkdown(f'Search Samples Results is {status}', results)
     demisto.results({
         'Type': entryTypes['note'],
         'ContentsFormat': formats['text'],
@@ -591,7 +594,10 @@ def sessions_search_results_command():
     args = demisto.args()
     af_cookie = args.get('af_cookie')
     results, status = get_search_results('sessions', af_cookie)
-    md = tableToMarkdown(f'Search Sessions Results is {status}:', results)
+    if len(results) < 1:
+        md = results = 'No entries found that match the query'
+    else:
+        md = tableToMarkdown(f'Search Samples Results is {status}', results)
     demisto.results({
         'Type': entryTypes['note'],
         'ContentsFormat': formats['text'],
