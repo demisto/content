b3349103df3703a9d35160498e3f531628c54e56
yaakovi <syaakovi@paloaltonetworks.com>
2019-07-31 02:42:30 +0300

    replaced deprecated argument

--- Tests/scripts/copy_content_data.sh
+++ Tests/scripts/copy_content_data.sh
@@ -20,11 +20,11 @@ scp -r ./Beta_Integrations/* ${USER}@${PUBLIC_IP}:~/Beta_Integrations
 
 # override exiting content with current
 # rm CommonServer*_4_1 as this was changed and is stuck on 4.1 server
-COPY_CONTENT_COMMAND="sudo rm -f /usr/local/demisto/res/script-CommonServer_4_1.yml /usr/local/demisto/res/script-CommonServerPython_4_1.yml \
+COPY_CONTENT_COMMAND="date && sudo rm -f /usr/local/demisto/res/script-CommonServer_4_1.yml /usr/local/demisto/res/script-CommonServerPython_4_1.yml \
     /usr/local/demisto/res/integration-Windows_Defender_Advanced_Threat_Protection.yml /usr/local/demisto/res/integration-Microsoft_Graph.yml \
     /usr/local/demisto/res/integration-Awake_Security.yml /usr/local/demisto/res/integration-WhatsMyBrowser.yml \
     && sudo unzip -q -o ~/content/content_new.zip -d /usr/local/demisto/res \
-    && sudo unzip -q -o ~/content/content_test.zip -d /usr/local/demisto/res && sudo cp `find Beta_Integrations -d 1 -type file` /usr/local/demisto/res"
+    && sudo unzip -q -o ~/content/content_test.zip -d /usr/local/demisto/res && sudo cp `find Beta_Integrations -depth 1 -type file` /usr/local/demisto/res && date"
 ssh -t ${USER}@${PUBLIC_IP} ${COPY_CONTENT_COMMAND}
 
 echo "[`date`] ${PUBLIC_IP}: start server"
