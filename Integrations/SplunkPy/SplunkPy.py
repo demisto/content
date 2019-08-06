f958028204d65df1865da9520322f052a0fd6804
olichter <olichter@paloaltonetworks.com>
2019-08-06 08:42:54 +0300

    guardrails ignore

--- Integrations/SplunkPy/SplunkPy.py
+++ Integrations/SplunkPy/SplunkPy.py
@@ -18,7 +18,7 @@ sys.setdefaultencoding('utf8')  # pylint: disable=maybe-no-member
 
 SPLUNK_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
 VERIFY_CERTIFICATE = not bool(demisto.params().get('unsecure'))
-FETCH_LIMIT = int(demisto.params().get('fetch_limit',200))
+FETCH_LIMIT = int(demisto.params().get('fetch_limit', 200))
 
 
 def get_current_splunk_time(splunk_service):
