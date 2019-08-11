6d9c7b0357133ffa6598501bf05f380dae18cecb
guyfreund <gfreund@paloaltonetworks.com>
2019-08-11 17:11:09 +0300

    add tests for functions get_alert_contents & get_alert_contents_war_room, deleted unecessary functions in ZeroFox.py

--- Integrations/ZeroFox/ZeroFox.py
+++ Integrations/ZeroFox/ZeroFox.py
@@ -12,29 +12,31 @@ requests.packages.urllib3.disable_warnings()
 
 ''' GLOBALS/PARAMS '''
 
-USERNAME = demisto.params().get('credentials').get('identifier')
-PASSWORD = demisto.params().get('credentials').get('password')
+USERNAME = None
+PASSWORD = None
 TOKEN = None
 # # Remove trailing slash to prevent wrong URL path to service
 # SERVER = demisto.params()['url'][:-1] \
 #     if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
 # Should we use SSL
-USE_SSL = not demisto.params().get('insecure', False)
+USE_SSL = None
 # How many time before the first fetch to retrieve incidents
-FETCH_TIME = demisto.params().get('fetch_time', '3 days')
 # Service base URL
-BASE_URL = 'https://api.zerofox.com/1.0' # disable-secrets-detection
+BASE_URL = None
 # Headers to be sent in requests
 HEADERS = None
-# Remove proxy if not set to true in params
-if not demisto.params().get('proxy'):
-    del os.environ['HTTP_PROXY']
-    del os.environ['HTTPS_PROXY']
-    del os.environ['http_proxy']
-    del os.environ['https_proxy']
 
 
 ''' HELPER FUNCTIONS '''
+
+def initialize_preset():
+    global USERNAME, PASSWORD, USE_SSL, BASE_URL
+    USERNAME = demisto.params().get('credentials').get('identifier')
+    PASSWORD = demisto.params().get('credentials').get('password')
+    USE_SSL = not demisto.params().get('insecure', False)
+    BASE_URL = 'https://api.zerofox.com/1.0'  # disable-secrets-detection
+    # Remove proxy if not set to true in params
+    handle_proxy()
 
 
 def get_alert_contents(alert):
@@ -78,19 +80,18 @@ def get_alert_contents(alert):
         'Notes': alert.get('notes'),
         'RuleID': alert.get('rule_id'),
         'EntityAccount': alert.get('entity_account'),
-        'Tags': alert.get('tags'),
-        'Notes': alert.get('notes')
+        'Tags': alert.get('tags')
     }
 
 
 def get_alert_contents_war_room(contents):
     return {
         'ID': contents.get('ID'),
-        'Protected Entity': contents.get('EntityName').title(),
-        'Content Type': contents.get('AlertType').title(),
-        'Alert Date': contents.get('Timestamp'),
-        'Status': contents.get('Status').title(),
-        'Source': contents.get('Network').title(),
+        'Protected Entity': contents.get('EntityName', '').title(),
+        'Content Type': contents.get('AlertType', '').title(),
+        'Alert Date': contents.get('Timestamp', ''),
+        'Status': contents.get('Status', '').title(),
+        'Source': contents.get('Network', '').title(),
         'Rule': contents.get('RuleName'),
         'Severity': contents.get('Severity'),
         'Notes': contents.get('Notes') if contents.get('Notes') != '' else None,
@@ -152,17 +153,6 @@ def http_request(method: str, url_suffix: str, params=None, data=None):
             return res_json
         except ValueError:
             return 'Success Message'
-
-
-def item_to_incident(item):
-    incident = {}
-    # Incident Title
-    incident['name'] = 'Example Incident: ' + item.get('name')
-    # Incident occurrence time, usually item creation date in service
-    incident['occurred'] = item.get('createdDate')
-    # The raw response from the service, providing full info regarding the item
-    incident['rawJSON'] = json.dumps(item)
-    return incident
 
 
 ''' COMMANDS + REQUESTS FUNCTIONS '''
@@ -231,8 +221,7 @@ def modify_alert_tags_command():
     addition_string = demisto.args().get('addition')
     addition = True if addition_string == 'true' else False
     tags_list_string = demisto.args().get('tags')
-    response_content = modify_alert_tags(alert_id, addition, tags_list_string)
-    alert_tags_change_uuid: str = response_content.get('uuid')
+    modify_alert_tags(alert_id, addition, tags_list_string)
     return_outputs('Changes were successfully made.', outputs={})
 
 
@@ -245,7 +234,9 @@ def get_alert(alert_id):
 def get_alert_command():
     alert_id = demisto.args().get('alert_id')
     response_content = get_alert(alert_id)
-    response_json_fields = response_content.get('alert', {})
+    response_json_fields = response_content.get('alert')
+    if not isinstance(response_json_fields, dict) or len(response_json_fields) <= 0:
+        demisto.results("NO RESULTS FOUND")
     contents = get_alert_contents(response_json_fields)
     contents_war_room = get_alert_contents_war_room(contents)
     context = {'ZeroFox.Alert(val.ID && val.ID === obj.ID)': contents}
@@ -311,96 +302,6 @@ def test_module():
     samples = http_request('GET', 'items/samples')
 
 
-def get_items_command():
-    """
-    Gets details about a items using IDs or some other filters
-    """
-    # Init main vars
-    headers = []
-    contents = []
-    context = {}
-    context_entries = []
-    title = ''
-    # Get arguments from user
-    item_ids = argToList(demisto.args().get('item_ids', []))
-    is_active = bool(strtobool(demisto.args().get('is_active', 'false')))
-    limit = int(demisto.args().get('limit', 10))
-    # Make request and get raw response
-    items = get_items_request(item_ids, is_active)
-    # Parse response into context & content entries
-    if items:
-        if limit:
-            items = items[:limit]
-        title = 'Example - Getting Items Details'
-
-        for item in items:
-            contents.append({
-                'ID': item.get('id'),
-                'Description': item.get('description'),
-                'Name': item.get('name'),
-                'Created Date': item.get('createdDate')
-            })
-            context_entries.append({
-                'ID': item.get('id'),
-                'Description': item.get('description'),
-                'Name': item.get('name'),
-                'CreatedDate': item.get('createdDate')
-            })
-
-        context['Example.Item(val.ID && val.ID === obj.ID)'] = context_entries
-
-    demisto.results({
-        'Type': entryTypes['note'],
-        'ContentsFormat': formats['json'],
-        'Contents': contents,
-        'ReadableContentsFormat': formats['markdown'],
-        'HumanReadable': tableToMarkdown(title, contents, removeNull=True),
-        'EntryContext': context
-    })
-
-
-def get_items_request(item_ids, is_active):
-    # The service endpoint to request from
-    endpoint_url = 'items'
-    # Dictionary of params for the request
-    params = {
-        'ids': item_ids,
-        'isActive': is_active
-    }
-    # Send a request using our http_request wrapper
-    response = http_request('GET', endpoint_url, params)
-    # Check if response contains errors
-    if response.get('errors'):
-        return_error(response.get('errors'))
-    # Check if response contains any data to parse
-    if 'data' in response:
-        return response.get('data')
-    # If neither was found, return back empty results
-    return {}
-
-
-def fetch_incidents():
-    last_run = demisto.getLastRun()
-    # Get the last fetch time, if exists
-    last_fetch = last_run.get('time')
-
-    # Handle first time fetch, fetch incidents retroactively
-    if last_fetch is None:
-        last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
-
-    incidents = []
-    items = get_items_request()
-    for item in items:
-        incident = item_to_incident(item)
-        incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        # Update last run and add incident if the incident is newer than last fetch
-        if incident_date > last_fetch:
-            last_fetch = incident_date
-            incidents.append(incident)
-
-    demisto.setLastRun({'time' : last_fetch})
-    demisto.incidents(incidents)
-
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
 
@@ -408,6 +309,7 @@ def fetch_incidents():
 
 
 def main():
+    initialize_preset()
     LOG('Command being called is %s' % (demisto.command()))
     try:
         get_authorization_token()
@@ -415,12 +317,6 @@ def main():
             # This is the call made when pressing the integration test button.
             test_module()
             demisto.results('ok')
-        elif demisto.command() == 'fetch-incidents':
-            # Set and define the fetch incidents command to run after activated via integration settings.
-            fetch_incidents()
-        elif demisto.command() == 'example-get-items':
-            # An example command
-            get_items_command()
         elif demisto.command() == 'zerofox-get-alert':
             get_alert_command()
         elif demisto.command() == 'zerofox-alert-user-assignment':
