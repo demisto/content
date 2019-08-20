e7222f0616f851ffda1378d6bbeb2dafea768b56
ShahafBenYakir <shahaf.benyakir@demisto.com>
2019-08-20 15:15:37 +0300

    Replaced is_nightly with options.nightly

--- Tests/test_content.py
+++ Tests/test_content.py
@@ -133,9 +133,10 @@ def send_slack_message(slack, chanel, text, user_name, as_user):
 def run_test_logic(c, failed_playbooks, integrations, playbook_id, succeed_playbooks, test_message, test_options, slack,
                    circle_ci, build_number, server_url, build_name, is_nightly, is_mock_run=False):
     status, inc_id = test_integration(c, integrations, playbook_id, test_options, is_mock_run)
+    options = options_handler()
     stdout, stderr = get_memory_data()
     text = stdout if not stderr else stderr
-    if not is_nightly:
+    if options.nightly:
         send_slack_message(slack, SLACK_CHANNEL_ID, text, 'Content CircleCI', 'False')
 
     if status == PB_Status.COMPLETED:
@@ -565,7 +566,7 @@ def execute_testing(server, server_ip, server_version, server_numeric_version, i
                                                     nightly_integrations)
     else:  # In case of a non AMI run we don't want to use the mocking mechanism
         mockless_tests = tests
-    if not is_nightly:
+    if options.nightly:
         send_slack_message(slack, SLACK_CHANNEL_ID,
                            'Build Number: {0}\n Server Address: {1}'.format(build_number, server),
                            'Content CircleCI', 'False')
