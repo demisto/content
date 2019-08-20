be9ccf533b70f60a1401c9c789e896a0519f8617
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-08-20 22:25:47 +0300

    unit test for frequency

---
+++ Integrations/OnboardingIntegration/OnboardingIntegration_test.py
@@ -0,0 +1,12 @@
+import demistomock as demisto
+import OnboardingIntegration
+
+
+def test_frequency(mocker):
+    mocker.patch.object(demisto, 'params',
+                        return_value={'frequency': '1'})
+    mocker.patch.object(demisto, 'command',
+                        return_value='fetch-incidents')
+    mocker.patch.object(demisto, 'incidents')
+    OnboardingIntegration.main()
+    assert demisto.incidents.call_count == 1
