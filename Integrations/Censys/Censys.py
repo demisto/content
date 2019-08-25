8c2569213e251d0fa9f0613258f43d9abe2e43b8
dtavori <dtavori@paloaltonetworks.com>
2019-08-25 13:40:14 +0300

    Censys - fix bug

--- Integrations/Censys/Censys.py
+++ Integrations/Censys/Censys.py
@@ -15,7 +15,7 @@ def test_module():
     url_suffix = "view/ipv4/8.8.8.8"
     res = send_request('GET', url_suffix)
     if res is not None:
-        demisto.results('OK')
+        demisto.results('ok')
     else:
         return_error('Error 404: Test failed.')
 
@@ -23,21 +23,23 @@ def test_module():
 def send_request(method, url_suffix, data=None):
     res = requests.request(method, API_URL + url_suffix,
                            auth=(API_ID, API_SECRET),
-                           data=json.dumps(data),
-                           verify=USE_SSL)
+                           data=data, verify=USE_SSL)
     data = json.loads(res.text)
 
     if res.status_code == 404:
         return None
     elif res.status_code >= 400:
-        return_error(
-            "Received an error - status code {0}, error message: {1}".format(res.status_code, data["error"].title()))
+        return_error("Received an error - status code [{0}], "
+                     "error message: {1}".format(res.status_code, data["error"].title()))
 
     return data
 
 
-def censys_view_command(query, index):
-    # query = args.get()
+def censys_view_command():
+    args = demisto.args()
+    query = args.get('query')
+    index = args.get('index')
+
     url_suffix = 'view/{0}/{1}'.format(index, query)
     raw = send_request('GET', url_suffix)
     if raw:
@@ -46,13 +48,17 @@ def censys_view_command(query, index):
         demisto.results("No view results for {0}.".format(query))
 
 
-def censys_search_command(query, index):
+def censys_search_command():
+    args = demisto.args()
+    query = args.get('query')
+    index = args.get('index')
+
     url_suffix = 'search/{0}'.format(index)
     data = {
         "query": query,
         "page": 1
     }
-    raw = send_request('POST', url_suffix, data)
+    raw = send_request('POST', url_suffix, json.dumps(data))
     readable = tableToMarkdown("Search results for {0} in {1}".format(query, index), raw["results"])
     return_outputs(readable, raw)
 
@@ -62,7 +68,6 @@ command = demisto.command()
 LOG('command is {0}'.format(command))
 try:
     handle_proxy()
-    args = demisto.args()
     if command == 'test-module':
         test_module()
     elif command == 'cen-view':
