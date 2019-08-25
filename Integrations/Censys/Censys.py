e78c1b07726d9915c7b1f8a29d6b42ff4677464e
dtavori <dtavori@paloaltonetworks.com>
2019-08-25 13:15:28 +0300

    Censys - fix bug

--- Integrations/Censys/Censys.py
+++ Integrations/Censys/Censys.py
@@ -15,25 +15,29 @@ def test_module():
     url_suffix = "view/ipv4/8.8.8.8"
     res = send_request('GET', url_suffix)
     if res is not None:
-        demisto.results('ok')
+        demisto.results('OK')
     else:
-        return_error('Error 404: test failed.')
+        return_error('Error 404: Test failed.')
 
 
 def send_request(method, url_suffix, data=None):
-    res = requests.request(method, API_URL + url_suffix, auth=(API_ID, API_SECRET),
-                           data=json.dumps(data), verify=USE_SSL)
+    res = requests.request(method, API_URL + url_suffix,
+                           auth=(API_ID, API_SECRET),
+                           data=json.dumps(data),
+                           verify=USE_SSL)
     data = json.loads(res.text)
 
     if res.status_code == 404:
         return None
     elif res.status_code >= 400:
-        return_error("Error {0}: {1}".format(res.status_code, data["error"].title()))
+        return_error(
+            "Received an error - status code {0}, error message: {1}".format(res.status_code, data["error"].title()))
 
     return data
 
 
 def censys_view_command(query, index):
+    # query = args.get()
     url_suffix = 'view/{0}/{1}'.format(index, query)
     raw = send_request('GET', url_suffix)
     if raw:
@@ -57,13 +61,14 @@ def censys_search_command(query, index):
 command = demisto.command()
 LOG('command is {0}'.format(command))
 try:
+    handle_proxy()
     args = demisto.args()
     if command == 'test-module':
         test_module()
     elif command == 'cen-view':
-        censys_view_command(**args)
+        censys_view_command()
     elif command == 'cen-search':
-        censys_search_command(**args)
+        censys_search_command()
 
 except Exception as ex:
     LOG(ex)
