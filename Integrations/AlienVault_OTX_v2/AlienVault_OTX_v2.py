d32700329c881dfde1db7ed3c74abedde1b9d3e5
Gal Rabin <grabin@paloaltonetworks.com>
2019-08-25 09:58:04 +0300

    * Change remove none recursive, more efficient * Change test playbooks command to specific commands when needed (url,file etc)

--- Integrations/AlienVault_OTX_v2/AlienVault_OTX_v2.py
+++ Integrations/AlienVault_OTX_v2/AlienVault_OTX_v2.py
@@ -165,10 +165,9 @@ def remove_none(obj):
     :return: Obj with None value removed
     """
     if isinstance(obj, (list, tuple, set)):
-        return type(obj)(remove_none(x) for x in obj if x is not (None or ''))
+        return type(obj)(remove_none(x) for x in obj if x is not None)
     elif isinstance(obj, dict):
-        return type(obj)((remove_none(k), remove_none(v))
-                         for k, v in obj.items() if k is not None and v is not (None or ''))
+        return type(obj)((remove_none(k), remove_none(v)) for k, v in obj.items() if v is not None)
     else:
         return obj
 
