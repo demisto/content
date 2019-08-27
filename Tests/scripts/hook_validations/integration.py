5071fc0ad9dff124d4fe29f061879c23ee7ff48d
liorblob <lblobstein@paloaltonetworks.com>
2019-08-27 13:18:35 +0300

    adjust docker image validation for 5.0 versions

--- Tests/scripts/hook_validations/integration.py
+++ Tests/scripts/hook_validations/integration.py
@@ -4,7 +4,7 @@ import requests
 import yaml
 
 from Tests.scripts.constants import CONTENT_GITHUB_MASTER_LINK
-from Tests.test_utils import print_error, get_yaml
+from Tests.test_utils import print_error, get_yaml, server_version_compare
 
 # disable insecure warnings
 requests.packages.urllib3.disable_warnings()
@@ -299,11 +299,13 @@ class IntegrationValidator(object):
 
     def is_docker_image_changed(self):
         """Check if the Docker image was changed or not."""
-        if self.old_integration.get('script', {}).get('dockerimage', "") != \
-                self.current_integration.get('script', {}).get('dockerimage', ""):
-            print_error("Possible backwards compatibility break, You've changed the docker for the file {}"
-                        " this is not allowed.".format(self.file_path))
-            self._is_valid = False
-            return True
+        # Unnecessary to check docker image only on 5.0 and up
+        if server_version_compare(self.old_integration.get('fromversion', '0'), '5.0.0') < 0:
+            if self.old_integration.get('script', {}).get('dockerimage', "") != \
+                    self.current_integration.get('script', {}).get('dockerimage', ""):
+                print_error("Possible backwards compatibility break, You've changed the docker for the file {}"
+                            " this is not allowed.".format(self.file_path))
+                self._is_valid = False
+                return True
 
         return False
