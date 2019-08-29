3c6d8a1f61beabfd259c7162e37a092c2ad4223e
Andrew Shamah <andrew@demisto.com>
2019-08-29 16:25:11 +0300

    mypy

--- Beta_Integrations/AWS-Athena/AWS-Athena.py
+++ Beta_Integrations/AWS-Athena/AWS-Athena.py
@@ -240,8 +240,8 @@ try:
 except ResponseParserError as e:
     return_error('Could not connect to the AWS endpoint. Please check that the region is valid.\n {error}'.format(
         error=type(e)))
-    LOG(e.message)
+    LOG(e)
 
 except Exception as e:
     return_error('Error has occurred in the AWS Athena Integration: {error}\n {message}'.format(
-        error=type(e), message=e.message))
+        error=type(e), message=e))
