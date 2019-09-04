06b4b5970d1a65e5c9c99bc60711104db3904bdc
yaakovi <syaakovi@paloaltonetworks.com>
2019-09-04 17:55:57 +0300

    added unittests

--- Tests/scripts/hook_validations/incident_field.py
+++ Tests/scripts/hook_validations/incident_field.py
@@ -4,7 +4,7 @@ This module is designed to validate the correctness of incident field entities i
 from Tests.test_utils import print_error, get_json, get_remote_file
 
 
-class IncidentFieldValidator:
+class IncidentFieldValidator(object):
     """IncidentFieldValidator is designed to validate the correctness of the file structure we enter to content repo.
     And also try to catch possible Backward compatibility breaks due to the performed changes.
 
@@ -34,23 +34,25 @@ class IncidentFieldValidator:
             return True
 
         is_bc_broke = any([
-            False
+            # in the future, add BC checks here
         ])
 
         return not is_bc_broke
 
     def is_valid(self):
         """Check whether the IncidentField is valid or not"""
-        is_incident_field_valid = any([
+        is_incident_field_valid = all([
             self.is_valid_name(),
+            self.is_valid_content_flag(),
+            self.is_valid_system_flag(),
         ])
 
         return is_incident_field_valid
 
     def is_valid_name(self):
         """Validate that the name and cliName does not contain: incident, case or alert."""
-        name = self.current_incident_field.get('name')
-        cli_name = self.current_incident_field.get('cliName')
+        name = self.current_incident_field.get('name', '')
+        cli_name = self.current_incident_field.get('cliName', '')
         bad_words = {'incident', 'case', 'alert', 'event', 'play', 'ticket', 'issue'}
         for word in bad_words:
             if word in name.lower() or word in cli_name.lower():
@@ -59,3 +61,11 @@ class IncidentFieldValidator:
                 return False
 
         return True
+
+    def is_valid_content_flag(self):
+        """Validate that field is marked as content."""
+        return self.current_incident_field.get('content') is True
+
+    def is_valid_system_flag(self):
+        """Validate that field is not marked as system."""
+        return self.current_incident_field.get('system', False) is False
