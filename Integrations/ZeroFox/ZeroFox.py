a0e0f2b4134f5162f6a29881d35866329fde08c1
guyfreund <gfreund@paloaltonetworks.com>
2019-09-04 21:43:41 +0300

    different if condition

--- Integrations/ZeroFox/ZeroFox.py
+++ Integrations/ZeroFox/ZeroFox.py
@@ -317,12 +317,11 @@ def http_request(method: str, url_suffix: str, params: Dict = None, data: Union[
         # Get originating Exception in Exception chain
         while '__context__' in dir(e) and e.__context__:
             e = cast(Any, e.__context__)
-        if e.strerror:
+        err_msg = "blah blah"
+        if not isinstance(e, TypeError):
             err_msg = f'\nMESSAGE: {e.strerror}\n' \
                       f'ADVICE: Check that the Server URL parameter is correct and that you' \
                       f' have access to the Server from your host.'
-        else:
-            err_msg = 'blah blah'
         raise Exception(err_msg)
 
 
