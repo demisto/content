1c9b69704e055719c13e58ef422c6566bde472c2
guyfreund <gfreund@paloaltonetworks.com>
2019-09-10 18:52:53 +0300

    change for debug

--- Tests/scripts/pkg_dev_test_tasks.py
+++ Tests/scripts/pkg_dev_test_tasks.py
@@ -287,7 +287,7 @@ Will lookup up what docker image to use and will setup the dev dependencies and 
     project_dir = os.path.abspath(args.dir)
     print('Project Directory: {}'.format(project_dir))
     # load yaml
-    project_dir_list = glob.glob(project_dir)
+    project_dir_list = glob.glob(project_dir + '/')
     print('project dir list: {}'.format(project_dir_list))
     yml_list = glob.glob(project_dir + '/*.yml')
     print('yml path: {}'.format(yml_list))
