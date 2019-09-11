91c16535a2403c8b0ee637eec910d29ae5446b69
Shelly Berman <sberman@paloaltonetworks.com>
2019-09-11 11:46:14 +0300

    New script

--- Scripts/EncryptEmail/EncryptEmail.py
+++ Scripts/EncryptEmail/EncryptEmail.py
@@ -2,26 +2,53 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
-import smime
+from M2Crypto import BIO, Rand, SMIME, X509
+
+
+def makebuf(text):
+    return BIO.MemoryBuffer(text)
 
 
 def main():
-    message_body = demisto.args().get('message')
-    encrypt_key = demisto.getFilePath(demisto.args().get('key'))
 
-    with open(encrypt_key['path'], 'rb') as pem:
-        msg = smime.encrypt(message_body, pem.read())
-    new_message = msg.split('\n\n')
-    headers = new_message[0]
-    content = new_message[1]
+    encrypt_key = demisto.getFilePath(demisto.args().get('key'))
+    message_body = demisto.args().get('message').encode('utf-8')
+
+    # Make a MemoryBuffer of the message.
+    buf = makebuf(message_body)
+
+    # Instantiate an SMIME object.
+    s = SMIME.SMIME()
+
+    # Load target cert to encrypt to.
+    x509 = X509.load_cert(os.path.abspath(encrypt_key['path']))
+    sk = X509.X509_Stack()
+    sk.push(x509)
+    s.set_x509_stack(sk)
+
+    # Set cipher: 3-key triple-DES in CBC mode.
+    s.set_cipher(SMIME.Cipher('des_ede3_cbc'))
+
+    # Encrypt the buffer.
+    p7 = s.encrypt(buf)
+
+    # Output p7 in mail-friendly format.
+    out = BIO.MemoryBuffer()
+
+    s.write(out, p7)
+    encrypted_message = out.read().decode('utf-8')
+    message = encrypted_message.split('\n\n')
+    content = message[1]
 
     entry_context = {
         'Email': {
-            'Message': content,
-            'Headers': headers
+            'Message': encrypted_message,
+            'Headers': 'MIME-Version=1.0,Content-Disposition=attachment; filename="smime.p7m",'
+                       'Content-Type=application/x-pkcs7-mime; smime-type=enveloped-data; name="smime.p7m",'
+                       'Content-Transfer-Encoding=base64'
         }
     }
-    return_outputs(content, entry_context)
+    return_outputs(encrypted_message, entry_context)
 
 
 if __name__ in ('__builtin__', 'builtins'):
