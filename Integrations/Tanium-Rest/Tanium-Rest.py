70aba84f00e8363584ca3d7ed1580095bc6d89a8
adi88d <adi88daud@gmail.com>
2019-09-11 17:19:28 +0300

    convert to new integration format

--- Integrations/Tanium-Rest/Tanium-Rest.py
+++ Integrations/Tanium-Rest/Tanium-Rest.py
@@ -12,172 +12,151 @@ requests.packages.urllib3.disable_warnings()
 
 ''' GLOBALS/PARAMS '''
 
-USERNAME = demisto.params().get('credentials').get('identifier')
-PASSWORD = demisto.params().get('credentials').get('password')
-Domain = demisto.params().get('domain')
-# Remove trailing slash to prevent wrong URL path to service
-SERVER = demisto.params()['url'][:-1] \
-    if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
-# Should we use SSL
-USE_SSL = not demisto.params().get('insecure', False)
-
-# Service base URL
-BASE_URL = SERVER + '/api/v2/'
-SESSION = ''
-
-# Remove proxy if not set to true in params
-if not demisto.params().get('proxy'):
-    del os.environ['HTTP_PROXY']
-    del os.environ['HTTPS_PROXY']
-    del os.environ['http_proxy']
-    del os.environ['https_proxy']
 
 
-''' HELPER FUNCTIONS '''
+class Client:
+    def __init__(self, username, password, domain, url, use_ssl):
+        self.url = url
+        self.base_url = self.url
+        self.verify = use_ssl
+        self.username = username
+        self.password = password
+        self.domain = domain
+        self.session = ''
 
+    def do_request(self, method, url_suffix, data=None):
+        if not self.session:
+            self.update_session()
 
-def do_request(method, url_suffix, data=None):
-    global SESSION
-    if not SESSION:
-        update_session()
+        res = self.http_request(method, url_suffix, data)
 
-    res = http_request(method, url_suffix, data)
+        if res.status_code == 403:
+            self.update_session()
+            res = self.http_request(method, url_suffix, data)
+            if res.status_code != 200:
+                return_error('Error in API call to ' + self.url + url_suffix + ' response= ' + res.text)
+            return res.json()
 
-    if res.status_code == 403:
-        update_session()
-        res = http_request(method, url_suffix, data)
-        if res.status_code != 200:
-            return_error('Error in API call to '+ BASE_URL + url_suffix + ' response= ' + res.text)
         return res.json()
 
-    return res.json()
+    def http_request(self, method, url_suffix, data=None,headers={}):
+        if self.session:
+            headers['session'] = self.session
+        # A wrapper for requests lib to send our requests and handle requests and responses better
+        res = requests.request(
+            method,
+            self.url + url_suffix,
+            verify=self.verify,
+            data=json.dumps(data),
+            headers=headers
+        )
 
+        if res.status_code ==404:
+            return_error(res.json().get('text'))
 
-def http_request(method, url_suffix, data=None,headers={}):
-    global SESSION
-    if SESSION:
-        headers['session'] = SESSION
-    # A wrapper for requests lib to send our requests and handle requests and responses better
-    res = requests.request(
-        method,
-        BASE_URL + url_suffix,
-        verify=USE_SSL,
-        data=json.dumps(data),
-        headers=headers
-    )
+        # Handle error responses gracefully
+        if res.status_code not in {200,403}:
+            return_error('Error in API call to Integration [%d] - %s' % (res.status_code, res.reason))
 
-    if res.status_code ==404:
-        return_error(res.json().get('text'))
+        return res
 
-    # Handle error responses gracefully
-    if res.status_code not in {200,403}:
-        return_error('Error in API call to Integration [%d] - %s' % (res.status_code, res.reason))
+    def update_session(self):
+        body = {
+            'username': self.username,
+            'domain': self.domain,
+            'password': self.password
+        }
 
-    return res
+        res = self.http_request('GET', 'session/login', body)
+        if res.status_code != 200:
+            return_error('')
 
+        self.session = res.json().get('data').get('session')
 
-def update_session():
-    body = {
-        'username': USERNAME,
-        'domain': Domain,
-        'password': PASSWORD
-    }
+    def parse_parameters(self, parameters):
+        sensors = parameters.split(';')
+        parameter_conditions=[]
 
-    res = http_request('GET', 'session/login', body)
-    if res.status_code != 200:
-        return_error('')
-    global SESSION
+        for sensor in sensors:
+            sensor_name = sensor.split('{')[0]
+            tmp_item = {'sensor': sensor_name, 'parameters': []}
 
-    SESSION = res.json().get('data').get('session')
+            parameters_txt = sensor.split('{')[1][:-1]
+            params = parameters_txt.split(',')
+            for param in params:
+                tmp_item['parameters'].append({
+                    'key': '||' + param.split('=')[0] + '||',
+                    'value': param.split('=')[1]
+                })
+            parameter_conditions.append(tmp_item)
 
+        return parameter_conditions
 
-def parse_parameters(parameters):
-    sensors = parameters.split(';')
-    parameter_conditions=[]
+    def add_parameters_to_question(self, question_response, parameters):
+        if not parameters:
+            return question_response
 
-    for sensor in sensors:
-        sensor_name = sensor.split('{')[0]
-        tmp_item = {'sensor': sensor_name, 'parameters': []}
+        for item in question_response.get('selects'):
+            sensor = item.get('sensor').get('name')
 
-        parameters_txt = sensor.split('{')[1][:-1]
-        params = parameters_txt.split(',')
-        for param in params:
-            tmp_item['parameters'].append({
-                'key': '||' + param.split('=')[0] + '||',
-                'value': param.split('=')[1]
-            })
-        parameter_conditions.append(tmp_item)
+            for parameter in parameters:
+                if parameter['sensor'] == sensor:
+                    item['sensor']['parameters'] = parameter['parameters']
+                    item['sensor']['source_hash'] = item['sensor']['hash']
+                    del item['sensor']['hash']
 
-    return parameter_conditions
-
-
-def add_parameters_to_question(question_response, parameters):
-    if not parameters:
         return question_response
 
-    for item in question_response.get('selects'):
-        sensor = item.get('sensor').get('name')
+    def parse_question(self, text, parameters):
+        parameters_condition = []
 
-        for parameter in parameters:
-            if parameter['sensor'] == sensor:
-                item['sensor']['parameters'] = parameter['parameters']
-                item['sensor']['source_hash'] = item['sensor']['hash']
-                del item['sensor']['hash']
+        if parameters:
+            try:
+                parameters_condition = self.parse_parameters(parameters)
+            except Exception:
+                return_error('parameters parsing failed ')
 
-    return question_response
+        res = self.do_request('POST', 'parse_question', {'text': text}).get('data')[0]
 
+        res = self.add_parameters_to_question(res, parameters_condition)
+        return res
 
-def parse_question(text, parameters):
-    parameters_condition = []
-
-    if parameters:
-        try:
-            parameters_condition =parse_parameters(parameters)
-        except Exception:
-            return_error('parameters parsing failed ')
-
-    res = do_request('POST', 'parse_question', {'text': text}).get('data')[0]
-
-    res = add_parameters_to_question(res, parameters_condition)
-    return res
+    def create_question(self, question_body):
+        res = self.do_request('POST', 'questions', question_body)
+        return res.get('data').get('id'),res
 
 
-def create_question(question_body):
-    res = do_request('POST', 'questions', question_body)
-    return res.get('data').get('id'),res
+    def parse_question_results(self, result):
+        results_sets = result.get('data').get('result_sets')[0]
+        if results_sets.get('estimated_total') != results_sets.get('mr_tested'):
+            return None
+        if results_sets.get('row_count') == 0:
+            return []
 
+        columns = []
+        for column in results_sets.get('columns'):
+            columns.append(column.get('name'))
 
-def parse_question_results(result):
-    results_sets = result.get('data').get('result_sets')[0]
-    if results_sets.get('estimated_total') != results_sets.get('mr_tested'):
-        return None
-    if results_sets.get('row_count') == 0:
-        return []
+        rows=[]
 
-    columns = []
-    for column in results_sets.get('columns'):
-        columns.append(column.get('name'))
-
-    rows=[]
-
-    for row in results_sets.get('rows'):
-        i=0
-        tmp_row = {}
-        for item in row.get('data'):
-            tmp_row[columns[i]] = item[0].get('text')
-            i += 1
-        rows.append(tmp_row)
-    return rows
+        for row in results_sets.get('rows'):
+            i=0
+            tmp_row = {}
+            for item in row.get('data'):
+                tmp_row[columns[i]] = item[0].get('text')
+                i += 1
+            rows.append(tmp_row)
+        return rows
 
 
 ''' COMMANDS + REQUESTS FUNCTIONS '''
 
 
-def test_module():
-    do_request('GET', 'groups')
+def test_module(client):
+    client.do_request('GET', 'groups')
 
 
-def get_package(data_args):
+def get_package(client, data_args):
     id = data_args.get('id')
     name = data_args.get('name')
     endpoint_url = ''
@@ -189,7 +168,7 @@ def get_package(data_args):
         endpoint_url = 'packages/by-name/' + name
 
 
-    raw_response = do_request('GET', endpoint_url)
+    raw_response = client.do_request('GET', endpoint_url)
     response = raw_response.get('data')
     response['ID'] = response['id']
     del response['id']
@@ -200,10 +179,10 @@ def get_package(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=raw_response)
 
 
-def get_packages(data_args):
+def get_packages(client, data_args):
     count = int(data_args.get('count'))
 
-    raw_response = do_request('GET', 'packages')
+    raw_response = client.do_request('GET', 'packages')
 
     packages = []
 
@@ -218,12 +197,12 @@ def get_packages(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=raw_response)
 
 
-def create_package(data_args):
+def create_package(client, data_args):
     name = data_args.get('name')
     command = data_args.get('command')
     body = {'name': name, 'command': command}
 
-    raw_response = do_request('POST', 'packages', body)
+    raw_response = client.do_request('POST', 'packages', body)
 
     package = raw_response.get('data')
     package['ID'] = package['id']
@@ -234,23 +213,23 @@ def create_package(data_args):
     human_readable = tableToMarkdown('Package information', context)
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=raw_response)
 
-def ask_question(data_args):
+def ask_question(client, data_args):
     question_text = data_args.get('question-text')
     parameters = data_args.get('parameters')
 
-    body = parse_question(question_text, parameters)
-    id, res = create_question(body)
+    body = client.parse_question(question_text, parameters)
+    id, res = client.create_question(body)
     context = {'ID': id,'text': question_text}
     context = createContext(context, removeNull=True)
     outputs = {'Tanium.Questions(val.ID === obj.ID)': context}
     return_outputs(readable_output='New question created. ID: ' + str(id), outputs=outputs, raw_response=res)
 
 
-def get_question_results(data_args):
+def get_question_results(client, data_args):
     id = data_args.get('question-id')
-    res = do_request('GET', 'result_data/question/' + str(id))
+    res = client.do_request('GET', 'result_data/question/' + str(id))
 
-    rows = parse_question_results(res)
+    rows = client.parse_question_results(res)
 
     if rows is None:
         return return_outputs(readable_output='Question is still executing, Question id: ' + str(id),
@@ -263,9 +242,9 @@ def get_question_results(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=res)
 
 
-def get_sensors(data_args):
+def get_sensors(client, data_args):
     count = int(data_args.get('count'))
-    res = do_request('GET', 'sensors/')
+    res = client.do_request('GET', 'sensors/')
 
     sensors=[]
     for sensor in res.get('data')[:-1][:count]:
@@ -279,7 +258,7 @@ def get_sensors(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=res)
 
 
-def get_sensor(data_args):
+def get_sensor(client, data_args):
     id = data_args.get('id')
     name = data_args.get('name')
     endpoint_url = ''
@@ -290,7 +269,7 @@ def get_sensor(data_args):
     if name:
         endpoint_url = 'sensors/by-name/' + name
 
-    raw_response = do_request('GET', endpoint_url)
+    raw_response = client.do_request('GET', endpoint_url)
     response = raw_response.get('data')
     response['ID'] = response['id']
     del response['id']
@@ -301,11 +280,11 @@ def get_sensor(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=response)
 
 
-def create_saved_question(data_args):
+def create_saved_question(client, data_args):
     id = data_args.get('question-id')
     name = data_args.get('name')
     body ={'name':name,'question':{'id':id}}
-    raw_response = do_request('POST', 'saved_questions',body)
+    raw_response = client.do_request('POST', 'saved_questions',body)
 
     response = raw_response.get('data')
     response['ID'] = response['id']
@@ -316,9 +295,9 @@ def create_saved_question(data_args):
     return_outputs(readable_output='Question saved. ID = ' + str(response['ID']), outputs=outputs, raw_response=raw_response)
 
 
-def get_saved_questions(data_args):
+def get_saved_questions(client, data_args):
     count = int(data_args.get('count'))
-    raw_response = do_request('GET', 'saved_questions')
+    raw_response = client.do_request('GET', 'saved_questions')
 
     questions=[]
     for question in raw_response.get('data')[:-1][:count]:
@@ -332,12 +311,12 @@ def get_saved_questions(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=raw_response)
 
 
-def get_saved_question_results(data_args):
+def get_saved_question_results(client, data_args):
     id = data_args.get('question-id')
 
-    res = do_request('GET', 'result_data/saved_question/' + str(id))
+    res = client.do_request('GET', 'result_data/saved_question/' + str(id))
 
-    rows = parse_question_results(res)
+    rows = client.parse_question_results(res)
     if rows is None:
         return return_outputs(readable_output='Question is still executing, Question id: ' + str(id),
                               outputs={}, raw_response=res)
@@ -349,8 +328,8 @@ def get_saved_question_results(data_args):
     return_outputs(readable_output=human_readable, outputs=outputs, raw_response=res)
 
 
-def get_system_status():
-    raw_response = do_request('GET', 'system_status')
+def get_system_status(client):
+    raw_response = client.do_request('GET', 'system_status')
     response = raw_response.get('data')
 
     context =[]
@@ -368,46 +347,56 @@ def get_system_status():
 
 
 def main():
+    username = demisto.params().get('credentials').get('identifier')
+    password = demisto.params().get('credentials').get('password')
+    domain = demisto.params().get('domain')
+    # Remove trailing slash to prevent wrong URL path to service
+    server = demisto.params()['url'][:-1] \
+        if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
+    # Service base URL
+    base_url = server + '/api/v2/'
+    # Should we use SSL
+    use_ssl = not demisto.params().get('insecure', False)
+
+    # Remove proxy if not set to true in params
+    handle_proxy()
     LOG('Command being called is %s' % (demisto.command()))
 
     try:
+        client = Client(username, password, domain, base_url, use_ssl)
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration test button.
-            test_module()
+            test_module(client)
             demisto.results('ok')
         elif demisto.command() == 'tn-get-package':
-            get_package(demisto.args())
+            get_package(client, demisto.args())
         elif demisto.command() == 'tn-ask-question':
-            ask_question(demisto.args())
+            ask_question(client, demisto.args())
         elif demisto.command() == 'tn-get-question-result':
-            get_question_results(demisto.args())
+            get_question_results(client, demisto.args())
         elif demisto.command() == 'tn-list-sensors':
-            get_sensors(demisto.args())
+            get_sensors(client, demisto.args())
         elif demisto.command() == 'tn-get-sensor':
-            get_sensor(demisto.args())
+            get_sensor(client, demisto.args())
         elif demisto.command() == 'tn-create-saved-question':
-            create_saved_question(demisto.args())
+            create_saved_question(client, demisto.args())
         elif demisto.command() == 'tn-list-saved-questions':
-            get_saved_questions(demisto.args())
+            get_saved_questions(client, demisto.args())
         elif demisto.command() == 'tn-get-saved-question-result':
-            get_saved_question_results(demisto.args())
+            get_saved_question_results(client, demisto.args())
         elif demisto.command() == 'tn-get-system-status':
-            get_system_status()
+            get_system_status(client)
         elif demisto.command() == 'tn-create-package':
-            create_package(demisto.args())
+            create_package(client, demisto.args())
         elif demisto.command() == 'tn-list-packages':
-            get_packages(demisto.args())
+            get_packages(client, demisto.args())
 
     # Log exceptions
-    except Exception, e:
-        LOG(e.message)
-        LOG.print_log()
-        return_error('error has occurred: {}'.format(str(e)))
-    finally:
-        LOG.print_log()
+    except Exception as e:
+        return_error('error has occurred: {}'.format(str(e)), error=e)
 
-# python2 uses __builtin__ python3 uses builtins
-if __name__ == "__builtin__" or __name__ == "builtins":
+
+if __name__ == 'builtins':
     main()
 
 
