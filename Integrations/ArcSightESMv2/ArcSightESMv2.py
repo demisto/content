153e5ae1952956c98317f51b37ccd329c17993c3
orenzohar <oren.zohar@demisto.com>
2019-09-19 15:34:14 +0300

    description

--- Integrations/ArcSightESMv2/ArcSightESMv2.py
+++ Integrations/ArcSightESMv2/ArcSightESMv2.py
@@ -13,7 +13,7 @@ requests.packages.urllib3.disable_warnings()
 """ GLOBALS """
 MAX_UNIQUE = int(demisto.params().get('max_unique', 2000))
 FETCH_CHUNK_SIZE = int(demisto.params().get('fetch_chunk_size', 50))
-FETCH_CHUNK_SIZE = 50 if FETCH_CHUNK_SIZE > 50 else FETCH_CHUNK_SIZE
+FETCH_CHUNK_SIZE = min(50, FETCH_CHUNK_SIZE)  # fetch size should no exceed 50
 
 BASE_URL = demisto.params().get('server').rstrip('/') + '/'
 VERIFY_CERTIFICATE = not demisto.params().get('insecure', True)
