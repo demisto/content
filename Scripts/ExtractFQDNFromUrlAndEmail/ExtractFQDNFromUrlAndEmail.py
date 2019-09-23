51830a3dcd3e614b89d99824a8ad3926feae998c
rshalem <rshalem@paloaltonetworks.com>
2019-09-23 11:55:12 +0300

    - FQDN formatting script

--- Scripts/ExtractFQDNFromUrlAndEmail/ExtractFQDNFromUrlAndEmail.py
+++ Scripts/ExtractFQDNFromUrlAndEmail/ExtractFQDNFromUrlAndEmail.py
@@ -3,11 +3,10 @@ from CommonServerPython import *
 from CommonServerUserPython import *
 
 
-FQDNs = []
 the_input = demisto.args().get('input')
 args = {
     'input': the_input,
     'extractFQDN': 'True'
 }
 
-demisto.results(demisto.executeCommand('ExtractDomainFromUrlAndEmail_copy', args))
+demisto.results(demisto.executeCommand('ExtractDomainFromUrlAndEmail', args))
