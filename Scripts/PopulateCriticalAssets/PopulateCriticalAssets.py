5f0c420604d15afee94ce9b6168d4bdd7fc8041c
idovandijk <43602124+idovandijk@users.noreply.github.com>
2019-09-23 11:57:44 +0300

    Fixed MyPy stuff

--- Scripts/PopulateCriticalAssets/PopulateCriticalAssets.py
+++ Scripts/PopulateCriticalAssets/PopulateCriticalAssets.py
@@ -6,18 +6,18 @@ field_cli_name = "criticalassets"
 current_value = (demisto.incidents()[0].get("CustomFields").get(field_cli_name)
                  or demisto.incidents()[0].get(field_cli_name))
 critical_assets = demisto.args().get('critical_assets')
-lst = []
+grid_lst = []  # type: <List>
 for key, value in critical_assets.items():
     if isinstance(value, list):
-        lst += [{'assetname': asset_value, 'assettype': key} for asset_value in value]
+        grid_lst += [{'assetname': asset_value, 'assettype': key} for asset_value in value]
     else:
-        lst += [{'assetname': value, 'assettype': key}]
+        grid_lst += [{'assetname': value, 'assettype': key}]
 
 # If the field is empty, we will set an empty value. Else, we will append the new values and set it to the field.
 if not current_value:
-    current_value = lst
+    current_value = grid_lst
 else:
-    current_value += lst
+    current_value += grid_lst
 val = json.dumps({field_cli_name: current_value})
 
 demisto.results(demisto.executeCommand("setIncident", {'customFields': val}))
