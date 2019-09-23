0aaed39054ac3f30bb520b82f4768797b6d77251
idovandijk <43602124+idovandijk@users.noreply.github.com>
2019-09-23 13:10:48 +0300

    Fixed MyPy stuff

--- Scripts/PopulateCriticalAssets/PopulateCriticalAssets.py
+++ Scripts/PopulateCriticalAssets/PopulateCriticalAssets.py
@@ -6,7 +6,7 @@ field_cli_name = "criticalassets"
 current_value = (demisto.incidents()[0].get("CustomFields").get(field_cli_name)
                  or demisto.incidents()[0].get(field_cli_name))
 critical_assets = demisto.args().get('critical_assets')
-grid_lst = []  # type: <List>
+grid_lst = []  # type: list
 for key, value in critical_assets.items():
     if isinstance(value, list):
         grid_lst += [{'assetname': asset_value, 'assettype': key} for asset_value in value]
