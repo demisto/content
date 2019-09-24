6537c436ccd5de63fe9bbe8307eb5ec67b4b6ef6
dtavori <dtavori@paloaltonetworks.com>
2019-09-24 16:10:45 +0300

    MSGraphMail: - Edited the description of the search argument in ***msgraph-mail-list-emails*** command. - Fixed a bug where ***msgraph-mail-delete-email*** command always returned an error.

--- Integrations/MicrosoftGraphMail/MicrosoftGraphMail.py
+++ Integrations/MicrosoftGraphMail/MicrosoftGraphMail.py
@@ -216,7 +216,9 @@ def http_request(method: str, url_suffix: str = '', params: dict = None, data: d
         error = error_parser(res)
         return_error(f'Error in API call to Microsoft Graph Mail Integration [{res.status_code}] - {error}')
     try:
-        return res.json()
+        if method.lower() != 'delete':  # the DELETE request returns nothing in response
+            return res.json()
+        return {}
     except ValueError:
         return_error('Could not decode response from API')
         return {}  # return_error will exit
