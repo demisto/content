f07c1233bb2e5662279bd2f227f4bb313c64341e
ikeren <itay@demisto.com>
2019-09-24 15:09:06 +0300

    fix linting

--- Integrations/Nmap/Nmap.py
+++ Integrations/Nmap/Nmap.py
@@ -11,7 +11,11 @@ if demisto.command() == 'nmap-scan':
     nm = NmapProcess(argToList(demisto.args()['targets']), options=demisto.args()['options'])
     rc = nm.run()
     if rc != 0:
-        demisto.results({'Type': entryTypes['error'], 'ContentsFormat': formats['text'], 'Contents': 'Unable to execute - ' + nm.stderr})
+        demisto.results({
+            'Type': entryTypes['error'],
+            'ContentsFormat': formats['text'],
+            'Contents': 'Unable to execute - ' + nm.stderr
+        })
         sys.exit(0)
     r = NmapParser.parse(nm.stdout)
     md = '## ' + r.summary + '\n'
@@ -27,13 +31,32 @@ if demisto.command() == 'nmap-scan':
         h['Address'] = host.address
         h['Status'] = host.status
         svc = []
-        md += "### Nmap scan report for {0}".format(tmp_host) + (" ({0})\n".format(host.address) if tmp_host != host.address else "\n")
+        md += "### Nmap scan report for {0}".format(tmp_host) + \
+              (" ({0})\n".format(host.address) if tmp_host != host.address else "\n")
         md += "#### Host is {0}.\n".format(host.status)
         for serv in host.services:
-            svc.append({'Port': serv.port, 'Protocol': serv.protocol, 'State': serv.state, 'Service': serv.service, 'Banner': serv.banner})
+            svc.append({
+                'Port': serv.port,
+                'Protocol': serv.protocol,
+                'State': serv.state,
+                'Service': serv.service,
+                'Banner': serv.banner
+            })
         md += tableToMarkdown('Services', svc, ['Port', 'Protocol', 'State', 'Service', 'Banner'])
         h['Services'] = svc
         hosts.append(h)
-    scan = {'Summary': r.summary, 'Version': r.version, 'Started': r.started, 'Ended': r.endtime, 'CommandLine': r.commandline, 'ScanType': r.scan_type, 'Hosts': hosts}
-    demisto.results({'ContentsFormat': formats['json'], 'Type': entryTypes['note'], 'Contents': json.dumps(r, cls=ReportEncoder),
-        'HumanReadable': md, 'EntryContext': {'NMAP.Scan': scan}})
+    scan = {
+        'Summary': r.summary,
+        'Version': r.version,
+        'Started': r.started,
+        'Ended': r.endtime,
+        'CommandLine': r.commandline,
+        'ScanType': r.scan_type,
+        'Hosts': hosts}
+    demisto.results({
+        'ContentsFormat': formats['json'],
+        'Type': entryTypes['note'],
+        'Contents': json.dumps(r, cls=ReportEncoder),
+        'HumanReadable': md,
+        'EntryContext': {'NMAP.Scan': scan}
+    })
