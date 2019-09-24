06cb0db68fccbb4709d7951b8f1f9e15dca4290a
Omer Cohen <omer@paloaltonetworks.com>
2019-09-24 23:32:18 +0300

    Update EWSv2.py

--- Integrations/EWSv2/EWSv2.py
+++ Integrations/EWSv2/EWSv2.py
@@ -1165,6 +1165,7 @@ def fetch_emails_as_incidents(account_email, folder_name):
 
         ids = deque(last_run.get(LAST_RUN_IDS), maxsize=LAST_RUN_IDS_QUEUE_SIZE)
         incidents = []
+        incident = {}
         for item in last_emails:
             if item.message_id:
                 ids.append(item.message_id)
