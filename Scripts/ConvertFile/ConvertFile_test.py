da2fce23a8ab19335cb9f24bd29318a39df8574f
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2019-10-14 21:52:35 +0300

    fix unit tests

--- Scripts/ConvertFile/ConvertFile_test.py
+++ Scripts/ConvertFile/ConvertFile_test.py
@@ -41,9 +41,10 @@ def test_conver_to_html(mocker):
     # validate our mocks are good
     assert demisto.args()['entry_id'] == 'test'
     main()
-    # call_args is tuple (args list, kwargs). we only need the first one
-    results = demisto.results.call_args[0]
     assert demisto.results.call_count > 10  # we have also a bunch of images
+    # call_args_list holds all calls as a tuple (args list, kwargs). we only need the first one
+    # we check if we have an html file
+    results = [x[0] for x in demisto.results.call_args_list if x[0][0]['File'].endswith('.html')][0]
     assert len(results) == 1
     assert results[0]['Type'] == entryTypes['file']
     assert results[0]['File'] == 'test.html'
@@ -53,7 +54,7 @@ def test_conver_to_html(mocker):
         contents = f.read()
         assert 'Extensions to the Office Open XML' in contents
     # assert the next result is an image
-    results = demisto.results.call_args_list[1][0]
+    results = [x[0] for x in demisto.results.call_args_list if x[0][0]['File'].endswith('.png')][0]
     assert results[0]['Type'] == entryTypes['file']
-    assert results[0]['File'].endswith('.png')
+    assert results[0]['File'].startswith('test_html_')
     assert glob.glob('./*' + results[0]['FileID'])
