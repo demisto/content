b29f7593f8620f6e7f5325cd127c823fd04e2e21
orenzohar <oren.zohar@demisto.com>
2019-10-15 14:40:54 +0300

    tests

--- Integrations/rasterize/rasterize_test.py
+++ Integrations/rasterize/rasterize_test.py
@@ -1,9 +1,10 @@
 from rasterize import *
+from tempfile import TemporaryFile
 import os
 
 
 def test_rasterize_email_image():
-    with open('emailHtmlBody2.html', 'w') as f:
+    with TemporaryFile('w') as f:
         f.write('<html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">'
                 '</head><body><br>---------- TEST FILE ----------<br></body></html>')
         path = os.path.realpath(f.name)
@@ -17,7 +18,7 @@ def test_rasterize_email_image():
 
 
 def test_rasterize_email_pdf():
-    with open('emailHtmlBody1.html', 'w') as f:
+    with TemporaryFile('w') as f:
         f.write('<html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">'
                 '</head><body><br>---------- TEST FILE ----------<br></body></html>')
         path = os.path.realpath(f.name)
@@ -26,5 +27,20 @@ def test_rasterize_email_pdf():
         width=1000,
         height=1000,
         r_type='pdf'
+    )
+    os.remove(path)
+
+
+def test_rasterize_email_pdf_offline():
+    with TemporaryFile('w') as f:
+        f.write('<html><head><meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">'
+                '</head><body><br>---------- TEST FILE ----------<br></body></html>')
+        path = os.path.realpath(f.name)
+    rasterize(
+        path=f'file://{path}',
+        width=1000,
+        height=1000,
+        r_type='pdf',
+        offline_mode=True
     )
     os.remove(path)
