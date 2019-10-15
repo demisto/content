c4d1da61a255c78c4a61f1537be02ade0ac7490d
Jochman <bar.hochman@demisto.com>
2019-10-15 13:27:30 +0300

    fix authentication

--- Templates/Integrations/Authentication/Authentication.py
+++ Templates/Integrations/Authentication/Authentication.py
@@ -27,7 +27,7 @@ INTEGRATION_CONTEXT_NAME = 'AuthenticationIntegration'
 
 
 class Client(BaseClient):
-    def test_module_request(self) -> Dict:
+    def test_module(self) -> Dict:
         """Performs basic GET request to check if the API is reachable and authentication is successful.
 
         Returns:
@@ -35,7 +35,7 @@ class Client(BaseClient):
         """
         return self._http_request('GET', 'version')
 
-    def list_credentials_request(self) -> Dict:
+    def list_credentials(self) -> Dict:
         """Uses to fetch credentials into Demisto
         Documentation: https://github.com/demisto/content/tree/master/docs/fetching_credentials
 
@@ -45,7 +45,7 @@ class Client(BaseClient):
         suffix = 'credential'
         return self._http_request('GET', suffix)
 
-    def list_accounts_request(self, max_results: int = None) -> Dict:
+    def list_accounts(self, max_results: int = None) -> Dict:
         """Lists all accounts.
         Args:
             max_results: maximum results to filter.
@@ -57,7 +57,7 @@ class Client(BaseClient):
         params = assign_params(limit=max_results)
         return self._http_request('GET', suffix, params=params)
 
-    def lock_account_request(self, account_id: AnyStr) -> Dict:
+    def lock_account(self, account_id: AnyStr) -> Dict:
         """Locks an account by the account ID.
 
         Args:
@@ -72,7 +72,7 @@ class Client(BaseClient):
         params = {'account': account_id}
         return self._http_request('POST', suffix, params=params)
 
-    def unlock_account_request(self, account_id: AnyStr) -> Dict:
+    def unlock_account(self, account_id: AnyStr) -> Dict:
         """Returns events by the account ID.
 
         Args:
@@ -88,7 +88,7 @@ class Client(BaseClient):
         # Send a request using our http_request wrapper
         return self._http_request('POST', suffix, params=params)
 
-    def reset_account_request(self, account_id: str):
+    def reset_account(self, account_id: str):
         """Resets an account by account ID.
 
         Args:
@@ -104,7 +104,7 @@ class Client(BaseClient):
         # Send a request using our http_request wrapper
         return self._http_request('POST', suffix, params=params)
 
-    def unlock_vault_request(self, vault_to_lock: AnyStr) -> Dict:
+    def unlock_vault(self, vault_to_lock: AnyStr) -> Dict:
         """Unlocks a vault by vault ID.
 
         Args:
@@ -117,7 +117,7 @@ class Client(BaseClient):
         params = {'vaultId': vault_to_lock}
         return self._http_request('POST', suffix, params=params)
 
-    def lock_vault_request(self, vault_to_lock: AnyStr) -> Dict:
+    def lock_vault(self, vault_to_lock: AnyStr) -> Dict:
         """Locks vault by vault ID.
 
         Args:
@@ -130,7 +130,7 @@ class Client(BaseClient):
         params = {'vaultId': vault_to_lock}
         return self._http_request('POST', suffix, params=params)
 
-    def list_vaults_request(self, max_results: int) -> Dict:
+    def list_vaults(self, max_results: int) -> Dict:
         """Return all vaults from API.
 
         Args:
@@ -148,7 +148,7 @@ class Client(BaseClient):
 ''' HELPER FUNCTIONS '''
 
 
-def build_account_context(credentials: Union[Dict, List]) -> Union[Dict, List]:
+def account_response_to_context(credentials: Union[Dict, List]) -> Union[Dict, List]:
     """Formats the API response to Demisto context.
 
     Args:
@@ -158,11 +158,11 @@ def build_account_context(credentials: Union[Dict, List]) -> Union[Dict, List]:
         The formatted Dict or List.
 
     Examples:
-        >>> build_account_context([{'username': 'user', 'name': 'demisto', 'isLocked': False}])
+        >>> account_response_to_context([{'username': 'user', 'name': 'demisto', 'isLocked': False}])
         [{'Username': 'user', 'Name': 'demisto', 'IsLocked': False}]
     """
     if isinstance(credentials, list):
-        return [build_account_context(credential) for credential in credentials]
+        return [account_response_to_context(credential) for credential in credentials]
     return {
         'Username': credentials.get('username'),
         'Name': credentials.get('name'),
@@ -204,44 +204,44 @@ def build_vaults_context(vaults: Union[List, Dict]) -> Union[List[Dict], Dict]:
 ''' COMMANDS '''
 
 
-def test_module(client: Client, *_) -> Tuple[str, Dict, Dict]:
+def test_module_command(client: Client, *_) -> str:
     """Performs a basic GET request to check if the API is reachable and authentication is successful.
     """
-    results = client.test_module_request()
+    results = client.test_module()
     if 'version' in results:
-        return 'ok', {}, {}
+        return 'ok'
     raise DemistoException('Test module failed, {}'.format(results))
 
 
-def fetch_credentials(client: Client):
+def fetch_credentials(client: Client) -> list:
     """Uses to fetch credentials into Demisto
     Documentation: https://github.com/demisto/content/tree/master/docs/fetching_credentials
     """
     # Get credentials from api
-    raw_response = client.list_credentials_request()
-    if 'credential' in raw_response:
-        raw_credentials = raw_response.get('credential', [])
+    raw_response = client.list_credentials()
+    raw_credentials = raw_response.get('credential', [])
+    if raw_credentials:
         # Creates credentials entry
         credentials = build_credentials_fetch(raw_credentials)
-        demisto.credentials(credentials)
+        return credentials
     else:
         raise DemistoException(f'`fetch-incidents` failed in `{INTEGRATION_NAME}`, no keyword `credentials` in'
                                f' response. Check API')
 
 
-def lock_account(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def lock_account_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Locks an account by account ID.
     """
     # Get arguments from user
     username = args.get('username', '')
     # Make request and get raw response
-    raw_response = client.lock_account_request(username)
+    raw_response = client.lock_account(username)
     # Get account from raw_response
     user_object = raw_response.get('account', [{}])[0]
     # Parse response into context & content entries
     if user_object.get('username') == username and user_object.get('isLocked') is True:
         title: str = f'{INTEGRATION_NAME} - Account `{username}` has been locked.'
-        context_entry = build_account_context(user_object)
+        context_entry = account_response_to_context(user_object)
         context = {
             f'{INTEGRATION_CONTEXT_NAME}.Account(val.Username && val.Username === obj.Username)': context_entry
         }
@@ -253,20 +253,20 @@ def lock_account(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
         raise DemistoException(f'{INTEGRATION_NAME} - Could not lock account `{username}`')
 
 
-def unlock_account(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def unlock_account_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Unlocks an account by account ID.
     """
     # Get arguments from user
     username = args.get('username', '')
     # Make request and get raw response
-    raw_response = client.unlock_account_request(username)
+    raw_response = client.unlock_account(username)
     # Get account from raw_response
     accounts = raw_response.get('account')
     # Parse response into context & content entries
     if accounts and accounts[0].get('username') == username and accounts[0].get('isLocked') is False:
         user_object = accounts[0]
         title = f'{INTEGRATION_NAME} - Account `{username}` has been unlocked.'
-        context_entry = build_account_context(user_object)
+        context_entry = account_response_to_context(user_object)
         context = {
             f'{INTEGRATION_CONTEXT_NAME}.Account(val.Username && val.Username === obj.Username)': context_entry}
         # Creating human readable for War room
@@ -277,20 +277,20 @@ def unlock_account(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
         raise DemistoException(f'{INTEGRATION_NAME} - Could not unlock account `{username}`')
 
 
-def reset_account(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def reset_account_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Resets an account by account ID
     """
     # Get arguments from user
     username = args.get('username', '')
     # Make request and get raw response
-    raw_response = client.reset_account_request(username)
+    raw_response = client.reset_account(username)
     # Get account from raw_response
     accounts = raw_response.get('account')
     # Parse response into context & content entries
     if accounts and accounts[0].get('username') == username and accounts[0].get('isLocked') is False:
         user_object = accounts[0]
         title = f'{INTEGRATION_NAME} - Account `{username}` has been returned to default.'
-        context_entry = build_account_context(user_object)
+        context_entry = account_response_to_context(user_object)
         context = {
             f'{INTEGRATION_CONTEXT_NAME}.Account(val.Username && val.Username === obj.Username)': context_entry}
         # Creating human readable for War room
@@ -301,11 +301,11 @@ def reset_account(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
         raise DemistoException(f'{INTEGRATION_NAME} - Could not reset account `{username}`')
 
 
-def list_accounts(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def list_accounts_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Returns credentials to user without passwords.
     """
     max_results = int(args.get('max_results')) if args.get('max_results') else None
-    raw_response = client.list_accounts_request(max_results=max_results)
+    raw_response = client.list_accounts(max_results=max_results)
     # Filtering out passwords for list_credentials, so it won't get back to the user
     raw_response['account'] = [
         assign_params(keys_to_ignore=['password'], **account) for account in raw_response.get('account', [])
@@ -313,7 +313,7 @@ def list_accounts(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     accounts = raw_response['account']
     if accounts:
         title = f'{INTEGRATION_NAME} - Account list.'
-        context_entry = build_account_context(accounts)
+        context_entry = account_response_to_context(accounts)
         context = {f'{INTEGRATION_CONTEXT_NAME}.Account(val.ID && val.ID ==== obj.ID)': context_entry}
         human_readable = tableToMarkdown(title, context_entry)
         return human_readable, context, raw_response
@@ -321,16 +321,17 @@ def list_accounts(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
         return f'{INTEGRATION_NAME} - Could not find any users.', {}, {}
 
 
-def lock_vault(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def lock_vault_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Locks a vault by vault ID.
     """
-    vault_to_lock = args.get('vault_id', '')
-    raw_response = client.lock_vault_request(vault_to_lock)
-    vaults = raw_response.get('vault')
-    if vaults and vaults[0].get('vaultId') and vaults[0].get('isLocked') is True:
-        vault_object = vaults[0]
+    vault_to_lock = args.get('vault_id')
+    raw_response = client.lock_vault(vault_to_lock)
+    vault_obj = raw_response.get('vault', [{}])[0]
+    vault_id = vault_obj.get('vaultId')
+    locked_status = vault_obj.get('isLocked')
+    if vault_id == vault_to_lock and locked_status:
         title = f'{INTEGRATION_NAME} - Vault {vault_to_lock} has been locked'
-        context_entry = build_vaults_context(vault_object)
+        context_entry = build_vaults_context(vault_obj)
         context = {f'{INTEGRATION_CONTEXT_NAME}.Vault(val.ID && val.ID === obj.ID)': context_entry}
         human_readable = tableToMarkdown(title, context_entry)
         return human_readable, context, raw_response
@@ -338,16 +339,17 @@ def lock_vault(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
         raise DemistoException(f'{INTEGRATION_NAME} - Could not lock vault ID: {vault_to_lock}')
 
 
-def unlock_vault(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def unlock_vault_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Unlocks a vault by vault ID.
     """
     vault_to_lock = args.get('vault_id', '')
-    raw_response = client.unlock_vault_request(vault_to_lock)
-    vaults = raw_response.get('vault')
-    if vaults and vaults[0].get('vaultId') and vaults[0].get('isLocked') is False:
-        vault_object = vaults[0]
+    raw_response = client.unlock_vault(vault_to_lock)
+    vault_obj = raw_response.get('vault', [{}])[0]
+    vault_id = vault_obj.get('vaultId')
+    locked_status = vault_obj.get('isLocked')
+    if vault_id == vault_to_lock and not locked_status:
         title = f'{INTEGRATION_NAME} - Vault {vault_to_lock} has been unlocked'
-        context_entry = build_vaults_context(vault_object)
+        context_entry = build_vaults_context(vault_obj)
         context = {f'{INTEGRATION_CONTEXT_NAME}.Vault(val.ID && val.ID === obj.ID)': context_entry}
         human_readable = tableToMarkdown(title, context_entry)
         return human_readable, context, raw_response
@@ -355,11 +357,11 @@ def unlock_vault(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
         raise DemistoException(f'{INTEGRATION_NAME} - Could not unlock vault ID: {vault_to_lock}')
 
 
-def list_vaults(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
+def list_vaults_command(client: Client, args: Dict) -> Tuple[str, Dict, Dict]:
     """Lists all vaults.
     """
     max_results = int(args.get('max_results', 0))
-    raw_response = client.list_vaults_request(max_results)
+    raw_response = client.list_vaults(max_results)
     vaults = raw_response.get('vault')
     if vaults:
         title = f'{INTEGRATION_NAME} - Total of {len(vaults)} has been found.'
@@ -385,21 +387,22 @@ def main():  # pragma: no cover
 
     # Switch case
     commands = {
-        'test-module': test_module,
+        'test-module': test_module_command,
         'fetch-credentials': fetch_credentials,
-        f'{INTEGRATION_COMMAND_NAME}-list-accounts': list_accounts,
-        f'{INTEGRATION_COMMAND_NAME}-lock-account': lock_account,
-        f'{INTEGRATION_COMMAND_NAME}-unlock-account': unlock_account,
-        f'{INTEGRATION_COMMAND_NAME}-reset-account': reset_account,
-        f'{INTEGRATION_COMMAND_NAME}-lock-vault': lock_vault,
-        f'{INTEGRATION_COMMAND_NAME}-unlock-vault': unlock_vault,
-        f'{INTEGRATION_COMMAND_NAME}-list-vaults': list_vaults
+        f'{INTEGRATION_COMMAND_NAME}-list-accounts': list_accounts_command,
+        f'{INTEGRATION_COMMAND_NAME}-lock-account': lock_account_command,
+        f'{INTEGRATION_COMMAND_NAME}-unlock-account': unlock_account_command,
+        f'{INTEGRATION_COMMAND_NAME}-reset-account': reset_account_command,
+        f'{INTEGRATION_COMMAND_NAME}-lock-vault': lock_vault_command,
+        f'{INTEGRATION_COMMAND_NAME}-unlock-vault': unlock_vault_command,
+        f'{INTEGRATION_COMMAND_NAME}-list-vaults': list_vaults_command
     }
     try:
         if command == 'fetch-credentials':
             # Fetch credentials is handled, no return statement.
-            commands[command](client)
-        if command in commands:
+            credentials = fetch_credentials(client)
+            demisto.credentials(credentials)
+        elif command in commands:
             return_outputs(*commands[command](client, demisto.args()))
     # Log exceptions
     except Exception as e:
--- Templates/Integrations/Authentication/Authentication_test.py
+++ Templates/Integrations/Authentication/Authentication_test.py
@@ -19,7 +19,6 @@ GET_USER_LIST = {
     ]
 }
 
-
 client = Client('https://example.com/v1/')
 
 
@@ -29,9 +28,9 @@ class TestBuildContext:
         results = build_credentials_fetch([{'username': 'user1', 'name': 'name1', 'password': 'password'}])
         assert results == [{'name': 'name1', 'password': 'password', 'user': 'user1'}]
 
-    def test_build_account_context(self):
-        from Authentication import build_account_context
-        results = build_account_context(GET_USER_LIST['account'])
+    def test_account_response_to_context(self):
+        from Authentication import account_response_to_context
+        results = account_response_to_context(GET_USER_LIST['account'])
         assert results == [
             {'IsLocked': False, 'Name': 'DBot Demisto', 'Username': 'User1'},
             {'IsLocked': True, 'Name': 'Demisto DBot', 'Username': 'User2'}]
@@ -45,8 +44,7 @@ class TestCredentialsOperations:
         requests_mock.get(
             f'{BASE_URL}credential',
             json=GET_CREDENTIALS)
-        fetch_credentials(client)
-        results = demisto.credentials.call_args[0][0]
+        results = fetch_credentials(client)
         assert results == [{'user': 'User1', 'name': 'DBot Demisto', 'password': 'mj54bk32gb'},
                            {'user': 'User2', 'name': 'Demisto DBot', 'password': 'mj54bk32gb'}]
 
@@ -61,116 +59,116 @@ class TestCredentialsOperations:
             fetch_credentials(client)
 
     def test_list_accounts_full(self, mocker):
-        from Authentication import list_accounts
-        mocker.patch.object(client, 'list_accounts_request', return_value=GET_USER_LIST)
-        _, _, raw_response = list_accounts(client, {})
+        from Authentication import list_accounts_command
+        mocker.patch.object(client, 'list_accounts', return_value=GET_USER_LIST)
+        _, _, raw_response = list_accounts_command(client, {})
         assert raw_response == {'account': [{'username': 'User1', 'name': 'DBot Demisto', 'isLocked': False},
                                             {'username': 'User2', 'name': 'Demisto DBot', 'isLocked': True}]}
 
     def test_list_accounts_negative(self, requests_mock):
-        from Authentication import list_accounts
+        from Authentication import list_accounts_command
         requests_mock.get(BASE_URL + 'account', json={'account': []})
-        human_readable, _, _ = list_accounts(client, {})
+        human_readable, _, _ = list_accounts_command(client, {})
         assert 'Could not find any users' in human_readable
 
 
 class TestTestModule:
     def test_test_module_positive(self, requests_mock):
-        from Authentication import test_module
+        from Authentication import test_module_command
         requests_mock.get(BASE_URL + 'version', json={'version': '1'})
-        human_readable, _, _ = test_module(client, None)
+        human_readable = test_module_command(client, None)
         assert human_readable == 'ok'
 
     def test_test_module_false(self, requests_mock):
-        from Authentication import test_module
+        from Authentication import test_module_command
         requests_mock.get(BASE_URL + 'version', json={})
         with raises(DemistoException, match='Test module failed'):
-            test_module(client, None)
+            test_module_command(client, None)
 
 
 class TestAccountOperations:
     def test_lock_account_positive(self, requests_mock):
-        from Authentication import lock_account
+        from Authentication import lock_account_command
         requests_mock.post(BASE_URL + 'account/lock?account=111', json={
             'account': [{'username': '111', 'isLocked': True}]})
-        human_readable, _, _ = lock_account(client, {'username': '111'})
+        human_readable, _, _ = lock_account_command(client, {'username': '111'})
         assert 'Authentication Integration - Account `111`' in human_readable
 
     def test_lock_account_negative(self, requests_mock):
-        from Authentication import lock_account
+        from Authentication import lock_account_command
         requests_mock.post(BASE_URL + 'account/lock?account=111', json={})
         with raises(DemistoException, match='Could not lock account'):
-            lock_account(client, {'username': '111'})
+            lock_account_command(client, {'username': '111'})
 
     def test_unlock_account_positive(self, requests_mock):
-        from Authentication import unlock_account
+        from Authentication import unlock_account_command
         requests_mock.post(BASE_URL + 'account/unlock?account=111', json={
             'account': [{'username': '111', 'isLocked': False}]})
-        human_readable, _, _ = unlock_account(client, {'username': '111'})
+        human_readable, _, _ = unlock_account_command(client, {'username': '111'})
         assert 'Authentication Integration - Account `111`' in human_readable
 
     def test_unlock_account_negative(self, requests_mock):
-        from Authentication import unlock_account
+        from Authentication import unlock_account_command
         requests_mock.post(BASE_URL + 'account/unlock?account=111', json={})
         with raises(DemistoException, match='Could not unlock account'):
-            unlock_account(client, {'username': '111'})
+            unlock_account_command(client, {'username': '111'})
 
     def test_reset_account_positive(self, requests_mock):
-        from Authentication import reset_account
+        from Authentication import reset_account_command
         requests_mock.post(BASE_URL + 'account/reset?account=111', json={
             'account': [{'username': '111', 'isLocked': False}]})
-        human_readable, _, _ = reset_account(client, {'username': '111'})
+        human_readable, _, _ = reset_account_command(client, {'username': '111'})
         assert 'Authentication Integration - Account `111`' in human_readable
 
     def test_reset_account_negative(self, requests_mock):
-        from Authentication import reset_account
+        from Authentication import reset_account_command
         requests_mock.post(BASE_URL + 'account/reset?account=111', json={})
         with raises(DemistoException, match='Could not reset account'):
-            reset_account(client, {'username': '111'})
+            reset_account_command(client, {'username': '111'})
 
 
 class TestVaultOperations:
     def test_lock_vault_positive(self, requests_mock):
-        from Authentication import lock_vault
+        from Authentication import lock_vault_command
         requests_mock.post(BASE_URL + 'vault/lock?vaultId=111', json={
             'vault': [{'vaultId': '111', 'isLocked': True}]})
-        results = lock_vault(client, {'vault_id': '111'})
+        results = lock_vault_command(client, {'vault_id': '111'})
         assert 'Vault 111 has been locked' in results[0]
 
     def test_lock_vault_negative(self, requests_mock):
-        from Authentication import lock_vault
+        from Authentication import lock_vault_command
         requests_mock.post(BASE_URL + 'vault/lock?vaultId=111', json={
             'vault': [{'vaultId': '111', 'isLocked': False}]})
         with raises(DemistoException, match='Could not lock vault'):
-            lock_vault(client, {'vault_id': '111'})
+            lock_vault_command(client, {'vault_id': '111'})
 
     def test_unlock_vault_positive(self, requests_mock):
-        from Authentication import unlock_vault
+        from Authentication import unlock_vault_command
         requests_mock.post(BASE_URL + 'vault/unlock?vaultId=111', json={
             'vault': [{'vaultId': '111', 'isLocked': False}]})
-        results = unlock_vault(client, {'vault_id': '111'})
+        results = unlock_vault_command(client, {'vault_id': '111'})
         assert 'Vault 111 has been unlocked' in results[0]
 
     def test_unlock_vault_negative(self, requests_mock):
-        from Authentication import unlock_vault
+        from Authentication import unlock_vault_command
         requests_mock.post(BASE_URL + 'vault/unlock?vaultId=111', json={
             'vault': [{'vaultId': '111', 'isLocked': True}]})
         with raises(DemistoException, match='Could not unlock vault'):
-            unlock_vault(client, {'vault_id': '111'})
+            unlock_vault_command(client, {'vault_id': '111'})
 
     def test_list_vaults_positive(self, requests_mock):
-        from Authentication import list_vaults
+        from Authentication import list_vaults_command
         requests_mock.get(BASE_URL + 'vault', json={
             'vault': [
                 {'vaultId': '111', 'isLocked': True},
                 {'vaultId': '121', 'isLocked': False},
                 {'vaultId': '164', 'isLocked': False}
             ]})
-        human_readable, _, _ = list_vaults(client, {})
+        human_readable, _, _ = list_vaults_command(client, {})
         assert 'Total of 3 has been found' in human_readable
 
     def test_list_vaults_negative(self, requests_mock):
-        from Authentication import list_vaults
+        from Authentication import list_vaults_command
         requests_mock.get(BASE_URL + 'vault', json={'vault': []})
-        human_readable, _, _ = list_vaults(client, {})
+        human_readable, _, _ = list_vaults_command(client, {})
         assert 'No vaults found' in human_readable
