9a32fda1b82b81ae21c12c5cdb91fe5d9a655d76
Jochman <bar.hochman@demisto.com>
2019-10-15 14:50:41 +0300

    *arg to test module

--- Templates/Integrations/DataEnrichmentThreatIntelligence/DataEnrichmentThreatIntelligence.py
+++ Templates/Integrations/DataEnrichmentThreatIntelligence/DataEnrichmentThreatIntelligence.py
@@ -66,7 +66,7 @@ class Client(BaseClient):
         # Unknown
         return 0
 
-    def test_module(self) -> Dict:
+    def test_module(self, *_) -> Dict:
         """Performs basic get request to see if the API is reachable and authentication works.
 
         Returns:
