62b94bcb4127e22e5f0f0c2647c8cf7065b0d3b8
Tim Underhay <tim.underhay@demisto.com>
2019-10-16 11:27:42 -0400

    Converted all to package format.

---
+++ Scripts/IsRFC1918Address/IsRFC1918Address.py
@@ -0,0 +1,8 @@
+import demistomock as demisto
+from CommonServerPython import *
+ADDRESS = demisto.args()['value']
+RANGES = '10.0.0.0/8,172.16.0.0/12,192.168.0.0/16'
+
+res = demisto.executeCommand('IsInCidrRanges', { 'value': ADDRESS, 'cidr_ranges': RANGES } )[0]
+
+demisto.results(res['Contents'])
