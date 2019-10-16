a734dedf189ac2241748783218b244f3ac638d93
eharush <erez@demisto.com>
2019-10-16 19:28:34 +0300

    DBot create phishing classifier V2

---
+++ Scripts/DBotTrainTextClassifierV2/DBotTrainTextClassifierV2.py
@@ -0,0 +1,248 @@
+# pylint: disable=no-member
+from collections import defaultdict
+import demisto_ml
+from tabulate import tabulate
+from CommonServerPython import *
+
+ALL_LABELS = "*"
+GENERAL_SCORES = ['macro avg', 'micro avg', 'weighted avg']
+
+
+def get_hr_for_scores(header, confusion_matrix, report):
+    scores_rows = ["#### Overall score: %.2f" % report['macro avg']['precision']]
+    for k, v in report.items():
+        if isinstance(v, dict):
+            if k not in GENERAL_SCORES:
+                scores_rows.append("- %s: %.2f" % (k, v['precision']))
+    scores_desc = "\n".join(scores_rows)
+
+    confusion_matrix_desc = tabulate(confusion_matrix,
+                                     tablefmt="pipe",
+                                     headers="keys").replace("True", "True \\ Predicted")
+    return "# {0}\n ## Confusion Matrix:\n{1}\n ## Scores:\n{2} ".format(header, confusion_matrix_desc, scores_desc)
+
+
+def canonize_label(label):
+    return label.replace(" ", "_")
+
+
+def get_phishing_map_labels(comma_values):
+    if comma_values == ALL_LABELS:
+        return comma_values
+    values = map(lambda x: x.strip(), comma_values.split(","))
+    labels_dict = {}
+    for v in values:
+        v = v.strip()
+        if ":" in v:
+            splited = v.split(":")
+            labels_dict[splited[0].strip()] = splited[1].strip()
+        else:
+            labels_dict[v] = v
+    return {k: canonize_label(v) for k, v in labels_dict.items()}
+
+
+def read_file(input_entry_or_string, file_type):
+    data = []
+    if not input_entry_or_string:
+        return data
+    if file_type.endswith("string"):
+        input_str = input_entry_or_string
+        if 'b64' in file_type:
+            input_str = base64.b64decode(input_entry_or_string)
+        return json.loads(input_str)
+    else:
+        res = demisto.getFilePath(input_entry_or_string)
+        if not res:
+            return_error("Entry {} not found".format(input_entry_or_string))
+        file_path = res['path']
+        with open(file_path, 'r') as f:
+            return json.load(f)
+
+
+def get_file_entry_id(file_name):
+    file_name = file_name.strip()
+    res = demisto.dt(demisto.context(), "File(val.Name == '%s')" % file_name)
+    if not res or len(res) == 0:
+        return_error("Cannot find file entry id in context by filename: " + file_name)
+    if type(res) is list:
+        res = res[0]
+    return res['EntryID']
+
+
+def read_files_by_name(file_names):
+    file_names = file_names.split(",")
+    file_names = [f for f in file_names if f]
+    data = []
+    for file_name in file_names:
+        data += read_file(get_file_entry_id(file_name), "json")
+    return data
+
+
+def get_data_with_mapped_label(data, labels_mapping, tag_field):
+    new_data = []
+    exist_labels_counter = defaultdict(int)
+    missing_labels_counter = defaultdict(int)
+    for row in data:
+        original_label = row[tag_field]
+        if labels_mapping == ALL_LABELS:
+            row[tag_field] = canonize_label(original_label)
+        else:
+            if original_label in labels_mapping:
+                row[tag_field] = labels_mapping[original_label]
+            else:
+                missing_labels_counter[original_label] += 1
+                continue
+        exist_labels_counter[original_label] += 1
+        new_data.append(row)
+    return new_data, dict(exist_labels_counter), dict(missing_labels_counter)
+
+
+def store_model_in_demisto(model_name, model_override, train_text_data, train_tag_data, confusion_matrix):
+    model = demisto_ml.train_text_classifier(train_text_data, train_tag_data, True)
+    model_data = demisto_ml.encode_model(model)
+    model_labels = demisto_ml.get_model_labels(model)
+
+    res = demisto.executeCommand('createMLModel', {'modelData': model_data,
+                                                   'modelName': model_name,
+                                                   'modelLabels': model_labels,
+                                                   'modelOverride': model_override})
+    if is_error(res):
+        return_error(get_error(res))
+
+    res = demisto.executeCommand('evaluateMLModel',
+                                 {'modelConfusionMatrix': confusion_matrix.to_json(),
+                                  'modelName': model_name})
+    if is_error(res):
+        return_error(get_error(res))
+
+
+def evaluate_model(train_text_data, train_tag_data, target_accuracy, max_samples_below_threshold):
+    confusion_matrix, report, cut, confusion_matrix_cut, report_cut \
+        = demisto_ml.evaluate_text_model(train_text_data,
+                                         train_tag_data,
+                                         target_precision_recall=target_accuracy)
+    model_evaluation_success = False
+    if report['macro avg']['precision'] >= target_accuracy and report['macro avg']['recall'] >= target_accuracy:
+        model_evaluation_success = True
+    else:
+        if report_cut and report_cut['belowThresholdRatio'] <= max_samples_below_threshold:
+            if report_cut['macro avg']['precision'] >= target_accuracy \
+                    and report_cut['macro avg']['recall'] >= target_accuracy:
+                model_evaluation_success = True
+
+    if not model_evaluation_success:
+        return_error("Model target accuracy %.2f is below %.2f" % (report['accuracy'], target_accuracy))
+
+    human_readable = get_hr_for_scores("Model Evaluation", confusion_matrix, report)
+    if cut > 0.5 and confusion_matrix_cut is not None:
+        human_readable += "\n"
+        human_readable += "### Found optimal probability  %.2f threshold for target accuracy %.2f " % (cut, target_accuracy)
+        human_readable += "Samples below threshold: %d (%.2f%%)\n" % (report_cut['belowThreshold'],
+                                                          report_cut['belowThresholdRatio'] * 100)
+        human_readable += get_hr_for_scores("Model Evaluation probability >= %.2f" % cut, confusion_matrix_cut, report_cut)
+    return human_readable, confusion_matrix, report
+
+
+def find_keywords(data, tag_field, text_field, min_score):
+    keywords = demisto_ml.get_keywords_for_labels(data, tag_field, text_field)
+    human_readable = "# Keywords per category\n"
+    for category, scores in keywords.items():
+        sorted_scores = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+        table_items = [{"Word": word, "Score": score} for word, score in sorted_scores if score >= min_score]
+        human_readable += tableToMarkdown(category, table_items, ["Word", "Score"])
+    demisto.results({
+        'Type': entryTypes['note'],
+        'Contents': keywords,
+        'ContentsFormat': formats['json'],
+        'HumanReadable': human_readable,
+        'HumanReadableFormat': formats['markdown'],
+    })
+
+
+def main():
+    input = demisto.args()['input']
+    input_type = demisto.args()['inputType']
+    model_name = demisto.args()['modelName']
+    store_model = demisto.args()['storeModel'] == 'true'
+    model_override = demisto.args()['overrideExistingModel'] == 'true'
+    target_accuracy = float(demisto.args()['targetAccuracy'])
+    max_samples_below_threshold = float(demisto.args()['maxBelowThreshold'])
+    text_field = demisto.args()['textField']
+    tag_field = demisto.args()['tagField']
+    labels_mapping = get_phishing_map_labels(demisto.args()['phishingLabels'])
+    keyword_min_score = float(demisto.args()['keywordMinScore'])
+
+    if input_type == "filename":
+        data = read_files_by_name(input)
+    else:
+        data = read_file(input, input_type)
+
+    data, exist_labels_counter, missing_labels_counter = get_data_with_mapped_label(data, labels_mapping, tag_field)
+    if len(missing_labels_counter) > 0:
+        human_readable = tableToMarkdown("Skip labels - did not match any of specified labels", exist_labels_counter)
+        entry = {
+            'Type': entryTypes['note'],
+            'Contents': missing_labels_counter,
+            'ContentsFormat': formats['json'],
+            'HumanReadable': human_readable,
+            'HumanReadableFormat': formats['markdown'],
+        }
+        demisto.results(entry)
+
+    if len(exist_labels_counter) > 0:
+        exist_labels_counter_mapped = {}
+        for label, count in exist_labels_counter.items():
+            mapped_label = labels_mapping[label] if isinstance(labels_mapping, dict) else label
+            if mapped_label != label:
+                label = "%s -> %s" % (label, mapped_label)
+            exist_labels_counter_mapped[label] = count
+        human_readable = tableToMarkdown("Found labels", exist_labels_counter_mapped)
+        entry = {
+            'Type': entryTypes['note'],
+            'Contents': exist_labels_counter,
+            'ContentsFormat': formats['json'],
+            'HumanReadable': human_readable,
+            'HumanReadableFormat': formats['markdown'],
+        }
+        demisto.results(entry)
+
+    train_tag_data = map(lambda x: x[tag_field], data)
+    train_text_data = map(lambda x: x[text_field], data)
+    if len(train_text_data) != len(train_tag_data):
+        return_error("Error: data and tag data are different length")
+
+    # print important words for each category
+    find_keywords(data, tag_field, text_field, keyword_min_score)
+
+    # evaluate model
+    human_readable, confusion_matrix, report = evaluate_model(train_text_data,
+                                                  train_tag_data,
+                                                  target_accuracy,
+                                                  max_samples_below_threshold)
+
+    # store model
+    if store_model:
+        store_model_in_demisto(model_name, model_override, train_text_data, train_tag_data, confusion_matrix)
+        human_readable += "\nDone training on {} samples model stored successfully".format(len(train_text_data))
+    else:
+        human_readable += "\n\nSkip storing model"
+
+    result_entry = {
+        'Type': entryTypes['note'],
+        'Contents': {'scores': report, 'confusion_matrix': confusion_matrix.to_dict()},
+        'ContentsFormat': formats['json'],
+        'HumanReadable': human_readable,
+        'HumanReadableFormat': formats['markdown'],
+        'EntryContext': {
+            'DBotPhishingClassifier': {
+                'ModelName': model_name,
+                'EvaluationScores': report,
+                'ConfusionMatrix': confusion_matrix.to_dict()
+            }
+        }
+    }
+    demisto.results(result_entry)
+
+
+if __name__ in ['__builtin__', '__main__']:
+    main()
---
+++ Scripts/DBotTrainTextClassifierV2/dbot_train_text_classifier_test.py
@@ -0,0 +1,47 @@
+from CommonServerPython import *
+from DBotTrainTextClassifierV2 import get_phishing_map_labels, read_file, read_files_by_name, get_data_with_mapped_label, find_keywords, ALL_LABELS
+
+
+def test_get_phishing_map_labels(mocker):
+    mapping = get_phishing_map_labels("Phishing:malicious , malware : malicious, spam")
+    assert mapping['Phishing'] == "malicious"
+    assert mapping['malware'] == "malicious"
+    assert mapping['spam'] == "spam"
+
+    mapping = get_phishing_map_labels(ALL_LABELS)
+    assert mapping == ALL_LABELS
+
+
+def test_read_file(mocker):
+    mocker.patch.object(demisto, 'getFilePath', return_value={'path': 'input_json_file_test'})
+    obj = read_file('231342@343', 'json')
+    assert len(obj) >= 1
+    with open('input_json_file_test', 'r') as f:
+        obj = read_file(f.read(), 'json_string')
+        assert len(obj) >= 1
+    with open('input_json_file_test', 'r') as f:
+        b64_input = base64.b64encode(f.read())
+        obj = read_file(b64_input, 'json_b64_string')
+        assert len(obj) >= 1
+
+
+def test_read_files_by_name(mocker):
+    mocker.patch.object(demisto, 'getFilePath', return_value={'path': 'input_json_file_test'})
+    mocker.patch.object(demisto, 'dt', return_value={'EntryID': 'ads@Asd2'})
+    data = read_files_by_name("file1,file2")
+    assert len(data) == 4
+    data = read_files_by_name("file1,")
+    assert len(data) == 2
+
+
+def test_get_data_with_mapped_label(mocker):
+    mapping = get_phishing_map_labels("Phishing:malicious , malware : malicious, spam")
+    data = [{"tag": "Phishing"}, {"tag": "malware"}, {"tag": "spam"}, {"tag": "not"}, {"tag": "not"}]
+    new_data, exist_labels_counter, missing_labels_counter = get_data_with_mapped_label(data, mapping, "tag")
+    assert new_data[0]['tag'] == 'malicious'
+    assert new_data[2]['tag'] == 'spam'
+    assert exist_labels_counter['Phishing'] == 1
+    assert exist_labels_counter['malware'] == 1
+    assert exist_labels_counter['spam'] == 1
+    assert missing_labels_counter['not'] == 2
+
