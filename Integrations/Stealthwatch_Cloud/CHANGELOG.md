9048beaaa4c97396d4873fe0790a4d7049987084
olichter <olichter@paloaltonetworks.com>
2019-10-22 17:41:50 +0300

    Fetch incidents fix

--- Integrations/Stealthwatch_Cloud/CHANGELOG.md
+++ Integrations/Stealthwatch_Cloud/CHANGELOG.md
@@ -1,5 +1,5 @@
 ## [Unreleased]
-
+- Fetch issue fixed. **First fetch after content update** might create duplicate incidents.
 
 ## [19.8.2] - 2019-08-22
 - 
--- Integrations/Stealthwatch_Cloud/Stealthwatch_Cloud.py
+++ Integrations/Stealthwatch_Cloud/Stealthwatch_Cloud.py
@@ -487,34 +487,39 @@ def list_sessions_command():
 
 
 def fetch_incidents():
+    date_format = "%Y-%m-%dT%H:%M:%SZ"
 
     list_params = {
-        "order_by": 'newest',
+        "ordering": 'created',
         "limit": 100
     }
     final_alerts = []
+    last_fetch_string = demisto.getLastRun().get('last_fetch_time', None)
 
-    known_ids = demisto.getLastRun().get('ids', None)
-    if known_ids is None or not known_ids:
-        known_ids = []
+    if last_fetch_string is None or not last_fetch_string:
+        now = datetime.now()
+        last_fetch = now - timedelta(days=20)
+    else:
+        last_fetch = parse_date_string(last_fetch_string)
 
+    # Coudn't find a way to sort descending so looking for last offset of 100 alerts
+    alerts_response = list_alerts(list_params)
+    num_alerts = alerts_response.get('meta', {'total_count': 100}).get('total_count')
+    offset = 0 if num_alerts < 100 else num_alerts - 100
+    list_params['offset'] = offset
     alerts_response = list_alerts(list_params)
 
     alerts_data = alerts_response.get('objects', None)
-
+    max_fetch_time = last_fetch_string if last_fetch_string else now.strftime(date_format)
     for alert in alerts_data:
-        current_alert_id = alert.get('id')
-        if current_alert_id not in known_ids:
+        created = alert.get('created')
+        if parse_date_string(created) > last_fetch:
             incident_from_alert = create_incident_data_from_alert(alert)
             final_alerts.append(incident_from_alert)
-
-            # maintaining queue of 100 last seen alert ids
-            if len(known_ids) >= 100:
-                known_ids.pop(0)
-            known_ids.append(current_alert_id)
+            max_fetch_time = created
 
     demisto.setLastRun({
-        'ids': known_ids
+        'last_fetch_time': max_fetch_time
     })
     demisto.incidents(final_alerts)
 
