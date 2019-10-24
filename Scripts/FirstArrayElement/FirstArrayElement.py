cd644eebc9ddad745f845cf31d6a0e0a5f022ab6
Tim Underhay <tim.underhay@demisto.com>
2019-10-24 11:45:51 -0400

    Unit tests provided

--- Scripts/FirstArrayElement/FirstArrayElement.py
+++ Scripts/FirstArrayElement/FirstArrayElement.py
@@ -1,8 +1,14 @@
 import demistomock as demisto
 
-value = demisto.args()['value']
 
-if isinstance(value, list) and value:
-    value = value[0]
+def main():
+    value = demisto.args()['value']
 
-demisto.results(value)
+    if isinstance(value, list) and value:
+        value = value[0]
+
+    demisto.results(value)
+
+
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
--- Scripts/LastArrayElement/LastArrayElement.py
+++ Scripts/LastArrayElement/LastArrayElement.py
@@ -1,8 +1,14 @@
 import demistomock as demisto
 
-value = demisto.args()['value']
 
-if type(value) is list and len(value) > 0:
-    value = value[-1]
+def main():
+    value = demisto.args()['value']
 
-demisto.results(value)
+    if type(value) is list and len(value) > 0:
+        value = value[-1]
+
+    demisto.results(value)
+
+
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
