c403671669e8e2086babd9f097b342da0ae9cd28
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2019-10-30 00:38:41 +0900

    Update Panorama.py

--- Integrations/Panorama/Panorama.py
+++ Integrations/Panorama/Panorama.py
@@ -302,6 +302,30 @@ def prepare_security_rule_params(api_action: str = None, rulename: str = None, s
 ''' FUNCTIONS'''
 
 
+
+def panorama_get_major_version():
+    major_ver = 0
+    context = demisto.getIntegrationContext()
+    if not context:
+        context = {}
+    else:
+        major_ver = context.get('PANOS_MAJOR_VER', 0)
+    
+    if major_ver == 0:
+        # Get PAN-OS Version
+        params = {
+            'type': 'version',
+            'key': API_KEY
+        }
+        result = http_request(URL, 'GET', params=params)
+        version = result['response']['result']['sw-version']
+        major_ver = int(version.split('.')[0])
+        
+        context['PANOS_MAJOR_VER'] = major_ver
+        demisto.setIntegrationContext(context)
+    return  major_ver
+
+
 def panorama_test():
     """
     test module
@@ -1802,11 +1826,17 @@ def panorama_get_custom_url_category_command():
 
 @logger
 def panorama_create_custom_url_category(custom_url_category_name: str, sites, description: str = None):
+    major_ver = panorama_get_major_version()
+    if major_ver >= 9:
+        element = add_argument(description, 'description', False) + add_argument_list(sites, 'list', True) + add_argument("URL List", 'type', False)
+    else:
+        element = add_argument(description, 'description', False) + add_argument_list(sites, 'list', True)
+    
     params = {
         'action': 'set',
         'type': 'config',
         'xpath': XPATH_OBJECTS + "profiles/custom-url-category/entry[@name='" + custom_url_category_name + "']",
-        'element': add_argument(description, 'description', False) + add_argument_list(sites, 'list', True),
+        'element': element,
         'key': API_KEY
     }
     result = http_request(
@@ -1893,13 +1923,21 @@ def panorama_delete_custom_url_category_command():
 
 @logger
 def panorama_edit_custom_url_category(custom_url_category_name, sites, description=None):
+    
+    description = add_argument(description, 'description', False)
+    list = add_argument_list(sites, 'list', True)
+    major_ver = panorama_get_major_version()
+    if major_ver >= 9:
+        type = add_argument("URL List", 'type', False)
+        element = f"<entry name='{custom_url_category_name}'>{description}{list}{type}</entry>"
+    else:
+        element = f"<entry name='{custom_url_category_name}'>{description}{list}</entry>"
+    
     params = {
         'action': 'edit',
         'type': 'config',
         'xpath': XPATH_OBJECTS + "profiles/custom-url-category/entry[@name='" + custom_url_category_name + "']",
-        'element': "<entry name='" + custom_url_category_name + "'>"
-                   + add_argument(description, 'description', False)
-                   + add_argument_list(sites, 'list', True) + "</entry>",
+        'element': element,
         'key': API_KEY
     }
     result = http_request(
@@ -2682,7 +2720,7 @@ def panorama_custom_block_rule_command():
             result = http_request(URL, 'POST', params=params)
         custom_block_output['IP'] = object_value
 
-    elif object_type == 'address-group' or 'edl':
+    elif object_type in ('address-group','edl'):
         if block_source:
             params = prepare_security_rule_params(api_action='set', action='drop', source=object_value,
                                                   destination='any', rulename=rulename + '-from', target=target,
