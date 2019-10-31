ad2aa2f1e2f113095f410d18932a9b79288f2562
Masahiko Inoue <54964121+spearmin10@users.noreply.github.com>
2019-10-31 11:06:42 +0900

    Update Panorama.py

--- Integrations/Panorama/Panorama.py
+++ Integrations/Panorama/Panorama.py
@@ -1828,11 +1828,11 @@ def panorama_create_custom_url_category(custom_url_category_name: str, sites, de
     major_ver = panorama_get_major_version()
     if major_ver >= 9:
         element = add_argument(description, 'description', False) + \
-                     add_argument_list(sites, 'list', True) + \
-                     add_argument("URL List", 'type', False)
+                add_argument_list(sites, 'list', True) + \
+                add_argument("URL List", 'type', False)
     else:
         element = add_argument(description, 'description', False) + \
-                     add_argument_list(sites, 'list', True)
+                add_argument_list(sites, 'list', True)
 
     params = {
         'action': 'set',
