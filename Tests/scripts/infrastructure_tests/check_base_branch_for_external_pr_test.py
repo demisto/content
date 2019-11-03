1d10366982b7390b907eb1ed907bf1c015569b91
guyfreund <gfreund@paloaltonetworks.com>
2019-11-03 20:48:43 +0200

    check base branch isn't master in external PR

---
+++ Tests/scripts/infrastructure_tests/check_base_branch_for_external_pr_test.py
@@ -0,0 +1,26 @@
+from pytest import raises
+
+bad_response = {
+    'base': {
+        'ref': 'master'
+    }
+}
+
+good_response = {
+    'base': {
+        'ref': 'not_master'
+    }
+}
+
+
+def check_base_branch_test(requests_mock):
+    from Tests.scripts.check_base_branch_for_external_pr import check_base_branch
+    url = f'https://api.github.com/repos/demisto/content/pulls/528'
+    requests_mock.get(url, json=bad_response)
+    with raises(SystemExit) as se:
+        check_base_branch(528)
+        assert se.value.code == 1
+    requests_mock.get(url, json=good_response)
+    with raises(SystemExit) as se:
+        check_base_branch(528)
+        assert se.value.code == 0
