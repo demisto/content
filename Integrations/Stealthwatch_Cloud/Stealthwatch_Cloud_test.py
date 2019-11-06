bf735742d5778fdc3e5169958d3a927e064fb110
olichter <olichter@paloaltonetworks.com>
2019-11-06 13:22:31 +0200

    v3

--- Integrations/Stealthwatch_Cloud/Stealthwatch_Cloud_test.py
+++ Integrations/Stealthwatch_Cloud/Stealthwatch_Cloud_test.py
@@ -43,3 +43,27 @@ def test_fetch(mocker, requests_mock):
     assert demisto.setLastRun.call_args[0][0].get('last_fetch_time') == '2019-10-22T15:00:00Z'
     # No new incidents were made
     assert len(demisto.incidents.call_args[0][0]) == 0
+
+
+def test_fetch_switch_to_new_fetch(mocker, requests_mock):
+    mocker.patch.object(demisto, 'params', return_value=INTEGRATION_CONNECTION)
+    mocker.patch.object(demisto, 'getLastRun', return_value={'ids': [6124]})
+    requests_mock.get('http://example.com/api/v3/alerts/alert/?ordering=created&limit=100', json=get_fetch_data())
+    from Stealthwatch_Cloud import fetch_incidents
+    mocker.patch.object(demisto, 'setLastRun')
+    mocker.patch.object(demisto, 'incidents')
+    fetch_incidents()
+
+    # only returned the new incident
+    assert len(demisto.incidents.call_args[0][0]) == 1
+    # now keeps only last_fetch_time and not ids
+    assert demisto.setLastRun.call_args[0][0].get('ids') == None
+    assert demisto.setLastRun.call_args[0][0].get('last_fetch_time') != None
+
+    mocker.patch.object(demisto, 'getLastRun', return_value={'ids': [6122]})
+    fetch_incidents()
+    assert len(demisto.incidents.call_args[0][0]) == 2
+
+
+
+
