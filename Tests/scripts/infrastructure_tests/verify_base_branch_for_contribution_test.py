327f61e157c0003d07122854004ceded8e7e65a4
guyfreund <gfreund@paloaltonetworks.com>
2019-11-07 20:23:12 +0200

    unit tests back to normal

--- Tests/scripts/infrastructure_tests/verify_base_branch_for_contribution_test.py
+++ Tests/scripts/infrastructure_tests/verify_base_branch_for_contribution_test.py
@@ -13,14 +13,18 @@ good_response = {
 }
 
 
-def test_check_base_branch(requests_mock):
+def test_check_base_branch(requests_mock, capsys):
     url = 'https://api.github.com/repos/demisto/content/pulls/528'
     from Tests.scripts.verify_base_branch_for_contribution import check_base_branch
     requests_mock.get(url, json=bad_response)
     with pytest.raises(SystemExit) as se:
         check_base_branch('528')
+        out, _ = capsys.readouterror()
+        assert 'Cannot merge a contribution directly to master, the pull request reviewer will handle that soon.' in out
         assert se.value.code == 1
     requests_mock.get(url, json=good_response)
     with pytest.raises(SystemExit) as se:
         check_base_branch('528')
+        out, _ = capsys.readouterror()
+        assert 'Verified pull request #528 base branch successfully.' in out
         assert se.value.code == 0
