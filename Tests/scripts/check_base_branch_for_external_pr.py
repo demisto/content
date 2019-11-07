7477dcc59c1b0d3797118901992afd2f54c57d1d
guyfreund <gfreund@paloaltonetworks.com>
2019-11-07 17:35:03 +0200

    CR fixes & trying tests

--- Tests/scripts/check_base_branch_for_external_pr.py
+++ Tests/scripts/check_base_branch_for_external_pr.py
@@ -1,65 +1,57 @@
 import requests
 import sys
+import re
 from Tests.test_utils import print_error, print_warning, print_color, LOG_COLORS
+from Tests.scripts.constants import EXTERNAL_PR_REGEX
 
 
-def get_base_branch(pr_num):
-    """
-    Fetches the base branch name of PR num {pr_num}
-    :param pr_num: The PR number
-    :return: The name of the base branch if exists
+def get_base_branch(pr_num) -> str:
+    """Fetches the base branch name of PR num {pr_num}
+
+    Args:
+        pr_num (string): The string representation of the pr number
+
+    Returns:
+        string. The name of the base branch of the pr if succeeds, '' otherwise.
     """
 
     # Disable insecure warnings
     requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
 
-    try:
-        url = 'https://api.github.com/repos/demisto/content/pulls/{}'.format(pr_num)
-        res = requests.get(url, verify=False)
-        if res.status_code != 200:
-            # If we didn't succeed to fetch the pr maybe it doesn't exist - then we don't want the build to fail
-            print_warning('Unable to fetch PR num {}'.format(pr_num))
-            return ''
-        response = res.json()
-        # Seems like GitHub API return a dict if it's only one PR, but it doesn't mentioned in their API
-        if response and isinstance(response, dict):
-            base = response.get('base', {})
-            base_branch = base.get('ref')
-            if base_branch:
-                return base_branch
-        # GitHub usually returns a list of PRs
-        elif response and isinstance(response, list) and len(response) == 1:
-            pr = response[0]
-            base = pr.get('base', {})
-            base_branch = base.get('ref')
-            if base_branch:
-                return base_branch
-    except requests.exceptions.ConnectionError:
-        # If we didn't succeed to fetch the pr maybe it doesn't exist - then we don't want the build to fail
-        print_warning('Unable to fetch PR num {}'.format(pr_num))
-        return ''
+    url = f'https://api.github.com/repos/demisto/content/pulls/{pr_num}'
 
-    return ''
+    try:
+        res = requests.get(url, verify=False)
+        res.raise_for_status()
+        pr = res.json()
+        if pr and isinstance(pr, list) and len(pr) == 1:
+            # github usually returns a list of PRs, if not pr is a dict
+            pr = pr[0]
+        return pr.get('base', {}).get('ref', '')
+
+    except (requests.exceptions.HTTPError, ValueError) as e:
+        # If we didn't succeed to fetch pr for any http error / res.json() we raise an error
+        # then we don't want the build to fail
+        print_warning(f'Unable to fetch pull request #{pr_num}.\nError: {str(e)}')
+        return ''
 
 
 def check_base_branch(pr_num):
-    print_color('Starting to fetch the base branch of PR num {}'.format(pr_num), LOG_COLORS.GREEN)
+    print_color(f'Fetching the base branch of pull request #{pr_num}.', LOG_COLORS.NATIVE)
     base_branch = get_base_branch(pr_num)
-    print_color('Finished to fetch the base branch of PR num {}'.format(pr_num), LOG_COLORS.GREEN)
     if base_branch == 'master':
-        print_error("You cannot merge into master when creating an external PR.")
+        print_error("Cannot merge a contribution directly to master, the pull request reviewer will handle that soon.")
         sys.exit(1)
     else:
-        print_color('Base branch of PR num {} is not master - Great!'.format(pr_num), LOG_COLORS.GREEN)
+        print_color(f'Verified pull request #{pr_num} base branch successfully.', LOG_COLORS.GREEN)
         sys.exit(0)
 
 
-def main():
-    circle_branch = sys.argv[1]
-    # If we run this script the circle branch of format "pull/[0-9]+"
-    pr_num = circle_branch.split('pull/')[1]
-    check_base_branch(pr_num)
-
-
 if __name__ == '__main__':
-    main()
+    circle_branch = sys.argv[1]
+    if re.match(EXTERNAL_PR_REGEX, circle_branch, re.IGNORECASE):
+        # we expect only 1 sequence of numbers in the circle branch name
+        pr_number = re.findall(r'\d+', circle_branch)[0]
+        check_base_branch(pr_number)
+    else:
+        print_warning('Unable to fetch pull request.')
--- Tests/scripts/infrastructure_tests/check_base_branch_for_external_pr_test.py
+++ Tests/scripts/infrastructure_tests/check_base_branch_for_external_pr_test.py
@@ -1,5 +1,4 @@
 import pytest
-import requests_mock
 
 bad_response = {
     'base': {
@@ -14,15 +13,17 @@ good_response = {
 }
 
 
-def test_check_base_branch():
-    with requests_mock.Mocker() as m:
-        from Tests.scripts.check_base_branch_for_external_pr import check_base_branch
-        url = 'https://api.github.com/repos/demisto/content/pulls/528'
-        m.get(url, json=bad_response)
-        with pytest.raises(SystemExit) as se:
-            check_base_branch(528)
-            assert se.value.code == 1
-        m.get(url, json=good_response)
-        with pytest.raises(SystemExit) as se:
-            check_base_branch(528)
-            assert se.value.code == 0
+def test_check_base_branch(requests_mock):
+    url = 'https://api.github.com/repos/demisto/content/pulls/528'
+    from Tests.scripts.check_base_branch_for_external_pr import check_base_branch
+    requests_mock.get(url, json=bad_response)
+    with pytest.raises(SystemExit) as se:
+        check_base_branch(528)
+        assert 'p' == 'k'
+        assert se.value.code == 1
+        # m.get(url, json=good_response)
+        # with pytest.raises(SystemExit) as se:
+        #     out, _ = capsys.readouterr()
+        #     assert 'Verified pull request #528 base branch successfully.' in out
+        #     check_base_branch(528)
+        #     assert se.value.code == 0
