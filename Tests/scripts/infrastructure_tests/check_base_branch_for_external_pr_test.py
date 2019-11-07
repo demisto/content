6aa0fb1dfe88cd2ad25cd11ffe8725aadc91b02a
guyfreund <gfreund@paloaltonetworks.com>
2019-11-07 10:18:10 +0200

    fixed test

--- Tests/scripts/infrastructure_tests/check_base_branch_for_external_pr_test.py
+++ Tests/scripts/infrastructure_tests/check_base_branch_for_external_pr_test.py
@@ -1,4 +1,5 @@
-from pytest import raises
+import pytest
+import requests_mock
 
 bad_response = {
     'base': {
@@ -13,14 +14,15 @@ good_response = {
 }
 
 
-def test_check_base_branch(requests_mock):
-    from Tests.scripts.check_base_branch_for_external_pr import check_base_branch
-    url = 'https://api.github.com/repos/demisto/content/pulls/528'
-    requests_mock.get(url, json=bad_response)
-    with raises(SystemExit) as se:
-        check_base_branch(528)
-        assert se.value.code == 1
-    requests_mock.get(url, json=good_response)
-    with raises(SystemExit) as se:
-        check_base_branch(528)
-        assert se.value.code == 0
+def test_check_base_branch():
+    with requests_mock.Mocker() as m:
+        from Tests.scripts.check_base_branch_for_external_pr import check_base_branch
+        url = 'https://api.github.com/repos/demisto/content/pulls/528'
+        m.get(url, json=bad_response)
+        with pytest.raises(SystemExit) as se:
+            check_base_branch(528)
+            assert se.value.code == 1
+        m.get(url, json=good_response)
+        with pytest.raises(SystemExit) as se:
+            check_base_branch(528)
+            assert se.value.code == 0
