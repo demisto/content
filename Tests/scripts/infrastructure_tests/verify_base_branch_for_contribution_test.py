f72982ec73b711dee9ab261d7d995633aadf50fa
guyfreund <gfreund@paloaltonetworks.com>
2019-11-07 19:50:02 +0200

    test not working

--- Tests/scripts/infrastructure_tests/verify_base_branch_for_contribution_test.py
+++ Tests/scripts/infrastructure_tests/verify_base_branch_for_contribution_test.py
@@ -16,13 +16,11 @@ good_response = {
 def test_check_base_branch(requests_mock, mocker):
     url = 'https://api.github.com/repos/demisto/content/pulls/528'
     from Tests.scripts.verify_base_branch_for_contribution import check_base_branch
-    # with pytest.raises(SystemExit) as se:
-    #     check_base_branch(528)
-    #     assert 'p' == 'k'
-    #     assert se.value.code == 1
-    # m.get(url, json=good_response)
-    # with pytest.raises(SystemExit) as se:
-    #     out, _ = capsys.readouterr()
-    #     assert 'Verified pull request #528 base branch successfully.' in out
-    #     check_base_branch(528)
-    #     assert se.value.code == 0
+    requests_mock.get(url, json=bad_response)
+    with pytest.raises(SystemExit) as se:
+        check_base_branch(528)
+        assert se.value.code == 1
+    requests_mock.get(url, json=good_response)
+    with pytest.raises(SystemExit) as se:
+        check_base_branch(528)
+        assert se.value.code == 0
