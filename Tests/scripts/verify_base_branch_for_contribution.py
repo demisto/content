0a698c86d99df4422b4bff706a09439978dcff80
guyfreund <gfreund@paloaltonetworks.com>
2019-11-07 19:48:44 +0200

    returned code to py2, changed files names

--- Tests/scripts/verify_base_branch_for_contribution.py
+++ Tests/scripts/verify_base_branch_for_contribution.py
@@ -5,7 +5,7 @@ from Tests.test_utils import print_error, print_warning, print_color, LOG_COLORS
 from Tests.scripts.constants import EXTERNAL_PR_REGEX
 
 
-def get_base_branch(pr_num) -> str:
+def get_base_branch(pr_num):
     """Fetches the base branch name of PR num {pr_num}
 
     Args:
@@ -18,7 +18,7 @@ def get_base_branch(pr_num) -> str:
     # Disable insecure warnings
     requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
 
-    url = f'https://api.github.com/repos/demisto/content/pulls/{pr_num}'
+    url = 'https://api.github.com/repos/demisto/content/pulls/{}'.format(pr_num)
 
     try:
         res = requests.get(url, verify=False)
@@ -32,18 +32,18 @@ def get_base_branch(pr_num) -> str:
     except (requests.exceptions.HTTPError, ValueError) as e:
         # If we didn't succeed to fetch pr for any http error / res.json() we raise an error
         # then we don't want the build to fail
-        print_warning(f'Unable to fetch pull request #{pr_num}.\nError: {str(e)}')
+        print_warning('Unable to fetch pull request #{0}.\nError: {1}'.format(pr_num, str(e)))
         return ''
 
 
 def check_base_branch(pr_num):
-    print_color(f'Fetching the base branch of pull request #{pr_num}.', LOG_COLORS.NATIVE)
+    print_color('Fetching the base branch of pull request #{}.'.format(pr_num), LOG_COLORS.NATIVE)
     base_branch = get_base_branch(pr_num)
     if base_branch == 'master':
         print_error("Cannot merge a contribution directly to master, the pull request reviewer will handle that soon.")
         sys.exit(1)
     else:
-        print_color(f'Verified pull request #{pr_num} base branch successfully.', LOG_COLORS.GREEN)
+        print_color('Verified pull request #{} base branch successfully.'.format(pr_num), LOG_COLORS.GREEN)
         sys.exit(0)
 
 
