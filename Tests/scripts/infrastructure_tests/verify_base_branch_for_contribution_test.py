862c29a0ad565c470b7f7999ee6037996ce17d69
guyfreund <gfreund@paloaltonetworks.com>
2019-11-10 15:57:08 +0200

    edited CR comments

--- Tests/scripts/infrastructure_tests/verify_base_branch_for_contribution_test.py
+++ Tests/scripts/infrastructure_tests/verify_base_branch_for_contribution_test.py
@@ -13,14 +13,14 @@ good_response = {
 }
 
 
-def test_check_base_branch(requests_mock, capsys):
+def test_check_base_branch(requests_mock):
     url = 'https://api.github.com/repos/demisto/content/pulls/528'
     from Tests.scripts.verify_base_branch_for_contribution import verify_base_branch
     requests_mock.get(url, json=bad_response)
-    verify_base_branch('528')
-    out, _ = capsys.readouterror()
-    assert 'Cannot merge a contribution directly to master, the pull request reviewer will handle that soon.' in out
+    msg, is_valid = verify_base_branch('528')
+    assert is_valid is False
+    assert 'Cannot merge a contribution directly to master, the pull request reviewer will handle that soon.' == msg
     requests_mock.get(url, json=good_response)
-    verify_base_branch('528')
-    out, _ = capsys.readouterror()
-    assert 'Verified pull request #528 base branch successfully.' in out
+    msg, is_valid = verify_base_branch('528')
+    assert is_valid is True
+    assert 'Verified pull request #528 base branch successfully.' == msg
