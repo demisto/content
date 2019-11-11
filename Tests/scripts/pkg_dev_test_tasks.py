dc7d2f28af21207b8f123e38d3dc67c4f49897f9
Itay Keren <ikeren@paloaltonetworks.com>
2019-11-11 09:47:03 +0200

    PowerShell content support (#4552)
    * powershell commonserver initial commit
    
    * add powershell common server docs build to script
    
    * added powershell support to id set creation
    
    * fix flake8 in constants.py
    
    * added ps support for files validation
    
    * no test
    
    * fix dir name
    
    * add ps support to package creator and to integration scheme
    
    * handle powershell in pkg dev test task
    
    * change script type to powershell
    
    * refactor Powershell to PowerShell
    
    * refactor Powershell to PowerShell
    
    * refactor Powershell to PowerShell
    
    * Update Documentation/common_server_docs.py
    
    Co-Authored-By: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
    
    * add ps integration regex to skipped shemas and todo ps linting
    
    * add ps integration regex to skipped shemas and todo ps linting
    
    * change ha detonate file integration to ha
    
    * Apply suggestions from code review
    
    * revert vxstream in conf.json
    
    * Update Scripts/CommonServerPowerShell/CommonServerPowerShell.ps1
    
    Co-Authored-By: Yarden Sade <ysade@paloaltonetworks.com>
    
    * Update Scripts/CommonServerPowerShell/CommonServerPowerShell.ps1
    
    Co-Authored-By: Yarden Sade <ysade@paloaltonetworks.com>

--- Tests/scripts/pkg_dev_test_tasks.py
+++ Tests/scripts/pkg_dev_test_tasks.py
@@ -304,6 +304,9 @@ Will lookup up what docker image to use and will setup the dev dependencies and 
         script_obj = script_obj.get('script')
     script_type = script_obj.get('type')
     if script_type != 'python':
+        if script_type == 'powershell':
+            # TODO powershell linting
+            return 0
         print('Script is not of type "python". Found type: {}. Nothing to do.'.format(script_type))
         return 1
     dockers = get_docker_images(script_obj)
