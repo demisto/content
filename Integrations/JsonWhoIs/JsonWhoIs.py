b1c72b5be86881a6ae2da964c6eaf5f2aa45b7cf
Gal Rabin <53563021+GalRabin@users.noreply.github.com>
2019-11-12 17:19:03 +0200

    Update Integrations/JsonWhoIs/JsonWhoIs.py
    Co-Authored-By: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>

--- Integrations/JsonWhoIs/JsonWhoIs.py
+++ Integrations/JsonWhoIs/JsonWhoIs.py
@@ -176,5 +176,5 @@ def main():
 
 
 # python2 uses __builtin__ python3 uses builtins
-if __name__ == "__builtin__" or __name__ == "builtins":
+if __name__ == ('__builtin__', 'builtins'):
     main()
