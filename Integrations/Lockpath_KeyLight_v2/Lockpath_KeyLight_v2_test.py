9762891e04d4a73c36971edf3c3f9deccf7e6c93
olichter <olichter@paloaltonetworks.com>
2019-11-13 16:22:30 +0200

    image + descritpion + fields update

--- Integrations/Lockpath_KeyLight_v2/Lockpath_KeyLight_v2_test.py
+++ Integrations/Lockpath_KeyLight_v2/Lockpath_KeyLight_v2_test.py
@@ -1,2 +1,19 @@
+import pytest
+
 def test_filter_creator():
     from Lockpath_KeyLight_v2 import create_filter
+    filt = create_filter('Starts With', 'Blue', '3881')
+    check = {
+        "FieldPath": [
+            3881
+        ],
+        "FilterType": "3",
+        "Value": "Blue"
+    }
+
+    assert filt == check
+
+    with pytest.raises(ValueError, match='Filter Type is invalid.'):
+        create_filter('>=', '5', '3881')
+
+
