027fb808c5a988933fb14bd486ef59a1edc4da86
Shelly Berman <sberman@paloaltonetworks.com>
2019-11-13 17:04:03 +0200

    Added some unittest

--- Tests/scripts/hook_validations/tests/id_set_update_test.py
+++ Tests/scripts/hook_validations/tests/id_set_update_test.py
@@ -1,5 +1,6 @@
-import pytest
 
+import pytest
+import unittest
 from Tests.scripts.update_id_set import has_duplicate, get_integration_data, get_script_data
 
 mocked_data = [
@@ -47,22 +48,45 @@ def test_had_duplicates(id_set, id_to_check, acceptable):
     assert acceptable == has_duplicate(id_set, id_to_check)
 
 
-integration_data = "{'Cortex XDR - IR': OrderedDict([('name', 'Cortex XDR - IR')"
+integration_data = {
+        "Cortex XDR - IR": {
+            "name": "Cortex XDR - IR",
+            "file_path": "Integrations/PaloAltoNetworks_XDR/PaloAltoNetworks_XDR.yml",
+            "fromversion": "4.1.0",
+            "commands": [
+                "xdr-get-incidents",
+                "xdr-get-incident-extra-data",
+                "xdr-update-incident"
+            ]
+        }
+    }
+
+script_data = {
+    "AnalyzeMemImage": {
+        "name": "AnalyzeMemImage",
+        "file_path": "Scripts/script-AnalyzeMemImage.yml"
+    }
+}
 
 
-def test_get_integration_data():
-    file_path = '~/dev/demisto/content/Integrations/PaloAltoNetworks_XDR/PaloAltoNetworks_XDR.yml'
-    data = get_integration_data(file_path)
+class TestIntegration(unittest.TestCase):
+    def test_get_integration_data(self):
+        """
+        Test for getting all the integration data
+        """
+        file_path = 'Integrations/PaloAltoNetworks_XDR/PaloAltoNetworks_XDR.yml'
+        data = get_integration_data(file_path)
+        self.assertDictEqual(data, integration_data)
 
-    assert integration_data in str(data)
+    def test_get_script_data(self):
+        """
+        Test for getting the script data
+        """
+        file_path = 'Scripts/script-AnalyzeMemImage.yml'
+        data = get_script_data(file_path)
+        self.assertDictEqual(data, script_data)
 
 
-script_data = "{'AnalyzeMemImage': OrderedDict([('name', 'AnalyzeMemImage')"
-
-
-def test_get_script_data():
-    file_path = '~/dev/demisto/content/Scripts/script-AnalyzeMemImage.yml'
-    data = get_script_data(file_path)
-
-    assert script_data in str(data)
+if __name__ == '__main__':
+    unittest.main()
 
