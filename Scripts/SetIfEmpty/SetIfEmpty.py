93f085d92facc39c3049bb9e67a01c1d4ea2cadf
Bob <eorgad@paloaltonetworks.com>
2019-11-14 11:12:58 +0200

    Remove temporary code

---
+++ Scripts/SetIfEmpty/SetIfEmpty.py
@@ -0,0 +1,20 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+
+
+def main(args):
+    demisto.results(getValueToSet(args))
+
+def getValueToSet(args):
+    value = args.get('value')
+    applyIfEmpty = True if args.get('applyIfEmpty') == 'true' else False
+
+    if value is None or (applyIfEmpty and len(value) < 1):
+        value = args.get('defaultValue')
+
+    return value
+
+
+if __name__ in ('builtins', '__builtin__'):
+    main(demisto.args())
