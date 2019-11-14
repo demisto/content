68080ad69eebfd6518c23448aebdb171ea629971
guyfreund <gfreund@paloaltonetworks.com>
2019-11-14 10:21:16 +0200

    removed import

--- Tests/scripts/hook_validations/tests/tests_data/docker_test.py
+++
@@ -1,6 +0,0 @@
-
-def test_parse_docker_image():
-    from Tests.scripts.hook_validations.docker import parse_docker_image
-    assert 'demisto/python', '1.3-alpine' == parse_docker_image('demisto/python:1.3-alpine')
-    assert 'demisto/slack', '1.2.3.4' == parse_docker_image('demisto/slack:1.2.3.4')
-    assert ('','') == parse_docker_image('demisto/python/1.2.3.4')
