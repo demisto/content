44c5cb8a01dbab76fd8a788a7c52f2fcf4ff8adb
guyfreund <gfreund@paloaltonetworks.com>
2019-11-14 10:12:47 +0200

    added common server python

--- Tests/scripts/hook_validations/tests/docker_test.py
+++ Tests/scripts/hook_validations/tests/docker_test.py
@@ -3,7 +3,7 @@ def test_parse_docker_image():
     from Tests.scripts.hook_validations.docker import parse_docker_image
     assert 'demisto/python', '1.3-alpine' == parse_docker_image('demisto/python:1.3-alpine')
     assert 'demisto/slack', '1.2.3.4' == parse_docker_image('demisto/slack:1.2.3.4')
-    assert ('','') == parse_docker_image('demisto/python/1.2.3.4')
+    assert ('', '') == parse_docker_image('demisto/python/1.2.3.4')
 
 
 def test_is_docker_image_latest_tag(mocker):
