aa0d27c0cdc0e5b46612d009a31a733bd2bd220d
teizenman <teizenman@paloaltonetworks.com>
2019-11-17 14:09:15 +0200

    Added matchexact and unit-testing

--- Scripts/FilterByList/FilterByList.py
+++ Scripts/FilterByList/FilterByList.py
@@ -2,64 +2,84 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
-lst = demisto.executeCommand("getList", {"listName": demisto.args()["listname"]})
 
-if isError(lst[0]):
-    demisto.results(lst)
-    sys.exit(0)
+def filter_list(lst, items, ignore_case, match_exact, list_name):
+    not_white_listed = []  # type: list
+    white_listed = []  # type: list
+    human_readable = ""
 
-notwhitelisted = []  # type: list
-whitlisted = []  # type: list
-hr = ""
-ignore_case = demisto.args().get("ignorecase", "").lower() == "yes"
+    # If the list is empty
+    if not lst[0]["Contents"]:
+        for item in items:
+            not_white_listed.append(item)
 
-items = demisto.args().get("values", "")
-if not isinstance(items, list):
-    items = items.split(",")
+        ec = {"List.In": white_listed, "List.NotIn": not_white_listed}
 
-if not lst[0]["Contents"]:
-    for item in items:
-        notwhitelisted.append(item)
-
-    ec = {"List.In": whitlisted, "List.NotIn": notwhitelisted}
-
-    demisto.results(
-        {
+        return {
             "ContentsFormat": formats["text"],
             "Type": entryTypes["note"],
-            "Contents": 'The list ' + demisto.args()["listname"] + ' is empty',
+            "Contents": 'The list ' + list_name + ' is empty',
             "EntryContext": ec
         }
-    )
-    sys.exit(0)
 
-lst = lst[0]["Contents"].split(",")
-search_flag = re.IGNORECASE if ignore_case else 0
+    lst = lst[0]["Contents"].split(",")
+    search_flag = re.IGNORECASE if ignore_case else 0
 
-# fill whitelisted array with all the the values that match the regex items in listname argument
-for item in items:
-    for list_item in lst:
-        if list_item and re.search(item, list_item, search_flag):
-            hr += item + " is in the list\n"
-            whitlisted.append(item)
+    # fill whitelisted array with all the the values that match the regex items in listname argument
+    for item in items:
+        if match_exact:
+            if ignore_case:
+                if not item.lower() in [list_item.lower().strip() for list_item in lst]:
+                    continue
+            else:
+                if item not in lst:
+                    continue
 
-# fill notwhitelisted array with all the the values that not in whitelisted
-for item in items:
-    if item not in whitlisted:
-        hr += item + " is not part of the list\n"
-        notwhitelisted.append(item)
+            human_readable += item + " is in the list\n"
+            white_listed.append(item)
 
-ec = {"List.In": whitlisted, "List.NotIn": notwhitelisted}
-contents = {"inList": whitlisted, "notInList": notwhitelisted}
-demisto.results(
-    {
+        else:
+            for list_item in lst:
+                if list_item and re.search(item, list_item, search_flag):
+                    human_readable += item + " is in the list\n"
+                    white_listed.append(item)
+
+    # fill not_white_listed array with all the the values that not in whitelisted
+    for item in items:
+        if item not in white_listed:
+            human_readable += item + " is not part of the list\n"
+            not_white_listed.append(item)
+
+    ec = {"List.In": white_listed, "List.NotIn": not_white_listed}
+    contents = {"inList": white_listed, "notInList": not_white_listed}
+
+    return {
         "ContentsFormat": formats["json"],
         "Type": entryTypes["note"],
         "Contents": contents,
-        "HumanReadable": hr,
+        "HumanReadable": human_readable,
         "HumanReadableFormat": formats["markdown"],
         "EntryContext": ec
     }
-)
 
-sys.exit(0)
+
+def main():
+    list_name = demisto.args()["listname"]
+    ignore_case = demisto.args().get("ignorecase", "").lower() == "yes"
+    match_exact = demisto.args().get("matchexact", "").lower() == "yes"
+    items = demisto.args().get("values", "")
+
+    lst = demisto.executeCommand("getList", {"listName": list_name})
+
+    if isError(lst[0]):
+        return_error('List not found')
+
+    if not isinstance(items, list):
+        items = items.split(",")
+
+    demisto.results(filter_list(lst, items, ignore_case, match_exact, list_name))
+
+
+# python2 uses __builtin__ python3 uses builtins
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
