b5d28ab3805364f0974ed8b8e7f2da467d0a1c85
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-11-19 10:15:47 +0200

    change calling of configure_and_test_integration_instances.py in run_tests.sh for updated cli args

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -22,6 +22,6 @@ USERNAME=$temp
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 [ -n "${MEM_CHECK}" ] && MEM_CHECK=true || MEM_CHECK=false
 
-python ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH"
+python ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -env "$1" 
 
 python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CIRCLE_BUILD_NUM" -g "$CIRCLE_BRANCH" -m "$MEM_CHECK" --isAMI true -d "$1"
