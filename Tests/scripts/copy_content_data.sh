1eefb44c0b7da3624fe7baf776147abb3c7344eb
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-11-20 14:34:15 +0200

    fix syntax error in copy_content_data.sh

--- Tests/scripts/copy_content_data.sh
+++ Tests/scripts/copy_content_data.sh
@@ -25,7 +25,7 @@ COPY_CONTENT_COMMAND="sudo rm -f /usr/local/demisto/res/playbook-Test Playbook T
   /usr/local/demisto/res/script-CommonServerPython_4_1.yml \
   /usr/local/demisto/res/integration-Windows_Defender_Advanced_Threat_Protection.yml /usr/local/demisto/res/integration-Microsoft_Graph.yml \
   /usr/local/demisto/res/integration-Awake_Security.yml /usr/local/demisto/res/integration-WhatsMyBrowser.yml \
-  && sudo cp -r ~/Beta_Integrations/* /usr/local/demisto/res && && sudo unzip -q -o ~/content/content_test.zip -d /usr/local/demisto/res"
+  && sudo cp -r ~/Beta_Integrations/* /usr/local/demisto/res && sudo unzip -q -o ~/content/content_test.zip -d /usr/local/demisto/res"
 ssh -t ${USER}@${PUBLIC_IP} ${COPY_CONTENT_COMMAND}
 
 echo "[`date`] ${PUBLIC_IP}: start server"
