5eb8fc7cc8d6db82760fd077c8c4a53c6fb14929
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-11-20 13:52:09 +0200

    change copy_content_data.sh to not sideload content of current branch

--- Tests/scripts/copy_content_data.sh
+++ Tests/scripts/copy_content_data.sh
@@ -14,8 +14,7 @@ ssh ${USER}@${PUBLIC_IP} 'mkdir ~/content'
 ssh ${USER}@${PUBLIC_IP} 'mkdir ~/TestPlaybooks'
 ssh ${USER}@${PUBLIC_IP} 'mkdir ~/Beta_Integrations'
 
-# copy content artifacts from last successful build on master
-scp prev_content_new.zip ${USER}@${PUBLIC_IP}:~/content
+# default content on AMI instance is from the last release
 scp content_test.zip ${USER}@${PUBLIC_IP}:~/content
 scp $(find ./Beta_Integrations/ -maxdepth 1 -type f) ${USER}@${PUBLIC_IP}:~/Beta_Integrations
 
@@ -26,8 +25,7 @@ COPY_CONTENT_COMMAND="sudo rm -f /usr/local/demisto/res/playbook-Test Playbook T
   /usr/local/demisto/res/script-CommonServerPython_4_1.yml \
   /usr/local/demisto/res/integration-Windows_Defender_Advanced_Threat_Protection.yml /usr/local/demisto/res/integration-Microsoft_Graph.yml \
   /usr/local/demisto/res/integration-Awake_Security.yml /usr/local/demisto/res/integration-WhatsMyBrowser.yml \
-  && sudo cp -r ~/Beta_Integrations/* /usr/local/demisto/res && sudo unzip -q -o ~/content/prev_content_new.zip -d /usr/local/demisto/res \
-  && sudo unzip -q -o ~/content/content_test.zip -d /usr/local/demisto/res"
+  && sudo cp -r ~/Beta_Integrations/* /usr/local/demisto/res && && sudo unzip -q -o ~/content/content_test.zip -d /usr/local/demisto/res"
 ssh -t ${USER}@${PUBLIC_IP} ${COPY_CONTENT_COMMAND}
 
 echo "[`date`] ${PUBLIC_IP}: start server"
