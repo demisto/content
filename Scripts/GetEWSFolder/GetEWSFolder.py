f1ab5378ac679c473964ebb081421cad3c915726
lperry <lperry@paloaltonetworks.com>
2019-11-21 13:00:32 +0200

    adding an automation for getting emails from multiple folders

--- Scripts/GetEWSFolder/GetEWSFolder.py
+++ Scripts/GetEWSFolder/GetEWSFolder.py
@@ -1,7 +1,4 @@
-import demistomock as demisto
 from CommonServerPython import *
-from CommonServerUserPython import *
-import json
 
 
 def convert_mail_to_json(item, folder):
@@ -14,7 +11,7 @@ def convert_mail_to_json(item, folder):
 def main():
     folders_paths_str = demisto.args()['foldersPaths']
     folders_paths_list = [folder.strip() for folder in folders_paths_str.split(',')]
-    path_to_mails = {folder: [] for folder in folders_paths_list}
+    path_to_mails = {folder: [] for folder in folders_paths_list} # type: Dict[str, List[str]]
     for folder in folders_paths_list:
 
         res = demisto.executeCommand('ews-get-items-from-folder', {
