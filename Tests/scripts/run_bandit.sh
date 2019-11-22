08c3e6f0f9106316c8539b1f4328d1ad6defd599
ikeren <itay@demisto.com>
2019-11-22 19:48:25 +0200

    remove bandit no fail env var

--- Tests/scripts/run_bandit.sh
+++ Tests/scripts/run_bandit.sh
@@ -5,8 +5,6 @@
 # This allows treating bandit as non-fatal.
 #
 # Arguments: python_file
-#
-# Env: BANDIT_NO_FAIL: if set will always return a 0 return code. Can be used in nightly to not fail the build.
 
 if [[ $# -lt 1 ]]; then
     echo "Usage: $BASH_SOURCE <python_file>"
@@ -18,10 +16,4 @@ PY_FILE=$1
 
 bandit -r $PY_FILE 2>&1
 
-res=$?
-
-if [[ -n "${BANDIT_NO_FAIL}" ]]; then
-    exit 0
-fi
-
-exit $res
+exit 0
