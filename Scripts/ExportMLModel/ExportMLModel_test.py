e810a72c2c5f0d16f969ec47ab6a14340c27c5ac
lperry <lperry@paloaltonetworks.com>
2019-11-24 11:41:20 +0200

    fixing flake8 erros

--- Scripts/ExportMLModel/ExportMLModel_test.py
+++ Scripts/ExportMLModel/ExportMLModel_test.py
@@ -4,10 +4,9 @@ from CommonServerPython import *
 
 dummy_data = [i for i in range(10)]
 
+
 def test_main(mocker):
-    mocker.patch.object(demisto, 'args', return_value={
-        "modelName": 'model',
-    })
+    mocker.patch.object(demisto, 'args', return_value={"modelName": 'model'})
 
     def get_file_path(entry_id):
         return {'path': './TestData/entry_file'}
@@ -16,16 +15,14 @@ def test_main(mocker):
         if command == 'getMLModel':
             key_args = ['modelName']
             assert (all(k in args for k in key_args))
-            return [{'Contents': dummy_data, 'Type': entryTypes['file'] }]
-
+            return [{'Contents': dummy_data, 'Type': entryTypes['file']}]
 
     def results(result):
         file_id = result['FileID']
         with open(demisto.investigation()['id'] + '_' + file_id, 'r') as f:
             file_data_str = f.read()
-        file_data =  json.loads(file_data_str)
-        assert ( all (x in file_data for x in dummy_data) and  all (x in dummy_data for x in file_data) )
-
+        file_data = json.loads(file_data_str)
+        assert (all(x in file_data for x in dummy_data) and all(x in dummy_data for x in file_data))
 
     mocker.patch.object(demisto, 'getFilePath', side_effect=get_file_path)
     mocker.patch.object(demisto, 'executeCommand', side_effect=execute_command)
@@ -33,4 +30,4 @@ def test_main(mocker):
     mocker.patch.object(demisto, 'results', side_effect=results)
 
     main()
-
+import
--- Scripts/ImportMLModel/ImportMLModel_test.py
+++ Scripts/ImportMLModel/ImportMLModel_test.py
@@ -16,10 +16,10 @@ def test_main(mocker):
 
     def execute_command(command, args):
         if command == 'createMLModel':
-            key_args = ['modelData', 'modelName', 'modelLabels',  'modelOverride']
+            key_args = ['modelData', 'modelName', 'modelLabels', 'modelOverride']
         if command == "evaluateMLModel":
             key_args = ['modelConfusionMatrix', 'modelName']
-        assert (all (k in args for k in key_args ))
+        assert(all(k in args for k in key_args))
         return []
 
     def results(result):
@@ -31,7 +31,5 @@ def test_main(mocker):
     mocker.patch.object(demisto, 'getFilePath', side_effect=get_file_path)
     mocker.patch.object(demisto, 'executeCommand', side_effect=execute_command)
     mocker.patch.object(demisto, 'results', side_effect=results)
-
     main()
-
     assert (done)
