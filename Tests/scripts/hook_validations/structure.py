0fd80709c3e6c50ed3be14ed4314c82bef238e0f
Jochman <bar.hochman@demisto.com>
2019-11-24 14:21:31 +0200

    added secrets

--- Tests/scripts/hook_validations/structure.py
+++ Tests/scripts/hook_validations/structure.py
@@ -13,9 +13,9 @@ import yaml
 from Tests.scripts.constants import YML_INTEGRATION_REGEXES, YML_SCRIPT_REGEXES, JSON_ALL_WIDGETS_REGEXES, \
     JSON_ALL_DASHBOARDS_REGEXES, JSON_ALL_CONNECTIONS_REGEXES, JSON_ALL_CLASSIFIER_REGEXES, \
     JSON_ALL_LAYOUT_REGEXES, JSON_ALL_INCIDENT_FIELD_REGEXES, YML_ALL_PLAYBOOKS_REGEX, JSON_ALL_REPORTS_REGEXES, \
-    PYTHON_ALL_REGEXES, MISC_REGEX, MISC_REPUTATIONS_REGEX
+    MISC_REGEX, MISC_REPUTATIONS_REGEX
 from Tests.scripts.error_constants import Errors
-from Tests.test_utils import print_error, print_warning, get_matching_regex, get_remote_file
+from Tests.test_utils import print_error, get_matching_regex, get_remote_file
 
 try:
     from pykwalify.core import Core
