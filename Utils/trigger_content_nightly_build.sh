9d2962be8608575bf47374cf3c91584417b7db38
Andrew Shamah <andrew@demisto.com>
2019-11-24 13:45:23 +0200

    trigger nightly workflow

--- Utils/trigger_content_nightly_build.sh
+++ Utils/trigger_content_nightly_build.sh
@@ -3,11 +3,12 @@
 _branch=$1
 _circle_token=$2
 
-trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_branch}?circle-token=${_circle_token}
+trigger_build_url=https://circleci.com/api/v2/project/github/demisto/content/pipeline?circle-token=${_circle_token}
 
 post_data=$(cat <<EOF
 {
-  "build_parameters": {
+  "branch": "${_branch}",
+  "parameters": {
     "NIGHTLY": "true"
   }
 }
