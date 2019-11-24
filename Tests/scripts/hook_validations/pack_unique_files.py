84beb63b8cab72253849ebfbba52b1324094dceb
yardensade <ysade@paloaltonetworks.com>
2019-11-24 19:56:55 +0200

    added internal util funcs

--- Tests/scripts/hook_validations/pack_unique_files.py
+++ Tests/scripts/hook_validations/pack_unique_files.py
@@ -32,7 +32,7 @@ class PackUniqueFilesValidator():
 
     def get_errors(self, raw=False):
         """Get the dict version or string version for print"""
-        errors = []
+        errors = ''
         if raw:
             errors = self._errors
         elif self._errors:
@@ -53,31 +53,48 @@ class PackUniqueFilesValidator():
 
         return True
 
+    def _open_and_read_file_content(self, file_name):
+        """Open & Read a file object's content throw exception if can't"""
+        try:
+            with io.open(self._get_pack_file_path(file_name), mode="r", encoding="utf-8") as file:
+                return file.read()
+        except IOError:
+            self._add_error('Could not open "{}" file'.format(file_name))
+        except ValueError:
+            self._add_error('Could not read the contents of "{}" file'.format(file_name))
+
+        return False
+
+    def _parse_file_into_list(self, file_name, delimiter='\n'):
+        """Parse file's content to list, throw exception if can't"""
+        file_content = self._open_and_read_file_content(file_name)
+        try:
+            if file_content:
+                return file_content.split(delimiter)
+        except ValueError:
+            self._add_error('Could not parse the contents of "{}" file into a list'.format(file_name))
+
+        return False
+
     # secrets validation
     def validate_secrets_file(self):
         """Validate everything related to .secrets-ignore file"""
-        if all([self._is_pack_file_exists(self.secrets_file),
-                self._is_secrets_file_structure_valid()]):
+        if self._is_pack_file_exists(self.secrets_file) and all([self._is_secrets_file_structure_valid()]):
             return True
 
         return False
 
     def _is_secrets_file_structure_valid(self):
         """Check if .secrets-ignore structure is parse-able"""
-        try:
-            with io.open(self._get_pack_file_path(self.secrets_file), mode="r", encoding="utf-8") as secrets_file:
-                if secrets_file.read().split('\n'):
-                    return True
-        except (IOError, ValueError):
-            self._add_error('Could not parse the contents of "{}" file'.format(self.secrets_file))
+        if self._parse_file_into_list(self.secrets_file):
+            return True
 
         return False
 
     # pack ignore validation
     def validate_pack_ignore_file(self):
         """Validate everything related to .pack-ignore file"""
-        if all([self._is_pack_file_exists(self.pack_ignore_file),
-                self._is_pack_ignore_file_structure_valid()]):
+        if self._is_pack_file_exists(self.pack_ignore_file) and all([self._is_pack_ignore_file_structure_valid()]):
             return True
 
         return False
@@ -85,12 +102,9 @@ class PackUniqueFilesValidator():
     def _is_pack_ignore_file_structure_valid(self):
         """Check if .pack-ignore structure is parse-able & has valid regex"""
         try:
-            with io.open(self._get_pack_file_path(self.pack_ignore_file), mode="r", encoding="utf-8") \
-                    as pack_ignore_file:
-                if all(re.compile(regex) for regex in pack_ignore_file.read().split('\n')):
-                    return True
-        except (IOError, ValueError):
-            self._add_error('Could not fetch {} file contents'.format(self.pack_ignore_file))
+            pack_ignore_regex_list = self._parse_file_into_list(self.pack_ignore_file)
+            if pack_ignore_regex_list and all(re.compile(regex) for regex in pack_ignore_regex_list):
+                return True
         except re.error:
             self._add_error('Detected none valid regex in {} file'.format(self.pack_ignore_file))
 
@@ -99,8 +113,7 @@ class PackUniqueFilesValidator():
     # pack metadata validation
     def validate_pack_meta_file(self):
         """Validate everything related to pack-metadata.json file"""
-        if all([self._is_pack_file_exists(self.pack_meta_file),
-                self._is_pack_meta_file_structure_valid()]):
+        if self._is_pack_file_exists(self.pack_meta_file) and all([self._is_pack_meta_file_structure_valid()]):
             return True
 
         return False
@@ -108,10 +121,10 @@ class PackUniqueFilesValidator():
     def _is_pack_meta_file_structure_valid(self):
         """Check if pack-metadata.json structure is json parse-able"""
         try:
-            with io.open(self._get_pack_file_path(self.pack_meta_file), mode="r", encoding="utf-8") as pack_meta_file:
-                if json.loads(pack_meta_file.read()):
-                    return True
-        except (IOError, ValueError):
+            pack_meta_file_content = self._open_and_read_file_content(self.pack_meta_file)
+            if pack_meta_file_content and json.loads(pack_meta_file_content):
+                return True
+        except ValueError:
             self._add_error('Could not parse {} file contents to json format'.format(self.pack_meta_file))
 
         return False
@@ -119,7 +132,7 @@ class PackUniqueFilesValidator():
     # pack README.md validation
     def validate_readme_file(self):
         """Validate everything related to README.md file"""
-        if all([self._is_pack_file_exists(self.readme_file)]):
+        if self._is_pack_file_exists(self.readme_file):
             return True
 
         return False
