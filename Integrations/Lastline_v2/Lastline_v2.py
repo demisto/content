66723cfc5f223d6eacf985597b44774ad08c2e93
esharf <esahrf@paloaltonetworks.com>
2019-11-26 13:02:51 +0200

    Lastline_v2 almost ready (lastline-upload-file command and result returns need fix)

--- Integrations/Lastline_v2/Lastline_v2.py
+++ Integrations/Lastline_v2/Lastline_v2.py
@@ -1,6 +1,9 @@
-# import demistomock as demisto
+import mimetypes
+import demistomock as demisto
 from CommonServerPython import *
-# from CommonServerUserPython import *
+from CommonServerUserPython import *
+
+from typing import Dict
 
 INTEGRATION_COMMAND_NAME = "lastline"
 INTEGRATION_NAME = "Lastline_v2"
@@ -10,16 +13,17 @@ class Client(BaseClient):
     MD5_LEN = 32
     SHA1_LEN = 40
     SHA256_LEN = 64
+    DEFAULT_THRESHOLD = 70
 
-    def __init__(self, base_url: str, api_params: dict, verify=True, proxy=False):
+    def __init__(self, base_url: str, api_params: Dict, verify=True, proxy=False):
         self.command_params = api_params
         super(Client, self).__init__(base_url, verify, proxy)
 
     def file(self):
-        self.command_params[help_hash_type_checker(self.command_params.get('file'))] = self.command_params.get('file')
-        # del self.command_params['file']
+        hash_type = help_hash_type_checker(self.command_params.get('file'))
+        self.command_params[hash_type] = self.command_params.get('file')
         result = self.request_in_path('/analysis/submit/file')
-        context_entry = {
+        context_entry: Dict = {
             'File': {
                 'MD5': "",
                 'SHA1': "",
@@ -32,10 +36,10 @@ class Client(BaseClient):
                 }
             },
             'DBotScore': {
-                'Indicator': "",
-                'Type': "",
-                'Vendor': "",
-                'Score': ""
+                'Indicator': self.command_params.get('file'),
+                'Type': hash_type,
+                'Vendor': "Lastline v2",
+                'Score': 0
             }
         }
         human_readable = tableToMarkdown(name=INTEGRATION_NAME, t=context_entry)
@@ -43,7 +47,7 @@ class Client(BaseClient):
 
     def check_status(self):
         result = self.request_in_path('/analysis/get')
-        context_entry = {
+        context_entry: Dict = {
             'Lastline': {
                 'Submission': {
                     'UUID': "",
@@ -57,13 +61,13 @@ class Client(BaseClient):
 
     def get_report(self):
         result = self.request_in_path('/analysis/get')
-        context_entry = help_get_report_context(result)
+        context_entry: Dict = help_get_report_context(result, self.command_params.get('threshold'))
         human_readable = tableToMarkdown(name=INTEGRATION_NAME, t=context_entry)
         return human_readable, context_entry, result
 
     def get_task_list(self):
         result = self.request_in_path('/analysis/get_completed')
-        context_entry = {
+        context_entry: Dict = {
             'key': [],
             'uuid': []
         }
@@ -74,12 +78,10 @@ class Client(BaseClient):
         entry_id = self.command_params.get('EntryID')
         self.command_params['push_to_portal'] = True
         file_params = demisto.getFilePath(entry_id)
-        with open(file_params.get('path'), 'rb') as file:
-            result = self.request_in_path('/analysis/submit/file', files={'file': file.read()})
-            # result = {"success": 0, "error_code": 115, "error": "Submission limit exceeded"}
-        demisto.info("open success")
-        demisto.info(result)
-        context_entry = {
+        file_params['mime_type'] = mimetypes.guess_type(file_params.get('name'))
+        with open(file_params.get('path') + file_params.get('name'), 'rb') as file:
+            result = self.request_in_path('/analysis/submit/file', files={file_params['mime_type']: file.read()})
+        context_entry: Dict = {
             'Lastline': {
                 'Submission': {
                     'Filename': file_params.get('name'),
@@ -89,42 +91,44 @@ class Client(BaseClient):
             }
         }
         help_upload_and_status(result, context_entry)
-        human_readable = tableToMarkdown(INTEGRATION_COMMAND_NAME, t=context_entry.get('Lastline'))
+        human_readable = tableToMarkdown(INTEGRATION_COMMAND_NAME, t=context_entry)
+
         return human_readable, context_entry, result
 
     def upload_url(self):
         result = self.request_in_path('/analysis/submit/url')
         demisto.info(result)
-        context_entry = {
+        context_entry: Dict = {
             'Lastline': {
                 'Submission': {
-                    'URL': "",
+                    'URL': self.command_params.get('url'),
                     'UUID': "",
                     'Status': ""
                 }
             }
         }
-        help_upload_and_status(dict, context_entry)
-        human_readable = tableToMarkdown(INTEGRATION_COMMAND_NAME, t=context_entry['Lastline'])
-        return human_readable, context_entry.get('Lastline'), result
+        help_upload_and_status(result, context_entry)
+        human_readable = tableToMarkdown(name=INTEGRATION_COMMAND_NAME, t=context_entry)
+        return human_readable, context_entry, result
 
     def test_module_command(self):
-        self.command_params['uuid'] = '64081e5251ae0010058d480cc8c1b68b'
-        self.get_report()
+        self.command_params['url'] = 'https://www.google.com'
+        self.upload_url()
         return 'ok'
 
-    def request_in_path(self, path: str, files=None) -> dict:
-        result = self._http_request('POST', path, data=self.command_params, files=files)
+    def request_in_path(self, path: str, files=None) -> Dict:
+        result: Dict = self._http_request('POST', path, data=self.command_params, files=files)
         help_lastline_exception_handler(result)
         return result
 
 
-def help_lastline_exception_handler(result: dict):
+def help_lastline_exception_handler(result: Dict):
     if not result.get("success"):
         error_msg = "error "
         if 'error_code' in result:
-            error_msg += "(" + str(result.get("error_code")) + ") "
-        error_msg += result.get("error")
+            error_msg += "(" + str(result['error_code']) + ") "
+        if 'error' in result:
+            error_msg += result['error']
         raise DemistoException(error_msg)
 
 
@@ -138,23 +142,17 @@ def help_hash_type_checker(hash_file: str) -> str:
     raise DemistoException(f'{INTEGRATION_NAME} File command support md5/ sha1/ sha256 only')
 
 
-def help_upload_and_status(result: dict, context_entry: dict):
+def help_upload_and_status(result: Dict, context_entry: Dict):
     if 'data' in result:
         context_entry['Lastline']['Submission']['UUID'] = result['data'].get('task_uuid')
         if 'score' in result['data']:
             context_entry['Lastline']['Submission']['Status'] = 'Completed'
         else:
             context_entry['Lastline']['Submission']['Status'] = 'Analyzing'
-        if 'analysis_subject' in result['data']:
-            if 'Filename' in result['data']['analysis_subject'] and \
-                    'Filename' in context_entry['Lastline']['Submission']:
-                context_entry['Lastline']['Submission']['Filename'] = result['data']['analysis_subject']['Filename']
-            elif 'url' in result['data']['analysis_subject'] and 'url' in context_entry['Lastline']['Submission']:
-                context_entry['Lastline']['Submission']['URL'] = result['data']['analysis_subject']['url']
 
 
-def help_get_report_context(result) -> dict:
-    context_entry = {
+def help_get_report_context(result: Dict, threshold=None) -> Dict:
+    context_entry: Dict = {
         'URL': {
             'Data': "",
             'Malicious': {
@@ -209,16 +207,24 @@ def help_get_report_context(result) -> dict:
                 context_entry['Lastline']['Submission']['Status'] = 'Completed'
                 context_entry['DBotScore']['type'] = 'url'
                 context_entry['DBotScore']['Indicator'] = result['data']['analysis_subject'].get('url')
-                if 'url' in result['data']['analysis_subject']:
-                    key = 'URL'
-                    context_entry[key]['Data'] = result['data']['analysis_subject'].get('url')
+                if threshold is None:
+                    threshold = Client.DEFAULT_THRESHOLD
+                if result['data']['score'] > threshold:
+                    if 'url' in result['data']['analysis_subject']:
+                        key = 'URL'
+                        context_entry[key]['Data'] = result['data']['analysis_subject'].get('url')
+                        context_entry['DBotScore']['Indicator'] = context_entry[key]['Data']
+                    else:
+                        key = 'File'
+                        context_entry[key]['md5'] = result['data']['analysis_subject'].get('md5')
+                        context_entry[key]['sha1'] = result['data']['analysis_subject'].get('sha1')
+                        context_entry[key]['sha256'] = result['data']['analysis_subject'].get('sha256')
+                        context_entry['DBotScore']['Indicator'] = 'md5-' + context_entry[key]['md5']
+                    context_entry['DBotScore']['Type'] = key
+                    context_entry[key]['Malicious']['Vendor'] = 'Lastline'
+                    context_entry[key]['Malicious']['score'] = result['data'].get('score')
+                    context_entry[key]['Malicious']['Description'] = 'Score above ' + str(threshold)
 
-                else:
-                    # context_entry['URL']['Data'] = result['data']['analysis_subject'].get('url')
-                    key = 'File'
-                context_entry['key']['Data']['Malicious']['Vendor'] = 'Lastline'
-                context_entry['key']['Data']['Malicious']['Description'] = 'Score above '
-                context_entry['key']['Data']['Malicious']['score'] = result['data'].get('score')
         else:
             context_entry['Lastline']['Submission']['Status'] = 'Analyzing'
     return context_entry
@@ -241,13 +247,12 @@ def main():
     # Switch case
     commands = {
         'test-module': Client.test_module_command,
-        # 'fetch-incidents': fetch_incidents_command,
         'file': Client.file,
-        f'{INTEGRATION_COMMAND_NAME}-check-status':  Client.check_status,
-        f'{INTEGRATION_COMMAND_NAME}-get-report':  Client.get_report,
-        f'{INTEGRATION_COMMAND_NAME}-get-task-list':  Client.get_task_list,
-        f'{INTEGRATION_COMMAND_NAME}-upload-file':  Client.upload_file,
-        f'{INTEGRATION_COMMAND_NAME}-upload-url':  Client.upload_url
+        f'{INTEGRATION_COMMAND_NAME}-check-status': Client.check_status,
+        f'{INTEGRATION_COMMAND_NAME}-get-report': Client.get_report,
+        f'{INTEGRATION_COMMAND_NAME}-get-task-list': Client.get_task_list,
+        f'{INTEGRATION_COMMAND_NAME}-upload-file': Client.upload_file,
+        f'{INTEGRATION_COMMAND_NAME}-upload-url': Client.upload_url
     }
     try:
         demisto.info(command)
@@ -258,7 +263,6 @@ def main():
     except Exception as every_error:
         err_msg = f'Error in {INTEGRATION_NAME} Integration [{every_error}]'
         return_error(err_msg, error=every_error)
-# main()
 
 
 if __name__ == "__builtin__" or __name__ == "builtins":
