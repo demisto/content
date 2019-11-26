2090ba8c9262822ae6d0a7b4193c41ac264a2f72
Bar Hochman <11165655+jochman@users.noreply.github.com>
2019-11-26 19:02:59 +0200

    Delete error_constants.py

--- Tests/scripts/hook_validations/error_constants.py
+++
@@ -1,4 +0,0 @@
-class Error:
-    @staticmethod
-    def wrong_file_extension(file_extension, accepted_extensions):
-        return "File extension {} is not valid. accepted {}".format(file_extension, accepted_extensions)
