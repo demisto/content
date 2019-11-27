ab44e7d5f2c6157b65e68466f67b8cead2b0d53a
yardensade <ysade@paloaltonetworks.com>
2019-11-27 21:32:07 +0200

    remove tests

--- Packs/CortexXDR/Scripts/XDRSyncScript/XDRSyncScript.py
+++ Packs/CortexXDR/Scripts/XDRSyncScript/XDRSyncScript.py
@@ -50,7 +50,6 @@ XDR_INCIDENT_FIELDS = [
     DETECTION_TIME_XDR_FIELD,
     MODIFICATION_TIME_XDR_FIELD
 ]
-a = 2
 
 
 def compare_incident_in_demisto_vs_xdr_context(incident_in_demisto, xdr_incident_in_context, incident_id, fields_mapping):
