9dca4945d3a446d70adf142d7c955faafa931637
teizenman <teizenman@paloaltonetworks.com>
2019-11-29 01:32:06 +0200

    UT

--- Scripts/QRadarGetOffenseCorrelations/QRadarGetOffenseCorrelations_test.py
+++ Scripts/QRadarGetOffenseCorrelations/QRadarGetOffenseCorrelations_test.py
@@ -1,11 +1,14 @@
+import pytest
 from QRadarGetOffenseCorrelations import get_query
 
 
-def test_get_query_cre_name_null_false():
-    query_from_false = get_query("False")
-    assert "\"CRE Name\" <> NULL" in query_from_false
+@pytest.mark.parametrize('bool_val', ["False"])
+def test_get_query_cre_name_null_false(bool_val):
+    query_from_false = get_query(bool_val)
+    assert "\"CRE NAME\" <> NULL" in query_from_false
 
 
-def test_get_query_cre_name_null_true():
-    query_from_false = get_query("True")
-    assert "\"CRE Name\" <> NULL" not in query_from_false
+@pytest.mark.parametrize('bool_val', ["True"])
+def test_get_query_cre_name_null_true(bool_val):
+    query_from_false = get_query(bool_val)
+    assert "\"CRE NAME\" <> NULL" not in query_from_false
