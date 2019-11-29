7e41064e23d29cd56783103a172901af730882e6
guykeller <g12k34ppp>
2019-11-29 18:19:08 +0200

    Added the fix, deprecated the script and removed docker

--- Scripts/ProofpointDecodeURL/ProofpointDecodeURL.py
+++ Scripts/ProofpointDecodeURL/ProofpointDecodeURL.py
@@ -1,47 +1,26 @@
 import demistomock as demisto
 from CommonServerPython import *
+import argparse
+import json
 import re
 import sys
 
-from urllib.parse import parse_qs, unquote, urlparse
+args = {
+    "input": demisto.args().get("url")
+}
+res = demisto.executeCommand("UnEscapeURLs", args)
 
-valid_versions = ['v1', 'v2']
-
-
-def find_version(url):
-    match = re.search(r'https://urldefense.proofpoint.com/(?P<version>v[0-9])/', url)
-    return match.group('version')  # type: ignore
-
-
-def ppdecode(url):
-    try:
-        version = find_version(url)
-    except AttributeError:
-        return_error('This does not appear to be a valid proofpoint url: {}'.format(url))
-    else:
-        if version not in valid_versions:
-            return_error('Ppdecode is unprepared to handle this version of proofpoint urls: {}'.format(url))
-    parsed_url = urlparse(url)
-    query_components = parse_qs(parsed_url.query)  # type: ignore
-    if 'u' in query_components:
-        if sys.version_info[0] < 3:
-            translated_url = query_components['u'][0].translate(str.maketrans('-_', '%/'))
-        else:
-            translated_url = query_components['u'][0].translate(str.maketrans('-_', '%/'))
-    else:
-        return_error('A URL was not detected in the query.')
-    decoded_url = unquote(translated_url)
-    query_components['decoded_url'] = decoded_url  # type: ignore
-    query_components['proofpoint_version'] = version
-    return query_components
-
-
-try:
-    results = ppdecode(demisto.args()['url'])
-    ec = {'URL': {'Data': results['decoded_url']}}
-    demisto.results({'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': results,
-                     'EntryContext': ec, 'HumanReadable': '#### Decoded URL\n' + results['decoded_url']})
-
-
-except RuntimeError as e:
-    return_error(str(e))
+if is_error(res):
+    return_error("Error: An error returned, could not parse URL.")
+else:
+    decoded_url = res[0].get("Contents")
+    entry_context = {
+        "Data": decoded_url
+    }
+    return_outputs(
+        readable_output="Decoded URL is: {0}".format(decoded_url),
+        raw_response=decoded_url,
+        outputs={
+            'URL(val.Data && val.Data == obj.Data)': entry_context
+        }
+    )
