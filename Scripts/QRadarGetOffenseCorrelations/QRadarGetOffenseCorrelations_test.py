5a721045de3d1b64b44e44a885c73c4cbf38720f
guykeller <g12k34ppp>
2019-11-30 18:08:08 +0200

    Improved tests

--- Scripts/QRadarGetOffenseCorrelations/QRadarGetOffenseCorrelations_test.py
+++ Scripts/QRadarGetOffenseCorrelations/QRadarGetOffenseCorrelations_test.py
@@ -1,14 +1,38 @@
 import pytest
 from QRadarGetOffenseCorrelations import get_query
+import pytest
+import demistomock as demisto
+from QRadarGetOffenseCorrelations import main
+
+@pytest.mark.parametrize('bool_val', ["True"])
+def test_get_query_cre_name_null_true(mocker, bool_val):
+    def executeCommand(name, args=None):
+        mock_dict = {
+            'Contents.events': None,
+            'Type': 'JSON'
+        }
+        return [mock_dict, "\"CRE Name\" <> NULL" not in args["query_expression"]]
+
+    mocker.patch.object(demisto, 'args', return_value={'is_cre_name_null': bool_val})
+    mocker.patch.object(demisto, 'executeCommand', side_effect=executeCommand)
+    mocker.patch.object(demisto, 'results')
+    main()
+    results = demisto.results.call_args[0]
+    assert results[0][1]
 
 
 @pytest.mark.parametrize('bool_val', ["False"])
-def test_get_query_cre_name_null_false(bool_val):
-    query_from_false = get_query(bool_val)
-    assert "\"CRE NAME\" <> NULL" in query_from_false
+def test_get_query_cre_name_null_false(mocker, bool_val):
+    def executeCommand(name, args=None):
+        mock_dict = {
+            'Contents.events': None,
+            'Type': 'JSON'
+        }
+        return [mock_dict, "\"CRE Name\" <> NULL" not in args["query_expression"]]
 
-
-@pytest.mark.parametrize('bool_val', ["True"])
-def test_get_query_cre_name_null_true(bool_val):
-    query_from_false = get_query(bool_val)
-    assert "\"CRE NAME\" <> NULL" not in query_from_false
+    mocker.patch.object(demisto, 'args', return_value={'is_cre_name_null': bool_val})
+    mocker.patch.object(demisto, 'executeCommand', side_effect=executeCommand)
+    mocker.patch.object(demisto, 'results')
+    main()
+    results = demisto.results.call_args[0]
+    assert not results[0][1]
