a868827c886fcc4b4174b7dde4b2616074834659
teizenman <teizenman@paloaltonetworks.com>
2019-11-30 17:56:31 +0200

    Added UT

---
+++ Integrations/MISP_V2/test_constants.py
@@ -0,0 +1,331 @@
+full_response_before_filtering = [
+    {
+        "Event": {
+            "Attribute": [
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Payload installation",
+                    "comment": "",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128241",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461142838",
+                    "to_ids": False,
+                    "type": "md5",
+                    "uuid": "57174536-95fc-474a-bb9f-46dc950d210f",
+                    "value": "116d442d8ec5c62f32c7ba507a5569be"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Payload installation",
+                    "comment": "- Xchecked via VT: 116d442d8ec5c62f32c7ba507a5569be",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128242",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461145369",
+                    "to_ids": False,
+                    "type": "sha256",
+                    "uuid": "57174f19-f06c-4809-92b3-476d02de0b81",
+                    "value": "ce98e9d9900609c45c948d34153c03b490bc2067e7a742048f60ed53fe95529d"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Payload installation",
+                    "comment": "- Xchecked via VT: 116d442d8ec5c62f32c7ba507a5569be",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128243",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461145369",
+                    "to_ids": False,
+                    "type": "sha1",
+                    "uuid": "57174f19-c3d8-43f0-96bb-43e402de0b81",
+                    "value": "fb93796afa470d87deb316823d3cd6e8d8b18596"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "External analysis",
+                    "comment": "",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128244",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461145370",
+                    "to_ids": False,
+                    "type": "link",
+                    "uuid": "57174f1a-a47c-458d-9900-451e02de0b81",
+                    "value": "https://www.virustotal.com/file/ce98e9d9900609c45c948d34153c03b490bc2067e7a742048f60ed53fe95529d/analysis/1461016885/"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Network activity",
+                    "comment": "Compromised wp installation",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128245",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461152759",
+                    "to_ids": False,
+                    "type": "hostname",
+                    "uuid": "57176bf7-4e28-4e60-88d4-4dea950d210f",
+                    "value": "kentamplin.net"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Network activity",
+                    "comment": "Compromised WP installation (fixed)",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128246",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461152810",
+                    "to_ids": False,
+                    "type": "url",
+                    "uuid": "57176c2a-2280-4f00-9208-42c1950d210f",
+                    "value": "kentamplin.net/wordpress/wp-includes/oops.php"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Network activity",
+                    "comment": "Compromised wp installation",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128247",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461152872",
+                    "to_ids": False,
+                    "type": "ip-dst",
+                    "uuid": "57176c68-3b48-47d1-a549-4f20950d210f",
+                    "value": "192.185.16.132"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Network activity",
+                    "comment": "",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128248",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461152936",
+                    "to_ids": False,
+                    "type": "url",
+                    "uuid": "57176ca8-4fd8-4eac-a432-4fdb950d210f",
+                    "value": "http://192.185.16.132/wordpress/wp-includes/oops.php?id=2886098\u0026cname=mh6SlMrX8b\u0026arch=0\u0026srv=0\u0026ver=6.1"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Other",
+                    "comment": "Ransom text",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128249",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461153063",
+                    "to_ids": False,
+                    "type": "comment",
+                    "uuid": "57176d27-f044-4038-9604-49da950d210f",
+                    "value": "YourID: 2886098\r\n    PC: HOME\r\n    USER: user\r\n    *********\r\n    Hey\r\n\r\n    Your files are now encrypted. I have the key to decrypt them back. I will give you a decrypter if you pay me. Email me at: momsbestfriend@protonmail.com or torrenttracker@india.com\r\n\r\n    If you don’t get a reply or if both emails die, then contact me using a guaranteed, foolproof\r\n    Bitmessage: download it form here https://github.com/mailchuck/PyBitmessage/releases/download/v0.5.8/Bitmessage-\r\n    0.5.8.exe\r\n\r\n    Run it, click New Identity and then send me a message at BM-NBvzKEY8raDBKb9Gp1xZMRQpeU5svwg2\r\n\r\n    Cheers"
+                },
+                {
+                    "Galaxy": [],
+                    "ShadowAttribute": [],
+                    "category": "Other",
+                    "comment": "",
+                    "deleted": False,
+                    "disable_correlation": False,
+                    "distribution": "5",
+                    "event_id": "2193",
+                    "id": "128250",
+                    "object_id": "0",
+                    "object_relation": None,
+                    "sharing_group_id": "0",
+                    "timestamp": "1461153134",
+                    "to_ids": False,
+                    "type": "comment",
+                    "uuid": "57176d6e-a4e8-4c08-bd9c-41d9950d210f",
+                    "value": "ID was seen to be same among the different infections..."
+                }
+            ],
+            "Galaxy": [],
+            "Object": [],
+            "Org": {
+                "id": "1",
+                "name": "ORGNAME",
+                "uuid": "5c77af86-8274-4106-862e-0045ac110002"
+            },
+            "Orgc": {
+                "id": "2",
+                "name": "CIRCL",
+                "uuid": "55f6ea5e-2c60-40e5-964f-47a8950d210f"
+            },
+            "RelatedEvent": [],
+            "ShadowAttribute": [],
+            "Tag": [
+                {
+                    "colour": "#ffffff",
+                    "exportable": True,
+                    "hide_tag": False,
+                    "id": "2",
+                    "name": "tlp:white",
+                    "numerical_value": None,
+                    "user_id": "0"
+                }
+            ],
+            "analysis": "0",
+            "attribute_count": "10",
+            "date": "2016-04-20",
+            "disable_correlation": False,
+            "distribution": "1",
+            "extends_uuid": "",
+            "id": "2193",
+            "info": "New ransomware",
+            "locked": False,
+            "org_id": "1",
+            "orgc_id": "2",
+            "proposal_email_lock": False,
+            "publish_timestamp": "1563282468",
+            "published": True,
+            "sharing_group_id": "0",
+            "threat_level_id": "3",
+            "timestamp": "1461153134",
+            "uuid": "57174526-23d8-4895-8c08-4ed1950d210f"
+        }
+    }
+]
+
+response_after_filtering_category_eventid_uuid = [
+    {
+        "Analysis": "0",
+        "Attribute": [
+            {
+                "Category": "Payload installation",
+                "EventID": "2193",
+                "UUID": "57174536-95fc-474a-bb9f-46dc950d210f"
+            },
+            {
+                "Category": "Payload installation",
+                "EventID": "2193",
+                "UUID": "57174f19-f06c-4809-92b3-476d02de0b81"
+            },
+            {
+                "Category": "Payload installation",
+                "EventID": "2193",
+                "UUID": "57174f19-c3d8-43f0-96bb-43e402de0b81"
+            },
+            {
+                "Category": "External analysis",
+                "EventID": "2193",
+                "UUID": "57174f1a-a47c-458d-9900-451e02de0b81"
+            },
+            {
+                "Category": "Network activity",
+                "EventID": "2193",
+                "UUID": "57176bf7-4e28-4e60-88d4-4dea950d210f"
+            },
+            {
+                "Category": "Network activity",
+                "EventID": "2193",
+                "UUID": "57176c2a-2280-4f00-9208-42c1950d210f"
+            },
+            {
+                "Category": "Network activity",
+                "EventID": "2193",
+                "UUID": "57176c68-3b48-47d1-a549-4f20950d210f"
+            },
+            {
+                "Category": "Network activity",
+                "EventID": "2193",
+                "UUID": "57176ca8-4fd8-4eac-a432-4fdb950d210f"
+            },
+            {
+                "Category": "Other",
+                "EventID": "2193",
+                "UUID": "57176d27-f044-4038-9604-49da950d210f"
+            },
+            {
+                "Category": "Other",
+                "EventID": "2193",
+                "UUID": "57176d6e-a4e8-4c08-bd9c-41d9950d210f"
+            }
+        ],
+        "Date": "2016-04-20",
+        "DisableCorrelation": False,
+        "Distribution": "1",
+        "Galaxy": [],
+        "ID": "2193",
+        "Info": "New ransomware",
+        "Locked": False,
+        "Object": [],
+        "Organisation": {
+            "ID": "1",
+            "Name": "ORGNAME",
+            "UUID": "5c77af86-8274-4106-862e-0045ac110002"
+        },
+        "OwnerOrganisation": {
+            "ID": "2",
+            "Name": "CIRCL",
+            "UUID": "55f6ea5e-2c60-40e5-964f-47a8950d210f"
+        },
+        "ProposalEmailLock": False,
+        "PublishTimestamp": "1563282468",
+        "Published": True,
+        "RelatedEvent": [],
+        "ShadowAttribute": [],
+        "SharingGroupID": "0",
+        "Tag": [
+            {
+                "Name": "tlp:white"
+            }
+        ],
+        "ThreatLevelID": "3",
+        "Timestamp": "1461153134",
+        "UUID": "57174526-23d8-4895-8c08-4ed1950d210f"
+    }
+]
