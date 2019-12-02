9d465c9ac260107cbd1b5a31e98fc30d9ac24ccf
mayagoldb <mayag@demisto.com>
2019-12-02 15:41:49 +0200

    New transformer script - Convert all except

---
+++ Scripts/ConvertAllExcept/ConvertAllExcept.py
@@ -0,0 +1,14 @@
+def main(args):
+    value = args.get('value')
+    convert_to = args.get('convertTo')
+    exceptions = argToList(args.get('except'))
+
+    if value in exceptions:
+        return value
+    else:
+        return convert_to
+
+
+if __name__ in ('builtins', '__builtin__'):
+    result = main(demisto.args())
+    demisto.results(result)
