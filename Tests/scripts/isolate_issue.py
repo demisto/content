ca42678e2a6cc60f8516cb53ea14e406416c005e
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-12-04 18:09:32 +0200

    alternate

--- Tests/scripts/isolate_issue.py
+++ Tests/scripts/isolate_issue.py
@@ -4,5 +4,5 @@ from Tests.test_utils import run_command
 # run_command('pwd', is_silenced=False)
 # run_command('find . -name "content_new.zip"', is_silenced=False)
 # run_command('unzip -o ./content_test.zip -d ./content_test', is_silenced=False)
-run_command('zip -j ./content_new.zip ./bundle_test/*', is_silenced=False)
+run_command('zip -j -u content_new.zip ./bundle_test/*', is_silenced=False)
 # run_command('rm -rf ./content_test', is_silenced=False)
