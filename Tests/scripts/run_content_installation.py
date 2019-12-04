3f4538e8960bceab900dc8812a991c5e647bde8b
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-12-04 15:41:39 +0200

    remove sudo

--- Tests/scripts/run_content_installation.py
+++ Tests/scripts/run_content_installation.py
@@ -30,9 +30,9 @@ def main():
 
     # # get content artifacts from last successful build on master
     # run_command('python ./Tests/get_build_content_artifacts.py', is_silenced=False)
-    run_command('sudo unzip -q -o ./content_test.zip -d ./content_test')
-    run_command('sudo zip -q -j ./content_new.zip ./content_test/*')
-    run_command('sudo rm -rf ./content_test')
+    run_command('unzip -q -o ./content_test.zip -d ./content_test')
+    run_command('zip -q -j ./content_new.zip ./content_test/*')
+    run_command('rm -rf ./content_test')
 
     print("Waiting 60 Seconds for SSH to start\n")
     sleep(60)
