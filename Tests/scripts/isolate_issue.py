c7449107750dfe90437b15538899d18f616e4cf8
Avidan Hessing <ahessing@paloaltonetworks.com>
2019-12-04 17:38:47 +0200

    add information

--- Tests/scripts/isolate_issue.py
+++ Tests/scripts/isolate_issue.py
@@ -1,6 +1,8 @@
 from Tests.test_utils import run_command
 
 
+run_command('pwd', is_silenced=False)
+run_command('find . -name "content_new.zip"', is_silenced=False)
 run_command('unzip -o ./content_test.zip -d ./content_test', is_silenced=False)
 run_command('zip -j ./content_new.zip ./content_test/*', is_silenced=False)
-run_command('rm -rf ./content_test', is_silenced=False)
+# run_command('rm -rf ./content_test', is_silenced=False)
