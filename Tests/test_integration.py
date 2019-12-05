709462cd5b0684eff32600e9f6af76316503d190
dtavori <dtavori@paloaltonetworks.com>
2019-12-05 19:05:25 +0200

    unit test filters - test commit - ignore pr please

--- Tests/test_integration.py
+++ Tests/test_integration.py
@@ -341,7 +341,7 @@ def __print_investigation_error(client, playbook_id, investigation_id, color=LOG
                 print_color('\t- Body: ' + entry['contents'].encode('utf-8'), color)
             info = 'brand: ' + entry['brand'].encode('utf-8') + '/n' \
                 'contents: ' + entry['contents'].encode('utf-8') + '/n' \
-                'type: ' + entry['type'].encode('utf-8') + '/n' \
+                'type: ' + entry['type'] + '/n' \
                 'parentContent: ' + entry['parentContent'].encode('utf-8') + '/n'
             print_color(info, LOG_COLORS.YELLOW)
 
