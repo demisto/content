56c00119b49be6378da60c853bdcfb4dbdc442a3
dtavori <dtavori@paloaltonetworks.com>
2019-12-09 11:21:56 +0200

    ignore pr please

--- Tests/test_integration.py
+++ Tests/test_integration.py
@@ -339,10 +339,14 @@ def __print_investigation_error(client, playbook_id, investigation_id, color=LOG
                 if entry['parentContent']:
                     print_color('\t- Command: ' + entry['parentContent'].encode('utf-8'), color)
                 print_color('\t- Body: ' + entry['contents'].encode('utf-8'), color)
-            info = 'brand: ' + entry.get('brand','').encode('utf-8') + '/n' \
-                'contents: ' + entry.get('contents','').encode('utf-8') + '/n' \
-                'type: ' + str(entry.get('type', -1)) + '/n' \
-                'parentContent: ' + entry.get('parentContent', '').encode('utf-8') + '/n'
+            if entry.get('brand'):
+            info = 'brand: ' + entry['brand'].encode('utf-8') + '/n'
+            if entry.get('contents'):
+                info += 'contents: ' + entry['contents'].encode('utf-8') + '/n'
+            if entry.get('type'):
+                info += 'type: ' + str(entry['type']) + '/n'
+            if entry.get('parentContent'):
+                info += 'parentContent: ' + entry['parentContent'].encode('utf-8') + '/n'
             print_color(info, LOG_COLORS.YELLOW)
 
 
