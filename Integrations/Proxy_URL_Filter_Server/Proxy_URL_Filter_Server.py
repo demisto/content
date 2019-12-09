3edb5544058612e2b3b0adb3408b51e408b32be0
Omer Cohen <omer@paloaltonetworks.com>
2019-12-09 13:52:35 +0100

    added auth and ssl
    Signed-off-by: Omer Cohen <omer@paloaltonetworks.com>

--- Integrations/Proxy_URL_Filter_Server/Proxy_URL_Filter_Server.py
+++ Integrations/Proxy_URL_Filter_Server/Proxy_URL_Filter_Server.py
@@ -1,7 +1,11 @@
 import demistomock as demisto
 from CommonServerPython import *
+import ssl
 import signal
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer  # No error!
+import base64
+import tempfile
+
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 
 def proxysg_filter(ctx):
@@ -21,7 +25,30 @@ def proxysg_filter(ctx):
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    auth = demisto.getParam('auth')
+
+    def check_auth(self):
+        username = self.auth.get('identifier')
+        password = self.auth.get('password')
+        if username and password:
+            auth = base64.b64encode(f'{username}:{password}'.encode()).decode()  # bytes are fun
+            if self.headers.get('Authorization') == f'Basic {auth}':
+                return True
+            else:
+                # auth failed
+                self.send_response(401)
+                self.send_header('WWW-Authenticate', 'Basic realm=\"Secret\"')
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                return False
+        else:
+            # no auth needed
+            return True
+
     def do_GET(self):
+        if not self.check_auth():
+            return
+        print('passed auth')
         self.send_response(200)
         self.send_header('Content-type', 'text')
         self.end_headers()
@@ -32,8 +59,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         if format_filter == "Symantec ProxySG":
             body = proxysg_filter(ctx)
 
-        self.wfile.write(bytes(body, "utf8"))
-        return
+        self.wfile.write(body.encode())
 
 
 def add_url_command(args):
@@ -46,7 +72,7 @@ def add_url_command(args):
     ctx[cat] = list(set(urls))
     demisto.setIntegrationContext(ctx)
 
-    return 'Done'
+    return_outputs('Done')
 
 
 def remove_url_command(args):
@@ -60,48 +86,71 @@ def remove_url_command(args):
         return_error(f'{url} not in block list for category {cat}')
         return
 
-    ctx[cat] = list(set(urls))
+    lst = list(set(urls))
+    if lst:
+        ctx[cat] = list(set(urls))
+    else:
+        del ctx[cat]
     demisto.setIntegrationContext(ctx)
 
-    return 'Done'
+    return_outputs('Done')
+
+
+def list_urls_command():
+    ctx = demisto.getIntegrationContext()
+    return_outputs(None, {"ProxyURL": {"URLs": ctx}}, ctx)
 
 
 def run_long_running(port):
+    port = int(port)
+
+    ssl_cert = demisto.getParam('auth').get('credentials', {}).get('sshkey')
     try:
         httpd = ThreadingHTTPServer(('0.0.0.0', port), RequestHandler)
+        if ssl_cert:
+            with tempfile.NamedTemporaryFile(delete=False) as fh:
+                fh.write(ssl_cert.encode())
+            httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile=fh.name, ssl_version=ssl.PROTOCOL_TLSv1)
+
         signal.signal(signal.SIGTERM, httpd.shutdown)
         signal.signal(signal.SIGINT, httpd.shutdown)
-        httpd.serve_forever()
-    except Exception:
+        if demisto.command() != 'test-module':
+            httpd.serve_forever()
+    except Exception as e:
+        print(str(e))
         httpd.shutdown()
         raise
+
+
+def test_module():
+    try:
+        run_long_running(demisto.getParam('longRunningPort'))
+        demisto.results('ok')
+    except Exception as ex:
+        demisto.results(ex)
 
 
 def main():
     try:
         if demisto.command() == 'test-module':
-            # validate that the port is integer
-            int(demisto.getParam('longRunningPort'))
+            test_module()
 
-            demisto.results('ok')
-            return
+        elif demisto.command() == 'proxy-filter-add-url':
+            add_url_command(demisto.args())
 
-        if demisto.command() == 'proxy-filter-add-url':
-            result = add_url_command(demisto.args())
-            demisto.results(result)
-            return
+        elif demisto.command() == 'proxy-filter-del-url':
+            remove_url_command(demisto.args())
 
-        if demisto.command() == 'proxy-filter-del-url':
-            result = remove_url_command(demisto.args())
-            demisto.results(result)
-            return
+        elif demisto.command() == 'proxy-filter-list-urls':
+            list_urls_command()
 
-        if demisto.command() == 'long-running-execution':
-            port = int(demisto.getParam('longRunningPort'))
-            run_long_running(port)
+        elif demisto.command() == 'long-running-execution':
+            run_long_running(demisto.getParam('longRunningPort'))
 
     except Exception as ex:
+        print(str(ex))
         return_error(f'Failed to run {demisto.command()}. Error: {str(ex)}', ex)
+        raise
 
 
 if __name__ in ("__builtin__", "builtins"):
