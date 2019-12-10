25f32c30fd0dc2348ae63e3e1896329c5653e88a
esharf <esahrf@paloaltonetworks.com>
2019-12-10 13:02:42 +0200

    ready

--- Integrations/McAfee_DXL/McAfee_DXL_test.py
+++ Integrations/McAfee_DXL/McAfee_DXL_test.py
@@ -4,12 +4,12 @@ import pytest
 data_test_get_trust_level_key = ['0', '1', '15', '30', '50', '70', '85', '99', '100']
 data_test_get_trust_level_key = [(k, k) for k in data_test_get_trust_level_key]
 data_test_raise_get_trust_level_key = ['-', '', '-1']
-data_test_push_ip = ['0.0.0.0', '-.-.-.-', '1.1.1']
+data_test_push_ip = ['-.-.-.-', '1.1.1']
 
 
 @pytest.mark.parametrize(argnames="input_key, output", argvalues=data_test_get_trust_level_key)
 def test_get_trust_level_key(input_key, output):
-    assert get_trust_level_key(TRUST_LEVELS.get(input_key)) == output, \
+    assert get_trust_level_key(EventSender.TRUST_LEVELS.get(input_key)) == output, \
         f"error in McAfee_DXL get_trust_level_key() key = {input_key}"
 
 
@@ -25,8 +25,5 @@ def test_raise_get_trust_level_key(input_key):
 
 
 @pytest.mark.parametrize(argnames='input_ip', argvalues=data_test_push_ip)
-def test_push_ip(input_ip):
-    try:
-        push_ip(input_ip, '')
-    except Exception as return_error:
-        assert str(return_error) == f'argument ip {input_ip} is not a valid IP'
+def test_is_ip_valid(input_ip):
+    assert not is_ip_valid(input_ip), f'argument ip {input_ip} is not a valid IP'
