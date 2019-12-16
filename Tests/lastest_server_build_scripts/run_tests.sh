2518069c60727739ee5f5758b5db61be75d122fe
guykeller <g12k34ppp>
2019-12-16 14:18:06 +0200

    Wrapped up test prints

--- Tests/lastest_server_build_scripts/run_tests.sh
+++ Tests/lastest_server_build_scripts/run_tests.sh
@@ -9,4 +9,4 @@ DEMISTO_API_KEY=$(cat $SECRET_CONF_PATH | jq '.temp_apikey')
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 
 echo "Starts tests with server url - $SERVER_URL"
-python ./Tests/test_content.py -a "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CIRCLE_BUILD_NUM" -g "$CIRCLE_BRANCH"
+python ./Tests/test_content.py -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CIRCLE_BUILD_NUM" -g "$CIRCLE_BRANCH"
