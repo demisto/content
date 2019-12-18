11eac2cafc16fb9c0ad81d672cc79ece8401b4f2
Avidan Hessing <46294017+avidan-H@users.noreply.github.com>
2019-12-18 20:20:12 +0200

    Configure and test fix (#5201)
    * add tests reporting status
    
    * add exit on error to run_tests.sh
    
    * fix exit status code for run_tests.sh

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -22,8 +22,15 @@ USERNAME=$temp
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 [ -n "${MEM_CHECK}" ] && MEM_CHECK=true || MEM_CHECK=false
 
+code_1=0
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
-  python ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -g "$GIT_SHA1" --ami_env "$1" 
+  python ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -g "$GIT_SHA1" --ami_env "$1"
+  code_1=$?
 fi
 
 python ./Tests/test_content.py -u "$USERNAME" -p "$PASSWORD" -s "$SERVER_URL" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CIRCLE_BUILD_NUM" -g "$CIRCLE_BRANCH" -m "$MEM_CHECK" --isAMI true -d "$1"
+
+code_2=$?
+let "exit_code = $code_1 + $code_2"
+
+exit $exit_code
