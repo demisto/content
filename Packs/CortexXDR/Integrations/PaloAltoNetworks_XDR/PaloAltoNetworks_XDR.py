8a4e1ea235716042fb6571342cc6c26a37596ec4
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2019-12-24 20:21:57 +0200

    check for proxy recording fix

--- Packs/CortexXDR/Integrations/PaloAltoNetworks_XDR/PaloAltoNetworks_XDR.py
+++ Packs/CortexXDR/Integrations/PaloAltoNetworks_XDR/PaloAltoNetworks_XDR.py
@@ -64,6 +64,87 @@ def create_auth(api_key):
 
 
 class Client(BaseClient):
+    def __init__(self, base_url, verify=True, proxy=False, ok_codes=tuple(), headers=None, auth=None):
+        self._base_url = base_url
+        self._verify = verify
+        self._ok_codes = ok_codes
+        self._headers = headers
+        self._auth = auth
+        self._session = requests.Session()
+        if not proxy:
+            self._session.trust_env = False
+
+    def _http_request(self, method, url_suffix, full_url=None, headers=None,
+                      auth=None, json_data=None, params=None, data=None, files=None,
+                      timeout=10, resp_type='json', ok_codes=None, **kwargs):
+        try:
+            # Replace params if supplied
+            address = full_url if full_url else self._base_url + url_suffix
+            headers = headers if headers else self._headers
+            auth = auth if auth else self._auth
+            # Execute
+
+            res = self._session.request(
+                method,
+                address,
+                verify=self._verify,
+                params=params,
+                data=data,
+                json=json_data,
+                files=files,
+                headers=headers,
+                auth=auth,
+                timeout=timeout,
+                **kwargs
+            )
+            # Handle error responses gracefully
+            if not self._is_status_code_valid(res, ok_codes):
+                err_msg = 'Error in API call [{}] - {}' \
+                    .format(res.status_code, res.reason)
+                try:
+                    # Try to parse json error response
+                    error_entry = res.json()
+                    err_msg += '\n{}'.format(json.dumps(error_entry))
+                    raise DemistoException(err_msg)
+                except ValueError as exception:
+                    raise DemistoException(err_msg, exception)
+
+            resp_type = resp_type.lower()
+            try:
+                if resp_type == 'json':
+                    return res.json()
+                if resp_type == 'text':
+                    return res.text
+                if resp_type == 'content':
+                    return res.content
+                if resp_type == 'xml':
+                    ET.parse(res.text)
+                return res
+            except ValueError as exception:
+                raise DemistoException('Failed to parse json object from response: {}'
+                                       .format(res.content), exception)
+        except requests.exceptions.ConnectTimeout as exception:
+            err_msg = 'Connection Timeout Error - potential reasons might be that the Server URL parameter' \
+                      ' is incorrect or that the Server is not accessible from your host.'
+            raise DemistoException(err_msg, exception)
+        except requests.exceptions.SSLError as exception:
+            err_msg = 'SSL Certificate Verification Failed - try selecting \'Trust any certificate\' checkbox in' \
+                      ' the integration configuration.'
+            raise DemistoException(err_msg, exception)
+        except requests.exceptions.ProxyError as exception:
+            err_msg = 'Proxy Error - if the \'Use system proxy\' checkbox in the integration configuration is' \
+                      ' selected, try clearing the checkbox.'
+            raise DemistoException(err_msg, exception)
+        except requests.exceptions.ConnectionError as exception:
+            # Get originating Exception in Exception chain
+            error_class = str(exception.__class__)
+            err_type = '<' + error_class[error_class.find('\'') + 1: error_class.rfind('\'')] + '>'
+            err_msg = '\nError Type: {}\nError Number: [{}]\nMessage: {}\n' \
+                      'Verify that the server URL parameter' \
+                      ' is correct and that you have access to the server from your host.' \
+                .format(err_type, exception.errno, exception.strerror)
+            raise DemistoException(err_msg, exception)
+
     def test_module(self, first_fetch_time):
         """
             Performs basic get request to get item samples
@@ -982,6 +1063,12 @@ def main():
         "x-xdr-auth-id": str(api_key_id),
         "Authorization": api_key_hash
     }
+
+    # if not demisto.params().get('proxy'):
+    #     del os.environ['HTTP_PROXY']
+    #     del os.environ['HTTPS_PROXY']
+    #     del os.environ['http_proxy']
+    #     del os.environ['https_proxy']
 
     client = Client(
         base_url=base_url,
