2823464f2e981149a1d2af34a363052fbbed3561
Jochman <bhochman@paloaltonetworks.com>
2019-12-30 10:56:09 +0200

    remove errors from Slack

--- Tests/instance_notifier.py
+++ Tests/instance_notifier.py
@@ -54,7 +54,7 @@ def test_instances(secret_conf_path, server, username, password):
             )
             if not instance_id:
                 print_error('Failed to create instance of {} with message: {}'.format(integration_name, failure_message))
-                failed_integrations.append("{0} {1} - failure message: {2} devops comments: {3}".format(
+                failed_integrations.append("{0} {1} - devops comments: {3}".format(
                     integration_name, product_description, failure_message, devops_comments))
             else:
                 instance_ids.append(instance_id)
@@ -70,7 +70,7 @@ def get_attachments(secret_conf_path, server, user, password, build_url):
     fields = []
     if failed_integration:
         field_failed_tests = {
-            "title": "{0} Problematic Instances".format(len(failed_integration)),
+            "title": "Found {0} Problematic Instances. See CircleCI for errors.".format(len(failed_integration)),
             "value": '\n'.join(failed_integration),
             "short": False
         }
