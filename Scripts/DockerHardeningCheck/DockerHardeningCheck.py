5399e67b0ed5be472d50db8bbcc169f6ea9ceeb9
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-01-01 17:29:08 +0200

    start of hardening script

---
+++ Scripts/DockerHardeningCheck/DockerHardeningCheck.py
@@ -0,0 +1,103 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+from multiprocessing import Process, Pool
+import resource
+import re
+
+
+def big_string(size):
+    s = 'a' * 1024
+    while len(s) < size:
+        s = s * 2
+    return len(s)
+
+
+def mem_size_to_bytes(mem: str) -> int:
+    res = re.match(r'(\d+)\s*([gm])?b?', mem, re.IGNORECASE)
+    if not res:
+        raise ValueError("Failed parsing memory string: {}".format(mem))
+    b = int(res.group(1))
+    if res.group(2):
+        b = b * 1024 * 1024  # convert to mega byte
+        if res.group(2).lower() == 'g':
+            b = b * 1024  # convert to giga
+    return b
+
+
+def check_memory(target_mem: str) -> str:
+    """Check allocating memory
+
+    Arguments:
+        target_mem {str} -- target memory size. Can specify as 1g 1m and so on
+
+    Returns:
+        str -- error string if failed
+    """
+    size = mem_size_to_bytes(target_mem)
+    LOG("starting process to check memory of size: {}".format(size))
+    p = Process(target=big_string, args=(size, ))
+    p.start()
+    p.join()
+    LOG("memory intensive process status code: {}".format(p.exitcode))
+    if p.exitcode == 0:
+        return ("Succeeded allocating memory of size: {}. "
+                "It seems that you haven't limited the available memory to the docker container.".format(target_mem))
+    return ""
+
+
+def check_pids(pid_num: int) -> str:
+    LOG("Starting pid check for: {}".format(pid_num))
+    try:
+        p = Pool(pid_num)
+        p.close()
+        return ("Succeeded creating process pool of size: {}. "
+                "It seems that you haven't limited the available pids to the docker container.".format(pid_num))
+    except Exception as ex:
+        LOG("Pool startup failed (as expected): {}".format(ex))
+    return ""
+
+
+def check_fd_limits(soft, hard) -> str:
+    s, h = resource.getrlimit(resource.RLIMIT_NOFILE)
+    if s > soft:
+        return "Soft limit: {} is above target: {}".format(s, soft)
+    if h > hard:
+        return "Hard limit: {} is above target: {}".format(h, hard)
+    return ""
+
+
+def check_non_root():
+    uid = os.getuid()
+    if uid == 0:
+        return ("Running as root with uid: {}."
+                " It seems that you haven't set the docker container to run with non-root internal user.".format(uid))
+    return ""
+
+
+def main():
+    mem = demisto.args().get('memory', "1g")
+    pids = demisto.args().get('pids', 256)
+    fds_soft = demisto.args().get('fds_soft', 1024)
+    fds_hard = demisto.args().get('fds_hard', 8192)
+    success = "Passed"
+    res = {
+        "Non-root User Check": check_non_root() or success,
+        "Memory Check": check_memory(mem) or success,
+        "Pids Check": check_pids(pids) or success,
+        "File Descriptors Check": check_fd_limits(fds_soft, fds_hard) or success,
+    }
+    table = tableToMarkdown("Docker Hardening Results Check", res)
+    return_outputs(table)
+    failed = False
+    for v in res.values():
+        if v != success:
+            failed = True
+    if failed:
+        return_error("Failed verifying docker hardening. "
+                     "More details at: https://support.demisto.com/hc/en-us/articles/360040922194")
+
+
+# python2 uses __builtin__ python3 uses builtins
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
