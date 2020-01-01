615bc144ad2287e606ed2851c2af1ee1056dbf22
liorblob <lblobstein@paloaltonetworks.com>
2020-01-01 11:29:33 +0200

    rename, tests

--- Integrations/SyslogSender/SyslogSender.py
+++ Integrations/SyslogSender/SyslogSender.py
@@ -13,7 +13,7 @@ import socket
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
-''' GLOBALS/PARAMS '''
+''' CONSTANTS '''
 
 
 PLAYGROUND_INVESTIGATION_TYPE = 9
@@ -33,16 +33,29 @@ SEVERITY_DICT = {
     'Critical': 4
 }
 
+''' Syslog Manager '''
 
-class SysLogManager:
+
+class SyslogManager:
     def __init__(self, address: str, port: int, protocol: str, logging_level: int):
+        """
+        Class for managing instances of a syslog logger.
+        :param address: The IP address of the syslog server.
+        :param port: The port of the syslog server.
+        :param protocol: The messaging protocol (TCP / UDP).
+        :param logging_level: The logging level.
+        """
         self.address = address
         self.port = port
         self.protocol = protocol
         self.logging_level = logging_level
 
-    @contextmanager
+    @contextmanager  # type: ignore[misc, arg-type]
     def get_logger(self) -> logging.Logger:
+        """
+        Get a new instance of a syslog logger.
+        :return: syslog logger
+        """
         handler = self._get_handler()
         syslog_logger = self._init_logger(handler)
         try:
@@ -52,12 +65,21 @@ class SysLogManager:
             handler.close()
 
     def _get_handler(self) -> SysLogHandler:
+        """
+        Get a syslog handler for a logger according to provided parameters.
+        :return: syslog handler
+        """
         if self.protocol == 'tcp':
             return SysLogHandler((self.address, self.port), socktype=socket.SOCK_STREAM)
         else:
             return SysLogHandler((self.address, self.port))
 
     def _init_logger(self, handler: SysLogHandler) -> logging.Logger:
+        """
+        Initialize a logger with a syslog handler.
+        :param handler: A syslog handler
+        :return: A syslog logger
+        """
         syslog_logger = logging.getLogger('SysLogLogger')
         syslog_logger.setLevel(self.logging_level)
         syslog_logger.addHandler(handler)
@@ -65,18 +87,46 @@ class SysLogManager:
         return syslog_logger
 
 
-def init_logger(params: dict) -> SysLogManager:
-    address = params.get('address')
+''' HELPER FUNCTIONS '''
+
+
+def init_manager(params: dict) -> SyslogManager:
+    """
+    Create a syslog manager instance according to provided parameters.
+    :param params: Parameters for the syslog manager.
+    :return: syslog manager
+    """
+    address = params.get('address', '')
     port = int(params.get('port', 514))
-    protocol = params.get('protocol').lower()
+    protocol = params.get('protocol', 'udp').lower()
     logging_level = LOGGING_LEVEL_DICT.get(params.get('logging_level', 'INFO'), logging.INFO)
 
-    return SysLogManager(address, port, protocol, logging_level)
+    return SyslogManager(address, port, protocol, logging_level)
+
+
+def send_log(manager: SyslogManager, message: str, log_level: str):
+    """
+    Use a syslog manager to get a logger and send a message to syslog.
+    :param manager: The syslog manager
+    :param message: The message to send
+    :param log_level: The logging level
+    """
+    with manager.get_logger() as syslog_logger:   # type: logging.Logger
+        if log_level == 'DEBUG':
+            syslog_logger.debug(message)
+        if log_level == 'INFO':
+            syslog_logger.info(message)
+        if log_level == 'WARNING':
+            syslog_logger.warning(message)
+        if log_level == 'ERROR':
+            syslog_logger.error(message)
+        if log_level == 'CRITICAL':
+            syslog_logger.critical(message)
 
 
 def check_for_mirrors():
     """
-    Checks for newly created mirrors and updates the server accordingly
+    Check for newly created mirrors and update the server accordingly
     """
     integration_context = demisto.getIntegrationContext()
     if integration_context.get('mirrors'):
@@ -96,7 +146,7 @@ def check_for_mirrors():
 
 def mirror_investigation():
     """
-    Updates the integration context with a new or existing mirror.
+    Update the integration context with a new or existing mirror.
     """
     mirror_type = demisto.args().get('type', 'all')
 
@@ -105,17 +155,22 @@ def mirror_investigation():
     if investigation.get('type') == PLAYGROUND_INVESTIGATION_TYPE:
         return_error('Can not perform this action in playground.')
 
+    investigation_id = investigation.get('id')
     integration_context = demisto.getIntegrationContext()
+    if 'mirrors' not in integration_context:
+        mirrors: list = []
+    else:
+        mirrors = json.loads(integration_context['mirrors'])
+
+    mirror_filter = list(filter(lambda m: m['investigation_id'] == investigation_id, mirrors))
+    if mirror_filter:
+        # Delete existing mirror
+        mirrors.pop(mirrors.index(mirror_filter[0]))
     mirror = {
         'investigation_id': investigation.get('id'),
         'mirror_type': mirror_type,
         'mirrored': False
     }
-
-    if 'mirrors' not in integration_context:
-        mirrors = []
-    else:
-        mirrors = json.loads(integration_context['mirrors'])
 
     mirrors.append(mirror)
     demisto.setIntegrationContext({'mirrors': json.dumps(mirrors)})
@@ -123,13 +178,17 @@ def mirror_investigation():
     demisto.results('Investigation mirrored successfully.')
 
 
-def syslog_send(manager: SysLogManager, min_severity: int):
+''' Syslog send command '''
+
+
+def syslog_send(manager: SyslogManager, min_severity: int):
     """
-    Sends a message to syslog
+    Send a message to syslog
     """
     message = demisto.args().get('message', '')
     entry = demisto.args().get('entry')
     ignore_add_url = demisto.args().get('ignoreAddURL', False)
+    log_level = demisto.args().get('log_level')
     severity = demisto.args().get('severity')  # From server
     message_type = demisto.args().get('messageType', '')  # From server
 
@@ -164,37 +223,44 @@ def syslog_send(manager: SysLogManager, min_severity: int):
                 if link:
                     if entry:
                         link += '/' + entry
-                    message += '\n{} {}'.format('View it on:', link)
+                    message += ' {}'.format(link)
             else:
                 link = server_links.get('server', '')
                 if link:
-                    message += '\n{} {}'.format('View it on:', link + '#/home')
+                    message += ' {}#/home'.format(link)
 
-    with manager.get_logger() as syslog_logger:
+    if not log_level:
         if severity == SEVERITY_DICT['Critical']:
-            syslog_logger.critical(message)
+            log_level = 'CRITICAL'
         else:
-            syslog_logger.info(message)
+            log_level = 'INFO'
+
+    send_log(manager, message, log_level)
+
+    demisto.results('Message sent to syslog successfully.')
 
 
 def long_running_main():
+    """
+    Loop for the long running process.
+    """
     while True:
         check_for_mirrors()
         time.sleep(5)
 
 
-''' COMMANDS MANAGER / SWITCH PANEL '''
+''' MAIN '''
 
 
 def main():
     LOG('Command being called is %s' % (demisto.command()))
 
-    syslog_manager = init_logger(demisto.params())
+    syslog_manager = init_manager(demisto.params())
     min_severity = SEVERITY_DICT.get(demisto.params().get('severity', 'Low'), 1)
 
     try:
         if demisto.command() == 'test-module':
-            with syslog_manager.get_logger() as syslog_logger:
+            with syslog_manager.get_logger() as syslog_logger:  # type: logging.Logger
                 syslog_logger.info('This is a test')
             demisto.results('ok')
         elif demisto.command() == 'mirror-investigation':
@@ -203,12 +269,9 @@ def main():
             syslog_send(syslog_manager, min_severity)
         elif demisto.command() == 'long-running-execution':
             long_running_main()
-
-    # Log exceptions
     except Exception as e:
         LOG(e)
         LOG.print_log()
-        raise
         return_error(str(e))
 
 
