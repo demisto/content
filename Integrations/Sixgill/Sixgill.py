fb111951ca61a500eff9b4a94c23100cf4dc3baa
bensterenson <ben@cybersixgill.com>
2020-01-01 10:50:22 +0200

    sixgill integration script

--- Integrations/Sixgill/Sixgill.py
+++ Integrations/Sixgill/Sixgill.py
@@ -7,65 +7,35 @@ import json
 import requests
 from distutils.util import strtobool
 from sixgill.sixgill_alert_client import SixgillAlertClient
+from sixgill.sixgill_request_classes.sixgill_auth_request import SixgillAuthRequest
 
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
 ''' GLOBALS/PARAMS '''
 
-USERNAME = demisto.params().get('credentials').get('identifier')
-PASSWORD = demisto.params().get('credentials').get('password')
-TOKEN = demisto.params().get('token')
-# Remove trailing slash to prevent wrong URL path to service
-SERVER = demisto.params()['url'][:-1] \
-    if (demisto.params()['url'] and demisto.params()['url'].endswith('/')) else demisto.params()['url']
-# Should we use SSL
-USE_SSL = not demisto.params().get('insecure', False)
-# How many time before the first fetch to retrieve incidents
-FETCH_TIME = demisto.params().get('fetch_time', '3 days')
-# Service base URL
-BASE_URL = SERVER + '/api/v2.0/'
-# Headers to be sent in requests
-HEADERS = {
-    'Authorization': 'Token ' + TOKEN + ':' + USERNAME + PASSWORD,
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
+CHANNEL_CODE = '31335bc6357f0eb6e3370803b9a6a318'
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+DEMISTO_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+FETCH_TIME = demisto.params().get('fetch_time', '30 days')
+THREAT_LEVEL_TO_SEVERITY = {
+    'imminent': 3,
+    'emerging': 2
 }
-# Remove proxy if not set to true in params
-if not demisto.params().get('proxy'):
-    del os.environ['HTTP_PROXY']
-    del os.environ['HTTPS_PROXY']
-    del os.environ['http_proxy']
-    del os.environ['https_proxy']
-
-
 ''' HELPER FUNCTIONS '''
 
 
-def http_request(method, url_suffix, params=None, data=None):
-    # A wrapper for requests lib to send our requests and handle requests and responses better
-    res = requests.request(
-        method,
-        BASE_URL + url_suffix,
-        verify=USE_SSL,
-        params=params,
-        data=data,
-        headers=HEADERS
-    )
-    # Handle error responses gracefully
-    if res.status_code not in {200}:
-        return_error('Error in API call to Example Integration [%d] - %s' % (res.status_code, res.reason))
-
-    return res.json()
-
-
 def item_to_incident(item):
-    incident = {}
-    # Incident Title
-    incident['name'] = 'Example Incident: ' + item.get('name')
-    # Incident occurrence time, usually item creation date in service
-    incident['occurred'] = item.get('createdDate')
-    # The raw response from the service, providing full info regarding the item
+    incident = dict()
+    incident['name'] = item.get('title', 'Sixgill Alert')
+    incident_date = datetime.strptime(item.get('date'), DATETIME_FORMAT)
+    incident['occurred'] = incident_date.strftime(DEMISTO_DATETIME_FORMAT)
+    incident['details'] = item.get('content', 'No details')
+    incident['severity'] = THREAT_LEVEL_TO_SEVERITY[item.get('threat_level', 0)]
+    incident['type'] = 'SixgillAlert'
+    item.pop('user_id', None)
+    item.pop('id', None)
+    item.pop('severity', None)
     incident['rawJSON'] = json.dumps(item)
     return incident
 
@@ -75,51 +45,30 @@ def item_to_incident(item):
 
 def test_module():
     """
-    Performs basic get request to get item samples
+    Performs basic Auth request
     """
-    samples = http_request('GET', 'items/samples')
-
-
-def get_items_request(item_ids, is_active):
-    # The service endpoint to request from
-    endpoint_url = 'items'
-    # Dictionary of params for the request
-    params = {
-        'ids': item_ids,
-        'isActive': is_active
-    }
-    # Send a request using our http_request wrapper
-    response = http_request('GET', endpoint_url, params)
-    # Check if response contains errors
-    if response.get('errors'):
-        return_error(response.get('errors'))
-    # Check if response contains any data to parse
-    if 'data' in response:
-        return response.get('data')
-    # If neither was found, return back empty results
-    return {}
+    response = SixgillAuthRequest(CHANNEL_CODE, demisto.params()['client_id'], demisto.params()['client_secret']).send()
+    if not response.ok:
+        raise Exception("Auth request failed - please verify client_id, and client_secret.")
 
 
 def fetch_incidents():
     last_run = demisto.getLastRun()
-    # Get the last fetch time, if exists
     last_fetch = last_run.get('time')
 
-    # Handle first time fetch, fetch incidents retroactively
     if last_fetch is None:
         last_fetch, _ = parse_date_range(FETCH_TIME, to_timestamp=True)
 
-    incidents = []
-    items = get_items_request()
-    for item in items:
-        incident = item_to_incident(item)
-        incident_date = date_to_timestamp(incident['occurred'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        # Update last run and add incident if the incident is newer than last fetch
-        if incident_date > last_fetch:
-            last_fetch = incident_date
-            incidents.append(incident)
+    sixgill_alert_client = SixgillAlertClient(demisto.params()['client_id'], demisto.params()['client_secret'],
+                                              CHANNEL_CODE)
 
-    demisto.setLastRun({'time' : last_fetch})
+    incidents = []
+    for alert in sixgill_alert_client.get_alerts():
+        sixgill_alert_client.mark_digested_item(alert)
+        incident = item_to_incident(alert)
+        incidents.append(incident)
+
+    demisto.setLastRun({'time': last_fetch})
     demisto.incidents(incidents)
 
 
@@ -129,15 +78,15 @@ LOG('Command being called is %s' % (demisto.command()))
 
 try:
     if demisto.command() == 'test-module':
-        # This is the call made when pressing the integration test button.
         test_module()
         demisto.results('ok')
+
     elif demisto.command() == 'fetch-incidents':
-        # Set and define the fetch incidents command to run after activated via integration settings.
         fetch_incidents()
 
 # Log exceptions
-except Exception, e:
+except Exception as e:
     LOG(e.message)
     LOG.print_log()
     raise
+
