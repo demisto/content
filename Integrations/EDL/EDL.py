91e8f1c77b3fdc434d6612a62285094f36f7ab02
Dean Arbel <darbel@paloaltonetworks.com>
2020-01-01 14:56:33 +0200

    altered edl to indicators query based service

--- Integrations/EDL/EDL.py
+++ Integrations/EDL/EDL.py
@@ -7,6 +7,7 @@ from tempfile import NamedTemporaryFile
 
 ''' GLOBAL VARIABLES '''
 INTEGRATION_NAME: str = 'EDL'
+PAGE_SIZE: int = 100
 APP: Flask = Flask('demisto-edl')
 
 ''' HELPER FUNCTIONS '''
@@ -34,23 +35,64 @@ def get_params_port(params: dict = demisto.params()) -> int:
                 port = int(port_mapping.split(':')[1])
             else:
                 port = int(port_mapping)
+        else:
+            raise ValueError('Please provide a Listen Port.')
     except (ValueError, TypeError):
-        return_error(f'EDL port must be an integer. {port_mapping} is not valid.')
+        raise ValueError(f'Listen Port must be an integer. {port_mapping} is not valid.')
     return port
 
 
-''' COMMAND FUNCTIONS '''
+def refresh_indicators_cache(indicator_query):
+    iocs = []
+    page = 0
+    fetched_iocs = demisto.findIndicators(query=indicator_query, page=page, size=PAGE_SIZE).get('iocs')
+    iocs.extend(fetched_iocs)
+    while len(fetched_iocs) == PAGE_SIZE:
+        page += 1
+        fetched_iocs = demisto.findIndicators(query=indicator_query, page=page, size=PAGE_SIZE).get('iocs')
+        iocs.extend(fetched_iocs)
+    ctx = {}
+    for ioc in iocs:
+        value = ioc.get('value', '')
+        if value:
+            ctx[value] = ioc
+    demisto.setLastRun({'last_run': date_to_timestamp(datetime.now())})
+    demisto.setIntegrationContext(ctx)
+    return list(ctx.values())
 
 
+def get_edl_values():
+    on_demand = demisto.params().get('on_demand')
+    # on_demand ignores cache
+    if on_demand:
+        values = demisto.getIntegrationContext()
+    else:
+        last_run = demisto.getLastRun().get('last_run')
+        indicator_query = demisto.params().get('indicators_query', '')
+        if last_run:
+            cache_refresh_rate = demisto.params().get('cache_refresh_rate')
+            cache_time, _ = parse_date_range(cache_refresh_rate, to_timestamp=True)
+            td = last_run - cache_time
+            if td <= 0:  # last_run is before cache_time
+                values = refresh_indicators_cache(indicator_query)
+            else:
+                values = demisto.getIntegrationContext()
+        else:
+            values = refresh_indicators_cache(indicator_query)
+    return values
+
+
+''' ROUTE FUNCTIONS '''
 @APP.route('/', methods=['GET'])
-def edl_values() -> Response:
+def route_edl_values() -> Response:
     """
     Main handler for values saved in the integration context
     """
-    values = list_to_str(list(demisto.getIntegrationContext().values()))
-    resp = APP.make_response(values)
-    resp.headers['Content-type'] = 'text'
-    return Response(values, status=200)
+    values = list_to_str(get_edl_values())
+    return Response(values, status=200, mimetype='text/plain')
+
+
+''' COMMAND FUNCTIONS '''
 
 
 def test_module(args):
@@ -59,6 +101,23 @@ def test_module(args):
     """
     params = demisto.params()
     get_params_port(params)
+    cache_refresh_rate = params.get('cache_refresh_rate', '')
+    if cache_refresh_rate:
+        # validate $cache_refresh_rate value
+        range_split = cache_refresh_rate.split(' ')
+        if len(range_split) != 2:
+            raise ValueError('Cache Refresh Rate must be "number date_range_unit", examples: (2 hours, 4 minutes,'
+                             '6 months, 1 day, etc.)')
+        if not range_split[1] in ['minute', 'minutes', 'hour', 'hours', 'day', 'days', 'month', 'months', 'year',
+                                  'years']:
+            raise ValueError('Cache Refresh Rate time unit is invalid. Must be minutes, hours, days, months or years')
+        parse_date_range(cache_refresh_rate, to_timestamp=True)
+    on_demand = params.get('on_demand', None)
+    if not on_demand:
+        # validate $indicators_query isn't empty
+        query = params.get('indicators_query')
+        if not query:
+            raise ValueError('"Indicator Query" cannot be empty, please provide a valid query')
     return 'ok', {}, {}
 
 
@@ -68,6 +127,7 @@ def run_long_running(params):
     """
     certificate: str = params.get('certificate', '')
     private_key: str = params.get('key', '')
+    http_server: bool = params.get('http_flag', True)
 
     certificate_path = str()
     private_key_path = str()
@@ -76,7 +136,7 @@ def run_long_running(params):
         port = get_params_port(params)
         ssl_args = dict()
 
-        if certificate and private_key:
+        if certificate and private_key and not http_server:  # TODO: Setup https server and http server when http_server and certificate+private_key
             certificate_file = NamedTemporaryFile(delete=False)
             certificate_path = certificate_file.name
             certificate_file.write(bytes(certificate, 'utf-8'))
@@ -88,10 +148,9 @@ def run_long_running(params):
             private_key_file.write(bytes(private_key, 'utf-8'))
             private_key_file.close()
             ssl_args['keyfile'] = private_key_path
-
-            demisto.info('Starting HTTPS Server')
+            demisto.debug('Starting HTTPS Server')
         else:
-            demisto.info('Starting HTTP Server')
+            demisto.debug('Starting HTTP Server')
 
         server = WSGIServer(('', port), APP, **ssl_args)
         server.serve_forever()
@@ -104,54 +163,14 @@ def run_long_running(params):
         raise ValueError(str(e))
 
 
-def add_to_edl(args):
-    """
-    Adds values to the IntegrationContext (key: value, val: value)
-    """
-    ctx = demisto.getIntegrationContext()
-    values = argToList(args.get('value'))
-    for value in values:
-        ctx[value] = value
-    demisto.setIntegrationContext(ctx)
-    hr = f'{values[0] + " was" if len(values) == 1 else ",".join(map(str, values)) + " were"} successfully ' \
-         f'added to the EDL.'
-    return hr, {}, {}
-
-
-def del_from_edl(args):
-    """
-    Deletes a value from the IntegrationContext (key: value)
-    """
-    ctx = demisto.getIntegrationContext()
-    values = argToList(args.get('value'))
-    for value in values:
-        ctx.pop(value, None)
-    demisto.setIntegrationContext(ctx)
-    hr = f'{values[0] + " was" if len(values) == 1 else list_to_str(values, ",") + " were"} successfully ' \
-         f'removed from the EDL.'
-    return hr, {}, {}
-
-
-def list_edl_values(args):
-    """
-    Lists all values in the edl
-    """
-    values = list(demisto.getIntegrationContext().values())
-    hr = tableToMarkdown('EDL Values', values, headers='Value')
-    return hr, {'EDL(val.Value === obj.Value).Value': values}, values
-
-
-def replace_edl_values(args):
-    """
-    Replaces the value in the EDL with the ones provided by the user
-    """
-    ctx = {}
-    values = argToList(args.get('value'))
-    for value in values:
-        ctx[value] = value
-    demisto.setIntegrationContext(ctx)
-    hr = f'{values[0] + " was" if len(values) == 1 else list_to_str(values, ",") + " were"} successfully ' \
-         f'added to the EDL.'
+def update_edl_command(args):
+    on_demand = demisto.params().get('on_demand')
+    if not on_demand:
+        raise DemistoException(
+            '"Update EDL On Demand" is turned off. If you want to update the EDL manually please turn it on.')
+    query = args.get('query')
+    indicators = refresh_indicators_cache(query)
+    hr = tableToMarkdown('EDL was updated successfully with the following values', indicators, ['indicators'])
     return hr, {}, {}
 
 
@@ -164,10 +183,7 @@ def main():
     demisto.info('Command being called is {}'.format(command))
     commands = {
         'test-module': test_module,
-        'add-to-edl': add_to_edl,
-        'del-from-edl': del_from_edl,
-        'list-edl-values': list_edl_values,
-        'replace-edl-values': replace_edl_values
+        'update-edl': update_edl_command
     }
 
     try:
