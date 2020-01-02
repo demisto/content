35e2aef63f05951e5d84189e4a3e3d394ca64419
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2020-01-02 11:40:35 +0200

    ConvertFile: fix zombie processes (#5299)
    * fix zombie processes. add test for pdf to html. fix lint/test permissions
    
    * Updated description
    
    * check for zombie proceses at end of pdf to html test
    
    Co-authored-by: Alex Fiedler <38628621+kirbles19@users.noreply.github.com>

--- Scripts/ConvertFile/ConvertFile_test.py
+++ Scripts/ConvertFile/ConvertFile_test.py
@@ -1,11 +1,13 @@
-from ConvertFile import main
+from ConvertFile import main, find_zombie_processes
 import demistomock as demisto
 from CommonServerPython import entryTypes
 import logging
 import pytest
 import glob
 import os
+import subprocess
 
+RETURN_ERROR_TARGET = 'ConvertFile.return_error'
 
 # set logging to DEBUG for better understanding when the tests fails
 @pytest.fixture(autouse=True)
@@ -14,8 +16,8 @@ def set_logging(caplog):
 
 
 # these tests use soffice. they will probably fail if running within an editor.
-@pytest.mark.parametrize('file', ['MS-DOCX-190319.docx', 'financial-sample.xslx'])
-def test_conver_to_pdf(mocker, file):
+@pytest.mark.parametrize('file', ['MS-DOCX-190319.docx', 'financial-sample.xslx'])  # disable-secrets-detection
+def test_convert_to_pdf(mocker, file):
     mocker.patch.object(demisto, 'args', return_value={'entry_id': 'test'})
     ext = os.path.splitext(file)[1]
     mocker.patch.object(demisto, 'getFilePath', return_value={"path": "test_data/" + file, 'name': 'test' + ext})
@@ -32,8 +34,8 @@ def test_conver_to_pdf(mocker, file):
     assert glob.glob('./*' + results[0]['FileID'])
 
 
-def test_conver_to_html(mocker):
-    file = 'MS-DOCX-190319.docx'
+def test_convert_to_html(mocker):
+    file = 'MS-DOCX-190319.docx'  # disable-secrets-detection
     mocker.patch.object(demisto, 'args', return_value={'entry_id': 'test', 'format': 'html', 'all_files': 'yes'})
     ext = os.path.splitext(file)[1]
     mocker.patch.object(demisto, 'getFilePath', return_value={"path": "test_data/" + file, 'name': 'test' + ext})
@@ -59,3 +61,62 @@ def test_conver_to_html(mocker):
     assert results[0]['Type'] == entryTypes['file']
     assert results[0]['File'].startswith('test_html_')
     assert glob.glob('./*' + results[0]['FileID'])
+
+
+def test_convert_pdf_to_html(mocker):
+    file = 'text-only.pdf'
+    mocker.patch.object(demisto, 'args', return_value={'entry_id': 'test', 'format': 'html', 'all_files': 'yes'})
+    ext = os.path.splitext(file)[1]
+    mocker.patch.object(demisto, 'getFilePath', return_value={"path": "test_data/" + file, 'name': 'test' + ext})
+    mocker.patch.object(demisto, 'results')
+    # validate our mocks are good
+    assert demisto.args()['entry_id'] == 'test'
+    main()
+    assert demisto.results.call_count == 1
+    results = [x[0] for x in demisto.results.call_args_list if x[0][0]['File'].endswith('.html')][0]
+    assert results[0]['Type'] == entryTypes['file']
+    assert results[0]['File'] == 'test.html'
+    glob_list = glob.glob('./*' + results[0]['FileID'])
+    logging.getLogger().info('glob list for results: {}. list: {}'.format(results[0], glob_list))
+    assert glob_list
+    # check no defunct processed
+    zombies, output = find_zombie_processes()
+    assert not zombies
+    assert 'defunct' not in output
+
+
+def test_convert_failure(mocker):
+    # test with BAD format to see that we fail
+    file = 'MS-DOCX-190319.docx'  # disable-secrets-detection
+    mocker.patch.object(demisto, 'args', return_value={'entry_id': 'test', 'format': 'BAD', 'all_files': 'yes'})
+    ext = os.path.splitext(file)[1]
+    mocker.patch.object(demisto, 'getFilePath', return_value={"path": "test_data/" + file, 'name': 'test' + ext})
+    return_error_mock = mocker.patch(RETURN_ERROR_TARGET)
+    # validate our mocks are good
+    assert demisto.args()['entry_id'] == 'test'
+    main()
+    assert return_error_mock.call_count == 1
+    # call_args last call with a tuple of args list and kwargs
+    err_msg = return_error_mock.call_args[0][0]
+    assert 'BAD' in err_msg
+    assert 'Error: no export filter' in err_msg
+
+
+def test_zombie_prcesses(mocker):
+    ps_output = '''   PID  PPID S CMD
+    1     0 S python /tmp/pyrunner/_script_docker_python_loop.py
+   39     1 Z [soffice.bin] <defunct>
+   55     1 Z [gpgconf] <defunct>
+   57     1 Z [gpgconf] <defunct>
+   59     1 Z [gpg] <defunct>
+   61     1 Z [gpgsm] <defunct>
+   63     1 Z [gpgconf] <defunct>
+   98     1 Z [gpgconf] <defunct>
+  100     1 Z [gpgconf] <defunct>
+  102     1 Z [gpg] <defunct>
+'''
+    mocker.patch.object(subprocess, 'check_output', return_value=ps_output)
+    mocker.patch.object(os, 'getpid', return_value=1)
+    zombies, output = find_zombie_processes()
+    assert len(zombies) == 9
+    assert output == ps_output
