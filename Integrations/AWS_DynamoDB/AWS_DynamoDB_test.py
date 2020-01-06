ce81d1501d119b5c154fa7ff776b9505b8b65ce1
Andrew Shamah <andrew@demisto.com>
2020-01-06 12:14:56 +0200

    Adding tests

--- Integrations/AWS_DynamoDB/AWS_DynamoDB_test.py
+++ Integrations/AWS_DynamoDB/AWS_DynamoDB_test.py
@@ -11,3 +11,32 @@ def test_safe_load_json():
     test_result = safe_load_json(sample_json)
 
     assert isinstance(test_result, dict)
+
+
+def test_remove_empty_elements():
+    from AWS_DynamoDB import remove_empty_elements
+
+    empty_dict = {}
+    sample_dict = {
+        "foo": None
+    }
+    empty_dict_result = remove_empty_elements(sample_dict)
+    assert empty_dict_result == empty_dict
+
+
+def test_parse_tag_field():
+    from AWS_DynamoDB import parse_tag_field
+
+    sample_tag_string = 'key=foo,value=bar;key=baz,value=qux'
+    tags = parse_tag_field(sample_tag_string)
+    expected_result = [
+        {
+            "key": "foo",
+            "value": "bar"
+        },
+        {
+            "key": "baz",
+            "value": "qux"
+        }
+    ]
+    assert tags == expected_result
