d62980bd88f613466fdb0b45189e47271ad92ec9
Meir Wahnon <mwahnon@paloaltonetworks.com>
2020-01-07 12:36:58 +0200

    fix indent

--- Scripts/FindSimilarIncidentsV2/FindSimilarIncidentsV2.py
+++ Scripts/FindSimilarIncidentsV2/FindSimilarIncidentsV2.py
@@ -135,7 +135,7 @@ def get_incidents_by_keys(similar_incident_keys, time_field, incident_time, inci
 
     res = demisto.executeCommand("getIncidents",
                                  {'query': query, 'fromdate': min_date.isoformat(), 'size': max_number_of_results,
-                                 'sort': '%s.desc' % time_field})
+                                  'sort': '%s.desc' % time_field})
     if res[0]['Type'] == entryTypes['error']:
         return_error(str(res[0]['Contents']))
 
