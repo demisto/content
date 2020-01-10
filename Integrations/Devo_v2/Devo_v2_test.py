c907e3a5aa012335266e2edcaa005a550b8d24ca
Anson Fan <anson.fan@devo.com>
2020-01-10 11:53:12 -0800

    Devo_v2 requested changes for more normalized workflows
    Devo_v2.yml:
        - Moved proxy to end of list
        - Changed the types of the JSON filters to long test.
    Devo_v2.py:
        - Renamed check_credentials to check_configuration and supported more config value checks when using test
        - Added in get_time_range to support multiple datetime formats.
        - Added in functionality to ignore insecure connections if setting is toggled.
        - Wraped socket logic in try/finally block
        - Return actual contents that are written instead of length of written contents.
    Devo_v2_description:
        - Additional integration configuration documentation along with expected values. Also links to Devo Docs.
    README.md:
        - Additional information on newly supported date formats as well as enhanced documentation to Devo.
    Devo_v2_test.py:
        - Fixed test for check_configuration.
        - Added in test for get_time_range.

--- Integrations/Devo_v2/Devo_v2_test.py
+++ Integrations/Devo_v2/Devo_v2_test.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 
 from Devo_v2 import fetch_incidents, run_query_command, get_alerts_command,\
     multi_table_query_command, write_to_table_command, write_to_lookup_table_command,\
-    check_credentials
+    check_configuration, get_time_range
 
 MOCK_READER_ENDPOINT = "https://fake.devo.com/query"
 MOCK_LINQ_LINK_BASE = "https://devo.com"
@@ -22,15 +22,18 @@ MOCK_FETCH_INCIDENTS_FILTER = {
     "filters": [
         {
             "key": "foo",
-            "operator": "bar",
+            "operator": "->",
             "value": "baz"
         },
         {
             "key": "baz",
-            "operator": "foo",
+            "operator": "or",
             "value": "bar"
         }
     ]
+}
+MOCK_FETCH_INCIDENTS_DEDUPE = {
+    "cooldown": 120
 }
 MOCK_HIGH_CPU_ALERT = {
     "eventdate": time.time() - 20,
@@ -152,14 +155,35 @@ class MOCK_SENDER(object):
         pass
 
 
+def test_time_range():
+    time_from = time.time() - 60
+    time_to = time.time()
+    time_from_string = '2020-01-10T01:30:30'
+    time_from_string_ts = 1578619830
+    time_to_string = '2020-01-10T02:30:30'
+    time_to_string_ts = 1578623430
+    assert get_time_range(time_from, None)[0] == time_from
+    assert get_time_range(time_from, time_to)[1] == time_to
+    assert get_time_range(str(time_from), None)[0] == time_from
+    assert get_time_range(str(time_from), str(time_to))[1] == time_to
+    assert get_time_range("1 minute", None)[0] >= time_from
+    assert get_time_range("2 minute", None)[0] <= time_from
+    assert get_time_range(time_from_string, None)[0] == time_from_string_ts
+    assert get_time_range(time_from_string, time_to_string)[1] == time_to_string_ts
+
+
 @patch('Devo_v2.READER_ENDPOINT', MOCK_READER_ENDPOINT, create=True)
 @patch('Devo_v2.READER_OAUTH_TOKEN', MOCK_READER_OAUTH_TOKEN, create=True)
 @patch('Devo_v2.WRITER_RELAY', MOCK_WRITER_RELAY, create=True)
 @patch('Devo_v2.WRITE_CREDENTIALS', MOCK_WRITER_CREDENTIALS, create=True)
-@patch('Devo_v2.ds.Reader.query', MOCK_QUERY_RESULTS)
-@patch('Devo_v2.ds.Writer.load', None)
-def test_command():
-    assert check_credentials
+@patch('Devo_v2.FETCH_INCIDENTS_FILTER', MOCK_FETCH_INCIDENTS_FILTER, create=True)
+@patch('Devo_v2.FETCH_INCIDENTS_DEDUPE', MOCK_FETCH_INCIDENTS_DEDUPE, create=True)
+@patch('Devo_v2.ds.Reader.query')
+@patch('Devo_v2.ds.Writer')
+def test_command(mock_query_results, mock_write_args):
+    mock_query_results.return_value = copy.deepcopy(MOCK_QUERY_RESULTS)
+    mock_write_args.return_value = MOCK_WRITER_ARGS
+    assert check_configuration()
 
 
 @patch('Devo_v2.READER_ENDPOINT', MOCK_READER_ENDPOINT, create=True)
@@ -235,7 +259,7 @@ def test_write_devo(mock_load_results, mock_write_args):
     mock_load_results.return_value.load.return_value = MOCK_LINQ_RETURN
     mock_write_args.return_value = MOCK_WRITER_ARGS
     results = write_to_table_command()
-    assert results[0]['EntryContext']['Devo.RecordsWritten'] == 3
+    assert len(results[0]['EntryContext']['Devo.RecordsWritten']) == 3
     assert results[0]['EntryContext']['Devo.LinqQuery'] == 'from whatever.table'
 
 
@@ -249,4 +273,4 @@ def test_write_lookup_devo(mock_lookup_writer_lookup, mock_lookup_writer_sender,
     mock_lookup_writer_sender.return_value = MOCK_SENDER()
     mock_lookup_writer_lookup.return_value = MOCK_LOOKUP()
     results = write_to_lookup_table_command()
-    assert results[0]['EntryContext']['Devo.RecordsWritten'] == 3
+    assert len(results[0]['EntryContext']['Devo.RecordsWritten']) == 3
