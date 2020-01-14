3ab24dcbed2a05dea6bf3d97a30f93ec6fb04a5a
bachen <bachen@paloaltonetworks.com>
2020-01-14 15:57:54 +0200

    Pushing test playbook mimecast

--- Integrations/MimecastV2/mimecast_test.py
+++ Integrations/MimecastV2/mimecast_test.py
@@ -1,20 +1,18 @@
 ''' IMPORTS '''
 from __future__ import print_function
-import demistomock as demisto
-import pytest
 import MimecastV2
 
 # Parameters for Get arguments test
 policy_data = {
-        'description': 'new',
-        'fromPart': 'bla bla',
-        'fromType': 'free_mail_domains',
-        'fromValue': 'gmail.com',
-        'toType': 'email_domain',
-        'toValue': 'gmail.com',
-        'option': 'no_action',
-        'policy_id': 'IDFROMMIMECAST'
-    }
+    'description': 'new',
+    'fromPart': 'bla bla',
+    'fromType': 'free_mail_domains',
+    'fromValue': 'gmail.com',
+    'toType': 'email_domain',
+    'toValue': 'gmail.com',
+    'option': 'no_action',
+    'policy_id': 'IDFROMMIMECAST'
+}
 
 policy_args = policy_data.copy()
 del policy_args['option']
@@ -23,26 +21,26 @@ get_args_response = (policy_args, 'no_action', 'IDFROMMIMECAST')
 
 # Parameters for Update policy test
 policy_obj = {
-        'description': 'new new',
-        'from': {
-            'emailDomain': 'gmail.com',
-            'type': 'free_mail_domains'
-        },
-        'to': {
-            'emailDomain': 'gmail.com',
-            'type': 'email_domain'
-        }
+    'description': 'new new',
+    'from': {
+        'emailDomain': 'gmail.com',
+        'type': 'free_mail_domains'
+    },
+    'to': {
+        'emailDomain': 'gmail.com',
+        'type': 'email_domain'
     }
+}
 
-update_policy_args = {'fromType': 'free_mail_domains', 'description': 'new new', 'policy_id':'IDFROMMIMECAST'}
+update_policy_args = {'fromType': 'free_mail_domains', 'description': 'new new', 'policy_id': 'IDFROMMIMECAST'}
 update_policy_req_response = {
-        'policy': policy_obj,
-        'option': 'no_action',
-        'id': 'IDFROMMIMECAST'
+    'policy': policy_obj,
+    'option': 'no_action',
+    'id': 'IDFROMMIMECAST'
 }
 
 
-def test_get_arguments():
+def get_arguments_for_policy_command():
     res = MimecastV2.get_arguments_for_policy_command('update', policy_data)
     assert get_args_response == res
 
@@ -53,5 +51,3 @@ def test_update_policy(mocker):
     res = MimecastV2.update_policy(update_policy_args)
     assert res['Contents']['Description'] == 'new new'
     assert res['Contents']['Sender']['Type'] == 'free_mail_domains'
-
-
