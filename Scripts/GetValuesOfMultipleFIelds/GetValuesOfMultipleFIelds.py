7f65cda6333696e379a024b3e76e5b68b780d14a
dbaumstein <dbaumstein@paloaltonetwork.com>
2020-01-15 14:47:03 +0200

    Added argument and Yaacobi fixed code.

--- Scripts/GetValuesOfMultipleFIelds/GetValuesOfMultipleFIelds.py
+++ Scripts/GetValuesOfMultipleFIelds/GetValuesOfMultipleFIelds.py
@@ -2,23 +2,26 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
-  def main():
-      args = demisto.args()
-      root = args.get('Key')
-      if not isinstance(root, list):
-          root = [root]
-      keys = args.get('List', '').split(',')
 
-      t = []
-      for obj in root:
-          for _key in keys:
-              temp = obj.get(_key)
-              if temp:
-                  t.extend(temp) if isinstance(temp, list) else t.append(temp)
+def main():
+    args = demisto.args()
+    root = args.get('Key')
+    if not isinstance(root, list):
+        root = [root]
+    keys = args.get('List', '').split(',')
 
-      initial_value = args.get('value')
-      if initial_value:
-          t.extend(initial_value) if isinstance(initial_value, list) else t.append(initial_value)
-      demisto.results(t)
+    t = []
+    for obj in root:
+        for _key in keys:
+            temp = obj.get(_key)
+            if temp:
+                t.extend(temp) if isinstance(temp, list) else t.append(temp)
 
-  if __name__ in ('builtins', '__builtin__'):
+    initial_value = args.get('value')
+    if initial_value:
+        t.extend(initial_value) if isinstance(initial_value, list) else t.append(initial_value)
+    demisto.results(t)
+
+
+if __name__ in ('builtins', '__builtin__'):
+    main()
