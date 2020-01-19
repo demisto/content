b070d45056110c5afbea002a75888ccecf963dde
Dean Arbel <darbel@paloaltonetworks.com>
2020-01-19 10:35:24 +0200

    expanded error handling

--- Scripts/PositiveDetectionsVSDetectionEngines/PositiveDetectionsVSDetectionEngines.py
+++ Scripts/PositiveDetectionsVSDetectionEngines/PositiveDetectionsVSDetectionEngines.py
@@ -4,58 +4,74 @@ from CommonServerUserPython import *
 
 
 def extract_engines_data_from_indicator(indicator_data):
-    if not (indicator_data and 'detectionengines' in indicator_data['CustomFields']
-            and 'positivedetections' in indicator_data['CustomFields']):
-        ext_no_wrn()
+    if not indicator_data:
+        exit_with_message("No indicator found")
+    cstm_fields = indicator_data.get("CustomFields")
+    if not cstm_fields:
+        # No content, so will display 0/0
+        return create_pie(0, 0)
+    elif "detectionengines" not in cstm_fields:
+        exit_with_message('Please provide  Custom Field "Detection Engines"')
+    elif "positivedetections" not in cstm_fields:
+        exit_with_message('Please provide  Custom Field "Positive Detections"')
 
-    detection_engines = indicator_data['CustomFields']['detectionengines']
-    positive_detections = indicator_data['CustomFields']['positivedetections']
+    detection_engines = try_parse_int(
+        cstm_fields["detectionengines"], '"detectionengines" must be an integer'
+    )
+    positive_detections = try_parse_int(
+        cstm_fields["positivedetections"], '"positivedetections" must be an integer'
+    )
 
+    return create_pie(detection_engines, positive_detections)
+
+
+def create_pie(detection_engines, positive_detections):
     data = {
         "Type": 17,
         "ContentsFormat": "pie",
         "Contents": {
             "stats": [
                 {
-                    "data": [
-                        positive_detections
-                    ],
+                    "data": [positive_detections],
                     "groups": None,
                     "name": "Positive Detections",
                     "label": "Positive Detections",
-                    "color": "rgb(255, 23, 68)"
+                    "color": "rgb(255, 23, 68)",
                 },
                 {
-                    "data": [
-                        detection_engines - positive_detections
-                    ],
+                    "data": [detection_engines - positive_detections],
                     "groups": None,
                     "name": "Unknown",
                     "label": "Unknown",
-                    "color": "rgb(255, 144, 0)"
-                }
+                    "color": "rgb(255, 144, 0)",
+                },
             ],
-            "params": {
-                "layout": "vertical"
-            }
-        }
+            "params": {"layout": "vertical"},
+        },
     }
-
     return data
 
 
-def ext_no_wrn():
-    demisto.results("None")
+def exit_with_message(msg="Failed to load"):
+    msg = f"Could not load widget:\n{msg}"
+    demisto.results(msg)
     sys.exit(0)
+
+
+def try_parse_int(num, err_msg):
+    try:
+        return int(num)
+    except (ValueError, TypeError):
+        raise ValueError(err_msg)
 
 
 def main():
     try:
-        indicator_data = demisto.args().get('indicator')
+        indicator_data = demisto.args().get("indicator")
         demisto.results(extract_engines_data_from_indicator(indicator_data))
     except Exception as e:
-        demisto.debug(f'PostiveDetectionsVSDetectiongEngines failed with [{e}]')
-        ext_no_wrn()
+        demisto.debug(f"PostiveDetectionsVSDetectiongEngines failed with [{e}]")
+        exit_with_message(str(e))
 
 
 # python2 uses __builtin__ python3 uses builtins
--- Scripts/PositiveDetectionsVSDetectionEngines/PositiveDetectionsVSDetectionEngines_test.py
+++ Scripts/PositiveDetectionsVSDetectionEngines/PositiveDetectionsVSDetectionEngines_test.py
@@ -116,6 +116,18 @@ positive_zero = {
     'version': 3
 }
 
+positive_missing = {
+    'CustomFields': {
+        'detectionengines': 71,
+    }
+}
+
+detectengines_missing = {
+    'CustomFields': {
+        'positivedetections': 71,
+    }
+}
+
 no_engines_data = {
     'CustomFields': {},  # type: dict[any: any]
     'ManuallyEditedFields': None,
@@ -163,6 +175,14 @@ def test_zero_not_treated_as_none(indicator_data, expected_result):
 
 @pytest.mark.parametrize('indicator_data', [no_engines_data])
 def test_no_engines_data(indicator_data):
+    res = extract_engines_data_from_indicator(indicator_data)
+    stats = res['Contents']['stats']
+    assert stats[0]['data'][0] == 0
+    assert stats[1]['data'][0] == 0
+
+
+@pytest.mark.parametrize('indicator_data', [positive_missing, detectengines_missing])
+def test_missing_fields(indicator_data):
     with pytest.raises(SystemExit) as exc:
         extract_engines_data_from_indicator(indicator_data)
         assert exc.value.code == 0
