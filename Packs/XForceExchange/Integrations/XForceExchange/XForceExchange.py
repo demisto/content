257612b7f0cd8f8ec38b7ff8c4444a17f92c1e03
RazOmry <raz.omri@cyberproof.com>
2020-01-20 16:48:37 +0200

    adding ip & url commands

--- Packs/XForceExchange/Integrations/XForceExchange/XForceExchange.py
+++ Packs/XForceExchange/Integrations/XForceExchange/XForceExchange.py
@@ -1,5 +1,4 @@
-from typing import Tuple
-from typing import Dict
+from typing import Tuple, Dict
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
@@ -15,6 +14,15 @@ class Client(BaseClient):
 
     def ip_report(self, ip: str) -> dict:
         return self._http_request('GET', f'/ipr/{ip}')
+
+    def url_report(self, url: str) -> dict:
+        return self._http_request('GET', f'/url/{url}').get('result')
+
+    def cve_report(self, code: str) -> dict:
+        return self._http_request('GET', f'/vulnerabilities/search/{code}')
+
+    def get_recent_vulnerabilities(self) -> dict:
+        return self._http_request('GET', f'/vulnerabilities')
 
 
 def calculate_score(score: int, threshold: int) -> int:
@@ -46,27 +54,59 @@ def ip_command(client: Client, args: Dict[str, str]) -> Tuple[str, dict, dict]:
     """
 
     threshold = demisto.params().get('threshold', 50)
-    result = client.ip_report(args['ip'])
+    report = client.ip_report(args['ip'])
 
-    outputs = {'Address': result['ip'],
-               'Score': result.get('score'),
-               'Country': result.get('geo', {'country': ''}).get('country'),
-               'Reason': result.get('reason'),
-               'Reason Description': result.get('reasonDescription'),
-               'Subnets': result.get('subnets'),
-               'Vendor': 'XFE',
-               'History': result.get('history')}
-    dbot_score = {'Indicator': result['ip'], 'Type': 'ip', 'Vendor': 'XFE',
-                  'Score': calculate_score(result['score'], threshold)}
+    outputs = {'Address': report['ip'],
+               'Score': report.get('score'),
+               'Geo': {'Country': report.get('geo', {}).get('country', '')},
+               'Malicious': {'Vendor': 'XFE'}
+               }
+    additional_info = {field.title(): report[field] for field in ['reason', 'reasonDescription', 'subnets']}
+    dbot_score = {'Indicator': report['ip'], 'Type': 'ip', 'Vendor': 'XFE',
+                  'Score': calculate_score(report['score'], threshold)}
 
-    context = {'IP(obj.Address==val.Address)': outputs, 'DBotScore': dbot_score}
-    table = outputs.copy()
-    table['Reason'] = f'{table["Reason"]}:\n{table["Reason Description"]}'
-    table['Subnets'] = ', '.join(subnet.get('subnet') for subnet in table['Subnets'])
-    mark_down = tableToMarkdown(f'X-Force IP Reputation for: {result.get("ip")}\n'
-                                f'{XFORCE_URL}/ip/{result.get("ip")}', table, removeNull=True,
-                                headers=['Reason', 'Subnets', 'Score', 'Country'])
-    return mark_down, context, result
+    context = {'IP(obj.Address==val.Address)': outputs,
+               'XForce.IP(obj.Address==val.Address)': additional_info,
+               'DBotScore': dbot_score}
+
+    table = {'Score': report['score'],
+             'Reason': f'{additional_info["Reason"]}:\n{additional_info["ReasonDescription"]}',
+             'Subnets': ', '.join(subnet.get('subnet') for subnet in additional_info['Subnets'])}
+    mark_down = tableToMarkdown(f'X-Force IP Reputation for: {report["ip"]}\n'
+                                f'{XFORCE_URL}/ip/{report["ip"]}', table, removeNull=True)
+
+    return mark_down, context, report
+
+
+def url_command(client: Client, args: Dict[str, str]) -> Tuple[str, dict, dict]:
+    report = client.url_report(args['url'])
+    threshold = demisto.params().get('threshold', 50)
+
+    outputs = {'Data': report['url'], 'Malicious': {'Vendor': 'XFE'}}
+    dbot_score = {'Indicator': report['url'], 'Type': 'url', 'Vendor': 'XFE',
+                  'Score': calculate_score(report['score'], threshold)}
+
+    context = {'URL(obj.Data==val.Data)': outputs, 'DBotScore': dbot_score}
+
+    table = {'Score': report['score'],
+             'Categories': '\n'.join(report['cats'].keys())
+             }
+    mark_down = tableToMarkdown(f'X-Force URL Reputation for: {report["url"]}\n'
+                                f'{XFORCE_URL}/ip/{report["url"]}', table, removeNull=True)
+
+    return mark_down, context, report
+
+
+def domain_command(client: Client, args: Dict[str, str]) -> Tuple[dict, dict, dict]:
+    return {}, {}, {}
+
+
+def cve_latest_command(client: Client, args: Dict[str, str]) -> Tuple[dict, dict, dict]:
+    return {}, {}, {}
+
+
+def cve_search_command(client: Client, args: Dict[str, str]) -> Tuple[dict, dict, dict]:
+    return {}, {}, {}
 
 
 def main():
@@ -79,7 +119,11 @@ def main():
                     use_ssl=not params.get('insecure', False),
                     use_proxy=params.get('proxy', False))
     commands = {
-        'ip': ip_command
+        'ip': ip_command,
+        'url': url_command,
+        'domain': url_command,
+        'cve-latest': cve_latest_command,
+        'cve-search': cve_search_command
     }
 
     command = demisto.command()
