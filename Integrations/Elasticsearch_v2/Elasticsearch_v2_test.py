d2df4f6373c555e37eca49e8c391dd4dcae051b7
rsagi <rsagi@paloaltonetworks.com>
2020-01-21 14:53:46 +0200

    elasticsearch dateutil parse enhancement

--- Integrations/Elasticsearch_v2/Elasticsearch_v2_test.py
+++ Integrations/Elasticsearch_v2/Elasticsearch_v2_test.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from unittest.mock import patch
+from dateutil.parser import parse
 
 """MOCKED RESPONSES"""
 
@@ -355,28 +356,26 @@ def test_context_creation_es6():
 
 
 @patch("Elasticsearch_v2.TIME_METHOD", 'Simple-Date')
-@patch("Elasticsearch_v2.TIME_FORMAT", '%Y-%m-%dT%H:%M:%SZ')
 @patch("Elasticsearch_v2.TIME_FIELD", 'Date')
 @patch("Elasticsearch_v2.FETCH_INDEX", "users")
 def test_incident_creation_e6():
     from Elasticsearch_v2 import results_to_incidents_datetime
-    last_fetch = datetime.strptime('2019-08-29T14:44:00Z', '%Y-%m-%dT%H:%M:%SZ')
+    last_fetch = parse('2019-08-29T14:44:00Z')
     incidents, last_fetch2 = results_to_incidents_datetime(ES_V6_RESPONSE, last_fetch)
 
-    assert str(last_fetch2) == '2019-08-29 14:46:00'
+    assert str(last_fetch2) == '2019-08-29T14:46:00Z'
     assert str(incidents) == MOCK_ES6_INCIDETNS
 
 
 @patch("Elasticsearch_v2.TIME_METHOD", 'Simple-Date')
-@patch("Elasticsearch_v2.TIME_FORMAT", '%Y-%m-%dT%H:%M:%SZ')
 @patch("Elasticsearch_v2.TIME_FIELD", 'Date')
 @patch("Elasticsearch_v2.FETCH_INDEX", "customer")
 def test_incident_creation_e7():
     from Elasticsearch_v2 import results_to_incidents_datetime
-    last_fetch = datetime.strptime('2019-08-27T17:59:00Z', '%Y-%m-%dT%H:%M:%SZ')
+    last_fetch = parse('2019-08-27T17:59:00')
     incidents, last_fetch2 = results_to_incidents_datetime(ES_V7_RESPONSE, last_fetch)
 
-    assert str(last_fetch2) == '2019-08-27 18:01:00'
+    assert str(last_fetch2) == '2019-08-27T18:01:00Z'
     assert str(incidents) == MOCK_ES7_INCIDENTS
 
 
