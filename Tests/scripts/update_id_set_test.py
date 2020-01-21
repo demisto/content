437f39f791855d6ca5beddb9434cbd35fefcfcc7
olichter <olichter@paloaltonetworks.com>
2020-01-21 12:16:36 +0200

    even more pylint fixes

--- Tests/scripts/update_id_set_test.py
+++ Tests/scripts/update_id_set_test.py
@@ -79,31 +79,31 @@ def test_find_duplicates():
     os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
     # create duplicate report
-    temp_report = tempfile.NamedTemporaryFile(mode="w+", prefix='report-',
+    temp_report = tempfile.NamedTemporaryFile(mode="w+", prefix='report-',  # disable-secrets-detection
                                               suffix='.json', dir='Reports')  # disable-secrets-detection
     json.dump(REPORT_DATA, temp_report)
     temp_report.flush()
     os.fsync(temp_report.fileno())
-    temp_report2 = tempfile.NamedTemporaryFile(mode="w+", prefix='report-',
+    temp_report2 = tempfile.NamedTemporaryFile(mode="w+", prefix='report-',  # disable-secrets-detection
                                                suffix='.json', dir='Reports')  # disable-secrets-detection
     json.dump(REPORT_DATA, temp_report2)
     temp_report2.flush()
     os.fsync(temp_report2.fileno())
 
     # create duplicate Widgets
-    temp_widget = tempfile.NamedTemporaryFile(mode="w+", prefix='widget-',
+    temp_widget = tempfile.NamedTemporaryFile(mode="w+", prefix='widget-',  # disable-secrets-detection
                                               suffix='.json', dir='Widgets')  # disable-secrets-detection
     json.dump(WIDGET_DATA, temp_widget)
     temp_widget.flush()
     os.fsync(temp_widget.fileno())
-    temp_widget2 = tempfile.NamedTemporaryFile(mode="w+", prefix='widget-',
+    temp_widget2 = tempfile.NamedTemporaryFile(mode="w+", prefix='widget-',  # disable-secrets-detection
                                                suffix='.json', dir='Widgets')  # disable-secrets-detection
     json.dump(WIDGET_DATA, temp_widget2)
     temp_widget2.flush()
     os.fsync(temp_widget2.fileno())
 
     # create duplicate Classifier
-    temp_classifier = tempfile.NamedTemporaryFile(mode="w+", prefix='classifier-',
+    temp_classifier = tempfile.NamedTemporaryFile(mode="w+", prefix='classifier-',  # disable-secrets-detection
                                                   suffix='.json', dir='Classifiers')  # disable-secrets-detection
     json.dump(WIDGET_DATA, temp_classifier)
     temp_classifier.flush()
@@ -115,7 +115,7 @@ def test_find_duplicates():
     os.fsync(temp_classifier2.fileno())
 
     # create duplicate Layout
-    temp_layout = tempfile.NamedTemporaryFile(mode="w+", prefix='layout-',
+    temp_layout = tempfile.NamedTemporaryFile(mode="w+", prefix='layout-',  # disable-secrets-detection
                                               suffix='.json', dir='Layouts')  # disable-secrets-detection
     json.dump(LAYOUT_DATA, temp_layout)
     temp_layout.flush()
@@ -127,25 +127,25 @@ def test_find_duplicates():
     os.fsync(temp_layout2.fileno())
 
     # create duplicate Dashboard
-    temp_dashboard = tempfile.NamedTemporaryFile(mode="w+", prefix='dashboard-',
+    temp_dashboard = tempfile.NamedTemporaryFile(mode="w+", prefix='dashboard-',  # disable-secrets-detection
                                                  suffix='.json', dir='Dashboards')  # disable-secrets-detection
     json.dump(DASHBOARD_DATA, temp_dashboard)
     temp_dashboard.flush()
     os.fsync(temp_dashboard.fileno())
-    temp_dashboard2 = tempfile.NamedTemporaryFile(mode="w+", prefix='dashboard-',
+    temp_dashboard2 = tempfile.NamedTemporaryFile(mode="w+", prefix='dashboard-',  # disable-secrets-detection
                                                   suffix='.json', dir='Dashboards')  # disable-secrets-detection
     json.dump(DASHBOARD_DATA2, temp_dashboard2)
     temp_dashboard2.flush()
     os.fsync(temp_dashboard2.fileno())
 
     # create one incident type field and one indicator type field with same data
-    temp_incident_field = tempfile.NamedTemporaryFile(mode='w+', prefix='incidentfield-', suffix='.json',
-                                                      dir='IncidentFields')  # disable-secrets-detection
+    temp_incident_field = tempfile.NamedTemporaryFile(mode='w+', prefix='incidentfield-',  # disable-secrets-detection
+                                                      suffix='.json', dir='IncidentFields')  # disable-secrets-detection
     json.dump(INCIDENT_FIELD_DATA, temp_incident_field)
     temp_incident_field.flush()
     os.fsync(temp_incident_field.fileno())
-    temp_indicator_field = tempfile.NamedTemporaryFile(mode='w+', prefix='incidentfield-', suffix='.json',
-                                                       dir='IndicatorFields')  # disable-secrets-detection
+    temp_indicator_field = tempfile.NamedTemporaryFile(mode='w+', prefix='incidentfield-',  # disable-secrets-detection
+                                                       suffix='.json', dir='IndicatorFields')
     json.dump(INCIDENT_FIELD_DATA, temp_indicator_field)
     temp_indicator_field.flush()
     os.fsync(temp_indicator_field.fileno())
