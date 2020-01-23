7e4dd86fd36b9e3cc137b6e05d26235f2ba4a672
Jochman <bhochman@paloaltonetworks.com>
2020-01-23 22:04:28 +0200

    startcure

--- Packs/FeedProofPoint/Integrations/FeedProofPoint/FeedProofPoint_test.py
+++ Packs/FeedProofPoint/Integrations/FeedProofPoint/FeedProofPoint_test.py
@@ -1,48 +1,27 @@
-from FeedCSV import *
-import requests_mock
+from FeedProofPoint import Client, fetch_indicators_command
+
+url = "https://example.com"
+auth_code = "cool"
+client = Client(url, auth_code)
 
 
-def test_get_indicators_1():
-    """Test with 1 fieldname"""
-    with open("test_data/ip_ranges.txt") as ip_ranges_txt:
-        ip_ranges = ip_ranges_txt.read().encode("utf8")
-
-    with requests_mock.Mocker() as m:
-        itype = "IP"
-        args = {"indicator_type": itype, "limit": 35}
-        m.get("https://ipstack.com", content=ip_ranges)
-        client = Client(url="https://ipstack.com", fieldnames="indicator")
-        hr, indicators_ec, raw_json = get_indicators_command(client, args)
-        indicators_ec = indicators_ec.get("CSV.Indicator")
-        assert len(indicators_ec) == 35
-        for ind_json in raw_json:
-            ind_val = ind_json.get("value")
-            ind_type = ind_json.get("type")
-            ind_rawjson = ind_json.get("rawJSON")
-            assert ind_val
-            assert ind_type == itype
-            assert ind_rawjson["value"] == ind_val
-            assert ind_rawjson["type"] == ind_type
+def test_fetch_ips(requests_mock):
+    ip_path = "./TestData/detailed-iprep.txt"
+    with open(ip_path) as f:
+        data = f.read()
+    requests_mock.get(
+        "https://example.com/cool/reputationdetailed-iprepdata.txt", text=data
+    )
+    indicators = fetch_indicators_command(client, client.IP_TYPE)
+    assert 4 == len(indicators)
 
 
-def test_get_indicators_2():
-    """Test with 1 fieldname that's not called indicator"""
-    with open("test_data/ip_ranges.txt") as ip_ranges_txt:
-        ip_ranges = ip_ranges_txt.read().encode("utf8")
-
-    with requests_mock.Mocker() as m:
-        itype = "IP"
-        args = {"indicator_type": itype, "limit": 35}
-        m.get("https://ipstack.com", content=ip_ranges)
-        client = Client(url="https://ipstack.com", fieldnames="special_ind")
-        hr, indicators_ec, raw_json = get_indicators_command(client, args)
-        indicators_ec = indicators_ec.get("CSV.Indicator")
-        assert len(indicators_ec) == 35
-        for ind_json in raw_json:
-            ind_val = ind_json.get("value")
-            ind_type = ind_json.get("type")
-            ind_rawjson = ind_json.get("rawJSON")
-            assert ind_val
-            assert ind_type == itype
-            assert ind_rawjson["value"] == ind_val
-            assert ind_rawjson["type"] == ind_type
+def test_fetch_domains(requests_mock):
+    ip_path = "./TestData/detalied-domainrepdata.txt"
+    with open(ip_path) as f:
+        data = f.read()
+    requests_mock.get(
+        "https://example.com/cool/reputationdetailed-domainrepdata.txt", text=data
+    )
+    indicators = fetch_indicators_command(client, client.DOMAIN_TYPE)
+    assert 9 == len(indicators)
