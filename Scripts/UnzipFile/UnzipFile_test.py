d1d2eeb9d0776108cfa894a52a725d96cfcee758
esharf <esahrf@paloaltonetworks.com>
2020-01-27 16:18:58 +0200

    unzip fix bug + unit tests

---
+++ Scripts/UnzipFile/UnzipFile_test.py
@@ -0,0 +1,26 @@
+import shutil
+
+from UnzipFile import *
+import os
+import pytest
+
+
+data_test_extract = [
+    ('testZip.yml', None),
+    ('ScanSummary.txt', None),
+    ('item.png', None)
+]
+
+
+@pytest.mark.parametrize('file_name, password', data_test_extract)
+def test_extract(file_name, password):
+    main_dir = '/'.join(__file__.split('/')[0:-1])
+    _dir = mkdtemp()
+    path = os.path.join(main_dir + '/data_test', file_name)
+    extract(path + '.zip', _dir, password)
+    with open(_dir + '/' + file_name, 'rb') as f:
+        extract_file = f.read()
+    with open(path, 'rb') as f:
+        _file = f.read()
+    assert _file == extract_file
+    shutil.rmtree(_dir)
