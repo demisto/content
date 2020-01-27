d86975098195cb018dd23a06d99b1dcd04d3c051
dbaumstein <dbaumstein@paloaltonetwork.com>
2020-01-27 11:54:34 +0200

    New script

---
+++ Scripts/IsInternalHostName/IsInternalHostName.py
@@ -0,0 +1,32 @@
+import demistomock as demisto
+import re
+
+def main():
+
+    args = demisto.args()
+    internalregex = args.get('internalRegex')
+    domainName = args.get('domainName')
+    hostName = argToList(args.get('hostName'))
+    human_readable = []
+    context_entry = []
+    for element in hostName:
+        if element:
+            internalRegexMatch = re.match(internalregex, element)
+            internalDomainMatch = re.match(".*\." + domainName, element)
+            context_entry.append({
+                'Hostname': element,
+                'IsInternal': True if internalRegexMatch or internalDomainMatch else False
+            })
+
+            if context_entry[-1]['IsInternal']:
+               readable = element + ' is internal'
+            else:
+                readable = element + ' is external'
+
+
+            human_readable.append(readable)
+
+    return_outputs('\n'.join(human_readable), {'Endpoint': context_entry})
+
+
+if __name__ in ('builtins', '__builtin__'):
