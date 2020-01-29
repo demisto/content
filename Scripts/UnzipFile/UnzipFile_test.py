d91011599ac3fec2104f6c26cedc4451ff0a6029
esharf <esahrf@paloaltonetworks.com>
2020-01-29 11:37:36 +0200

    fixup! minor test fixes

--- Scripts/UnzipFile/UnzipFile_test.py
+++ Scripts/UnzipFile/UnzipFile_test.py
@@ -1,44 +1,79 @@
-import shutil
-
+from tempfile import mkdtemp
 from UnzipFile import *
 import os
 import pytest
 
 
-data_test_extract = [
-    ('testZip.yml', None),
-    ('ScanSummary.txt', None),
-    ('item.png', None),
+data_test_unzip_no_password = ['testZip.yml', 'ScanSummary.txt', 'item.png']
+
+
+@pytest.mark.parametrize('file_name', data_test_unzip_no_password)
+def test_unzip_no_password(file_name):
+    """
+    Given
+    - valid zip file - no password required
+    - empty folder _dir
+    When
+    - run extract on that zip file and export the internal files to _dir
+    Then
+    - ensure zip file content have be saved at _dir directory with the original filename
+    - ensure that the saved file has expected content
+    """
+    # Given
+    # - valid zip file - no password required
+    main_dir = '/'.join(__file__.split('/')[0:-1])
+    expected_file_unzipped = os.path.join(main_dir + '/data_test', file_name)
+    zipped_file_path = expected_file_unzipped + '.zip'
+    # - empty folder _dir
+    _dir = mkdtemp()
+    # When
+    # - run extract on that zip file and export the internal files to _dir
+    extract(zipped_file_path, _dir)
+    # Then
+    # - ensure zip file content have been saved at _dir directory with the original filename
+    with open(_dir + '/' + file_name, 'rb') as f:
+        actual_file_data = f.read()
+    with open(expected_file_unzipped, 'rb') as f:
+        expected_data = f.read()
+    shutil.rmtree(_dir)
+    # - ensure that the saved file has expected content data
+    assert expected_data == actual_file_data, 'failed extracting ' + zipped_file_path
+
+
+data_test_unzip_with_password = [
     ('fix_unzip.png', 'demisto'),
 ]
 
 
-@pytest.mark.parametrize('file_name, password', data_test_extract)
-def test_extract(file_name, password):
+@pytest.mark.parametrize('file_name, password', data_test_unzip_with_password)
+def test_unzip_with_password(file_name, password):
     """
-    :param file_name: file name in dir Scripts/UnzipFile/data_test/
-    :param password: if the is encrypted
+    Given
+    - valid zip file - with password required
+    - empty folder _dir
+    When
+    - run extract on that zip file and export the internal files to _dir
+    Then
+    - ensure zip file content have be saved at _dir directory with the original filename
+    - ensure that the saved file has expected content
     """
-    # crate the full path to Scripts/UnzipFile/data_test/
+    # Given
+    # - valid zip file - no password required
     main_dir = '/'.join(__file__.split('/')[0:-1])
-    path = os.path.join(main_dir + '/data_test', file_name)
-
-    error_message = 'Failed extracting ' + path
-    if password:
-        error_message += ' with password: ' + password
-
-    # crate a temp directory for extracting the files
-    # extracting file
+    expected_file_unzipped = os.path.join(main_dir + '/data_test', file_name)
+    zipped_file_path = expected_file_unzipped + '.zip'
+    # - empty folder _dir
     _dir = mkdtemp()
-    extract(path + '.zip', _dir, password)
-
-    # get the extracted file content from temp dir
+    # When
+    # - run extract on that zip file and export the internal files to _dir
+    extract(zipped_file_path, _dir, password)
+    # Then
+    # - ensure zip file content have been saved at _dir directory with the original filename
     with open(_dir + '/' + file_name, 'rb') as f:
-        extract_file = f.read()
-
-    # get the original file content from Scripts/UnzipFile/data_test/
-    with open(path, 'rb') as f:
-        _file = f.read()
-
+        actual_file_data = f.read()
+    with open(expected_file_unzipped, 'rb') as f:
+        expected_data = f.read()
     shutil.rmtree(_dir)
-    assert _file == extract_file, error_message
+    # - ensure that the saved file has expected content data
+    assert expected_data == actual_file_data,\
+        'failed unzipping file: ' + zipped_file_path + ' with password: ' + password
