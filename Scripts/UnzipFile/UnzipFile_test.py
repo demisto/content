a497d74f84c8194dd5911fb3d4139e7f323e00c5
esharf <esahrf@paloaltonetworks.com>
2020-01-29 08:23:47 +0200

    minor test fixes

--- Scripts/UnzipFile/UnzipFile_test.py
+++ Scripts/UnzipFile/UnzipFile_test.py
@@ -15,13 +15,30 @@ data_test_extract = [
 
 @pytest.mark.parametrize('file_name, password', data_test_extract)
 def test_extract(file_name, password):
+    """
+    :param file_name: file name in dir Scripts/UnzipFile/data_test/
+    :param password: if the is encrypted
+    """
+    # crate the full path to Scripts/UnzipFile/data_test/
     main_dir = '/'.join(__file__.split('/')[0:-1])
-    _dir = mkdtemp()
     path = os.path.join(main_dir + '/data_test', file_name)
+
+    error_message = 'Failed extracting ' + path
+    if password:
+        error_message += ' with password: ' + password
+
+    # crate a temp directory for extracting the files
+    # extracting file
+    _dir = mkdtemp()
     extract(path + '.zip', _dir, password)
+
+    # get the extracted file content from temp dir
     with open(_dir + '/' + file_name, 'rb') as f:
         extract_file = f.read()
+
+    # get the original file content from Scripts/UnzipFile/data_test/
     with open(path, 'rb') as f:
         _file = f.read()
-    assert _file == extract_file
+
     shutil.rmtree(_dir)
+    assert _file == extract_file, error_message
