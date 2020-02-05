fa596b3c6627c224c3d8878c0c529a30668f9678
syaakovi <syaakovi@paloaltonetworks.com>
2020-02-05 15:17:32 +0200

    decrease sleep time before running tests on server master

--- Tests/test_content.py
+++ Tests/test_content.py
@@ -908,10 +908,8 @@ def main():
     # should be removed after solving: https://github.com/demisto/etc/issues/21383
     # -------------
     if 'master' in tests_settings.serverVersion.lower():
-        sleep(100)
-        print('slept for 100 secs')
-        sleep(100)
-        print('slept for 100 secs')
+        print('[{}] sleeping for 60 secs'.format(datetime.now()))
+        sleep(60)
     # -------------
     manage_tests(tests_settings)
 
