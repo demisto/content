a20bb60a3fd795e0e3936fb384bab6395c24f7b9
Rony Kozakish <37589583+ronykoz@users.noreply.github.com>
2020-02-06 14:52:12 +0200

    fix bug in SearchIncidens (#5646)
    * fix bug
    
    * Update SearchIncidents.py
    
    * remove fromversion
    
    * deprecating script
    
    * improve script implementation and adjust test
    
    * fix changelog and remove whitespaces from yml descriptions
    
    * fixed deprecated script comment and add is_error validation
    
    * add no test
    
    Co-authored-by: Itay Keren <ikeren@paloaltonetworks.com>

--- Scripts/SearchIncidentsV2/SearchIncidentsV2.py
+++ Scripts/SearchIncidentsV2/SearchIncidentsV2.py
@@ -12,8 +12,8 @@ def check_if_found_incident(res: List):
         if 'data' not in res[0]['Contents']:
             raise DemistoException(res[0].get('Contents'))
         elif res[0]['Contents']['data'] is None:
-            raise DemistoException("Incidents not found.")
-
+            return False
+        return True
     else:
         raise DemistoException(f'failed to get incidents from demisto.\nGot: {res}')
 
@@ -42,18 +42,24 @@ def is_valid_args(args: Dict):
 def search_incidents(args: Dict):
     if is_valid_args(args):
         res: List = demisto.executeCommand('getIncidents', args)
-        check_if_found_incident(res)
-        data: Dict = res[0]['Contents']['data']
-        context_entry: Dict = {'foundIncidents': data}
-        headers: List[str] = ['id', 'name', 'severity', 'status', 'owner', 'created', 'closed']
-        md: str = tableToMarkdown(name="Incidents found", t=data, headers=headers)
-        return_outputs(md, context_entry, res)
+        if is_error(res):
+            raise DemistoException(get_error(res))
+        incident_found: bool = check_if_found_incident(res)
+        if incident_found is False:
+            return 'Incidents not found.', {}, {}
+        else:
+            data: Dict = res[0]['Contents']['data']
+            context_entry: Dict = {'foundIncidents': data}
+            headers: List[str] = ['id', 'name', 'severity', 'status', 'owner', 'created', 'closed']
+            md: str = tableToMarkdown(name="Incidents found", t=data, headers=headers)
+            return md, context_entry, res
 
 
 def main():
     args: Dict = demisto.args()
     try:
-        search_incidents(args)
+        readable_output, outputs, raw_response = search_incidents(args)
+        return_outputs(readable_output, outputs, raw_response)
     except DemistoException as error:
         return_error(str(error), error)
 
