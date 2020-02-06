1c4d5ec08f37d69659a0c78903fb8991de533577
esharf <esahrf@paloaltonetworks.com>
2020-02-06 16:37:03 +0200

    fixup! addind keys creator for DXL

--- Integrations/McAfee_DXL/create_keys.sh
+++
@@ -1,3 +0,0 @@
-openssl genpkey -out client.key -algorithm RSA -pkeyopt rsa_keygen_bits:2048
-openssl req -key client.key -new -out client.csr
-openssl x509 -req -days 365 -in client.csr -signkey client.key -sha256 -out client.crt
