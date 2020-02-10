f756fd0bfa05a6b337a34dd2ccbb385d29f35e5b
rsagi <rsagi@paloaltonetworks.com>
2020-02-10 15:22:43 +0200

    dropping lgtm fix

--- Packs/FeedBruteForceBlocker/Integrations/FeedBruteForceBlocker/FeedBruteForceBlocker.py
+++ Packs/FeedBruteForceBlocker/Integrations/FeedBruteForceBlocker/FeedBruteForceBlocker.py
@@ -1,4 +1,4 @@
-from CommonServerPython import *  # lgtm [py/polluting-import]
+from CommonServerPython import *
 
 
 def main():
@@ -28,7 +28,7 @@ def main():
     feed_main('BruteForceBlocker Feed', params, 'bruteforceblocker-')
 
 
-from HTTPFeedApiModule import *  # noqa: E402 lgtm [py/polluting-import]
+from HTTPFeedApiModule import *  # noqa: E402
 
 
 if __name__ == '__builtin__' or __name__ == 'builtins':
