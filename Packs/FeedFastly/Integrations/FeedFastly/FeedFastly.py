6318dc6d9dfeaeaf32f05b30bdeb62ac33690075
dtavori <dtavori@paloaltonetworks.com>
2020-02-11 17:05:37 +0200

    fastly - in progress

---
+++ Packs/FeedFastly/Integrations/FeedFastly/FeedFastly.py
@@ -0,0 +1,31 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+
+from JSONFeedApiModule import *  # noqa: E402
+
+
+def main():
+    params = {k: v for k, v in demisto.params().items() if v is not None}
+    params['indicator'] = 'ip'
+    params['url'] = 'https://api.fastly.com/public-ip-list'
+
+    indicator_types = [
+        {
+            'indicator_type': FeedIndicatorType.CIDR,
+            'extractor': 'addresses'
+        },
+        {
+            'indicator_type': FeedIndicatorType.IPv6CIDR,
+            'extractor': 'ipv6_addresses'
+        }
+    ]
+
+    for item in indicator_types:
+        params['indicator_type'] = item['indicator_type']
+        params['extractor'] = item['extractor']
+        feed_main(params, 'Fastly Feed', 'fastly')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
