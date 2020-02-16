d4c1a1c0863ecc91dd296d3d78cf886c9802314b
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-02-16 13:49:59 +0200

    added docstring

--- Tests/store_packs.py
+++ Tests/store_packs.py
@@ -14,9 +14,10 @@ from zipfile import ZipFile, ZIP_DEFLATED
 from Tests.test_utils import run_command, print_error, print_warning, print_color, LOG_COLORS, \
     collect_content_items_data, input_to_list
 
+# global constants
 STORAGE_BASE_PATH = "content/packs"
 IGNORED_PATHS = ['Packs/__init__.py']
-
+# the format is defined in issue #19786, may change in the future
 DIR_NAME_TO_CONTENT_TYPE = {
     "Classifiers": "Classifiers",
     "Dashboards": "Dashboards",
@@ -33,6 +34,22 @@ DIR_NAME_TO_CONTENT_TYPE = {
 
 
 class Pack(object):
+    """Class that manipulates and manages the upload of pack's artifact and metadata to cloud storage.
+
+    Args:
+        pack_name (str): Pack root folder name.
+        pack_path (str): Full path to pack folder.
+
+    Attributes:
+        PACK_INITIAL_VERSION (str): pack initial version that will be used as default.
+        DATE_FORMAT (str): date format of.
+        CHANGELOG_JSON (str): changelog json full name, may be changed in the future.
+        CHANGELOG_MD (str): changelog md full name.
+        README (str): pack's readme file name.
+        METADATA (str): pack's metadata file name, may be changed in the future.
+        INDEX_NAME (str): pack's index name, may be changed in the future.
+
+    """
     PACK_INITIAL_VERSION = "1.0.0"
     DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
     CHANGELOG_JSON = "changelog.json"
@@ -48,17 +65,33 @@ class Pack(object):
 
     @property
     def name(self):
+        """str: pack root folder name.
+        """
         return self._pack_name
 
     @property
     def path(self):
+        """str: pack folder full path.
+        """
         return self._pack_path
 
     @property
     def latest_version(self):
+        """str: Pack latest version from sorted keys of changelog.json file.
+        """
         return self._get_latest_version()
 
     def _get_latest_version(self):
+        """Return latest semantic version of the pack.
+
+        In case that changelog.json file was not found, default value of 1.0.0 will be returned.
+        Otherwise, keys of semantic pack versions will be collected and sorted in descending and return latest version.
+        For additional information regarding changelog.json format go to issue #19786
+
+        Returns:
+            str: Pack latest version.
+
+        """
         changelog_path = os.path.join(self._pack_path, Pack.CHANGELOG_JSON)
 
         if not os.path.exists(changelog_path):
@@ -67,12 +100,22 @@ class Pack(object):
         with open(changelog_path, "r") as changelog_file:
             changelog = json.load(changelog_file)
             pack_versions = [LooseVersion(v) for v in changelog.keys()]
-            pack_versions.sort()
+            pack_versions.sort(reverse=True)
 
             return pack_versions[0].vstring
 
     def _parse_pack_metadata(self, user_metadata):
+        """Parses pack metadata according to issue #19786 and #20091. Part of field may change over the time.
+
+        Args:
+            user_metadata (dict): user metadata that was created in pack initialization.
+
+        Returns:
+            dict: parsed pack metadata.
+
+        """
         pack_metadata = {}
+        # part of old packs are initialized with empty list
         user_metadata = {} if isinstance(user_metadata, list) else user_metadata
         pack_metadata['name'] = user_metadata.get('displayName', '')
         pack_metadata['id'] = self._pack_name
@@ -116,6 +159,12 @@ class Pack(object):
         return pack_metadata
 
     def zip_pack(self):
+        """Zips pack folder and excludes not wanted directories.
+
+        Returns:
+            str: full path to created pack zip.
+
+        """
         zip_pack_path = f"{self._pack_path}.zip"
 
         with ZipFile(zip_pack_path, 'w', ZIP_DEFLATED) as pack_zip:
@@ -132,6 +181,19 @@ class Pack(object):
         return zip_pack_path
 
     def upload_to_storage(self, zip_pack_path, latest_version, storage_bucket):
+        """ Manages the upload of pack zip artifact to correct path in cloud storage.
+        The zip pack will be uploaded to following path: /content/packs/pack_name/pack_latest_version.
+        In case that zip pack artifact already exist at constructed path, the upload will be skipped.
+
+        Args:
+            zip_pack_path (str): full path to pack zip artifact.
+            latest_version (str): pack latest version.
+            storage_bucket (google.cloud.storage.bucket.Bucket): google cloud storage bucket.
+
+        Returns:
+            bool: True is pack was successfully uploaded. False in case that packs already exists at the bucket.
+
+        """
         version_pack_path = os.path.join(STORAGE_BASE_PATH, self._pack_name, latest_version)
         existing_files = [f.name for f in storage_bucket.list_blobs(prefix=version_pack_path)]
 
@@ -150,6 +212,10 @@ class Pack(object):
         return True
 
     def format_metadata(self):
+        """Re-formats metadata according to marketplace metadata format defined in issue #19786 and writes back
+        the result.
+
+        """
         metadata_path = os.path.join(self._pack_path, Pack.METADATA)
 
         if not os.path.exists(metadata_path):
@@ -166,6 +232,9 @@ class Pack(object):
                     LOG_COLORS.GREEN)
 
     def parse_release_notes(self):
+        """Need to implement the changelog.md parsing and changelog.json creation after design is finalized.
+
+        """
         changelog_md_path = os.path.join(self._pack_path, Pack.CHANGELOG_MD)
 
         if not os.path.exists(changelog_md_path):
@@ -179,6 +248,9 @@ class Pack(object):
         return {}
 
     def prepare_for_index_upload(self):
+        """Removes and leaves only necessary files in pack folder.
+
+        """
         files_to_leave = [Pack.METADATA, Pack.CHANGELOG_JSON, Pack.README]
 
         for file_or_folder in os.listdir(self._pack_path):
@@ -193,12 +265,28 @@ class Pack(object):
                 os.remove(files_or_folder_path)
 
     def cleanup(self):
+        """Finalization action, removes extracted pack folder.
+
+        """
         if os.path.exists(self._pack_path):
             shutil.rmtree(self._pack_path)
             print(f"Cleanup {self._pack_name} pack from: {self._pack_path}")
 
 
 def get_modified_packs(specific_packs=None):
+    """Detects and returns modified or new packs names.
+
+    Checks the git difference between two commits, current and previous and greps only ones with prefix Packs/.
+    After content repo will move only for Packs structure, the grep pipe can be removed.
+    In case of local dev mode, the function will receive comma separated list of target packs.
+
+    Args:
+        specific_packs (str): comma separated packs names, used only in local dev mode.
+
+    Returns:
+        set: unique collection of modified/new packs names.
+
+    """
     if specific_packs:
         modified_packs = {p.strip() for p in specific_packs.split(',')}
         print(f"Number of selected packs is: {len(modified_packs)}")
@@ -213,6 +301,14 @@ def get_modified_packs(specific_packs=None):
 
 
 def extract_modified_packs(modified_packs, packs_artifacts_path, extract_destination_path):
+    """Extracts changed packs from content pack artifact zip.
+
+    Args:
+        modified_packs (set): collection of modified/new packs.
+        packs_artifacts_path (str): full path to content artifacts zip file.
+        extract_destination_path (str): full path to directory where to extract the packs.
+
+    """
     print("Starting extracting modified pack:")
     with ZipFile(packs_artifacts_path) as packs_artifacts:
         for pack in packs_artifacts.namelist():
@@ -225,20 +321,43 @@ def extract_modified_packs(modified_packs, packs_artifacts_path, extract_destina
 
 
 def init_storage_client(service_account=None):
+    """Initialize google cloud storage client.
+
+    In case of local dev usage the client will be initialized with user default credentials.
+    Otherwise, client will be initialized from service account json that is stored in CirlceCI.
+
+    Args:
+        service_account (str): full path to service account json.
+
+    Return:
+        storage.Client: initialized google cloud storage client.
+    """
     if service_account:
         return storage.Client.from_service_account_json(service_account)
     else:
+        # in case of local dev use, ignored the warning of non use of service account.
         warnings.filterwarnings("ignore", message=google.auth._default._CLOUD_SDK_CREDENTIALS_WARNING)
         credentials, project = google.auth.default()
         return storage.Client(credentials=credentials, project=project)
 
 
 def download_and_extract_index(storage_bucket, extract_destination_path):
+    """Downloads and extracts index zip from cloud storage.
+
+    Args:
+        storage_bucket (google.cloud.storage.bucket.Bucket): google storage bucket where index.zip is stored.
+        extract_destination_path (str): the full path of extract folder.
+
+    Returns:
+        str: extracted index folder full path.
+        Blob: google cloud storage object that represents index.zip blob.
+
+    """
     index_storage_path = os.path.join(STORAGE_BASE_PATH, f"{Pack.INDEX_NAME}.zip")
     download_index_path = os.path.join(extract_destination_path, f"{Pack.INDEX_NAME}.zip")
 
     index_blob = storage_bucket.blob(index_storage_path)
-    index_blob.cache_control = "no-cache"
+    index_blob.cache_control = "no-cache"  # index zip should never be cached in the memory, should be updated version
     index_blob.reload()
     index_blob.download_to_filename(download_index_path)
 
@@ -263,6 +382,14 @@ def download_and_extract_index(storage_bucket, extract_destination_path):
 
 
 def update_index_folder(index_folder_path, pack_name, pack_path):
+    """Copies pack folder into index folder.
+
+    Args:
+        index_folder_path (str): full path to index folder.
+        pack_name (str): pack folder name to copy.
+        pack_path (str): pack folder full path.
+
+    """
     index_folder_subdirectories = [d for d in os.listdir(index_folder_path) if
                                    os.path.isdir(os.path.join(index_folder_path, d))]
     index_pack_path = os.path.join(index_folder_path, pack_name)
@@ -273,6 +400,15 @@ def update_index_folder(index_folder_path, pack_name, pack_path):
 
 
 def upload_index_to_storage(index_folder_path, extract_destination_path, index_blob, build_number):
+    """Upload updated index zip to cloud storage.
+
+    Args:
+        index_folder_path (str): index folder full path.
+        extract_destination_path (str): extract folder full path.
+        index_blob (Blob): google cloud storage object that represents index.zip blob.
+        build_number (str): circleCI build number, used as an index revision.
+
+    """
     with open(os.path.join(index_folder_path, f"{Pack.INDEX_NAME}.json"), "w+") as index_file:
         index = {
             'description': 'Master index for Demisto Content Packages',
@@ -294,7 +430,7 @@ def upload_index_to_storage(index_folder_path, extract_destination_path, index_b
     index_zip_path = shutil.make_archive(base_name=index_folder_path, format="zip",
                                          root_dir=extract_destination_path, base_dir=index_zip_name)
 
-    index_blob.cache_control = "no-cache"
+    index_blob.cache_control = "no-cache"  # disabling caching for index blob
     index_blob.reload()
     index_blob.upload_from_filename(index_zip_path)
     shutil.rmtree(index_folder_path)
@@ -302,6 +438,12 @@ def upload_index_to_storage(index_folder_path, extract_destination_path, index_b
 
 
 def option_handler():
+    """Validates and parses script arguments.
+
+    Returns:
+        Namespace: Parsed arguments object.
+
+    """
     parser = argparse.ArgumentParser(description="Store packs in cloud storage.")
     # disable-secrets-detection-start
     parser.add_argument('-a', '--artifacts_path', help="The full path of packs artifacts", required=True)
