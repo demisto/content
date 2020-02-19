29f1c59f200251a25f345e320b799e6e69b6634a
eharush <erez@demisto.com>
2020-02-19 18:03:10 +0200

    Get incidents by query enhancement

--- Scripts/GetIncidentsByQuery/GetIncidentsByQuery.py
+++ Scripts/GetIncidentsByQuery/GetIncidentsByQuery.py
@@ -1,13 +1,36 @@
 from CommonServerPython import *
-
-import uuid
 import pickle
+import re
+import uuid
+from datetime import datetime, timedelta
 from dateutil import parser
 
 
 def parse_datetime(datetime_str):
     try:
         return parser.parse(datetime_str).isoformat()
+    except Exception:
+        return datetime_str
+
+
+def parse_relative_time(datetime_str):
+    try:
+        res = re.search("([0-9]+) (minutes|hours|days|weeks|months|years) ago", datetime_str)
+        if res:
+            number = int(res.group(1))
+            unit = res.group(2)
+            if unit == 'years':
+                unit = 'days'
+                number *= 365
+            elif unit == 'months':
+                number *= 43800
+                unit = 'minutes'
+
+            kargs = {}
+            kargs[unit] = int(number)
+            result = datetime.now() - timedelta(**kargs)
+            return result
+
     except Exception:
         return datetime_str
 
@@ -34,8 +57,8 @@ def build_incidents_query(extra_query, incident_types, time_field, from_date, to
         to_part = '%s:<"%s"' % (time_field, parse_datetime(to_date))
         query_parts.append(to_part)
     if non_empty_fields:
-        non_emprty_fields_part = " and ".join(map(lambda x: "%s:*" % x, non_empty_fields.split(",")))
-        query_parts.append(non_emprty_fields_part)
+        non_empty_fields_part = " and ".join(map(lambda x: "%s:*" % x, non_empty_fields.split(",")))
+        query_parts.append(non_empty_fields_part)
     if len(query_parts) == 0:
         return_error("Incidents query is empty - please fill one of the arguments")
     query = " and ".join(map(lambda x: "(%s)" % x, query_parts))
@@ -43,13 +66,28 @@ def build_incidents_query(extra_query, incident_types, time_field, from_date, to
     return query
 
 
-def get_incidents(query, time_field, size):
-    res = demisto.executeCommand("getIncidents", {"query": query, "size": size, "sort": time_field})
+def get_incidents(query, time_field, size, from_date):
+    args = {"query": query, "size": size, "sort": time_field}
+    if time_field == "created":
+        from_datetime = None
+        try:
+            from_datetime = parser.parse(from_date)
+        except Exception:
+            pass
+        if from_datetime is None and from_date.strip().endswith("ago"):
+            from_datetime = parse_relative_time(from_date)
+        if from_datetime:
+            args['from'] = from_datetime.isoformat()
+    res = demisto.executeCommand("getIncidents", args)
     if res[0]['Type'] == entryTypes['error']:
         error_message = str(res[0]['Contents'])
         return_error("Failed to get incidents by query: %s error: %s" % (query, error_message))
     incident_list = res[0]['Contents'].get('data') or []
     return incident_list
+
+
+def get_comma_sep_list(value):
+    return map(lambda x: x.strip(), value.split(","))
 
 
 def main():
@@ -60,15 +98,31 @@ def main():
                                   demisto.args().get('fromDate'),
                                   demisto.args().get('toDate'),
                                   demisto.args().get('NonEmptyFields'))
-    incident_list = get_incidents(query, demisto.args()['timeField'], int(demisto.args()['limit']))
+
+    incident_list = get_incidents(query, demisto.args()['timeField'],
+                                  int(demisto.args()['limit']),
+                                  demisto.args().get('fromDate'))
+    whitelist_fields = demisto.args().get('whitelistFields')
+    if whitelist_fields:
+        whitelist_fields = get_comma_sep_list(whitelist_fields)
+        whitelist_fields += get_comma_sep_list(demisto.args().get('NonEmptyFields', ''))
+        whitelist_fields = set([x for x in whitelist_fields if x])
+    print whitelist_fields
     include_context = demisto.args()['includeContext'] == 'true'
     # extend incidents fields \ context
+    new_incident_list = []
     for i in incident_list:
         # we flat the custom field to the incident structure, like in the context
         custom_fields = i.get('CustomFields', {}) or {}
         i.update(custom_fields)
         if include_context:
             i['context'] = get_context(i['id'])
+
+        if whitelist_fields and len(whitelist_fields) > 0:
+            i = {k: v for k, v in i.items() if k in whitelist_fields}
+        new_incident_list.append(i)
+
+    incident_list = new_incident_list
 
     # output
     file_name = str(uuid.uuid4())
