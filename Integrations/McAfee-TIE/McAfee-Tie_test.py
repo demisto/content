e37b6e5f58b0c25ceae0d7f767e68c7b123e7fe6
Or Lichter <50324325+orlichter1@users.noreply.github.com>
2020-02-21 10:48:54 +0200

    Mcafee TIE+DXL validate certificates (#5440)
    * Clarify and check the correctness of certificates
    
    * update_id_set for all demisto objects
    
    * add to function
    
    * temp docker image
    
    * temp docker image
    
    * typo
    
    * private key end check
    
    * private key end check
    
    * self instead of even_sender in __del__
    
    * fix __del__ func
    
    * unittests + tie restructure
    
    * unittests
    
    * Updated description
    
    * Updated description
    
    * Updated descriptions and formatting
    
    * Updated description and formatting
    
    * Tie restructure + unittests
    
    Co-authored-by: Alex Fiedler <38628621+kirbles19@users.noreply.github.com>

--- Integrations/McAfee-TIE/McAfee-Tie_test.py
+++ Integrations/McAfee-TIE/McAfee-Tie_test.py
@@ -1,10 +1,6 @@
 import demistomock as demisto
-
-from McAfee_DXL import *
+import importlib
 import pytest
-
-data_test_push_ip = ['-.-.-.-', '1.1.1']
-
 
 valid_private_key = """-----BEGIN PRIVATE KEY-----
 This is a vaild Private Key
@@ -25,20 +21,17 @@ This is a valid Certificate
 -----END CERTIFICATE-----   """
 
 
-@pytest.mark.parametrize(argnames='input_ip', argvalues=data_test_push_ip)
-def test_is_ip_valid(input_ip):
-    assert not is_ip_valid(input_ip), f'argument ip {input_ip} is not a valid IP'
-
-
 def test_validate_certificate_format(mocker):
+    mcafee_tie = importlib.import_module("McAfee-TIE")
 
     # Invalid private Key
     valid_params = {'private_key': invalid_private_key,
                     'cert_file': valid_certificate,
                     'broker_ca_bundle': valid_certificate}
     mocker.patch.object(demisto, "params", return_value=valid_params)
+
     with pytest.raises(SystemExit):
-        validate_certificates_format()
+        mcafee_tie.validate_certificates_format()
 
     # Invalid cert file
     valid_params = {'private_key': valid_private_key,
@@ -46,7 +39,7 @@ def test_validate_certificate_format(mocker):
                     'broker_ca_bundle': valid_certificate}
     mocker.patch.object(demisto, "params", return_value=valid_params)
     with pytest.raises(SystemExit):
-        validate_certificates_format()
+        mcafee_tie.validate_certificates_format()
 
     # Invalid broker_ca_bundle
     valid_params = {'private_key': valid_private_key,
@@ -54,11 +47,11 @@ def test_validate_certificate_format(mocker):
                     'broker_ca_bundle': invalid_certificate}
     mocker.patch.object(demisto, "params", return_value=valid_params)
     with pytest.raises(SystemExit):
-        validate_certificates_format()
+        mcafee_tie.validate_certificates_format()
 
     # Everything is valid + spaces
     valid_params = {'private_key': valid_private_key,
                     'cert_file': valid_certificate,
                     'broker_ca_bundle': spaces_in_certificate}
     mocker.patch.object(demisto, "params", return_value=valid_params)
-    validate_certificates_format()
+    mcafee_tie.validate_certificates_format()
