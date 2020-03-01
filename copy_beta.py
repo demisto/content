179cdf0f13fac5dcef2538ee214346897fb56503
Jochman <bhochman@paloaltonetworks.com>
2020-03-01 19:40:05 +0200

    added copy

--- copy_beta.py
+++ copy_beta.py
@@ -2,4 +2,6 @@ import os
 import shutil
 import sys
 path = sys.argv[1]
-shutil.copyfile('beta-release-notes.md', os.path.join(path, 'beta-release-notes.md'))
+copy_to_path = os.path.join(path, 'beta-release-notes.md')
+shutil.copyfile('beta-release-notes.md', copy_to_path)
+print("Is beta path {} exists? {}".format(copy_to_path, os.path.isfile(copy_to_path)))
