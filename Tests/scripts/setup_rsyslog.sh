0162771ab989cee8143297bfe87516f994afe8ca
liorblob <lblobstein@paloaltonetworks.com>
2020-03-05 12:44:57 +0200

    rsyslog

--- Tests/scripts/setup_rsyslog.sh
+++ Tests/scripts/setup_rsyslog.sh
@@ -1,5 +1,3 @@
-apk --update add --no-cache rsyslog
-
 wget -q https://raw.githubusercontent.com/demisto/dockerfiles/syslog/docker/rsyslog/rsyslog.conf --no-check-certificate \
 && rm -rf /etc/rsyslog.conf
 
