4e24bff9f8efe37281586592da40788dae8f4dd4
liorblob <lblobstein@paloaltonetworks.com>
2020-03-05 14:50:46 +0200

    tests

--- Tests/scripts/setup_rsyslog.sh
+++ Tests/scripts/setup_rsyslog.sh
@@ -1,3 +1,3 @@
-echo "module(load=\"imudp\")" >> /etc/rsyslog.conf
-echo "input(type=\"imudp\" port=\"514\")" >> /etc/rsyslog.conf
-echo "*.* @@`hostname -i`:5140" >> /etc/rsyslog.conf
+sudo echo "module(load=\"imudp\")" >> /etc/rsyslog.conf
+sudo echo "input(type=\"imudp\" port=\"514\")" >> /etc/rsyslog.conf
+sudo echo "*.* @@`hostname -i`:5140" >> /etc/rsyslog.conf
