ca02824dcf8a5268a25bffa8b41cd57425f6f230
liorblob <lblobstein@paloaltonetworks.com>
2020-03-05 12:19:38 +0200

    rsyslog

--- Tests/scripts/setup_rsyslog.sh
+++ Tests/scripts/setup_rsyslog.sh
@@ -1,8 +1,8 @@
-run apk --update add --no-cache rsyslog
+apk --update add --no-cache rsyslog
 
-run wget -q https://raw.githubusercontent.com/demisto/dockerfiles/syslog/docker/rsyslog/rsyslog.conf --no-check-certificate \
+wget -q https://raw.githubusercontent.com/demisto/dockerfiles/syslog/docker/rsyslog/rsyslog.conf --no-check-certificate \
 && rm -rf /etc/rsyslog.conf
 
-run mv rsyslog.conf /etc/rsyslog.conf
+mv rsyslog.conf /etc/rsyslog.conf
 
-run echo "*.* @@`hostname -i`:5140" >> /etc/rsyslog.conf
+echo "*.* @@`hostname -i`:5140" >> /etc/rsyslog.conf
