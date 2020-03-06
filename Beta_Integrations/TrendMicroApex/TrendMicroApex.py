8db71533925c064d345b8d73a90f8c405a0cee3d
ikeren <itay@demisto.com>
2020-03-06 18:56:02 +0200

    move to beta dir, improve general implementation and add test

--- Beta_Integrations/TrendMicroApex/TrendMicroApex.py
+++ Beta_Integrations/TrendMicroApex/TrendMicroApex.py
@@ -19,14 +19,14 @@ PRODAGENTAPIPATH = '/WebApp/API/AgentResource/ProductAgents'
 
 
 class Client(BaseClient):
-    def __init__(self, url_base, api_key, app_id, verify, proxy):
-        self.url_base = url_base
+    def __init__(self, base_url, api_key, app_id, verify, proxy):
+        super().__init__(base_url=base_url, verify=verify, proxy=proxy)
+        self.base_url = base_url
         self.api_key = api_key
         self.application_id = app_id
 
-        BaseClient.__init__(self, url_base, verify, proxy, ok_codes=None, headers=None, auth=None)
-
-    def __create_checksum(self, http_method, api_path, headers, request_body):
+    @staticmethod
+    def __create_checksum(http_method, api_path, headers, request_body):
         string_to_hash = http_method.upper() + '|' + api_path.lower() + '|' + headers + '|' + request_body
         base64_string = base64.b64encode(hashlib.sha256(str.encode(string_to_hash)).digest()).decode('utf-8')
         return base64_string
@@ -48,7 +48,7 @@ class Client(BaseClient):
             'Authorization': 'Bearer ' + self.__create_jwt_token(http_method='GET', api_path=USDOAPIPATH + querystring,
                                                                  headers='', request_body='')}
         response = (
-            self._http_request("GET", USDOAPIPATH, full_url=self.url_base + USDOAPIPATH + querystring, headers=headers))
+            self._http_request("GET", USDOAPIPATH, full_url=self.base_url + USDOAPIPATH + querystring, headers=headers))
         return response
 
     def usdo_delete(self, list_type="", content=""):
@@ -56,9 +56,8 @@ class Client(BaseClient):
         headers = {'Authorization': 'Bearer ' + self.__create_jwt_token(http_method='DELETE',
                                                                         api_path=USDOAPIPATH + querystring, headers='',
                                                                         request_body='')}
-        response = (self._http_request("DELETE", USDOAPIPATH, full_url=self.url_base + USDOAPIPATH + querystring,
+        response = (self._http_request("DELETE", USDOAPIPATH, full_url=self.base_url + USDOAPIPATH + querystring,
                                        headers=headers))
-        # demisto.log(str(response))
         return response
 
     def usdo_add(self, add_type=None, content=None, scan_action=None, notes='', expiration=''):
@@ -77,7 +76,7 @@ class Client(BaseClient):
                 'Content-Type': 'application/json;charset=utf-8',
                 'Authorization': 'Bearer ' + self.__create_jwt_token(http_method='PUT', api_path=USDOAPIPATH + '/',
                                                                      headers='', request_body=json.dumps(req_body))}
-            response = (self._http_request("PUT", USDOAPIPATH + '/', full_url=self.url_base + USDOAPIPATH + '/',
+            response = (self._http_request("PUT", USDOAPIPATH + '/', full_url=self.base_url + USDOAPIPATH + '/',
                                            headers=headers, data=json.dumps(req_body)))
 
             return response
@@ -100,9 +99,12 @@ class Client(BaseClient):
             'Authorization': 'Bearer ' + self.__create_jwt_token(http_method='POST', api_path=PRODAGENTAPIPATH + '/',
                                                                  headers='', request_body=json.dumps(req_body))}
         response = (
-            self._http_request("POST", PRODAGENTAPIPATH + '/', full_url=self.url_base + PRODAGENTAPIPATH + '/',
+            self._http_request("POST", PRODAGENTAPIPATH + '/', full_url=self.base_url + PRODAGENTAPIPATH + '/',
                                headers=headers,
                                data=json.dumps(req_body)))
+        if response.get('result_code') != 1:
+            err_msg = f'Operation failed - {response.get("result_description", "")}'
+            raise ValueError(err_msg)
         return response
 
     def prodagent_isolate(self, multi_match=False, entity_id="", ip_add="", mac_add="", host="", prod=""):
@@ -121,8 +123,8 @@ def test_module(client: Client, *_) -> str:
     """
     Performs basic get request to get item samples
     """
-    client._http_request('GET', 'items/samples')
-    return 'ok'
+    client.usdo_list()
+    return 'ok', None, None
 
 
 def usdo_list_command(client: Client, args):
@@ -134,7 +136,7 @@ def usdo_list_command(client: Client, args):
     data = response.get('Data')
 
     return (tableToMarkdown("Apex USDO List", data),
-            {"TrendMicro.Apex.USDO": data}, response)
+            {"TrendMicroApex.USDO": data}, response)
 
 
 def usdo_delete_command(client: Client, args):
@@ -143,7 +145,7 @@ def usdo_delete_command(client: Client, args):
 
     response = client.usdo_delete(list_type, content)
 
-    return "OK - Deleted", None, response
+    return f'USDO {content} of type {list_type} was deleted successfully', None, response
 
 
 def usdo_add_command(client: Client, args):
@@ -153,35 +155,51 @@ def usdo_add_command(client: Client, args):
 
     response = client.usdo_add(add_type=add_type, content=content, scan_action=scan_action)
 
-    return "OK - Added: " + add_type + ": " + content + " - " + scan_action, None, response
+    return f'USDO {content} of type {add_type} was added successfully with scan action {scan_action}', None, response
 
 
 def prodagent_isolate_command(client: Client, args):
-    multi_match = args.get('multi_match')
+    multi_match = args.get('multi_match', 'true') == 'true'
     entity_id = args.get('entity_id')
     ip = args.get('ip_address')
     mac = args.get('mac_address')
     host = args.get('host_name')
     product = args.get('product')
+
+    if not any([entity_id, ip, mac, host, product]):
+        raise ValueError('At least one of the following arguments must be provided: '
+                         'entity_id, ip_address, mac_address, host_name, product')
 
     response = client.prodagent_isolate(multi_match=multi_match, entity_id=entity_id, ip_add=ip, mac_add=mac, host=host,
                                         prod=product)
-    return (tableToMarkdown("Apex ProductAgent Isolate", response["result_content"]),
-            {"TrendMicro.Apex.ProductAgent": response["result_content"]}, response)
+    result_content = response.get('result_content', [])
+    if result_content:
+        return (tableToMarkdown("Apex ProductAgent Isolate", result_content),
+            {"TrendMicroApex.ProductAgent": result_content}, response)
+    else:
+        return 'No agents were affected.', None, None
 
 
 def prodagent_restore_command(client: Client, args):
-    multi_match = args.get('multi_match')
+    multi_match = args.get('multi_match', 'true') == 'true'
     entity_id = args.get('entity_id')
     ip = args.get('ip_address')
     mac = args.get('mac_address')
     host = args.get('host_name')
     product = args.get('product')
 
+    if not any([entity_id, ip, mac, host, product]):
+        raise ValueError('At least one of the following arguments must be provided: '
+                         'entity_id, ip_address, mac_address, host_name, product')
+
     response = client.prodagent_restore(multi_match=multi_match, entity_id=entity_id, ip_add=ip, mac_add=mac, host=host,
                                         prod=product)
-    return (tableToMarkdown("Apex ProductAgent Restore", response["result_content"]),
-            {"TrendMicro.Apex.ProductAgent": response["result_content"]}, response)
+    result_content = response.get('result_content', [])
+    if result_content:
+        return (tableToMarkdown("Apex ProductAgent Restore", result_content),
+                {"TrendMicroApex.ProductAgent": result_content}, response)
+    else:
+        return 'No agents were affected.', None, None
 
 
 ''' COMMANDS MANAGER / SWITCH PANEL '''
@@ -195,35 +213,27 @@ def main():
     api_key = params.get('token')
     app_id = params.get('application_id')
 
-    # Service base URL
     base_url = urljoin(params['url'], '')
-    # Should we use SSL
-    use_ssl = not params.get('insecure', False)
-    # Should we use system proxy settings
-    use_proxy = params.get('proxy', False)
+    verify = not params.get('insecure', False)
+    proxy = params.get('proxy', False)
 
-    # Headers to be sent in requests
-
-    # Initialize Client object
-    client = Client(base_url, api_key, app_id, verify=use_ssl, proxy=use_proxy)
+    client = Client(base_url, api_key, app_id, verify=verify, proxy=proxy)
     command = demisto.command()
     LOG(f'Command being called is {command}')
-    # Commands dict
+
     commands = {
+        'test-module': test_module,
         'trendmicro-apex-usdo-list': usdo_list_command,
         'trendmicro-apex-usdo-add': usdo_add_command,
         'trendmicro-apex-usdo-delete': usdo_delete_command,
-        'trendmicro-prodagent-isolate': prodagent_isolate_command,
-        'trendmicro-prodagent-restore': prodagent_restore_command
+        'trendmicro-apex-isolate': prodagent_isolate_command,
+        'trendmicro-apex-restore': prodagent_restore_command
     }
-    # Run the commands
     try:
         if command in commands:
             return_outputs(*commands[command](client, demisto.args()))
-
-    # Log exceptions
     except ValueError as e:
-        return_error(f'Error from TrendMicro Apex integration', e)
+        return_error(f'Error from TrendMicro Apex integration: {str(e)}', e)
 
 
 if __name__ in ['__main__', 'builtin', 'builtins']:
