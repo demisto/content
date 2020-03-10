0386cca0f1982fe1a41e6b43500925961e1b00a0
liorblob <lblobstein@paloaltonetworks.com>
2020-03-10 10:38:55 +0200

    Fix test module

--- Packs/ApiModules/Scripts/HTTPFeedApiModule/HTTPFeedApiModule.py
+++ Packs/ApiModules/Scripts/HTTPFeedApiModule/HTTPFeedApiModule.py
@@ -358,29 +358,30 @@ def get_indicators_command(client: Client, args):
     return hr, {}, indicators_list
 
 
-def test_module(client, args):
-    indicator_type = args.get('indicator_type', demisto.params().get('indicator_type'))
-    if not FeedIndicatorType.is_valid_type(indicator_type):
-        supported_values = ', '.join((
-            FeedIndicatorType.Account,
-            FeedIndicatorType.CVE,
-            FeedIndicatorType.Domain,
-            FeedIndicatorType.Email,
-            FeedIndicatorType.File,
-            FeedIndicatorType.MD5,
-            FeedIndicatorType.SHA1,
-            FeedIndicatorType.SHA256,
-            FeedIndicatorType.Host,
-            FeedIndicatorType.IP,
-            FeedIndicatorType.CIDR,
-            FeedIndicatorType.IPv6,
-            FeedIndicatorType.IPv6CIDR,
-            FeedIndicatorType.Registry,
-            FeedIndicatorType.SSDeep,
-            FeedIndicatorType.URL
-        ))
-        raise ValueError(f'Indicator type of {indicator_type} is not supported. Supported values are:'
-                         f' {supported_values}')
+def test_module(client: Client, args):
+    if not client.feed_url_to_config:
+        indicator_type = args.get('indicator_type', demisto.params().get('indicator_type'))
+        if not FeedIndicatorType.is_valid_type(indicator_type):
+            supported_values = ', '.join((
+                FeedIndicatorType.Account,
+                FeedIndicatorType.CVE,
+                FeedIndicatorType.Domain,
+                FeedIndicatorType.Email,
+                FeedIndicatorType.File,
+                FeedIndicatorType.MD5,
+                FeedIndicatorType.SHA1,
+                FeedIndicatorType.SHA256,
+                FeedIndicatorType.Host,
+                FeedIndicatorType.IP,
+                FeedIndicatorType.CIDR,
+                FeedIndicatorType.IPv6,
+                FeedIndicatorType.IPv6CIDR,
+                FeedIndicatorType.Registry,
+                FeedIndicatorType.SSDeep,
+                FeedIndicatorType.URL
+            ))
+            raise ValueError(f'Indicator type of {indicator_type} is not supported. Supported values are:'
+                             f' {supported_values}')
     client.build_iterator()
     return 'ok', {}, {}
 
