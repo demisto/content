a2377c9671f9f61fc361a7f4176d51d4815173dd
rsagi <rsagi@paloaltonetworks.com>
2020-03-11 16:48:33 +0200

    added unit test for ip range and cidr collapse

--- Packs/ExportIndicators/Integrations/ExportIndicators/ExportIndicators_test.py
+++ Packs/ExportIndicators/Integrations/ExportIndicators/ExportIndicators_test.py
@@ -2,6 +2,8 @@
 import json
 import pytest
 import demistomock as demisto
+from netaddr import IPAddress
+
 
 IOC_RES_LEN = 38
 
@@ -369,3 +371,31 @@ class TestHelperFunctions:
         assert not validate_basic_authentication(data.get('wrong_length_auth'), username, password)
         assert not validate_basic_authentication(data.get('wrong_credentials_auth'), username, password)
         assert validate_basic_authentication(data.get('right_credentials_auth'), username, password)
+
+    @pytest.mark.create_values_out_dict
+    def test_ips_to_ranges_range(self):
+        from ExportIndicators import ips_to_ranges, COLLAPSE_TO_RANGES
+        ip_list = [IPAddress("1.1.1.1"), IPAddress("25.24.23.22"), IPAddress("22.21.20.19"),
+                   IPAddress("1.1.1.2"), IPAddress("1.2.3.4"), IPAddress("1.1.1.3"), IPAddress("2.2.2.2"),
+                   IPAddress("1.2.3.5")]
+
+        ip_range_list = ips_to_ranges(ip_list, COLLAPSE_TO_RANGES)
+        assert "1.1.1.1-1.1.1.3" in ip_range_list
+        assert "1.2.3.4-1.2.3.5" in ip_range_list
+        assert "1.1.1.2" not in ip_range_list
+        assert "2.2.2.2" in ip_range_list
+
+    @pytest.mark.create_values_out_dict
+    def test_ips_to_ranges_cidr(self):
+        from ExportIndicators import ips_to_ranges, COLLAPSE_TO_CIDR
+        ip_list = [IPAddress("1.1.1.1"), IPAddress("25.24.23.22"), IPAddress("22.21.20.19"),
+                   IPAddress("1.1.1.2"), IPAddress("1.2.3.4"), IPAddress("1.1.1.3"), IPAddress("2.2.2.2"),
+                   IPAddress("1.2.3.5")]
+
+        ip_range_list = ips_to_ranges(ip_list, COLLAPSE_TO_CIDR)
+        assert "1.1.1.1" in ip_range_list
+        assert "1.1.1.2/31" in ip_range_list
+        assert "1.2.3.4/31" in ip_range_list
+        assert "1.2.3.5" not in ip_range_list
+        assert "1.1.1.3" not in ip_range_list
+        assert "2.2.2.2" in ip_range_list
