a18e6377b38ee354440328fba6264a82bc1ba074
ShahafBenYakir <shahaf.benyakir@demisto.com>
2020-03-16 17:24:31 +0200

    Added UT Fixed mypy and flake 8 issues

--- Packs/ThreatIntelligenceManagement/Scripts/ThreatIntelligenceManagementGetIncidentsPerFeed/ThreatIntelligenceManagementGetIncidentsPerFeed.py
+++ Packs/ThreatIntelligenceManagement/Scripts/ThreatIntelligenceManagementGetIncidentsPerFeed/ThreatIntelligenceManagementGetIncidentsPerFeed.py
@@ -27,12 +27,14 @@ def get_all_incidents(from_date):
 
 
 def get_feeds_for_incident(incident_id):
-    indicator_query = f'sourceBrands:*Feed* incident.id:{incident_id}'
+    indicator_query = f'sourceBrands:*Feed* and incident.id:{incident_id}'
     fetched_iocs = demisto.searchIndicators(query=indicator_query).get('iocs')
     feeds = set()
     for indicator in fetched_iocs:
         source_brands = indicator.get('sourceBrands')
-        [feeds.add(brand) for brand in source_brands if 'Feed' in brand]
+        for brand in source_brands:
+            if 'Feed' in brand:
+                feeds.add(brand)
     return feeds
 
 
@@ -44,20 +46,25 @@ def sum_number_of_feeds_for_an_incident(incident_id, feed_counter):
 
 def get_incidents_per_feed(from_date):
     all_incidents = get_all_incidents(from_date)
-    feed_counter = {}
+    feed_counter: dict = {}
     for incident in all_incidents:
         incident_id = incident.get('investigationId')
         sum_number_of_feeds_for_an_incident(incident_id, feed_counter)
     return feed_counter
 
 
-default_from_date = get_default_from_date('30 days')
-from_date = demisto.args().get('from', default_from_date)
-data = get_incidents_per_feed(from_date)
-demisto.results({
-    'total': len(data),
-    'data': [{
-        'Feed Name': key,
-        'Number Of Incidents': val
-    } for key, val in data.items()]})
-demisto.results('Done')
+def main():
+    default_from_date = get_default_from_date('30 days')
+    from_date = demisto.args().get('from', default_from_date)
+    data = get_incidents_per_feed(from_date)
+    demisto.results({
+        'total': len(data),
+        'data': [{
+            'Feed Name': key,
+            'Number Of Incidents': val
+        } for key, val in data.items()]})
+    demisto.results('Done')
+
+
+if __name__ == "__main__":
+    main()
