56260a8d9da53b47e326df3c64268a94f24bbfd3
ShahafBenYakir <shahaf.benyakir@demisto.com>
2020-03-17 11:57:02 +0200

    Doc string and typing

--- Packs/ThreatIntelligenceManagement/Scripts/ThreatIntelligenceManagementGetIncidentsPerFeed/ThreatIntelligenceManagementGetIncidentsPerFeed.py
+++ Packs/ThreatIntelligenceManagement/Scripts/ThreatIntelligenceManagementGetIncidentsPerFeed/ThreatIntelligenceManagementGetIncidentsPerFeed.py
@@ -3,21 +3,35 @@ from CommonServerPython import *
 from CommonServerUserPython import *
 
 
-def get_default_from_date(date_range):
+def get_default_from_date(date_range: str) -> str:
+    """
+    Gets a range string (eg. 30 days) and return a date string in the relevant Demisto query format.
+    :param date_range: string
+        Range (eg. 2 months) to create the date string from
+    :return: string
+        Date string in the relevant Demisto query format.
+    """
     from_date, _ = parse_date_range(date_range=date_range)
     str_from_date = from_date.strftime('%Y-%m-%dT%H:%M:%SZ')
     return str_from_date
 
 
-def get_all_incidents(from_date):
-    contents = demisto.executeCommand("getIncidents", {"fromdate": from_date})[0]['Contents']
+def get_all_incidents(from_date: str) -> list:
+    """
+    Query all incidents starting a given date.
+    :param from_date: string
+        The date to query incidents from.
+    :return: list
+        List of incidents.
+    """
+    contents = demisto.executeCommand('getIncidents', {'fromdate': from_date})[0]['Contents']
     incidents = contents['data']
     size = len(incidents)
     total = contents['total']
     page = 1
 
     while total > size:
-        contents = demisto.executeCommand("getIncidents", {"fromdate": from_date, "page": page})[0]['Contents']
+        contents = demisto.executeCommand('getIncidents', {'fromdate': from_date, 'page': page})[0]['Contents']
         new_incidents = contents['data']
         incidents += new_incidents
         size = len(incidents)
@@ -26,7 +40,14 @@ def get_all_incidents(from_date):
     return incidents
 
 
-def get_feeds_for_incident(incident_id):
+def get_feeds_for_incident(incident_id: int) -> list:
+    """
+    Retrieves a list feeds based on indicators that appear in a given incident.
+    :param incident_id: int
+        Incident ID to query by.
+    :return: list
+        List of feeds that have indicators in the given incident.
+    """
     indicator_query = f'sourceBrands:*Feed* and incident.id:{incident_id}'
     fetched_iocs = demisto.searchIndicators(query=indicator_query).get('iocs')
     feeds = set()
@@ -38,13 +59,30 @@ def get_feeds_for_incident(incident_id):
     return feeds
 
 
-def sum_number_of_feeds_for_an_incident(incident_id, feed_counter):
+def sum_number_of_feeds_for_an_incident(incident_id: int, feed_counter: dict) -> dict:
+    """
+    Counts the number of feeds that are related to a given incident (due to indicators that appear in the incident)
+    :param incident_id: int
+        the incident ID to count the feeds for.
+    :param feed_counter: dict
+        The general dictionary that holds all the sums.
+    :return: dict
+        Updated dictionary counter.
+    """
     feeds = get_feeds_for_incident(incident_id)
     for feed in feeds:
         feed_counter[feed] = feed_counter.get(feed, 0) + 1
 
 
-def get_incidents_per_feed(from_date):
+def get_incidents_per_feed(from_date: str) -> dict:
+    """
+    Counts the number of feeds that are related to all incidents created since a given date (due to indicators that
+    appear in the incident)
+    :param from_date: string
+        The date to starty query incidents from.
+    :return: dict
+        Dictionary with feeds as keys and related incident count as values
+    """
     all_incidents = get_all_incidents(from_date)
     feed_counter: dict = {}
     for incident in all_incidents:
