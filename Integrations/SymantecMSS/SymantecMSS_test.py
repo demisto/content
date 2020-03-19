ea1ed7753cbc1768a9c85bc9528dacab8cecdd40
olichter <olichter@paloaltonetworks.com>
2020-03-19 15:01:08 +0200

    Changelog

---
+++ Integrations/SymantecMSS/SymantecMSS_test.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import demistomock as demisto
+
+
+def test_bug(mocker, requests_mock):
+    mocker.patch.object(demisto, 'params', return_value={"server": 'https://example.com',
+                                                         "certificate": 'cert',
+                                                         'severities': 'Emergency,Critical'
+                                                         })
+    mocker.patch.object(demisto, 'args', return_value={'number': '14'})
+    mocker.patch.object(demisto, 'command', return_value="symantec-mss-get-incident")
+    with open('../../TestData/SymantecXML.txt', 'rb') as f:
+        text_to_return = f.read()
+    text_to_return = text_to_return.decode('UTF-8')
+    requests_mock.post('https://example.com/SWS/incidents.asmx', text=text_to_return)
+    import SymantecMSS as mss  # noqa
