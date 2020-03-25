b604d2d5a0e0afd9082b1e7953de00e4be04dd1a
liorblob <lblobstein@paloaltonetworks.com>
2020-03-25 17:54:50 +0200

    delete

--- Tests/scripts/setup_rsyslog.sh
+++
@@ -1,3 +0,0 @@
-sudo echo "module(load=\"imudp\")" >> /etc/rsyslog.conf
-sudo echo "input(type=\"imudp\" port=\"514\")" >> /etc/rsyslog.conf
-sudo echo "*.* @@`hostname -i`:5140" >> /etc/rsyslog.conf
