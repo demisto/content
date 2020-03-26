760b62a361a052fcfa79188d6c9eb9c3734d5817
liorblob <lblobstein@paloaltonetworks.com>
2020-03-26 13:08:56 +0200

    more cleanup

--- Tests/store_packs.py
+++ Tests/store_packs.py
@@ -16,7 +16,7 @@ from distutils.util import strtobool
 from distutils.version import LooseVersion
 from datetime import datetime
 from zipfile import ZipFile, ZIP_DEFLATED
-from Tests.test_utils import run_command, print_error, print_warning, print_color, LOG_COLORS, \
+from demisto_sdk.commands.common.tools import run_command, print_error, print_warning, print_color, LOG_COLORS, \
     collect_pack_content_items, input_to_list
 
 # global constants
