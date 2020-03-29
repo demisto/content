c07aad5dfe95ed9453c8517ff643a9ef29d851c6
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-03-29 18:14:13 +0300

    added dependencies images logic

--- Tests/scripts/infrastructure_tests/store_packs_test.py
+++ Tests/scripts/infrastructure_tests/store_packs_test.py
@@ -38,7 +38,8 @@ USER_PACK_METADATA = {
 class TestMetadata:
     def test_validate_fields_of_parsed_metadata(self):
         parsed_metadata = Pack._parse_pack_metadata(user_metadata=USER_PACK_METADATA, pack_content_items={},
-                                                    pack_id='test_pack_id', integration_images=[], author_image="")
+                                                    pack_id='test_pack_id', integration_images=[], author_image="",
+                                                    all_level_pack_dependencies={})
         assert parsed_metadata['name'] == 'Test Pack Name'
         assert parsed_metadata['id'] == 'test_pack_id'
         assert parsed_metadata['description'] == 'Description of test pack'
@@ -68,7 +69,8 @@ class TestMetadata:
     @pytest.mark.parametrize("empty_metadata", [{}, []])
     def test_parsed_metadata_empty_input(self, empty_metadata):
         parsed_metadata = Pack._parse_pack_metadata(user_metadata=empty_metadata, pack_content_items={},
-                                                    pack_id='test_pack_id', integration_images=[], author_image=[])
+                                                    pack_id='test_pack_id', integration_images=[], author_image=[],
+                                                    all_level_pack_dependencies={})
 
         assert parsed_metadata['name'] == "test_pack_id"
         assert parsed_metadata['id'] == "test_pack_id"
