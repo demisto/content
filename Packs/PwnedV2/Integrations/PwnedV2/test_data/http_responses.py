19d33ed82b3b66c2bf5bd779c4e24e6c46b7fd4f
ohaim <ohaim@paloaltonetworks.com>
2020-03-29 11:18:33 +0300

    Merge branch 'master' into pawnedv2

---
+++ Packs/PwnedV2/Integrations/PwnedV2/test_data/http_responses.py
@@ -0,0 +1,49 @@
+username_req = [
+    {
+        u'PwnCount': 1247574, u'Domain': u'gawker.com', u'IsSensitive': False, u'Name': u'Gawker', u'Title':
+        u'Gawker', u'DataClasses': [u'Email addresses', u'Passwords', u'Usernames'], u'IsRetired':
+        False, u'IsSpamList': False, u'BreachDate': u'2010-12-11', u'IsFabricated': False, u'ModifiedDate':
+        u'2013-12-04T00:00:00Z', u'LogoPath': u'https://haveibeenpwned.com/Content/Images/PwnedLogos/Gawker.png',
+        u'AddedDate': u'2013-12-04T00:00:00Z', u'IsVerified': True, u'Description':
+        u'In December 2010, Gawker was attacked by the hacker collective &quot;Gnosis&quot; in retaliation for what was'
+        u' reported to be a feud between Gawker and 4Chan. Information about Gawkers 1.3M users was published along'
+        u' with the data from Gawker\'s other web presences including Gizmodo and Lifehacker. Due to the prevalence of'
+        u' password reuse, many victims of the breach <a href="http://www.troyhunt.com/2011/01/why-your-apps-security'
+        u'-design-could.html" target="_blank" rel="noopener">then had their Twitter accounts compromised to send Acai'
+        u' berry spam</a>.'}, {u'PwnCount': 191540, u'Domain': u'hackforums.net', u'IsSensitive': False, u'Name':
+        u'HackForums', u'Title': u'hackforums.net', u'DataClasses': [u'Dates of birth', u'Email addresses',
+                                                                     u'Instant messenger identities', u'IP addresses',
+                                                                     u'Passwords', u'Social connections',
+                                                                     u'Spoken languages', u'Time zones',
+                                                                     u'User website URLs', u'Usernames',
+                                                                     u'Website activity'], u'IsRetired':
+        False, u'IsSpamList': False, u'BreachDate': u'2011-06-25', u'IsFabricated': False, u'ModifiedDate':
+        u'2014-05-11T10:30:43Z', u'LogoPath': u'https://haveibeenpwned.com/Content/Images/PwnedLogos/HackForums.png',
+                               u'AddedDate': u'2014-05-11T10:30:43Z', u'IsVerified': True, u'Description':
+                                       u'In June 2011, the hacktivist group known as "LulzSec" leaked <a href='
+                                       u'"http://www.forbes.com/sites/andygreenberg/2011/06/25/lulzsec-says-goodbye-'
+                                       u'dumping-nato-att-gamer-data/" target="_blank" rel="noopener">one final large'
+                                       u' data breach they titled "50 days of lulz"</a>. The compromised data came from'
+                                       u' sources such as AT&T, Battlefield Heroes and the <a href="http://hackforums.'
+                                       u'net" target="_blank" rel="noopener">hackforums.net website</a>. The leaked '
+                                       u'Hack Forums data included credentials and personal information of nearly '
+                                       u'200,000 registered forum users.'
+                               }
+]
+
+domain_req = [{u'PwnCount': 152445165, u'Domain': u'adobe.com', u'IsSensitive': False, u'Name': u'Adobe', u'Title':
+    u'Adobe', u'DataClasses': [u'Email addresses', u'Password hints', u'Passwords', u'Usernames'], u'IsRetired': False,
+               u'IsSpamList': False, u'BreachDate': u'2013-10-04', u'IsFabricated': False, u'ModifiedDate':
+                   u'2013-12-04T00:00:00Z', u'LogoPath': u'https://haveibeenpwned.com/Content/Images/PwnedLogos/Adobe'
+                                                         u'.png', u'AddedDate': u'2013-12-04T00:00:00Z', u'IsVerified':
+                   True, u'Description': u'In October 2013, 153 million Adobe accounts were breached with each'
+                                         u' containing an internal ID, username, email, <em>encrypted</em> password and'
+                                         u' a password hint in plain text. The password cryptography was poorly done'
+                                         u' and <a href="http://stricture-group.com/files/adobe-top100.txt" target="_'
+                                         u'blank" rel="noopener">many were quickly resolved back to plain text</a>. '
+                                         u'The unencrypted hints also <a href="http://www.troyhunt.com/2013/11/adobe-'
+                                         u'credentials-and-serious.html" target="_blank" rel="noopener">disclosed much'
+                                         u' about the passwords</a> adding further to the risk that hundreds of '
+                                         u'millions of Adobe customers already faced.'
+               }
+              ]
