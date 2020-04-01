f6458c8fd2d729d33c9927f1803b3faf9d16cf93
ikeren <itay@demisto.com>
2020-04-01 18:59:14 +0300

    add test data to getusersoncall widget

--- Packs/ShiftManagement/Scripts/GetUsersOnCall/GetUsersOnCall_test.py
+++ Packs/ShiftManagement/Scripts/GetUsersOnCall/GetUsersOnCall_test.py
@@ -8,10 +8,10 @@ USERS_ON_CALL_RESULTS = [
         'HumanReadable': '## On-Call Users'
                          'Username | Email | Name | Phone | Roles'
                          '-|-|-|-|'
-                         'bar | bar@demisto.com | Bar Lolz | \+650-123456 | demisto: \[AnalystShift\]'
-                         'eliya | eliya@demisto.com | Eliya Sadan | \+650-655989 | demisto: \[AnalystShift\]'
-                         'ido | ido@demisto.com | Ido Shavit | \+650-655989 | demisto: \[AnalystShift\]'
-                         'yuval | yuval@demisto.com | Yuval Ben-Shalom | \+650-655989 | demisto: \[AnalystShift\]'
+                         'batman | batman@demisto.com | Batman | \+650-123456 | demisto: \[AnalystShift\]'
+                         'robin | robin@demisto.com | Robin | \+650-655989 | demisto: \[AnalystShift\]'
+                         'joker | joker@demisto.com | The Joker | \+650-655989 | demisto: \[AnalystShift\]'
+                         'brucewaine | bwaine@demisto.com | Bruce Waine | \+650-655989 | demisto: \[AnalystShift\]'
     }
 ]
 
