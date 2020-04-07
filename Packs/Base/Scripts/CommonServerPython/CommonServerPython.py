1b7992f850d264bc0d092378ed4cdc11f6dc1ee1
Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
2020-04-07 09:31:42 +0300

    Update CommonServerPython.py

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -25,8 +25,8 @@ try:
 except Exception:
     pass
 
-CONTENT_RELEASE_VERSION = '2.5.0'
-CONTENT_BRANCH_NAME = 'content_legacy_pack'
+CONTENT_RELEASE_VERSION = '0.0.0'
+CONTENT_BRANCH_NAME = 'master'
 IS_PY3 = sys.version_info[0] == 3
 
 # pylint: disable=undefined-variable
