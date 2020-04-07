f2eaa6896882da7e64168ba9893469eee788ced7
ikeren <itay@demisto.com>
2020-04-07 11:17:23 +0300

    handle no users on-call

--- Packs/ShiftManagement/Scripts/GetUsersOnCall/GetUsersOnCall.py
+++ Packs/ShiftManagement/Scripts/GetUsersOnCall/GetUsersOnCall.py
@@ -10,10 +10,13 @@ def main():
     if is_error(get_users_response):
         demisto.error(f'Failed to get users on call: {str(get_error(get_users_response))}')
     else:
+        contents = get_users_response[0]['HumanReadable']
+        if contents == 'No data returned':
+            contents = '### On-Call Users\nNo analysts were found on-call.'
         demisto.results({
             'Type': entryTypes['note'],
             'ContentsFormat': formats['markdown'],
-            'Contents': get_users_response[0]['HumanReadable']
+            'Contents': contents
         })
 
 
