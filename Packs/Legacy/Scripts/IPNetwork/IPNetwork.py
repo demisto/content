c30d2a18aa8d8d477c7430718d7642f25fa421fc
gal <galrabin@gmail.com>
2020-04-16 16:58:32 +0300

    fix secrets

--- Packs/Legacy/Scripts/IPNetwork/IPNetwork.py
+++ Packs/Legacy/Scripts/IPNetwork/IPNetwork.py
@@ -7,16 +7,6 @@ from CommonServerPython import *
 
 # Local imports
 
-# Gather information regarding CIDR -
-#     1. Broadcast_address
-#     2. CIDR
-#     3. First_address
-#     4. Last address
-#     5. Max prefix len
-#     6. Num addresses
-#     7. Private
-#     8. Version
-
 
 def ip_cidr(cidr: str) -> Dict[str, str]:
     """ Gather info regarding the supplied network
