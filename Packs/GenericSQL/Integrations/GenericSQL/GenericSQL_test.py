e7b6d8dd6a516e27c0a532b96fa07793f85963b5
ohaim <ohaim@paloaltonetworks.com>
2020-04-18 13:42:07 +0300

    fixed

--- Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
+++ Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
@@ -16,18 +16,18 @@ args2 = {
 raw1 = [{'Name': 'Kabul'}, {'Name': 'Qandahar'}, {'Name': 'Herat'}, {'Name': 'Mazar-e-Sharif'}]
 
 raw2 = [{'Host': '%', 'User': 'admin', 'Select_priv': 'Y', 'Insert_priv': 'Y', 'Update_priv': 'Y', 'Delete_priv': 'Y',
-          'Create_priv': 'Y', 'Drop_priv': 'Y', 'Reload_priv': 'Y', 'Shutdown_priv': 'N', 'Process_priv': 'Y',
-          'File_priv': 'N', 'Grant_priv': 'Y', 'References_priv': 'Y', 'Index_priv': 'Y', 'Alter_priv': 'Y',
-          'Show_db_priv': 'Y', 'Super_priv': 'N', 'Create_tmp_table_priv': 'Y', 'Lock_tables_priv': 'Y',
-          'Execute_priv': 'Y', 'Repl_slave_priv': 'Y', 'Repl_client_priv': 'Y', 'Create_view_priv': 'Y',
-          'Show_view_priv': 'Y', 'Create_routine_priv': 'Y', 'Alter_routine_priv': 'Y', 'Create_user_priv': 'Y',
-          'Event_priv': 'Y', 'Trigger_priv': 'Y', 'Create_tablespace_priv': 'N', 'ssl_type': '', 'ssl_cipher': b'',
-          'x509_issuer': b'', 'x509_subject': b'', 'max_questions': 0, 'max_updates': 0, 'max_connections': 0,
-          'max_user_connections': 0, 'plugin': 'mysql_native_password',
-          'authentication_string': 'test', 'password_expired': 'N',
-          'password_last_changed': '2020-02-17 08:49:45', 'password_lifetime': None,
-          'account_locked': 'N', 'Create_role_priv': 'N', 'Drop_role_priv': 'N', 'Password_reuse_history': None,
-          'Password_reuse_time': None, 'Password_require_current': None, 'User_attributes': None}]
+         'Create_priv': 'Y', 'Drop_priv': 'Y', 'Reload_priv': 'Y', 'Shutdown_priv': 'N', 'Process_priv': 'Y',
+         'File_priv': 'N', 'Grant_priv': 'Y', 'References_priv': 'Y', 'Index_priv': 'Y', 'Alter_priv': 'Y',
+         'Show_db_priv': 'Y', 'Super_priv': 'N', 'Create_tmp_table_priv': 'Y', 'Lock_tables_priv': 'Y',
+         'Execute_priv': 'Y', 'Repl_slave_priv': 'Y', 'Repl_client_priv': 'Y', 'Create_view_priv': 'Y',
+         'Show_view_priv': 'Y', 'Create_routine_priv': 'Y', 'Alter_routine_priv': 'Y', 'Create_user_priv': 'Y',
+         'Event_priv': 'Y', 'Trigger_priv': 'Y', 'Create_tablespace_priv': 'N', 'ssl_type': '', 'ssl_cipher': b'',
+         'x509_issuer': b'', 'x509_subject': b'', 'max_questions': 0, 'max_updates': 0, 'max_connections': 0,
+         'max_user_connections': 0, 'plugin': 'mysql_native_password',
+         'authentication_string': 'test', 'password_expired': 'N',
+         'password_last_changed': '2020-02-17 08:49:45', 'password_lifetime': None,
+         'account_locked': 'N', 'Create_role_priv': 'N', 'Drop_role_priv': 'N', 'Password_reuse_history': None,
+         'Password_reuse_time': None, 'Password_require_current': None, 'User_attributes': None}]
 
 expected_output1 = {
     'GenericSQL(val.Query && val.Query === obj.Query)':
@@ -69,7 +69,6 @@ header2 = ['Host', 'User', 'Select_priv', 'Insert_priv', 'Update_priv', 'Delete_
            'max_user_connections', 'plugin', 'authentication_string', 'password_expired', 'password_last_changed',
            'password_lifetime', 'account_locked', 'Create_role_priv', 'Drop_role_priv', 'Password_reuse_history',
            'Password_reuse_time', 'Password_require_current', 'User_attributes']
-
 
 
 @pytest.mark.parametrize('command, args, response, expected_result, header', [
