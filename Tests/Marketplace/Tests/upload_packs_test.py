4829fd9fcdd4cad0a804389fd22217dfdcffb4a9
liorblob <lblobstein@paloaltonetworks.com>
2020-04-19 19:07:47 +0300

    add docs, more tests

--- Tests/Marketplace/Tests/upload_packs_test.py
+++ Tests/Marketplace/Tests/upload_packs_test.py
@@ -43,7 +43,129 @@ def scan_dir(dirs=None):
 
 
 class TestUpdateIndex:
-    def test_update_index_folder_versioned(self, mocker):
+    def test_update_index_folder_new_version(self, mocker):
+        """
+        Scenario: Update the bucket index when a pack is updated (new version)
+
+        Given
+        - Pack exists in the index folder
+        - Pack has a new version
+
+        When
+        - Updating the bucket index
+
+        Then
+        - Ensure new metadata files are created for the new version
+        - Ensure previous metadata files are not deleted
+        - Ensure other files in the index are removed and replaced
+        """
+        from Tests.Marketplace import upload_packs
+        import shutil
+        import os
+
+        mocker.patch('glob.glob', return_value=['Index/HelloWorld/metadata-1.0.1.json',
+                                                'Index/HelloWorld/metadata-1.0.0.json',
+                                                'Index/HelloWorld/metadata-2.0.0.json'])
+        mocker.patch('os.listdir', return_value=['HelloWorld'])
+        mocker.patch('os.path.isdir', return_value=True)
+        mocker.patch('os.remove')
+        mocker.patch('shutil.copy')
+        pack_dirs = scan_dir([('HelloWorld/metadata.json', 'metadata.json'),
+                              ('HelloWorld/changelog.json', 'changelog.json'),
+                              ('HelloWorld/README.md', 'README.md')])
+        index_dirs = scan_dir([('Index/HelloWorld/metadata-1.0.1.json', 'metadata-1.0.1.json'),
+                               ('Index/HelloWorld/metadata-1.0.0.json', 'metadata-1.0.0.json'),
+                               ('Index/HelloWorld/metadata-2.0.0.json', 'metadata-2.0.0.json'),
+                               ('Index/HelloWorld/metadata.json', 'metadata.json'),
+                               ('Index/HelloWorld/changelog.json', 'changelog.json'),
+                               ('Index/HelloWorld/README.md', 'README.md')])
+
+        mocker.patch('os.scandir', side_effect=[index_dirs, pack_dirs])
+
+        upload_packs.update_index_folder('Index', 'HelloWorld', 'HelloWorld', '2.0.1')
+
+        expected_remove_args = ['Index/HelloWorld/metadata.json',
+                                'Index/HelloWorld/changelog.json', 'Index/HelloWorld/README.md']
+        expected_copy_args = [('HelloWorld/metadata.json', 'Index/HelloWorld'),
+                              ('HelloWorld/metadata.json', 'Index/HelloWorld/metadata-2.0.1.json'),
+                              ('HelloWorld/changelog.json', 'Index/HelloWorld'),
+                              ('HelloWorld/README.md', 'Index/HelloWorld')]
+
+        remove_call_count = os.remove.call_count
+        remove_call_args = os.remove.call_args_list
+        copy_call_count = shutil.copy.call_count
+        copy_call_args = shutil.copy.call_args_list
+
+        assert remove_call_count == 3
+        assert copy_call_count == 4
+        for call_arg in remove_call_args:
+            assert call_arg[0][0] in expected_remove_args
+        for call_arg in copy_call_args:
+            assert call_arg[0] in expected_copy_args
+
+    def test_update_index_folder_new_pack(self, mocker):
+        """
+        Scenario: Update the bucket index when a new pack is created
+
+        Given
+        - Pack does not in the index folder
+
+        When
+        - Updating the bucket index
+
+        Then
+        - Ensure new metadata files are created for the new pack
+        - Ensure other files in the index are copied
+        """
+        from Tests.Marketplace import upload_packs
+        import shutil
+        import os
+
+        mocker.patch('glob.glob', return_value=[])
+        mocker.patch('os.listdir', return_value=[])
+        mocker.patch('os.remove')
+        mocker.patch('shutil.copy')
+        pack_dirs = scan_dir([('HelloWorld/metadata.json', 'metadata.json'),
+                              ('HelloWorld/changelog.json', 'changelog.json'),
+                              ('HelloWorld/README.md', 'README.md')])
+
+        mocker.patch('os.scandir', return_value=pack_dirs)
+
+        upload_packs.update_index_folder('Index', 'HelloWorld', 'HelloWorld', '1.0.0')
+
+        expected_copy_args = [('HelloWorld/metadata.json', 'Index/HelloWorld'),
+                              ('HelloWorld/metadata.json', 'Index/HelloWorld/metadata-1.0.0.json'),
+                              ('HelloWorld/changelog.json', 'Index/HelloWorld'),
+                              ('HelloWorld/README.md', 'Index/HelloWorld')]
+
+        remove_call_count = os.remove.call_count
+        copy_call_count = shutil.copy.call_count
+        copy_call_args = shutil.copy.call_args_list
+
+        assert remove_call_count == 0
+        assert copy_call_count == 4
+        for call_arg in copy_call_args:
+            assert call_arg[0] in expected_copy_args
+
+    def test_update_index_folder_multiple_versions(self, mocker):
+        """
+        Scenario: Update the bucket index when a pack is not updated (same version)
+                  and has multiple versions
+
+        Given
+        - Pack exists in the index folder
+        - Pack has multiple versions
+        - Pack is not updated
+
+        When
+        - Updating the bucket index
+
+        Then
+        - Ensure no new metadata files are created for the new version
+        - Ensure current metadata files are replaced
+        - Ensure previous metadata files are not deleted
+        - Ensure other files in the index are removed and replaced
+        """
         from Tests.Marketplace import upload_packs
         import shutil
         import os
@@ -89,6 +211,22 @@ class TestUpdateIndex:
             assert call_arg[0] in expected_copy_args
 
     def test_update_index_folder_one_version(self, mocker):
+        """
+        Scenario: Update the bucket index when a pack is not updated (same version)
+
+        Given
+        - Pack exists in the index folder
+        - Pack is not updated
+        - Pack has one version
+
+        When
+        - Updating the bucket index
+
+        Then
+        - Ensure no new metadata files are created for the new version
+        - Ensure current metadata files are replaced
+        - Ensure other files in the index are removed and replaced
+        """
         from Tests.Marketplace import upload_packs
         import shutil
         import os
@@ -129,6 +267,22 @@ class TestUpdateIndex:
             assert call_arg[0] in expected_copy_args
 
     def test_update_index_folder_not_versioned(self, mocker):
+        """
+        Scenario: Update the bucket index when a pack is not versioned
+
+        Given
+        - Pack exists in the index folder
+        - The pack is not versioned (no metadata-<version>.json in the index)
+
+        When
+        - Updating the bucket index
+
+        Then
+        - Ensure a metadata file is created with the version name
+        - Ensure current metadata files are replaced
+        - Ensure previous metadata files are not deleted
+        - Ensure other files in the index are removed and replaced
+        """
         from Tests.Marketplace import upload_packs
         import shutil
         import os
@@ -168,11 +322,28 @@ class TestUpdateIndex:
             assert call_arg[0] in expected_copy_args
 
     def test_update_index_folder_no_version(self, mocker):
+        """
+        Scenario: Update the bucket index when a pack has no version
+
+        Given
+        - Pack exists in the index folder
+        - The pack is has no version (e.g. private pack which we do not have details for)
+
+        When
+        - Updating the bucket index
+
+        Then
+        - Ensure no new metadata files are created
+        - Ensure previous metadata files are not deleted
+        - Ensure other files in the index are removed and replaced
+        """
         from Tests.Marketplace import upload_packs
         import shutil
         import os
 
-        mocker.patch('glob.glob', return_value=[])
+        mocker.patch('glob.glob', return_value=['Index/HelloWorld/metadata-1.0.1.json',
+                                                'Index/HelloWorld/metadata-1.0.0.json',
+                                                'Index/HelloWorld/metadata-2.0.0.json'])
         mocker.patch('os.listdir', return_value=['HelloWorld'])
         mocker.patch('os.path.isdir', return_value=True)
         mocker.patch('os.remove')
