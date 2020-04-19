f3ed4b2a6a13bb54d8ab5f3545a9fa834d08c6e1
bar katzir <bkatzir@paloaltonetworks.com>
2020-04-19 17:59:35 +0300

    cb-query, cb-check-query-status, cb-get-query-results, cb-query-process-details, cb-query-process-analysis, cb-check-query-process-status, cb-get-query-process-results, cb-get-watchlist-reports

--- Packs/CarbonBlackThreatHunter/Integrations/CarbonBlackThreatHunter/CarbonBlackThreatHunter.py
+++ Packs/CarbonBlackThreatHunter/Integrations/CarbonBlackThreatHunter/CarbonBlackThreatHunter.py
@@ -1,4 +1,4 @@
-from typing import Dict, Callable, Tuple, Any
+from typing import Dict, Callable, Tuple, Any, Optional
 
 import urllib3
 
@@ -46,30 +46,6 @@ class Client(BaseClient):
         start_time = now - timedelta(days=int(time_window))
         return str(start_time.strftime(cb_time_format))
 
-    @staticmethod
-    def delete_unnecessary_fields(response: dict):
-        """Delete unnecessary fields from the response.
-
-        Args:
-            response: the response dictionary.
-        """
-        if 'crossproc' in response:
-            del response['crossproc']
-        if 'crossproc_target_complete' in response:
-            del response['crossproc_target_complete']
-        if 'modload_complete' in response:
-            del response['modload_complete']
-        if 'modload_name' in response:
-            del response['modload_name']
-        if 'modload_hash' in response:
-            del response['modload_hash']
-        if 'hash' in response:
-            del response['hash']
-        if 'process_complete' in response:
-            del response['process_complete']
-        if 'regmod_complete' in response:
-            del response['regmod_complete']
-
     def http_request(self, method: str, url_suffix: str, version: str, params: str = None, data: dict = None):
         """
         Generic request to Carbon Black Threat Hunter
@@ -99,13 +75,14 @@ class Client(BaseClient):
 
         return result.json()
 
-    def search_jobs(self, payload: dict, query_type: str, version: str) -> dict:
-        """Search jobs in Threat Hunter.
+    def post_query_job(self, payload: dict, query_type: str, version: str) -> dict:
+        """Post a query job in Threat Hunter.
 
         Args:
             payload: payload
             query_type: query type
             version: API version
+
         Returns:
             Response from API.
         """
@@ -123,6 +100,7 @@ class Client(BaseClient):
             job_id: job ID
             query_type: query type
             max_rows: max rows of the job to return
+
         Returns:
             Response from API.
         """
@@ -158,11 +136,23 @@ class Client(BaseClient):
             }]
         }
 
-        job_id = self.search_jobs(payload, query_type, 'v2').get('job_id', {})
-        return self.get_job_results(job_id, query_type, '50')
+        job_id = self.post_query_job(payload, query_type, 'v2').get('job_id', {})
+        return job_id
 
-    def cb_get_process_details_request(self, process_guid: str, document_guid: str) -> dict:
-        """Get process details
+    def cb_query_status_request(self, job_id: str, query_type: str, max_rows: Optional[str] = '50') -> str:
+        """Check query status.
+
+        Args:
+            job_id: job ID
+            query_type: query type
+            max_rows: max rows
+        Returns:
+            Response from API.
+        """
+        return self.get_job_results(job_id, query_type, max_rows)
+
+    def cb_query_process_details_request(self, process_guid: str, document_guid: str) -> dict:
+        """Query process details.
 
         Args:
             process_guid: process ID
@@ -188,16 +178,51 @@ class Client(BaseClient):
             }
         }
 
-        query_id = self.search_jobs(payload, 'processes', '1').get('query_id', {})
-        response = self.get_job_results(query_id, 'processes', '1').get('results', [])
-        if not response:
-            raise Exception(f'No process details where retrieved from Carbon Black Threat Hunter'
-                            f' for process:{process_guid}.')
-        if response and isinstance(response, list):
-            response = response[0]
-        self.delete_unnecessary_fields(response)  # Remove all unnecessary fields from result
+        return self.post_query_job(payload, 'processes', 'v1').get('query_id', {})
 
-        return response
+    def cb_query_process_analysis_request(self, process_guid: str) -> dict:
+        """Query process analysis.
+
+        Args:
+            process_guid: process ID
+        Returns:
+            Response from API.
+        """
+        payload = {
+            "search_params": {
+                "q": f"process_guid:{process_guid} -(legacy:true OR enriched:true)",
+                "cb.full_docs": False,
+                "cb.all_fields": True,
+                "cb.min_device_timestamp": 0,
+                "rows": 500,
+                "facet": True,
+                "facet.field": [],
+                "facet.mincount": 1,
+                "fl": "*,document_guid",
+                "sort": "device_timestamp desc",
+                "fq": "{!collapse field=process_collapse_id sort='max(0,legacy) asc,device_timestamp desc'}"
+            }
+        }
+
+        return self.post_query_job(payload, 'processes', 'v1').get('query_id', {})
+
+    def cb_get_watchlists_request(self) -> dict:
+        """Get watchlists reports.
+
+        Returns:
+            Response from API.
+        """
+        self._base_url.replace('investigate', 'watchlistmgr')
+        return self.http_request('GET', url_suffix='/watchlist', version='v1')
+
+    def cb_get_watchlist_report_by_id(self, report_id: str) -> dict:
+        """Get watchlists reports.
+
+        Returns:
+            Response from API.
+        """
+        self._base_url.replace('investigate', 'watchlistmgr')
+        return self.http_request('GET', url_suffix='/report', version='v1')
 
 
 def test_module(client: Client, *_) -> Tuple[str, Dict, Dict]:
@@ -227,22 +252,17 @@ def cb_query(client: Client, args: dict) -> Tuple[Any, Dict, Dict]:
     except ValueError:
         raise Exception('time_window argument must receive an integer, e.g: 4.')
 
-    response = client.cb_query_request(query, query_type, time_window)
-    if response.get('results'):
-        result = response.get('results', {})
-        md_ = tableToMarkdown('Carbon Black Threat Hunter Query Result', result)
-    else:
-        result = response
-        md_ = "No result found for the given query."
-    # TODO - add a prettify for the ec obj
-    entry_context = {
-        'CB.ThreatHunter.Query.Result(val["Event ID"] && val["Event ID"] == obj["Event ID"])': result
-    }
-    return md_, entry_context, result
+    job_id = client.cb_query_request(query, query_type, time_window)
+
+    md_ = f'Query for: {query_type} with Job ID: {job_id} was submitted to Carbon Black Threat Hunter successfully.'
+    ec_ = {'JobID': job_id, 'Status': 'Pending'}
+    entry_context = {'CB.ThreatHunter.Query(val.JobID == obj.JobID)': ec_}
+
+    return md_, entry_context, job_id
 
 
-def cb_get_process_details(client: Client, args: dict) -> Tuple[str, Dict, Dict]:
-    """Get process details.
+def cb_check_query_status(client: Client, args: dict) -> Tuple[Any, Dict, Dict]:
+    """Check query status.
 
     Args:
         client: Client object with request.
@@ -250,14 +270,241 @@ def cb_get_process_details(client: Client, args: dict) -> Tuple[str, Dict, Dict]
     Returns:
         Demisto Outputs.
     """
+    job_id = str(args.get('job_id', ''))
+    query_type = str(args.get('type', ''))
+
+    response = client.cb_query_status_request(job_id, query_type)
+
+    if 'contacted' not in response or 'completed' not in response:
+        raise Exception("An error occurred with job result query.")
+    contacted = int(response.get('contacted', 0))
+    completed = int(response.get('completed', 0))
+    if contacted and completed and contacted == completed:
+        status = 'Completed'
+    else:
+        status = 'Pending'
+
+    md_ = f'Query status for: {query_type} with Job ID: {job_id} is: {status}.'
+    ec_ = {'JobID': job_id, 'Status': status}
+    entry_context = {'CB.ThreatHunter.Query(val.JobID == obj.JobID)': ec_}
+
+    return md_, entry_context, job_id
+
+
+def cb_get_query_results(client: Client, args: dict) -> Tuple[Any, Dict, Dict]:
+    """Get query results.
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+    Returns:
+        Demisto Outputs.
+    """
+    job_id = str(args.get('job_id', ''))
+    query_type = str(args.get('type', ''))
+
+    response = client.cb_query_status_request(job_id, query_type)
+    if 'contacted' not in response or 'completed' not in response:
+        raise Exception("An error occurred with job result query.")
+    contacted = int(response.get('contacted', 0))
+    completed = int(response.get('completed', 0))
+    if contacted and completed and contacted == completed:
+        status = 'Completed'
+    else:
+        status = 'Pending'
+
+    if status == 'Pending':
+        md_ = f'Results for query with Job ID: {job_id} are not available yet.'
+        ec_ = {'JobID': job_id, 'Status': status}
+        entry_context = {'CB.ThreatHunter.Query(val.JobID == obj.JobID)': ec_}
+    else:
+        if response.get('results'):
+            results = response.get('results', {})
+            md_ = tableToMarkdown('Carbon Black Threat Hunter Query Results:', t=results)
+            # TODO - add a prettify for the ec obj
+            entry_context = {'CB.ThreatHunter.Query(val.JobID == obj.JobID)': results}
+        else:
+            md_ = f'No results found for the given query: {job_id}.'
+            entry_context = {'CB.ThreatHunter.Query(val.JobID == obj.JobID)': response}
+
+    return md_, entry_context, response
+
+
+def cb_query_process_details(client: Client, args: dict) -> Tuple[str, Dict, Dict]:
+    """Get process details.
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        Demisto Outputs.
+    """
     process_guid = str(args.get('process_guid', ''))
     document_guid = str(args.get('document_guid', ''))
 
-    response = client.cb_get_process_details_request(process_guid, document_guid)
-    # TODO - add a prettify for the ec obj
-    entry_context = {'CB.ThreatHunter.Process.Details(val["Event ID"] && val["Event ID"] == obj["Event ID"])': response}
+    query_id = client.cb_query_process_details_request(process_guid, document_guid)
 
-    return f'Process details for: {process_guid} retrieved successfully.', entry_context, response
+    md_ = f'Query for process details with Query ID: {query_id} ' \
+          f'was submitted to Carbon Black Threat Hunter successfully.'
+    ec_ = {'QueryID': query_id, 'Status': 'Pending'}
+    entry_context = {'CB.ThreatHunter.Process(val.QueryID == obj.QueryID)': ec_}
+
+    return md_, entry_context, query_id
+
+
+def cb_query_process_analysis(client: Client, args: dict) -> Tuple[str, Dict, Dict]:
+    """Get process analysis.
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        Demisto Outputs.
+    """
+    process_guid = str(args.get('process_guid', ''))
+
+    query_id = client.cb_query_process_analysis_request(process_guid)
+
+    md_ = f'Query for process analysis with Query ID: {query_id} ' \
+          f'was submitted to Carbon Black Threat Hunter successfully.'
+    ec_ = {'QueryID': query_id, 'Status': 'Pending'}
+    entry_context = {'CB.ThreatHunter.Process(val.QueryID == obj.QueryID)': ec_}
+
+    return md_, entry_context, query_id
+
+
+def cb_check_query_process_status(client: Client, args: dict) -> Tuple[str, Dict, Dict]:
+    """Check process query status.
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        Demisto Outputs.
+    """
+    query_id = str(args.get('query_id', ''))
+
+    response = client.cb_query_status_request(query_id, 'processes', '1')
+
+    if 'contacted' not in response or 'completed' not in response:
+        raise Exception("An error occurred with job result query.")
+    contacted = int(response.get('contacted', 0))
+    completed = int(response.get('completed', 0))
+    if contacted and completed and contacted == completed:
+        status = 'Completed'
+    else:
+        status = 'Pending'
+
+    md_ = f'Query status for process with Query ID: {query_id} is: {status}.'
+    ec_ = {'QueryID': query_id, 'Status': status}
+    entry_context = {'CB.ThreatHunter.Process(val.QueryID == obj.QueryID)': ec_}
+
+    return md_, entry_context, query_id
+
+
+def cb_get_query_process_results(client: Client, args: dict) -> Tuple[str, Dict, Dict]:
+    """Get process query results.
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        Demisto Outputs.
+    """
+    query_id = str(args.get('query_id', ''))
+
+    response = client.cb_query_status_request(query_id, 'processes')
+
+    if 'contacted' not in response or 'completed' not in response:
+        raise Exception("An error occurred with job result query.")
+    contacted = int(response.get('contacted', 0))
+    completed = int(response.get('completed', 0))
+    if contacted and completed and contacted == completed:
+        status = 'Completed'
+    else:
+        status = 'Pending'
+
+    if status == 'Pending':
+        md_ = f'Results for query with Query ID: {query_id} are not available yet.'
+        ec_ = {'QueryID': query_id, 'Status': status}
+        entry_context = {'CB.ThreatHunter.Query(val.QueryID == obj.QueryID)': ec_}
+    else:
+        if response.get('results'):
+            if isinstance(response.get('results'), list):
+                results = response.get('results', [])[0]
+            else:
+                results = response.get('results')
+            delete_unnecessary_fields(results)  # Remove all unnecessary fields from result
+            md_ = tableToMarkdown('Carbon Black Threat Hunter Process Query Results:', t=results)
+            # TODO - add a prettify for the ec obj
+            entry_context = {'CB.ThreatHunter.Process(val.QueryID == obj.QueryID)': results}
+        else:
+            md_ = f'No results where retrieved from Carbon Black Threat Hunter for the given process query: {query_id}.'
+            entry_context = {'CB.ThreatHunter.Process(val.QueryID == obj.QueryID)': response}
+
+    return md_, entry_context, response
+
+
+def delete_unnecessary_fields(response: dict):
+    """Delete unnecessary fields from the response.
+
+    Args:
+        response: the response dictionary.
+    """
+    if 'crossproc' in response:
+        del response['crossproc']
+    if 'crossproc_target_complete' in response:
+        del response['crossproc_target_complete']
+    if 'modload_complete' in response:
+        del response['modload_complete']
+    if 'modload_name' in response:
+        del response['modload_name']
+    if 'modload_hash' in response:
+        del response['modload_hash']
+    if 'hash' in response:
+        del response['hash']
+    if 'process_complete' in response:
+        del response['process_complete']
+    if 'regmod_complete' in response:
+        del response['regmod_complete']
+
+
+def cb_get_watchlist_reports(client: Client, *_) -> Tuple[str, Dict, Dict]:
+    """Get watchlists reports.
+
+    Args:
+        client: Client object with request.
+
+    Returns:
+        Demisto Outputs.
+    """
+    response = client.cb_get_watchlists_request()
+
+    if 'results' not in response:
+        raise Exception('Watchlists reports are not available')
+
+    result = []
+    watchlists = response.get('results', {})
+    for watchlist in watchlists:
+        temp_dict = {}
+        report = watchlist.get('report', {})
+        if report:
+            temp_dict['WatchlistName'] = report.get('name')
+            report_ids = report.get('report_ids')
+            temp_dict['Reports'] = []
+            for report_id in report_ids:
+                report = client.cb_get_watchlist_report_by_id(report_id)
+                temp_dict['Reports'].append(report)
+            result.append(temp_dict)
+
+    md_ = tableToMarkdown('Carbon Black Threat Hunter Watchlists:', t=result)
+    entry_context = {'CB.ThreatHunter.Watchlists': result}
+
+    return md_, entry_context, response
 
 
 def main():
@@ -284,7 +531,13 @@ def main():
         commands: Dict[str, Callable[[Client, Dict[str, str]], Tuple[str, Dict[Any, Any], Dict[Any, Any]]]] = {
             'test-module': test_module,
             'cb-query': cb_query,
-            'cb-get-process-details': cb_get_process_details
+            'cb-check-query-status': cb_check_query_status,
+            'cb-get-query-results': cb_get_query_results,
+            'cb-query-process-details': cb_query_process_details,
+            'cb-query-process-analysis': cb_query_process_analysis,
+            'cb-check-query-process-status': cb_check_query_process_status,
+            'cb-get-query-process-results': cb_get_query_process_results,
+            'cb-get-watchlist-reports': cb_get_watchlist_reports,
         }
         if command in commands:
             return_outputs(*commands[command](client, demisto.args()))
