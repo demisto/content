429978d95b91a43219898fdb3f261033aef48798
ikeren <itay@demisto.com>
2020-04-20 17:36:36 +0300

    copy master to build subdir using gsutil

---
+++ Tests/scripts/prepare_content_packs_for_testing.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# exit on errors
+set -e
+
+CIRCLE_BRANCH=${CIRCLE_BRANCH:-unknown}
+CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-00000}
+
+if [[ -z "$GCS_MARKET_KEY" ]]; then
+    echo "$GCS_MARKET_KEY not set aborting!"
+    exit 1
+fi
+
+echo "Preparing content packs for testing ..."
+
+KF=$(mktemp)
+echo "$GCS_MARKET_KEY" > "$KF"
+gcloud auth activate-service-account --key-file="$KF" > auth.out 2>&1
+rm "$KF"
+echo "Auth loaded successfully."
+
+GCS_MARKET_BUCKET="marketplace-dist-dev"
+SOURCE_PATH="content/packs"
+TARGET_PATH="content/builds/$CIRCLE_BRANCH/$CIRCLE_BUILD_NUM"
+echo "Copying master files at: $SOURCE_PATH to target path: $TARGET_PATH ..."
+gsutil -m cp -r "gs://$GCS_MARKET_BUCKET/$SOURCE_PATH" "gs://$GCS_MARKET_BUCKET/$TARGET_PATH"
+echo "Finished copying successfully."
+
+echo "Finished preparing content packs for testing successfully."
