4d5d66483fc7a981cbe3a657ca25a291877179f1
grabin <grabin@paloaltonetworks.com>
2020-04-21 13:09:24 +0300

    New automation - SetGridField setting grid in incident command

--- Packs/Legacy/Scripts/SetGridField/SetGridField.py
+++ Packs/Legacy/Scripts/SetGridField/SetGridField.py
@@ -1,55 +1,131 @@
-from typing import Optional, NoReturn, Union, List, Dict
-
+# STD Libaries
+from typing import Optional, NoReturn, Union, List, Dict, Tuple
+# 3-rd party libaries
 import numpy as np
 import pandas as pd
-from jsonpath_ng import parse, jsonpath
+from jsonpath_ng import parse
 import phrases_case
-
+# Local packages
 import demistomock as demisto
-from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
-from CommonServerUserPython import *  # noqa: E402 lgtm [py/polluting-import]
+from CommonServerPython import *
+from CommonServerUserPython import *
 
 
-def get_table_columns(context_paths: str, headers: Optional[str], sort: Optional[str]) -> List[str]:
-    """ Decide on column in table.
+def get_current_table(grid_id: str, sort_by: Optional[str], columns: str) -> \
+        Tuple[List[Dict[str, str]], List[str]]:
+    """ Get current grid data
 
-        If grid headers choose it else use grid keys and normalize strings by: CamelCase, SnakeCase, Capitalized
+    Date retreived:
+        1. Column names.
+        2. Current grid data.
+    Validate:
+        1. Correct number of context paths.
+        2. Sort_by is a name of a column.
+        3. Grid ID.
+        4. Columns exists.
 
      Args:
-        context_paths: Path to item to collext the keys from (e.g. Key1.Key2: [])
-        headers: optinal headers for the girid.
+        grid_id(str): Normalized Grid ID (Machine name in `Settings -> Advanced -> Fields -> Field property` or in Incident
+                      Context Data.
+        sort_by(str): The static name of the column to sort the table rows by.
+        columns(str): Comma separated list of columns names, Should be defined if grid is empty otherwise the automation
+                      detect it automatically.
 
      Returns:
-         list: string representing the columns
+        list: Current grid as dict in following structure - [{'col1': 'val1'},{'col2': 'val2'},{'col3': 'val3'},
+              {'col4': 'val4'}].
+        list: Table columns name.
      """
-    columns: List[str] = argToList(context_paths.split('.')[-1]) if not headers else argToList(headers)
-    columns = [phrases_case.space(header).capitalize() for header in columns]
+    # Get current Grid data
+    current_table: Optional[dict] = demisto.incidents()[0].get("CustomFields", {}).get(grid_id)
+    if not current_table:
+        raise ValueError(f"The grid id isn't valid : {grid_id}")
+    # Validate columns number the same as context paths - If no data initiated skip validation, but check if columns specified
+    if not columns:
+        raise ValueError("Columns not specified - Its a mandatory arg when grid is empty.")
+    # Get columns
+    columns = argToList(columns)
+    # Validate sort is valide col
+    if sort_by and sort_by not in columns:
+        raise ValueError(f'sort_by: {sort_by} is not columns: {columns}')
 
-    sort_by = ''
-    if sort:
-        sort_by = phrases_case.space(sort.split('.')[-1]).capitalize()
-
-    return columns, sort_by
+    return current_table, columns
 
 
-def get_data_from_entry_context(context_paths: str, row_correlated: bool, columns: List[str]) -> np.ndarray:
-    """ Get data from Entry context
+def populate_dict(obj_dict: Dict[str, str], keys: List[str]) -> Dict[str, str]:
+    """ Populate dict with missing keys if not exists using empty string - used for saving on row correlation.
+
+    Args:
+        obj_dict: dict to validate.
+        keys: keys to validate
+
+    Returns:
+        dict: Full dictionary.
+
+    Examples:
+        >>> populate_dict(obj_dict={'a': 1, 'b': 2}, keys=['a', 'b', 'c'])
+        {'a': 1, 'b': 2, 'c': ''}
+    """
+    obj_dict_keys = list(obj_dict.keys())
+    # Remove unwanted keys
+    for key in obj_dict_keys:
+        if key not in keys:
+            obj_dict.pop(key)
+    # Add missing keys
+    for key in keys:
+        if key not in obj_dict_keys:
+            obj_dict[key] = ''
+    # Orgenize keys
+    new_obj_dict = {}
+    for key in keys:
+        new_obj_dict[key] = obj_dict[key]
+
+    return new_obj_dict
+
+
+def numpy_fillna(data: np.ndarray) -> np.ndarray:
+    """ Fill missing rows with empty strings in order to complete to full table size (if row isn't in full length complete, it
+        will complete to full size.
+
+    Args:
+        data(np.ndarray): data to evaluate and complete.
+
+    Returns:
+        np.ndarray: Filled table.
+    """
+    # Get lengths of each row of data
+    lens = np.array([len(i) for i in data])
+    # Mask of valid places in each row
+    mask = np.arange(lens.max()) < lens[:, None]
+    # Setup output array and put elements from data into masked positions
+    out = np.zeros(mask.shape, dtype=data.dtype)
+    out[mask] = np.concatenate(data)
+    out[out == 0] = ''
+
+    return out
+
+
+def get_data_from_entry_context(context_paths: str, row_correlation: bool) -> Union[np.ndarray, list]:
+    """ Build ndarray from Entry context.
 
      Args:
-         context_paths: Path to item to collext the keys from (e.g. Key1.Key2: [])
-         row_correlated: True if the rows correlated, if a row has meaning for example representing object propertied.
-
+        context_paths(str): Context path to build the Table from, If the Table is row correlated, The path until the last
+                            key should be the same for all paths, In addition the number of the given paths should be the
+                            same as the columns number in the original grid, because the grid column is immutable.
+        row_correlation(bool): True if the rows correlated (represent an object/item), else False.
      Returns:
-         list: Data to DataFrame
+         np.ndarray || list : table built from Entry Context.
      """
     context_paths_list: List[str] = argToList(context_paths)
-    if row_correlated:
+    if row_correlation:
+        # Get and validate Data when rows correlated
         correlated_keys = []
         absolute_path = ''
+        # Vaidation of path in the same item/object
         for context_path in context_paths_list:
             splited_path = context_path.split('.')
             if splited_path:
-                current_absolute_path = '.[*].'.join(splited_path[:-1])
+                current_absolute_path = '..'.join(splited_path[:-1])
                 if absolute_path:
                     if absolute_path != current_absolute_path:
                         raise ValueError(
@@ -59,45 +135,37 @@ def get_data_from_entry_context(context_paths: str, row_correlated: bool, column
                 correlated_keys.append(splited_path[-1])
             else:
                 raise ValueError(f'Unable to correlate rows due to not valid context path {context_path}')
+        # Search for requested data
+        jsonpath_expression = parse(f'{absolute_path}.[*]')
+        data = [populate_dict(obj_dict=item.value, keys=correlated_keys) for item in jsonpath_expression.find(demisto.context())]
 
-        jsonpath_expression_str = f'{absolute_path}.[*]'
-        jsonpath_expression = parse(jsonpath_expression_str)
-
-        return [populate_dict(item.value, correlated_keys) for item in jsonpath_expression.find(demisto.context())]
+        return data
     else:
+        # Get Data when rows not correlated
         data = []
         for context_path in context_paths_list:
-            jsonpath_expression_str = context_path.replace('.', '.[*].')
-            jsonpath_expression = parse(jsonpath_expression_str)
+            jsonpath_expression = parse(context_path.replace('.', '.[*].'))
             data.append(np.array([item.value for item in jsonpath_expression.find(demisto.context())]))
 
         return numpy_fillna(np.array(data))
 
 
-def populate_dict(obj_dict: Dict[str, str], correlated_keys: List[str]):
-    obj_dict_keys = obj_dict.keys()
-    for key in correlated_keys:
-        if key not in obj_dict_keys:
-            obj_dict[key] = ""
-    return obj_dict
+def build_table(data: Union[List[Dict[str, str]], np.ndarray], columns: List[str], sort_by: Optional[str], overwrite: bool,
+                current_table: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    """ Build updated table
 
+    Args:
+        data(np.ndarray || list): Data to add to the grid.
+        columns(list): Columns name.
+        sort_by(str): The static name of the column to sort the table rows by.
+        overwrite(bool): True if to overwrite current grid, False in order to append data.
+        current_table: Current grid/table data.
 
-def numpy_fillna(data):
-    # Get lengths of each row of data
-    lens = np.array([len(i) for i in data])
-
-    # Mask of valid places in each row
-    mask = np.arange(lens.max()) < lens[:, None]
-
-    # Setup output array and put elements from data into masked positions
-    out = np.zeros(mask.shape, dtype=data.dtype)
-    out[mask] = np.concatenate(data)
-    out[out == 0] = ''
-
-    return out
-
-
-def build_table(data, columns, sort_by):
+    Returns:
+        list: updated to table.
+    """
+    # Create new Table
+    demisto.results(str(data))
     if isinstance(data[0], dict):
         table = pd.DataFrame.from_dict(data=data)
         table.columns = columns
@@ -106,47 +174,81 @@ def build_table(data, columns, sort_by):
                              columns=columns)
     else:
         raise ValueError('No valid values')
-
+    # Sort table if mentioned
     if sort_by:
+        demisto.results(table)
         table.sort_values(by=sort_by)
+    # Change to Grid form
+    table = table.to_dict('records')
+    # Overwrite data if specified
+    if not overwrite:
+        demisto.results('here')
+        table.append(current_table)
 
     return table
 
 
-def build_grid_command(context_paths: str, row_correlated: bool, headers: Optional[str], sort: Optional[str]) -> str:
-    """ Build markdown grid by user input.
+def build_grid_command(grid_id: str, context_paths: str, sort_by: Optional[str], row_correlation: bool, overwrite: bool,
+                       columns: Optional[str]) -> List[Dict[str, str]]:
+    """ Build Table as dictionary for populate grid in incident layout.
+
+    The required table structure to populate in the incident is as follow:
+    [{'col1': 'val1'},{'col2': 'val2'},{'col3': 'val3'},{'col4': 'val4'}] which represent:
+    | col1 | col2 |
+    | ---- | ---- |
+    | val1 | val2 |
+    | val3 | val4 |
 
     Args:
-        context_paths: Path to item to collext the keys from (e.g. Key1.Key2: []).
-        row_correlated: True if the rows correlated, if a row has meaning for example representing object propertied.
-        headers: optinal headers for the girid.
-        sort: The entry context path to sort the rows by.
+        grid_id(str): Normalized Grid ID (Machine name in `Settings -> Advanced -> Fields -> Field property` or in Incident
+                      Context Data.
+        context_paths(str): Context path to build the Table from, If the Table is row correlated, The path until the last
+                            key should be the same for all paths, In addition the number of the given paths should be the
+                            same as the columns number in the original grid, because the grid column is immutable.
+        sort_by(str): The static name of the column to sort the table rows by.
+        row_correlation(bool): True if the rows correlated (represent an object/item), else False.
+        overwrite(bool): True if to overwrite current grid, False in order to append data.
+        columns(str): Comma separated list of columns names, Should be defined if grid is empty otherwise the automation
+                      detect it automatically.
 
     Returns:
-        str: Markdown table
+        dict: Table dictianary as shown above in explaination.
     """
-    columns, sort_by = get_table_columns(context_paths=context_paths,
-                                         headers=headers,
-                                         sort=sort)
+    current_table, columns = get_current_table(grid_id=grid_id,
+                                               sort_by=sort_by,
+                                               columns=columns)
     data = get_data_from_entry_context(context_paths=context_paths,
-                                       row_correlated=row_correlated,
-                                       columns=columns)
+                                       row_correlation=row_correlation)
     table = build_table(data=data,
                         columns=columns,
-                        sort_by=sort_by)
+                        sort_by=sort_by,
+                        overwrite=overwrite,
+                        current_table=current_table)
 
-    return table.to_markdown()
+    return table
 
 
 def main() -> Union[NoReturn, str]:
     try:
-        markdown = build_grid_command(context_paths=demisto.getArg('context_paths'),
-                                      headers=demisto.getArg('headers'),
-                                      row_correlated=(False if demisto.getArg('correlated_row') == 'false' else True),
-                                      sort=demisto.getArg('sort'))
-        demisto.results(markdown)
-    except Exception as ex:
-        return_error(f'Failed to execute setGridField. Error: {str(ex)}')
+        # Normalize grid id from any form to connected lower words, e.g. my_word/myWord -> myword
+        grid_id = phrases_case.camel(demisto.getArg('grid_id')).replace("'", "").lower()
+        # Build updated table
+        table = build_grid_command(grid_id=grid_id,
+                                   context_paths=demisto.getArg('context_paths'),
+                                   row_correlation=False if demisto.getArg('row_correlation') == 'false' else True,
+                                   sort_by=demisto.getArg('sort_by'),
+                                   overwrite=False if demisto.getArg('overwrite') == 'false' else True,
+                                   columns=demisto.getArg('columns'))
+        # Execute automation 'setIncident` which change the Context data in the incident
+        demisto.executeCommand("setIncident",
+                               {
+                                   'customFields':
+                                       {
+                                           grid_id: table
+                                       }
+                               })
+    except Exception as e:
+        return_error(f'Failed to execute setGridField. Error: {str(e)}')
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
--- Packs/Legacy/Scripts/SetGridField/SetGridField_test.py
+++ Packs/Legacy/Scripts/SetGridField/SetGridField_test.py
@@ -1,53 +1,69 @@
 import pytest
-from typing import Optional, List
+from typing import Optional, List, Dict
+import numpy as np
 
 
-@pytest.mark.parametrize(argnames="grid_keys, grid_headers, expected",
+@pytest.mark.parametrize(argnames="sort_by, columns, exp_columns, exp_exception",
                          argvalues=[
-                             (
-                                     "testName1, testName2, testName3",
-                                     "gridTestName1, gridTestName2, gridTestName3",
-                                     ["Grid test name1", "Grid test name2", "Grid test name3"]
-                             ),
-                             (
-                                     "testName1, testName2, testName3",
-                                     "",
-                                     ["Test name1", "Test name2", "Test name3"]
-                             ),
-                             (
-                                     "testName1, test_name2, testName3",
-                                     "",
-                                     ["Test name1", "Test name2", "Test name3"]
-                             )
+                             ('header1', 'header1,header2', ['header1', 'header2'], False),
+                             ('header3', 'header1,header2', ['header1', 'header2'], True),
+                             ('header1', '', ['header1', 'header2'], True)
                          ])
-def test_get_table_columns(grid_keys: str, grid_headers: Optional[str], expected: List[str]):
-    from SetGridField import get_table_columns
-    assert expected == get_table_columns(grid_headers=grid_headers,
-                                         grid_keys=grid_keys)
+def test_get_current_table(mocker, sort_by: Optional[str], columns: str, exp_columns: List[str], exp_exception: bool):
+    from SetGridField import get_current_table
+    mock = mocker.patch('SetGridField.demisto.incidents')
+    if exp_exception:
+        with pytest.raises(ValueError):
+            get_current_table(grid_id='mygrid',
+                              sort_by=sort_by,
+                              columns=columns)
+    else:
+        assert mock, exp_columns == get_current_table(grid_id='mygrid',
+                                                      sort_by=sort_by,
+                                                      columns=columns)
 
 
-@pytest.mark.parametrize(argnames="entry_context, context_keys, grid_keys, expected",
+@pytest.mark.parametrize(argnames='obj_dict, keys, exp_dict',
+                         argvalues=[
+                             ({'a': 1, 'b': 2}, ['a'], {'a': 1}),
+                             ({'b': 2, 'a': 1}, ['a', 'b'], {'a': 1, 'b': 2}),
+                             ({'b': 2}, ['a', 'b'], {'a': '', 'b': 2})
+                         ])
+def test_populate_dict(obj_dict: Dict[str, str], keys: List[str], exp_dict: Dict[str, str]):
+    from SetGridField import populate_dict
+    assert exp_dict == populate_dict(obj_dict=obj_dict, keys=keys)
+
+
+@pytest.mark.parametrize(argnames='data, exp_data',
+                         argvalues=[
+                             (np.array([['a'], ['b', 'c']]), np.array([['a', ''], ['b', 'c']])),
+                             (np.array([['a', 'b'], ['c']]), np.array([['a', 'b'], ['c', '']]))
+                         ])
+def test_numpy_fillna(data: np.ndarray, exp_data: np.ndarray):
+    from SetGridField import numpy_fillna
+    assert exp_data.tolist() == numpy_fillna(data=data).astype(np.str).tolist()
+
+
+@pytest.mark.parametrize(argnames="entry_context, context_paths, row_correlation, expected",
                          argvalues=[
                              (
-                                     {'a': {'b': {'c': 1}}}, "a.b", "c", [1]
+                                     {'a': {'b': {'c': 1}}}, "a.b.c", True, [{'c': 1}]
                              ),
                              (
-                                     {'a': {'b': [{'c': 1}, {'c': 2}]}}, "a.b", "c", [1, 2]
+                                     {'a': {'b': [{'c': 1}, {'c': 2}]}}, "a.b.c", True, [{'c': 1}, {'c': 2}]
                              ),
                              (
-                                     {'a': {'b': [{'c': 1}, {'c': 2}]}}, "a", "c", []
-                             ),
-                             (
-                                     {'a': {'b': [{'c': 1}, {'d': 2}]}}, "a.b, a.b", "c, d", [[1], [2]]
+                                     {'a': {'b': [{'c': 1}, {'d': 2}]}}, "a.b.c, a.b.d", True, [{'c': 1, 'd': ''},
+                                                                                                {'c': '', 'd': 2}]
                              ),
                              (
                                      {}, "a.b, a.b", "c, d", []
                              ),
                          ])
-def test_get_data_from_entry_context(mocker, entry_context, context_keys: str, grid_keys: str, expected: List):
+def test_get_data_from_entry_context(mocker, entry_context, context_paths: str, row_correlation: bool, expected: List):
     import SetGridField
     from numpy import array as np_array
     mocker.patch('SetGridField.demisto.context')
     SetGridField.demisto.context.return_value = entry_context
-    assert np_array(expected).all() == SetGridField.get_data_from_entry_context(context_keys=context_keys,
-                                                                                grid_keys=grid_keys).all()
+    assert expected == SetGridField.get_data_from_entry_context(context_paths=context_paths,
+                                                                row_correlation=row_correlation)
