80ea7673100f59e299fc179ff95a0e34449c60fe
Lior Kolnik <lior.kolnik@gmail.com>
2020-04-21 23:45:07 -0700

    IP Whitelisting - Adding GCP and AWS Playbooks, ListCompare Script

---
+++ Packs/IPWhitelisting/Scripts/CompareLists/CompareLists.py
@@ -0,0 +1,22 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+left = argToList(demisto.args().get('left'))
+right = argToList(demisto.args().get('right'))
+
+out = {
+    'ListCompare':
+        {
+            'LeftOnly': [x for x in left if x not in right],
+            'RightOnly': [x for x in right if x not in left],
+            'Both': [x for x in left if x in right]
+        }
+}
+
+demisto.results({
+    'Type': entryTypes['note'],
+    'ContentsFormat': formats['json'],
+    'Contents': json.dumps(out),
+    'HumanReadable': 'Set comparisons in Context.',
+    'EntryContext': out
+})
