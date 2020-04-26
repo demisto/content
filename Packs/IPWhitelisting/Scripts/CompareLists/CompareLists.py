e2ebbaa7884ec578e95f59b37e5af1042a728f2c
Lior Kolnik <lior.kolnik@gmail.com>
2020-04-26 02:45:27 -0700

    CompareLists -  put all code in  main

--- Packs/IPWhitelisting/Scripts/CompareLists/CompareLists.py
+++ Packs/IPWhitelisting/Scripts/CompareLists/CompareLists.py
@@ -1,22 +1,26 @@
 import demistomock as demisto
 from CommonServerPython import *
 
-left = argToList(demisto.args().get('left'))
-right = argToList(demisto.args().get('right'))
+def main():
+    left = argToList(demisto.args().get('left'))
+    right = argToList(demisto.args().get('right'))
 
-out = {
-    'ListCompare':
-        {
-            'LeftOnly': [x for x in left if x not in right],
-            'RightOnly': [x for x in right if x not in left],
-            'Both': [x for x in left if x in right]
-        }
-}
+    out = {
+        'ListCompare':
+            {
+                'LeftOnly': [x for x in left if x not in right],
+                'RightOnly': [x for x in right if x not in left],
+                'Both': [x for x in left if x in right]
+            }
+    }
 
-demisto.results({
-    'Type': entryTypes['note'],
-    'ContentsFormat': formats['json'],
-    'Contents': json.dumps(out),
-    'HumanReadable': 'Set comparisons in Context.',
-    'EntryContext': out
-})
+    demisto.results({
+        'Type': entryTypes['note'],
+        'ContentsFormat': formats['json'],
+        'Contents': json.dumps(out),
+        'HumanReadable': 'Set comparisons in Context.',
+        'EntryContext': out
+    })
+    
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
