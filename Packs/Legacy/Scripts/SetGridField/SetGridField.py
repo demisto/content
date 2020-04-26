ac2b56f7d6b2e962fdd68085bb6337b39776f4ea
grabin <grabin@paloaltonetworks.com>
2020-04-26 20:59:07 +0300

    fix after review 1.5

--- Packs/Legacy/Scripts/SetGridField/SetGridField.py
+++ Packs/Legacy/Scripts/SetGridField/SetGridField.py
@@ -1,9 +1,7 @@
 # STD Libaries
-from typing import Optional, Union, List, Dict, Tuple, Any
+from typing import Optional, List, Dict, Any, NoReturn
 # 3-rd party libaries
-import numpy as np
 import pandas as pd
-from jsonpath_ng import parse
 import phrases_case
 # Local packages
 import demistomock as demisto
@@ -12,225 +10,164 @@ from CommonServerUserPython import *
 
 
 def normalized_string(phrase: str) -> str:
-    return phrases_case.camel(phrase).replace("'", "").lower()
-
-def get_current_table(grid_id: str, sort_by: Optional[str], columns: Optional[str], context_paths: str) -> \
-        Tuple[List[Dict[Any, Any]], Any]:
-    """ Get current grid data
-
-    Data retreived:
-        1. Column names.
-        2. Current grid data.
-    Validate:
-        1. Correct number of context paths.
-        2. Sort_by is a name of a column.
-        3. Grid ID.
-        4. Columns exists.
-
-     Args:
-        grid_id(str): Normalized Grid ID (Machine name in `Settings -> Advanced -> Fields -> Field property` or in Incident
-                      Context Data.
-        sort_by(str): The static name of the column to sort the table rows by.
-        columns(str): Comma separated list of columns names, Should be defined if grid is empty otherwise the automation
-                      detect it automatically.
-        context_paths(str): Context path to build the Table from, If the Table is row correlated, The path until the last
-                            key should be the same for all paths, In addition the number of the given paths should be the
-                            same as the columns number in the original grid, because the grid column is immutable.
-
-     Returns:
-        list: Current grid as dict in following structure - [{'col1': 'val1'},{'col2': 'val2'},{'col3': 'val3'},
-              {'col4': 'val4'}].
-        list: Table columns name.
-     """
-    # Get current Grid data
-    current_table: Optional[List[dict]] = demisto.incidents()[0].get("CustomFields", {}).get(grid_id)
-    if not current_table:
-        raise ValueError(f"The grid id isn't valid: {grid_id}")
-    # Validate columns number the same as context paths - If no data initiated skip validation, but check if columns specified
-    columns_list = [normalized_string(phrase) for phrase in argToList(columns)]
-    if len(columns_list) == len(argToList(context_paths)):
-        raise ValueError("Columns not specified correctly!")
-    # Validate sort is valide col
-    if sort_by and sort_by not in columns:
-        raise ValueError(f'sort_by "{sort_by}" is not one of the columns: {columns}')
-
-    return current_table, columns
-
-
-def populate_dict(obj_dict: Dict[str, str], keys: List[str]) -> Dict[str, str]:
-    """ Populate dict with missing keys if not exists using empty string - used for saving on row correlation.
+    """ Normalize columns or Grid to connected word in lower-case.
 
     Args:
-        obj_dict: dict to validate.
-        keys: keys to validate
+        phrase: Phrase to normalize.
 
     Returns:
-        dict: Full dictionary.
+        str: Normalized phrase.
 
     Examples:
-        >>> populate_dict(obj_dict={'a': 1, 'b': 2}, keys=['a', 'b', 'c'])
-        {'a': 1, 'b': 2, 'c': ''}
+        >>> normalized_string("TestWord")
+        "testword"
+        >>> normalized_string("hello_world")
+        "hello_world"
     """
-    obj_dict_keys = list(obj_dict.keys())
-    # Remove unwanted keys
-    for key in obj_dict_keys:
-        if key not in keys:
-            obj_dict.pop(key)
-    # Add missing keys
-    for key in keys:
-        if key not in obj_dict_keys:
-            obj_dict[key] = ''
-    # Orgenize keys
-    new_obj_dict = {}
-    for key in keys:
-        new_obj_dict[key] = obj_dict[key]
-
-    return new_obj_dict
+    return phrases_case.camel(phrase).replace("'", "").lower()
 
 
-def numpy_fillna(data: np.ndarray) -> np.ndarray:
-    """ Fill missing rows with empty strings in order to complete to full table size (if row isn't in full length complete, it
-        will complete to full size.
+def filterTheDict(dict_obj: Dict[Any, Any], keys: List[str], max_keys: Optional[int] = None) -> Dict[Any, Any]:
+    """ Filter keys from Dictionary:
+            1. Will save all keys in keys parameter.
+            2. If key in index 0 is "*", will save all keys until max_keys (as much as Grid can include).
 
     Args:
-        data(np.ndarray): data to evaluate and complete.
+        dict_obj: Dictionary to filter keys from.
+        keys: Keys to save.
+        max_keys: Max keys to save in case of keys[0] = "*"
 
     Returns:
-        np.ndarray: Filled table.
+        dict: Filtered dict.
     """
-    # Get lengths of each row of data
-    lens = np.array([len(i) for i in data])
-    # Mask of valid places in each row
-    mask = np.arange(lens.max()) < lens[:, None]
-    # Setup output array and put elements from data into masked positions
-    out = np.zeros(mask.shape, dtype=data.dtype)
-    out[mask] = np.concatenate(data)
-    out[out == 0] = ''
+    new_dict = dict()
+    # Iterate over all the items in dictionary
+    if keys[0] != "*":
+        for (key, value) in dict_obj.items():
+            # Check if item satisfies the given condition then add to new dict
+            if key in keys:
+                new_dict[key] = value
+    else:
+        if max_keys:
+            new_dict = list(dict_obj.items())[:max_keys]
+        new_dict = dict_obj
 
-    return out
+    return new_dict
 
 
-def get_data_from_entry_context(context_paths: str, row_correlation: bool) -> Union[np.ndarray, list]:
-    """ Build ndarray from Entry context.
+def get_current_table(grid_id: str) -> List[Dict[Any, Any]]:
+    """ Get current Data from the grid
 
-     Args:
-        context_paths(str): Context path to build the Table from, If the Table is row correlated, The path until the last
-                            key should be the same for all paths, In addition the number of the given paths should be the
-                            same as the columns number in the original grid, because the grid column is immutable.
-        row_correlation(bool): True if the rows correlated (represent an object/item), else False.
-     Returns:
-         np.ndarray || list : table built from Entry Context.
-     """
-    context_paths_list: List[str] = argToList(context_paths)
-    if row_correlation:
-        # Get and validate Data when rows correlated
-        correlated_keys = []
-        absolute_path = ''
-        # Vaidation of path in the same item/object
-        for context_path in context_paths_list:
-            splited_path = context_path.split('.')
-            if splited_path:
-                current_absolute_path = '..'.join(splited_path[:-1])
-                if absolute_path:
-                    if absolute_path != current_absolute_path:
-                        raise ValueError(
-                            f'Unable to correlate rows due to keys not in the same context path {current_absolute_path}')
-                else:
-                    absolute_path = current_absolute_path
-                correlated_keys.append(splited_path[-1])
+    Args:
+        grid_id: Grid ID to retrieve data from.
+
+    Returns:
+        list: Exsiting grid data.
+    """
+    current_table: Optional[List[dict]] = demisto.incidents()[0].get("CustomFields", {}).get(grid_id)
+    if current_table is None:
+        raise ValueError(f"The grid id isn't valid: {grid_id}")
+
+    return pd.DataFrame(current_table)
+
+
+def validate_entry_context(entry_context: Any) -> NoReturn:
+    """ Validate entry context structure is valid should be:
+            1. List[Dict[str, str]
+            2. Dict[str, str] - for developer it will be in first index of a list.
+
+    Args:
+        entry_context: Entry context to validate
+
+    Raises:
+        ValueError: If structure is not valid.
+    """
+    exception_msg = "Not valid entry context path - dict[str,str] or list[dict[str,str]]"
+    if isinstance(entry_context, list):
+        for item in entry_context:
+            if not isinstance(item, dict):
+                raise ValueError(exception_msg)
             else:
-                raise ValueError(f'Unable to correlate rows due to not valid context path {context_path}')
-        # Search for requested data
-        jsonpath_expression = parse(f'{absolute_path}.[*]')
-        data = [populate_dict(obj_dict=item.value, keys=correlated_keys) for item in jsonpath_expression.find(demisto.context())]
-
-        return data
+                for key, value in item.items():
+                    if not (isinstance(key, str) and isinstance(value, str)):
+                        raise ValueError(exception_msg)
     else:
-        # Get Data when rows not correlated
-        data = []
-        for context_path in context_paths_list:
-            jsonpath_expression = parse(context_path.replace('.', '.[*].'))
-            data.append(np.array([item.value for item in jsonpath_expression.find(demisto.context())]))
-
-        return numpy_fillna(np.array(data))
+        raise ValueError(exception_msg)
 
 
-def build_table(data: Union[List[Dict[Any, Any]], Any], columns: List[str], sort_by: Optional[str],
-                overwrite: bool, current_table: List[Dict[Any, Any]]) -> List[Dict[Any, Any]]:
-    """ Build updated table
+def build_grid(context_path: str, keys: List[str], columns: List[str]) -> pd.DataFrame:
+    """ Build new DateFrame from current context retrieved by DT.
+        There is 3 cases:
+            1. DT returns dict (list including 1 item only)- In this case we will insert it in the table as key,
+            value each row.
+            2. DT returns list - In this case each entry in the list will represent a row.
+            3. DT return unknown obj (str..) - return empty list.
 
     Args:
-        data(np.ndarray || list): Data to add to the grid.
-        columns(list): Columns name.
-        sort_by(str): The static name of the column to sort the table rows by.
-        overwrite(bool): True if to overwrite current grid, False in order to append data.
-        current_table: Current grid/table data.
+        context_path: DT context path.
+        keys: Keys to be included
+        columns: Grid columns name.
 
     Returns:
-        list: updated to table.
+        pd.DataFrame: New Table include data from Entry Context
     """
-    # Create new Table
-    demisto.results(str(data))
-    if isinstance(data[0], dict):
-        table = pd.DataFrame.from_dict(data=data)
-        table.columns = columns
-    elif isinstance(data[0], np.ndarray):
-        table = pd.DataFrame(data=data.T,  # type: ignore
-                             columns=columns)
+    # Retrieve entry context data
+    entry_context_data = demisto.dt(demisto.context(), context_path)
+    # Validate entry context structure
+    validate_entry_context(entry_context_data)
+    # Building new Grid
+    if len(entry_context_data) > 1:
+        # Handle entry context list option
+        entry_context_filtered_by_keys = [filterTheDict(item, keys, len(columns)) for item in entry_context_data]
+        table = pd.DataFrame(entry_context_filtered_by_keys)
+        table.rename(columns=dict(zip(table.columns, columns)), inplace=True)
+    elif len(entry_context_data) == 1 and isinstance(entry_context_data[0], dict):
+        # Handle entry context key, vlaue option
+        entry_context_filtered_by_keys = filterTheDict(entry_context_data[0], keys).items()
+        table = pd.DataFrame(entry_context_filtered_by_keys, columns=columns[:2])
     else:
-        raise ValueError('No valid values')
-    # Sort table if mentioned
-    if sort_by:
-        demisto.results(table)
-        table.sort_values(by=sort_by)
-    # Change to Grid form
-    table = table.to_dict('records')
-    # Overwrite data if specified
+        table = []
+
+    return table
+
+
+def build_grid_command(grid_id: str, context_path: str, keys: List[str], columns: List[str], overwrite: bool, sort_by: str) \
+        -> List[Dict[any, any]]:
+    """ Build Grid in one of the 2 options:
+            1. Context_path contain list, e.g. [{'a': 1, 'b': 2}, {'a': 1, 'b': 2}]
+            2. Context_path contain dict (key value pairs), e.g. {'a': 1, 'b': 2}
+
+        Warnings:
+            1. The automation can't validate that the columns name correct and its mandatory.
+            2. The automation knows how to handle only list or dict and doesn't know hoe to handle.
+
+        Args:
+            grid_id: Grid ID to modify.
+            context_path: Entry context path to collect the values from.
+            keys: Keys to be included in the table, If specified "*" will retrieve all availble keys.
+            columns: Name of the columns in the must be equal.
+            overwrite: True if to overwrite existing data else False.
+            sort_by: Name of the column to sort by.
+
+        Returns:
+            list: Table representation for the Grid.
+    """
+    # Get old Data
+    old_table = get_current_table(grid_id=grid_id)
+    # Normalize columns to match connected words.
+    columns = [normalized_string(phrase) for phrase in columns]
+    # Create new Table from the given context path.
+    new_table: pd.DataFrame = build_grid(context_path=context_path,
+                                         keys=keys,
+                                         columns=columns)
+    # Merge tabels if not specified to overwrite.
     if not overwrite:
-        table.append(current_table)
+        new_table = pd.concat([new_table, old_table])
+    # Sory by column name if specified
+    if sort_by and sort_by in new_table.columns:
+        new_table.sort_values(by=sort_by)
 
-    return table
-
-
-def build_grid_command(grid_id: str, context_paths: str, sort_by: Optional[str], row_correlation: bool, overwrite: bool,
-                       columns: Optional[str]) -> List[Dict[str, str]]:
-    """ Build Table as dictionary for populate grid in incident layout.
-
-    The required table structure to populate in the incident is as follow:
-    [{'col1': 'val1', 'col2': 'val2'}, {'col1': 'val3', 'col2': 'val4'}] which represent:
-    | col1 | col2 |
-    | ---- | ---- |
-    | val1 | val2 |
-    | val3 | val4 |
-
-    Args:
-        grid_id(str): Normalized Grid ID (Machine name in `Settings -> Advanced -> Fields -> Field property` or in Incident
-                      Context Data.
-        context_paths(str): Context path to build the Table from, If the Table is row correlated, The path until the last
-                            key should be the same for all paths, In addition the number of the given paths should be the
-                            same as the columns number in the original grid, because the grid column is immutable.
-        sort_by(str): The static name of the column to sort the table rows by.
-        row_correlation(bool): True if the rows correlated (represent an object/item), else False.
-        overwrite(bool): True if to overwrite current grid, False in order to append data.
-        columns(str): Comma separated list of columns names, Should be defined if grid is empty otherwise the automation
-                      detect it automatically.
-
-    Returns:
-        dict: Table dictianary as shown above in explaination.
-    """
-    current_table, columns = get_current_table(grid_id=grid_id,
-                                               sort_by=sort_by,
-                                               columns=columns,
-                                               context_paths=context_paths)
-    data = get_data_from_entry_context(context_paths=context_paths,
-                                       row_correlation=row_correlation)
-    table = build_table(data=data,
-                        columns=columns,
-                        sort_by=sort_by,
-                        overwrite=overwrite,
-                        current_table=current_table)
-
-    return table
+    return new_table.to_dict(orient='records')
 
 
 def main():
@@ -239,11 +176,11 @@ def main():
         grid_id = normalized_string(demisto.getArg('grid_id'))
         # Build updated table
         table = build_grid_command(grid_id=grid_id,
-                                   context_paths=demisto.getArg('context_paths'),
-                                   row_correlation=False if demisto.getArg('row_correlation') == 'false' else True,
-                                   sort_by=demisto.getArg('sort_by'),
+                                   context_path=demisto.getArg('context_path'),
+                                   keys=argToList(demisto.getArg('keys')),
                                    overwrite=False if demisto.getArg('overwrite') == 'false' else True,
-                                   columns=demisto.getArg('columns'))
+                                   columns=argToList(demisto.getArg('columns')),
+                                   sort_by=demisto.getArg('sort_by'))
         # Execute automation 'setIncident` which change the Context data in the incident
         demisto.executeCommand("setIncident",
                                {
