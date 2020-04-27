857ffc532675fe1f552cb1552f642051b6bb4dfd
ikeren <itay@demisto.com>
2020-04-27 11:42:02 +0300

    refactor configure tests to collect tests and content packs

--- Tests/scripts/infrastructure_tests/test_collect_tests_and_content_packs.py
+++ Tests/scripts/infrastructure_tests/test_collect_tests_and_content_packs.py
@@ -1,6 +1,8 @@
 import json
 
-from Tests.scripts.configure_tests import get_test_list_and_content_packs_to_install, get_modified_files, RANDOM_TESTS_NUM
+from Tests.scripts.collect_tests_and_content_packs import (
+    RANDOM_TESTS_NUM, get_modified_files,
+    get_test_list_and_content_packs_to_install)
 
 with open('Tests/scripts/infrastructure_tests/tests_data/mock_id_set.json', 'r') as mock_id_set_f:
     MOCK_ID_SET = json.load(mock_id_set_f)
@@ -267,7 +269,10 @@ def create_get_modified_files_ret(modified_files_list=[], modified_tests_list=[]
 def get_mock_test_list(two_before_ga='4.5.0', get_modified_files_ret=None, mocker=None, git_diff_ret=''):
     branch_name = 'BranchA'
     if get_modified_files_ret is not None:
-        mocker.patch('Tests.scripts.configure_tests.get_modified_files', return_value=get_modified_files_ret)
+        mocker.patch(
+            'Tests.scripts.collect_tests_and_content_packs.get_modified_files',
+            return_value=get_modified_files_ret
+        )
     tests = get_test_list_and_content_packs_to_install(
         git_diff_ret, branch_name, two_before_ga, id_set=MOCK_ID_SET, conf=MOCK_CONF
     )
