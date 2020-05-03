051487ff77096af4f088ed68a58cba2bcf7af73c
Rony Kozakish <37589583+ronykoz@users.noreply.github.com>
2020-05-03 16:51:53 +0300

    Update ConvertToArray.py

--- Packs/Legacy/Scripts/ConvertToArray/ConvertToArray.py
+++ Packs/Legacy/Scripts/ConvertToArray/ConvertToArray.py
@@ -2,7 +2,7 @@ args = demisto.args()
 value = args.get("value")
 if value and isinstance(value, list):
     demisto.results(value)
-    elif value:
-        demisto.results([value])
-    else:
-        demisto.results([])
+elif value:
+    demisto.results([value])
+else:
+    demisto.results([])
