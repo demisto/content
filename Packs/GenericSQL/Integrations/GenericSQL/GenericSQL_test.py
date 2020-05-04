5b19b9d05d8a3d376a6e8a3908870f46baaf02e4
ohaim <ohaim@paloaltonetworks.com>
2020-05-04 10:08:41 +0300

    small changes for build

--- Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
+++ Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
@@ -212,7 +212,7 @@ def test_sql_queries_with_empty_table(mocker):
     - validate the expected_result and the created context
     """
     mocker.patch.object(Client, '_create_engine_and_connect')
-    client = Client('sql_dialect', 'server_url', 'username', 'password', 'port', 'database', "")
+    client = Client('sql_dialect', 'server_url', 'username', 'password', 'port', 'database', "", False)
     mocker.patch.object(client.connection, 'execute', return_value=ConnectionMock())
     result = sql_query_execute(client, ARGS3)
     assert EMPTY_OUTPUT == result[1]  # entry context is found in the 2nd place in the result of the command
