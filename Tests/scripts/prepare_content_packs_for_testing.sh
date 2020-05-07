0b2847de00d0be873e17b81f14e966f8fca1f5af
ikeren <itay@demisto.com>
2020-05-07 13:01:26 +0300

    pass content packs as csv to upload_packs

--- Tests/scripts/prepare_content_packs_for_testing.sh
+++ Tests/scripts/prepare_content_packs_for_testing.sh
@@ -19,7 +19,6 @@ echo "Preparing content packs for testing ..."
 KF=$(mktemp)
 echo "$GCS_MARKET_KEY" > "$KF"
 gcloud auth activate-service-account --key-file="$KF" > auth.out 2>&1
-#rm "$KF"
 echo "Auth loaded successfully."
 
 GCS_MARKET_BUCKET="marketplace-dist"
@@ -31,14 +30,12 @@ gsutil -m cp -r "gs://$GCS_MARKET_BUCKET/$SOURCE_PATH" "gs://$GCS_BUILD_BUCKET/$
 echo "Finished copying successfully."
 
 echo "Updating modified content packs in the bucket ..."
-CONTENT_PACKS_TO_INSTALL="./Tests/content_packs_to_install.txt"
-while IFS= read -r PACK_NAME
-do
-  echo "Updating $PACK_NAME ..."
-  python3 ./Tests/Marketplace/upload_packs.py -a $PACK_ARTIFACTS -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s $KF -n $CIRCLE_BUILD_NUM -k $PACK_SIGNING_KEY -p $PACK_NAME -o -sb $TARGET_PATH -sn false -rt false
-#  gsutil -m cp -r "./Packs/$PACK_NAME" "gs://$GCS_BUILD_BUCKET/$TARGET_PATH"
-  echo "Updated $PACK_NAME successfully."
-done < "$CONTENT_PACKS_TO_INSTALL"
+
+CONTENT_PACKS_TO_INSTALL_FILE="./Tests/content_packs_to_install.txt"
+CONTENT_PACKS_TO_INSTALL=$(paste -sd, CONTENT_PACKS_TO_INSTALL_FILE)
+
+echo "Updating the following content packs: $CONTENT_PACKS_TO_INSTALL ..."
+python3 ./Tests/Marketplace/upload_packs.py -a $PACK_ARTIFACTS -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s $KF -n $CIRCLE_BUILD_NUM -k $PACK_SIGNING_KEY -p CONTENT_PACKS_TO_INSTALL -o -sb $TARGET_PATH -sn false -rt false
 echo "Finished updating content packs successfully."
 
 echo "Finished preparing content packs for testing successfully."
