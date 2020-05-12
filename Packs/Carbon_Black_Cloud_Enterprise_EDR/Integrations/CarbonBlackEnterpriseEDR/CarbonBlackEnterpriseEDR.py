165ad97bd0fd1063df8f0fdb526845373297833b
sberman <sberman@paloaltonetworks.com>
2020-05-12 19:34:50 +0300

    New integration - Carbon Black EDR - Part one

--- Packs/Carbon_Black_Cloud_Enterprise_EDR/Integrations/CarbonBlackEnterpriseEDR/CarbonBlackEnterpriseEDR.py
+++ Packs/Carbon_Black_Cloud_Enterprise_EDR/Integrations/CarbonBlackEnterpriseEDR/CarbonBlackEnterpriseEDR.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Dict, Optional, Any
 
 import dateparser
 import demistomock as demisto
@@ -11,6 +11,9 @@ from CommonServerUserPython import *  # noqa: E402 lgtm [py/polluting-import]
 
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
+
+# GLOBALS
+CB_ORG_KEY = demisto.params().get('organization_key')
 
 
 class Client(BaseClient):
@@ -26,15 +29,11 @@ class Client(BaseClient):
             self._headers.update({'X-Auth-Token': self.token})
 
     def test_module_request(self):
-        url_suffix = '/alerts/_search'
+        url_suffix = f'/appservices/v6/orgs/{CB_ORG_KEY}/alerts/_search'
         body = {
             "criteria": {
                 "group_results": True,
-                "minimum_severity": 3,
-                "create_time": {
-                    "start": "2020-01-27T23:10:20.814Z",
-                    "end": "2020-01-27T23:10:25.814Z"
-                }
+                "minimum_severity": 3
             },
             "sort": [{"field": "first_event_time", "order": "DESC"}],
             "rows": 1,
@@ -43,19 +42,39 @@ class Client(BaseClient):
 
         return self._http_request('POST', url_suffix=url_suffix, json_data=body)
 
-    def search_alerts_request(self, limit: int):
+    def search_alerts_request(self, group_results, minimum_severity, create_time, device_os_version,
+                              policy_id, alert_tag, alert_id, device_username, device_id, device_os,
+                              process_sha256, policy_name, reputation, alert_type, alert_category,
+                              workflow, device_name, process_name, sort_field, sort_order, limit):
 
-        suffix_url = '/alerts/_search'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/alerts/_search'
         body = {
             "criteria": {
-                'group_results': True,
-                'minimum_severity': 3,
-                'create_time': {
-                    'start': '2020-01-27T23:10:20.814Z',
-                    'end': '2020-01-27T23:10:25.814Z'
-                }
+                'group_results': group_results,
+                'minimum_severity': minimum_severity,
+                'create_time': create_time,
+                'device_os_version': device_os_version,
+                'policy_id': policy_id,
+                'tag': alert_tag,
+                'id': alert_id,
+                'device_username': device_username,
+                'device_id': device_id,
+                'device_os': device_os,
+                'process_sha256': process_sha256,
+                'policy_name': policy_name,
+                'reputation': reputation,
+                'type': alert_type,
+                'category': alert_category,
+                'workflow': workflow,
+                'device_name': device_name,
+                'process_name': process_name
             },
-            'sort': [{'field': 'first_event_time', 'order': 'DESC'}],
+            'sort': [
+                {
+                    'field': sort_field,
+                    'order': sort_order
+                }
+            ],
             'rows': limit,
             'start': 0
         }
@@ -64,7 +83,7 @@ class Client(BaseClient):
 
     def alert_workflow_update_request(self, alert_id, state, comment, remediation_state):
 
-        suffix_url = f'/alerts/{alert_id}/workflow'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/alerts/{alert_id}/workflow'
         body = assign_params(
             state=state,
             comment=comment,
@@ -73,11 +92,11 @@ class Client(BaseClient):
 
         return self._http_request('POST', suffix_url, json_data=body)
 
-    def devices_list_request(self, device_id: Union[list, str], status: str, device_os: str, last_contact_time: str,
-                             ad_group_id: int, policy_id: int, target_priority: str, limit: int, sort_field: str,
-                             sort_order: str):
+    def devices_list_request(self, device_id: Union[list, str], status: str, device_os: str,
+                             last_contact_time: Dict[str, Optional[Any]], ad_group_id: int, policy_id: int,
+                             target_priority: str, limit: int, sort_field: str, sort_order: str):
 
-        suffix_url = '/devices/_search'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/devices/_search'
 
         body = {
             'criteria': {
@@ -103,7 +122,7 @@ class Client(BaseClient):
 
     def device_quarantine_request(self, device_id: Union[list, str]):
 
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'QUARANTINE',
@@ -116,7 +135,7 @@ class Client(BaseClient):
         return self._http_request('POST', suffix_url, json_data=body, resp_type='content')
 
     def device_unquarantine_request(self, device_id: Union[list, str]):
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'QUARANTINE',
@@ -129,7 +148,7 @@ class Client(BaseClient):
         return self._http_request('POST', suffix_url, json_data=body, resp_type='content')
 
     def device_bypass_request(self, device_id: Union[list, str]):
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'BYPASS',
@@ -142,7 +161,7 @@ class Client(BaseClient):
         return self._http_request('POST', suffix_url, json_data=body, resp_type='content')
 
     def device_unbypass_request(self, device_id: Union[list, str]):
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'BYPASS',
@@ -155,7 +174,7 @@ class Client(BaseClient):
         return self._http_request('POST', suffix_url, json_data=body, resp_type='content')
 
     def device_background_scan_request(self, device_id: Union[list, str]):
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'BACKGROUND_SCAN',
@@ -168,7 +187,7 @@ class Client(BaseClient):
         return self._http_request('POST', suffix_url, json_data=body, resp_type='content')
 
     def device_background_scan_request_stop(self, device_id: Union[list, str]):
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'BACKGROUND_SCAN',
@@ -181,7 +200,7 @@ class Client(BaseClient):
         return self._http_request('POST', suffix_url, json_data=body, resp_type='content')
 
     def device_policy_update(self, device_id: Union[list, str], policy_id: str):
-        suffix_url = '/device_actions'
+        suffix_url = f'/appservices/v6/orgs/{CB_ORG_KEY}/device_actions'
 
         body = {
             'action_type': 'UPDATE_POLIC',
@@ -208,6 +227,64 @@ def test_module(client):
     return 'ok'
 
 
+def alert_list_command(client: Client, args: dict):
+
+    group_results = args.get('group_results')
+    minimum_severity = args.get('minimum_severity')
+    create_time = {
+        'start': args.get('start_time'),
+        'end': args.get('end_time')
+    }
+    device_os_version = argToList(args.get('device_os_version'))
+    policy_id = argToList(args.get('policy_id'))
+    alert_tag = argToList(args.get('alert_tag'))
+    alert_id = argToList(args.get('alert_id'))
+    device_username = argToList(args.get('device_username'))
+    device_id = argToList(args.get('device_id'))
+    device_os = argToList(args.get('device_os'))
+    process_sha256 = argToList(args.get('process_sha256'))
+    policy_name = argToList(args.get('policy_name'))
+    reputation = argToList(args.get('reputation'))
+    alert_type = argToList(args.get('alert_type'))
+    alert_category = argToList(args.get('alert_category'))
+    workflow = argToList(args.get('workflow'))
+    device_name = argToList(args.get('device_name'))
+    process_name = argToList(args.get('process_name'))
+    sort_field = args.get('sort_field')
+    sort_order = args.get('sort_order')
+    limit = args.get('limit')
+    contents = []
+
+    result = client.search_alerts_request(group_results, minimum_severity, create_time,
+                                          device_os_version, policy_id, alert_tag, alert_id, device_username,
+                                          device_id, device_os, process_sha256, policy_name,
+                                          reputation, alert_type, alert_category, workflow, device_name,
+                                          process_name, sort_field, sort_order, limit)
+
+    alerts = result.get('results')
+    for alert in alerts:
+        contents.append({
+            'AlertID': alert.get('id'),
+            'CreateTime': alert.get('create_time'),
+            'DeviceName': alert.get('device_name'),
+            'DeviceOS': alert.get('device_os'),
+            'PolicyName': alert.get('policy_name'),
+            'ProcessName': alert.get('process_name'),
+            'Type': alert.get('type'),
+            'WorkflowState': alert.get('workflow').get('state')
+        })
+
+    readable_output = tableToMarkdown('Alerts list results', contents)
+    results = CommandResults(
+        outputs_prefix='CarbonBlackEEDR.Alert',
+        outputs_key_field='id',
+        outputs=alerts,
+        readable_output=readable_output,
+        raw_response=result
+    )
+    return results
+
+
 def alert_workflow_update_command(client: Client, args: dict):
 
     alert_id = args.get('alert_id')
@@ -231,7 +308,6 @@ def alert_workflow_update_command(client: Client, args: dict):
         outputs_prefix='CarbonBlackEEDR.Alert',
         outputs_key_field='AlertID',
         outputs=outputs,
-
         readable_output=readable_output,
         raw_response=result
     )
@@ -240,18 +316,49 @@ def alert_workflow_update_command(client: Client, args: dict):
 
 def list_devices_command(client: Client, args: dict):
     device_id = argToList(args.get('device_id'))
-    status = args.get('status')
-    device_os = args.get('device_os')
-    last_contact_time = args.get('last_contact_time')
-    ad_group_id = args.get('ad_group_id')
-    policy_id = args.get('policy_id')
-    target_priority = args.get('target_priority')
+    status = argToList(args.get('status'))
+    device_os = argToList(args.get('device_os'))
+    last_contact_time = {
+        'start': args.get('start_time'),
+        'end': args.get('end_time')
+    }
+    args.get('last_contact_time')
+    ad_group_id = argToList(args.get('ad_group_id'))
+    policy_id = argToList(args.get('policy_id'))
+    target_priority = argToList(args.get('target_priority'))
     limit = args.get('limit')
-    sort_field = args.get('sort_field')
+    sort_field = args.get('sort_field', '')
     sort_order = args.get('sort_order')
+    contents = []
 
     result = client.devices_list_request(device_id, status, device_os, last_contact_time, ad_group_id, policy_id,
                                          target_priority, limit, sort_field, sort_order)
+    devices = result.get('results')
+    for device in devices:
+        contents.append({
+            'ID': device.get('id'),
+            'Name': device.get('name'),
+            'OS': device.get('os'),
+            'LastInternalIpAddress': device.get('last_internal_ip_address'),
+            'LastExternalIpAddress': device.get('last_external_ip_address'),
+            'LastContactTime': device.get('last_contact_time'),
+            'LastLocation': device.get('last_location'),
+            'PolicyName': device.get('policy_name'),
+            'Quarantined': device.get('quarantined'),
+            'status': device.get('status'),
+            'TargetPriority': device.get('target_priority')
+        })
+
+
+    readable_output = tableToMarkdown('Devices list results', contents)
+    results = CommandResults(
+        outputs_prefix='CarbonBlackEEDR.Device',
+        outputs_key_field='id',
+        outputs=devices,
+        readable_output=readable_output,
+        raw_response=result
+    )
+    return results
 
 
 def device_quarantine_command(client: Client, args: dict):
@@ -259,14 +366,52 @@ def device_quarantine_command(client: Client, args: dict):
     device_id = argToList(args.get('device_id'))
     client.device_quarantine_request(device_id)
 
-    return 'The device has been quarantine', {}, {}
+    return 'The device has been quarantine'
 
 
 def device_unquarantine_command(client: Client, args: dict):
     device_id = argToList(args.get('device_id'))
-    result = client.device_quarantine_request(device_id)
+    client.device_unquarantine_request(device_id)
 
-    return 'The device has been unquarantine', {}, result
+    return 'The device has been unquarantine'
+
+
+def device_bypass_command(client: Client, args: dict):
+
+    device_id = argToList(args.get('device_id'))
+    client.device_bypass_request(device_id)
+
+    return 'The bypassed has been enabled'
+
+
+def device_unbypass_command(client: Client, args: dict):
+    device_id = argToList(args.get('device_id'))
+    client.device_unbypass_request(device_id)
+
+    return 'The bypass has been disabled.'
+
+
+def device_background_scan_command(client: Client, args: dict):
+    device_id = argToList(args.get('device_id'))
+    client.device_background_scan_request(device_id)
+
+    return 'The background scan has been enabled.'
+
+
+def device_background_scan_stop_command(client: Client, args: dict):
+    device_id = argToList(args.get('device_id'))
+    client.device_background_scan_request_stop(device_id)
+
+    return 'The background scan has been disabled.'
+
+
+def device_policy_update_command(client: Client, args: dict):
+    device_id = argToList(args.get('device_id'))
+    policy_id = args.get('policy_id')
+
+    client.device_policy_update(device_id, policy_id)
+
+    return f'The policy has been assigned to device {device_id}'
 
 # def fetch_incidents(client, last_run, first_fetch_time):
 #     """
@@ -318,9 +463,8 @@ def main():
     cb_custom_key = demisto.params().get('custom_key')
     cb_custom_id = demisto.params().get('custom_id')
     token = f'{cb_custom_key}/{cb_custom_id}'
-    cb_org_key = demisto.params().get('organization_key')
     # get the service API url
-    base_url = urljoin(demisto.params().get('url'), f'/appservices/v6/orgs/{cb_org_key}')
+    base_url = demisto.params().get('url')
 
     verify_certificate = not demisto.params().get('insecure', False)
 
@@ -351,15 +495,35 @@ def main():
         #
         #     demisto.setLastRun(next_run)
         #     demisto.incidents(incidents)
+        elif demisto.command() == 'cb-eedr-list-alerts':
+            return_results(alert_list_command(client, demisto.args()))
 
         elif demisto.command() == 'cb-eedr-alert-workflow-update':
             return_results(alert_workflow_update_command(client, demisto.args()))
-        # elif demisto.command() == 'cb-eedr-devices-list':
-        #     return_outputs(*list_devices_command(client, demisto.args()))
+
+        elif demisto.command() == 'cb-eedr-devices-list':
+            return_results(list_devices_command(client, demisto.args()))
+
         elif demisto.command() == 'cb-eedr-device-quarantine':
-            return_outputs(*device_quarantine_command(client, demisto.args()))
+            return_results(device_quarantine_command(client, demisto.args()))
+
         elif demisto.command() == 'cb-eedr-device-unquarantine':
-            return_outputs(*device_unquarantine_command(client, demisto.args()))
+            return_results(device_unquarantine_command(client, demisto.args()))
+
+        elif demisto.command() == 'cb-eedr-device-background-scan-stop':
+            return_results(device_background_scan_stop_command(client, demisto.args()))
+
+        elif demisto.command() == 'cb-eedr-device-background-scan':
+            return_results(device_background_scan_command(client, demisto.args()))
+
+        elif demisto.command() == 'cb-eedr-device-bypass':
+            return_results(device_bypass_command(client, demisto.args()))
+
+        elif demisto.command() == 'cb-eedr-device-unbypass':
+            return_results(device_unbypass_command(client, demisto.args()))
+
+        elif demisto.command() == 'cb-eedr-device-policy-update':
+            return_results(device_policy_update_command(client, demisto.args()))
 
     # Log exceptions
     except Exception as e:
