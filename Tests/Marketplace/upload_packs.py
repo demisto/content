2721f608ebdbba69fae5ff1510af995918c1d174
ikeren <itay@demisto.com>
2020-05-12 12:14:05 +0300

    remove sign pack flag

--- Tests/Marketplace/upload_packs.py
+++ Tests/Marketplace/upload_packs.py
@@ -456,8 +456,6 @@ def option_handler():
     parser.add_argument('-pb', '--private_bucket_name', help="Private storage bucket name", required=False)
     parser.add_argument('-sb', '--storage_bash_path', help="Storage base path of the directory to upload to.",
                         required=False)
-    # parser.add_argument('-sn', '--should_sign_pack', type=str2bool,
-    #                     help='Should sign content packs or not.', default=True)
     parser.add_argument('-rt', '--remove_test_playbooks', type=str2bool,
                         help='Should remove test playbooks from content packs or not.', default=True)
     # disable-secrets-detection-end
@@ -477,7 +475,6 @@ def main():
     signature_key = option.key_string
     id_set_path = option.id_set_path
     storage_bash_path = option.storage_bash_path
-    # should_sign_pack = option.should_sign_pack
     remove_test_playbooks = option.remove_test_playbooks
 
     # google cloud storage client initialized
