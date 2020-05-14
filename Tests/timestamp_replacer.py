97f0bfebd6ff953bd7224a192aa76d763eef802f
avidan-H <ahessing@paloaltonetworks.com>
2020-05-14 10:49:19 +0300

    add some debugging stuff to timestamp_replacer.py

--- Tests/timestamp_replacer.py
+++ Tests/timestamp_replacer.py
@@ -1,10 +1,35 @@
 import json
+import functools
+import urllib
+from collections import OrderedDict
 from os import path
 from copy import deepcopy
 from typing import List, Union
 from mitmproxy import ctx, flow
+from mitmproxy.http import HTTPRequest
+from mitmproxy.script import concurrent
 from time import ctime
 from dateutil.parser import parse
+
+
+def record_concurrently(replaying=''):
+    '''
+    A decorator to return a decorator that just executes the function it decorates normally if 'replaying' has a value,
+    (AKA mitmdump is executing in server-replay mode), otherwise pass the 'concurrant' decorater so that when mitmdump
+    is executing in recording mode, that requests will be processed concurrently and not be blocking which can cause
+    proxy errors during recording if multiple requests are made in short timespan
+    '''
+    print(f'replaying={replaying}')
+    if replaying:
+        def nonconcurrent_decorator(func):
+            @functools.wraps(func)
+            def passthrough_wrapper(*args, **kwargs):
+                value = func(*args, **kwargs)
+                return value
+            return passthrough_wrapper
+        return nonconcurrent_decorator
+    else:
+        return concurrent
 
 
 class TimestampReplacer:
@@ -45,6 +70,14 @@ class TimestampReplacer:
             in the same directory.
             '''
         )
+        loader.add_option(
+            name='debug',
+            typespec=bool,
+            default=False,
+            help='''
+            Set to True to print out additional information for each request that comes in.
+            '''
+        )
 
     def running(self):
         # # need to do this because arguments for these options are interpreted as 1 list item
@@ -52,17 +85,31 @@ class TimestampReplacer:
         # ctx.options.server_replay_ignore_params = query_keys[0].split() if len(query_keys) == 1 else query_keys
         # form_keys = ctx.options.server_replay_ignore_payload_params
         # ctx.options.server_replay_ignore_payload_params = form_keys[0].split() if len(form_keys) == 1 else form_keys
+        if ctx.options.debug:
+            print(f'ctx.options={ctx.options}')
 
         self.bad_keys_filepath = ctx.options.keys_filepath
         if ctx.options.detect_timestamps:
-            ctx.log.info('Detecting Timestamp Fields')
+            print('Detecting Timestamp Fields')
             self.detect_timestamps = True
         else:
             self.load_problematic_keys()
 
+    def _debug_request(self, req: HTTPRequest) -> None:
+        '''Print details of the request'''
+        print(f'{req.method} {req.pretty_url}')
+        _, _, path, _, query, _ = urllib.parse.urlparse(req.url)
+        queriesArray = urllib.parse.parse_qsl(query, keep_blank_values=True)
+        print(f'queriesArray={queriesArray}')
+        if req.multipart_form:
+            print(f'multipart_form data = {req.multipart_form.items()}')
+        if req.urlencoded_form:
+            print(f'urlencoded_form data = {req.urlencoded_form.items()}')
     def request(self, flow: flow.Flow) -> None:
         self.count += 1
         req = flow.request
+        if ctx.options.debug:
+            self._debug_request(req)
         if ctx.options.detect_timestamps:
             self.handle_url_query(flow)
         if req.method == 'POST':
@@ -82,13 +129,13 @@ class TimestampReplacer:
                 elif req.urlencoded_form:
                     self.handle_urlencoded_form(flow)
                 elif json_data:
-                    ctx.log.info('req num: {}\n{}'.format(self.count, content))
+                    print('req num: {}\n{}'.format(self.count, content))
                     for problem_key in self.determine_problematic_keys(content):
                         self.json_keys.add(problem_key)
             elif json_data:
                 self.modify_json_body(flow, content)
         if ctx.options.detect_timestamps:
-            ctx.log.info('updating problem_keys file at "{}"'.format(self.bad_keys_filepath))
+            print('updating problem_keys file at "{}"'.format(self.bad_keys_filepath))
             self.update_problem_keys_file()
 
     def modify_json_body(self, flow: flow.Flow, json_body: dict) -> None:
@@ -102,18 +149,18 @@ class TimestampReplacer:
         body = json_body
         modified = False
         keys_to_replace = ctx.options.keys_to_replace.split() or []
-        ctx.log.info('{}'.format(keys_to_replace))
+        print('{}'.format(keys_to_replace))
         for key_path in keys_to_replace:
             body = json_body
             keys = key_path.split('.')
-            ctx.log.info('keypath parts: {}'.format(keys))
+            print('keypath parts: {}'.format(keys))
             lastkey = keys[-1]
-            ctx.log.info('lastkey: {}'.format(lastkey))
+            print('lastkey: {}'.format(lastkey))
             skip_key = False
             for k in keys[:-1]:
                 if k in body:
                     body = body[k]
-                    # ctx.log.info('updated body: {}'.format(body))
+                    # print('updated body: {}'.format(body))
                 elif isinstance(body, list) and k.isdigit():
                     if int(k) > len(body) - 1:
                         skip_key = True
@@ -124,23 +171,23 @@ class TimestampReplacer:
                     break
             if not skip_key:
                 if lastkey in body:
-                    ctx.log.info('modifying request to "{}"'.format(flow.request.pretty_url))
+                    print('modifying request to "{}"'.format(flow.request.pretty_url))
                     # body[lastkey] = self.count
                     body[lastkey] = self.constant
                     modified = True
                 elif isinstance(body, list) and lastkey.isdigit() and int(lastkey) <= len(body) - 1:
-                    ctx.log.info('modifying request to "{}"'.format(flow.request.pretty_url))
+                    print('modifying request to "{}"'.format(flow.request.pretty_url))
                     # body[int(lastkey)] = self.count
                     body[int(lastkey)] = self.constant
                     modified = True
         if modified:
-            ctx.log.info('original request body:\n{}'.format(json.dumps(original_content, indent=4)))
-            ctx.log.info('modified request body:\n{}'.format(json.dumps(json_body, indent=4)))
+            print('original request body:\n{}'.format(json.dumps(original_content, indent=4)))
+            print('modified request body:\n{}'.format(json.dumps(json_body, indent=4)))
             flow.request.raw_content = json.dumps(json_body).encode()
 
     def handle_url_query(self, flow: flow.Flow) -> None:
         query_data = flow.request._get_query()
-        ctx.log.info('query_data: {}'.format(query_data))
+        print('query_data: {}'.format(query_data))
         for key, val in query_data:
             # don't bother trying to interpret an argument less than 4 characters as some type of timestamp
             if len(val) > 4:
@@ -202,7 +249,7 @@ class TimestampReplacer:
                         possible_timestamp = isinstance(val, (int, float)) and len(str(val)) >= 8
                         try:
                             if is_string or possible_timestamp:
-                                # ctx.log.info('req num: {} keypath: {}'.format(self.count, sub_key_path))
+                                # print('req num: {} keypath: {}'.format(self.count, sub_key_path))
                                 for_eval = val
                                 if possible_timestamp:
                                     if isinstance(for_eval, float):
@@ -217,7 +264,7 @@ class TimestampReplacer:
                                 # if it continues to the next line that means it successfully parsed the object
                                 # and it's some sort of time-related object
                                 bad_key_paths.append(sub_key_path)
-                        except ValueError:
+                        except (ValueError, OverflowError):
                             pass
             elif isinstance(obj, list):
                 for i, val in enumerate(obj):
@@ -229,7 +276,7 @@ class TimestampReplacer:
                         possible_timestamp = isinstance(val, (int, float)) and len(str(val)) >= 8
                         try:
                             if is_string or possible_timestamp:
-                                # ctx.log.info('req num: {} keypath: {}'.format(self.count, sub_key_path))
+                                # print('req num: {} keypath: {}'.format(self.count, sub_key_path))
                                 for_eval = val
                                 if possible_timestamp:
                                     if isinstance(for_eval, float):
@@ -244,7 +291,7 @@ class TimestampReplacer:
                                 # if it continues to the next line that means it successfully parsed the object
                                 # and it's some sort of time-related object
                                 bad_key_paths.append(sub_key_path)
-                        except ValueError:
+                        except (ValueError, OverflowError):
                             pass
             return bad_key_paths
         bad_keys = travel_dict(content)
@@ -267,9 +314,9 @@ class TimestampReplacer:
         '''Load problematic keys dictionary from the keys_filepath argument filepath in content-test-data repo
         if it exists. Otherwise, return the dictionary with empty values.
         '''
-        ctx.log.info('executing "read_in_problematic_keys" method')
+        print('executing "read_in_problematic_keys" method')
         repo_bad_keys_filepath = self.bad_keys_filepath.replace('/tmp/Mocks', 'content-test-data')
-        ctx.log.info('reading in problematic keys data from "{}"'.format(repo_bad_keys_filepath))
+        print('reading in problematic keys data from "{}"'.format(repo_bad_keys_filepath))
         if path.exists(repo_bad_keys_filepath):
             with open(repo_bad_keys_filepath, 'r') as fp:
                 problem_keys = json.load(fp)
@@ -294,17 +341,17 @@ class TimestampReplacer:
         '''Load problematic keys from the keys_filepath argument filepath if it exists. Only necessary when running
         mitmdump in playback mode. Resets command line options with the key value pairs from the loaded dictionary.
         '''
-        ctx.log.info('executing "load_problematic_keys" method')
+        print('executing "load_problematic_keys" method')
         if path.exists(self.bad_keys_filepath):
-            ctx.log.info('"{}" path exists - loading bad keys'.format(self.bad_keys_filepath))
+            print('"{}" path exists - loading bad keys'.format(self.bad_keys_filepath))
             log_msg = 'options pre update: \nkeys_to_replace: {}'.format(ctx.options.keys_to_replace)
             log_msg += '\nserver_replay_ignore_params: {}'.format(ctx.options.server_replay_ignore_params)
             log_msg += '\nserver_replay_ignore_payload_params: {}'.format(
                 ctx.options.server_replay_ignore_payload_params
             )
-            ctx.log.info(log_msg)
+            print(log_msg)
 
-            problem_keys = json.load(self.bad_keys_filepath)
+            problem_keys = json.load(open(self.bad_keys_filepath, 'r'))
             # ctx.options.set(problem_keys.items())
 
             # need to do this because arguments for these options are interpreted as 1 list item
@@ -315,18 +362,20 @@ class TimestampReplacer:
                 form_keys.split() if isinstance(form_keys, str) else form_keys
             )
             keys_to_replace = problem_keys.get('keys_to_replace')
-            ctx.options.keys_to_replace = (
-                keys_to_replace.split() if isinstance(keys_to_replace, str) else keys_to_replace
-            )
+            # ctx.options.keys_to_replace = (
+            #     keys_to_replace.split() if isinstance(keys_to_replace, str) else keys_to_replace
+            # )
+            ctx.options.keys_to_replace = keys_to_replace
 
             log_msg = 'options post update: \nkeys_to_replace: {}'.format(ctx.options.keys_to_replace)
             log_msg += '\nserver_replay_ignore_params: {}'.format(ctx.options.server_replay_ignore_params)
             log_msg += '\nserver_replay_ignore_payload_params: {}'.format(
                 ctx.options.server_replay_ignore_payload_params
             )
-            ctx.log.info(log_msg)
+            print(log_msg)
         else:
-            ctx.log.info('"{}" path doesn\'t exist - no bad keys to set'.format(self.bad_keys_filepath))
+            print('"{}" path doesn\'t exist - no bad keys to set'.format(self.bad_keys_filepath))
+            print('not setting bad keys from file')
 
     # def done(self):
     #     print('timestamp_replacer.py "done()" called')
