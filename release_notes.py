af990cd8c81aa4b20abcb446cafae23536e31507
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-05-17 20:31:17 +0300

    Move to older functions for release_notes.py

--- release_notes.py
+++ release_notes.py
@@ -13,7 +13,7 @@ from demisto_sdk.commands.common.constants import INTEGRATIONS_DIR, SCRIPTS_DIR,
     DASHBOARDS_DIR, WIDGETS_DIR, INCIDENT_FIELDS_DIR, LAYOUTS_DIR, CLASSIFIERS_DIR, INDICATOR_TYPES_DIR
 from demisto_sdk.commands.common.tools import print_error, print_warning, get_last_release_version, \
     filter_packagify_changes, is_file_path_in_pack, \
-    run_command, server_version_compare, get_release_notes_file_path, get_latest_release_notes_text, get_remote_file
+    run_command, server_version_compare, old_get_release_notes_file_path, old_get_latest_release_notes_text, get_remote_file
 from demisto_sdk.commands.validate.file_validator import FilesValidator
 
 CONTENT_LIB_PATH = "./"
