a3df98d053e1deb4879ab8c21f90fde3dcc0e228
ikeren <itay@demisto.com>
2020-05-19 13:08:56 +0300

    pass id set artifact to upload_packs

--- Tests/scripts/prepare_content_packs_for_testing.sh
+++ Tests/scripts/prepare_content_packs_for_testing.sh
@@ -7,6 +7,7 @@ CIRCLE_BRANCH=${CIRCLE_BRANCH:-unknown}
 CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-00000}
 CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS}
 PACK_ARTIFACTS=$CIRCLE_ARTIFACTS/content_packs.zip
+ID_SET=$CIRCLE_ARTIFACTS/id_set.json
 EXTRACT_FOLDER=$(mktemp -d)
 
 if [[ -z "$GCS_MARKET_KEY" ]]; then
@@ -38,7 +39,7 @@ if [[ -z "$CONTENT_PACKS_TO_INSTALL" ]]; then
   echo "Did not get content packs to update in the bucket."
 else
   echo "Updating the following content packs: $CONTENT_PACKS_TO_INSTALL ..."
-  python3 ./Tests/Marketplace/upload_packs.py -a $PACK_ARTIFACTS -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s $KF -n $CIRCLE_BUILD_NUM -p $CONTENT_PACKS_TO_INSTALL -o -sb $TARGET_PATH -rt false
+  python3 ./Tests/Marketplace/upload_packs.py -a $PACK_ARTIFACTS -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s $KF -n $CIRCLE_BUILD_NUM -p $CONTENT_PACKS_TO_INSTALL -o -sb $TARGET_PATH -rt false --id_set_path $ID_SET
   echo "Finished updating content packs successfully."
 fi
 
