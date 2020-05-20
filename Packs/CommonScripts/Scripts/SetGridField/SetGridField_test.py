693733275ef88d22a2396b5067eebd8d1ef15e37
teizenman <teizenman@paloaltonetworks.com>
2020-05-20 14:41:11 +0300

    updated tests

--- Packs/CommonScripts/Scripts/SetGridField/SetGridField_test.py
+++ Packs/CommonScripts/Scripts/SetGridField/SetGridField_test.py
@@ -25,11 +25,11 @@ def test_filter_the_dict(before_dict: dict, keys: dict, max_keys: int, after_dic
 
 @pytest.mark.parametrize(argnames="entry_context, keys, raise_exception, skip_nested",
                          argvalues=[
-                             ([{'a': 'val', 'b': 'val'}], ['a', 'b'], False, True),
-                             ([{'a': [], 'b': 'val'}], ['a', 'b'], True, True),
-                             ([{'a': [], 'b': 'val'}], ['b'], False, True),
-                             (['a', 'b', 1, False], ['b'], False, True),
-                             (['a', 'b', 1, False, []], ['*'], True, True),
+                             ([{'a': 'val', 'b': 'val'}], ['a', 'b'], False, False),
+                             ([{'a': [], 'b': 'val'}], ['a', 'b'], True, False),
+                             ([{'a': [], 'b': 'val'}], ['b'], False, False),
+                             (['a', 'b', 1, False], ['b'], False, False),
+                             (['a', 'b', 1, False, []], ['*'], True, False),
                          ])
 def test_validate_entry_context(entry_context: dict, keys: list, raise_exception: bool, skip_nested: bool):
     from SetGridField import validate_entry_context
@@ -37,25 +37,25 @@ def test_validate_entry_context(entry_context: dict, keys: list, raise_exception
         with pytest.raises(ValueError):
             validate_entry_context(entry_context=entry_context,
                                    keys=keys,
-                                   skip_nested_elements=skip_nested)
+                                   unpack_nested_elements=skip_nested)
     else:
         validate_entry_context(entry_context=entry_context,
                                keys=keys,
-                               skip_nested_elements=skip_nested)
+                               unpack_nested_elements=skip_nested)
 
 
-@pytest.mark.parametrize(argnames="keys, columns, dt_response_json, expected_json, skip_nested",
+@pytest.mark.parametrize(argnames="keys, columns, dt_response_json, expected_json, unpack_nested",
                          argvalues=[
                              (["name", "value"], ["col1", "col2"], "context_entry_list.json", "expected_list_grid.json",
-                              True),
-                             (["*"], ["col1", "col2"], "context_entry_dict.json", "expected_dict_grid.json", True),
+                              False),
+                             (["*"], ["col1", "col2"], "context_entry_dict.json", "expected_dict_grid.json", False),
                              (["*"], ["col1"], "context_entry_list_of_values.json", "expected_list_of_values_grid.json",
-                              True),
+                              False),
                              (["*"], ["col1", "col2"], "context_entry_dict_with_elements.json",
-                              "expected_dict_with_elements_grid.json", False),
+                              "expected_dict_with_elements_grid.json", True),
                          ])
 def test_build_grid(datadir, mocker, keys: list, columns: list, dt_response_json: str, expected_json: str,
-                    skip_nested: bool):
+                    unpack_nested: bool):
     import SetGridField
     import json
     import pandas as pd
@@ -63,7 +63,6 @@ def test_build_grid(datadir, mocker, keys: list, columns: list, dt_response_json
     mocker.patch.object(SetGridField, 'demisto')
     SetGridField.demisto.dt.return_value = json.load(open(datadir[dt_response_json]))
     expected_grid = json.load(open(datadir[expected_json]))
-    assert pd.DataFrame(expected_grid).to_dict() == SetGridField.build_grid(context_path=mocker.MagicMock(),
-                                                                            keys=keys,
-                                                                            columns=columns,
-                                                                            skip_nested_elements=skip_nested).to_dict()
+    assert pd.DataFrame(expected_grid).to_dict() == \
+        SetGridField.build_grid(context_path=mocker.MagicMock(), keys=keys, columns=columns,
+                                unpack_nested_elements=unpack_nested).to_dict()
