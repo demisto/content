b7030470456ead74939270656d6689c52696b7b8
Jochman <bhochman@paloaltonetworks.com>
2020-05-20 13:55:01 +0300

    fix problem with datetime object

--- Packs/MongoDB/Integrations/MongoDB/CHANGELOG.md
+++ Packs/MongoDB/Integrations/MongoDB/CHANGELOG.md
@@ -1,5 +1,5 @@
 ## [Unreleased]
-
+Fixed an issue when pulling a object that contains date.
 
 ## [20.4.0] - 2020-04-14
 -
--- Packs/MongoDB/Integrations/MongoDB/MongoDB_test.py
+++ Packs/MongoDB/Integrations/MongoDB/MongoDB_test.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 import pytest
-from MongoDB import convert_id_to_object_id, convert_object_id_to_str
 from bson.objectid import ObjectId
+
+from MongoDB import convert_id_to_object_id, convert_object_id_to_str, convert_str_to_datetime, Client, search_query
 
 id_to_obj_inputs = [
     (
@@ -33,3 +36,74 @@ object_to_id = [
 @pytest.mark.parametrize("func_input, expected", object_to_id)
 def test_convert_object_id_to_str(func_input, expected):
     assert expected == convert_object_id_to_str(func_input)
+
+
+def test_normalize_id():
+    res = Client.normalize_id({'_id': ObjectId('5e4412f230c5b8f63a7356ba')})
+    assert '5e4412f230c5b8f63a7356ba' == res['_id']
+
+
+class TestConvertStrToDatetime:
+    dict_inputs = [
+        {"testing": 123, "time": "ISODate('2020-06-12T08:23:07.000Z')"},
+        pytest.param(
+            {"testing": 123, "time": "ISODate('2018-06-12T08:23:07.000')"},
+            marks=pytest.mark.xfail),
+    ]
+
+    @pytest.mark.parametrize("func_input", dict_inputs)
+    def test_convert_str_to_datetime(self, func_input):
+        res = convert_str_to_datetime(func_input)
+        assert isinstance(res['time'], datetime)
+
+    def test_convert_str_to_datetime_no_datetime_obj(self):
+        inputs = {1: 2}
+        res = convert_str_to_datetime(inputs)
+        assert isinstance(res[1], int)
+
+
+class TestDatetimeToStr:
+    datetime_obj = datetime.strptime('2020-05-19T09:05:28.000Z', '%Y-%m-%dT%H:%M:%S.000Z')
+    datetime_str = '2020-05-19T09:05:28.000Z'
+
+    def test_datetime_to_str_dict(self):
+        raw = Client.datetime_to_str({'time': self.datetime_obj})
+        assert self.datetime_str == raw['time']
+
+    def test_datetime_to_str_list(self):
+        raw = Client.datetime_to_str([self.datetime_obj])
+        assert [self.datetime_str] == raw
+
+    def test_datetime_to_str_str(self):
+        raw = Client.datetime_to_str(self.datetime_obj)
+        assert self.datetime_str == raw
+
+    def test_datetime_to_str_dict_no_datetime(self):
+        raw = Client.datetime_to_str({'time': 5})
+        assert 5 == raw['time']
+
+    def test_datetime_to_str_list_no_datetime(self):
+        raw = Client.datetime_to_str([5])
+        assert [5] == raw
+
+    def test_datetime_to_str_str_no_datetime(self):
+        raw = Client.datetime_to_str('str')
+        assert 'str' == raw
+
+
+class MockedQuery:
+    class Limit:
+        @staticmethod
+        def limit(number):
+            return [{'time': TestDatetimeToStr.datetime_obj, '_id': ObjectId('5e4412f230c5b8f63a7356ba')}]
+
+    @classmethod
+    def find(cls, query):
+        return cls.Limit
+
+
+def test_query(mocker):
+    client = Client(['aaaaa'], 'a', 'b', 'd')
+    mocker.patch.object(Client, 'get_collection', return_value=MockedQuery)
+    readable_outputs, outputs, raw_response = search_query(client, 'a', '{}', '50')
+    assert all(not isinstance(obj, datetime) for obj in raw_response)
