89f523268944f10b5f63a2abaf16fc936ac646f3
ikeren <itay@demisto.com>
2020-05-21 15:49:29 +0300

    handle no results found for an ip

--- Packs/Shodan/Integrations/Shodan_v2/Shodan_v2.py
+++ Packs/Shodan/Integrations/Shodan_v2/Shodan_v2.py
@@ -45,7 +45,8 @@ def http_request(method, uri, params=None, data=None, headers=None):
                            data=data,
                            headers=headers,
                            verify=USE_SSL)
-
+    if res.status_code == 404:
+        return {}
     if res.status_code != 200:
         error_msg = f'Error in API call {url} [{res.status_code}] - {res.reason}'
         if 'application/json' in res.headers['content-type'] and 'error' in res.json():
@@ -183,59 +184,62 @@ def ip_command():
 
     res = http_request('GET', f'/shodan/host/{ip}')
 
-    hostnames = res.get('hostnames')
-    hostname = hostnames[0] if hostnames else ''  # It's a list, only if it exists and not empty we take the first value
+    if not res:
+        demisto.results('No information available for that IP.')
+    else:
+        hostnames = res.get('hostnames')
+        hostname = hostnames[0] if hostnames else ''  # It's a list, only if it exists and not empty we take the first value
 
-    location = f'{round(res.get("latitude", 0.0), 3)},{round(res.get("longitude", 0.0), 3)}'
+        location = f'{round(res.get("latitude", 0.0), 3)},{round(res.get("longitude", 0.0), 3)}'
 
-    ip_details = {
-        'ASN': res.get('asn', ''),
-        'Address': ip,
-        'Hostname': hostname,
-        'Geo': {
-            'Country': res.get('country_name', ''),
-            'Location': location
+        ip_details = {
+            'ASN': res.get('asn', ''),
+            'Address': ip,
+            'Hostname': hostname,
+            'Geo': {
+                'Country': res.get('country_name', ''),
+                'Location': location
+            }
         }
-    }
 
-    shodan_ip_details = {
-        'Tag': res.get('tags', []),
-        'Latitude': res.get('latitude', 0.0),
-        'Longitude': res.get('longitude', 0.0),
-        'Org': res.get('org', ''),
-        'ASN': res.get('asn', ''),
-        'ISP': res.get('isp', ''),
-        'LastUpdate': res.get('last_update', ''),
-        'CountryName': res.get('country_name', ''),
-        'Address': ip,
-        'OS': res.get('os', ''),
-        'Port': res.get('ports', [])
-    }
-
-    ec = {
-        outputPaths['ip']: ip_details,
-        'Shodan': {
-            'IP': shodan_ip_details
+        shodan_ip_details = {
+            'Tag': res.get('tags', []),
+            'Latitude': res.get('latitude', 0.0),
+            'Longitude': res.get('longitude', 0.0),
+            'Org': res.get('org', ''),
+            'ASN': res.get('asn', ''),
+            'ISP': res.get('isp', ''),
+            'LastUpdate': res.get('last_update', ''),
+            'CountryName': res.get('country_name', ''),
+            'Address': ip,
+            'OS': res.get('os', ''),
+            'Port': res.get('ports', [])
         }
-    }
 
-    human_readable = tableToMarkdown(f'Shodan details for IP {ip}', {
-        'Country': ec[outputPaths['ip']]['Geo']['Country'],
-        'Location': ec[outputPaths['ip']]['Geo']['Location'],
-        'ASN': ec[outputPaths['ip']]['ASN'],
-        'ISP': ec['Shodan']['IP']['ISP'],
-        'Ports': ', '.join([str(x) for x in ec['Shodan']['IP']['Port']]),
-        'Hostname': ec[outputPaths['ip']]['Hostname']
-    })
+        ec = {
+            outputPaths['ip']: ip_details,
+            'Shodan': {
+                'IP': shodan_ip_details
+            }
+        }
 
-    demisto.results({
-        'Type': entryTypes['note'],
-        'Contents': res,
-        'ContentsFormat': formats['json'],
-        'HumanReadable': human_readable,
-        'HumanReadableFormat': formats['markdown'],
-        'EntryContext': ec
-    })
+        human_readable = tableToMarkdown(f'Shodan details for IP {ip}', {
+            'Country': ec[outputPaths['ip']]['Geo']['Country'],
+            'Location': ec[outputPaths['ip']]['Geo']['Location'],
+            'ASN': ec[outputPaths['ip']]['ASN'],
+            'ISP': ec['Shodan']['IP']['ISP'],
+            'Ports': ', '.join([str(x) for x in ec['Shodan']['IP']['Port']]),
+            'Hostname': ec[outputPaths['ip']]['Hostname']
+        })
+
+        demisto.results({
+            'Type': entryTypes['note'],
+            'Contents': res,
+            'ContentsFormat': formats['json'],
+            'HumanReadable': human_readable,
+            'HumanReadableFormat': formats['markdown'],
+            'EntryContext': ec
+        })
 
 
 def shodan_search_count_command():
