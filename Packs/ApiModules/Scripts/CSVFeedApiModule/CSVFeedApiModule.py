80836c1cb1af7cdfae790df9514559883ab129cb
rshalem <rshalem@paloaltonetworks.com>
2020-05-24 19:20:43 +0300

    sdk 1-0-9 check

--- Packs/ApiModules/Scripts/CSVFeedApiModule/CSVFeedApiModule.py
+++ Packs/ApiModules/Scripts/CSVFeedApiModule/CSVFeedApiModule.py
@@ -9,7 +9,7 @@ import urllib3
 from dateutil.parser import parse
 from typing import Optional, Pattern, Dict, Any, Tuple, Union
 
-# disable insecure warnings sdf
+# disable insecure warnings
 urllib3.disable_warnings()
 
 
