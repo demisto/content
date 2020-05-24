ccbd5ed0db7aab11465529087e811efa337e5041
Timor Eizenman <50326704+teizenman@users.noreply.github.com>
2020-05-24 12:24:37 +0300

    Setgridfield (#7108)
    * added functionality and tests
    
    * added unpacking feature and tests
    
    * added functionality and tests
    
    * added unpacking feature and tests
    
    * added unpacking feature and tests
    
    * rn
    
    * added the rn file
    
    * update to changelog
    
    * CR fixes
    
    * updated tests
    
    * updated tests
    
    * Rename 1_0_0.md to 1_1_0.md
    
    * Updated descriptions
    
    * method documentation update
    
    * updated rn from 1.0.1
    
    * updated rn from 1.0.1
    
    * Delete 1_0_1.md
    
    * Create 1_0_1.MD
    
    Reverted delete
    
    * Update 1_1_0.md
    
    reverted 1.0.1 update
    
    * Rename 1_0_1.MD to 1_0_1.md
    
    Co-authored-by: Alex Fiedler <38628621+kirbles19@users.noreply.github.com>

--- Packs/CommonScripts/Scripts/SetGridField/SetGridField_test.py
+++ Packs/CommonScripts/Scripts/SetGridField/SetGridField_test.py
@@ -23,29 +23,39 @@ def test_filter_the_dict(before_dict: dict, keys: dict, max_keys: int, after_dic
                                      max_keys=max_keys)
 
 
-@pytest.mark.parametrize(argnames="entry_context, keys, raise_exception",
+@pytest.mark.parametrize(argnames="entry_context, keys, raise_exception, unpack_nested",
                          argvalues=[
-                             ([{'a': 'val', 'b': 'val'}], ['a', 'b'], False),
-                             ([{'a': [], 'b': 'val'}], ['a', 'b'], True),
-                             ([{'a': [], 'b': 'val'}], ['b'], False)
+                             ([{'a': 'val', 'b': 'val'}], ['a', 'b'], False, False),
+                             ([{'a': [], 'b': 'val'}], ['a', 'b'], True, False),
+                             ([{'a': [], 'b': 'val'}], ['b'], False, False),
+                             (['a', 'b', 1, False], ['b'], False, False),
+                             (['a', 'b', 1, False, []], ['*'], True, False),
                          ])
-def test_validate_entry_context(entry_context: dict, keys: list, raise_exception: bool):
+def test_validate_entry_context(entry_context: dict, keys: list, raise_exception: bool, unpack_nested: bool):
     from SetGridField import validate_entry_context
     if raise_exception:
         with pytest.raises(ValueError):
             validate_entry_context(entry_context=entry_context,
-                                   keys=keys)
+                                   keys=keys,
+                                   unpack_nested_elements=unpack_nested)
     else:
         validate_entry_context(entry_context=entry_context,
-                               keys=keys)
+                               keys=keys,
+                               unpack_nested_elements=unpack_nested)
 
 
-@pytest.mark.parametrize(argnames="keys, columns, dt_response_json, expected_json",
+@pytest.mark.parametrize(argnames="keys, columns, dt_response_json, expected_json, unpack_nested",
                          argvalues=[
-                             (["name", "value"], ["col1", "col2"], "context_entry_list.json", "expected_list_grid.json"),
-                             (["*"], ["col1", "col2"], "context_entry_dict.json", "expected_dict_grid.json"),
+                             (["name", "value"], ["col1", "col2"], "context_entry_list.json", "expected_list_grid.json",
+                              False),
+                             (["*"], ["col1", "col2"], "context_entry_dict.json", "expected_dict_grid.json", False),
+                             (["*"], ["col1"], "context_entry_list_of_values.json", "expected_list_of_values_grid.json",
+                              False),
+                             (["*"], ["col1", "col2"], "context_entry_dict_with_elements.json",
+                              "expected_dict_with_elements_grid.json", True),
                          ])
-def test_build_grid(datadir, mocker, keys: list, columns: list, dt_response_json: str, expected_json: str):
+def test_build_grid(datadir, mocker, keys: list, columns: list, dt_response_json: str, expected_json: str,
+                    unpack_nested: bool):
     import SetGridField
     import json
     import pandas as pd
@@ -53,6 +63,6 @@ def test_build_grid(datadir, mocker, keys: list, columns: list, dt_response_json
     mocker.patch.object(SetGridField, 'demisto')
     SetGridField.demisto.dt.return_value = json.load(open(datadir[dt_response_json]))
     expected_grid = json.load(open(datadir[expected_json]))
-    assert pd.DataFrame(expected_grid).to_dict() == SetGridField.build_grid(context_path=mocker.MagicMock(),
-                                                                            keys=keys,
-                                                                            columns=columns).to_dict()
+    assert pd.DataFrame(expected_grid).to_dict() == \
+        SetGridField.build_grid(context_path=mocker.MagicMock(), keys=keys, columns=columns,
+                                unpack_nested_elements=unpack_nested).to_dict()
