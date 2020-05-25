e451fb977933a360e52f5d4794bfff5351b1179a
esharf <esahrf@paloaltonetworks.com>
2020-05-25 17:19:17 +0300

    code ready exept ioc from xdr to demisto

--- Packs/XDR_iocs/Integrations/XDR_iocs/XDR_iocs.py
+++ Packs/XDR_iocs/Integrations/XDR_iocs/XDR_iocs.py
@@ -1,3 +1,6 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
 import hashlib
 import secrets
 import string
@@ -5,16 +8,20 @@ import tempfile
 from datetime import timezone
 from typing import Dict, Optional, List, Tuple
 from dateutil.parser import parse
+from urllib3 import disable_warnings
+from math import ceil
 
-import demistomock as demisto
-from CommonServerPython import *
+
+disable_warnings()
 
 
 class Client:
+    severity = ''
     query = 'type:File and (sha256:* or (-sha1:* and md5:*)) or type:Domain or type:IP'
     error_codes = {
-        500: 'internal server error.',
-        401: 'Unauthorized access. An issue occurred during authentication. This can indicate an incorrect key, id, or other invalid authentication parameters.',
+        500: 'XDR internal server error.',
+        401: 'Unauthorized access. An issue occurred during authentication. This can indicate an ' +    # noqa: W504
+             'incorrect key, id, or other invalid authentication parameters.',
         402: 'Unauthorized access. User does not have the required license type to run this API.',
         403: 'Unauthorized access. The provided API key does not have the required RBAC permissions to run this API'
     }
@@ -35,8 +42,7 @@ class Client:
                             **requests_kwargs)
 
         if res.status_code in self.error_codes:
-            print(res.content)
-            raise DemistoException(self.error_codes[res.status_code])
+            raise DemistoException(f'{self.error_codes[res.status_code]}\t({res.content.decode()})')
 
         return res.json()
 
@@ -92,7 +98,7 @@ def create_file_iocs_to_keep(file_path):
     with open(file_path, 'a') as _file:
         total_size = demisto.searchIndicators(query='', page=0, size=1).get('total', 0)
         batch_size = 200
-        for i in range(4, total_size, batch_size):
+        for i in range(0, ceil(total_size / batch_size)):
             iocs = demisto.searchIndicators(query='', page=i, size=batch_size).get('iocs', [])
             for ios in map(lambda x: x.get('value'), iocs):
                 _file.write(ios)
@@ -102,12 +108,9 @@ def create_file_iocs_to_keep(file_path):
 def create_file_sync(file_path):
     with open(file_path, 'a') as _file:
         total_size = get_iocs_size()
-        # total_size = 1
         batch_size = 200
-        for i in range(0, total_size, batch_size):
+        for i in range(0, ceil(total_size / batch_size)):
             iocs = get_iocs(page=i, size=batch_size)
-            for i in iocs:
-                print(i.get('expirationStatus'))
             for ios in map(lambda x: json.dumps(demisto_ioc_to_xdr(x)), iocs):
                 _file.write(ios)
                 _file.write('\n')
@@ -120,12 +123,13 @@ demisto_score_to_xdr = {
 }
 
 
-def get_iocs_size(_from=None, to=None):
-    return demisto.searchIndicators(query=Client.query, page=0, size=1).get('total', 0)
+def get_iocs_size(from_date=None, to_date=None):
+    return demisto.searchIndicators(query=Client.query, page=0, size=1, fromDate=from_date, toDate=to_date).get('total', 0)
 
 
-def get_iocs(page=0, size=200, _from=None, to=None):
-    return demisto.searchIndicators(query=Client.query, page=page, size=size).get('iocs', [])
+def get_iocs(page=0, size=200, from_date=None, to_date=None):
+    return demisto.searchIndicators(query=Client.query, page=page, size=size,
+                                    fromDate=from_date, toDate=to_date).get('iocs', [])
 
 
 def demisto_expiration_to_xdr(expiration):
@@ -171,9 +175,9 @@ def demisto_types_to_xdr(_type: str):
 def demisto_ioc_to_xdr(ioc: Dict) -> Dict:
     xdr_ioc = {
         'indicator': ioc['value'],
-        'severity': 'INFO',
+        'severity': Client.severity,
         'type': demisto_types_to_xdr(ioc['indicator_type']),
-        'reputation': demisto_score_to_xdr.get(ioc.get('score'), 'UNKNOWN')
+        'reputation': demisto_score_to_xdr.get(ioc.get('score', 0), 'UNKNOWN')
     }
     if 'expiration' in ioc:
         xdr_ioc['expiration_date'] = demisto_expiration_to_xdr(ioc['expiration'])
@@ -181,9 +185,11 @@ def demisto_ioc_to_xdr(ioc: Dict) -> Dict:
         xdr_ioc['comment'] = ioc['comment']
     if ioc.get('aggregatedReliability'):
         xdr_ioc['reliability'] = ioc['aggregatedReliability'][0]
-    if vendors := demisto_vendors_to_xdr(ioc.get('moduleToFeedMap', {})):
+    vendors = demisto_vendors_to_xdr(ioc.get('moduleToFeedMap', {}))
+    if vendors:
         xdr_ioc['vendors'] = vendors
-    if threat_type := ioc.get('CustomFields', {}).get('threattypes', {}).get('threatcategory', False):
+    threat_type = ioc.get('CustomFields', {}).get('threattypes', {}).get('threatcategory', False)
+    if threat_type:
         xdr_ioc['class'] = threat_type
     return xdr_ioc
 
@@ -199,12 +205,13 @@ def sync(client: Client):
     requests_kwargs = get_requests_kwargs(file_path=temp_file_path)
     path = 'sync_tim_iocs'
     client.http_request(path, requests_kwargs)
-    demisto.setLastRun({'next_run': int(datetime.utcnow().timestamp() * 1000)})
+    demisto.setIntegrationContext({'ts': int(datetime.now(timezone.utc).timestamp() * 1000),
+                                   'time': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')})
     return_outputs('sync with XDR completed.')
 
 
 def iocs_to_keep(client: Client):
-    if datetime.utcnow().hour in range(1, 3):
+    if not datetime.utcnow().hour in range(1, 3):
         raise DemistoException('iocs_to_keep runs only between 01:00 and 03:00.')
     temp_file_path = get_temp_file()
     create_file_iocs_to_keep(temp_file_path)
@@ -215,7 +222,16 @@ def iocs_to_keep(client: Client):
 
 
 def get_last_iocs():
-    iocs = get_iocs()
+    current_run = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    last_run = demisto.getIntegrationContext()
+    print(last_run['time'])
+    total_size = get_iocs_size(from_date=last_run['time'], to_date=current_run)
+    batch_size = 200
+    iocs = []
+    for i in range(0, ceil(total_size / batch_size)):
+        iocs.extend(get_iocs(from_date=last_run['time'], to_date=current_run, page=i, size=batch_size))
+    last_run['time'] = current_run
+    demisto.setIntegrationContext(last_run)
     return iocs
 
 
@@ -227,39 +243,47 @@ def tim_insert_jsons(client: Client):
 
 
 def iocs_command(client: Client):
-    command = demisto.command()[4:10]
+    command = demisto.command().split('-')[1]
+    indicators = demisto.args().get('indicator')
     if command == 'enable':
-        path, iocs = prepare_enable_iocs(demisto.args().get('iocs'))
+        path, iocs = prepare_enable_iocs(indicators)
     else:   # command == 'disable'
-        path, iocs = prepare_disable_iocs(demisto.args().get('iocs'))
+        path, iocs = prepare_disable_iocs(indicators)
+    print(iocs)
     requests_kwargs = get_requests_kwargs(_json=iocs)
     client.http_request(url_suffix=path, requests_kwargs=requests_kwargs)
-    return_outputs(f'indicators {iocs} enabled.', {}, {})
+    return_outputs(f'indicators {indicators} {command}d.', {}, {})
 
 
 def xdr_ioc_to_demisto(ioc):
-    return {}
+    ioc_time_line = {
+        'Value': ioc.get('RULE_INDICATOR'),
+        'Message': 'indicator was updated in XDR.',
+        'Category': 'Integration Update'
+    }
+    return ioc_time_line
 
 
 def filter_inserts(ioc: Dict) -> bool:
-    return not ioc.get('RULE_MODIFY_TIME', False) or \
-           not ioc.get('RULE_INSERT_TIME', False) or \
-           ioc['RULE_MODIFY_TIME'] - ioc['RULE_INSERT_TIME'] > 4
+    if ioc.get('RULE_MODIFY_TIME', False) and ioc.get('RULE_INSERT_TIME', False):
+        if ioc['RULE_MODIFY_TIME'] - ioc['RULE_INSERT_TIME'] < 1000 * 30:   # 0.5 sec
+            return False
+    return True
 
 
 def get_changes(client: Client):
-    from_time = demisto.getLastRun().get('next_run')
+    from_time = demisto.getIntegrationContext()
+    print(from_time)
     if not from_time:
-        raise DemistoException('xdr never synced.')
-    path, requests_kwargs = prepare_get_changes(from_time)
+        raise DemistoException('XDR is not synced.')
+    path, requests_kwargs = prepare_get_changes(from_time['ts'])
     requests_kwargs = get_requests_kwargs(_json=requests_kwargs)
     iocs = client.http_request(url_suffix=path, requests_kwargs=requests_kwargs).get('reply', [])
     if iocs:
-        demisto.setLastRun({'next_run': iocs[-1].get('RULE_MODIFY_TIME', from_time) + 1})
-        for i in iocs:
-            print(i)
+        from_time['ts'] = iocs[-1].get('RULE_MODIFY_TIME', from_time) + 1
+        demisto.setIntegrationContext(from_time)
         iocs = filter(filter_inserts, iocs)
-        demisto.createIndicators(map(lambda x: xdr_ioc_to_demisto(x), iocs))
+        demisto.results({'IndicatorTimeline': list(map(lambda x: xdr_ioc_to_demisto(x), iocs))})
 
 
 def main():
@@ -267,14 +291,17 @@ def main():
     # Executes an integration command
     # """
     params = demisto.params()
+    Client.severity = params.get('severity', '').upper()
+    Client.query = params.get('query', Client.query)
+
     client = Client(params)
-    sync(client)
     commands = {
         'xdr-iocs-sync': sync,
-        'xdr-iocs-iocs_to_keep': iocs_to_keep,
+        'xdr-iocs-iocs-to-keep': iocs_to_keep,
         'xdr-enable-iocs': iocs_command,
         'xdr-disable-iocs': iocs_command,
         'xdr-push-iocs': tim_insert_jsons,
+        'fetch-indicators': get_changes,
         'xdr-iocs-get-changes': get_changes
     }
 
