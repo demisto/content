88daff8f89c51b85fa90eb42a4870b833caa7336
dtavori <dtavori@paloaltonetworks.com>
2020-05-25 19:37:18 +0300

    in progress

--- Tests/scripts/infrastructure_tests/release_notes_generator_test.py
+++ Tests/scripts/infrastructure_tests/release_notes_generator_test.py
@@ -1,12 +1,11 @@
+from demisto_sdk.TestSuite.repo import Repo
 
 
 def test_get_release_notes_dict():
     from release_notes_generator import get_release_notes_dict
-    release_notes_files = [
-        'Tests/scripts/infrastructure_tests/tests_data/fake_release_notes/1_0_1.md',
-        'Tests/scripts/infrastructure_tests/tests_data/fake_release_notes/1_0_2.md',
-        'Tests/scripts/infrastructure_tests/tests_data/fake_release_notes/1_0_3.md'
-    ]
+    repo = Repo()
+    pack = repo.create_pack('FakePack')
+    pack.add_release_notes()
 
     mocker.patch.object(demisto_sdk.commands.common.tools, 'get_pack_name', return_value='FakePack')
     release_notes_dict = get_release_notes_dict(release_notes_files)  # todo: fix
