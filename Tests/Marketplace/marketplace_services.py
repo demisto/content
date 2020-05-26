158cee5eaf4110bdc38a4b953569231ddef0bab0
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-05-26 17:44:19 +0300

    included only first level of dependencies

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -306,7 +306,14 @@ class Pack(object):
             dependency_integration_images = dependency_data.get('integrations', [])
 
             for dependency_integration in dependency_integration_images:
-                if dependency_integration not in pack_integration_images:
+                dependency_integration_gcs_path = dependency_integration.get('imagePath', '')  # image public url
+                dependency_pack_name = os.path.dirname(  # extract pack name from public url
+                    os.path.relpath(dependency_integration_gcs_path, GCPConfig.STORAGE_BASE_PATH))
+
+                if dependency_pack_name not in display_dependencies_images:
+                    continue  # skip if integration image is not part of displayed pack
+
+                if dependency_integration not in pack_integration_images:  # avoid duplicates in list
                     pack_integration_images.append(dependency_integration)
 
         return pack_integration_images
