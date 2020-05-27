bac174642c07bf4c105c164d792daa738c8477e2
dtavori <dtavori@paloaltonetworks.com>
2020-05-27 19:06:41 +0300

    test nightly build with new ami

--- Tests/scripts/infrastructure_tests/test_collect_tests_and_content_packs.py
+++ Tests/scripts/infrastructure_tests/test_collect_tests_and_content_packs.py
@@ -239,7 +239,7 @@ class TestChangedTestPlaybook:
         with open("./Tests/filter_envs.json", "r") as filter_envs_file:
             filter_envs = json.load(filter_envs_file)
         assert filter_envs.get('Server Master') is True
-        assert filter_envs.get('Demisto PreGA') is True
+        assert filter_envs.get('Demisto Marketplace') is True
         assert filter_envs.get('Demisto two before GA') is False
         assert filter_envs.get('Demisto one before GA') is True
         assert filter_envs.get('Demisto GA') is True
@@ -276,7 +276,7 @@ class TestChangedTestPlaybook:
         with open("./Tests/filter_envs.json", "r") as filter_envs_file:
             filter_envs = json.load(filter_envs_file)
         assert filter_envs.get('Server Master') is True
-        assert filter_envs.get('Demisto PreGA') is True
+        assert filter_envs.get('Demisto Marketplace') is True
         assert filter_envs.get('Demisto two before GA') is False
         assert filter_envs.get('Demisto one before GA') is False
         assert filter_envs.get('Demisto GA') is True
