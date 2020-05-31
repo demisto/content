9c4646349efcab52cc93f06e5f6477b438b3b04f
dtavori <dtavori@paloaltonetworks.com>
2020-05-31 21:25:48 +0300

    small fix

--- Tests/Marketplace/zip_packs.py
+++ Tests/Marketplace/zip_packs.py
@@ -128,7 +128,7 @@ def download_packs_from_gcp(storage_bucket, gcp_path, destination_path, circle_b
             if pack.name in IGNORED_FILES:
                 continue
             # Search for the pack in the bucket
-            pack_prefix = os.path.join(gcp_path, branch_name, circle_build, pack.name, 'content', 'packs')
+            pack_prefix = os.path.join(gcp_path, branch_name, circle_build, 'content', 'packs', pack.name)
             blobs = list(storage_bucket.list_blobs(prefix=pack_prefix))
             if blobs:
                 blob = get_latest_pack_zip_from_blob(pack.name, blobs)
