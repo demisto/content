a2158f07be1fae87601d18fe2520cc215c5f4992
Rony Kozakish <37589583+ronykoz@users.noreply.github.com>
2020-06-01 12:21:57 +0300

    Update checkout_contribution.sh

--- Utils/checkout_contribution.sh
+++ Utils/checkout_contribution.sh
@@ -1,15 +1,26 @@
-#!/usr/bin/env bash
-
-if [[ "$#" -lt 2 ]]; then
-  echo "Usage: $0 <fork name> <branch name within the fork> "
-  exit 1
+#!/bin/bash
+if (( $# < 1 )); then
+    echo "please run ${0} <USER NAME>:<BRANCH NAME> [true]"
+    echo by passing true as third argument this will update branch with origin/master
+    echo for example:
+    echo "${0} yaakovi:NewIntegrationBranch true"
+    exit
 fi
 
+USER=$(echo $1 | cut -d ":" -f 1)
+BRANCH=$(echo $1 | cut -d ":" -f 2)
 
-_fork_name=$1
-_branch=$2
+if [[ $2 == "true" ]]; then
+    git checkout master
+    git pull
+fi
 
-git remote add ${_fork_name} git@github.com:${_fork_name}/content.git
-git fetch ${_fork_name}
-git checkout -t ${_fork_name}/${_branch}
+git remote add $USER git@github.com:$USER/content.git
+git fetch $USER
+git checkout -t $USER/$BRANCH
 git pull
+
+if [[ $2 == "true" ]]; then
+    echo merging branch with origin/master
+    git merge origin/master
+fi
