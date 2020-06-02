cb5c91072ae859239fe92a84594a067c2bac2b03
yorhov <Orekhova97229!>
2020-06-02 09:59:53 +0300

    updated None to ''

--- Packs/FeedCSV/Integrations/FeedCSV/FeedCSV.py
+++ Packs/FeedCSV/Integrations/FeedCSV/FeedCSV.py
@@ -1,4 +1,11 @@
+import demistomock as demisto
+from CommonServerPython import *
+
 def main():
+    params = {k: v for k, v in demisto.params().items() if v is not None}
+    if not params.get('auto_detect_type'):
+        if not params.get('indicator_type'):
+            return_error('Indicator Type cannot be empty when Auto Detect Indicator Type is unchecked')
     feed_main('CSV', prefix='csv')
 
 
