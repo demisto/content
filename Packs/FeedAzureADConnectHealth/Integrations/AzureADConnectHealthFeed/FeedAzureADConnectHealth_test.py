790604a6a84eb3001186942dfc606463db674a7a
DctrG <jg.serrano@me.com>
2020-06-05 08:24:03 +0800

    Add pack Azure AD Connect Health Feed

---
+++ Packs/FeedAzureADConnectHealth/Integrations/AzureADConnectHealthFeed/FeedAzureADConnectHealth_test.py
@@ -0,0 +1,21 @@
+from AzureADConnectHealthFeed import Client
+
+
+URL = 'https://docs.microsoft.com/en-us/azure/active-directory/hybrid/how-to-connect-health-agent-install#outbound-connectivity-to-the-azure-service-endpoints'
+
+
+def test_build_iterator():
+    with open('test_data/Microsoft_endpoint_mock.html', 'r') as file:
+        response = file.read()
+    expected_url = 'https://login.microsoftonline.com'
+    expected_domain_glob = '*.blob.core.windows.net'
+    client = Client(
+        base_url=URL,
+        verify=False,
+        proxy=False,
+    )
+    indicators = client.build_iterator()
+    url_indicators = {indicator['value'] for indicator in indicators if indicator['type'] == 'URL'}
+    domain_glob_indicators = {indicator['value'] for indicator in indicators if indicator['type'] == 'DomainGlob'}
+    assert expected_url in url_indicators
+    assert expected_domain_glob in domain_glob_indicators
