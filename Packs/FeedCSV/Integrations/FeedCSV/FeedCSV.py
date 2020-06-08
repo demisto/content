bdaa8dbdedf45f48a6a6db15709aa091723780ed
yorhov <Orekhova97229!>
2020-06-08 12:22:07 +0300

    updated pr

--- Packs/FeedCSV/Integrations/FeedCSV/FeedCSV.py
+++ Packs/FeedCSV/Integrations/FeedCSV/FeedCSV.py
@@ -16,5 +16,5 @@ def main():
 from CSVFeedApiModule import *  # noqa: E402
 
 
-if __name__ == '__builtin__' or __name__ == 'builtins' or __name__ == '__main__':
+if __name__ in ('__builtin__', 'builtins', '__main__'):
     main()
