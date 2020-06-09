089a763dd0bdf731e544da0a844ee11766df6e46
guyfreund <gfreund@paloaltonetworks.com>
2020-06-09 15:01:12 +0300

    unit test is failing

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -460,11 +460,14 @@ def test_argToList():
     test2 = 'a,b,c'
     test3 = '["a","b","c"]'
     test4 = 'a;b;c'
+    test5 = 1
 
     results = [argToList(test1), argToList(test2), argToList(test2, ','), argToList(test3), argToList(test4, ';')]
 
     for result in results:
         assert expected == result, 'argToList test failed, {} is not equal to {}'.format(str(result), str(expected))
+
+    assert argToList(test5) == [1]
 
 
 def test_remove_nulls():
