db570418181f64446a1c9bf7489b61f10b295e27
rsagi <rsagi@paloaltonetworks.com>
2020-06-09 16:26:31 +0300

    script now handles playbooks, testplaybooks, integrations and scripts

--- Utils/create_old_content_pack.py
+++ Utils/create_old_content_pack.py
@@ -3,7 +3,11 @@ import shutil
 import sys
 import os
 import click
+import yaml
+import io
+from pkg_resources import parse_version
 from datetime import datetime
+
 from typing import Dict
 
 DIR_LIST = ['Integrations', 'Scripts', 'IncidentFields', 'IncidentTypes', 'IndicatorFields',
@@ -74,6 +78,69 @@ def copytree(src, dst):
                 shutil.copy(s, d)
 
 
+def get_file(method, file_path, type_of_file):
+    data_dictionary = None
+    with open(os.path.expanduser(file_path), mode="r", encoding="utf8") as f:
+        if file_path.endswith(type_of_file):
+            read_file = f.read()
+            replaced = read_file.replace("simple: =", "simple: '='")
+            # revert str to stream for loader
+            stream = io.StringIO(replaced)
+            try:
+                data_dictionary = method(stream)
+            except Exception as e:
+                click.secho("{} has a structure issue of file type{}. Error was: {}".format(file_path, type_of_file, str(e)), fg="red")
+                return {}
+    if type(data_dictionary) is dict:
+        return data_dictionary
+    return {}
+
+
+def get_yaml(file_path):
+    return get_file(yaml.safe_load, file_path, ('yml', 'yaml'))
+
+def handle_yml_file(file_path):
+    yml_content = get_yaml(file_path)
+    if 'toversion' in yml_content:
+        if parse_version(str(yml_content['toversion'])) < parse_version(yml_old_version):
+            return False
+
+    if 'fromversion' in yml_content:
+        if parse_version(str(yml_content['fromversion'])) > parse_version(yml_old_version):
+            return False
+
+    yml_content['toversion'] = yml_old_version
+    with open(file_path, 'w') as f:
+        yaml.dump(yml_content, f)
+        print(f"Updating {file_path}")
+
+    return True
+
+def delete_playbook(file_path):
+    os.remove(file_path)
+    print(f"Deleting {file_path}")
+
+    changelog_file = os.path.join(os.path.splitext(file_path)[0] + '_CHANGELOG.md')
+    readme_file = os.path.join(os.path.splitext(file_path)[0] + '_README.md')
+    if os.path.isfile(changelog_file):
+        os.remove(changelog_file)
+
+    if os.path.isfile(readme_file):
+        os.remove(readme_file)
+
+
+def delete_script_or_integration(path):
+    if os.path.isfile(path):
+        os.remove(path)
+        changelog_file = os.path.splitext(path)[0] + '_CHANGELOG.md'
+        if os.path.isfile(changelog_file):
+            os.remove(changelog_file)
+
+    else:
+        shutil.rmtree(path)
+    print(f"Deleting {path}")
+
+
 for copied_pack_name in os.listdir('Packs'):
     if copied_pack_name != old_version:
         copied_pack_path = os.path.join('Packs', copied_pack_name)
@@ -90,3 +157,47 @@ for copied_pack_name in os.listdir('Packs'):
             copytree(dir_path, old_pack_directory)
 
 click.secho("Finished content copy", fg="green")
+
+if old_version.count('.') == 1:
+    yml_old_version = old_version + ".9"
+
+if new_from_version.count('.') == 1:
+    yml_from_version = new_from_version + ".0"
+click.secho("Starting file editing")
+for content_dir in os.listdir(pack_path):
+    dir_path = os.path.join(pack_path, content_dir)
+    if content_dir in ['Playbooks', 'TestPlaybooks']:
+        for file_name in os.listdir(dir_path):
+            file_path = os.path.join(dir_path, file_name)
+            if file_path.endswith('md'):
+                continue
+
+            if os.path.isfile(file_path):
+                if file_path.endswith('.yml'):
+                    if not handle_yml_file(file_path):
+                        delete_playbook(file_path)
+
+            else:
+                inner_dir_path = file_path
+                for inner_file_name in os.listdir(inner_dir_path):
+                    file_path = os.path.join(inner_dir_path, inner_file_name)
+                    if file_path.endswith('.yml'):
+                        if not handle_yml_file(file_path):
+                            delete_playbook(file_path)
+
+    if content_dir in ['Scripts', 'Integrations']:
+        for script_name in os.listdir(dir_path):
+            path = os.path.join(dir_path, script_name)
+            if path.endswith('.md'):
+                continue
+
+            if os.path.isfile(path):
+                yml_file_path = path
+
+            else:
+                yml_file_path = os.path.join(path, script_name + '.yml')
+            if not handle_yml_file(yml_file_path):
+                delete_script_or_integration(path)
+
+
+click.secho("Finished file editing", fg='green')
