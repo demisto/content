68be19d44781b6a1b86414a7ae922c8726bf7ad3
ShahafBenYakir <shahaf.benyakir@demisto.com>
2020-06-16 20:19:43 +0300

    Added failed instances to file artifact

--- Tests/instance_notifier.py
+++ Tests/instance_notifier.py
@@ -13,12 +13,13 @@ SERVER_URL = "https://{}"
 
 def options_handler():
     parser = argparse.ArgumentParser(description='Parser for slack_notifier args')
-    parser.add_argument('-n', '--instance_tests', type=str2bool, help='is instance test build?', required=True)
+    parser.add_argument('-t', '--instance_tests', type=str2bool, help='is instance test build?', required=True)
     parser.add_argument('-s', '--slack', help='The token for slack', required=True)
     parser.add_argument('-e', '--secret', help='Path to secret conf file', required=True)
     parser.add_argument('-u', '--user', help='The username for the login', required=True)
     parser.add_argument('-p', '--password', help='The password for the login', required=True)
     parser.add_argument('-b', '--buildUrl', help='The url for the build', required=True)
+    parser.add_argument('-n', '--buildNumber', help='The build number', required=True)
     options = parser.parse_args()
 
     return options
@@ -74,7 +75,8 @@ def test_instances(secret_conf_path, server, username, password):
                 print_error(
                     'Failed to create instance of {} with message: {}'.format(integration_name, failure_message))
                 failed_integrations.append("{} {} - devops comments: {}".format(
-                    integration_name, product_description, devops_comments))
+                    integration_name, product_description,
+                ))
             else:
                 instance_ids.append(instance_id)
                 print('Create integration %s succeed' % (integration_name,))
@@ -85,8 +87,14 @@ def test_instances(secret_conf_path, server, username, password):
     return failed_integrations, integrations_counter
 
 
+def create_failed_integrations_file(failed_instances):
+    with open("./Tests/failed_instances.txt", "w") as failed_instances_file:
+        failed_instances_file.write('\n'.join(failed_instances))
+
+
 def get_attachments(secret_conf_path, server, user, password, build_url):
     failed_integration, integrations_counter = test_instances(secret_conf_path, server, user, password)
+    create_failed_integrations_file(failed_integration)
 
     fields = []
     if failed_integration:
@@ -111,31 +119,46 @@ def get_attachments(secret_conf_path, server, user, password, build_url):
     return attachment, integrations_counter
 
 
-def slack_notifier(slack_token, secret_conf_path, server, user, password, build_url):
+def slack_notifier(slack_token, secret_conf_path, server, user, password, build_url, build_number):
     print_color("Starting Slack notifications about instances", LOG_COLORS.GREEN)
     attachments, integrations_counter = get_attachments(secret_conf_path, server, user, password, build_url)
 
     sc = SlackClient(slack_token)
+
+    # Failing instances list
     sc.api_call(
         "chat.postMessage",
-        channel="dmst-content-lab",
+        # channel="dmst-content-lab",
+        channel="WHCL130LE",
         username="Instances nightly report",
         as_user="False",
         attachments=attachments,
         text="You have {0} instances configurations".format(integrations_counter)
     )
 
+    # Failing instances file
+    sc.api_call(
+        "chat.postMessage",
+        # channel="dmst-content-lab",
+        channel="WHCL130LE",
+        username="Instances nightly report",
+        as_user="False",
+        attachments='{}-60525392-gh.circle-artifacts.com/0/artifacts/failed_instances.txt'.format(build_number),
+        text="Detailed list of failing instances could be found in the following link"
+    )
+
 
 if __name__ == "__main__":
     options = options_handler()
-    if options.instance_tests:
-        with open('./env_results.json', 'r') as json_file:
-            env_results = json.load(json_file)
-            server = SERVER_URL.format(env_results[0]["InstanceDNS"])
+    # if options.instance_tests:
+    with open('./env_results.json', 'r') as json_file:
+        env_results = json.load(json_file)
+        server = SERVER_URL.format(env_results[0]["InstanceDNS"])
 
-        slack_notifier(options.slack, options.secret, server, options.user, options.password, options.buildUrl)
-        # create this file for destroy_instances script
-        with open("./Tests/is_build_passed_{}.txt".format(env_results[0]["Role"].replace(' ', '')), 'a'):
-            pass
-    else:
-        print_error("Not instance tests build, stopping Slack Notifications about instances")
+    slack_notifier(options.slack, options.secret, server, options.user, options.password, options.buildUrl,
+                   options.buildNumber)
+    # create this file for destroy_instances script
+    with open("./Tests/is_build_passed_{}.txt".format(env_results[0]["Role"].replace(' ', '')), 'a'):
+        pass
+    # else:
+    #     print_error("Not instance tests build, stopping Slack Notifications about instances")
