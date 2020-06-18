8547b642e29a9d97fbad6c6950c023abae198531
bachen <bachen@paloaltonetworks.com>
2020-06-18 14:23:31 +0300

    add support for contributor pack

--- Utils/trigger_content_build_with_time_to_live.sh
+++ Utils/trigger_content_build_with_time_to_live.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$#" -lt 2 ]]; then
-  echo "Usage: $0 <content branch name: such as master> <circle ci token> <time to live>"
+  echo "Usage: $0 <content branch name: such as master> <circle ci token> [time to live] [contributor branch]"
   echo "You can get a circle ci token from: CircleCI -> User -> Personal API Tokens"
   echo "Time to live is in minutes, for example 360 equals 6 hours"
   echo "Minimum time to live is 180 minutes, maximum time is 540 minutes"
@@ -12,6 +12,10 @@ fi
 _branch=$1
 _circle_token=$2
 _time_to_live=$3
+_contrib_branch=$4
+
+trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_branch}?circle-token=${_circle_token}
+
 
 if [ -z $_time_to_live ] || [ $_time_to_live -lt 180 ]
 then
@@ -25,16 +29,28 @@ then
     exit 1
 fi
 
-
-trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_branch}?circle-token=${_circle_token}
-
-post_data=$(cat <<EOF
-{
-  "build_parameters": {
-    "TIME_TO_LIVE": ${_time_to_live}
+if [ -z $_contrib_branch ]
+then
+  post_data=$(cat <<EOF
+  {
+    "build_parameters": {
+      "TIME_TO_LIVE": ${_time_to_live}
+    }
   }
-}
-EOF)
+EOF
+)
+else
+  post_data=$(cat <<-EOF
+  {
+    "build_parameters": {
+      "TIME_TO_LIVE": ${_time_to_live}
+      "CONTRIB_BRANCH": ${_contrib_branch}
+    }
+  }
+EOF
+)
+fi
+
 
 curl \
 --header "Accept: application/json" \
