b7f463ab0de714a7729a04701143798e254592c5
bar katzir <bkatzir@paloaltonetworks.com>
2020-06-21 13:49:38 +0300

    suppress warnings

--- Packs/FeedUnit42/Integrations/FeedUnit42/FeedUnit42.py
+++ Packs/FeedUnit42/Integrations/FeedUnit42/FeedUnit42.py
@@ -5,6 +5,9 @@ from taxii2client.v20 import Server, as_pages
 
 from CommonServerPython import *
 
+# disable insecure warnings
+requests.packages.urllib3.disable_warnings()
+
 UNIT42_TYPES_TO_DEMISTO_TYPES = {
     'ipv4-addr': FeedIndicatorType.IP,
     'ipv6-addr': FeedIndicatorType.IPv6,
