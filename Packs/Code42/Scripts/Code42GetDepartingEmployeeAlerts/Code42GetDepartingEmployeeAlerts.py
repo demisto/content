752883895d04676e9db15c8e1ed5c62904c74748
Juliya Smith <juliya.smith@code42.com>
2020-06-23 18:21:34 +0000

    Main

--- Packs/Code42/Scripts/Code42GetDepartingEmployeeAlerts/Code42GetDepartingEmployeeAlerts.py
+++ Packs/Code42/Scripts/Code42GetDepartingEmployeeAlerts/Code42GetDepartingEmployeeAlerts.py
@@ -2,31 +2,35 @@ import demistomock as demisto
 from CommonServerPython import *
 
 
-res = {"total": 0}
-res_data = []
-top = demisto.args().get("top") or 10
+def main():
+    res = {"total": 0}
+    res_data = []
+    top = demisto.args().get("top") or 10
 
-try:
-    employees = demisto.executeCommand("code42-departingemployee-get-all", {})[0]["Contents"]
-    res["total"] = len(employees)
+    try:
+        employees = demisto.executeCommand("code42-departingemployee-get-all", {})[0]["Contents"]
+        res["total"] = len(employees)
 
-    # Get each employee on the Departing Employee List and their total alerts.
-    for employee in employees:
-        username = employee["userName"]
-        alerts = demisto.executeCommand("code42-alert-search", {"username": username})[0]["Contents"]
-        alerts_count = len(alerts)
+        # Get each employee on the Departing Employee List and their total alerts.
+        for employee in employees:
+            username = employee["userName"]
+            alerts = demisto.executeCommand("code42-alert-search", {"username": username})[0]["Contents"]
+            alerts_count = len(alerts)
 
-        # Ignores employees without alerts
-        if alerts_count:
-            employee_res = {"Username": username, "Alerts Count": alerts_count}
-            res_data.append(employee_res)
+            # Ignores employees without alerts
+            if alerts_count:
+                employee_res = {"Username": username, "Alerts Count": alerts_count}
+                res_data.append(employee_res)
 
-    # Sort such that highest alert counts are first and get top.
-    res["data"] = sorted(res_data, key=lambda x: x["Alerts Count"], reverse=True)[:top]
+        # Sort such that highest alert counts are first and get top.
+        res["data"] = sorted(res_data, key=lambda x: x["Alerts Count"], reverse=True)[:top]
+        demisto.results(res)
+    except Exception as e:
+        res["total"] = -1
+        res["data"] = str(e)
+
+    # Submit final results to Cortex XSOAR
     demisto.results(res)
-except Exception as e:
-    res["total"] = -1
-    res["data"] = str(e)
 
-# Submit final results to Cortex XSOAR
-demisto.results(res)
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main()
