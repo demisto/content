ef6ba571493368dbbc17ccf820a1fe7a660015c7
bachen <bachen@paloaltonetworks.com>
2020-06-23 13:04:11 +0300

    Add pack name arg to trigger script

--- Utils/trigger_content_build_with_time_to_live.sh
+++ Utils/trigger_content_build_with_time_to_live.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$#" -lt 2 ]]; then
-  echo "Usage: $0 <content branch name: such as master> <circle ci token> [time to live] [contributor branch: such as testUser:testBranch]"
+  echo "Usage: $0 <content branch name: such as master> <circle ci token> [time to live] [contributor branch: such as testUser:testBranch] [changed pack: such as Packs/test_pack/]"
   echo "You can get a circle ci token from: CircleCI -> User -> Personal API Tokens"
   echo "Time to live is in minutes, for example 360 equals 6 hours"
   echo "Minimum time to live is 180 minutes, maximum time is 540 minutes"
@@ -13,6 +13,7 @@ _branch=$1
 _circle_token=$2
 _time_to_live=$3
 _contrib_branch=$4
+_changed_pack=$5
 
 trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_branch}?circle-token=${_circle_token}
 
@@ -26,6 +27,12 @@ fi
 if [ $_time_to_live -gt 540 ]
 then
     echo "Maximum time is 540 minutes, please change the time_to_live argument"
+    exit 1
+fi
+
+if [ -n $_contrib_branch ] && [ -z $_changed_pack]
+then
+    echo "You must specify the pack name"
     exit 1
 fi
 
@@ -44,7 +51,7 @@ else
   {
     "build_parameters": {
       "TIME_TO_LIVE": ${_time_to_live},
-      "CONTRIB_BRANCH": "${_contrib_branch}"
+      "CONTRIB_BRANCH": "${_contrib_branch}:${_changed_pack}"
     }
   }
 EOF
