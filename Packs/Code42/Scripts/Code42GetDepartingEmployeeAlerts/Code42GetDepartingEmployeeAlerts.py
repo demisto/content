3d50bd0727a2935871586836c7144268d243805b
Juliya Smith <juliya.smith@code42.com>
2020-06-23 18:02:55 +0000

    Handle getting only top x number

--- Packs/Code42/Scripts/Code42GetDepartingEmployeeAlerts/Code42GetDepartingEmployeeAlerts.py
+++ Packs/Code42/Scripts/Code42GetDepartingEmployeeAlerts/Code42GetDepartingEmployeeAlerts.py
@@ -1,8 +1,10 @@
 import demistomock as demisto
 from CommonServerPython import *
 
+
 res = {"total": 0}
 res_data = []
+top = demisto.args().get("top") or 10
 
 try:
     employees = demisto.executeCommand("code42-departingemployee-get-all", {})[0]["Contents"]
@@ -16,15 +18,12 @@ try:
         employee_res = {"Username": username, "Alerts Count": alerts_count}
         res_data.append(employee_res)
 
-    # Sort such that highest alert counts are first.
-    res["data"] = sorted(res_data, key=lambda x: x["Alerts Count"], reverse=True)
+    # Sort such that highest alert counts are first and get top.
+    res["data"] = sorted(res_data, key=lambda x: x["Alerts Count"], reverse=True)[:top]
     demisto.results(res)
 except Exception as e:
-    res = {
-        "Type": entryTypes["error"],
-        "ContentsFormat": formats["text"],
-        "Contents": "Exception info:\n{0}".format(str(ex))
-    }
+    res["total"] = -1
+    res["data"] = str(e)
 
 # Submit final results to Cortex XSOAR
 demisto.results(res)
