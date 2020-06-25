6e650e9e18adad123c4687e5ffee75657602c882
guykeller <g12k34ppp>
2020-06-25 12:34:48 +0300

    some fixes to zipAndEncrypt logic

--- Tests/Marketplace/upload_packs.py
+++ Tests/Marketplace/upload_packs.py
@@ -526,8 +526,6 @@ def option_handler():
                         help='Should encrypt pack or not.', default=False)
     parser.add_argument('-ek', '--encryption_key', type=str,
                         help='The encryption key for the pack, if it should be encrypted.', default='')
-    parser.add_argument('-ep', '--encryption_script_path', type=str,
-                        help='Path to the ZipAndEncryptDirectory script.', default='')
     # disable-secrets-detection-end
     return parser.parse_args()
 
@@ -546,7 +544,6 @@ def main():
     id_set_path = option.id_set_path
     should_encrypt_pack = option.encrypt_pack
     enc_key = option.encryption_key
-    enc_script_path = option.encryption_script_path
     packs_dependencies_mapping = load_json(option.pack_dependencies) if option.pack_dependencies else {}
     storage_base_path = option.storage_base_path
     remove_test_playbooks = option.remove_test_playbooks
@@ -634,7 +631,7 @@ def main():
             pack.cleanup()
             continue
 
-        task_status, zip_pack_path = pack.zip_pack(should_encrypt_pack, enc_script_path, enc_key)
+        task_status, zip_pack_path = pack.zip_pack(extract_destination_path, should_encrypt_pack, enc_key)
 
         if not task_status:
             pack.status = PackStatus.FAILED_ZIPPING_PACK_ARTIFACTS.name
