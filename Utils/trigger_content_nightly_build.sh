779d86ee26c25e30efce4307ef58c77a52c2d2ac
hod-alpert <haplert@paloaltonetworks.com>
2020-06-29 19:05:58 +0300

    Added support in more build triggers

--- Utils/trigger_content_nightly_build.sh
+++ Utils/trigger_content_nightly_build.sh
@@ -3,19 +3,22 @@
 _branch=$1
 _circle_token=$2
 
-trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_branch}?circle-token=${_circle_token}
+trigger_build_url="https://circleci.com/api/v2/project/github/demisto/content/pipeline"
 
-post_data=$(cat <<EOF
+post_data=$(cat <<-EOF
 {
-  "build_parameters": {
-    "NIGHTLY": "true",
-    "TIME_TO_LIVE": 900
+  "branch": "${_branch}",
+  "parameters": {
+    "nightly": "true"
   }
 }
-EOF)
+EOF
+)
+
 
 curl \
 --header "Accept: application/json" \
 --header "Content-Type: application/json" \
 --data "${post_data}" \
---request POST ${trigger_build_url}
+--request POST ${trigger_build_url} \
+--user "$_circle_token:"
--- Utils/trigger_nightly_sdk_build.sh
+++ Utils/trigger_nightly_sdk_build.sh
@@ -3,18 +3,22 @@
 _branch=$1
 _circle_token=$2
 
-trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_branch}?circle-token=${_circle_token}
+trigger_build_url="https://circleci.com/api/v2/project/github/demisto/content/pipeline"
+
 
 post_data=$(cat <<EOF
 {
-  "build_parameters": {
-    "DEMISTO_SDK_NIGHTLY": "true"
+  "branch": "${_branch}",
+  "parameters": {
+    "demisto_sdk_nightly": "true",
   }
 }
-EOF)
+EOF
+)
 
 curl \
 --header "Accept: application/json" \
 --header "Content-Type: application/json" \
 --data "${post_data}" \
---request POST ${trigger_build_url}
+--request POST ${trigger_build_url} \
+--user "$_circle_token:"
