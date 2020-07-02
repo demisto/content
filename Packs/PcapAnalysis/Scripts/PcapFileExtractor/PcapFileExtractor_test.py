8ea1f1a3b131e5a65f0d61a0c9f2d579f5bee798
sberman <sberman@paloaltonetworks.com>
2020-07-02 21:33:48 +0300

    added another test

--- Packs/PcapAnalysis/Scripts/PcapFileExtractor/PcapFileExtractor_test.py
+++ Packs/PcapAnalysis/Scripts/PcapFileExtractor/PcapFileExtractor_test.py
@@ -1,4 +1,7 @@
-from PcapFileExtractor import find_files_protocol
+from tempfile import mkdtemp
+import os
+
+from PcapFileExtractor import find_files_protocol, extract_files
 
 
 def test_find_protocol():
@@ -6,3 +9,9 @@ def test_find_protocol():
     protocol, _ = find_files_protocol(file_path)
     assert 'tftp' == protocol
 
+
+def test_extract_files():
+    dir_path = mkdtemp()
+    file_path = './TestData/tftp_rrq.pcap'
+    extract_files(file_path, dir_path)
+    assert os.path.isfile(f'{dir_path}/rfc1350.txt')
