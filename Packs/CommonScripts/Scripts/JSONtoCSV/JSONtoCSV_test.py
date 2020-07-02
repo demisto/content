21d68c9ac3cd88ba64cee676b559f87b2c83221d
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-07-02 12:43:00 +0300

    Package JSONtoCSV.py

---
+++ Packs/CommonScripts/Scripts/JSONtoCSV/JSONtoCSV_test.py
@@ -0,0 +1,21 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+
+
+def test_main(mocker):
+    from JSONtoCSV import main
+
+    mocker.patch.object(demisto, 'args', return_value={
+        'entryid': 'something@something'
+    })
+    mocker.patch.object(demisto, 'executeCommand', return_value=[
+        {"Contents": [
+            {"Test": "value"},
+            {"Test": "value2"}
+        ]}
+    ])
+
+    main()
+    results = demisto.results.call_args
+    assert results[0][0] is False
