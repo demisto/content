dda8f7f9e43e11e1e23aed9a0ef188a33bd825dc
sberman <sberman@paloaltonetworks.com>
2020-07-06 11:37:23 +0300

    Fixed CR

--- Packs/PcapAnalysis/Scripts/PcapFileExtractor/PcapFileExtractor.py
+++ Packs/PcapAnalysis/Scripts/PcapFileExtractor/PcapFileExtractor.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import demistomock as demisto
 from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
 from tempfile import mkdtemp
@@ -6,7 +8,7 @@ import subprocess
 import hashlib
 
 
-def get_pcap_path(args):
+def get_pcap_path(args: Dict):
     """
 
     Args:
@@ -15,56 +17,13 @@ def get_pcap_path(args):
     Returns: path of pcap file
 
     """
-    file_entry_id = args.get('entry_id')  # type: ignore
-
-    if not file_entry_id:
-        return_error('You must set entry_id')
+    file_entry_id = args.get('entry_id', '')
 
     res = demisto.executeCommand('getFilePath', {'id': file_entry_id})
-    if isError(res):
-        raise Exception(f'Failed to get the file path for entry: {file_entry_id}')
+    if is_error(res):
+        raise Exception(f'Failed to get the file path for entry: {file_entry_id} - {get_error(res)}')
 
     return res[0]['Contents']['path']
-
-
-def find_files_packets(file_path):
-    """
-
-    Args:
-        file_path: the pcap file path
-
-    Returns:
-        protocol: the protocol to use while extracting the files
-        data_list: the relevant packets for the files.
-
-    """
-    protocol = ''
-    data_list = []
-    process = subprocess.Popen(['tshark', '-r', f'{file_path}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout = process.communicate()
-    stdout = str(stdout).split('\\n')
-    for line in stdout:
-        if line.endswith(')') and 'HTTP' in line:
-            data_list.append(line)
-            protocol = 'http'
-
-        if line.endswith(')') and 'IMF' in line:
-            data_list.append(line)
-            protocol = 'imf'
-
-        if line.endswith(')') and 'SMB' in line or 'DCERPC' in line:
-            data_list.append(line)
-            protocol = 'smb'
-
-        if line.endswith(')') and 'DICOM' in line:
-            data_list.append(line)
-            protocol = 'dicom'
-
-        if line.endswith(')') and 'TFTP' in line:
-            data_list.append(line)
-            protocol = 'tftp'
-
-    return protocol, data_list
 
 
 def upload_files(dir_path, file_path):
@@ -86,7 +45,6 @@ def upload_files(dir_path, file_path):
     process.communicate()
 
     context = []
-    protocol, packet_data = find_files_packets(file_path)
 
     md5 = hashlib.md5()
     sha1 = hashlib.sha1()
@@ -99,18 +57,10 @@ def upload_files(dir_path, file_path):
             file_path = os.path.join(root, f)
             file_name = os.path.join(f)
 
-            for data in packet_data:
-                packet = data.split()
-                try:
-                    source_ip = packet[2]
-                    dest_ip = packet[4]
-                except Exception:
-                    pass
-
             with open(file_path, 'rb') as file:
-                demisto.results(fileResult(file_name, file.read()))
-
                 data = file.read()
+                demisto.results(fileResult(file_name, data))
+
                 md5.update(data)
                 sha1.update(data)
                 sha256.update(data)
@@ -121,17 +71,14 @@ def upload_files(dir_path, file_path):
                 'FileSHA256': sha256.hexdigest(),
                 'FileName': file_name,
                 'FileSize': os.path.getsize(file_path),
-                'DetectedInProtocol': protocol,
-                'FileExtension': os.path.splitext(file_name)[1],
-                'SourceIP': source_ip,
-                'DestinationIP': dest_ip
+                'FileExtension': os.path.splitext(file_name)[1]
             })
 
         readable_output = tableToMarkdown('Pcap Extracted Files', [{'name': file_name} for file_name in files])
 
         results = CommandResults(
             outputs_prefix='PcapExtractedFiles',
-            outputs_key_field='FileName',
+            outputs_key_field='FileMD5',
             outputs=context,
             readable_output=readable_output
         )
