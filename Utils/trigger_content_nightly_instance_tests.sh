01b8ca11f9c1ee5915f5184293a35070dd58037d
ShahafBenYakir <shahaf.benyakir@demisto.com>
2020-07-06 11:15:18 +0300

    Increased instance test ttl to 240

--- Utils/trigger_content_nightly_instance_tests.sh
+++ Utils/trigger_content_nightly_instance_tests.sh
@@ -8,7 +8,8 @@ trigger_build_url=https://circleci.com/api/v1/project/demisto/content/tree/${_br
 post_data=$(cat <<EOF
 {
   "build_parameters": {
-    "INSTANCE_TESTS": "true"
+    "INSTANCE_TESTS": "true",
+    "TIME_TO_LIVE": 240
   }
 }
 EOF)
