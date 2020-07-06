fc83572931469aeaa739c3e7a18b984301fe3cd3
Bar Katzir <37335599+bakatzir@users.noreply.github.com>
2020-07-06 19:26:51 +0300

    Update Packs/PAN-OS/Integrations/Panorama/Panorama.py
    Co-authored-by: yuvalbenshalom <ybenshalom@paloaltonetworks.com>

--- Packs/PAN-OS/Integrations/Panorama/Panorama.py
+++ Packs/PAN-OS/Integrations/Panorama/Panorama.py
@@ -4766,7 +4766,7 @@ def panorama_block_vulnerability():
     """
     Ovverride a vulnerability signature such that it is in block mode
     """
-    threatid = demisto.args()['threat_id']
+    threatid = demisto.args().get('threat_id')
     vulnerability_profile = demisto.args()['vulnerability_profile']
     drop_mode = demisto.args().get('drop_mode', 'drop')
 
