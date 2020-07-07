1219387cd3785b7fe856bb64a7d7ed104c3f8363
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-07 14:58:01 +0100

    Fixed linting errors

--- Packs/CommonScripts/Scripts/ModifyDateTime/ModifyDateTime.py
+++ Packs/CommonScripts/Scripts/ModifyDateTime/ModifyDateTime.py
@@ -1,15 +1,15 @@
 import demistomock as demisto
 from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
 from CommonServerUserPython import *  # noqa: E402 lgtm [py/polluting-import]
-from datetime import timedelta
 import dateparser
-
 
 def apply_variation(original_datetime: datetime = None, variation: str = None) -> datetime:
     try:
         new_time = dateparser.parse(variation, settings={'RELATIVE_BASE': original_datetime})
+        return new_time
     except Exception as err:
         return_error(f"Error adding variation to the date / time - {err}")
+
 
 def main():
     args = demisto.args()
@@ -20,8 +20,9 @@ def main():
         return_error(f"Error with input date / time - {err}")
 
     variation = args.get('variation')
-    new_time = apply_variation(variation, original_datetime)
+    new_time = apply_variation(original_datetime, variation)
     demisto.results(new_time.isoformat())
+
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
