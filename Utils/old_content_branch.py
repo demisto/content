4e0890cb07d616b6240cdc4f4389d7554dc9b186
rsagi <rsagi@paloaltonetworks.com>
2020-07-09 14:53:48 +0300

    fixing rewrite to be consistent with content_creator

--- Utils/old_content_branch.py
+++ Utils/old_content_branch.py
@@ -3,39 +3,16 @@ import argparse
 import os
 import click
 import yaml
+import ujson
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import FoldedScalarString
 import io
 import json
 from pkg_resources import parse_version
 
-
-def get_file(method, file_path, type_of_file):
-    data_dictionary = None
-    with open(os.path.expanduser(file_path), mode="r", encoding="utf8") as f:
-        if file_path.endswith(type_of_file):
-            read_file = f.read()
-            replaced = read_file.replace("simple: =", "simple: '='")
-            # revert str to stream for loader
-            stream = io.StringIO(replaced)
-            try:
-                data_dictionary = method(stream)
-            except Exception as e:
-                click.secho("{} has a structure issue of file type{}. Error was: {}".format(file_path, type_of_file,
-                                                                                            str(e)), fg="red")
-                return {}
-    if type(data_dictionary) is dict:
-        return data_dictionary
-    return {}
-
-
-def get_yaml(file_path):
-    return get_file(yaml.safe_load, file_path, ('yml', 'yaml'))
-
-
-def get_json(file_path):
-    return get_file(json.load, file_path, 'json')
-
+ryaml = YAML()
+ryaml.preserve_quotes = True
+ryaml.width = 50000  # make sure long lines will not break (relevant for code section)
 
 def should_keep_yml_file(yml_content, new_to_version):
     if parse_version(yml_content.get('toversion', '99.99.99')) < parse_version(new_to_version) or \
@@ -91,7 +68,9 @@ def rewrite_json(file_path, json_content, new_to_version):
     json_content['toVersion'] = new_to_version
 
     with open(file_path, 'w') as f:
-        json.dump(json_content, f, indent=4)
+        ujson.dump(json_content, f, indent=4, encode_html_chars=True, escape_forward_slashes=False,
+                   ensure_ascii=False)
+
         print(f" - Updating {file_path}")
 
 
@@ -105,8 +84,8 @@ def rewrite_yml(file_path, yml_content, new_to_version):
         elif yml_content.get('script').get('script') not in ('-', ''):
             yml_content['script']['script'] = FoldedScalarString(yml_content.get('script').get('script'))
 
-    with open(file_path, 'w') as f:
-        YAML().dump(yml_content, f)
+    with open(file_path, mode='w', encoding='utf-8') as f:
+        ryaml.dump(yml_content, f)
         print(f" - Updating {file_path}")
 
 
@@ -115,7 +94,10 @@ def edit_json_content_entity_directory(new_to_version, dir_path):
         file_path = os.path.join(dir_path, file_name)
         if os.path.isfile(file_path) and file_name.endswith('.json') and \
                 file_path != "Packs/NonSupported/IndicatorTypes/reputations.json":
-            json_content = get_json(file_path)
+
+            with open(file_path, 'r') as f:
+                json_content = ujson.load(f)
+
             if should_keep_json_file(json_content, new_to_version):
                 rewrite_json(file_path, json_content, new_to_version)
 
@@ -135,7 +117,9 @@ def edit_scripts_or_integrations_directory(new_to_version, dir_path):
         else:
             yml_file_path = os.path.join(package_path, script_name + '.yml')
 
-        yml_content = get_yaml(yml_file_path)
+        with open(yml_file_path, 'r') as yml_file:
+            yml_content = ryaml.load(yml_file)
+
         if should_keep_yml_file(yml_content, new_to_version):
             rewrite_yml(yml_file_path, yml_content, new_to_version)
 
@@ -151,7 +135,9 @@ def edit_playbooks_directory(new_to_version, dir_path):
 
         if os.path.isfile(file_path):
             if file_path.endswith('.yml'):
-                yml_content = get_yaml(file_path)
+                with open(file_path, 'r') as yml_file:
+                    yml_content = ryaml.load(yml_file)
+
                 if should_keep_yml_file(yml_content, new_to_version):
                     rewrite_yml(file_path, yml_content, new_to_version)
 
@@ -165,7 +151,9 @@ def edit_playbooks_directory(new_to_version, dir_path):
             for inner_file_name in os.listdir(inner_dir_path):
                 file_path = os.path.join(inner_dir_path, inner_file_name)
                 if file_path.endswith('.yml'):
-                    yml_content = get_yaml(file_path)
+                    with open(file_path, 'r') as yml_file:
+                        yml_content = ryaml.load(yml_file)
+
                     if should_keep_yml_file(yml_content, new_to_version):
                         rewrite_yml(file_path, yml_content, new_to_version)
 
@@ -203,7 +191,9 @@ parser.add_argument('-v', '--new-to-version', help='The new to version to assign
 def edit_reputations_json(new_to_version):
     print("Updating reputations.json\n")
     rep_json_path = "Packs/NonSupported/IndicatorTypes/reputations.json"
-    rep_content = get_json(rep_json_path)
+    with open(rep_json_path, 'r') as f:
+        rep_content = ujson.load(f)
+
     for reputation in rep_content.get('reputations', []):
         if parse_version(reputation.get('toVersion', "99.99.99")) > parse_version(new_to_version) > \
                 parse_version(reputation.get("fromVersion", "0.0.0")):
