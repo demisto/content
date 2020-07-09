f6496519fb015bb06fa3d379c8938305f32f02a2
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-07-09 20:23:15 +0300

    lowercase github users

--- .github/workflows/helper_scripts/request_contributor_review.py
+++ .github/workflows/helper_scripts/request_contributor_review.py
@@ -74,7 +74,7 @@ def check_pack_and_request_review(pr_number, github_token=None, verify_ssl=True)
 
             if pack_metadata.get('support') != XSOAR_SUPPORT and PACK_METADATA_GITHUB_USER_FIELD in pack_metadata \
                     and pack_metadata[PACK_METADATA_GITHUB_USER_FIELD]:
-                github_user = pack_metadata[PACK_METADATA_GITHUB_USER_FIELD]
+                github_user = pack_metadata[PACK_METADATA_GITHUB_USER_FIELD].lower()
                 user_exists = check_if_user_exists(github_user=github_user, github_token=github_token,
                                                    verify_ssl=verify_ssl)
 
