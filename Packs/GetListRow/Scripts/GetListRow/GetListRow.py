b0da83de4a49e382b254c79adc379d8fac164dfb
cshayner <cshayner@paloaltonetworks.com>
2020-07-12 13:09:37 +0300

    last code review

--- Packs/GetListRow/Scripts/GetListRow/GetListRow.py
+++ Packs/GetListRow/Scripts/GetListRow/GetListRow.py
@@ -4,7 +4,7 @@ from CommonServerPython import *
 
 def validate_args(parse_all, header, value):
     if parse_all.lower() != 'true' and (not header or not value):
-        return_error("No parse all, and no header and value to know what to parse")
+        return_error("Error: If parse_all is false, both header and value must be specified.")
 
 
 def validate_list_exists(list_result):
@@ -17,56 +17,56 @@ def validate_header_exists(headers, header):
         return_error("Error: The supplied header name not found.")
 
 
-def list_to_headers_and_list_lines(list_result):
-    list_lines = [line.split(',') for line in list_result.split('\n')]
-    headers = list_lines[0]
-    return list_lines[1:], headers
+def list_to_headers_and_lines(list_result):
+    lines_without_headers = [line.split(',') for line in list_result.split('\n')]
+    headers = lines_without_headers[0]
+    return lines_without_headers[1:], headers
 
 
-def list_lines_to_parse(list_lines, headers):
-    all_result = []
+def list_to_context_format(list_lines, headers):
+    result = []
     for line in list_lines:
         dict_result = {headers[item]: line[item] for item in range(len(line))}
-        all_result.append(dict_result)
-    return all_result
+        result.append(dict_result)
+    return result
 
 
-def parse_relevant_rows(headers, list_lines, header, value, context_result, parse_all=False):
+def parse_relevant_rows(headers, lines, header, value, context, parse_all=False):
     if parse_all:
-        all_result = list_lines_to_parse(list_lines, headers)
+        result = list_to_context_format(lines, headers)
     else:
         header_location = headers.index(header)
-        specific_lines_to_parse = [line for line in list_lines if line[header_location] == value]
-        all_result = list_lines_to_parse(specific_lines_to_parse, headers)
-    if all_result:
-        context_result["results"] = all_result
-        markdown = tableToMarkdown('List Result', all_result, headers=headers)
+        specific_lines_to_parse = [line for line in lines if line[header_location] == value]
+        result = list_to_context_format(specific_lines_to_parse, headers)
+    if not result:
         return CommandResults(
-            outputs_prefix='''getListRow.Result(val.header && val.header == obj.header
-                              && val.value == obj.value && val.list_name == obj.list_name)''',
-            outputs=context_result,
-            readable_output=markdown
+            readable_output="No results found"
         )
+    context["results"] = result
+    markdown = tableToMarkdown('List Result', result, headers=headers)
     return CommandResults(
-        readable_output="No results found"
+        outputs_prefix='''getListRow.Result(val.header && val.header == obj.header
+                          && val.value == obj.value && val.list_name == obj.list_name)''',
+        outputs=context,
+        readable_output=markdown
     )
 
 
 def parse_list(parse_all, header, value, list_name):
     validate_args(parse_all, header, value)
     list_result = demisto.executeCommand("getList", {'listName': list_name})[0]['Contents']
-    context_result = {
+    context = {
         "list_name": list_name,
         "header": header,
         "value": value,
     }
     validate_list_exists(list_result)
-    list_lines, headers = list_to_headers_and_list_lines(list_result)
+    lines, headers = list_to_headers_and_lines(list_result)
     if parse_all.lower() == "true":
-        command_results = parse_relevant_rows(headers, list_lines, header, value, context_result, parse_all=True)
+        command_results = parse_relevant_rows(headers, lines, header, value, context, parse_all=True)
     else:
         validate_header_exists(headers, header)
-        command_results = parse_relevant_rows(headers, list_lines, header, value, context_result)
+        command_results = parse_relevant_rows(headers, lines, header, value, context)
     return command_results
 
 
