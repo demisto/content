281055ec2ba0f010830c1b56aa1673ea76cdfff2
bachen <bachen@paloaltonetworks.com>
2020-07-12 13:47:26 +0300

    fix cr

--- Utils/merge_content_new_zip.py
+++ Utils/merge_content_new_zip.py
@@ -1,17 +1,16 @@
 import zipfile as z
-import sys
 import requests
 import os
 from google.cloud import storage
 import google.auth
 import warnings
+import argparse
+
 # disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
-
 ACCEPT_TYPE = "Accept: application/json"
 CONTENT_API_URI = "https://circleci.com/api/v1/project/demisto/content"
-FILES_TO_REMOVE_LIST = ['content-descriptor.json', 'doc-CommonServer.json', 'doc-howto.json']
 ARTIFACTS_PATH = '/home/circleci/project/artifacts/'
 STORAGE_BUCKET_NAME = 'xsoar-ci-artifacts'
 
@@ -44,7 +43,6 @@ def init_storage_client(service_account=None):
 
 
 def http_request(method, url_suffix, params=None):
-
     full_url = CONTENT_API_URI + url_suffix
 
     r = requests.request(
@@ -99,72 +97,108 @@ def get_last_successful_build_num(feature_branch_name):
     return last_successful_build_num
 
 
-def download_zip_file_from_gc(current_feature_content_zip_file_path, extract_destination_path):
+def download_zip_file_from_gcp(current_feature_content_zip_file_path, zip_destination_path,
+                               gcp_service_account=None):
     """Save the content_new.zip file from the feature branch into artifacts folder.
 
     Args:
-        gc_service_account: full path to service account json.
         current_feature_content_zip_file_path (str): Content_new.zip file path in google cloud.
-        extract_destination_path: The folder path to download the content_new.zip file to.
+        zip_destination_path: The folder path to download the content_new.zip file to.
+        gcp_service_account: Full path to service account json.
 
     Returns:
         The new path of the content_new.zip file.
     """
-    storage_client = init_storage_client()
+    storage_client = init_storage_client(gcp_service_account)
     storage_bucket = storage_client.bucket(STORAGE_BUCKET_NAME)
 
     index_blob = storage_bucket.blob(current_feature_content_zip_file_path)
 
-    if not os.path.exists(extract_destination_path):
-        os.mkdir(extract_destination_path)
-    index_blob.download_to_filename(extract_destination_path)
+    if not os.path.exists(zip_destination_path):
+        os.mkdir(zip_destination_path)
+    index_blob.download_to_filename(zip_destination_path)
 
-    if os.path.exists(f'{extract_destination_path}/content_new.zip'):
-        return f'{extract_destination_path}/content_new.zip'
+    if os.path.exists(f'{zip_destination_path}/content_new.zip'):
+        return f'{zip_destination_path}/content_new.zip'
 
     return ''
 
 
-def merge_zip_files(master_branch_content_zip_file_path, feature_branch_content_zip_file_path):
+def merge_zip_files(master_branch_content_zip_file_path, feature_branch_content_zip_file_path, files_to_remove):
     """Merge content_new zip files and remove the unnecessary files.
 
     Args:
-        master_branch_content_zip_file_path (str): Content_new.zip file path
-        feature_branch_content_zip_file_path:
+        master_branch_content_zip_file_path (str): Master content_new.zip file path
+        feature_branch_content_zip_file_path: Feature content_new.zip file path
+        files_to_remove (list): The list of the file to remove from the feature branch's content_new.zip
 
     """
 
     feature_branch_content_dir = z.ZipFile(feature_branch_content_zip_file_path, 'r')
     master_content_zip = z.ZipFile(master_branch_content_zip_file_path, 'w')
-    feature_branch_content_dir.extractall(feature_branch_content_zip_file_path)
-    feature_content_zip_files = os.listdir(feature_branch_content_zip_file_path)
+    extract_dir = f'{ARTIFACTS_PATH}/feature_content_new_dir'
+    feature_branch_content_dir.extractall(extract_dir)
+    feature_content_zip_files = os.listdir(extract_dir)
 
     for file_name in feature_content_zip_files:
-        if file_name not in FILES_TO_REMOVE_LIST:
-            master_content_zip.write(f'{feature_branch_content_dir}/{file_name}')
+        if file_name not in files_to_remove:
+            master_content_zip.write(f'{extract_dir}/{file_name}')
     master_content_zip.close()
+    feature_branch_content_dir.close()
 
 
-def main(argv):
-    if len(argv) < 2:
-        print("")
-        sys.exit(1)
-    feature_branch_name = argv[0]
-    master_content_new_zip_path = argv[1]
-    if len(argv) == 3:
-        gc_service_account = argv[2]
+def get_new_feature_zip_file_path(feature_branch_name, build_num, gcp_service_account):
+    """Merge content_new zip files and remove the unnecessary files.
+
+    Args:
+        feature_branch_name (str): The name of the feature branch.
+        build_num (str): Last successful build number of the feature branch.
+        gcp_service_account: Full path to service account json.
+
+    """
+    current_feature_content_zip_file_path = f'/content/{feature_branch_name}/{build_num}/0/content_new.zip'
+    zip_destination_path = f'{ARTIFACTS_PATH}/feature_content_new_zip'
+    new_feature_content_zip_file_path = download_zip_file_from_gcp(current_feature_content_zip_file_path,
+                                                                   zip_destination_path, gcp_service_account)
+    return new_feature_content_zip_file_path
+
+
+def option_handler():
+    parser = argparse.ArgumentParser(description='Merging two content_new.zip files from different builds.')
+    parser.add_argument('-f', '--feature_branch', help='The name of the feature branch', required=True)
+    parser.add_argument('-g', '--gcp', help='full path to service account json', required=False)
+    parser.add_argument('-v', '--version', help='The version which was entered in content creator. for example 4_1',
+                        required=True)
+
+    options = parser.parse_args()
+
+    return options
+
+
+def main():
+    options = option_handler()
+    feature_branch_name = options.feature_branch
+    gcp_service_account = options.gcp
+    version = options.version
+
     build_num = get_last_successful_build_num(feature_branch_name)
     if not build_num:
         print("Couldn't find successful build in this branch")
-    current_feature_content_zip_file_path = f'/content/{feature_branch_name}/{build_num}/0/content_new.zip'
-    extract_destination_path = f'{ARTIFACTS_PATH}/feature_content_new_zip'
-    new_feature_content_zip_file_path = download_zip_file_from_gc(current_feature_content_zip_file_path, extract_destination_path)
+
+    new_feature_content_zip_file_path = get_new_feature_zip_file_path(feature_branch_name, build_num,
+                                                                      gcp_service_account)
+
+    files_to_remove = [f'content-descriptor_{version}.json', f'doc-CommonServer_{version}.json',
+                       f'doc-howto_{version}.json', f'reputations_{version}.json']
+
+    master_content_zip_path = f"{ARTIFACTS_PATH}/content_new.zip"
+
     if new_feature_content_zip_file_path:
-        merge_zip_files(master_content_new_zip_path, new_feature_content_zip_file_path)
+        merge_zip_files(master_content_zip_path, new_feature_content_zip_file_path, files_to_remove)
+        print('Done merging content_new.zip files')
     else:
-        print('error')
+        print(f'Failed to download content_new.zip from feature branch {feature_branch_name}')
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
-
+    main()
