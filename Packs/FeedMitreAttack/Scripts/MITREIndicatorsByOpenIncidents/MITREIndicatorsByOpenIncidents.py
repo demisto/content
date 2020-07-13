4492169fd27267107b58e58160668ac9da718ab9
mayagoldb <mayag@demisto.com>
2020-07-13 14:58:39 +0300

    ran flake8

--- Packs/FeedMitreAttack/Scripts/MITREIndicatorsByOpenIncidents/MITREIndicatorsByOpenIncidents.py
+++ Packs/FeedMitreAttack/Scripts/MITREIndicatorsByOpenIncidents/MITREIndicatorsByOpenIncidents.py
@@ -1,22 +1,29 @@
 import demistomock as demisto
 from CommonServerPython import *
 
-res = demisto.executeCommand("demisto-api-post", { "uri": "statistics/widgets/query", "body":
-    json.dumps({"dataType": "indicators",
-                "widgetType": "bar",
-                "query": "type:\"MITRE ATT\u0026CK\" and investigationsCount:\u003e0","params": {
-                    "groupBy": [
-                        "name"
-                    ],
-                    "keys": [
-                        "sum|investigationsCount"
-                    ]
-                }  }) })
+res = demisto.executeCommand("demisto-api-post",
+                             {
+                                 "uri": "statistics/widgets/query",
+                                 "body":
+                                     json.dumps(
+                                         {"dataType": "indicators",
+                                          "widgetType": "bar",
+                                          "query": "type:\"MITRE ATT\u0026CK\" and investigationsCount:\u003e0",
+                                          "params": {
+                                              "groupBy": [
+                                                  "name"
+                                              ],
+                                              "keys": [
+                                                  "sum|investigationsCount"
+                                              ]
+                                          }
+                                          })
+                             })
 
 indicators = []
 for v in res[0]['Contents']['response']:
     value = v["name"]
-    indicator_res = demisto.executeCommand("getIndicator", {"value":value})[0]['Contents']
+    indicator_res = demisto.executeCommand("getIndicator", {"value": value})[0]['Contents']
     for ind in indicator_res:
         indicators.append({
             'Value': ind['value'],
