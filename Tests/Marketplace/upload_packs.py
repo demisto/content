84a1089c2c06fb6e35ad21613b89ea302b3da76b
guykeller <g12k34ppp>
2020-07-14 19:13:36 +0300

    added debugging print + ls and pwd

--- Tests/Marketplace/upload_packs.py
+++ Tests/Marketplace/upload_packs.py
@@ -621,6 +621,8 @@ def main():
     index_folder_path, index_blob = download_and_extract_index(default_storage_bucket, extract_destination_path)
     print_error(index_folder_path)
 
+    print_error(f"ls: {subprocess.check_output('ls')}")
+    print_error(f"pwd: {subprocess.check_output('pwd')}")
     if private_bucket_name:  # Add private packs to the index
         # TODO : maybe remove this line - private_storage_bucket = storage_client.bucket(private_bucket_name)
         private_packs, private_index_path, private_index_blob = update_index_with_priced_packs(private_storage_bucket,
