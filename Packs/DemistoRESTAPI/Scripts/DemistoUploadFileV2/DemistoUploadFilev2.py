a7aa8655f513afd94b701e9fa48787f1abaa3251
bar katzir <bkatzir@paloaltonetworks.com>
2020-07-14 13:16:50 +0300

    rm comments

--- Packs/DemistoRESTAPI/Scripts/DemistoUploadFileV2/DemistoUploadFilev2.py
+++ Packs/DemistoRESTAPI/Scripts/DemistoUploadFileV2/DemistoUploadFilev2.py
@@ -3,46 +3,6 @@ from typing import Tuple
 from CommonServerPython import *
 
 
-# res = executeCommand("demisto-api-multipart",
-#                      {"uri": 'entry/upload/' + args.incidentID, "entryID": args.entryID, "body": args.body});
-# if (isError(res[0])) {
-# return res;
-# }
-# var
-# entryId = dq(res, 'Contents.response.entries.id');
-#
-# var
-# md = 'File uploaded successfully. Entry ID is ' + entryId;
-# if (args.body)
-# {
-# md += '. Comment is:' + args.body;
-# }
-#
-# return {
-#     ContentsFormat: formats.json,
-#     Type: entryTypes.note,
-#     Contents: res,
-#     HumanReadable: md
-# };
-
-# args = demisto.args()
-# incident_id = args.get('incident_id')
-# entry_id = args.get('entryID')
-# body = args.get('body', None)
-#
-# response = demisto.executeCommand("demisto-api-multipart",
-#                                   {"uri": "incident/upload/{}".format(incident_id), "entryID": entry_id, "body": body})[
-#     0]
-# if isError(response):
-#     demisto.results({"Type": entryTypes["error"], "ContentsFormat": formats["text"],
-#                      "Contents": "There was an issue uploading file.  Check API key and input arguments."})
-# else:
-#     if body:
-#         demisto.results("Successfully uploaded file to incident. Comment is:" + body)
-#     else:
-#         demisto.results("Successfully uploaded file to incident")
-
-
 def upload_file(incident_id: str, entry_id: str, body: str = ''):
     return demisto.executeCommand("demisto-api-multipart",
                                   {"uri": f'entry/upload/{incident_id}', "entryID": entry_id, "body": body})
