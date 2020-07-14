099b908868d7b80bd8c54dee79de899ff0248f9c
IkaDemisto <igabashvili@paloaltonetworks.com>
2020-07-14 13:53:38 +0300

    additional comment changes

--- .github/workflows/helper_scripts/request_contributor_review.py
+++ .github/workflows/helper_scripts/request_contributor_review.py
@@ -71,14 +71,15 @@ def tag_user_on_pr(reviewers, pr_number, github_token=None, verify_ssl=True):
     review_endpoint = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/issues/{pr_number}/comments"
     headers = {"Authorization": "Bearer " + github_token} if github_token else {}
 
-    reviewers_data = {
-        "reviewers": list(reviewers)
+    comment = "\n".join({f"- @{r}" for r in reviewers})
+    comment_body = {
+        "body": comment
     }
 
-    response = requests.post(review_endpoint, json=reviewers_data, headers=headers, verify=verify_ssl)
+    response = requests.post(review_endpoint, headers=headers, verify=verify_ssl, json=comment_body)
 
     if response.status_code not in [200, 201]:
-        print(f"Failed requesting review on PR {pr_number}")
+        print(f"Failed posting comment on PR {pr_number}")
         sys.exit(1)
 
 
