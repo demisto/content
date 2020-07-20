2407d696e7f4e83780fb5be8f2282589e9f23ae5
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-20 15:45:47 +0100

    Made the test in function

--- Packs/CommonScripts/Scripts/ModifyDateTime/ModifyDateTime_test.py
+++ Packs/CommonScripts/Scripts/ModifyDateTime/ModifyDateTime_test.py
@@ -1,7 +1,9 @@
 from ModifyDateTime import apply_variation
-args = {
-    'value': '2020/01/01',
-    'variation': 'in 1 day'
-}
-results = apply_variation(args.get('value'), args.get('variation'))
-assert results == '2020-01-02T00:00:00'
+
+def test_apply_variation():
+    args = {
+        'value': '2020/01/01',
+        'variation': 'in 1 day'
+    }
+    results = apply_variation(args.get('value'), args.get('variation'))
+    assert results == '2020-01-02T00:00:00'
