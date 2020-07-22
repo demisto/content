88e292c871cde01648ec90fa3b968619934733be
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-22 09:47:47 +0100

    Updated the script to support JSON dictionaries and lists.

--- Packs/MapValuesTransformer/Scripts/MapValuesTransformer/MapValuesTransformer.py
+++ Packs/MapValuesTransformer/Scripts/MapValuesTransformer/MapValuesTransformer.py
@@ -1,36 +1,43 @@
-import demistomock as demisto
-from CommonServerPython import *
 args = demisto.args()
-
-# Get the input value
 value = args.get('value')
-
-# Get the input array (CSV)
 input_values = args.get('input_values', []).split(",")
-
-# Get the mapped array (CSV)
 mapped_values = args.get('mapped_values', []).split(",")
+try:
+    value = json.loads(value)
+except Exception:
+    pass
 
-# Convert the input value to a string
-value = str(value)
+# Convert all to string
+value = str(value) if type(value) not in [dict, list] else value
+input_values [:] = [str(x) for x in input_values]
+mapped_values [:] = [str(x) for x in mapped_values]
 
-# Convert the array items to strings
-input_values[:] = [str(x) for x in input_values]
-mapped_values[:] = [str(x) for x in mapped_values]
+# If the provided input_value and mapper_values are not equal in length
+# then return an error
 
-# If the length of the arrays are not equal, return an error
 if len(input_values) != len(mapped_values):
     return_error('Length of input_values and mapped_values are not the same')
 
-# Create the dictionary of input and mapped values
+# Create a dictionary to look up values against
 mapper = dict()
 for index in range(0, len(input_values)):
     mapper[input_values[index]] = mapped_values[index]
 
-# If the value exists in the dictionary, return its value
-if value in mapper:
-    demisto.results(mapper[value])
 
-# Otherwise return the original value
-else:
-    demisto.results(value)
+# If the input is a dictionary then attempt to convert each
+# "key: value" as a string into the value in the mapper
+if type(value) == dict:
+    new_dict = dict()
+    for k, v in value.items():
+        key_value = f"{k}: {v}"
+        new_dict[k] = mapper[key_value] if key_value in mapper else v
+    value = json.dumps(new_dict)
+
+elif type(value) == list:
+    for val in value:
+        val = mapper[val] if val in mapper else val
+
+elif value in mapper:
+    value = mapper[value]
+
+demisto.results(value)
