012f4fde7daad40ebf6e7cddc471b1b63e6772b4
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-24 17:25:10 +0100

    CHanged code to allow for lint tests

--- Packs/MapValuesTransformer/Scripts/MapValuesTransformer/MapValuesTransformer.py
+++ Packs/MapValuesTransformer/Scripts/MapValuesTransformer/MapValuesTransformer.py
@@ -4,46 +4,54 @@ from CommonServerUserPython import *
 import json
 
 
-args = demisto.args()
-value = args.get('value')
-input_values = args.get('input_values', []).split(",")
-mapped_values = args.get('mapped_values', []).split(",")
-try:
-    value = json.loads(value)
-except Exception:
-    pass
+def mapValues(value, input_values, mapped_values):
+    # Convert all to string
+    value = str(value) if type(value) not in [dict, list] else value
+    input_values[:] = [str(x) for x in input_values]
+    mapped_values[:] = [str(x) for x in mapped_values]
 
-# Convert all to string
-value = str(value) if type(value) not in [dict, list] else value
-input_values[:] = [str(x) for x in input_values]
-mapped_values[:] = [str(x) for x in mapped_values]
+    # If the provided input_value and mapper_values are not equal in length
+    # then return an error
 
-# If the provided input_value and mapper_values are not equal in length
-# then return an error
+    if len(input_values) != len(mapped_values):
+        return_error('Length of input_values and mapped_values are not the same')
 
-if len(input_values) != len(mapped_values):
-    return_error('Length of input_values and mapped_values are not the same')
+    # Create a dictionary to look up values against
+    mapper = dict()
+    for index in range(0, len(input_values)):
+        mapper[input_values[index]] = mapped_values[index]
 
-# Create a dictionary to look up values against
-mapper = dict()
-for index in range(0, len(input_values)):
-    mapper[input_values[index]] = mapped_values[index]
+    # If the input is a dictionary then attempt to convert each
+    # "key: value" as a string into the value in the mapper
+    if type(value) == dict:
+        new_dict = dict()
+        for k, v in value.items():
+            key_value = f"{k}: {v}"
+            new_dict[k] = mapper[key_value] if key_value in mapper else v
+        value = json.dumps(new_dict)
+
+    elif type(value) == list:
+        for val in value:
+            val = mapper[val] if val in mapper else val
+
+    elif value in mapper:
+        value = mapper[value]
+
+    return value
 
 
-# If the input is a dictionary then attempt to convert each
-# "key: value" as a string into the value in the mapper
-if type(value) == dict:
-    new_dict = dict()
-    for k, v in value.items():
-        key_value = f"{k}: {v}"
-        new_dict[k] = mapper[key_value] if key_value in mapper else v
-    value = json.dumps(new_dict)
+def main():
+    args = demisto.args()
+    value = args.get('value')
+    input_values = args.get('input_values', []).split(",")
+    mapped_values = args.get('mapped_values', []).split(",")
+    try:
+        value = json.loads(value)
+    except Exception:
+        pass
 
-elif type(value) == list:
-    for val in value:
-        val = mapper[val] if val in mapper else val
+    value = mapValues(value, input_values, mapped_values)
+    demisto.results(value)
 
-elif value in mapper:
-    value = mapper[value]
-
-demisto.results(value)
+if __name__ in ('__main__', 'builtin', 'builtins'):
+    main()
