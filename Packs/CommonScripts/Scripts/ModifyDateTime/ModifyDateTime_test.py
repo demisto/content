5def8cf43561ed57796150d73df219535daed0a5
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-24 10:45:40 +0100

    Modified test script to input a datetime object rather than string.

--- Packs/CommonScripts/Scripts/ModifyDateTime/ModifyDateTime_test.py
+++ Packs/CommonScripts/Scripts/ModifyDateTime/ModifyDateTime_test.py
@@ -7,4 +7,4 @@ def test_apply_variation():
         'variation': 'in 1 day'
     }
     results = apply_variation(args.get('value'), args.get('variation'))
-    assert results == '2020-01-02T00:00:00'
+    assert results == (dateparser.parse('2020-01-02T00:00:00'))
