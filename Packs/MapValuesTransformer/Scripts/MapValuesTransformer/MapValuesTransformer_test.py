14a40fadec434877ccf2209c7dfeb0dcc92e08d3
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-24 17:28:50 +0100

    Added test python file

---
+++ Packs/MapValuesTransformer/Scripts/MapValuesTransformer/MapValuesTransformer_test.py
@@ -0,0 +1,11 @@
+from MapValuesTransformer import mapValues
+
+
+def test_apply_tranformer():
+    args = {
+        'value': '3',
+        'input_values': '1,2,3,4',
+        'mapped_values': '4,3,2,1'
+    }
+    results = mapValues(args.get('value'), args.get('input_values'), args.get('mapped_values'))
+    assert results == '2'
