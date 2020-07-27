88c9f1ddb7194e8003e6477bd65d28e3e851e703
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-27 10:55:36 +0100

    Supported handling of space or not space in input_values and updated unit tests

--- Packs/CommonScripts/Scripts/MapValuesTransformer/MapValuesTransformer_test.py
+++ Packs/CommonScripts/Scripts/MapValuesTransformer/MapValuesTransformer_test.py
@@ -1,17 +1,35 @@
-from MapValuesTransformer import mapValues
+from MapValuesTransformer import mapvalues
 
 
-def test_mapvalues():
-
+def test_mapvalues_dict():
     value = {
         "testkey1": "testvalue1",
         "testkey2": "testvalue2"
     }
     input_v = "testkey2: testvalue2"
     mapped_v = "testvalue2changed"
-    assert mapValues(value, input_v, mapped_v) == '{"testkey1": "testvalue1", "testkey2": "testvalue2changed"}'
+    assert mapvalues(value, input_v, mapped_v) == '{"testkey1": "testvalue1", "testkey2": "testvalue2changed"}'
 
+
+def test_mapvalues_str():
     value = "3"
     input_v = "4,3,2,1"
     mapped_v = "1,2,3,4"
-    assert mapValues(value, input_v, mapped_v) == "2"
+    assert mapvalues(value, input_v, mapped_v) == "2"
+
+
+def test_mapvalues_int():
+    value = 3
+    input_v = "4,3,2,1"
+    mapped_v = "1,2,3,4"
+    assert mapvalues(value, input_v, mapped_v) == "2"
+
+
+def test_mapvalues_dict_abnorm():
+    value = {
+        "testkey1": "testvalue1",
+        "testkey2": "testvalue2"
+    }
+    input_v = "testkey2:testvalue2"
+    mapped_v = "testvalue2changed"
+    assert mapvalues(value, input_v, mapped_v) == '{"testkey1": "testvalue1", "testkey2": "testvalue2changed"}'
