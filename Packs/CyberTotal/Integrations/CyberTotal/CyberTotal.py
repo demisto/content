215b63eeb2e153261194f3bf4568f0bee7306d6d
bpyliu <bpyliu@gmail.com>
2020-07-28 15:35:20 +0800

    Update Packs/CyberTotal/Integrations/CyberTotal/CyberTotal.py
    Co-authored-by: hod <halpert@paloaltonetworks.com>

--- Packs/CyberTotal/Integrations/CyberTotal/CyberTotal.py
+++ Packs/CyberTotal/Integrations/CyberTotal/CyberTotal.py
@@ -252,7 +252,7 @@ class Client(BaseClient):
 
         cybertotal_result = self._http_request(
             method='GET',
-            url_suffix='/_api/search/ip/whois/{}'.format(ip)
+            url_suffix=f'/_api/search/ip/whois/{ip}'
         )
         if 'task_state' in cybertotal_result:
             return {'task_state': cybertotal_result['task_state'], 'message': 'this search is in progress, try again later...'}
