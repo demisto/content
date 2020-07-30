d4b37b38bfe548bd7bb0ef2c42d3788261746b1c
santosh.giri <santosh.giri@crestdatasys.com>
2020-07-30 20:30:18 +0530

    fix  - mypy error

--- Packs/PassiveTotal/Scripts/RiskIQPassiveTotalSSLScript/RiskIQPassiveTotalSSLScript.py
+++ Packs/PassiveTotal/Scripts/RiskIQPassiveTotalSSLScript/RiskIQPassiveTotalSSLScript.py
@@ -2,11 +2,11 @@ from CommonServerPython import *
 
 indicator_value = demisto.args().get('indicator_value')
 
-sha1Regex = r'\b[a-fA-F\d]{40}\b'
+sha1Regex = re.compile(r'\b[a-fA-F\d]{40}\b')
 indicator_type = 'serialNumber'
 if re.match(emailRegex, indicator_value):
     indicator_type = 'subjectEmailAddress'
-if re.match(sha1Regex, indicator_value):
+if sha1Regex.match(indicator_value):
     indicator_type = 'sha1'
 
 result = demisto.executeCommand('pt-ssl-cert-search', {'field': indicator_type, 'query': indicator_value})
