eecef74e83e882c792484627f7ba15c317ce13cc
Adam Burt - Demisto <aburt-demisto@users.noreply.github.com>
2020-07-31 10:48:38 +0100

    Fixed flake8 errors

--- Packs/CommonScripts/Scripts/Jmespath/jmespath_test.py
+++ Packs/CommonScripts/Scripts/Jmespath/jmespath_test.py
@@ -1,18 +1,19 @@
 from jmespath_transformer import jmespath_search
 
+
 def test_search():
     expression = "AutoFocus.IP[?IndicatorType=='IPV4_ADDRESS'].{Value: IndicatorValue, Type: IndicatorType}"
     value = {
-              "AutoFocus": {
-                "IP": [{
-                  "IndicatorType": "IPV4_ADDRESS",
-                  "IndicatorValue": "1.1.1.1",
-                }, {
-                  "IndicatorType": "IPV4_ADDRESS",
-                  "IndicatorValue": "2.2.2.2",
-                }]
-              }
-            }
+        "AutoFocus": {
+            "IP": [{
+                "IndicatorType": "IPV4_ADDRESS",
+                "IndicatorValue": "1.1.1.1",
+            }, {
+                "IndicatorType": "IPV4_ADDRESS",
+                "IndicatorValue": "2.2.2.2",
+            }]
+        }
+    }
     result = jmespath_search(expression, value)
     assert result == [
         {
