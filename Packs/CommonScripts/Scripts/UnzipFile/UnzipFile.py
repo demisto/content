108dc170513bed28fd5217abf5eac2a4979727b4
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-08-04 11:52:31 +0300

    Docstring for version check rationale

--- Packs/CommonScripts/Scripts/UnzipFile/UnzipFile.py
+++ Packs/CommonScripts/Scripts/UnzipFile/UnzipFile.py
@@ -23,6 +23,7 @@ def get_zip_path(args):
         for entry in entries:
             fn = demisto.get(entry, 'File')
 
+            # We check the python version to prevent encoding issues.
             if sys.version_info > (3, 0):
                 is_text = type(fn) is str
             else:
@@ -86,6 +87,7 @@ def extract(file_info, dir_path, password=None):
     excluded_files = [f for f in os.listdir('.') if isfile(f)]
     excluded_dirs = [d for d in os.listdir('.') if isdir(d)]
     # extracting the zip file
+    # We check the python version to ensure the docker image contains the necessary packages
     if '.rar' in file_name and sys.version_info > (3, 0):
         if password:
             cmd = 'unrar x -p {} {} {}'.format(password, file_path, dir_path)
