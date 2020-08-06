432efa57023d620852faeaf1415ea5fa895185b7
bachen <bachen@paloaltonetworks.com>
2020-08-06 10:34:34 +0300

    update collect_tests_and_content_packs

--- Tests/scripts/collect_tests_and_content_packs.py
+++ Tests/scripts/collect_tests_and_content_packs.py
@@ -930,7 +930,7 @@ def extract_matching_object_from_id_set(obj_id, obj_set, server_version='0'):
 def get_test_from_conf(branch_name, conf=deepcopy(CONF)):
     tests = set([])
     changed = set([])
-    change_string = tools.run_command("git diff origin/master...{} Tests/conf.json".format(branch_name))
+    change_string = tools.run_command("git diff origin/new_freeze_4_1...{} Tests/conf.json".format(branch_name))
     added_groups = re.findall(r'(\+[ ]+")(.*)(":)', change_string)
     if added_groups:
         for group in added_groups:
@@ -1240,8 +1240,8 @@ def create_test_file(is_nightly, skip_save=False):
         branch_name = branch_name_reg.group(1)
 
         logging.info("Getting changed files from the branch: {0}".format(branch_name))
-        if branch_name != 'master':
-            files_string = tools.run_command("git diff --name-status origin/master...{0}".format(branch_name))
+        if branch_name != 'new_freeze_4_1':
+            files_string = tools.run_command("git diff --name-status origin/new_freeze_4_1...{0}".format(branch_name))
             # Checks if the build is for contributor PR and if so add it's pack.
             if os.getenv('CONTRIB_BRANCH'):
                 packs_diff = tools.run_command("git diff --name-status HEAD -- Packs")
