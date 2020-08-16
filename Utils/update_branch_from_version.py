88ee4f72690edf9fcce0b7e4e6ca5f797eea8af1
rsagi <rsagi@paloaltonetworks.com>
2020-08-16 18:16:26 +0300

    update master branch script (and minor fixes to old branch script)

--- Utils/update_branch_from_version.py
+++ Utils/update_branch_from_version.py
@@ -13,17 +13,17 @@ ryaml.preserve_quotes = True
 ryaml.width = 50000  # make sure long lines will not break (relevant for code section)
 
 
-def should_keep_yml_file(yml_content, new_to_version):
-    if parse_version(yml_content.get('toversion', '99.99.99')) < parse_version(new_to_version) or \
-            parse_version(yml_content.get('fromversion', '0.0.0')) >= parse_version(new_to_version):
+def should_keep_yml_file(yml_content, new_from_version):
+    # if the file's toversion is lower than the new from version we should delete it
+    if parse_version(yml_content.get('toversion', '99.99.99')) < parse_version(new_from_version):
         return False
 
     return True
 
 
-def should_keep_json_file(json_content, new_to_version):
-    if parse_version(json_content.get('toVersion', '99.99.99')) < parse_version(new_to_version) or \
-            parse_version(json_content.get('fromVersion', '0.0.0')) >= parse_version(new_to_version):
+def should_keep_json_file(json_content, new_from_version):
+    # if the file's toVersion is lower than the new from version we should delete it
+    if parse_version(json_content.get('toVersion', '99.99.99')) < parse_version(new_from_version):
         return False
 
     return True
@@ -66,52 +66,56 @@ def delete_json(file_path):
         os.remove(changelog_file)
 
 
-def rewrite_json(file_path, json_content, new_to_version):
-    json_content['toVersion'] = new_to_version
+def rewrite_json(file_path, json_content, new_from_version):
+    # if there is no fromVersion in json or the fromVersion is lower than the new from version then update
+    if ('fromVersion' in json_content and parse_version(json_content.get('fromVersion')) < new_from_version) \
+            or ('fromVersion' not in json_content):
+        json_content['toVersion'] = new_from_version
 
-    with open(file_path, 'w') as f:
-        ujson.dump(json_content, f, indent=4, encode_html_chars=True, escape_forward_slashes=False,
-                   ensure_ascii=False)
+        with open(file_path, 'w') as f:
+            ujson.dump(json_content, f, indent=4, encode_html_chars=True, escape_forward_slashes=False,
+                       ensure_ascii=False)
 
-        print(f" - Updating {file_path}")
-
-
-def rewrite_yml(file_path, yml_content, new_to_version):
-    yml_content['toversion'] = new_to_version
-
-    check_dockerimage45(yml_content, new_to_version)
-
-    if 'script' in yml_content:
-        if isinstance(yml_content.get('script'), str):
-            if yml_content.get('script') not in ('-', ''):
-                yml_content['script'] = FoldedScalarString(yml_content.get('script'))
-
-        elif yml_content.get('script').get('script') not in ('-', ''):
-            yml_content['script']['script'] = FoldedScalarString(yml_content.get('script').get('script'))
-
-    # resetting the tests associated with each yml
-    yml_content['tests'] = ['No test']
-
-    with open(file_path, mode='w', encoding='utf-8') as f:
-        ryaml.dump(yml_content, f)
-        print(f" - Updating {file_path}")
+            print(f" - Updating {file_path}")
 
 
-def check_dockerimage45(yml_content, new_to_version):
+def rewrite_yml(file_path, yml_content, new_from_version):
+    # if there is no fromVersion in json or the fromversion is lower than the new from version then update
+    if ('fromversion' in yml_content and parse_version(yml_content.get('fromversion')) < new_from_version) \
+            or ('fromversion' not in yml_content):
+        yml_content['toversion'] = new_from_version
+
+        check_dockerimage45(yml_content, new_from_version)
+
+        if 'script' in yml_content:
+            if isinstance(yml_content.get('script'), str):
+                if yml_content.get('script') not in ('-', ''):
+                    yml_content['script'] = FoldedScalarString(yml_content.get('script'))
+
+            elif yml_content.get('script').get('script') not in ('-', ''):
+                yml_content['script']['script'] = FoldedScalarString(yml_content.get('script').get('script'))
+
+        # resetting the tests associated with each yml
+        yml_content['tests'] = ['No test']
+
+        with open(file_path, mode='w', encoding='utf-8') as f:
+            ryaml.dump(yml_content, f)
+            print(f" - Updating {file_path}")
+
+
+def check_dockerimage45(yml_content, new_from_version):
     # check in scripts
     if 'dockerimage45' in yml_content:
-        if parse_version(new_to_version) <= parse_version('4.5.9'):
-            yml_content['dockerimage'] = yml_content['dockerimage45']
-        del yml_content['dockerimage45']
+        if parse_version(new_from_version) > parse_version('4.5.9'):
+            del yml_content['dockerimage45']
 
     # check in integrations
     elif 'dockerimage45' in yml_content.get('script', {}):
-        if parse_version(new_to_version) <= parse_version('4.5.9'):
-            yml_content['script']['dockerimage'] = yml_content['script']['dockerimage45']
-        del yml_content['script']['dockerimage45']
+        if parse_version(new_from_version) > parse_version('4.5.9'):
+            del yml_content['script']['dockerimage45']
 
 
-def edit_json_content_entity_directory(new_to_version, dir_path):
+def edit_json_content_entity_directory(new_from_version, dir_path):
     for file_name in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file_name)
         if os.path.isfile(file_path) and file_name.endswith('.json') and \
@@ -120,14 +124,14 @@ def edit_json_content_entity_directory(new_to_version, dir_path):
             with open(file_path, 'r') as f:
                 json_content = ujson.load(f)
 
-            if should_keep_json_file(json_content, new_to_version):
-                rewrite_json(file_path, json_content, new_to_version)
+            if should_keep_json_file(json_content, new_from_version):
+                rewrite_json(file_path, json_content, new_from_version)
 
             else:
                 delete_json(file_path)
 
 
-def edit_scripts_or_integrations_directory(new_to_version, dir_path):
+def edit_scripts_or_integrations_directory(new_from_version, dir_path):
     for script_name in os.listdir(dir_path):
         package_path = os.path.join(dir_path, script_name)
         if package_path.endswith('.md'):
@@ -144,14 +148,14 @@ def edit_scripts_or_integrations_directory(new_to_version, dir_path):
             with open(yml_file_path, 'r') as yml_file:
                 yml_content = ryaml.load(yml_file)
 
-            if should_keep_yml_file(yml_content, new_to_version):
-                rewrite_yml(yml_file_path, yml_content, new_to_version)
+            if should_keep_yml_file(yml_content, new_from_version):
+                rewrite_yml(yml_file_path, yml_content, new_from_version)
 
             else:
                 delete_script_or_integration(package_path)
 
 
-def edit_playbooks_directory(new_to_version, dir_path):
+def edit_playbooks_directory(new_from_version, dir_path):
     for file_name in os.listdir(dir_path):
         file_path = os.path.join(dir_path, file_name)
         if file_path.endswith('md'):
@@ -162,8 +166,8 @@ def edit_playbooks_directory(new_to_version, dir_path):
                 with open(file_path, 'r') as yml_file:
                     yml_content = ryaml.load(yml_file)
 
-                if should_keep_yml_file(yml_content, new_to_version):
-                    rewrite_yml(file_path, yml_content, new_to_version)
+                if should_keep_yml_file(yml_content, new_from_version):
+                    rewrite_yml(file_path, yml_content, new_from_version)
 
                 else:
                     delete_playbook(file_path)
@@ -178,68 +182,52 @@ def edit_playbooks_directory(new_to_version, dir_path):
                     with open(file_path, 'r') as yml_file:
                         yml_content = ryaml.load(yml_file)
 
-                    if should_keep_yml_file(yml_content, new_to_version):
-                        rewrite_yml(file_path, yml_content, new_to_version)
+                    if should_keep_yml_file(yml_content, new_from_version):
+                        rewrite_yml(file_path, yml_content, new_from_version)
 
                     else:
                         delete_playbook(file_path)
 
 
-def edit_pack(new_to_version, pack_name):
+def edit_pack(new_from_version, pack_name):
     pack_path = os.path.join('Packs', pack_name)
     click.secho(f"Starting process for {pack_path}:")
     for content_dir in os.listdir(pack_path):
         dir_path = os.path.join(pack_path, content_dir)
         if content_dir in ['Playbooks', 'TestPlaybooks']:
-            edit_playbooks_directory(new_to_version, dir_path)
+            edit_playbooks_directory(new_from_version, dir_path)
 
         elif content_dir in ['Scripts', 'Integrations']:
-            edit_scripts_or_integrations_directory(new_to_version, dir_path)
+            edit_scripts_or_integrations_directory(new_from_version, dir_path)
 
         elif content_dir in ['IncidentFields', 'IncidentTypes', 'IndicatorFields', 'Layouts', 'Classifiers',
                              'Connections', 'Dashboards', 'IndicatorTypes', 'Reports', 'Widgets']:
-            edit_json_content_entity_directory(new_to_version, dir_path)
+            edit_json_content_entity_directory(new_from_version, dir_path)
 
     click.secho(f"Finished process for {pack_path}\n")
 
 
-def edit_all_packs(new_to_version):
+def edit_all_packs(new_from_version):
     for pack_name in os.listdir('Packs'):
-        edit_pack(new_to_version, pack_name)
+        edit_pack(new_from_version, pack_name)
 
 
-parser = argparse.ArgumentParser("Alter the branch to assign a new toVersion to all relevant files.")
-parser.add_argument('-v', '--new-to-version', help='The new to version to assign.', required=True)
-
-
-def edit_reputations_json(new_to_version):
-    print("Updating reputations.json\n")
-    rep_json_path = "Packs/NonSupported/IndicatorTypes/reputations.json"
-    with open(rep_json_path, 'r') as f:
-        rep_content = ujson.load(f)
-
-    for reputation in rep_content.get('reputations', []):
-        if parse_version(reputation.get('toVersion', "99.99.99")) > parse_version(new_to_version):
-            reputation['toVersion'] = new_to_version
-
-    with open(rep_json_path, 'w') as f:
-        ujson.dump(rep_content, f, indent=4, ensure_ascii=True)
+parser = argparse.ArgumentParser("Alter the branch to assign a new fromVersion to all relevant files.")
+parser.add_argument('-v', '--new-from-version', help='The new from version to assign.', required=True)
 
 
 def main():
-    new_to_version = parser.parse_args().new_to_version
-    if new_to_version.count('.') == 1:
-        new_to_version = new_to_version + ".9"
+    new_from_version = parser.parse_args().new_from_version
+    if new_from_version.count('.') == 1:
+        new_from_version = new_from_version + ".0"
 
     click.secho("Starting Branch Editing")
-    edit_all_packs(new_to_version)
-
-    edit_reputations_json(new_to_version)
+    edit_all_packs(new_from_version)
 
     click.secho("Deleting empty directories\n")
     subprocess.call(["find", "Packs", "-type", "d", "-empty", "-delete"])
 
-    click.secho("Finished creating branch", fg="green")
+    click.secho("Finished updating branch", fg="green")
 
 
 if __name__ == "__main__":
