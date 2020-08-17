b2502b49f037a467067d839f4da6ff38609633ef
rsagi <rsagi@paloaltonetworks.com>
2020-08-17 17:59:18 +0300

    adding time control to LargestInvestigationsWidget

--- Packs/CommonWidgets/Scripts/LargestInvestigationsWidget/LargestInvestigationsWidget.py
+++ Packs/CommonWidgets/Scripts/LargestInvestigationsWidget/LargestInvestigationsWidget.py
@@ -2,18 +2,20 @@ import traceback
 from typing import List
 from operator import itemgetter
 from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
 
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
 
-def get_investigations(raw_output, investigations):
+def get_investigations(raw_output, investigations, db_name):
     for db in raw_output[0].get('Contents'):
         buckets = db.get('buckets')
         for entry in buckets.keys():
             if entry.startswith('investigations-'):
                 investigations[entry] = buckets.get(entry)
+                investigations[entry].update({"Date": db_name})
 
 
 def parse_investigations_to_table(investigations):
@@ -22,25 +24,36 @@ def parse_investigations_to_table(investigations):
     for investigation in investigations.keys():
         size = investigations[investigation].get('leafSize').split(' ')
         if float(size[0]) >= 1.0 and size[1] == 'MB':
+            db_name = investigations[investigation].get('Date')
             data.append({
                 "IncidentID": investigation.split('-')[1],
                 "Size": investigations[investigation].get('leafSize'),
-                "AmountOfEntries": investigations[investigation].get('keyN')
+                "AmountOfEntries": investigations[investigation].get('keyN'),
+                "Date": db_name[:2] + "-" + db_name[2:]
             })
 
     widget_table['data'] = sorted(data, key=itemgetter('Size'), reverse=True)  # type: ignore
 
     return widget_table
 
-def
+
+def get_month_db_from_date(date):
+    month = date.strftime('%m')
+    year = date.strftime('%Y')
+    return month + year
+
 
 def get_month_database_names():
-    db_names = []
-    from_date = parse(demisto.args().get('fromDate'))
-    to_date = parse(demisto.args().get('toDate'))
-    current_month = datetime.now().strftime('%m')
-    current_year = datetime.now().strftime('%Y')
-    return current_month + current_year
+    db_names = set()
+    from_date = parse(demisto.args().get('fromDate')) if demisto.args().get('fromDate') else datetime.now()
+    to_date = parse(demisto.args().get('toDate')) if demisto.args().get('toDate') else datetime.now()
+    current = from_date
+    while current < to_date:
+        db_names.add(get_month_db_from_date(current))
+        current = current + relativedelta(months=1)
+
+    db_names.add(get_month_db_from_date(to_date))
+    return db_names
 
 
 def main():
@@ -48,7 +61,7 @@ def main():
         investigations = {}
         for db_name in get_month_database_names():
             raw_output = demisto.executeCommand('getDBStatistics', args={"filter": db_name})
-            get_investigations(raw_output, investigations)
+            get_investigations(raw_output, investigations, db_name)
         demisto.results(parse_investigations_to_table(investigations))
     except Exception:
         demisto.error(traceback.format_exc())  # print the traceback
