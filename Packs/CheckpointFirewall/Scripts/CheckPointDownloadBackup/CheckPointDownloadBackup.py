b8e00ebf130cdeef7034fcb4cc5dfdaf439bd6a1
Lior-Dahan-qm <liord@qmasters.co>
2020-08-18 12:24:33 +0300

    lint code

--- Packs/CheckpointFirewall/Scripts/CheckPointDownloadBackup/CheckPointDownloadBackup.py
+++ Packs/CheckpointFirewall/Scripts/CheckPointDownloadBackup/CheckPointDownloadBackup.py
@@ -19,18 +19,21 @@ try:
     demisto.info("Starting backup")
     creation_status = demisto.executeCommand('CheckpointFWCreateBackup', {'devices': device})
     creation_content = creation_status[0].get('Contents')
+
     if not isinstance(creation_content[0], dict):
         res.append({"Type": entryTypes["error"], "ContentsFormat": formats["text"],
                     "Contents": "Error before starting backup: " + creation_content + "\nHint: make"
                                 " sure the device selected exists and is correctly configured"})
         demisto.results(res)
         sys.exit()
-    output = creation_content[0].get('Output')
+
+    output = str(creation_content[0].get('Output'))
     if 'Error' in output:
         res.append({"Type": entryTypes["error"], "ContentsFormat": formats["text"],
                     "Contents": "Failed creating the backup!" + output})
         demisto.results(res)
         sys.exit()
+
     current_status = ''
     while current_status.find('Local backup succeeded') == -1:
 
@@ -39,17 +42,20 @@ try:
 
         content = status_entries[0]
         if content:
-            content = json.dumps(content.get('Contents'))
+            content = json.dumps(content.get('Contents')) 
 
         for entry in status_entries:
             current_status = str(demisto.get(entry, 'Contents'))
+
         if 'Progress' in current_status:
             percent_completed = re.findall(r'\d+%', current_status)[0]
             demisto.results('Current progress: ' + percent_completed)
+
         process_good = False
         for good_status in ['Progress', 'succeeded', 'Creating']:
             if good_status in current_status:
                 process_good = True
+
         if not process_good:
             res.append({"Type": entryTypes["error"], "ContentsFormat": formats["text"],
                         "Contents": "Failed while checking the backup status! " + current_status})
