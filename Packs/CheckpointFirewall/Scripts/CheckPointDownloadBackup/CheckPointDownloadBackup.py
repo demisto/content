a54f803ac6068855bfc1ce904ff0e9605b4cd839
Lior-Dahan-qm <liord@qmasters.co>
2020-08-18 12:37:01 +0300

    lint code

--- Packs/CheckpointFirewall/Scripts/CheckPointDownloadBackup/CheckPointDownloadBackup.py
+++ Packs/CheckpointFirewall/Scripts/CheckPointDownloadBackup/CheckPointDownloadBackup.py
@@ -42,7 +42,7 @@ try:
 
         content = status_entries[0]
         if content:
-            content = json.dumps(content.get('Contents')) 
+            content = json.dumps(content.get('Contents'))
 
         for entry in status_entries:
             current_status = str(demisto.get(entry, 'Contents'))
