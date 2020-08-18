d8e217cae6e7e39ad55a5698808bbf685fc50b42
avidan-H <ahessing@paloaltonetworks.com>
2020-08-18 17:58:39 +0300

    make it all better

--- Utils/contribution_sync/sync_contrib_base.py
+++ Utils/contribution_sync/sync_contrib_base.py
@@ -2,10 +2,13 @@
 
 import os
 import sys
-import requests
+import urllib3
 from github import Github, enable_console_debug_logging
 from github.Repository import Repository
-from typing import List, Bool
+from typing import List
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def get_master_commit_sha(repo: Repository) -> str:  # noqa: E999
@@ -23,60 +26,41 @@ def get_master_commit_sha(repo: Repository) -> str:  # noqa: E999
 
 
 def get_branch_names_with_contrib(repo: Repository) -> List[str]:  # noqa: E999
-    '''Return the list of branches that have the prefix of "contrib/"
+    '''Return the list of branches that have the prefix of "contrib/" and that are base branches of open PRs
 
     Args:
         repo (Repository): The repository whose branches will be searched and listed
 
     Returns:
-        (List[str]): List of branch names that have the "contrib/" prefix
+        (List[str]): List of branch names that have the "contrib/" prefix and are base branches of open PRs
     '''
     branch_names = []
     for branch in repo.get_branches():
         if branch.name.startswith('contrib/'):
-            branch_names.append(branch.name)
+            prs_with_branch_as_base = repo.get_pulls(state='OPEN', base=branch.name)
+            if prs_with_branch_as_base.totalCount >= 1:
+                branch_names.append(branch.name)
     return branch_names
-
-
-def is_pr(branch_name: str) -> Bool:  # noqa: E999
-    '''Returns whether a branch has a PR for it
-
-    Args:
-        branch_name (str): The name of the branch to check
-
-    Returns:
-        Bool: True if there is a PR for the branch, False otherwise
-    '''
-    response = requests.get(
-        f"https://api.github.com/repos/demisto/content/pulls?head=demisto:{branch_name}",
-        verify=False, headers={'Accept': 'application/json'}
-    )
-    if response.status_code == 200 and len(response.json()) > 0:
-        return True
-    return False
 
 
 def main():
     debug_mode = len(sys.argv) >= 2 and 'debug' in sys.argv[1].casefold
     if debug_mode:
-        enable_console_debug_logging
+        enable_console_debug_logging()
     gh = Github(os.getenv('CONTENTBOT_GH_ADMIN_TOKEN'), verify=False)
     organization = 'demisto'
     repo = 'content'
     content_repo = gh.get_repo(f'{organization}/{repo}')
 
-    list_of_all_branches = []
     master_sha = get_master_commit_sha(content_repo)
-    contrib_branches = get_branch_names_with_contrib(content_repo)
-    for branch_name in contrib_branches:
-        if not is_pr(branch_name):
-            list_of_all_branches.append(branch_name)
-            git_ref = content_repo.get_git_ref(f'heads/{branch_name}')
-            print(f'Updating branch "{branch_name}" to sha "{master_sha}"')
-            git_ref.edit(master_sha, force=True)
+    contrib_base_branches = get_branch_names_with_contrib(content_repo)
+    for branch_name in contrib_base_branches:
+        git_ref = content_repo.get_git_ref(f'heads/{branch_name}')
+        print(f'Updating branch "{branch_name}" to sha "{master_sha}"')
+        git_ref.edit(master_sha, force=True)
 
     if debug_mode:
-        print(f'{list_of_all_branches=}')
+        print(f'{contrib_base_branches=}')
 
 
 if __name__ == "__main__":
