8b267215f086d8bb6648be7421a4c8b5908f9192
guykeller <g12k34ppp>
2020-08-19 10:28:43 +0300

    changed branch access

--- Tests/scripts/run_private_build_tests.sh
+++ Tests/scripts/run_private_build_tests.sh
@@ -33,7 +33,7 @@ code_2=0
 echo "starting configure_and_test_integration_instances"
 PREVIOUS_JOB_NUMBER=`cat create_instances_build_num.txt`
 
-python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$GIT_BRANCH" --build-number "$GITHUB_RUN_NUMBER" -pr true
+python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$BRANCH_NAME" --build-number "$GITHUB_RUN_NUMBER" -pr true
 code_1=$?
 
 # echo 'export GOOGLE_APPLICATION_CREDENTIALS="creds.json"' >> $BASH_ENV TODO - commented
@@ -46,10 +46,10 @@ EOF
 if [ $code_1 -ne 1 ] ; then
   if [ -n "${NON_AMI_RUN}" ]; then
     # non AMI
-    python3 ./Tests/test_content.py -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$GIT_BRANCH" -m "$MEM_CHECK" -d "$1"
+    python3 ./Tests/test_content.py -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$BRANCH_NAME" -m "$MEM_CHECK" -d "$1"
   else
     # AMI
-    python3 ./Tests/test_content.py -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$GIT_BRANCH" -m "$MEM_CHECK" --isAMI true -d "$1"
+    python3 ./Tests/test_content.py -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$BRANCH_NAME" -m "$MEM_CHECK" --isAMI true -d "$1"
   fi
 fi
 
