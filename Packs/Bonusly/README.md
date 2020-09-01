10557be43db72897070125e8a5a45a4b30521ec8
Tyler Rorabaugh <jrorabaugh@paloaltonetworks.com>
2020-09-01 01:26:10 -0700

    Initial Bonusly Package Content (#7198)
    * Initial Add
    
    * Initial Add of Bonusly. I want to thank francesco and the content team for all of the nice work on the updated dev docs. I also wish I could provide AutoGratitude but I dont have admin level points
    
    * Added additional readme content based on generate docs request
    
    * Updated Ignore
    
    * fixed validation outputs
    
    * fixed validation outputs
    
    * updating validation checks
    
    * initial commit
    
    * added again and also added new comments and fixed incorrect logic
    
    * Fixed commit issues, removed duplicate files, removed individual create bonus playbook
    
    * Added new adjustments based on feedback thanks a bunch Roy
    
    * Added new adjustments based on feedback thanks a bunch Roy
    
    * Update IncOwnerToBonuslyUser.py
    
    adding line at the end of py file
    
    * added pb readme, img
    
    * updated img link in pb readme
    
    * Updated secrets ignore file to ignore the email addresses
    
    * Updated descriptions
    
    * Updated
    
    * Updated descriptions
    
    * Update Bonusly.py
    
    fixing test_module
    
    * Update IncOwnerToBonuslyUser.py
    
    * Update IncOwnerToBonuslyUser.yml
    
    * Update IncOwnerToBonuslyUser.yml
    
    * Update IncOwnerToBonuslyUser.py
    
    * Update IncOwnerToBonuslyUser.py
    
    * Update Bonusly_-_AutoGratitude.yml
    
    * Update Bonusly_-_AutoGratitude.yml
    
    * Update Bonusly_-_AutoGratitude.yml
    
    * Update Bonusly_-_AutoGratitude.yml
    
    * Update IncOwnerToBonuslyUser.yml
    
    * Update IncOwnerToBonuslyUser.yml
    
    * Update Packs/Bonusly/pack_metadata.json
    
    * Update Bonusly.yml
    
    * Update IncOwnerToBonuslyUser.yml
    
    Co-authored-by: Tyler Rorabaugh <jrorabaugh@spaloaltonetworks.com>
    Co-authored-by: roysagi <50295826+roysagi@users.noreply.github.com>
    Co-authored-by: idovandijk <43602124+idovandijk@users.noreply.github.com>
    Co-authored-by: Alex Fiedler <38628621+kirbles19@users.noreply.github.com>
    Co-authored-by: Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>

---
+++ Packs/Bonusly/README.md
@@ -0,0 +1,14 @@
+## Overview
+
+Bonus.ly is an employee recognition platform which enterprises use to for employee recognition. We're building tools to help people feel a sense of purpose and progress at work. The platform which also has an API enables employees to recognize each other by providing a point based bonus system. Bonus.ly helps your employees feel connected, engaged, and aligned is mission critical right now. Bonusly makes employee recognition easy and fun, fostering community and creating company-wide alignment. It also provides employees with positive feedback in the work that they are doing.
+
+To utilize the example playbooks in this content package you will need a list in your XSOAR list settings similar to the following.
+
+Create a Lookup JSON List In The Lists Section Containing XSOAR Email To Bonusly User
+
+{
+"shrek@paloaltonetworks.com":"@shrek", 
+"donkey@paloaltonetworks.com":"@donkey"
+}
+
+   
---
+++ Packs/Bonusly/Scripts/IncOwnerToBonuslyUser/IncOwnerToBonuslyUser.py
@@ -0,0 +1,31 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+"""
+Pass in JSON key value lookup then fetches the incident owner and finds the bonusly user
+
+"""
+
+json_lookup = demisto.args().get('json')
+if isinstance(json_lookup, str):
+    json_lookup = json.loads(json_lookup)
+
+
+def inc_owner_bonusly_user():
+    owner_username = demisto.args().get('owner')
+    if owner_username:
+        try:
+            owner_info = demisto.executeCommand('getUserByUsername', {"username": owner_username})[0]
+            owner_email = owner_info.get("EntryContext").get("UserByUsername").get("email")
+            bonusly_user = json_lookup[owner_email]
+            readable_output = "# Incident Owners Email \n" + owner_email + '\n # Bonusly User ' + bonusly_user
+            outputs = {'IncOwnerEmail': owner_email, 'BonuslyUser': bonusly_user}
+            return return_outputs(readable_output, outputs, owner_email)
+        except Exception as ex:
+            return_error("Error: {}".format(ex))
+    else:
+        return_error("Error: Email for owner of incident was not found")
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    inc_owner_bonusly_user()
