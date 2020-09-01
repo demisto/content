1d229565dc0e158e33935b8458e7fd89b490f3a6
lperry <lperry@paloaltonetworks.com>
2020-09-01 17:54:27 +0300

    1. adding frommail logic 2. adding first tests

--- Packs/Base/Scripts/PhishingDedupPreprocessingRule/PhishingDedupPreprocessingRule.py
+++ Packs/Base/Scripts/PhishingDedupPreprocessingRule/PhishingDedupPreprocessingRule.py
@@ -3,31 +3,60 @@ from CommonServerPython import *
 from CommonServerUserPython import *
 import pandas as pd
 # from bs4 import BeautifulSoup
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from scipy import spatial
+from sklearn.feature_extraction.text import TfidfVectorizer
 from numpy import dot
 from numpy.linalg import norm
+from email.utils import parseaddr
+import tldextract
+NO_FETCH_EXTRACT = tldextract.TLDExtract(suffix_list_urls=None)
+
 
 SIMILARITY_THRESHOLD = 0.98
 
 EMAIL_BODY_FIELD = 'emailbody'
 EMAIL_SUBJECT_FIELD = 'emailsubject'
 EMAIL_HTML_FIELD = 'emailbodyhtml'
+FROM_FIELD = 'emailfrom'
+FROM_DOMAIN_FIELD = 'fromdomain'
 MERGED_TEXT_FIELD = 'mereged_text'
 MIN_TEXT_LENGTH = 50
+DEFAULT_ARGS = {
+    'limit': '1000',
+    'incidentTypes': 'Phishing',
+    'exsitingIncidentsLookback': '100 days ago',
+}
+FROM_POLICY_TEXT_ONLY = 'TextOnly'
+FROM_POLICY_EXACT = 'Exact'
+FROM_POLICY_DOMAIN = 'Domain'
+
+FROM_POLICY = FROM_POLICY_TEXT_ONLY
+
 
 def get_existing_incidents(input_args):
+    global DEFAULT_ARGS
     get_incidents_args = {}
     for arg in ['incidentTypes', 'query', 'limit']:
         if arg in input_args:
             get_incidents_args[arg] = input_args[arg]
+        elif arg in DEFAULT_ARGS:
+            get_incidents_args[arg] = DEFAULT_ARGS[arg]
     if 'exsitingIncidentsLookback' in input_args:
         get_incidents_args['fromDate'] = input_args['exsitingIncidentsLookback']
+    elif 'exsitingIncidentsLookback' in DEFAULT_ARGS:
+        get_incidents_args['fromDate'] = DEFAULT_ARGS['exsitingIncidentsLookback']
     incidents_query_res = demisto.executeCommand('GetIncidentsByQuery', get_incidents_args)
     if is_error(incidents_query_res):
         return_error(get_error(incidents_query_res))
     incidents = json.loads(incidents_query_res[-1]['Contents'])
     return incidents
+
+
+def extract_domain(address):
+    if address == '':
+        return ''
+    email_address = parseaddr(address)[1]
+    ext = NO_FETCH_EXTRACT(email_address)
+    return ext.domain
 
 
 def get_text_from_html(html):
@@ -47,22 +76,23 @@ def get_text_from_html(html):
     text = '\n'.join(chunk for chunk in chunks if chunk)
     return text
 
+
 def preprocess_text_fields(incident):
     email_body = email_subject = email_html = ''
     if EMAIL_BODY_FIELD in incident:
         email_body = incident[EMAIL_BODY_FIELD]
-    if "CustomFields" in incident and EMAIL_BODY_FIELD in incident["CustomFields"]:
-        email_body = incident["CustomFields"][EMAIL_BODY_FIELD]
-
-    if EMAIL_SUBJECT_FIELD in incident:
-        email_subject = incident[EMAIL_SUBJECT_FIELD]
-    if "CustomFields" in incident and EMAIL_SUBJECT_FIELD in incident["CustomFields"]:
-        email_subject = incident["CustomFields"][EMAIL_SUBJECT_FIELD]
-
     if EMAIL_HTML_FIELD in incident:
         email_html = incident[EMAIL_HTML_FIELD]
-    if "CustomFields" in incident and EMAIL_HTML_FIELD in incident["CustomFields"]:
-        email_html = incident["CustomFields"][EMAIL_HTML_FIELD]
+    if EMAIL_SUBJECT_FIELD in incident:
+        email_subject = incident[EMAIL_SUBJECT_FIELD]
+
+    if "CustomFields" in incident and isinstance(incident["CustomFields"], dict):
+        if EMAIL_BODY_FIELD in incident["CustomFields"]:
+            email_body = incident["CustomFields"][EMAIL_BODY_FIELD]
+        if EMAIL_SUBJECT_FIELD in incident["CustomFields"]:
+            email_subject = incident["CustomFields"][EMAIL_SUBJECT_FIELD]
+        if EMAIL_HTML_FIELD in incident["CustomFields"]:
+            email_html = incident["CustomFields"][EMAIL_HTML_FIELD]
 
     if isinstance(email_html, float):
         email_html = ''
@@ -72,31 +102,56 @@ def preprocess_text_fields(incident):
         email_subject = ''
     return email_subject + ' ' + email_body
 
-def preprocess_existing_incidents(existing_incidents, new_incident):
-    global MERGED_TEXT_FIELD
-    existing_incidents_df = pd.DataFrame(existing_incidents)
+
+def preprocess_incidents_df(existing_incidents):
+    global MERGED_TEXT_FIELD, FROM_FIELD, FROM_DOMAIN_FIELD
+    incidents_df = pd.DataFrame(existing_incidents)
+    incidents_df['CustomFields'] = incidents_df['CustomFields'].fillna(value={})
+    incidents_df = pd.concat([incidents_df.drop('CustomFields', axis=1),
+                              incidents_df['CustomFields'].apply(pd.Series)], axis=1)
+    incidents_df[MERGED_TEXT_FIELD] = incidents_df.apply(lambda x: preprocess_text_fields(x), axis=1)
+    incidents_df = incidents_df[incidents_df[MERGED_TEXT_FIELD].str.len() >= MIN_TEXT_LENGTH]
+    incidents_df.reset_index(inplace=True)
+    if FROM_FIELD in incidents_df:
+        incidents_df[FROM_FIELD] = incidents_df[FROM_FIELD].fillna(value='')
+    else:
+        incidents_df[FROM_FIELD] = ''
+    incidents_df[FROM_FIELD] = incidents_df[FROM_FIELD].apply(lambda x: x.strip())
+    incidents_df[FROM_DOMAIN_FIELD] = incidents_df[FROM_FIELD].apply(lambda address: extract_domain(address))
+    return incidents_df
+
+
+def filter_out_new_incident(existing_incidents_df, new_incident):
     same_id_mask = existing_incidents_df['id'] == new_incident['id']
     existing_incidents_df = existing_incidents_df[~same_id_mask]
-    existing_incidents_df[MERGED_TEXT_FIELD] = existing_incidents_df.apply(lambda x: preprocess_text_fields(x), axis=1)
-    existing_incidents_df = existing_incidents_df[existing_incidents_df[MERGED_TEXT_FIELD].str.len() >= MIN_TEXT_LENGTH]
-    existing_incidents_df.reset_index(inplace=True)
     return existing_incidents_df
+
 
 def vectorize(text, vectorizer):
     return vectorizer.transform([text]).toarray()[0]
 
 
 def cosine_sim(a, b):
-    return dot(a, b)/(norm(a)*norm(b))
+    return dot(a, b) / (norm(a) * norm(b))
 
 
-def find_duplicate_incidents(new_incident_text, existing_incidents_df):
-    global MERGED_TEXT_FIELD
+def find_duplicate_incidents(new_incident, existing_incidents_df):
+    global MERGED_TEXT_FIELD, FROM_POLICY
+    new_incident_text = new_incident[MERGED_TEXT_FIELD]
     text = [new_incident_text] + existing_incidents_df[MERGED_TEXT_FIELD].tolist()
     vectorizer = TfidfVectorizer(token_pattern=r"(?u)\b\w\w+\b|!|\?|\"|\'").fit(text)
     new_incident_vector = vectorize(new_incident_text, vectorizer)
-    existing_incidents_df['vector'] = existing_incidents_df[MERGED_TEXT_FIELD].apply(lambda x: vectorize(x,vectorizer))
-    existing_incidents_df['similarity'] = existing_incidents_df['vector'].apply(lambda x: cosine_sim(x, new_incident_vector))
+    existing_incidents_df['vector'] = existing_incidents_df[MERGED_TEXT_FIELD].apply(lambda x: vectorize(x, vectorizer))
+    existing_incidents_df['similarity'] = existing_incidents_df['vector'].apply(
+        lambda x: cosine_sim(x, new_incident_vector))
+    if FROM_POLICY == FROM_POLICY_DOMAIN:
+        mask = (existing_incidents_df[FROM_DOMAIN_FIELD] != '') &\
+               (existing_incidents_df[FROM_DOMAIN_FIELD] == new_incident[FROM_DOMAIN_FIELD])
+        existing_incidents_df = existing_incidents_df[mask]
+    elif FROM_POLICY == FROM_POLICY_EXACT:
+        mask = (existing_incidents_df[FROM_FIELD] != '') & \
+               (existing_incidents_df[FROM_FIELD] == new_incident[FROM_FIELD])
+        existing_incidents_df = existing_incidents_df[mask]
     existing_incidents_df.sort_values(by='similarity', ascending=False, inplace=True)
     if len(existing_incidents_df) > 0:
         return existing_incidents_df.iloc[0], existing_incidents_df.iloc[0]['similarity']
@@ -106,39 +161,50 @@ def find_duplicate_incidents(new_incident_text, existing_incidents_df):
 
 def close_new_incident_and_link_to_existing(new_incident, existing_incident, similarity):
     entries = []
+    # dumpable_new_incident = {k: v for k, v in new_incident.items() if not isinstance(v, (np.floating, np.integer))}
     entries.append({'Contents': "Duplicate incident: " + new_incident['name']})
-    entries.append({"Type": entryTypes['note'], "ContentsFormat": "json", "Contents": json.dumps(new_incident)})
+    entries.append({"Type": entryTypes['note'],
+                    "ContentsFormat": "json",
+                    "Contents": json.dumps(new_incident)
+                    })
     entries_str = json.dumps(entries)
     demisto.executeCommand("addEntries", {"id": existing_incident["id"], "entries": entries_str})
+    res = demisto.executeCommand("linkIncidents", {
+        'linkedIncidentIDs': existing_incident['id'],
+        'incidentId': existing_incident['id']})
+    if is_error(res):
+        return_error(res)
     demisto.results(False)
-    return 'most similar message: {} {}'.format(similarity, existing_incident[MERGED_TEXT_FIELD])
 
 
 def create_new_incident():
     demisto.results(True)
 
 
-
 def main():
-    global EMAIL_BODY_FIELD, EMAIL_SUBJECT_FIELD, EMAIL_HTML_FIELD, MIN_TEXT_LENGTH
+    global EMAIL_BODY_FIELD, EMAIL_SUBJECT_FIELD, EMAIL_HTML_FIELD, FROM_FIELD, MIN_TEXT_LENGTH, FROM_POLICY
     input_args = demisto.args()
     EMAIL_BODY_FIELD = input_args.get('emailBody', EMAIL_BODY_FIELD)
     EMAIL_SUBJECT_FIELD = input_args.get('emailSubject', EMAIL_SUBJECT_FIELD)
     EMAIL_HTML_FIELD = input_args.get('emailBodyHTML', EMAIL_HTML_FIELD)
+    FROM_FIELD = input_args.get('emailFrom', FROM_FIELD)
+    FROM_POLICY = input_args.get('fromPolicy', FROM_POLICY)
     existing_incidents = get_existing_incidents(input_args)
     if len(existing_incidents) == 0:
         create_new_incident()
         return
     new_incident = demisto.incidents()[0]
-    existing_incidents_df = preprocess_existing_incidents(existing_incidents, new_incident)
-    if len(existing_incidents_df) == 0:
+    new_incident_df = preprocess_incidents_df([new_incident])
+    existing_incidents_df = preprocess_incidents_df(existing_incidents)
+    existing_incidents_df = filter_out_new_incident(existing_incidents_df, new_incident)
+
+    if len(existing_incidents_df) == 0 or len(
+            new_incident_df) == 0:  # len(new_incident_df)==0 means new incident is too short
         create_new_incident()
         return
-    new_incident_text = preprocess_text_fields(new_incident)
-    if len(new_incident_text) < MIN_TEXT_LENGTH:
-        create_new_incident()
-        return
-    duplicate_incident_row, similarity = find_duplicate_incidents(new_incident_text, existing_incidents_df)
+    new_incident_preprocessed = new_incident_df.iloc[0].to_dict()
+    duplicate_incident_row, similarity = find_duplicate_incidents(new_incident_preprocessed,
+                                                                  existing_incidents_df)
     if duplicate_incident_row is None or similarity < SIMILARITY_THRESHOLD:
         create_new_incident()
     else:
---
+++ Packs/Base/Scripts/PhishingDedupPreprocessingRule/PhishingDedupPreprocessingRule_test.py
@@ -0,0 +1,89 @@
+from CommonServerPython import *
+from PhishingDedupPreprocessingRule import *
+import json
+
+ID_CONtER = 0
+
+EXISTING_INCIDENTS = []
+
+RESULTS = None
+EXISTING_INCIDENT_ID = DUP_INCIDENT_ID = None
+
+IDS_COUNTER = 0
+
+
+def create_incident(subject=None, body=None, html=None, frommail=None):
+    global IDS_COUNTER
+    incident = {
+        "CustomFields": {},
+        "id": str(IDS_COUNTER),
+        "name": ' '.join(str(x) for x in [subject, body, html, frommail, id])
+    }
+    IDS_COUNTER += 1
+    if subject is not None:
+        incident['CustomFields']['emailsubject'] = subject
+    if body is not None:
+        incident['CustomFields']['emailbody'] = body
+    if html is not None:
+        incident['CustomFields']['emailbodyhtml'] = html
+    if frommail is not None:
+        incident['CustomFields']['frommail'] = frommail
+    return incident
+
+
+def set_existing_incidents_list(incidents_list):
+    global EXISTING_INCIDENTS
+    EXISTING_INCIDENTS = incidents_list
+
+
+def executeCommand(command, args=None):
+    global EXISTING_INCIDENTS, EXISTING_INCIDENT_ID, DUP_INCIDENT_ID
+    if command == 'GetIncidentsByQuery':
+        incidents_str = json.dumps(EXISTING_INCIDENTS)
+        return [{'Contents': incidents_str, 'Type': 'not error'}]
+    if command == 'linkIncidents':
+        EXISTING_INCIDENT_ID = args['incidentId']
+        DUP_INCIDENT_ID = args['linkedIncidentIDs']
+
+
+def results(arg):
+    global RESULTS
+    RESULTS = arg
+
+
+def duplicated_incidents_found(existing_incident):
+    return existing_incident['id'] == EXISTING_INCIDENT_ID
+
+
+def test_same_incidents_text_only(mocker):
+    global RESULTS, EXISTING_INCIDENT_ID, DUP_INCIDENT_ID
+    EXISTING_INCIDENT_ID = DUP_INCIDENT_ID = None
+    text = "Imagine there's no countries It isn't hard to do Nothing to kill or die for And no religion too " \
+           "Imagine all the people Living life in peace"
+    existing_incident = create_incident(body=text, frommail='mt.kb.user@gmail.com')
+    set_existing_incidents_list([existing_incident])
+    mocker.patch.object(demisto, 'args', return_value={'fromPolicy': 'TextOnly'})
+    mocker.patch.object(demisto, 'executeCommand', side_effect=executeCommand)
+    new_incident = create_incident(body=text, frommail='mt.kb.user@gmail.com')
+    mocker.patch.object(demisto, 'incidents', return_value=[new_incident])
+    mocker.patch.object(demisto, 'results', side_effect=results)
+    main()
+    assert duplicated_incidents_found(existing_incident)
+
+
+def test_different_text_only(mocker):
+    global RESULTS, EXISTING_INCIDENT_ID, DUP_INCIDENT_ID
+    EXISTING_INCIDENT_ID = DUP_INCIDENT_ID = None
+    text = "Imagine there's no countries It isn't hard to do Nothing to kill or die for And no religion too " \
+           "Imagine all the people Living life in peace"
+    text2 = "Love of my life, you've hurt me You've broken my heart and now you leave me Love of my life, can't you see?\
+          Bring it back, bring it back Don't take it away from me, because you don't know What it means to me"
+    existing_incident = create_incident(body=text2, frommail='mt.kb.user@gmail.com')
+    set_existing_incidents_list([existing_incident])
+    mocker.patch.object(demisto, 'args', return_value={'fromPolicy': 'TextOnly'})
+    mocker.patch.object(demisto, 'executeCommand', side_effect=executeCommand)
+    new_incident = create_incident(body=text, frommail='mt.kb.user@gmail.com')
+    mocker.patch.object(demisto, 'incidents', return_value=[new_incident])
+    mocker.patch.object(demisto, 'results', side_effect=results)
+    main()
+    assert not duplicated_incidents_found(existing_incident)
