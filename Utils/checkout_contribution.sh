ab52660bc6d2e92c88b8e82a3bfba77545ec883a
yorhov <Orekhova97229!>
2020-09-02 18:17:28 +0300

    added comments

--- Utils/checkout_contribution.sh
+++ Utils/checkout_contribution.sh
@@ -15,13 +15,17 @@ if [[ $2 == "true" ]]; then
     git pull
 fi
 
+# if remote does not exists already add it, if exists re-add it.
 git remote set-url $USER git@github.com:$USER/content.git
+# Check if branch exists locally
 LOCAL_BRANCH_EXISTS=$(git show-branch --list "${USER}/${BRANCH}")
 if [[ -z ${LOCAL_BRANCH_EXISTS} ]]; then
+    # If branch does not exists - fetch and checkout to it with a trace to the origin.
       git fetch "${USER}"
       git checkout -t "${USER}/${BRANCH}"
       git pull
 else
+    # If branch already exists - checkout to it(will checkout to head) and switch to wanted branch.
       git checkout "${USER}/${BRANCH}"
       git switch "${BRANCH}"
       git pull
