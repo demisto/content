b25baa38166c3fb2dad8d2b30967c630f22c61a0
Matt Houston <58996090+mhouston007@users.noreply.github.com>
2020-09-03 16:08:51 -0400

    Restricted builtins to allow safer execution

--- Packs/CommonScripts/Scripts/PyDT/PyDT.py
+++ Packs/CommonScripts/Scripts/PyDT/PyDT.py
@@ -1,10 +1,21 @@
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
+
+
+# Restrict the builtins available to the user
+from RestrictedPython import compile_restricted
+from RestrictedPython import safe_builtins
+
+
 def py_dt(val):
-    source = 'return_results({})'.format(demisto.args().get('Python', "Python arg not found"))
-    code = compile(source, '<string>', 'exec')
-    exec(code)
+    # Pass additional builtins
+    safe_builtins['return_results'] = return_results
+    safe_builtins['val'] = val
+
+    source = '{}'.format(demisto.args().get('Python', "Python arg not found"))
+    code = compile_restricted(source, '<string>', 'exec')
+    exec(code, {'__builtins__': safe_builtins}, None)
 
 
 if __name__ in ('__main__','__builtin__','builtins'):
