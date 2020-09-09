81efad960b0fdc4193a4c274826a0c7ffc8cbaed
Omer Cohen <git@omer.io>
2020-09-09 10:11:29 +0300

    fixed link script

--- Packs/CaseManagement-Generic/Scripts/LinkIncidentsButton/LinkIncidentsButton.py
+++ Packs/CaseManagement-Generic/Scripts/LinkIncidentsButton/LinkIncidentsButton.py
@@ -1,15 +1,4 @@
 import demistomock as demisto
 from CommonServerPython import *
 
-linkedIncidentIDs = demisto.args().get("linkedIncidentIDs")
-action = demisto.args().get("action")
-if action not in ["link", "unlink"]:
-    action = "link"
-counter = int(demisto.args().get("retryLimit"))
-res = demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": linkedIncidentIDs, "action": action})
-while isError(res[0]) and counter > 0 and ("DB Version" in res[0]["Contents"]):
-    time.sleep(2)
-    demisto.error(f"{str(counter)} retry linkIncidents: {str(linkedIncidentIDs)}")
-    res = demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": linkedIncidentIDs, "action": action})
-    counter = counter - 1
-demisto.results(res)
+demisto.results(demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": demisto.getArg("linkedIncidentIDs"), "action": demisto.getArg("action")}))
