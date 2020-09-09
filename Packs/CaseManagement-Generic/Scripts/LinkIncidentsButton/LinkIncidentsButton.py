40833d84c89a32e53a18ee00fe3cea624b5474c4
guyfreund <gfreund@paloaltonetworks.com>
2020-09-09 17:40:26 +0300

    pass validations except report

--- Packs/CaseManagement-Generic/Scripts/LinkIncidentsButton/LinkIncidentsButton.py
+++ Packs/CaseManagement-Generic/Scripts/LinkIncidentsButton/LinkIncidentsButton.py
@@ -1,4 +1,5 @@
 import demistomock as demisto
 from CommonServerPython import *
 
-demisto.results(demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": demisto.getArg("linkedIncidentIDs"), "action": demisto.getArg("action")}))
+demisto.results(demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": demisto.getArg(
+    "linkedIncidentIDs"), "action": demisto.getArg("action")}))
