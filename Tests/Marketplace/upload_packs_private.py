ad3a7540e590e5a7fae3cde5f496935c8a7e994b
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-09-10 10:20:19 +0300

    Fixing url presented

--- Tests/Marketplace/upload_packs_private.py
+++ Tests/Marketplace/upload_packs_private.py
@@ -819,8 +819,7 @@ def create_and_upload_marketplace_pack(upload_config, pack, storage_bucket, inde
                                                                  bucket_for_uploading,
                                                                  override_all_packs or pack_was_modified)
     if full_pack_path is not None:
-        bucket_path = f'https://console.cloud.google.com/storage/browser/' \
-                      f'marketplace-ci-build/{branch_name}/{build_number}/'
+        bucket_path = 'https://console.cloud.google.com/storage/browser/marketplace-ci-build/'
         bucket_url = bucket_path + full_pack_path
     else:
         bucket_url = 'Pack was not uploaded.'
