1bc235276f709aa5a66c866401d30d58a74cf290
guyfreund <gfreund@paloaltonetworks.com>
2020-09-10 10:29:45 +0300

    removed report

--- Packs/CaseManagement-Generic/Scripts/LinkIncidentsButton/LinkIncidentsButton.py
+++ Packs/CaseManagement-Generic/Scripts/LinkIncidentsButton/LinkIncidentsButton.py
@@ -4,4 +4,5 @@ action = demisto.getArg('action')
 if action not in ['link', 'unlink']:
     action = 'link'
 
-demisto.results(demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": demisto.getArg("linkedIncidentIDs"), "action": action}))
+demisto.results(demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": demisto.getArg("linkedIncidentIDs"),
+                                                         "action": action}))
