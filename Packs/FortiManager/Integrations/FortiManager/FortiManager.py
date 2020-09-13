139a6f0ce84d76826d8e02f1617221ccff4dc371
rsagi <rsagi@paloaltonetworks.com>
2020-09-13 11:07:34 +0300

    adding additional commands

--- Packs/FortiManager/Integrations/FortiManager/FortiManager.py
+++ Packs/FortiManager/Integrations/FortiManager/FortiManager.py
@@ -12,7 +12,7 @@ urllib3.disable_warnings()
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 MAX_INCIDENTS_TO_FETCH = 50
-HELLOWORLD_SEVERITIES = ['Low', 'Medium', 'High', 'Critical']
+GLOBAL_VAR = 'global'
 
 ''' CLIENT CLASS '''
 
@@ -46,7 +46,7 @@ class Client(BaseClient):
         )
         return response.get('session')
 
-    def fortimanager_http_request(self, method, url):
+    def fortimanager_http_request(self, method, url, data=None):
         body = {
             "id": 1,
             "method": method,
@@ -56,6 +56,9 @@ class Client(BaseClient):
             "session": self.session_token
         }
 
+        if data:
+            body['params'][0]['data'] = [data]
+
         response = self._http_request(
             method='POST',
             url_suffix='jsonrpc',
@@ -63,6 +66,18 @@ class Client(BaseClient):
         )
 
         return response.get('result')[0].get('data')
+
+
+def get_global_or_adom(client, args):
+    adom = args.get('adom') if args.get('adom') else client.adom
+    if adom == GLOBAL_VAR:
+        return GLOBAL_VAR
+    else:
+        return f"adom/{adom}"
+
+
+def setup_request_data(args, excluded_args):
+    return {key.replace('_', '-'): args.get(key) for key in args if key not in excluded_args}
 
 
 def list_adom_devices_command(client):
@@ -76,6 +91,184 @@ def list_adom_devices_command(client):
                                         headerTransform=string_to_table_header),
         raw_response=devices_data,
     )
+
+
+def list_adom_devices_groups_command(client):
+    device_groups_data = client.fortimanager_http_request("get", f"/dvmdb/adom/{client.adom}/group")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.DeviceGroup',
+        outputs_key_field='name',
+        outputs=device_groups_data,
+        readable_output=tableToMarkdown(f"ADOM {client.adom} Device Groups", device_groups_data, removeNull=True,
+                                        headerTransform=string_to_table_header),
+        raw_response=device_groups_data,
+    )
+
+
+def list_firewall_addresses_command(client, args):
+    firewall_addresses = client.fortimanager_http_request("get", f"/pm/config/{get_global_or_adom(client, args)}"
+                                                                 f"/obj/firewall/address")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.Address',
+        outputs_key_field='name',
+        outputs=firewall_addresses,
+        readable_output=tableToMarkdown("Firewall IPv4 Addresses", firewall_addresses,
+                                        removeNull=True, headerTransform=string_to_table_header),
+        raw_response=firewall_addresses,
+    )
+
+
+def list_firewall_address_groups_command(client, args):
+    firewall_address_groups = client.fortimanager_http_request("get", f"/pm/config/"
+                                                                      f"{get_global_or_adom(client, args)}"
+                                                                      f"/obj/firewall/addrgrp")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.AddressGroup',
+        outputs_key_field='name',
+        outputs=firewall_address_groups,
+        readable_output=tableToMarkdown("Firewall IPv4 Address Groups", firewall_address_groups,
+                                        removeNull=True, headerTransform=string_to_table_header),
+        raw_response=firewall_address_groups,
+    )
+
+
+def list_service_categories_command(client, args):
+    service_categories = client.fortimanager_http_request("get", f"/pm/config/"
+                                                                 f"{get_global_or_adom(client, args)}/"
+                                                                 f"obj/firewall/service/category")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.ServiceCategory',
+        outputs_key_field='name',
+        outputs=service_categories,
+        readable_output=tableToMarkdown("Service Categories", service_categories, removeNull=True,
+                                        headerTransform=string_to_table_header),
+        raw_response=service_categories,
+    )
+
+
+def list_service_groups_command(client, args):
+    service_groups = client.fortimanager_http_request("get", f"/pm/config/"
+                                                             f"{get_global_or_adom(client, args)}"
+                                                             f"/obj/firewall/service/group")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.ServiceGroup',
+        outputs_key_field='name',
+        outputs=service_groups,
+        readable_output=tableToMarkdown("Service Groups", service_groups, removeNull=True,
+                                        headerTransform=string_to_table_header),
+        raw_response=service_groups,
+    )
+
+
+def list_policy_packages_command(client, args):
+    policy_packages = client.fortimanager_http_request("get", f"pm/pkg/{get_global_or_adom(client, args)}")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.PolicyPackage',
+        outputs_key_field='name',
+        outputs=policy_packages,
+        readable_output=tableToMarkdown("Policy Packages", policy_packages, removeNull=True,
+                                        headerTransform=string_to_table_header),
+        raw_response=policy_packages,
+    )
+
+
+def list_policies_command(client, args):
+    policies = client.fortimanager_http_request("get", f"/pm/config/"
+                                                       f"{get_global_or_adom(client, args)}"
+                                                       f"pkg/{args.get('package')}/firewall/policy")
+
+    return CommandResults(
+        outputs_prefix='FortiManager.PolicyPackage.Policy',
+        outputs_key_field='name',
+        outputs=policies,
+        readable_output=tableToMarkdown(f"ADOM {client.adom} Policy Package {args.get('package')} Policies",
+                                        policies, removeNull=True, headerTransform=string_to_table_header),
+        raw_response=policies,
+    )
+
+
+def create_address_command(client, args):
+    firewall_addresses = client.fortimanager_http_request("add", f"/pm/config/{get_global_or_adom(client, args)}"
+                                                                 f"/obj/firewall/address",
+                                                          data=setup_request_data(args, ['adom']))
+
+    return f"Created new Address {firewall_addresses[0].get('name')}"
+
+
+def create_firewall_address_groups_command(client, args):
+    firewall_address_groups = client.fortimanager_http_request("add", f"/pm/config/"
+                                                                      f"{get_global_or_adom(client, args)}"
+                                                                      f"/obj/firewall/addrgrp",
+                                                               data=setup_request_data(args, ['adom']))
+
+    return f"Created new Address Group {firewall_address_groups[0].get('name')}"
+
+
+def create_service_groups_command(client, args):
+    service_groups = client.fortimanager_http_request("add", f"/pm/config/"
+                                                             f"{get_global_or_adom(client, args)}"
+                                                             f"/obj/firewall/service/group",
+                                                      data=setup_request_data(args, ['adom']))
+
+    return f"Created new Service Group {service_groups[0].get('name')}"
+
+
+def create_policy_packages_command(client, args):
+    package_settings = {
+        'central-nat': args.get('central_nat'),
+        'consolidated-firewall': args.get('consolidated_firewall'),
+        'fwpolicy-implicit-log': args.get('fwpolicy_implicit_log'),
+        'fwpolicy6-implicit-log': args.get('fwpolicy6_implicit_log'),
+        'inspection-mode': args.get('inspection_mode'),
+        'ngfw-mode': args.get('ngfw_mode'),
+        'ssl-ssh-profile': args.get('ssl_ssh_profile')
+    }
+
+    args['package settings'] = package_settings
+    client.fortimanager_http_request("add", f"pm/pkg/{get_global_or_adom(client, args)}",
+                                     data=setup_request_data(args, ['adom', 'central_nat', 'consolidated_firewall',
+                                                                    'fwpolicy_implicit_log',
+                                                                    'fwpolicy6_implicit_log', 'inspection_mode',
+                                                                    'ngfw_mode', 'ssl_ssh_profile']))
+
+    return f"Created new Policy Package {args.get('name')}"
+
+
+def create_policy_command(client, args):
+    for additional_param in args.get('additional_params').split(','):
+        field_and_value = additional_param.split('=')
+        args[field_and_value[0]] = field_and_value[1]
+
+    policies = client.fortimanager_http_request("add", f"/pm/config/"
+                                                       f"{get_global_or_adom(client, args)}"
+                                                       f"pkg/{args.get('package')}/firewall/policy",
+                                                data=setup_request_data(args, ['adom', 'package', 'additional_params']))
+
+    return f"Created policy with ID {policies[0].get('id')}"
+
+
+def delete_address_command(client, args):
+    client.fortimanager_http_request("delete", f"/pm/config/{get_global_or_adom(client, args)}"
+                                               f"/obj/firewall/address/{args.get('address')}")
+    return f"Deleted Address {args.get('address')}"
+
+
+def delete_address_group_command(client, args):
+    client.fortimanager_http_request("delete", f"/pm/config/{get_global_or_adom(client, args)}"
+                                               f"/obj/firewall/addrgrp/{args.get('address_group')}")
+    return f"Deleted Address Group {args.get('address_group')}"
+
+
+def delete_service_group_command(client, args):
+    client.fortimanager_http_request("delete", f"/pm/config/{get_global_or_adom(client, args)}"
+                                               f"/obj/firewall/service/group/{args.get('service_group')}")
+    return f"Deleted Address Group {args.get('service_group')}"
 
 
 ''' MAIN FUNCTION '''
@@ -126,6 +319,48 @@ def main() -> None:
 
         elif demisto.command() == 'fortimanager-devices-list':
             return_results(list_adom_devices_command(client))
+
+        elif demisto.command() == 'fortimanager-device-groups-list':
+            return_results(list_adom_devices_groups_command(client))
+
+        elif demisto.command() == 'fortimanager-addresses-list':
+            return_results(list_firewall_addresses_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-address-groups-list':
+            return_results(list_firewall_address_groups_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-service-categories-list':
+            return_results(list_service_categories_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-service-group-list':
+            return_results(list_service_groups_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-firewall-policy-package-list':
+            return_results(list_policy_packages_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-firewall-policy-list':
+            return_results(list_policies_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-address-create':
+            return_results(create_address_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-address-group-create':
+            return_results(create_firewall_address_groups_command(client, demisto.args))
+
+        elif demisto.command() == 'fortimanager-firewall-policy-package-create':
+            return_results(create_policy_packages_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-firewall-policy-create':
+            return_results(create_policy_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-address-delete':
+            return_results(delete_address_command(client, demisto.args()))
+
+        elif demisto.command() == 'fortimanager-address-group-delete':
+            return_results(delete_address_group_command(client, demisto.args()))
+
+        elif demisto.command() == '':
+            return_results(delete_service_group_command(client, demisto.args()))
 
     # Log exceptions and return errors
     except Exception as e:
