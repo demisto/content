46d08ad171f6d3eb4ad9d31efc51012178372c19
adaud <adaud@paloaltonetworks.com>
2020-09-14 12:35:12 +0300

    use the parameters status, priority, rule_intent, rule_method and rule_strategy

--- Packs/AlienVault_USM_Anywhere/Integrations/AlienVault_USM_Anywhere/AlienVault_USM_Anywhere.py
+++ Packs/AlienVault_USM_Anywhere/Integrations/AlienVault_USM_Anywhere/AlienVault_USM_Anywhere.py
@@ -292,10 +292,18 @@ def search_alarms_command():
     end_time = args.get('end_time', 'now')
     show_suppressed = args.get('show_suppressed', 'false')
     limit = int(args.get('limit', 100))
+    status = args.get('status', None)
+    priority = args.get('priority', None)
+    rule_intent = args.get('rule_intent', None)
+    rule_method = args.get('rule_method', None)
+    rule_strategy = args.get('rule_strategy', None)
 
     start_time, end_time = get_time_range(time_frame, start_time, end_time)
 
-    result = search_alarms(start_time=start_time, end_time=end_time, show_suppressed=show_suppressed, limit=limit)
+    result = search_alarms(start_time=start_time, end_time=end_time, show_suppressed=show_suppressed, limit=limit,
+                           status=status, priority=priority, rule_intent=rule_intent, rule_method=rule_method,
+                           rule_strategy=rule_strategy)
+
     alarms = parse_alarms(result)
 
     return_outputs(tableToMarkdown('Alarms:', alarms),
