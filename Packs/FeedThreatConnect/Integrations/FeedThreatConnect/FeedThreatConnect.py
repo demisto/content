42cb434c178220b9d978e5e91ed278bbeb3b82b9
teizenman <teizenman@paloaltonetworks.com>
2020-09-16 17:50:36 +0300

    Update tlp only if defined

--- Packs/FeedThreatConnect/Integrations/FeedThreatConnect/FeedThreatConnect.py
+++ Packs/FeedThreatConnect/Integrations/FeedThreatConnect/FeedThreatConnect.py
@@ -81,16 +81,21 @@ def parse_indicator(indicator: Dict[str, str]) -> Dict[str, Any]:
     Returns:
         dict: Parsed indicator.
     """
-    return {
+    indicator_obj = {
         "value": indicator.get('summary'),
         "type": INDICATOR_MAPPING_NAMES.get(indicator.get('type', '')),
         "rawJSON": indicator,
         "score": calculate_dbot_score(indicator.get("threatAssessScore")),
         "fields": {
             "tags": argToList(demisto.getParam("feedTags")),
-            'trafficlightprotocol': demisto.getParam('tlp_color')
         },
     }
+
+    tlp_color = demisto.getParam('tlp_color')
+    if tlp_color:
+        indicator_obj['fields']['trafficlightprotocol'] = tlp_color
+
+    return indicator_obj
 
 
 ##########
@@ -100,6 +105,7 @@ def parse_indicator(indicator: Dict[str, str]) -> Dict[str, Any]:
 
 class Client:
     """Object represnt a client for ThreatConnect actions"""
+
     def __init__(self, access_key: str, secret_key: str, api_path: str):
         """ Initialize client configuration:
 
