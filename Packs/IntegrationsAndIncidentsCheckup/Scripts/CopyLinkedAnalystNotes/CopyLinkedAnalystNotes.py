4f512ac5e0780dbee4a7b138957e39223c25b83e
yaltmann <yaltmann@paloaltonetworks.com>
2020-09-23 15:28:55 +0300

    Changed CopyLinkedAnalystNotes script

--- Packs/IntegrationsAndIncidentsCheckup/Scripts/CopyLinkedAnalystNotes/CopyLinkedAnalystNotes.py
+++ Packs/IntegrationsAndIncidentsCheckup/Scripts/CopyLinkedAnalystNotes/CopyLinkedAnalystNotes.py
@@ -5,28 +5,33 @@ incident = demisto.incidents()[0]
 incidentID = incident.get('id')
 custom_fields = incident.get('CustomFields')
 LinkedIncident = custom_fields.get('similarincident', [""])[0]
-print(LinkedIncident)
 
 if LinkedIncident:
-    linkedlistData = demisto.executeCommand("getIncidents", {'id': LinkedIncident})
-    linkedContent = linkedlistData[0].get("Contents", {}).get("data")[0]
-    linkedIntegrationsData = linkedContent.get("CustomFields").get("integrationstestgrid", {})  # table of the linked incident
+    linkedlistData= demisto.executeCommand("getIncidents", {'id': LinkedIncident})
+    linkedContent = linkedlistData[0].get("Contents",{}).get("data")[0]
+    linkedCreatedDate = linkedContent.get("created",{}).split("T")[0]
+    linkedIntegrationsData = linkedContent.get("CustomFields").get("integrationstestgrid",{}) #table of the linked incident
     integrationsData = {}
     for row in linkedIntegrationsData:
-        integrationsData[row.get("instance")] = row.get("analystnote", "")
-    mainIntegrationGrid = custom_fields.get("integrationstestgrid")  # Main incident table for integrations
+        integrationsData[row.get("instance")] = row.get("analystnote","")
+    mainIntegrationGrid = custom_fields.get("integrationstestgrid") # Main incident table for integrations
     for mainRow in mainIntegrationGrid:
         if not mainRow.get("analystnote"):
             mainRow["analystnote"] = integrationsData.get(mainRow.get('instance'), '')
+            if mainRow.get("analystnote"):
+                mainRow["analystnote"] = integrationsData.get(mainRow.get('instance'), '') + " (" + str(linkedCreatedDate) + ")"
 
-    linkedIncidentsData = linkedContent.get("CustomFields").get("playbooktaskserrors", {})  # table of the linked incident
+    linkedIncidentsData = linkedContent.get("CustomFields").get("playbooktaskserrors",{}) #table of the linked incident
     incidentsData = {}
     for row in linkedIncidentsData:
-        incidentsData[row.get("incidentid")] = row.get("analystnote", "")
-    mainIncidentGrid = custom_fields.get("playbooktaskserrors")  # Main incident table for incidents
+        incidentsData[row.get("incidentid")] = row.get("analystnote","")
+    mainIncidentGrid = custom_fields.get("playbooktaskserrors") # Main incident table for incidents
     for mainRow in mainIncidentGrid:
         if not mainRow.get("analystnote"):
             mainRow["analystnote"] = incidentsData.get(mainRow.get('incidentid'), '')
+            if mainRow.get("analystnote"):
+                mainRow["analystnote"] = incidentsData.get(mainRow.get('incidentid'), '') + " (" + str(linkedCreatedDate) + ")"
+
 
 demisto.executeCommand("setIncident", {'customFields': {'integrationstestgrid': mainIntegrationGrid}})
 demisto.executeCommand("setIncident", {'customFields': {'playbooktaskserrors': mainIncidentGrid}})
