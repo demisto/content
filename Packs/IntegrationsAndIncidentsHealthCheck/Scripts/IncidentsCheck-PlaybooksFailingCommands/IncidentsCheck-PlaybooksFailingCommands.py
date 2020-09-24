41eb1cdad81e01e05f006ea713d07f6f28be0439
teizenman <teizenman@paloaltonetworks.com>
2020-09-24 16:51:43 +0300

    updated IncidentsCheck-PlaybooksFailingCommands.py updated IncidentsCheck-PlaybooksHealthNames.py

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IncidentsCheck-PlaybooksFailingCommands/IncidentsCheck-PlaybooksFailingCommands.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IncidentsCheck-PlaybooksFailingCommands/IncidentsCheck-PlaybooksFailingCommands.py
@@ -5,22 +5,23 @@ from typing import Counter
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
+commands_data = []
 incident = demisto.incidents()
-failedCommands = incident[0].get('CustomFields', {}).get('playbooksfailedcommands', 0)
-commandsData = []
 
-if failedCommands:
-    commandsscollections: Counter = collections.Counter(failedCommands)
-    topCommands = commandsscollections.most_common(10)
-    commands_count = len(topCommands)
-    commandnumber = 0
-    playbookTrue = True
-    while commandnumber < commands_count:
-        for command in topCommands:
+failed_commands = incident[0].get('CustomFields', {}).get('playbooksfailedcommands', 0)
+if failed_commands:
+    commands_collections: Counter = collections.Counter(failed_commands)
+    top_commands = commands_collections.most_common(10)
+    commands_count = len(top_commands)
+    command_number = 0
+
+    while command_number < commands_count:
+        for command in top_commands:
             random_number = random.randint(0, 16777215)
             hex_number = str(hex(random_number))  # convert to hexadecimal
             color = '#' + hex_number[2:]  # remove 0x and prepend '#'
-            commandWidgetData = {
+
+            command_widget_data = {
                 "data": [
                     command[1]
                 ],
@@ -29,20 +30,22 @@ if failedCommands:
                 "label": str(command[0]),
                 "color": color
             }
-            commandsData.append(commandWidgetData)
-            commandnumber = commandnumber + 1
+
+            commands_data.append(command_widget_data)
+            command_number += 1
+
     data = {
         "Type": 17,
         "ContentsFormat": "pie",
         "Contents": {
             "stats":
-            commandsData,
+            commands_data,
                 "params": {
                     "layout": "vertical"
                 }
         }
     }
-    demisto.results(data)
+
 else:
     data = {
         "Type": 17,
@@ -54,8 +57,8 @@ else:
                         0
                     ],
                     "groups": None,
-                    "name": "N\A",
-                    "label": "N\A",
+                    "name": "N\\A",
+                    "label": "N\\A",
                     "color": "rgb(255, 23, 68)"
                 },
             ],
@@ -64,4 +67,5 @@ else:
             }
         }
     }
-    demisto.results(data)
+
+demisto.results(data)
--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IncidentsCheck-PlaybooksHealthNames/IncidentsCheck-PlaybooksHealthNames.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IncidentsCheck-PlaybooksHealthNames/IncidentsCheck-PlaybooksHealthNames.py
@@ -5,21 +5,23 @@ from typing import Counter
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
+playbooks_data = []
 incident = demisto.incidents()
-playbookNames = incident[0].get('CustomFields', {}).get('playbooknameswithfailedtasks', 0)
-playbooksData = []
-if playbookNames:
-    playbookscollections: Counter = collections.Counter(playbookNames)
-    topPlaybooks = playbookscollections.most_common(10)
-    playbooksnumber = len(topPlaybooks)
-    playbooknumber = 0
-    playbookTrue = True
-    while playbooknumber < playbooksnumber:
-        for topPlaybook in topPlaybooks:
+
+playbook_names = incident[0].get('CustomFields', {}).get('playbooknameswithfailedtasks', 0)
+if playbook_names:
+    playbooks_collections: Counter = collections.Counter(playbook_names)
+    top_playbooks = playbooks_collections.most_common(10)
+    playbooks_number = len(top_playbooks)
+    playbook_number = 0
+
+    while playbook_number < playbooks_number:
+        for topPlaybook in top_playbooks:
             random_number = random.randint(0, 16777215)
             hex_number = str(hex(random_number))  # convert to hexadecimal
             color = '#' + hex_number[2:]  # remove 0x and prepend '#'
-            playbookWidgetData = {
+
+            playbook_widget_data = {
                 "data": [
                     topPlaybook[1]
                 ],
@@ -28,20 +30,21 @@ if playbookNames:
                 "label": str(topPlaybook[0]),
                 "color": color
             }
-            playbooksData.append(playbookWidgetData)
-            playbooknumber = playbooknumber + 1
+
+            playbooks_data.append(playbook_widget_data)
+            playbook_number += 1
+
     data = {
         "Type": 17,
         "ContentsFormat": "bar",
         "Contents": {
             "stats":
-            playbooksData,
-                "params": {
-                    "layout": "horizontal"
-                }
+                playbooks_data,
+            "params": {
+                "layout": "horizontal"
+            }
         }
     }
-    demisto.results(data)
 
 else:
     data = {
@@ -54,8 +57,8 @@ else:
                         0
                     ],
                     "groups": None,
-                    "name": "N\A",
-                    "label": "N\A",
+                    "name": "N\\A",
+                    "label": "N\\A",
                     "color": "rgb(255, 23, 68)"
                 },
             ],
@@ -64,4 +67,5 @@ else:
             }
         }
     }
-    demisto.results(data)
+
+demisto.results(data)
