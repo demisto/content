1109ed8ccecadeddc990ebbd0a8e8bc79c2a95a6
genians-rnd <integration@genians.com>
2020-09-25 13:47:57 +0900

    Change the product name in the source

--- Packs/GenianNAC/Integrations/GenianNAC/GenianNAC.py
+++ Packs/GenianNAC/Integrations/GenianNAC/GenianNAC.py
@@ -120,7 +120,7 @@ def assign_ip_tag_command():
                 'ReadableContentsFormat': formats['text'],
                 'HumanReadable': hr,
                 'EntryContext': {
-                    "genians.tag.(val.Tag == obj.Tag)": assign_tag
+                    "geniannac.tag.(val.Tag == obj.Tag)": assign_tag
                 }
             })
         else:
@@ -167,7 +167,7 @@ def unassign_ip_tag_command():
                         'ReadableContentsFormat': formats['text'],
                         'HumanReadable': hr,
                         'EntryContext': {
-                            "genians.tag.(val.Tag == obj.Tag)": unassign_tag
+                            "geniannac.tag.(val.Tag == obj.Tag)": unassign_tag
                         }
                     })
                 else:
@@ -187,9 +187,9 @@ def main():
         if demisto.command() == "test-module":
             get_ip_nodeid('8.8.8.8')
             demisto.results('ok')
-        elif demisto.command() == 'genians-assign-ip-tag':
+        elif demisto.command() == 'geniannac-assign-ip-tag':
             assign_ip_tag_command()
-        elif demisto.command() == 'genians-unassign-ip-tag':
+        elif demisto.command() == 'geniannac-unassign-ip-tag':
             unassign_ip_tag_command()
         else:
             raise NotImplementedError("Command {} was not implemented.".format(demisto.command()))
