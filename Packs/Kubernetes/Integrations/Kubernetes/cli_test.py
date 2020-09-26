0d19d12c568b4503f65fc324cda786f3f976cc29
allisterb <allister.beharry@gmail.com>
2020-09-25 22:02:50 -0400

    CLI can list services.

--- Packs/Kubernetes/Integrations/Kubernetes/cli_test.py
+++ Packs/Kubernetes/Integrations/Kubernetes/cli_test.py
@@ -8,29 +8,38 @@ parser.add_argument("namespace", help="The Kubernetes namespace to use.")
 parser.add_argument("--pods", help="List pods in namespace.", action="store_true")
 parser.add_argument("--services", help="List services in namespace.", action="store_true")
 parser.add_argument("--spec", help="List all properties of an object's specification.", action="store_true")
+parser.add_argument("--meta", help="List all properties of an object's metadata.", action="store_true")
 parser.add_argument("--prop", type=str, help="Print a specific property.")
 args = parser.parse_args()
 ns = args.namespace
+
 cluster_host_url = os.environ['KUBERNETES_CLUSTER_HOST_URL']
 token = os.environ['KUBERNETES_CLUSTER_TOKEN']
 k8s = Client(cluster_host_url, token, ns)
 
+obj = ''
+ret = None
 if args.pods:
+    obj = "Pod"
     ret = k8s.list_pods()
-    if len(ret.items) > 0:
-        if args.spec:            
-            if args.prop is None:
-                print ("Pod spec: %s" % dir(ret.items[0].spec))
-            else:
-                print ("Pod %s: %s" % (args.prop, getattr(ret.items[0].spec, args.prop)))            
-        else:
-            print ("Pods in namespace %s:" % ns)
-            for i in ret.items:
-                print("%s\t%s" % (i.metadata.name, i.status.pod_ip))
-    else: print ("No pods returned.")
-
-elif (args.services):
-    print ("Services in namespace %s:" % ns)
+elif args.services:
+    obj = "Service"
     ret = k8s.list_services()
-    for i in ret.items:
-        print("%st%s" % (i.spec, i.metadata))
+else: exit()
+
+if len(ret.items) > 0:
+    if args.spec:            
+        if args.prop is None:
+            print ("%s spec: %s" % (obj, [ x for x in dir(ret.items[0].spec) if not x.startswith('_')]))
+        else:
+            print ("%s %s: %s" % (obj, args.prop, getattr(ret.items[0].spec, args.prop)))            
+    elif args.meta:
+        if args.prop is None:
+            print ("%s metadata: %s" % (obj, [ x for x in dir(ret.items[0].metadata) if not x.startswith('_')]))
+        else:
+            print ("%s %s: %s" % (obj, args.prop, getattr(ret.items[0].metadata, args.prop)))
+    else:
+        print ("%ss in namespace %s:" % (obj, ns))
+        for i in ret.items:
+            print("%s\t%s" % (i.spec, i.metadata))
+else: print ("No %ss returned." % obj)
