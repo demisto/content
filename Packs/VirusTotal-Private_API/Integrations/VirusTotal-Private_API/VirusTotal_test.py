e8256e71d2fea7c6745f96486026047f0a259670
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2020-09-27 11:33:16 +0300

    fix vt private url to return entry per indicator

--- Packs/VirusTotal-Private_API/Integrations/VirusTotal-Private_API/VirusTotal_test.py
+++ Packs/VirusTotal-Private_API/Integrations/VirusTotal-Private_API/VirusTotal_test.py
@@ -14,3 +14,14 @@ def test_get_file_response_queued_response(mocker, requests_mock):
     output = vt.get_file_report_command()
     assert output.get('HumanReadable') == 'The file is queued for analysis. Try again in a short while.'
     assert output.get('EntryContext', {}).get('VirusTotal(val.ID == obj.ID)', {}).get('Status') == 'Queued'
+
+
+def test_get_url_multiple_results(mocker, requests_mock):
+    mocker.patch.object(demisto, 'args', return_value={'resource': 'https://linkedin.com, https://twitter.com'})
+    requests_mock.get('https://www.virustotal.com/vtapi/v2/url/report', json=queued_response)
+    vt = importlib.import_module("VirusTotal-Private_API")
+
+    output = vt.get_url_report_command()
+    assert len(output) == 2
+    assert isinstance(output[0]['DBotScore'], dict)
+    assert isinstance(output[1]['DBotScore'], dict)
