a6c3bd51312372ba6d239fcd77b28895bb4eaa30
adaud <adaud@paloaltonetworks.com>
2020-09-29 13:52:28 +0300

    update docker change return_outputs to CommandResults

--- Packs/CommonScripts/Scripts/URLEncode/URLEncode.py
+++ Packs/CommonScripts/Scripts/URLEncode/URLEncode.py
@@ -1,12 +1,10 @@
 import demistomock as demisto
-
+from urllib.parse import quote, unquote
 ''' MAIN FUNCTION '''
 
 
-def main():
-    from urllib.parse import quote, unquote
-
-    value = demisto.args()["value"]
+def main(args):
+    value = args.get('value')
     decoded_value = unquote(value)
     processed_value = quote(decoded_value)
 
@@ -14,11 +12,13 @@ def main():
         'EncodedURL': processed_value
     }
 
-    return_outputs(processed_value, eContext)
-
-
-''' ENTRY POINT '''
+    return CommandResults(readable_output=processed_value,
+                          outputs=eContext,
+                          raw_response=[])
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
-    main()
+    try:
+        return_results(main(demisto.args()))
+    except Exception as exc:
+        return_error(str(exc), error=exc)
