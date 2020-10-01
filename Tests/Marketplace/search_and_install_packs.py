3db86ff2873f9617c076ad0f2f916069c4d91e47
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-10-01 19:37:28 +0300

    More refactoring

--- Tests/Marketplace/search_and_install_packs.py
+++ Tests/Marketplace/search_and_install_packs.py
@@ -221,9 +221,9 @@ def install_packs(client, host, prints_manager, thread_index, packs_to_install, 
                                      include_timestamp=True)
         with open('./Tests/content_packs_to_install.txt', 'r') as packs_stream:
             pack_ids = packs_stream.readlines()
-            pack_ids_correct = [pack_id.rstrip('\n') for pack_id in pack_ids]
+            pack_ids_to_install = [pack_id.rstrip('\n') for pack_id in pack_ids]
         for local_pack in local_packs:
-            if any(pack_id in local_pack for pack_id in pack_ids_correct):
+            if any(pack_id in local_pack for pack_id in pack_ids_to_install):
                 upload_zipped_packs(client=client, host=host, prints_manager=prints_manager,
                                     thread_index=thread_index, pack_path=local_pack)
     else:
