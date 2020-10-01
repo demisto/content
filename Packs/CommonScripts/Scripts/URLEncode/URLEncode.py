319dd0d1950bb4e233750dd353d97b664644a1db
adaud <adaud@paloaltonetworks.com>
2020-10-01 13:08:52 +0300

    update pr comments

--- Packs/CommonScripts/Scripts/URLEncode/URLEncode.py
+++ Packs/CommonScripts/Scripts/URLEncode/URLEncode.py
@@ -8,15 +8,7 @@ from urllib.parse import quote, unquote
 def main(args):
     value = args.get('value')
     decoded_value = unquote(value)
-    processed_value = quote(decoded_value)
-
-    eContext = {
-        'EncodedURL': processed_value
-    }
-
-    return CommandResults(readable_output=processed_value,
-                          outputs=eContext,
-                          raw_response=[])
+    return quote(decoded_value)
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
