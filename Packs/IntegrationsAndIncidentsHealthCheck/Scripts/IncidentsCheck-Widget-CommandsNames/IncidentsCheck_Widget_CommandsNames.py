3cb71feea983492cc1c327d658bbbfe82bfd8e17
teizenman <teizenman@paloaltonetworks.com>
2020-10-03 18:44:11 +0300

    more UTs, more names changes

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IncidentsCheck-Widget-CommandsNames/IncidentsCheck_Widget_CommandsNames.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IncidentsCheck-Widget-CommandsNames/IncidentsCheck_Widget_CommandsNames.py
@@ -5,19 +5,11 @@ from typing import Counter
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-datesOnlyList = []
-incident = demisto.incidents()
 
-list_data = demisto.executeCommand("getList", {"listName": "XSOAR Health - Failed Incidents Dates"})
-list_content = list_data[0].get('Contents').split(",")
+def parse_data(list_content):
+    lists_data = []
 
-for full_date in list_content:
-    date_only = full_date.split(" ")[0]
-    datesOnlyList.append(date_only)
-
-lists_data: List = []
-if list_content != ['']:
-    list_collections: Counter = collections.Counter(datesOnlyList)
+    list_collections: Counter = collections.Counter(list_content)
     top_lists = list_collections.most_common(10)
     lists_number = len(top_lists)
     list_number = 0
@@ -28,7 +20,7 @@ if list_content != ['']:
             hex_number = str(hex(random_number))  # convert to hexadecimal
             color = '#' + hex_number[2:]  # remove 0x and prepend '#'
 
-            listW_widget_data = {
+            list_widget_data = {
                 "data": [
                     list_element[1]
                 ],
@@ -36,20 +28,29 @@ if list_content != ['']:
                 "color": color
             }
 
-            list_data.append(listW_widget_data)
+            lists_data.append(list_widget_data)
             list_number += 1
 
-    demisto.results(json.dumps(list_data))
+    return lists_data
 
-else:
-    data = {
-        {
+
+def main():
+    list_data = demisto.executeCommand("getList", {"listName": "XSOAR Health - Failed Incident Commands"})
+    list_content = list_data[0].get('Contents', '').split(",")
+
+    if list_content != ['']:
+        data = parse_data(list_content)
+    else:
+        data = {
             "data": [
                 0
             ],
-            "name": "N\A",
-                    "color": "#00CD33"
-        },
+            "name": "N\\A",
+            "color": "#00CD33"
+        }
 
-    }
     demisto.results(json.dumps(data))
+
+
+if __name__ in ["__main__", "builtin", "builtins"]:
+    main()
