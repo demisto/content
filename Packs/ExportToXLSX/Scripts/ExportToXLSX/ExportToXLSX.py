b97074fbcdce0b122090e07864335032a79edcf5
rshalem <rshalem@paloaltonetworks.com>
2020-10-04 19:40:57 +0300

    code fixes

--- Packs/ExportToXLSX/Scripts/ExportToXLSX/ExportToXLSX.py
+++ Packs/ExportToXLSX/Scripts/ExportToXLSX/ExportToXLSX.py
@@ -1,3 +1,6 @@
+import traceback
+from typing import List, Optional
+
 import demistomock as demisto
 import xlsxwriter
 from CommonServerPython import *
@@ -23,22 +26,24 @@ def write_data(sheet, data_item, data_headers, workbook, bold, border):
             col = 0
             row += 1
             for value in data_headers:
-                worksheet.write(row, col, item.get(value, ""), border)
-                col += 1
+                if item.get(value):
+                    worksheet.write(row, col, item.get(value), border)
+                    col += 1
+                else:
+                    raise ValueError(f'The header "{value}" does not exist in the given data item.')
 
 
 def parse_data(data, sheets):
-    if isinstance(data, str):  # check that
-        if len(sheets) <= 1:  # why?
-            return_error("Multiple sheet names are required for an object with multiple data items")
-        data_list = json.loads("[" + data + "]")  # that too
-        data_dict = {}
-        counter = 0
-        for list_item in data_list:
-            data_dict[counter] = list_item
-            counter += 1
+    if isinstance(data, str):  # Indicates that the data is a comma-separated list of context keys.
+        data_list = json.loads("[" + data + "]")
+        if len(sheets) != len(data_list):
+            raise ValueError("Number of sheet names should be equal to the number of data items.")
 
-        return data_dict
+        return data_list
+    else:
+        if len(sheets) != 1:
+            raise ValueError("Number of sheet names should be equal to the number of data items.")
+    return [data]
 
 
 def prepare_bold_and_border(workbook, is_bold, is_border):
@@ -52,53 +57,50 @@ def prepare_bold_and_border(workbook, is_bold, is_border):
 
 
 def main():
-    args = demisto.args()
-    data = args.get("data")
-    file_name = args.get("file_name")
-    sheet_name = args.get("sheet_name")
-    headers = args.get("headers", None)
-    is_bold = argToBool(args.get("bold"))
-    is_border = argToBool(args.get("border"))
+    try:
+        args = demisto.args()
+        data = args.get("data")
+        file_name = args.get("file_name")
+        sheet_name = args.get("sheet_name")
+        headers = args.get("headers", None)
+        is_bold = argToBoolean(args.get("bold", 'true'))
+        is_border = argToBoolean(args.get("border", 'true'))
 
-    sheets = sheet_name.split(",")
-    data = parse_data(data, sheets)
+        sheets = sheet_name.split(",")
+        data = parse_data(data, sheets)
 
-    if len(sheets) > 1 and len(sheets) != len(data):
-        return_error("Number of sheet names should be equal to the number of data items")
+        if len(sheets) != len(data):
+            raise ValueError("Number of sheet names should be equal to the number of data items.")
 
-    if headers:
-        headers_list = headers.split(";")
+        if headers:
+            headers_list = headers.split(";")
 
-        if len(sheets) != len(headers_list):
-            return_error("Number of sheet headers should be equal to the number of sheet names")
-    else:
-        headers_list = None
+            if len(sheets) != len(headers_list):
+                raise ValueError("Number of sheet headers should be equal to the number of sheet names")
+        else:
+            headers_list = None
 
-    workbook = xlsxwriter.Workbook(file_name)
+        workbook = xlsxwriter.Workbook(file_name)
 
-    bold, border = prepare_bold_and_border(workbook, is_bold, is_border)
+        bold, border = prepare_bold_and_border(workbook, is_bold, is_border)
 
-    # if len(sheets) == 1:
-    #     if headers_list:
-    #         data_headers = headers_list[0].split(",")
-    #     else:
-    #         data_headers = None
-    #     write_data(sheets[0], data, data_headers, workbook, bold, border)
-    #
-    # else:
+        multi_header_list: List[Optional[List]] = []
+        if headers_list:  # Can be 1 item in case there is one sheet, or multiple items in case there are multiple
+            # sheets
+            for header_list in headers_list:
+                multi_header_list.append(header_list.split(","))
+        else:
+            multi_header_list = [None] * len(sheets)
+        for sheet, data_item, multi_header_list in zip(sheets, data, multi_header_list):
+            write_data(sheet, data_item, headers_list, workbook, bold, border)
 
-    multi_header_list = []
-    if headers_list:  # Can be 1 item in case there is one sheet, or multiple items in case there are multiple sheets
-        for header_list in headers_list:
-            multi_header_list.append(header_list.split(","))
-    else:
-        multi_header_list = [None] * len(sheets)
-    for sheet, data_item, multi_header_list in zip(sheets, data, multi_header_list):
-        write_data(sheet, data[data_item], multi_header_list, workbook, bold, border)
+        workbook.close()
+        demisto.results(file_result_existing_file(file_name))
 
-    workbook.close()
-    demisto.results(file_result_existing_file(file_name))
+    except Exception as ex:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to execute ExportToXLSX script. Error: {str(ex)}')
 
 
-if __name__ in ('__builtin__', '__main__'):
+if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
