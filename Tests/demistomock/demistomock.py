91dc2a0b3b3bdadd75a90d7666ae4aafe07d91fc
Itay Keren <ikeren@paloaltonetworks.com>
2020-10-06 17:55:13 +0300

    fix createIncidents docstring (#9327)

--- Tests/demistomock/demistomock.py
+++ Tests/demistomock/demistomock.py
@@ -869,17 +869,16 @@ def createIncidents(incidents, lastRun=None, userID=None):
     Creates incident in long running execution
 
     Args:
-      incidents (list):
-        List of incident objects to create, with the following required keys, and some common optional keys
-            - name (required) - str
-            - type (required - if not provided will create unclassified incident) - str
-            - labels (optonal) - list of {"type": _, "value": _} objects
-            - rawJSON (required) - str
-            - occurred (optional) - str
-            - details (optional) - str
-            - severity (optional) - str
+      incidents (list): List of incident objects to create, with the following optional keys:
+            - name (str)
+            - type (str) - If not provided, an Unclassified incident will be created
+            - labels (list) - List of {"type": _, "value": _} objects
+            - rawJSON (str) - Will be omitted after the classification & mapping step
+            - occurred (str)
+            - details (str)
+            - severity (int)
       lastRun (dict): the LastRun object to set (Default value = None)
-      userID lastIndicator: The user associated with the request (Default value = None)
+      userID (str): The user associated with the request (Default value = None)
 
     Returns:
       Union[list, dict]: Created incident object
