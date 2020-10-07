611d3a787e7aed5e1ff600f4118e7ddb2b91cf97
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-10-07 15:23:05 +0300

    Revert keys

--- Tests/scripts/run_private_build_tests.sh
+++ Tests/scripts/run_private_build_tests.sh
@@ -17,20 +17,14 @@ echo "starting configure_and_test_integration_instances"
 python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$BRANCH_NAME" --build-number "$GITHUB_RUN_NUMBER" -pr true
 code_1=$?
 
-echo 'export GOOGLE_APPLICATION_CREDENTIALS="creds.json"' >> $BASH_ENV
-source $BASH_ENV
-cat <<EOF > "$GOOGLE_APPLICATION_CREDENTIALS"
-$GCS_MARKET_KEY
-EOF
-
 
 if [ $code_1 -ne 1 ] ; then
   if [ -n "${NON_AMI_RUN}" ]; then
     # non AMI
-    python3 ./Tests/test_content.py -sa "$GCS_PATH" -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$BRANCH_NAME" -m "$MEM_CHECK" -d "$1" --private true
+    python3 ./Tests/test_content.py -sa "$GCS_MARKET_KEY" -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$BRANCH_NAME" -m "$MEM_CHECK" -d "$1" --private true
   else
     # AMI
-    python3 ./Tests/test_content.py -sa "$GCS_PATH" -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$BRANCH_NAME" -m "$MEM_CHECK" --isAMI true -d "$1" --private true
+    python3 ./Tests/test_content.py -sa "$GCS_MARKET_KEY" -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$GITHUB_RUN_NUMBER" -g "$BRANCH_NAME" -m "$MEM_CHECK" --isAMI true -d "$1" --private true
   fi
 fi
 
@@ -42,6 +36,6 @@ if [ $code_1 -eq 0 ] && [ $code_2 -eq 0 ] ; then
   touch "$filepath"
 fi
 let "exit_code = $code_1 + $code_2"
-#rm $GOOGLE_APPLICATION_CREDENTIALS
+rm $GOOGLE_APPLICATION_CREDENTIALS
 
 exit $exit_code
