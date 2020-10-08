99c72cfc98d2d566be8e13d40bc33aa94d0403b3
Jochman <bhochman@paloaltonetworks.com>
2020-10-08 18:22:32 +0300

    pep8

--- Tests/unlock_integrations.py
+++ Tests/unlock_integrations.py
@@ -8,6 +8,12 @@ if __name__ == "__main__":
     try:
         integration_to_unlock = sys.argv[1]
         client = demisto_client.configure(verify_ssl=False)
-        update_server_configuration(client, {'content.unlock.integrations': integration_to_unlock}, 'Could not update configurations')
+        update_server_configuration(
+            client,
+            {"content.unlock.integrations": integration_to_unlock},
+            "Could not update configurations",
+        )
     except IndexError:
-        print('You must supply an integration to unlock. Can be comma separated values.')
+        print(
+            "You must supply an integration to unlock. Can be comma separated values."
+        )
