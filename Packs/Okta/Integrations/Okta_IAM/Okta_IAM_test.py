1ecb09b149d65cd36baea360d80e385249e94d46
dtavori <dtavori@paloaltonetworks.com>
2020-10-18 21:54:40 +0300

    lint fixes

--- Packs/Okta/Integrations/Okta_IAM/Okta_IAM_test.py
+++ Packs/Okta/Integrations/Okta_IAM/Okta_IAM_test.py
@@ -1,4 +1,3 @@
 # from requests.models import Response
 # import demistomock as demisto
 # from Okta_IAM import Client, get_user_command, create_user_command, update_user_command, enable_disable_user_command
-
