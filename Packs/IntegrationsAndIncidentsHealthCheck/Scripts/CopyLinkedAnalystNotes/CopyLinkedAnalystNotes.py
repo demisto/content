5405ca4167b3bde49962ccd27c93982fdf1bb3ef
abaumgarten <abaumgarten@paloaltonetworks.com>
2020-10-18 10:47:37 +0300

    Incident grid rows are now sorted by creation date

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/CopyLinkedAnalystNotes/CopyLinkedAnalystNotes.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/CopyLinkedAnalystNotes/CopyLinkedAnalystNotes.py
@@ -55,6 +55,9 @@ def main():
 
     if linked_incident:
         main_integration_grid, main_incident_grid = create_grids(custom_fields, linked_incident)
+        if len(main_incident_grid) != 1:
+            # sort incident grid by creation date
+            main_incident_grid.sort(key=lambda incident_dict: incident_dict['creationdate'], reverse=True)
 
         demisto.executeCommand("setIncident", {'customFields': {'integrationstestgrid': main_integration_grid}})
         demisto.executeCommand("setIncident", {'customFields': {'playbooktaskserrors': main_incident_grid}})
