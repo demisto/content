f7a455da46d7a0e36c23eb622f56d9f0513f21e2
guyfreund <gfreund@paloaltonetworks.com>
2020-10-20 11:03:46 +0300

    changed flow

--- Tests/Marketplace/install_packs.sh
+++ Tests/Marketplace/install_packs.sh
@@ -10,6 +10,6 @@ DEMISTO_API_KEY=$(cat $SECRET_CONF_PATH | jq '.temp_apikey')
 echo "starting configure_and_install_packs ..."
 PREVIOUS_JOB_NUMBER=$(cat create_instances_build_num.txt)
 
-python3 ./Tests/configure_and_install_packs.py -s "$SECRET_CONF_PATH" --ami_env "$1" --branch "$CIRCLE_BRANCH" --build-number "$PREVIOUS_JOB_NUMBER"
+python3 ./Tests/Marketplace/configure_and_install_packs.py -s "$SECRET_CONF_PATH" --ami_env "$1" --branch "$CIRCLE_BRANCH" --build-number "$PREVIOUS_JOB_NUMBER"
 
 exit $RETVAL
