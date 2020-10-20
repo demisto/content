cdf1eab12b82580d30e749eb3b482d46e274bd59
jgranot <jgranot@paloaltonetworks.com>
2020-10-20 12:57:16 +0300

    added unittests

--- Packs/CarbonBlackEnterpriseEDR/Integrations/CarbonBlackEnterpriseEDR/CarbonBlackEnterpriseEDR_test.py
+++ Packs/CarbonBlackEnterpriseEDR/Integrations/CarbonBlackEnterpriseEDR/CarbonBlackEnterpriseEDR_test.py
@@ -11,9 +11,9 @@ PROCESS_CASES = [
     ),
     (
         {"process_name": "svchost.exe,vmtoolsd.exe", 'event_id': None, 'query': None,
-         'process_hash': '63d423ea882264dbb157a965c200306212fc5e1c6ddb8cbbb0f1d3b51ecd82e6'},   # args
+         'process_hash': '63d423ea882264dbb157a965c200306212fc5e1c6ddb8cbbb0f1d3b51ecd82e6'},  # args
         {'criteria': {'process_hash': ['63d423ea882264dbb157a965c200306212fc5e1c6ddb8cbbb0f1d3b51ecd82e6'],
-                      "process_name": ["svchost.exe", "vmtoolsd.exe"]}, 'start': 0}   # expected
+                      "process_name": ["svchost.exe", "vmtoolsd.exe"]}, 'start': 0}  # expected
     )
 ]
 
@@ -45,10 +45,47 @@ def test_create_process_search_body(mocker, demisto_args, expected_results):
     assert m.call_args[1].get('json_data') == expected_results
 
 
+PROCESS_BAD_CASES = [
+    (
+        {'process_hash': None, 'process_name': None, 'event_id': None, 'query': None},  # args for missing parameters
+        "To perform an process search, please provide at least one of the following: "
+        "'process_hash', 'process_name', 'event_id' or 'query'"  # expected
+    ),
+
+]
+
+
+@pytest.mark.parametrize('demisto_args,expected_error_msg', PROCESS_BAD_CASES)
+def test_create_process_search_failing(mocker, requests_mock, demisto_args, expected_error_msg):
+    """
+    Given:
+      - search task's argument
+
+    When:
+     - creating a search event by process task
+
+    Then:
+       - validating the body sent to request is matching the search
+    """
+
+    mocker.patch.object(demisto, 'args', return_value=demisto_args)
+    client = cbe.Client(
+        base_url='https://server_url.com',
+        use_ssl=False,
+        use_proxy=False,
+        token=None,
+        cb_org_key="123")
+    mocker.patch.object(client, '_http_request', return_value={})
+
+    with pytest.raises(ValueError) as e:
+        client.create_search_process_request(**demisto_args)
+    assert str(e.value) == expected_error_msg
+
+
 EVENT_CASES = [
     (
         {"process_guid": "1234", 'event_type': 'modload', 'query': None},  # args
-        {'criteria': {'event_type': ['modload']}, 'start': 0}   # expected
+        {'criteria': {'event_type': ['modload']}, 'start': 0}  # expected
     )
 ]
 
@@ -78,3 +115,43 @@ def test_create_event_by_process_search_body(mocker, demisto_args, expected_resu
 
     client.create_search_event_by_process_request(**demisto_args)
     assert m.call_args[1].get('json_data') == expected_results
+
+
+EVENT_BAD_CASES = [
+    (
+        {"process_guid": "1234", 'event_type': 'invalid', 'query': None},  # args for invalid parameters
+        "Only the following event types can be searched: "
+        "'filemod', 'netconn', 'regmod', 'modload', 'crossproc', 'childproc'"  # expected
+    ),
+    (
+        {"process_guid": "1234", 'event_type': None, 'query': None},  # args for missing parameters
+        "To perform an event search, please provide either event_type or query."  # expected
+    )
+]
+
+
+@pytest.mark.parametrize('demisto_args,expected_error_msg', EVENT_BAD_CASES)
+def test_event_by_process_failing(mocker, requests_mock, demisto_args, expected_error_msg):
+    """
+    Given:
+      - search task's argument
+
+    When:
+     - creating a search event by process task
+
+    Then:
+       - validating the body sent to request is matching the search
+    """
+
+    mocker.patch.object(demisto, 'args', return_value=demisto_args)
+    client = cbe.Client(
+        base_url='https://server_url.com',
+        use_ssl=False,
+        use_proxy=False,
+        token=None,
+        cb_org_key="123")
+    mocker.patch.object(client, '_http_request', return_value={})
+
+    with pytest.raises(ValueError) as e:
+        client.create_search_event_by_process_request(**demisto_args)
+    assert str(e.value) == expected_error_msg
