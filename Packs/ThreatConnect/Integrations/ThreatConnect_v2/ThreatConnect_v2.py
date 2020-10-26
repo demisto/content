0ee0c91b1612f6e9fbbe6a4982881b9cd71d4380
adaud <adaud@paloaltonetworks.com>
2020-10-26 18:17:08 +0200

    fix get_indicators method

--- Packs/ThreatConnect/Integrations/ThreatConnect_v2/ThreatConnect_v2.py
+++ Packs/ThreatConnect/Integrations/ThreatConnect_v2/ThreatConnect_v2.py
@@ -255,7 +255,7 @@ def get_indicators(indicator_value=None, indicator_type=None, owners=None, ratin
                 raw_indicators.append(indicator)
     else:
         raw_indicators = get_xindapi(tc, indicator_value, indicator_type, owners)
-        if raw_indicators and loopowners is True:
+        if raw_indicators:
             owners = get_indicator_owner(indicator_value)
             if 'owner' in owners.get('data', {}):
                 for owner in owners['data']['owner']:
