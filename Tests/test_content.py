5383857248fe80e578e0a93e383f37d92874d051
guyfreund <gfreund@paloaltonetworks.com>
2020-10-26 14:59:11 +0200

    merge from master

--- Tests/test_content.py
+++ Tests/test_content.py
@@ -756,6 +756,11 @@ def get_server_numeric_version(ami_env, is_local_run=False):
         return default_version
 
     instances_ami_name = list(instances_ami_names)[0]
+
+    return extract_server_numeric_version(instances_ami_name, default_version)
+
+
+def extract_server_numeric_version(instances_ami_name, default_version):
     # regex doesn't catch Server Master execution
     extracted_version = re.findall(r'Demisto-(?:Circle-CI|Marketplace)-Content-[A-Za-z]*[-_](\d[._]\d)-[\d]{5}',
                                    instances_ami_name)
