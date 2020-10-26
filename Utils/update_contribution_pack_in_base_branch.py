440d52e06937063351c8e398e4ce1c15a9afa32c
mgalitzki <mgalitzki@paloaltonetworks.com>
2020-10-26 17:06:51 +0200

    fixed & added unittest

--- Utils/update_contribution_pack_in_base_branch.py
+++ Utils/update_contribution_pack_in_base_branch.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import shutil
 import sys
+from typing import List
 
 import requests
 from demisto_sdk.commands.common.tools import run_command, print_error, print_success
@@ -18,24 +19,25 @@ def main():
     pr_number = args.pr_number
     repo = args.contrib_repo
     branch = args.branch
-    pack_dir_name = get_pack_dir(branch, pr_number, repo)
-    if not pack_dir_name:
+    packs_dir_names = get_pack_dir(branch, pr_number, repo)
+    if not packs_dir_names:
         print_error('Did not find a pack in the PR')
         sys.exit(1)
+    print(f'Copy the changes from the contributor branch {repo}/{branch} in the packs: {packs_dir_names}')
 
-    pack_dir = f'Packs/{pack_dir_name}'
-    str_dir_names = ''
+    string_dir_names = ''
     try:
-        for pack_dir in pack_dir_name:
-            if os.path.isdir(f'Packs/{pack_dir}'):
+        for pack_dir in packs_dir_names:
+            pack_path = f'Packs/{pack_dir}'
+            if os.path.isdir(pack_path):
                 # Remove existing pack
-                shutil.rmtree(f'Packs/{pack_dir}')
-            str_dir_names += f' Packs/{pack_dir}'
+                shutil.rmtree(pack_path)
+            string_dir_names += f' {pack_path}'
 
         commands = [
             f'git remote add {repo} git@github.com:{repo}/content.git',
             f'git fetch {repo} {branch}',
-            f'git checkout {repo}/{branch}{str_dir_names}'
+            f'git checkout {repo}/{branch}{string_dir_names}'
         ]
 
         for command in commands:
@@ -45,24 +47,23 @@ def main():
         print_error(f'Failed to deploy contributed pack to base branch: {e}')
         sys.exit(1)
 
-    print_success(f'Successfully updated the base branch with the contrib pack {pack_dir_name}')
+    print_success(f'Successfully updated the base branch with the contrib packs: {packs_dir_names}')
 
 
-def get_pack_dir(branch: str, pr_number: str, repo: str):
+def get_pack_dir(branch: str, pr_number: str, repo: str) -> List[str]:
     """
-    Get a pack dir name from a contribution pull request changed files
+    Get a packs dir names from a contribution pull request changed files
     Args:
         branch: The contrib branch
         pr_number: The contrib PR
         repo: The contrib repo
 
     Returns:
-        A list of
-        packs dir name, if found.
+        A list of packs dir names, if found.
     """
 
     page = 1
-    list_packs_dir_name = []
+    list_packs_dir_names = []
     while True:
         response = requests.get(f'https://api.github.com/repos/demisto/content/pulls/{pr_number}/files',
                                 params={'page': str(page)})
@@ -73,12 +74,10 @@ def get_pack_dir(branch: str, pr_number: str, repo: str):
         for pr_file in files:
             if pr_file['filename'].startswith('Packs/'):
                 pack_dir_name = pr_file['filename'].split('/')[1]
-                if pack_dir_name not in list_packs_dir_name:
-                    list_packs_dir_name.append(pack_dir_name)
+                if pack_dir_name not in list_packs_dir_names:
+                    list_packs_dir_names.append(pack_dir_name)
         page += 1
-
-    print(f'Copy the changes from the contributor branch {repo}/{branch} in the pack {list_packs_dir_name}')
-    return list_packs_dir_name
+    return list_packs_dir_names
 
 
 if __name__ == '__main__':
