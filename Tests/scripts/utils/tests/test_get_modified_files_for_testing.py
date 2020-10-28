7d10ca29a898a4c36f724c82c5e7ab458c9e1594
Jochman <bhochman@paloaltonetworks.com>
2020-10-28 15:01:03 +0200

    flake8 fixes

--- Tests/scripts/utils/tests/test_get_modified_files_for_testing.py
+++ Tests/scripts/utils/tests/test_get_modified_files_for_testing.py
@@ -1,11 +1,15 @@
 import pytest
 
-from Tests.scripts.utils.collect_helpers import COMMON_YML_LIST
-from Tests.scripts.utils.get_modified_files_for_testing import get_modified_files_for_testing
+from Tests.scripts.utils.get_modified_files_for_testing import (
+    get_modified_files_for_testing,
+)
 
 
 def mock_get_dict_from_yaml(mocker, _dict: dict, ext: str):
-    mocker.patch('demisto_sdk.commands.common.tools.get_dict_from_file', return_value=(_dict, ext))
+    mocker.patch(
+        "demisto_sdk.commands.common.tools.get_dict_from_file",
+        return_value=(_dict, ext),
+    )
 
 
 class TestGetModifiedFilesForTesting:
@@ -46,8 +50,7 @@ class TestGetModifiedFilesForTesting:
 
     def test_yaml_file(self, mocker):
         diff_line = "M      Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.yml"
-        mock_get_dict_from_yaml(mocker, {"category": "c"
-                                         }, "yml")
+        mock_get_dict_from_yaml(mocker, {"category": "c"}, "yml")
         (
             modified_files_list,
             modified_tests_list,
@@ -118,8 +121,7 @@ class TestGetModifiedFilesForTesting:
             "R100	Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.yml	"
             "Packs/NewHelloWorld/Integrations/HelloWorld/NewHelloWorld.yml"
         )
-        mock_get_dict_from_yaml(mocker, {"category": "c"
-                                         }, "yml")
+        mock_get_dict_from_yaml(mocker, {"category": "c"}, "yml")
         (
             modified_files_list,
             modified_tests_list,
@@ -130,7 +132,9 @@ class TestGetModifiedFilesForTesting:
             is_reputations_json,
             is_indicator_json,
         ) = get_modified_files_for_testing(diff_line)
-        assert modified_files_list == ["Packs/NewHelloWorld/Integrations/HelloWorld/NewHelloWorld.yml"]
+        assert modified_files_list == [
+            "Packs/NewHelloWorld/Integrations/HelloWorld/NewHelloWorld.yml"
+        ]
         assert modified_tests_list == []
         assert changed_common == []
         assert is_conf_json is False
@@ -141,8 +145,7 @@ class TestGetModifiedFilesForTesting:
 
     def test_test_playbook(self, mocker):
         diff_line = "M Packs/HelloWorld/TestPlaybooks/HelloWorld.yml"
-        mock_get_dict_from_yaml(mocker, {"tasks": "c"
-                                         }, "yml")
+        mock_get_dict_from_yaml(mocker, {"tasks": "c"}, "yml")
         (
             modified_files_list,
             modified_tests_list,
@@ -209,7 +212,9 @@ class TestGetModifiedFilesForTesting:
         diff_line = f"M    Packs/Base/Scripts/CommonServerPython/CommonServerPython.py"
         mocker.patch(
             "Tests.scripts.utils.get_modified_files_for_testing.glob.glob",
-            return_value=["Packs/Base/Scripts/CommonServerPython/CommonServerPython.yml"],
+            return_value=[
+                "Packs/Base/Scripts/CommonServerPython/CommonServerPython.yml"
+            ],
         )
         mock_get_dict_from_yaml(mocker, {"category": "cat"}, "yml")
         (
@@ -224,12 +229,15 @@ class TestGetModifiedFilesForTesting:
         ) = get_modified_files_for_testing(diff_line)
         assert modified_files_list == []
         assert modified_tests_list == []
-        assert changed_common == ["Packs/Base/Scripts/CommonServerPython/CommonServerPython.yml"]
+        assert changed_common == [
+            "Packs/Base/Scripts/CommonServerPython/CommonServerPython.yml"
+        ]
         assert is_conf_json is False
         assert sample_tests == []
         assert modified_metadata_list == set()
         assert is_reputations_json is False
         assert is_indicator_json is False
+
     @pytest.mark.parametrize(
         "path",
         (
@@ -326,7 +334,7 @@ class TestGetModifiedFilesForTesting:
 
     def test_indicator_fields(self, mocker):
         diff_line = "M Packs/HelloWorld/IndicatorFields/sample-field.json"
-        mock_get_dict_from_yaml(mocker, {"id": "indicator-sample-field"}, 'json')
+        mock_get_dict_from_yaml(mocker, {"id": "indicator-sample-field"}, "json")
         (
             modified_files_list,
             modified_tests_list,
@@ -368,10 +376,10 @@ class TestGetModifiedFilesForTesting:
         assert is_reputations_json is False
         assert is_indicator_json is False
 
-    @pytest.mark.parametrize("file_path", (
-        "Tests/scripts/integration-test.yml",
-        "Tests/Util/Scripts/new_script.py"
-    ))
+    @pytest.mark.parametrize(
+        "file_path",
+        ("Tests/scripts/integration-test.yml", "Tests/Util/Scripts/new_script.py"),
+    )
     def test_sample(self, file_path):
         diff_line = "M Tests/Util/Scripts/new_script.py"
         py_file = "Tests/Util/Scripts/new_script.py"
