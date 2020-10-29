58594980f7a57df3526f34735bad07c7d988a06e
sberman <sberman@paloaltonetworks.com>
2020-10-29 15:41:46 +0200

    Added test + fixed cr's

--- Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
+++ Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
@@ -2034,7 +2034,8 @@ def update_remote_system_command(client: Client, args: Dict[str, Any], params: D
     ticket_id = parsed_args.remote_incident_id
     if parsed_args.incident_changed:
         demisto.debug(f'Incident changed: {parsed_args.incident_changed}')
-        if ticket_type == 'sc_task' and parsed_args.inc_status == 2 and params.get('close_ticket'):
+        # Closing sc_type ticket. This ticket type can be closed only when changing the ticket state.
+        if ticket_type == 'sc_task' and parsed_args.inc_status == IncidentStatus.DONE and params.get('close_ticket'):
             parsed_args.data['state'] = '3'
         fields = get_ticket_fields(parsed_args.data, ticket_type=ticket_type)
         if not params.get('close_ticket'):
