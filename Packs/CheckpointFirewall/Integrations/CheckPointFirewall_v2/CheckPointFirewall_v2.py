eb7d143088ca915792a45a9846d9b7f8a3f9c6e1
Adi Daud <46249224+adi88d@users.noreply.github.com>
2020-11-01 15:21:05 +0200

    Update CheckPointFirewall_v2.py

--- Packs/CheckpointFirewall/Integrations/CheckPointFirewall_v2/CheckPointFirewall_v2.py
+++ Packs/CheckpointFirewall/Integrations/CheckPointFirewall_v2/CheckPointFirewall_v2.py
@@ -260,7 +260,7 @@ class Client(BaseClient):
                                       headers=self.headers,
                                       json_data={'limit': limit, 'offset': offset})
         return response.get('packages')
-      
+
     def list_package(self, identifier: str):
         return self._http_request(method='POST', url_suffix='show-package',
                                   headers=self.headers, json_data={'name': identifier})
