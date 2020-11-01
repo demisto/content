2293c1d1a9392b5183c48c4e6ff6eed359a2821f
cshayner <cshayner@paloaltonetworks.com>
2020-11-01 10:22:44 +0200

    comment

--- Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail.py
+++ Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail.py
@@ -645,8 +645,8 @@ class MsGraphClient:
         """
         target_modified_time = add_second_to_str_date(last_fetch)  # workaround to Graph API bug
         suffix_endpoint = f"/users/{self._mailbox_to_fetch}/mailFolders/{folder_id}/messages"
-        # If you add to the select filter the $ sign, 'internetMessageHeaders' field returns from the API empty,
-        # (looks like a bug in the API).
+        # If you add to the select filter the $ sign, The 'internetMessageHeaders' field not contained within the
+        # API response, (looks like a bug in graph API).
         params = {
             "$filter": f"receivedDateTime gt {target_modified_time}",
             "$orderby": "receivedDateTime asc",
