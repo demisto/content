76d57b20b42708ef233394aba10f8cf3163a66bd
dkoval <dkoval@paloaltonetworks.com>
2020-11-01 18:03:23 +0200

    added types at the yml outputs

--- Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
+++ Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
@@ -1,6 +1,6 @@
-import demistomock as demisto
-from CommonServerPython import *
-from CommonServerUserPython import *
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
 
 from datetime import timezone
 import secrets
