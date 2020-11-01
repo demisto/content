6b44642f4f34572de34cd3837e7c6f1da0f600f9
bar katzir <bkatzir@paloaltonetworks.com>
2020-11-01 15:45:06 +0200

    'threatvault-antivirus-signature-search' 'threatvault-dns-signature-search' 'threatvault-antispyware-signature-search'

--- Packs/PaloAltoNetworks_Threat_Vault/Integrations/Threat_Vault/Threat_Vault.py
+++ Packs/PaloAltoNetworks_Threat_Vault/Integrations/Threat_Vault/Threat_Vault.py
@@ -57,6 +57,39 @@ class Client(BaseClient):
         """
         return self._http_request(method='GET', url_suffix=f'/ip/{ip_}/geolocation', params=self._params)
 
+    def search_request(self, path: str, signature_name: str = '', from_: int = 0, size: int = 10,
+                       domain_name: str = '', vendor : str = '', cve: str = '') -> dict:
+        """Initiate a search by sending a POST request.
+
+        Args:
+            path: API endpoint path to search.
+            signature_name: signature name.
+            from_: todo
+            size: todo
+            domain_name: domain name
+            vendor: vendor ID
+            cve: cve ID
+        Returns:
+            Response from API.
+        """
+        if path == 'dns':  # DNS search
+            if signature_name and domain_name:
+                raise Exception('Please provide either a signature_name or a domain_name')
+
+        data = {
+            'from': from_,
+            'size': size
+        }
+        if signature_name:
+            data['field'] = 'signatureName'
+            data['value'] = signature_name
+        if domain_name:
+            data['field'] = 'domainName'
+            data['value'] = domain_name
+
+        return self._http_request(method='POST', url_suffix=f'/threatvault/{path}/search', params=self._params,
+                                  json_data=data)
+
 
 def test_module(client: Client, *_) -> str:
     """Performs basic get request to get a DNS signature
@@ -82,6 +115,7 @@ def antivirus_signature_get(client: Client, args: dict) -> CommandResults:
         CommandResults.
     """
     sha256 = str(args.get('sha256', ''))
+
     response = client.antivirus_signature_get_request(sha256)
     demisto.log(str(response))
     # headers = ['objectId', 'alias', 'firstName', 'middleName', 'lastName', 'email']
@@ -110,6 +144,7 @@ def dns_get_by_id(client: Client, args: dict) -> CommandResults:
         CommandResults.
     """
     dns_signature_id = str(args.get('dns_signature_id', ''))
+
     response = client.dns_signature_get_request(dns_signature_id)
 
     headers = ['signatureId', 'signatureName', 'domainName', 'createTime', 'category']
@@ -135,7 +170,9 @@ def antispyware_get_by_id(client: Client, args: dict) -> CommandResults:
         CommandResults.
     """
     signature_id = str(args.get('signature_id', ''))
+
     response = client.antispyware_get_by_id_request(signature_id)
+
     headers = ['signatureId', 'signatureName', 'signatureType', 'status', 'firstReleaseTime', 'latestReleaseTime']
     readable_output = tableToMarkdown(name=f"Anti Spyware Signature:", t=response, headers=headers, removeNull=True)
 
@@ -159,12 +196,95 @@ def ip_geo_get(client: Client, args: dict) -> CommandResults:
         CommandResults.
     """
     ip_ = str(args.get('ip', ''))
+
     response = client.ip_geo_get_request(ip_)
+
     readable_output = tableToMarkdown(name=f"Anti Spyware Signature:", t=response, removeNull=True)
 
     return CommandResults(
         outputs_prefix=f'{client.name}.IP',
         outputs_key_field='ipAddress',
+        outputs=response,
+        readable_output=readable_output,
+        raw_response=response
+    )
+
+
+def antivirus_signature_search(client: Client, args: dict) -> CommandResults:
+    """Initiate antivirus signature search
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        CommandResults.
+    """
+    signature_name = str(args.get('signature_name', ''))
+    from_ = int(args.get('from', 0))
+    size = int(args.get('size', 10))
+
+    response = client.search_request('panav', signature_name, from_, size)
+    readable_output = tableToMarkdown(name=f"Antivirus Signature Search:", t=response, removeNull=True)
+
+    return CommandResults(
+        outputs_prefix=f'{client.name}.Search',
+        outputs_key_field='search_request_id',
+        outputs=response,
+        readable_output=readable_output,
+        raw_response=response
+    )
+
+
+def dns_signature_search(client: Client, args: dict) -> CommandResults:
+    """Initiate DNS signature search
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        CommandResults.
+    """
+    signature_name = str(args.get('signature_name', ''))
+    from_ = int(args.get('from', 0))
+    size = int(args.get('size', 10))
+    domain_name = str(args.get('domain_name', ''))
+
+    response = client.search_request('dns', signature_name, from_, size, domain_name=domain_name)
+    readable_output = tableToMarkdown(name=f"DNS Signature Search:", t=response, removeNull=True)
+
+    return CommandResults(
+        outputs_prefix=f'{client.name}.Search',
+        outputs_key_field='search_request_id',
+        outputs=response,
+        readable_output=readable_output,
+        raw_response=response
+    )
+
+
+def antispyware_signature_search(client: Client, args: dict) -> CommandResults:
+    """Initiate anti spyware signature search
+
+    Args:
+        client: Client object with request.
+        args: Usually demisto.args()
+
+    Returns:
+        CommandResults.
+    """
+    signature_name = str(args.get('signature_name', ''))
+    from_ = int(args.get('from', 0))
+    size = int(args.get('size', 10))
+    vendor = str(args.get('vendor', ''))
+    cve = str(args.get('cve', ''))
+
+    response = client.search_request('ips', signature_name, from_, size, vendor=vendor, cve=cve)
+    readable_output = tableToMarkdown(name=f"Anti Spyware Signature Search:", t=response, removeNull=True)
+
+    return CommandResults(
+        outputs_prefix=f'{client.name}.Search',
+        outputs_key_field='search_request_id',
         outputs=response,
         readable_output=readable_output,
         raw_response=response
@@ -189,6 +309,10 @@ def main():
             'threatvault-dns-signature-get-by-id': dns_get_by_id,
             'threatvault-antispyware-signature-get-by-id': antispyware_get_by_id,
             'threatvault-ip-geo-get': ip_geo_get,
+            'threatvault-antivirus-signature-search': antivirus_signature_search,
+            'threatvault-dns-signature-search': dns_signature_search,
+            'threatvault-antispyware-signature-search': antispyware_signature_search,
+            # 'threatvault-signature-search-results', signature_search_results,
         }
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration Test button.
