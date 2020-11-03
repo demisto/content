0d51cfd44aa1cc49357ed27afaf3f3b7419d0b47
cshayner <cshayner@paloaltonetworks.com>
2020-11-03 21:08:43 +0200

    formatting script

---
+++ Packs/CommonScripts/Scripts/VerifyIPv6Indicator/VerifyIPv6Indicator_test.py
@@ -0,0 +1,21 @@
+import demistomock as demisto
+from VerifyIPv6Indicator import is_valid_ipv6_address, main
+import pytest
+
+
+@pytest.mark.parametrize(
+    "address, expected",
+    [
+        ('12::34::56', None),
+        ('1:2:3:4:5:6:7:8', '1:2:3:4:5:6:7:8'),
+        ('1:2:3:4::5::7', None),
+        ('1::3:4:5:6:7:8', '1::3:4:5:6:7:8'),
+        ('1::2::3::4::5::6::7::8', None),
+        ('1abc:2abc::4de:1', '1abc:2abc::4de:1')
+
+
+    ]
+)
+def test_set_limit(address, expected):
+    ipv6_address = is_valid_ipv6_address(address)
+    assert ipv6_address == expected
