2634b8c9af2ae4eba889ceb5a675408cf94d06a1
guyfreund <gfreund@paloaltonetworks.com>
2020-11-05 11:29:05 +0200

    packs

--- Tests/scripts/slack_notifier.sh
+++ Tests/scripts/slack_notifier.sh
@@ -5,6 +5,7 @@ echo "start slack notifier"
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 [ -n "${BUCKET_UPLOAD}" ] && IS_BUCKET_UPLOAD=true || IS_BUCKET_UPLOAD=false
 [ -n "${2}" ] && ENV_FILE="$2" || ENV_FILE="fake_path"
+echo "$ENV_FILE"
 
 python3 ./Tests/scripts/slack_notifier.py -n $IS_NIGHTLY -u "$CIRCLE_BUILD_URL" -b "$CIRCLE_BUILD_NUM" -s "$SLACK_TOKEN" -c "$CIRCLECI_TOKEN" -t $1 -f "$2" -bu $IS_BUCKET_UPLOAD -j "$CIRCLE_JOB" -ca $CIRCLE_ARTIFACTS
 
