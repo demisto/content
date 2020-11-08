dd02f957c0c1c8fe62cd905b5d5254e2996f6613
guyfreund <gfreund@paloaltonetworks.com>
2020-11-08 16:34:24 +0200

    force

--- Tests/Marketplace/copy_and_upload_packs.py
+++ Tests/Marketplace/copy_and_upload_packs.py
@@ -293,8 +293,6 @@ def main():
     if production_base_path:
         GCPConfig.STORAGE_BASE_PATH = production_base_path
 
-    # TODO: refactor force upload
-
     # Download and extract build and prod index from build and prod buckets
     build_index_folder_path, build_index_blob, build_index_generation = \
         download_and_extract_index(build_bucket, extract_destination_path)
--- Tests/scripts/prepare_content_packs_for_testing.sh
+++ Tests/scripts/prepare_content_packs_for_testing.sh
@@ -69,7 +69,7 @@ else
       PACKS_LIST="all"
     fi
   fi
-  python3 ./Tests/Marketplace/upload_packs.py -a $PACK_ARTIFACTS -d $CIRCLE_ARTIFACTS/packs_dependencies.json -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s $KF -n $CIRCLE_BUILD_NUM -o $OVERRIDE_ALL_PACKS -sb $TARGET_PATH -k $PACK_SIGNING_KEY -rt $REMOVE_PBS --id_set_path $ID_SET -pb 'marketplace-dist-private' -bu $BUCKET_UPLOAD_FLOW -p "$PACKS_LIST"
+  python3 ./Tests/Marketplace/upload_packs.py -a $PACK_ARTIFACTS -d $CIRCLE_ARTIFACTS/packs_dependencies.json -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s $KF -n $CIRCLE_BUILD_NUM -o $OVERRIDE_ALL_PACKS -sb $TARGET_PATH -k $PACK_SIGNING_KEY -rt $REMOVE_PBS --id_set_path $ID_SET -pb 'marketplace-dist-private' -bu $BUCKET_UPLOAD_FLOW -p "$PACKS_LIST" -c "$FORCE_PREVIOUS_COMMIT"
   echo "Finished updating content packs successfully."
 fi
 
