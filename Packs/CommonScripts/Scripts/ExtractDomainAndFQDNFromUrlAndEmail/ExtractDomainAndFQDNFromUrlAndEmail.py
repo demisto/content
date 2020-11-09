d40996c38b4a2aad0b5aa02a5e6de4a58a92c130
cshayner <cshayner@paloaltonetworks.com>
2020-11-09 14:55:28 +0200

    test without fix changes

--- Packs/CommonScripts/Scripts/ExtractDomainAndFQDNFromUrlAndEmail/ExtractDomainAndFQDNFromUrlAndEmail.py
+++ Packs/CommonScripts/Scripts/ExtractDomainAndFQDNFromUrlAndEmail/ExtractDomainAndFQDNFromUrlAndEmail.py
@@ -114,15 +114,9 @@ def main():
             "ContentsFormat": formats["json"],
             "Contents": [extract_fqdn_or_domain(item, is_fqdn=True), extract_fqdn_or_domain(item, is_domain=True)]
         }
-        if input_entry.get("Contents") == ['', '']:
-            entries_list.append([])
-        else:
-            entries_list.append(input_entry)
-    if entries_list:
-        demisto.results(entries_list)
-    else:
-        # Return empty string so it wouldn't create an empty domain indicator.
-        demisto.results('')
+        entries_list.append(input_entry)
+
+    demisto.results(entries_list)
 
 
 # python2 uses __builtin__ python3 uses builtins
