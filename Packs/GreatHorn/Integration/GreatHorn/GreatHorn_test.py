a534244b0c3f921759041b3fe8f2b61c506a40bd
Eric <vector@tmhk.it>
2020-11-08 22:14:05 -0500

    More unit tests

--- Packs/GreatHorn/Integration/GreatHorn/GreatHorn_test.py
+++ Packs/GreatHorn/Integration/GreatHorn/GreatHorn_test.py
@@ -30,6 +30,28 @@ def test_gh_get_policy_command(requests_mock):
     assert response.outputs_key_field == 'id'
 
 
+def test_gh_search_message_command(requests_mock):
+    from GreatHorn import Client, gh_search_message_command
+    mock_response = util_load_json('test_data/message.json')
+    requests_mock.post('https://api.greathorn.com/v2/search/events', json=mock_response)
+
+    client = Client(
+        base_url='https://api.greathorn.com/v2',
+        verify=False,
+        headers={
+            'Authentication': 'Bearer some_api_key'
+        })
+
+    args = {
+        "filters": '[{"eventId":["15708"]}]'
+    }
+
+    response = gh_search_message_command(client, args)
+
+    assert response.outputs_prefix == 'GreatHorn.Message'
+    assert response.outputs_key_field == 'eventId'
+
+
 def test_gh_get_message_command(requests_mock):
     from GreatHorn import Client, gh_get_message_command
     mock_response = util_load_json('test_data/message.json')
@@ -50,4 +72,49 @@ def test_gh_get_message_command(requests_mock):
 
     assert response.outputs_prefix == 'GreatHorn.Message'
     assert response.outputs_key_field == 'eventId'
-    assert response.outputs == mock_response["results"][0]
+
+
+def test_gh_remediate_message_command(requests_mock):
+    from GreatHorn import Client, gh_remediate_message_command
+    mock_response = util_load_json('test_data/remediate_success.json')
+    requests_mock.post('https://api.greathorn.com/v2/remediation/quarantine', json=mock_response)
+
+    client = Client(
+        base_url='https://api.greathorn.com/v2',
+        verify=False,
+        headers={
+            'Authentication': 'Bearer some_api_key'
+        })
+
+    args = {
+        "eventId": "14807",
+        "action": "quarantine"
+    }
+
+    response = gh_remediate_message_command(client, args)
+
+    assert response.outputs_prefix == 'GreatHorn.Remediation'
+    assert response.outputs_key_field == 'eventId'
+
+
+def test_gh_revert_remediate_message_command(requests_mock):
+    from GreatHorn import Client, gh_revert_remediate_message_command
+    mock_response = util_load_json('test_data/revert_success.json')
+    requests_mock.post('https://api.greathorn.com/v2/remediation/revert/quarantine', json=mock_response)
+
+    client = Client(
+        base_url='https://api.greathorn.com/v2',
+        verify=False,
+        headers={
+            'Authentication': 'Bearer some_api_key'
+        })
+
+    args = {
+        "eventId": "14807",
+        "action": "quarantinerelease"
+    }
+
+    response = gh_revert_remediate_message_command(client, args)
+
+    assert response.outputs_prefix == 'GreatHorn.Remediation'
+    assert response.outputs_key_field == 'eventId'
