bd9046d827c10ac74842f236b7e26bcbd79926e0
guyfreund <gfreund@paloaltonetworks.com>
2020-11-09 21:21:53 +0200

    slack debug

--- Tests/scripts/slack_notifier.sh
+++ Tests/scripts/slack_notifier.sh
@@ -4,6 +4,8 @@ echo "start slack notifier"
 
 [ -n "${NIGHTLY}" ] && IS_NIGHTLY=true || IS_NIGHTLY=false
 [ -n "${BUCKET_UPLOAD}" ] && IS_BUCKET_UPLOAD=true || IS_BUCKET_UPLOAD=false
+echo $IS_BUCKET_UPLOAD
+echo $BUCKET_UPLOAD
 
 python3 ./Tests/scripts/slack_notifier.py -n $IS_NIGHTLY -u "$CIRCLE_BUILD_URL" -b "$CIRCLE_BUILD_NUM" -s "$SLACK_TOKEN" -c "$CIRCLECI_TOKEN" -t $1 -f "$2" -bu $IS_BUCKET_UPLOAD -j "$CIRCLE_JOB" -ca $CIRCLE_ARTIFACTS
 
