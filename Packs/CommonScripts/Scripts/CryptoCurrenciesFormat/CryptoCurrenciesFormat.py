a4517d35bce0e0036acc304d9bbe8180c02dc257
bgenish <bgenish@paloaltonetworks.com>
2020-11-09 16:39:46 +0200

    Removed reputation script (will be replaced with a new integration)

--- Packs/CommonScripts/Scripts/CryptoCurrenciesFormat/CryptoCurrenciesFormat.py
+++ Packs/CommonScripts/Scripts/CryptoCurrenciesFormat/CryptoCurrenciesFormat.py
@@ -1,8 +1,8 @@
-from typing import Union
-
 import demistomock as demisto
 from hashlib import sha256
 from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
+
+from typing import Union
 
 DIGITS58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
@@ -18,7 +18,8 @@ def verify_is_bitcoin(address) -> Union[bytes, bool]:
     try:
         bitcoin_bytes = decode_base58(address, 25)
         '''Check if the last four bytes are equal to the
-        first four bytes of a double SHA-256 digest of the previous 21 bytes.'''
+        first four bytes of a double SHA-256 digest of the previous 21 bytes.
+        Source: https://rosettacode.org/wiki/Bitcoin/address_validation#Python '''
         return bitcoin_bytes[-4:] == sha256(sha256(bitcoin_bytes[:-4]).digest()).digest()[:4]
     except Exception:
         return False
@@ -28,6 +29,7 @@ def main():
     address_list = argToList(demisto.args().get('input'))
 
     list_results = [address for address in address_list if verify_is_bitcoin(address)]
+
     if list_results:
         demisto.results(list_results)
     else:
