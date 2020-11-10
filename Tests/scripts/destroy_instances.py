7dbb23be24377513e2320d4d27e23cc03f3d2777
sberman <sberman@paloaltonetworks.com>
2020-11-10 13:22:20 +0200

    destroy instances - server logs

--- Tests/scripts/destroy_instances.py
+++ Tests/scripts/destroy_instances.py
@@ -2,11 +2,14 @@ import sys
 import os
 import json
 import subprocess
+import logging
 from demisto_sdk.commands.common.tools import print_warning, print_error
 import Tests.scripts.awsinstancetool.aws_functions as aws_functions
+from Tests.scripts.utils.log_util import install_logging
 
 
 def main():
+    install_logging('Destroy_instances.log')
     circle_aritfact = sys.argv[1]
     env_file = sys.argv[2]
     instance_role = sys.argv[3]
@@ -16,18 +19,19 @@ def main():
 
     filtered_results = [env_result for env_result in env_results if env_result["Role"] == instance_role]
     for env in filtered_results:
-        print(f'Downloading server log from {env.get("Role", "Unknown role")}')
+        logging.info(f'Downloading server log from {env.get("Role", "Unknown role")}')
         ssh_string = 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null {}@{} ' \
                      '"sudo chmod -R 755 /var/log/demisto"'
         scp_string = 'scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ' \
                      '{}@{}:/var/log/demisto/server.log {} || echo "WARN: Failed downloading server.log"'
 
         try:
+            logging.debug(f'Trying to run {ssh_string}')
             subprocess.check_output(
                 ssh_string.format(env["SSHuser"], env["InstanceDNS"]), shell=True)
 
         except subprocess.CalledProcessError as exc:
-            print(exc.output)
+            logging.exception(exc.output)
 
         try:
             server_ip = env["InstanceDNS"].split('.')[0]
@@ -39,18 +43,18 @@ def main():
                 shell=True)
 
         except subprocess.CalledProcessError as exc:
-            print(exc.output)
+            logging.exception(exc.output)
 
         if time_to_live:
-            print(f'Skipping - Time to live was set to {time_to_live} minutes')
-            return
+            logging.info(f'Skipping - Time to live was set to {time_to_live} minutes')
+            continue
         if os.path.isfile("./Tests/is_build_passed_{}.txt".format(env["Role"].replace(' ', ''))):
-            print(f'Destroying instance {env.get("Role", "Unknown role")}')
+            logging.info(f'Destroying instance {env.get("Role", "Unknown role")}')
             rminstance = aws_functions.destroy_instance(env["Region"], env["InstanceID"])
             if aws_functions.isError(rminstance):
-                print_error(rminstance)
+                logging.error(rminstance)
         else:
-            print_warning(f'Tests failed on {env.get("Role", "Unknown role")}, keeping instance alive')
+            logging.warning(f'Tests failed on {env.get("Role", "Unknown role")}, keeping instance alive')
 
 
 if __name__ == "__main__":
