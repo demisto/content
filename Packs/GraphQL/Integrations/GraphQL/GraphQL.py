7cf2d95bf3ac04060bdc94482d4e14a9651628e0
ikeren <itay@demisto.com>
2020-11-12 18:37:43 +0200

    graphql initial commit

---
+++ Packs/GraphQL/Integrations/GraphQL/GraphQL.py
@@ -0,0 +1,56 @@
+from typing import Dict
+
+import urllib3
+from gql import Client, gql
+from gql.transport.requests import RequestsHTTPTransport
+
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+
+# Disable insecure warnings
+urllib3.disable_warnings()
+
+
+def execute_query(client: Client, args: Dict) -> CommandResults:
+    query = gql(args.get('query'))
+    result = client.execute(query)
+    return CommandResults(outputs=result, outputs_prefix='GraphQL')
+
+
+def main() -> None:
+    command = demisto.command()
+    try:
+        params = demisto.params()
+        request_params = {
+            'url': params.get('url'),
+            'verify': not params.get('insecure', False),
+            'retries': 3,
+        }
+        if params.get('credentials'):
+            request_params['auth'] = (
+                params.get('credentials').get('identifier'), params.get('credentials').get('password')
+            )
+        if params.get('headers'):
+            try:
+                request_params['headers'] = json.loads(params.get('headers'))
+            except json.JSONDecodeError as e:
+                raise ValueError(f'Headers are not in valid JSON format: {e}')
+
+        transport = RequestsHTTPTransport(**request_params)
+        if not params.get('proxy'):
+            transport.session.trust_env = False
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+
+        demisto.debug(f'Command being called is {command}')
+        if command == 'test-module':
+            return_results('ok')
+        elif command == 'graphql-query':
+            return_results(execute_query(client, demisto.args()))
+    except Exception as e:
+        demisto.error(traceback.format_exc())
+        return_error(f'Failed to execute {command} command. Error: {str(e)}')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
---
+++ Packs/GraphQL/Integrations/GraphQL/README.md
@@ -0,0 +1,60 @@
+Generic GraphQL client to interact with any GraphQL server API.
+
+## Configure GraphQL on Cortex XSOAR
+
+1. Navigate to **Settings** > **Integrations** > **Servers & Services**.
+2. Search for GraphQL.
+3. Click **Add instance** to create and configure a new integration instance.
+
+| **Parameter** | **Description** | **Required** |
+| --- | --- | --- |
+| url | GraphQL Server URL \(e.g. https://countries.trevorblades.com\) | True |
+| credentials | Username | False |
+| headers | HTTP Headers | False |
+| insecure | Trust any certificate \(not secure\) | False |
+| proxy | Use system proxy settings | False |
+
+4. Click **Test** to validate the URLs, token, and connection.
+## Commands
+You can execute these commands from the Cortex XSOAR CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
+### graphql-query
+***
+Execute a query request to the GraphQL server.
+
+
+#### Base Command
+
+`graphql-query`
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| query | The GraphQL query to execute. | Required | 
+
+
+#### Context Output
+
+There is no context output for this command.
+
+#### Command Example
+```!graphql-query query=`{country(code: "BR"){name}}````
+
+#### Context Example
+```json
+{
+    "GraphQL": {
+        "country": {
+            "name": "Brazil"
+        }
+    }
+}
+```
+
+#### Human Readable Output
+
+>### Results
+>|country|
+>|---|
+>| name: Brazil |
+
