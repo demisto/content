3b04e559f254cf3e41964f52c1f194b62b65ad05
hod-alpert <haplert@paloaltonetworks.com>
2020-11-12 16:28:54 +0200

    Added a method to implement the changes

--- Tests/configure_and_test_integration_instances.py
+++ Tests/configure_and_test_integration_instances.py
@@ -1072,7 +1072,7 @@ def install_packs(build, pack_ids=None):
 
 def configure_server_instances(build: Build, tests_for_iteration, all_new_integrations, modified_integrations):
     old_module_instances = []
-    brand_new_integrations = []
+    new_module_instances = []
     testing_client = build.servers[0].client
     for test in tests_for_iteration:
         integrations = get_integrations_for_test(test, build.skipped_integrations_conf)
@@ -1107,18 +1107,46 @@ def configure_server_instances(build: Build, tests_for_iteration, all_new_integr
             logging.error(f'failed setting parameters for integrations: {integrations_to_configure}')
         if not (new_ints_params_set and ints_to_configure_params_set):
             continue
-        for integration in integrations_to_configure:
-            placeholders_map = {'%%SERVER_HOST%%': build.servers[0]}
-            module_instance = configure_integration_instance(integration, testing_client, placeholders_map)
-            if module_instance:
-                old_module_instances.append(module_instance)
 
-        for integration in new_integrations:
-            placeholders_map = {'%%SERVER_HOST%%': build.servers[0]}
-            module_instance = configure_integration_instance(integration, testing_client, placeholders_map)
-            if module_instance:
-                brand_new_integrations.append(module_instance)
-    return old_module_instances, brand_new_integrations
+        old_module_instances_for_test, new_module_instances_for_test = configure_old_and_new_integrations(
+            build,
+            integrations_to_configure,
+            new_integrations,
+            testing_client)
+
+        old_module_instances.extend(old_module_instances_for_test)
+        new_module_instances.extend(new_module_instances_for_test)
+
+    return old_module_instances, new_module_instances
+
+
+def configure_old_and_new_integrations(build: Build,
+                                       old_integrations_to_configure: list,
+                                       new_integrations_to_configure: list,
+                                       demisto_client: demisto_client) -> tuple:
+    """
+    Configures old and new integrations in the server configured in the demisto_client.
+    Args:
+        build: The build object
+        old_integrations_to_configure: Integrations to configure that are already exists
+        new_integrations_to_configure: Integrations to configure that were created in this build
+        demisto_client: A demisto client
+    """
+    old_modules_instances = []
+    new_modules_instances = []
+    for integration in old_integrations_to_configure:
+        placeholders_map = {'%%SERVER_HOST%%': build.servers[0]}
+        module_instance = configure_integration_instance(integration, demisto_client, prints_manager,
+                                                         placeholders_map)
+        if module_instance:
+            old_modules_instances.append(module_instance)
+    for integration in new_integrations_to_configure:
+        placeholders_map = {'%%SERVER_HOST%%': build.servers[0]}
+        module_instance = configure_integration_instance(integration, demisto_client, prints_manager,
+                                                         placeholders_map)
+        if module_instance:
+            new_modules_instances.append(module_instance)
+    return old_modules_instances, new_modules_instances
 
 
 def instance_testing(build: Build, all_module_instances, pre_update):
