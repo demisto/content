81f3dce449076c7fe471afaa824e753e1f5361b6
dmeylakh <dmeylakh@paloaltonetworks.com>
2020-11-15 18:43:45 +0200

    Trying it out

---
+++ Tests/scripts/validate_premium_packs.py
@@ -0,0 +1,126 @@
+import demisto_client
+import argparse
+import logging
+import json
+import ast
+import sys
+
+from Tests.configure_and_test_integration_instances import set_marketplace_url, MARKET_PLACE_CONFIGURATION, \
+    Build, Server
+from Tests.test_content import get_json_file, ParallelPrintsManager
+from Tests.Marketplace.search_and_install_packs import install_all_content_packs
+from Tests.scripts.utils.log_util import install_logging
+from demisto_sdk.commands.common.tools import print_color, LOG_COLORS, run_threads_list, print_error
+
+
+def options_handler():
+    parser = argparse.ArgumentParser(description='Utility for instantiating integration instances')
+    parser.add_argument('--ami_env', help='The AMI environment for the current run. Options are '
+                                          '"Demisto 6.0", "Demisto Marketplace". The server url is determined by the'
+                                          ' AMI environment.', default="Demisto Marketplace")
+    parser.add_argument('--index_file_path', help='The index file path, generated on the server', required=True)
+    #parser.add_argument('--commit_hash', help='The commit hash of the current build', required=True)
+    parser.add_argument('-s', '--secret', help='Path to secret conf file')
+    #parser.add_argument('--branch', help='GitHub branch name', required=True)
+    #parser.add_argument('--build_number', help='CI job number where the instances were created', required=True)
+
+    options = parser.parse_args()
+
+    return options
+
+
+def update_expectations_from_git(index_data):
+    # TODO: implement
+    return index_data
+
+
+def check_and_return_index_data(index_file_path, commit_hash):
+    with open(index_file_path, 'r') as index_file:
+        index_data = json.load(index_file)
+    # TODO: check commit hash with master
+    assert index_data["commit"] == commit_hash
+    assert len(index_data["packs"]) != 0
+    for pack in index_data["packs"]:
+        assert pack["id"] != ""
+        assert pack["price"] > 0
+    return index_data
+
+
+def get_paid_packs(client: demisto_client, prints_manager: ParallelPrintsManager,
+                  thread_index: int, request_timeout: int = 999999):
+
+    request_data = {
+        'page': 0,
+        'size': 50,
+        'sort': [
+            {
+                'field': 'updated',
+                'asc': 0
+            }
+        ],
+        'general': ["generalFieldPaid"]
+    }
+
+    message = f'Getting premium packs from server {client.api_client.configuration.host}:\n'
+    prints_manager.add_print_job(message, print_color, thread_index, LOG_COLORS.GREEN,
+                                 include_timestamp=True)
+    prints_manager.execute_thread_prints(thread_index)
+
+    # make the pack installation request
+    response_data, status_code, _ = demisto_client.generic_request_func(client,
+                                                                        path='/contentpacks/marketplace/search',
+                                                                        method='POST',
+                                                                        body=request_data,
+                                                                        accept='application/json',
+                                                                        _request_timeout=request_timeout)
+
+    if status_code == 200:
+        response = json.load(response_data)
+        packs_message = 'Got premium packs from server'
+        prints_manager.add_print_job(packs_message, print_color, thread_index, LOG_COLORS.GREEN, include_timestamp=True)
+        return response["packs"]
+    result_object = ast.literal_eval(response_data)
+    message = result_object.get('message', '')
+    err_msg = f'Failed to retrieve premium packs - with status code {status_code}\n{message}\n'
+    prints_manager.add_print_job(err_msg, print_error, thread_index, include_timestamp=True)
+    return None
+
+
+def verify_server_paid_packs_by_index(server_paid_packs, index_data):
+
+    # Sorting both lists by id
+    sorted_server_packs = sorted(server_paid_packs, key=lambda i: i['id'])
+    sorted_index_packs = sorted(index_data, key=lambda i: i['id'])
+
+    # Checking lists are the same.
+    for (server_pack, index_pack) in zip(sorted_server_packs, sorted_index_packs):
+        assert server_pack["id"] == index_pack["id"]
+        assert server_pack["price"] == index_pack["price"]
+
+
+def main():
+    options = options_handler()
+
+    index_data = check_and_return_index_data(options.index_file_path, options.commit_hash)
+    update_expectations_from_git(index_data)
+
+    # Get the host by the ami env
+    hosts, _ = Build.get_servers(ami_env=options.ami_env)
+
+    logging.info('Retrieving the credentials for Cortex XSOAR server')
+    secret_conf_file = get_json_file(path=options.secret)
+    username: str = secret_conf_file.get('username')
+    password: str = secret_conf_file.get('userPassword')
+
+    # Check the marketplace
+    for host in hosts:
+        server = Server(host=host, user_name=username, password=password)
+        paid_packs = get_paid_packs(server.client)
+        if paid_packs is not None:
+            verify_server_paid_packs_by_index(paid_packs, index_data)
+        else:
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ Tests/scripts/validate_premium_packs.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# exit on errors
+set -e
+
+CIRCLE_BRANCH=${CIRCLE_BRANCH:-unknown}
+CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-00000}
+CIRCLE_ARTIFACTS=${CIRCLE_ARTIFACTS}
+PACK_ARTIFACTS=$CIRCLE_ARTIFACTS/content_packs.zip
+ID_SET=$CIRCLE_ARTIFACTS/id_set.json
+EXTRACT_FOLDER=$(mktemp -d)
+
+if [[ -z "$GCS_MARKET_KEY" ]]; then
+    echo "GCS_MARKET_KEY not set aborting!"
+    exit 1
+fi
+
+echo "Preparing index file for testing ..."
+
+KF=$(mktemp)
+echo "$GCS_MARKET_KEY" > "$KF"
+gcloud auth activate-service-account --key-file="$KF" > auth.out 2>&1
+echo "Auth loaded successfully."
+
+# ====== BUILD CONFIGURATION ======
+
+GCS_BUILD_BUCKET="marketplace-ci-build"
+BUILD_BUCKET_PATH="content/builds/$CIRCLE_BRANCH/$CIRCLE_BUILD_NUM"
+TARGET_PATH="$BUILD_BUCKET_PATH/content/packs"
+INDEX_FULL_TARGET_PATH="$GCS_BUILD_BUCKET/$TARGET_PATH"
+BUCKET_FULL_TARGET_PATH="$GCS_BUILD_BUCKET/$BUILD_BUCKET_PATH"
+
+# ====== PRODUCTION CONFIGURATION ======
+
+GCS_MARKET_BUCKET="marketplace-dist"
+INDEX_PATH="content/packs/index.json"
+
+# ====== TESTING CONFIGURATION ======
+
+GCS_MARKET_TESTING_BUCKET="marketplace-dist-dev"
+INDEX_TESTING_PATH="dev/content/packs/index.json"
+
+if [ ! -n "${BUCKET_UPLOAD}" ]; then
+  echo "Copying master files at: gs://$GCS_MARKET_BUCKET/$INDEX_PATH to target path: gs://$INDEX_FULL_TARGET_PATH ..."
+  gsutil -m cp -r "gs://$GCS_MARKET_BUCKET/$INDEX_PATH" "gs://$INDEX_FULL_TARGET_PATH" > "$CIRCLE_ARTIFACTS/logs/Validate Premium Packs.log" 2>&1
+else
+  echo "Copying testing files at: gs://$GCS_MARKET_TESTING_BUCKET/$INDEX_TESTING_PATH to target path: gs://$PACKS_FULL_TARGET_PATH ..."
+  gsutil -m cp -r "gs://$GCS_MARKET_TESTING_BUCKET/$INDEX_TESTING_PATH" "gs://$INDEX_FULL_TARGET_PATH" > "$CIRCLE_ARTIFACTS/logs/Validate Premium Packs.log" 2>&1
+fi
+echo "Finished copying successfully."
+
+if [ ! -f "./$INDEX_PATH" ]; then
+  echo "Could not find file ./$INDEX_PATH"
+else
+  echo "Testing premium packs in against index file ./$INDEX_PATH"
+  python3 ./Tests/scripts/validate_premium_packs.py --index_file_path ./$INDEX_PATH -s "$SECRET_CONF_PATH" --ami_env "$1"
+fi
