74b4babb0940b976032c457c99ba2d13522c75c4
orhovy <yorhov@paloaltonetworks.com>
2020-11-17 16:14:34 +0200

    added ut

--- Packs/Tripwire/Integrations/Tripwire/Tripwire.py
+++ Packs/Tripwire/Integrations/Tripwire/Tripwire.py
@@ -130,6 +130,7 @@ def filter_rules(args):
     return filters
 
 
+
 def filter_elements(args):
     filters = ""
     if args.get('element_oids'):
@@ -157,8 +158,8 @@ def filter_nodes(args):
         filters += f"id={args.get('node_oids')}&"
     if args.get('node_ips'):
         filters += f"ipAddress={args.get('node_ips')}&"
-    # if args.get('node_mac_adresses'):
-    #     filters += f"type={args.get('rule_types')}&"
+    if args.get('node_mac_adresses'):
+         filters += f"macAddress={args.get('rule_types')}&"
     if args.get('node_names'):
         filters += f"ic_name={args.get('node_names')}&"
     if args.get('node_os_names'):
@@ -170,6 +171,28 @@ def filter_nodes(args):
     if args.get('page_start'):
         filters += f"pageStart={args.get('page_start')}&"
     return filters
+
+
+def prepare_fetch(params: dict, first_fetch: str):
+
+    # check that either ruleid exist or node id for fetch to run
+    if not params.get('rule_oids') and not params.get('node_oids'):
+        raise DemistoException(
+            'Test failed, missing both rule ids and node ids. \n At least one of the above is needed.')
+
+    # set last run
+    last_run = demisto.getLastRun().get('lastRun', '')
+    if last_run:
+        last_fetch = last_run
+    else:
+        last_fetch = parse_date_range(first_fetch, date_format=DATE_FORMAT)[0]
+
+    # set filter for fetch
+    time_now = datetime.utcnow().strftime(DATE_FORMAT)
+    params['time_received_range'] = f'{last_fetch},{time_now}'
+    fetch_filter = filter_version(params)
+
+    return params, fetch_filter, last_fetch
 
 
 ''' COMMAND FUNCTIONS '''
@@ -192,12 +215,9 @@ def test_module(client: Client, args) -> str:
     :rtype: ``str``
     """
     if demisto.params().get('isFetch'):
-        version_filter = filter_version(demisto.params())
-        if not version_filter:
-            raise DemistoException(
-                'Missing both rule ids and node ids. \n At least one of the above is needed.')
-        fetch_incidents(client, max_results=1, first_fetch=demisto.params().get('first_fetch'),
-                        fetch_filter=version_filter,last_run={})
+        params, fetch_filter, last_fetch = prepare_fetch(demisto.params(), demisto.params().get('first_fetch'))
+        fetch_incidents(client=client, last_fetch=last_fetch, fetch_filter=fetch_filter, max_results=1)
+
     client.get_nodes("")
     return 'ok'
 
@@ -245,8 +265,33 @@ def rules_list_command(client: Client, args: Dict[str, Any]) -> CommandResults:
     result = client.get_rules(rules_filter)
     readable_output = tableToMarkdown('Tripwire Rules list results', result, removeNull=True)
     return CommandResults(
-        readable_output - readable_output,
+        readable_output=readable_output,
         outputs_prefix='Tripwire.Rules',
+        outputs_key_field='id',
+        outputs=result
+    )
+
+
+def nodes_list_command(client: Client, args: Dict[str, Any]) -> CommandResults:
+    """tripwire-notes-list command
+
+    :type client: ``Client``
+    :param Client: Tripwire client to use
+
+    :type args: `` Dict[str, Any]``
+    :param args:
+        all command arguments, passed from ``demisto.args()``.
+
+    :return:
+        A ``CommandResults`` object that is then passed to ``return_results``
+    :rtype: ``CommandResults``
+    """
+    node_filter = filter_nodes(args)
+    result = client.get_nodes(node_filter)
+    readable_output = tableToMarkdown('Tripwire Nodes list results', result, removeNull=True)
+    return CommandResults(
+        readable_output=readable_output,
+        outputs_prefix='Tripwire.Nodes',
         outputs_key_field='id',
         outputs=result
     )
@@ -277,8 +322,7 @@ def elements_list_command(client: Client, args: Dict[str, Any]) -> CommandResult
     )
 
 
-def fetch_incidents(client: Client, max_results: int, last_run: Dict[str, int], first_fetch: Optional[str] = None,
-                    fetch_filter: Optional[str] = ''
+def fetch_incidents(client: Client, max_results: int, last_fetch: str, fetch_filter: Optional[str] = ''
                     ) -> Tuple[Dict[str, str], List[dict]]:
     """This function retrieves new alerts every interval (default is 1 minute).
 
@@ -317,32 +361,24 @@ def fetch_incidents(client: Client, max_results: int, last_run: Dict[str, int], 
 
     :rtype: ``Tuple[Dict[str, int], List[dict]]``
     """
-    if not filter_version:
-        raise DemistoException(
-            'Test failed, missing both rule ids and node ids. \n At least one of the above is needed.')
 
-    last_run = last_run.get('lastRun', '')
-    if last_run:
-        last_fetch = last_run
-    else:
-        last_fetch = parse_date_range(first_fetch, date_format=DATE_FORMAT)[0]
     incidents: List[Dict[str, Any]] = []
     last_fetch = datetime.strptime(last_fetch, DATE_FORMAT)
     alerts = client.get_versions(fetch_filter)
     alerts = alerts[:int(max_results)]
     for alert in alerts:
         incident_created_time = datetime.strptime(alert.get('timeDetected'), '%Y-%m-%dT%H:%M:%S.000Z')
-        if incident_created_time <= last_fetch:
+        if incident_created_time < last_fetch:
             continue
         incident_name = alert.get('id')
         incident = {
             'name': incident_name,
             'occurred': incident_created_time.strftime(DATE_FORMAT),
             'rawJSON': json.dumps(alert),
-            # 'type': 'Hello World Alert',  # Map to a specific XSOAR incident Type
-        }
+            }
         incidents.append(incident)
         last_fetch = incident_created_time
+
     next_run = {'lastRun': last_fetch.strftime(DATE_FORMAT)}
     return next_run, incidents
 
@@ -377,17 +413,18 @@ def main() -> None:
             'test-module': test_module,
             "tripwire-versions-list": versions_list_command,
             "tripwire-rules-list": rules_list_command,
-            "tripwire-elements-list": elements_list_command
+            "tripwire-elements-list": elements_list_command,
+            "tripwire-nodes-list": nodes_list_command
+
         }
 
         if command in commands:
             return_results(commands[command](client, demisto.args()))
         elif command == 'fetch-incidents':
             max_fetch = params.get('max_fetch', 10)
-            fetch_filter = filter_version(params)
-            next_run, incidents = fetch_incidents(client=client, first_fetch=params.get('first_fetch'),
-                                                  fetch_filter=fetch_filter, max_results=max_fetch,
-                                                  last_run=demisto.getLastRun())
+            params, fetch_filter, last_fetch = prepare_fetch(params, params.get('first_fetch'))
+            next_run, incidents = fetch_incidents(client=client, last_fetch=last_fetch,
+                                                  fetch_filter=fetch_filter, max_results=max_fetch)
             demisto.setLastRun(next_run)
             demisto.incidents(incidents)
         else:
