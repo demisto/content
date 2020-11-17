da5dee7716963d50f4b9e695236be129d6dfad9b
katherinemcgauleydarktrace <67451657+katherinemcgauleydarktrace@users.noreply.github.com>
2020-11-16 23:33:54 -0800

    Update Packs/Darktrace/Integrations/Darktrace/Darktrace.py
    Co-authored-by: Yana Orhov <yorhov@paloaltonetworks.com>

--- Packs/Darktrace/Integrations/Darktrace/Darktrace.py
+++ Packs/Darktrace/Integrations/Darktrace/Darktrace.py
@@ -159,7 +159,7 @@ class Client(BaseClient):
         :return: list containing the found Darktrace model breach as a Dict
         :rtype: ``List[Dict[str, Any]]``
         """
-        request = "/endpointdetails?" + endpoint_type + "=" + endpoint_value + "&additionalinfo=" + additional_info \
+        request = f"/endpointdetails?{endpoint_type}={endpoint_value}&additionalinfo={additional_info}&devices={devices}&score={score}```
                   + "&devices=" + devices + "&score=" + score
         http_headers = get_headers(request)
         return self._http_request(
