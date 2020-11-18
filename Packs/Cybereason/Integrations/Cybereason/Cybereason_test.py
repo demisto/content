b3cecf8b129615f64124529ecf8572f7bf5c059b
ikeren <itay@demisto.com>
2020-11-18 13:09:43 +0200

    change order of tests

--- Packs/Cybereason/Integrations/Cybereason/Cybereason_test.py
+++ Packs/Cybereason/Integrations/Cybereason/Cybereason_test.py
@@ -239,28 +239,6 @@ FILE_OUTPUTS = {
     }
 }
 
-params = {
-    'server': 'https://integration.cybereason.net:8443',
-    'credentials': {'credentials': {'sshkey': 'shelly'}},
-    'proxy': True}
-
-
-def test_query_file(mocker):
-    mocker.patch.object(demisto, 'params', return_value=params)
-    mocker.patch.object(demisto, 'args', return_value={'file_hash': '4778901e54f55d54435b2626923054a8'})
-    mocker.patch('Cybereason.client_certificate', side_effect=lambda: None, autospec=False)
-    mocker.patch('Cybereason.get_file_machine_details', return_value=MACHINE_OUTPUTS)
-    mocker.patch('Cybereason.query_file', return_value=FILE_OUTPUTS)
-    mocker.patch.object(demisto, 'results')
-    import Cybereason
-    Cybereason.query_file_command()
-    result = demisto.results.call_args[0]
-
-    assert result[0]['ContentsFormat'] == 'json'
-    assert 'Cybereason file query results' in result[0]['HumanReadable']
-    assert result[0]['EntryContext']['Cybereason.File(val.MD5 && val.MD5===obj.MD5 || val.SHA1 && '
-                                     'val.SHA1===obj.SHA1)'][0]['Machine'] == 'desktop-p0m5vad'
-
 
 def test_login_failed(requests_mock, mocker):
     """
@@ -306,3 +284,26 @@ def test_login_failed(requests_mock, mocker):
     assert return_error_mock.call_count == 1
     err_msg = return_error_mock.call_args[0][0]
     assert 'Failed to process the API response. Authentication failed, verify the credentials are correct.' in err_msg
+
+
+params = {
+    'server': 'https://integration.cybereason.net:8443',
+    'credentials': {'credentials': {'sshkey': 'shelly'}},
+    'proxy': True}
+
+
+def test_query_file(mocker):
+    mocker.patch.object(demisto, 'params', return_value=params)
+    mocker.patch.object(demisto, 'args', return_value={'file_hash': '4778901e54f55d54435b2626923054a8'})
+    mocker.patch('Cybereason.client_certificate', side_effect=lambda: None, autospec=False)
+    mocker.patch('Cybereason.get_file_machine_details', return_value=MACHINE_OUTPUTS)
+    mocker.patch('Cybereason.query_file', return_value=FILE_OUTPUTS)
+    mocker.patch.object(demisto, 'results')
+    import Cybereason
+    Cybereason.query_file_command()
+    result = demisto.results.call_args[0]
+
+    assert result[0]['ContentsFormat'] == 'json'
+    assert 'Cybereason file query results' in result[0]['HumanReadable']
+    assert result[0]['EntryContext']['Cybereason.File(val.MD5 && val.MD5===obj.MD5 || val.SHA1 && '
+                                     'val.SHA1===obj.SHA1)'][0]['Machine'] == 'desktop-p0m5vad'
