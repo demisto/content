6a70a8c596604ac4b294a54b9d2428ac26b6c308
jgranot <jgranot@paloaltonetworks.com>
2020-11-19 16:45:08 +0200

    added new feed

--- Packs/ApiModules/Scripts/JSONFeedApiModule/JSONFeedApiModule.py
+++ Packs/ApiModules/Scripts/JSONFeedApiModule/JSONFeedApiModule.py
@@ -77,6 +77,7 @@ class Client:
 
     def build_iterator(self, **kwargs) -> List:
         results = []
+
         for feed_name, feed in self.feed_name_to_config.items():
             r = requests.get(
                 url=feed.get('url', self.url),
@@ -113,46 +114,73 @@ def fetch_indicators_command(client: Client, indicator_type: str, feedTags: list
     :param feedTags: the indicator tags
     """
     indicators = []
-    for result in client.build_iterator(**kwargs):
+    feeds_results = []
+    for feed_name, feed in client.feed_name_to_config.items():
+        build_iterator_paging = feed.get('build_iterator_paging')
+        if build_iterator_paging:
+            feeds_results.append({feed_name: build_iterator_paging(client, feed, **kwargs)})
+        else:
+            feeds_results.append({feed_name: client.build_iterator(feed, **kwargs)})
+    for result in feeds_results:
         for service_name, items in result.items():
             feed_config = client.feed_name_to_config.get(service_name, {})
-            indicator_field = feed_config.get('indicator') if feed_config.get('indicator') else 'indicator'
+            indicator_field = feed_config.get('indicator', 'indicator')
             indicator_type = feed_config.get('indicator_type', indicator_type)
+            use_prefix_flat = feed_config.get('flat_json_with_prefix', False)
+            use_with_flatten = False
+
             for item in items:
-                mapping = feed_config.get('mapping')
+                try:
+                    mapping = feed_config.get('mapping')
+                    if isinstance(item, str):
+                        item = {indicator_field: item}
+                    indicator_value = item.get(indicator_field)
 
-                if isinstance(item, str):
-                    item = {indicator_field: item}
-                indicator_value = item.get(indicator_field)
+                    if not indicator_value:
+                        use_with_flatten = True
 
-                current_indicator_type = determine_indicator_type(indicator_type, auto_detect, indicator_value)
-                if not current_indicator_type:
-                    continue
+                    current_indicator_type = determine_indicator_type(indicator_type, auto_detect, indicator_value)
+                    if not current_indicator_type:
+                        continue
 
-                indicator = {
-                    'value': indicator_value,
-                    'type': current_indicator_type,
-                    'fields': {
-                        'tags': feedTags,
+                    indicator = {
+                        'type': current_indicator_type,
+                        'fields': {
+                            'tags': feedTags,
+                        }
                     }
-                }
 
-                if client.tlp_color:
-                    indicator['fields']['trafficlightprotocol'] = client.tlp_color
+                    if client.tlp_color:
+                        indicator['fields']['trafficlightprotocol'] = client.tlp_color
 
-                attributes = {'source_name': service_name, 'value': indicator_value,
-                              'type': current_indicator_type}
+                    attributes = {'source_name': service_name, 'type': current_indicator_type}
+                    attributes.update(extract_all_fields_from_indicator(item, indicator_field,
+                                                                        flat_with_prefix=use_prefix_flat))
+                    if use_with_flatten:
+                        indicator_value = attributes.get(indicator_field)
 
-                attributes.update(extract_all_fields_from_indicator(item, indicator_field))
+                    indicator['value'] = indicator_value
+                    attributes['value'] = indicator_value
 
-                if mapping:
-                    for map_key in mapping:
-                        if map_key in attributes:
-                            indicator['fields'][mapping[map_key]] = attributes.get(map_key)  # type: ignore
+                    if mapping:
+                        for map_key in mapping:
+                            if map_key in attributes:
+                                fields = mapping[map_key].split(".")
+                                if len(fields) > 1:
+                                    if indicator['fields'].get(fields[0]):
+                                        indicator['fields'][fields[0]].extend([{fields[1]: attributes.get(map_key)}])
+                                    else:
+                                        indicator['fields'][fields[0]] = [{fields[1]: attributes.get(map_key)}]
+                                else:
+                                    indicator['fields'][mapping[map_key]] = attributes.get(map_key)  # type: ignore
 
-                indicator['rawJSON'] = item
+                    indicator['rawJSON'] = item
 
-                indicators.append(indicator)
+                    indicators.append(indicator)
+                except Exception as e:
+                    demisto.debug(item)
+                    demisto.debug(e)
+                    break
 
     return indicators
 
@@ -172,12 +200,13 @@ def determine_indicator_type(indicator_type, auto_detect, value):
     return indicator_type
 
 
-def extract_all_fields_from_indicator(indicator, indicator_key):
+def extract_all_fields_from_indicator(indicator: Dict, indicator_key: str, flat_with_prefix: bool = False) -> Dict:
     """Flattens the JSON object to create one dictionary of values
 
     Args:
         indicator(dict): JSON object that holds indicator full data.
         indicator_key(str): The key that holds the indicator value.
+        flat_with_prefix(bool): Indicates whether should add the inner json path as part of the keys in the flatten json
 
     Returns:
         dict. A dictionary of the fields in the JSON object.
@@ -192,19 +221,27 @@ def extract_all_fields_from_indicator(indicator, indicator_key):
         else:
             fields[key] = value
 
-    def extract(json_element):
+    def extract(json_element, prefix_field="", use_prefix=False):
         if isinstance(json_element, dict):
             for key, value in json_element.items():
                 if value and isinstance(value, dict):
-                    extract(value)
+                    if use_prefix:
+                        extract(value, prefix_field=f"{prefix_field}_{key}" if prefix_field else key,
+                                use_prefix=use_prefix)
+                    else:
+                        extract(value)
                 elif key != indicator_key:
-                    insert_value_to_fields(key, value)
+                    if use_prefix:
+                        insert_value_to_fields(f"{prefix_field}_{key}" if prefix_field else key, value)
+                    else:
+                        insert_value_to_fields(key, value)
 
         elif json_element and indicator_key not in json_element:
             for key, value in json_element:
                 insert_value_to_fields(key, value)
 
-    extract(indicator)
+    extract(indicator, use_prefix=flat_with_prefix)
+
     return fields
 
 
@@ -227,7 +264,9 @@ def feed_main(params, feed_name, prefix):
             indicators = fetch_indicators_command(client, params.get('indicator_type'), feedTags,
                                                   params.get('auto_detect_type'))
             for b in batch(indicators, batch_size=2000):
+                demisto.debug("here1")
                 demisto.createIndicators(b)
+                demisto.debug("here2")
 
         elif command == f'{prefix}get-indicators':
             # dummy command for testing
