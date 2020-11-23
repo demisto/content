09e8cfb21ae1b4f03c184adb987776cbb5863ac7
Dean Arbel <darbel@paloaltonetworks.com>
2020-11-23 13:57:38 +0200

    added check for internal prs and changed deletion impl

--- Utils/github_workflow_scripts/close_stale_contrib_branches.py
+++ Utils/github_workflow_scripts/close_stale_contrib_branches.py
@@ -4,14 +4,16 @@ import os
 import sys
 import urllib3
 import inspect
+from blessings import Terminal
 from github import Github, enable_console_debug_logging
 from github.Repository import Repository
 from typing import List
 from dateparser import parse
 from datetime import datetime, timezone
-from demisto_sdk.commands.common.tools import run_command, print_error, print_warning
+from utils import timestamped_print
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+print = timestamped_print
 
 
 def get_stale_branch_names_with_contrib(repo: Repository) -> List[str]:  # noqa: E999
@@ -25,26 +27,32 @@ def get_stale_branch_names_with_contrib(repo: Repository) -> List[str]:  # noqa:
         (List[str]): List of branch names that are stale and have the "contrib/" prefix
     """
     now = datetime.now(timezone.min)
+    organization = 'demisto'
     branch_names = []
     for branch in repo.get_branches():
+        # Make sure the branch is contrib
         if branch.name.startswith('contrib/'):
             prs_with_branch_as_base = repo.get_pulls(state='OPEN', base=branch.name)
-            if prs_with_branch_as_base.totalCount < 1:
+            prs_with_branch_as_head = repo.get_pulls(state='OPEN', head=f'{organization}:{branch.name})')
+
+            # Make sure there are no open prs pointing to/from the branch
+            if prs_with_branch_as_base.totalCount < 1 and prs_with_branch_as_head.totalCount < 1:
                 # load all members
                 inspect.getmembers(branch.commit)
 
-                # check if HEAD commit is stale
+                # Make sure HEAD commit is stale
                 if (last_modified := branch.commit.last_modified) and (last_commit_datetime := parse(last_modified)):
                     elapsed_days = (now - last_commit_datetime).days
                     if elapsed_days >= 60:
                         branch_names.append(branch.name)
                 else:
-                    print_warning(f"Couldn't load HEAD for {branch.name}")
+                    print(f"Couldn't load HEAD for {branch.name}")
     return branch_names
 
 
 def main():
     debug_mode = len(sys.argv) >= 2 and 'debug' in sys.argv[1].casefold()
+    t = Terminal()
     if debug_mode:
         enable_console_debug_logging()
     gh = Github(os.getenv('CONTENTBOT_GH_ADMIN_TOKEN'), verify=False)
@@ -56,11 +64,10 @@ def main():
     for branch_name in stale_contrib_branches:
         try:
             print(f'Deleting {branch_name}')
-            run_command(f"git push origin --delete {branch_name}", exit_on_error=False)
-        except RuntimeError as e:
-            # check delete was successful
-            if '[deleted]' not in str(e):
-                print_error(f"Deletion of {branch_name} encountered an issue: {str(e)}")
+            branch_ref = content_repo.get_git_ref(os.path.join('heads', branch_name))
+            branch_ref.delete()
+        except Exception as e:
+            print(f"{t.red}Deletion of {branch_name} encountered an issue: {str(e)}{t.normal}")
 
     if debug_mode:
         print(f'{stale_contrib_branches=}')
