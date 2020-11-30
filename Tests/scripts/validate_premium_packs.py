68bb3cd4799193ae1ee2e6f9e5847422eac6f47f
dmeylakh <dmeylakh@paloaltonetworks.com>
2020-11-30 20:01:47 +0200

    split the files.

--- Tests/scripts/validate_premium_packs.py
+++ Tests/scripts/validate_premium_packs.py
@@ -9,129 +9,39 @@ Validate the pack id's in the index file are present on the server and the price
 import demisto_client
 import argparse
 import logging
-import zipfile
-import json
 import ast
 import sys
-import os
 
-from Tests.Marketplace.marketplace_services import init_storage_client, GCPConfig, CONTENT_ROOT_PATH
-from Tests.Marketplace.upload_packs import download_and_extract_index, get_content_git_client
+from Tests.Marketplace.marketplace_services import GCPConfig
 from Tests.configure_and_test_integration_instances import Build, Server
 from Tests.scripts.utils.log_util import install_logging
+from Tests.scripts.validate_index import log_message_if_statement, get_index_json_data
 from Tests.test_content import get_json_file
 from pprint import pformat
 
-INDEX_FILE_PATH = 'index.json'
 DEFAULT_PAGE_SIZE = 50
 
 
 def options_handler():
-    parser = argparse.ArgumentParser(description='Utility for instantiating integration instances')
+    parser = argparse.ArgumentParser(description='Test for validating premium packs on servers.')
     parser.add_argument('--ami_env', help='The AMI environment for the current run. Options are '
                                           '"Demisto 6.0", "Demisto Marketplace". The server url is determined by the'
                                           ' AMI environment.', default="Demisto Marketplace")
-    parser.add_argument('--index_path', help='The index.zip file path, generated on the cloud\n'
-                                             ' In case only_check_index_file is set, specify path to index.json',
+    parser.add_argument('-e', '--extract_path',
+                        help=f"Full path of folder to extract the {GCPConfig.INDEX_NAME}.zip to",
                         required=True)
-    parser.add_argument('--master_history', help='Path to a file that contains the master history commit hashes',
-                        required=True)
-    parser.add_argument('-e', '--extract_path', help="Full path of folder to extract wanted packs", required=True)
     parser.add_argument('-pb', '--production_bucket_name', help="Production bucket name", required=True)
-    parser.add_argument('-sa', '--service_account',
-                        help=("Path to gcloud service account, is for circleCI usage. "
-                              "For local development use your personal account and "
-                              "authenticate using Google Cloud SDK by running: "
-                              "`gcloud auth application-default login` and leave this parameter blank. "),
-                        required=True)
+    parser.add_argument('-sa', '--service_account', help="Path to gcloud service account", required=True)
     parser.add_argument('-s', '--secret', help='Path to secret conf file', required=True)
 
     options = parser.parse_args()
     return options
 
 
-def log_message_if_statement(statement, error_message, success_message=None):
-    """Log error message if statement is false, Log success otherwise
-
-    Args:
-        statement: The boolean statement to check.
-        error_message: The error message to log if statement is false
-        success_message: The success message to log if statement is true
-
-    Returns: The statements boolean value.
-    """
-    if not statement:
-        logging.error(error_message)
-    elif success_message is not None:
-        logging.success(success_message)
-    return statement
-
-
-def unzip_index_and_return_index_file(index_zip_path):
-    """Unzip index.zip and return the extracted index.json path
-
-    Args:
-        index_zip_path: The path to the index.zip file.
-
-    Returns: The extracted index.json path
-    """
-    logging.info('Unzipping')
-    with zipfile.ZipFile(index_zip_path, 'r') as zip_obj:
-        extracted_path = zip_obj.extract(member=f"index/{INDEX_FILE_PATH}", path="./extracted-index")
-
-    logging.info(f'extracted path is now: {extracted_path}')
-
-    return f"./{extracted_path}"
-
-
-def check_index_data(index_data):
-    """Check index.json file inside the index.zip archive in the cloud.
-
-    Validate no missing ids are found and that all packs have a positive price.
-
-    Args:
-        index_data: The path to the index.json.
-
-    Returns: Dict with the index data.
-
-    """
-    logging.info("Found index data in index file. Checking...")
-    logging.debug(f"Index data is:\n {pformat(index_data)}")
-
-    packs_list_exists = log_message_if_statement(statement=(len(index_data.get("packs", [])) != 0),
-                                                 error_message="Found 0 packs in index file."
-                                                               "\nAborting the rest of the check.")
-    if not packs_list_exists:
-        return False
-
-    packs_are_valid = True
-    for pack in index_data["packs"]:
-        pack_is_good = verify_pack(pack)
-        if not pack_is_good:
-            packs_are_valid = False
-
-    return packs_are_valid
-
-
-def verify_pack(pack):
-    """
-
-    Args:
-        pack:
-
-    Returns:
-
-    """
-    id_exists = log_message_if_statement(statement=(pack.get("id", "") != ""),
-                                         error_message="There is a missing pack id.",
-                                         success_message=f"Found pack with a valid id: {pack['id']}.")
-    price_is_valid = log_message_if_statement(statement=(pack.get("price", -1) > 0),
-                                              error_message=f"The price on the pack {pack['id']} is 0 or less.",
-                                              success_message=f"The price on the pack {pack['id']} is valid.")
-    return all(id_exists, price_is_valid)
-
-
-def get_paid_packs_page(client: demisto_client, page: int = 0, size: int = DEFAULT_PAGE_SIZE, request_timeout: int = 999999):
+def get_paid_packs_page(client: demisto_client,
+                        page: int = 0,
+                        size: int = DEFAULT_PAGE_SIZE,
+                        request_timeout: int = 999999):
     """Get premium packs from client.
 
     Trigger an API request to demisto server.
@@ -139,13 +49,13 @@ def get_paid_packs_page(client: demisto_client, page: int = 0, size: int = DEFAU
 
     Args:
         client: The demisto client to the preform request on.
-        page:
-        size:
+        page: Page number of the request.
+        size: Number of packs to bring each time.
         request_timeout: Timeout of API request
 
     Returns:
-        Dict of premium packs as found in the server.
-        Return None if no premium packs were found.
+        (Dict: premium packs as found in the server, int: Total premium packs that exist)
+        (None, 0) if no premium packs were found.
     """
     request_data = \
         {
@@ -178,18 +88,21 @@ def get_paid_packs_page(client: demisto_client, page: int = 0, size: int = DEFAU
     result_object = ast.literal_eval(response_data)
     message = result_object.get('message', '')
     logging.error(f'Failed to retrieve premium packs - with status code {status_code}\n{message}\n')
-    return None
+    return None, 0
 
 
 def get_premium_packs(client: demisto_client, request_timeout: int = 999999):
-    """
+    """Get premium packs from client.
+
+    Handle the pagination.
 
     Args:
-        client:
-        request_timeout:
+        client: The demisto client to the preform request on.
+        request_timeout: Timeout of each API request
 
     Returns:
-
+        Dict of premium packs as found in the server.
+        Return None if no premium packs were found.
     """
     server_packs, total = get_paid_packs_page(client=client,
                                               page=0,
@@ -271,71 +184,16 @@ def verify_server_paid_packs_by_index(server_paid_packs, index_data_packs):
                                                                        f' not found exactly the same as in the server'
                                                                        f' packs:\n{pformat(missing_index_packs)}')
 
-    return all(all_index_packs_in_server, all_server_packs_in_index)
-
-
-def get_hexsha(commit):
-    return commit.hexsha
-
-
-def check_commit_in_master_history(index_commit_hash):
-    """Assert commit hash is in master history.
-
-    Args:
-        index_commit_hash: commit hash
-
-    Returns: True if commit hash is in master history, False otherwise.
-    """
-    content_repo = get_content_git_client(CONTENT_ROOT_PATH)
-    master_commits = list(map(get_hexsha, list(content_repo.iter_commits("master"))))
-
-    return log_message_if_statement(statement=(index_commit_hash in master_commits),
-                                    error_message=f'Commit hash {index_commit_hash} is not in master history',
-                                    success_message="Commit hash in index file is valid.")
-
-
-def get_and_validate_index_json(service_account, production_bucket_name, extract_path):
-    """
-
-    Args:
-        service_account:
-        production_bucket_name:
-        extract_path:
-        master_history:
-
-    Returns:
-
-    """
-    logging.info('Downloading and extracting index.zip from the cloud')
-    storage_client = init_storage_client(service_account)
-    production_bucket = storage_client.bucket(production_bucket_name)
-    index_folder_path, build_index_blob, build_index_generation = \
-        download_and_extract_index(production_bucket, extract_path)
-
-    logging.info('Retrieving the index file')
-    index_file_path = os.path.join(index_folder_path, f'{GCPConfig.INDEX_NAME}.json')
-    with open(index_file_path, 'r') as index_file:
-        index_data = json.load(index_file)
-
-    # Validate index.json file
-    index_is_valid = check_index_data(index_data)
-    log_message_if_statement(statement=index_is_valid,
-                             error_message=f"The packs in the {index_file_path} file were found invalid.",
-                             success_message=f"{index_file_path} file was found valid")
-
-    # Validate commit hash in master history
-    commit_hash_is_valid = check_commit_in_master_history(index_data.get("commit", ""))
-    return all(index_is_valid, commit_hash_is_valid), index_data
+    return all([all_index_packs_in_server, all_server_packs_in_index])
 
 
 def extract_credentials_from_secret(secret_path):
-    """
+    """Extract Credentials from secret file.
 
     Args:
-        secret_path:
+        secret_path: The path to the secret file.
 
-    Returns:
-
+    Returns: (username, password) found in the secret file.
     """
     logging.info('Retrieving the credentials for Cortex XSOAR server')
     secret_conf_file = get_json_file(path=secret_path)
@@ -348,12 +206,9 @@ def main():
     install_logging('Validate Premium Packs.log')
     options = options_handler()
 
-    index_is_valid, index_data = get_and_validate_index_json(service_account=options.service_account,
-                                                             production_bucket_name=options.production_bucket_name,
-                                                             extract_path=options.extract_path)
-    if not index_is_valid:
-        logging.critical('Index content is invalid. Aborting.')
-        sys.exit(1)
+    index_data = get_index_json_data(service_account=options.service_account,
+                                     production_bucket_name=options.production_bucket_name,
+                                     extract_path=options.extract_path)
 
     # Get the first host by the ami env
     hosts, _ = Build.get_servers(ami_env=options.ami_env)
--- Tests/scripts/validate_premium_packs.sh
+++ Tests/scripts/validate_premium_packs.sh
@@ -12,7 +12,9 @@ GCS_MARKET_BUCKET="marketplace-dist"
 
 # ====== RUN VALIDATIONS ======
 
-echo "Validating index file and testing premium packs in server"
+echo "Validating index file."
+python3 ./Tests/scripts/validate_index.py -sa "$GCS_PATH" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET"
+
+echo "Validating premium packs in server against index file."
 python3 ./Tests/scripts/validate_premium_packs.py -sa "$GCS_PATH" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" --secret "$SECRET_CONF_PATH" --ami_env "$1"
 rm "$GCS_PATH"
-
