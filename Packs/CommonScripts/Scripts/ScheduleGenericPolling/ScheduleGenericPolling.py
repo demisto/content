70a3d0a5d99fcf2509ce0715f664cfc4ab1f638f
ShahafBenYakir <shahaf.benyakir@demisto.com>
2020-11-30 16:48:46 +0200

    Changed args_names and values logic

--- Packs/CommonScripts/Scripts/ScheduleGenericPolling/ScheduleGenericPolling.py
+++ Packs/CommonScripts/Scripts/ScheduleGenericPolling/ScheduleGenericPolling.py
@@ -8,16 +8,14 @@ def parseIds(idsArg):
     if idsArg is None:
         return
     if isinstance(idsArg, list):
-        if len(idsArg) == 0 or isinstance(idsArg[0], str):
-            return idsArg
-        return ','.join(map(str, idsArg))
+        return ','.join(map(lambda item: str(item).encode('utf-8'), idsArg))
     if isinstance(idsArg, str) or isinstance(idsArg, bytes):
-        return ','.join(argToList(idsArg))
+        return ','.join(argToList(idsArg.encode('utf-8')))
     return str(idsArg)
 
 
 args = demisto.args()
-ids = parseIds(args['ids'].encode('utf-8'))
+ids = parseIds(args['ids'])
 dt = args['dt']
 pollingCommand = args['pollingCommand']
 pollingCommandArgName = args['pollingCommandArgName']
@@ -26,8 +24,8 @@ playbookId = ' playbookId="{}"'.format(args['playbookId']) if 'playbookId' in ar
 interval = int(demisto.get(args, 'interval'))
 timeout = int(demisto.get(args, 'timeout'))
 
-args_names = [name.strip() for name in argToList(demisto.getArg('additionalPollingCommandArgNames'))]
-args_values = [value.strip() for value in argToList(demisto.getArg('additionalPollingCommandArgValues'))]
+args_names = demisto.getArg('additionalPollingCommandArgNames').strip()
+args_values = demisto.getArg('additionalPollingCommandArgValues').strip()
 
 if interval <= 0 or timeout <= 0:
     return_error("Interval and timeout must be positive numbers")
@@ -38,23 +36,14 @@ if not demisto.dt(demisto.context(), dt):
         return_error("Incorrect dt path: no ids found")
     demisto.results("Warning: no ids matching the dt condition were found.\nVerify that the condition is correct and "
                     "that all ids have finished running.")
-
+command_string = '''!GenericPollingScheduledTask pollingCommand="{0}" pollingCommandArgName="{1}"{2} ids="{3}" \
+                    pendingIds="{4}" interval="{5}" timeout="{6}" tag="{7}" additionalPollingCommandArgNames="{8}" \
+                    additionalPollingCommandArgValues="{9}"'''.format(pollingCommand, pollingCommandArgName, playbookId,
+                                                                      ids.replace('"', r'\"'), dt.replace('"', r'\"'),
+                                                                      interval, timeout, tag, args_names, args_values)
 res = demisto.executeCommand("ScheduleCommand",
                              {
-                                 'command': '''!GenericPollingScheduledTask pollingCommand="{0}"
-                                     pollingCommandArgName="{1}"{2} ids="{3}" pendingIds="{4}" interval="{5}"
-                                     timeout="{6}" tag="{7}" additionalPollingCommandArgNames="{8}"
-                                     additionalPollingCommandArgValues="{9}"'''.format(
-                                     pollingCommand,
-                                     pollingCommandArgName,
-                                     playbookId,
-                                     ids.replace('"', r'\"'),
-                                     dt.replace('"', r'\"'),
-                                     interval,
-                                     timeout,
-                                     tag,
-                                     args_names,
-                                     args_values),
+                                 'command': command_string,
                                  'cron': '*/{} * * * *'.format(interval),
                                  'times': 1
                              })
