b84735f8a5ac047736d49b4c5dc9d66b0a8b13ef
cshayner <cshayner@paloaltonetworks.com>
2020-12-01 09:09:26 +0200

    add service account

--- Tests/Marketplace/merge_private_and_public_id_sets.py
+++ Tests/Marketplace/merge_private_and_public_id_sets.py
@@ -1,7 +1,6 @@
 import os
-import json
 import argparse
-from google.cloud import storage
+from Tests.Marketplace.marketplace_services import init_storage_client
 
 
 STORAGE_BUCKET_NAME = 'marketplace-dist-private-test'
@@ -10,12 +9,16 @@ ARTIFACTS_PATH = '/home/circleci/project/artifacts/'
 
 
 def option_handler():
-    parser = argparse.ArgumentParser(description='Merging two content_new.zip files from different builds.')
-    parser.add_argument('-f', '--feature_branch', help='The name of the feature branch', required=True)
-
-    options = parser.parse_args()
-
-    return options
+    parser = argparse.ArgumentParser(description="Store packs in cloud storage.")
+    parser.add_argument('-s', '--service_account',
+                        help=("Path to gcloud service account, is for circleCI usage. "
+                              "For local development use your personal account and "
+                              "authenticate using Google Cloud SDK by running: "
+                              "`gcloud auth application-default login` and leave this parameter blank. "
+                              "For more information go to: "
+                              "https://googleapis.dev/python/google-api-core/latest/auth.html"),
+                        required=False)
+    return parser.parse_args()
 
 
 def download_private_id_set_from_gcp():
@@ -25,17 +28,14 @@ def download_private_id_set_from_gcp():
         The new path of the zip file.
     """
 
-    # file_path = "creds.json"
-    # json_content = json.loads(os.environ.get('GCS_ARTIFACTS_KEY'))
-    # with open(file_path, "w") as file:
-    #     json.dump(json_content, file)
-    # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = file_path
-    storage_client = storage.Client()
+    upload_config = option_handler()
+    service_account = upload_config.service_account
+    storage_client = init_storage_client(service_account)
 
     storage_bucket = storage_client.bucket(STORAGE_BUCKET_NAME)
 
     index_blob = storage_bucket.blob(STORAGE_ID_SET_PATH)
-    # os.remove(file_path)
+
     if not os.path.exists(ARTIFACTS_PATH):
         os.mkdir(ARTIFACTS_PATH)
     index_blob.download_to_filename(f'{ARTIFACTS_PATH}/private_id_set.json')
