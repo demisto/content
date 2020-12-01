bf1345178bdc26c8e0c1b5c828fe195fc42ffe42
cshayner <cshayner@paloaltonetworks.com>
2020-12-01 09:28:59 +0200

    add GOOGLE_APPLICATION_CREDENTIALS

--- Tests/Marketplace/merge_private_and_public_id_sets.py
+++ Tests/Marketplace/merge_private_and_public_id_sets.py
@@ -1,6 +1,6 @@
 import os
-import argparse
-from Tests.Marketplace.marketplace_services import init_storage_client
+import json
+from google.cloud import storage
 
 
 STORAGE_BUCKET_NAME = 'marketplace-dist-private-test'
@@ -8,25 +8,24 @@ STORAGE_ID_SET_PATH = 'content/id_set.json'
 ARTIFACTS_PATH = '/home/circleci/project/artifacts/'
 
 
-def option_handler():
-    parser = argparse.ArgumentParser(description="Store packs in cloud storage.")
-    parser.add_argument('-s', '--service_account',
-                        help=("Path to gcloud service account, is for circleCI usage. "
-                              "For local development use your personal account and "
-                              "authenticate using Google Cloud SDK by running: "
-                              "`gcloud auth application-default login` and leave this parameter blank. "
-                              "For more information go to: "
-                              "https://googleapis.dev/python/google-api-core/latest/auth.html"),
-                        required=False)
-    return parser.parse_args()
-
-
-def download_private_id_set_from_gcp(index_blob):
+def download_private_id_set_from_gcp():
     """Save the zip file from the feature branch into artifacts folder.
 
     Returns:
         The new path of the zip file.
     """
+
+    file_path = "creds.json"
+    json_content = json.loads(os.environ.get('GCS_ARTIFACTS_KEY'))
+    with open(file_path, "w") as file:
+        json.dump(json_content, file)
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = file_path
+    storage_client = storage.Client()
+
+    storage_bucket = storage_client.bucket(STORAGE_BUCKET_NAME)
+
+    index_blob = storage_bucket.blob(STORAGE_ID_SET_PATH)
+
     if not os.path.exists(ARTIFACTS_PATH):
         os.mkdir(ARTIFACTS_PATH)
     index_blob.download_to_filename(f'{ARTIFACTS_PATH}/private_id_set.json')
@@ -38,13 +37,7 @@ def download_private_id_set_from_gcp(index_blob):
 
 
 def main():
-    upload_config = option_handler()
-    service_account = upload_config.service_account
-    storage_client = init_storage_client(service_account)
-    storage_bucket = storage_client.bucket(STORAGE_BUCKET_NAME)
-
-    index_blob = storage_bucket.blob(STORAGE_ID_SET_PATH)
-    private_id_set = download_private_id_set_from_gcp(index_blob)
+    private_id_set = download_private_id_set_from_gcp()
     return private_id_set
 
 
