cde56cf3f6400ef0d047073e0dbc51084b11b940
ikeren <itay@demisto.com>
2020-12-06 15:25:18 +0200

    use set_integration_context

--- Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule.py
+++ Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule.py
@@ -200,7 +200,7 @@ class MicrosoftClient(BaseClient):
         if self.multi_resource:
             integration_context.update(self.resource_to_access_token)
 
-        set_to_integration_context_with_retries(integration_context)
+        set_integration_context(integration_context)
 
         if self.multi_resource:
             return self.resource_to_access_token[resource]
@@ -529,5 +529,5 @@ class MicrosoftClient(BaseClient):
             response_json = response.json()
         except Exception as e:
             return_error(f'Error in Microsoft authorization: {str(e)}')
-        set_to_integration_context_with_retries({'device_code': response_json.get('device_code')})
+        set_integration_context({'device_code': response_json.get('device_code')})
         return response_json.get('user_code', '')
