02606143f607308d5c531bc6b93cd22e5c141503
syaakovi <syaakovi@paloaltonetworks.com>
2020-12-06 20:38:40 +0200

    fixed lint errors and updated docker image

--- Packs/CommonScripts/Scripts/ConvertDateToUTC/ConvertDateToUTC.py
+++ Packs/CommonScripts/Scripts/ConvertDateToUTC/ConvertDateToUTC.py
@@ -1,33 +1,48 @@
-import datetime
-
 import demistomock as demisto  # noqa: F401
-import pytz
 from CommonServerPython import *  # noqa: F401
+import pytz
 
-date = demisto.getArg('date')
-date_format = demisto.getArg('date_format')
-timezone = demisto.getArg('timezone')
 
-timezone = pytz.timezone(timezone)
+''' MAIN FUNCTION '''
 
-# Initialize datetime
-date = datetime.datetime.strptime(date, date_format)
 
-# Convert to timezone aware date
-localized_date = timezone.localize(date)
+def main():
+    try:
+        date = demisto.getArg('date')
+        date_format = demisto.getArg('date_format')
+        time_zone = demisto.getArg('timezone')
 
-# Convert to UTC timezone
-utc_converted_date = localized_date.astimezone(pytz.timezone("UTC"))
-epoch_time = utc_converted_date.strftime('%s')
+        time_zone = pytz.timezone(time_zone)
 
-# Initialize entry context to return
-entry_context = {}
-entry_context['UTCDate'] = utc_converted_date.strftime(date_format)
-entry_context['UTCDateEpoch'] = epoch_time
+        # Initialize datetime
+        date = datetime.strptime(date, date_format)
 
-demisto.results({
-    'Contents': json.dumps(entry_context),
-    'ContentsFormat': formats['json'],
-    'EntryContext': entry_context
-}
-)
+        # Convert to timezone aware date
+        localized_date = time_zone.localize(date)
+
+        # Convert to UTC timezone
+        utc_converted_date = localized_date.astimezone(pytz.timezone("UTC"))
+        epoch_time = utc_converted_date.strftime('%s')
+
+        # Initialize entry context to return
+        entry_context = {
+            'UTCDate': utc_converted_date.strftime(date_format),
+            'UTCDateEpoch': epoch_time,
+        }
+
+        return_results({
+            'Contents': json.dumps(entry_context),
+            'ContentsFormat': formats['json'],
+            'EntryContext': entry_context,
+        })
+
+    except Exception as exc:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to execute ConvertDateToUTC. Error: {str(exc)}')
+
+
+''' ENTRY POINT '''
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
