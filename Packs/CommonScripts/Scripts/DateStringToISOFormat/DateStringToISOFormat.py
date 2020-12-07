521ddf16b69e2f83ae85d8eb7d4861e6e14374a2
ikeren <itay@demisto.com>
2020-12-07 22:56:55 +0200

    handle no tz

--- Packs/CommonScripts/Scripts/DateStringToISOFormat/DateStringToISOFormat.py
+++ Packs/CommonScripts/Scripts/DateStringToISOFormat/DateStringToISOFormat.py
@@ -1,10 +1,14 @@
 import demistomock as demisto
-from dateutil.parser import ParserError, parse  # type: ignore
+from dateutil.parser import ParserError, parse
+from datetime import timezone
 
 
 def parse_datestring_to_iso(date_value: str, day_first: bool, year_first: bool, fuzzy: bool) -> str:
     try:
-        date_string = parse(date_value, dayfirst=day_first, yearfirst=year_first, fuzzy=fuzzy).isoformat()
+        datetime_obj = parse(date_value, dayfirst=day_first, yearfirst=year_first, fuzzy=fuzzy)
+        if not datetime_obj.tzinfo:
+            datetime_obj = datetime_obj.replace(tzinfo=timezone.utc)
+        return datetime_obj.isoformat()
     except ParserError as e:
         demisto.error(f'ParserError occurred: {e}\n Returning the original date string.')
         date_string = date_value
