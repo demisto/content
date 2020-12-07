8dccad42b9f58b3e61b4fa2cf1b6fc8a0e57215f
cshayner <cshayner@paloaltonetworks.com>
2020-12-07 12:42:43 +0200

    fix test

--- Tests/Marketplace/Tests/prepare_private_id_set_for_merge_test.py
+++ Tests/Marketplace/Tests/prepare_private_id_set_for_merge_test.py
@@ -18,9 +18,11 @@ def test_remove_old_pack_from_private_id_set():
                                                                       "test_data", "id_set.json"), 'Workday')
     assert workday_pack not in private_id_set['integrations']
 
-    private_id_set = remove_old_pack_from_private_id_set('test_data/id_set.json', 'Feedsslabusech')
+    private_id_set = remove_old_pack_from_private_id_set(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                                      "test_data", "id_set.json"), 'Feedsslabusech')
     assert Feedsslabusech_pack not in private_id_set['integrations']
 
-    private_id_set = remove_old_pack_from_private_id_set('test_data/id_set.json', '')
+    private_id_set = remove_old_pack_from_private_id_set(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                                      "test_data", "id_set.json"), '')
     assert Feedsslabusech_pack in private_id_set['integrations']
     assert workday_pack in private_id_set['integrations']
