08fcec878c4d7bd9047f32cd4cd9e30722732c46
ShahafBenYakir <shahaf.benyakir@demisto.com>
2020-12-07 20:26:01 +0200

    Added check for none

--- Packs/SentinelOne/Integrations/SentinelOne-V2/SentinelOne-V2.py
+++ Packs/SentinelOne/Integrations/SentinelOne-V2/SentinelOne-V2.py
@@ -40,7 +40,8 @@ def http_request(method, url_suffix, params={}, data=None):
         try:
             errors = ''
             for error in res.json().get('errors'):
-                errors = '\n' + errors + error.get('detail')
+                if error.get('detail'):
+                    errors = '\n' + errors + error.get('detail')
             raise Exception(
                 f'Error in API call to Sentinel One [{res.status_code}] - [{res.reason}] \n'
                 f'Error details: [{errors}]'
