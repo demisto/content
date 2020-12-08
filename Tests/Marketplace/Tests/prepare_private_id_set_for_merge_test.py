7e9209bea744d76c7f0dc2c705fd45eb8df39a05
cshayner <cshayner@paloaltonetworks.com>
2020-12-08 09:42:04 +0200

    fix test and validation

--- Tests/Marketplace/Tests/prepare_private_id_set_for_merge_test.py
+++ Tests/Marketplace/Tests/prepare_private_id_set_for_merge_test.py
@@ -3,26 +3,19 @@ from Tests.Marketplace.prepare_private_id_set_for_merge import remove_old_pack_f
 
 
 def test_remove_old_pack_from_private_id_set():
-    workday_pack = {
-            "Workday": {
-                "pack": "Workday"
-            }
-        }
+    workday_pack = {"Workday": {"pack": "Workday"}}
 
-    Feedsslabusech_pack = {
-            "abuse.ch SSL Blacklist Feed": {
-                "pack": "Feedsslabusech"
-            }
-        }
+    abuse_pack = {"abuse.ch SSL Blacklist Feed": {"pack": "Feedsslabusech"}}
+
     private_id_set = remove_old_pack_from_private_id_set(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                                       "test_data", "id_set.json"), 'Workday')
     assert workday_pack not in private_id_set['integrations']
 
     private_id_set = remove_old_pack_from_private_id_set(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                                       "test_data", "id_set.json"), 'Feedsslabusech')
-    assert Feedsslabusech_pack not in private_id_set['integrations']
+    assert abuse_pack not in private_id_set['integrations']
 
     private_id_set = remove_old_pack_from_private_id_set(os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                                       "test_data", "id_set.json"), '')
-    assert Feedsslabusech_pack in private_id_set['integrations']
+    assert abuse_pack in private_id_set['integrations']
     assert workday_pack in private_id_set['integrations']
--- Tests/Marketplace/Tests/upload_private_id_set_test.py
+++ Tests/Marketplace/Tests/upload_private_id_set_test.py
@@ -1,4 +1,5 @@
 import os
+from google.cloud.storage.blob import Blob
 
 from Tests.Marketplace.upload_private_id_set import upload_private_id_set
 from Tests.Marketplace.marketplace_services import GCPConfig
@@ -6,10 +7,15 @@ from Tests.Marketplace.marketplace_services import GCPConfig
 
 def check_path(private_id_set_gcs_path):
     assert private_id_set_gcs_path == 'content/private_id_set.json'
+    blob = Blob
+    return blob
 
 
 def test_upload_private_id_set(mocker):
 
+    # Skip the open and upload part
+    mocker.patch('builtins.open', read_data=None)
+    mocker.patch.object(Blob, 'upload_from_file', return_value=None)
     dummy_storage_bucket = mocker.Mock()
     dummy_storage_bucket.name = GCPConfig.PRODUCTION_BUCKET
     dummy_storage_bucket.blob = check_path
