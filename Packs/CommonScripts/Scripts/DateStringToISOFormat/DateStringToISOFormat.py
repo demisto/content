328fd80beb123151a3ae05fa3404fa7147178e60
ikeren <itay@demisto.com>
2020-12-08 11:52:31 +0200

    add add_utc_timezone arg

--- Packs/CommonScripts/Scripts/DateStringToISOFormat/DateStringToISOFormat.py
+++ Packs/CommonScripts/Scripts/DateStringToISOFormat/DateStringToISOFormat.py
@@ -3,10 +3,12 @@ from dateutil.parser import ParserError, parse  # type: ignore[attr-defined]
 from datetime import timezone
 
 
-def parse_datestring_to_iso(date_value: str, day_first: bool, year_first: bool, fuzzy: bool) -> str:
+def parse_datestring_to_iso(
+        date_value: str, day_first: bool, year_first: bool, fuzzy: bool, add_utc_timezone: bool = False
+) -> str:
     try:
         datetime_obj = parse(date_value, dayfirst=day_first, yearfirst=year_first, fuzzy=fuzzy)
-        if not datetime_obj.tzinfo:
+        if add_utc_timezone and not datetime_obj.tzinfo:
             datetime_obj = datetime_obj.replace(tzinfo=timezone.utc)
         return datetime_obj.isoformat()
     except ParserError as e:
@@ -21,7 +23,8 @@ def main():
     day_first = args.get('dayfirst', 'True').lower() == 'true'
     year_first = args.get('yearfirst', 'False').lower() == 'true'
     fuzzy = args.get('fuzzy', 'True').lower() == 'true'
-    iso_string = parse_datestring_to_iso(date_value, day_first, year_first, fuzzy)
+    add_utc_timezone = args.get('add_utc_timezone', 'true').lower() == 'true'
+    iso_string = parse_datestring_to_iso(date_value, day_first, year_first, fuzzy, add_utc_timezone)
     demisto.results(iso_string)
 
 
