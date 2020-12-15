f55851c79768e2f17e5e785fbef4729b23694cf3
dawar-zingbox <51930552+dawar-zingbox@users.noreply.github.com>
2020-12-14 23:50:22 -0800

    Update Packs/PaloAltoNetworks_IoT3rdParty/Scripts/SendAllPANWIoTDevicesToServiceNow/SendAllPANWIoTDevicesToServiceNow.py
    Co-authored-by: Bar Katzir <37335599+bakatzir@users.noreply.github.com>

--- Packs/PaloAltoNetworks_IoT3rdParty/Scripts/SendAllPANWIoTDevicesToServiceNow/SendAllPANWIoTDevicesToServiceNow.py
+++ Packs/PaloAltoNetworks_IoT3rdParty/Scripts/SendAllPANWIoTDevicesToServiceNow/SendAllPANWIoTDevicesToServiceNow.py
@@ -1,5 +1,5 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
+import demistomock as demisto
+from CommonServerPython import *
 PANW_IOT_INSTANCE = "PANW IoT 3rd Party Integration Instance"
 SERVICENOW_INSTANCE = "PANW IoT 3rd Party ServiceNow Integration Instance"
 PAGE_SIZE = 100
