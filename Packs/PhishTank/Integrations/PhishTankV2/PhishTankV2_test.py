f585f6a9c808a57a5b3756388f3bfa0d82c498fd
bar katzir <bkatzir@paloaltonetworks.com>
2020-12-16 11:04:04 +0200

    fix ut 102

--- Packs/PhishTank/Integrations/PhishTankV2/PhishTankV2_test.py
+++ Packs/PhishTank/Integrations/PhishTankV2/PhishTankV2_test.py
@@ -233,7 +233,9 @@ def test_url_command(mocker, data, url, expected_score, expected_table):
     output = command_results[0].to_context().get('EntryContext', {})
     dbot_key = 'DBotScore(val.Indicator && val.Indicator == obj.Indicator &&' \
                ' val.Vendor == obj.Vendor && val.Type == obj.Type)'
-    assert output.get(dbot_key, []).get('Score') == expected_score
+    print(str(output))
+    print(str(output.get(dbot_key, {})))
+    assert output.get(dbot_key, {}).get('Score') == expected_score
 
     # validate human readable
     hr_ = command_results[0].to_context().get('HumanReadable', {})
