f2afce9b09ad743a855e096873f4e56856683a0b
tlieber <tlieber@paloaltonetworks.com>
2020-12-16 17:37:37 +0200

    new integration

--- Packs/AnsibleTower/Integrations/AnsibleTower/AnsibleTower.py
+++ Packs/AnsibleTower/Integrations/AnsibleTower/AnsibleTower.py
@@ -9,6 +9,9 @@ from typing import Any, Dict, Tuple, List, Optional, Union, cast
 # Disable insecure warnings
 urllib3.disable_warnings()
 
+'''CONSTANTS'''
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
 
 class Client(BaseClient):
     def __init__(self, input_url: str, username: str, password: str, verify_certificate: bool, proxy: bool):
@@ -27,10 +30,85 @@ class Client(BaseClient):
         return self._http_request(method='GET', url_suffix=url_suffix, params=data)
 
 
-def inventories_list(client: Client, url_suffix: str, args: dict) -> List[CommandResults]:
+def generate_info(res: dict):
+    return {'id': res.get('id'),
+            'type': res.get('type'),
+            'url': res.get('url'),
+            'inventory name': res.get('name'),
+            'created': str(parse_date_string(res.get('created'), DATE_FORMAT)),
+            'modified': str(parse_date_string(res.get('modified'), DATE_FORMAT)),
+            'description': res.get('description'),
+            'organization': res.get('organization'),
+            'kind': res.get('kind'),
+            'host_filter': res.get('host_filter'),
+            'variables': res.get('variables'),
+            'has_active_failures': res.get('has_active_failures'),
+            'total_hosts': res.get('total_hosts'),
+            'hosts_with_active_failures': res.get('hosts_with_active_failures'),
+            'total_groups': res.get('total_groups'),
+            'groups_with_active_failures': res.get('groups_with_active_failures'),
+            'has_inventory_sources': res.get('has_inventory_sources'),
+            'total_inventory_sources': res.get('total_inventory_sources'),
+            'inventory_sources_with_failures': res.get('inventory_sources_with_failures'),
+            'insights_credential': res.get('insights_credential'),
+            'pending_deletion': res.get('pending_deletion')
+            }
 
-    res = client.api_request(url_suffix, args)
 
+def inventories_list(client: Client, args: dict) -> List[CommandResults]:
+    command_results = []
+    response = client.api_request('inventories/', args)
+    results = response.get('results')
+    if not len(results):
+        command_results.append(CommandResults(
+            readable_output=f"No results were found for the following argument {str(args)}",
+            raw_response=response
+        ))
+    for res in results:
+        info = generate_info(res)
+        command_results.append(CommandResults(
+            outputs_prefix='AnsibleAWX.Inventory',
+            outputs_key_field='id',
+            outputs=info,
+            readable_output=tableToMarkdown(name='Results', t=info),
+            raw_response=res
+        ))
+    return command_results
+
+
+def hosts_list(client: Client, args: dict) -> List[CommandResults]:
+    inventory_id = args.pop('inventory_id')
+    if inventory_id:
+        url_suffix = f'inventories/{inventory_id}/hosts'
+    else:
+        url_suffix = 'hosts/'
+
+    command_results = []
+    response = client.api_request(url_suffix, args)
+    results = response.get('results')
+    if not len(results):
+        command_results.append(CommandResults(
+            readable_output=f"No results were found for the following argument {str(args)}",
+            raw_response=response
+        ))
+    for res in results:
+        info = generate_info(res)
+        command_results.append(CommandResults(
+            outputs_prefix='AnsibleAWX.Inventory',
+            outputs_key_field='id',
+            outputs=info,
+            readable_output=tableToMarkdown(name='Results', t=info),
+            raw_response=res
+        ))
+    return command_results
+
+
+def test_module(client: Client) -> str:
+     try:
+        client.api_request('inventories/', {})
+        return 'ok'
+     except Exception as e:
+         raise DemistoException(f"Error in API call - check the input parameters. Error: {e}.")
 
 
 def main() -> None:
@@ -39,35 +117,36 @@ def main() -> None:
     :return:
     :rtype:
     """
+    params = demisto.params()
     commands = {
-        'ansible-awx-inventories-list': inventories_list
+        'ansible-awx-inventories-list': inventories_list,
+        'ansible-awx-hosts-list': hosts_list
     }
 
-    base_url = demisto.params()['url']
+    base_url = params.get("url")
 
-    verify_certificate = not demisto.params().get('insecure', False)
+    verify_certificate = not params.get('insecure', False)
 
-    proxy = demisto.params().get('proxy', False)
+    proxy = params.get('proxy', False)
 
     demisto.debug(f'Command being called is {demisto.command()}')
 
-    username = demisto.params().get("credentials")["identifier"],
-    password = demisto.params().get("credentials")["password"],
+    username = params.get("username")
+    password = params.get("password")
 
     try:
 
         client = Client(
-            base_url=base_url,
+            input_url=base_url,
             username=username,
             password=password,
-            verify=verify_certificate,
+            verify_certificate=verify_certificate,
             proxy=proxy)
 
         command = demisto.command()
 
         if command == 'test-module':
-            x=1
-            # return_results(test_module(client))
+            return_results(test_module(client))
         elif command in commands:
             return_results(commands[command](client, demisto.args()))
 
