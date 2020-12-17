9ddaf97cf784a3d5ae365b8c48ef197c187d520e
ikeren <itay@demisto.com>
2020-12-17 09:51:06 +0200

    revert global params assign

--- Packs/PagerDuty/Integrations/PagerDuty/PageDuty_test.py
+++ Packs/PagerDuty/Integrations/PagerDuty/PageDuty_test.py
@@ -2,11 +2,8 @@
 from PagerDuty import get_incidents_command
 from CommonServerPython import *
 
-reload(sys)
-sys.setdefaultencoding('utf8')  # pylint: disable=no-member
 
-
-def test_get_incidents(requests_mock):
+def test_get_incidents(requests_mock, mocker):
     """
     Given:
         - An incident with non-ascii character in its documentation
@@ -18,6 +15,15 @@ def test_get_incidents(requests_mock):
         - Ensure command run without failing on UnicodeError
         - Verify the non-ascii character appears in the human readable output as expected
     """
+    mocker.patch.object(
+        demisto,
+        'params',
+        return_value={
+            'API_KEY': 'API_KEY',
+            'SERVICE_KEY': 'SERVICE_KEY',
+            'FETCH_INTERVAL': 'FETCH_INTERVAL'
+        }
+    )
     requests_mock.get(
         'https://api.pagerduty.com/incidents?include%5B%5D=assignees&statuses%5B%5D=triggered&statuses%5B%5D'
         '=acknowledged&include%5B%5D=first_trigger_log_entries&include%5B%5D=assignments&time_zone=UTC',
