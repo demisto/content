6a076669080e9e4d6fd9a9125a1f1d3a6efea807
Alan Nix <alan.nix@lacework.net>
2020-12-22 11:59:59 -0500

    Changed logic to allow 'rec_id' to be a list of recommendation IDs

--- Packs/Lacework/Integrations/Lacework/Lacework.py
+++ Packs/Lacework/Integrations/Lacework/Lacework.py
@@ -78,7 +78,7 @@ def format_compliance_data(compliance_data, rec_id):
         if rec_id:
             # Iterate through all recommendations, removing irrelevant ones
             for recommendation in compliance_data["recommendations"][:]:
-                if rec_id != recommendation["REC_ID"]:
+                if recommendation["REC_ID"] not in rec_id:
                     compliance_data["recommendations"].remove(recommendation)
 
         # Build Human Readable Output
@@ -111,7 +111,7 @@ def get_aws_compliance_assessment():
     """
 
     account_id = demisto.args().get('account_id')
-    rec_id = demisto.args().get('rec_id')
+    rec_id = argToList(demisto.args().get('rec_id'))
     report_type = demisto.args().get('report_type', 'AWS_CIS_S3')
 
     response = lacework_client.compliance.get_latest_aws_report(account_id,
@@ -129,7 +129,7 @@ def get_azure_compliance_assessment():
 
     tenant_id = demisto.args().get('tenant_id')
     subscription_id = demisto.args().get('subscription_id')
-    rec_id = demisto.args().get('rec_id')
+    rec_id = argToList(demisto.args().get('rec_id'))
     report_type = demisto.args().get('report_type', 'AZURE_CIS')
 
     response = lacework_client.compliance.get_latest_azure_report(tenant_id,
@@ -148,7 +148,7 @@ def get_gcp_compliance_assessment():
 
     organization_id = demisto.args().get('organization_id')
     project_id = demisto.args().get('project_id')
-    rec_id = demisto.args().get('rec_id')
+    rec_id = argToList(demisto.args().get('rec_id'))
     report_type = demisto.args().get('report_type', 'GCP_CIS')
 
     response = lacework_client.compliance.get_latest_gcp_report(organization_id,
