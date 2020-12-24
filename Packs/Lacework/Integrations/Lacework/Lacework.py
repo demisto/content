4a53e9871fa17285dbebf7e5337b53b1dd8c2970
Alan Nix <65611624+alannix-lw@users.noreply.github.com>
2020-12-24 02:59:47 -0500

    Added Recommendation ID filtering to Compliance data. (#10450)
    * Added Recommendation ID filtering to Compliance data.
    
    - Added a Recommendation ID filter for compliance report data.
    - Added NIST_800-171_Rev2 AWS Compliance Report option.
    - Upgraded the Docker image to demisto/lacework:1.0.0.14553
    
    * Changed logic to allow 'rec_id' to be a list of recommendation IDs

--- Packs/Lacework/Integrations/Lacework/Lacework.py
+++ Packs/Lacework/Integrations/Lacework/Lacework.py
@@ -65,7 +65,7 @@ def create_entry(title, data, ec, human_readable=None):
     }
 
 
-def format_compliance_data(compliance_data):
+def format_compliance_data(compliance_data, rec_id):
     """
     Simplify the output/contents for Compliance reports
     """
@@ -73,6 +73,13 @@ def format_compliance_data(compliance_data):
     if len(compliance_data['data']) > 0:
 
         compliance_data = compliance_data['data'][0]
+
+        # If the user wants to filter on a recommendation ID
+        if rec_id:
+            # Iterate through all recommendations, removing irrelevant ones
+            for recommendation in compliance_data["recommendations"][:]:
+                if recommendation["REC_ID"] not in rec_id:
+                    compliance_data["recommendations"].remove(recommendation)
 
         # Build Human Readable Output
         readable_output = tableToMarkdown("Compliance Summary",
@@ -104,13 +111,14 @@ def get_aws_compliance_assessment():
     """
 
     account_id = demisto.args().get('account_id')
+    rec_id = argToList(demisto.args().get('rec_id'))
     report_type = demisto.args().get('report_type', 'AWS_CIS_S3')
 
     response = lacework_client.compliance.get_latest_aws_report(account_id,
                                                                 file_format="json",
                                                                 report_type=report_type)
 
-    results = format_compliance_data(response)
+    results = format_compliance_data(response, rec_id)
     return_results(results)
 
 
@@ -121,6 +129,7 @@ def get_azure_compliance_assessment():
 
     tenant_id = demisto.args().get('tenant_id')
     subscription_id = demisto.args().get('subscription_id')
+    rec_id = argToList(demisto.args().get('rec_id'))
     report_type = demisto.args().get('report_type', 'AZURE_CIS')
 
     response = lacework_client.compliance.get_latest_azure_report(tenant_id,
@@ -128,7 +137,7 @@ def get_azure_compliance_assessment():
                                                                   file_format="json",
                                                                   report_type=report_type)
 
-    results = format_compliance_data(response)
+    results = format_compliance_data(response, rec_id)
     return_results(results)
 
 
@@ -139,6 +148,7 @@ def get_gcp_compliance_assessment():
 
     organization_id = demisto.args().get('organization_id')
     project_id = demisto.args().get('project_id')
+    rec_id = argToList(demisto.args().get('rec_id'))
     report_type = demisto.args().get('report_type', 'GCP_CIS')
 
     response = lacework_client.compliance.get_latest_gcp_report(organization_id,
@@ -146,7 +156,7 @@ def get_gcp_compliance_assessment():
                                                                 file_format="json",
                                                                 report_type=report_type)
 
-    results = format_compliance_data(response)
+    results = format_compliance_data(response, rec_id)
     return_results(results)
 
 
