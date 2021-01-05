443c03862a1bfc80abbc2c8b16c437ae6169e1d8
bachen <bachen@paloaltonetworks.com>
2021-01-05 13:33:35 +0200

    fix lint and validate

--- Tests/Marketplace/Tests/marketplace_services_test.py
+++ Tests/Marketplace/Tests/marketplace_services_test.py
@@ -4,7 +4,7 @@ import json
 import os
 import random
 from unittest.mock import mock_open
-#from mock_open import MockOpen
+from mock_open import MockOpen
 from google.cloud.storage.blob import Blob
 from distutils.version import LooseVersion
 from freezegun import freeze_time
