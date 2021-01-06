309dcd143e99138542d693bc3cab2298ba728efb
bar katzir <bkatzir@paloaltonetworks.com>
2021-01-06 17:13:28 +0200

    add post-commit

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -5,8 +5,8 @@ echo "CIRCLE_BRANCH: $CIRCLE_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_
 
 if [[ $CIRCLE_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]];
   then
-    demisto-sdk validate -a --id-set
+    demisto-sdk validate -a --id-set --post-commit
 else
-  demisto-sdk validate -a --id-set
+  demisto-sdk validate -a --id-set --post-commit
 fi
 
