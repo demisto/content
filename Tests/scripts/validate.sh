d2b2e1b640de03b3ac4733421565829e44a3afe2
bar katzir <bkatzir@paloaltonetworks.com>
2021-01-06 15:55:51 +0200

    check 101

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -5,8 +5,8 @@ echo "CIRCLE_BRANCH: $CIRCLE_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_
 
 if [[ $CIRCLE_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]];
   then
-    demisto-sdk validate -a
+    demisto-sdk validate -a --id-set
 else
-  demisto-sdk validate -g --post-commit
+  demisto-sdk validate -g --post-commit --id-set
 fi
 
