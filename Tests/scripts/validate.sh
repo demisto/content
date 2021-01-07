27d530688426ed1af9c4960d155f8448b1b6481c
bar katzir <bkatzir@paloaltonetworks.com>
2021-01-07 10:27:55 +0200

    fix indent

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -6,7 +6,7 @@ echo "CIRCLE_BRANCH: $CIRCLE_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_
 if [[ $CIRCLE_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]];
   then
     demisto-sdk validate -a --id-set --post-commit
-else
-  demisto-sdk validate -a --id-set --post-commit
+  else
+    demisto-sdk validate -g --id-set --post-commit
 fi
 
