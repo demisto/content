99434e921256aebc4218c15aef2219ad5160dc64
grabin <grabin@paloaltonetworks.com>
2021-01-09 22:00:45 +0200

    modify after customer validation

--- Packs/Base/Scripts/CommonServerPowerShell/CommonServerPowerShell.ps1
+++ Packs/Base/Scripts/CommonServerPowerShell/CommonServerPowerShell.ps1
@@ -274,7 +274,7 @@ class DemistoObject {
             $integration_context = $integration_context.context
         }
 
-        return $integration_context.context
+        return $integration_context
     }
 
     SetIntegrationContext ($Value) {
@@ -614,8 +614,8 @@ function FileResult([string]$file_name, [string]$data, [string]$file_type) {
 function DemistoVersionEqualGreaterThen([string]$version) {
     $demisto_version = $demisto.DemistoVersion().version
     $version_pattern = "\d{1,2}\.\d{1,2}\.\d{1,2}"
-    $current_version = (Select-string -Pattern $version_pattern -InputObject $demisto_version).Matches[0].Value
+    $demisto_version = (Select-string -Pattern $version_pattern -InputObject $demisto_version).Matches[0].Value
     $version = (Select-string -Pattern $version_pattern -InputObject $version).Matches[0].Value
 
-    return [version]::Parse($version) -ge  [version]::Parse($current_version)
+    return [version]::Parse($demisto_version) -ge  [version]::Parse($version)
 }
