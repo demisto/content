95589448b2faae2853dbc58bc5b06f900f29bcfc
tneeman <tneeman@paloaltonetworks.com>
2021-01-11 18:44:44 +0200

    fail test playbook on purpose

--- Packs/BitcoinAbuse/Integrations/BitcoinAbuse/BitcoinAbuse.py
+++ Packs/BitcoinAbuse/Integrations/BitcoinAbuse/BitcoinAbuse.py
@@ -175,10 +175,11 @@ def report_address_command(params: Dict, args: Dict) -> CommandResults:
                                                   description=description)
 
     if argToBoolean(http_response.get('success')):
-        return CommandResults(
-            readable_output=f'Bitcoin address {address} by abuse bitcoin user {abuser}'
-                            f' was reported to BitcoinAbuse service'
-        )
+        raise DemistoException('failing on purpose')
+        # return CommandResults(
+        #     readable_output=f'Bitcoin address {address} by abuse bitcoin user {abuser}'
+        #                     f' was reported to BitcoinAbuse service'
+        # )
     else:
         failure_message = http_response.get('response')
         raise DemistoException(f'bitcoin report address did not succeed: {failure_message}')
