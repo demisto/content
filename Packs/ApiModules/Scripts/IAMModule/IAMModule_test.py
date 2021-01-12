0446f49426ccef43ca7ec223927df1ee34508176
sberman <sberman@paloaltonetworks.com>
2021-01-12 12:49:17 +0200

    reverted changes in package-lock.json

---
+++ Packs/ApiModules/Scripts/IAMModule/IAMModule_test.py
@@ -0,0 +1,15 @@
+class MockCLient():
+    def get_user(self):
+        return None
+
+    def create_user(self):
+        return None
+
+    def update_user(self):
+        return None
+
+    def enable_user(self):
+        return None
+
+    def disable_user(self):
+        return None
