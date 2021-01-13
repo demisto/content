e0aec0efeda85a417b938283742ffe3333a21ada
esharf <esahrf@paloaltonetworks.com>
2021-01-13 11:15:09 +0200

    update RN

--- Packs/ActiveMQ/Integrations/ActiveMQ/ActiveMQ.py
+++ Packs/ActiveMQ/Integrations/ActiveMQ/ActiveMQ.py
@@ -7,7 +7,11 @@ import stomp
 import os
 import traceback
 
-PROXIES = handle_proxy()
+if not demisto.params()['proxy']:
+    del os.environ['HTTP_PROXY']
+    del os.environ['HTTPS_PROXY']
+    del os.environ['http_proxy']
+    del os.environ['https_proxy']
 
 ''' GLOBAL VARS '''
 
