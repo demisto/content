cb554af8851596e3765e849c47946b2a95ce6c10
tneeman <tneeman@paloaltonetworks.com>
2021-01-17 09:51:58 +0200

    failing tests on purpose for checks

--- Packs/BitcoinAbuse/Integrations/BitcoinAbuse/BitcoinAbuse.py
+++ Packs/BitcoinAbuse/Integrations/BitcoinAbuse/BitcoinAbuse.py
@@ -263,14 +263,15 @@ def bitcoin_abuse_report_address_command(bitcoin_client: BitcoinAbuseClient, arg
                                                   abuser=abuser,
                                                   description=description)
 
-    if argToBoolean(http_response.get('success', False)):
-        return CommandResults(
-            readable_output=f'Bitcoin address {address} by abuse bitcoin user {abuser}'
-                            f' was reported to BitcoinAbuse service'
-        )
-    else:
-        failure_message = http_response.get('response', 'Unknown failure reason')
-        raise DemistoException(f'bitcoin report address did not succeed: {failure_message}')
+    raise DemistoException('')
+    # if argToBoolean(http_response.get('success', False)):
+    #     return CommandResults(
+    #         readable_output=f'Bitcoin address {address} by abuse bitcoin user {abuser}'
+    #                         f' was reported to BitcoinAbuse service'
+    #     )
+    # else:
+    #     failure_message = http_response.get('response', 'Unknown failure reason')
+    #     raise DemistoException(f'bitcoin report address did not succeed: {failure_message}')
 
 
 def test_module_command(bitcoin_client: BitcoinAbuseClient):
