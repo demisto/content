15d94e064795d00b0327d4135d826e14e71f0d4b
abaumgarten <abaumgarten@paloaltonetworks.com>
2021-01-19 15:10:48 +0200

    added 2 commands by Alex

--- Packs/SMB/Integrations/SMB/SMB.py
+++ Packs/SMB/Integrations/SMB/SMB.py
@@ -2,15 +2,12 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
-
 ''' IMPORTS '''
-
 
 import tempfile
 from smb.SMBConnection import SMBConnection
 
 ''' GLOBAL VARS '''
-
 
 USER = demisto.params()['credentials']['identifier']
 PASSWORD = demisto.params()['credentials']['password']
@@ -18,7 +15,6 @@ HOSTNAME = demisto.params()['hostname']
 PORT = int(demisto.params()['port'])
 NBNAME = demisto.params()['nbname']
 DOMAIN = demisto.params().get('domain', None)
-
 
 ''' HELPER FUNCTIONS '''
 
@@ -106,6 +102,82 @@ def smb_upload():
         connection.close()
 
 
+def smb_delete_files():
+    share, path_pattern = split_path(demisto.getArg('file_path'))
+    hostname = demisto.args().get('hostname') if demisto.args().get('hostname') else HOSTNAME
+    nbname = demisto.args().get('nbname') if demisto.args().get('nbname') else NBNAME
+    domain = demisto.args().get('domain') if demisto.args().get('domain') else DOMAIN
+
+    if not hostname:
+        return_error('No hostname was configured for the integration, cannot establish connection.')
+    elif not nbname:
+        return_error('No NetBIOS name was configured for the integration, cannot establish connection.')
+    connection = connect(hostname=hostname, domain=domain, user=USER, password=PASSWORD, nb_name=nbname, port=PORT)
+    try:
+        connection.deleteFiles(share, path_pattern)
+        demisto.results("Deleted file {}.".format(path_pattern))
+        return_outputs('Success')
+    finally:
+        connection.close()
+
+
+def smb_list():
+    hostname = demisto.args().get('hostname') if demisto.args().get('hostname') else HOSTNAME
+    nbname = demisto.args().get('nbname') if demisto.args().get('nbname') else NBNAME
+    domain = demisto.args().get('domain') if demisto.args().get('domain') else DOMAIN
+    sharename = []
+
+    if not hostname:
+        return_error('No hostname was configured for the integration, cannot establish connection.')
+    elif not nbname:
+        return_error('No NetBIOS name was configured for the integration, cannot establish connection.')
+    connection = connect(hostname=hostname, domain=domain, user=USER, password=PASSWORD, nb_name=nbname, port=PORT)
+    try:
+        shares = connection.listShares()
+        for share in shares:
+            if not share.isSpecial and share.name not in ['NETLOGON', 'SYSVOL']:
+                sharename.append(str(share.name))
+        demisto.results("The list of shares on the SMB server: {}.".format(sharename))
+    finally:
+        connection.close()
+
+
+def smb_filelist():
+    share = demisto.getArg('file_share')
+    path = demisto.getArg('file_path')
+    hostname = demisto.args().get('hostname') if demisto.args().get('hostname') else HOSTNAME
+    nbname = demisto.args().get('nbname') if demisto.args().get('nbname') else NBNAME
+    domain = demisto.args().get('domain') if demisto.args().get('domain') else DOMAIN
+    fileslist = []
+
+    if not hostname:
+        return_error('No hostname was configured for the integration, cannot establish connection.')
+    elif not nbname:
+        return_error('No NetBIOS name was configured for the integration, cannot establish connection.')
+    connection = connect(hostname=hostname, domain=domain, user=USER, password=PASSWORD, nb_name=nbname, port=PORT)
+    try:
+        filelist = connection.listPath(share, path)
+        for file in filelist:
+            if file.filename not in ['.', '..']:
+                fileslist.append({'Name': str(file.filename), 'Size': str(file.file_size),
+                                  'Path': str(share + '/' + path + '/' + file.filename)})
+        human_readable = tableToMarkdown('Files List', fileslist)
+        demisto.results({
+            'Type': entryTypes['note'],
+            'Contents': fileslist,
+            'ContentsFormat': formats['json'],
+            'HumanReadable': human_readable,
+            'ReadableContentsFormat': formats['markdown'],
+            'EntryContext': {
+                "SMB.File(val.Name == obj.Name || val.Path == obj.Path)": fileslist
+            }
+        })
+    except Exception:
+        demisto.results("No results found at location {}/{}".format(share, path))
+    finally:
+        connection.close()
+
+
 ''' EXECUTION CODE '''
 
 LOG('command is %s' % (demisto.command(),))
@@ -117,5 +189,11 @@ try:
         smb_download()
     elif demisto.command() == 'smb-upload':
         smb_upload()
+    elif demisto.command() == 'smb-list-files':
+        smb_filelist()
+    elif demisto.command() == 'smb-list-shares':
+        smb_list()
+    elif demisto.command() == 'smb-delete-files':
+        smb_delete_files()
 except Exception as e:
     return_error(str(e))
