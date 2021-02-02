b390063d2912299e7f043869fc83091a530aab51
Bar Katzir <37335599+bakatzir@users.noreply.github.com>
2021-02-02 10:44:52 +0200

    Update Packs/PaloAltoNetworks_IoT3rdParty/Scripts/SendAllPANWIoTAssetsToSIEM/SendAllPANWIoTAssetsToSIEM.py
    Co-authored-by: dawar-zingbox <dhamid@paloaltonetworks.com>

--- Packs/PaloAltoNetworks_IoT3rdParty/Scripts/SendAllPANWIoTAssetsToSIEM/SendAllPANWIoTAssetsToSIEM.py
+++ Packs/PaloAltoNetworks_IoT3rdParty/Scripts/SendAllPANWIoTAssetsToSIEM/SendAllPANWIoTAssetsToSIEM.py
@@ -9,7 +9,7 @@ VULNERABILITIES_FIELDS_MAP = [
     ("name", "dvchost="),
     ("profile", "cs1Label=Profile cs1="),
     ("display_profile_category", "cs2Label=Category cs2="),
-    ("profile_vertical", "cs1Labe3=Profile cs3="),
+    ("profile_vertical", "cs3Label=ProfileVertical cs3="),
     ("vendor", "cs4Label=Vendor cs4="),
     ("model", "cs5Label=Model cs5="),
     ("vlan", "cs6Label=Vlan cs6="),
