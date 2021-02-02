4c308a61b96c8e51432fc6290396187c3ffeee94
ohaim <ohaim@paloaltonetworks.com>
2021-02-02 19:22:28 +0200

    apply new patch in order to trigger upload from this branch

--- Tests/scripts/slack_notifier.py
+++ Tests/scripts/slack_notifier.py
@@ -285,7 +285,7 @@ def slack_notifier(build_url, slack_token, test_type, env_results_file_name=None
     branch_name_reg = re.search(r'\* (.*)', branches)
     branch_name = branch_name_reg.group(1)
 
-    if branch_name == 'master':
+    if branch_name == 'fix-index-dates':
         logging.info("Extracting build status")
         if test_type == UNITTESTS_TYPE:
             logging.info("Starting Slack notifications about nightly build - unit tests")
