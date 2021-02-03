12737dfdfaa4c7029a406e74e6fb576f9384ce54
adaud <adaud@paloaltonetworks.com>
2021-02-03 12:43:42 +0200

    add documentation add error handling

--- Packs/CortexXDR/Scripts/XDRIncidentWidgetBase/XDRIncidentWidgetBase.py
+++ Packs/CortexXDR/Scripts/XDRIncidentWidgetBase/XDRIncidentWidgetBase.py
@@ -1,7 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-QUERY_TYPE_TO_PATH = {
+QUERY_TYPE_TO_KEY = {
     'Categories': 'PaloAltoNetworksXDR.Incident.alerts.category',
     'Users': 'PaloAltoNetworksXDR.Incident.users',
     'Hosts': 'PaloAltoNetworksXDR.Incident.hosts',
@@ -13,9 +13,15 @@ QUERY_TYPE_TO_PATH = {
 }
 
 
-def get_path_value(context, path):
-    path = path.split('.')
-    for item in path:
+def get_key_value(context: dict, key: str) -> []:
+    """
+    Return the value of the key from the context dict item, if the key is not exists the function return empty array.
+    :param context: The incident context.
+    :param key: The Key inside the context to get the value from.
+    :return: List of values from this key.
+    """
+    key = key.split('.')
+    for item in key:
         if isinstance(context, list):
             res = []
             for val in context:
@@ -28,7 +34,13 @@ def get_path_value(context, path):
     return context
 
 
-def get_context(incident_id, path):
+def get_context(incident_id: str, key: str) -> list:
+    """
+    Gets incident ID and key inside the incident context and returns the value from this key.
+    :param incident_id: Cortex XDR incident ID.
+    :param key: The Key inside the context to get the value from.
+    :return: List of values from this key.
+    """
     res = demisto.executeCommand("getContext", {'id': incident_id})
     if isError(res):
         return_error(f'Error occurred while trying to get context from incident {incident_id} : {get_error(res)}')
@@ -37,10 +49,17 @@ def get_context(incident_id, path):
     except Exception:
         return []
 
-    return get_path_value(context, path)
+    return get_key_value(context, key)
 
 
-def get_incidents_ids(limit, to_date, from_date):
+def get_incidents_ids(limit: int, to_date: str, from_date: str) -> []:
+    """
+    Returns Cortex XDR incident ids with status Active or Pending using GetIncidentsByQuery script.
+    :param limit: The maximum number of incidents to fetch
+    :param to_date: The end date by which to filter incidents.
+    :param from_date: The start date by which to filter incidents.
+    :return: Cortex XDR incident ids
+    """
     get_incidents_args = {
         'query': 'status:Active or status:Pending and type:"Cortex XDR Incident"',
         'limit': limit,
@@ -56,7 +75,13 @@ def get_incidents_ids(limit, to_date, from_date):
     return [inc.get('id') for inc in incidents]
 
 
-def update_result_dict(context, res_dict, query_type):
+def update_result_dict(context: list, res_dict: dict, query_type: str):
+    """
+    Updated the result dict counters with the current context values.
+    :param context: List of values from the incident context key.
+    :param res_dict: The result dictionary that contains the counters of the query.
+    :param query_type: The item to gets the result in the incident(users/hosts/alarms/e.g).
+    """
     for val in context:
         if not val:
             continue
@@ -78,24 +103,27 @@ def update_result_dict(context, res_dict, query_type):
             res_dict[val] = res_dict[val] + 1
         else:
             res_dict[val] = 1
-    return res_dict
 
 
 def main():
     try:
         args = demisto.args()
 
-        limit = int(args.get('limit'))
         res_type = args.get('reultType')
         query_type = args.get('queryType')
         to_date = args.get('toDate')
         from_date = args.get('fromDate')
 
+        try:
+            limit = int(args.get('limit'))
+        except ValueError:
+            limit = 3000
+
         incidents_ids = get_incidents_ids(limit, to_date, from_date)
 
         res_dict = {}  # type:dict
         for incident_id in incidents_ids:
-            context = get_context(incident_id, QUERY_TYPE_TO_PATH[query_type])
+            context = get_context(incident_id, QUERY_TYPE_TO_KEY[query_type])
             update_result_dict(context, res_dict, query_type)
 
         if res_type == 'Top10':
