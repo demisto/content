51d9a937cc056451b5284c4d093322211b278ad0
esharf <esahrf@paloaltonetworks.com>
2021-02-03 09:51:02 +0200

    fixing the override of the insightCache

--- Packs/CommonScripts/Scripts/GetIndicatorDBotScore/GetIndicatorDBotScore.py
+++ Packs/CommonScripts/Scripts/GetIndicatorDBotScore/GetIndicatorDBotScore.py
@@ -2,6 +2,7 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
+DbotScoreKey = 'DBotScore(val.Indicator && val.Indicator == obj.Indicator && val.Vendor == obj.Vendor && val.Type == obj.Type)'
 indicator_types = {
     'IP': "ip",
     'File SHA1': "file",
@@ -20,29 +21,31 @@ def iterate_indicator_entry(indicator, entry):
     for source in sources:
         dbot_score = Common.DBotScore(indicator=indicator, indicator_type=indicator_type,
                                       integration_name=source, score=entry["score"]).to_context()
-        yield CommandResults(
-            readable_output=tableToMarkdown('Indicator DBot Score', dbot_score),
-            outputs=dbot_score
-        )
+        dbot_score = dbot_score.get(DbotScoreKey, dbot_score)
+        yield CommandResults(readable_output=tableToMarkdown('Indicator DBot Score', dbot_score)), dbot_score
 
 
 def main():
-    indicator = demisto.args()['indicator']
-    resp = demisto.executeCommand("getIndicator", {'value': indicator})
+    try:
+        indicator = demisto.args()['indicator']
+        resp = demisto.executeCommand("getIndicator", {'value': indicator})
 
-    if isError(resp):
-        demisto.results(resp)
-        sys.exit(0)
+        if isError(resp) or not resp:
+            demisto.results(resp)
+            sys.exit(0)
 
-    data = demisto.get(resp[0], "Contents")
+        data = resp[0].get("Contents")
 
-    if not data:
-        demisto.results("No results.")
-        sys.exit(0)
-    for entry in data:
-        for db_entry in iterate_indicator_entry(indicator, entry):
-            return_results(db_entry)
+        if not data:
+            demisto.results("No results.")
+            sys.exit(0)
+        for entry in data:
+            for results, outputs in iterate_indicator_entry(indicator, entry):
+                return_results(results)
+                appendContext(DbotScoreKey, outputs)
+    except Exception as error:
+        return_error(str(error), error)
 
 
-if __name__ in ["builtins", "__main__"]:
+if __name__ in ('builtins', '__builtin__', '__main__'):
     main()
