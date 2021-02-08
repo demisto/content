c9cf8e954c2943735633d5266a623910401fa039
Or Lichter <50324325+orlichter1@users.noreply.github.com>
2021-02-08 21:06:12 +0200

    Maintain "New" tag in packs (#10729)
    * test fixed
    
    Co-authored-by: ohaim <ohaim@paloaltonetworks.com>

--- Tests/Marketplace/Tests/marketplace_services_test.py
+++ Tests/Marketplace/Tests/marketplace_services_test.py
@@ -8,6 +8,7 @@ from mock_open import MockOpen
 from google.cloud.storage.blob import Blob
 from distutils.version import LooseVersion
 from freezegun import freeze_time
+from datetime import datetime, timedelta
 
 from Tests.Marketplace.marketplace_services import Pack, Metadata, input_to_list, get_valid_bool, convert_price, \
     get_higher_server_version, GCPConfig, BucketUploadFlow, PackStatus, load_json, \
@@ -135,6 +136,38 @@ class TestMetadataParsing:
                                                           downloads_count=10)
 
         assert parsed_metadata['price'] == expected
+
+    def test_new_tag_added(self, dummy_pack_metadata, dummy_pack):
+        """
+        Given a new pack (created less than 30 days ago)
+        Then: add "New" tag
+        """
+        dummy_pack._create_date = (datetime.utcnow() - timedelta(5)).strftime(Metadata.DATE_FORMAT)
+        parsed_metadata = dummy_pack._parse_pack_metadata(user_metadata=dummy_pack_metadata, pack_content_items={},
+                                                          pack_id='test_pack_id', integration_images=[],
+                                                          author_image="", dependencies_data={},
+                                                          server_min_version="5.5.0", build_number="dummy_build_number",
+                                                          commit_hash="dummy_commit", downloads_count=10,
+                                                          is_feed_pack=False)
+
+        assert parsed_metadata['tags'] == ['tag number one', 'Tag number two', 'Use Case', 'New']
+
+    def test_new_tag_removed(self, dummy_pack_metadata, dummy_pack):
+        """
+        Given a pack that was created more than 30 days ago
+        Then: remove "New" tag
+        """
+        dummy_pack._create_date = (datetime.utcnow() - timedelta(35)).strftime(Metadata.DATE_FORMAT)
+        if 'New' not in dummy_pack_metadata['tags']:
+            dummy_pack_metadata['tags'].append('New')
+        parsed_metadata = dummy_pack._parse_pack_metadata(user_metadata=dummy_pack_metadata, pack_content_items={},
+                                                          pack_id='test_pack_id', integration_images=[],
+                                                          author_image="", dependencies_data={},
+                                                          server_min_version="5.5.0", build_number="dummy_build_number",
+                                                          commit_hash="dummy_commit", downloads_count=10,
+                                                          is_feed_pack=False)
+
+        assert parsed_metadata['tags'] == ["tag number one", "Tag number two", 'Use Case']
 
     def test_use_case_tag_added_to_metadata(self, dummy_pack_metadata, dummy_pack):
         """
