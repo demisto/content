450fce54d757b8ee9d864cfada2cda6e5d8c704e
lperry <lperry@paloaltonetworks.com>
2021-02-09 20:32:46 +0200

    canvas

--- Packs/Phishing/Scripts/FindEmailsCampaign/FindEmailsCampaign.py
+++ Packs/Phishing/Scripts/FindEmailsCampaign/FindEmailsCampaign.py
@@ -330,7 +330,21 @@ def return_involved_incidents_entry(incidents_df, indicators_df, fields_to_displ
 
 
 def draw_canvas(incidents, indicators):
-    pass
+    incident_ids = set(map(lambda x: x['id'], incidents))
+    filtered_indicators = []
+    for indicator in indicators:
+        investigations = indicator.get('investigationIDs', [])
+        mutual_incidents_in_canvas = len(set(investigations).intersection(incident_ids))
+        if mutual_incidents_in_canvas >= 2:
+            filtered_indicators.append(indicator)
+
+    res = demisto.executeCommand('DrawRelatedIncidentsCanvas', {'relatedIncidentsIDs': list(incident_ids),
+                                                                'indicators': filtered_indicators,
+                                                                'overrideUserCanvas': 'true'
+                                                                })
+
+    if not is_error(res) or True:
+        demisto.results(res)
 
 
 def analyze_incidents_campaign(incidents, fields_to_display):
