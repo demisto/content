c6637ef979f07bd63ea43ad268b862b087530409
Maciej Drobniuch <mdrobniuch@paloaltonetworks.com>
2021-02-16 10:08:56 +0100

    added fixes in code as per Bar's suggestion

--- Packs/UnifiVideoNVR/Integrations/UnifiVideo/UnifiVideo.py
+++ Packs/UnifiVideoNVR/Integrations/UnifiVideo/UnifiVideo.py
@@ -94,7 +94,7 @@ if demisto.command() == 'unifivideo-get-recording':
     output = f.read()
     filename = recording_file_name
     file = fileResult(filename=filename, data=output)
-    file['Type'] = entryTypes['file']
+    fileResult(filename=filename, data=output, file_type=EntryType.ENTRY_INFO_FILE)
     demisto.results(file)
 
 if demisto.command() == 'unifivideo-get-recording-motion-snapshot':
