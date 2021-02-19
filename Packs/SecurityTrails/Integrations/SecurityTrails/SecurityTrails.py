09fad07e6aded1c7b8c6afc23d95f5754facaa33
Matt Houston <58996090+mhouston007@users.noreply.github.com>
2021-02-19 11:43:47 -0500

    Update Packs/SecurityTrails/Integrations/SecurityTrails/SecurityTrails.py
    Co-authored-by: reut shalem <50294648+reutshal@users.noreply.github.com>

--- Packs/SecurityTrails/Integrations/SecurityTrails/SecurityTrails.py
+++ Packs/SecurityTrails/Integrations/SecurityTrails/SecurityTrails.py
@@ -92,7 +92,7 @@ def get_domain_details(client: Client, **args) -> CommandResults:
     for record_type, record_values in current_dns.items():
         # If a record type has multiple values, this will output the last item in MD
         temp_values: Dict[Any, Any] = {}
-        for x in record_values["values"]:
+        for x in record_values.get("values", []):
             temp_values.update(**x)
         record_values.update(temp_values)
         del record_values["values"]
