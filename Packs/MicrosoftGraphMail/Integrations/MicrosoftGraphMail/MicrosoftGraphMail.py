85ba61d9df102870d6abd9854a93734f8b15992f
tlieber <tlieber@paloaltonetworks.com>
2021-02-21 15:15:48 +0200

    add logout function

--- Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail.py
+++ Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail.py
@@ -109,14 +109,19 @@ class MsGraphClient:
         no_folder = f'/users/{user_id}/messages'
         with_folder = f'/users/{user_id}/{build_folders_path(folder_id)}/messages'
         pages_to_pull = demisto.args().get('pages_to_pull', 1)
-        user_timeout = int(demisto.args().get('timeout', 300))
+        user_timeout = float(demisto.args().get('timeout', 300))
 
         if search:
             odata = f'{odata}&$search="{search}"' if odata else f'$search="{search}"'
         suffix = with_folder if folder_id else no_folder
         if odata:
             suffix += f'?{odata}'
-        response = self.ms_client.http_request('GET', suffix, timeout=user_timeout)
+        try:
+            response = self.ms_client.http_request('GET', suffix, timeout=user_timeout)
+        except DemistoException as e:
+            if 'requests.exceptions.ConnectionError' in e.message:
+                raise DemistoException("Connection timeout error. You can set timeout as an argument while running this "
+                                       "command, try to increase the value.", e)
         return self.pages_puller(response, assert_pages(pages_to_pull))
 
     def delete_mail(self, user_id: str, message_id: str, folder_id: str = None) -> bool:
