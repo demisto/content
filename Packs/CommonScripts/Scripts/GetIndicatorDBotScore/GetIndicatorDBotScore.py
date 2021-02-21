63b47b4e5f88cdfa4f143ddd7fede15bd53b38a4
jgranot <jgranot@paloaltonetworks.com>
2021-02-21 09:33:11 +0200

    GetIndicatorDbotScore script - remove invalid email type

--- Packs/CommonScripts/Scripts/GetIndicatorDBotScore/GetIndicatorDBotScore.py
+++ Packs/CommonScripts/Scripts/GetIndicatorDBotScore/GetIndicatorDBotScore.py
@@ -11,7 +11,6 @@ INDICATOR_TYPES = {
     'File MD5': DBotScoreType.FILE,
     'File SHA256': DBotScoreType.FILE,
     'File SHA-256': DBotScoreType.FILE,
-    'Email': DBotScoreType.URL,
     'URL': DBotScoreType.URL
 }
 
