743097361f9c0d5e827bed6146af6fcacce99afd
nmaimon <nmaimon@paloaltonetworks.com>
2021-02-22 15:07:51 +0200

    feed template

--- Packs/HelloWorld/Integrations/FeedHelloWorld/FeedHelloWorld.py
+++ Packs/HelloWorld/Integrations/FeedHelloWorld/FeedHelloWorld.py
@@ -35,6 +35,9 @@ class Client(BaseClient):
 
         result = []
 
+        # In this case the feed output is in text format, so extracting the indicators from the response requires
+        # iterating over it's lines solely. Other feeds could be in other kinds of formats (CSV, MISP, etc.), or might
+        # require additional processing as well.
         try:
             indicators = res.split('\n')
 
@@ -49,11 +52,10 @@ class Client(BaseClient):
         except ValueError as err:
             demisto.debug(str(err))
             raise ValueError(f'Could not parse returned data to Json. \n\nError massage: {err}')
-
         return result
 
 
-def test_module(client: Client, *_) -> Tuple[str, Dict[Any, Any], Dict[Any, Any]]:
+def test_module(client: Client) -> Tuple[str, Dict[Any, Any], Dict[Any, Any]]:
     """Builds the iterator to check that the feed is accessible.
     Args:
         client: Client object.
@@ -70,6 +72,7 @@ def fetch_indicators(client: Client, tlp_color: Optional[str] = None, limit: int
     """Retrieves indicators from the feed
     Args:
         client (Client): Client object with request
+        tlp_color (str): Traffic Light Protocol color
         limit (int): limit the results
     Returns:
         Indicators.
@@ -95,6 +98,9 @@ def fetch_indicators(client: Client, tlp_color: Optional[str] = None, limit: int
             'rawJSON': raw_data
         }
 
+        if tlp_color:
+            indicator_obj['fields']['trafficlightprotocol'] = tlp_color
+
         indicators.append(indicator_obj)
     return indicators
 
@@ -112,8 +118,9 @@ def get_indicators_command(client: Client,
         Outputs.
     """
     limit = int(args.get('limit', '10'))
-    indicators = fetch_indicators(client, limit)
-    human_readable = tableToMarkdown('Indicators from Talos Feed:', indicators,
+    tlp_color = params.get('tlp_color')
+    indicators = fetch_indicators(client, tlp_color, limit)
+    human_readable = tableToMarkdown('Indicators from FeedHelloWorld:', indicators,
                                      headers=['value', 'type'], removeNull=True)
 
     return human_readable, {}, {'raw_response': indicators}
@@ -127,21 +134,39 @@ def fetch_indicators_command(client: Client, params: Dict[str, str]) -> List[Dic
     Returns:
         Indicators.
     """
-    indicators = fetch_indicators(client)
+    tlp_color = params.get('tlp_color')
+    indicators = fetch_indicators(client, tlp_color)
     return indicators
 
 
 def main():
     """
-    PARSE AND VALIDATE INTEGRATION PARAMS
+    main function, parses params and runs command functions
     """
+
     params = demisto.params()
+
+    # get the service API url
     base_url = params.get('url')
+
+    # if your Client class inherits from BaseClient, SSL verification is
+    # handled out of the box by it, just pass ``verify_certificate`` to
+    # the Client constructor
     insecure = not params.get('insecure', False)
+
+    # if your Client class inherits from BaseClient, system proxy is handled
+    # out of the box by it, just pass ``proxy`` to the Client constructor
     proxy = params.get('proxy', False)
 
     command = demisto.command()
-    demisto.info(f'Command being called is {command}')
+    args = demisto.args()
+
+    # INTEGRATION DEVELOPER TIP
+    # You can use functions such as ``demisto.debug()``, ``demisto.info()``,
+    # etc. to print information in the XSOAR server log. You can set the log
+    # level on the server configuration
+    # See: https://xsoar.pan.dev/docs/integrations/code-conventions#logging
+    demisto.debug(f'Command being called is {command}')
 
     try:
         client = Client(
@@ -150,16 +175,19 @@ def main():
             proxy=proxy,
         )
 
-        commands: Dict[
-            str, Callable[[Client, Dict[str, str], Dict[str, str]], Tuple[str, Dict[Any, Any], Dict[Any, Any]]]
-        ] = {
-            'test-module': test_module,
-            'FeedHelloWorld-get-indicators': get_indicators_command
-        }
-        if command in commands:
-            return_outputs(*commands[command](client, demisto.params(), demisto.args()))
+        if command == 'test-module':
+            # This is the call made when pressing the integration Test button.
+            return_outputs(*test_module(client))
+
+        elif command == 'FeedHelloWorld-get-indicators':
+            # This is the command that fetches a limited number of indicators from the feed source
+            # and displays them in the war room.
+            return_outputs(*get_indicators_command(client, params, args))
 
         elif command == 'fetch-indicators':
+            # This is the command that initiates a request to the feed endpoint and create new indicators objects from
+            # the data fetched. If the integration instance is configured to fetch indicators, then this is the command
+            # that will be executed at the specified Feed Fetch Interval
             indicators = fetch_indicators_command(client, demisto.params())
             for iter_ in batch(indicators, batch_size=2000):
                 demisto.createIndicators(iter_)
@@ -167,11 +195,10 @@ def main():
         else:
             raise NotImplementedError(f'Command {command} is not implemented.')
 
-    except Exception:
-        err_msg = (f'Error in {INTEGRATION_NAME} Integration.\n\n'
-                   'Verify that the server URL parameter is correct and that you'
-                   ' have access to the server from your host.\n')
-        return_error(err_msg)
+    # Log exceptions and return errors
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to execute {command} command.\nError:\n{str(e)}')
 
 
 if __name__ in ['__main__', 'builtin', 'builtins']:
