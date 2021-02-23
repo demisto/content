c818f911d9c52464f257a63eebb31aa3cda690cc
abaumgarten <abaumgarten@paloaltonetworks.com>
2021-02-23 13:06:28 +0200

    update by CR

--- Packs/Box/Integrations/BoxV2/BoxV2.py
+++ Packs/Box/Integrations/BoxV2/BoxV2.py
@@ -717,7 +717,7 @@ class Client(BaseClient):
                 upload_url_suffix = '/files/content'
                 attributes = {
                     'name': file_name,
-                    'parent': {'id': '0'}
+                    'parent': {'id': folder_id}
                 }
                 data = {'attributes': json.dumps(attributes)}
                 files = {'file': ('unused', file)}
