eb93ffe34a8d8139037f4b6bb0b4446b23dabd95
nmaimon <nmaimon@paloaltonetworks.com>
2021-02-23 12:09:38 +0200

    feed template

--- Packs/HelloWorld/Integrations/FeedHelloWorld/FeedHelloWorld_test.py
+++ Packs/HelloWorld/Integrations/FeedHelloWorld/FeedHelloWorld_test.py
@@ -1,7 +1,16 @@
-from FeedHelloWorld import Client
-
+from FeedHelloWorld import Client, get_indicators_command, fetch_indicators_command
+from CommonServerPython import tableToMarkdown
+import json
+import io
+import pytest
+import demistomock as demisto
 
 URL = "https://openphish.com/feed.txt"
+
+
+def util_load_json(path):
+    with io.open(path, mode='r', encoding='utf-8') as f:
+        return json.loads(f.read())
 
 
 def test_build_iterator(requests_mock):
@@ -28,7 +37,8 @@ def test_build_iterator(requests_mock):
     url_indicators = {indicator['value'] for indicator in indicators if indicator['type'] == 'URL'}
     assert expected_url in url_indicators
 
- def test_fetch_indicators():
+
+def test_fetch_indicators(mocker):
     """
 
     Given:
@@ -39,8 +49,27 @@ def test_build_iterator(requests_mock):
         - Create indicator objects list
 
     """
-    mocker.patch.object(Client, "build_iterator", return_value=util_load_json('./build_iterator_results')
-    results = fetch_indicators_command(client, limit=limit)
-#
-#
-# def test_get_indicators_command()
+    client = Client(base_url=URL)
+    mocker.patch.object(Client, 'build_iterator', return_value=util_load_json('./test_data/build_iterator_results.json'))
+    results = fetch_indicators_command(client, params={'tlp_color': 'RED'})
+    assert results == util_load_json('./test_data/get_indicators_command_results.json')
+
+
+def test_get_indicators_command(mocker):
+    """
+
+    Given:
+        - Output of the feed API as list
+    When:
+        - Getting a limited number of indicators from the API
+    Then:
+        - Return results as war-room entry
+
+    """
+    client = Client(base_url=URL)
+    indicators_list = util_load_json('./test_data/build_iterator_results.json')[:10]
+    mocker.patch.object(Client, 'build_iterator', return_value=indicators_list)
+    results = get_indicators_command(client, params={'tlp_color': 'RED'}, args={'limit': '10'})
+    human_readable = tableToMarkdown('Indicators from HelloWorld Feed:', indicators_list,
+                                     headers=['value', 'type'], removeNull=True)
+    assert results.readable_output == human_readable
