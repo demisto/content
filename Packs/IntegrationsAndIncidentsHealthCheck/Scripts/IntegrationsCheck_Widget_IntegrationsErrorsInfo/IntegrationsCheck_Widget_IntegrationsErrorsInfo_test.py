3f4e0a6ad0fc4fc84f83f85f7feb563a806d0d1c
tlieber <tlieber@paloaltonetworks.com>
2021-03-01 13:56:07 +0200

    Add unit test for the bug use case

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IntegrationsCheck_Widget_IntegrationsErrorsInfo/IntegrationsCheck_Widget_IntegrationsErrorsInfo_test.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/IntegrationsCheck_Widget_IntegrationsErrorsInfo/IntegrationsCheck_Widget_IntegrationsErrorsInfo_test.py
@@ -5,6 +5,11 @@ from test_data.constants import FAILED_TABLE, FAILED_TABLE_EXPECTED
 
 
 @pytest.mark.parametrize('list_, expected', [
+    ([{'Contents': 'Item not found (8)'}], {'data': [{'Brand': 'N\\A',
+                                                      'Category': 'N\\A',
+                                                      'Information': 'N\\A',
+                                                      'Instance': 'N\\A'}],
+                                            'total': 1}),
     ([{'Contents': FAILED_TABLE}], FAILED_TABLE_EXPECTED),
     ([{'Contents': ''}], {'data': [{'Brand': 'N\\A',
                                     'Category': 'N\\A',
