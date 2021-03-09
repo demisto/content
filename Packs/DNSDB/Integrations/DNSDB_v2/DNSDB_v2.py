4b5811646aaecc5ba3ef323d3377c04ce7e84073
Henry Stern <stern@fsi.io>
2021-03-09 08:17:14 -0400

    Remove hyphen from swclient

--- Packs/DNSDB/Integrations/DNSDB_v2/DNSDB_v2.py
+++ Packs/DNSDB/Integrations/DNSDB_v2/DNSDB_v2.py
@@ -39,7 +39,7 @@ RATE_SUBCONTEXT_NAME = 'Rate'
 # CONSTANTS
 DEFAULT_DNSDB_SERVER = 'https://api.dnsdb.info'
 TIMEOUT = 60
-SWCLIENT = "demisto-integration"
+SWCLIENT = "demisto"
 VERSION = "v2.0"
 PATH_PREFIX = 'dnsdb/v2'
 IDN_REGEX = re.compile(r'(?:^|(?<=[\s=.:@]))xn--[a-z0-9\-]+\.')
