9237747db9bc97b840b786d8f4a057ea0bdba7d9
yguezpa <yguez@paloaltonetworks.com>
2021-03-10 12:04:10 +0200

    UT

--- Packs/Base/Scripts/DBotFindSimilarIncidents/dbot_find_similar_incidents_test.py
+++ Packs/Base/Scripts/DBotFindSimilarIncidents/dbot_find_similar_incidents_test.py
@@ -3,6 +3,45 @@ import pandas as pd
 # import pytest
 from DBotFindSimilarIncidents import *
 
+
+def executeCommand(command, args):
+    indicator = [{'id': 'a', 'investigationIDs': ['1', '2', '10'], 'value': 'value_a', 'indicator_type': 'URL'},
+                 {'id': 'b', 'investigationIDs': ['2', '10'], 'value': 'value_b', 'indicator_type': 'File'}]
+
+    indicators_list = [{'id': 'a', 'investigationIDs': ['1', '2', '10'], 'value': 'value_a', 'indicator_type': 'File'},
+                       {'id': 'b', 'investigationIDs': ['2', '10'], 'value': 'value_b', 'indicator_type': 'Domain'},
+                       {'id': 'c', 'investigationIDs': ['3', '45'], 'value': 'value_c', 'indicator_type': 'Email'},
+                       {'id': 'd', 'investigationIDs': ['1', '45'], 'value': 'value_d', 'indicator_type': 'File'},
+                       {'id': 'c', 'investigationIDs': ['2', '45'], 'value': 'value_c', 'indicator_type': 'File'}
+                       ]
+
+    if command == 'DBotFindSimilarIncidentsByIndicators' and 'OR' in args['query']:
+        return [{'Contents': indicators_list, 'Type': 'note'}]
+    if command == 'GetIncidentsByQuery':
+        if 'limit' in args:
+            return [{'Contents': indicators_list, 'Type': 'note'}]
+        else:
+            return [{'Contents': indicators_list, 'Type': 'note'}]
+
+
+
+
+def test_get_prediction_for_incident(mocker):
+    mocker.patch.object(demisto, 'args',
+                        return_value={
+                            'incidentId': 12345,
+                            'maxIncidentsInIndicatorsForWhiteList': '150',
+                            'aggreagateIncidents': 'True',
+                            'minNumberOfIndicators': '0',
+                            'threshold': '0.1',
+                            'indicatorsTypes': 'File,  URL, IP, Domain, IPv6',
+                            'showActualIncident': "True",
+                            'maxIncidentsToDisplay': '150',
+                            'fieldsIncidentToDisplay': 'type'
+                        })
+    mocker.patch.object(demisto, 'executeCommand', side_effect=executeCommand)
+
+
 TRANSFORMATION = {
     'commandline': {'transformer': Tfidf,
                     'normalize': normalize_command_line,
