8ceb8f94e4da522719bada73eb90ec2f7eccf4aa
bar katzir <bkatzir@paloaltonetworks.com>
2021-03-12 10:29:44 +0200

    flake8 and xsoar linter

--- Packs/CommonScripts/Scripts/IsRFC1918Address/IsRFC1918Address.py
+++ Packs/CommonScripts/Scripts/IsRFC1918Address/IsRFC1918Address.py
@@ -5,18 +5,21 @@ from netaddr import IPAddress, IPNetwork
 
 
 def main():
-    args = demisto.args()
-    ip_address = args.get('value')
-    if not ip_address:
-        ip_address = args.get('left')
-    cidr_range_list = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
+    try:
+        args = demisto.args()
+        ip_address = args.get('value')
+        if not ip_address:
+            ip_address = args.get('left')
+        cidr_range_list = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
 
-    for cidr in cidr_range_list:
-        if IPAddress(ip_address) in IPNetwork(cidr):
-            demisto.results(True)
-            return
+        for cidr in cidr_range_list:
+            if IPAddress(ip_address) in IPNetwork(cidr):
+                demisto.results(True)
+                return
 
-    demisto.results(False)
+        demisto.results(False)
+    except Exception as err:
+        return_error(str(err))
 
 
 if __name__ == "__builtin__" or __name__ == "builtins":
