0897f07f4326c1024e36a9c30673cf2b59c3375b
bar katzir <bkatzir@paloaltonetworks.com>
2021-03-12 09:16:26 +0200

    IsRFC1918Address - Fixed an issue where the script was not working as a transformer.

--- Packs/CommonScripts/Scripts/IsRFC1918Address/IsRFC1918Address.py
+++ Packs/CommonScripts/Scripts/IsRFC1918Address/IsRFC1918Address.py
@@ -4,8 +4,13 @@ from netaddr import IPAddress, IPNetwork
 
 
 def main():
-    ip_address = demisto.args()['value']
-    cidr_range_list = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
+    args = demisto.args()
+
+    ip_address = args.get('value', 'left')
+
+    cidr_range_list = argToList(args.get('right'))
+    if not cidr_range_list:
+        cidr_range_list = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
 
     for cidr in cidr_range_list:
         if IPAddress(ip_address) in IPNetwork(cidr):
