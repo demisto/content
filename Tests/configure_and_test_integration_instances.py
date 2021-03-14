3596388902132234a2c18b03c1729d089f0c7ffd
hod-alpert <haplert@paloaltonetworks.com>
2021-03-14 12:21:59 +0200

    Supporting docker signing

--- Tests/configure_and_test_integration_instances.py
+++ Tests/configure_and_test_integration_instances.py
@@ -39,14 +39,14 @@ MARKET_PLACE_MACHINES = ('master',)
 SKIPPED_PACKS = ['NonSupported', 'ApiModules']
 NO_PROXY = ','.join([
     'oproxy.demisto.ninja',
-    'oproxy-dev.demisto.ninja',
+    'oproxy-dev.demisto.ninja'
 ])
 NO_PROXY_CONFIG = {'python.pass.extra.keys': f'--env##no_proxy={NO_PROXY}'}  # noqa: E501
 DOCKER_HARDENING_CONFIGURATION = {
     'docker.cpu.limit': '1.0',
     'docker.run.internal.asuser': 'true',
     'limit.docker.cpu': 'true',
-    'python.pass.extra.keys': f'--memory=1g##--memory-swap=-1##--pids-limit=256##--ulimit=nofile=1024:8192##--env##no_proxy={NO_PROXY}',    # noqa: E501
+    'python.pass.extra.keys': f'--memory=1g##--memory-swap=-1##--pids-limit=256##--ulimit=nofile=1024:8192##--env##no_proxy={NO_PROXY}##--disable-content-trust=false',    # noqa: E501
     'powershell.pass.extra.keys': f'--env##no_proxy={NO_PROXY}',
 }
 DOCKER_HARDENING_CONFIGURATION_FOR_PODMAN = {
