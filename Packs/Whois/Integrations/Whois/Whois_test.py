d84347f244be5d1da50577a8496e73e09c40f69a
ypreisler <ypreisler@paloaltonetworks.com>
2021-03-14 13:49:34 +0200

    Updated review changes

--- Packs/Whois/Integrations/Whois/Whois_test.py
+++ Packs/Whois/Integrations/Whois/Whois_test.py
@@ -8,6 +8,8 @@ import time
 import tempfile
 import sys
 
+from CommonServerPython import DBotScoreReliability
+
 
 def assert_results_ok():
     assert demisto.results.call_count == 1
@@ -73,12 +75,14 @@ TEST_QUERY_RESULT_INPUT = [
         {'contacts': {'admin': None, 'billing': None, 'registrant': None, 'tech': None},
          'raw': ['NOT FOUND\n>>> Last update of WHOIS database: 2020-05-07T13:55:34Z <<<']},
         'rsqupuo.info',
+        DBotScoreReliability.A,
         False
     ),
     (
         {'contacts': {'admin': None, 'billing': None, 'registrant': None, 'tech': None},
          'raw': ['No match for "BLABLA43213422342AS.COM".>>> Last update of whois database: 2020-05-20T08:39:17Z <<<']},
-        "BLABLA43213422342AS.COM", False
+        "BLABLA43213422342AS.COM",
+        DBotScoreReliability.A, False
     ),
     (
         {'status': ['clientUpdateProhibited (https://www.icann.org/epp#clientUpdateProhibited)'],
@@ -92,11 +96,13 @@ TEST_QUERY_RESULT_INPUT = [
          'raw': ['Domain Name: google.com\nRegistry Domain ID: 2138514_DOMAIN_COM-VRSN'],
          'creation_date': [datetime.datetime(1997, 9, 15, 0, 0)], 'id': ['2138514_DOMAIN_COM-VRSN']},
         'google.com',
+        DBotScoreReliability.A,
         True
     ),
     (
         {'contacts': {'admin': None, 'billing': None, 'registrant': None, 'tech': None}},
         'rsqupuo.info',
+        DBotScoreReliability.A,
         False
     ),
     (
@@ -111,6 +117,7 @@ TEST_QUERY_RESULT_INPUT = [
          'raw': 'Domain Name: google.com\nRegistry Domain ID: 2138514_DOMAIN_COM-VRSN',
          'creation_date': [datetime.datetime(1997, 9, 15, 0, 0)], 'id': ['2138514_DOMAIN_COM-VRSN']},
         'google.com',
+        DBotScoreReliability.A,
         True
     ),
     (
@@ -125,19 +132,21 @@ TEST_QUERY_RESULT_INPUT = [
          'raw': {'data': 'Domain Name: google.com\nRegistry Domain ID: 2138514_DOMAIN_COM-VRSN'},
          'creation_date': [datetime.datetime(1997, 9, 15, 0, 0)], 'id': ['2138514_DOMAIN_COM-VRSN']},
         'google.com',
+        DBotScoreReliability.A,
         True
     ),
     (
         {'contacts': {'admin': None, 'billing': None, 'registrant': None, 'tech': None},
          'raw': {'data': 'Domain Name: google.com\nRegistry Domain ID: 2138514_DOMAIN_COM-VRSN'}},
         'rsqupuo.info',
+        DBotScoreReliability.A,
         True
     ),
 ]
 
 
-@pytest.mark.parametrize('whois_result, domain, expected', TEST_QUERY_RESULT_INPUT)
-def test_query_result(whois_result, domain, expected):
+@pytest.mark.parametrize('whois_result, domain, reliability, expected', TEST_QUERY_RESULT_INPUT)
+def test_query_result(whois_result, domain, reliability, expected):
     from Whois import create_outputs
-    md, standard_ec, dbot_score = create_outputs(whois_result, domain)
+    md, standard_ec, dbot_score = create_outputs(whois_result, domain, reliability)
     assert standard_ec['Whois']['QueryResult'] == expected
