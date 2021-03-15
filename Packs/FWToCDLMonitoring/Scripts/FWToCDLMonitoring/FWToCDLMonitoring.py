a757a37b3a5f5941694d2911fafa223dc1bdfad0
Brice RENAUD <brenaud@paloaltonetworks.com>
2021-03-15 18:22:55 +0100

    Update FWToCDLMonitoring.py

--- Packs/FWToCDLMonitoring/Scripts/FWToCDLMonitoring/FWToCDLMonitoring.py
+++ Packs/FWToCDLMonitoring/Scripts/FWToCDLMonitoring/FWToCDLMonitoring.py
@@ -1,7 +1,6 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
-
 import datetime
+import demistomock as demisto
+from CommonServerPython import *
 
 # Cortex Data Lake FW log monitoring monitoring
 # PRE_REQUISITE: Enable CDL integration and/or Panorama integration (if the automated creation of a FW list is expected)
@@ -52,7 +51,7 @@ if not fw_monitor_list:
 
     if panorama_integration in allInstances:
         if not allInstances[panorama_integration]['state'] == 'active' and \
-            allInstances[panorama_integration]['brand'] == 'Panorama':
+                allInstances[panorama_integration]['brand'] == 'Panorama':
             return_error("Integration %s is not active or is not a Panorama integration." % panorama_integration)
     else:
         return_error(
