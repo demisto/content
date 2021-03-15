cb23483bb44bce42255a374138e7a316290657dd
ypreisler <ypreisler@paloaltonetworks.com>
2021-03-15 20:37:19 +0200

    Added unit test

--- Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
+++ Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
@@ -1,10 +1,24 @@
-import unittest
+import json
+import pytest
+import demistomock as demisto
+from ThreatMiner import file_command
 
+def load_test_data(json_path):
+    with open(json_path) as f:
+        return json.load(f)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_file(mocker):
+    mocker.patch.object(demisto, 'params', return_value={'threat_miner_url': 'https://api.threatminer.org/v2/',
+                                                            'verify_certificates': True,
+                                                            'reliability': 'C - Fairly reliable',
+                                                            'max_array_size': 30,
+                                                            'proxy': False
+                                                         })
+    args = {
+        'threat_miner_url': 'https://api.threatminer.org/v2/',
+        'verify_certificates': True,
+        'reliability': 'C - Fairly reliable',
+        'max_array_size': 30
+    }
+    res = file_command(**args)
+    print(res)
