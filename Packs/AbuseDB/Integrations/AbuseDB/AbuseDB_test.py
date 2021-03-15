e7fdf05abed9b8e06faee87b6378030f73389134
jgranot <jgranot@paloaltonetworks.com>
2021-03-15 17:39:46 +0200

    added readme

--- Packs/AbuseDB/Integrations/AbuseDB/AbuseDB_test.py
+++ Packs/AbuseDB/Integrations/AbuseDB/AbuseDB_test.py
@@ -1,3 +1,4 @@
+import pytest
 from CommonServerPython import *
 
 RETURN_ERROR_TARGET = 'AbuseDB.return_error'
@@ -34,5 +35,5 @@ def test_ip_command_when_api_quota_reached(mocker):
     mocker.patch.object(Session, 'request', return_value=api_quota_reached_request_response_with_dot_access)
     return_error_mock = mocker.patch(RETURN_ERROR_TARGET)
     from AbuseDB import check_ip_command
-    check_ip_command(['1.1.1.1'], days=7, verbose=False, threshold=10)
+    check_ip_command(['1.1.1.1'], DBotScoreReliability.C, days=7, verbose=False, threshold=10)
     assert return_error_mock.call_count == 0
