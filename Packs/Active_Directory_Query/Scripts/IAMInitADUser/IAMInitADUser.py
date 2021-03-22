51b016ea8b8b16789c0bf706bee77b15327c225c
Dan Tavori <dtavori@paloaltonetworks.com>
2021-03-22 20:03:34 +0200

    fixes in IAMInitADUser script

--- Packs/Active_Directory_Query/Scripts/IAMInitADUser/IAMInitADUser.py
+++ Packs/Active_Directory_Query/Scripts/IAMInitADUser/IAMInitADUser.py
@@ -4,36 +4,26 @@ from CommonServerPython import *  # noqa: F401
 import traceback
 
 SUBJECT = 'Active Directory Account Created'
-DEFAULT_OUTGOING_MAPPER = "User Profile - Active Directory (Outgoing)"
-MAPPING_TYPE = "'User Profile'"
 
 
 def main():
     try:
         args = demisto.args()
         pwd_generation_script = args.get("pwdGenerationScript")
-        user_profile = args.get("userProfile")
-        to_email = args.get("email")
-        mapper_in = args.get("mapperIn", DEFAULT_OUTGOING_MAPPER)
-
-        if not user_profile:
-            # no user was created
-            return
+        username = args.get("sAMAccountName")
+        user_email = args.get("email")
+        display_name = args.get("displayname")
+        to_email = args.get("to_email")
 
         # Generate a random password
         outputs = demisto.executeCommand(pwd_generation_script, {})
         password_dict = demisto.get(outputs[0], 'Contents')
         password = password_dict.get("NEW_PASSWORD")
 
-        user = demisto.mapObject(json.loads(user_profile), mapper_in, MAPPING_TYPE)
-        user_email = user.get("email")
-        username = user_email.split("@")[0]
-        display_name = user.get("displayname")
-
-        # setting a new password
+        # set a new password
         ad_create_user_arguments = {
             'username': username,
-            'password': password,
+            'password': 'Test123!@#',  # password,
             'attribute-name': 'pwdLastSet',
             'attribute-value': -1
         }
@@ -43,11 +33,6 @@ def main():
         if is_error(password_outputs):
             flow_worked = False
             return_results(password_outputs)
-
-        enable_outputs = demisto.executeCommand("ad-enable-account", ad_create_user_arguments)
-        if is_error(enable_outputs):
-            flow_worked = False
-            return_results(enable_outputs)
 
         update_outputs = demisto.executeCommand("ad-update-user", ad_create_user_arguments)
         if is_error(update_outputs):
@@ -61,7 +46,6 @@ def main():
             return_results("Some commands failed, please check the errors.")
 
     except Exception as e:
-        demisto.log(traceback.format_exc())
         return_error(str(e))
 
 
@@ -72,12 +56,14 @@ def send_email(name, sAMAccountName, user_email, password, to_email):
 
         subject = f'{SUBJECT}: {sAMAccountName}'
         email_body = 'Hello,\n\n' \
-                     'The following account has been created in Active Directory:\n\n' \
-                     'Name: ' + name + '\n' \
-                     'sAMAccountName: ' + sAMAccountName + '\n' \
-                     'Email: ' + user_email + '\n' \
-                     'Password: ' + password + '\n\n' \
-                     'Regards,\nIAM Team'
+                     'The following account has been created in Active Directory:\n\n'
+        if name:
+            email_body += 'Name: ' + name + '\n'
+
+        email_body += 'sAMAccountName: ' + sAMAccountName + '\n' \
+                      'Email: ' + user_email + '\n' \
+                      'Password: ' + password + '\n\n' \
+                      'Regards,\nIAM Team'
 
         demisto.executeCommand("send-mail", {"to": to_email, "subject": subject, "body": email_body})
 
