275f4814edc65526d49718738d31fc71d97c2101
ypreisler <ypreisler@paloaltonetworks.com>
2021-03-22 09:19:55 +0200

    Added unit tests

--- Packs/Perch/Integrations/Perch/Perch_test.py
+++ Packs/Perch/Integrations/Perch/Perch_test.py
@@ -1,14 +1,5 @@
-import pytest
 import json
 
-
-STATUSES = {
-    'Not Reviewed': '0',
-    'Investigating': '1',
-    'On hold': '2',
-    'False Positive': '3',
-    'Escalated': '4'
-}
 
 ALERTS = {
     "results": [
@@ -49,7 +40,7 @@ ALERTS = {
             "status": 0,
             "status_updated_at": None,
             "team_id": 5394,
-            "title": "ET POLICY MS Terminal Server Root login",
+            "title": "ET POLICY MS Terminal Server Root login 1",
             "ts": "2020-11-19T15:25:42.576650+0000"
         },
         {
@@ -89,14 +80,14 @@ ALERTS = {
             "status": 0,
             "status_updated_at": None,
             "team_id": 5394,
-            "title": "ET POLICY MS Terminal Server Root login",
+            "title": "ET POLICY MSS Terminal Server Root login 2",
             "ts": "2020-11-18T02:38:14.828625+0000"
         },
         {
             "closed_at": "2020-11-18T20:05:58.969797Z",
             "community_id": 8,
             "count": 0,
-            "created_at": "2020-11-18T03:14:25.451196Z",
+            "created_at": "2020-11-17T03:14:25.451196Z",
             "dest_geo_ip": {},
             "dest_ip": "8.8.8.8",
             "dest_port": 3389,
@@ -129,37 +120,59 @@ ALERTS = {
             "status": 2,
             "status_updated_at": None,
             "team_id": 5394,
-            "title": "ET POLICY MS Terminal Server Root login",
+            "title": "ET POLICY MSF Terminal Server Root login 3",
             "ts": "2020-11-18T02:38:14.828625+0000"
         }
     ]
 }
 
 
-def get_alerts_by_status(status=[]):
-    return [alert for alert in ALERTS["results"] if STATUSES[status] == str(alert["status"])]
+def get_alerts_by_status(method, url, params=None, data=None, headers=None):
+    """
+        The function simulate the http_request method for the test_fetch_alerts_command_by_soc_status.
+        The function got the params as the http_request got, and return from the json above only the incidents
+        that are in the given status.
+    """
+    status = params['soc_status']
+    results = {'results': [alert for alert in ALERTS["results"] if int(status) == alert["status"]]}
+    return results
 
 
 def test_fetch_alerts_command_by_soc_status(requests_mock, mocker):
+    """
+        Given:
+            - List of statuses to fetch
+        When
+            - Running fetch_incident
+        Then
+            - Verify the returned incidents is with the expected status
+    """
     params = {
         'credentials': {
             'identifier': 1234,
             'password': 5678
         },
         'url': "https://api.perch.rocks",
-        'soc_status': ['On hold']
+        'soc_status': ['Not Reviewed']
     }
     mocker.patch('demistomock.params', return_value=params)
 
     from Perch import fetch_alerts
 
-    # requests_mock.get('https://api.perch.rocks/v1/alerts', json=ALERTS)
-    mocker.patch('http_request', side_effect=get_alerts_by_status)
+    mocker.patch('Perch.http_request', side_effect=get_alerts_by_status)
 
     headers = {'Content-Type': 'application/json'}
     last_run = {'time': 1561017202}
 
     _, incidents = fetch_alerts(last_run, headers)
-    print(incidents)
-    assert len(incidents) == 3
 
+    assert len(incidents) == 2
+
+    assert incidents[0]['name'] == 'Perch Incident: ET POLICY MSS Terminal Server Root login 2'
+    assert incidents[1]['name'] == 'Perch Incident: ET POLICY MS Terminal Server Root login 1'
+
+    raw_json = json.loads(incidents[0]['rawJSON'])
+    assert raw_json['status'] == 0
+
+    raw_json = json.loads(incidents[1]['rawJSON'])
+    assert raw_json['status'] == 0
