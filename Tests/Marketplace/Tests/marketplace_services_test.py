95ff7447b469bb8c60731e75fc871a62af275e07
Guy Freund <53565845+guyfreund@users.noreply.github.com>
2021-03-29 15:08:08 +0300

    Optimize upload (#11922)
    * optimize upload - ignore specific pack files
    
    * cr fixes

--- Tests/Marketplace/Tests/marketplace_services_test.py
+++ Tests/Marketplace/Tests/marketplace_services_test.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import os
 import random
+import glob
 from unittest.mock import mock_open
 from mock_open import MockOpen
 from google.cloud.storage.blob import Blob
@@ -12,7 +13,7 @@ from datetime import datetime, timedelta
 
 from Tests.Marketplace.marketplace_services import Pack, Metadata, input_to_list, get_valid_bool, convert_price, \
     get_updated_server_version, GCPConfig, BucketUploadFlow, PackStatus, load_json, \
-    store_successful_and_failed_packs_in_ci_artifacts, PACKS_FOLDER
+    store_successful_and_failed_packs_in_ci_artifacts, PACKS_FOLDER, is_ignored_pack_file, PackFolders
 
 CHANGELOG_DATA_INITIAL_VERSION = {
     "1.0.0": {
@@ -453,6 +454,26 @@ class TestParsingInternalFunctions:
 class TestHelperFunctions:
     """ Class for testing helper functions that are used in marketplace_services and upload_packs modules.
     """
+
+    @pytest.mark.parametrize('modified_file_path_parts, expected_result', [
+        (['Packs', 'A', PackFolders.INTEGRATIONS.value, 'A', 'test_data', 'a.json'], True),
+        (['Packs', 'A', PackFolders.TEST_PLAYBOOKS.value, 'playbook-wow.yml'], True),
+        (['Packs', 'A', '.pack-ignore'], True),
+        (['Packs', 'A', '.secrets-ignore'], True),
+        (['Packs', 'A', PackFolders.PLAYBOOKS.value, 'playbook-wow_README.md'], True),
+        (['Packs', 'A', PackFolders.INTEGRATIONS.value, 'A', 'README.md'], True),
+        (['Packs', 'A', PackFolders.INTEGRATIONS.value, 'A', 'A_test.py'], True),
+        (['Packs', 'A', PackFolders.INTEGRATIONS.value, 'A', 'commands.txt'], True),
+        (['Packs', 'A', PackFolders.SCRIPTS.value, 'A', 'Pipfile'], True),
+        (['Packs', 'A', PackFolders.SCRIPTS.value, 'A', 'Pipfile.lock'], True),
+        (['Packs', 'A', Pack.README], False),
+        (['Packs', 'A', Pack.USER_METADATA], False),
+        (['Packs', 'A', PackFolders.INTEGRATIONS.value, 'A', 'A.py'], False)
+    ])
+    def test_is_ignored_pack_file(self, modified_file_path_parts, expected_result, mocker):
+        mocker.patch.object(glob, 'glob', return_value=['Packs/A/Integrations/A/test_data/a.json'])
+        mocker.patch.object(os.path, 'isdir', return_value=True)
+        assert is_ignored_pack_file(modified_file_path_parts) is expected_result
 
     @pytest.mark.parametrize("input_data,capitalize_input,expected_result",
                              [
