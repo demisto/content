c350885d85c5c0f51a850dca46f058ee815f4234
ypreisler <ypreisler@paloaltonetworks.com>
2021-04-01 17:25:00 +0300

    Added unit test

--- Packs/URLHaus/Integrations/URLHaus/URLHaus_test.py
+++ Packs/URLHaus/Integrations/URLHaus/URLHaus_test.py
@@ -23,4 +23,3 @@ def test_reliability_in_dbot(mocker):
     URLHaus.url_command(**params)
 
     assert demisto.results.call_args_list[0][0][0]['EntryContext']['DBotScore']['Reliability'] == DBotScoreReliability.C
-
