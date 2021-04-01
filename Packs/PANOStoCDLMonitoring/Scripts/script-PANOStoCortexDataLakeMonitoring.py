490cbca24e1dbb6bf4940b3b42a4d905008159bb
bar katzir <bkatzir@paloaltonetworks.com>
2021-04-01 16:46:21 +0300

    use instance

--- Packs/PANOStoCDLMonitoring/Scripts/script-PANOStoCortexDataLakeMonitoring.py
+++ Packs/PANOStoCDLMonitoring/Scripts/script-PANOStoCortexDataLakeMonitoring.py
@@ -1,32 +1,33 @@
-from CommonServerPython import *
-
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
 
 # Cortex Data Lake PAN-OS log monitoring monitoring
 # PRE_REQUISITE: Enable Cortex Data Lake integration and/or PAN-OS integration (if the automated creation of a FW list is expected)
 
-def check_instance(all_instances: list, integration_name: str, err_msg: str):
+def check_instance(all_instances: dict, integration_name: str, err_msg: str):
     """
     :param all_instances: All integration instances in the platform.
-    :param integration_name: integration brand name
+    :param integration_name: integration name
     :param err_msg: error message string
     """
     instance_found_active: bool = False
-    for instance in all_instances:
-        if all_instances[instance]['brand'] == integration_name and all_instances[instance]['state'] == 'active':
+    for instance in all_instances.keys():
+        if (all_instances[instance]['brand'] == integration_name or instance == integration_name) \
+                and all_instances[instance]['state'] == 'active':
             instance_found_active = True
             break
     if not instance_found_active:
         raise Exception(err_msg)
 
 
-def get_firewall_serials(pan_os_instance_name: str) -> list:
+def get_firewall_serials(pan_os_integration_instance_name: str) -> list:
     """
-    :param pan_os_instance_name: Name of the instance of the PAN-OS integration.
+    :param pan_os_integration_instance_name: Name of the instance of the PAN-OS integration.
     :return: List of the FWs serials associated with this instance.
     """
     fw_monitor_list: list = []
     fw_query = {'type': 'op', 'cmd': '<show><devices><all></all></devices></show>', 'raw-response': 'true',
-                'using': pan_os_instance_name}
+                'using': pan_os_integration_instance_name}
     fw_query_result = demisto.executeCommand("panorama", fw_query)
     if fw_query_result and isinstance(fw_query_result, list):
         for fw in fw_query_result[0]['Contents']['response']['result']['devices']['entry']:
@@ -46,7 +47,7 @@ def query_cdl(fw_monitor_list: list) -> CommandResults:
     no_logs_str = "### Logs traffic table\n**No entries.**\n"
     firewalls_with_logs = []
     firewalls_without_logs = []
-    start_time = datetime.datetime.utcnow() - datetime.timedelta(hours=12)  # Looking for the last 12 hours of logs
+    start_time = datetime.utcnow() - timedelta(hours=12)  # Looking for the last 12 hours of logs
     start_time = start_time.strftime('%Y-%m-%d %H:%M:%S')
     query = {'fields': 'all', 'time_range': '1 day', 'limit': str(1), 'start_time': start_time}
     for current_fw in fw_monitor_list:
@@ -84,14 +85,14 @@ def main():
         # The Firewall list must be a comma-separated list of FW serials
         fw_monitor_list = argToList(args.get('fw_serials'))
         if not fw_monitor_list:  # List of FW to monitor is empty, get it from Panorama
-            pan_os_integration_name = args.get('pan_os_integration_name', 'Panorama')
-            if not pan_os_integration_name:
-                raise Exception("A Firewall serial list or a PAN-OS integration name is needed(default is Panorama).")
+            pan_os_integration_instance_name = args.get('pan_os_integration_instance_name')
+            if not pan_os_integration_instance_name:
+                raise Exception("A Firewall serial list or a PAN-OS integration instance name is needed.")
             # Look for active PAN-OS instance
-            check_instance(all_instances, pan_os_integration_name,
-                           f'Integration instance {pan_os_integration_name} is not active or is not a PAN-OS integration.')
+            check_instance(all_instances, pan_os_integration_instance_name,
+                           f'Integration instance {pan_os_integration_instance_name} is not active or is not a PAN-OS integration.')
             # Get FW serials
-            fw_monitor_list = get_firewall_serials(fw_monitor_list, pan_os_integration_name)
+            fw_monitor_list = get_firewall_serials(pan_os_integration_instance_name)
 
         # Log the list of firewalls to be monitored
         demisto.debug(f'List of FW serials: {fw_monitor_list}')
