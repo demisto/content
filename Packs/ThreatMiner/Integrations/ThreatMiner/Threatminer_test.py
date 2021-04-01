0514fb321d6dce4601fb505c3100956f2337ef31
ypreisler <ypreisler@paloaltonetworks.com>
2021-04-02 00:01:21 +0300

    Fixed Flake8 error

--- Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
+++ Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
@@ -15,6 +15,6 @@ def test_reliability_in_dbot(mocker):
     mocker.patch.object(demisto, 'args', return_value={'threshold': '10'})
 
     dbot_score = get_dbot_score_report(0, 'CA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB', {},
-                                 DBotScoreReliability.C)
+                                       DBotScoreReliability.C)
 
     assert dbot_score == DBOT_SCORES
