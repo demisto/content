b9e609b0b6615740cdc0491cca506d10e5b88596
ypreisler <ypreisler@paloaltonetworks.com>
2021-04-01 23:09:48 +0300

    Update test

--- Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
+++ Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
@@ -2,11 +2,19 @@ import demistomock as demisto
 from ThreatMiner import get_dbot_score_report
 from CommonServerPython import DBotScoreReliability
 
+DBOT_SCORES = {
+    'Reliability': 'C - Fairly reliable',
+    'Vendor': 'ThreatMiner',
+    'Indicator': 'CA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB',
+    'Score': 0,
+    'Type': 'File'
+}
+
 
 def test_reliability_in_dbot(mocker):
     mocker.patch.object(demisto, 'args', return_value={'threshold': '10'})
 
-    dbot = get_dbot_score_report(0, 'CA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB', {},
-                                    DBotScoreReliability.C)
+    dbot_score = get_dbot_score_report(0, 'CA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB', {},
+                                 DBotScoreReliability.C)
 
-    assert dbot['Reliability'] == DBotScoreReliability.C
+    assert dbot_score == DBOT_SCORES
