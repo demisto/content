8d9e2c6adab6b52941e5ef46a901bcb8d1834e0d
syaakovi <syaakovi@paloaltonetworks.com>
2021-04-04 09:31:13 +0300

    updated Tier2 login information

--- Packs/EscapeRoomTier1/Scripts/ERTokenReputation/ERTokenReputation.py
+++ Packs/EscapeRoomTier1/Scripts/ERTokenReputation/ERTokenReputation.py
@@ -16,9 +16,10 @@ def set_user_credentials(indicator_id):
     res = demisto.executeCommand('setIndicator', {
         'id': indicator_id,
         'customFields': {
-            'logininfo': '',
+            'logininfo': 'Go to Tier2',
             'username': 'Isaac',
-            'usercredentials': 'TBD![]()',
+            'usercredentials': '![Reveal Password](https://raw.githubusercontent.com/demisto/content/'
+                               'EscapeRoomMaterials/Packs/EscapeRoomTier1/images/indicator_QR.png)',
         },
     })
 
