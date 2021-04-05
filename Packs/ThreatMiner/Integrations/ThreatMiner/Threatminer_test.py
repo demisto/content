f37a8801bb54c311f774ffeb58255cbcc83b63e2
ypreisler <ypreisler@paloaltonetworks.com>
2021-04-05 13:11:45 +0300

    Fixed review changes

--- Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
+++ Packs/ThreatMiner/Integrations/ThreatMiner/Threatminer_test.py
@@ -12,6 +12,14 @@ DBOT_SCORES = {
 
 
 def test_reliability_in_dbot(mocker):
+    """
+        Given:
+            - The user reliability param
+        When:
+            - Running get_dbot_score_report
+        Then:
+            - Verify dbot_score outputs as excepted
+    """
     mocker.patch.object(demisto, 'args', return_value={'threshold': '10'})
 
     dbot_score = get_dbot_score_report(0, 'CA978112CA1BBDCAFAC231B39A23DC4DA786EFF8147C4E72B9807785AFEE48BB', {},
