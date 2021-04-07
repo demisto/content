08cdaafb424071c46681b3a04a806106e20e6b0a
bar katzir <bkatzir@paloaltonetworks.com>
2021-04-07 14:32:19 +0300

    update RN, uncomment return_error

--- Packs/TheHiveProject/Integrations/TheHiveProject/TheHiveProject.py
+++ Packs/TheHiveProject/Integrations/TheHiveProject/TheHiveProject.py
@@ -877,7 +877,7 @@ def main() -> None:
 
     except Exception as err:
         demisto.error(traceback.format_exc())  # print the traceback
-        # return_error(f'Failed to execute {command} command. \nError: {str(err)}')
+        return_error(f'Failed to execute {command} command. \nError: {str(err)}')
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
