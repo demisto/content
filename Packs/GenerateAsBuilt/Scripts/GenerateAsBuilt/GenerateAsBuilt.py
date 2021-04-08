27111997c2793f39885cc25f9cbb654b9fe0d20a
Adam Baumeister <abigserve@gmail.com>
2021-04-08 15:18:15 +1000

    Working basic

--- Packs/GenerateAsBuilt/Scripts/GenerateAsBuilt/GenerateAsBuilt.py
+++ Packs/GenerateAsBuilt/Scripts/GenerateAsBuilt/GenerateAsBuilt.py
@@ -2,6 +2,7 @@ from CommonServerPython import *
 import demistomock as demisto
 import sys
 import jinja2
+
 """
 GenerateAsBuilt
 
@@ -9,6 +10,8 @@ Uses the XSOAR API to query for custom content, configuration, and statistics, t
 a HTML and Markdown output based on this.
 """
 DEMISTO_INTEGRATIONS_PATH = "/settings/integration/search"
+DEMISTO_INSTALLED_PATH = "/contentpacks/metadata/installed"
+DEMISTO_PLAYBOOKS_PATH = "/playbook/search"
 MAX_REQUEST_SIZE = demisto.args().get("size", 500)
 HTML_TABLE_TEMPLATE = """
 <table>
@@ -17,26 +20,67 @@ HTML_TABLE_TEMPLATE = """
         <th>{{ header }}</th>
         {% endfor %}
     </tr>
+    {% for row in rows %}
+    <tr>
+        {% for header in headers %}
+        <td>{{ row[header] }}</td>
+        {% endfor %}
+    </tr>
+    {% endfor %}
 </table>
 """
+MD_DOCUMENT_TEMPLATE = """
+{{ integrations_table }}
+
+{{ installed_packs_table }}
+
+{{ playbooks_table }}
+"""
+
+
 class ReturnedAPIData:
     def __init__(self, data, name):
         self.data = data
         self.name = name
 
     def as_markdown(self, headers):
-        tableToMarkdown(self.name, self.data, headers=headers)
+        return tableToMarkdown(self.name, self.data, headers=headers)
 
     def as_html(self, headers):
         template = jinja2.Template(HTML_TABLE_TEMPLATE)
-        return template.render(headers=headers)
+        return template.render(headers=headers, rows=self.data)
 
-def api_request(url, body, command):
+    def total(self):
+        return len(self.data)
+
+
+def build_md_document(integrations_table, installed_packs_table, playbooks_table):
+    template = jinja2.Template(MD_DOCUMENT_TEMPLATE)
+    return template.render(
+        integrations_table=integrations_table,
+        installed_packs_table=installed_packs_table,
+        playbooks_table=playbooks_table
+    )
+
+
+def post_api_request(url, body):
     api_args = {
         "uri": url,
         "body": body
     }
-    raw_res = demisto.executeCommand(command, api_args)
+    raw_res = demisto.executeCommand("demisto-api-post", api_args)
+    try:
+        res = raw_res[0]['Contents']['response']
+        return res
+    except KeyError:
+        return_error(f'API Request failed, no response from API call to {url}')
+
+
+def get_api_request(url):
+    api_args = {
+        "uri": url
+    }
+    raw_res = demisto.executeCommand("demisto-api-get", api_args)
     try:
         res = raw_res[0]['Contents']['response']
         return res
@@ -47,20 +91,52 @@ def api_request(url, body, command):
 def get_enabled_integrations():
     """
     Retrieve all the running instances.
-    :return:
+    :return: ReturnedAPIData
     """
-    r = api_request(DEMISTO_INTEGRATIONS_PATH, {"size": MAX_REQUEST_SIZE}, "demisto-api-post")
+    r = post_api_request(DEMISTO_INTEGRATIONS_PATH, {"size": MAX_REQUEST_SIZE})
     instances = r.get("instances")
-    total_instances = len(instances)
     enabled_instances = []
     for instance in instances:
         if instance.get("enabled"):
             enabled_instances.append(instance)
 
-    return_results(tableToMarkdown("Enabled Instances", enabled_instances, headers=["brand","name"]))
+    rd = ReturnedAPIData(enabled_instances, "Enabled Instances")
+    # return_results(rd.as_markdown(headers=["name", "brand"]))
+    return rd
+
+
+def get_installed_packs():
+    """
+    Get all the installed Content Packs
+    :return: ReturnedAPIData
+    """
+    r = get_api_request(DEMISTO_INSTALLED_PATH)
+    rd = ReturnedAPIData(r, "Installed Content Packs")
+    return rd
+
+
+def get_custom_playbooks():
+    """
+    Return all the custom playbooks installed in XSOAR>
+    :return: ReturnedAPIData
+    """
+    r = post_api_request(DEMISTO_PLAYBOOKS_PATH, {"query": "system:F AND hidden:F"}).get("playbooks")
+    for pb in r:
+        pb["TotalTasks"] = len(pb.get("tasks", []))
+    rd = ReturnedAPIData(r, "Custom Playbooks")
+    return rd
+
 
 def main():
-    get_enabled_integrations()
+    integrations = get_enabled_integrations()
+    installed_packs = get_installed_packs()
+    playbooks = get_custom_playbooks()
+    hr = build_md_document(
+        integrations.as_markdown(["name", "brand"]),
+        installed_packs.as_markdown(["name", "currentVersion"]),
+        playbooks.as_markdown(["name", "TotalTasks"])
+    )
+    return_results(hr)
 
 
 if __name__ in ('__builtin__', 'builtins'):
--- Packs/GenerateAsBuilt/Scripts/GenerateAsBuilt/GenerateAsBuilt_test.py
+++ Packs/GenerateAsBuilt/Scripts/GenerateAsBuilt/GenerateAsBuilt_test.py
@@ -1,9 +1,12 @@
 from GenerateAsBuilt import ReturnedAPIData
+
+
 def test_as_html():
     test_data = [{
         "name": "test",
         "blah": "test2"
     }]
-    o = ReturnedAPIData("test table", test_data)
+    o = ReturnedAPIData(test_data, "Test table")
     r = o.as_html(["name", "blah"])
     assert "<th>name</th>" in r
+    assert "<td>test</td>" in r
