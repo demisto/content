7da677ea12f47437d1c463857df539ed388c6424
bgenish <bgenish@paloaltonetworks.com>
2021-04-11 09:43:23 +0300

    keeping the usage of format instead of f strings

--- Packs/RedCanary/Integrations/RedCanary/RedCanary.py
+++ Packs/RedCanary/Integrations/RedCanary/RedCanary.py
@@ -82,7 +82,7 @@ def http_request(requests_func, url_suffix, **kwargs):
     if res.status_code not in [200, 201, ]:
         LOG('result is: %s' % (res.json(),))
         error = res.json()
-        raise Exception(f'Your request failed with the following error: {error}.\n')
+        raise Exception('Your request failed with the following error: {}.\n'.format(error, ))
 
     return res.json()
 
@@ -113,7 +113,7 @@ def playbook_name_to_id(name):
 
 def get_endpoint_context(res=None, endpoint_id=None):
     if res is None:
-        res = http_get(f'/endpoints/{endpoint_id}').get('data', [])
+        res = http_get('/endpoints/{}'.format(endpoint_id)).get('data', [])
 
     endpoint_context = []
     for endpoint in res:
@@ -151,7 +151,7 @@ def get_endpoint_context(res=None, endpoint_id=None):
 
 def get_endpoint_user_context(res=None, endpoint_user_id=None):
     if res is None:
-        res = http_get(f'/endpoint_users/{endpoint_user_id}')['data']
+        res = http_get('/endpoint_users/{}'.format(endpoint_user_id))['data']
 
     endpoint_users = []
     for endpoint_user in res:
@@ -179,7 +179,7 @@ def get_full_timeline(detection_id, per_page=100):
     last_data = {}  # type:ignore
 
     while not done:
-        res = http_get(f'/detections/{detection_id}/timeline',
+        res = http_get('/detections/{}/timeline'.format(detection_id),
                        params={
                            'page': page,
                            'per_page': per_page,
@@ -303,7 +303,7 @@ def detections_to_entry(detections, show_timeline=False):
     activities = ''
     title = 'Detections'
     if show_timeline and len(detections) == 1:
-        title = f'Detection {fixed_detections[0]["Headline"]}'
+        title = 'Detection {}'.format(fixed_detections[0]['Headline'])
         activities, domains, files, ips, processes = process_timeline(fixed_detections[0]['ID'])
         activities = tableToMarkdown('Detection Timeline', activities,
                                      headers=['Time', 'Type', 'Activity Details', 'Notes'])
@@ -403,7 +403,7 @@ def get_detection_command():
 
 @logger
 def get_detection(_id):
-    res = http_get(f'/detections/{_id}')
+    res = http_get('/detections/{}'.format(_id))
     return res['data']
 
 
@@ -417,7 +417,7 @@ def acknowledge_detection_command():
 
 @logger
 def acknowledge_detection(_id):
-    res = http_patch(f'/detections/{_id}/mark_acknowledged')
+    res = http_patch('/detections/{}/mark_acknowledged'.format(_id))
     return res['data']
 
 
@@ -433,7 +433,7 @@ def remediate_detection_command():
 
 @logger
 def remediate_detection(_id, remediation_state, comment):
-    res = http_patch(f'/detections/{_id}/update_remediation_state',
+    res = http_patch('/detections/{}/update_remediation_state'.format(_id),
                      data={
                          'remediation_state': remediation_state,
                          'comment': comment,
@@ -485,7 +485,7 @@ def get_endpoint_command():
         'Type': entryTypes['note'],
         'Contents': endpoints,
         'ReadableContentsFormat': formats['markdown'],
-        'HumanReadable': tableToMarkdown(f'EndPoint {endpoints[0]["Hostname"]}', endpoints, headers=headers,
+        'HumanReadable': tableToMarkdown('EndPoint {}'.format(endpoints[0]['Hostname']), endpoints, headers=headers,
                                          removeNull=True),
         'EntryContext': {
             'EndPoint(val.Hostname == obj.Hostname)': createContext(endpoints, removeNull=True),
@@ -495,7 +495,7 @@ def get_endpoint_command():
 
 @logger
 def get_endpoint(_id):
-    res = http_get(f'/endpoints/{_id}')
+    res = http_get('/endpoints/{}'.format(_id))
 
     return res['data']
 
@@ -532,11 +532,11 @@ def execute_playbook_command():
 
     execute_playbook(playbook_id, detection_id)
 
-    return f'playbook #{playbook_id} execution started successfully.'
+    return 'playbook #{} execution started successfully.'.format(playbook_id)
 
 
 def execute_playbook(playbook_id, detection_id):
-    res = http_post(f'/exec/playbooks/{playbook_id}/execute',
+    res = http_post('/exec/playbooks/{}/execute'.format(playbook_id),
                     params={
                         'resource_type': 'Detection',
                         'resource_id': detection_id,
@@ -556,11 +556,11 @@ def fetch_incidents(last_run):
         # first time fetching
         last_fetch = parse_date_range(demisto.params().get('fetch_time', '3 days'), TIME_FORMAT)[0]
 
-    demisto.debug(f'iterating on detections, looking for more recent than {last_fetch}')
+    demisto.debug('iterating on detections, looking for more recent than {}'.format(last_fetch))
     incidents = []
     new_incidents_ids = []
     for raw_detection in get_unacknowledged_detections(last_fetch, per_page=2):
-        demisto.debug(f'found a new detection in RedCanary #{raw_detection["id"]}')
+        demisto.debug('found a new detection in RedCanary #{}'.format(raw_detection['id']))
         incident = detection_to_incident(raw_detection)
         # the rawJson is a string of dictionary e.g. - ('{"ID":2,"Type":5}')
         incident_id = json.loads(incident.get('rawJSON')).get("ID")
@@ -618,7 +618,7 @@ def main():
         LOG(str(e))
         if demisto.command() != 'test-module':
             LOG.print_log()
-        return_error(f'error has occurred: {str(e)}')
+        return_error('error has occurred: {}'.format(e.message, ))
 
 
 if __name__ in ('__builtin__', 'builtins'):
