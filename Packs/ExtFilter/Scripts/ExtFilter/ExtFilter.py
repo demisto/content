04d15b384b41d88ddac54a7879ffec639532f7dc
spearmin10 <minoue@paloaltonetworks.com>
2021-04-14 14:49:22 +0900

    Modified the code according to the review comments.

--- Packs/ExtFilter/Scripts/ExtFilter/ExtFilter.py
+++ Packs/ExtFilter/Scripts/ExtFilter/ExtFilter.py
@@ -1546,6 +1546,7 @@ class ExtFilter:
                     else:
                         out += decoded_s
             except Exception:
+                demisto.debug(f'Failed to decode by `email-header: decode`: {lhs}')
                 out = str(lhs)
             return Value(out)
 
