127b563dd2d9411d96e3b4289847eca8b1ed9576
hod-alpert <haplert@paloaltonetworks.com>
2021-04-20 10:25:20 +0300

    Supporting circle run: - Writing the ""gitlab-ci-env-variables.sh" which assigns the gitlabci env variables with their matching circeci env variables - Writing the service account keys values into tmp files - moving files that are used in the circle build between jobs into artifacts folder.

--- Tests/scripts/validate_premium_packs.sh
+++ Tests/scripts/validate_premium_packs.sh
@@ -4,9 +4,7 @@
 set -e
 
 EXTRACT_FOLDER=$(mktemp -d)
-GCS_PATH=$(mktemp)
-CIRCLE_BRANCH=${CIRCLE_BRANCH:-unknown}
-echo $GCS_MARKET_KEY > $GCS_PATH
+BRANCH=${CI_COMMIT_BRANCH:-unknown}
 
 SECRET_CONF_PATH="./conf_secret.json"
 
@@ -14,7 +12,7 @@ SECRET_CONF_PATH="./conf_secret.json"
 
 CREATE_INSTANCES_JOB_NUMBER=$(cat create_instances_build_num.txt)
 if [[ $GCS_MARKET_BUCKET != "marketplace-dist" ]]; then
-  STORAGE_BASE_PATH="upload-flow/builds/$CIRCLE_BRANCH/$CREATE_INSTANCES_JOB_NUMBER/content/packs"
+  STORAGE_BASE_PATH="upload-flow/builds/$BRANCH/$CREATE_INSTANCES_JOB_NUMBER/content/packs"
 fi
 
 # ====== RUN VALIDATIONS ======
@@ -24,11 +22,11 @@ if [[ -n "$STORAGE_BASE_PATH" ]]; then
 else
   echo "Validating index file in bucket at path gs://$GCS_MARKET_BUCKET/content/packs"
 fi
-python3 ./Tests/scripts/validate_index.py -sa "$GCS_PATH" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" -sb "$STORAGE_BASE_PATH" -c "$CIRCLE_BRANCH"
+python3 ./Tests/scripts/validate_index.py -sa "$GCS_MARKET_KEY" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" -sb "$STORAGE_BASE_PATH" -c "$BRANCH"
 
 if [[ -n "$STORAGE_BASE_PATH" ]]; then
   echo "Validating premium packs in server against index file in bucket at path gs://$GCS_MARKET_BUCKET/$STORAGE_BASE_PATH."
 else
   echo "Validating premium packs in server against index file in bucket at path gs://$GCS_MARKET_BUCKET/content/packs."
 fi
-python3 ./Tests/scripts/validate_premium_packs.py -sa "$GCS_PATH" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" -s "$SECRET_CONF_PATH" -a "$1" -sb "$STORAGE_BASE_PATH"
+python3 ./Tests/scripts/validate_premium_packs.py -sa "$GCS_MARKET_KEY" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" -s "$SECRET_CONF_PATH" -a "$1" -sb "$STORAGE_BASE_PATH"
