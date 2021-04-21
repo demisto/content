a17f3462e7b6fbf36dbdf578fae82366f6602a4d
nericksen <nericksen@paloaltonetworks.com>
2021-04-21 15:25:15 -0400

    Fix lint issues

--- Packs/Base/Scripts/AddDBotScoreToContext/AddDBotScoreToContext.py
+++ Packs/Base/Scripts/AddDBotScoreToContext/AddDBotScoreToContext.py
@@ -1,12 +1,14 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+
+
 def main():
     indicator = demisto.args().get("indicator")
     indicatorType = demisto.args().get("indicatorType")
     score = int(demisto.args().get("score"))
     vendor = demisto.args().get("vendor")
     reliability = demisto.args().get("reliability", None)
-    
+
     dbotscore = {
         "Indicator": indicator,
         "Type": indicatorType,
@@ -14,12 +16,13 @@ def main():
         "Score": score,
         "Reliability": reliability
     }
-    
+
     command_results = CommandResults(
         outputs_prefix='DBotScore',
         outputs=dbotscore
     )
     return_results(command_results)
+
 
 if __name__ == "__builtin__" or __name__ == "builtins":
     main()
