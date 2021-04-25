9b6183fa258155eeaa51335e721581bed78a94c9
dmeylakh <dmeylakh@paloaltonetworks.com>
2021-04-25 16:07:34 +0300

    installing packs right

--- Tests/Marketplace/install_packs.sh
+++ Tests/Marketplace/install_packs.sh
@@ -7,11 +7,10 @@ SECRET_CONF_PATH=$(cat secret_conf_path)
 echo "starting configure_and_install_packs ..."
 
 # $1 = INSTANCE_ROLE
-# $2 = artifacts folder (where env_results.json is stored) - will be used only in case of gitlab
 # $2 = gitlab or ''
 
 if [ "$2" = "gitlab" ]; then
-  PREVIOUS_JOB_NUMBER=$(cat $ARTIFACTS_FOLDER/create_instances_build_num.txt)
+  PREVIOUS_JOB_NUMBER=$(cat "$ARTIFACTS_FOLDER"/create_instances_build_num.txt)
   python3 ./Tests/Marketplace/configure_and_install_packs.py -s "$SECRET_CONF_PATH" --ami_env "$1" --branch "$CI_COMMIT_BRANCH" --build_number "$PREVIOUS_JOB_NUMBER" -af "$ARTIFACTS_FOLDER"
 else
   PREVIOUS_JOB_NUMBER=$(cat create_instances_build_num.txt)
