bf86896bfe6218adeda7a5dddbfd7f67b615b3e6
dmeylakh <dmeylakh@paloaltonetworks.com>
2021-04-28 17:16:35 +0300

    debugging slack

--- Tests/scripts/slack_notifier.py
+++ Tests/scripts/slack_notifier.py
@@ -59,19 +59,15 @@ def get_circle_failed_steps(ci_token, build_number):
 def get_gitlab_failed_steps(ci_token, build_number, server_url, project_id):
     failed_steps_list = []
     gitlab_client = gitlab.Gitlab(server_url, private_token=ci_token)
-    logging.info(f'project id is {project_id}')
-    projects = gitlab_client.projects.list()
-    logging.info(f'projects are {projects}')
     project = gitlab_client.projects.get(int(project_id))
-    logging.info(f'project is {project}')
     pipeline = project.pipelines.get(int(build_number))
-    logging.info(f'pipeline is {pipeline}')
     jobs = pipeline.jobs.list()
+    logging.info(f'jobs are {jobs}')
 
     for job in jobs:
-        job_name = job.get('name', '')
-        if job.get('status', '') == 'failed':
-            failed_steps_list.append(f'{job_name}')
+        if job.status == 'failed':
+            logging.info(f'collecting failed job {job.name}')
+            failed_steps_list.append(f'{job.name}')
 
     return failed_steps_list
 
