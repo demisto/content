c5f6859470b91aba11b1c43812cc21436d981954
yguezpa <yguez@paloaltonetworks.com>
2021-04-29 20:32:26 +0300

    lint

--- Packs/Base/Scripts/DBotFindSimilarIncidents/DBotFindSimilarIncidents.py
+++ Packs/Base/Scripts/DBotFindSimilarIncidents/DBotFindSimilarIncidents.py
@@ -406,7 +406,7 @@ class Model:
             if field not in self.incident_to_match.columns \
                     or not self.incident_to_match[field].values[0] \
                     or (not isinstance(self.incident_to_match[field].values[0], str) and not isinstance(
-                self.incident_to_match[field].values[0], list)) \
+                    self.incident_to_match[field].values[0], list)) \
                     or self.incident_to_match[field].values[0] == 'None' \
                     or self.incident_to_match[field].values[0] == 'N/A':
                 remove_list.append(field)
