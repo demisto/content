ce7e3e5d2f3e04c313ac2b9341e5496676f5800d
ohaim <ohaim@paloaltonetworks.com>
2021-05-04 16:26:56 +0300

    test change

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -4173,7 +4173,7 @@ class TestCommonTypes:
 
 
 class TestIndicatorsSearcher:
-    def mock_search_after_output(self, fromDate, toDate, query, size, value, page, searchAfter):
+    def mock_search_after_output(self, fromDate='', toDate='', query='', size=0, value='', page=0, searchAfter=''):
         if not searchAfter:
             searchAfter = 0
 
