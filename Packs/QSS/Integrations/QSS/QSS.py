171c6e42717cade2e6c546ce295e51561183c859
Thamir Alshammari <thamir@qss.com.sa>
2021-05-04 17:19:33 +0300

    fix CommonServerPython funtcions and remove redundant functions in QSS.py

--- Packs/QSS/Integrations/QSS/QSS.py
+++ Packs/QSS/Integrations/QSS/QSS.py
@@ -5,7 +5,6 @@ from CommonServerPython import *  # noqa: F401
 import json
 import urllib3
 import traceback
-import dateparser
 from typing import Any, Dict, Tuple, List, Optional, cast
 
 # Disable insecure warnings
@@ -25,30 +24,27 @@ QSS_SEVERITIES = ['Low', 'Medium', 'High', 'Critical']
 class Client(BaseClient):
 
     def search_alerts(self, alert_status: Optional[str], severity: Optional[str],
-                      max_results: Optional[int], start_time: Optional[int], 
+                      max_results: Optional[int], start_time: Optional[int],
                       api_key: Optional[str], false_positive: Optional[str]
                       ) -> List[Dict[str, Any]]:
 
         request_params: Dict[str, Any] = {}
 
-		if api_key:
+        if api_key:
             request_params['apikey'] = api_key
 
         if start_time:
             request_params['start_time'] = start_time
 
-        
         if severity:
             request_params['severity'] = severity
-	
-		
+
         if alert_status:
             request_params['status'] = alert_status
 
         if max_results:
             request_params['max_fetch'] = max_results
 
-       
         if false_positive:
             request_params['false_positive'] = false_positive
 
@@ -63,37 +59,6 @@ class Client(BaseClient):
 
 ''' HELPER FUNCTIONS '''
 
-def arg_to_int(arg: Any, arg_name: str, required: bool = False) -> Optional[int]:
-
-    if arg is None:
-        if required is True:
-            raise ValueError(f'Missing "{arg_name}"')
-        return None
-    if isinstance(arg, str):
-        if arg.isdigit():
-            return int(arg)
-        raise ValueError(f'Invalid number: "{arg_name}"="{arg}"')
-    if isinstance(arg, int):
-        return arg
-    raise ValueError(f'Invalid number: "{arg_name}"')
-
-def arg_to_timestamp(arg: Any, arg_name: str, required: bool = False) -> Optional[int]:
-
-    if arg is None:
-        if required is True:
-            raise ValueError(f'Missing "{arg_name}"')
-        return None
-
-    if isinstance(arg, str) and arg.isdigit():
-        return int(arg)
-    if isinstance(arg, str):
-        date = dateparser.parse(arg, settings={'TIMEZONE': 'UTC'})
-        if date is None:
-            raise ValueError(f'Invalid date: {arg_name}')
-        return int(date.timestamp())
-    if isinstance(arg, (int, float)):
-        return int(arg)
-    raise ValueError(f'Invalid date: "{arg_name}"')
 
 def convert_to_demisto_severity(severity: str) -> int:
     return {
@@ -106,7 +71,8 @@ def convert_to_demisto_severity(severity: str) -> int:
 
 def test_module(client: Client, first_fetch_time: int, api_key: str) -> str:
     try:
-        client.search_alerts(max_results=1, alert_status=None, severity=None, start_time=first_fetch_time, api_key=api_key, false_positive=None)
+        client.search_alerts(max_results=1, alert_status=None, severity=None,
+                             start_time=first_fetch_time, api_key=api_key, false_positive=None)
     except DemistoException as e:
         if 'Forbidden' in str(e):
             return 'Authorization Error: make sure API Key is correctly set'
@@ -136,7 +102,7 @@ def fetch_incidents(client: Client, max_results: int, last_run: Dict[str, int],
         severity=min_severity,
         false_positive=false_positive,
         api_key=api_key
-        
+
     )
 
     demisto.debug("Alerts Fetched")
@@ -185,18 +151,18 @@ def main() -> None:
 
     verify_certificate = not demisto.params().get('insecure', False)
 
-    first_fetch_time = arg_to_timestamp(
+    first_fetch_time = arg_to_datetime(
         arg=demisto.params().get('first_fetch', '3 days'),
         arg_name='First fetch time',
         required=True
     )
 
-    assert isinstance(first_fetch_time, int)
+    first_fetch_timestamp = int(first_fetch_time.timestamp()) if first_fetch_time else None
+    assert isinstance(first_fetch_timestamp, int)
 
     proxy = demisto.params().get('proxy', False)
 
     try:
-
 
         api_key = demisto.params().get('apikey')
 
@@ -211,19 +177,18 @@ def main() -> None:
 
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration Test button.
-            result = test_module(client, first_fetch_time, api_key)
+            result = test_module(client, first_fetch_timestamp, api_key)
             return_results(result)
 
         elif demisto.command() == 'fetch-incidents':
 
             # Set and define the fetch incidents command to run after activated via integration settings.
-            alert_status = demisto.params().get('alert_status')
-            min_severity = demisto.params().get('min_severity')
+            alert_status = demisto.params().get('status')
+            min_severity = demisto.params().get('severity')
             false_positive = demisto.params().get('false_positive')
-        	 
 
             # Convert the argument to an int using helper function or set to MAX_INCIDENTS_TO_FETCH
-            max_results = arg_to_int(
+            max_results = arg_to_number(
                 arg=demisto.params().get('max_fetch'),
                 arg_name='max_fetch',
                 required=False
@@ -235,7 +200,7 @@ def main() -> None:
                 client=client,
                 max_results=max_results,
                 last_run=demisto.getLastRun(),  # getLastRun() gets the last run dict
-                first_fetch_time=first_fetch_time,
+                first_fetch_time=first_fetch_timestamp,
                 alert_status=alert_status,
                 min_severity=min_severity,
                 api_key=api_key,
