304e6c4376c4462f33bc6582a59b8470a18d690e
avidan-H <ahessing@paloaltonetworks.com>
2021-05-05 14:22:30 +0300

    Artifactory balagan

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -4,9 +4,9 @@ set -e
 echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_README_VALIDATION"
 
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
-    demisto-sdk validate -a --post-commit --id-set --id-set-path $ARTIFACTS_FOLDER/unified_id_set.json
+    demisto-sdk validate -a --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json"
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
-    demisto-sdk validate -g --post-commit --id-set --id-set-path ./Tests/id_set.json
+    demisto-sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/id_set.json"
 else
-    demisto-sdk validate -g --post-commit --id-set --id-set-path $ARTIFACTS_FOLDER/unified_id_set.json
+    demisto-sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json"
 fi
