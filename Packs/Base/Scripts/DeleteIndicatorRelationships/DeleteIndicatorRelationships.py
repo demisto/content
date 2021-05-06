feb19e1505ba64530c231f6be8b9a618ccf405e6
Yana Orhov <yorhov@paloaltonetworks.com>
2021-05-06 12:15:51 +0300

    Delete Indicator Relationships script (#12539)
    * added DeleteIndicatorRelationships script
    
    * added rn
    
    * added rn
    
    * added no tests

---
+++ Packs/Base/Scripts/DeleteIndicatorRelationships/DeleteIndicatorRelationships.py
@@ -0,0 +1,27 @@
+import demistomock as demisto
+from CommonServerPython import *
+from CommonServerUserPython import *
+
+import traceback
+
+''' MAIN FUNCTION '''
+
+
+def main():
+    try:
+        ids = demisto.args().get('ids')
+        res = demisto.executeCommand("deleteRelationships", {'relationshipsIDsKey': ids})
+        if is_error(res[0]):
+            raise Exception("Error in DeleteIndicatorRelationships command - {}".format(res[0]["Contents"]))
+        hr = f"The relationships {str(ids)} were deleted successfully."
+        return_results(CommandResults(readable_output=hr))
+    except Exception as ex:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to execute DeleteIndicatorRelationships. Error: {str(ex)}')
+
+
+''' ENTRY POINT '''
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
