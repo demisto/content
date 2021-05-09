401f2a3db7d262cd087dd22115405601eb3fe459
tlieber <tlieber@paloaltonetworks.com>
2021-05-09 10:19:18 +0300

    test class

---
+++ Packs/ApiModules/Scripts/AWSApiModule/AWSApiModule_test.py
@@ -0,0 +1,11 @@
+from AWSApiModule import *
+import demistomock as demisto
+
+PARAMS = {
+    'insecure': True,
+    'access_key': 'access_key',
+    'proxy': False,
+    'secret_key': 'secret_key',
+    'roleSessionName': 'test',
+    'roleArn': 'example'
+}
