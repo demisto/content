a197b48d0b58be1ae3040f37a6e1b5ad3ffa9f73
meichlerpanw <meichler@paloaltonetworks.com>
2021-05-09 14:57:25 +0300

    revert unit test

--- Packs/CarbonBlackDefense/Integrations/CarbonBlackEndpointStandard/CarbonBlackEndpointStandard_test.py
+++ Packs/CarbonBlackDefense/Integrations/CarbonBlackEndpointStandard/CarbonBlackEndpointStandard_test.py
@@ -23,14 +23,16 @@ def test_get_alert_details_command(mocker):
     """
     mocker_result = {'id': '1234', 'severity': 7, 'category': 'THREAT', 'device_username': 'demo'}
     expected_result = {
-        'id': '1234', 'severity': 7, 'category': 'THREAT', 'device_username': 'demo'
+        'CarbonBlackDefense.Alert(val.id == obj.id)': {
+            'id': '1234', 'severity': 7, 'category': 'THREAT', 'device_username': 'demo'
+        }
     }
     client = create_client()
     mocker.patch.object(client, 'get_alert_by_id', return_value=mocker_result)
     from CarbonBlackEndpointStandard import get_alert_details_command
 
     command_results = get_alert_details_command(client, {'alertId': 1234})
-    output = command_results.to_context().get('EntryContext', {}).get('CarbonBlackDefense.Alert(val.id == obj.id)')
+    output = command_results.to_context().get('EntryContext', {})
 
     assert output == expected_result
 
@@ -49,16 +51,17 @@ def test_device_search_command(mocker):
             {'id': 1234, 'name': 'carbon-black-integration-endpoint', 'os': 'MAC'}
         ]
     }
-    expected_result = [
-        {'id': 1234, 'name': 'carbon-black-integration-endpoint', 'os': 'MAC'}
-    ]
+    expected_result = {
+        'CarbonBlackDefense.Device(val.id == obj.id)': [
+            {'id': 1234, 'name': 'carbon-black-integration-endpoint', 'os': 'MAC'}
+        ]
+    }
     client = create_client()
     mocker.patch.object(client, 'get_devices', return_value=mocker_result)
     from CarbonBlackEndpointStandard import device_search_command
 
     command_results = device_search_command(client, {'device_id': '1234', 'os': 'MAC', 'status': 'sleep'})
-    output = command_results.to_context().get('EntryContext', {}).get('CarbonBlackDefense.Device(val.id == '
-                                                                      'obj.id)')
+    output = command_results.to_context().get('EntryContext', {})
 
     assert output == expected_result
 
@@ -73,14 +76,13 @@ def test_find_events_command(mocker):
         Assert that the output is we are expected
     """
     mocker_result = {'job_id': '123456'}
-    expected_result = {'job_id': '123456'}
+    expected_result = {'CarbonBlackDefense.Events.Search(val.job_id == obj.job_id)': {'job_id': '123456'}}
     client = create_client()
     mocker.patch.object(client, 'get_events', return_value=mocker_result)
     from CarbonBlackEndpointStandard import find_events_command
 
     command_results = find_events_command(client, {})
-    output = command_results.to_context().get('EntryContext', {}).get('CarbonBlackDefense.Events.Search(val.job_id == '
-                                                                      'obj.job_id)')
+    output = command_results.to_context().get('EntryContext', {})
 
     assert output == expected_result
 
@@ -95,12 +97,12 @@ def test_find_processes_command(mocker):
         Assert that the output is we are expected
     """
     mocker_result = {'job_id': '123456'}
-    expected_result = {'job_id': '123456'}
+    expected_result = {'CarbonBlackDefense.Process.Search(val.job_id == obj.job_id)': {'job_id': '123456'}}
     client = create_client()
     mocker.patch.object(client, 'get_processes', return_value=mocker_result)
     from CarbonBlackEndpointStandard import find_processes_command
 
     command_results = find_processes_command(client, {})
-    output = command_results.to_context().get('EntryContext', {}).get('CarbonBlackDefense.Process.Search(val.job_id == obj.job_id)')
+    output = command_results.to_context().get('EntryContext', {})
 
     assert output == expected_result
