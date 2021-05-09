07350032df2cbe2877423af77bbde2b9001837bc
dmeylakh <dmeylakh@paloaltonetworks.com>
2021-05-09 10:18:17 +0300

    fixes from review

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -6,7 +6,7 @@ echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DE
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
     demisto-sdk validate -a --post-commit --id-set --id-set-path $ARTIFACTS_FOLDER/unified_id_set.json
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
-    demisto-sdk validate -g --post-commit --id-set --id-set-path ./Tests/id_set.json
+    demisto-sdk validate -g --post-commit --id-set --id-set-path $ARTIFACTS_FOLDER/id_set.json
 else
     demisto-sdk validate -g --post-commit --id-set --id-set-path $ARTIFACTS_FOLDER/unified_id_set.json
 fi
