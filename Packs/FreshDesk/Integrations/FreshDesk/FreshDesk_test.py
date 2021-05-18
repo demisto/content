8c360c8457c1474d66c2eb3d73310cde2a4902aa
dsterenson <dsterenson@paloaltonetworks.com>
2021-05-18 11:05:28 +0300

    remove freeze

--- Packs/FreshDesk/Integrations/FreshDesk/FreshDesk_test.py
+++ Packs/FreshDesk/Integrations/FreshDesk/FreshDesk_test.py
@@ -49,7 +49,6 @@ def test_fetch_incidents_no_pagination(mocker, requests_mock):
     # 1620826211000 was taken according to the AWS machine timestamp
     assert demisto.setLastRun.call_args_list[0][0][0] == {'last_created_incident_timestamp': 1620826211000}
 
-@freeze_time("2020-11-04T13:34:14.758295Z")
 def test_fetch_incidents_with_pagination(mocker, requests_mock):
     """Unit test
     Given
