5a6e595b4b7ab227bffc87d67752287cc297bbe8
devika <devika@linkshadow.com>
2021-05-19 15:44:01 +0400

    update in python code

--- Packs/Linkshadow/Integrations/Linkshadow/Linkshadow.py
+++ Packs/Linkshadow/Integrations/Linkshadow/Linkshadow.py
@@ -199,7 +199,8 @@ def main():
             demisto.setLastRun(next_run)
             demisto.incidents(incidents)
         elif demisto.command() == 'Linkshadow-fetch-entity-anomalies':
-            return_results(fetch_entity_anomalies(client, demisto.args()))
+            result = fetch_entity_anomalies(client, demisto.args())
+            return_results(result)
     except Exception as e:
         demisto.error(traceback.format_exc())  # print the traceback
         return_error(f'Failed to execute {demisto.command()} command', e)
