aa98507a83f8ae9c73fa413ca0c7e9838cd80d6b
eharush <erez@demisto.com>
2021-05-20 19:07:36 +0300

    Add script and widgets

---
+++ Packs/CortexXDR/Scripts/DBotGroupXDRIncidents/DBotGroupXDRIncidents.py
@@ -0,0 +1,36 @@
+from CommonServerPython import *
+
+model_name = 'xdr_clustering'
+field_for_grouping = 'xdralerts'
+field_for_name = 'xdralerts.causalityactorprocessimagename'
+
+return_type = demisto.args()['returnWidgetType']
+
+if return_type == 'incidents':
+    res = demisto.executeCommand('DBotShowClusteringModelInfo', {
+        'searchQuery': demisto.args().get('searchQuery'),
+        'modelName': model_name,
+        'returnType': 'incidents'
+    })
+    demisto.results(res)
+elif return_type == 'summary':
+    res = demisto.executeCommand('DBotShowClusteringModelInfo', {
+        'modelName': model_name
+    })
+    demisto.results(res)
+else:
+    args = demisto.args()
+    res = demisto.executeCommand('DBotTrainClustering', {
+        'modelName': model_name,
+        'type': demisto.args().get('incidentType'),
+        'fromDate': demisto.args().get('fromDate'),
+        'limit': demisto.args().get('limit'),
+        'fieldsForClustering': field_for_grouping,
+        'fieldForClusterName': field_for_name,
+        'storeModel': 'True',
+        'searchQuery': demisto.args().get('searchQuery'),
+        'forceRetrain': demisto.args().get('forceRetrain'),
+        'numberOfFeaturesPerField': 500,
+        'fieldsToDisplay': demisto.args().get('fieldsToDisplay')
+    })
+    demisto.results(res[-1])
