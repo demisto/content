fb3e3cad434a40d7a08876b72973b8c11dee26f1
devika <devika@linkshadow.com>
2021-05-25 17:25:11 +0400

    test-file-syntax

--- Packs/Linkshadow/Integrations/Linkshadow/Linkshadow_test.py
+++ Packs/Linkshadow/Integrations/Linkshadow/Linkshadow_test.py
@@ -16,31 +16,26 @@ def test_fetch_incidents(requests_mock):
 
     test_api_response = util_load_json('test_data/fetch_incident.json')
     requests_mock.post('https://LS_test_fetch_incidents.com/api/plugin/',
-                      json=test_api_response)
-    
+                        json=test_api_response) 
     client = Client(
-            base_url='https://LS_test_fetch_incidents.com/',
-            verify=False,
-            proxy=False)
-    
+        base_url='https://LS_test_fetch_incidents.com/',
+        verify=False,
+        proxy=False)  
     last_run = {
         'last_fetch': 1621860339000  # Mon, May 24, 2021 12:45 PM Pacific
     }
-    # WHEN the desired anomaly has 
     integration_response = fetch_incidents(
-        client = client,
+        client=client,
         max_alerts=20,
         last_run=last_run,
         first_fetch_time='1 day ago',
-        apiKey = "",
-        api_username = "lsadmin"    ,
-        plugin_id = "xsoar_integration_1604211382",
-        action = "fetch_entity_anomalies",
-        time_frame = "01",
-    )
-    
+        apiKey="",
+        api_username="lsadmin",
+        plugin_id="xsoar_integration_1604211382",
+        action="fetch_entity_anomalies",
+        time_frame="01",
+    )  
     expected_response = util_load_json('test_data/formatted_fetch_incident.json')
     # raise ValueError(integration_response.incidents,":::::",expected_response)
     # THEN the response should be returned and formatted
     assert integration_response[1] == expected_response
- 
