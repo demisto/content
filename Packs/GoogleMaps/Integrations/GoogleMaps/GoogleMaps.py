aac9b490f1dd4d45e6f703b297e2f8e58915119c
dschwartz <dschwartz@paloaltonetworks.com>
2021-05-30 09:28:17 +0300

    Returning both MAP and NOTE entry_type results

--- Packs/GoogleMaps/Integrations/GoogleMaps/GoogleMaps.py
+++ Packs/GoogleMaps/Integrations/GoogleMaps/GoogleMaps.py
@@ -1,26 +1,22 @@
 from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
 from CommonServerUserPython import *  # noqa
 
-import requests
-import traceback
-from typing import Dict, Any, Tuple, List
-
 # Disable insecure warnings
-from Packs.Base.Scripts.CommonServerPython.CommonServerPython import BaseClient
 
 requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
 
 
 class Client(BaseClient):
-    def __init__(self, api_key: str, proxy: bool):
+    def __init__(self, api_key: str, proxy: bool, insecure: bool):
         """
         Client to use in the IPinfo integration. Uses BaseClient
         """
         super().__init__(base_url='https://maps.googleapis.com/maps/api/',
+                         verify=not insecure,
                          proxy=proxy)
         self.api_key = api_key
 
-    def geocode(self, address: str):
+    def geocode(self, address: str) -> List[CommandResults]:
         address = re.sub(r'\s+', '+', address).strip('+')  # Google Maps API format
 
         # noinspection PyTypeChecker
@@ -29,21 +25,27 @@ class Client(BaseClient):
                                                             params=assign_params(address=address,
                                                                                  api_key=self.api_key))
 
-        coordinate_dict = response[0]['geometry']['location']
-        return_results(CommandResults(outputs_prefix='GoogleMaps',
-                                      outputs_key_field=['lat', 'lon'],
-                                      entry_type=EntryType.MAP_ENTRY_TYPE,
-                                      outputs=coordinate_dict,
-                                      raw_response=response))
+        result_note = CommandResults(outputs_prefix='GoogleMaps',
+                                     outputs_key_field=['results.[0].geometry.location.lat',
+                                                        'results.[0].geometry.location.lon'],
+                                     entry_type=EntryType.NOTE,
+                                     raw_response=response)
+
+        result_map = CommandResults(outputs_prefix='GoogleMaps',
+                                    outputs_key_field=['lat', 'lng'],
+                                    entry_type=EntryType.MAP_ENTRY_TYPE,
+                                    raw_response=coordinate_dict)
+
+        return [result_note, result_map]
 
 
-def google_maps_geocode_command(client: Client, address: str):
+def google_maps_geocode_command(client: Client, address: str) -> List[CommandResults]:
     return client.geocode(address)
 
 
 def test_module(client: Client) -> str:
-    """Tests GoogleMaps by geocoding the address of our TLV site"""
-    client.geocode('94 Yigal Alon, Tel Aviv')
+    """Tests GoogleMaps by geocoding the address of Demisto's (original) HQ"""
+    client.geocode('45 Rothschild, Tel Aviv')
     return 'ok'  # on any failure, an exception is raised
 
 
@@ -54,19 +56,21 @@ def main():
 
     proxy = demisto.get(params, 'proxy') or False
     api_key = demisto.get(params, 'credentials.password') or ''
+    insecure = demisto.get(params, 'insecure') or False
 
     demisto.debug(f'Command being called is {command}')
 
     try:
-        client = Client(api_key=api_key, proxy=proxy)
+        client = Client(api_key=api_key, proxy=proxy, insecure=insecure)
 
         if command == 'test-module':
             return_results(test_module(client))
 
         elif command == 'geocode':
-            google_maps_geocode_command(client, **args)
+            return_results(google_maps_geocode_command(client, **args))
+
         else:
-            raise NotImplementedError(f"command {command} is not supported")
+            raise NotImplementedError(f"command '{command}' is not supported")
 
     except Exception as e:
         demisto.error(traceback.format_exc())  # print the traceback
---
+++ Packs/GoogleMaps/Integrations/GoogleMaps/README.md
@@ -0,0 +1,48 @@
+Use the Google Maps API.
+This integration was integrated and tested with version 3.43 of the Google Maps API.
+
+## Configure GoogleMaps on Cortex XSOAR
+
+1. Navigate to **Settings** > **Integrations** > **Servers & Services**.
+2. Search for GoogleMaps.
+3. Click **Add instance** to create and configure a new integration instance.
+
+    | **Parameter** | **Description** | **Required** |
+    | --- | --- | --- |
+    | Google Maps API Key | The API key to use for the connection. | True |
+    | Trust any certificate (not secure) |  | False |
+    | Use system proxy settings |  | False |
+
+4. Click **Test** to validate the URLs, token, and connection.
+## Commands
+You can execute these commands from the Cortex XSOAR CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
+### geocode
+***
+Returns the coordinates of the given physical address.
+
+
+#### Base Command
+
+`geocode`
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| address | The physical address to be geocoded. | Required | 
+
+
+#### Context Output
+
+| **Path** | **Type** | **Description** |
+| --- | --- | --- |
+| GoogleMaps.lat | Number | Latitude of the provided location. | 
+| GoogleMaps.lng | Number | Longitude of the provided location | 
+
+
+#### Command Example
+``` ```
+
+#### Human Readable Output
+
+
