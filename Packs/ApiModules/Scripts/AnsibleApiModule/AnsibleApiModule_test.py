786a0e9c6c840b6aad1ea05245fcfc01f20ef26e
Serge Bakharev <serge.bakharev@gmail.com>
2021-05-31 01:21:51 +1000

    rec_ansible_key_strip unit test

--- Packs/ApiModules/Scripts/AnsibleApiModule/AnsibleApiModule_test.py
+++ Packs/ApiModules/Scripts/AnsibleApiModule/AnsibleApiModule_test.py
@@ -1,8 +1,8 @@
-from AnsibleApiModule import dict2md
+from AnsibleApiModule import dict2md, rec_ansible_key_strip
 from TestsInput.markdown import MOCK_SINGLE_LEVEL_LIST, EXPECTED_MD_LIST, MOCK_SINGLE_LEVEL_DICT, EXPECTED_MD_DICT
 from TestsInput.markdown import MOCK_MULTI_LEVEL_DICT, EXPECTED_MD_MULTI_DICT, MOCK_MULTI_LEVEL_LIST
 from TestsInput.markdown import EXPECTED_MD_MULTI_LIST, MOCK_MULTI_LEVEL_LIST_ID_NAMES, EXPECTED_MD_MULTI_LIST_ID_NAMES
-
+from TestsInput.ansible_keys import MOCK_ANSIBLE_DICT, EXPECTED_ANSIBLE_DICT, MOCK_ANSIBLELESS_DICT, EXPECTED_ANSIBLELESS_DICT
 
 def test_dict2md_simple_lists():
     """
@@ -13,9 +13,7 @@ def test_dict2md_simple_lists():
     - Single level dict
 
     When:
-    - Convert to markdown
-
-    Then:
+    - Convert to markdownMOCK_ANSIBLE_DICT
     - Validate that the returned text is converted to a markdown list correctly
 
     """
@@ -49,3 +47,25 @@ def test_dict2md_complex_lists():
     assert markdown_multi_dict == EXPECTED_MD_MULTI_DICT
     assert markdown_multi_list == EXPECTED_MD_MULTI_LIST
     assert markdown_multi_list_id_name == EXPECTED_MD_MULTI_LIST_ID_NAMES
+
+
+def test_rec_ansible_key_strip():
+    """
+    Scenario: Given a multi level dict rec_ansible_key_strip should recursively remove the string 'ansible_' from any keys
+
+    Given:
+    - Multi-level dict with some keys starting with ansible_
+    - Multi-level dict with no keys starting with ansible_
+
+    When:
+    - rec_ansible_key_strip is used to santise the value
+
+    Then:
+    - Return de-branded result
+
+    """
+    ansible_dict = rec_ansible_key_strip(MOCK_ANSIBLE_DICT)
+    ansibleless_dict = rec_ansible_key_strip(MOCK_ANSIBLELESS_DICT)
+
+    assert ansible_dict == EXPECTED_ANSIBLE_DICT
+    assert ansibleless_dict == EXPECTED_ANSIBLELESS_DICT
