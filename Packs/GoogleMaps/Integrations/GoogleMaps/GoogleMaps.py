5d2a1f7244054a507ff92da9c9298a67bed2a597
dschwartz <dschwartz@paloaltonetworks.com>
2021-05-30 09:29:34 +0300

    using a dummy response

--- Packs/GoogleMaps/Integrations/GoogleMaps/GoogleMaps.py
+++ Packs/GoogleMaps/Integrations/GoogleMaps/GoogleMaps.py
@@ -5,6 +5,95 @@ from CommonServerUserPython import *  # noqa
 
 requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
 
+DUMMY_RESPONSE = {
+    "results": [
+        {
+            "address_components": [
+                {
+                    "long_name": "1600",
+                    "short_name": "1600",
+                    "types": [
+                        "street_number"
+                    ]
+                },
+                {
+                    "long_name": "Amphitheatre Parkway",
+                    "short_name": "Amphitheatre Pkwy",
+                    "types": [
+                        "route"
+                    ]
+                },
+                {
+                    "long_name": "Mountain View",
+                    "short_name": "Mountain View",
+                    "types": [
+                        "locality",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "Santa Clara County",
+                    "short_name": "Santa Clara County",
+                    "types": [
+                        "administrative_area_level_2",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "California",
+                    "short_name": "CA",
+                    "types": [
+                        "administrative_area_level_1",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "United States",
+                    "short_name": "US",
+                    "types": [
+                        "country",
+                        "political"
+                    ]
+                },
+                {
+                    "long_name": "94043",
+                    "short_name": "94043",
+                    "types": [
+                        "postal_code"
+                    ]
+                }
+            ],
+            "formatted_address": "1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA",
+            "geometry": {
+                "location": {
+                    "lat": 37.4267861,
+                    "lng": -122.0806032
+                },
+                "location_type": "ROOFTOP",
+                "viewport": {
+                    "northeast": {
+                        "lat": 37.4281350802915,
+                        "lng": -122.0792542197085
+                    },
+                    "southwest": {
+                        "lat": 37.4254371197085,
+                        "lng": -122.0819521802915
+                    }
+                }
+            },
+            "place_id": "ChIJtYuu0V25j4ARwu5e4wwRYgE",
+            "plus_code": {
+                "compound_code": "CWC8+R3 Mountain View, California, United States",
+                "global_code": "849VCWC8+R3"
+            },
+            "types": [
+                "street_address"
+            ]
+        }
+    ],
+    "status": "OK"
+}
+
 
 class Client(BaseClient):
     def __init__(self, api_key: str, proxy: bool, insecure: bool):
@@ -20,10 +109,13 @@ class Client(BaseClient):
         address = re.sub(r'\s+', '+', address).strip('+')  # Google Maps API format
 
         # noinspection PyTypeChecker
-        response: List[Dict[str, Any]] = self._http_request(method='GET',
-                                                            url_suffix='geocode/json?',
-                                                            params=assign_params(address=address,
-                                                                                 api_key=self.api_key))
+        # response: List[Dict[str, Any]] = self._http_request(method='GET',
+        #                                                     url_suffix='geocode/json?',
+        #                                                     params=assign_params(address=address,
+        #                                                                          api_key=self.api_key))
+        response: Dict = DUMMY_RESPONSE  # temporary todo replace with a proper client request
+
+        coordinate_dict = response['results'][0]['geometry']['location']
 
         result_note = CommandResults(outputs_prefix='GoogleMaps',
                                      outputs_key_field=['results.[0].geometry.location.lat',
