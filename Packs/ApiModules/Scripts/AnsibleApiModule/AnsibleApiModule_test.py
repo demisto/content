3ab5dff2ddc4f25fdb77c30590773530adf68e72
Serge Bakharev <serge.bakharev@gmail.com>
2021-06-01 00:04:09 +1000

    Inventory unit tests

--- Packs/ApiModules/Scripts/AnsibleApiModule/AnsibleApiModule_test.py
+++ Packs/ApiModules/Scripts/AnsibleApiModule/AnsibleApiModule_test.py
@@ -1,8 +1,10 @@
-from AnsibleApiModule import dict2md, rec_ansible_key_strip
+from AnsibleApiModule import dict2md, rec_ansible_key_strip, generate_ansible_inventory
 from TestsInput.markdown import MOCK_SINGLE_LEVEL_LIST, EXPECTED_MD_LIST, MOCK_SINGLE_LEVEL_DICT, EXPECTED_MD_DICT
 from TestsInput.markdown import MOCK_MULTI_LEVEL_DICT, EXPECTED_MD_MULTI_DICT, MOCK_MULTI_LEVEL_LIST
 from TestsInput.markdown import EXPECTED_MD_MULTI_LIST, MOCK_MULTI_LEVEL_LIST_ID_NAMES, EXPECTED_MD_MULTI_LIST_ID_NAMES
 from TestsInput.ansible_keys import MOCK_ANSIBLE_DICT, EXPECTED_ANSIBLE_DICT, MOCK_ANSIBLELESS_DICT, EXPECTED_ANSIBLELESS_DICT
+from TestsInput.ansible_inventory import ANSIBLE_INVENTORY_HOSTS_LIST, ANSIBLE_INVENTORY_HOSTS_CSV_LIST
+from TestsInput.ansible_inventory import ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS
 
 
 def test_dict2md_simple_lists():
@@ -70,3 +72,81 @@ def test_rec_ansible_key_strip():
 
     assert ansible_dict == EXPECTED_ANSIBLE_DICT
     assert ansibleless_dict == EXPECTED_ANSIBLELESS_DICT
+
+
+def test_generate_ansible_inventory_hosts():
+    """
+    Scenario: Given different types of host input a valid ansible inventory should be generated
+
+    Given:
+    A. hosts as a python list
+    B. comma seperated list of hosts
+    C. host with port specified in args overriding integration port config
+    D. host with type of local
+
+    When:
+    - credentials are valid
+
+    Then:
+    A. Valid Ansible inventory dict is generated with correct number of hosts
+    B. Valid Ansible inventory dict is generated with correct number of hosts
+    C. Correct port should be present if integration default overridden by command args
+    D. host address should not be recorded if host type is local
+    """
+
+    # A
+    list_inv, _ = generate_ansible_inventory(ANSIBLE_INVENTORY_HOSTS_LIST, ANSIBLE_INVENTORY_INT_PARAMS, host_type="ssh")
+    assert len(list_inv.get('all').get('hosts')) == 3
+
+    # B
+    comma_inv, _ = generate_ansible_inventory(ANSIBLE_INVENTORY_HOSTS_CSV_LIST, ANSIBLE_INVENTORY_INT_PARAMS, host_type="ssh")
+    assert len(comma_inv.get('all').get('hosts')) == 2
+
+    # C
+    port_override_inv, _ = generate_ansible_inventory(
+        ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS, host_type="ssh")
+    assert port_override_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_port') == '45678'
+    assert port_override_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_host') == '123.123.123.123'
+
+    # D
+    local_inv, _ = generate_ansible_inventory(ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS, host_type="local")
+    assert local_inv == {'all': {'hosts': {'localhost': {'ansible_connection': 'local'}}}}
+
+
+def test_generate_ansible_inventory_creds():
+    """
+    Scenario: Given different types of credentials the appropriate one should be selected
+
+    Given:
+    A. username / sshkey for NXOS host
+    B. SSH credential for Linux host
+    C. windows winrm credentials
+
+    When:
+    - valid host address
+
+    Then:
+    - Valid Ansible Inventory dict is generated as appropriate for host type
+    """
+
+    # A
+    nxos_inv, nxos_sshkey = generate_ansible_inventory(
+        ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS, host_type="nxos")
+    assert nxos_sshkey == 'aaaaaaaaaaaaaa'
+    assert nxos_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_network_os') == 'nxos'
+    assert nxos_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_become_method') == 'enable'
+    assert nxos_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_user') == 'joe'
+
+    # B
+    ssh_inv, ssh_sshkey = generate_ansible_inventory(ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS, host_type="ssh")
+    assert ssh_sshkey == 'aaaaaaaaaaaaaa'
+    assert ssh_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_network_os') is None
+    assert ssh_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_user') == 'joe'
+
+    # C
+    winrm_inv, winrm_sshkey = generate_ansible_inventory(
+        ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS, host_type="winrm")
+    assert winrm_sshkey == ''
+    assert winrm_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_user') == 'joe'
+    assert winrm_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_winrm_transport') == 'ntlm'
+    assert winrm_inv.get('all').get('hosts').get('123.123.123.123:45678').get('ansible_connection') == 'winrm'
