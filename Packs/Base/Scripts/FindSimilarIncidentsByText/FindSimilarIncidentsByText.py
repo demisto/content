cdde6ebaf5f1b47ccde97844f7814adedca947f5
sabaghadar <hsabag@paloaltonetworks.com>
2021-05-31 18:15:50 +0300

    fix unit tests

--- Packs/Base/Scripts/FindSimilarIncidentsByText/FindSimilarIncidentsByText.py
+++ Packs/Base/Scripts/FindSimilarIncidentsByText/FindSimilarIncidentsByText.py
@@ -30,7 +30,7 @@ def get_texts_from_incident(incident, text_fields):
 
     # custom fields + incident fields.
     custom_fields = incident.get('CustomFields') or {}
-    for field_name, field_value in (custom_fields.items() | incident.items()):
+    for field_name, field_value in (list(custom_fields.items()) + list(incident.items())):
         if field_name in text_fields and isinstance(field_value, string_types):
             texts.append(field_value)
 
