faae63481b80a7d380c33149ce227eb3bb988539
bgenish <bgenish@paloaltonetworks.com>
2021-06-09 15:23:27 +0300

    Fixes

--- Packs/Linkshadow/Integrations/Linkshadow/Linkshadow.py
+++ Packs/Linkshadow/Integrations/Linkshadow/Linkshadow.py
@@ -80,13 +80,11 @@ def format_JSON_for_fetch_incidents(ls_anomaly):
 
 
 def fetch_incidents(client, max_alerts, last_run, first_fetch_time, apiKey, api_username, plugin_id, action):
-
-    if len(last_run) <= 0:
+    # handle first time fetch
+    if not last_run.get('last_fetch'):
         last_fetch = dateparser.parse(first_fetch_time, settings={'TIMEZONE': 'UTC'})
     else:
         last_fetch = dateparser.parse(last_run.get('last_fetch'))
-        if last_fetch is None:
-            last_fetch = dateparser.parse(first_fetch_time, settings={'TIMEZONE': 'UTC'})
     latest_created_time = last_fetch
     diff_timedelta = float(datetime.utcnow().strftime('%s')) - float(latest_created_time.strftime('%s'))
     time_frame = int(math.ceil(diff_timedelta / 60))
