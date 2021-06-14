5e71105a8b25c2896b7d3322d9c17bc094270ca6
Pierre MAHOT <pierre.mahot@orange.com>
2021-06-14 11:37:27 +0200

    set default value to 10

--- Packs/SentinelOne/Integrations/SentinelOne-V2/SentinelOne-V2.py
+++ Packs/SentinelOne/Integrations/SentinelOne-V2/SentinelOne-V2.py
@@ -958,7 +958,7 @@ def list_agents_command(client: Client, args: dict) -> CommandResults:
         scan_status=args.get('scan_status'),
         os_type=args.get('os_type'),
         created_at=args.get('created_at'),
-        limit=int(args.get('limit', 50)),
+        limit=int(args.get('limit', 10)),
     )
 
     # Make request and get raw response
