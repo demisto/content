fe699282106b77f67eb12e407338823f24e6d2e2
mgalitzki <mgalitzki@paloaltonetworks.com>
2021-06-15 09:49:38 +0300

    test

--- .circleci/content_release_vars.sh
+++ .circleci/content_release_vars.sh
@@ -1,3 +1,3 @@
-export CONTENT_VERSION="21.6.0"
+export CONTENT_VERSION="21.6.1"
 export SERVER_VERSION="6.0.0"
 export GIT_SHA1="6af55df6c64be2e81edf60af8b285b73e03eda61" # guardrails-disable-line disable-secrets-detection
