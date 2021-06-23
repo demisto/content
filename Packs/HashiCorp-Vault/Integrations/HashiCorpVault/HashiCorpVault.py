e9a9d50f9661c10b335b91d46740b3f6ee62d655
glicht <glicht@users.noreply.github.com>
2021-06-23 10:59:08 +0300

    ignores

--- Packs/HashiCorp-Vault/Integrations/HashiCorpVault/HashiCorpVault.py
+++ Packs/HashiCorp-Vault/Integrations/HashiCorpVault/HashiCorpVault.py
@@ -28,8 +28,8 @@ TOKEN = demisto.params().get('token')
 
 def get_server_url():
     url = demisto.params()['server']
-    url = re.sub('/[\/]+$/', '', url)
-    url = re.sub('\/$', '', url)
+    url = re.sub('/[\/]+$/', '', url)  # guardrails-disable-line
+    url = re.sub('\/$', '', url)  # guardrails-disable-line
     return url
 
 
