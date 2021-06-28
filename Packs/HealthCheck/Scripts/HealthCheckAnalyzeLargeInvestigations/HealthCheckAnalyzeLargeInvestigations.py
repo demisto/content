deee94ba913ada61b9568505791598baeccf03f5
yaakovi <syaakovi@paloaltonetworks.com>
2021-06-28 08:38:24 +0300

    added unit tests for HealthCheckAnalyzeLargeInvestigations

--- Packs/HealthCheck/Scripts/HealthCheckAnalyzeLargeInvestigations/HealthCheckAnalyzeLargeInvestigations.py
+++ Packs/HealthCheck/Scripts/HealthCheckAnalyzeLargeInvestigations/HealthCheckAnalyzeLargeInvestigations.py
@@ -1,54 +1,13 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-
 import dateutil.relativedelta
 
-args = demisto.args()
-Thresholds = {
+THRESHOLDS = {
     "numberofincidentswithmorethan500entries": 300,
     "numberofincidentsbiggerthan10mb": 1,
     "numberofincidentsbiggerthan1mb": 300,
 }
-thresholds = args.get('Thresholds', Thresholds)
-
-prevMonth = datetime.today() + dateutil.relativedelta.relativedelta(months=-1)
-
-res = demisto.executeCommand("GetLargestInvestigations", {"from": prevMonth.strftime("%Y-%m-%d"),
-                                                          "to": prevMonth.strftime("%Y-%m-%d"),
-                                                          "table_result": "true"})
-
-incidentsbiggerthan1mb = res[0]['Contents']['data']
-incidentsbiggerthan10mb = list(filter(lambda x: x['Size(MB)'] > 10, res[0]['Contents']['data']))
-incidentswithmorethan500entries = list(filter(lambda x: x['AmountOfEntries'] > 500, res[0]['Contents']['data']))
-numberofincidentsbiggerthan1mb = res[0]['Contents']['total']
-numberofincidentsbiggerthan10mb = len(list(filter(lambda x: x['Size(MB)'] > 10, res[0]['Contents']['data'])))
-numberofincidentswithmorethan500entries = len(list(filter(lambda x: x['AmountOfEntries'] > 500, res[0]['Contents']['data'])))
-
-
-for table in [incidentsbiggerthan1mb, incidentsbiggerthan10mb, incidentswithmorethan500entries]:
-    for entry in table:
-        for key in list(entry):
-            if key == 'Size(MB)':
-                new_key = key.replace("(MB)", "")
-                entry[new_key.lower()] = entry.pop(key)
-                entry[new_key.lower()] = str(entry[new_key.lower()]) + " MB"
-                continue
-
-            entry[key.lower()] = entry.pop(key)
-
-analyzeFields = {
-    "investigationsbiggerthan1mb": incidentsbiggerthan1mb,
-    "investigationsbiggerthan10mb": incidentsbiggerthan10mb,
-    "investigationswithmorethan500entries": incidentswithmorethan500entries,
-    "numberofinvestigationsbiggerthan1mb": numberofincidentsbiggerthan1mb,
-    "numberofinvestigationsbiggerthan10mb": numberofincidentsbiggerthan10mb,
-    "numberofinvestigationswithmorethan500entries": numberofincidentswithmorethan500entries
-}
-
-demisto.executeCommand('setIncident', analyzeFields)
-
-actionItems = []
 DESCRIPTION = [
     "Too many incidents with high number of war room entries were found, consider to use quite mode in task settings",
     "Large incidents were found, consider to use quite mode in task settings and delete context unneeded Context"
@@ -58,30 +17,87 @@ RESOLUTION = [
     "Extending Context and Ignore Outputs: https://xsoar.pan.dev/docs/playbooks/playbooks-extend-context"
 ]
 
-if numberofincidentswithmorethan500entries > int(thresholds['numberofincidentswithmorethan500entries']):
 
-    actionItems.append({'category': 'DB Analysis',
-                        'severity': 'High',
-                        'description': DESCRIPTION[0],
-                        'resolution': '{}'.format(RESOLUTION[0])
-                        })
+def format_dict_keys(data: List[Dict[str, Any]]) -> List:
+    formated_data = []
+    for entry in data:
+        new_entry = {}
+        for key, value in entry.items():
+            if key == 'Size(MB)':
+                new_entry['size'] = f'{value} MB'
+            else:
+                new_entry[key.lower()] = value
 
-if numberofincidentsbiggerthan10mb > thresholds['numberofincidentsbiggerthan10mb']:
-    actionItems.append({'category': 'DB Analysis',
-                        'severity': 'High',
-                        'description': DESCRIPTION[1],
-                        'resolution': '{} \n{}'.format(RESOLUTION[0], RESOLUTION[1])
-                        })
+        formated_data.append(new_entry)
+    return formated_data
 
-if numberofincidentsbiggerthan1mb > thresholds['numberofincidentsbiggerthan1mb']:
-    actionItems.append({'category': 'DB Analysis',
-                        'severity': 'High',
-                        'description': DESCRIPTION[1],
-                        'resolution': '{} \n{}'.format(RESOLUTION[0], RESOLUTION[1])
-                        })
 
-results = CommandResults(
-    outputs_prefix="dbstatactionableitems",
-    outputs=actionItems)
+def main(args):
+    thresholds = args.get('Thresholds', THRESHOLDS)
+    prev_month = datetime.today() + dateutil.relativedelta.relativedelta(months=-1)
 
-return_results(results)
+    res = demisto.executeCommand("GetLargestInvestigations", {"from": prev_month.strftime("%Y-%m-%d"),
+                                                              "to": prev_month.strftime("%Y-%m-%d"),
+                                                              "table_result": "true"})
+    if is_error(res):
+        return_results(res)
+        return_error('Failed to run GetLargestInvestigations.')
+
+    res_data = format_dict_keys(res[0]['Contents']['data'])
+    incidentsbiggerthan1mb = res_data
+    numberofincidentsbiggerthan1mb = res[0]['Contents']['total']
+
+    incidentsbiggerthan10mb = [incident for incident in res_data if int(incident['size'].split()[0]) > 10]
+    numberofincidentsbiggerthan10mb = len(incidentsbiggerthan10mb)
+
+    incidentswithmorethan500entries = [incident for incident in res_data if incident['amountofentries'] > 500]
+    numberofincidentswithmorethan500entries = len(incidentswithmorethan500entries)
+
+    analyze_fields = {
+        "investigationsbiggerthan1mb": incidentsbiggerthan1mb,
+        "investigationsbiggerthan10mb": incidentsbiggerthan10mb,
+        "investigationswithmorethan500entries": incidentswithmorethan500entries,
+        "numberofinvestigationsbiggerthan1mb": numberofincidentsbiggerthan1mb,
+        "numberofinvestigationsbiggerthan10mb": numberofincidentsbiggerthan10mb,
+        "numberofinvestigationswithmorethan500entries": numberofincidentswithmorethan500entries,
+    }
+
+    res = demisto.executeCommand('setIncident', analyze_fields)
+    if is_error(res):
+        return_results(res)
+        return_error('Failed to run setIncident.')
+
+    action_items = []
+    if numberofincidentswithmorethan500entries > int(thresholds['numberofincidentswithmorethan500entries']):
+        action_items.append({
+            'category': 'DB Analysis',
+            'severity': 'High',
+            'description': DESCRIPTION[0],
+            'resolution': '{}'.format(RESOLUTION[0]),
+        })
+
+    if numberofincidentsbiggerthan10mb > thresholds['numberofincidentsbiggerthan10mb']:
+        action_items.append({
+            'category': 'DB Analysis',
+            'severity': 'High',
+            'description': DESCRIPTION[1],
+            'resolution': '{} \n{}'.format(RESOLUTION[0], RESOLUTION[1]),
+        })
+
+    if numberofincidentsbiggerthan1mb > thresholds['numberofincidentsbiggerthan1mb']:
+        action_items.append({
+            'category': 'DB Analysis',
+            'severity': 'High',
+            'description': DESCRIPTION[1],
+            'resolution': '{} \n{}'.format(RESOLUTION[0], RESOLUTION[1]),
+        })
+
+    results = CommandResults(
+        outputs_prefix="dbstatactionableitems",
+        outputs=action_items)
+
+    return results
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):  # pragma: no cover
+    return_results(main(demisto.args()))
