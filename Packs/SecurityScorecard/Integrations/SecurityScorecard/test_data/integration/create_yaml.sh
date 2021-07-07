d4ed3c224516cde553dabb93a94a2491b3dc346d
Kobbi Gal <kgal@m-c02fd2yumd6r.paloaltonetworks.local>
2021-07-07 14:53:44 +0300

    rm api key

--- Packs/SecurityScorecard/Integrations/SecurityScorecard/test_data/integration/create_yaml.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DEMISTO_BASE_URL=https://10.180.188.128
-export DEMISTO_API_KEY=EE81DF1F870DE84DE50CAB3C1C6CBEE5
-
-rm integration-integration.yml
-demisto-sdk unify -i .
-demisto-sdk upload -i integration-integration.yml --insecure --verbose
