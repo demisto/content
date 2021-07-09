9bcb203e31950e1d08d9839ebc15cf8bf76e89b4
darkushin <darkushin@paloaltonetworks.com>
2021-07-09 12:15:15 +0300

    Updated endpoints

--- Packs/MicrosoftGraphUser/Integrations/MicrosoftGraphUser/MicrosoftGraphUser.py
+++ Packs/MicrosoftGraphUser/Integrations/MicrosoftGraphUser/MicrosoftGraphUser.py
@@ -12,7 +12,7 @@ BLOCK_ACCOUNT_JSON = '{"accountEnabled": false}'
 UNBLOCK_ACCOUNT_JSON = '{"accountEnabled": true}'
 NO_OUTPUTS: dict = {}
 APP_NAME = 'ms-graph-user'
-GRAPH_ENDPOINTS = {
+GRAPH_BASE_ENDPOINTS = {
     'https://graph.microsoft.com': 'com',
     'https://graph.microsoft.us': 'gcc-high',
     'https://dod-graph.microsoft.us': 'dod',
@@ -60,13 +60,13 @@ class MsGraphClient:
     """
 
     def __init__(self, tenant_id, auth_id, enc_key, app_name, base_url, verify, proxy, self_deployed,
-                 redirect_uri, auth_code, auth_endpoint):
+                 redirect_uri, auth_code, endpoint):
         grant_type = AUTHORIZATION_CODE if self_deployed else CLIENT_CREDENTIALS
         resource = None if self_deployed else ''
         self.ms_client = MicrosoftClient(tenant_id=tenant_id, auth_id=auth_id, enc_key=enc_key, app_name=app_name,
                                          base_url=base_url, verify=verify, proxy=proxy, self_deployed=self_deployed,
                                          redirect_uri=redirect_uri, auth_code=auth_code, grant_type=grant_type,
-                                         resource=resource, auth_endpoint=auth_endpoint)
+                                         resource=resource, endpoint=endpoint)
 
     #  If successful, this method returns 204 No Content response code.
     #  Using resp_type=text to avoid parsing error.
@@ -376,7 +376,7 @@ def main():
     params: dict = demisto.params()
     host = params.get('host', '').rstrip('/')
     url = host + '/v1.0/'
-    auth_endpoint = GRAPH_ENDPOINTS.get(host, 'com')
+    endpoint = GRAPH_BASE_ENDPOINTS.get(host, 'com')
     tenant = params.get('tenant_id')
     auth_and_token_url = params.get('auth_id', '')
     enc_key = params.get('enc_key')
@@ -409,7 +409,7 @@ def main():
         client: MsGraphClient = MsGraphClient(tenant_id=tenant, auth_id=auth_and_token_url, enc_key=enc_key,
                                               app_name=APP_NAME, base_url=url, verify=verify, proxy=proxy,
                                               self_deployed=self_deployed, redirect_uri=redirect_uri,
-                                              auth_code=auth_code, auth_endpoint=auth_endpoint)
+                                              auth_code=auth_code, endpoint=endpoint)
         human_readable, entry_context, raw_response = commands[command](client, demisto.args())  # type: ignore
         return_outputs(readable_output=human_readable, outputs=entry_context, raw_response=raw_response)
 
