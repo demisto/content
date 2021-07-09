466595d3be560aab60c6a1114f604351189745da
lperry <lperry@paloaltonetworks.com>
2021-07-09 15:43:32 +0300

    stablizing fetch data testa

--- Packs/Base/Scripts/DBotMLFetchData/DBotMLFetchData_test.py
+++ Packs/Base/Scripts/DBotMLFetchData/DBotMLFetchData_test.py
@@ -6,7 +6,6 @@ import string
 from bs4 import BeautifulSoup
 import math
 import pandas as pd
-import numpy as np
 
 
 def test_find_label_fields_candidates():
@@ -381,7 +380,7 @@ def test_whole_preprocessing(mocker):
             json.dump(data, fp=file, indent=4)
         prof.print_stats(sort='cumtime')
     assert len(data['log']['exceptions']) == 0
-    assert len(data['X']) == 100
+    assert len(data['X']) + len(data['log']['n_timout']) + len(data['log']['n_short_text_fields']) == 100
 
 
 def test_whole_preprocessing_short_incident(mocker):
@@ -404,7 +403,7 @@ def test_whole_preprocessing_short_incident(mocker):
             json.dump(data, fp=file, indent=4)
         prof.print_stats(sort='cumtime')
     assert len(data['log']['exceptions']) == 0
-    assert len(data['X']) == 100
+    assert len(data['X']) + len(data['log']['n_timout']) + len(data['log']['n_short_text_fields']) == 100
     # check labels order kept as original excluding the short label
     assert Counter(x['closeReason'] for x in data['X']) == Counter(
         [inc['closeReason'] for i, inc in enumerate(incidents) if i != short_text_incident_index])
@@ -429,7 +428,7 @@ def test_whole_preprocessing_incdient_without_label(mocker):
             json.dump(data, fp=file, indent=4)
         prof.print_stats(sort='cumtime')
     assert len(data['log']['exceptions']) == 0
-    assert len(data['X']) == 100
+    assert len(data['X']) + len(data['log']['n_timout']) + len(data['log']['n_short_text_fields']) == 100
     # check labels order kept as original excluding the short label
     assert Counter(x['closeReason'] for x in data['X']) == Counter(
         [inc['closeReason'] for i, inc in enumerate(incidents) if i != no_label_idx])
