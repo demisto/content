a36f8ecad28080fb2578c69f1984369428161af6
tlieber <tlieber@paloaltonetworks.com>
2021-07-11 12:28:04 +0300

    added test update rn

--- Packs/CommonScripts/Scripts/ParseEmailFiles/ParseEmailFiles_test.py
+++ Packs/CommonScripts/Scripts/ParseEmailFiles/ParseEmailFiles_test.py
@@ -666,6 +666,22 @@ def test_eml_contains_htm_attachment(mocker):
     assert results[0]['EntryContext']['Email'][u'Attachments'] == '1.htm'
 
 
+def test_eml_contains_html_and_text(mocker):
+    mocker.patch.object(demisto, 'args', return_value={'entryid': 'test'})
+    mocker.patch.object(demisto, 'executeCommand',
+                        side_effect=exec_command_for_file('multipart_alternative_format.p7m',
+                                                          info="multipart/alternative;, ISO-8859 text, with CRLF line terminators"))
+    mocker.patch.object(demisto, 'results')
+    # validate our mocks are good
+    assert demisto.args()['entryid'] == 'test'
+    main()
+
+    results = demisto.results.call_args[0]
+    assert len(results) == 1
+    assert results[0]['Type'] == entryTypes['note']
+    assert results[0]['EntryContext']['Email']['HTML'] == """<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:w="urn:schemas-microsoft-com:office:word" xmlns:m="http://schemas.microsoft.com/office/2004/12/omml" xmlns="http://www.w3.org/TR/REC-html40"><head><meta name="Generator" content="Microsoft Word 15 (filtered medium)"><style><!--/* Font Definitions */@font-face	{font-family:"Cambria Math";	panose-1:2 4 5 3 5 4 6 3 2 4;}@font-face	{font-family:Calibri;	panose-1:2 15 5 2 2 2 4 3 2 4;}@font-face	{font-family:"PKO Bank Polski";	panose-1:2 11 6 4 2 2 2 2 2 4;}/* Style Definitions */p.MsoNormal, li.MsoNormal, div.MsoNormal	{margin:0cm;	margin-bottom:.0001pt;	font-size:11.0pt;	font-family:"Calibri",sans-serif;	mso-fareast-language:EN-US;}a:link, span.MsoHyperlink	{mso-style-priority:99;	color:#0563C1;	text-decoration:underline;}a:visited, span.MsoHyperlinkFollowed	{mso-style-priority:99;	color:#954F72;	text-decoration:underline;}span.Stylwiadomocie-mail17	{mso-style-type:personal-compose;	font-family:"PKO Bank Polski",sans-serif;	color:black;	font-weight:normal;	font-style:normal;	text-decoration:none none;}.MsoChpDefault	{mso-style-type:export-only;	font-family:"Calibri",sans-serif;	mso-fareast-language:EN-US;}@page WordSection1	{size:612.0pt 792.0pt;	margin:70.85pt 70.85pt 70.85pt 70.85pt;}div.WordSection1	{page:WordSection1;}--></style><!--[if gte mso 9]><xml><o:shapedefaults v:ext="edit" spidmax="1026" /></xml><![endif]--><!--[if gte mso 9]><xml><o:shapelayout v:ext="edit"><o:idmap v:ext="edit" data="1" /></o:shapelayout></xml><![endif]--></head><body lang="PL" link="#0563C1" vlink="#954F72"><div id="BodyTopText"><p style="font-family:PKO Bank Polski;font-size:15px;color:#0098d4">Wewnętrzne</p></div><div class="WordSection1"><p class="MsoNormal"><span style='font-size:10.0pt;font-family:"PKO Bank Polski",sans-serif;color:black'>żółć<o:p></o:p></span></p></div></body></html>"""
+
+
 def test_eml_base64_header_comment_although_string(mocker):
     def executeCommand(name, args=None):
         if name == 'getFilePath':
