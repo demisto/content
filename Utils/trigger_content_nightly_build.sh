4adab331f8b64d86ba15f009583e978d0ae29aed
avidan-H <46294017+avidan-H@users.noreply.github.com>
2021-07-14 23:54:41 +0300

    Revert "Update Circle Nightly to only Run Validations and Lint & Unit Tests (#13674)"
    This reverts commit fdbd14f45af61868d328e4b80bfd13bd586c148a.

--- Utils/trigger_content_nightly_build.sh
+++ Utils/trigger_content_nightly_build.sh
@@ -1,42 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$#" -lt "1" ]; then
-  echo "Usage:
-  $0 -ct <token> -b <branch>
-  [-ct, --ci-token]           The ci token.
-  [-b, --branch]              The content repo branch name. Default is the current branch.
-  [-l, --legacy]              Flag to pass if triggering a legacy nightly build (aka running all test playbooks)
-  "
-  exit 1
-fi
-
-_branch="$(git branch  --show-current)"
-_nightly_type="nightly"
-
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
-
-  -ct|--ci-token) _ci_token="$2"
-    shift
-    shift;;
-
-  -b|--branch) _branch="$2"
-    shift
-    shift;;
-
-  -l|--legacy) _nightly_type="legacy_nightly"
-    shift
-    shift;;
-
-  *)    # unknown option.
-    shift;;
-  esac
-done
-
-if [ -z "$_ci_token" ]; then
-    echo "You must provide a ci token."
-    exit 1
-fi
+_circle_token=$1
+[ -n "$2" ] && _branch="$2" || _branch="$(git branch  --show-current)"
 
 trigger_build_url="https://circleci.com/api/v2/project/github/demisto/content/pipeline"
 
@@ -44,7 +9,7 @@ post_data=$(cat <<-EOF
 {
   "branch": "${_branch}",
   "parameters": {
-    "${_nightly_type}": "true",
+    "nightly": "true",
     "time_to_live": "900"
   }
 }
@@ -58,4 +23,4 @@ curl \
 -k \
 --data "${post_data}" \
 --request POST ${trigger_build_url} \
---user "$_ci_token:"
+--user "$_circle_token:"
