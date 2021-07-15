0c830f12d5e3c54fb223cbd5f1a8e285dfb87170
tlieber <tlieber@paloaltonetworks.com>
2021-07-15 18:24:29 +0300

    ut

--- Packs/CommonScripts/Scripts/ParseEmailFiles/ParseEmailFiles_test.py
+++ Packs/CommonScripts/Scripts/ParseEmailFiles/ParseEmailFiles_test.py
@@ -697,7 +697,6 @@ def test_eml_format_multipart_mix(mocker):
     results = demisto.results.call_args[0]
     assert len(results) == 1
     assert results[0]['Type'] == entryTypes['note']
-    assert results[0]['FileMetadata']['info'] == "multipart/mixed"
     assert "Rondo ONZ 1, 00-124 Warsaw, Poland <o:p></o:p>" in results[0]['EntryContext']['Email']['HTML']
 
 
