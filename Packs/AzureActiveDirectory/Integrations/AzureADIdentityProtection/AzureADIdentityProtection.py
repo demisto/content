ed82fab7a335957e05587bcf2f71b432cd5a197b
dschwartz <dschwartz@paloaltonetworks.com>
2021-07-19 15:53:14 +0300

    formatting

--- Packs/AzureActiveDirectory/Integrations/AzureADIdentityProtection/AzureADIdentityProtection.py
+++ Packs/AzureActiveDirectory/Integrations/AzureADIdentityProtection/AzureADIdentityProtection.py
@@ -27,10 +27,11 @@ class AzureADClient:
             proxy=proxy,
             resource=f'{BASE_URL}/{resource_group_name}',
             scope=' '.join(
-                ('offline_access',  # allows device-flow login
-                 'IdentityRiskEvent.Read.All',
-                 'IdentityRiskyUser.ReadWrite.All'
-                 )
+                (
+                    'offline_access',  # allows device-flow login
+                    'IdentityRiskEvent.Read.All',
+                    'IdentityRiskyUser.ReadWrite.All'
+                )
             ),
             azure_ad_endpoint=azure_ad_endpoint
         )
