cfda167963c39841149274c6e10e32e9d7a22ec8
cshayner <cshayner@paloaltonetworks.com>
2021-07-21 12:07:25 +0300

    fix formatting script

--- Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern.py
+++ Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern.py
@@ -17,7 +17,7 @@ def is_valid_attack_pattern(items):
 
     except ValueError as e:
         if 'verify you have proper integration enabled to support it' in str(e):
-            demisto.info('Unsupported Command : mitre-get-attack-pattern-value, '
+            demisto.info('Unsupported Command : mitre-get-indicator-name, '
                          'verify you have proper integration (MITRE ATTACK v2) enabled to support it. '
                          'This Is needed in order to auto extract MITRE IDs and translate them to Attack Pattern IOCs')
         else:
