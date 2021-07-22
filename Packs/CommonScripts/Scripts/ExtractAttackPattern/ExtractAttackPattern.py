4c3b9cc0f4db486b94860e48c7e625bd6b9e644d
cshayner <cshayner@paloaltonetworks.com>
2021-07-22 12:17:13 +0300

    add formatting script to attack pattern

---
+++ Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern.py
@@ -0,0 +1,28 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+import requests
+
+# Disable insecure warnings
+requests.packages.urllib3.disable_warnings()
+
+
+def is_valid_attack_pattern(items):
+
+    try:
+        results = demisto.executeCommand('mitre-get-indicator-name', {'attack_ids': items})
+        list_contents = results[2]['Contents']
+        values = [content.get('value') for content in list_contents]
+        return values if values else False
+
+    except ValueError as e:
+        if 'verify you have proper integration enabled to support it' in str(e):
+            demisto.info('Unsupported Command : mitre-get-indicator-name, '
+                         'verify you have proper integration (MITRE ATTACK v2) enabled to support it. '
+                         'This Is needed in order to auto extract MITRE IDs and translate them to Attack Pattern IOCs')
+        else:
+            demisto.info(f'MITRE Attack formatting script, {str(e)}')
+        return False
+    except Exception as e:
+        demisto.info(f'MITRE Attack formatting script, {str(e)}')
+        return False
