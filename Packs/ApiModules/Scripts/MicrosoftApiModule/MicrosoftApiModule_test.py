b654f4d5da7f4f7af3307108235c49f2b586ebea
tlieber <tlieber@paloaltonetworks.com>
2021-07-29 14:55:36 +0300

    fix ut

--- Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule_test.py
+++ Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule_test.py
@@ -238,7 +238,8 @@ def test_oproxy_request(mocker, requests_mock, client, enc_content, tokens, res)
         'app_name': APP_NAME,
         'registration_id': AUTH_ID,
         'encrypted_token': enc_content + ENC_KEY,
-        'scope': None
+        'scope': None,
+        'resource': ''
     }
     mocker.patch.object(client, '_add_info_headers')
     mocker.patch.object(client, 'get_encrypted', side_effect=get_encrypted)
