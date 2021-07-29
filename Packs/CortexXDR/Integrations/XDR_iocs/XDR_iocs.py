0f8686b943f5374156e395dbccc3a1e845527547
eli sharf <57587340+esharf@users.noreply.github.com>
2021-07-29 07:31:31 +0300

    added comment

--- Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs.py
+++ Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs.py
@@ -67,6 +67,7 @@ class Client:
 
     @property
     def _headers(self):
+        # the header should be calculated at most 5 min before the request fired 
         return get_headers(self._params)
 
 
