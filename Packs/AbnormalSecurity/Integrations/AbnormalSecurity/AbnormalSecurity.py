eea7b928caa3e61433edc4881c41ea2566d932e1
De Sheng Chuan <dschuan@abnormalsecurity.com>
2021-07-30 19:19:28 +0800

    Initial push of new commands available for the integration

--- Packs/AbnormalSecurity/Integrations/AbnormalSecurity/AbnormalSecurity.py
+++ Packs/AbnormalSecurity/Integrations/AbnormalSecurity/AbnormalSecurity.py
@@ -24,12 +24,30 @@ class Client(BaseClient):
 
         return response
 
+    def download_data_from_threat_log_in_csv_format_request(self, filter_, source):
+        params = assign_params(filter=filter_, source=source)
+
+        headers = self._headers
+
+        response = self._http_request('get', 'threats_export/csv', params=params, headers=headers)
+
+        return response
+
     def get_a_list_of_abnormal_cases_identified_by_abnormal_security_request(self, filter_, page_size, page_number):
         params = assign_params(filter=filter_, pageSize=page_size, pageNumber=page_number)
 
         headers = self._headers
 
         response = self._http_request('get', 'cases', params=params, headers=headers)
+
+        return response
+
+    def get_a_list_of_campaigns_submitted_to_abuse_mailbox_request(self, filter_, pageSize, pageNumber):
+        params = assign_params(filter=filter_, pageSize=pageSize, pageNumber=pageNumber)
+
+        headers = self._headers
+
+        response = self._http_request('get', 'abusecampaigns', params=params, headers=headers)
 
         return response
 
@@ -56,6 +74,42 @@ class Client(BaseClient):
 
         return response
 
+    def get_details_of_an_abuse_mailbox_campaign_request(self, campaignId):
+
+        headers = self._headers
+
+        response = self._http_request('get', f'abusecampaigns/{campaignId}', headers=headers)
+
+        return response
+
+
+    def get_employee_identity_analysis_genome_data_request(self, emailAddress):
+
+        headers = self._headers
+
+        response = self._http_request('get', f'employee/{emailAddress}/identity', headers=headers)
+
+        return response
+
+
+    def get_employee_information_request(self, emailAddress):
+
+        headers = self._headers
+
+        response = self._http_request('get', f'employee/{emailAddress}', headers=headers)
+
+        return response
+
+
+    def get_employee_login_information_for_last_30_days_in_csv_format_request(self, emailAddress):
+
+        headers = self._headers
+
+        response = self._http_request('get', f'employee/{emailAddress}/logins', headers=headers)
+
+        return response
+
+
     def get_the_latest_threat_intel_feed_request(self):
 
         headers = self._headers
@@ -76,6 +130,14 @@ class Client(BaseClient):
         json_data = {'action': action}
 
         response = self._http_request('post', f'cases/{case_id}', json_data=json_data, headers=headers)
+
+        return response
+
+    def provides_the_analysis_and_timeline_details_of_a_case_request(self, caseId):
+
+        headers = self._headers
+
+        response = self._http_request('get', f'cases/{caseId}/analysis', headers=headers)
 
         return response
 
@@ -120,6 +182,21 @@ def check_the_status_of_an_action_requested_on_a_threat_command(client, args):
     return command_results
 
 
+def download_data_from_threat_log_in_csv_format_command(client, args):
+    filter_ = str(args.get('filter', ''))
+    source = str(args.get('source', ''))
+
+    response = client.download_data_from_threat_log_in_csv_format_request(filter_, source)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
 def get_a_list_of_abnormal_cases_identified_by_abnormal_security_command(client, args):
     filter_ = str(args.get('filter', ''))
     page_size = args.get('page_size', None)
@@ -131,6 +208,22 @@ def get_a_list_of_abnormal_cases_identified_by_abnormal_security_command(client,
     command_results = CommandResults(
         readable_output=markdown,
         outputs_prefix='AbnormalSecurity.inline_response_200_1',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
+def get_a_list_of_campaigns_submitted_to_abuse_mailbox_command(client, args):
+    filter_ = str(args.get('filter', ''))
+    pageSize = args.get('pageSize', None)
+    pageNumber = args.get('pageNumber', None)
+
+    response = client.get_a_list_of_campaigns_submitted_to_abuse_mailbox_request(filter_, pageSize, pageNumber)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity.inline_response_200_3',
         outputs_key_field='',
         outputs=response,
         raw_response=response
@@ -210,6 +303,62 @@ def get_details_of_an_abnormal_case_command(client, args):
     return command_results
 
 
+def get_details_of_an_abuse_mailbox_campaign_command(client, args):
+    campaignId = str(args.get('campaignId', ''))
+
+    response = client.get_details_of_an_abuse_mailbox_campaign_request(campaignId)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity.AbuseCampaignDetails',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
+def get_employee_identity_analysis_genome_data_command(client, args):
+    emailAddress = str(args.get('emailAddress', ''))
+
+    response = client.get_employee_identity_analysis_genome_data_request(emailAddress)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity.EmployeeIdentityDetails',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
+def get_employee_information_command(client, args):
+    emailAddress = str(args.get('emailAddress', ''))
+
+    response = client.get_employee_information_request(emailAddress)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity.EmployeeDetails',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
+def get_employee_login_information_for_last_30_days_in_csv_format_command(client, args):
+    emailAddress = str(args.get('emailAddress', ''))
+
+    response = client.get_employee_login_information_for_last_30_days_in_csv_format_request(emailAddress)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
 def get_the_latest_threat_intel_feed_command(client, args=None):
 
     response = client.get_the_latest_threat_intel_feed_request()
@@ -245,6 +394,20 @@ def manage_an_abnormal_case_command(client, args):
     response = client.manage_an_abnormal_case_request(case_id, action)
     command_results = CommandResults(
         outputs_prefix='AbnormalSecurity.CaseManageResults',
+        outputs_key_field='',
+        outputs=response,
+        raw_response=response
+    )
+
+    return command_results
+
+
+def provides_the_analysis_and_timeline_details_of_a_case_command(client, args):
+    caseId = str(args.get('caseId', ''))
+
+    response = client.provides_the_analysis_and_timeline_details_of_a_case_request(caseId)
+    command_results = CommandResults(
+        outputs_prefix='AbnormalSecurity.inline_response_200_2',
         outputs_key_field='',
         outputs=response,
         raw_response=response
@@ -296,19 +459,26 @@ def main():
                 check_the_status_of_an_action_requested_on_a_case_command,
             'abnormal-security-check-threat-action-status':
                 check_the_status_of_an_action_requested_on_a_threat_command,
+            'abnormalsecurity-download-data-from-threat-log-in-csv-format': download_data_from_threat_log_in_csv_format_command,
             'abnormal-security-list-abnormal-cases':
                 get_a_list_of_abnormal_cases_identified_by_abnormal_security_command,
+            'abnormalsecurity-get-a-list-of-campaigns-submitted-to-abuse-mailbox': get_a_list_of_campaigns_submitted_to_abuse_mailbox_command,
             'abnormal-security-list-threats':
                 get_a_list_of_threats_command,
             'abnormal-security-get-threat':
                 get_details_of_a_threat_command,
             'abnormal-security-get-abnormal-case':
                 get_details_of_an_abnormal_case_command,
+            'abnormalsecurity-get-details-of-an-abuse-mailbox-campaign': get_details_of_an_abuse_mailbox_campaign_command,
+            'abnormalsecurity-get-employee-identity-analysis-genome-data': get_employee_identity_analysis_genome_data_command,
+            'abnormalsecurity-get-employee-information': get_employee_information_command,
+            'abnormalsecurity-get-employee-login-information-for-last-30-days-in-csv-format': get_employee_login_information_for_last_30_days_in_csv_format_command,
             'abnormal-security-get-latest-threat-intel-feed': get_the_latest_threat_intel_feed_command,
             'abnormal-security-manage-threat':
                 manage_a_threat_identified_by_abnormal_security_command,
             'abnormal-security-manage-abnormal-case':
                 manage_an_abnormal_case_command,
+            'abnormalsecurity-provides-the-analysis-and-timeline-details-of-a-case': provides_the_analysis_and_timeline_details_of_a_case_command,
             'abnormal-security-submit-inquiry-to-request-a-report-on-misjudgement':
                 submit_an_inquiry_to_request_a_report_on_misjudgement_by_abnormal_security_command,
         }
