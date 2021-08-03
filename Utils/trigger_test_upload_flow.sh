6d712cd554dced439730ea7055bdf85c10277db1
iyeshaya <iyeshaya@paloaltonetworks.com>
2021-08-03 18:59:59 +0300

    merged scripts

--- Utils/trigger_test_upload_flow.sh
+++ Utils/trigger_test_upload_flow.sh
@@ -1,111 +1,367 @@
 #!/usr/bin/env bash
 
-if [ "$#" -lt "1" ]; then
-  echo "Usage:
-  $0 -ct <token>
+# This script creates new branch with changes that will test the upload flow with given sdk and content branches.
+# Note: This script creates new remote branch, please delete the branch once the pipeline finished.
 
-  -ct, --ci-token             The ci token.
-  [-b, --branch]              The branch name. Default is the current branch.
-  [-gb, --bucket]             The name of the bucket to upload the packs to. Default is marketplace-dist-dev.
-  [-f, --force]               Whether to trigger the force upload flow.
-  [-p, --packs]               CSV list of pack IDs. Mandatory when the --force flag is on.
-  [-ch, --slack-channel]      A slack channel to send notifications to. Default is dmst-bucket-upload.
-  [-g, --gitlab]              Flag indicating to trigger the flow in GitLab.
-  "
+# fail
+# show fail message and quit
+# :param $1: message
+function fail() {
+  echo "$1"
+  git checkout "${content_branch_name}"
+  git branch -D "${new_content_branch}" # delete local branch
   exit 1
-fi
+}
 
-_branch="$(git branch  --show-current)"
-_bucket="marketplace-dist-dev"
-_bucket_upload="true"
-_slack_channel="dmst-bucket-upload"
+# check_arguments
+# Check if the given arguments are valid
+function check_arguments {
+  echo " Running - check_arguments"
 
-# Parsing the user inputs.
+  if [ -z "$content_branch_name" ]; then
+    content_branch_name=$(git branch --show-current)
+  fi
 
-while [[ "$#" -gt 0 ]]; do
-  case $1 in
+  if [ -z "$sdk_branch_name" ]; then
+    sdk_branch_name="master"
+  fi
 
-  -ct|--ci-token) _ci_token="$2"
-    shift
-    shift;;
+  if [ -z "$gitlab_token" ] && [ -z "$circle_token" ]; then
+    fail "At least one token [-gt, --gitlab-ci-token] or [-ct, --circle-ci-token] is required."
+  fi
 
-  -b|--branch) _branch="$2"
-    shift
-    shift;;
+  if [ -n "$force" ] && [ -z "$packs" ]; then
+    fail "You must provide a csv list of packs to force upload."
+  fi
+}
 
-  -gb|--bucket)
-  if [ "$(echo "$2" | tr '[:upper:]' '[:lower:]')" == "marketplace-dist" ]; then
-    echo "Only test buckets are allowed to use. Using marketplace-dist-dev instead."
+# copy_pack
+# Copies a pack and changing the name in the files to the new name.
+# :param $1: pack name
+# :param $2: copied pack name
+function create_new_pack {
+  echo " Running - create_new_pack"
+
+  if [ "$#" -ne 2 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local pack_name=$1
+  local new_pack_name=$2
+
+  original_path=$(pwd)
+  pack_path="${CONTENT_PATH}/Packs/${pack_name}"
+  new_pack_path="${CONTENT_PATH}/Packs/${new_pack_name}"
+
+  cp -R "${pack_path}" "${new_pack_path}" || fail
+  cd "${new_pack_path}" || fail
+
+  # rename inside yml and json files
+  find . -type f \( -name "*.json" -o -name "*.yml" \) -exec sed -i "" "s/${pack_name}/${new_pack_name}/g" {} \;
+
+  rename_files_and_folders "$pack_name" "$new_pack_name"
+
+  if [ "$pack_name" == "HelloWorld" ]; then
+    find . -type f \( -name "*.json" -o -name "*.yml" \) -exec sed -i "" "s/Hello_World/Hello_World_New/g" {} \;
+    rename_files_and_folders "Hello_World" "Hello_World_New"
+
+    find . -type f \( -name "*.json" -o -name "*.yml" \) -exec sed -i "" "s/Sanity Test/Sanity Test New/g" {} \;
+    rename_files_and_folders "Sanity Test" "Sanity Test New"
+  fi
+
+  cd "${original_path}" || fail
+  git add "$new_pack_path"
+
+  git commit -am  "Created new pack - $new_pack_name"
+}
+
+# rename_files_and_folders
+# Change all files and folder to the new name
+# :param $1: pack name
+# :param $2: new pack name
+function rename_files_and_folders {
+
+  if [ "$#" -ne 2 ]; then
+    fail " Illegal number of parameters "
+  fi
+  local pack_name=$1
+  local new_pack_name=$2
+
+  find . -type d -mindepth 1 -maxdepth 1 | \
+  while read -r folder;
+  do
+    cd "$folder" || continue ;
+    find . -type f -maxdepth 1 -name  "*${pack_name}*" -exec sh -c 'mv $1 "${1//$2/$3}"' sh {} "$pack_name" "$new_pack_name"  \;
+    rename_files_and_folders "$pack_name" "$new_pack_name";
+    cd ../;
+    if [ "$folder" != "${folder//$pack_name/$new_pack_name}" ]; then
+      mv "$folder" "${folder//$pack_name/$new_pack_name}"
+    fi
+  done
+
+}
+
+# add_dependency
+# Edits pack_metadata and adding Dependency to desired pack
+# :param $1: pack to add dependencies to
+# :param $2: pack to be depended on
+function add_dependency {
+  echo " Running - add_dependency"
+
+  if [ "$#" -ne 2 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local source_pack=$1
+  local pack_name=$2
+
+  pack_path="${CONTENT_PATH}/Packs/${source_pack}/pack_metadata.json"
+
+  sed -i "" "s/\"dependencies\": {/\"dependencies\": {\n\t\t\"${pack_name}\": {\n\t\t\t\"mandatory\": true,\n\t\t\t\"display_name\": \"${pack_name}\"\n\t\t},/g" "${pack_path}" || fail
+  git commit -am  "Added dependency for - $pack_name to $source_pack pack"
+
+}
+
+# add_author_image
+# Copies the author image from Base to desired pack
+# :param $1: pack to add author image to
+function add_author_image {
+  echo " Running - add_author_image"
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+
+  local pack_name=$1
+  cp "${CONTENT_PATH}/Packs/Base/Author_image.png" "${CONTENT_PATH}/Packs/${pack_name}" || fail
+
+  git add "${CONTENT_PATH}/Packs/${pack_name}/Author_image.png"
+  git commit -am  "Added author image for - $pack_name"
+
+}
+
+# add_1_0_0_release_note
+# add 1_0_0 release note for given pack by copying the last available release note
+# :param $1: pack name
+function add_1_0_0_release_note {
+  echo " Running - add_1_0_0_release_note"
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local pack_name=$1
+
+  cd "${CONTENT_PATH}/Packs/${pack_name}/ReleaseNotes" || fail
+  current_latest_note=$(ls -t | head -1)
+  cp "${current_latest_note}" 1_0_0.md
+  git add 1_0_0.md
+  cd "${CONTENT_PATH}" || fail
+
+  git commit -am  "Added release note 1_0_0.md to - $pack_name"
+
+}
+
+# change_sdk_requirements
+# changing the requirements file inorder to install the desired sdk branch
+# :param $1: sdk branch name
+# :param $2: requirements file
+function change_sdk_requirements {
+  echo " Running - change_sdk_requirements"
+
+  if [ "$#" -ne 2 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local sdk_branch=$1
+  local requirements_file_name=$2
+
+  sed -i "" "s#demisto-sdk.*#git+https://github.com/demisto/demisto-sdk.git@${sdk_branch}#g" "${requirements_file_name}"
+
+  git commit -am  "Change sdk in $requirements_file_name to be $sdk_branch"
+
+}
+
+# enhancement_release_notes
+# update release notes
+# :param $1: pack name
+function enhancement_release_notes {
+  echo " Running - enhancement_release_notes"
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local pack_name=$1
+  local pack_path="${CONTENT_PATH}/Packs/${pack_name}"
+  demisto-sdk update-release-notes -i "${pack_path}" --force --text "Adding release notes to check the upload flow"
+
+  git commit -am  "Added release note $pack_name"
+
+}
+
+# change_integration_image
+# Copies integration image from one to another
+# :param $1: source pack name
+# :param $2: dest pack name
+function change_integration_image {
+  echo " Running - change_integration_image"
+
+  if [ "$#" -ne 2 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local source_pack_name=$1
+  local dest_pack_name=$2
+
+  local source_integration_path="${CONTENT_PATH}/Packs/${source_pack_name}/Integrations/${source_pack_name}/${source_pack_name}_image.png"
+  local dest_integration_path="${CONTENT_PATH}/Packs/${dest_pack_name}/Integrations/${dest_pack_name}/${dest_pack_name}_image.png"
+  cp "${source_integration_path}" "${dest_integration_path}"
+
+  git commit -am  "Copied integration image from  $source_pack_name to $dest_pack_name"
+
+}
+
+# updating_old_release_notes
+# adding text to the latest release note in pack
+# :param $1: pack name
+function updating_old_release_notes {
+  echo " Running - updating_old_release_notes"
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local pack_name=$1
+
+  local path="${CONTENT_PATH}/Packs/${pack_name}/ReleaseNotes/"
+
+  cd "${path}" || fail
+  local current_latest_note=$(ls -t | head -1)
+  printf "\n#### Upload flow\n - Test\n" >>"${current_latest_note}"
+  cd "${CONTENT_PATH}" || return
+
+  git commit -am "Updated release note - $current_latest_note"
+
+}
+
+# set_pack_hidden
+# set pack as hidden
+# :param $1: pack name
+function set_pack_hidden {
+  echo " Running - set_pack_hidden "
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters"
+  fi
+
+  local pack_name=$1
+  local pack_metadata="${CONTENT_PATH}/Packs/${pack_name}/pack_metadata.json"
+  if grep "\"hidden\": true" "${pack_metadata}"; then
+    # pack is already hidden
+    return
+  elif grep "\"hidden\": false" "${pack_metadata}"; then
+    # pack set hidden to false
+    sed -i "" "s/\"hidden\": false/\"hidden\": true/g" "${pack_metadata}"
   else
-    _bucket=$2
-  fi
-    shift
-    shift;;
-
-  -f|--force) _force=true
-    _bucket_upload=""
-    shift;;
-
-  -p|--packs) _packs="$2"
-    shift
-    shift;;
-
-  -ch|--slack-channel) _slack_channel="$2"
-    shift
-    shift;;
-
-  -g|--gitlab) _gitlab=true
-    shift
-    shift;;
-
-  *)    # unknown option.
-    shift;;
-  esac
-done
-
-
-if [ -z "$_ci_token" ]; then
-    echo "You must provide a ci token."
-    exit 1
-fi
-
-if [ -n "$_force" ] && [ -z "$_packs" ]; then
-    echo "You must provide a csv list of packs to force upload."
-    exit 1
-fi
-
-if [ -n "$_gitlab" ]; then
-
-  _variables="variables[BUCKET_UPLOAD]=true"
-  if [ -n "$_force" ]; then
-    _variables="variables[FORCE_BUCKET_UPLOAD]=true"
+    # pack hidden key is missing
+    sed -i "" "s/{/{\n\t\"hidden\": true,\n/g" "${pack_metadata}"
   fi
 
-  source Utils/gitlab_triggers/trigger_build_url.sh
+  git commit -am "Set pack - $current_latest_note to be hidden"
 
-  curl --request POST \
-    --form token="${_ci_token}" \
-    --form ref="${_branch}" \
-    --form "${_variables}" \
-    --form "variables[SLACK_CHANNEL]=${_slack_channel}" \
-    --form "variables[PACKS_TO_UPLOAD]=${_packs}" \
-    --form "variables[GCS_MARKET_BUCKET]=${_bucket}" \
-    --form "variables[IFRA_ENV_TYPE]=Bucket-Upload" \
-    "$BUILD_TRIGGER_URL"
+}
 
-else
+# update_readme
+# Update readme file
+# :param $1: pack name
+function update_integration_readme {
+  echo " Running - update_integration_readme"
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
 
-  trigger_build_url="https://circleci.com/api/v2/project/github/demisto/content/pipeline"
+  local pack_name=$1
+
+  local readme_file="${CONTENT_PATH}/Packs/${pack_name}/Integrations/${pack_name}/README.md"
+
+  printf "\n#### Upload flow\n - Test\n" >>"${readme_file}"
+
+  git commit -am "Updated integration - $pack_name README.md file"
+
+}
+
+# update_pack_ignore
+# Update pack ignore file
+# :param $1: pack name
+function update_pack_ignore {
+  echo " Running - update_pack_ignore"
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local pack_name=$1
+
+  local pack_ignore_file="${CONTENT_PATH}/Packs/${pack_name}/.pack-ignore"
+
+  printf "\n[file:README.md]\nignore=RM104\n" >>"${pack_ignore_file}"
+
+  git commit -am "Updated to pack ignore - $pack_name"
+
+
+}
+
+# add_pack_to_landing_page
+# Add pack to the getting started landing page
+# :param $1: pack name
+function add_pack_to_landing_page {
+  echo " Running - add_pack_to_landing_page"
+
+  if [ "$#" -ne 1 ]; then
+    fail " Illegal number of parameters "
+  fi
+
+  local pack_name=$1
+  local json_file="${CONTENT_PATH}/Tests/Marketplace/landingPage_sections.json"
+
+  sed -i "" "s/\"Getting Started\":\[/\"Getting Started\":\[\n\"${pack_name}\",\n/g" "${json_file}" || fail
+  sed -i "" "s/\"Featured\":\[/\"Featured\":\[\n\"${pack_name}\",\n/g" "${json_file}" || fail
+
+  git commit -am "Added $pack_name to landing page"
+
+}
+
+# trigger_circle_ci
+# Trigger Circleci uploading packs workflow.
+# :param $1: The ci token for circle.
+# :param $2: Content branch to upload from.
+# :param $3: The name of the bucket to upload the packs to.
+# :param $4: Whether to trigger the force upload flow.
+# :param $5: CSV list of pack IDs.
+# :param $6: A slack channel to send notifications to.
+function trigger_circle_ci {
+
+  local $circle_token=$1
+  local content_branch=$2
+  local bucket=$3
+  local bucket_upload=$4
+  local force=$5
+  local packs=$6
+  local slack_channel=$7
+
+  local trigger_build_url="https://circleci.com/api/v2/project/github/demisto/content/pipeline"
 
   post_data=$(cat <<-EOF
   {
-    "branch": "${_branch}",
+    "branch": "${content_branch}",
     "parameters": {
-      "gcs_market_bucket": "${_bucket}",
-      "bucket_upload": "${_bucket_upload}",
-      "force_pack_upload": "${_force}",
-      "packs_to_upload": "${_packs}",
-      "slack_channel": "${_slack_channel}"
+      "gcs_market_bucket": "${bucket}",
+      "bucket_upload": "${bucket_upload}",
+      "force_pack_upload": "${force}",
+      "packs_to_upload": "${packs}",
+      "slack_channel": "${slack_channel}"
     }
   }
   EOF
@@ -117,5 +373,178 @@ else
   -k \
   --data "${post_data}" \
   --request POST ${trigger_build_url} \
-  --user "$_ci_token:"
+  --user "$circle_token:"
+}
+
+# trigger_gitlab_ci
+# Trigger GitLabci uploading packs workflow.
+# :param $1: The ci token for gitlab.
+# :param $2: Content branch to upload from.
+# :param $3: The name of the bucket to upload the packs to.
+# :param $5: CSV list of pack IDs.
+# :param $6: A slack channel to send notifications to.
+function trigger_gitlab_ci {
+
+  local gitlab_token=$1
+  local new_content_branch=$2
+  local bucket=$3
+  local packs=$4
+  local slack_channel=$5
+
+  local variables="variables[BUCKET_UPLOAD]=true"
+  if [ -n "$_force" ]; then
+    variables="variables[FORCE_BUCKET_UPLOAD]=true"
+  fi
+
+  source Utils/gitlab_triggers/trigger_build_url.sh
+
+  curl --request POST \
+    --form token="${gitlab_token}" \
+    --form ref="${new_content_branch}" \
+    --form "${variables}" \
+    --form "variables[SLACK_CHANNEL]=${slack_channel}" \
+    --form "variables[PACKS_TO_UPLOAD]=${packs}" \
+    --form "variables[GCS_MARKET_BUCKET]=${bucket}" \
+    --form "variables[IFRA_ENV_TYPE]=Bucket-Upload" \
+    "$BUILD_TRIGGER_URL"
+
+}
+
+
+# Define default arguments
+CONTENT_PATH="$HOME/dev/demisto/content"
+base_pack_name="HelloWorld"
+bucket="marketplace-dist-dev"
+bucket_upload="true"
+slack_channel="dmst-bucket-upload"
+
+# parse inputs
+if [ "$#" -lt "1" ]; then
+  fail "
+  [-sb, --sdk-branch]           The sdk branch name, if empty will run the version specified in the requirements file.
+  [-cb, --content-branch]       The content branch name, if empty will run on master branch.
+  [-gt, --gitlab-ci-token]      The ci token for gitlab, if provided wil run gitlab pipeline.
+  [-ct, --circle-ci-token]      The ci token for circle, if provided wil run circle pipeline.
+  [-p, --path]                  The path of content, default is ~/dev/demistio/content
+  [-gb, --bucket]               The name of the bucket to upload the packs to. Default is marketplace-dist-dev.
+  [-f, --force]                 Whether to trigger the force upload flow.
+  [-p, --packs]                 CSV list of pack IDs. Mandatory when the --force flag is on.
+  [-ch, --slack-channel]        A slack channel to send notifications to. Default is dmst-bucket-upload.
+  "
 fi
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+
+  -sb|--sdk-branch) sdk_branch_name="$2"
+    shift
+    shift;;
+
+  -cb|--content-branch) content_branch_name="$2"
+    shift
+    shift;;
+
+  -gt|--gitlab-ci-token) gitlab_token="$2"
+    shift
+    shift;;
+
+  -ct|--circle-ci-token) circle_token="$2"
+    shift
+    shift;;
+
+  -gb|--bucket)
+    if [ "$(echo "$2" | tr '[:upper:]' '[:lower:]')" == "marketplace-dist" ]; then
+      echo "Only test buckets are allowed to use. Using marketplace-dist-dev instead."
+    else
+      bucket=$2
+    fi
+    shift
+    shift;;
+
+  -f|--force) _force=true
+    bucket_upload=""
+    shift;;
+
+  -p|--packs) packs="$2"
+    shift
+    shift;;
+
+  -ch|--slack-channel) slack_channel="$2"
+    shift
+    shift;;
+
+  *)    # unknown option.
+    shift;;
+  esac
+done
+
+# Setup
+cd "${CONTENT_PATH}" || fail
+check_arguments
+
+new_content_branch="${sdk_branch_name}_${content_branch_name}_uploadFlow_test"
+new_pack_name="${base_pack_name}New"
+
+git checkout "$content_branch_name" || fail
+git pull || fail
+
+existed_in_remote=$(git ls-remote --heads origin "${new_content_branch}")
+existed_in_local=$(git branch --list "${new_content_branch}")
+
+# Deletes the remote branch if exists
+if [[ -z ${existed_in_remote} ]]; then
+  git push origin --delete "${new_content_branch}"
+fi
+# Deletes the local branch if exists
+if [[ -z ${existed_in_local} ]]; then
+  git branch -D "${new_content_branch}" # delete local branch
+fi
+
+git checkout -b "${new_content_branch}" || fail
+git commit -am "Initial commit"
+
+# Changes
+change_sdk_requirements "${sdk_branch_name}" "dev-requirements-py3.txt"
+
+# New Pack
+create_new_pack "${base_pack_name}" "${new_pack_name}"
+add_dependency "${new_pack_name}" "${base_pack_name}"
+add_author_image "${new_pack_name}"
+add_1_0_0_release_note "${new_pack_name}"
+
+## Existing pack
+enhancement_release_notes "${base_pack_name}"
+change_integration_image "PaloAltoNetworks_IoT" "${base_pack_name}"
+updating_old_release_notes "Base"
+enhancement_release_notes "Base"
+updating_old_release_notes "${base_pack_name}"
+add_1_0_0_release_note "${base_pack_name}"
+set_pack_hidden "Microsoft365Defender"
+updating_old_release_notes "${new_pack_name}" # Update release notes in content that are not in the bucket
+update_integration_readme "Microsoft365Defender"
+update_pack_ignore "Microsoft365Defender"
+
+# External changes
+add_pack_to_landing_page "${new_pack_name}"
+
+git commit -am "Adding changes"
+git push origin "${new_content_branch}"
+
+if [ -n "$circle_token" ]; then
+  trigger_circle_ci $new_content_branch $bucket $bucket_upload $force $packs $slack_channel
+fi
+
+if [ -n "$gitlab_token" ]; then
+  trigger_gitlab_ci $gitlab_token $new_content_branch $bucket $packs $slack_channel
+fi
+
+git checkout "${content_branch_name}"
+git branch -D "${new_content_branch}"
+
+if [ -d "$new_pack_path" ]; then
+  rm -r "$new_pack_path"
+fi
+
+echo ""
+echo "Please run the following commands once the pipelines are finished"
+echo "git push origin --delete ${new_content_branch}"
