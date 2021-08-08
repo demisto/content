499ff22eaec2e1ce604cc628335eed8e3edeadef
Dean Arbel <darbel@paloaltonetworks.com>
2021-08-08 17:08:49 +0300

    add file handling

--- Packs/DeveloperTools/Scripts/CompareIndicators/CompareIndicators.py
+++ Packs/DeveloperTools/Scripts/CompareIndicators/CompareIndicators.py
@@ -9,6 +9,18 @@ CIDR_RE = re.compile(ipv4cidrRegex)
 IP_RE = re.compile(ipv4Regex)
 
 
+def extract_list_from_args(args: dict, list_key: str) -> List[str]:
+    ioc_list = argToList(args.get(list_key, []))
+    try:
+        if len(ioc_list) == 1:
+            fp = demisto.getFilePath(ioc_list[0])
+            if isinstance(fp, dict) and 'path' in fp:
+                with open(fp['path']) as f:
+                    ioc_list = f.read().splitlines()
+    finally:
+        return ioc_list
+
+
 def ip_groups_to_ranges(ip_range_groups: Iterable) -> set:
     """Collapse ip groups to ranges.
 
@@ -42,13 +54,13 @@ def collect_ips(ioc_list: List[str]) -> Tuple[IPSet, set]:
             else:
                 non_ip_group.add(ioc)
         elif CIDR_RE.findall(ioc) or IP_RE.match(ioc):
-            ip_set.add(ioc)
+            ip_set.add(ioc.strip('\n'))
         else:
             non_ip_group.add(ioc)
     return ip_set, non_ip_group
 
 
-def collect_unique_indicators_from_lists(ioc_list_1: List[str], ioc_list_2: List[str]) -> Tuple[set, set]:
+def collect_unique_indicators_from_lists(ioc_list_1: List[str], ioc_list_2: List[str]) -> Tuple[list, list]:
     ip_set_1, non_ip_set_1 = collect_ips(ioc_list_1)
     ip_set_2, non_ip_set_2 = collect_ips(ioc_list_2)
     ip_diff1 = ip_set_1.difference(ip_set_2)
@@ -57,19 +69,20 @@ def collect_unique_indicators_from_lists(ioc_list_1: List[str], ioc_list_2: List
     diff2 = ip_groups_to_ranges(ip_diff2.iter_ipranges())
     diff1.update(non_ip_set_1.difference(non_ip_set_2))
     diff2.update(non_ip_set_2.difference(non_ip_set_1))
-    return diff1, diff2
+    return list(diff1), list(diff2)
 
 
 def main():
     args = demisto.args()
-    ioc_list1 = argToList(args.get('ioc_list1', []))
-    ioc_list2 = argToList(args.get('ioc_list2', []))
+    ioc_list1 = extract_list_from_args(args, 'base_list')
+    ioc_list2 = extract_list_from_args(args, 'compare_to_list')
     diff1, diff2 = collect_unique_indicators_from_lists(ioc_list1, ioc_list2)
     outputs = {
-        'UniqueIndicators1': list(diff1),
-        'UniqueIndicators2': list(diff2)
+        'BaseList': {'UniqueIndicators': diff1},
+        'CompareList': {'UniqueIndicators': diff2}
     }
-    return_results(CommandResults(outputs=outputs, outputs_key_field='Query', outputs_prefix='IndicatorsCheck'))
+    readable = tableToMarkdown("Differences", {'BaseList': diff1, 'CompareList': diff2})
+    return_results(CommandResults(outputs=outputs, outputs_prefix='IndicatorCompare', readable_output=readable))
 
 
 if __name__ in ['__main__', 'builtin', 'builtins']:
