f9ad5b8f386cd6ffcd3a99a32b7611b8f465327e
Dan <dansterenson@gmail.com>
2021-08-10 18:13:41 +0300

    XQL commands

--- Packs/CortexXDR/Integrations/XQLQueryingEngine/XQLQueryingEngine.py
+++ Packs/CortexXDR/Integrations/XQLQueryingEngine/XQLQueryingEngine.py
@@ -68,7 +68,8 @@ class Client(BaseClient):
         :rtype: ``str``
         """
 
-        res = self._http_request(method='POST', url_suffix='/xql/get_query_results_stream', json_data=data, resp_type='response')
+        res = self._http_request(method='POST', url_suffix='/xql/get_query_results_stream', json_data=data,
+                                 resp_type='response')
         return res.content
 
     def get_xql_quota(self, data: dict) -> str:
@@ -92,15 +93,99 @@ class Client(BaseClient):
 def init_commands() -> dict:
     return {
         'test-module': test_module,
-        'xdr-start-xql-query': start_xql_query_command,
+        'xdr-start-xql-query': start_xql_query_polling_command,
         'xdr-get-xql-query-results': get_xql_query_results_polling_command,
+        'xdr-get-xql-quota': get_xql_quota_command
     }
+
+
+def start_xql_query(client: Client, args: Dict[str, Any]) -> str:
+    query = args.get('query', '')
+    if not query:
+        raise ValueError('query is not specified')
+    if '//' in query:
+        raise DemistoException('Please remove notes (//) from query')
+    time_frame = args.get('time_frame', '')
+    tenant_ids = args.get('tenant_ids', [])
+    converted_time = arg_to_datetime(time_frame) if time_frame else None
+    data = {
+        'request_data': {
+            'query': query,
+            'tenants': tenant_ids,
+            'timeframe': time_frame
+        }
+    }
+    # Call the Client function and get the raw response
+    execution_id = client.start_xql_query(data)
+    return execution_id
+
+
+def get_xql_query_results(client: Client, args: dict) -> dict:
+    query_id = args.get('query_id')
+    if not query_id:
+        raise ValueError('query ID is not specified')
+    format_method = args.get('format', 'json')
+    data = {
+        'request_data': {
+            'query_id': query_id,
+            'pending_flag': True,
+            'format': format_method
+        }
+    }
+
+    # Call the Client function and get the raw response
+    response = client.get_xql_query_results(data)
+    response['ExecutionID'] = query_id
+    results = response.get('results', {})
+    stream_id = results.get('stream_id')
+    if stream_id:
+        return {"data": xdr_get_query_result_stream(client, stream_id)}
+    return response
+
+
+def xdr_get_query_result_stream(client: Client, stream_id: str) -> bytes:
+    if not stream_id:
+        raise ValueError('stream_id is not specified')
+    data = {
+        'request_data': {
+            'stream_id': stream_id,
+            'is_gzip_compressed': True,
+        }
+    }
+    # Call the Client function and get the raw response
+    return client.get_query_result_stream(data)
+
+
+def format_results(dict_to_format: dict, remove_empty_fields=True):
+    """
+    Recursively remove empty lists, empty dicts, or None elements from a dictionary.
+    :param d: Input dictionary.
+    :type d: dict
+    :return: Dictionary with all empty lists, and empty dictionaries removed.
+    :rtype: dict
+    """
+    new_dict = {}
+    for k, v in dict_to_format.items():
+        if isinstance(v, dict):
+            new_dict[k] = format_results(v, remove_empty_fields=remove_empty_fields)
+        else:
+            if remove_empty_fields and not v:
+                continue
+            elif v == 'FALSE':
+                new_dict[k] = False
+            elif v == 'TRUE':
+                new_dict[k] = True
+            elif v == 'NULL':
+                new_dict[k] = None
+            else:
+                new_dict[k] = v
+    return new_dict
 
 
 ''' COMMAND FUNCTIONS '''
 
 
-def test_module(client: Client,  args: Dict[str, Any]) -> str:
+def test_module(client: Client, args: Dict[str, Any]) -> str:
     """Tests API connectivity and authentication'
 
     Returning 'ok' indicates that the integration works like it is supposed to.
@@ -126,71 +211,20 @@ def test_module(client: Client,  args: Dict[str, Any]) -> str:
     return message
 
 
-def start_xql_query_command(client: Client, args: Dict[str, Any]) -> CommandResults:
-    query = stringEscape(args.get('query', ''))
-    if not query:
-        raise ValueError('query is not specified')
-    time_frame = args.get('time_frame', '')
-    tenant_ids = args.get('tenant_ids', [])
-    converted_time = arg_to_datetime(time_frame) if time_frame else None
-    data = {
-        'request_data': {
-            'query': query,
-            'tenants': tenant_ids,
-            'timeframe': converted_time
-        }
-    }
-    # Call the Client function and get the raw response
-    execution_id = client.start_xql_query(data)
+def get_xql_query_results_polling_command(client: Client, args: dict):
+    ScheduledCommand.raise_error_if_not_supported()
+    interval_in_secs = int(args.get('interval_in_seconds', 60))
+    outputs = get_xql_query_results(client, args)
 
-    return CommandResults(
-        outputs_prefix='PaloAltoNetworksXDR.XQL.Query',
-        outputs_key_field='ExecutionID',
-        outputs={
-            'ExecutionID': execution_id
-        },
-    )
-
-
-def get_xql_query_results_command(client: Client, args: Dict[str, Any]) -> CommandResults:
-    query_id = args.get('query_id')
-    if not query_id:
-        raise ValueError('query is not specified')
-    results_limit = int(args.get('limit', 100))
-    format_method = args.get('format', 'json')
-    data = {
-        'request_data': {
-            'query_id': query_id,
-            'pending_flag': True,
-            'limit': results_limit,
-            'format': format_method
-        }
-    }
-
-    # Call the Client function and get the raw response
-    response = client.get_xql_query_results(data)
-    stream_id = response.get('results', {}).get('stream_id')
-    if stream_id:
-        return xdr_get_query_result_stream(client, stream_id)
-    outputs = camelize(response, delim='_')
-    outputs['ExecutionID'] = query_id
-    return CommandResults(
+    command_results = CommandResults(
         outputs_prefix='PaloAltoNetworksXDR.XQL.Query',
         outputs_key_field='ExecutionID',
         outputs=outputs,
     )
 
-
-def get_xql_query_results_polling_command(client: Client, args: dict):
-    ScheduledCommand.raise_error_if_not_supported()
-    interval_in_secs = int(args.get('interval_in_seconds', 60))
-    command_results = get_xql_query_results_command(client, args)
-    outputs = command_results.outputs
-    return_warning('STATUS :' + outputs.get('Status'))
-    if outputs.get('Status') == 'PENDING':
+    if outputs.get('status') == 'PENDING':
         polling_args = {
             'interval_in_seconds': interval_in_secs,
-            'polling': True,
             **args
         }
         scheduled_command = ScheduledCommand(
@@ -199,31 +233,26 @@ def get_xql_query_results_polling_command(client: Client, args: dict):
             args=polling_args,
             timeout_in_seconds=600)
         command_results.scheduled_command = scheduled_command
+        command_results.readable_output = 'Query is still running, it may take a little while'
+        return command_results
+
+    # If true then the user did not specify in the query which fields to return, thus all fields are returned
+    # including empty fields so we need to clean them.
+    dict_to_format = command_results.outputs
+    command_results.outputs = format_results(dict_to_format, remove_empty_fields=True) \
+        if 'fields' not in args.get('query', '') else format_results(dict_to_format)
     return command_results
 
 
-def xdr_get_query_result_stream(client: Client, stream_id: str) -> CommandResults:
-
-    if not stream_id:
-        raise ValueError('stream_id is not specified')
-    data = {
-        'request_data': {
-            'stream_id': stream_id,
-            'is_gzip_compressed': True,
-        }
-    }
-    # Call the Client function and get the raw response
-    result = client.get_query_result_stream(data)
-
-    return CommandResults(
-        outputs_prefix='BaseIntegration',
-        outputs_key_field='',
-        outputs=result,
-    )
+def start_xql_query_polling_command(client: Client, args: dict):
+    execution_id = start_xql_query(client, args)
+    if not execution_id:
+        raise DemistoException('Failed to start query\n')
+    args['query_id'] = execution_id
+    return get_xql_query_results_polling_command(client, args)
 
 
-def xdr_get_xql_quota_command(client: Client, args: Dict[str, Any]) -> CommandResults:
-
+def get_xql_quota_command(client: Client, args: Dict[str, Any]) -> CommandResults:
     data = {
         'request_data': {
         }
@@ -247,6 +276,7 @@ def main() -> None:
     :return:
     :rtype:
     """
+    return_warning(demisto.args())
     params = demisto.params()
     api_key = params.get('apikey')
     api_key_id = params.get('apikey_id')
@@ -274,11 +304,10 @@ def main() -> None:
     # Log exceptions and return errors
     except Exception as e:
         demisto.error(traceback.format_exc())  # print the traceback
-        return_error(f'Failed to execute {command} command.\nError:\n{str(e)}')
+        return_error(f'Failed to execute {command} command.\nError: {str(e)}')
 
 
 ''' ENTRY POINT '''
-
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
