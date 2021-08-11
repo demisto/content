3a764e9107a4cd25b52b91674da5d4740096f073
rweinstock <rweinstock@paloaltonetworks.com>
2021-08-11 17:43:50 +0300

    Added unit tests and description

--- Packs/DeveloperTools/Integrations/CustomIndicatorDemo/Integrations/CustomIndicatorDemo/CustomIndicatorDemo_description.md
+++ Packs/DeveloperTools/Integrations/CustomIndicatorDemo/Integrations/CustomIndicatorDemo/CustomIndicatorDemo_description.md
@@ -1,7 +1,4 @@
-## BaseIntegration Help
+## Custom Indicator Demo Help
 
-Markdown file for integration configuration  help snippet. In this file add:
-- Brief information about how to retrieve the API key of your product
-- Other useful information on how to configure your integration in XSOAR
-
-Since this is a Markdown file, we encourage you to use MD formatting for sections, sub-sections, lists, etc.
+How to configure:
+- This is a dummy integration, therefore any dummy server URL and API key will suffice.
--- Packs/DeveloperTools/Integrations/CustomIndicatorDemo/Integrations/CustomIndicatorDemo/CustomIndicatorDemo_test.py
+++ Packs/DeveloperTools/Integrations/CustomIndicatorDemo/Integrations/CustomIndicatorDemo/CustomIndicatorDemo_test.py
@@ -1,42 +1,20 @@
-"""Base Integration for Cortex XSOAR - Unit Tests file
-
-Pytest Unit Tests: all funcion names must start with "test_"
-
-More details: https://xsoar.pan.dev/docs/integrations/unit-testing
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-You must add at least a Unit Test function for every XSOAR command
-you are implementing with your integration
-"""
-
-import json
-import io
+from CustomIndicatorDemo import Client, test_custom_indicator
 
 
-def util_load_json(path):
-    with io.open(path, mode='r', encoding='utf-8') as f:
-        return json.loads(f.read())
-
-
-# TODO: REMOVE the following dummy unit test function
-def test_baseintegration_dummy():
-    """Tests helloworld-say-hello command function.
-
-    Checks the output of the command function with the expected output.
-
-    No mock is needed here because the say_hello_command does not call
-    any external API.
+def test_custom_indicator_test():
     """
-    from BaseIntegration import Client, baseintegration_dummy_command
-
+    Given
+        - Dummy result
+    When
+        - dummy client is passed
+    Then
+        - return enriched indicator and result
+    """
     client = Client(base_url='some_mock_url', verify=False)
-    args = {
-        'dummy': 'this is a dummy response'
-    }
-    response = baseintegration_dummy_command(client, args)
+    res = test_custom_indicator(client)
+    indicator = res.indicator
+    assert indicator.data['param1'] == 'value1'
+    assert indicator.data['param2'] == 'value2'
+    assert indicator.value == 'custom_value'
+    assert res.outputs['dummy'] == 'test'
 
-    mock_response = util_load_json('test_data/baseintegration-dummy.json')
-
-    assert response.outputs == mock_response
-# TODO: ADD HERE unit tests for every command
