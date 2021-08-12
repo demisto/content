3932a26fe84cf9bce65dd14d75661071d67fc533
tneeman <tneeman@paloaltonetworks.com>
2021-08-12 13:42:36 +0300

    small modifications

--- Packs/MicrosoftTeams/Integrations/MicrosoftTeams/MicrosoftTeams.py
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeams/MicrosoftTeams.py
@@ -1159,7 +1159,7 @@ def send_message():
         # Got a notification from server
         channel_name = demisto.params().get('incident_notifications_channel', 'General')
         severity: int = int(demisto.args().get('severity'))
-        auto_notifications: bool = demisto.params().get('auto_notifications', False)
+        auto_notifications: bool = argToBoolean(demisto.params().get('auto_notifications') or True)
 
         if auto_notifications:
             severity_threshold: int = translate_severity(demisto.params().get('min_incident_severity', 'Low'))
