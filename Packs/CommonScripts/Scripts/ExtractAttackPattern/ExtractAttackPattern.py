33a07aaeffa2aef7212b795789c809d0e5c7abba
cshayner <cshayner@paloaltonetworks.com>
2021-08-17 09:58:29 +0300

    CR

--- Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern.py
+++ Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern.py
@@ -7,13 +7,13 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 
-def is_valid_attack_pattern(items) -> str:
+def is_valid_attack_pattern(items) -> list:
 
     try:
         results = demisto.executeCommand('mitre-get-indicator-name', {'attack_ids': items})
         list_contents = results[2]['Contents']
         values = [content.get('value') for content in list_contents]
-        return values if values else ''
+        return values if values else []
 
     except ValueError as e:
         if 'verify you have proper integration enabled to support it' in str(e):
@@ -22,10 +22,10 @@ def is_valid_attack_pattern(items) -> str:
                          'This Is needed in order to auto extract MITRE IDs and translate them to Attack Pattern IOCs')
         else:
             demisto.info(f'MITRE Attack formatting script, {str(e)}')
-        return ''
+        return []
     except Exception as e:
         demisto.info(f'MITRE Attack formatting script, {str(e)}')
-        return ''
+        return []
 
 
 def main():
@@ -39,6 +39,5 @@ def main():
         return_results('')
 
 
-# python2 uses __builtin__ python3 uses builtins
-if __name__ == "__builtin__" or __name__ == "builtins":
+if __name__ in ("__builtin__", "builtins"):
     main()
