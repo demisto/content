56683ce4d51c71c8648ac5a9a58a19d8e8ece6db
esharf <esahrf@paloaltonetworks.com>
2021-08-18 13:14:27 +0300

    final fix

--- Utils/upload_code_coverage_report.py
+++ Utils/upload_code_coverage_report.py
@@ -7,6 +7,9 @@ from typing import Dict
 from Tests.Marketplace.marketplace_services import init_storage_client
 
 
+TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+
 def create_minimal_report(source_file: str, destination_file: str):
     with open(source_file, 'r') as cov_util_output:
         data = json.load(cov_util_output)
@@ -14,32 +17,39 @@ def create_minimal_report(source_file: str, destination_file: str):
     # TODO Check that we were able to read the json report corretly
 
     minimal_coverage_contents_files: Dict[str, float] = {}
-    for current_file_name in data.get('files').keys():
+    for current_file_name in data['files'].keys():
         minimal_coverage_contents_files[current_file_name] = data['files'][current_file_name]['summary']['percent_covered']
-    minimal_coverage_contents: Dict[str, any] = {}
-    minimal_coverage_contents['files'] = minimal_coverage_contents_files
-    minimal_coverage_contents['last_updated'] = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    minimal_coverage_contents = {
+        'files': minimal_coverage_contents_files,
+        'last_updated': datetime.utcnow().strftime(TIME_FORMAT),
+        'total_coverage': data['totals']['percent_covered']
+    }
     with open(destination_file, 'w') as minimal_output:
         minimal_output.write(json.dumps(minimal_coverage_contents))
 
 
 def upload_file_to_google_cloud_storage(service_account: str,
                                         bucket_name: str,
-                                        source_file_name: str,
-                                        destination_blob_name: str,
+                                        minimal_file_name: str,
+                                        destination_blob_dir: str,
                                         ):
     """Uploads a file to the bucket."""
-
+    json_dest = '{}/coverage-min.json'.format(destination_blob_dir)
+    with open(minimal_file_name, 'r') as data_file:
+        updated = datetime.strptime(json.load(data_file)['last_updated'], TIME_FORMAT)
+    historic_data_dest = '{}/history/updated_{}.json'.format(destination_blob_dir, updated.strftime('%Y_%m_%d'))
     # google cloud storage client initialized
     storage_client = init_storage_client(service_account)
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    blob = bucket.blob(json_dest)
+    blob.upload_from_filename(minimal_file_name)
+    blob = bucket.blob(historic_data_dest)
+    blob.upload_from_filename(minimal_file_name)
 
-    blob.upload_from_filename(source_file_name)
-
+    destination_blob_name = ', '.join([json_dest, historic_data_dest])
     print(
         "File {} uploaded to {}.".format(
-            source_file_name, destination_blob_name
+            minimal_file_name, destination_blob_name
         )
     )
 
@@ -64,6 +74,7 @@ def options_handler():
                         required=True)
 
     parser.add_argument('-b', '--bucket_name',
+                        default="marketplace-dist-dev",
                         help=("Name of the bucket in Google Cloud Storage. "
                               "Default value is marketplace-dist-dev."),
                         required=False)
@@ -82,16 +93,14 @@ def options_handler():
                               "Default value is coverage_data.json."),
                         required=False)
 
-    parser.add_argument('-d', '--destination_blob_name',
-                        default='code-coverage/coverage_data.json',
+    parser.add_argument('-d', '--destination_blob_dir',
+                        default='code-coverage',
                         help=("Blob Name in Google Cloud Storage. "
-                              "Default value is code-coverage/coverage_data.json."),
+                              "Default value is code-coverage."),
                         required=False)
 
     parser.add_argument('-cov', '--cov_bin_dir',
                         default='code-coverage/coverage_data.json',
-                        help=("Blob Name in Google Cloud Storage. "
-                              "Default value is code-coverage/coverage_data.json."),
                         required=False)
 
     return parser.parse_args()
@@ -100,7 +109,9 @@ def options_handler():
 def coverage_json(cov_file, json_file):
     # this method will be removed when merge to sdk
     from coverage import Coverage
-    Coverage(data_file=cov_file).json_report(outfile=json_file)
+    cov = Coverage(data_file=cov_file, auto_data=False)
+    cov.load()
+    cov.json_report(outfile=json_file)
 
 
 def main():
@@ -113,8 +124,8 @@ def main():
 
     upload_file_to_google_cloud_storage(service_account=options.service_account,
                                         bucket_name=options.bucket_name,
-                                        source_file_name=options.source_file_name,
-                                        destination_blob_name=options.destination_blob_name,
+                                        minimal_file_name=options.minimal_file_name,
+                                        destination_blob_dir=options.destination_blob_dir,
                                         )
 
 
