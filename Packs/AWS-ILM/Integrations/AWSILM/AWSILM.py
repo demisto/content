675877b75de3450a7f107d376adbb75662d97a81
meichlerpanw <meichler@paloaltonetworks.com>
2021-08-19 11:32:35 +0300

    fix the update command

--- Packs/AWS-ILM/Integrations/AWSILM/AWSILM.py
+++ Packs/AWS-ILM/Integrations/AWSILM/AWSILM.py
@@ -22,9 +22,7 @@ class Client(BaseClient):
     def test(self):
         """ Tests connectivity with the application. """
 
-        uri = userUri
-
-        self._http_request(method='GET', url_suffix=uri)
+        self._http_request(method='GET', url_suffix=userUri)
 
     def get_user(self, email: str) -> Optional['IAMUserAppData']:
         """ Queries the user in the application using REST API by its email, and returns an IAMUserAppData object
@@ -36,14 +34,13 @@ class Client(BaseClient):
         :return: An IAMUserAppData object if user exists, None otherwise.
         :rtype: ``Optional[IAMUserAppData]``
         """
-        uri = userUri
         params = {
             'filter': f'userName eq "{email}"'  # I'm not sure email is always the user name.
         }
 
         res = self._http_request(
             method='GET',
-            url_suffix=uri,
+            url_suffix=userUri,
             params=params
         )
 
@@ -65,13 +62,11 @@ class Client(BaseClient):
         :return: An IAMUserAppData object that contains the data of the created user in the application.
         :rtype: ``IAMUserAppData``
         """
-        uri = userUri
-
         user_data['emails'] = [user_data['emails']]
 
         res = self._http_request(
             method='POST',
-            url_suffix=uri,
+            url_suffix=userUri,
             json_data=user_data
         )
 
@@ -93,15 +88,37 @@ class Client(BaseClient):
         :return: An IAMUserAppData object that contains the data of the updated user in the application.
         :rtype: ``IAMUserAppData``
         """
-        uri = userUri + user_id
+        old_user_data = self._http_request(
+            method='GET',
+            url_suffix=userUri + user_id
+        )
+
+        # The following fields are required for the PUT method
+        user_data['id'] = user_id
+        new_given_name = user_data.get('name', {}).get('givenName')
+        new_family_name = user_data.get('name', {}).get('familyName')
+        old_given_name = old_user_data.get('name', {}).get('givenName')
+        old_family_name = old_user_data.get('name', {}).get('familyName')
+        user_name = {
+            'givenName': new_given_name if new_family_name else old_given_name,
+            'familyName': new_family_name if new_family_name else old_family_name
+        }
+        user_data['name'] = user_name
+        if not user_data.get('displayName'):
+            user_data['displayName'] = old_user_data.get('displayName')
+        if not user_data.get('userName'):
+            user_data['userName'] = old_user_data.get('userName')
+
+        # The 'emails' value type is a list
+        if user_data.get('emails'):
+            user_data['emails'] = [user_data['emails']]
 
         res = self._http_request(
-            method='PATCH',
-            url_suffix=uri,
+            method='PUT',
+            url_suffix=userUri + user_id,
             json_data=user_data
         )
 
-        user_id = res.get('id')
         is_active = res.get('active')
         username = res.get('userName')
 
@@ -176,6 +193,57 @@ class Client(BaseClient):
         username = res.get('userName')
 
         return IAMUserAppData(user_id, username, is_active, res)
+
+    def http_request(self, method, url_suffix, full_url=None, params=None, data=None, headers=None):
+        if headers is None:
+            headers = self._headers
+        full_url = full_url if full_url else urljoin(self._base_url, url_suffix)
+
+        res = requests.request(
+            method,
+            full_url,
+            verify=self._verify,
+            headers=headers,
+            params=params,
+            json=data
+        )
+        return res
+
+    def get_group_by_id(self, group_id):
+        return self.http_request(
+            method='GET',
+            url_suffix=groupUri + group_id
+        )
+
+    def search_group(self, group_name):
+        params = {
+            'filter': f'displayName eq "{group_name}"'
+        }
+        return self.http_request(
+            method="GET",
+            url_suffix=groupUri,
+            params=params
+        )
+
+    def create_group(self, data):
+        return self.http_request(
+            method="POST",
+            url_suffix=groupUri,
+            data=data
+        )
+
+    def update_group(self, group_id, data):
+        return self.http_request(
+            method="PATCH",
+            url_suffix=groupUri + group_id,
+            data=data
+        )
+
+    def delete_group(self, group_id):
+        return self.http_request(
+            method="DELETE",
+            url_suffix=groupUri + group_id
+        )
 
     def get_app_fields(self) -> Dict[str, Any]:
         """ Gets a dictionary of the user schema fields in the application and their description.
@@ -260,6 +328,50 @@ def get_error_details(res: Dict[str, Any]) -> str:
     return f'{message}: {details}'
 
 
+class OutputContext:
+    """
+        Class to build a generic output and context.
+    """
+
+    def __init__(self, success=None, active=None, id=None, iden=None, username=None, email=None, errorCode=None,
+                 errorMessage=None, details=None, displayName=None, members=None):
+        self.instanceName = demisto.callingContext['context']['IntegrationInstance']
+        self.brand = demisto.callingContext['context']['IntegrationBrand']
+        self.command = demisto.command().replace('-', '_').title().replace('_', '')
+        self.success = success
+        self.active = active
+        self.id = id
+        self.iden = iden
+        self.username = username
+        self.email = email
+        self.errorCode = errorCode
+        self.errorMessage = errorMessage
+        self.details = details
+        self.displayName = displayName  # Used in group
+        self.members = members  # Used in group
+        self.data = {
+            "brand": self.brand,
+            "instanceName": self.instanceName,
+            "success": success,
+            "active": active,
+            "id": iden,
+            "username": username,
+            "email": email,
+            "errorCode": errorCode,
+            "errorMessage": errorMessage,
+            "details": details,
+            "displayName": displayName,
+            "members": members
+        }
+
+
+def verify_and_load_scim_data(scim):
+    scim = json.loads(scim)
+    if type(scim) != dict:
+        raise Exception("SCIM data is not a valid JSON")
+    return scim
+
+
 '''COMMAND FUNCTIONS'''
 
 
@@ -268,6 +380,244 @@ def test_module(client: Client):
 
     client.test()
     return_results('ok')
+
+
+def get_group_command(client, args):
+    scim = verify_and_load_scim_data(args.get('scim'))
+
+    group_id = scim.get('id')
+    group_name = scim.get('displayName')
+
+    if not (group_id or group_name):
+        return_error("You must supply either 'id' or 'displayName' in the scim data")
+
+    if not group_id:
+        res = client.search_group(group_name)
+        res_json = res.json()
+
+        if res.status_code == 200:
+            if res_json.get('totalResults') < 1:
+                generic_iam_context = OutputContext(success=False, displayName=group_name, errorCode=404,
+                                                    errorMessage="Group Not Found", details=res_json)
+
+                readable_output = tableToMarkdown(f'AWS Get Group:', generic_iam_context.data, removeNull=True)
+
+                return CommandResults(
+                    raw_response=generic_iam_context.data,
+                    outputs_prefix=generic_iam_context.command,
+                    outputs_key_field='id',
+                    outputs=generic_iam_context.data,
+                    readable_output=readable_output
+                )
+
+            else:
+                group_id = res_json['Resources'][0].get('id')
+                group_name = res_json['Resources'][0].get('displayName')
+                generic_iam_context = OutputContext(success=True, iden=group_id,
+                                                    displayName=group_name, details=res_json)
+                readable_output = tableToMarkdown(f'AWS Get Group:', generic_iam_context.data, removeNull=True)
+
+                return CommandResults(
+                    raw_response=generic_iam_context.data,
+                    outputs_prefix=generic_iam_context.command,
+                    outputs_key_field='id',
+                    outputs=generic_iam_context.data,
+                    readable_output=readable_output
+                )
+
+        else:
+            generic_iam_context = OutputContext(success=False, displayName=group_name, iden=group_id,
+                                                errorCode=res_json.get('code'),
+                                                errorMessage=res_json.get('message'), details=res_json)
+
+            readable_output = tableToMarkdown(f'AWS Get Group:', generic_iam_context.data, removeNull=True)
+
+            return CommandResults(
+                raw_response=generic_iam_context.data,
+                outputs_prefix=generic_iam_context.command,
+                outputs_key_field='id',
+                outputs=generic_iam_context.data,
+                readable_output=readable_output
+            )
+
+    res = client.get_group_by_id(group_id)
+    res_json = res.json()
+
+    if res.status_code == 200:
+        generic_iam_context = OutputContext(success=True, iden=res_json.get('id'),
+                                            displayName=res_json.get('displayName'), details=res_json)
+    elif res.status_code == 404:
+        generic_iam_context = OutputContext(success=False, displayName=group_name, iden=group_id, errorCode=404,
+                                            errorMessage="Group Not Found", details=res_json)
+    else:
+        generic_iam_context = OutputContext(success=False, displayName=group_name, iden=group_id,
+                                            errorCode=res_json.get('code'),
+                                            errorMessage=res_json.get('message'), details=res_json)
+
+    readable_output = tableToMarkdown(f'AWS Get Group:', generic_iam_context.data, removeNull=True)
+
+    return CommandResults(
+        raw_response=generic_iam_context.data,
+        outputs_prefix=generic_iam_context.command,
+        outputs_key_field='id',
+        outputs=generic_iam_context.data,
+        readable_output=readable_output
+    )
+
+
+def create_group_command(client, args):
+    scim = verify_and_load_scim_data(args.get('scim'))
+    group_name = scim.get('displayName')
+
+    if not group_name:
+        return_error("You must supply 'displayName' of the group in the scim data")
+
+    res = client.create_group(scim)
+    res_json = res.json()
+
+    if res.status_code == 201:
+
+        generic_iam_context = OutputContext(success=True, iden=res_json.get('id'),
+                                            displayName=res_json.get('displayName'), details=res_json)
+    else:
+        res_json = res.json()
+        generic_iam_context = OutputContext(success=False, displayName=group_name,
+                                            errorCode=res_json.get('code'),
+                                            errorMessage=res_json.get('message'), details=res_json)
+
+    readable_output = tableToMarkdown(f'AWS Create Group:', generic_iam_context.data, removeNull=True)
+
+    return CommandResults(
+        raw_response=generic_iam_context.data,
+        outputs_prefix=generic_iam_context.command,
+        outputs_key_field='id',
+        outputs=generic_iam_context.data,
+        readable_output=readable_output
+    )
+
+
+def update_group_command(client, args):
+    scim = verify_and_load_scim_data(args.get('scim'))
+
+    group_id = scim.get('id')
+    group_name = scim.get('displayName')
+
+    if not group_id:
+        return_error("You must supply 'id' in the scim data")
+
+    member_ids_to_add = args.get('memberIdsToAdd')
+    member_ids_to_delete = args.get('memberIdsToDelete')
+
+    if member_ids_to_add:
+        if type(member_ids_to_add) != list:
+            member_ids_to_add = json.loads(member_ids_to_add)
+
+        for member_id in member_ids_to_add:
+            operation = {
+                "op": "add",
+                "path": "members",
+                "value": [{"value": member_id}]
+            }
+            group_input = {'schemas': [patchSchema], 'Operations': [operation]}
+            res = client.update_group(group_id, group_input)
+            if res.status_code != 204:
+                res_json = res.json()
+                generic_iam_context = OutputContext(success=False, displayName=group_name, iden=member_id,
+                                                    errorCode=res_json.get('code'),
+                                                    errorMessage=res_json.get('message'), details=res_json)
+
+                readable_output = tableToMarkdown(f'AWS Update Group:', generic_iam_context.data, removeNull=True)
+
+                return CommandResults(
+                    raw_response=generic_iam_context.data,
+                    outputs_prefix=generic_iam_context.command,
+                    outputs_key_field='id',
+                    outputs=generic_iam_context.data,
+                    readable_output=readable_output
+                )
+
+    if member_ids_to_delete:
+        if type(member_ids_to_delete) is not list:
+            member_ids_to_delete = json.loads(member_ids_to_delete)
+        for member_id in member_ids_to_delete:
+            operation = {
+                "op": "remove",
+                "path": "members",
+                "value": [{"value": member_id}]
+            }
+            group_input = {'schemas': [patchSchema], 'Operations': [operation]}
+            res = client.update_group(group_id, group_input)
+            if res.status_code != 204:
+                res_json = res.json()
+                generic_iam_context = OutputContext(success=False, displayName=group_name, iden=member_id,
+                                                    errorCode=res_json.get('code'),
+                                                    errorMessage=res_json.get('message'), details=res_json)
+
+                readable_output = tableToMarkdown(f'AWS Update Group:', generic_iam_context.data, removeNull=True)
+
+                return CommandResults(
+                    raw_response=generic_iam_context.data,
+                    outputs_prefix=generic_iam_context.command,
+                    outputs_key_field='id',
+                    outputs=generic_iam_context.data,
+                    readable_output=readable_output
+                )
+
+    if res.status_code == 204:
+        res_json = res.headers
+        generic_iam_context = OutputContext(success=True, iden=group_id, displayName=group_name, details=str(res_json))
+    elif res.status_code == 404:
+        res_json = res.json()
+        generic_iam_context = OutputContext(success=False, iden=group_id, displayName=group_name, errorCode=404,
+                                            errorMessage="Group/User Not Found or User not a member of group",
+                                            details=res_json)
+    else:
+        res_json = res.json()
+        generic_iam_context = OutputContext(success=False, iden=group_id, displayName=group_name,
+                                            errorCode=res_json.get('code'), errorMessage=res_json.get('message'),
+                                            details=res_json)
+
+    readable_output = tableToMarkdown(f'AWS Update Group:', generic_iam_context.data, removeNull=True)
+
+    return CommandResults(
+        raw_response=generic_iam_context.data,
+        outputs_prefix=generic_iam_context.command,
+        outputs_key_field='id',
+        outputs=generic_iam_context.data,
+        readable_output=readable_output
+    )
+
+
+def delete_group_command(client, args):
+    scim = verify_and_load_scim_data(args.get('scim'))
+    group_id = scim.get('id')
+    group_name = scim.get('displayName')
+
+    if not group_id:
+        return_error("The group id needs to be provided.")
+
+    res = client.delete_group(group_id)
+    if res.status_code == 204:
+        res_json = res.headers
+        generic_iam_context = OutputContext(success=True, iden=group_id, displayName=group_name, details=str(res_json))
+    elif res.status_code == 404:
+        generic_iam_context = OutputContext(success=False, iden=group_id, displayName=group_name, errorCode=404,
+                                            errorMessage="Group Not Found", details=res.json())
+    else:
+        res_json = res.json()
+        generic_iam_context = OutputContext(success=False, displayName=group_name, iden=group_id,
+                                            errorCode=res_json.get('code'), errorMessage=res_json.get('message'),
+                                            details=res_json)
+
+    readable_output = tableToMarkdown(f'AWS Delete Group:', generic_iam_context.data, removeNull=True)
+
+    return CommandResults(
+        raw_response=generic_iam_context.data,
+        outputs_prefix=generic_iam_context.command,
+        outputs_key_field='id',
+        outputs=generic_iam_context.data,
+        readable_output=readable_output
+    )
 
 
 def get_mapping_fields(client: Client) -> GetMappingFieldsResponse:
@@ -338,14 +688,26 @@ def main():
     elif command == 'iam-disable-user':
         user_profile = iam_command.disable_user(client, args)
 
+    '''non-CRUD commands'''
+
     if user_profile:
         return_results(user_profile)
-
-    '''non-CRUD commands'''
 
     try:
         if command == 'test-module':
             test_module(client)
+
+        elif command == 'iam-get-group':
+            return_results(get_group_command(client, args))
+
+        elif command == 'iam-create-group':
+            return_results(create_group_command(client, args))
+
+        elif command == 'iam-update-group':
+            return_results(update_group_command(client, args))
+
+        elif command == 'iam-delete-group':
+            return_results(delete_group_command(client, args))
 
         elif command == 'get-mapping-fields':
             return_results(get_mapping_fields(client))
