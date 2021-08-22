028f1cee077dac2b6b6d7925c47ac7a7ff391e19
nmaimon <nmaimon@paloaltonetworks.com>
2021-08-22 18:14:09 +0300

    testing

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -567,7 +567,7 @@ class Pack(object):
             dict: parsed pack metadata.
 
         """
-        pack_metadata = {  #T ODO: transfer to constants
+        pack_metadata = {
             'name': self._display_name or self._pack_name,
             'id': self._pack_name,
             'description': self._description or self._pack_name,
