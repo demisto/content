492c7f5f3128da752deb91785914e6c9040ecec8
merit <meretmaayta@gmail.com>
2021-08-22 16:52:20 +0300

    added RN

--- Packs/AutoFocus/Integrations/AutofocusV2/AutofocusV2.py
+++ Packs/AutoFocus/Integrations/AutofocusV2/AutofocusV2.py
@@ -1120,13 +1120,13 @@ def resolve_ip_address(ip):
 
 def convert_url_to_ascii_character(url_name):
     regex_groups = re.search('([a-zA-Z\W]*)([^a-zA-Z\W]*)([a-zA-Z\W]*)', url_name)
-
-    if regex_groups.group(2) != '':
-        decoded_value = str(regex_groups.group(1)) + str(regex_groups.group(2)).encode('idna').decode(
-            "utf-8") + regex_groups.group(3)
-        return decoded_value
-    else:
-        return url_name
+    if regex_groups:
+        if regex_groups.group(2) != '':
+            decoded_value = str(regex_groups.group(1)) + str(regex_groups.group(2)).encode('idna').decode(
+                "utf-8") + regex_groups.group(3)
+            return decoded_value
+        else:
+            return url_name
 
 
 ''' COMMANDS'''
