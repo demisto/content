481d60948962874a7ef935fd5a528cc4b440c970
ypreisler <ypreisler@paloaltonetworks.com>
2021-08-22 21:45:16 +0300

    lint fix

--- Packs/Campaign/Scripts/ShowCampaignIncidentsOwners/ShowCampaignIncidentsOwners.py
+++ Packs/Campaign/Scripts/ShowCampaignIncidentsOwners/ShowCampaignIncidentsOwners.py
@@ -2,9 +2,6 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
-from typing import Dict, Any
-import traceback
-
 
 def get_incident_ids():
 
@@ -23,24 +20,26 @@ def get_incident_owners(incident_ids):
 
 
 def main():
+    try:
+        incident_ids = get_incident_ids()
+        incident_owners = get_incident_owners(incident_ids)
 
-    incident_ids = get_incident_ids()
-    incident_owners = get_incident_owners(incident_ids)
+        if incident_owners:
+            html_readable_output = f"<div style='text-align:center; font-size:17px; padding: 5px;'> Incident Owners" \
+                                   f"</br> <div style='font-size:32px;'> {len(incident_owners)} </div>" \
+                                   f" <div style='font-size:16px;'> {', '.join(incident_owners)} </div></div>"
 
-    if incident_owners:
-        html_readable_output = f"<div style='text-align:center; font-size:17px; padding: 5px;'> Incident Owners</br>" \
-                               f" <div style='font-size:32px;'> {len(incident_owners)} </div>" \
-                               f" <div style='font-size:16px;'> {', '.join(incident_owners)} </div></div>"
+        else:
+            html_readable_output = "<div style='text-align:center; font-size:17px; padding: 15px;'> Incident Owners" \
+                                   "</br> <div style='font-size:32px;'> Owners Not Found </div></div>"
 
-    else:
-        html_readable_output = "<div style='text-align:center; font-size:17px; padding: 15px;'> Incident Owners</br>" \
-                               " <div style='font-size:32px;'> Owners Not Found </div></div>"
-
-    demisto.results({
-        'ContentsFormat': formats['html'],
-        'Type': entryTypes['note'],
-        'Contents': html_readable_output
-    })
+        demisto.results({
+            'ContentsFormat': formats['html'],
+            'Type': entryTypes['note'],
+            'Contents': html_readable_output
+        })
+    except Exception as err:
+        return_error(str(err))
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
