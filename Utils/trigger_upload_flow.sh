26f5a8ee8003b43ef9e59480c1df0393257541c2
iyeshaya <iyeshaya@paloaltonetworks.com>
2021-08-22 19:35:23 +0300

    removed copied pack if exists

--- Utils/trigger_upload_flow.sh
+++ Utils/trigger_upload_flow.sh
@@ -66,6 +66,7 @@ function create_new_pack {
   local pack_path="${CONTENT_PATH}/Packs/${pack_name}"
   local new_pack_path="${CONTENT_PATH}/Packs/${new_pack_name}"
 
+  rm -rf "${new_pack_path}"
   cp -R "${pack_path}" "${new_pack_path}" || fail
   cd "${new_pack_path}" || fail
 
