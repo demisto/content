34da5faaca471c4f6053f0d8af566810e118abfc
adaud <adaud@paloaltonetworks.com>
2021-08-23 15:21:47 +0300

    validate demisto version is correct before using noUpdate arg

--- Packs/ApiModules/Scripts/JSONFeedApiModule/JSONFeedApiModule.py
+++ Packs/ApiModules/Scripts/JSONFeedApiModule/JSONFeedApiModule.py
@@ -375,11 +375,25 @@ def feed_main(params, feed_name, prefix):
             create_relationships = params.get('create_relationships')
             indicators, no_update = fetch_indicators_command(client, indicator_type, feedTags, auto_detect,
                                                              create_relationships)
-            if not len(indicators):
-                demisto.createIndicators(indicators, noUpdate=no_update)
+
+            # get demisto version
+            demisto_version = get_demisto_version().get('version', '5.5.0')
+
+            # check if the version is higher than 6.5.0 so we can use noUpdate parameter
+            if LooseVersion(demisto_version) >= LooseVersion('6.5.0'):
+                if not len(indicators):
+                    demisto.createIndicators(indicators, noUpdate=no_update)
+                else:
+                    for b in batch(indicators, batch_size=2000):
+                        demisto.createIndicators(b, noUpdate=no_update)
+
             else:
-                for b in batch(indicators, batch_size=2000):
-                    demisto.createIndicators(b, noUpdate=no_update)
+                # call createIndicators without noUpdate arg
+                if not len(indicators):
+                    demisto.createIndicators(indicators)
+                else:
+                    for b in batch(indicators, batch_size=2000):
+                        demisto.createIndicators(b)
 
         elif command == f'{prefix}get-indicators':
             # dummy command for testing
