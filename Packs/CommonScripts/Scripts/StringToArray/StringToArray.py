2dc4cef82aa7bae5aa4a727cc1e4040ee9c4bd3b
sberman <sberman@paloaltonetworks.com>
2021-08-25 15:16:44 +0300

    Added new transformer script

---
+++ Packs/CommonScripts/Scripts/StringToArray/StringToArray.py
@@ -0,0 +1,14 @@
+import json
+
+
+def main(args):
+    value = args.get('value')
+    json_value = [value]
+    return json.dumps(json_value)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    try:
+        return_results(main(demisto.args()))
+    except Exception as exc:
+        return_error(str(exc), error=exc)
