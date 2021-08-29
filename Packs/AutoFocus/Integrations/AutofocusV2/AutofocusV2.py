25251b72efbeea2aa1de497a10b7fd5a4c5262cc
merit <meretmaayta@gmail.com>
2021-08-29 13:36:23 +0300

    edit

--- Packs/AutoFocus/Integrations/AutofocusV2/AutofocusV2.py
+++ Packs/AutoFocus/Integrations/AutofocusV2/AutofocusV2.py
@@ -1524,9 +1524,9 @@ def search_url_command(url, reliability, create_relationships):
     relationships = []
 
     for url_name in url_list:
-        url_name = convert_url_to_ascii_character(url_name)
+        # url_name = convert_url_to_ascii_character(url_name)
 
-        raw_res = search_indicator('url', url_name)
+        raw_res = search_indicator('url', convert_url_to_ascii_character(url_name))
         if not raw_res.get('indicator'):
             raise ValueError('Invalid response for indicator')
 
