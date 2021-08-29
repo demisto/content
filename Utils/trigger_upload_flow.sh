b2e431481f512755c61d8dab17aa9b9e9b9623b6
iyeshaya <iyeshaya@paloaltonetworks.com>
2021-08-29 15:35:41 +0300

    fix force flag issue

--- Utils/trigger_upload_flow.sh
+++ Utils/trigger_upload_flow.sh
@@ -22,7 +22,7 @@ function fail {
 
 # check_arguments
 # Check if the given arguments are valid
-function check_arguments {
+function check_aarguments {
   echo " Running - check_arguments"
 
   if [ -z "$content_branch_name" ]; then
@@ -33,6 +33,15 @@ function check_arguments {
     fail "At least one token [-gt, --gitlab-ci-token] or [-ct, --circle-ci-token] is required." "skip"
   fi
 
+  if [ -n "$production" ]; then
+    if [ -n "$force" ] || [ -n "$packs" ]; then
+      echo "Ignoring force and packs arguments."
+      packs=
+      force=
+    fi
+    echo "Uploading all the changed packs to production."
+  fi
+
   if [ -n "$force" ] && [ -z "$packs" ]; then
     fail "You must provide a csv list of packs to force upload." "skip"
   fi
@@ -41,9 +50,6 @@ function check_arguments {
     fail "Only test buckets are allowed to use. Using marketplace-dist-dev instead."
   fi
 
-  if [ -n "$production" ]; then
-    echo "Uploading to production bucket - ${production}."
-  fi
 }
 
 # copy_pack
@@ -496,8 +502,8 @@ cd "${CONTENT_PATH}" || fail
 
 check_arguments
 
-# If production flag is set - upload master branch
-if [ -n "$production" ]; then
+# If production or force flag is set - upload master branch
+if [ -n "$production" ] || [ -n "$force"]; then
   slack_channel="dmst-content-team"
   bucket="marketplace-dist"
 
