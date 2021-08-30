bb8ecd4c589f3f3baf7bee0f6d811bf2bab0825a
teizenman <teizenman@paloaltonetworks.com>
2021-08-30 15:44:27 +0300

    auth

---
+++ Packs/Microsoft365Defender/Integrations/O365ATPSafeLinks/O365ATPSafeLinks.ps1
@@ -0,0 +1,138 @@
+Import-Module ExchangeOnlineManagement
+
+
+$COMMAND_PREFIX = "o365-atp-safelinks"
+$INTEGRATION_ENTRY_CONTEXT = "O365ATP.SafeLinks"
+
+
+class ExchangeOnlinePowershellV2Client {
+    [string]$url
+    [System.Security.Cryptography.X509Certificates.X509Certificate2]$certificate
+    [string]$organization
+    [string]$app_id
+    [SecureString]$password
+    ExchangeOnlinePowershellV2Client(
+        [string]$url,
+        [string]$app_id,
+        [string]$organization,
+        [string]$certificate,
+        [SecureString]$password
+    ) {
+        $this.url = $url
+        try {
+            $ByteArray = [System.Convert]::FromBase64String($certificate)
+        }
+        catch {
+            throw "Could not decode the certificate. Try to re-enter it"
+        }
+        $this.certificate = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($ByteArray, $password)
+
+        $this.organization = $organization
+        $this.app_id = $app_id
+    }
+    CreateSession() {
+        $cmd_params = @{
+            "AppID"        = $this.app_id
+            "Organization" = $this.organization
+            "Certificate"  = $this.certificate
+        }
+        Connect-ExchangeOnline @cmd_params -ShowBanner:$false -SkipImportSession -WarningAction:SilentlyContinue | Out-Null
+    }
+    DisconnectSession() {
+        Disconnect-ExchangeOnline -Confirm:$false -WarningAction:SilentlyContinue | Out-Null
+    }
+}
+
+
+function TestModuleCommand($client)
+{
+    try
+    {
+        $client.CreateSession()
+        $demisto.results("ok")
+    }
+    finally
+    {
+        $client.DisconnectSession()
+    }
+
+}
+
+function Main {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
+    param()
+    $command = $demisto.GetCommand()
+    $command_arguments = $demisto.Args()
+    $integration_params = [Hashtable] $demisto.Params()
+
+    if ($integration_params.certificate.password) {
+        $password = ConvertTo-SecureString $integration_params.certificate.password -AsPlainText -Force
+    }
+    else {
+        $password = $null
+    }
+
+    $exo_client = [ExchangeOnlinePowershellV2Client]::new(
+        $integration_params.url,
+        $integration_params.app_id,
+        $integration_params.organization,
+        $integration_params.certificate.identifier,
+        $password
+    )
+    try {
+        # Executing command
+        $Demisto.Debug("Command being called is $Command")
+        switch ($command) {
+            "test-module" {
+                ($human_readable, $entry_context, $raw_response) = TestModuleCommand $exo_client
+            }
+            "$script:COMMAND_PREFIX-cas-mailbox-list" {
+                ($human_readable, $entry_context, $raw_response) = GetEXOCASMailboxCommand $exo_client $command_arguments
+            }
+            "$script:COMMAND_PREFIX-mailbox-list" {
+                ($human_readable, $entry_context, $raw_response) = GetEXOMailBoxCommand $exo_client $command_arguments
+            }
+            "$script:COMMAND_PREFIX-mailbox-permission-list" {
+                ($human_readable, $entry_context, $raw_response) = GetEXOMailBoxPermissionCommand $exo_client $command_arguments
+            }
+            "$script:COMMAND_PREFIX-recipient-permission-list" {
+                ($human_readable, $entry_context, $raw_response) = GetEXORecipientPermissionCommand $exo_client $command_arguments
+            }
+            "$script:COMMAND_PREFIX-recipient-list" {
+                ($human_readable, $entry_context, $raw_response) = GetEXORecipientCommand $exo_client $command_arguments
+            }
+            default {
+                ReturnError "Could not recognize $command"
+            }
+        }
+        # Return results to Demisto Server
+        ReturnOutputs $human_readable $entry_context $raw_response | Out-Null
+    }
+    catch {
+        $Demisto.debug(
+            "Integration: $script:INTEGRATION_NAME
+        Command: $command
+        Arguments: $( $command_arguments | ConvertTo-Json )
+        Error: $( $_.Exception.Message )"
+        )
+        if ($command -ne "test-module") {
+            ReturnError "Error:
+            Integration: $script:INTEGRATION_NAME
+            Command: $command
+            Arguments: $( $command_arguments | ConvertTo-Json )
+            Error: $( $_.Exception )"
+        }
+        else {
+            ReturnError $_.Exception.Message
+        }
+    }
+    finally {
+        # Always disconnect the session, even if no sessions available.
+        $exo_client.DisconnectSession()
+    }
+}
+
+# Execute Main when not in Tests
+if ($MyInvocation.ScriptName -notlike "*.tests.ps1" -AND -NOT$Test) {
+    Main
+}
