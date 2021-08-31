5995f50e3aa1bd6cfd7e0df16ba933ce5f9c27d3
meichlerpanw <meichler@paloaltonetworks.com>
2021-08-31 13:42:40 +0300

    ut fixes

--- Packs/Oracle_IAM/Integrations/OracleIAM/OracleIAM.py
+++ Packs/Oracle_IAM/Integrations/OracleIAM/OracleIAM.py
@@ -131,7 +131,10 @@ class Client(BaseClient):
         :rtype: ``IAMUserAppData``
         """
         user_data['schemas'] = ['urn:ietf:params:scim:schemas:core:2.0:User']
-        user_data['emails'] = [user_data.get('emails')]
+        if not isinstance(user_data.get('emails'), list):
+            user_data['emails'] = [user_data.get('emails')]
+        if not isinstance(user_data.get('phoneNumbers'), list):
+            user_data['phoneNumbers'] = [user_data.get('phoneNumbers')]
 
         user_app_data = self._http_request(
             method='POST',
