068f29924cf42ddd8230cf26176890ede0434776
meichlerpanw <meichler@paloaltonetworks.com>
2021-09-01 15:23:01 +0300

    add email manager

--- Packs/Clarizen/Integrations/ClarizenIAM/ClarizenIAM_test.py
+++ Packs/Clarizen/Integrations/ClarizenIAM/ClarizenIAM_test.py
@@ -8,7 +8,7 @@ APP_USER_OUTPUT = {
     'first_name': 'mock_first_name',
     'last_name': 'mock_last_name',
     'state': {'id': '/State/Active'},
-    'email': 'testdemisto@paloaltonetworks.com',
+    'email': 'emploee@paloaltonetworks.com',
 }
 
 APP_DISABLED_USER_OUTPUT = {
@@ -17,7 +17,7 @@ APP_DISABLED_USER_OUTPUT = {
     'first_name': 'mock_first_name',
     'last_name': 'mock_last_name',
     'state': {'id': '/State/Disabled'},
-    'email': 'testdemisto@paloaltonetworks.com',
+    'email': 'emploee@paloaltonetworks.com',
 }
 
 
@@ -45,7 +45,12 @@ class TestGetUserCommand:
             - Ensure the resulted User Profile object holds the correct user details
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            }
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={'entities': [{'id': '/User/1234'}]})
@@ -75,7 +80,12 @@ class TestGetUserCommand:
             - Ensure the resulted User Profile object holds information about an unsuccessful result.
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            }
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={})
@@ -104,7 +114,12 @@ class TestGetUserCommand:
         mocker.patch.object(demisto, 'error')
 
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            }
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', status_code=500, json={'error': {'message': 'INTERNAL SERVER ERROR'}})
@@ -131,7 +146,12 @@ class TestCreateUserCommand:
             - Ensure a User Profile object with the user data is returned
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            }
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={})  # Get user by email (check if there is a user before creating it)
@@ -163,7 +183,13 @@ class TestCreateUserCommand:
             - Ensure the command is considered successful and the user is still disabled
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}, 'allow-enable': 'false'}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            },
+            'allow-enable': 'false',
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={'entities': [{'id': '/User/1234'}]})
@@ -199,7 +225,7 @@ class TestUpdateUserCommand:
             - Ensure a User Profile object with the user data is returned
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com', 'givenname': 'mock_first_name'}}
+        args = {'user-profile': {'email': 'emploee@paloaltonetworks.com', 'givenname': 'mock_first_name'}}
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={})
@@ -230,7 +256,7 @@ class TestUpdateUserCommand:
             - Ensure the command is considered successful and skipped
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com', 'givenname': 'mock_first_name'}}
+        args = {'user-profile': {'email': 'emploee@paloaltonetworks.com', 'givenname': 'mock_first_name'}}
 
         user_profile = IAMCommand(is_update_enabled=False).update_user(client, args)
 
@@ -254,8 +280,13 @@ class TestUpdateUserCommand:
             - Ensure the user is enabled at the end of the command execution.
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com', 'givenname': 'mock_first_name'},
-                'allow-enable': 'true'}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            },
+            'allow-enable': 'true',
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={'entities': [{'id': '/User/1234'}]})
@@ -287,7 +318,12 @@ class TestDisableUserCommand:
             - Ensure that the command is successful and the user is disabled
         """
         client = mock_client()
-        args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}}
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            }
+        }
 
         with requests_mock.Mocker() as m:
             m.post('/data/findUserQuery', json={'entities': [{'id': '/User/1234'}]})
@@ -307,31 +343,36 @@ class TestDisableUserCommand:
         assert outputs.get('details', {}).get('last_name') == 'mock_last_name'
 
     def test_non_existing_user(self):
-            """
-            Given:
-                - An app client object
-                - A user-profile argument that contains an email of a user
-            When:
-                - create-if-not-exists parameter is unchecked
-                - The user does not exist in the application
-                - Calling function disable_user_command
-            Then:
-                - Ensure the command is considered successful and skipped
-            """
-            client = mock_client()
-            args = {'user-profile': {'email': 'testdemisto@paloaltonetworks.com'}}
+        """
+        Given:
+            - An app client object
+            - A user-profile argument that contains an email of a user
+        When:
+            - create-if-not-exists parameter is unchecked
+            - The user does not exist in the application
+            - Calling function disable_user_command
+        Then:
+            - Ensure the command is considered successful and skipped
+        """
+        client = mock_client()
+        args = {
+            'user-profile': {
+                'email': 'emploee@paloaltonetworks.com',
+                'manageremailaddress': 'manager@paloaltonetworks.com',
+            }
+        }
 
-            with requests_mock.Mocker() as m:
-                m.post('/data/findUserQuery', json={})
+        with requests_mock.Mocker() as m:
+            m.post('/data/findUserQuery', json={})
 
-                user_profile = IAMCommand().disable_user(client, args)
+            user_profile = IAMCommand().disable_user(client, args)
 
-            outputs = get_outputs_from_user_profile(user_profile)
+        outputs = get_outputs_from_user_profile(user_profile)
 
-            assert outputs.get('action') == IAMActions.DISABLE_USER
-            assert outputs.get('success') is True
-            assert outputs.get('skipped') is True
-            assert outputs.get('reason') == IAMErrors.USER_DOES_NOT_EXIST[1]
+        assert outputs.get('action') == IAMActions.DISABLE_USER
+        assert outputs.get('success') is True
+        assert outputs.get('skipped') is True
+        assert outputs.get('reason') == IAMErrors.USER_DOES_NOT_EXIST[1]
 
 
 def test_get_mapping_fields_command(mocker):
