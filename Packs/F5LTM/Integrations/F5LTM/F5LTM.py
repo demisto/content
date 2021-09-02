2bdb51032af78ae67f8652ca516e441843c1c07b
Ayman Mahmoud <ayman.s.mahmoud@outlook.com>
2021-09-02 17:33:04 +0400

    Additional Commands

--- Packs/F5LTM/Integrations/F5LTM/F5LTM.py
+++ Packs/F5LTM/Integrations/F5LTM/F5LTM.py
@@ -1,11 +1,9 @@
 
-import demistomock as demisto
 from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
 from CommonServerUserPython import *  # noqa
 
 import requests
 import traceback
-from typing import Dict, Any
 
 
 requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
@@ -25,6 +23,13 @@ class Client(BaseClient):
         self.headers = {'Content-Type': 'application/json',
                         'X-F5-Auth-Token': token}
         self.partition = partition
+        self.stats_keys = {
+            'activeMemberCnt', 'availableMemberCnt', 'connq.ageEdm', 'connq.ageEma', 'connq.ageHead',
+            'connq.ageMax', 'connq.depth', 'connq.serviced', 'connqAll.ageEdm', 'connqAll.ageEma',
+            'connqAll.ageHead', 'connqAll.ageMax', 'connqAll.depth', 'connqAll.serviced', 'curPriogrp',
+            'curSessions', 'highestPriogrp', 'lowestPriogrp', 'memberCnt', 'monitorRule',
+            'status.statusReason'
+        }
 
     def get_pools(self):
 
@@ -69,11 +74,47 @@ class Client(BaseClient):
                                       headers=self.headers, json_data={"session": "user-disabled"})
         return response
 
+    def enable_node(self, node):
+        url_suffix = f'ltm/node/~{self.partition}~{node}'
+        response = self._http_request(method='PATCH', url_suffix=url_suffix,
+                                      headers=self.headers, json_data={"session": "user-enabled"})
+        return response
+
+    def get_pool_member_stats(self, pool, member):
+        pool_stats = {}
+        url_suffix = f'ltm/pool/{pool}/members/~{self.partition}~{member}/stats'
+        response = self._http_request(method='GET', url_suffix=url_suffix,
+                                      headers=self.headers, params={})
+        member_stats = response.get('entries')[f'https://localhost/mgmt/tm/ltm/pool/{pool}/members/'
+                                               f'~{self.partition}~{member}/~{self.partition}~{pool}/'
+                                               f'stats']['nestedStats']['entries'][f'https://localhost/mgmt/tm/ltm/pool/{pool}/members/~{self.partition}~{member}/~{self.partition}~{pool}/members/stats']['nestedStats']['entries'][f'https://localhost/mgmt/tm/ltm/pool/{pool}/members/~{self.partition}~{member}/~{self.partition}~{pool}/members/~{self.partition}~{member}/stats']['nestedStats']['entries']
+
+        for key, value in response.get('entries').items():
+            raw_stats = value.get('nestedStats')['entries']
+            pool_stats = {key: raw_stats[key] for key in raw_stats.keys() & self.stats_keys}
+
+        return {
+            'pool': pool,
+            'stats': pool_stats,
+            'members': [{'name': member, 'stats': member_stats}]
+        }
+
+    def get_node_stats(self, node):
+        url_suffix = f'ltm/node/~{self.partition}~{node}/stats'
+        response = self._http_request(method='GET', url_suffix=url_suffix,
+                                      headers=self.headers, params={})
+        node_stats = (response.get('entries')[f'https://localhost/mgmt/tm/ltm/node/~'
+                                              f'{self.partition}~{node}/stats']['nestedStats']['entries'])
+        return {
+            'name': node,
+            'stats': node_stats
+        }
+
 
 ''' HELPER FUNCTIONS '''
 
 
-def login(server: str, port: str, username: str, password: str, verify_certificate: bool) -> str:
+def login(server: str, port: str, username: str, password: str, verify_certificate: bool):
 
     response = requests.post(f'https://{server}:{port}/mgmt/shared/authn/login',
                              verify=verify_certificate,
@@ -166,6 +207,38 @@ def ltm_disable_node_command(client, args) -> CommandResults:
     )
 
 
+def ltm_enable_node_command(client, args) -> CommandResults:
+    node = args.get('node')
+    results = client.enable_node(node)
+
+    return CommandResults(
+        outputs_prefix='F5.LTM.Nodes',
+        outputs_key_field='name',
+        outputs=results,
+    )
+
+
+def ltm_get_pool_member_stats_command(client, args) -> CommandResults:
+    pool = args.get('pool')
+    member = args.get('member')
+    results = client.get_pool_member_stats(pool=pool, member=member)
+    return CommandResults(
+        outputs_prefix='F5.LTM.Stats',
+        outputs_key_field='name',
+        outputs=results,
+    )
+
+
+def ltm_get_node_stats_command(client, args) -> CommandResults:
+    node = args.get('node')
+    results = client.get_node_stats(node=node)
+    return CommandResults(
+        outputs_prefix='F5.LTM.Nodes',
+        outputs_key_field='name',
+        outputs=results,
+    )
+
+
 ''' MAIN FUNCTION '''
 
 
@@ -216,6 +289,15 @@ def main() -> None:
 
         elif demisto.command() == 'f5-ltm-disable-node':
             return_results(ltm_disable_node_command(client, args))
+
+        elif demisto.command() == 'f5-ltm-enable-node':
+            return_results(ltm_enable_node_command(client, args))
+
+        elif demisto.command() == 'f5-ltm-get-pool-member-stats':
+            return_results(ltm_get_pool_member_stats_command(client, args))
+
+        elif demisto.command() == 'f5-ltm-get-node-stats':
+            return_results(ltm_get_node_stats_command(client, args))
 
     except Exception as e:
         demisto.error(traceback.format_exc())  # print the traceback
