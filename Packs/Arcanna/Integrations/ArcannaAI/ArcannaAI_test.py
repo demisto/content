87fb5664ee2556b123517b39c221bbcce6877fa8
Marian Craciunescu <marian.craciunescu@siscale.com>
2021-09-02 14:14:54 +0300

    fix test2

--- Packs/Arcanna/Integrations/ArcannaAI/ArcannaAI_test.py
+++ Packs/Arcanna/Integrations/ArcannaAI/ArcannaAI_test.py
@@ -120,9 +120,11 @@ def test_arcanna_send_event_feedback_command(mocker):
         "closing_notes": "notes"
     }
     mocker.patch.object(demisto, 'args', return_value=command_args)
-    command_result = send_event_feedback(client=client, feature_mapping_field="closeReason", args=command_args)
+    command_result = send_event_feedback(client=client,
+                                         feature_mapping_field='"Resolved"="escalate_alert"',
+                                         args=command_args)
 
-    assert command_result.outputs_prefix == "Arcanna.Feedback"
+    assert command_result.outputs_prefix == "Arcanna.Event"
     assert command_result.raw_response['status'] == "updated"
 
 
