26494624c470269788b68a83c618e18b18acb60d
ikeren <itay@demisto.com>
2021-09-07 19:46:39 +0300

    update to py3

--- Packs/CommonScripts/Scripts/Cut/Cut_test.py
+++ Packs/CommonScripts/Scripts/Cut/Cut_test.py
@@ -3,11 +3,9 @@ from Cut import cut
 import pytest
 
 
-@pytest.mark.parametrize("value,delimiter,fields,,expected",
-    [
-        ('A-B-C-D-E', '-', '1,5', 'A-E'),
-        ('a,ב,c', ',', '2,3', 'ב,c'),
-    ]
-)
+@pytest.mark.parametrize('value,delimiter,fields,expected', [
+    ('A-B-C-D-E', '-', '1,5', 'A-E'),
+    ('a,ב,c', ',', '2,3', 'ב,c'),
+])
 def test_cut(value, delimiter, fields, expected):
     assert cut(value, fields, delimiter) == expected
