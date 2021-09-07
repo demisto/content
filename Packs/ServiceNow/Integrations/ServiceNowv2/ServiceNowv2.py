31101749b357eeaff820af83d9033067403282d0
Clay Curtis <jccurtis@presidio.com>
2021-09-07 07:44:27 -0700

    fixed issue with user not showing in comments

--- Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
+++ Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
@@ -1,9 +1,9 @@
 import os
 import shutil
-import dateparser
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from urllib import parse
-from typing import List, Tuple, Dict, Callable, Any, Union, Optional
 
+import dateparser
 from CommonServerPython import *
 
 # disable insecure warnings
@@ -2231,6 +2231,8 @@ def update_remote_system_command(client: Client, args: Dict[str, Any], params: D
                 elif params.get('comment_tag') in tags:
                     key = 'comments'
                 user = entry.get('user', 'dbot')
+                # Sometimes user is an empty str, not None, therefore nothing is displayed in ServiceNow
+                user = 'dbot' if user == '' else user
                 text = f"({user}): {str(entry.get('contents', ''))}\n\n Mirrored from Cortex XSOAR"
                 client.add_comment(ticket_id, ticket_type, key, text)
 
@@ -2410,7 +2412,6 @@ def main():
 
 
 from ServiceNowApiModule import *  # noqa: E402
-
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
