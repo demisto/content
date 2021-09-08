e2e9d5c4e7e1e3601583fa7235beab039bd617a2
ikeren <itay@demisto.com>
2021-09-08 17:21:30 +0300

    add test docstring

--- Packs/CommonScripts/Scripts/Cut/Cut_test.py
+++ Packs/CommonScripts/Scripts/Cut/Cut_test.py
@@ -8,4 +8,16 @@ import pytest
     ('a,ב,c', ',', '2,3', 'ב,c'),
 ])
 def test_cut(value, delimiter, fields, expected):
+    """
+    Given:
+        Case 1: A-B-C-D-E to split by - from char 1 to 5
+        Case 2: a,ב,c to split by , from char 2 to 3
+
+    When:
+        Running Cut
+
+    Then:
+        Case 1: Ensure A-E is returned
+        Case 2: Ensure ב,c is returned
+    """
     assert cut(value, fields, delimiter) == expected
