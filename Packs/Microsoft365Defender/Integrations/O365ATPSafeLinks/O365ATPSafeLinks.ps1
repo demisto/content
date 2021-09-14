695f3a774d2d29c48b6827c837d49e05abe32b80
teizenman <teizenman@paloaltonetworks.com>
2021-09-14 14:24:57 +0200

    commands ready

--- Packs/Microsoft365Defender/Integrations/O365ATPSafeLinks/O365ATPSafeLinks.ps1
+++ Packs/Microsoft365Defender/Integrations/O365ATPSafeLinks/O365ATPSafeLinks.ps1
@@ -44,12 +44,10 @@ class ExchangeOnlinePowershellV2Client {
 
     [PSObject]
     GetPolicyList(
-            [string]$identity
-    )
-    {
+        [string]$identity
+    ) {
         $cmd_params = @{ }
-        if ($identity)
-        {
+        if ($identity) {
             $cmd_params.Identity = $identity
         }
         $this.CreateSession()
@@ -82,7 +80,8 @@ class ExchangeOnlinePowershellV2Client {
     }
     
     [PSObject]
-    CreatePolicy(
+    CreateUpdatePolicy(
+        [string]$command_type,
         [string]$name,
         [string]$admin_display_name,
         [string]$custom_notification_text,
@@ -136,33 +135,192 @@ class ExchangeOnlinePowershellV2Client {
         if ($use_translated_notification_text) {
             $cmd_params.UseTranslatedNotificationText = $use_translated_notification_text
         }
-
         
         $this.CreateSession()
-        $results = New-SafeLinksPolicy @cmd_params
+        
+        if (command_type -e "create") {
+            $results = New-SafeLinksPolicy @cmd_params
+        }
+        else {
+            $results = Set-SafeLinksPolicy @cmd_params
+        }
+        
         $this.DisconnectSession()
         return $results
 
         <#
         .DESCRIPTION
-        Use this cmdlet to create Safe Links policies in your cloud-based organization.
+        Use this cmdlet to create or update Safe Links policies in your cloud-based organization.
         This cmdlet is available only in the cloud-based service.
 
         .EXAMPLE
-        GetPolicyList("1254y894-feae-9yn7-a3e1-f2483a154tft")
+        CreateUpdatePolicy("1254y894-feae-9yn7-a3e1-f2483a154tft")
 
         .OUTPUTS
         PSObject - Raw response
 
         .LINK
         https://docs.microsoft.com/en-us/powershell/module/exchange/new-safelinkspolicy?view=exchange-ps
+        https://docs.microsoft.com/en-us/powershell/module/exchange/set-safelinkspolicy?view=exchange-ps
+        #>
+    }
+
+    [PSObject]
+    RemovePolicy(
+        [string]$identity
+    ) {
+        $cmd_params = @{ }
+        if ($identity) {
+            $cmd_params.Identity = $identity
+        }
+        $this.CreateSession()
+        $results = Remove-SafeLinksPolicy @cmd_params
+        $this.DisconnectSession()
+        return $results
+
+        <#
+        .DESCRIPTION
+        Use this cmdlet to remove Safe Links policies from your cloud-based organization.
+        This cmdlet is available only in the cloud-based service.
+
+        .PARAMETER identity
+        The Identity parameter specifies the Safe Links policy that you want to remove.
+
+        You can use any value that uniquely identifies the policy. For example:
+        * Name
+        * Distinguished name (DN)
+        * GUID
+
+        .EXAMPLE
+        RemovePolicy("1254y894-feae-9yn7-a3e1-f2483a154tft")
+
+        .OUTPUTS
+        PSObject - Raw response
+
+        .LINK
+        https://docs.microsoft.com/en-us/powershell/module/exchange/remove-safelinkspolicy?view=exchange-ps
+        #>
+    }
+
+    [PSObject]
+    GetRules(
+        [string]$identity
+    ) {
+        $cmd_params = @{ }
+        if ($identity) {
+            $cmd_params.Identity = $identity
+        }
+        $this.CreateSession()
+        $results = Get-SafeLinksRule @cmd_params
+        $this.DisconnectSession()
+        return $results
+
+        <#
+        .DESCRIPTION
+        Use this cmdlet to view Safe Links rules in your cloud-based organization.
+        This cmdlet is available only in the cloud-based service.
+
+        .PARAMETER identity
+        The Identity parameter specifies the Safe Links rule that you want to view.
+
+        You can use any value that uniquely identifies the policy. For example:
+        * Name
+        * Distinguished name (DN)
+        * GUID
+
+        .EXAMPLE
+        GetRules("1254y894-feae-9yn7-a3e1-f2483a154tft")
+
+        .OUTPUTS
+        PSObject - Raw response
+
+        .LINK
+        https://docs.microsoft.com/en-us/powershell/module/exchange/get-safelinksrule?view=exchange-ps
+        #>
+    }
+
+    [PSObject]
+    CreateUpdateRule(
+        [string]$command_type,
+        [string]$name,
+        [string]$safe_links_policy,
+        [string]$comments,
+        [bool]$enabled,
+        [string]$except_if_recipient_domain_is,
+        [string]$except_if_sent_to,
+        [string]$except_if_sent_to_member_of,
+        [Int32]$priority,
+        [string]$recipient_domain_is,
+        [string]$sent_to,
+        [string]$sent_to_member_of
+
+    ) {
+        $cmd_params = @{ 
+            Name = $name
+        }
+        if ($safe_links_policy) {
+            $cmd_params.SafeLinksPolicy = $safe_links_policy
+        }
+        if ($comments) {
+            $cmd_params.Comments = $comments
+        }
+        if ($enabled) {
+            $cmd_params.Enabled = $enabled
+        }
+        if ($except_if_recipient_domain_is) {
+            $cmd_params.ExceptIfRecipientDomainIs = $except_if_recipient_domain_is
+        }
+        if ($except_if_sent_to) {
+            $cmd_params.ExceptIfSentTo = $except_if_sent_to
+        }
+        if ($except_if_sent_to_member_of) {
+            $cmd_params.ExceptIfSentToMemberOf = $except_if_sent_to_member_of
+        }
+        if ($priority) {
+            $cmd_params.Priority = $priority
+        }
+        if ($recipient_domain_is) {
+            $cmd_params.RecipientDomainIs = $recipient_domain_is
+        }
+        if ($sent_to) {
+            $cmd_params.EnableForInternalSenders = $sent_to
+        }
+        if ($sent_to_member_of) {
+            $cmd_params.SentToMemberOf = $sent_to_member_of
+        }
+        
+        $this.CreateSession()
+        
+        if (command_type -e "create") {
+            $results = New-SafeLinksRule @cmd_params
+        }
+        else {
+            $results = Set-SafeLinksRule @cmd_params
+        }
+        
+        $this.DisconnectSession()
+        return $results
+
+        <#
+        .DESCRIPTION
+        Use this cmdlet to create or update Safe Links rules in your cloud-based organization.
+        This cmdlet is available only in the cloud-based service.
+
+        .EXAMPLE
+        CreateUpdatePolicy("1254y894-feae-9yn7-a3e1-f2483a154tft")
+
+        .OUTPUTS
+        PSObject - Raw response
+
+        .LINK
+        https://docs.microsoft.com/en-us/powershell/module/exchange/new-safelinksrule?view=exchange-ps
+        https://docs.microsoft.com/en-us/powershell/module/exchange/set-safelinksrule?view=exchange-ps
         #>
     }
 }
 
 
-function GetPolicyListCommand
-{
+function GetPolicyListCommand {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
@@ -170,18 +328,18 @@ function GetPolicyListCommand
     )
     $identity = $kwargs.identity
     $raw_response = $client.GetPolicyList(
-            $identity
+        $identity
     )
     $human_readable = TableToMarkdown $raw_response "Results of $command"
     $entry_context = @{ "$script:INTEGRATION_ENTRY_CONTEXT.PolicyList(obj.Guid === val.Guid)" = $raw_response }
     Write-Output $human_readable, $entry_context, $raw_response
 }
 
-function CreatePolicyCommand
-{
+function CreateUpdatePolicyCommand {
     [CmdletBinding()]
     Param (
         [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
+        [Parameter(Mandatory)][string]$command_type,
         [hashtable]$kwargs
     )
     $name = $kwargs.name
@@ -198,24 +356,100 @@ function CreatePolicyCommand
     $scan_urls = $kwargs.scan_urls -as [bool]
     $use_translated_notification_text = $kwargs.use_translated_notification_text -as [bool]
 
-    $raw_response = $client.GetPolicyList(
-            $identity
+    $raw_response = $client.CreateUpdatePolicy(
+        $command_type,
+        $name,
+        $admin_display_name,
+        $custom_notification_text,
+        $deliver_message_after_scan,
+        $do_not_allow_click_through,
+        $do_not_rewrite_urls,
+        $do_not_track_user_clicks,
+        $enable_for_internal_senders,
+        $enable_organization_branding,
+        $enable_safe_links_for_teams,
+        $is_enabled,
+        $scan_urls,
+        $use_translated_notification_text
     )
     $human_readable = TableToMarkdown $raw_response "Results of $command"
-    $entry_context = @{ "$script:INTEGRATION_ENTRY_CONTEXT.PolicyList(obj.Guid === val.Guid)" = $raw_response }
+    $entry_context = @{ "$script:INTEGRATION_ENTRY_CONTEXT.Policy(obj.Guid === val.Guid)" = $raw_response }
     Write-Output $human_readable, $entry_context, $raw_response
 }
 
+function RemovePolicyCommand {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
+        [hashtable]$kwargs
+    )
+    $identity = $kwargs.identity
+    $raw_response = $client.RemovePolicy(
+        $identity
+    )
+    $human_readable = TableToMarkdown $raw_response "Results of $command"
+    $entry_context = @{ "$script:INTEGRATION_ENTRY_CONTEXT.Policy(obj.Guid === val.Guid)" = $raw_response }
+    Write-Output $human_readable, $entry_context, $raw_response
+}
 
-function TestModuleCommand($client)
-{
-    try
-    {
+function GetRulesCommand {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
+        [hashtable]$kwargs
+    )
+    $identity = $kwargs.identity
+    $raw_response = $client.GetRules(
+        $identity
+    )
+    $human_readable = TableToMarkdown $raw_response "Results of $command"
+    $entry_context = @{ "$script:INTEGRATION_ENTRY_CONTEXT.Policy(obj.Guid === val.Guid)" = $raw_response }
+    Write-Output $human_readable, $entry_context, $raw_response
+}
+
+function CreateUpdateRuleCommand {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
+        [Parameter(Mandatory)][string]$command_type,
+        [hashtable]$kwargs
+    )
+    $name = $kwargs.name
+    $safe_links_policy = $kwargs.safe_links_policy
+    $comments = $kwargs.comments,
+    $enabled = $kwargs.enabled -as [bool]
+    $except_if_recipient_domain_is = $kwargs.except_if_recipient_domain_is
+    $except_if_sent_to = $kwargs.except_if_sent_to
+    $except_if_sent_to_member_of = $kwargs.except_if_sent_to_member_of
+    $priority = $kwargs.enable_for_internal_senders -as [Int32]
+    $recipient_domain_is = $kwargs.recipient_domain_is
+    $sent_to = $kwargs.sent_to
+    $sent_to_member_of = $kwargs.sent_to_member_of
+
+    $raw_response = $client.CreateUpdateRule(
+        $command_type,
+        $name,
+        $safe_links_policy,
+        $comments,
+        $enabled,
+        $except_if_recipient_domain_is,
+        $except_if_sent_to,
+        $except_if_sent_to_member_of,
+        $priority,
+        $recipient_domain_is,
+        $sent_to,
+        $sent_to_member_of
+    )
+    $human_readable = TableToMarkdown $raw_response "Results of $command"
+    $entry_context = @{ "$script:INTEGRATION_ENTRY_CONTEXT.Policy(obj.Guid === val.Guid)" = $raw_response }
+    Write-Output $human_readable, $entry_context, $raw_response
+}
+function TestModuleCommand($client) {
+    try {
         $client.CreateSession()
         $demisto.results("ok")
     }
-    finally
-    {
+    finally {
         $client.DisconnectSession()
     }
 
@@ -253,17 +487,24 @@ function Main {
                 ($human_readable, $entry_context, $raw_response) = GetPolicyListCommand $exo_client $command_arguments
             }
             "$script:COMMAND_PREFIX-policy-create" {
-                ($human_readable, $entry_context, $raw_response) = GetEXOMailBoxCommand $exo_client $command_arguments
+                ($human_readable, $entry_context, $raw_response) = CreateUpdatePolicyCommand $exo_client "create" $command_arguments
             }
-            "$script:COMMAND_PREFIX-mailbox-permission-list" {
-                ($human_readable, $entry_context, $raw_response) = GetEXOMailBoxPermissionCommand $exo_client $command_arguments
+            "$script:COMMAND_PREFIX-policy-update" {
+                ($human_readable, $entry_context, $raw_response) = CreateUpdatePolicyCommand $exo_client "update" $command_arguments
             }
-            "$script:COMMAND_PREFIX-recipient-permission-list" {
-                ($human_readable, $entry_context, $raw_response) = GetEXORecipientPermissionCommand $exo_client $command_arguments
+            "$script:COMMAND_PREFIX-policy-remove" {
+                ($human_readable, $entry_context, $raw_response) = RemovePolicyCommand $exo_client $command_arguments
             }
-            "$script:COMMAND_PREFIX-recipient-list" {
-                ($human_readable, $entry_context, $raw_response) = GetEXORecipientCommand $exo_client $command_arguments
+            "$script:COMMAND_PREFIX-rule-list" {
+                ($human_readable, $entry_context, $raw_response) = GetRulesCommand $exo_client $command_arguments
             }
+            "$script:COMMAND_PREFIX-rule-create" {
+                ($human_readable, $entry_context, $raw_response) = CreateUpdateRuleCommand $exo_client "create" $command_arguments
+            }
+            "$script:COMMAND_PREFIX-rule-update" {
+                ($human_readable, $entry_context, $raw_response) = CreateUpdateRuleCommand $exo_client "update" $command_arguments
+            }
+            
             default {
                 ReturnError "Could not recognize $command"
             }
