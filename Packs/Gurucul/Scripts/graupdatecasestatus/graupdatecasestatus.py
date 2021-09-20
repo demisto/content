b553382f7917c9ad1e17a09f3bf0c88b2cfe2fe3
Umesh Balshetwar <upbalshetwar@users.noreply.github.com>
2021-09-20 16:27:48 +0530

    analyticalfeature display on layout screen

--- Packs/Gurucul/Scripts/graupdatecasestatus/graupdatecasestatus.py
+++ Packs/Gurucul/Scripts/graupdatecasestatus/graupdatecasestatus.py
@@ -7,14 +7,15 @@ def _get_incident():
 
 
 def closeCase():
-    demisto.info(str(_get_incident()))
     incident = _get_incident()
+    close_reason = demisto.args().get('closeReason')
+    close_notes = demisto.args().get('closeNotes', 'No close notes provided')
     _caseId = ""
     for label in incident['labels']:
         if label['type'] == 'caseId':
             _caseId = label['value']
             break
-    demisto.info(_caseId)
+
     if _caseId == "":
         raise Exception('caseId was not found in the incident labels')
 
@@ -22,7 +23,7 @@ def closeCase():
         'action': 'closeCase',
         'subOption': 'True Incident',
         'caseId': _caseId,
-        'caseComment': 'Case closed from XSoar'
+        'caseComment': "Case marked as \"" + close_reason + "\" with comment \"" + close_notes + "\" from XSOAR"
     })
 
 
