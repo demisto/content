a71faffe5ec5b2dd44f97d240240cfb00c6d5b63
Prabhu Subramanian <prabhu@shiftleft.io>
2021-09-21 13:04:11 +0100

    Fix test

--- Packs/ShiftLeft/Integrations/shiftleft/shiftleft_test.py
+++ Packs/ShiftLeft/Integrations/shiftleft/shiftleft_test.py
@@ -3,30 +3,25 @@
 
 import json
 import io
-
+from shiftleft import list_app_findings_command, ShiftLeftClient
 
 def util_load_json(path):
     with io.open(path, mode="r", encoding="utf-8") as f:
         return json.loads(f.read())
 
 
-def test_baseintegration_dummy():
-    """Tests helloworld-say-hello command function.
+def test_list_app_findings_command(requests_mock):
+    """Tests list_app_findings_command function.
 
     Checks the output of the command function with the expected output.
-
-    No mock is needed here because the say_hello_command does not call
-    any external API.
     """
-    from BaseIntegration import Client, baseintegration_dummy_command
+    mock_response = util_load_json("test_data/test_list_findings.json")
+    requests_mock.get("https://www.shiftleft.io/orgs/2c089ac1-3378-44d5-94da-9507e84351c3/apps/shiftleft-java-example/findings", json=mock_response)
 
-    client = Client(
-        base_url="/orgs/2c089ac1-3378-44d5-94da-9507e84351c3/apps/shiftleft-java-example/findings",
+    client = ShiftLeftClient(
+        base_url="https://www.shiftleft.io",
         verify=False,
     )
-    args = {"severity": "critical", "type": ["vuln"], "version": None}
-    response = baseintegration_dummy_command(client, args)
-
-    mock_response = util_load_json("test_data/baseintegration-dummy.json")
-
+    args = {"app_name": "shiftleft-java-example", "severity": "critical", "type": ["vuln"], "version": None}
+    response = list_app_findings_command(client, "2c089ac1-3378-44d5-94da-9507e84351c3", args)
     assert response.outputs
