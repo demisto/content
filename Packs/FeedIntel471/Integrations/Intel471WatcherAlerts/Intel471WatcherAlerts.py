1f30486056af77c6be140414a6caea5204d89cde
Richard Hallick <rhallick@intel471.com>
2021-09-23 08:49:17 +0100

    Intel 471 watcher alerts.

---
+++ Packs/FeedIntel471/Integrations/Intel471WatcherAlerts/Intel471WatcherAlerts.py
@@ -0,0 +1,380 @@
+"""Base Integration for Cortex XSOAR (aka Demisto)
+
+This is an empty Integration with some basic structure according
+to the code conventions.
+
+MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
+
+Developer Documentation: https://xsoar.pan.dev/docs/welcome
+Code Conventions: https://xsoar.pan.dev/docs/integrations/code-conventions
+Linting: https://xsoar.pan.dev/docs/integrations/linting
+
+This is an empty structure file. Check an example at;
+https://github.com/demisto/content/blob/master/Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.py
+
+"""
+
+import demistomock as demisto
+from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
+from CommonServerUserPython import *  # noqa
+
+import json
+import requests
+import traceback
+from typing import Tuple, List, Dict, Any, cast
+
+
+''' CONSTANTS '''
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
+FEED_URL = 'https://api.intel471.com/v1/alerts?'
+MAX_INCIDENTS_TO_FETCH = 100
+INTEL471_SEVERITIES = ['Low', 'Medium', 'High', 'Critical']
+INCIDENT_TYPE = 'Intel 471 Watcher Alert'
+DEMISTO_VERSION = demisto.demistoVersion()
+CONTENT_PACK = 'Intel471 Feed/2.0.1'
+INTEGRATION = 'Intel471 Watcher Alerts Feed'
+USER_AGENT = f'XSOAR/{DEMISTO_VERSION["version"]}.{DEMISTO_VERSION["buildNumber"]} - {CONTENT_PACK} - {INTEGRATION}'
+
+
+''' CLIENT CLASS '''
+
+
+class Client(BaseClient):
+    """Client class to interact with the service API
+
+    This Client implements API calls, and does not contain any XSOAR logic.
+    Should only do requests and return data.
+    It inherits from BaseClient defined in CommonServer Python.
+    Most calls use _http_request() that handles proxy, SSL verification, etc.
+    For this  implementation, no special attributes defined
+    """
+
+    # TODO: REMOVE the following dummy function:
+    def baseintegration_dummy(self, dummy: str) -> Dict[str, str]:
+        """Returns a simple python dict with the information provided
+        in the input (dummy).
+
+        :type dummy: ``str``
+        :param dummy: string to add in the dummy dict that is returned
+
+        :return: dict as {"dummy": dummy}
+        :rtype: ``str``
+        """
+
+        return {"dummy": dummy}
+
+    # TODO: ADD HERE THE FUNCTIONS TO INTERACT WITH YOUR PRODUCT API
+    def search_alerts(self, alert_type: Optional[str], watcher_group: Optional[str],
+                      max_results: Optional[int],
+                      start_time: Optional[int]) -> List[Dict[str, Any]]:
+        """Searches for Intel 471 Watcher Alerts using the '/get_alerts' API endpoint
+
+        All the parameters are passed directly to the API as HTTP POST parameters in the request
+
+        :type alert_type: ``Optional[str]``
+        :param alert_type: type of alerts to search for. There is no list of predefined types
+
+        :type watcher_group: ``Optional[str]``
+        :param watcher_group: the name of the watcher group for which alerts should be fetched
+
+        :type max_results: ``Optional[int]``
+        :param max_results: maximum number of results to return
+
+        :type start_time: ``Optional[int]``
+        :param start_time: start timestamp (epoch in seconds) for the alert search
+
+        :return: list containing the found Intel 471 Watcher alerts as dicts
+        :rtype: ``List[Dict[str, Any]]``
+        """
+
+        request_params: Dict[str, Any] = {}
+
+        if watcher_group:
+            request_params['watcherGroup'] = watcher_group
+
+        if alert_type:
+            request_params['alert_type'] = alert_type
+
+        if max_results:
+            request_params['max_results'] = max_results
+
+        if start_time:
+            request_params['start_time'] = start_time
+
+        alerts = [
+            {
+                'created': '1631885116',
+                'name': 'Test alert 1',
+                'severity': 'Medium',
+                'type': alert_type
+            }
+        ]
+
+        return alerts
+
+        #return self._http_request(
+        #    method='GET',
+        #    url_suffix='/get_alerts',
+        #    params=request_params
+        #)
+
+
+''' HELPER FUNCTIONS '''
+
+# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
+def convert_to_demisto_severity(severity: str) -> int:
+    """Maps HelloWorld severity to Cortex XSOAR severity
+
+    Converts the HelloWorld alert severity level ('Low', 'Medium',
+    'High', 'Critical') to Cortex XSOAR incident severity (1 to 4)
+    for mapping.
+
+    :type severity: ``str``
+    :param severity: severity as returned from the HelloWorld API (str)
+
+    :return: Cortex XSOAR Severity (1 to 4)
+    :rtype: ``int``
+    """
+
+    # In this case the mapping is straightforward, but more complex mappings
+    # might be required in your integration, so a dedicated function is
+    # recommended. This mapping should also be documented.
+    return {
+        'Low': IncidentSeverity.LOW,
+        'Medium': IncidentSeverity.MEDIUM,
+        'High': IncidentSeverity.HIGH,
+        'Critical': IncidentSeverity.CRITICAL
+    }[severity]
+
+
+''' COMMAND FUNCTIONS '''
+
+
+def test_module(client: Client) -> str:
+    """Tests API connectivity and authentication'
+
+    Returning 'ok' indicates that the integration works like it is supposed to.
+    Connection to the service is successful.
+    Raises exceptions if something goes wrong.
+
+    :type client: ``Client``
+    :param Client: client to use
+
+    :return: 'ok' if test passed, anything else will fail the test.
+    :rtype: ``str``
+    """
+
+    message: str = ''
+    try:
+        # TODO: ADD HERE some code to test connectivity and authentication to your service.
+        # This  should validate all the inputs given in the integration configuration panel,
+        # either manually or by using an API that uses them.
+        message = 'ok'
+    except DemistoException as e:
+        if 'Forbidden' in str(e) or 'Authorization' in str(e):  # TODO: make sure you capture authentication errors
+            message = 'Authorization Error: make sure API Key is correctly set'
+        else:
+            raise e
+    return message
+
+
+# TODO: REMOVE the following dummy command function
+def baseintegration_dummy_command(client: Client, args: Dict[str, Any]) -> CommandResults:
+
+    dummy = args.get('dummy', None)
+    if not dummy:
+        raise ValueError('dummy not specified')
+
+    # Call the Client function and get the raw response
+    result = client.baseintegration_dummy(dummy)
+
+    return CommandResults(
+        outputs_prefix='BaseIntegration',
+        outputs_key_field='',
+        outputs=result,
+    )
+# TODO: ADD additional command functions that translate XSOAR inputs/outputs to Client
+def fetch_incidents(client: Client, max_results: int, last_run: Dict[str, int],
+                    first_fetch_time: Optional[int],
+                    watcher_group: Optional[str], severity: str, alert_type: Optional[str]
+                    ) -> Tuple[Dict[str, int], List[dict]]:
+
+    # Get the last fetch time, if exists
+    # last_run is a dict with a single key, called last_fetch
+    last_fetch = last_run.get('last_fetch', None)
+    # Handle first fetch time
+    if last_fetch is None:
+        # if missing, use what provided via first_fetch_time
+        last_fetch = first_fetch_time
+    else:
+        # otherwise use the stored last fetch
+        last_fetch = int(last_fetch)
+
+    # for type checking, making sure that latest_created_time is int
+    latest_created_time = cast(int, last_fetch)
+
+    # Initialize an empty list of incidents to return
+    # Each incident is a dict with a string as a key
+    incidents: List[Dict[str, Any]] = []
+
+    # Get the CSV list of severities from severity
+    severity = ','.join(INTEL471_SEVERITIES[INTEL471_SEVERITIES.index(severity):])
+
+    alerts = client.search_alerts(
+        alert_type=alert_type,
+        watcher_group=watcher_group,
+        max_results=max_results,
+        start_time=last_fetch
+    )
+
+    for alert in alerts:
+        # If no created_time set is as epoch (0). We use time in ms so we must
+        # convert it from the HelloWorld API response
+        incident_created_time = int(alert.get('created', '0'))
+        incident_created_time_ms = incident_created_time * 1000
+
+        # to prevent duplicates, we are only adding incidents with creation_time > last fetched incident
+        if last_fetch:
+            if incident_created_time <= last_fetch:
+                continue
+
+        # If no name is present it will throw an exception
+        incident_name = alert['name']
+
+        # INTEGRATION DEVELOPER TIP
+        # The incident dict is initialized with a few mandatory fields:
+        # name: the incident name
+        # occurred: the time on when the incident occurred, in ISO8601 format
+        # we use timestamp_to_datestring() from CommonServerPython.py to
+        # handle the conversion.
+        # rawJSON: everything else is packed in a string via json.dumps()
+        # and is included in rawJSON. It will be used later for classification
+        # and mapping inside XSOAR.
+        # severity: it's not mandatory, but is recommended. It must be
+        # converted to XSOAR specific severity (int 1 to 4)
+        # Note that there are other fields commented out here. You can do some
+        # mapping of fields (either out of the box fields, like "details" and
+        # "type") or custom fields (like "helloworldid") directly here in the
+        # code, or they can be handled in the classification and mapping phase.
+        # In either case customers can override them. We leave the values
+        # commented out here, but you can use them if you want.
+        incident = {
+            'name': incident_name,
+            # 'details': alert['name'],
+            'occurred': timestamp_to_datestring(incident_created_time_ms),
+            'rawJSON': json.dumps(alert),
+            # 'type': 'Hello World Alert',  # Map to a specific XSOAR incident Type
+            'severity': convert_to_demisto_severity(alert.get('severity', 'Medium')),
+            # 'CustomFields': {  # Map specific XSOAR Custom Fields
+            #     'helloworldid': alert.get('alert_id'),
+            #     'helloworldstatus': alert.get('alert_status'),
+            #     'helloworldtype': alert.get('alert_type')
+            # }
+        }
+
+        incidents.append(incident)
+
+        # Update last run and add incident if the incident is newer than last fetch
+        if incident_created_time > latest_created_time:
+            latest_created_time = incident_created_time
+
+    # Save the next_run as a dict with the last_fetch key to be stored
+    next_run = {'last_fetch': latest_created_time}
+    return next_run, incidents
+
+
+''' MAIN FUNCTION '''
+
+
+def main() -> None:
+    """main function, parses params and runs command functions
+
+    :return:
+    :rtype:
+    """
+
+    # TODO: make sure you properly handle authentication
+    # api_key = demisto.params().get('apikey')
+
+    # get the service API url
+    base_url = FEED_URL
+
+    # if your Client class inherits from BaseClient, SSL verification is
+    # handled out of the box by it, just pass ``verify_certificate`` to
+    # the Client constructor
+    verify_certificate = not demisto.params().get('insecure', False)
+
+    # How much time before the first fetch to retrieve incidents
+    first_fetch_time = arg_to_datetime(
+        arg=demisto.params().get('fetch_time', '3 days'),
+        arg_name='First fetch time',
+        required=True
+    )
+    first_fetch_timestamp = int(first_fetch_time.timestamp()) if first_fetch_time else None
+    # Using assert as a type guard (since first_fetch_time is always an int when required=True)
+    assert isinstance(first_fetch_timestamp, int)
+
+    # if your Client class inherits from BaseClient, system proxy is handled
+    # out of the box by it, just pass ``proxy`` to the Client constructor
+    proxy = demisto.params().get('proxy', False)
+
+    demisto.debug(f'Command being called is {demisto.command()}')
+    try:
+
+        # TODO: Make sure you add the proper headers for authentication
+        # (i.e. "Authorization": {api key})
+        headers: Dict = {
+            'user-agent': USER_AGENT
+        }
+
+        client = Client(
+            base_url=base_url,
+            verify=verify_certificate,
+            headers=headers,
+            proxy=proxy)
+
+        if demisto.command() == 'test-module':
+            # This is the call made when pressing the integration Test button.
+            result = test_module(client)
+            return_results(result)
+
+        # TODO: REMOVE the following dummy command case:
+        elif demisto.command() == 'baseintegration-dummy':
+            return_results(baseintegration_dummy_command(client, demisto.args()))
+
+        # TODO: ADD command cases for the commands you will implement
+        elif demisto.command() == 'fetch-incidents':
+            # Set and define the fetch incidents command to run after activated via integration settings.
+            watcher_group = demisto.params().get('watcher_group', None)
+            alert_type = INCIDENT_TYPE
+            severity = demisto.params().get('severity', 'Medium')
+
+            max_results = MAX_INCIDENTS_TO_FETCH
+
+            next_run, incidents = fetch_incidents(
+                client=client,
+                max_results=max_results,
+                last_run=demisto.getLastRun(),  # getLastRun() gets the last run dict
+                first_fetch_time=first_fetch_timestamp,
+                watcher_group=watcher_group,
+                severity=severity,
+                alert_type=alert_type
+            )
+
+            # saves next_run for the time fetch-incidents is invoked
+            demisto.setLastRun(next_run)
+            # fetch-incidents calls ``demisto.incidents()`` to provide the list
+            # of incidents to create
+            demisto.incidents(incidents)
+
+    # Log exceptions and return errors
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to execute {demisto.command()} command.\nError:\n{str(e)}')
+
+
+''' ENTRY POINT '''
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
