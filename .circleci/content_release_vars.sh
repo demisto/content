95c02ce2b816ca9125f9721e56a09f97956b1377
mgalitzki <mgalitzki@paloaltonetworks.com>
2021-09-26 17:22:10 +0300

    bump to 21.9.1

--- .circleci/content_release_vars.sh
+++ .circleci/content_release_vars.sh
@@ -1,3 +1,3 @@
-export CONTENT_VERSION="21.9.0"
+export CONTENT_VERSION="21.9.1"
 export SERVER_VERSION="6.0.0"
 export GIT_SHA1="26b1cc1404a6edcb7ad13e28e41b9d4d0299fe19" # guardrails-disable-line disable-secrets-detection
