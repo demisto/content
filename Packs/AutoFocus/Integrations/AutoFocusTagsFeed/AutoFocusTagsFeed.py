a11705db72e37cac3552b1b03195f599c27af12d
yucohen <yucohen@paloaltonetworks.com>
2021-09-29 20:19:48 +0300

    changed fetch logic

--- Packs/AutoFocus/Integrations/AutoFocusTagsFeed/AutoFocusTagsFeed.py
+++ Packs/AutoFocus/Integrations/AutoFocusTagsFeed/AutoFocusTagsFeed.py
@@ -48,9 +48,7 @@ MAP_RELATIONSHIPS = {
 
 }
 
-EXECUTOR = concurrent.futures.ThreadPoolExecutor(max_workers=59)
-EXECUTOR1 = concurrent.futures.ThreadPoolExecutor(max_workers=59)
-PAGE_SIZE = 59
+PAGE_SIZE = 50
 
 ''' CLIENT CLASS '''
 
@@ -63,41 +61,19 @@ class Client(BaseClient):
     def __init__(self, api_key, verify, proxy):
         super().__init__(BASE_URL, verify, proxy)
         self.api_key = api_key
-        self.calls_count = 0
-        self.test = 0
-        self.page_num = -1
-        self.thread_lock_for_calls_count = threading.Lock()
-        self.thread_lock_for_page_num = threading.Lock()
 
-    def get_all_tags(self):
-        with self.thread_lock_for_calls_count:
-            self.calls_count = (self.calls_count + 1)
-            demisto.debug(f"in the first print , call count all tags {self.calls_count}")
-            if self.calls_count >= 100:
-                threading.Event.wait(threading.Event())
-                self.calls_count = 0
-        with self.thread_lock_for_page_num:
-            self.page_num += 1
+    def get_tags(self, page_num: int):
         res = self._http_request('POST',
                                  url_suffix='tags',
                                  headers={
                                      'apiKey': self.api_key,
                                      'Content-Type': 'application/json'
                                  },
-                                 json_data={"pageSize": PAGE_SIZE, "pageNum": self.page_num},
+                                 json_data={"pageNum": page_num, "pageSize": PAGE_SIZE},
                                  )
-        with self.thread_lock_for_calls_count:
-            demisto.debug(f"before exiting get all tags, call count all tags {self.calls_count}")
         return res
 
     def get_tag_details(self, public_tag_name: str):
-        with self.thread_lock_for_calls_count:
-            self.calls_count = (self.calls_count + 1)
-            demisto.debug(f"get tag details,  {self.calls_count}")
-            if self.calls_count >= 100:
-                demisto.debug("im in sleep")
-                threading.Event.wait(threading.Event(), 70)
-                self.calls_count = 0
         res = self._http_request('POST',
                                  url_suffix=f'tag/{public_tag_name}',
                                  headers={
@@ -107,52 +83,44 @@ class Client(BaseClient):
                                  )
         return res
 
-    def build_iterator(self) -> list:
-        """Retrieves all entries from the feed.
+    def build_iterator(self, is_get_command: bool) -> list:
+        """
+        Retrieves all entries from the feed.
         Returns:
             A list of objects, containing the indicators.
         """
-
-        tag_details = []
-        futures = []
-        future_all_tags = []
-        all_tags = []
-        real_all_tags = []
-        total_count_of_tags = 3625
-        num_of_calls = total_count_of_tags // PAGE_SIZE + 1
-        for i in range(3):
-            future_all_tags.append(
-                EXECUTOR.submit(
-                    self.get_all_tags
-                )
-            )
-
-        for future in concurrent.futures.as_completed(future_all_tags):
-            all_tags.append(future.result())
-
-
-        demisto.debug("out from all tags")
-
-        for tags_list in all_tags:
-            tags = tags_list.get('tags', [])
-            real_all_tags.extend(tags)
-        demisto.debug("before the details")
-        for tag in real_all_tags:
+        results = []
+        if is_get_command:
+            page_num = 0
+        else:
+            integration_context = demisto.getIntegrationContext()
+            if not integration_context:
+                page_num = 0
+            else:
+                page_num = int(integration_context.get('page_num'))
+            if is_last_page_for_first_fetch():
+                pass
+        get_tags_response = self.get_tags(page_num)
+        tags = get_tags_response.get('tags', [])
+        demisto.debug(tags)
+        demisto.debug(f"page num{page_num}")
+        for tag in tags:
             public_tag_name = tag.get('public_tag_name', '')
-            demisto.debug(f"public tag name:{public_tag_name}")
-            if public_tag_name:
-                futures.append(
-                    EXECUTOR1.submit(
-                        self.get_tag_details,
-                        public_tag_name=public_tag_name,
-                    )
-                )
-        for future in concurrent.futures.as_completed(futures):
-            tag_details.append(future.result())
-        return tag_details
+            demisto.debug(f"puvlic tag name is {public_tag_name}")
+            tag_details_response = self.get_tag_details(public_tag_name)
+            results.append(tag_details_response)
+            demisto.debug(f"public tag name is {public_tag_name}")
+        if not is_get_command:
+            page_num += 1
+            set_integration_context({'page_num': str(page_num)})
+        return results
 
 
 ''' HELPER FUNCTIONS '''
+
+
+def is_last_page_for_first_fetch():
+    pass
 
 
 def get_tag_class(tag_class: Optional[str], source: Optional[str]) -> Optional[str]:
@@ -195,10 +163,11 @@ def get_fields(tag_details: Dict[str, Any]) -> Dict[str, Any]:
             fields['publications'].append({'link': url, 'title': title, 'source': source, 'timestamp': time_stamp})
     fields['aliases'] = tag_details.get('aliases')
     fields['description'] = tag.get('description')
-    fields['lastseenbysource'] = tag.get('lasthit')
-    fields['updateddate'] = tag.get('updated_at')
+    fields['lastseenbysource'] = tag.get('lasthit', '')
+    fields['updateddate'] = tag.get('updated_at', '')
     fields['threattypes'] = [{'threatcategory': tag_details.get('tag_groups')}]
     fields['reportedby'] = tag.get('source')
+    remove_nulls_from_dictionary(fields)
     return fields
 
 
@@ -221,10 +190,10 @@ def create_dict_of_all_tags(tags_list: list) -> Dict[str, Any]:
     return all_tags
 
 
-def create_relationships_for_tag(name: str, tag_type: str, related_tags: List[str], all_tags: Dict[str, Any]):
+def create_relationships_for_tag(client: Client, name: str, tag_type: str, related_tags: List[str]):
     relationships: list = []
     for related_tag in related_tags:
-        related_tag_details = all_tags.get(related_tag)
+        related_tag_details = client.get_tag_details(related_tag)
         if related_tag_details:
             tag = related_tag_details.get('tag')
             related_tag_name = tag.get('tag_name')
@@ -259,14 +228,18 @@ def test_module(client: Client) -> str:
         Outputs.
     """
 
-    client.build_iterator()
+    client.build_iterator(True)
     return 'ok'
 
 
-def fetch_indicators(client: Client, tlp_color: Optional[str] = None, feed_tags: List = [], limit: int = -1) \
-        -> List[Dict]:
+def fetch_indicators(client: Client,
+                     is_get_command: bool,
+                     tlp_color: Optional[str] = None,
+                     feed_tags: List = [],
+                     limit: int = -1) -> List[Dict]:
     """Retrieves indicators from the feed
     Args:
+        is_get_command: is this method called from the get-indicators-command
         client (Client): Client object with request
         tlp_color (str): Traffic Light Protocol color
         feed_tags (list): tags to assign fetched indicators
@@ -274,9 +247,8 @@ def fetch_indicators(client: Client, tlp_color: Optional[str] = None, feed_tags:
     Returns:
         Indicators.
     """
-    iterator = client.build_iterator()
+    iterator = client.build_iterator(is_get_command)
     indicators = []
-    relationships = []
     all_tags = create_dict_of_all_tags(iterator)
     # extract values from iterator
     for tag_details in iterator:
@@ -305,18 +277,15 @@ def fetch_indicators(client: Client, tlp_color: Optional[str] = None, feed_tags:
         }
         related_tags = tag_details.get('related_tags', [])
         if related_tags:
-            relationships.append(create_relationships_for_tag(value_,type_,related_tags,all_tags))
+            relationships = (create_relationships_for_tag(client, value_, type_, related_tags))
+            if relationships:
+                indicator_obj['relationships'] = relationships
         if feed_tags:
             indicator_obj['fields']['tags'] = feed_tags
 
         if tlp_color:
             indicator_obj['fields']['trafficlightprotocol'] = tlp_color
         indicators.append(indicator_obj)
-    dummy_indicator = {
-        "value": "$$DummyIndicator$$",
-        "relationships": relationships
-    }
-    indicators.append(dummy_indicator)
     return indicators
 
 
@@ -335,7 +304,7 @@ def get_indicators_command(client: Client,
     limit = int(args.get('limit', '10'))
     tlp_color = params.get('tlp_color')
     feed_tags = argToList(params.get('feedTags', ''))
-    indicators = fetch_indicators(client, tlp_color, feed_tags, limit)
+    indicators = fetch_indicators(client, True, tlp_color, feed_tags, limit)
     human_readable = tableToMarkdown('Indicators from AutoFocus Tags Feed:', indicators,
                                      headers=['value', 'type', 'fields'], headerTransform=string_to_table_header, removeNull=True)
     return CommandResults(
@@ -357,7 +326,7 @@ def fetch_indicators_command(client: Client, params: Dict[str, str]) -> List[Dic
     """
     feed_tags = argToList(params.get('feedTags', ''))
     tlp_color = params.get('tlp_color')
-    indicators = fetch_indicators(client, tlp_color, feed_tags)
+    indicators = fetch_indicators(client, False, tlp_color, feed_tags)
     return indicators
 
 
