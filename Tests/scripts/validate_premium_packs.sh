7223593b7a4f8bc76a12428e7983c1cb9be43477
nmaimon <nmaimon@paloaltonetworks.com>
2021-09-29 16:30:31 +0300

    removed '/packs' from paths

--- Tests/scripts/validate_premium_packs.sh
+++ Tests/scripts/validate_premium_packs.sh
@@ -19,16 +19,10 @@ if [[ -z $STORAGE_BASE_PATH ]]; then
 fi
 # ====== RUN VALIDATIONS ======
 
-if [[ -n "$STORAGE_BASE_PATH" ]]; then
-  echo "Validating index file in bucket at path gs://$GCS_MARKET_BUCKET/$STORAGE_BASE_PATH"
-else
-  echo "Validating index file in bucket at path gs://$GCS_MARKET_BUCKET/content/packs"
-fi
+echo "Validating index file in bucket at path gs://$GCS_MARKET_BUCKET/$STORAGE_BASE_PATH/packs"
+
 python3 ./Tests/scripts/validate_index.py -sa "$GCS_MARKET_KEY" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" -sb "$STORAGE_BASE_PATH/packs" -c "$BRANCH"
 
-if [[ -n "$STORAGE_BASE_PATH" ]]; then
-  echo "Validating premium packs in server against index file in bucket at path gs://$GCS_MARKET_BUCKET/$STORAGE_BASE_PATH."
-else
-  echo "Validating premium packs in server against index file in bucket at path gs://$GCS_MARKET_BUCKET/content/packs."
-fi
+echo "Validating premium packs in server against index file in bucket at path gs://$GCS_MARKET_BUCKET/$STORAGE_BASE_PATH/packs."
+
 python3 ./Tests/scripts/validate_premium_packs.py -sa "$GCS_MARKET_KEY" -e "$EXTRACT_FOLDER" -pb "$GCS_MARKET_BUCKET" -s "$SECRET_CONF_PATH" -a "$1" -sb "$STORAGE_BASE_PATH/packs"
