37e440b85d1d910bb5513134b062e1a9925b2550
teizenman <teizenman@paloaltonetworks.com>
2021-09-29 10:11:46 +0300

    enclose

--- Packs/Microsoft365Defender/Integrations/O365ATPSafeLinks/O365ATPSafeLinks.ps1
+++ Packs/Microsoft365Defender/Integrations/O365ATPSafeLinks/O365ATPSafeLinks.ps1
@@ -5,6 +5,44 @@ $COMMAND_PREFIX = "o365-atp-safe-links"
 $INTEGRATION_ENTRY_CONTEXT = "O365ATP.SafeLinks"
 
 
+function EncloseArgWithQuotes {
+    Param (
+        [string]$arg_value
+    )
+    if (!($arg_value)) {
+        return ""
+    }
+
+    $arrayed_value = $arg_value -split ","
+
+    for ($i = 0; $i -lt $arrayed_value.Count; $i++) {
+        $temp_val = $arrayed_value[$i]
+        if ($temp_val[0] -ne '"') {
+            $arrayed_value[$i] = '"' + $arrayed_value[$i].Trim() + '"'
+        }
+    }
+
+    $return_value = $arrayed_value -join ","
+
+    return $return_value
+    
+    <#
+    .DESCRIPTION
+    Encloses the value of an argument with double quotes and trim it. In the case it's a CSV value
+    it will do that for every value in the list.
+
+    .PARAMETER arg_value
+    The raw value of the argument.
+
+    .EXAMPLE
+    EncloseArgWithQuotes("a,b,c") -> "a","b","c"
+    EncloseArgWithQuotes("a, b,c ") -> "a","b","c"
+    EncloseArgWithQuotes("\"a\",b,c") -> "a","b","c"
+    EncloseArgWithQuotes("") -> ""
+    EncloseArgWithQuotes -> ""
+    #>
+}
+
 class ExchangeOnlinePowershellV2Client {
     [string]$url
     [System.Security.Cryptography.X509Certificates.X509Certificate2]$certificate
@@ -326,7 +364,7 @@ function GetPolicyListCommand {
         [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
         [hashtable]$kwargs
     )
-    $identity = $kwargs.identity
+    $identity = EncloseArgWithQuotes($kwargs.identity)
     $raw_response = $client.GetPolicyList(
         $identity
     )
@@ -342,19 +380,19 @@ function CreateUpdatePolicyCommand {
         [Parameter(Mandatory)][string]$command_type,
         [hashtable]$kwargs
     )
-    $name = $kwargs.name
-    $admin_display_name = $kwargs.admin_display_name
-    $custom_notification_text = $kwargs.custom_notification_text,
-    $deliver_message_after_scan = $kwargs.deliver_message_after_scan -as [bool]
-    $do_not_allow_click_through = $kwargs.do_not_allow_click_through -as [bool]
-    $do_not_rewrite_urls = $kwargs.do_not_rewrite_urls
-    $do_not_track_user_clicks = $kwargs.do_not_track_user_clicks -as [bool]
-    $enable_for_internal_senders = $kwargs.enable_for_internal_senders -as [bool]
-    $enable_organization_branding = $kwargs.enable_organization_branding -as [bool]
-    $enable_safe_links_for_teams = $kwargs.enable_safe_links_for_teams -as [bool]
-    $is_enabled = $kwargs.is_enabled -as [bool]
-    $scan_urls = $kwargs.scan_urls -as [bool]
-    $use_translated_notification_text = $kwargs.use_translated_notification_text -as [bool]
+    $name = EncloseArgWithQuotes($kwargs.name)
+    $admin_display_name = EncloseArgWithQuotes($kwargs.admin_display_name)
+    $custom_notification_text = EncloseArgWithQuotes($kwargs.custom_notification_text)
+    $deliver_message_after_scan = $kwargs.deliver_message_after_scan -eq "true"
+    $do_not_allow_click_through = $kwargs.do_not_allow_click_through -eq "true"
+    $do_not_rewrite_urls = EncloseArgWithQuotes($kwargs.do_not_rewrite_urls)
+    $do_not_track_user_clicks = $kwargs.do_not_track_user_clicks -eq "true"
+    $enable_for_internal_senders = $kwargs.enable_for_internal_senders -eq "true"
+    $enable_organization_branding = $kwargs.enable_organization_branding -eq "true"
+    $enable_safe_links_for_teams = $kwargs.enable_safe_links_for_teams -eq "true"
+    $is_enabled = $kwargs.is_enabled -eq "true"
+    $scan_urls = $kwargs.scan_urls -eq "true"
+    $use_translated_notification_text = $kwargs.use_translated_notification_text -eq "true"
 
     $raw_response = $client.CreateUpdatePolicy(
         $command_type,
@@ -383,7 +421,7 @@ function RemovePolicyCommand {
         [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
         [hashtable]$kwargs
     )
-    $identity = $kwargs.identity
+    $identity = EncloseArgWithQuotes($kwargs.identity)
     $raw_response = $client.RemovePolicy(
         $identity
     )
@@ -398,7 +436,7 @@ function GetRulesCommand {
         [Parameter(Mandatory)][ExchangeOnlinePowershellV2Client]$client,
         [hashtable]$kwargs
     )
-    $identity = $kwargs.identity
+    $identity = EncloseArgWithQuotes($kwargs.identity)
     $raw_response = $client.GetRules(
         $identity
     )
@@ -414,17 +452,17 @@ function CreateUpdateRuleCommand {
         [Parameter(Mandatory)][string]$command_type,
         [hashtable]$kwargs
     )
-    $name = $kwargs.name
-    $safe_links_policy = $kwargs.safe_links_policy
-    $comments = $kwargs.comments,
-    $enabled = $kwargs.enabled -as [bool]
-    $except_if_recipient_domain_is = $kwargs.except_if_recipient_domain_is
-    $except_if_sent_to = $kwargs.except_if_sent_to
-    $except_if_sent_to_member_of = $kwargs.except_if_sent_to_member_of
+    $name = EncloseArgWithQuotes($kwargs.name)
+    $safe_links_policy = EncloseArgWithQuotes($kwargs.safe_links_policy)
+    $comments = EncloseArgWithQuotes($kwargs.comments)
+    $enabled = $kwargs.enabled -eq "true"
+    $except_if_recipient_domain_is = EncloseArgWithQuotes($kwargs.except_if_recipient_domain_is)
+    $except_if_sent_to = EncloseArgWithQuotes($kwargs.except_if_sent_to)
+    $except_if_sent_to_member_of = EncloseArgWithQuotes($kwargs.except_if_sent_to_member_of)
     $priority = $kwargs.enable_for_internal_senders -as [Int32]
-    $recipient_domain_is = $kwargs.recipient_domain_is
-    $sent_to = $kwargs.sent_to
-    $sent_to_member_of = $kwargs.sent_to_member_of
+    $recipient_domain_is = EncloseArgWithQuotes($kwargs.recipient_domain_is)
+    $sent_to = EncloseArgWithQuotes($kwargs.sent_to)
+    $sent_to_member_of = EncloseArgWithQuotes($kwargs.sent_to_member_of)
 
     $raw_response = $client.CreateUpdateRule(
         $command_type,
