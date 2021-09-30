e4ae79579a56ab2c8a866864e30b2e171767157f
MosheGalitzky <57589449+moishce@users.noreply.github.com>
2021-09-30 15:10:24 +0300

    bump version to 21.9.1 (#15117)

--- .circleci/content_release_vars.sh
+++ .circleci/content_release_vars.sh
@@ -1,3 +1,3 @@
-export CONTENT_VERSION="21.9.0"
+export CONTENT_VERSION="21.9.1"
 export SERVER_VERSION="6.0.0"
-export GIT_SHA1="26b1cc1404a6edcb7ad13e28e41b9d4d0299fe19" # guardrails-disable-line disable-secrets-detection
+export GIT_SHA1="a3f8f071020fd8b42523c6f7bf31c66f8769fc61" # guardrails-disable-line disable-secrets-detection
