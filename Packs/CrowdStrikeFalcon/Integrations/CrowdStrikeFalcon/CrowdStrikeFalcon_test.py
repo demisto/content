58d8eb269748a826ad17c9669dff7aafbae1afc4
ierukhimovic <ierukhimovic@paloaltonetworks.com>
2021-10-03 11:33:50 +0300

    fixing genreate_status_field added tests

--- Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/CrowdStrikeFalcon_test.py
+++ Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/CrowdStrikeFalcon_test.py
@@ -2241,7 +2241,7 @@ class TestFetch:
             The `first_behavior_time` changes and no `offset` is added.
         """
         mocker.patch.object(demisto, 'getLastRun', return_value={'first_behavior_detection_time':
-                                                                 '2020-09-04T09:16:10Z', 'detection_offset': 2})
+                                                                     '2020-09-04T09:16:10Z', 'detection_offset': 2})
         # Override post to have 1 results so FETCH_LIMIT won't be reached
         requests_mock.post(f'{SERVER_URL}/detects/entities/summaries/GET/v1',
                            json={'resources': [{'detection_id': 'ldt:1',
@@ -2945,3 +2945,21 @@ def test_list_host_group_members(requests_mock):
     expected_results = load_json('test_data/expected_list_hostgroup_members_results.json')
     for expected_results, ectual_results in zip(expected_results, command_results.outputs):
         assert expected_results == ectual_results
+
+
+@pytest.mark.parametrize('endpoint_status, status, is_isolated',
+                         [('Normal', 'Online', ''),
+                          ('containment_pending', '', 'Pending isolation'),
+                          ('contained', '', 'Yes'),
+                          ('lift_containment_pending', '', 'Pending unisolation'),
+                          ])
+def test_generate_status_field(endpoint_status, status, is_isolated):
+    from CrowdStrikeFalcon import generate_status_fields
+    assert status, is_isolated == generate_status_fields(endpoint_status)
+
+
+def test_generate_status_field_invalid():
+    from CrowdStrikeFalcon import generate_status_fields
+    with pytest.raises(DemistoException):
+        generate_status_fields('unknown status')
+
