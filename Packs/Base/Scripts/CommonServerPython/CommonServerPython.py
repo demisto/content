6bcec7d2c43d27c3b9d3f22a7e28573ef7db16c5
ypreisler <ypreisler@paloaltonetworks.com>
2021-10-04 09:47:47 +0300

    using datetime

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -1873,7 +1873,7 @@ def tableToMarkdown(name, t, headers=None, headerTransform=None, removeNull=Fals
                 for field in date_fields:
 
                     try:
-                        entry_copy[field] = epochToTimestamp(int(entry_copy[field]), False)
+                        entry_copy[field] = datetime.fromtimestamp(int(entry_copy[field])/1000).strftime('%Y-%m-%d %H:%M:%S')
 
                     except BaseException:
                         pass
