4bb0d36b170f8e19e6ce634bfee44bc073d3e88c
nmaimon <nmaimon@paloaltonetworks.com>
2021-10-05 19:30:02 +0300

    fix

--- Utils/trigger_test_upload_flow.sh
+++ Utils/trigger_test_upload_flow.sh
@@ -85,12 +85,12 @@ if [ -n "$_force" ] && [ -n "$_storage_base_path"]; then
     exit 1
 fi
 if [[ -n "$_storage_base_path" ]] && [ "$_storage_base_path" != *content ]; then
-  echo "The given storage base path should look like upload-flow/builds/branch_name/build_number/content"
+  echo "The given storage base path should look like upload-flow/builds/branch_name/build_number/content."
   exit 1
 fi
 
 if [[ -n "$_storage_base_path" ]] && [ "$_storage_base_path" != upload-flow* ]; then
-  echo "The given storage base path should look like upload-flow/builds/branch_name/build_number/content"
+  echo "The given storage base path should look like upload-flow/builds/branch_name/build_number/content."
   exit 1
 fi
 
