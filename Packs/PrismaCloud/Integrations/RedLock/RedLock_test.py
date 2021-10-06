dadf2d5586b69c324de1481986a2f08d25ee4ca1
ierukhimovic <ierukhimovic@paloaltonetworks.com>
2021-10-06 14:13:44 +0300

    added empty case

--- Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
+++ Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+from freezegun import freeze_time
 import demistomock as demisto
 
 integration_params = {
@@ -21,6 +22,7 @@ def set_mocks(mocker):
     mocker.patch.object(demisto, 'params', return_value=integration_params)
 
 
+@freeze_time()
 def test_fetch_incident(mocker):
     from RedLock import fetch_incidents
     example_alerts = load_json('test_data/example_alert.json')
@@ -30,3 +32,12 @@ def test_fetch_incident(mocker):
     for incident, alert in zip(incidents, example_alerts):
         assert incident.get('name') == f"{alert.get('policy.name', 'No policy')} - {alert.get('id')}"
     assert last_seen_time == 1633109203697
+
+
+@freeze_time("2021-07-10T16:34:14.758295 UTC+1")
+def test_empty_incident(mocker):
+    from RedLock import fetch_incidents
+    mocker.patch('RedLock.req', return_value=[])
+    incidents, last_fetches, last_seen_time = fetch_incidents()
+    assert len(incidents) == 0, 'There are no alerts in example response'
+    assert last_seen_time == 1625668454000
