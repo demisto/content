b935208c0a543c11d1c8ead02429a2bdfbb86198
ierukhimovic <ierukhimovic@paloaltonetworks.com>
2021-10-07 10:08:24 +0300

    updated unit tests with limit param

--- Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
+++ Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
@@ -27,17 +27,18 @@ def test_fetch_incident(mocker):
     from RedLock import fetch_incidents
     example_alerts = load_json('test_data/example_alert.json')
     mocker.patch('RedLock.req', return_value=example_alerts)
-    incidents, last_fetches, last_seen_time = fetch_incidents()
+    incidents, last_fetches, last_seen_time = fetch_incidents(limit=50)
     assert len(incidents) == 2, 'There are two alerts in example response'
     for incident, alert in zip(incidents, example_alerts):
         assert incident.get('name') == f"{alert.get('policy.name', 'No policy')} - {alert.get('id')}"
     assert last_seen_time == 1633109203697
 
 
+
 @freeze_time("2021-07-10T16:34:14.758295 UTC+1")
 def test_empty_incident(mocker):
     from RedLock import fetch_incidents
     mocker.patch('RedLock.req', return_value=[])
-    incidents, last_fetches, last_seen_time = fetch_incidents()
+    incidents, last_fetches, last_seen_time = fetch_incidents(50)
     assert len(incidents) == 0, 'There are no alerts in example response'
     assert last_seen_time == 1625679254
