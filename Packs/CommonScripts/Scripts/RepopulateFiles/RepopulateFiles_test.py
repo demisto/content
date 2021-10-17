9c48e00b4b15d1b71ce89a2d7de6ff9b16a83e1f
spearmin10 <minoue@paloaltonetworks.com>
2021-10-17 16:17:50 +0900

    Modified the test code

--- Packs/CommonScripts/Scripts/RepopulateFiles/RepopulateFiles_test.py
+++ Packs/CommonScripts/Scripts/RepopulateFiles/RepopulateFiles_test.py
@@ -1,5 +1,6 @@
 from RepopulateFiles import main
 
+
 class TestRepopulateFiles:
     def test_main_no_entries(self, mocker):
         mocker.patch('RepopulateFiles.demisto.executeCommand', return_value=None)
