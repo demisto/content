75b0bcb8f9fccd3e80a821d6aa0f4d1ea073acaf
Daniel Pascual <daniel.s.pascual@gmail.com>
2021-10-18 16:02:58 +0200

    wip

--- Packs/VirusTotal/Integrations/FeedLivehunt/FeedLivehunt.py
+++ Packs/VirusTotal/Integrations/FeedLivehunt/FeedLivehunt.py
@@ -1,98 +1,189 @@
-import demistomock as demisto
-from CommonServerPython import *
 import urllib3
+
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
 
 # Disable insecure warnings
 urllib3.disable_warnings()
 
-INTEGRATION_NAME = 'VirusTotal Livehunt'
-
 
 class Client(BaseClient):
-    def __init__(self, params: dict):
-        super().__init__(
-            'https://www.virustotal.com/api/v3/',
-            verify=not argToBoolean(params.get('insecure')),
-            proxy=argToBoolean(params.get('proxy')),
-            headers={'x-apikey': params['credentials']['password']}
-        )
+    def build_iterator(self) -> List:
+        """Retrieves all entries from the feed.
+        Returns:
+            A list of objects, containing the indicators.
+        """
+
+        result = []
+
+        res = self._http_request('GET',
+                                 url_suffix='intelligence/hunting_notifications',
+                                 full_url=self._base_url,
+                                 resp_type='json',
+                                 )
+
+        try:
+            for indicator in res.get('data'):
+                result.append({
+                    'value': indicator.get('attributes'),
+                    'type': 'file',
+                    'FeedURL': self._base_url
+                })
+        except ValueError as err:
+            demisto.debug(str(err))
+            raise ValueError(f'Could not parse returned data as indicator. \n\nError message: {err}')
+        return result
 
 
-def test_module(client: Client, args: dict):
-    pass
+def test_module(client: Client, args: dict) -> str:
+    client.build_iterator()
+    return 'ok'
 
-
-def get_indicators_command(client: Client, args: dict):
-    """Initiate a single fetch-indicators
-
+def fetch_indicators(client: Client,
+                     tlp_color: Optional[str] = None,
+                     feed_tags: List = [],
+                     limit: int = -1) -> List[Dict]:
+    """Retrieves indicators from the feed
     Args:
-        client(Client): The AutoFocus Client.
-        args(dict): Command arguments.
-
+        client (Client): Client object with request
+        tlp_color (str): Traffic Light Protocol color
+        feed_tags (list): tags to assign fetched indicators
+        limit (int): limit the results
     Returns:
-        str, dict, list. the markdown table, context JSON and list of indicators
+        Indicators.
     """
-    offset = int(args.get('offset', 0))
-    limit = int(args.get('limit', 100))
-    indicators = fetch_indicators_command(client, limit, offset)
+    iterator = client.build_iterator()
+    indicators = []
+    if limit > 0:
+        iterator = iterator[:limit]
 
-    hr_indicators = []
-    for indicator in indicators:
-        hr_indicators.append({
-            'Value': indicator.get('value'),
-            'Type': indicator.get('type'),
-            'rawJSON': indicator.get('rawJSON'),
-            'fields': indicator.get('fields'),
-        })
+    # extract values from iterator
+    for item in iterator:
+        value_ = item
+        type_ = FeedIndicatorType.File
+        raw_data = {
+            'value': value_,
+            'type': type_,
+        }
 
-    human_readable = tableToMarkdown("Indicators from VirusTotal Livehunt:",
-                                     hr_indicators,
-                                     headers=['Value', 'Type', 'rawJSON', 'fields'],
+        # Create indicator object for each value.
+        # The object consists of a dictionary with required and optional keys and values, as described blow.
+        for key, value in item.items():
+            raw_data.update({key: value})
+        indicator_obj = {
+            # The indicator value.
+            'value': value_,
+            # The indicator type as defined in Cortex XSOAR.
+            # One can use the FeedIndicatorType class under CommonServerPython to populate this field.
+            'type': type_,
+            # The name of the service supplying this feed.
+            'service': 'VirusTotal',
+            # A dictionary that maps values to existing indicator fields defined in Cortex XSOAR.
+            # One can use this section in order to map custom indicator fields previously defined
+            # in Cortex XSOAR to their values.
+            'fields': {},
+            # A dictionary of the raw data returned from the feed source about the indicator.
+            'rawJSON': raw_data
+        }
+
+        if feed_tags:
+            indicator_obj['fields']['tags'] = feed_tags
+
+        if tlp_color:
+            indicator_obj['fields']['trafficlightprotocol'] = tlp_color
+
+        indicators.append(indicator_obj)
+
+    return indicators
+
+
+def get_indicators_command(client: Client,
+                           params: Dict[str, str],
+                           args: Dict[str, str]
+                           ) -> CommandResults:
+    """Wrapper for retrieving indicators from the feed to the war-room.
+    Args:
+        client: Client object with request
+        params: demisto.params()
+        args: demisto.args()
+    Returns:
+        Outputs.
+    """
+    limit = int(args.get('limit', 10))
+    tlp_color = params.get('tlp_color')
+    feed_tags = argToList(params.get('feedTags', ''))
+    indicators = fetch_indicators(client, tlp_color, feed_tags, limit)
+
+    human_readable = tableToMarkdown('Indicators from VirusTotal Livehunt Feed:',
+                                     indicators,
+                                     headers=['value', 'type'],
+                                     headerTransform=string_to_table_header,
                                      removeNull=True)
 
-    if args.get('limit'):
-        human_readable = human_readable + f"\nTo bring the next batch of indicators run:\n!vt-livehunt-get-indicators " \
-                                          f"limit={args.get('limit')} " \
-                                          f"offset={int(str(args.get('limit'))) + int(str(args.get('offset')))}"
-
-    return human_readable, {}, indicators
-
-
-def fetch_indicators_command(client, limit=None, offset=None):
-    pass
-
+    return CommandResults(
+        readable_output=human_readable,
+        outputs_prefix='',
+        outputs_key_field='',
+        raw_response=indicators,
+        outputs={},
+    )
 
 def main():
     """
-    PARSE AND VALIDATE INTEGRATION PARAMS
+    main function, parses params and runs command functions
     """
-    # params = demisto.params()
+
+    params = demisto.params()
+
+    # If your Client class inherits from BaseClient, SSL verification is
+    # handled out of the box by it, just pass ``verify_certificate`` to
+    # the Client constructor
+    insecure = not params.get('insecure', False)
+
+    # If your Client class inherits from BaseClient, system proxy is handled
+    # out of the box by it, just pass ``proxy`` to the Client constructor
+    proxy = params.get('proxy', False)
 
     command = demisto.command()
-    demisto.info(f'Command being called is {command}')
+    args = demisto.args()
+
+
+    demisto.debug(f'Command being called is {command}')
 
     try:
-        client = Client({})
-        commands = {
-            'test-module': test_module,
-            'vt-livehunt-get-indicators': get_indicators_command
-        }
+        client = Client(
+            base_url='https://www.virustotal.com/api/v3/',
+            verify=insecure,
+            proxy=proxy,
+            headers={'x-apikey': params['credentials']['password']}
+        )
 
-        if command == 'fetch-indicators':
-            indicators = fetch_indicators_command(client)
+        if command == 'test-module':
+            # This is the call made when pressing the integration Test button.
+            return_results(test_module(client, {}))
+
+        elif command == 'vt-livehunt-get-indicators':
+            # This is the command that fetches a limited number of indicators
+            # from the feed source and displays them in the war room.
+            return_results(get_indicators_command(client, params, args))
+
+        elif command == 'fetch-indicators':
+            # This is the command that initiates a request to the feed endpoint
+            # and create new indicators objects from the data fetched. If the
+            # integration instance is configured to fetch indicators, then this
+            # is the commandthat will be executed at the specified feed fetch
+            # interval.
+            indicators = fetch_indicators_command(client, params)
             for iter_ in batch(indicators, batch_size=2000):
                 demisto.createIndicators(iter_)
 
-        elif command in commands:
-
-            readable_output, outputs, raw_response = commands[command](client, demisto.args())
-            return_outputs(readable_output, outputs, raw_response)
         else:
             raise NotImplementedError(f'Command {command} is not implemented.')
 
-    except Exception as err:
-        err_msg = f'Error in {INTEGRATION_NAME} Integration. [{err}]'
-        return_error(err_msg)
+    # Log exceptions and return errors
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # Print the traceback
+        return_error(f'Failed to execute {command} command.\nError:\n{str(e)}')
 
 
 if __name__ in ['__main__', 'builtin', 'builtins']:
