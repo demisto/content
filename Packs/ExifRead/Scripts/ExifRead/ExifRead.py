5a2341ad8cf92d6c32d3be67f6e69a733d81baa9
sberman <sberman@paloaltonetworks.com>
2021-10-19 22:01:53 +0300

    Added test and fixed comments

--- Packs/ExifRead/Scripts/ExifRead/ExifRead.py
+++ Packs/ExifRead/Scripts/ExifRead/ExifRead.py
@@ -2,20 +2,32 @@ import demistomock as demisto  # noqa: F401
 import exifread
 from CommonServerPython import *  # noqa: F401
 
-res = demisto.getFilePath(demisto.args()['EntryID'])
-f = open(res['path'], 'rb')
 
-tags = exifread.process_file(f)
-arr = []
-for tag in tags.keys():
-    arr.append({'tag': str(tag), 'value': str(tags[tag])})
+def get_exif_tags(file_entry_id):
+    res = demisto.getFilePath(file_entry_id)
+    f = open(res['path'], 'rb')
 
-md = tableToMarkdown('Exif Tags', arr)
+    tags = exifread.process_file(f)
+    arr = []
+    for tag in tags.keys():
+        arr.append({'tag': str(tag), 'value': str(tags[tag])})
 
-demisto.results({
-    'ContentsFormat': formats['json'],
-    'Type': entryTypes['note'],
-    'Contents': {'Exif': arr},
-    'HumanReadable': md,
-    'EntryContext': {'Exif': arr}
-})
+    md = tableToMarkdown('Exif Tags', arr)
+
+    demisto.results({
+        'ContentsFormat': formats['json'],
+        'Type': entryTypes['note'],
+        'Contents': {'Exif': arr},
+        'HumanReadable': md,
+        'EntryContext': {'Exif': arr}
+    })
+
+
+def main():
+    file_entry_id = demisto.args()['EntryID']
+    get_exif_tags(file_entry_id)
+
+
+# python2 uses __builtin__ python3 uses builtins
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
