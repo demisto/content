4e76ea23abc67e6367c02a002ca9ea39230da789
merit <meretmaayta@gmail.com>
2021-10-20 16:52:03 +0300

    edits after build failed

--- Packs/FeedTAXII/Integrations/FeedTAXII2/FeedTAXII2_test.py
+++ Packs/FeedTAXII/Integrations/FeedTAXII2/FeedTAXII2_test.py
@@ -121,7 +121,7 @@ class TestFetchIndicators:
         - fetch 7 indicators
         - update last run with latest collection fetch time
         """
-        mock_client = Taxii2FeedClient(url='', collection_to_fetch=None, proxies=[], verify=False,objects_to_fetch='')
+        mock_client = Taxii2FeedClient(url='', collection_to_fetch=None, proxies=[], verify=False, objects_to_fetch='')
         id_1 = 1
         id_2 = 2
         mock_client.collections = [MockCollection(id_1, 'a'), MockCollection(id_2, 'b')]
