b3b84cc8b354578b2aba9d1ffa5cab2602b5f48c
tlieber <tlieber@paloaltonetworks.com>
2021-10-20 10:57:55 +0300

    fix test

--- Packs/EmailCommunication/Scripts/DisplayEmailHtml/DisplayEmailHtml_test.py
+++ Packs/EmailCommunication/Scripts/DisplayEmailHtml/DisplayEmailHtml_test.py
@@ -13,8 +13,8 @@ EMAIL_HTML = """
 
 EXPECTED_RESULT_1 = """
 <html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body><div dir="ltr">image 1:
-<div><div><img src=entry/download/37@119  alt="image_1.png" width="275" height="184"><br></div></div><div>image 2:
-</div><div><div><img src=entry/download/38@120  alt="image_2.png" width="225" height="224"><br></div></div></div><br>
+<div><div><img src=/entry/download/37@119  alt="image_1.png" width="275" height="184"><br></div></div><div>image 2:
+</div><div><div><img src=/entry/download/38@120  alt="image_2.png" width="225" height="224"><br></div></div></div><br>
 <div class="gmail_quote"><div dir="ltr" class="gmail_attr">On Thu, Oct 22, 2020 at 1:56 AM Avishai Brandeis &lt;
 <a href="mailto:avishai@demistodev.onmicrosoft.com">avishai@demistodev.onmicrosoft.com</a>&gt; wrote:<br></div>
 <blockquote class="gmail_quote" style="margin: 0px 0px 0px 0.8ex;border-left:1px solid rgb(204,204,204)"><u></u><div>
