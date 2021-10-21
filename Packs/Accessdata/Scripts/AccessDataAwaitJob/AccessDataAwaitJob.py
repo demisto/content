ec0f6f0ba6390c8bc89ed891e31fe2b49d83b499
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2021-10-21 10:06:04 +0100

    Added delay to polling time of AwaitJob script.

--- Packs/Accessdata/Scripts/AccessDataAwaitJob/AccessDataAwaitJob.py
+++ Packs/Accessdata/Scripts/AccessDataAwaitJob/AccessDataAwaitJob.py
@@ -1,6 +1,7 @@
 """ AccessData - Await Job Status """
 
-import traceback
+from time import sleep
+from traceback import format_exc
 
 from CommonServerPython import return_error
 import demistomock as demisto
@@ -12,9 +13,10 @@ def main():
         res = demisto.executeCommand('accessdata-api-get-job-status', **demisto.args())
         while res["accessdata.case.job.state"] not in ("InProgress", "Submitted"):
             res = demisto.executeCommand('accessdata-api-get-job-status', **demisto.args())
+            sleep(10)
         demisto.results(res)
     except Exception as ex:
-        demisto.error(traceback.format_exc())  # print the traceback
+        demisto.error(format_exc())  # print the traceback
         return_error(f'Failed to execute gather Job Status. Error: {str(ex)}')
 
 """ Entry Point """
