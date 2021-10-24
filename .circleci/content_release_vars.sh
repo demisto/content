09d252eaac4bece1ddad3d14ae83db38d2b2be44
mgalitzki <mgalitzki@paloaltonetworks.com>
2021-10-24 17:16:37 +0300

    last release

--- .circleci/content_release_vars.sh
+++ .circleci/content_release_vars.sh
@@ -1,3 +1,3 @@
-export CONTENT_VERSION="21.10.0"
+export CONTENT_VERSION="21.10.1"
 export SERVER_VERSION="6.0.0"
 export GIT_SHA1="c4ca75958f4e90fd7053990284b1a60145114d78" # guardrails-disable-line disable-secrets-detection
