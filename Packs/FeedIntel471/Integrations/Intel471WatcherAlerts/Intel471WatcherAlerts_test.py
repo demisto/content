9fcdd97a2498a0f2c5990382ce54c3dd0ae45699
Richard Hallick <rhallick@intel471.com>
2021-10-25 15:45:56 +0100

    Yet more changes required to get through validation

--- Packs/FeedIntel471/Integrations/Intel471WatcherAlerts/Intel471WatcherAlerts_test.py
+++ Packs/FeedIntel471/Integrations/Intel471WatcherAlerts/Intel471WatcherAlerts_test.py
@@ -1,5 +1,12 @@
 import pytest
+import json
+import io
 import Intel471WatcherAlerts as feed
+
+
+def util_load_json(path):
+    with io.open(path, mode='r', encoding='utf-8') as f:
+        return json.loads(f.read())
 
 
 GET_REPORT_TYPE_DATA = [
@@ -33,3 +40,55 @@ def test_get_report_type(mocker, input, expected_results):
     """
     report_type: str = feed.get_report_type(input)
     assert report_type == expected_results
+
+
+def test_fetch_incidents(requests_mock):
+    """Tests the fetch-incidents command function.
+
+    Configures requests_mock instance to generate the appropriate
+    get_alert API response, loaded from a local JSON file. Checks
+    the output of the command function with the expected output.
+    """
+    from Intel471WatcherAlerts import Client, fetch_incidents
+
+    mock_response = util_load_json('test_data/search_alerts.json')
+    requests_mock.get(
+        'https://test.com/api/v1/get_alerts?showRead=true&markAsRead=false&displayWatchers=true&count=1'
+        , json=mock_response['alerts'])
+
+    client = Client(
+        base_url='https://test.com/api/v1',
+        verify=False,
+        headers={
+            'Authentication': 'Bearer some_api_key'
+        }
+    )
+
+    last_run = {
+        'last_fetch': 1581944401  # Mon Feb 17 2020
+    }
+
+    latest_alert_uid, next_run, incidents = fetch_incidents(
+        client=client,
+        max_results=1,
+        last_run=last_run,
+        first_fetch_time=0,
+        watcher_group_uids=None,
+        severity='Medium',
+        last_alert_uid=''
+    )
+
+    assert incidents == [
+        {
+            'name': 'INSTANT MESSAGE:\nPart 1 sell part2',
+            'occurred': '2021-10-25T11:34:47.376Z',
+            'rawJSON': json.dumps(mock_response['alerts'][0]),
+            'type': 'Intel 471 Watcher Alert',
+            'severity': 2,
+            'CustomFields': {
+                'titanurl': 'https://api.intel471.com/v1/ims_thread/45678901234567890123456789012345?message_uid=34567890123456789012345678901234',
+                'titanwatchergroup': 'Test Watcher Group 1',
+                'titanwatcher': "Watcher on \"sell\""
+            }
+        }
+    ]
