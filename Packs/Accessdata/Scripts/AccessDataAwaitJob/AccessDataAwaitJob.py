5beba07021d93fc846aa1a4d3eef9517f7574583
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2021-10-27 09:15:48 +0100

    Updated script to remedy Flake8 complaints in Script.

--- Packs/Accessdata/Scripts/AccessDataAwaitJob/AccessDataAwaitJob.py
+++ Packs/Accessdata/Scripts/AccessDataAwaitJob/AccessDataAwaitJob.py
@@ -8,6 +8,7 @@ import demistomock as demisto
 
 """ Main Definition """
 
+
 def main():
     try:
         res = demisto.executeCommand('accessdata-api-get-job-status', **demisto.args())
@@ -18,6 +19,7 @@ def main():
     except Exception as ex:
         demisto.error(format_exc())  # print the traceback
         return_error(f'Failed to execute gather Job Status. Error: {str(ex)}')
+
 
 """ Entry Point """
 
