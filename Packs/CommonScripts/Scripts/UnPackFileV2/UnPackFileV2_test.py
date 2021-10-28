6c0ee8bdd127ff47c172790d6c9360d51037d7ec
sberman <sberman@paloaltonetworks.com>
2021-10-28 15:35:02 +0300

    Added TPB

--- Packs/CommonScripts/Scripts/UnPackFileV2/UnPackFileV2_test.py
+++ Packs/CommonScripts/Scripts/UnPackFileV2/UnPackFileV2_test.py
@@ -5,5 +5,4 @@ def test_extract(tmpdir):
     from UnPackFileV2 import extract
     rf = rarfile.RarFile('./test_data/rar3-archive-example.rar')
     res, files = extract(rf, tmpdir)
-    assert res.outputs == ['file2.txt', 'long fn.txt', 'file.txt', 'file1.txt']
-    assert len(files) == 4
+    assert set(res.outputs) == {'long fn.txt', 'file2.txt', 'file1.txt', 'file.txt'}
