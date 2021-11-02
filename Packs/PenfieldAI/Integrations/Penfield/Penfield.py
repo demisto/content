203bbe27de341c46f1a738c581dc626890c95f42
penfield-chan <84140868+penfield-chan@users.noreply.github.com>
2021-11-02 12:03:32 -0400

    remove code comment

--- Packs/PenfieldAI/Integrations/Penfield/Penfield.py
+++ Packs/PenfieldAI/Integrations/Penfield/Penfield.py
@@ -52,19 +52,7 @@ def get_assignee(client: Client, args) -> CommandResults:
     arg_id = args.get('id')
     name = args.get('name')
     severity = args.get('severity')
-
     return client.live_assign_get(analyst_ids, category, created, arg_id, name, severity)
-    # raw_response = client.live_assign_get(analyst_ids, category, created, arg_id, name, severity)
-
-    # analyst = raw_response.get('analyst')
-    # human_readable=tableToMarkdown('Analyst Penfield Recommends', analyst, headerTransform=pascalToSpace, removeNull=True)
-    # return CommandResults(
-    #         readable_output=human_readable,
-    #         outputs_prefix='Penfield.Recommended',
-    #         outputs_key_field='',
-    #         outputs=analyst
-    #   )
-
 
 def test_api(client: Client):
     return client.test()
