c80ef0b114d2b0e3b7033bdb001a602f8c7cdc30
mgalitzki <mgalitzki@paloaltonetworks.com>
2021-11-08 08:59:57 +0200

    bump version

--- .circleci/content_release_vars.sh
+++ .circleci/content_release_vars.sh
@@ -1,3 +1,3 @@
-export CONTENT_VERSION="21.10.1"
+export CONTENT_VERSION="21.11.0"
 export SERVER_VERSION="6.0.0"
 export GIT_SHA1="fe5feef33812c76e7dfe88206e49fda471f52e0c" # guardrails-disable-line disable-secrets-detection
