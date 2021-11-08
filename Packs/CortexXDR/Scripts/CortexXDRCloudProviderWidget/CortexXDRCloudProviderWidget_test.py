95e7916522f657f601281c7d5cb428e5652a954e
ilan <ierukhimovic@paloaltonetworks.com>
2021-11-08 10:38:49 +0200

    cloud test removed

---
+++ Packs/CortexXDR/Scripts/CortexXDRCloudProviderWidget/CortexXDRCloudProviderWidget_test.py
@@ -0,0 +1,13 @@
+"""Base Script for Cortex XSOAR - Unit Tests file
+
+Pytest Unit Tests: all funcion names must start with "test_"
+
+More details: https://xsoar.pan.dev/docs/integrations/unit-testing
+
+MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
+
+"""
+
+
+
+
