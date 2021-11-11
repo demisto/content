97725cc89513895997a2333696a748dc66a7b738
ybenshalom <ybenshalom@paloaltonetworks.com>
2021-11-11 13:00:53 +0200

    fix lint and set test playbook

--- Packs/FeedGCPWhitelist/Integrations/FeedGoogleIPRanges/FeedGoogleIPRanges.py
+++ Packs/FeedGCPWhitelist/Integrations/FeedGoogleIPRanges/FeedGoogleIPRanges.py
@@ -5,41 +5,35 @@ from JSONFeedApiModule import *  # noqa: E402
 
 feed_config = {
     'All GCP customer global and regional external IP ranges':
-        {
-            'CIDR': {
-                'url': 'https://www.gstatic.com/ipranges/cloud.json',
-                'extractor': "prefixes[]",
-                'indicator': 'ipv4Prefix',
-                'indicator_type': FeedIndicatorType.CIDR,
-            },
-            'IPv6CIDR': {
-                'url': 'https://www.gstatic.com/ipranges/cloud.json',
-                'extractor': "prefixes[]",
-                'indicator': 'ipv6Prefix',
-                'indicator_type': FeedIndicatorType.IPv6CIDR,
-            },
-        },
+        dict(CIDR={
+            'url': 'https://www.gstatic.com/ipranges/cloud.json',
+            'extractor': "prefixes[]",
+            'indicator': 'ipv4Prefix',
+            'indicator_type': FeedIndicatorType.CIDR,
+        }, IPv6CIDR={
+            'url': 'https://www.gstatic.com/ipranges/cloud.json',
+            'extractor': "prefixes[]",
+            'indicator': 'ipv6Prefix',
+            'indicator_type': FeedIndicatorType.IPv6CIDR,
+        }),
     'All available Google IP ranges':
-        {
-                'CIDR': {
-                    'url': 'https://www.gstatic.com/ipranges/goog.json',
-                    'extractor': "prefixes[]",
-                    'indicator': 'ipv4Prefix',
-                    'indicator_type': FeedIndicatorType.CIDR,
-                },
-                'IPv6CIDR': {
-                    'url': 'https://www.gstatic.com/ipranges/goog.json',
-                    'extractor': "prefixes[]",
-                    'indicator': 'ipv6Prefix',
-                    'indicator_type': FeedIndicatorType.IPv6CIDR,
-                },
-        },
+        dict(CIDR={
+            'url': 'https://www.gstatic.com/ipranges/goog.json',
+            'extractor': "prefixes[]",
+            'indicator': 'ipv4Prefix',
+            'indicator_type': FeedIndicatorType.CIDR,
+        }, IPv6CIDR={
+            'url': 'https://www.gstatic.com/ipranges/goog.json',
+            'extractor': "prefixes[]",
+            'indicator': 'ipv6Prefix',
+            'indicator_type': FeedIndicatorType.IPv6CIDR,
+        }),
 }
 
 
 def main():
     params = {k: v for k, v in demisto.params().items() if v is not None}
-    params['feed_name_to_config'] = feed_config.get(params.get('ip_ranges'))
+    params['feed_name_to_config'] = feed_config.get(str(params.get('ip_ranges')))
     feed_main(params, 'Google IP Ranges Feed', 'google-ip-ranges')
 
 
