d1e207571ee3ccb42a0b04b127b43b2f5cba8fbb
cshayner <cshayner@paloaltonetworks.com>
2021-11-15 09:20:53 +0200

    Add UT

--- Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern_test.py
+++ Packs/CommonScripts/Scripts/ExtractAttackPattern/ExtractAttackPattern_test.py
@@ -22,16 +22,6 @@ def test_extract_existing_mitre_ids(mocker):
     indicators = is_valid_attack_pattern(['T1530', 'T1602'])
     assert indicators == ['Data from Cloud Storage Object', 'Data from Configuration Repository']
 
-    mocker.patch.object(eap, 'get_mitre_results', side_effect=ValueError(
-        'verify you have proper integration enabled to support it'))
-    mocker.patch.object(demisto, 'info')
-
-    result = is_valid_attack_pattern(['T1530', 'T1602'])
-    assert not result
-    assert demisto.info.call_args[0][0] == 'Unsupported Command : mitre-get-indicator-name, ' \
-        'verify you have proper integration (MITRE ATTACK v2) enabled to support it. ' \
-        'This Is needed in order to auto extract MITRE IDs and translate them to Attack Pattern IOCs'
-
 
 def test_extract_non_existing_mitre_ids(mocker):
     mocker.patch.object(eap, 'get_mitre_results', return_value=[])
@@ -55,3 +45,34 @@ def test_extract_existing_mitre_id(mocker):
 
     indicators = is_valid_attack_pattern(['T1530'])
     assert indicators == ['Data from Cloud Storage Object']
+
+
+def test_extract_with_value_error(mocker):
+    mocker.patch.object(demisto, 'info')
+    mocker.patch.object(eap, 'get_mitre_results', side_effect=ValueError(
+        'verify you have proper integration enabled to support it'))
+    mocker.patch.object(demisto, 'info')
+
+    result = is_valid_attack_pattern(['T1530', 'T1602'])
+    assert not result
+    assert demisto.info.call_args[0][0] == 'Unsupported Command : mitre-get-indicator-name, ' \
+        'verify you have proper integration (MITRE ATTACK v2) enabled to support it. ' \
+        'This Is needed in order to auto extract MITRE IDs and translate them to Attack Pattern IOCs'
+
+    mocker.patch.object(eap, 'get_mitre_results', side_effect=ValueError(
+        'Something went wrong'))
+    mocker.patch.object(demisto, 'info')
+
+    result = is_valid_attack_pattern(['T1530', 'T1602'])
+    assert not result
+    assert demisto.info.call_args[0][0] == 'MITRE Attack formatting script, Something went wrong'
+
+
+def test_extract_with_unknown_error(mocker):
+    mocker.patch.object(eap, 'get_mitre_results', side_effect=TypeError(
+        'Something went wrong'))
+    mocker.patch.object(demisto, 'info')
+
+    result = is_valid_attack_pattern(['T1530', 'T1602'])
+    assert not result
+    assert demisto.info.call_args[0][0] == "MITRE Attack formatting script, Something went wrong"
