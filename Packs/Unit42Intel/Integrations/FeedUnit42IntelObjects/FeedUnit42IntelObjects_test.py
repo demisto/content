f740723d9dfa1672ce092f44bce0906a05bcb9ef
ikeren <itay@demisto.com>
2021-11-17 18:34:24 +0200

    fix lint reports

--- Packs/Unit42Intel/Integrations/FeedUnit42IntelObjects/FeedUnit42IntelObjects_test.py
+++ Packs/Unit42Intel/Integrations/FeedUnit42IntelObjects/FeedUnit42IntelObjects_test.py
@@ -1,9 +1,8 @@
-"""
-"""
-
-from AutoFocusTagsFeed import Client, fetch_indicators_command, incremental_level_fetch
-import json
 import io
+import json
+
+from FeedUnit42IntelObjects import (Client, fetch_indicators_command,
+                                    incremental_level_fetch)
 
 
 def util_load_json(path):
@@ -50,8 +49,8 @@ def test_fetch_indicators(mocker):
 def test_incremental_level_fetch(mocker):
     context = {'tags_need_to_be_fetched': [], 'time_of_first_fetch': 133033333}
     client = Client(api_key='1234', base_url='url', verify=False, proxy=False)
-    mocker.patch('AutoFocusTagsFeed.get_integration_context', return_value=context)
-    mocker.patch('AutoFocusTagsFeed.get_all_updated_tags_since_last_fetch', return_value=[{'publictag_name': 'Mock.mocktag1'}])
+    mocker.patch('FeedUnit42IntelObjects.get_integration_context', return_value=context)
+    mocker.patch('FeedUnit42IntelObjects.get_all_updated_tags_since_last_fetch', return_value=[{'publictag_name': 'Mock.mocktag1'}])  # noqa: E501
     mocker.patch.object(client, 'get_tag_details', return_value=util_load_json('test_data/tag_details_result.json'))
     actual_result = incremental_level_fetch(client)
     assert actual_result == [util_load_json('test_data/tag_details_result.json')]
