c85747f6d34e0fa2796339eea8ff05f97cb9b6db
tamarfink <tamar@cybersixgill.com>
2021-11-18 15:58:49 +0200

    fixed tests

--- Packs/Cybersixgill-ActionableAlerts/Integrations/CybersixgillActionableAlerts/CybersixgillActionableAlerts_test.py
+++ Packs/Cybersixgill-ActionableAlerts/Integrations/CybersixgillActionableAlerts/CybersixgillActionableAlerts_test.py
@@ -4,26 +4,14 @@ import demistomock as demisto
 
 incidents_list = [
     {'alert_name': 'Your organization was potentially targeted by a ransomware group',
-     'content': 'Poirier\xa0Sport\xa0Complex\xa0(PSLC)\xa0Arena\xa03\xa0Conversion\xa0–\xa0Phase\xa02,'
-                '\xa0Coquitlam\xa0BC\xa0 \nCapilano\xa0University\xa0Library\xa0Reno\xa0&#x2F;\xa0Center\xa0for'
-                '\xa0Student\xa0Success\xa0–\xa0Phase\xa02\xa0 '
-                '\n@sixgill-start-highlight@Walmart@sixgill-end-highlight@\xa03042\xa0Kelowna\xa0Relay\nCity\xa0of'
-                '\xa0Vancouver\xa02780\xa0East\xa0Broadway\nCompany:Traugott Building Contractors Inc.',
+     'content': 'text',
      'date': '2021-11-08 06:01:05', 'id': '6188bd21017198385e228437', 'read': True, 'severity': 1,
      'site': 'rw_everest', 'status': {'name': 'in_treatment', 'user': '60b604a048ce2cb294629a2d'}, 'threat_level':
          'imminent', 'threats': ['Brand Protection', 'Data Leak'],
      'title': 'Your organization was potentially targeted '
               'by a ransomware group', 'user_id':
          '5d233575f8db38787dbe24b6'}, {'alert_name': 'Gift Cards of {organization_name} are Sold on the Underground ',
-                                       'category': 'regular', 'content': 'New carded gift cards\nHi fellow friend and '
-                                                                         'business dealer here I got any kind of gift '
-                                                                         'cards you want and I carded by me. I can send '
-                                                                         'it to your address or give you code.especially '
-                                                                         '..Amazon ,Gift Card,Walmart ,Gift Card,Ebay ,'
-                                                                         'Gift Card,BestBuy ,Gift CardTarget ,'
-                                                                         'Xbox Gift Card,Psn Gift Card,Nordstrom Gift '
-                                                                         'Cardsand Nike Gift Cards  interested can '
-                                                                         'contact me on  telegram...@kartel25',
+                                       'category': 'regular', 'content': 'text',
                                        'date': '2021-11-02 06:00:27', 'id': '6180d4011dbb8edcb496ec8b',
                                        'lang': 'English', 'langcode': 'en', 'read': False, 'severity': 1, 'status':
                                            {'name': 'treatment_required', 'user': '604f58a6dc7c8a8437fd8154'},
@@ -34,10 +22,7 @@ incidents_list = [
         'alert_name': "Access to {matched_domain_names}, One of {organization_name}'s Assets, was Compromised and "
                       "Offered for Sale on a Compromised Endpoint Market",
         'category': 'regular',
-        'content': 'Bots head info:\n COUNTRY   |  RESOURCES   |  BROWSERS   |   INSTALLED   |   UPDATED   |  IP  |  '
-                   'OS  |  PRICE USD   \n PL  |  17  |  2  |  2021-10-19 19:57:25  |  2021-10-27 09:39:16  |  '
-                   '79.163...  |  Windows 10 Home  |  1.00   \nBrowsers for Genesis Security: \n Last update info: : '
-                   '2021-10-27 09:39:16             \n 384E75E0851A820644B79EC124865B75 ',
+        'content': 'text',
         'date': '2021-11-02 06:00:16', 'id': '6180d3f01dbb8edcb496ec86', 'lang': 'English', 'langcode': 'en',
         'read': False, 'severity': 1, 'sub_alerts': [], 'threat_level': 'imminent',
         'threats': ['Compromised Accounts'],
@@ -57,8 +42,7 @@ info_item = {
     "alert_id": "616ffed97a1b66036a138f73",
     "alert_name": "Your organization was potentially targeted by a ransomware group",
     "alert_type": "QueryBasedManagedAlertRule",
-    "assessment": "This could indicate that \"Walmart\" is being actively attacked by a ransomware campaign, "
-                  "or that its data has already been compromised and dumped publicly on the site.",
+    "assessment": "text",
     "category": "regular",
     "content_type": "search_result_item",
     "description": "A ransomware group posted on its leak site, rw_everest, focusing on \"Walmart\" ",
@@ -96,15 +80,7 @@ content_item = {
                     "category": "Ransomware",
                     "collection_date": "2021-05-11T13:11:46",
                     "comments_count": 0,
-                    "content": "Full documentation includes architecture, electricity, structure, security and more "
-                               ".\n\nPoirier Sport Complex (PSLC) Arena 3 Conversion – Phase 2, Coquitlam BC  \nCapilano "
-                               "University Library Reno / Center for Student Success – Phase 2  \nWalmart 3042 Kelowna "
-                               "Relay\n\nCity of Vancouver 2780 East Broadway\n\nCompany:Traugott Building Contractors Inc.\n "
-                               "Address:3740 11A Street NE, Unit 101B \nCalgary, Alberta T2E 6M6 Canada\n Website: "
-                               "http://traugott.com \n Email:bids@traugott.com\n Phone:(403) 276-6444\n Files: "
-                               "Traugott_Building_Contractors_Inc.zip ("
-                               "http://ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad.onion"
-                               "/Traugott_Building_Contractors_Inc.zip) \n Published data: 2 GB\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                    "content": "text",
                     "creator": "Everest ransom team",
                     "date": "2021-05-11T13:11:46",
                     "enrichment_version": 46,
@@ -133,10 +109,10 @@ content_item = {
                     ],
                     "pds": {
                         "email_address": [
-                            "bids@traugott.com"
+                            "example@com"
                         ],
                         "phone_number": [
-                            "4032766444"
+                            "000000"
                         ]
                     },
                     "product": [
@@ -165,15 +141,7 @@ content_item = {
                     "category": "Ransomware",
                     "collection_date": "2021-11-07T14:55:26",
                     "comments_count": 0,
-                    "content": "Full documentation includes architecture, electricity, structure, security and more "
-                               ".\n\nPoirier Sport Complex (PSLC) Arena 3 Conversion – Phase 2, Coquitlam BC  \nCapilano "
-                               "University Library Reno / Center for Student Success – Phase 2  \nWalmart 3042 Kelowna "
-                               "Relay\n\nCity of Vancouver 2780 East Broadway\n\nCompany:Traugott Building Contractors Inc.\n "
-                               "Address:3740 11A Street NE, Unit 101B \nCalgary, Alberta T2E 6M6 Canada\n Website: "
-                               "http://traugott.com [http://www.traugott.com/]  \n Email:bids@traugott.com\n Phone:(403) "
-                               "276-6444\n Files: Traugott_Building_Contractors_Inc.zip ("
-                               "http://ransomocmou6mnbquqz44ewosbkjk3o5qjsl3orawojexfook2j7esad.onion"
-                               "/Traugott_Building_Contractors_Inc.zip) \n Published data: 2 GB\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n",
+                    "content": "text",
                     "creator": "Everest ransom team",
                     "date": "2021-11-07T14:55:26",
                     "enrichment_version": 46,
@@ -199,10 +167,10 @@ content_item = {
                     ],
                     "pds": {
                         "email_address": [
-                            "bids@traugott.com"
+                            "example@com"
                         ],
                         "phone_number": [
-                            "4032766444"
+                            "000000"
                         ]
                     },
                     "rep_grade": 1,
@@ -234,10 +202,7 @@ expected_alert_output = [{'name': 'Your organization was potentially targeted by
                                            'cybersixgillportalurl': 'https://portal.cybersixgill.com'
                                                                     '/#/?actionable_alert=6188bd21017198385e228437',
                                            'cybersixgillthreattype': ['Brand Protection', 'Data Leak'],
-                                           'cybersixgillassessment': 'This could indicate that "Walmart" is being '
-                                                                     'actively attacked by a ransomware campaign, '
-                                                                     'or that its data has already been compromised '
-                                                                     'and dumped publicly on the site.',
+                                           'cybersixgillassessment': 'text',
                                            'cybersixgillrecommendations': '',
                                            'cybersixgillstatus': 'In Treatment',
                                            'cybersixgillsite': 'rw_everest',
@@ -252,9 +217,7 @@ expected_alert_output = [{'name': 'Your organization was potentially targeted by
                                      '"5fd0d2acddd06410ac5348d1", "vendor": "Sixgill"}, "alert_id": '
                                      '"616ffed97a1b66036a138f73", "alert_name": "Your organization was potentially '
                                      'targeted by a ransomware group", "alert_type": "QueryBasedManagedAlertRule", '
-                                     '"assessment": "This could indicate that \\"Walmart\\" is being actively '
-                                     'attacked by a ransomware campaign, or that its data has already been '
-                                     'compromised and dumped publicly on the site.", "category": "regular", '
+                                     '"assessment": "text", "category": "regular", '
                                      '"content_type": "search_result_item", "description": "A ransomware group posted '
                                      'on its leak site, rw_everest, focusing on \\"Walmart\\" ", '
                                      '"es_id": "51baa80bb1a01ba4b4a08f59a2313ff60e78bf75", "es_item": {}, '
