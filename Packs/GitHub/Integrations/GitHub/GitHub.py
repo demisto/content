79c51357f0f6c40f1e86430466a1fbae132c5a18
ShahafBenYakir <sbenyakir@paloaltonetworks.com>
2021-11-18 16:17:28 +0100

    Added te fix and the RN

--- Packs/GitHub/Integrations/GitHub/GitHub.py
+++ Packs/GitHub/Integrations/GitHub/GitHub.py
@@ -1021,9 +1021,9 @@ def get_branch_command():
     branch_name = args.get('branch_name')
     response = get_branch(branch_name)
 
-    commit = response.get('commit', {})
-    author = commit.get('author', {})
-    parents = commit.get('parents', [])
+    commit = response.get('commit', {}) or {}
+    author = commit.get('author', {}) or {}
+    parents = commit.get('parents', []) or []
     ec_object = {
         'Name': response.get('name'),
         'CommitSHA': commit.get('sha'),
