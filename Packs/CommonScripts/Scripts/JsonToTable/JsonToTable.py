1e1c260baa42b956073585d46dfaea5b16f1f445
ilan <ierukhimovic@paloaltonetworks.com>
2021-11-21 10:23:25 +0200

    added rn moved script to jsontotable

--- Packs/CommonScripts/Scripts/JsonToTable/JsonToTable.py
+++ Packs/CommonScripts/Scripts/JsonToTable/JsonToTable.py
@@ -7,9 +7,15 @@ def main():
     value = args.get('value')
     title = args.get('title')
     headers = argToList(args.get('headers'))
-
-    markdown = tableToMarkdown(title, value, headers=headers)
-
+    is_auto_json_transform = argToBoolean(args.get('is_auto_json_transform', False))
+    json_transform_properties = args.get('json_transform_properties')
+    json_transformers = {}
+    if json_transform_properties:
+        json_transform_properties = safe_load_json(json_transform_properties)
+        for header_key, values in json_transform_properties.items():
+            json_transformers[header_key] = JsonTransformer(**values)
+    markdown = tableToMarkdown(title, value, headers=headers, json_transform=json_transformers,
+                               is_auto_json_transform=is_auto_json_transform)
     return_results(markdown)
 
 
