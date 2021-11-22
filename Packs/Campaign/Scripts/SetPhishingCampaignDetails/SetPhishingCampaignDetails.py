6008eff1f97a016365914e2fa7bb2518427fed8d
jgranot <jgranot@paloaltonetworks.com>
2021-11-22 09:12:39 +0200

    remove option for multiple incidents.

--- Packs/Campaign/Scripts/SetPhishingCampaignDetails/SetPhishingCampaignDetails.py
+++ Packs/Campaign/Scripts/SetPhishingCampaignDetails/SetPhishingCampaignDetails.py
@@ -1,3 +1,5 @@
+import traceback
+
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
@@ -9,7 +11,12 @@ def get_campaign_context():
     current_incident_campaing_data = current_incident_context.get(EMAIL_CAMPAIGN_KEY)
     return current_incident_campaing_data
 
+
 def copy_campaign_data_to_incident(incident_id: int, campaign_data: dict, append: bool):
+    if not campaign_data:
+        demisto.debug(f"Error - {EMAIL_CAMPAIGN_KEY} was not found. Ignoring incident id: {incident_id}")
+        return
+
     args = {'key': EMAIL_CAMPAIGN_KEY, 'value': campaign_data, 'append': append}
 
     res = demisto.executeCommand(
@@ -21,19 +28,25 @@ def copy_campaign_data_to_incident(incident_id: int, campaign_data: dict, append
         }
     )
     if is_error(res):
-        return_error(res)
+        return_error(f"error in setting context to incident id {incident_id}. Error: {res}")
+
     return res
 
+
 def main():
-    args = demisto.args()
-    incident_id = args['id']
-    append = argToBoolean(args['append'])
+    try:
+        args = demisto.args()
+        incident_id = args['id']
+        append = argToBoolean(args['append'])
 
-    campaign_data = get_campaign_context()
-    res = copy_campaign_data_to_incident(incident_id, campaign_data, append)
+        campaign_data = get_campaign_context()
+        res = copy_campaign_data_to_incident(incident_id, campaign_data, append)
+        if res:
+            demisto.results(res)
 
-    demisto.results(res)
-
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to set campaign details.\nError:\n{str(e)}')
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
--- Packs/Campaign/Scripts/SetPhishingCampaignDetails/SetPhishingCampaignDetails_test.py
+++ Packs/Campaign/Scripts/SetPhishingCampaignDetails/SetPhishingCampaignDetails_test.py
@@ -19,17 +19,56 @@ CONTEXT_MOCK_CASES = [
 
 @pytest.mark.parametrize('context_mock, expected_results', CONTEXT_MOCK_CASES)
 def test_get_campaign_context(mocker, context_mock, expected_results):
+    """
+    Given:  context with campaign data (has EmailCampaign key) and other data
+            empty context
+            context without campaign data
+    When:   Getting context from the original incident in order to put it in other incident
+    Then:   Validate we take only the campaign data part.
+    """
     from SetPhishingCampaignDetails import get_campaign_context
     mocker.patch.object(demisto, 'context', return_value=context_mock)
     res = get_campaign_context()
     assert res == expected_results
 
 
-def test_copy_campaign_data_to_incident(mocker):
+CONTEXT_COPY_CASES = [
+    (CONTEXT_WITH_CAMPAIGN.get("EmailCampaign"), {'incidents': 1, 'command': 'Set', 'arguments':
+        {'key': 'EmailCampaign', 'value': {'field_example': 'field_example', 'field_example_2': 'field_example'},
+         'append': False}}),
+]
+
+
+@pytest.mark.parametrize('context_mock, expected_results', CONTEXT_COPY_CASES)
+def test_copy_campaign_data_to_incident(mocker, context_mock, expected_results):
+    """
+    Given:  context with only campaign data
+    When:   adding context from the original incident to other incident
+    Then:   Validate we call the set command with the correct arguments.
+    """
     from SetPhishingCampaignDetails import copy_campaign_data_to_incident
-    expected_args = {'incidents': 1, 'command': 'Set', 'arguments': {'key': 'EmailCampaign', 'value': {
-        'EmailCampaign': {'field_example': 'field_example', 'field_example_2': 'field_example'},
-        'NotCampaign': 'field_example'}, 'append': False}}
     execute_mock = mocker.patch.object(demisto, 'executeCommand')
-    copy_campaign_data_to_incident(1, CONTEXT_WITH_CAMPAIGN, False)
-    execute_mock.assert_called_once_with('executeCommandAt', expected_args)
+    copy_campaign_data_to_incident(1, context_mock, False)
+    execute_mock.assert_called_once_with('executeCommandAt', expected_results)
+
+
+CONTEXT_COPY_CASES_FAILS = [
+    (EMPTY_CONTEXT),
+    (None)
+]
+
+
+@pytest.mark.parametrize('context_mock', CONTEXT_COPY_CASES_FAILS)
+def test_copy_campaign_data_to_incident_fails(mocker, context_mock):
+    """
+        Given:  empty context
+                context without campaign data
+        When:   adding context from the original incident to other incident
+        Then:   Validate we ignore the incident if no campaign data is found with correct error logged.
+    """
+    from SetPhishingCampaignDetails import copy_campaign_data_to_incident, EMAIL_CAMPAIGN_KEY
+    debugging_mock = mocker.patch.object(demisto, 'debug')
+    execute_mock = mocker.patch.object(demisto, 'executeCommand')
+    copy_campaign_data_to_incident(1, context_mock, False)
+    debugging_mock.assert_called_once_with(f'Error - {EMAIL_CAMPAIGN_KEY} was not found. Ignoring incident id: 1')
+    execute_mock.assert_not_called()
