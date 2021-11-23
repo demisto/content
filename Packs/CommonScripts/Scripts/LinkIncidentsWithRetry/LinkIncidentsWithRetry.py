06801c3e178947ae007e17b9215fc7f39666e8db
Dan <dansterenson@gmail.com>
2021-11-23 14:46:48 +0200

    flake8 fix

--- Packs/CommonScripts/Scripts/LinkIncidentsWithRetry/LinkIncidentsWithRetry.py
+++ Packs/CommonScripts/Scripts/LinkIncidentsWithRetry/LinkIncidentsWithRetry.py
@@ -6,11 +6,11 @@ linkedIncidentIDs = args.get("linkedIncidentIDs")
 
 counter = int(args.get("retryLimit"))
 
-res = demisto.executeCommand("linkIncidents", {"linkedIncidentIDs":linkedIncidentIDs})
+res = demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": linkedIncidentIDs})
 
-while isError(res[0]) and counter>0 and ("DB Version" in res[0].get("Contents")):
+while isError(res[0]) and counter > 0 and ("DB Version" in res[0].get("Contents")):
     time.sleep(2)
     demisto.error(f"{counter} retry linkIncidents:{linkedIncidentIDs}")
-    res = demisto.executeCommand("linkIncidents", {"linkedIncidentIDs":linkedIncidentIDs})
+    res = demisto.executeCommand("linkIncidents", {"linkedIncidentIDs": linkedIncidentIDs})
     counter -= 1
 return_results(res)
