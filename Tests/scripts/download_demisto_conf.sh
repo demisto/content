45f8ca591ceae0c3f7da28a0af86c69f5fd069d6
Dan <dansterenson@gmail.com>
2021-11-24 09:49:43 +0200

    test new clone path

--- Tests/scripts/download_demisto_conf.sh
+++ Tests/scripts/download_demisto_conf.sh
@@ -22,20 +22,12 @@ git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@code.pan.run/xsoar/c
 if [ "$?" != "0" ]; then
     echo "No such branch in content-test-conf: $UNDERSCORE_BRANCH , falling back to master"
     git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@code.pan.run/xsoar/content-test-conf.git
-    cp -r ./content-test-conf-master/awsinstancetool ./Tests/scripts/awsinstancetool
-    cp -r ./content-test-conf-master/demisto.lic $DEMISTO_LIC_PATH
-    cp -r ./content-test-conf-master/conf.json $SECRET_CONF_PATH
-    cp -r ./content-test-conf-master/signDirectory $DEMISTO_PACK_SIGNATURE_UTIL_PATH
-    rm -rf ./content-test-conf-master
-    rm -rf ./test_configuration.zip
-  else
-    cp -r ./content-test-conf-$UNDERSCORE_BRANCH/awsinstancetool ./Tests/scripts/awsinstancetool
-    cp -r ./content-test-conf-$UNDERSCORE_BRANCH/demisto.lic $DEMISTO_LIC_PATH
-    cp -r ./content-test-conf-$UNDERSCORE_BRANCH/conf.json $SECRET_CONF_PATH
-    cp -r ./content-test-conf-$UNDERSCORE_BRANCH/signDirectory $DEMISTO_PACK_SIGNATURE_UTIL_PATH
-    rm -rf ./content-test-conf-$UNDERSCORE_BRANCH
-    rm -rf ./test_configuration.zip
 fi
+cp -r ./content-test-conf/awsinstancetool ./Tests/scripts/awsinstancetool
+cp -r ./content-test-conf/demisto.lic $DEMISTO_LIC_PATH
+cp -r ./content-test-conf/conf.json $SECRET_CONF_PATH
+cp -r ./content-test-conf/signDirectory $DEMISTO_PACK_SIGNATURE_UTIL_PATH
+rm -rf ./content-test-conf
 
 set -e
 echo "Successfully downloaded configuration files"
