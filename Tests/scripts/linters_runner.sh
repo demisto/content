34231249fbd5a886643e3fb534ed0296d31eff1b
yaakovi <syaakovi@paloaltonetworks.com>
2021-11-29 00:08:54 +0200

    remove errors from pylint ignore list

--- Tests/scripts/linters_runner.sh
+++ Tests/scripts/linters_runner.sh
@@ -3,9 +3,11 @@
 
 # Run flake8 pylint and mypy on all excluding Packs, (Integraions and Scripts) - they will be handled in linting
 errors=0
-all_dirs=$(find . -type d -not \( -path "./.*" -o -path "./Templates*" -o -path "./TestPlaybooks*" -o -path "./node_modules*" -o -path "./venv*" -o -path "./Packs*" -o -path "*infrastructure_tests*" -o -path "*scripts/awsinstancetool*" \))
-all_1_depth_dirs=$(find . -maxdepth 1 -type d -not \( -path . -o -path ./Packs -o -path ./venv -o -path ./Templates -o -path ./TestPlaybooks -o -path ./node_modules -o -path "./.*" \))
+all_dirs=$(find . -type d -not \( -path "./.*" -o -path "./Templates*" -o -path "./TestPlaybooks*" -o -path "./node_modules*" -o -path "./venv*" -o -path "./Packs*" -o -path "*infrastructure_tests*" -o -path "*scripts/awsinstancetool*" -o -path "./docs*" \))
+all_1_depth_dirs=$(find . -maxdepth 1 -type d -not \( -path . -o -path ./Packs -o -path ./venv -o -path ./Templates -o -path ./TestPlaybooks -o -path ./node_modules -o -path "./.*" -o -path ./docs \))
 
+echo -e "all dirs:\n${all_dirs}\n"
+echo -e "all_1_depth_dirs:\n${all_1_depth_dirs}\n"
 # run mypy
 ./Tests/scripts/mypy.sh $all_dirs || errors=$?
 
