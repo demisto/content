4dc4c15aba3e3d63473c0ecb285e4ceff9540d61
Darya Koval <dkoval@paloaltonetworks.com>
2021-12-05 17:20:01 +0200

    saving all changes before moving from fastapi to flask

--- Packs/TAXIIServer/Integrations/TAXII2Server/TAXII2Server.py
+++ Packs/TAXIIServer/Integrations/TAXII2Server/TAXII2Server.py
@@ -1,4 +1,5 @@
 import functools
+import uuid
 from copy import copy
 from tempfile import NamedTemporaryFile
 from typing import Callable
@@ -16,7 +17,7 @@ import uvicorn
 
 ''' GLOBAL VARIABLES '''
 INTEGRATION_NAME: str = 'TAXII Server'
-API_ROOT = '/threatintel/'
+API_ROOT = 'threatintel'
 APP: FastAPI = FastAPI()
 NAMESPACE_URI = 'https://www.paloaltonetworks.com/cortex'
 MEDIA_TYPE_TAXII_ANY = "application/taxii+json"
@@ -28,6 +29,8 @@ MEDIA_TYPE_STIX_V20 = "application/vnd.oasis.stix+json; version=2.0"
 ACCEPT_TYPE_ALL = '*/*'
 TAXII_VER_2_0 = "2.0"
 TAXII_VER_2_1 = "2.1"
+PAWN_UUID = uuid.uuid5(uuid.NAMESPACE_URL, 'https://www.paloaltonetworks.com')
+SCO_DET_ID_NAMESPACE = uuid.UUID("00abedb4-aa42-466c-9c01-fed23315a9b7")
 
 
 class TAXII2ServerAccessFormatter(AccessFormatter):
@@ -66,34 +69,25 @@ class TAXII2Server:
             credentials: The user credentials.
             version: API version.
         """
-        self.url_scheme = url_scheme
-        self.host = host
-        self.port = port
-        self.certificate = certificate
-        self.private_key = private_key
-        self.http_server = http_server
-        self.service_address = service_address
-        self.auth = None
+        self._url_scheme = url_scheme
+        self._host = host
+        self._port = port
+        self._certificate = certificate
+        self._private_key = private_key
+        self._http_server = http_server
+        self._service_address = service_address
+        self._auth = None
         if credentials:
-            self.auth = (credentials.get('identifier', ''), credentials.get('password', ''))
-        self.api_root = API_ROOT
+            self._auth = (credentials.get('identifier', ''), credentials.get('password', ''))
+        self._api_root = API_ROOT
         self.version = version
         if not (version == TAXII_VER_2_0 or version == TAXII_VER_2_1):
             raise Exception(f'Wrong TAXII 2 Server version: {version}. '
                             f'Possible values: {TAXII_VER_2_0}, {TAXII_VER_2_1}.')
-        collections_resource = []
-        for name, query in collections.items():
-            collection = {
-                'id': 123,  # TODO: RFC 4122-compliant Version 4 UUID
-                'title': name,
-                'description': query,
-                'can_read': True,
-                'can_write': False,
-                'media_types': [SERVER.taxii_collections_media_type]
-            }
-            collections_resource.append(collection)
-
-        self.collections = collections
+        self._collections_resource = []
+        self._collections_by_id = dict()
+        self._seed = uuid.uuid5(PAWN_UUID, demisto.getLicenseID())
+        self.create_collections(collections)
 
     @property
     def taxii_collections_media_type(self):
@@ -127,6 +121,29 @@ class TAXII2Server:
             api_root_version = MEDIA_TYPE_TAXII_V21
         return api_root_version
 
+    @property
+    def auth(self):
+        return self._auth
+
+    def create_collections(self, collections):
+        collections_resource = []
+        collections_by_id = dict()
+        for name, query in collections.items():
+            collection_uuid = str(uuid.uuid5(self._seed, 'Collection_' + name))
+            collection = {
+                'id': collection_uuid,
+                'title': name,
+                'description': query,
+                'can_read': True,
+                'can_write': False,
+                'media_types': [self.taxii_collections_media_type]
+            }
+            collections_resource.append(collection)
+            collections_by_id[collection_uuid] = collection
+
+        self._collections_resource = collections_resource
+        self._collections_by_id = collections_by_id
+
     def get_discovery_service(self):
         """
         Handle discovery request.
@@ -134,7 +151,8 @@ class TAXII2Server:
         Returns:
             The discovery response.
         """
-        default = urljoin(self.service_address, self.api_root)
+        default = urljoin(self._service_address, self._api_root)
+        default = urljoin(default, '/')
 
         return {
             'title': 'XSOAR TAXII2 Server',
@@ -150,8 +168,8 @@ class TAXII2Server:
         Returns:
             The API ROOT response.
         """
-        if not api_root == self.api_root:
-            raise Exception(f'Unknown API Root {api_root}. Check possible API Roots using "{self.discovery_route}"')
+        if not api_root == self._api_root:
+            raise Exception(f"Unknown API Root {api_root}. Check possible API Roots using '{self.discovery_route}'")
 
         return {
             'title': 'XSOAR TAXII2 Server ThreatIntel',
@@ -161,15 +179,49 @@ class TAXII2Server:
         }
 
     def get_collections(self, api_root):
-        if not api_root == self.api_root:
-            raise Exception(f'Unknown API Root {api_root}. Check possible API Roots using "{self.discovery_route}"')
-        # TODO Implement collections
+        """
+        Handle Collections request.
 
-        return {'collection': 'darya'}
+        Returns:
+            The Collections response.
+        """
+        if not api_root == self._api_root:
+            raise Exception(f"Unknown API Root {api_root}. Check possible API Roots using '{self.discovery_route}'")
 
-    def get_objects(self, api_root, collection_id, added_after, limit):
-        # TODO Implement indicators
-        return '', '', ''
+        return self._collections_resource
+
+    def get_collection_by_id(self, api_root, collection_id):
+        """
+        Handle Collection ID request.
+
+        Returns:
+            The Collection with given ID response.
+        """
+        if not api_root == self._api_root:
+            raise Exception(f"Unknown API Root {api_root}. Check possible API Roots using '{self.discovery_route}'")
+        found_collection = self._collections_by_id.get(collection_id)
+
+        if not found_collection:
+            raise Exception(f'No collection with id "{collection_id}". '
+                            f'Use "/{api_root}/collections/" to get all existing collections.')
+        return found_collection
+
+    def get_objects(self, api_root: str, collection_id: str, added_after, limit: int, offset: int,):
+        # TODO: change added after to timestamp param
+        if not api_root == self._api_root:
+            raise Exception(f"Unknown API Root {api_root}. Check possible API Roots using '{self.discovery_route}'")
+        found_collection = self._collections_by_id.get(collection_id)
+
+        if not found_collection:
+            raise Exception(f'No collection with id "{collection_id}". '
+                            f'Use "/{api_root}/collections/" to get all existing collections.')
+
+        query = found_collection.get('description')
+
+        # TODO: change limit to requested limit
+        new_limit = offset + limit
+        ios = find_indicators(query, added_after, new_limit)
+        return ios, '', ''
 
 
 SERVER: TAXII2Server
@@ -183,7 +235,7 @@ def taxii_validate_request(f):
     @functools.wraps(f)
     async def validate_request(*args, **kwargs):
         """
-        function of HTTP requests to validate authentication headers.
+        function of HTTP requests to validate authentication and Accept headers.
         """
         request: Request = kwargs.get('request')
         credentials: HTTPBasicCredentials = kwargs.get('credentials')
@@ -219,10 +271,11 @@ def handle_long_running_error(error: str):
 
 
 def handle_response(status_code, content, date_added_first=None, date_added_last=None,
-                    content_type=SERVER.taxii_content_type):
+                    content_type=None):
     """
     Create an HTTP taxii response from a taxii message.
     Args:
+        status_code:
         content_type:
         date_added_last:
         date_added_first:
@@ -231,6 +284,8 @@ def handle_response(status_code, content, date_added_first=None, date_added_last
     Returns:
         A taxii HTTP response.
     """
+    if not content_type:
+        content_type = SERVER.taxii_content_type
     headers = {
         'Content-Type': content_type,
     }
@@ -247,6 +302,13 @@ def handle_response(status_code, content, date_added_first=None, date_added_last
         media_type=content_type,
         headers=headers
     )
+
+
+def find_indicators(indicator_query: str) -> list:
+    # TODO: create indicator searcher
+    # TODO: parse all queries
+    # TODO: build functions for each indicator type
+    pass
 
 
 def get_collections(params: dict = demisto.params()) -> dict:
@@ -288,13 +350,15 @@ async def taxii2_server_discovery(request: Request, credentials: HTTPBasicCreden
     return handle_response(status.HTTP_200_OK, discovery_response)
 
 
-@APP.get('/{api_root}/')  # TAXII v2.1
+@APP.get('/{api_root}/')
 @taxii_validate_request
 async def taxii2_api_root(api_root, request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth)):
     """
      Defines TAXII API - Server Information:
      Get API Root Information section (4.2) `here <https://docs.oasis-open.org/cti/taxii/v2.1/cs01/taxii-v2.1-cs01.html#_Toc31107528>`__
      Args:
+         credentials:
+         request:
          api_root (str): the base URL of the API Root
      Returns:
          api-root: An API Root Resource upon successful requests.
@@ -310,13 +374,21 @@ async def taxii2_api_root(api_root, request: Request, credentials: HTTPBasicCred
     return handle_response(status.HTTP_200_OK, api_root_response)
 
 
+@APP.get('/{api_root}/status/{status_id}/')
+@taxii_validate_request
+async def taxii2_status(api_root, status_id, request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth)):
+    # TODO: Not allowed
+    pass
+
+
 @APP.get('/{api_root}/collections/')
 @taxii_validate_request
 async def taxii2_collections(request: Request, api_root: str, credentials: HTTPBasicCredentials = Depends(basic_auth)):
     """
     Defines TAXII API - Collections:
-    Get Collection section (5.4) `here for v.2 <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988049>`__
+    Get Collection section (5.1) `here for v.2 <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988049>`__
     Args:
+        request:
         credentials:
         api_root (str): the base URL of the API Root
     Returns:
@@ -324,22 +396,59 @@ async def taxii2_collections(request: Request, api_root: str, credentials: HTTPB
         `here <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988050>`__.
     """
     try:
-        collection_response = SERVER.get_collections(api_root)
+        collections_response = SERVER.get_collections(api_root)
     except Exception as e:
         error = f'Could not perform the collections request: {str(e)}'
         handle_long_running_error(error)
         return handle_response(status.HTTP_400_BAD_REQUEST, {'title': 'Collections Request Error',
                                                              'description': error})
+    return handle_response(status.HTTP_200_OK, collections_response)
+
+
+@APP.get('/{api_root}/collections/{collection_id}')
+@taxii_validate_request
+async def taxii2_collection_by_id(request: Request, api_root: str, collection_id: str,
+                                  credentials: HTTPBasicCredentials = Depends(basic_auth)):
+    """
+    Defines TAXII API - Collections:
+    Get Collection section (5.2) `here for v.2.0 <http://docs.oasis-open.org/cti/taxii/v2.0/cs01/taxii-v2.0-cs01.html#_Toc496542736>`__
+    and `here for v.2.1 <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988051>`__
+    Args:
+        request:
+        collection_id:
+        credentials:
+        api_root (str): the base URL of the API Root
+    Returns:
+        collections: A Collection Resource with given id upon successful requests.
+    """
+    try:
+        collection_response = SERVER.get_collection_by_id(api_root, collection_id)
+    except Exception as e:
+        error = f'Could not perform the collection request: {str(e)}'
+        handle_long_running_error(error)
+        return handle_response(status.HTTP_400_BAD_REQUEST, {'title': 'Collection Request Error',
+                                                             'description': error})
     return handle_response(status.HTTP_200_OK, collection_response)
 
 
+@APP.get('/{api_root}/collections/{collection_id}/manifest/')
+@taxii_validate_request
+async def taxii2_manifest(request: Request, api_root: str, collection_id: str, added_after: int = None,
+                          limit: int = 500,
+                          credentials: HTTPBasicCredentials = Depends(basic_auth)):
+    # TODO: implement
+    pass
+
+
 @APP.get('/{api_root}/collections/{collection_id}/objects/')
-async def taxii2_objects(api_root: str, collection_id: str, added_after: int = None, limit: int = 500,
-                         credentials: HTTPBasicCredentials = Depends(basic_auth)):
+@taxii_validate_request
+async def taxii2_objects(request: Request, api_root: str, collection_id: str, added_after: int = None, limit: int = 500,
+                         offset: int = 0, credentials: HTTPBasicCredentials = Depends(basic_auth)):
     """
     Defines TAXII API - Collections Objects:
-    Get Collection section (5.1) `here <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988055>`__
+    Get Collection section (5.4) `here <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988055>`__
     Args:
+        offset:
         credentials:
         limit:
         added_after:
@@ -349,12 +458,19 @@ async def taxii2_objects(api_root: str, collection_id: str, added_after: int = N
         envelope: A Envelope Resource upon successful requests. Additional information
         `here <https://docs.oasis-open.org/cti/taxii/v2.1/csprd01/taxii-v2.1-csprd01.html#_Toc532988038>`__.
     """
+    # TODO: Parse match arguments
+    ids = request.query_params.get('match[id]'),
+    types = request.query_params.get('match[type]'),
+    versions = request.query_params.get('match[version]'),
+    spec_versions = request.query_params.get('match[spec_version]')
+    # TODO: get limit and offset for v2.0 or v2.1
     try:
         objects_response, date_added_first, date_added_last = SERVER.get_objects(
-            api_root,
-            collection_id,
-            added_after,
-            limit
+            api_root=api_root,
+            collection_id=collection_id,
+            added_after=added_after,
+            offset=offset,
+            limit=limit,
         )
     except Exception as e:
         error = f'Could not perform the objects request: {str(e)}'
@@ -396,17 +512,17 @@ def run_server(taxii_server: TAXII2Server, port: int, certificate: str, private_
     else:
         demisto.debug('Starting HTTP Server')
 
-    integration_logger = IntegrationLogger()
-    integration_logger.buffering = False
-    log_config = dict(uvicorn.config.LOGGING_CONFIG)
-    log_config['handlers']['default']['stream'] = integration_logger
-    log_config['handlers']['access']['stream'] = integration_logger
-    log_config['formatters']['access'] = {
-        '()': TAXII2ServerAccessFormatter,
-        'fmt': '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s "%(user_agent)s"'
-    }
+    # integration_logger = IntegrationLogger()
+    # integration_logger.buffering = False
+    # log_config = dict(uvicorn.config.LOGGING_CONFIG)
+    # log_config['handlers']['default']['stream'] = integration_logger
+    # log_config['handlers']['access']['stream'] = integration_logger
+    # log_config['formatters']['access'] = {
+    #     '()': TAXII2ServerAccessFormatter,
+    #     'fmt': '%(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s "%(user_agent)s"'
+    # }
 
-    uvicorn.run(APP, host='127.0.0.1', port=port)
+    uvicorn.run(APP, host='0.0.0.0', port=port, **ssl_args)
 
 
 def main():
@@ -460,7 +576,6 @@ def main():
 
     except Exception as e:
         err_msg = f'Error in {INTEGRATION_NAME} Integration [{e}]'
-        print(err_msg)
         return_error(err_msg)
 
 
