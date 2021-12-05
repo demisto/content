7a69eb4cad0d7e49ef250b02d5643b2c6cf5f4aa
tneeman <tneeman@paloaltonetworks.com>
2021-12-05 18:28:02 +0200

    added ExtractURL formatting script, tests

---
+++ Packs/CommonScripts/Scripts/ExtractURL/ExtractURL.py
@@ -0,0 +1,175 @@
+from html import unescape
+from typing import Tuple
+from urllib.parse import urlparse, parse_qs, ParseResult, unquote
+
+from CommonServerPython import *
+
+ATP_REGEX = re.compile(r'(https://\w*|\w*)\.safelinks\.protection\.outlook\.com/.*\?url=')
+PROOF_POINT_URL_REG = re.compile(r'https://urldefense(?:\.proofpoint)?\.(com|us)/(v[0-9])/')
+HTTPS = 'https'
+HTTP = 'http'
+PREFIX_TO_NORMALIZE = {
+    'hxxp',
+    'meow',
+    'hXXp',
+}
+# Tuple of starts_with, does_not_start_with (if exists), replace to.
+PREFIX_CHANGES: List[Tuple[str, Optional[str], str]] = [
+    ('https:/', 'https://', 'https://'),
+    ('http:/', 'http://', 'http://'),
+    ('https:\\', 'https:\\\\', 'https://'),
+    ('http:\\', 'http:\\\\', 'http://'),
+    ('https:\\\\', None, 'https://'),
+    ('http:\\\\', None, 'http://')
+]
+
+
+def get_redirect_url_proof_point_v2(non_formatted_url: str, parse_results: ParseResult) -> str:
+    """
+    Extracts redirect URL from Proof Point V2.
+    Args:
+        non_formatted_url (str): Non formatted URL.
+        parse_results (ParseResult): Parse results of the given URL.
+
+    Returns:
+        (str): Redirected URL from Proof Point.
+    """
+    url_: str = get_redirect_url_from_query(non_formatted_url, parse_results, 'u')
+    trans = str.maketrans('-_', '%/')
+    url_ = url_.translate(trans)
+    return url_
+
+
+def get_redirect_url_proof_point_v3(non_formatted_url: str) -> str:
+    """
+    Extracts redirect URL from Proof Point V3.
+    Args:
+        non_formatted_url (str): Non formatted URL.
+
+    Returns:
+        (str): Redirected URL from Proof Point.
+    """
+    url_regex = re.compile(r'v3/__(?P<url>.+?)__;(?P<enc_bytes>.*?)!')
+    if match := url_regex.search(non_formatted_url):
+        non_formatted_url = match.group('url')
+    else:
+        demisto.error(f'Could not parse Proof Point redirected URL. Returning original URL: {non_formatted_url}')
+    return non_formatted_url
+
+
+def get_redirect_url_from_query(non_formatted_url: str, parse_results: ParseResult, redirect_param_name: str) -> str:
+    """
+    Receives an ATP Safe Link URL, returns the URL the ATP Safe Link points to.
+    Args:
+        non_formatted_url (str): The raw URL. For debugging purposes.
+        parse_results (str): ATP Safe Link URL parse results.
+        redirect_param_name (str): Name of the redirect parameter.
+    Returns:
+        (str): The URL the ATP Safe Link points to.
+    """
+    query_params_dict: dict[str, list[str]] = parse_qs(parse_results.query)
+    if not (url_ := query_params_dict.get(redirect_param_name)):
+        demisto.error(f'Could not find redirected URL. Returning the original URL: {non_formatted_url}')
+        return non_formatted_url
+    if len(url_) > 1:
+        demisto.debug(f'Found more than one URL query parameters for redirect in the given URL {non_formatted_url}\n'
+                      f'Returning the first URL: {url_[0]}')
+    url_ = url_[0]
+    return url_
+
+
+def replace_protocol(url_: str) -> str:
+    """
+    Replaces URL protocol with expected protocol.
+    Examples:
+    1)  http:/www.test.com
+        > http://www.test.com
+    2)  https:/www.test.com
+        > https://www.test.com
+    3)  http:\\www.test.com
+        > http://www.test.com
+    4)  https:\\www.test.com
+        > https://www.test.com
+    5)  http:\www.test.com
+        > http://www.test.com
+    6)  https:\www.test.com
+        > https://www.test.com
+    7)  hxxp://www.test.com
+        > http://www.test.com
+    8)  hXXp://www.test.com
+        > http://www.test.com
+    9)  hxxps://www.test.com
+        > https://www.test.com
+    10)  hXXps://www.test.com
+        > https://www.test.com
+    Args:
+        url_ (str): URL to replace the protocol by the given examples above.
+
+    Returns:
+        (str): URL with replaced protocol, if needed to replace, else the URL itself.
+    """
+    for prefix_to_normalize in PREFIX_TO_NORMALIZE:
+        if url_.startswith(prefix_to_normalize):
+            url_ = url_.replace(prefix_to_normalize, 'http')
+    lowercase_url = url_.lower()
+    for starts_with, does_not_start_with, to_replace in PREFIX_CHANGES:
+        if lowercase_url.startswith(starts_with) and (
+                not does_not_start_with or not lowercase_url.startswith(does_not_start_with)):
+            url_ = url_.replace(starts_with, to_replace)
+    return url_
+
+
+def normalize_url(non_normalized_url: str) -> str:
+    """
+    Normalizes a URL.
+    Args:
+        non_normalized_url (str): URL to normalize.
+
+    Returns:
+        (str): Normalized URL.
+    """
+    non_normalized_url = unescape(non_normalized_url.replace('[.]', '.'))
+    return non_normalized_url
+
+
+def format_url(non_formatted_url: str) -> str:
+    """
+    Formats a single URL.
+    Args:
+        non_formatted_url (str): Non formatted URL.
+
+    Returns:
+        (str): Formatted URL.
+    """
+    parse_results: ParseResult = urlparse(non_formatted_url)
+    # ATP redirect URL
+    if re.match(ATP_REGEX, non_formatted_url):
+        non_formatted_url = get_redirect_url_from_query(non_formatted_url, parse_results, 'url')
+    elif match := PROOF_POINT_URL_REG.search(non_formatted_url):
+        proof_point_ver: str = match.group(2)
+        if proof_point_ver == 'v3':
+            non_formatted_url = get_redirect_url_proof_point_v3(non_formatted_url)
+        elif proof_point_ver == 'v2':
+            non_formatted_url = get_redirect_url_proof_point_v2(non_formatted_url, parse_results)
+        else:
+            non_formatted_url = get_redirect_url_from_query(non_formatted_url, parse_results, 'u')
+    # Common handling for unescape and normalizing
+    non_formatted_url = unquote(unescape(non_formatted_url.replace('[.]', '.')))
+    formatted_url = replace_protocol(non_formatted_url)
+
+    return formatted_url
+
+
+def main():
+    try:
+        non_formatted_urls: List[str] = [url_.trim() for url_ in argToList(demisto.args().get('input'))]
+        return_results(CommandResults(outputs=non_formatted_urls))
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error(f'Failed to execute ExtractURL. Error: {str(e)}')
+
+
+''' ENTRY POINT '''
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
---
+++ Packs/CommonScripts/Scripts/ExtractURL/ExtractURL_test.py
@@ -0,0 +1,170 @@
+from urllib.parse import urlparse, ParseResult
+
+import pytest
+
+TEST_URL_HTTP = 'http://www.test.com'
+TEST_URL_HTTPS = 'https://www.test.com'
+
+NOT_FORMAT_TO_FORMAT = [  # Start of http:/ replacements.
+    ('http:/www.test.com', TEST_URL_HTTP),
+    ('https:/www.test.com', TEST_URL_HTTPS),
+    ('http:\\\\www.test.com', TEST_URL_HTTP),
+    ('https:\\\\www.test.com', TEST_URL_HTTPS),
+    ('http:\\www.test.com', TEST_URL_HTTP),
+    ('https:\\www.test.com', TEST_URL_HTTPS),
+    # End of http/s replacements.
+
+    # Start of hxxp/s replacements.
+    ('hxxp:/www.test.com', TEST_URL_HTTP),
+    ('hxxps:/www.test.com', TEST_URL_HTTPS),
+    ('hXXp:/www.test.com', TEST_URL_HTTP),
+    ('hXXps:/www.test.com', TEST_URL_HTTPS),
+
+    ('hxxp:\\www.test.com', TEST_URL_HTTP),
+    ('hxxps:\\www.test.com', TEST_URL_HTTPS),
+    ('hXXp:\\www.test.com', TEST_URL_HTTP),
+    ('hXXps:\\www.test.com', TEST_URL_HTTPS),
+
+    ('hxxp:\\\\www.test.com', TEST_URL_HTTP),
+    ('hxxps:\\\\www.test.com', TEST_URL_HTTPS),
+    ('hXXp:\\\\www.test.com', TEST_URL_HTTP),
+    ('hXXps:\\\\www.test.com', TEST_URL_HTTPS),
+    # End of hxxp/s replacements.
+
+    # start of meow/s replacements.
+    ('meow:/www.test.com', TEST_URL_HTTP),
+    ('meows:/www.test.com', TEST_URL_HTTPS),
+    ('meow:\\\\www.test.com', TEST_URL_HTTP),
+    ('meows:\\\\www.test.com', TEST_URL_HTTPS),
+    ('meow:\\www.test.com', TEST_URL_HTTP),
+    ('meows:\\www.test.com', TEST_URL_HTTPS),
+    # end of meow/s replacements.
+
+    # Start of Sanity test, no replacement should be done.
+    (TEST_URL_HTTP, TEST_URL_HTTP),
+    (TEST_URL_HTTPS, TEST_URL_HTTPS),
+    # End of Sanity test, no replacement should be done.
+]
+
+ATP_REDIRECTS = [('https://na01.safelinks.protection.outlook.com/?url=https%3A%2F%2Foffice.memoriesflower.com'
+                  '%2FPermission%2Foffice.php&data=01%7C01%7Cdavid.levin%40mheducation.com'
+                  '%7C0ac9a3770fe64fbb21fb08d50764c401%7Cf919b1efc0c347358fca0928ec39d8d5%7C0&sdata=PEoDOerQnha'
+                  '%2FACafNx8JAep8O9MdllcKCsHET2Ye%2B4%3D&reserved=0',
+                  'https://office.memoriesflower.com/Permission/office.php')]
+
+PROOF_POINT_REDIRECTS = [
+    ('https://urldefense.proofpoint.com/v2/url?u=https-3A__example.com_something.html',
+     'https://example.com/something.html'),
+    ('https://urldefense.proofpoint.com/v2/url?u=http-3A__links.mkt3337.com_ctt-3Fkn-3D3-26ms-3DMzQ3OTg3MDQS1-26r'
+     '-3DMzkxNzk3NDkwMDA0S0-26b-3D0-26j-3DMTMwMjA1ODYzNQS2-26mt-3D1-26rt-3D0&d=DwMFaQ&c'
+     '=Vxt5e0Osvvt2gflwSlsJ5DmPGcPvTRKLJyp031rXjhg&r=MujLDFBJstxoxZI_GKbsW7wxGM7nnIK__qZvVy6j9Wc&m'
+     '=QJGhloAyfD0UZ6n8r6y9dF-khNKqvRAIWDRU_K65xPI&s=ew-rOtBFjiX1Hgv71XQJ5BEgl9TPaoWRm_Xp9Nuo8bk&e=',
+     'http://links.mkt3337.com/ctt?kn=3&ms=MzQ3OTg3MDQS1&r=MzkxNzk3NDkwMDA0S0&b=0&j=MTMwMjA1ODYzNQS2&mt=1&rt=0'),
+    ('https://urldefense.proofpoint.com/v1/url?u=http://www.bouncycastle.org/&amp;k=oIvRg1%2BdGAgOoM1BIlLLqw%3D%3D%0A'
+     '&amp;r=IKM5u8%2B%2F%2Fi8EBhWOS%2BqGbTqCC%2BrMqWI%2FVfEAEsQO%2F0Y%3D%0A&amp;m'
+     '=Ww6iaHO73mDQpPQwOwfLfN8WMapqHyvtu8jM8SjqmVQ%3D%0A&amp;s'
+     '=d3583cfa53dade97025bc6274c6c8951dc29fe0f38830cf8e5a447723b9f1c9a',
+     'http://www.bouncycastle.org/'),
+    ('https://urldefense.com/v3/__https://google.com:443/search?q=a*test&gs=ps__;Kw!-612Flbf0JvQ3kNJkRi5Jg'
+     '!Ue6tQudNKaShHg93trcdjqDP8se2ySE65jyCIe2K1D_uNjZ1Lnf6YLQERujngZv9UWf66ujQIQ$',
+     'https://google.com:443/search?q=a*test&gs=ps'),
+    ('https://urldefense.us/v3/__https://google.com:443/search?q=a*test&gs=ps__;Kw!-612Flbf0JvQ3kNJkRi5Jg'
+     '!Ue6tQudNKaShHg93trcdjqDP8se2ySE65jyCIe2K1D_uNjZ1Lnf6YLQERujngZv9UWf66ujQIQ$',
+     'https://google.com:443/search?q=a*test&gs=ps')
+]
+
+REDIRECT_TEST_DATA = ATP_REDIRECTS + PROOF_POINT_REDIRECTS
+
+NORMALIZE_TEST_DATA = [('ftps://foo.bar/baz%20%21%22%23%24%25%26', 'ftps://foo.bar/baz !"#$%&'),
+                       ('ftps://foo.bar/baz%27%28%29%2A%2B,', "ftps://foo.bar/baz'()*+,"),
+                       ]
+
+FORMAT_URL_ADDITIONAL_TEST_CASES = [
+    ('https://test.co.uk/test.html', 'https://test.co.uk/test.html'),
+    ('www.test.test.com/test.html?paramaters=testagain', 'www.test.test.com/test.html?paramaters=testagain'),
+    ('http://ötest.com/', 'http://ötest.com/'),
+    ('https://testö.com/test.html', 'https://testö.com/test.html'),
+    ('www.testö.com/test.aspx', 'www.testö.com/test.aspx'),
+    ('https://www.teöst.com/', 'https://www.teöst.com/'),
+    ('www.test.com/check', 'www.test.com/check'),
+    ('http://xn--t1e2s3t4.com/testagain.aspx', 'http://xn--t1e2s3t4.com/testagain.aspx'),
+    ('https://www.xn--t1e2s3t4.com', 'https://www.xn--t1e2s3t4.com'),
+    ('www.test.com:443/path/to/file.html', 'www.test.com:443/path/to/file.html'),
+    ('https://1.2.3.4/path/to/file.html', 'https://1.2.3.4/path/to/file.html'),
+    ('1.2.3.4/path', '1.2.3.4/path'),
+    ('1.2.3.4/path/to/file.html', '1.2.3.4/path/to/file.html'),
+    ('http://142.42.1.1:8080/', 'http://142.42.1.1:8080/'),
+    ('http://☺.damowmow.com/', 'http://☺.damowmow.com/'),
+    ('http://223.255.255.254', 'http://223.255.255.254'),
+    ('ftp://foo.bar/baz', 'ftp://foo.bar/baz'),
+    ('ftps://foo.bar/baz', 'ftps://foo.bar/baz'),
+    ('hxxps://www[.]cortex-xsoar[.]com', 'https://www.cortex-xsoar.com')
+]
+
+FORMAT_URL_TEST_DATA = NOT_FORMAT_TO_FORMAT + PROOF_POINT_REDIRECTS + FORMAT_URL_ADDITIONAL_TEST_CASES + \
+                       NORMALIZE_TEST_DATA
+
+
+@pytest.mark.parametrize('non_formatted_url, expected', NOT_FORMAT_TO_FORMAT)
+def test_replace_protocol(non_formatted_url: str, expected: str):
+    """
+    Given:
+    - non_formatted_url: A URL.
+
+    When:
+    - Replacing protocol to http:// or https://.
+
+    Then:
+    - Ensure for every expected protocol given, it is replaced with the expected value.
+    """
+    from ExtractURL import replace_protocol
+    assert replace_protocol(non_formatted_url) == expected
+
+
+@pytest.mark.parametrize('url_, expected', FORMAT_URL_TEST_DATA)
+def test_format_url(url_, expected):
+    from ExtractURL import format_url
+    assert format_url(url_) == expected
+
+
+@pytest.mark.parametrize('url_, expected', [
+    ('https://urldefense.proofpoint.com/v2/url?u=http-3A__links.mkt3337.com_ctt-3Fkn-3D3-26ms-3DMzQ3OTg3MDQS1-26r'
+     '-3DMzkxNzk3NDkwMDA0S0-26b-3D0-26j-3DMTMwMjA1ODYzNQS2-26mt-3D1-26rt-3D0&d=DwMFaQ&c'
+     '=Vxt5e0Osvvt2gflwSlsJ5DmPGcPvTRKLJyp031rXjhg&r=MujLDFBJstxoxZI_GKbsW7wxGM7nnIK__qZvVy6j9Wc&m'
+     '=QJGhloAyfD0UZ6n8r6y9dF-khNKqvRAIWDRU_K65xPI&s=ew-rOtBFjiX1Hgv71XQJ5BEgl9TPaoWRm_Xp9Nuo8bk&e=',
+     'http%3A//links.mkt3337.com/ctt%3Fkn%3D3%26ms%3DMzQ3OTg3MDQS1%26r%3DMzkxNzk3NDkwMDA0S0%26b%3D0%26j'
+     '%3DMTMwMjA1ODYzNQS2%26mt%3D1%26rt%3D0')
+])
+def test_get_redirect_url_proof_point_v2(url_: str, expected: str):
+    from ExtractURL import get_redirect_url_proof_point_v2
+    assert get_redirect_url_proof_point_v2(url_, urlparse(url_)) == expected
+
+
+@pytest.mark.parametrize('url_, expected', [
+    ('https://urldefense.com/v3/__https://google.com:443/search?q=a*test&gs=ps__;Kw!-612Flbf0JvQ3kNJkRi5Jg'
+     '!Ue6tQudNKaShHg93trcdjqDP8se2ySE65jyCIe2K1D_uNjZ1Lnf6YLQERujngZv9UWf66ujQIQ$',
+     'https://google.com:443/search?q=a*test&gs=ps'),
+    ('https://urldefense.us/v3/__https://google.com:443/search?q=a*test&gs=ps__;Kw!-612Flbf0JvQ3kNJkRi5Jg'
+     '!Ue6tQudNKaShHg93trcdjqDP8se2ySE65jyCIe2K1D_uNjZ1Lnf6YLQERujngZv9UWf66ujQIQ$',
+     'https://google.com:443/search?q=a*test&gs=ps')
+])
+def test_get_redirect_url_proof_point_v3(url_: str, expected: str):
+    from ExtractURL import get_redirect_url_proof_point_v3
+    assert get_redirect_url_proof_point_v3(url_) == expected
+
+
+@pytest.mark.parametrize('non_formatted_url, redirect_param_name, expected', [
+    ('https://urldefense.proofpoint.com/v1/url?u=http://www.bouncycastle.org/&amp;k=oIvRg1%2BdGAgOoM1BIlLLqw%3D%3D%0A'
+     '&amp;r=IKM5u8%2B%2F%2Fi8EBhWOS%2BqGbTqCC%2BrMqWI%2FVfEAEsQO%2F0Y%3D%0A&amp;m'
+     '=Ww6iaHO73mDQpPQwOwfLfN8WMapqHyvtu8jM8SjqmVQ%3D%0A&amp;s'
+     '=d3583cfa53dade97025bc6274c6c8951dc29fe0f38830cf8e5a447723b9f1c9a', 'u', 'http://www.bouncycastle.org/'),
+    ('https://na01.safelinks.protection.outlook.com/?url=https%3A%2F%2Foffice.memoriesflower.com'
+     '%2FPermission%2Foffice.php&data=01%7C01%7Cdavid.levin%40mheducation.com'
+     '%7C0ac9a3770fe64fbb21fb08d50764c401%7Cf919b1efc0c347358fca0928ec39d8d5%7C0&sdata=PEoDOerQnha'
+     '%2FACafNx8JAep8O9MdllcKCsHET2Ye%2B4%3D&reserved=0', 'url',
+     'https://office.memoriesflower.com/Permission/office.php')
+])
+def test_get_redirect_url_from_query(non_formatted_url: str, redirect_param_name: str,
+                                     expected: str):
+    from ExtractURL import get_redirect_url_from_query
+    assert get_redirect_url_from_query(non_formatted_url, urlparse(non_formatted_url), redirect_param_name) == expected
