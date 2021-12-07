d2e24c6a34674b651f4b51fbafdd6caf3f8ef36d
Binat Ziser <bziser@paloaltonetworks.com>
2021-12-07 15:36:47 +0200

    playbook

--- Packs/CommonScripts/Scripts/FormattedDateToEpoch/FormattedDateToEpoch.py
+++ Packs/CommonScripts/Scripts/FormattedDateToEpoch/FormattedDateToEpoch.py
@@ -6,7 +6,8 @@ from typing import Optional
 
 def date_to_epoch(date: str, formatter: Optional[str] = None) -> int:
     epoch = datetime(1970, 1, 1, tzinfo=timezone.utc)
-    date_obj = datetime.strptime(date, formatter) if formatter else dateparser.parse(date)
+    date_obj = datetime.strptime(date, formatter) if formatter \
+        else dateparser.parse(date, settings={'RELATIVE_BASE': datetime(1900, 1, 1)})
     return int(date_obj.strftime('%s') if date_obj.tzinfo is None else (date_obj - epoch).total_seconds())
 
 
