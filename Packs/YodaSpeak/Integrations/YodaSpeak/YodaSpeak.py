575214a84b874f50ef0588addf383ac7b4a8af83
dschwartz <81086590+dorschw@users.noreply.github.com>
2021-12-14 17:52:06 +0200

    yodaspeak

---
+++ Packs/YodaSpeak/Integrations/YodaSpeak/YodaSpeak.py
@@ -0,0 +1,110 @@
+import demistomock as demisto
+from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
+from CommonServerUserPython import *  # noqa
+
+import requests
+import traceback
+from typing import Dict, Any
+
+# Disable insecure warnings
+requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
+
+
+class Client(BaseClient):
+    def __init__(self, api_key: str, base_url: str, proxy: bool, verify: bool):
+        super().__init__(base_url=base_url,
+                         proxy=proxy,
+                         verify=verify)
+        self.api_key = api_key
+
+        if self.api_key:
+            self._headers = {'X-Funtranslations-Api-Secret': self.api_key}
+
+    def translate(self, text: str):
+        return self._http_request(method='POST', url_suffix='yoda', data={'text': text}, resp_type='json',
+                                  ok_codes=(200,))
+
+
+def test_module(client: Client) -> str:
+    """
+    Tests API connectivity and authentication'
+
+    Returning 'ok' indicates that connection to the service is successful.
+    Raises exceptions if something goes wrong.
+    """
+
+    try:
+        response = client.translate('I have the high ground!')
+
+        success = demisto.get(response, 'success.total')  # Safe access to response['success']['total']
+        if success != 1:
+            return f'Unexpected result from the service: success={success} (expected success=1)'
+
+        return 'ok'
+
+    except DemistoException as e:
+        exception_text = str(e).lower()
+        if 'forbidden' in exception_text or 'authorization' in exception_text:
+            return 'Authorization Error: make sure API Key is correctly set'
+        else:
+            raise e
+
+
+def translate_command(client: Client, args: Dict[str, Any]) -> CommandResults:
+    text = args.get('text')
+    if not text:
+        raise DemistoException('the text argument cannot be empty.')
+
+    response = client.translate(text)
+    translated = demisto.get(response, 'contents.translated')
+
+    if translated is None:
+        raise DemistoException('Translation failed: the response from server did not include `translated`.',
+                               res=response)
+
+    outputs = {'Original': text,
+               'Translation': translated}
+
+    return CommandResults(outputs_prefix='YodaSpeak',
+                          outputs_key_field='TheForce',
+                          outputs=outputs,
+                          raw_response=response,
+                          readable_output=tableToMarkdown(name='Yoda Says...', t=outputs))
+
+
+def main() -> None:
+    params = demisto.params()
+    command = demisto.command()
+
+    api_key = params.get('apikey')
+    base_url = params.get('url', '')
+    verify = not params.get('insecure', False)
+    proxy = params.get('proxy', False)
+
+    demisto.debug(f'Command being called is {command}')
+    try:
+        client = Client(api_key=api_key, base_url=base_url, verify=verify, proxy=proxy)
+
+        if command == 'test-module':
+            # This is the call made when pressing the integration Test button.
+            module = test_module(client)
+            print(module)
+            return_results(module)
+
+        elif command == 'translate':
+            translate = client.translate(**demisto.args())
+            print(translate)
+            return_results(translate)
+
+    # Log exceptions and return errors
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error("\n".join(("Failed to execute {command} command.",
+                                "Error:",
+                                str(e))))
+
+
+''' ENTRY POINT '''
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
