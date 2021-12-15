846ff6f5296bdfaf768700ad9a0b6f1ccc9a8c52
Darya Koval <dkoval@paloaltonetworks.com>
2021-12-15 12:27:35 +0200

    fixes flake error

--- Packs/TaniumThreatResponse/Integrations/TaniumThreatResponseV2/TaniumThreatResponseV2_test.py
+++ Packs/TaniumThreatResponse/Integrations/TaniumThreatResponseV2/TaniumThreatResponseV2_test.py
@@ -966,7 +966,7 @@ def test_filter_list_evidence(requests_mock, command_args, expected_output_len):
     _, outputs, _ = TaniumThreatResponseV2.list_evidence(MOCK_CLIENT, command_args)
     response = outputs.get('Tanium.Evidence(val.uuid && val.uuid === obj.uuid)', {})
     assert (
-            len(response) == expected_output_len
+        len(response) == expected_output_len
     ), f'Actual length: {len(response)}, Expected length: {expected_output_len}'
 
 
