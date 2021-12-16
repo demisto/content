2be8aa3332eb66eb48652d459f676fc2b866671b
sberman <sberman@paloaltonetworks.com>
2021-12-16 12:28:28 +0200

    change getParam to params

--- Packs/Cymulate/Integrations/Cymulate_v2/Cymulate_v2.py
+++ Packs/Cymulate/Integrations/Cymulate_v2/Cymulate_v2.py
@@ -1642,7 +1642,7 @@ def main() -> None:
     """main function, parses params and runs command functions"""
     params = demisto.params()
 
-    api_key = params.get('api_key') or (demisto.getParam('credentials') or {}).get('password')
+    api_key: str = params.get('api_key') or (params.get('credentials') or {}).get('password')  # type: ignore # [assignment]
     base_url = params.get('base_url')
 
     verify_certificate = not params.get('insecure', False)
