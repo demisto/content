acfd17798af460208bc7021a415044469e47ffb7
ilan <ierukhimovic@paloaltonetworks.com>
2021-12-20 13:32:03 +0200

    from master

--- Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
+++ Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
@@ -1,6 +1,6 @@
 import pytest
-import json
 from freezegun import freeze_time
+
 import demistomock as demisto
 
 integration_params = {
@@ -12,46 +12,29 @@ integration_params = {
 }
 
 
-def load_json(file: str):
-    with open(file) as f:
-        return json.load(f)
-
-
 @pytest.fixture(autouse=True)
 def set_mocks(mocker):
     mocker.patch.object(demisto, 'params', return_value=integration_params)
 
 
-def test_fetch_incident(mocker):
-    from RedLock import fetch_incidents
-    example_alerts = load_json('test_data/example_alert.json')
-    mocker.patch('RedLock.req', return_value=example_alerts)
-    incidents, last_fetches, last_seen_time = fetch_incidents(limit=50)
-    assert len(incidents) == 2, 'There are two alerts in example response'
-    for incident, alert in zip(incidents, example_alerts.get('items')):
-        policy_name = demisto.get(alert, 'policy.name')
-        policy_name = policy_name if policy_name else 'No policy'
-        assert incident.get('name') == f"{policy_name} - {alert.get('id')}"
-    assert last_seen_time == 1633109203697
-
-
 @freeze_time("2021-07-10T16:34:14.758295 UTC+1")
-def test_empty_incident(mocker):
+def test_fetch_incidents_first_time_fetch(mocker):
+    """
+        Given
+            - fetch incidents command
+            - command args
+        When
+            - mock the integration parameters
+        Then
+            - Validate that the last_time is as the now time(not changed, not of the incident)
+    """
+    mocker.patch.object(demisto, 'command', return_value='fetch-incidents')
     from RedLock import fetch_incidents
-    mocker.patch('RedLock.req', return_value={'items': []})
-    incidents, last_fetches, last_seen_time = fetch_incidents(50)
-    assert len(incidents) == 0, 'There are no alerts in example response'
-    assert last_seen_time == 1625679254000
+    mocker.patch('RedLock.req', return_value=[])
 
+    _, next_run = fetch_incidents()
+    assert next_run == 1625938454758
 
-def test_fetch():
-    from RedLock import fetch_incidents
-    all_incidents = []
-    for _ in range(10):
-        incidents, last_fetches, last_seen_time = fetch_incidents(5)
-        demisto.setLastRun({'time': last_seen_time, 'last_fetches': last_fetches})
-        all_incidents.extend(incidents)
-    print(incidents)
 
 def test_redlock_list_scans(mocker):
     """
