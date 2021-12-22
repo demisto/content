edfb374e3435b12bcef943f81919d1d03eea7409
BEAdi <adi.bamberger@gmail.com>
2021-12-22 14:36:41 +0200

    paging heading UT

--- Packs/Grafana/Integrations/Grafana/Grafana_test.py
+++ Packs/Grafana/Integrations/Grafana/Grafana_test.py
@@ -5,7 +5,7 @@ from pytz import utc
 
 from Grafana import Client, change_key, keys_to_lowercase, decapitalize, calculate_fetch_start_time, parse_alerts, \
     alert_to_incident, filter_alerts_by_time, filter_alerts_by_id, reduce_incidents_to_limit, set_state, \
-    set_time_to_epoch_millisecond
+    set_time_to_epoch_millisecond, paging_heading
 from freezegun import freeze_time
 from CommonServerPython import urljoin
 
@@ -437,3 +437,27 @@ def test_set_time_for_annotation(time_input, time_output):
 
     """
     assert set_time_to_epoch_millisecond(time_input) == time_output
+
+
+no_page_number = (None, '100', 'Showing 100 results:\n')
+no_page_size = ('50', None, 'Showing results from page 50:\n')
+no_page_number_size = (None, None, '')
+page_number_size = ('1', '20', 'Showing 20 results from page 1:\n')
+PAGING_HEADING = (no_page_number, no_page_size, no_page_number_size, page_number_size)
+
+
+@pytest.mark.parametrize('page_number, page_size, expected_output', PAGING_HEADING)
+def test_paging_heading(page_number, page_size, expected_output):
+    """
+
+    Given:
+        - 'page_number' and 'page_size' arguments are or aren't given to commands that have paging
+
+    When:
+        - A command that has paging is executed
+
+    Then:
+        - Returns the right sentence to write in the beginning of the readable output
+
+    """
+    assert paging_heading(page_number, page_size) == expected_output
