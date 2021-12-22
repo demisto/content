95e822d1235289608ed85b50375acf5d3c6683f5
merit <meretmaayta@gmail.com>
2021-12-22 13:25:18 +0200

    added unit tests

--- Packs/ApiModules/Scripts/CSVFeedApiModule/CSVFeedApiModule_test.py
+++ Packs/ApiModules/Scripts/CSVFeedApiModule/CSVFeedApiModule_test.py
@@ -423,6 +423,36 @@ def test_build_iterator_not_modified_header(mocker):
                                                 'createIndicators will be executed with noUpdate=True.'
 
 
+def test_build_iterator_with_version_6_2_0(mocker):
+    """
+    Given
+    - server version 6.2.0
+
+    When
+    - Running build_iterator method.
+
+    Then
+    - Ensure that the no_update value is True
+    - Request is called without headers "If-None-Match" and "If-Modified-Since"
+    """
+    feed_name = 'mock_feed_name'
+    mocker.patch.object(demisto, 'debug')
+    mocker.patch('CommonServerPython.get_demisto_version', return_value={"version": "6.2.0"})
+
+    with requests_mock.Mocker() as m:
+        m.get('https://api.github.com/meta', status_code=304)
+
+        client = Client(
+            url='https://api.github.com/meta',
+            headers={}
+        )
+        result, no_update = client.build_iterator(feed={'url': 'https://api.github.com/meta'}, feed_name=feed_name)
+        assert not result
+        assert no_update
+        assert 'If-None-Match' not in client.headers
+        assert 'If-Modified-Since' not in client.headers
+
+
 def test_get_no_update_value_without_headers(mocker):
     """
     Given
