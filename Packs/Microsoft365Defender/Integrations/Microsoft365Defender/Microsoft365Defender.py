efc9b6b3d36e521725eedfca7dc75b5a52804a9d
Yaakov Praisler <59408745+yaakovpraisler@users.noreply.github.com>
2021-12-23 22:13:00 +0200

    Update Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
    Co-authored-by: Dan Tavori <38749041+dantavori@users.noreply.github.com>

--- Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
+++ Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
@@ -547,7 +547,7 @@ def main() -> None:
 
     try:
         if not app_id:
-            raise Exception('Aplication ID must be provided.')
+            raise Exception('Application ID must be provided.')
 
         client = Client(
             app_id=app_id,
