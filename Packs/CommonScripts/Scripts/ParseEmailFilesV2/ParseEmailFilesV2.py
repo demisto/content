60ea8eff6d8165f86242480ac033add17070043a
mgalitzki <mgalitzki@paloaltonetworks.com>
2021-12-28 19:27:51 +0200

    added v2 script

---
+++ Packs/CommonScripts/Scripts/ParseEmailFilesV2/ParseEmailFilesV2.py
@@ -0,0 +1,115 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+from parse_emails.parse_emails import ParseEmails
+
+
+def recursive_convert_to_unicode(replace_to_utf):
+    """Converts object into UTF-8 characters
+    ignores errors
+    Args:
+        replace_to_utf (object): any object
+
+    Returns:
+        object converted to UTF-8
+    """
+    try:
+        if isinstance(replace_to_utf, dict):
+            return {recursive_convert_to_unicode(k): recursive_convert_to_unicode(v) for k, v in replace_to_utf.items()}
+        if isinstance(replace_to_utf, list):
+            return [recursive_convert_to_unicode(i) for i in replace_to_utf if i]
+        if not replace_to_utf:
+            return replace_to_utf
+        return replace_to_utf
+    except TypeError:
+        return replace_to_utf
+
+
+def data_to_md(email_data, email_file_name=None, parent_email_file=None, print_only_headers=False):
+    if email_data is None:
+        return 'No data extracted from email'
+    # email_data = recursive_convert_to_unicode(email_data)
+    # email_file_name = recursive_convert_to_unicode(email_file_name)
+    # parent_email_file = recursive_convert_to_unicode(parent_email_file)
+
+    md = u"### Results:\n"
+    if email_file_name:
+        md = f"### {email_file_name}\n"
+
+    if print_only_headers:
+        return tableToMarkdown(f"Email Headers: {email_file_name}", email_data.get('HeadersMap'))
+
+    if parent_email_file:
+        md += f"### Containing email: {parent_email_file}\n"
+
+    md += u"* {0}:\t{1}\n".format('From', email_data.get('From') or "")
+    md += u"* {0}:\t{1}\n".format('To', email_data.get('To') or "")
+    md += u"* {0}:\t{1}\n".format('CC', email_data.get('CC') or "")
+    md += u"* {0}:\t{1}\n".format('Subject', email_data.get('Subject') or "")
+    if email_data.get('Text'):
+        text = email_data['Text'].replace('<', '[').replace('>', ']')
+        md += u"* {0}:\t{1}\n".format('Body/Text', text or "")
+    if email_data.get('HTML'):
+        md += u"* {0}:\t{1}\n".format('Body/HTML', email_data['HTML'] or "")
+
+    md += u"* {0}:\t{1}\n".format('Attachments', email_data.get('Attachments') or "")
+    md += u"\n\n" + tableToMarkdown('HeadersMap', email_data.get('HeadersMap'))
+    return md
+
+
+def main():
+    file_type = ''
+    entry_id = demisto.args()['entryid']
+    max_depth = int(demisto.args().get('max_depth', '3'))
+
+    if max_depth < 1:
+        return_error('Minimum max_depth is 1, the script will parse just the top email')
+
+    parse_only_headers = demisto.args().get('parse_only_headers', 'false').lower() == 'true'
+    try:
+        result = demisto.executeCommand('getFilePath', {'id': entry_id})
+        if is_error(result):
+            return_error(get_error(result))
+
+        file_path = result[0]['Contents']['path']
+        file_name = result[0]['Contents']['name']
+        result = demisto.executeCommand('getEntry', {'id': entry_id})
+        if is_error(result):
+            return_error(get_error(result))
+
+        file_metadata = result[0]['FileMetadata']
+        file_type = file_metadata.get('info', '') or file_metadata.get('type', '')
+
+    except Exception as ex:
+        return_error(
+            "Failed to load file entry with entry id: {}. Error: {}".format(
+                entry_id, str(ex) + "\n\nTrace:\n" + traceback.format_exc()))
+
+    try:
+        email_parser = ParseEmails(file_path=file_path, max_depth=max_depth, parse_only_headers=parse_only_headers,
+                                   file_info=file_type)
+        output = email_parser.parse_emails()
+
+        results = []
+        if isinstance(output, list) and len(output) > 0:
+            for email in output:
+                results.append(CommandResults(outputs_prefix='Email',
+                               outputs=email,
+                               readable_output=data_to_md(email, file_name, print_only_headers=parse_only_headers),
+                               raw_response=email,
+                               entry_type=EntryType.NOTE))
+            return_results(results)
+        if isinstance(output, dict):
+            results.append(CommandResults(outputs_prefix='Email',
+                                          outputs=output,
+                                          readable_output=data_to_md(output, file_name,
+                                                                     print_only_headers=parse_only_headers),
+                                          raw_response=output,
+                                          entry_type=EntryType.NOTE))
+
+    except Exception as e:
+        demisto.error(str(e) + "\n\nTrace:\n" + traceback.format_exc())
+        return_error(str(e) + "\n\nTrace:\n" + traceback.format_exc())
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
