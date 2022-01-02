5eff9a97ed8c3b1f1d7b093e5254799356944296
Rajesh Acharya <sorkan@hotmail.com>
2022-01-02 10:02:05 -0500

    Update Okta_v2_test.py
    Fix flake8 errors

--- Packs/Okta/Integrations/Okta_v2/Okta_v2_test.py
+++ Packs/Okta/Integrations/Okta_v2/Okta_v2_test.py
@@ -610,11 +610,10 @@ def test_get_user_command(mocker, args, expected_context, expected_readable):
 def test_list_user_command(mocker, args, expected_context, expected_readable):
     mocker.patch.object(client, 'list_users', return_value=user_data)
     readable, outputs, _ = list_users_command(client, args)
-    
     assert outputs.get('Account(val.ID && val.ID == obj.ID)')[0] == expected_context
     assert expected_readable in readable
 
-    
+
 @pytest.mark.parametrize(
     "args ,expected_context",
     [
