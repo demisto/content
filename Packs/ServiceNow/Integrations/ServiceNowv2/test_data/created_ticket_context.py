e0a62fd07c701ee80f273fb4df768462bb295011
Binat Ziser <bziser@paloaltonetworks.com>
2022-01-06 14:42:27 +0200

    test_create_co_from_template_command

---
+++ Packs/ServiceNow/Integrations/ServiceNowv2/test_data/created_ticket_context.py
@@ -0,0 +1,11 @@
+CREATED_TICKET_CONTEXT = {
+    'ID': {'display_value': '11ab5ccfdb784d505da401a3ca96190a', 'value': '11ab5ccfdb784d505da401a3ca96190a'},
+    'Summary': {'display_value': 'Add network switch to cabinet', 'value': 'Add network switch to cabinet'},
+    'Number': {'display_value': 'CHG0031889', 'value': 'CHG0031889'},
+    'CreatedOn': {'display_value': '06/01/2022 04:12:43', 'value': '2022-01-06 12:12:43',
+                  'display_value_internal': '2022-01-06 04:12:43'}, 'Active': {'display_value': 'true', 'value': True},
+    'AdditionalComments': {'display_value': '', 'value': ''}, 'CloseCode': {'display_value': '', 'value': ''},
+    'OpenedAt': {'display_value': '06/01/2022 04:12:43', 'value': '2022-01-06 12:12:43',
+                 'display_value_internal': '2022-01-06 04:12:43'}, 'OpenedBy': '46428af1db202c1026fca015ca961994',
+    'Creator': '46428af1db202c1026fca015ca961994', 'Priority': ('4 - Low',), 'State': {'display_value': 'New', 'value': -5.0}
+}
