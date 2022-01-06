1659f2e7f0493f8475dc48ec09343d1724d95985
tneeman <tneeman@paloaltonetworks.com>
2022-01-06 15:52:53 +0200

    add test failure to indicate bug

--- Packs/CommonScripts/Scripts/ShowLocationOnMap/ShowLocationOnMap.py
+++ Packs/CommonScripts/Scripts/ShowLocationOnMap/ShowLocationOnMap.py
@@ -1,12 +1,13 @@
-import demistomock as demisto
 from CommonServerPython import *
-from CommonServerUserPython import *
 
-loc = demisto.get(demisto.args()['indicator'], "CustomFields.geolocation")
+loc = demisto.get(demisto.args()['indicator'], "CustomFields.geolocation").strip()
 err_msg = "No location data was available"
-
+long_lat_regex = re.compile(
+    r'^\s*[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)\s*,\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)\s*$')
 if not loc:
     return_error(err_msg)
+# if not long_lat_regex.search(loc):
+#     return_error(f'Given loc format: {loc} does not match the expected format')
 try:
     lat, lng = loc.split(',')
 except ValueError:
