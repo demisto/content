61064c0078fca41f56bca587f710c719d5f11868
Mehmet Cagri Caliskan <cagri@diatics.com>
2022-01-11 16:03:33 +0300

    blank line

--- Packs/PhishUp/Integrations/PhishUp/PhishUp_test.py
+++ Packs/PhishUp/Integrations/PhishUp/PhishUp_test.py
@@ -9,34 +9,27 @@ def util_load_json(path):
 
 def test_investigate_url_command():
     from PhishUp import Client, investigate_url_command
-    
     base_url = "https://apiv2.phishup.co"
     client = Client(
         base_url=base_url,
         verify=False)
-
     args = {
         "apikey": "not"
     }
     params = {
         "Url": "https://www.paloaltonetworks.com/"
     }
-
     response = investigate_url_command(client, args, params)
-
     mock_response = util_load_json('test_data/error.json')
-
     assert mock_response == list(response)
 
 
 def test_investigate_bulk_url_command():
     from PhishUp import Client, investigate_bulk_url_command
-
     base_url = "https://apiv2.phishup.co"
     client = Client(
         base_url=base_url,
         verify=False)
-
     args = {
         "Urls": []
     }
@@ -50,7 +43,6 @@ def test_investigate_bulk_url_command():
 
 def test_get_chosen_phishup_action_command():
     from PhishUp import get_chosen_phishup_action_command
-
     params = {
         "phishup-playbook-action": "Nothing"
     }
@@ -66,14 +58,12 @@ def test_investigate_url_http_request(mocker):
     client = Client(
         base_url=base_url,
         verify=False)
-
     args = {
         "apikey": "not"
     }
     params = {
         "Url": "https://www.paloaltonetworks.com/"
     }
-
     r = client.investigate_url_http_request(client, args, params)
     assert r == "Error"
 
@@ -86,27 +76,23 @@ def test_investigate_bulk_url_http_request(mocker):
     client = Client(
         base_url=base_url,
         verify=False)
-
     args = {
         "apikey": "not"
     }
     params = {
         "Url": "https://www.paloaltonetworks.com/"
     }
-
     r = client.investigate_bulk_url_http_request(client, args, params)
     assert r == "Error"
 
 
 def test_check_api_key_test_module_http_request(mocker):
     from PhishUp import Client, test_module
-
     patcher = mocker.patch("PhishUp.Client.check_api_key_test_module_http_request", return_value={"Status": "Success"})
     patcher.start()
     base_url = "https://apiv2.phishup.co"
     client = Client(
         base_url=base_url,
         verify=False)
-
     r = test_module(client)
     assert r == "ok"
