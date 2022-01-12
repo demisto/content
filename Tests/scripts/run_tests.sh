0289546eee8e84464a8f1e618d37a92f3931c3b1
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-12 19:42:54 +0200

    $CIRCLE_BUILD_NUM

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -8,6 +8,9 @@ CONF_PATH="./Tests/conf.json"
 [ -z "${NON_AMI_RUN}" ] && IS_AMI_RUN=true || IS_AMI_RUN=false
 
 echo "export GOOGLE_APPLICATION_CREDENTIALS=$GCS_ARTIFACTS_KEY" >> $BASH_ENV
+echo "export CI_BUILD_ID=$CIRCLE_BUILD_NUM" >> $BASH_ENV
+echo this is the ci build id "$CI_BUILD_ID"
+echo this is the CIRCLE_BUILD_NUM "$CIRCLE_BUILD_NUM"
 source $BASH_ENV
 
 demisto-sdk test-content -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CI_BUILD_ID" -g "$CI_COMMIT_BRANCH" -m "$MEM_CHECK" --is-ami $IS_AMI_RUN -d "$1"
