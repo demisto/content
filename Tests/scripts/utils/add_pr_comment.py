e623a621ad6922f2a6f0020b35f51455acf2258b
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-16 12:11:29 +0200

    verify true

--- Tests/scripts/utils/add_pr_comment.py
+++ Tests/scripts/utils/add_pr_comment.py
@@ -16,22 +16,22 @@ def add_pr_comment():
     url = 'https://api.github.com/search/issues'
     headers = {'Authorization': 'Bearer ' + token}
     try:
-        response = requests.get(url + query, headers=headers, verify=False)
+        response = requests.get(url + query, headers=headers)
         res_dict = response.json()
 
         if res_dict and res_dict.get('total_count', 0) == 1:
             issue_url = res_dict['items'][0].get('comments_url') if res_dict.get('items', []) else None
             if issue_url:
-                response = requests.get(issue_url, headers=headers, verify=False)
+                response = requests.get(issue_url, headers=headers)
                 issue_comments = response.json()
                 for existing_comment in issue_comments:
                     # Check if a comment about report coverage already exists. If there is delete it first and then post
                     # a new comment:
                     if COVERAGE_REPORT_COMMENT in existing_comment.get('body'):
                         comment_url = existing_comment.get('url')
-                        requests.delete(comment_url, headers=headers, verify=False)
+                        requests.delete(comment_url, headers=headers)
                 requests.post(issue_url, json={'body': f'{COVERAGE_REPORT_COMMENT}:\n {COVERAGE_LINK}'},
-                              headers=headers, verify=False)
+                              headers=headers)
         else:
             print('Add pull request comment failed: There is more then one open pull request for branch {branch_name}.')
     except Exception as e:
