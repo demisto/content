c90fad5ca20459323e9ca2d431c21cde3ddba387
eyalpalo <epintzov@paloaltonetworks.com>
2022-01-16 16:16:35 +0200

    script added

---
+++ Utils/git_pull_master_into_fork.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CONTENT_URL='https://github.com/demisto/content.git'
+CURRENT=$(git branch --show-current)
+
+{
+  git remote add upstream_content $CONTENT_URL ||
+  git remote set-url upstream_content $CONTENT_URL
+} &&
+git fetch upstream_content &&
+git checkout master &&
+git rebase upstream_content/master &&
+git push -f origin master &&
+git checkout $CURRENT &&
+git pull origin master
+
