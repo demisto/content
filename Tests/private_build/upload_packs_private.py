fdfefab17a4a3c9d71fbb5dd9a4a912abb53d903
Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
2022-01-18 16:51:45 +0200

    Add marketplace v2 upload flow (#16670)
    * duplicate create-instances for mpv2 - wip
    
    * add support for mp_v2 in prepare_content_packs_for_testing
    
    * fix yml structure
    
    * change demisto-sdk version to new id-set branch (d49c3271147566e32952c08533137f9e96648ce1)
    
    * fix create-content-artifacts command for MP v2
    
    * update demisto-sdk commit (0a19d8e4d5e72051cfaf702166c85dbc6a84809a)
    
    * workaround for first bucket copy
    
    * restore collect tests and installed packs
    
    * restore conf.json copy
    
    * update demisto-sdk commit (bb15d8495d4bca36861c5ce7e2579ce706b052ff)
    
    * fix create-id-set param and upload all packs
    
    * upload all packs
    
    * collapse some steps
    
    * Merge branch 'master' into lint_in_upload_flow
    
    # Conflicts:
    #	.circleci/config.yml
    #	.gitlab/ci/global.yml
    #	dev-requirements-py3.txt
    
    * update demisto-sdk commit (6a6d443e4a610023a858e64ffeb0f5a003ee2442)
    
    * filter uploaded packs by marketplaces field
    
    * fix KeyError in pack statuses
    
    * fix status description
    
    * fix build bucket path
    
    * update mp v2 production bucket
    
    * update core packs for mp v2
    
    * fix missing parameter
    
    * add debug print
    
    * fix marketplace v2 key and remove debug print
    
    * fix unit tests
    
    * fix unit tests
    
    * fix private build
    
    * fix private build
    
    * fix lint errors
    
    * fix CR comments
    
    * fix find-dependencies command paramter
    
    * unified sections between the two create-instances versions
    
    * fix last comments
    
    Co-authored-by: nmaimon <nmaimon@paloaltonetworks.com>

--- Tests/private_build/upload_packs_private.py
+++ Tests/private_build/upload_packs_private.py
@@ -506,7 +506,7 @@ def main():
     # detect packs to upload
     pack_names = get_packs_names(target_packs)
     extract_packs_artifacts(packs_artifacts_path, extract_destination_path)
-    packs_list = [Pack(pack_name, os.path.join(extract_destination_path, pack_name)) for pack_name in pack_names
+    packs_list = [Pack(pack_name, os.path.join(extract_destination_path, pack_name), 'xsoar') for pack_name in pack_names
                   if os.path.exists(os.path.join(extract_destination_path, pack_name))]
 
     if not is_private_build:
