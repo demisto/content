018cbbaf58cd15a21b4ae02ec2d9e5128c90e039
darkushin <darkushin@paloaltonetworks.com>
2022-01-18 18:44:05 +0200

    Updated UTs and added RNs

--- Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
+++ Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
@@ -164,7 +164,7 @@ def test_redlock_search_event(mocker):
             },
             "relativeTimeType": "BACKWARD"
         },
-        "query": "event from cloud.audit_logs where ip EXISTS AND ip IN (10.0.199.50)",
+        "query": "event from cloud.audit_logs where ip EXISTS AND ip IN (35.180.1.1)",
         "data": {
             "totalRows": 11310,
             "items": [{
@@ -176,7 +176,7 @@ def test_redlock_search_event(mocker):
                 "source": "s3.amazonaws.com",
                 "name": "CreateBucket",
                 "id": 2559773241,
-                "ip": "10.0.199.50",
+                "ip": "35.180.1.1",
                 "accessKeyUsed": False,
                 "cityId": -4,
                 "cityName": "Private",
@@ -188,7 +188,6 @@ def test_redlock_search_event(mocker):
                 "cityLongitude": -1.0,
                 "success": False,
                 "internal": False,
-                "dynamicData": {},
                 "location": "Private",
                 "accountName": "aws-emea-tac",
                 "regionName": "AWS Oregon",
@@ -198,19 +197,20 @@ def test_redlock_search_event(mocker):
     }
     expected_context_entry = {
         'Redlock.Event(val.id == obj.id)': [{
-                                                'account': '712829893241', 'regionId': 4, 'eventTs': 1642497763000,
-                                                'subject': 'ejb-iam-cloudops', 'type': 'CREATE',
-                                                'source': 's3.amazonaws.com', 'name': 'CreateBucket', 'id': 2559773241,
-                                                'ip': '10.0.199.50', 'accessKeyUsed': False, 'cityId': -4,
-                                                'cityName': 'Private', 'stateId': -4, 'stateName': 'Private',
-                                                'countryId': -4, 'countryName': 'Private', 'cityLatitude': -1.0,
-                                                'cityLongitude': -1.0, 'success': False, 'internal': False,
-                                                'dynamicData': {}, 'location': 'Private', 'accountName': 'aws-emea-tac',
-                                                'regionName': 'AWS Oregon'
-                                            }]
+            'account': '712829893241', 'regionId': 4, 'eventTs': 1642497763000,
+            'subject': 'ejb-iam-cloudops', 'type': 'CREATE',
+            'source': 's3.amazonaws.com', 'name': 'CreateBucket', 'id': 2559773241,
+            'ip': '35.180.1.1', 'accessKeyUsed': False, 'cityId': -4,
+            'cityName': 'Private', 'stateId': -4, 'stateName': 'Private',
+            'countryId': -4, 'countryName': 'Private', 'cityLatitude': -1.0,
+            'cityLongitude': -1.0, 'success': False, 'internal': False,
+            'dynamicData': {}, 'location': 'Private', 'accountName': 'aws-emea-tac',
+            'regionName': 'AWS Oregon'
+        }]
     }
     mocker.patch('RedLock.req', return_value=get_result_response)
-    mocker.patch.object(demisto, 'args', return_value={"query": "event from cloud.audit_logs where ip EXISTS AND ip IN (10.0.199.50)"})
+    mocker.patch.object(demisto, 'args', return_value={"query": "event from cloud.audit_logs where ip EXISTS AND ip "
+                                                                "IN (35.180.1.1)"})
     mocker.patch.object(demisto, 'results')
     redlock_search_event()
     assert demisto.results.call_args[0][0].get('EntryContext') == expected_context_entry
@@ -246,61 +246,12 @@ def test_redlock_search_network(mocker):
         "data": {
             "nodes": [{
                 "id": -1065892745,
-                "name": "172.31.26.196",
-                "ipAddr": "172.31.26.196",
+                "name": "172.31.34.235",
+                "ipAddr": "172.31.34.235",
                 "grouped": False,
                 "suspicious": False,
                 "vulnerable": False,
-                "metadata": {
-                    "redlock_alert_count": 0,
-                    "host_vulnerability_count": 0,
-                    "vpc_name": [
-                        "Unavailable"
-                    ],
-                    "initial": True,
-                    "ip_addresses": [
-                        "172.31.26.196"
-                    ],
-                    "inspector_rba_count": 0,
-                    "region_id": [
-                        "us-east-2"
-                    ],
-                    "guard_duty_iam_count": 0,
-                    "net_iface_id": [
-                        "Unavailable"
-                    ],
-                    "guard_duty_host_count": 0,
-                    "tags": [
-                        "None"
-                    ],
-                    "security_groups": [
-                        "Unavailable"
-                    ],
-                    "serverless_vulnerability_count": 0,
-                    "instance_id": [
-                        "N/A"
-                    ],
-                    "account_id": [
-                        "274307705868"
-                    ],
-                    "cloud_type": [
-                        "aws"
-                    ],
-                    "asset_role": [
-                        "Unavailable"
-                    ],
-                    "account_name": [
-                        "RedlockSandbox"
-                    ],
-                    "resource_id": [
-                        "None"
-                    ],
-                    "inspector_sbp_count": 0,
-                    "region_name": [
-                        "AWS Ohio"
-                    ],
-                    "compliance_count": 0
-                }
+                "metadata": {"keys": "values"}
             }],
             "connections": [
                 {
@@ -308,52 +259,17 @@ def test_redlock_search_network(mocker):
                     "to": 1418248367,
                     "label": "Postgres",
                     "suspicious": False,
-                    "metadata": {
-                        "account_id": [
-                            "274307705868"
-                        ],
-                        "cloud_type": [
-                            "aws"
-                        ],
-                        "bytes_attempted": 0,
-                        "connection_overview_table": [
-                            {
-                                "port": "Postgres",
-                                "traffic_volume": 83938,
-                                "accepted": "yes"
-                            }
-                        ],
-                        "region_id": [
-                            "us-east-2"
-                        ],
-                        "bytes_accepted": 83938,
-                        "to_ip_addresses": [
-                            "172.31.17.210"
-                        ],
-                        "flow_class": [
-                            "Postgres"
-                        ],
-                        "from_ip_addresses": [
-                            "172.31.20.235"
-                        ],
-                        "bytes_rejected": 0
-                    }
+                    "metadata": {"keys": "values"}
                 }
             ]
         }
     }
     expected_context_entry = {
         'Redlock.Network.Node(val.id == obj.id)': [{
-            'id': -1065892745, 'name': '172.31.26.196',
-            'ipAddr': '172.31.26.196', 'grouped': False, 'suspicious': False,
+            'id': -1065892745, 'name': '172.31.34.235',
+            'ipAddr': '172.31.34.235', 'grouped': False, 'suspicious': False,
             'vulnerable': False, 'metadata': {
-                'redlock_alert_count': 0, 'host_vulnerability_count': 0, 'vpc_name': ['Unavailable'], 'initial': True,
-                'ip_addresses': ['172.31.26.196'], 'inspector_rba_count': 0, 'region_id': ['us-east-2'],
-                'guard_duty_iam_count': 0, 'net_iface_id': ['Unavailable'], 'guard_duty_host_count': 0,
-                'tags': ['None'], 'security_groups': ['Unavailable'], 'serverless_vulnerability_count': 0,
-                'instance_id': ['N/A'], 'account_id': ['274307705868'], 'cloud_type': ['aws'],
-                'asset_role': ['Unavailable'], 'account_name': ['RedlockSandbox'], 'resource_id': ['None'],
-                'inspector_sbp_count': 0, 'region_name': ['AWS Ohio'], 'compliance_count': 0
+                'keys': 'values'
             }
         }], 'Redlock.Network.Connection(val.id == obj.from)': [{
             'from': 994246246,
@@ -361,33 +277,13 @@ def test_redlock_search_network(mocker):
             'label': 'Postgres',
             'suspicious': False,
             'metadata': {
-                'account_id': [
-                    '274307705868'],
-                'cloud_type': [
-                    'aws'],
-                'bytes_attempted': 0,
-                'connection_overview_table': [
-                    {
-                        'port': 'Postgres',
-                        'traffic_volume': 83938,
-                        'accepted': 'yes'
-                    }],
-                'region_id': [
-                    'us-east-2'],
-                'bytes_accepted': 83938,
-                'to_ip_addresses': [
-                    '172.31.17.210'],
-                'flow_class': [
-                    'Postgres'],
-                'from_ip_addresses': [
-                    '172.31.20.235'],
-                'bytes_rejected': 0
+                'keys': 'values'
             }
         }]
     }
     mocker.patch('RedLock.req', return_value=get_result_response)
     mocker.patch.object(demisto, 'args', return_value={"query": "event from cloud.audit_logs where ip EXISTS AND ip "
-                                                                "IN (10.0.199.50)"})
+                                                                "IN (35.180.1.1)"})
     mocker.patch.object(demisto, 'results')
     redlock_search_network()
     assert demisto.results.call_args[0][0].get('EntryContext') == expected_context_entry
