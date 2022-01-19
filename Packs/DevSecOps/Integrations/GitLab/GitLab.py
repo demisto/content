70654a45a5a439003be598527937e9bbb76d0244
merit <meretmaayta@gmail.com>
2022-01-19 12:28:45 +0200

    edits command name

--- Packs/DevSecOps/Integrations/GitLab/GitLab.py
+++ Packs/DevSecOps/Integrations/GitLab/GitLab.py
@@ -758,7 +758,7 @@ def main():
             'gitlab-issue-edit': gitlab_edit_issue_command,
             'gitlab-group-projects-list': gitlab_group_projects_list_command,
             'gitlab-raw-file-get': gitlab_get_raw_file_command,
-            'gitlab-trigger-build': gitlab_trigger_build_command,
+            'gitlab-trigger-pipeline': gitlab_trigger_build_command,
         }
 
         if command == 'test-module':
