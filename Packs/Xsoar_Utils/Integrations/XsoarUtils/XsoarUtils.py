740480326b8c141871f453a8e2b147f87b028b1e
arnarayanan <arnarayanan@paloaltonetworks.com>
2022-01-20 16:16:15 +0100

    modifying python image on yml and changed string str

--- Packs/Xsoar_Utils/Integrations/XsoarUtils/XsoarUtils.py
+++ Packs/Xsoar_Utils/Integrations/XsoarUtils/XsoarUtils.py
@@ -48,7 +48,7 @@ def log_response_terminal(res: requests.Response):
     print(res.text)
 
 
-def send_request(obj: Main_Object, path: str, method="get", data="",test=False):
+def send_request(obj: Main_Object, path: str, method="get", data="", test=False):
     endpoint = f'{obj.endpoint}{path}'
     headers = {'Authorization': obj.api_key, 'content-type': 'application/json'}
     if method == "get":
@@ -65,7 +65,7 @@ def send_request(obj: Main_Object, path: str, method="get", data="",test=False):
             return_error(f"please validate your credentials.{res.text}")
 
 
-def create_entry(obj: Main_Object, data: string, inv_id: string):
+def create_entry(obj: Main_Object, data: str, inv_id: str):
     req_args = {
         "id": "",
         "version": 0,
@@ -74,6 +74,7 @@ def create_entry(obj: Main_Object, data: string, inv_id: string):
         "markdown": True
     }
     send_request(obj, path="/entry", method="Post", data=json.dumps(req_args))
+
 
 def main():
     obj = Main_Object()
@@ -92,7 +93,7 @@ def main():
         except Exception as e:
             print(e)
             print("Calling /engines and printing results")
-            send_request(obj, path="/engines", method="get",test=True)
+            send_request(obj, path="/engines", method="get", test=True)
 
     else:
         demisto.info("Executing Xsoar_Utils, detected demisto as environment")
@@ -100,8 +101,8 @@ def main():
         command_args = demisto.args()
         if "inv_id" not in command_args.keys():
             command_args["inv_id"] = obj.playground
-        if command=="test-module":
-            send_request(obj, path="/engines", method="get",test=True)
+        if command == "test-module":
+            send_request(obj, path="/engines", method="get", test=True)
         else:
             commands_list[command](obj, **command_args)
 
