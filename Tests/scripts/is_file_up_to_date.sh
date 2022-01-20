099aa08052a242e3880bbdd1dd934ec8e0b16216
ilan <ierukhimovic@paloaltonetworks.com>
2022-01-20 15:31:55 +0200

    just delete the if

--- Tests/scripts/is_file_up_to_date.sh
+++ Tests/scripts/is_file_up_to_date.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 FILE_TO_CHECK=$1
-CONTENT_URL='https://github.com/demisto/content.git'
 
 # Checks if there's any diff from master
-git remote add mainRepo CONTENT_URL
-git fetch mainRepo
-if [[ `git diff mainRepo/master -- ${FILE_TO_CHECK}` ]]; then
+if [[ `git diff origin/master -- ${FILE_TO_CHECK}` ]]; then
     # Checks if part of the branch's changes
-    if [[ -z `git diff mainRepo/master..."$CI_COMMIT_BRANCH" --name-only | grep ${FILE_TO_CHECK}` ]]; then
+    if [[ -z `git diff origin/master..."$CI_COMMIT_BRANCH" --name-only | grep ${FILE_TO_CHECK}` ]]; then
         echo "${FILE_TO_CHECK} has been changed. Merge from master"
         exit 1
     else
