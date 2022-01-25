ae5478ba383ce6ad44490daa82e6a8d59f610d33
GuyAfik <guyafik11@gmail.com>
2022-01-25 21:41:56 +0200

    update unit-tests

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -3300,21 +3300,7 @@ INDICATOR_VALUE_AND_TYPE = [
     (
         '53e6baa124f54462786f1122e98e38ff1be3de82fe2a96b1849a8637043f'
         'd847eec7e0f53307bddf7a066565292d500c36c941f1f3bb9dcac807b2f4a0bfce1b', 'File'
-    ),
-    ('3.21.32.65/path', 'URL'),
-    ('19.117.63.253:28/other/path', 'URL'),
-    ('19.117.63.253:28/path', 'URL'),
-    ('1.1.1.1/7/server/somestring/something.php?fjjasjkfhsjasofds=sjhfhdsfhasld', 'URL'),
-    ('flake8.pycqa.org/en/latest', 'URL'),
-    ('2001:db8:85a3:8d3:1319:8a2e:370:7348/path/path', 'URL'),
-    ('2001:db8:85a3:8d3:1319:8a2e:370:7348/32/path/path', 'URL'),
-    ('https://google.com/sdlfdshfkle3247239elkxszmcdfdstgk4e5pt0/path/path/oatdsfk/sdfjjdf', 'URL'),
-    ('www.123.43.6.89/path', 'URL'),
-    ('https://15.12.76.123', 'URL'),
-    ('www.google.com/path', 'URL'),
-    ('2001:db8:85a3:8d3:1319:8a2e:370:7348/65/path/path', 'URL'),
-    ('2001:db8:3333:4444:5555:6666:7777:8888//32/path/path', 'URL'),
-    ('1.1.1.1/7/server', 'URL')
+    )
 ]
 
 
@@ -3341,7 +3327,40 @@ def test_auto_detect_indicator_type(indicator_value, indicatory_type):
                              " use a docker image with it installed such as: demisto/jmespath"
 
 
-INVALID_INDICATOR_VALUES = [
+VALID_URL_INDICATORS = [
+    '3.21.32.65/path',
+    '19.117.63.253:28/other/path',
+    '19.117.63.253:28/path',
+    '1.1.1.1/7/server/somestring/something.php?fjjasjkfhsjasofds=sjhfhdsfhasld',
+    'flake8.pycqa.org/en/latest',
+    '2001:db8:85a3:8d3:1319:8a2e:370:7348/path/path',
+    '2001:db8:85a3:8d3:1319:8a2e:370:7348/32/path/path',
+    'https://google.com/sdlfdshfkle3247239elkxszmcdfdstgk4e5pt0/path/path/oatdsfk/sdfjjdf',
+    'www.123.43.6.89/path',
+    'https://15.12.76.123',
+    'www.google.com/path',
+    '2001:db8:85a3:8d3:1319:8a2e:370:7348/65/path/path',
+    '2001:db8:3333:4444:5555:6666:7777:8888//32/path/path',
+    '1.1.1.1/7/server'
+]
+
+
+@pytest.mark.parametrize('indicator_value', VALID_URL_INDICATORS)
+def test_valid_url_indicator_types(indicator_value):
+    """
+    Given
+    - Valid URL indicators.
+
+    When
+    - Trying to match those indicators with the URL regex.
+
+    Then
+    - The indicators are classified as URL indicators.
+    """
+    assert re.match(urlRegex, indicator_value)
+
+
+INVALID_URL_INDICATORS = [
     "test",
     "httn://bla.com/path",
     "google.com*",
@@ -3368,8 +3387,18 @@ INVALID_INDICATOR_VALUES = [
 ]
 
 
-@pytest.mark.parametrize('indicator_value', INVALID_INDICATOR_VALUES)
+@pytest.mark.parametrize('indicator_value', INVALID_URL_INDICATORS)
 def test_invalid_url_indicator_types(indicator_value):
+    """
+    Given
+    - invalid URL indicators.
+
+    When
+    - Trying to match those indicators with the URL regex.
+
+    Then
+    - The indicators are not classified as URL indicators.
+    """
     assert not re.match(urlRegex, indicator_value)
 
 
