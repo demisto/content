89c5349bd34b23e476b9ea55ada580723338dea1
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-26 10:47:40 +0200

    change whois

--- Packs/Whois/Integrations/Whois/Whois.py
+++ Packs/Whois/Integrations/Whois/Whois.py
@@ -13,7 +13,7 @@ SHOULD_ERROR = demisto.params().get('with_error', False)
 # flake8: noqa
 
 """
-    This integration is built using the joepie91 "Whois" module. For more information regarding this package please see
+    aThis integration is built using the joepie91 "Whois" module. For more information regarding this package please see
     the following - https://github.com/joepie91/python-whois
 """
 
