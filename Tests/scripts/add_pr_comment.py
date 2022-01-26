d3f9426891336b0a4bbdb5a5eb73ff41f398c44e
meichlerpanw <meichler@paloaltonetworks.com>
2022-01-26 10:47:11 +0200

    import function

--- Tests/scripts/add_pr_comment.py
+++ Tests/scripts/add_pr_comment.py
@@ -1,5 +1,6 @@
 import os
-import requests
+from demisto_sdk.commands.test_content.execute_test_content import _add_pr_comment
+from demisto_sdk.commands.test_content.execute_test_content import ParallelLoggingManager
 
 
 COVERAGE_REPORT_COMMENT = 'Link to the unit tests coverage report'
@@ -8,37 +9,6 @@ COVERAGE_LINK = f'https://xsoar.docs.pan.run/-/content/-/jobs/{JOB_ID}/artifacts
                 f'index.html'
 
 
-def add_pr_comment():
-    token = os.environ['CONTENT_GITHUB_TOKEN']
-    branch_name = os.environ['CI_COMMIT_BRANCH']
-    sha1 = os.environ['CI_COMMIT_SHA']
-
-    query = '?q={}+repo:demisto/content+org:demisto+is:pr+is:open+head:{}+is:open'.format(sha1, branch_name)
-    url = 'https://api.github.com/search/issues'
-    headers = {'Authorization': 'Bearer ' + token}
-    try:
-        response = requests.get(url + query, headers=headers)
-        res_dict = response.json()
-
-        if res_dict and res_dict.get('total_count', 0) == 1:
-            issue_url = res_dict['items'][0].get('comments_url') if res_dict.get('items', []) else None
-            if issue_url:
-                response = requests.get(issue_url, headers=headers)
-                issue_comments = response.json()
-                for existing_comment in issue_comments:
-                    # Check if a comment about report coverage already exists. If there is delete it first and then post
-                    # a new comment:
-                    if COVERAGE_REPORT_COMMENT in existing_comment.get('body'):
-                        comment_url = existing_comment.get('url')
-                        requests.delete(comment_url, headers=headers)
-                requests.post(issue_url, json={'body': f'{COVERAGE_REPORT_COMMENT}:\n {COVERAGE_LINK}'},
-                              headers=headers)
-        else:
-            print(f'Failed adding coverage report comment for pull request: There is more than one open pull request '
-                  f'for branch {branch_name}.')
-    except Exception as e:
-        print(f'Failed adding coverage report comment for pull request: {e}')
-
-
 if __name__ == "__main__":
-    add_pr_comment()
+    logging_manager = ParallelLoggingManager('Run_Tests.log')
+    _add_pr_comment(COVERAGE_LINK, logging_manager)
