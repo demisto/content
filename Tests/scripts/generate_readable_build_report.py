d28cbe43ec9a0d649b422ee531e2d0bcd7f56615
yucohen <yucohen@paloaltonetworks.com>
2022-01-27 17:19:05 +0200

    chandes the url in login

---
+++ Tests/scripts/generate_readable_build_report.py
@@ -0,0 +1,96 @@
+import os
+import requests
+from gitlab_slack_notifier import get_artifact_data
+
+
+def generate_report_from_build_results():
+    failed_ut_msg = get_failing_ut()
+    failed_validate_msg = get_failing_validations()
+    failed_tpb_msg = get_failed_tbp()
+
+
+
+
+FAILED_UT_COMMENT = 'This is the link to the failed unit tests'
+JID = os.environ.get("CI_JOB_ID")
+FAILED_UT_LINK = f'https://xsoar.docs.pan.run/-/content/-/jobs/{JID}/artifacts/failed_unit_tests.txt'
+example_run_tests_file = "Users/yucohen/Downloads/Run_Tests (1).log"
+
+
+
+
+def get_failing_ut():
+    failed_ut = get_artifact_data(ARTIFACTS_FOLDER, "failed_unit_tests.txt")
+    if failed_ut:
+        failed_ut_list = failed_ut.split('\n')
+        write_data_to_summay_file()
+        return f'you have {len(failed_ut_list)} failed unit test on this push.\n'
+    return 'no failing unit tests on this one. nice job!'
+
+
+def get_failing_validations():
+    failed_ut = get_artifact_data(ARTIFACTS_FOLDER, "failed_unit_tests.txt")
+    if failed_ut:
+        failed_ut_list = failed_ut.split('\n')
+        write_data_to_summay_file()
+        return f'you have {len(failed_ut_list)} failed unit test on this push.\n'
+    return 'no failing unit tests on this one. nice job!'
+
+
+
+def get_failed_tests_log_block_from_log_file(log_file_path: str, failed_tpb_file_path: str):
+    failed_tests = failed_tests_list(failed_tpb_file_path)
+    for test in failed_tests:
+        log_block = get_log_block_for_test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def add_pr_comment():
+    token = os.environ['CONTENT_GITHUB_TOKEN']
+    branch_name = os.environ['CI_COMMIT_BRANCH']
+    sha1 = os.environ['CI_COMMIT_SHA']
+
+    query = '?q={}+repo:demisto/content+org:demisto+is:pr+is:open+head:{}+is:open'.format(sha1, branch_name)
+    url = 'https://api.github.com/search/issues'
+    headers = {'Authorization': 'Bearer ' + token}
+    try:
+        response = requests.get(url + query, headers=headers)
+        res_dict = response.json()
+
+        if res_dict and res_dict.get('total_count', 0) == 1:
+            issue_url = res_dict['items'][0].get('comments_url') if res_dict.get('items', []) else None
+            if issue_url:
+                response = requests.get(issue_url, headers=headers)
+                issue_comments = response.json()
+                for existing_comment in issue_comments:
+                    # Check if a comment about report coverage already exists. If there is delete it first and then post
+                    # a new comment:
+                    if FAILED_UT_COMMENT in existing_comment.get('body'):
+                        comment_url = existing_comment.get('url')
+                        requests.delete(comment_url, headers=headers)
+                requests.post(issue_url, json={'body': f'{FAILED_UT_COMMENT}:\n {FAILED_UT_LINK}'},
+                              headers=headers)
+        else:
+            print(f'Add pull request comment failed: There is more then one open pull request for branch {branch_name}.')
+    except Exception as e:
+        print(f'Add pull request comment failed with error {e}')
+
+
+if __name__ == "__main__":
+    add_pr_comment()
