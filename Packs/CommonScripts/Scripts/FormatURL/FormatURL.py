0f64726b3dc5df10eaf417bb2f5944990bc1a80d
Farrukh Ahmed <33961435+linuxpakistan@users.noreply.github.com>
2022-01-31 18:55:37 +0500

    Update FormatURL.py

--- Packs/CommonScripts/Scripts/FormatURL/FormatURL.py
+++ Packs/CommonScripts/Scripts/FormatURL/FormatURL.py
@@ -24,6 +24,27 @@ PREFIX_CHANGES: List[Tuple[str, Optional[str], str]] = [
 ]
 
 
+def get_redirect_url_fireeye(non_formatted_url: str, parse_results: ParseResult, redirect_param_name: str) -> str:
+    """
+    Receives an FireEye Safe Link URL, returns the URL the FireEye Safe Link points to.
+    Args:
+        non_formatted_url (str): The raw URL. For debugging purposes.
+        parse_results (str): FireEye Safe Link URL parse results.
+        redirect_param_name (str): Name of the redirect parameter.
+    Returns:
+        (str): The URL the FireEye Safe Link points to.
+    """
+    query_params_dict: Dict[str, List[str]] = parse_qs(parse_results.query)
+    if not (query_urls := query_params_dict.get(redirect_param_name, [])):
+        demisto.error(f'Could not find redirected URL. Returning the original URL: {non_formatted_url}')
+        return non_formatted_url
+    if len(query_urls) > 1:
+        demisto.debug(f'Found more than one URL query parameters for redirect in the given URL {non_formatted_url}\n'
+                      f'Returning the first URL: {query_urls[0]}')
+    url_: str = query_urls[0]
+    return url_
+
+
 def get_redirect_url_proof_point_v2(non_formatted_url: str, parse_results: ParseResult) -> str:
     """
     Extracts redirect URL from Proof Point V2.
@@ -153,9 +174,9 @@ def format_urls(non_formatted_urls: List[str]) -> List[Dict]:
             non_formatted_url = get_redirect_url_from_query(non_formatted_url, parse_results, 'url')
         elif re.match(FIREEYE_REGEX, non_formatted_url):
             if '&amp;' in non_formatted_url:
-                non_formatted_url = get_redirect_url_from_query(non_formatted_url, parse_results, 'amp;u')
+                non_formatted_url = get_redirect_url_fireeye(non_formatted_url, parse_results, 'amp;u')
             else:
-                non_formatted_url = get_redirect_url_from_query(non_formatted_url, parse_results, 'u')
+                non_formatted_url = get_redirect_url_fireeye(non_formatted_url, parse_results, 'u')
         elif match := PROOF_POINT_URL_REG.search(non_formatted_url):
             proof_point_ver: str = match.group(2)
             if proof_point_ver == 'v3':
