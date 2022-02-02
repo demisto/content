869fcdaa986bc51620c7e0107c92450233c38e34
MLainer1 <maylainer96@gmail.com>
2022-02-02 15:24:31 +0200

    lint

--- Packs/Cylance_Protect/Integrations/Cylance_Protect_v2/Cylance_Protect_v2_test.py
+++ Packs/Cylance_Protect/Integrations/Cylance_Protect_v2/Cylance_Protect_v2_test.py
@@ -1,7 +1,18 @@
 from CommonServerPython import Common
 from Cylance_Protect_v2 import create_dbot_score_entry, translate_score, FILE_THRESHOLD
 
-THREAT_OUTPUT = {u'cylance_score': -1.0, u'name': u'SysMonitor.exe', u'classification': u'Malware', u'sub_classification': u'Virus', u'av_industry': None, u'unique_to_cylance': False, u'last_found': u'2019-01-28T23:36:58', u'global_quarantined': False, u'file_size': 2177386, u'safelisted': False, u'sha256': u'055D7A25DECF6769BF4FB2F3BC9FD3159C8B42972818177E44975929D97292DE', u'md5': u'B4EA38EB798EA1C1E067DFD176B882BB'}
+THREAT_OUTPUT = {u'cylance_score': -1.0, u'name': u'SysMonitor.exe',
+                 u'classification': u'Malware',
+                 u'sub_classification': u'Virus',
+                 u'av_industry': None,
+                 u'unique_to_cylance': False,
+                 u'last_found': u'2019-01-28T23:36:58',
+                 u'global_quarantined': False,
+                 u'file_size': 2177386,
+                 u'safelisted': False,
+                 u'sha256': u'055D7A25DECF6769BF4FB2F3BC9FD3159C8B42972818177E44975929D97292DE',
+                 u'md5': u'B4EA38EB798EA1C1E067DFD176B882BB',
+                 }
 
 
 def test_get_threats():
