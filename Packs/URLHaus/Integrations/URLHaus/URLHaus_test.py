8586a0f66ec35245e04dbb5906d4bc9204d99c03
aradcarmi <aradcarmi220@gmail.com>
2022-02-03 16:19:21 +0200

    Fixed tests

--- Packs/URLHaus/Integrations/URLHaus/URLHaus_test.py
+++ Packs/URLHaus/Integrations/URLHaus/URLHaus_test.py
@@ -1,9 +1,14 @@
 import json
-import requests
-import URLHaus
 import pytest
+import io
 import demistomock as demisto
-from CommonServerPython import Common, return_results
+from CommonServerPython import Common
+
+
+def util_load_json(path):
+    with io.open(path, mode='r', encoding='utf-8') as f:
+        return json.loads(f.read())
+
 
 url_command_test = [
     ('ok', ['test_tag1', 'test_tag2']),
@@ -15,7 +20,7 @@ url_command_test = [
 
 
 @pytest.mark.parametrize('query_status,tags', url_command_test)
-def test_url_command(mocker, query_status, tags):
+def test_url_command(mocker, requests_mock, query_status, tags, ):
     """
         Given
         - An URL.
@@ -29,58 +34,28 @@ def test_url_command(mocker, query_status, tags):
         - Validate that the relationships were created
 
     """
-    response = requests.models.Response()
-    response._content = json.dumps({'query_status': query_status,
-                                    'id': '105821',
-                                    'urlhaus_reference': 'https:\/\/urlhaus.abuse.ch\/url\/105821\/',
-                                    'url': 'http:\/\/sskymedia.com\/VMYB-ht_JAQo-gi\/INV\/99401FORPO\/20673114777\/US'
-                                           '\/Outstanding-Invoices\/',
-                                    'url_status': 'online',
-                                    'host': 'sskymedia.com',
-                                    'date_added': '2019-01-19 01:33:26 UTC',
-                                    'threat': 'malware_download',
-                                    'tags': tags,
-                                    'blacklists': {
-                                        'spamhaus_dbl': 'not listed',
-                                        'surbl': 'not listed'
-                                    },
-                                    'payloads': [
-                                        {
-                                            'firstseen': '2019-01-19',
-                                            'filename': '676860772178.doc',
-                                            'file_type': 'doc',
-                                            'response_size': '172752',
-                                            'response_md5': 'cf6bc359bc8a667c1b8d241e9591f392',
-                                            'response_sha256':
-                                                '72820698de9b69166ab226b99ccf70f3f58345b88246f7d5e4e589c21dd44435',
-                                            'urlhaus_download': 'https:\/\/urlhaus-api.abuse.ch\/v1\/download'
-                                                                '\/72820698de9b69166ab226b99ccf70'
-                                                                'f3f58345b88246f7d5e4e589c21dd44435\/',
-                                            'signature': 'Heodo',
-                                            'virustotal': {
-                                                'result': '18 \/ 58',
-                                                'percent': '31.03',
-                                                'link': 'https:\/\/www.virustotal.com\/file'
-                                                        '\/72820698de9b69166ab226b99ccf70f3f58345b882'
-                                                        '46f7d5e4e589c21dd44435\/analysis\/1547876224\/ '
-                                            }
-                                        }]}).encode('utf-8')
-    mocker.patch.object(URLHaus, 'query_url_information', return_value=response)
-    result = mocker.patch.object(demisto, 'results')
-    URLHaus.main()
+    from URLHaus import url_command
 
-    context = result.call_args[0][0]['EntryContext']
-    URL = context.get('URL(val.Data && val.Data == obj.Data)', {})
-    assert 'Data' in URL[0] if query_status == 'ok' or query_status == 'no_results' else 'Data' not in URL
-    assert all(elem in URL[0]['Tags'] for elem in tags) if query_status == 'ok' else 'Tags' not in URL
-    assert 'Relationships' in URL[0] if query_status == 'ok' else 'Relationships' not in URL
-    Dbot_score = context.get('DBotScore(val.Indicator && val.Indicator '
-                             '== obj.Indicator && val.Vendor == obj.Vendor '
-                             '&& val.Type == obj.Type)')
-    if query_status == 'ok':
-        assert Dbot_score[0]['Score'] == 3
-    else:
-        assert Dbot_score[0]['Score'] == 0 if query_status == 'no_results' else not Dbot_score
+    url_to_check = "www.test_url.com"
+    mock_response = util_load_json('test_data/url_command.json')
+    mock_params = util_load_json('test_data/params.json')
+    mock_response['query_status'] = query_status
+    mock_response['tags'] = tags
+    requests_mock.post(f'http://test.com/api/v1/url/',
+                       json=mock_response)
+    mocker.patch.object(demisto, 'args',
+                        return_value={'url': url_to_check})
+    results = url_command(**mock_params)
+
+    URL = results.indicator
+    if URL:
+        assert URL.url == url_to_check
+        if query_status == 'ok':
+            assert all(elem in URL.tags for elem in tags)
+            assert URL.relationships
+        else:
+            assert not URL.tags
+            assert not URL.relationships
 
 
 url_command_test_reliability_dbot_score = [
@@ -106,8 +81,9 @@ def test_url_reliability_dbot_score(status, excepted_output):
         - Successes with right dbot score.
 
     """
+    from URLHaus import calculate_dbot_score
 
-    output = URLHaus.calculate_dbot_score('url', status)
+    output = calculate_dbot_score('url', status)
     for i in range(len(excepted_output)):
         assert output[i] == excepted_output[i]
 
@@ -146,7 +122,8 @@ def test_url_create_payloads(test_data, excepted_output):
         - list of {name,type,md5,sha256,vt}
 
         """
-    assert URLHaus.url_create_payloads(url_information=test_data) == excepted_output
+    from URLHaus import url_create_payloads
+    assert url_create_payloads(url_information=test_data) == excepted_output
 
 
 url_command_test_create_blacklists = [
@@ -175,8 +152,8 @@ def test_url_create_blacklists(test_data, excepted_output):
         - Return list of {name,status}.
 
     """
-
-    assert URLHaus.url_create_blacklist(url_information=test_data) == excepted_output
+    from URLHaus import url_create_blacklist
+    assert url_create_blacklist(url_information=test_data) == excepted_output
 
 
 url_command_test_create_relationships = [
@@ -210,6 +187,8 @@ def test_url_command_create_relationships(host, host_type, create_relationship, 
         - Return indicators relationships table {Relationship,EntityA,EntityAType,EntityB,EntityBType}.
 
     """
+    from URLHaus import url_create_relationships
+
     files = [{
         'Name': f'test_file{i}',
         'Type': f'test_type{i}',
@@ -238,7 +217,7 @@ def test_url_command_create_relationships(host, host_type, create_relationship, 
             'EntityBType': 'File',
         } for i in range(max_num_relationships - 1)])
     kwargs = {'create_relationships': create_relationship, 'max_num_of_relationships': max_num_relationships}
-    results = URLHaus.url_create_relationships(uri, host, files, **kwargs)
+    results = url_create_relationships(uri, host, files, **kwargs)
     assert len(results) == len(excepted_output)
     for i in range(len(results)):
         assert results[i].to_context() == excepted_output[i]
@@ -258,7 +237,7 @@ domain_command_test = [
 
 
 @pytest.mark.parametrize('query_status,spamhaus_dbl,expected_tag', domain_command_test)
-def test_domain_command(mocker, query_status, spamhaus_dbl, expected_tag):
+def test_domain_command(requests_mock, mocker, query_status, spamhaus_dbl, expected_tag):
     """
         Given
         - A Domain.
@@ -271,61 +250,20 @@ def test_domain_command(mocker, query_status, spamhaus_dbl, expected_tag):
         - Validate that the relationships were created
 
     """
-    params = {
-        'api_url': 'test',
-        'use_ssl': False,
-        'threshold': 1,
-        'create_relationships': True,
-        'max_num_of_relationships': 1
-    }
-    response = requests.models.Response()
-    response._content = json.dumps({
-        "query_status": query_status,
-        "urlhaus_reference": "https:\/\/urlhaus.abuse.ch\/host\/vektorex.com\/",
-        "host": "vektorex.com",
-        "firstseen": "2019-01-15 07:09:01 UTC",
-        "url_count": "120",
-        "blacklists": {
-            "spamhaus_dbl": spamhaus_dbl,
-            "surbl": "not listed"
-        },
-        "urls": [
-            {
-                "id": "121319",
-                "urlhaus_reference": "https:\/\/urlhaus.abuse.ch\/url\/121319\/",
-                "url": "http:\/\/vektorex.com\/source\/Z\/5016223.exe",
-                "url_status": "online",
-                "date_added": "2019-02-11 07:45:05 UTC",
-                "threat": "malware_download",
-                "reporter": "abuse_ch",
-                "larted": "false",
-                "takedown_time_seconds": '',
-                "tags": [
-                    "AZORult",
-                    "exe"
-                ]
-            },
-            {
-                "id": "121316",
-                "urlhaus_reference": "https:\/\/urlhaus.abuse.ch\/url\/121316\/",
-                "url": "http:\/\/vektorex.com\/source\/Z\/Order%20839.png",
-                "url_status": "online",
-                "date_added": "2019-02-11 06:47:03 UTC",
-                "threat": "malware_download",
-                "reporter": "abuse_ch",
-                "larted": "false",
-                "takedown_time_seconds": '',
-                "tags": [
-                    "exe",
-                    "Loki"
-                ]
-            }]}).encode('utf-8')
-    mocker.patch.object(URLHaus, 'query_host_information', return_value=response)
-    result = mocker.patch.object(demisto, 'results')
-    return_results(URLHaus.domain_command(**params))
+    from URLHaus import domain_command
 
-    context = result.call_args[0][0]['EntryContext']
-    Domain = context['Domain'] if not(query_status == 'invalid_host') else None
+    domain_to_check = "test.com"
+    mock_response = util_load_json('test_data/domain_command.json')
+    mock_params = util_load_json('test_data/params.json')
+    mock_response['query_status'] = query_status
+    mock_response['blacklists']['spamhaus_dbl'] = spamhaus_dbl
+    requests_mock.post('http://test.com/api/v1/host/',
+                       json=mock_response)
+    mocker.patch.object(demisto, 'args',
+                        return_value={'domain': domain_to_check})
+    results = domain_command(**mock_params)
+
+    Domain = results.outputs.get('Domain', '')
     if Domain:
         assert 'Name' in Domain
         if expected_tag:
@@ -379,8 +317,9 @@ def test_domain_reliability_dbot_score(blacklist, excepted_output):
         - Successes with right dbot score.
 
     """
+    from URLHaus import calculate_dbot_score
 
-    output = URLHaus.calculate_dbot_score('domain', blacklist)
+    output = calculate_dbot_score('domain', blacklist)
     for i in range(len(excepted_output)):
         assert output[i] == excepted_output[i]
 
@@ -416,6 +355,8 @@ def test_domain_command_test_create_relationships(create_relationship, max_num_r
         - Add blacklist status information to the tags list.
 
     """
+    from URLHaus import domain_create_relationships
+
     urls = [{
         'url': f'test_url{i}',
     } for i in range(10000)]  # Large amounts of urls
@@ -430,7 +371,7 @@ def test_domain_command_test_create_relationships(create_relationship, max_num_r
             'EntityBType': 'URL',
         } for i in range(max_num_relationships)])
     kwargs = {'create_relationships': create_relationship, 'max_num_of_relationships': max_num_relationships}
-    results = URLHaus.domain_create_relationships(urls, domain, **kwargs)
+    results = domain_create_relationships(urls, domain, **kwargs)
     assert len(results) == len(excepted_output)
     for i in range(len(results)):
         assert results[i] == excepted_output[i]
@@ -462,8 +403,10 @@ def test_domain_add_tags(blacklist_status, excepted_output):
         - Return indicators relationships table {Relationship,EntityA,EntityAType,EntityB,EntityBType}.
 
     """
+    from URLHaus import domain_add_tags
+
     tags = []
-    URLHaus.domain_add_tags(blacklist_status, tags)
+    domain_add_tags(blacklist_status, tags)
     assert tags == excepted_output
 
 
@@ -477,7 +420,7 @@ file_command_test = [
 
 
 @pytest.mark.parametrize('query_status,ssdeep,expected_ssdeep', file_command_test)
-def test_file_command(mocker, query_status, ssdeep, expected_ssdeep):
+def test_file_command(mocker, requests_mock, query_status, ssdeep, expected_ssdeep):
     """
         Given
         - A File.
@@ -490,58 +433,20 @@ def test_file_command(mocker, query_status, ssdeep, expected_ssdeep):
         - Validate that the relationships were created.
 
     """
-    params = {
-        'api_url': 'test',
-        'use_ssl': False,
-        'threshold': 1,
-        'create_relationships': True,
-        'max_num_of_relationships': 1}
-    response = requests.models.Response()
-    response._content = json.dumps({
-        "query_status": query_status,
-        "md5_hash": "1585ad28f7d1e0ca696e6c6c2f1d008a",
-        "sha256_hash": "35e304d10d53834e3e41035d12122773c9a4d183a24e03f980ad3e6b2ecde7fa",
-        "file_type": "exe",
-        "file_size": "241664",
-        "signature": "test_sig",
-        "firstseen": "2019-01-19 13:59:06",
-        "lastseen": "2019-01-19 14:48:08",
-        "urlhaus_download": "test",
-        "virustotal": {
-            "result": "17 \/ 69",
-            "percent": "24.64",
-            "link": "test"
-        },
-        "imphash": "3b91ed9563d0f99f26b86bd20539306b",
-        "ssdeep": ssdeep,
-        "tlsh": "7934BF47B4F1C871E4B30D311831D9A05A"
-                "2F7D715F659E6B2778222A8E342D09E35FAB",
-        "urls": [
-            {
-                "url_id": "105243",
-                "url": "test_url",
-                "url_status": "offline",
-                "urlhaus_reference": "https:\/\/urlhaus.abuse.ch\/url\/105243\/",
-                "filename": "PAY2632216543098764.doc",
-                "firstseen": "2019-01-19",
-                "lastseen": ''
-            },
-            {
-                "url_id": "105214",
-                "url": "http:\/\/demo.trydaps.com\/gzVv-22Omv_aIQZybVK-aJ\/En\/Question\/",
-                "url_status": "offline",
-                "urlhaus_reference": "https:\/\/urlhaus.abuse.ch\/url\/105214\/",
-                "filename": "5518475554292.doc",
-                "firstseen": "2019-01-19",
-                "lastseen": ''
-            }]
-    }).encode('utf-8')
-    mocker.patch.object(URLHaus, 'query_payload_information', return_value=response)
-    result = mocker.patch.object(demisto, 'results')
-    return_results(URLHaus.file_command(**params))
+    from URLHaus import file_command
 
-    context = result.call_args[0][0]['EntryContext']
-    File = context['File'] if (query_status == 'ok') else None
+    file_to_check = 'a' * 32
+    mock_response = util_load_json('test_data/file_command.json')
+    mock_params = util_load_json('test_data/params.json')
+    mock_response['query_status'] = query_status
+    mock_response['ssdeep'] = ssdeep
+    requests_mock.post('http://test.com/api/v1/payload/',
+                       json=mock_response)
+    mocker.patch.object(demisto, 'args',
+                        return_value={'file': file_to_check})
+    results = file_command(**mock_params)
+
+    File = '' if not results.outputs else results.outputs.get('File', '')
     if File:
         assert 'SHA256' in File
         if expected_ssdeep:
@@ -562,7 +467,8 @@ def test_file_reliability_dbot_score():
         - Return Bad sbot score.
 
     """
-    dbot_score = URLHaus.calculate_dbot_score('file', '')[0]
+    from URLHaus import calculate_dbot_score
+    dbot_score = calculate_dbot_score('file', '')[0]
     assert dbot_score == Common.DBotScore.BAD
 
 
@@ -590,6 +496,7 @@ def test_file_create_relationships(create_relationship, max_num_relationships):
         - Return indicators relationships table {Relationship,EntityA,EntityAType,EntityB,EntityBType}.
 
     """
+    from URLHaus import file_create_relationships
     urls = [{
         'url': f'test_url{i}',
     } for i in range(10000)]  # Large amounts of urls
@@ -612,7 +519,7 @@ def test_file_create_relationships(create_relationship, max_num_relationships):
             'EntityBType': 'URL',
         } for i in range(max_num_relationships - 1)])
     kwargs = {'create_relationships': create_relationship, 'max_num_of_relationships': max_num_relationships}
-    results = URLHaus.file_create_relationships(file=file, urls=urls, sig=sig, **kwargs)
+    results = file_create_relationships(file=file, urls=urls, sig=sig, **kwargs)
     assert len(results) == len(excepted_output)
     for i in range(len(results)):
         assert results[i] == excepted_output[i]
