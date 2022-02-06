0729a0754f301117bd6f877ade84d481063da840
esharf <esahrf@paloaltonetworks.com>
2022-02-06 13:18:59 +0200

    remove unused imports

--- Tests/scripts/gitlab_slack_notifier.py
+++ Tests/scripts/gitlab_slack_notifier.py
@@ -1,10 +1,8 @@
 import argparse
 from datetime import datetime, timedelta
-import json
 import logging
 import os
 from typing import Tuple, Optional
-import requests
 import gitlab
 from slack import WebClient as SlackClient
 
