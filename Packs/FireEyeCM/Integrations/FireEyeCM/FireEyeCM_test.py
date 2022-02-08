1b688489fc9e482807d81cbf8011259ae407a6c7
ShahafBenYakir <sbenyakir@paloaltonetworks.com>
2022-02-08 19:31:01 +0100

    Another failing UT for a bug in FE CM

--- Packs/FireEyeCM/Integrations/FireEyeCM/FireEyeCM_test.py
+++ Packs/FireEyeCM/Integrations/FireEyeCM/FireEyeCM_test.py
@@ -350,10 +350,12 @@ def test_fetch_incidents_last_alert_ids(mocker):
     mocker.patch.object(FireEyeClient, '_get_token', return_value='token')
     client = Client(base_url="https://fireeye.cm.com/", username='user', password='pass', verify=False, proxy=False)
     mocker.patch.object(FireEyeClient, 'get_alerts_request', return_value=util_load_json('test_data/alerts.json'))
+    last_run_time = '2021-05-18T12:02:54+00:00'
+    next_run_time = (dateparser.parse(last_run_time[:-6]) + timedelta(hours=48)).isoformat()
     last_run = {
-        'time': "2021-05-18 12:02:54 +0000",
-        'last_alert_ids': '["35267", "35268", "35269", "35272", "35273", "35274",'
-                          ' "35275", "35276", "35277", "35278", "35279"]'
+        'time': last_run_time,
+        'last_alert_ids': '["35267", "35268", "35269", "35272", "35273", "35274", "35275", "35276", "35277", "35278", '
+                          '"35279"]'
     }
     next_run, incidents = fetch_incidents(client=client,
                                           last_run=last_run,
@@ -363,4 +365,4 @@ def test_fetch_incidents_last_alert_ids(mocker):
 
     assert len(incidents) == 0
     # trim miliseconds to avoid glitches such as 2021-05-19T10:21:52.121+00:00 != 2021-05-19T10:21:52.123+00:00
-    assert next_run.get('time')[:-9] == last_run.get('time')[:-9]
+    assert next_run.get('time')[:-9] == next_run_time
