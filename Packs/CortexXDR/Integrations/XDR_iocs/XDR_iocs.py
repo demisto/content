05b6b90101372dca0b806dd8dedf93b631f146fa
esharf <esahrf@paloaltonetworks.com>
2022-02-09 16:25:51 +0200

    fixed

--- Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs.py
+++ Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs.py
@@ -122,38 +122,23 @@ def prepare_disable_iocs(iocs: str) -> Tuple[str, List]:
 
 
 def create_file_iocs_to_keep(file_path, batch_size: int = 200):
-    with open(file_path, 'a') as _file:
-        total_size: int = get_iocs_size()
-        for i in range(0, ceil(total_size / batch_size)):
-            iocs: List = get_iocs(page=i, size=batch_size)
-            for ios in map(lambda x: x.get('value', ''), iocs):
-                _file.write(ios + '\n')
+    with open(file_path, 'w') as _file:
+        for ios in map(lambda x: x.get('value', ''), get_iocs_generator(size=batch_size)):
+            _file.write(ios + '\n')
 
 
 def create_file_sync(file_path, batch_size: int = 200):
-    with open(file_path, 'a') as _file:
-        total_size: int = get_iocs_size()
-        for i in range(0, ceil(total_size / batch_size)):
-            iocs: List = get_iocs(page=i, size=batch_size)
-            for ioc in map(lambda x: demisto_ioc_to_xdr(x), iocs):
-                if ioc:
-                    _file.write(json.dumps(ioc) + '\n')
+    with open(file_path, 'w') as _file:
+        iocs: List = get_iocs_generator(size=batch_size)
+        for ioc in map(lambda x: demisto_ioc_to_xdr(x), iocs):
+            if ioc:
+                _file.write(json.dumps(ioc) + '\n')
 
 
-def get_iocs_size(query=None) -> int:
-    search_indicators = IndicatorsSearcher()
+def get_iocs_generator(size=200, query=None) -> List:
     query = query if query else Client.query
     query = f'expirationStatus:active AND ({query})'
-    return search_indicators.search_indicators_by_version(query=query, size=1)\
-        .get('total', 0)
-
-
-def get_iocs(page=0, size=200, query=None) -> List:
-    search_indicators = IndicatorsSearcher(page=page)
-    query = query if query else Client.query
-    query = f'expirationStatus:active AND ({query})'
-    return search_indicators.search_indicators_by_version(query=query, size=size)\
-        .get('iocs', [])
+    return IndicatorsSearcher(size=size, query=query)
 
 
 def demisto_expiration_to_xdr(expiration) -> int:
@@ -271,10 +256,7 @@ def get_last_iocs(batch_size=200) -> List:
     current_run: str = datetime.utcnow().strftime(DEMISTO_TIME_FORMAT)
     last_run: Dict = get_integration_context()
     query = create_last_iocs_query(from_date=last_run['time'], to_date=current_run)
-    total_size = get_iocs_size(query)
-    iocs: List = []
-    for i in range(0, ceil(total_size / batch_size)):
-        iocs.extend(get_iocs(query=query, page=i, size=batch_size))
+    iocs: List = list(get_iocs_generator(query=query, size=batch_size))
     last_run['time'] = current_run
     set_integration_context(last_run)
     return iocs
