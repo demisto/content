2ee7398724cc06c5b69aca550b914420f082a6ac
Dan <dansterenson@gmail.com>
2022-02-09 13:22:16 +0200

    test flag

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -10,6 +10,7 @@ CONF_PATH="./Tests/conf.json"
 echo "export GOOGLE_APPLICATION_CREDENTIALS=$GCS_ARTIFACTS_KEY" >> $BASH_ENV
 source $BASH_ENV
 
+echo "USE RETRIES MECHANISM FLAG DOWN"
 demisto-sdk test-content -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n $IS_NIGHTLY -t "$SLACK_TOKEN" -a "$CIRCLECI_TOKEN" -b "$CI_BUILD_ID" -g "$CI_COMMIT_BRANCH" -m "$MEM_CHECK" --is-ami $IS_AMI_RUN -d "$1" -u false
 
 RETVAL=$?
