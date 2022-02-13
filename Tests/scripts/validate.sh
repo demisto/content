b39ab0dd5bf05ab0a6d49f7f0e4bf52fc42b21af
yucohen <yucohen@paloaltonetworks.com>
2022-02-13 09:34:29 +0200

    added flag to validate

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -4,11 +4,11 @@ set -e
 echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_README_VALIDATION"
 
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
-    demisto-sdk validate -a --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json"
+    demisto-sdk validate -a --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json" --failed-validations-file
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
     demisto-sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/id_set.json"
 elif [[ $CI_COMMIT_BRANCH = demisto/python ]] || [[ $CI_COMMIT_BRANCH = demisto/python3 ]]; then
     demisto-sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json" --no-conf-json
 else
-    demisto-sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json"
+    demisto-sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json" --failed-validations-file
 fi
