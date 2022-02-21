962a0ee84a1b5bfefc66b38228094f37fcb71bf3
Adam Burt <aburt@paloaltonetworks.com>
2022-02-21 09:52:31 +0000

    Code changes

--- Packs/CommonScripts/Scripts/KeyValueToJson/KeyValueToJson.py
+++ Packs/CommonScripts/Scripts/KeyValueToJson/KeyValueToJson.py
@@ -2,6 +2,9 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 def key_to_json(keys, values):
     return_data = dict()
+    if len(values) != len(keys):
+        return_error("The length of the value and key are not the same")
+
     try:
         for index in range(0, len(values)):
             return_data[keys[index]] = values[index]
@@ -14,9 +17,6 @@ def main():
     args = demisto.args()
     values = argToList(args.get('value'))
     keys = argToList(args.get('key'))
-
-    if len(values) != len(keys):
-        return_error("The length of the value and key are not the same")
     return_results(key_to_json(keys, values))
 
 
