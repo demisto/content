58182a30e7130c2a2f7ed6d3fc04a28f3c28ddc1
Adam Burt <aburt@paloaltonetworks.com>
2022-02-21 09:45:32 +0000

    Moved translation to new function

--- Packs/CommonScripts/Scripts/KeyValueToJson/KeyValueToJson.py
+++ Packs/CommonScripts/Scripts/KeyValueToJson/KeyValueToJson.py
@@ -1,46 +1,23 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+def key_to_json(keys, values):
+    return_data = dict()
+    try:
+        for index in range(0, len(values)):
+            return_data[keys[index]] = values[index]
+    except Exception as err:
+        return_error(f"There was an error - {err}")
+    return return_data
+
+
 def main():
     args = demisto.args()
     values = argToList(args.get('value'))
     keys = argToList(args.get('key'))
 
-    if not isinstance(values, list):
-        try:
-            values = json.loads(values)
-        except TypeError:
-            pass
-        if not isinstance(values, list):
-            try:
-                values = values.split(",")
-            except AttributeError:
-                pass
-
-    if not isinstance(values, list):
-        return_error("Canoot convert the input values into a list")
-
-    if not isinstance(keys, list):
-        try:
-            keys = json.loads(keys)
-        except TypeError:
-            pass
-        if not isinstance(keys, list):
-            try:
-                keys = keys.split(",")
-            except AttributeError:
-                pass
-
     if len(values) != len(keys):
         return_error("The length of the value and key are not the same")
-
-    try:
-        return_data = dict()
-        for index in range(0, len(values)):
-            return_data[keys[index]] = values[index]
-    except Exception as err:
-        return_error(f"There was an error - {err}")
-
-    return_results(return_data)
+    return_results(key_to_json(keys, values))
 
 
 if __name__ in ['__main__', '__builtin__', 'builtins']:
