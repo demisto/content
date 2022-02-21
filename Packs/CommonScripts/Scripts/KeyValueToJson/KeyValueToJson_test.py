0a5ddfc3f4ffe585a107f9dc4c2e72b6a7f84e60
Adam Burt <aburt@paloaltonetworks.com>
2022-02-21 09:59:34 +0000

    Added unit tests

---
+++ Packs/CommonScripts/Scripts/KeyValueToJson/KeyValueToJson_test.py
@@ -0,0 +1,19 @@
+from KeyValueToJson import key_to_json
+
+import pytest
+
+
+@pytest.mark.parametrize('values,keys,expected', [
+    ([1, 2, 3, 4], ["A", "B", "C", "D"], {"A": 1, "B": 2, "C": 3, "D": 4})
+])
+def test_key_to_json(values, keys, expected):
+    """
+    Given:
+        Case 1: Values = [1,2,3,4] and keys = ["A", "B", "C", "D"] will return
+        formatted key:value pairs as {"A": 1, "B": 2, "C": 3, "D": 4}
+    When:
+        Running key_to_json
+    Then:
+        Case 1: Ensure {"A": 1, "B": 2, "C": 3, "D": 4} is returned
+    """
+    assert key_to_json(keys, values) == expected
