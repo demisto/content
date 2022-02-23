58aed43d3903417deaf8f77ee8d7f09b85152cc4
Shubham Kaushik <its.shubham.kaushik@gmail.com>
2022-02-23 20:44:24 +0530

    updated tests

--- Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
+++ Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
@@ -93,6 +93,7 @@ EXPECTED_OUTPUT1 = {
                                    {'Name': 'Qandahar'},
                                    {'Name': 'Herat'},
                                    {'Name': 'Mazar-e-Sharif'}],
+                        'Headers': ['Name'],
                         'Query': 'select Name from city',
                         'InstanceName': 'sql_dialect_database'}}
 }
@@ -150,7 +151,8 @@ EXPECTED_OUTPUT2 = \
         'Password_reuse_time': 'None',
         'Password_require_current': 'None',
         'User_attributes': 'None',
-    }], 'Query': 'select * from mysql.user',
+    }], 'Headers': ['Host', 'User', 'Select_priv', 'Insert_priv', 'Update_priv', 'Delete_priv', 'Create_priv', 'Drop_priv', 'Reload_priv', 'Shutdown_priv', 'Process_priv', 'File_priv', 'Grant_priv', 'References_priv', 'Index_priv', 'Alter_priv', 'Show_db_priv', 'Super_priv', 'Create_tmp_table_priv', 'Lock_tables_priv', 'Execute_priv', 'Repl_slave_priv', 'Repl_client_priv', 'Create_view_priv', 'Show_view_priv', 'Create_routine_priv', 'Alter_routine_priv', 'Create_user_priv', 'Event_priv', 'Trigger_priv', 'Create_tablespace_priv', 'ssl_type', 'ssl_cipher', 'x509_issuer', 'x509_subject', 'max_questions', 'max_updates', 'max_connections', 'max_user_connections', 'plugin', 'authentication_string', 'password_expired', 'password_last_changed', 'password_lifetime', 'account_locked', 'Create_role_priv', 'Drop_role_priv', 'Password_reuse_history', 'Password_reuse_time', 'Password_require_current', 'User_attributes'],
+        'Query': 'select * from mysql.user',
         'InstanceName': 'sql_dialect_database'}}}
 
 HEADER1 = ['Name']
@@ -170,6 +172,7 @@ EMPTY_OUTPUT = {
         'GenericSQL':
             {
                 'Result': [],
+                'Headers': [],
                 'Query': 'select Name from city where 1=2',
                 'InstanceName': 'sql_dialect_database'
             }
