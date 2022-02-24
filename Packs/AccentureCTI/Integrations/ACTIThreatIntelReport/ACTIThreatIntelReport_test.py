d01d932f6ce32559aa96e3904b70d4e2b5f7f55f
Ankit Mordhwaj <ankit.mordhwaj@accenture.com>
2022-02-24 09:42:16 +0000

    unit tests created

--- Packs/AccentureCTI/Integrations/ACTIThreatIntelReport/ACTIThreatIntelReport_test.py
+++ Packs/AccentureCTI/Integrations/ACTIThreatIntelReport/ACTIThreatIntelReport_test.py
@@ -12,6 +12,12 @@ you are implementing with your integration
 
 import json
 import io
+from ACTIThreatIntelReport import Client, _calculate_dbot_score, getThreatReport_command
+from test_data.response_constants import *
+import requests_mock
+from CommonServerPython import DBotScoreReliability
+
+API_URL = "https://test.com"
 
 
 def util_load_json(path):
@@ -19,24 +25,60 @@ def util_load_json(path):
         return json.loads(f.read())
 
 
-# TODO: REMOVE the following dummy unit test function
-def test_baseintegration_dummy():
-    """Tests helloworld-say-hello command function.
 
-    Checks the output of the command function with the expected output.
-
-    No mock is needed here because the say_hello_command does not call
-    any external API.
+def test_calculate_dbot_score():
     """
-    from BaseIntegration import Client, baseintegration_dummy_command
+    Given:
+        - number represents severity
 
-    client = Client(base_url='some_mock_url', verify=False)
-    args = {
-        'dummy': 'this is a dummy response'
-    }
-    response = baseintegration_dummy_command(client, args)
+    When:
+        - api call with indicator returns response that includes them severity score
 
-    mock_response = util_load_json('test_data/baseintegration-dummy.json')
+    Then:
+        - returns dbotscore according to internal conversion
 
-    assert response.outputs == mock_response
-# TODO: ADD HERE unit tests for every command
+    """
+    assert _calculate_dbot_score(0) == 0
+    assert _calculate_dbot_score(1) == 1
+    assert _calculate_dbot_score(2) == 1
+    assert _calculate_dbot_score(3) == 2
+    assert _calculate_dbot_score(4) == 2
+    assert _calculate_dbot_score(5) == 3
+    assert _calculate_dbot_score(6) == 3
+    assert _calculate_dbot_score(7) == 3
+
+
+
+def test_getThreatReport_command():
+    """
+    Given:
+        - an CVE
+
+    When:
+        - running Vulnerability command and validate whether the CVE is malicious
+
+    Then:
+        - return command results containing Vulnerability, dbotscore
+
+    """
+    url = 'https://test.com/rest/vulnerability/v0?key.values=CVE-2022-23021'
+    status_code = 200
+    json_res = JSON_IA1
+
+    expected_output = {}
+
+    uuid_to_check = {'uuid': ''}
+
+    with requests_mock.Mocker() as m:
+        m.get(url, status_code=status_code, json=json_res)
+        client = Client(API_URL, 'api_token', True, False, '/rest/document')
+        results = getThreatReport_command(client, uuid_to_check, DBotScoreReliability.B)
+        output = results[0].to_context().get('EntryContext', {})
+
+        assert True
+        assert True
+
+
+
+def test_getThreatReport_not_found():
+    pass
