2d09bb132a951a81fad4ae21efd247ca2eb415b4
philippe.laporte <philippe.laporte@upstart.com>
2022-02-25 14:22:45 -0800

    Add test_add_responders_default to test run_response_play command, add doc for run_response_play

--- Packs/PagerDuty/Integrations/PagerDuty/PageDuty_test.py
+++ Packs/PagerDuty/Integrations/PagerDuty/PageDuty_test.py
@@ -184,3 +184,136 @@ def test_add_responders(requests_mock, mocker):
     assert demisto.args().get('message') == res.outputs[0].get('Message')
     assert demisto.params().get('DefaultRequestor') == res.outputs[1].get('RequesterID')
     assert demisto.args().get('user_requests') == expected_users_requested
+
+def test_add_responders_default(requests_mock, mocker):
+    """
+    Test sending request to a responder without specifying responders
+    """
+    mocker.patch.object(
+        demisto,
+        'params',
+        return_value={
+            'APIKey': 'API_KEY',
+            'ServiceKey': 'SERVICE_KEY',
+            'FetchInterval': 'FETCH_INTERVAL',
+            'DefaultRequestor': 'P09TT3C'
+        }
+    )
+    mocker.patch.object(
+        demisto,
+        'args',
+        return_value={
+            "incident_id": "PXP12GZ",
+            "message": "Please help with issue - join bridge at +1(234)-567-8910",
+         }
+    )
+    requests_mock.post(
+        'https://api.pagerduty.com/incidents/PXP12GZ/responder_requests',
+        json={
+            "responder_request": {
+                "incident": {
+                    "id": "PXP12GZ",
+                    "type": "incident_reference",
+                    "summary": "[#99999] Test Incident for Demisto Integration - No Action",
+                    "self": "https://api.pagerduty.com/incidents/PXP12GZ",
+                    "html_url": "https://upstart.pagerduty.com/incidents/PXP12GZ"
+                },
+                "requester": {
+                    "id": "P09TT3C",
+                    "type": "user_reference",
+                    "summary": "John Doe",
+                    "self": "https://api.pagerduty.com/users/P09TT3C",
+                    "html_url": "https://mycompany.pagerduty.com/users/P09TT3C"
+                },
+                "requested_at": "2022-02-24T18:08:50-08:00",
+                "message": "Please help with issue - join bridge at +1(234)-567-8910",
+                "responder_request_targets": [
+                    {
+                        "responder_request_target": {
+                            "type": "user",
+                            "id": "P09TT3C",
+                            "summary": "",
+                            "incidents_responders": [
+                                {
+                                    "state": "pending",
+                                    "user": {
+                                        "id": "P09TT3C",
+                                        "type": "user_reference",
+                                        "summary": "John Doe",
+                                        "self": "https://api.pagerduty.com/users/P09TT3C",
+                                        "html_url": "https://mycompany.pagerduty.com/users/P09TT3C",
+                                        "avatar_url": "https://secure.gravatar.com/avatar/\
+                                        1c747247b75acc1f724e2784c838b3f8.png?d=mm&r=PG",
+                                        "job_title": ""
+                                    },
+                                    "incident": {
+                                        "id": "PXP12GZ",
+                                        "type": "incident_reference",
+                                        "summary": "[#30990] Test Incident for Demisto Integration - No Action",
+                                        "self": "https://api.pagerduty.com/incidents/PXP12GZ",
+                                        "html_url": "https://upstart.pagerduty.com/incidents/PXP12GZ"
+                                    },
+                                    "updated_at": "2022-02-24T16:58:27-08:00",
+                                    "message": "Please help with issue - join bridge at +1(234)-567-8910",
+                                    "requester": {
+                                        "id": "P09TT3C",
+                                        "type": "user_reference",
+                                        "summary": "John Doe",
+                                        "self": "https://api.pagerduty.com/users/P09TT3C",
+                                        "html_url": "https://mycompany.pagerduty.com/users/P09TT3C",
+                                        "avatar_url": "https://secure.gravatar.com/avatar/\
+                                        1c747247b75acc1f724e2784c838b3f8.png?d=mm&r=PG",
+                                        "job_title": ""
+                                    },
+                                    "requested_at": "2022-02-25T00:58:27Z"
+                                }
+                            ]
+                        }
+                    },
+                ]
+            }
+        }
+    )
+
+    from PagerDuty import add_responders_to_incident
+    res = add_responders_to_incident(**demisto.args())
+    expected_users_requested = ','.join([x.get("ID") for x in res.outputs])
+    assert demisto.args().get('incident_id') == res.outputs[0].get('IncidentID')
+    assert demisto.args().get('message') == res.outputs[0].get('Message')
+    assert demisto.params().get('DefaultRequestor') == res.outputs[0].get('RequesterID')
+    assert demisto.params().get('DefaultRequestor') == expected_users_requested
+
+def test_play_response_play(requests_mock, mocker):
+    """
+    Test sending request to a responder without specifying responders
+    """
+    mocker.patch.object(
+        demisto,
+        'params',
+        return_value={
+            'APIKey': 'API_KEY',
+            'ServiceKey': 'SERVICE_KEY',
+            'FetchInterval': 'FETCH_INTERVAL',
+            'DefaultRequestor': 'P09TT3C'
+        }
+    )
+    mocker.patch.object(
+        demisto,
+        'args',
+        return_value={
+            "incident_id": "PXP12GZ",
+            "from_email": "john.doe@example.com",
+            "response_play_uuid": "response_play_id",
+        }
+    )
+    requests_mock.post(
+        'https://api.pagerduty.com/response_plays/response_play_id/run',
+        json={
+          "status": "ok"
+        }
+    )
+
+    from PagerDuty import run_response_play
+    res = run_response_play(**demisto.args())
+
+    assert res.raw_response == {"status": "ok"}
