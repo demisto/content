db97f7439e838e986b1722562667176031ebbcd3
dschwartz <81086590+dorschw@users.noreply.github.com>
2022-02-28 10:37:15 +0200

    22/22 UT

--- Packs/CrowdStrikeFalconX/Integrations/CrowdStrikeFalconX/CrowdStrikeFalconX_test.py
+++ Packs/CrowdStrikeFalconX/Integrations/CrowdStrikeFalconX/CrowdStrikeFalconX_test.py
@@ -221,8 +221,8 @@ def ddiff(dict1: dict, dict2: dict):  # todo remove
 
     for k in set(dict1.keys()).intersection(dict2.keys()):
         if dict1[k] != dict2[k]:
-            print(f'{k} in dict1:', dict1[k])
-            print(f'{k} in dict2:', dict2[k])
+            print(f'{k} in dict1\n', dict1[k])
+            print(f'{k} in dict2\n', dict2[k])
     for k in set(dict1.keys()).difference(dict2.keys()):
         print(f'key {k} missing from dict2')
     for k in set(dict2.keys()).difference(dict1.keys()):
