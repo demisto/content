4978dc78683cf4dda820d112da7c760b79e417e0
meichlerpanw <meichler@paloaltonetworks.com>
2022-03-01 12:10:56 +0200

    docs

--- Packs/EWSMailSender/Integrations/EWSMailSender/EWSMailSender_test.py
+++ Packs/EWSMailSender/Integrations/EWSMailSender/EWSMailSender_test.py
@@ -1,5 +1,6 @@
 import logging
 import EWSMailSender
+from exchangelib.errors import UnauthorizedError
 
 
 def test_prepare():
@@ -11,3 +12,12 @@ def test_start_logging():
     EWSMailSender.start_logging()
     logging.getLogger().debug("test this")
     assert "test this" in EWSMailSender.log_stream.getvalue()
+
+
+def test_get_account(mocker):
+    email_account = 'test@test.com'
+    mocker.patch.object(EWSMailSender.get_account(email_account), return_value=UnauthorizedError)
+
+    EWSMailSender.get_account(email_account)
+
+    assert True
