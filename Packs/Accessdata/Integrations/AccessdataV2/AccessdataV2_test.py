0e28790c9e5b03f034435f95e13cb81bff9f545a
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2022-03-02 08:40:57 +0000

    Added extra tests for validating case management and context checks.

--- Packs/Accessdata/Integrations/AccessdataV2/AccessdataV2_test.py
+++ Packs/Accessdata/Integrations/AccessdataV2/AccessdataV2_test.py
@@ -1,12 +1,17 @@
 from requests_mock import Mocker
 
 from accessdata.client import Client
-from accessdata.api.extensions import *
+from accessdata.api.extensions import (
+	attribute_list_ext,
+	case_list_ext,
+	server_setting_ext,
+	status_check_ext
+)
 
-from CommonServerPython import *
+from AccessdataV2 import _get_case_by_name
 
-API_URL = "http://randomurl.com"
-API_KEY = None
+API_URL = "http://randomurl.com/"
+API_KEY = "API-TEST-KEY"
 
 def generate_mock_client():
 	"""Creates a mock client using falsified
@@ -16,8 +21,11 @@ def generate_mock_client():
 	"""
 
 	with Mocker() as mocker:
-		mocker.get(API_URL + status_check_ext,
-			status_code=200, data='Ok')
+		mocker.get(
+			API_URL + status_check_ext[1],
+			status_code=200,
+			json="Ok"
+		)
 		client = Client(API_URL, API_KEY)
 
 	return client
@@ -34,3 +42,41 @@ def test_mock_client():
 	}
 
 
+def test_mock_case_list():
+	"""Tests the case list getter."""
+	client = generate_mock_client()
+
+	with Mocker() as mocker:
+		mocker.get(
+			API_URL + case_list_ext[1],
+			status_code=200,
+			json=[
+				{
+					"id": 1,
+					"name": "Test Case",
+					"casepath": "\\\\FTKC\\Cases\\Test Case"
+				}
+			]
+		)
+		mocker.get(
+			API_URL + attribute_list_ext[1],
+			status_code=200,
+			json=[]
+		)
+		mocker.get(
+			API_URL + server_setting_ext[1].format(setting="FTKDefaultPath"),
+			status_code=200,
+			json="\\\\FTKC\\Cases"
+		)
+
+		cases = client.cases
+
+		assert len(cases) == 1
+
+		results = get_case_by_name(client, "Test Case")
+		outputs = results.outputs
+
+		assert outputs.get("ID") == 1
+		assert outputs.get("Name") == "Test Case"
+
+test_mock_case_list()
