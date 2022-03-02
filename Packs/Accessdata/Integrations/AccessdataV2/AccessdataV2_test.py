0560eaba306a99b3db3a33982ca8b4ef04f849d8
Thomas Vieth <thomas_vieth@hotmail.co.uk>
2022-03-02 16:28:15 +0000

    Updated linting standards on tests.

--- Packs/Accessdata/Integrations/AccessdataV2/AccessdataV2_test.py
+++ Packs/Accessdata/Integrations/AccessdataV2/AccessdataV2_test.py
@@ -2,10 +2,10 @@ from requests_mock import Mocker
 
 from accessdata.client import Client
 from accessdata.api.extensions import (
-	attribute_list_ext,
-	case_list_ext,
-	server_setting_ext,
-	status_check_ext
+    attribute_list_ext,
+    case_list_ext,
+    server_setting_ext,
+    status_check_ext
 )
 
 from AccessdataV2 import _get_case_by_name
@@ -13,70 +13,72 @@ from AccessdataV2 import _get_case_by_name
 API_URL = "http://randomurl.com/"
 API_KEY = "API-TEST-KEY"
 
+
 def generate_mock_client():
-	"""Creates a mock client using falsified
-	information.
+    """Creates a mock client using falsified
+    information.
 
-	:return: Client
-	"""
+    :return: Client
+    """
 
-	with Mocker() as mocker:
-		mocker.get(
-			API_URL + status_check_ext[1],
-			status_code=200,
-			json="Ok"
-		)
-		client = Client(API_URL, API_KEY)
+    with Mocker() as mocker:
+        mocker.get(
+            API_URL + status_check_ext[1],
+            status_code=200,
+            json="Ok"
+        )
+        client = Client(API_URL, API_KEY)
 
-	return client
+    return client
 
 
 def test_mock_client():
-	"""Tests the client generator."""
-	client = generate_mock_client()
+    """Tests the client generator."""
+    client = generate_mock_client()
 
-	assert client.session.status_code == 200
+    assert client.session.status_code == 200
 
-	assert client.session.headers == {
-		"EnterpriseApiKey": API_KEY
-	}
+    assert client.session.headers == {
+        "EnterpriseApiKey": API_KEY
+    }
 
 
 def test_mock_case_list():
-	"""Tests the case list getter."""
-	client = generate_mock_client()
+    """Tests the case list getter."""
+    client = generate_mock_client()
 
-	with Mocker() as mocker:
-		mocker.get(
-			API_URL + case_list_ext[1],
-			status_code=200,
-			json=[
-				{
-					"id": 1,
-					"name": "Test Case",
-					"casepath": "\\\\FTKC\\Cases\\Test Case"
-				}
-			]
-		)
-		mocker.get(
-			API_URL + attribute_list_ext[1],
-			status_code=200,
-			json=[]
-		)
-		mocker.get(
-			API_URL + server_setting_ext[1].format(setting="FTKDefaultPath"),
-			status_code=200,
-			json="\\\\FTKC\\Cases"
-		)
+    with Mocker() as mocker:
+        mocker.get(
+            API_URL + case_list_ext[1],
+            status_code=200,
+            json=[
+                {
+                    "id": 1,
+                    "name": "Test Case",
+                    "casepath": "\\\\FTKC\\Cases\\Test Case"
+                }
+            ]
+        )
+        mocker.get(
+            API_URL + attribute_list_ext[1],
+            status_code=200,
+            json=[]
+        )
+        mocker.get(
+            API_URL + server_setting_ext[1].format(setting="FTKDefaultPath"),
+            status_code=200,
+            json="\\\\FTKC\\Cases"
+        )
 
-		cases = client.cases
+        cases = client.cases
 
-		assert len(cases) == 1
+        assert len(cases) == 1
 
-		results = get_case_by_name(client, "Test Case")
-		outputs = results.outputs
+        results = _get_case_by_name(client, "Test Case")
+        outputs = results.outputs
 
-		assert outputs.get("ID") == 1
-		assert outputs.get("Name") == "Test Case"
+        assert outputs.get("ID") == 1
+        assert outputs.get("Name") == "Test Case"
+
 
 test_mock_case_list()
