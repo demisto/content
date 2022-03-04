8b7fa5cd68b607467eb974dce0c151b2e05da17f
darkushin <darkushin@paloaltonetworks.com>
2022-03-04 10:39:18 +0100

    Fixed build

--- Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/CrowdStrikeFalcon.py
+++ Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/CrowdStrikeFalcon.py
@@ -1329,8 +1329,10 @@ def search_device(filter_operator='AND'):
         'local_ip': str(args.get('ip', '')).split(',')
     }
     url_filter = '{}'.format(str(args.get('filter', '')))
-    op = ',' if filter_operator == 'OR' else '+'  # In Falcon Query Language, '+' stands for AND and ',' for OR
-                                                  # (https://falcon.crowdstrike.com/documentation/45/falcon-query-language-fql)
+    op = ',' if filter_operator == 'OR' else '+'
+    # In Falcon Query Language, '+' stands for AND and ',' for OR
+    # (https://falcon.crowdstrike.com/documentation/45/falcon-query-language-fql)
+
     for k, arg in input_arg_dict.items():
         if arg:
             if type(arg) is list:
@@ -1344,7 +1346,8 @@ def search_device(filter_operator='AND'):
                                                                    arg_filter=arg_filter)
             else:
                 # All args should be a list. this is a fallback
-                url_filter = "{url_filter}{operator}{inp_arg}:'{arg_val}'".format(url_filter=url_filter, operator=op, inp_arg=k, arg_val=arg)
+                url_filter = "{url_filter}{operator}{inp_arg}:'{arg_val}'".format(url_filter=url_filter, operator=op,
+                                                                                  inp_arg=k, arg_val=arg)
     raw_res = http_request('GET', '/devices/queries/devices/v1', params={'filter': url_filter})
     device_ids = raw_res.get('resources')
     if not device_ids:
