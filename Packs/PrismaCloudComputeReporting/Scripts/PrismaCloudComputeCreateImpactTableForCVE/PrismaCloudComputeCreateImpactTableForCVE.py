b86a45c7338366a63ff5ea05050931b6aaf6dd78
nericksen <nericksen@paloaltonetworks.com>
2022-03-09 00:40:32 -0500

    Update based on feedback

--- Packs/PrismaCloudComputeReporting/Scripts/PrismaCloudComputeCreateImpactTableForCVE/PrismaCloudComputeCreateImpactTableForCVE.py
+++ Packs/PrismaCloudComputeReporting/Scripts/PrismaCloudComputeCreateImpactTableForCVE/PrismaCloudComputeCreateImpactTableForCVE.py
@@ -2,24 +2,30 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 # Create CVE Host table from running: !prisma-cloud-compute-vulnerabilities-impacted-resources-list and setting to indicator (custom script)
 
-if "indicator" in demisto.args():
-    cve = demisto.args()["indicator"]["value"]
-else:
-    cve = demisto.args().get("cve")
+def main():
+    dArgs = demisto.args()
 
-markdownField = demisto.args().get("markdownField", "prismacloudcomputeimpacttable")
-
-try:
-    results = demisto.executeCommand("prisma-cloud-compute-vulnerabilities-impacted-resources-list",
-                                     {"cve": cve})[0]["HumanReadable"]
-except:
-    return_results("Nothing found in Prisma Cloud Compute!")
-
-demisto.executeCommand("setIndicator", {"value": cve, markdownField: results})
-
-# TODO pretty format of md table in warroom results
-result = CommandResults(
-    readable_output=results
-)
-
-return_results(result)
+    if "indicator" in dArgs:
+        cve = dArgs.get("indicator").get("value")
+    else:
+        cve = dArgs.get("cve")
+    
+    markdownField = dArgs.get("markdownField", "prismacloudcomputeimpacttable")
+    
+    try:
+        results = demisto.executeCommand("prisma-cloud-compute-vulnerabilities-impacted-resources-list",
+                                         {"cve": cve})[0]["HumanReadable"]
+    except:
+        return_results("Nothing found in Prisma Cloud Compute!")
+    
+    demisto.executeCommand("setIndicator", {"value": cve, markdownField: results})
+    
+    # TODO pretty format of md table in warroom results
+    result = CommandResults(
+        readable_output=results
+    )
+    
+    return_results(result)
+    
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
