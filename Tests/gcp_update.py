48f70a26dac6565d590fe8c119b2dd98f7b8b0e8
Darya Koval <dkoval@paloaltonetworks.com>
2022-03-10 17:02:24 +0200

    trying to add machines names

--- Tests/gcp_update.py
+++ Tests/gcp_update.py
@@ -7,25 +7,19 @@ def main():
     logging.info('Starting create bucket folder')
     from google.cloud import storage
     storage_client = storage.Client()
-
-    all_blobs = list(storage_client.list_blobs('xsoar-ci-artifacts'))
-    logging.info(f'All blobs: {all_blobs}')
-
     bucket = storage_client.bucket('xsoar-ci-artifacts')
+
+    # all_blobs = list(storage_client.list_blobs('xsoar-ci-artifacts'))
+    # logging.info(f'All blobs: {all_blobs}')
+
+    blob = bucket.blob('xsiam-ci-locks/TestMachines')
+    s = """qa2-test-999733383500\nqa2-test-9994443226862\nqa2-test-9997461765391"""
+    blob.upload_from_string(s)
+    logging.info('Created bucket folder successfully.')
+
     blob = bucket.blob('xsiam-ci-locks')
     blob.delete()
     logging.info('File deleted successfully.')
-
-    blob = bucket.blob('xsiam-ci-locks/')
-    # blob.upload_from_string('queue')
-    # logging.info('Created bucket folder successfully.')
-
-    # logging.info('Creating new file"')
-    s = """qa2-test-999733383500\nqa2-test-9994443226862\nqa2-test-9997461765391"""
-    with open('TestMachines', 'w') as f:
-        f.write(s)
-
-    # blob.upload_from_filename('TestMachines')
 
 
 if __name__ == '__main__':
