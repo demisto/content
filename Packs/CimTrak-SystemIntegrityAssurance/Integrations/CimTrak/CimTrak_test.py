184e02aa13f3ea50e8e18ad89db5215bf5c5add4
Joel Kiger <kiger.joel@cimcor.com>
2022-03-14 12:50:11 -0500

    Fixes for validation

--- Packs/CimTrak-SystemIntegrityAssurance/Integrations/CimTrak/CimTrak_test.py
+++ Packs/CimTrak-SystemIntegrityAssurance/Integrations/CimTrak/CimTrak_test.py
@@ -16,7 +16,6 @@ https://xsoar.pan.dev/docs/integrations/unit-testing
 import json
 import io
 import logging
-from typing import Any, Dict, List
 
 LOGGER = logging.getLogger(__name__)
 
@@ -28,16 +27,16 @@ def util_load_json(path):
 
 def test_cimtrak(requests_mock):
     import glob
-    from CimTrak import Client
-    import CimTrak
 
     for file in glob.glob("test_data/*.json"):
         # print("Testing " + file)
         test_json = util_load_json(file)
         for post in test_json["posts"]:
             requests_mock.post(post["url"], json=post["reply"])
+        global response
         pre_script = "from CimTrak import Client\n"
         pre_script += "import CimTrak\n"
+        pre_script += "from typing import List,Dict,Any\n"
         pre_script += "client = Client(\n"
         pre_script += "    base_url='https://test.com/',\n"
         pre_script += "    verify=False,\n"
