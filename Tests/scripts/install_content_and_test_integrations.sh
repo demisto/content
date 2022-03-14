77633f6557051f1343b58ad15a1d6ba5327a77fb
Darya Koval <dkoval@paloaltonetworks.com>
2022-03-14 16:18:12 +0200

    chosen machine

--- Tests/scripts/install_content_and_test_integrations.sh
+++ Tests/scripts/install_content_and_test_integrations.sh
@@ -15,8 +15,7 @@ fi
 
 ## todo: remove this line
 #export XSIAM_CHOSEN_MACHINE_ID=qa2-test-9997333835008
-# todo: add support for $XSIAM_CHOSEN_MACHINE_ID, write "$3" instead
-python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" --tests_to_run "$ARTIFACTS_FOLDER/filter_file.txt"  --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$CI_COMMIT_BRANCH" --build-number "$CI_PIPELINE_ID" -sa "$GCS_MARKET_KEY" --build_object_type "$2" --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --xsiam_servers_path $XSIAM_SERVERS_PATH
+python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" --tests_to_run "$ARTIFACTS_FOLDER/filter_file.txt"  --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$CI_COMMIT_BRANCH" --build-number "$CI_PIPELINE_ID" -sa "$GCS_MARKET_KEY" --build_object_type "$2" --xsiam_machine "$XSIAM_CHOSEN_MACHINE_ID" --xsiam_servers_path $XSIAM_SERVERS_PATH
 if [ -f ./Tests/test_pack.zip ]; then
   cp ./Tests/test_pack.zip "$ARTIFACTS_FOLDER"
 fi
