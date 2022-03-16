6858c7ad3e3382bf4bc5eead4ee3c27c4ba59d78
haldun meriç <49325040+haldunerbay@users.noreply.github.com>
2022-03-16 11:06:22 +0300

    Some PR fixes

--- Packs/Cryptosim/Integrations/Cryptosim/Cryptosim.py
+++ Packs/Cryptosim/Integrations/Cryptosim/Cryptosim.py
@@ -68,14 +68,11 @@ def correlation_alerts_command(client: Client):
 
 def test_module(client: Client) -> str:
     """Tests API connectivity and authentication'
-
     Returning 'ok' indicates that the integration works like it is supposed to.
     Connection to the service is successful.
     Raises exceptions if something goes wrong.
-
     :type client: ``Client``
     :param Client: client to use
-
     :return: 'ok' if test passed, anything else will fail the test.
     :rtype: ``str``
     """
@@ -90,7 +87,7 @@ def test_module(client: Client) -> str:
                             Error: {client.correlations().get('ErrorMessage')}
                             """)
     except DemistoException as e:
-        if '401' in str(e):  # TODO: make sure you capture authentication errors
+        if '401' in str(e):
             message = 'Authorization Error: make sure API User and Password is correctly set'
         else:
             raise e
@@ -107,10 +104,8 @@ def fetch_incidents(client: Client):
 
     first_fetch_time = arg_to_datetime(demisto.params().get('first_fetch')).isoformat()
 
-
     last_run = demisto.getLastRun()
     last_fetch = last_run.get('last_fetch', first_fetch_time)
-
 
     incidentsList=[]
     alert_response = client.correlation_alerts()
@@ -118,23 +113,20 @@ def fetch_incidents(client: Client):
 
     for inc in incident_data:
 
-
         if len(incidentsList) > max_results:
             break
 
         incident_name = inc['CorrelationAlert']['NAME']
         time_stamp = inc['CorrelationAlert']['CREATEDATE']+"Z"
 
-
-
-        severityLvl = int(inc['CorrelationAlert']['RISK'])
-        if severityLvl >=0 and severityLvl <= 5:
+        severity_level = int(inc['CorrelationAlert']['RISK'])
+        if severity_level >=0 and severity_level <= 5:
             severity = 1
-        elif severityLvl > 5 and severityLvl <= 7:
+        elif severity_level > 5 and severity_level <= 7:
             severity = 2
-        elif severityLvl > 7 and severityLvl <= 9:
+        elif severity_level > 7 and severity_level <= 9:
             severity = 3
-        elif severityLvl > 9 and severityLvl <= 10:
+        elif severity_level > 9 and severity_level <= 10:
             severity = 4
         else:
             severity = 0
@@ -142,35 +134,30 @@ def fetch_incidents(client: Client):
         # "log" column is stringfyed 'Log' data.
         inc['Log'].pop("log")
 
-        incidentObject = {**inc['Log'], **inc['CorrelationAlert']}
+        incident_object = {**inc['Log'], **inc['CorrelationAlert']}
 
         incident = {
             'name': incident_name,
             'occurred': time_stamp,
-            'rawJSON': json.dumps(incidentObject),
+            'rawJSON': json.dumps(incident_object),
             "severity": severity,
             'type': 'Crpyotsim CorrelationAlert'
         }
 
         incidentsList.append(incident)
-
+        
+        created_incident = datetime.strptime(time_stamp, DATE_FORMAT)
+        last_fetch = datetime.strptime(last_fetch,DATE_FORMAT)
+        if created_incident > last_fetch:
+            last_fetch = created_incident
 
 
     # Save the next_run as a dict with the last_fetch key to be stored
     next_run = {'last_fetch': last_fetch}
     return next_run, incidentsList
 
-
-''' MAIN FUNCTION '''
-
-
-def main() -> None:
-    """main function, parses params and runs command functions
-
-    :return:
-    :rtype:
-    """
-    params = demisto.params()
+''' HELPERS '''
+def get_client(params):
     authorization = params.get('credentials').get(
         'identifier') + ":" + params.get('credentials').get('password')
     auth_byte = authorization.encode('utf-8')
@@ -185,16 +172,27 @@ def main() -> None:
     # get the service API url
     base_url = urljoin(params.get('url'), '/api/service/')
     proxy = params.get('proxy', False)
+    
+    client = Client(
+        base_url=base_url,
+        verify=False,
+        headers=headers,
+        proxy=proxy)
+    return client
 
+''' MAIN FUNCTION '''
+
+
+def main() -> None:
+    """main function, parses params and runs command functions
+    :return:
+    :rtype:
+    """
+    params = demisto.params()
 
     demisto.debug(f'Command being called is {demisto.command()}')
     try:
-
-        client = Client(
-            base_url=base_url,
-            verify=False,
-            headers=headers,
-            proxy=proxy)
+        client = get_client(params)
 
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration Test button.
@@ -209,7 +207,7 @@ def main() -> None:
 
         elif demisto.command() == 'fetch-incidents':
 
-            next_run, incidents = fetch_incidents()
+            next_run, incidents = fetch_incidents(client)
 
             demisto.setLastRun(next_run)
             demisto.incidents(incidents)
