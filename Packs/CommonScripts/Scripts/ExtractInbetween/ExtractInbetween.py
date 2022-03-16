92b205a33abbb36cebc7c7c98f25796df768e459
Adam Burt <aburt@paloaltonetworks.com>
2022-03-16 11:16:45 +0000

    Added docstrings

--- Packs/CommonScripts/Scripts/ExtractInbetween/ExtractInbetween.py
+++ Packs/CommonScripts/Scripts/ExtractInbetween/ExtractInbetween.py
@@ -4,6 +4,18 @@ import re
 
 
 def iter_value(value: str, character: str, incr: bool) -> list:
+
+    """Iterate through characters in the 'value' string and match
+    against the 'chraracter'
+
+    Args:
+        value (str): The value to iterate through (each character)
+        character (str): The character to match against
+
+    Returns:
+        list: A list of all the indexes in the 'value' string, where the 'character' matches
+    """
+
     indexes = []
     for x in range(0, len(value)):
         if value[x] == character:
@@ -15,6 +27,22 @@ def iter_value(value: str, character: str, incr: bool) -> list:
 
 
 def extract_inbetween(value: str, start: str, end: str) -> str:
+
+    """Use regex to extract the value from 'value' using 'start' as
+    the starting point and 'end' as the ending point. The 'start' will 
+    be used as a regex match and will match the FIRST instance. The 'end' 
+    will be used as a regex macth and will match the LAST instance.
+
+    Args:
+        value (str): The string to search for regex matches in
+        start (str): The regex to use for the starting match
+        end (str): The regex to use for the ending match
+
+    Returns:
+        str: The extracted string from 'value'.
+
+    """
+
     if isinstance(value, str):
         if start in value and end in value:
             start_indicies = []
@@ -29,6 +57,8 @@ def extract_inbetween(value: str, start: str, end: str) -> str:
             start_index = start_indicies[0]
             end_index = end_indicies[-1]
             value = value[start_index:end_index]
+    else:
+        return_error("The input value must be a string")
     return value
 
 
