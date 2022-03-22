4925b383f0b8d55c3d5465a3a4b33c0ad99195b8
nmaimon <nmaimon@paloaltonetworks.com>
2022-03-22 13:45:58 +0200

    fix

--- Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance.py
+++ Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance.py
@@ -1,39 +1,43 @@
 import demistomock as demisto  # noqa: F401
 import geopy.distance
 from CommonServerPython import *  # noqa: F401
-
+import itertools
 requests.packages.urllib3.disable_warnings()
 
-try:
-    existing = demisto.get(demisto.context(), "ImpossibleTraveler.Events")
-    EventsDict = {}
-    for o in existing:
-        EventsDict[o["location"]] = o
-    DistanceList = []
-    # Extract each set of coordinates and create a list
-    src_coords_list = demisto.args()['src_coords']
-    dest_coords_list = demisto.args()['dest_coords']
-    for i in range(len(src_coords_list)):
-        for dest in dest_coords_list[i:]:
-            if src_coords_list[i] != dest:
 
-                geo_distance = round(geopy.distance.geodesic(src_coords_list[i], dest).miles, 2)
-                hr = 'Calculated Distance: {} miles.'.format(str(geo_distance))
-                context = {
-                    "distance": geo_distance,
-                    "src_coords": src_coords_list[i],
-                    "dest_coords": dest,
-                    "source_ip": EventsDict[src_coords_list[i]]["ip"],
-                    "source_country": EventsDict[src_coords_list[i]]["Country"],
-                    "dest_ip": EventsDict[dest]["ip"],
-                    "dest_country": EventsDict[dest]["Country"],
-                    "timestamp": EventsDict[src_coords_list[i]]["event_timestamp"],
-                    "identity": EventsDict[src_coords_list[i]]["identity_display_name"]
-                }
-                DistanceList.append(CommandResults(readable_output=hr, outputs=context,
-                                    outputs_prefix="GeoEvents", outputs_key_field=""))
+def main():
+    try:
+        existing = demisto.get(demisto.context(), "ImpossibleTraveler.Events")
+        events_dict = {}
+        for o in existing:
+            events_dict[o["location"]] = o
+        distance_list = []
+        # Extract each set of coordinates and create a list
+        args = demisto.args()
+        src_coords_list = args['src_coords']
+        dest_coords_list = args['dest_coords']
+        for unique_pair in itertools.combinations(src_coords_list, 2):
+            geo_distance = round(geopy.distance.geodesic(unique_pair[0], unique_pair[1]).miles, 2)
+            hr = 'Calculated Distance: {} miles.'.format(str(geo_distance))
+            context = {
+                "distance": geo_distance,
+                "src_coords": unique_pair[0],
+                "dest_coords": unique_pair[1],
+                "source_ip": events_dict[unique_pair[0]]["ip"],
+                "source_country": events_dict[unique_pair[0]]["Country"],
+                "dest_ip": events_dict[unique_pair[1]]["ip"],
+                "dest_country": events_dict[unique_pair[1]]["Country"],
+                "timestamp": events_dict[unique_pair[0]]["event_timestamp"],
+                "identity": events_dict[unique_pair[0]]["identity_display_name"]
+            }
+            distance_list.append(CommandResults(readable_output=hr, outputs=context,
+                                outputs_prefix="GeoEvents", outputs_key_field=""))
 
-    return_results(DistanceList)
+        return_results(distance_list)
 
-except Exception as ex:
-    return_error('Error occurred while parsing output from command. Exception info:\n' + str(ex))
+    except Exception as ex:
+        return_error('Error occurred while parsing output from command. Exception info:\n' + str(ex))
+
+
+if __name__ == '__main__':
+    main()
