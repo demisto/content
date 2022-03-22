d3f7730dbd5b7d7091599fcc0add1ef5ec9a54da
shleeNc <101694717+shleeNc@users.noreply.github.com>
2022-03-22 10:08:33 +0900

    Update Ncurion.py
    change the argument

--- Packs/Ncurion/Integrations/Ncurion/Ncurion.py
+++ Packs/Ncurion/Integrations/Ncurion/Ncurion.py
@@ -79,7 +79,7 @@ def get_log_list(base_url, username, password):
     return_results(results)
 
 
-def fetch_incidents(base_url, username, password, last_run: Dict[str, int])-> Tuple[Dict[str, int], List[dict]]:
+def fetch_incidents(base_url, username, password, last_run: Dict[str, int], first_fetch_time: Optional[int])-> Tuple[Dict[str, int], List[dict]]:
     access_token, refresh_token, headers1 = login(base_url, username, password)
     log_list = loglist(base_url, access_token, refresh_token, headers1)
     log_server_id = [e["id"] for e in log_list if e["is_connected"] == True]
