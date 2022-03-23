f29808264306ef48464f4b1b5ff1781a7f54a17f
Darya Koval <dkoval@paloaltonetworks.com>
2022-03-23 10:00:55 +0200

    trying to set xsiam step without waiting

--- Tests/configure_and_test_integration_instances.py
+++ Tests/configure_and_test_integration_instances.py
@@ -769,7 +769,8 @@ class XSIAMBuild(Build):
     @staticmethod
     def set_marketplace_url(servers, branch_name, ci_build_number):
         logging.info('Copying custom build bucket to xsiam_instance_bucket.')
-        from_bucket = f'{MARKETPLACE_TEST_BUCKET}/{branch_name}/{ci_build_number}/marketplacev2/content'
+        # todo: from_bucket = f'{MARKETPLACE_TEST_BUCKET}/{branch_name}/{ci_build_number}/marketplacev2/content'
+        from_bucket = f'{MARKETPLACE_TEST_BUCKET}/{branch_name}/2625991/marketplacev2/content'
         output_file = f'{ARTIFACTS_FOLDER_MPV2}/Copy_custom_bucket_to_xsiam_machine.log'
         for server in servers:
             to_bucket = f'{MARKETPLACE_XSIAM_BUCKETS}/{server.name}'
