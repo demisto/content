083f0be6dd1a156369d8f1e915b4f4241b172ed1
nmaimon <nmaimon@paloaltonetworks.com>
2022-03-27 16:15:18 +0300

    fix

--- Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance.py
+++ Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance.py
@@ -1,48 +1,48 @@
+from CommonServerPython import *  # noqa: F401
 import demistomock as demisto  # noqa: F401
 import geopy.distance
-from CommonServerPython import *  # noqa: F401
 import itertools
 requests.packages.urllib3.disable_warnings()
 
 
 def main():
     try:
-        existing = [] #demisto.get(demisto.context(), "ImpossibleTraveler.Events")
-        print('!')
+        existing = demisto.get(demisto.context(), "ImpossibleTraveler.Events")
         events_dict = {}
         for o in existing:
             events_dict[o["location"]] = o
-        print('!')
         distance_list = []
-        # Extract each set of coordinates and create a list
         args = demisto.args()
-        print('!')
-        src_coords_list = [(32.1, 42.3), (21.2, 43.2)]
-        #dest_coords_list = args['dest_coords']
+        src_coords_list = argToList(args['src_coords'])
+        dest_coords_list = argToList(args['dest_coords'])
+
+        # verify the two lists are identical - we receive two lists (and not one) for BC reasons
+        if not set(src_coords_list) == set(dest_coords_list):
+            raise DemistoException('The source coordination list and the destination coordination list '
+                                   'should be identical.')
+
         for unique_pair in itertools.combinations(src_coords_list, 2):
-            print(unique_pair)
             geo_distance = round(geopy.distance.geodesic(unique_pair[0], unique_pair[1]).miles, 2)
             hr = 'Calculated Distance: {} miles.'.format(str(geo_distance))
             context = {
                 "distance": geo_distance,
                 "src_coords": unique_pair[0],
                 "dest_coords": unique_pair[1],
-                # "source_ip": events_dict[unique_pair[0]]["ip"],
-                # "source_country": events_dict[unique_pair[0]]["Country"],
-                # "dest_ip": events_dict[unique_pair[1]]["ip"],
-                # "dest_country": events_dict[unique_pair[1]]["Country"],
-                # "timestamp": events_dict[unique_pair[0]]["event_timestamp"],
-                # "identity": events_dict[unique_pair[0]]["identity_display_name"]
+                "source_ip": events_dict[unique_pair[0]]["ip"],
+                "source_country": events_dict[unique_pair[0]]["Country"],
+                "dest_ip": events_dict[unique_pair[1]]["ip"],
+                "dest_country": events_dict[unique_pair[1]]["Country"],
+                "timestamp": events_dict[unique_pair[0]]["event_timestamp"],
+                "identity": events_dict[unique_pair[0]]["identity_display_name"]
             }
-            print(context)
             distance_list.append(CommandResults(readable_output=hr, outputs=context,
-                                outputs_prefix="GeoEvents", outputs_key_field=""))
+                                                outputs_prefix="GeoEvents", outputs_key_field=""))
 
         return_results(distance_list)
 
-    except Exception as ex:
-        return_error('Error occurred while parsing output from command. Exception info:\n' + str(ex))
+    except Exception as e:
+        return_error('Error occurred while parsing output from command. Exception info:\n' + str(e))
 
 
-if __name__ == '__main__':
+if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
