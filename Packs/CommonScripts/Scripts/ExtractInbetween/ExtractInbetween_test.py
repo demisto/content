0062bb5f914431538cd62ebf019dffb6bcacd04f
Adam Burt <aburt@paloaltonetworks.com>
2022-03-28 09:44:19 +0100

    Added more tests

--- Packs/CommonScripts/Scripts/ExtractInbetween/ExtractInbetween_test.py
+++ Packs/CommonScripts/Scripts/ExtractInbetween/ExtractInbetween_test.py
@@ -12,8 +12,24 @@ def test_extract_inbetween():
     Then:
         Validate the right output returns.
     """
+
+    # Test 1
     value = "<This is a value>"
     start = "<"
     end = ">"
     res = extract_inbetween(value, start, end)
     assert res == "This is a value"
+
+    # Test 2
+    value = {
+        "key1": "value1",
+        "key2": "value2"
+    }
+    res = extract_inbetween(value, start, end)
+    assert res == value
+
+    # Test3
+    value = 10
+    res = extract_inbetween(value, start, end)
+    assert res == "ERROR: The input value must be a string"
+
