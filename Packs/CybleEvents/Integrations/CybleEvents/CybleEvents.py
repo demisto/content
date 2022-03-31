3f555a98fe1041415131a98c648301c9d53e9e20
Cyble Infosec <developers@cyble.com>
2022-03-31 21:34:21 +0530

    updates for review suggestions
    changes done as per review inputs

--- Packs/CybleEvents/Integrations/CybleEvents/CybleEvents.py
+++ Packs/CybleEvents/Integrations/CybleEvents/CybleEvents.py
@@ -44,7 +44,7 @@ class Client(BaseClient):
 
         payload = {}
         headers = {
-            'X-API-KEY': '{}'.format(params['token'])
+            'X-API-KEY': '{}'.format(params.get('token',''))
         }
         url = urljoin(self._base_url, etypeurl)
 
@@ -69,33 +69,34 @@ class Client(BaseClient):
         :param params: parameters to be used as part of request
         :return:indicator details as JSON
         """
-        ioc_data = None
-
+        ioc_data = {}
+        token = params.get('token', '')
         payload = {
-            'token': '{}'.format(params['token']),
-            'from': int(params['from']),
-            'limit': int(params['limit']),
-            'start_date': '{}'.format(params['start_date']),
-            'end_date': '{}'.format(params['end_date']),
-            'type': '{}'.format(params['type']),
-            'keyword': '{}'.format(params['keyword'])
+            'token': '{}'.format(token),
+            'from': int(params.get('from', '')),
+            'limit': int(params.get('limit', '50')),
+            'start_date': '{}'.format(params.get('start_date')),
+            'end_date': '{}'.format(params.get('end_date')),
+            'type': '{}'.format(params.get('type')),
+            'keyword': '{}'.format(params.get('keyword'))
         }
         files = []
         headers = {
-            "Cookie": "XSRF-TOKEN={}".format(params['token'])
+            "Cookie": "XSRF-TOKEN={}".format(token)
         }
         url = urljoin(self._base_url, iocurl)
 
         response = requests.request('{}'.format(str(method).upper()), url, headers=headers, data=payload, files=files)
-        resp = response.json()
 
         try:
+            resp = response.json()
             if 'count' in resp.keys():
                 ioc_data = resp
             else:
+                ioc_data = {"error": "Failed to Fetch Taxiis !!"}
                 demisto.error("Error trying to Fetch IOC's {}".format(resp))
         except Exception as e:
-            demisto.error("[{}] exception seen for response [{}]".format(e, resp))
+            demisto.error("Error: [{}] for response [{}]".format(e, resp))
 
         return ioc_data
 
@@ -110,14 +111,14 @@ class Client(BaseClient):
 
         events_data = None
         payload = json.dumps({
-            'from': params['from'],
-            'limit': params['limit'],
-            'start_date': params['start_date'],
-            'end_date': params['end_date'],
-            'order_by': params['order_by']
+            'from': params.get('from'),
+            'limit': params.get('limit', '50'),
+            'start_date': params.get('start_date'),
+            'end_date': params.get('end_date'),
+            'order_by': params.get('order_by')
         })
         headers = {
-            'X-API-KEY': '{}'.format(params['token']),
+            'X-API-KEY': '{}'.format(params.get('token')),
             'Content-Type': 'application/json'
         }
 
@@ -127,8 +128,8 @@ class Client(BaseClient):
         try:
             resp = response.json()
 
-            if 'success' in resp.keys() and resp['success'] is True:
-                events_data = resp['data']['results']
+            if 'success' in resp.keys() and (resp.get('success') or False):
+                events_data = resp.get('data', {}).get('results')
             else:
                 demisto.error("Error trying to Fetch Events {}".format(resp))
         except Exception as e:
@@ -147,11 +148,11 @@ class Client(BaseClient):
         """
 
         payload = json.dumps({
-            'from': params['from'],
-            'limit': params['limit']
+            'from': params.get('from'),
+            'limit': params.get('limit', '50')
         })
         headers = {
-            'X-API-KEY': '{}'.format(params['token']),
+            'X-API-KEY': '{}'.format(params.get('token')    ),
             'Content-Type': 'application/json'
         }
 
@@ -165,11 +166,10 @@ class Client(BaseClient):
         except Exception as e:
             demisto.error('Exception while fetching the event details {}'.format(e))
 
-        if response.status_code == 200 and 'success' in resp.keys() and resp['success'] is True:
-            events_data.extend(resp['events'])
+        if response.status_code == 200 and 'success' in resp.keys() and (resp.get('success') or False):
+            events_data.extend(resp.get('events', []))
             if resp['total_count'] != len(events_data) and len(events_data) <= MAX_EVENT_ITEMS:
-                params['from'] += params['limit']
-                time.sleep(0.05)
+                params['from'] += params.get('limit', '50')
                 self.get_event_details(method, eventurl, params, events_data)
         else:
             demisto.error("Fetch event detail error (code:{}, reason:{})".format(response.status_code, response.reason))
@@ -189,7 +189,7 @@ def get_test_response(client, method, token):
     eventtypes_url = r'/api/v2/events/types'
     eventTypes = client.get_event_types(method, eventtypes_url, params)
 
-    if eventTypes is not None:
+    if eventTypes:
         return 'ok'
     else:
         demisto.error("Failed to connect")
@@ -211,9 +211,9 @@ def get_event_types(client, method, token):
     eventtypes_url = r'/api/v2/events/types'
     eventTypes = client.get_event_types(method, eventtypes_url, params)
 
-    if eventTypes != None:
+    if eventTypes:
         for eachone in eventTypes:
-            eTypeAlias[eachone['type']] = eachone['alias']
+            eTypeAlias[eachone['type']] = eachone.get('alias')
 
     return eTypeAlias
 
@@ -227,22 +227,27 @@ def cyble_fetch_iocs(client, method, args):
     :return: indicators from server
     """
     params = {
-        'token': args['token'],
-        'from': int(args['from']),
-        'limit': int(args['limit']),
-        'start_date': args['start_date'],
-        'end_date': args['end_date'],
-        'type': args['type'] if 'type' in args.keys() else "",
-        'keyword': args['keyword'] if 'keyword' in args.keys() else ""
+        'token': args.get('token', ''),
+        'from': arg_to_number(args.get('from')),
+        'limit': arg_to_number(args.get('limit', '50')),
+        'start_date': args.get('start_date'),
+        'end_date': args.get('end_date'),
+        'type': args.get('type') or '',
+        'keyword': args.get('keyword') or '',
     }
 
     ioc_url = r'/api/iocs'
-    result = client.get_iocs(method, ioc_url, params)
-
-    if result is not None:
-        return result
+    if args.get('token'):
+        result = client.get_iocs(method, ioc_url, params)
     else:
-        return 'Failed to Fetch IOCs !!'
+        result = {"error": "Invalid token !!"}
+
+    command_results = CommandResults(
+        outputs_prefix='CybleEvents.IoCs',
+        outputs_key_field='data',
+        outputs=result
+    )
+    return command_results
 
 
 def format_incidents(resp, eventTypes):
@@ -252,34 +257,34 @@ def format_incidents(resp, eventTypes):
     :param eventTypes: event types available
     :return: incidents to feed into XSOAR
     """
-    alerts: List[Dict[str, Any]] = []
+    events: List[Dict[str, Any]] = []
     try:
-        for eachalert in resp['data']:
-            for e_type in list(eachalert['alert']['services'].keys()):
-                e_id = eachalert['alert']['id']
-                e_priority = eachalert['alert']['priority']
-                e_created = eachalert['alert']['created_at']
-                e_keyword = eachalert['alert']['tag_name']
-                e_bucket = eachalert['alert']['bucket']['name']
-
+        alerts = resp.get('data') or []
+        for alert in alerts:
+            alert_data = alert.get('alert', {})
+            alert_id = alert_data.get('id')
+            alert_priority = alert_data.get('priority')
+            alert_created_at = alert_data.get('created_at')
+            alert_keyword = alert_data.get('tag_name')
+            alert_bucket_name = alert_data.get('bucket', {}).get('name')
+            for e_type in list(alert_data.get('services', {}).keys()):
+                event_type = eventTypes.get(e_type)
                 alert_details = {
-                    "name": "Cyble Intel Alert on {}".format(eventTypes[e_type]),
+                    "name": "Cyble Intel Alert on {}".format(event_type),
                     "cybleeventtype": "{}".format(e_type),
-                    "severity": INCIDENT_SEVERITY[e_priority.lower()],
-                    "occurred": "{}".format(e_created),
-                    "cybleeventid": "{}".format(e_id),
-                    "cybleeventname": "Incident of {} type".format(eventTypes[e_type]),
-                    "cybleeventbucket": "{}".format(e_bucket),
-                    "cybleeventkeyword": "{}".format(e_keyword),
-                    "cybleeventalias": "{}".format(eventTypes[e_type]),
-
+                    "severity": INCIDENT_SEVERITY.get(alert_priority.lower()),
+                    "occurred": "{}".format(alert_created_at),
+                    "cybleeventid": "{}".format(alert_id),
+                    "cybleeventname": "Incident of {} type".format(event_type),
+                    "cybleeventbucket": "{}".format(alert_bucket_name),
+                    "cybleeventkeyword": "{}".format(alert_keyword),
+                    "cybleeventalias": "{}".format(event_type)
                 }
-
-                alerts.append(alert_details)
-
-        return alerts
+                events.append(alert_details)
+        return events
     except Exception as e:
-        return "Format incident issue"
+        demisto.debug('Unable to format incidents, error: {e}')
+        return []
 
 
 def cyble_fetch_events(client, method, args):
@@ -291,23 +296,32 @@ def cyble_fetch_events(client, method, args):
     :return: events from the server
     """
     params = {
-        'token': args['token'],
-        'from': int(args['from']),
-        'limit': int(args['limit']),
-        'start_date': args['start_date'],
-        'end_date': args['end_date'],
-        'order_by': args['order_by']
+        'token': args.get('token'),
+        'from': arg_to_number(args.get('from', '0')),
+        'limit': arg_to_number(args.get('limit', '50')),
+        'start_date': args.get('start_date'),
+        'end_date': args.get('end_date'),
+        'order_by': args.get('order_by')
     }
 
     events_url = r'/api/v2/events/all'
-    result = client.get_alerts(method, events_url, params)
+    if args.get('token'):
+        result = client.get_alerts(method, events_url, params)
+    else:
+        result = {}
 
     incidents: List[Dict[str, Any]] = []
-    if result is not None:
+    if result:
         eventTypes = get_event_types(client, "GET", args['token'])
         incidents = format_incidents(result, eventTypes)
 
-    return incidents
+    command_results = CommandResults(
+        outputs_prefix='CybleEvents.Events',
+        outputs_key_field=['cybleeventid', 'cybleeventtype'],
+        outputs=incidents
+    )
+
+    return command_results
 
 
 def fetch_alert_details(client, args):
@@ -331,14 +345,16 @@ def fetch_alert_details(client, args):
         'from': fetch_from,
         'limit': LIMIT_EVENT_ITEMS
     }
-    client.get_event_details("POST", events_url, params, results)
+    if args.get('token'):
+        client.get_event_details("POST", events_url, params, results)
 
-    if results:
-        return results
-    else:
-        demisto.error('Fetch event details for {} with ID {}'.format(eventtype, eventid))
-        return []
+    command_results = CommandResults(
+        outputs_prefix='CybleEvents.Events',
+        outputs_key_field='Details',
+        outputs=results
+    )
 
+    return command_results
 
 def fetch_incidents(client, method, token, maxResults):
     """
@@ -360,9 +376,9 @@ def fetch_incidents(client, method, token, maxResults):
 
     params = {
         'token': token,
-        'from': int(last_run['fetched_alert_count']),
+        'from': int(last_run.get('fetched_alert_count', '0')),
         'limit': int(MAX_EVENT_ITEMS) if maxResults > 50 else int(maxResults),
-        'start_date': last_run['event_pull_start_date'],
+        'start_date': last_run.get('event_pull_start_date', '0'),
         'end_date': date.today().strftime("%Y/%m/%d"),
         'order_by': 'Ascending'
     }
@@ -371,19 +387,19 @@ def fetch_incidents(client, method, token, maxResults):
     result = client.get_alerts(method, events_url, params)
 
     incidents: List[Dict[str, Any]] = []
-    if result is not None:
-        last_run['total_alert_count'] = result['total_count']
-        last_run['fetched_alert_count'] += len(result['data'])
+    if result:
+        last_run['total_alert_count'] = result.get('total_count', 0)
+        last_run['fetched_alert_count'] += len(result.get('data', 0))
         eventTypes = get_event_types(client, "GET", token)
         events = format_incidents(result, eventTypes)
 
         try:
-            for eachinci in events:
+            for event in events:
                 inci = {
-                    'name': eachinci['name'],
-                    'severity': eachinci['severity'],
-                    'occurred': eachinci['occurred'],
-                    'rawJSON': json.dumps(eachinci)
+                    'name': event.get('name'),
+                    'severity': event.get('severity'),
+                    'occurred': event.get('occurred'),
+                    'rawJSON': json.dumps(event)
                 }
                 incidents.append(inci)
 
@@ -405,12 +421,13 @@ def main():
     """
 
     # get the service API url
-    base_url = demisto.params().get('url')
-    token = demisto.params().get('token')
+    params = demisto.params()
+    base_url = params.get('url')
+    token = params.get('token')
 
-    verify_certificate = not demisto.params().get('insecure', False)
+    verify_certificate = not params.get('insecure', False)
 
-    proxy = demisto.params().get('proxy', False)
+    proxy = params.get('proxy', False)
 
     LOG(f'Command being called is {demisto.command()}')
     try:
@@ -430,7 +447,7 @@ def main():
         elif demisto.command() == 'fetch-incidents':
             # Convert the argument to an int using helper function or set to MAX_INCIDENTS_TO_FETCH
             max_results = arg_to_number(
-                arg=demisto.params().get('max_fetch'),
+                arg=params.get('max_fetch'),
                 arg_name='max_fetch',
                 required=True
             )
@@ -451,15 +468,7 @@ def main():
             if 'end_date' not in args.keys():
                 args['end_date'] = datetime.today().strftime('%Y-%m-%d')
 
-            if args['token'] is not None:
-                command_results = CommandResults(
-                    outputs_prefix='CybleEvents.IoCs',
-                    outputs_key_field='data',
-                    outputs=cyble_fetch_iocs(client, 'POST', args)
-                )
-                return_results(command_results)
-            else:
-                demisto.error("Error fetching Threat Indicators.")
+            return_results(cyble_fetch_iocs(client, 'POST', args))
 
         elif demisto.command() == 'cyble-vision-fetch-events':
             # This is the call made when cyble-fetch-events command.
@@ -469,27 +478,11 @@ def main():
             if 'end_date' not in args.keys():
                 args['end_date'] = datetime.today().strftime('%Y/%m/%d')
 
-            if args['token'] is not None:
-                command_results = CommandResults(
-                    outputs_prefix='CybleEvents.Events',
-                    outputs_key_field=['cybleeventid', 'cybleeventtype'],
-                    outputs=cyble_fetch_events(client, 'POST', args)
-                )
-                return_results(command_results)
-            else:
-                demisto.error("Error fetching Incident alerts.")
+            return_results(cyble_fetch_events(client, 'POST', args))
 
         elif demisto.command() == "cyble-vision-fetch-event-detail":
             # Fetch event detail.
-            if args['token'] is not None:
-                command_results = CommandResults(
-                    outputs_prefix='CybleEvents.Events',
-                    outputs_key_field='Details',
-                    outputs=fetch_alert_details(client, args)
-                )
-                return_results(command_results)
-            else:
-                demisto.error("Error fetching Incident alert details.")
+            return_results(fetch_alert_details(client, args))
 
     # Log exceptions
     except Exception as e:
