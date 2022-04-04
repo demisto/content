a88a3a8a62aa8a380758c89699b3591d543a3ef0
nmaimon <nmaimon@paloaltonetworks.com>
2022-04-04 15:11:21 +0300

    UT

--- Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance.py
+++ Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance.py
@@ -7,40 +7,44 @@ from CommonServerPython import *  # noqa: F401
 requests.packages.urllib3.disable_warnings()
 
 
+def get_distances_list(src_coords_list: list, events_dict: dict):
+
+    distance_list = []
+    for unique_pair in itertools.combinations(src_coords_list, 2):
+        geo_distance = round(geopy.distance.geodesic(unique_pair[0], unique_pair[1]).miles, 2)
+        hr = 'Calculated Distance: {} miles.'.format(str(geo_distance))
+        context = {
+            "distance": geo_distance,
+            "src_coords": unique_pair[0],
+            "dest_coords": unique_pair[1],
+            "source_ip": events_dict[unique_pair[0]]["ip"],
+            "source_country": events_dict[unique_pair[0]]["Country"],
+            "dest_ip": events_dict[unique_pair[1]]["ip"],
+            "dest_country": events_dict[unique_pair[1]]["Country"],
+            "timestamp": events_dict[unique_pair[0]]["event_timestamp"],
+            "identity": events_dict[unique_pair[0]]["identity_display_name"]
+        }
+        distance_list.append(CommandResults(readable_output=hr, outputs=context,
+                                            outputs_prefix="GeoEvents", outputs_key_field=""))
+    return distance_list
+
+
 def main():
     try:
         existing = demisto.get(demisto.context(), "ImpossibleTraveler.Events")
         events_dict = {}
         for o in existing:
             events_dict[o["location"]] = o
-        distance_list = []
         args = demisto.args()
         src_coords_list = argToList(args['src_coords'])
         dest_coords_list = argToList(args['dest_coords'])
 
         # verify the two lists are identical - we receive two lists (and not one) for BC reasons
         if not set(src_coords_list) == set(dest_coords_list):
-            raise DemistoException('The source coordination list and the destination coordination list '
+            raise ValueError('The source coordination list and the destination coordination list '
                                    'should be identical.')
 
-        for unique_pair in itertools.combinations(src_coords_list, 2):
-            geo_distance = round(geopy.distance.geodesic(unique_pair[0], unique_pair[1]).miles, 2)
-            hr = 'Calculated Distance: {} miles.'.format(str(geo_distance))
-            context = {
-                "distance": geo_distance,
-                "src_coords": unique_pair[0],
-                "dest_coords": unique_pair[1],
-                "source_ip": events_dict[unique_pair[0]]["ip"],
-                "source_country": events_dict[unique_pair[0]]["Country"],
-                "dest_ip": events_dict[unique_pair[1]]["ip"],
-                "dest_country": events_dict[unique_pair[1]]["Country"],
-                "timestamp": events_dict[unique_pair[0]]["event_timestamp"],
-                "identity": events_dict[unique_pair[0]]["identity_display_name"]
-            }
-            distance_list.append(CommandResults(readable_output=hr, outputs=context,
-                                                outputs_prefix="GeoEvents", outputs_key_field=""))
-
-        return_results(distance_list)
+        return_results(get_distances_list(src_coords_list, events_dict))
 
     except Exception as e:
         return_error('Error occurred while parsing output from command. Exception info:\n' + str(e))
--- Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance_test.py
+++ Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance_test.py
@@ -1,9 +1,27 @@
 from impossibleTravelerGetDistance import *
-import mocker.
+import pytest
 
-COORDS="[\"32.0123, 34.7705\",\"50.8847, 4.5049\",\"40.7157, -74\",\"31.9522, 34.8943\"]"
+COORDS="[\"32.0123, 34.7705\",\"50.8847, 4.5049\"]"
+BAD_DEST_COORDS = "[\"32.0123, 34.7705\"]"
 
-def test_impossible_traveler_get_distance(mocker):
+EXISTING_MOCK = [
+    {
+        'Country': 'IL',
+        'event_timestamp': '2022-03-02T10: 06: 09Z',
+        'identity_display_name': 'paanalyticstest',
+        'ip': 'ip',
+        'location': '32.0123, 34.7705'
+    },
+    {
+        'Country': 'BE',
+        'event_timestamp': '2022-03-02T10: 06: 09Z',
+        'identity_display_name': 'paanalyticstest',
+        'ip': 'ip',
+        'location': '50.8847, 4.5049'
+    }]
+
+
+def test_get_distances_list():
     """
 
     Given:
@@ -14,6 +32,9 @@ def test_impossible_traveler_get_distance(mocker):
         Return valid distances
 
     """
-    mocker.patch.object(demisto, 'get', return_value=[])
-    mocker.patch.object(demisto, 'args', return_value={'src_coords': COORDS, 'dest_coords': COORDS})
-
+    events_dict = {}
+    for o in EXISTING_MOCK:
+        events_dict[o["location"]] = o
+    result = get_distances_list(argToList(COORDS), events_dict)
+    assert result[0].outputs.get('distance') == 2016.25
+    assert result[0].outputs.get('source_country') == 'IL'
