aba7fa7798b325bab3be277eafb0eb5c2e120ada
Dror Avrahami <droravr@gmail.com>
2022-04-04 10:12:17 +0300

    added spaces for better visualization

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -3643,7 +3643,7 @@ class Common(object):
             if self.md5:
                 file_context['MD5'] = self.md5
                 file_context.setdefault('Hashes', []).append({'type': 'MD5',
-                                               'value': self.md5})
+                                                              'value': self.md5})
 
             if self.sha1:
                 file_context['SHA1'] = self.sha1
