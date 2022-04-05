53eebb83d79ce79a2d554de2266ecf523f63fdc7
darkushin <darkushin@paloaltonetworks.com>
2022-04-05 19:03:10 +0300

    Skipped TPB and small updates

--- Packs/CortexDataLake/Integrations/CortexDataLake/CortexDataLake.py
+++ Packs/CortexDataLake/Integrations/CortexDataLake/CortexDataLake.py
@@ -860,7 +860,8 @@ def prepare_fetch_incidents_query(fetch_timestamp: str,
         fetch_severity: Severity associated with the incident.
         fetch_subtype: Identifies the log subtype.
         fetch_table: Identifies the fetch type.
-        fetch_fields: Fields to fetch fro the table.
+        fetch_fields: Fields to fetch from the table.
+        fetch_filter: Filter that should be used for the query
 
     Returns:
         SQL query that matches the arguments
