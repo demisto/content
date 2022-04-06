78878ad1b2865bc285b55fcf02bbc27caa37b429
YuvHayun <yhayun@paloaltonetworks.com>
2022-04-06 11:23:52 +0300

    Added unit tests

--- Packs/Ipstack/Integrations/Ipstack/Ipstack_test.py
+++ Packs/Ipstack/Integrations/Ipstack/Ipstack_test.py
@@ -1,7 +1,7 @@
 import pytest
 import demistomock as demisto
 
-RAW_RESPONSE_MOCK ={
+RAW_RESPONSE_MOCK = {
     'ip': '98.136.103.23',
     'country_name': 'country_name',
     'latitude': '1234',
@@ -10,10 +10,11 @@ RAW_RESPONSE_MOCK ={
     'type': 'type'
 }
 
+
 @pytest.mark.parametrize('raw_response_mock', [
     (RAW_RESPONSE_MOCK)
 ])
-def test_params(mocker, raw_response_mock):
+def test_right_location_format(mocker, raw_response_mock):
     """
     Given:
       - Case 1: raw_response mock
@@ -27,15 +28,7 @@ def test_params(mocker, raw_response_mock):
     mocker.patch('Ipstack.do_ip', return_value=raw_response_mock)
     mocker.patch.object(demisto, 'results')
     from Ipstack import do_ip_command
-
+    do_ip_command()
     results = demisto.results.call_args[0][0]
-        assert len(demisto.results.call_args[0]) == 1
-        assert demisto.results.call_count == 1
-        assert raw_response == results['Contents']
-        assert 'json' == results['ContentsFormat']
-        assert outputs == results['EntryContext']
-        assert md == results['HumanReadable']
-
-
-    _, output, _ = do_ip_command()
-    assert output.get('Location') == '1234:5678'
+    output = results.get('EntryContext').get('IP(val.Address == obj.Address)')
+    assert output.get('Geo').get('Location') == '1234:5678'
