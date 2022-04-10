3c363b23eb42a7a2725f2f98e95599db0fd6b619
Dror Av <droravr@gmail.com>
2022-04-10 11:56:12 +0300

    Updated to check multiple IPs rather than a single one.

--- Packs/CommonScripts/Scripts/IsInCidrRanges/IsInCidrRanges.py
+++ Packs/CommonScripts/Scripts/IsInCidrRanges/IsInCidrRanges.py
@@ -1,19 +1,30 @@
-import demistomock as demisto
-from CommonServerPython import *
+import ipaddress
 
-from netaddr import IPAddress, IPNetwork
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
 
 
 def main():
-    ip_address = demisto.args()['left']
+    """ Check if given IP (or IPs) address is part of a given CIDR (or a list of CIDRs).
+
+    Args:
+        ip_addresses (str): A list of IPs/IPv6s
+        cidr_range_list (str): A list of CIDRs to be checked against.
+
+    Returns:
+        bool: True if given IP is part of given CIDR range.
+    """
+
+    ip_addresses = argToList(demisto.args()['left'])
     cidr_range_list = argToList(demisto.args()['right'])
 
-    for cidr in cidr_range_list:
-        if IPAddress(ip_address) in IPNetwork(cidr):
-            demisto.results(True)
-            return
+    for ip in ip_addresses:
+        for cidr in cidr_range_list:
+            if ipaddress.ip_address(ip) in ipaddress.ip_network(cidr):
+                demisto.results(True)
+                return
 
-    demisto.results(False)
+        demisto.results(False)
 
 
 if __name__ == "__builtin__" or __name__ == "builtins":
