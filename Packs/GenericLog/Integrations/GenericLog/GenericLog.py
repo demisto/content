3fd6a01324925d31e572e52e53fe92f8e436a409
adaud <adaud@paloaltonetworks.com>
2022-04-11 17:03:57 +0300

    add github xsiam integration

--- Packs/GenericLog/Integrations/GenericLog/GenericLog.py
+++ Packs/GenericLog/Integrations/GenericLog/GenericLog.py
@@ -36,11 +36,12 @@ class ReqParams(BaseModel):
     include: str
     order: str = 'asc'
     after: str
+    per_page: str = '100'
     _normalize_after = validator('after', pre=True, allow_reuse=True)(
         convert_to_github_date
     )
 
-    def set_after(self, after):
+    def set_after_value(self, after):
         self.after = convert_to_github_date(after)
 
 
@@ -71,51 +72,51 @@ class Client:
             LOG(msg)
             raise DemistoException(msg) from exc
 
-    def set_after(self, after: str):
-        self.request.params.set_after(after)
+    def set_next_run_filter(self, after: str):
+        self.request.params.set_after_value(after)
 
 
-class GetLogs:
+class GetEvents:
     def __init__(self, client: Client) -> None:
         self.client = client
 
-    def _iter_logs(self):
+    def _iter_events(self):
         # region First Call
         response = self.client.call()
-        logs: list = response.json()
-        if not logs:
+        events: list = response.json()
+        if not events:
             return []
         # endregion
         # region Yield Response
         while True:
-            yield logs
-            # endregion
+            yield events
+        # endregion
             # region Prepare Next Iteration (Paging)
-            last = logs.pop()
-            self.client.set_after(last['@timestamp'])
+            last = events.pop()
+            self.client.set_next_run_filter(last['@timestamp'])
             # endregion
             # region Do next call
             response = self.client.call()
-            logs: list = response.json()
+            events: list = response.json()
             try:
-                logs.pop(0)
-                assert logs
+                events.pop(0)
+                assert events
             except (IndexError, AssertionError):
                 LOG('empty list, breaking')
                 break
             # endregion
 
-    def get_logs(self, limit=10):
-        stored = []
-        for logs in self._iter_logs():
-            stored.extend(logs)
-            if len(stored) >= limit:
-                return stored[:limit]
-        return stored
+    def aggregated_results(self, limit=10):
+        stored_events = []
+        for events in self._iter_events():
+            stored_events.extend(events)
+            if len(stored_events) >= limit:
+                return stored_events[:limit]
+        return stored_events
 
     @staticmethod
-    def get_last_run(logs) -> dict:
-        last_time = logs[-1].get('@timestamp') / 1000
+    def get_last_run(events) -> dict:
+        last_time = events[-1].get('@timestamp') / 1000
         next_fetch_time = datetime.fromtimestamp(last_time) + timedelta(seconds=1)
         return {'after': next_fetch_time.isoformat()}
 
@@ -124,22 +125,39 @@ if __name__ in ('__main__', '__builtin__', 'builtins'):
     # Args is always stronger. Get last run even stronger
     demisto_params = demisto.params() | demisto.args() | demisto.getLastRun()
 
+    # merge the headers together
+    headers = json.loads(demisto_params['headers'])
+    encrypted_headers = json.loads(demisto_params['encrypted_headers'])
+    demisto_params['headers'] = dict(encrypted_headers.items() | headers.items())
+    del demisto_params['encrypted_headers']
+
     demisto_params['params'] = ReqParams(**demisto_params)
+
+    last_run = '2022-04-11T13:20:00Z'
+    demisto_params['params'].set_after_value(last_run)
 
     request = Request(**demisto_params)
 
     client = Client(request)
 
-    get_logs = GetLogs(client)
+    get_events = GetEvents(client)
 
     command = demisto.command()
     if command == 'test-module':
-        limit = 1
-        get_logs.get_logs(limit)
+        get_events.aggregated_results(limit=1)
         demisto.results('ok')
     else:
         args = Args(**demisto_params)
-        logs = get_logs.get_logs(args.limit)
-        if logs:
-            demisto.setLastRun(GetLogs.get_last_run(logs))
-        demisto.results({'github': logs})
+        events = get_events.aggregated_results(args.limit)
+        if events:
+            demisto.setLastRun(GetEvents.get_last_run(events))
+
+        command_results = CommandResults(
+            readable_output=tableToMarkdown('Github events', events, headerTransform=pascalToSpace),
+            outputs_prefix='Github.Events',
+            outputs_key_field='@timestamp',
+            outputs=events,
+            raw_response=events,
+        )
+        return_results(command_results)
+
