2e5415f34d1ee55aa1e47c0776eef4f47b0bc88b
ostolero <ostolero@paloaltonetworks.com>
2022-04-12 17:34:06 +0300

    fix import

--- Packs/SymantecEndpointProtection/Integrations/SymantecEndpointProtection_V2/SymantecEndpointProtection_V2.py
+++ Packs/SymantecEndpointProtection/Integrations/SymantecEndpointProtection_V2/SymantecEndpointProtection_V2.py
@@ -3,7 +3,7 @@ from CommonServerPython import *
 import requests
 import json
 import re
-import urllib.parse
+from urllib.parse import quote
 
 requests.packages.urllib3.disable_warnings()
 
@@ -92,10 +92,9 @@ def build_query_params(params):
 
 def do_auth(server, crads, insecure, domain):
     url = fix_url(str(server)) + 'sepm/api/v1/identity/authenticate'
-    password = urllib.quote(crads.get('password'))
     body = {
         'username': crads.get('identifier') if crads.get('identifier') else '',
-        'password': password if crads.get('password') else '',
+        'password': quote(crads.get('password')) if crads.get('password') else '',
         'domain': domain if domain else ''
     }
     res = requests.post(url, headers={"Content-Type": "application/json"}, data=json.dumps(body), verify=not insecure)
