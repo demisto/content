6de1201208cd169dc330e43bb2961c94b71a4526
epintzov <epintzov@paloaltonetworks.com>
2022-04-14 16:40:07 +0300

    revert to master

--- Utils/trigger_private_build.py
+++ Utils/trigger_private_build.py
@@ -134,6 +134,7 @@ def main():
                                      'Authorization': f'Bearer {github_token}'},
                             data=json.dumps(payload),
                             verify=False)
+
         if res.status_code != 204:
             logging.critical(f'Failed to trigger private repo build, request to '
                              f'{TRIGGER_BUILD_URL} failed with error: {str(res.content)}')
@@ -144,6 +145,7 @@ def main():
             # wait 5 seconds and get the workflow ids again
             time.sleep(5)
             workflow_ids_after_dispatch = get_dispatch_workflows_ids(github_token, 'master')
+
             # compare with the first workflows list to get the current id
             workflow_ids_diff = [x for x in workflow_ids_after_dispatch if x not in pre_existing_workflow_ids]
             if workflow_ids_diff:
