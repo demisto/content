730099ce8f7dfecb3037b5d8c3a53123dc30bb97
shleeNc <101694717+shleeNc@users.noreply.github.com>
2022-04-15 14:00:23 +0900

    Update Ncurion.py

--- Packs/Ncurion/Integrations/Ncurion/Ncurion.py
+++ Packs/Ncurion/Integrations/Ncurion/Ncurion.py
@@ -1,6 +1,6 @@
 import json
 import traceback
-from typing import Dict, List, Union, Tuple, cast
+from typing import Dict, List, Union, Tuple
 import demistomock as demisto  # noqa: F401
 import requests
 from CommonServerPython import *  # noqa: F401
