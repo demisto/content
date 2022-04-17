1646c5d405520b47b3ec8e0e962bece7e70201c1
Darya Koval <dkoval@paloaltonetworks.com>
2022-04-17 13:20:00 +0300

    builds

--- Tests/scripts/darya_test.sh
+++ Tests/scripts/darya_test.sh
@@ -5,7 +5,7 @@ function get_build_job_statuses() {
 	export BUILD_STATUSES=`echo $1 | tr ' ' '\n' | xargs -I {} curl --header "PRIVATE-TOKEN: $GITLAB_STATUS_TOKEN" $BUILD_STATUS_API/{}/jobs -s | jq -c '.[] | select(.name=="xsiam_server_master" or .name=="install-packs-in-server6_1") | .status' | sed 's/"//g' | tr ' ' '\n' | sort | uniq`
 }
 
-$BUILDS="$( echo -e "2762133\n2759618" )"
+BUILDS="$( echo -e '2762133\n2759618' )"
 echo "$BUILDS"
 get_build_job_statuses "$BUILDS"
 echo -e "Build statuses found are: \n $BUILD_STATUSES"
