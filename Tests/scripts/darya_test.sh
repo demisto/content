2a882fca05682b973acecbd3ad9b3a137822cb8b
Darya Koval <dkoval@paloaltonetworks.com>
2022-04-17 11:29:35 +0300

    trying to test wait_in_line script

--- Tests/scripts/darya_test.sh
+++ Tests/scripts/darya_test.sh
@@ -2,7 +2,7 @@ export BUILD_STATUS_API=https://code.pan.run/api/v4/projects/2596/pipelines   # 
 
 
 function get_build_job_statuses() {
-	export BUILD_STATUSES=`echo $1 | tr ' ' '\n' | xargs -I {} curl --header "PRIVATE-TOKEN: $GITLAB_STATUS_TOKEN" $BUILD_STATUS_API/{}/jobs -s | jq -c '.[] | select(.name=="xsiam_server_master" or .name=="install-packs-in-server6_1") | .status' | sed 's/"//g' | tr ' ' '\n' | sort | uniq`
+	export BUILD_STATUSES=`echo $1 | tr ' ' '\n' | xargs -I {} curl --header "PRIVATE-TOKEN: $GITLAB_STATUS_TOKEN" $BUILD_STATUS_API/{}/jobs -s | jq -c '.[] | select(.name=="install-packs-in-server6_1") | .status' | sed 's/"//g' | tr ' ' '\n' | sort | uniq`
 }
 
 BUILDS="2762133\n2759618"
