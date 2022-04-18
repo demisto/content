8a48fcefc8727c291fcf56c44d1d409adc292aff
tamarfink <tamar@cybersixgill.com>
2022-04-18 17:01:13 +0300

    fixed tests

--- Packs/Cybersixgill-ActionableAlerts/Integrations/CybersixgillActionableAlerts/CybersixgillActionableAlerts_test.py
+++ Packs/Cybersixgill-ActionableAlerts/Integrations/CybersixgillActionableAlerts/CybersixgillActionableAlerts_test.py
@@ -208,7 +208,13 @@ expected_alert_output = [{'name': 'Your organization was potentially targeted by
                                            'cybersixgillstatus': 'In Treatment',
                                            'cybersixgillsite': 'rw_everest',
                                            'cybersixgillactor': None,
-                                           'cybersixgilltriggeredassets': ['Walmart']},
+                                           'cybersixgilltriggeredassets': ['Walmart'],
+                                           'cybersixgillcvss31': -1,
+                                           'cybersixgillcvss20': -1,
+                                           'cybersixgilldvescore': -1,
+                                           'cve': None,
+                                           'cybersixgillattributes': None
+                                           },
                           'status': 1,
                           'details': 'A ransomware group posted on its leak site, rw_everest, focusing on "Walmart" '
                                      '\n\n\n\n',
@@ -238,7 +244,7 @@ class MockedResponse(object):
 
 
 def get_incidents_list():
-    return copy.deepcopy(incidents_list)
+    return copy.deepcopy(incidents_list[:1])
 
 
 def get_info_item():
@@ -291,5 +297,5 @@ def test_fetch_incidents(mocker):
     assert demisto.incidents.call_count == 1
     incidents = demisto.incidents.call_args[0][0]
 
-    assert (len(incidents) == 3)
-    assert (incidents[-1] == expected_alert_output[0])
+    assert (len(incidents) == 1)
+    assert (incidents == expected_alert_output)
