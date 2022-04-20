082d59692bb33cbe8192073c53ddf81bd1d2bc86
tamarfink <tamar@cybersixgill.com>
2022-04-20 10:55:11 +0300

    update docker and fetch size

--- Packs/Cybersixgill-ActionableAlerts/Scripts/CybersixgillActionableAlertStatusUpdate/CybersixgillActionableAlertStatusUpdate.py
+++ Packs/Cybersixgill-ActionableAlerts/Scripts/CybersixgillActionableAlertStatusUpdate/CybersixgillActionableAlertStatusUpdate.py
@@ -4,14 +4,14 @@ from CommonServerPython import *  # noqa: F401
 
 def main():
     args = demisto.args()
-    command_args = {}
+    command_args = {'alert_status': None, 'alert_id': None, 'aggregate_alert_id': None}
     incident = demisto.incident()
 
     alert_status = args['new']
     alert_status = alert_status.lower().replace(' ', '_')
     command_args['alert_status'] = alert_status
 
-    labels = incident.get('labels')
+    labels = incident.get('labels', [])
     for label in labels:
         if label.get('type') == 'id':
             command_args['alert_id'] = label.get('value')
@@ -19,9 +19,8 @@ def main():
             command_args['aggregate_alert_id'] = label.get('value')
 
     demisto.info(
-        "Update status: alert id - {}, aggregate_alert_id - {}, new status - {}".format(command_args['alert_id'],
-                                                                                        command_args[
-                                                                                            'aggregate_alert_id'],
+        "Update status: alert id - {}, aggregate_alert_id - {}, new status - {}".format(command_args.get('alert_id', ''),
+                                                                                        command_args.get('aggregate_alert_id', ''),
                                                                                         alert_status))
     res = demisto.executeCommand("cybersixgill-update-alert-status-new", command_args)
 
