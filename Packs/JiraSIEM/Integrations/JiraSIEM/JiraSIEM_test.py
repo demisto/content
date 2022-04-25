fd60b9cda39171ed6f41f856b5b587539545131c
meichlerpanw <meichler@paloaltonetworks.com>
2022-04-25 11:40:28 +0300

    fix UT

--- Packs/JiraSIEM/Integrations/JiraSIEM/JiraSIEM_test.py
+++ Packs/JiraSIEM/Integrations/JiraSIEM/JiraSIEM_test.py
@@ -2,6 +2,22 @@ import json
 import io
 import requests_mock
 from freezegun import freeze_time
+import demistomock as demisto
+
+
+DEMISTO_PARAMS = {
+    'method': 'GET',
+    'url': 'https://your.domain/rest/api/3/auditing/record',
+    'max_fetch': 100,
+    'first_fetch': '3 days',
+    'credentials': {
+        'identifier': 'admin@your.domain',
+        'password': '123456',
+    }
+}
+URL = 'https://your.domain/rest/api/3/auditing/record'
+FIRST_REQUESTS_PARAMS = 'from=2022-04-11T00:00:00.000000&limit=1000&offset=0'
+SECOND_REQUESTS_PARAMS = 'from=2022-04-11T00:00:00.000000&limit=1000&offset=1000'
 
 
 def util_load_json(path):
@@ -16,7 +32,7 @@ def calculate_next_run(time):
 
 
 @freeze_time('2022-04-14T00:00:00Z')
-def test_fetch_incidents_default_time(mocker):
+def test_fetch_incidents_few_incidents(mocker):
     """
     Given
         - raw response of the http request
@@ -26,44 +42,27 @@ def test_fetch_incidents_default_time(mocker):
         - check the number of incidents that are being created
         check that the time in last_run is the on of the latest incident
     """
-    import demistomock as demisto
-    params = {
-        'method': 'GET',
-        'url': 'https://your.domain/rest/api/3/auditing/record',
-        'max_fetch': 10,
-        'first_fetch': '3 days',
-        'credentials': {
-            'identifier': 'admin@your.domain',
-            'password': '123456',
-        }
-    }
-    mocker.patch.object(demisto, 'params', return_value=params)
+
+    mocker.patch.object(demisto, 'params', return_value=DEMISTO_PARAMS)
     mocker.patch.object(demisto, 'args', return_value={})
     last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
-    results = mocker.patch.object(demisto, 'results')
     incidents = mocker.patch.object(demisto, 'incidents')
 
     with requests_mock.Mocker() as m:
-        m.get(
-            'https://your.domain/rest/api/3/auditing/record?from=2022-04-11T00:00:00.000000&limit=1000&offset=0',
-            json=util_load_json('test_data/events.json')
-        )
-        m.get(
-            'https://your.domain/rest/api/3/auditing/record?from=2022-04-11T00:00:00.000000&limit=1000&offset=1000',
-            json={}
-        )
+        m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json=util_load_json('test_data/events.json'))
+        m.get(f'{URL}?{SECOND_REQUESTS_PARAMS}', json={})
 
         from JiraSIEM import main
         main()
 
-    events = results.call_args[0][0]['Contents']
-    assert last_run.return_value.get('from') == calculate_next_run(events[0].get('created'))
+    assert last_run.return_value.get('from') == calculate_next_run(incidents.call_args[0][0][0].get('occurred')[:-1])
+    assert not last_run.return_value.get('next_time')
     assert last_run.return_value.get('offset') == 0
     assert incidents.call_args[0][0][0].get('name') == f'JiraSIEM - User Changed - 3'
 
 
 @freeze_time('2022-04-14T00:00:00Z')
-def test_fetch_incidents_one_hour(mocker):
+def test_fetch_events_no_incidents(mocker):
     """
     Given
         - raw response of the http request
@@ -73,36 +72,25 @@ def test_fetch_incidents_one_hour(mocker):
         - check the number of incidents that are being created
         check that the time in last_run is the on of the latest incident
     """
-    import demistomock as demisto
-    params = {
-        'method': 'GET',
-        'url': 'https://your.domain/rest/api/3/auditing/record',
-        'max_fetch': 10,
-        'first_fetch': '1 hour',
-        'credentials': {
-            'identifier': 'admin@your.domain',
-            'password': '123456',
-        }
-    }
-    mocker.patch.object(demisto, 'params', return_value=params)
+
+    mocker.patch.object(demisto, 'params', return_value=DEMISTO_PARAMS)
     mocker.patch.object(demisto, 'args', return_value={})
     last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
+    incidents = mocker.patch.object(demisto, 'incidents')
 
     with requests_mock.Mocker() as m:
-        m.get(
-            'https://your.domain/rest/api/3/auditing/record?from=2022-04-13T23:00:00.000000&limit=1000&offset=0',
-            json={}
-        )
+        m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json={})
 
         from JiraSIEM import main
         main()
 
     assert not last_run.return_value.get('from')
     assert last_run.return_value.get('offset') == 0
+    assert not incidents.call_args
 
 
 @freeze_time('2022-04-14T00:00:00Z')
-def test_fetch_incidents_with_a_limit_of_one(mocker):
+def test_fetch_events_max_fetch_set_to_one(mocker):
     """
     Given
         - raw response of the http request
@@ -112,34 +100,22 @@ def test_fetch_incidents_with_a_limit_of_one(mocker):
         - check the number of incidents that are being created
         check that the time in last_run is the on of the latest incident
     """
-    import demistomock as demisto
-    params = {
-        'method': 'GET',
-        'url': 'https://your.domain/rest/api/3/auditing/record',
-        'max_fetch': 1,
-        'first_fetch': '4 days',
-        'credentials': {
-            'identifier': 'admin@your.domain',
-            'password': '123456',
-        }
-    }
+    params = DEMISTO_PARAMS
+    params['max_fetch'] = 1
     mocker.patch.object(demisto, 'params', return_value=params)
     mocker.patch.object(demisto, 'args', return_value={})
     last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
+    incidents = mocker.patch.object(demisto, 'incidents')
 
     with requests_mock.Mocker() as m:
-        m.get(
-            'https://your.domain/rest/api/3/auditing/record?from=2022-04-10T00:00:00.000000&limit=1000&offset=0',
-            json=util_load_json('test_data/event.json')
-        )
-        m.get(
-            'https://your.domain/rest/api/3/auditing/record?from=2022-04-10T00:00:00.000000&limit=1000&offset=1000',
-            json={}
-        )
+        m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json=util_load_json('test_data/events.json'))
+        m.get(f'{URL}?{SECOND_REQUESTS_PARAMS}', json={})
 
         from JiraSIEM import main
         main()
 
-    assert not last_run.return_value.get('next_time')
-    assert last_run.return_value.get('from') == '2022-04-12T18:40:42.968'
-    assert last_run.return_value.get('offset') == 0
+    assert not last_run.return_value.get('from')
+    assert last_run.return_value.get('next_time') == '2022-04-12T18:45:42.968'
+    assert last_run.return_value.get('offset') == 1
+    assert incidents.call_args[0][0][0].get('name') == f'JiraSIEM - User Changed - 3'
+
