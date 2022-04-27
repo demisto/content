6327dcc5a117d78c954ad103036c650b467f7b19
meichlerpanw <meichler@paloaltonetworks.com>
2022-04-27 10:13:52 +0300

    fix ut

--- Packs/JiraSIEM/Integrations/JiraSIEM/JiraSIEM_test.py
+++ Packs/JiraSIEM/Integrations/JiraSIEM/JiraSIEM_test.py
@@ -55,65 +55,68 @@ def test_fetch_incidents_few_incidents(mocker):
         from JiraSIEM import main
         main()
 
-    assert last_run.return_value
+    assert last_run.return_value.get('from') == calculate_next_run(incidents.call_args[0][0][0].get('occurred')[:-1])
+    assert not last_run.return_value.get('next_time')
+    assert last_run.return_value.get('offset') == 0
+    assert len(incidents.call_args[0][0]) == 3
 
 
-# @freeze_time('2022-04-14T00:00:00Z')
-# def test_fetch_events_no_incidents(mocker):
-#     """
-#     Given
-#         - No events was created in Jira side in the last 3 days.
-#     When
-#         - fetch-events is running.
-#     Then
-#         - Make sure no events was created in XSIAM.
-#         - Make sure last_run was set as expected.
-#     """
-#
-#     mocker.patch.object(demisto, 'params', return_value=DEMISTO_PARAMS)
-#     mocker.patch.object(demisto, 'args', return_value={})
-#     last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
-#     incidents = mocker.patch.object(demisto, 'incidents')
-#
-#     with requests_mock.Mocker() as m:
-#         m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json={})
-#
-#         from JiraSIEM import main
-#         main()
-#
-#     assert not last_run.return_value.get('from')
-#     assert last_run.return_value.get('offset') == 0
-#     assert not incidents.call_args
-#
-#
-# @freeze_time('2022-04-14T00:00:00Z')
-# def test_fetch_events_max_fetch_set_to_one(mocker):
-#     """
-#     Given
-#         - 3 events was created in Jira side in the last 3 days.
-#     When
-#         - fetch-events is running (with max_fetch set to 1).
-#     Then
-#         - Verify that only 1 event were created in XSIAM.
-#         - Verify last_run was set as expected.
-#     """
-#
-#     params = DEMISTO_PARAMS
-#     params['max_fetch'] = 1
-#
-#     mocker.patch.object(demisto, 'params', return_value=params)
-#     mocker.patch.object(demisto, 'args', return_value={})
-#     last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
-#     incidents = mocker.patch.object(demisto, 'incidents')
-#
-#     with requests_mock.Mocker() as m:
-#         m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json=util_load_json('test_data/events.json'))
-#         m.get(f'{URL}?{SECOND_REQUESTS_PARAMS}', json={})
-#
-#         from JiraSIEM import main
-#         main()
-#
-#     assert not last_run.return_value.get('from')
-#     assert last_run.return_value.get('next_time') == '2022-04-12T18:45:42.968'
-#     assert last_run.return_value.get('offset') == 1
-#     assert len(incidents.call_args[0][0]) == 1
+@freeze_time('2022-04-14T00:00:00Z')
+def test_fetch_events_no_incidents(mocker):
+    """
+    Given
+        - No events was created in Jira side in the last 3 days.
+    When
+        - fetch-events is running.
+    Then
+        - Make sure no events was created in XSIAM.
+        - Make sure last_run was set as expected.
+    """
+
+    mocker.patch.object(demisto, 'params', return_value=DEMISTO_PARAMS)
+    mocker.patch.object(demisto, 'args', return_value={})
+    last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
+    incidents = mocker.patch.object(demisto, 'incidents')
+
+    with requests_mock.Mocker() as m:
+        m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json={})
+
+        from JiraSIEM import main
+        main()
+
+    assert not last_run.return_value.get('from')
+    assert last_run.return_value.get('offset') == 0
+    assert not incidents.call_args
+
+
+@freeze_time('2022-04-14T00:00:00Z')
+def test_fetch_events_max_fetch_set_to_one(mocker):
+    """
+    Given
+        - 3 events was created in Jira side in the last 3 days.
+    When
+        - fetch-events is running (with max_fetch set to 1).
+    Then
+        - Verify that only 1 event were created in XSIAM.
+        - Verify last_run was set as expected.
+    """
+
+    params = DEMISTO_PARAMS
+    params['max_fetch'] = 1
+
+    mocker.patch.object(demisto, 'params', return_value=params)
+    mocker.patch.object(demisto, 'args', return_value={})
+    last_run = mocker.patch.object(demisto, 'getLastRun', return_value={})
+    incidents = mocker.patch.object(demisto, 'incidents')
+
+    with requests_mock.Mocker() as m:
+        m.get(f'{URL}?{FIRST_REQUESTS_PARAMS}', json=util_load_json('test_data/events.json'))
+        m.get(f'{URL}?{SECOND_REQUESTS_PARAMS}', json={})
+
+        from JiraSIEM import main
+        main()
+
+    assert not last_run.return_value.get('from')
+    assert last_run.return_value.get('next_time') == '2022-04-12T18:45:42.968'
+    assert last_run.return_value.get('offset') == 1
+    assert len(incidents.call_args[0][0]) == 1
