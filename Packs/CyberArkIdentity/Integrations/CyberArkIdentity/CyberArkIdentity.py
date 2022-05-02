640d82d04e44e230b42337d12a82ee60feee203f
meichlerpanw <meichler@paloaltonetworks.com>
2022-05-02 14:41:12 +0300

    first call

--- Packs/CyberArkIdentity/Integrations/CyberArkIdentity/CyberArkIdentity.py
+++ Packs/CyberArkIdentity/Integrations/CyberArkIdentity/CyberArkIdentity.py
@@ -1,172 +1,158 @@
-"""Base Integration for Cortex XSOAR (aka Demisto)
-
-This is an empty Integration with some basic structure according
-to the code conventions.
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-Developer Documentation: https://xsoar.pan.dev/docs/welcome
-Code Conventions: https://xsoar.pan.dev/docs/integrations/code-conventions
-Linting: https://xsoar.pan.dev/docs/integrations/linting
-
-This is an empty structure file. Check an example at;
-https://github.com/demisto/content/blob/master/Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.py
-
-"""
-
-import demistomock as demisto
-from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
-from CommonServerUserPython import *  # noqa
+from enum import Enum
 
 import requests
-import traceback
-from typing import Dict, Any
-
-# Disable insecure warnings
-requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
-
-
-''' CONSTANTS '''
-
-DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
-
-''' CLIENT CLASS '''
+import urllib3
+from CommonServerPython import *
+import demistomock as demisto
+from pydantic import BaseModel, AnyUrl, Json, validator
+import dateparser
+from collections.abc import Generator
 
 
-class Client(BaseClient):
-    """Client class to interact with the service API
+class Method(str, Enum):
+    """
+    A list that represent the types of http request available
+    """
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    HEAD = 'HEAD'
+    PATCH = 'PATCH'
+    DELETE = 'DELETE'
 
-    This Client implements API calls, and does not contain any XSOAR logic.
-    Should only do requests and return data.
-    It inherits from BaseClient defined in CommonServer Python.
-    Most calls use _http_request() that handles proxy, SSL verification, etc.
-    For this  implementation, no special attributes defined
+
+class ReqParams(BaseModel):
+    """
+    A class that stores the request query params
+    """
+    include: str
+    order: str = 'asc'
+    after: str
+    per_page: int = 100  # Maximum is 100
+    # _normalize_after = validator('after', pre=True, allow_reuse=True)(
+    #     get_github_timestamp_format
+    # )
+
+
+class Request(BaseModel):
+    """
+    A class that stores a request configuration
+    """
+    method: Method
+    url: AnyUrl
+    headers: Optional[Union[Json[dict], dict]]
+    params: Optional[ReqParams]
+    verify = True
+    data: Optional[str] = None
+
+
+class Client:
+    """
+    A class for the client request handling
     """
 
-    # TODO: REMOVE the following dummy function:
-    def baseintegration_dummy(self, dummy: str) -> Dict[str, str]:
-        """Returns a simple python dict with the information provided
-        in the input (dummy).
+    def __init__(self, request: Request):
+        self.request = request
+        self.session = self.get_session()
 
-        :type dummy: ``str``
-        :param dummy: string to add in the dummy dict that is returned
 
-        :return: dict as {"dummy": dummy}
-        :rtype: ``str``
+    def call(self, requests=requests) -> requests.Response:
+        try:
+            response = requests.request(**self.request.dict())
+            response.raise_for_status()
+            return response
+        except Exception as exc:
+            msg = f'something went wrong with the http call {exc}'
+            LOG(msg)
+            raise DemistoException(msg) from exc
+
+    @staticmethod
+    def get_session():
+        url = "https://aam4730.my.idaptive.app/Security/StartAuthentication"
+        payload = {
+            "TenantId": "AAM4730",
+            "Version": "1.0",
+            "User": "a55668899A"
+        }
+        headers = {
+            "Accept": "*/*",
+            "Content-Type": "application/json"
+        }
+        response = requests.get(url, json=payload, headers=headers, verify=False)
+        return response.json().get('Result').get('SessionId'), response.json().get('Result').get('Challenges')[0].get('Mechanisms')[0].get('MechanismId')
+
+    # def set_next_run_filter(self, after: str):
+    #     self.request.params.after = get_github_timestamp_format(after)
+
+
+class GetEvents:
+    """
+    A class to handle the flow of the integration
+    """
+    def __init__(self, client: Client) -> None:
+        self.client = client
+
+    def _iter_events(self) -> Generator:
+        """
+        Function that responsible for the iteration over the events returned from github api
+        """
+        response = self.client.call()
+        events: list = response.json()
+
+        if len(events) == 0:
+            return []
+        while True:
+            yield events
+            last = events.pop()
+            self.client.set_next_run_filter(last['@timestamp'])
+            response = self.client.call()
+            events = response.json()
+            try:
+                events.pop(0)
+                assert events
+            except (IndexError, AssertionError):
+                LOG('empty list, breaking')
+                break
+
+    def aggregated_results(self, limit=2000) -> List[dict]:
+        """
+        Function to group the events returned from the api
+        """
+        stored_events = []
+        for events in self._iter_events():
+            stored_events.extend(events)
+            if len(stored_events) >= limit:
+                return stored_events[:limit]
+        return stored_events
+
+    @staticmethod
+    def get_last_run(events: List[dict]) -> dict:
+        """
+        Get the info from the last run, it returns the time to query from and a list of ids to prevent duplications
         """
 
-        return {"dummy": dummy}
-    # TODO: ADD HERE THE FUNCTIONS TO INTERACT WITH YOUR PRODUCT API
+        last_timestamp = events[-1]['@timestamp']
+        last_time = last_timestamp / 1000
+        next_fetch_time = datetime.fromtimestamp(last_time) + timedelta(
+            seconds=1
+        )
+        return {'after': next_fetch_time.isoformat()}
 
 
-''' HELPER FUNCTIONS '''
+def main():
+    # Args is always stronger. Get last run even stronger
+    demisto_params = demisto.params() | demisto.args() | demisto.getLastRun()
+    command = demisto.command()
+    demisto.debug(f'Command {command} was called!!!')
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
-
-''' COMMAND FUNCTIONS '''
-
-
-def test_module(client: Client) -> str:
-    """Tests API connectivity and authentication'
-
-    Returning 'ok' indicates that the integration works like it is supposed to.
-    Connection to the service is successful.
-    Raises exceptions if something goes wrong.
-
-    :type client: ``Client``
-    :param Client: client to use
-
-    :return: 'ok' if test passed, anything else will fail the test.
-    :rtype: ``str``
-    """
-
-    message: str = ''
     try:
-        # TODO: ADD HERE some code to test connectivity and authentication to your service.
-        # This  should validate all the inputs given in the integration configuration panel,
-        # either manually or by using an API that uses them.
-        message = 'ok'
-    except DemistoException as e:
-        if 'Forbidden' in str(e) or 'Authorization' in str(e):  # TODO: make sure you capture authentication errors
-            message = 'Authorization Error: make sure API Key is correctly set'
-        else:
-            raise e
-    return message
+        if command == 'test-module':
+            request = Request(**demisto_params)
+            client = Client(request)
+            get_events = GetEvents(client)
 
-
-# TODO: REMOVE the following dummy command function
-def baseintegration_dummy_command(client: Client, args: Dict[str, Any]) -> CommandResults:
-
-    dummy = args.get('dummy', None)
-    if not dummy:
-        raise ValueError('dummy not specified')
-
-    # Call the Client function and get the raw response
-    result = client.baseintegration_dummy(dummy)
-
-    return CommandResults(
-        outputs_prefix='BaseIntegration',
-        outputs_key_field='',
-        outputs=result,
-    )
-# TODO: ADD additional command functions that translate XSOAR inputs/outputs to Client
-
-
-''' MAIN FUNCTION '''
-
-
-def main() -> None:
-    """main function, parses params and runs command functions
-
-    :return:
-    :rtype:
-    """
-
-    # TODO: make sure you properly handle authentication
-    # api_key = demisto.params().get('credentials', {}).get('password')
-
-    # get the service API url
-    base_url = urljoin(demisto.params()['url'], '/api/v1')
-
-    # if your Client class inherits from BaseClient, SSL verification is
-    # handled out of the box by it, just pass ``verify_certificate`` to
-    # the Client constructor
-    verify_certificate = not demisto.params().get('insecure', False)
-
-    # if your Client class inherits from BaseClient, system proxy is handled
-    # out of the box by it, just pass ``proxy`` to the Client constructor
-    proxy = demisto.params().get('proxy', False)
-
-    demisto.debug(f'Command being called is {demisto.command()}')
-    try:
-
-        # TODO: Make sure you add the proper headers for authentication
-        # (i.e. "Authorization": {api key})
-        headers: Dict = {}
-
-        client = Client(
-            base_url=base_url,
-            verify=verify_certificate,
-            headers=headers,
-            proxy=proxy)
-
-        if demisto.command() == 'test-module':
-            # This is the call made when pressing the integration Test button.
-            result = test_module(client)
-            return_results(result)
-
-        # TODO: REMOVE the following dummy command case:
-        elif demisto.command() == 'baseintegration-dummy':
-            return_results(baseintegration_dummy_command(client, demisto.args()))
-        # TODO: ADD command cases for the commands you will implement
-
-    # Log exceptions and return errors
     except Exception as e:
-        demisto.error(traceback.format_exc())  # print the traceback
-        return_error(f'Failed to execute {demisto.command()} command.\nError:\n{str(e)}')
-
-
-''' ENTRY POINT '''
+        return_error(str(e))
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
