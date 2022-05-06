3ee86ea4c36778a62c7c011e151e40709d7397d5
Jérémy HOCDÉ <jho@secinfra.fr>
2022-05-06 17:45:59 +0200

    Remove unused Automations

--- Packs/GLPI/Scripts/GLPIGetUserID/GLPIGetUserID.py
+++
@@ -1,5 +0,0 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
-username = demisto.args().get('value')
-user_id = demisto.executeCommand('glpi-get-userid', {"name": username})[0]['Contents']
-demisto.results(str(user_id))
--- Packs/GLPI/Scripts/GLPIGetUsername/GLPIGetUsername.py
+++
@@ -1,5 +0,0 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
-user_id = demisto.args().get('value')
-username = demisto.executeCommand('glpi-get-username', {"id": user_id})[0]['Contents']
-demisto.results(str(username))
