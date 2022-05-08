1929dcd35d2f8c136742f193dad2b7b8c2863eea
Bar Hochman <bhochman@paloaltonetworks.com>
2022-05-08 22:47:45 +0300

    finished maybe

--- Packs/Box/Integrations/BoxEventsCollector/BoxEventsCollector.py
+++ Packs/Box/Integrations/BoxEventsCollector/BoxEventsCollector.py
@@ -1,17 +1,16 @@
-from datetime import datetime
+# pylint: disable=no-name-in-module
+# pylint: disable=no-self-argument
+
 import secrets
-import urllib3
-from CommonServerPython import *
-import demistomock as demisto
-from pydantic import BaseModel, validator, Field, parse_obj_as
-import dateparser
+
 import jwt
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.backends import default_backend
+import urllib3
 from cryptography import exceptions
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import load_pem_private_key
+from pydantic import Field, parse_obj_as
 
 from SiemApiModule import *  # noqa: E402
-
 
 urllib3.disable_warnings()
 
@@ -76,7 +75,7 @@ class BoxEventsParams(BaseModel):
 
 
 class BoxEventsRequest(IntegrationHTTPRequest):
-    url = parse_obj_as(AnyUrl, 'https://api.box.com/2.0/events/')
+    url = parse_obj_as(AnyUrl, 'https://api.box.com/2.0/events')
     method = Method.GET
     params: BoxEventsParams
 
@@ -90,9 +89,10 @@ class BoxEventsClient(IntegrationEventsClient):
         request: BoxEventsRequest,
         options: IntegrationOptions,
         box_credentials: BoxCredentials,
+        session=requests.Session()
     ) -> None:
         self.box_credentials = box_credentials
-        super().__init__(request, options)
+        super().__init__(request, options, session)
 
     def set_request_filter(self, after: Any):
         self.request.params.stream_position = after  # type: ignore[attr-defined]
@@ -135,14 +135,14 @@ class BoxEventsClient(IntegrationEventsClient):
         return body
 
 
-class BoxEventsGetEvents(IntegrationGetEvents):
+class BoxGetEvents(IntegrationGetEvents):
     def get_last_run(self: Any) -> dict:  # type: ignore
         demisto.debug(f'setting {self.client.request.params.stream_position=}')
         return {'stream_position': self.client.request.params.stream_position}
 
     def _iter_events(self):
         self.client.authenticate()
-        demisto.debug('authenticated succesfully')
+        demisto.debug('authenticated successfully')
         # region First Call
         events = self.client.call(self.client.request).json()
         # endregion
@@ -184,7 +184,7 @@ def _decrypt_private_key(app_auth: AppAuth):
     return key
 
 
-def main(demisto_params: dict):
+def main(command: str, demisto_params: dict):
     box_credentials = BoxCredentials.parse_raw(
         demisto_params['credentials_json']
     )
@@ -197,21 +197,31 @@ def main(demisto_params: dict):
     # set_authorization(request, demisto_params['auth_credendtials'])
     options = IntegrationOptions.parse_obj(demisto_params)
     client = BoxEventsClient(request, options, box_credentials)
-    get_events = BoxEventsGetEvents(client, options)
-    command = demisto.command()
+    get_events = BoxGetEvents(client, options)
     if command == 'test-module':
         get_events.client.request.params.limit = 1  # type: ignore[attr-defined]
         get_events.run()
         demisto.results('ok')
+        return
+    demisto.debug('not in test module, running box-get-events')
+    events = get_events.run()
+    demisto.debug(f'got {len(events)=} from api')
+    if command == 'box-get-events':
+        demisto.debug('box-get-events, publishing events to incident')
+        return_results(CommandResults(
+            'BoxEvents', 'event_id', events
+        ))
     else:
-        events = get_events.run()
-        demisto.debug(f'got {len(events)=} from api')
+        demisto.debug('in event collection')
         if events:
+            demisto.debug('publishing events')
             demisto.setLastRun(get_events.get_last_run())
             send_events_to_xsiam(events, 'box', 'box')
+        else:
+            demisto.debug('no events found, finishing script.')
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     # Args is always stronger. Get getIntegrationContext even stronger
-    demisto_params = demisto.params() | demisto.args() | demisto.getLastRun()
-    main(demisto_params)
+    demisto_params_ = demisto.params() | demisto.args() | demisto.getLastRun()
+    main(demisto.command(), demisto_params_)
