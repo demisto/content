06b839532f61dd4940a4e28ebdfc94d1bb89e46f
Ayman Mahmoud <ayman.s.mahmoud@outlook.com>
2022-05-10 09:52:32 +0400

    Updated the test functions

--- Packs/AzureCompute/Integrations/AzureCompute_v2/AzureCompute_v2_test.py
+++ Packs/AzureCompute/Integrations/AzureCompute_v2/AzureCompute_v2_test.py
@@ -1,4 +1,6 @@
 import pytest
+import json
+
 from AzureCompute_v2 import MsGraphClient, screen_errors, assign_image_attributes, list_vms_command, create_vm_parameters
 
 # test_create_vm_parameters data:
@@ -52,136 +54,30 @@ Expected_VM_PARAMS = {
     'name': 'TestVM'
 }
 
+''' HELPER FUNCTIONS '''
+
+
+def load_test_data(json_path):
+    with open(json_path) as f:
+        return json.load(f)
+
 # test_list_vms data:
-LIST_VM_RAW_DATA = {
-    'value': [
-        {
-            'name': 'testvm',
-            'id': '/subscriptions/subscription_id/resourceGroups/compute-integration/providers/Microsoft.Compute'
-                  '/virtualMachines/testvm',
-            'type': 'Microsoft.Compute/virtualMachines',
-            'location': 'westeurope',
-            'properties': {
-                'vmId': 'vm_id',
-                'hardwareProfile': {
-                    'vmSize': 'Standard_D1_v2'
-                },
-                'storageProfile': {
-                    'imageReference': {
-                        'publisher': 'OpenLogic',
-                        'offer': 'CentOS',
-                        'sku': '7.5',
-                        'version': 'latest'
-                    },
-                    'osDisk': {
-                        'osType': 'Linux',
-                        'name': 'testvm',
-                        'createOption': 'FromImage',
-                        'caching': 'ReadWrite',
-                        'managedDisk': {
-                            'storageAccountType': 'Standard_LRS',
-                            'id': '/subscriptions/subscription_id/resourceGroups/compute-integration/providers'
-                                  '/Microsoft.Compute/disks/testvm '
-                        },
-                        'diskSizeGB': 30
-                    },
-                    'dataDisks': [
 
-                    ]
-                },
-                'osProfile': {
-                    'computerName': 'testvm',
-                    'adminUsername': 'admin',
-                    'linuxConfiguration': {
-                        'disablePasswordAuthentication': False,
-                        'provisionVMAgent': True
-                    },
-                    'secrets': [
 
-                    ],
-                    'allowExtensionOperations': True
-                },
-                'networkProfile': {
-                    'networkInterfaces': [
-                        {
-                            'id': '/subscriptions/subscription_id/resourceGroups/compute-integration/providers'
-                                  '/Microsoft.Network/networkInterfaces/compute-integration-nic1',
-                            'properties': {
-                                'primary': True
-                            }
-                        }
-                    ]
-                },
-                'provisioningState': 'Succeeded'
-            }
-        },
-        {
-            'name': 'vm2_name',
-            'id': '/subscriptions/subscription_id/resourceGroups/compute-integration/providers/Microsoft.Compute'
-                  '/virtualMachines/vm2_name',
-            'type': 'Microsoft.Compute/virtualMachines',
-            'location': 'westeurope',
-            'properties': {
-                'vmId': 'vm2_id',
-                'hardwareProfile': {
-                    'vmSize': 'Standard_B1s'
-                },
-                'storageProfile': {
-                    'imageReference': {
-                        'publisher': 'RedHat',
-                        'offer': 'RHEL',
-                        'sku': '7-RAW',
-                        'version': 'latest'
-                    },
-                    'osDisk': {
-                        'osType': 'Linux',
-                        'name': 'vm2_name',
-                        'createOption': 'FromImage',
-                        'caching': 'ReadWrite',
-                        'managedDisk': {
-                            'storageAccountType': 'Standard_LRS',
-                            'id': '/subscriptions/subscription_id/resourceGroups/compute-integration/providers'
-                                  '/Microsoft.Compute/disks/vm2_name '
-                        },
-                        'diskSizeGB': 32
-                    },
-                    'dataDisks': [
-
-                    ]
-                },
-                'osProfile': {
-                    'computerName': 'vm2_name',
-                    'adminUsername': 'admin',
-                    'linuxConfiguration': {
-                        'disablePasswordAuthentication': False,
-                        'provisionVMAgent': True
-                    },
-                    'secrets': [
-
-                    ],
-                    'allowExtensionOperations': True
-                },
-                'networkProfile': {
-                    'networkInterfaces': [
-                        {
-                            'id': '/subscriptions/subscription_id/resourceGroups/compute-integration/providers'
-                                  '/Microsoft.Network/networkInterfaces/compute-integration-nic',
-                            'properties': {
-                                'primary': True
-                            }
-                        }
-                    ]
-                },
-                'provisioningState': 'Succeeded'
-            }
-        }
-    ]
-}
 VM_LIST_EC = {'Azure.Compute(val.Name && val.Name === obj.Name)': [
     {'Name': 'testvm', 'ID': 'vm_id', 'Size': 30, 'OS': 'Linux', 'Location': 'westeurope',
      'ProvisioningState': 'Succeeded', 'ResourceGroup': 'resource_group'},
     {'Name': 'vm2_name', 'ID': 'vm2_id', 'Size': 32, 'OS': 'Linux', 'Location': 'westeurope',
      'ProvisioningState': 'Succeeded', 'ResourceGroup': 'resource_group'}]}
+
+INTERFACE_EC = {'Azure.NetworkInterfaces(val.ID && val.ID === obj.id)': [
+    {'Name': 'testnic', 'ID': 'nic_id', 'MACAddress': '00-22-48-1C-73-AF', 'NetworkSecurityGroup': 'security_group_id',
+     'IsPrimaryInterface': "true", 'Location': 'eastus', 'AttachedVirtualMachine': 'vm_id',
+     'ResourceGroup': 'resource_group', 'NICType': 'Standard', 'DNSSuffix': '', 'IPConfigurations': {
+        "provisioningState": "Succeeded",
+        "privateIPAddress": "10.0.0.4",
+    }}]}
+
 
 client = MsGraphClient(
     base_url="url", tenant_id="tenant", auth_id="auth_id", enc_key="enc_key", app_name="APP_NAME", verify="verify",
@@ -216,6 +112,14 @@ def test_create_vm_parameters(args, expected_parameters):
 
 
 def test_list_vms_command(mocker):
-    mocker.patch.object(client, 'list_vms', return_value=LIST_VM_RAW_DATA)
+    vms_data = load_test_data('./test_data/list_vms_command.json')
+    mocker.patch.object(client, 'list_vms', return_value=vms_data)
     _, ec, _ = list_vms_command(client, {'resource_group': 'resource_group'})
     assert VM_LIST_EC == ec
+
+
+def test_get_network_interface_command(mocker):
+    interface_data = load_test_data('./test_data/get_network_interface_command.json')
+    mocker.patch.object(client, 'get_network_interface', return_value=interface_data)
+    _, ec, _ = list_vms_command(client, {'resource_group': 'resource_group', 'nic_name': 'nic_name'})
+    assert INTERFACE_EC == ec
