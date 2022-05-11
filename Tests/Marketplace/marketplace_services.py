7c7e1f767ffb1a2a27dd4624330d1cf4ad64e5eb
meichlerpanw <meichler@paloaltonetworks.com>
2022-05-11 22:20:29 +0300

    video list in upload flow

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -1,3 +1,4 @@
+
 import base64
 import fnmatch
 import glob
@@ -639,7 +640,7 @@ class Pack(object):
             Metadata.USE_CASES: self._use_cases,
             Metadata.KEY_WORDS: self._keywords,
             Metadata.DEPENDENCIES: self._parsed_dependencies,
-            Metadata.VIDEOS: self.user_metadata.get(Metadata.VIDEOS, ''),
+            Metadata.VIDEOS: list(self.user_metadata.get(Metadata.VIDEOS, '') or []),
         }
 
         if self._is_private_pack:
