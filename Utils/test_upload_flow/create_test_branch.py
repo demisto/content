d6e484da429e37a5d3e07a6b86d9a1cc0a728f04
iyeshaya <iyeshaya@paloaltonetworks.com>
2022-05-12 17:53:50 +0300

    Add dependency funciton

--- Utils/test_upload_flow/create_test_branch.py
+++ Utils/test_upload_flow/create_test_branch.py
@@ -5,19 +5,46 @@ from pathlib import Path
 import subprocess
 from shutil import rmtree
 import os
+import json
 
 
+def print_status(func):
+    """Log the date and time of a function"""
+
+    def wrapper():
+        print(f'Running {func.__name__}')
+        func()
+        print("Done\n")
+    return wrapper
+
+
+@print_status
 def create_new_pack():
     """
         Creates new pack with given pack name
     """
-    print("Creating new pack TestUploadFlow...", end='')
+    print("Creating new pack TestUploadFlow...", end=' ')
     content_path = Path(__file__).parent.parent.parent
     source_path = Path(__file__).parent / 'TestUploadFlow'
     dest_path = content_path / 'Packs' / 'TestUploadFlow'
     shutil.copytree(source_path, dest_path)
     subprocess.call(['demisto-sdk', 'format', '-i', dest_path], stdout=subprocess.DEVNULL)
     print("Done")
+    return dest_path
+
+
+@print_status
+def add_dependency(base_pack: Path, new_depndency_pack: Path):
+    with base_pack.open('r') as f:
+        base_metadata = json.load(f)
+    new_pack_name = new_depndency_pack.name
+    base_metadata['dependencies'][new_pack_name] = {
+        "mandatory": True,
+        "display_name": new_pack_name
+    }
+
+    with base_pack.open('w') as f:
+      json.dump(base_metadata, f)
 
 
 def create_new_branch(repo: Repo, new_branch_name: str) -> Head:
@@ -46,12 +73,13 @@ if __name__ == "__main__":
         original_branch = repo.active_branch
 
     new_branch_name = f"{original_branch}_upload_test_branch_{repo.active_branch.object.hexsha}"
+    content_path = Path(__file__).parent.parent.parent
 
     branch = create_new_branch(repo, new_branch_name)
-    
-    create_new_pack()
-    
+
+    new_pack_path = create_new_pack()
+    add_dependency(content_path/'Packs'/'Armis', new_pack_path)
+
     repo.git.commit(m=f"Added Test file")
     repo.git.push('--set-upstream', 'https://code.pan.run/xsoar/content.git', branch)
     repo.git.checkout(original_branch)
-
