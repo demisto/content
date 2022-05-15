3b749d723c9dfe9e1c0391847e4f0ab094332913
merit-maita <49760643+merit-maita@users.noreply.github.com>
2022-05-16 01:13:41 +0300

    Update Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection.py
    Co-authored-by: Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>

--- Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection.py
+++ Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection.py
@@ -4490,7 +4490,7 @@ def run_polling_command(client: MsClient, args: dict, cmd: str, action_func: Cal
     command_result = results_function(client, args)
     action_status = command_result.outputs.get("status")
     demisto.debug(f"action status is: {action_status}")
-    if len(command_result.outputs.get("commands", [])) > 0:
+    if command_result.outputs.get("commands", []):
         command_status = command_result.outputs.get("commands", [{}])[0].get("commandStatus")
     else:
         command_status = 'Completed' if action_status == "Succeeded" else None
