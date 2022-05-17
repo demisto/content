864690646209e715981dc96aa0bc926cde9acd62
Amar Shishodia <amarshishodia@MacBook-Pro-4.local>
2022-05-17 13:02:30 +0530

    Uncomment the imports

--- Packs/CTIX/Integrations/CTIX/CTIX.py
+++ Packs/CTIX/Integrations/CTIX/CTIX.py
@@ -1,23 +1,22 @@
-# Uncomment while development
-# import demistomock as demisto
-# from CommonServerPython import (
-#     Common,
-#     urlRegex,
-#     re,
-#     regexFlags,
-#     hashRegex,
-#     BaseClient,
-#     DemistoException,
-#     CommandResults,
-#     List,
-#     tableToMarkdown,
-#     argToList,
-#     return_warning,
-#     handle_proxy,
-#     return_results,
-#     traceback,
-#     return_error,
-# )
+ import demistomock as demisto
+ from CommonServerPython import (
+     Common,
+     urlRegex,
+     re,
+     regexFlags,
+     hashRegex,
+     BaseClient,
+     DemistoException,
+     CommandResults,
+     List,
+     tableToMarkdown,
+     argToList,
+     return_warning,
+     handle_proxy,
+     return_results,
+     traceback,
+     return_error,
+ )
 
 """IMPORTS"""
 
