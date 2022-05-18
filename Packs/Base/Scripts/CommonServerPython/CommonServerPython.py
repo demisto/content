c9fbd54236ff23ab2511ab62ea5e49cc1c52c3df
dorschw <81086590+dorschw@users.noreply.github.com>
2022-05-18 12:48:26 +0200

    added logging to CommandRunner

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -7053,11 +7053,13 @@ class CommandRunner:
         results, errors = [], []
         for command, args in zip(command.commands, command.args_lst):
             try:
+                demisto.debug(f'calling {command} with {args=}')
                 execute_command_results = demisto.executeCommand(command, args)
                 for res in execute_command_results:
                     brand_name = res.get('Brand', 'Unknown') if isinstance(res, dict) else 'Unknown'
                     module_name = res.get('ModuleName', 'Unknown') if isinstance(res, dict) else 'Unknown'
                     if is_error(res):
+                        demisto.debug(f'got an error when calling {command} with {args=}: {res}')
                         errors.append(CommandRunner.Result(command, args, brand_name, module_name, get_error(res)))
                     else:
                         if extract_contents:
