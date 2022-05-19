3ab34491da0c73101733132e09caef57974bc9cc
spearmin10 <minoue@paloaltonetworks.com>
2022-05-19 19:57:48 +0900

    Added FileCreateAndUploadPy

---
+++ Packs/CommonScripts/Scripts/FileCreateAndUploadPy/FileCreateAndUploadPy.py
@@ -0,0 +1,40 @@
+import base64
+import binascii
+
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+
+def main():
+    args = demisto.args()
+    filename = args.get('filename') or ''
+    data = args.get('data') or ''
+    data_encoding = args.get('data_encoding') or 'raw'
+    entdy_id = args.get('entryId')
+
+    try:
+        if entdy_id:
+            res = demisto.executeCommand('getEntry', {'id': entdy_id})
+            if is_error(res):
+                demisto.results(res)  # noqa
+                sys.exit(0)
+
+            data = demisto.get(res[0], 'Contents')
+
+        if data_encoding == 'raw':
+            pass
+        elif data_encoding == 'base64':
+            try:
+                data = base64.b64decode(data)
+            except binascii.Error:
+                raise ValueError('Invalid base64 encoding value.')
+        else:
+            raise ValueError(f'Invalid data encoding name: {data_encoding}')
+
+        demisto.results(fileResult(filename, data))  # noqa
+    except Exception as e:
+        return_error(str(e) + "\n\nTrace:\n" + traceback.format_exc())
+
+
+if __name__ in ('__builtin__', 'builtins', '__main__'):
+    main()
---
+++ Packs/CommonScripts/Scripts/FileCreateAndUploadPy/README.md
@@ -0,0 +1,25 @@
+Will create a file (using the given data input or entry ID) and upload it to current investigation war room.
+
+
+## Script Data
+---
+
+| **Name** | **Description** |
+| --- | --- |
+| Script Type | python3 |
+| Tags | Utility |
+| Cortex XSOAR Version | 6.2.0 |
+
+## Inputs
+---
+
+| **Argument Name** | **Description** |
+| --- | --- |
+| filename | The name of the file to be created. |
+| data | Input data to write to file. |
+| entryId | Entry ID contents to write into file. |
+| data_encoding | Encoding type of the input data or contents. |
+
+## Outputs
+---
+There are no outputs for this script.
