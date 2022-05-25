11742b7c832e15a098587c292d3f128e8d7f0655
Wissam Ghammashi <wghammashi@paloaltonetworks.com>
2022-05-25 04:20:52 +0300

    fix the variable name misspell

--- Tests/scripts/sdk_pylint_check.sh
+++ Tests/scripts/sdk_pylint_check.sh
@@ -5,9 +5,9 @@ echo Checking for sdk related pylint errors
 pylint_tests_result="$(python3 -m pylint --errors-only ./Tests | grep demisto_sdk)"
 pylint_utils_result="$(python3 -m pylint --errors-only ./Utils | grep demisto_sdk)"
 
-if [ -n "$pylint_test_result" ] || [ -n "$pylint_utils_result" ]; then
-    echo Pylint result on Test directory: $pylint_test_result
-    echo Pylint result on Utils directory: $pylint_utils_result
+if [ -n "$pylint_tests_result" ] || [ -n "$pylint_utils_result" ]; then
+    echo Pylint result on Test directory: "$pylint_tests_result"
+    echo Pylint result on Utils directory: "$pylint_utils_result"
     exit 1
 fi
 
