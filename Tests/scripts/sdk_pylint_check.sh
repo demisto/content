08d72b592d1ebc9d1621ee4533b8a95e955f7798
Wissam Ghammashi <wghammashi@paloaltonetworks.com>
2022-05-28 23:17:16 +0300

    fix the code review comment

--- Tests/scripts/sdk_pylint_check.sh
+++ Tests/scripts/sdk_pylint_check.sh
@@ -5,10 +5,15 @@ echo Checking for sdk related pylint errors
 pylint_tests_result="$(python3 -m pylint --errors-only ./Tests | grep demisto_sdk)"
 pylint_utils_result="$(python3 -m pylint --errors-only ./Utils | grep demisto_sdk)"
 
-if [ -n "$pylint_tests_result" ] || [ -n "$pylint_utils_result" ]; then
+if [ -n "$pylint_tests_result" ]; then
     echo Pylint result on Test directory: "$pylint_tests_result"
+fi
+if [ -n "$pylint_utils_result" ]; then
     echo Pylint result on Utils directory: "$pylint_utils_result"
     exit 1
+fi
+if [ -n "$pylint_tests_result" ]; then
+    exit 1    
 fi
 
 echo No SDK related pylint errors
