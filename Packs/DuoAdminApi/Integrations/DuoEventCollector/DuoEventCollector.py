29e36cdbd48fcc8ea97973bb20a17da7b1b38d15
gal-forer <gforer@paloaltonetworks.com>
2022-05-29 17:30:44 +0300

    pylint errors

--- Packs/DuoAdminApi/Integrations/DuoEventCollector/DuoEventCollector.py
+++ Packs/DuoAdminApi/Integrations/DuoEventCollector/DuoEventCollector.py
@@ -1,7 +1,7 @@
 from collections import deque
 from enum import Enum
 import duo_client
-from pydantic import BaseModel  # pylint: disable=import-error
+from pydantic import BaseModel  # pylint: disable=E0611
 from CommonServerPython import *
 
 
