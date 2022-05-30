83fdf4f7aa1b50718dc8045e0c0327a8a5949a17
iyeshaya <iyeshaya@paloaltonetworks.com>
2022-05-30 10:58:40 +0300

    mypy fix

--- Utils/test_upload_flow/TestUploadFlow/Integrations/TestUploadFlow/TestUploadFlow.py
+++ Utils/test_upload_flow/TestUploadFlow/Integrations/TestUploadFlow/TestUploadFlow.py
@@ -1,7 +1,11 @@
 from typing import Dict
+import demistomock as demisto  # noqa: F401
 from CommonServerPython import *
-class Client(BaseClient): # type: ignore
+
+
+class Client(BaseClient):  # type: ignore
     pass
+
 
 def test_module(client: Client) -> str:
     """Tests API connectivity and authentication'
@@ -20,7 +24,7 @@ def test_module(client: Client) -> str:
     message: str = ''
     try:
         message = 'ok'
-    except DemistoException as e: # type: ignore
+    except DemistoException as e:  # type: ignore
         if 'Forbidden' in str(e) or 'Authorization' in str(e):
             message = 'Authorization Error: make sure API Key is correctly set'
         else:
@@ -38,7 +42,7 @@ def main() -> None:
     # api_key = demisto.params().get('credentials', {}).get('password')
 
     # get the service API url
-    base_url = urljoin(demisto.params()['url'], '/api/v1') # type: ignore
+    base_url = urljoin(demisto.params()['url'], '/api/v1')  # type: ignore
 
     # if your Client class inherits from BaseClient, SSL verification is
     # handled out of the box by it, just pass ``verify_certificate`` to
@@ -64,10 +68,10 @@ def main() -> None:
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration Test button.
             result = test_module(client)
-            return_results(result) # type: ignore
+            return_results(result)  # type: ignore
 
         elif demisto.command() == 'hello':
-            return_results(CommandResults( # type: ignore
+            return_results(CommandResults(  # type: ignore
                 outputs_prefix='TestUploadFlow',
                 outputs_key_field='',
                 outputs="hello",
@@ -75,11 +79,10 @@ def main() -> None:
 
     # Log exceptions and return errors
     except Exception as e:
-        return_error(f'Failed to execute {demisto.command()} command.\nError:\n{str(e)}') # type: ignore
+        return_error(f'Failed to execute {demisto.command()} command.\nError:\n{str(e)}')  # type: ignore
 
 
 ''' ENTRY POINT '''
-
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
