12691fad9d667cc25db0a0a86843840f4d938666
avidan-H <>
2022-05-31 15:09:01 +0300

    feat(base): add GetErrors pb error handling utility script
    fix(geterrors): address crs
    
    fix: add lastCompletedTaskEntries to known words list
    
    fix: increase unit tests coverage
    
    fix(docs): update the release notes
    
    fix: move script to CommonScripts pack
    
    fix: update name to GetErrorsFromEntry

---
+++ Packs/CommonScripts/Scripts/GetErrorsFromEntry/GetErrorsFromEntry.py
@@ -0,0 +1,44 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+
+def get_errors(entries):
+    """
+        entries should be a list of demisto entries
+
+        :type entries: ``list``
+        :param entries: multiples entries of results of demisto.executeCommand()
+
+        :return: Error message extracted from the demisto.executeCommand() result
+        :rtype: ``string``
+    """
+    error_messages = []
+    for entry in entries:
+        assert len(entry) == 1
+        entry_details = entry[0]
+        is_error_entry = isinstance(entry_details, dict) and entry_details['Type'] == entryTypes['error']
+        if is_error_entry:
+            demisto.debug(f'error entry contents: "{entry_details["Contents"]}"')
+            error_messages.append(entry_details['Contents'])
+
+    return error_messages
+
+
+def main():
+    try:
+        args = demisto.args()
+        entry_ids = args.get('entry_id')
+        entries = [demisto.executeCommand('getEntry', {'id': entry_id}) for entry_id in entry_ids]
+        error_messages = get_errors(entries)
+        return_results(CommandResults(
+            readable_output='\n'.join(error_messages),
+            outputs_prefix='ErrorEntries',
+            outputs=error_messages,
+            raw_response=error_messages,
+        ))
+    except Exception as e:
+        return_error(f'Failed to fetch errors for the given entry id(s). Problem: {str(e)}')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
