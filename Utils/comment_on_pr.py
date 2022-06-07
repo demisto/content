b845cfc42262558d2f3879376f51e9374cb8f3ba
aradcarmi <aradcarmi220@gmail.com>
2022-06-07 17:13:09 +0300

    Test

--- Utils/comment_on_pr.py
+++ Utils/comment_on_pr.py
@@ -16,7 +16,7 @@ def main():
 
     pr_number = args.pr_number
     comment = args.comment
-    token = os.environ['XSOAR_BOT_TEST_CONTENT']
+    token = os.environ['CONTENT_GITHUB_TOKEN']
 
     comments_url = get_pr_comments_url(pr_number)
 
