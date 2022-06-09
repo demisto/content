dabde3910548cfbe6264c62eff7042cb2059dbd3
aradcarmi <aradcarmi220@gmail.com>
2022-06-09 11:47:21 +0300

    Updated token

--- Packs/URLHaus/Integrations/URLHaus/URLHaus.py
+++ Packs/URLHaus/Integrations/URLHaus/URLHaus.py
@@ -217,9 +217,9 @@ def url_create_tags(urlhaus_data: dict) -> List[str]:
             urlhaus_data (dict): The data retrieved from URLHaus db.
 
         Returns:
-            tags (list): a list of tags to add.
+            tags (list): a list of tags to add .
     """
-    tags = urlhaus_data.get('Tags')
+    tags = urlhaus_data.get('Tags', [])
     if urlhaus_data.get('Threat'):
         tags.append(urlhaus_data.get('Threat'))
     return tags
