5004c3a30067d2b4ac2ce23212406abc61043e5b
merit <meretmaayta@gmail.com>
2022-06-11 15:09:35 +0300

    edits

--- Packs/ApiModules/Scripts/AtlassianApiModule/AtlassianApiModule.py
+++ Packs/ApiModules/Scripts/AtlassianApiModule/AtlassianApiModule.py
@@ -51,8 +51,7 @@ class AtlassianClient(BaseClient):
                 '- Personal Access Tokens requires AccessToken'
             )
         self.headers = headers
-        super().__init__(base_url=self.base_url, verify=self.use_ssl, headers=self.headers,
-                         auth=self.auth)
+        super().__init__(headers=self.headers, auth=self.auth)
 
     def get_auth(self):
         return self.auth
