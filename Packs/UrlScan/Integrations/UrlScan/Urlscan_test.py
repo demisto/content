d3533a445f8bae85b6df316ae1c8890e47ef4b52
ashamah <ashamah@paloaltonetworks.com>
2022-06-15 16:30:56 +0300

    Changes per suggestions + UTs

--- Packs/UrlScan/Integrations/UrlScan/Urlscan_test.py
+++ Packs/UrlScan/Integrations/UrlScan/Urlscan_test.py
@@ -4,7 +4,8 @@ from threading import Thread
 
 import pytest
 
-import demistomock as demisto
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
 
 RETURN_ERROR_TARGET = 'UrlScan.return_error'
 SCAN_URL = 'https://urlscan.io/api/v1/scan/'
@@ -60,3 +61,40 @@ def test_endless_loop_on_failed_response(requests_mock, mocker):
     thread.start()
     time.sleep(10)
     assert not thread.is_alive(), 'format_results method have probably entered an endless loop'
+
+
+def test_urlscan_submit_url(requests_mock, mocker):
+    """
+    Given
+    - Two URLs which are rate limited
+    When
+    - running the !url command
+    Then
+    - Assert the items are scheduled and the metrics are correct.
+    """
+    from UrlScan import urlscan_submit_command, Client
+    import CommonServerPython
+
+    response_json = {
+        'is_error': True
+    }
+    args = {
+        'url': 'https://something.com,https://somethingelse.com'
+    }
+    requests_mock.post(SCAN_URL, status_code=429, json=response_json, headers={'X-Rate-Limit-Reset-After': '123'})
+    mocker.patch.object(demisto, 'args', return_value=args)
+    mocker.patch.object(CommonServerPython, 'is_demisto_version_ge', return_value=True)
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported')
+
+    client = Client()
+
+    response = urlscan_submit_command(client=client)
+    scheduled_command = response[0].scheduled_command
+    scheduled_command_args = scheduled_command._args
+    assert scheduled_command_args['polling'] is True
+    assert scheduled_command_args['url'] == ['https://something.com', 'https://somethingelse.com']
+    assert scheduled_command._next_run == '123'
+    assert scheduled_command._items_remaining == 2
+
+    metrics = response[1]
+    assert metrics.execution_metrics == [{'Type': 'QuotaError', 'APICallsCount': 2}]
