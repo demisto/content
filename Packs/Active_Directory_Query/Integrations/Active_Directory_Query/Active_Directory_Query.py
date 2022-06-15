9284f44d68a7b1cb744c9d8109e45070ab1cd009
Abel Santamarina <asantamarina@paloaltonetworks.com>
2022-06-14 20:18:24 -0700

    Updating latest files

--- Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query.py
+++ Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query.py
@@ -14,6 +14,9 @@ from ldap3.utils.log import (EXTENDED, get_library_log_detail_level,
                              set_library_log_detail_level,
                              set_library_log_hide_sensitive_data)
 
+CIPHERS_STRING = '@SECLEVEL=1:ECDHE+AESGCM:ECDHE+CHACHA20:DHE+AESGCM:DHE+CHACHA20:ECDH+AESGCM:DH+AESGCM:ECDH+AES:DH+AES:' \
+                     'RSA+ANESGCM:RSA+AES:!aNULL:!eNULL:!MD5:!DSS'
+
 # global connection
 conn: Optional[Connection] = None
 
@@ -30,7 +33,6 @@ COMMON_ACCOUNT_CONTROL_FLAGS = {
     514: "Disabled account",
     544: "Password Not Required",
     4096: "Workstation/server",
-    65536: "Password never expires",
     66048: "Enabled, password never expires",
     66050: "Disabled, password never expires",
     66080: "Enables, password never expires, password not required.",
@@ -85,7 +87,11 @@ def initialize_server(host, port, secure_connection, unsecure):
 
     if secure_connection == "TLS":
         demisto.debug(f"initializing sever with TLS (unsecure: {unsecure}). port: {port or 'default(636)'}")
-        tls = Tls(validate=ssl.CERT_NONE)
+        if unsecure:
+            # Add support for all CIPHER_STRING
+            tls = Tls(validate=ssl.CERT_NONE, ciphers=CIPHERS_STRING)
+        else:
+            tls = Tls(validate=ssl.CERT_NONE)
         if port:
             return Server(host, port=port, use_ssl=unsecure, tls=tls)
         return Server(host, use_ssl=unsecure, tls=tls)
@@ -94,15 +100,19 @@ def initialize_server(host, port, secure_connection, unsecure):
         # intialize server with ssl
         # port is configured by default as 389 or as 636 for LDAPS if not specified in configuration
         demisto.debug(f"initializing sever with SSL (unsecure: {unsecure}). port: {port or 'default(636)'}")
+        # If Trust any certificate is false 
         if not unsecure:
             demisto.debug("will require server certificate.")
             tls = Tls(validate=ssl.CERT_REQUIRED, ca_certs_file=os.environ.get('SSL_CERT_FILE'))
             if port:
                 return Server(host, port=port, use_ssl=True, tls=tls)
             return Server(host, use_ssl=True, tls=tls)
-        if port:
-            return Server(host, port=port, use_ssl=True)
-        return Server(host, use_ssl=True)
+        else:
+            # Add support for all CIPHER_STRING 
+            tls = Tls(ciphers=CIPHER_STRING) 
+            if port:
+                return Server(host, port=port, use_ssl=True, tls=tls)
+            return Server(host, use_ssl=True)
     demisto.debug(f"initializing server without secure connection. port: {port or 'default(389)'}")
     if port:
         return Server(host, port=port)
@@ -428,7 +438,7 @@ def search_with_paging(search_filter, search_base, attributes=None, page_size=10
         entries_left_to_fetch -= len(conn.entries)
         total_entries += len(conn.entries)
         cookie = dict_safe_get(conn.result, ['controls', '1.2.840.113556.1.4.319', 'value', 'cookie'])
-        time_diff = (start - datetime.now()).seconds
+        time_diff = (datetime.now() - start).seconds
 
         entries.extend(conn.entries)
 
@@ -1296,18 +1306,68 @@ def set_user_password(default_base_dn, port):
     }
     demisto.results(demisto_entry)
 
+    
+def restore_user(default_base_dn: str, page_size: int) -> int:
+    """
+         Restore the user UserAccountControl flags.
+         Args:
+             default_base_dn (str): The default base dn.
+             page_size (int): The page size to query.
+         Returns:
+             flags (int): The UserAccountControl flags.
+     """ 
+    args = demisto.args()
+
+    # default query - list all users
+    query = "(&(objectClass=User)(objectCategory=person))"
+
+    # query by sAMAccountName
+    if args.get('username') or args.get('sAMAccountName'):
+        if args.get('username'):
+            username = escape_filter_chars(args['username'])
+        else:
+            username = escape_filter_chars(args['sAMAccountName'])
+        query = "(&(objectClass=User)(objectCategory=person)(sAMAccountName={}))".format(username)
+
+    attributes = list(set(DEFAULT_PERSON_ATTRIBUTES))
+
+    entries = search_with_paging(
+        query,
+        default_base_dn,
+        attributes=attributes,
+        size_limit=0,
+        page_size=page_size
+    )
+    if entries.get('flat'):
+        return entries.get('flat')[0].get('userAccountControl')[0]
+    return 0
+
+
+def turn_disable_flag_off(flags: int) -> int:
+    """
+        Turn off the ACCOUNTDISABLE flag in UserAccountControl flags.
+        https://docs.microsoft.com/en-US/troubleshoot/windows-server/identity/useraccountcontrol-manipulate-account-properties
+         Args:
+             flags (int): The UserAccountControl flags to update.
+         Returns:
+             flags (int): The UserAccountControl flags with the ACCOUNTDISABLE turned off.
+     """
+    return flags & ~(1 << (2 - 1))
 
 def enable_user(default_base_dn):
     args = demisto.args()
-
+    account_options = NORMAL_ACCOUNT
     # get user DN
     sam_account_name = args.get('username')
     search_base = args.get('base-dn') or default_base_dn
     dn = user_dn(sam_account_name, search_base)
+    
+    if args.get('restore_user'):
+        account_options = restore_user(search_base, default_page_size)
 
     # modify user
     modification = {
-        'userAccountControl': [('MODIFY_REPLACE', NORMAL_ACCOUNT)]
+        'userAccountControl': [('MODIFY_REPLACE', turn_disable_flag_off(account_options))]
     }
     modify_object(dn, modification)
 
@@ -1319,7 +1379,7 @@ def enable_user(default_base_dn):
     demisto.results(demisto_entry)
 
 
-def disable_user(default_base_dn):
+def disable_user(default_base_dn, default_page_size):
     args = demisto.args()
 
     # get user DN
@@ -1329,7 +1389,7 @@ def disable_user(default_base_dn):
 
     # modify user
     modification = {
-        'userAccountControl': [('MODIFY_REPLACE', DISABLED_ACCOUNT)]
+        'userAccountControl': [('MODIFY_REPLACE', (account_options | DISABLED_ACCOUNT))]
     }
     modify_object(dn, modification)
 
@@ -1727,10 +1787,10 @@ def main():
             unlock_account(DEFAULT_BASE_DN)
 
         if command == 'ad-disable-account':
-            disable_user(DEFAULT_BASE_DN)
+            disable_user(DEFAULT_BASE_DN, DEFAULT_PAGE_SIZE)
 
         if command == 'ad-enable-account':
-            enable_user(DEFAULT_BASE_DN)
+            enable_user(DEFAULT_BASE_DN, DEFAULT_PAGE_SIZE)
 
         if command == 'ad-remove-from-group':
             remove_member_from_group(DEFAULT_BASE_DN)
