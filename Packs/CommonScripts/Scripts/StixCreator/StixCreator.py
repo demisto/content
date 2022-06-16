cbdc325efdc3f533b134d059e69a8748a601be7b
Ni-Knight <davrahami@paloaltonetwors.com>
2022-06-16 11:54:45 +0300

    Added main function and switched to use `CommandResults` and `return_results`

--- Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
+++ Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
@@ -11,141 +11,147 @@ from stix2 import Bundle, ExternalReference, Indicator, Vulnerability
 from stix2 import AttackPattern, Campaign, Malware, Infrastructure, IntrusionSet, Report, ThreatActor
 from stix2 import Tool, CourseOfAction
 
-SCOs = {
-    "file md5": "[file:hashes.md5 ='{}']",
-    "file sha1": "[file:hashes.sha1 = '{}']",
-    "file sha256": "[file:hashes.sha256 = '{}']",
-    "ssdeep": "[file:hashes.ssdeep = '']",
-    "ip": "[ipv4-addr:value = '{}']",
-    "cidr": "[ipv4-addr:value = '{}']",
-    "ipv6": "[ipv6-addr:value = '{}']",
-    "ipv6cidr": "[ipv6-addr:value = '{}']",
-    "url": "[url:value = '{}']",
-    "email": "[email-message:sender_ref.value = '{}']",
-    "username": "[user-account:account_login = '{}']",
-    "domain": "[domain-name:value = '{}']",
-    "hostname": "[domain-name:value = '{}']",
-    "registry key": "[windows-registry-key:key = '{}']"
-}
 
-SDOs = {
-    "malware": Malware,
-    "attack pattern": AttackPattern,
-    "campaign": Campaign,
-    "infrastructure": Infrastructure,
-    "tool": Tool,
-    "intrusion set": IntrusionSet,
-    "report": Report,
-    "threat actor": ThreatActor,
-    "cve": Vulnerability,
-    "course of action": CourseOfAction
-}
+def extract_stix(all_args, doubleBackslash):
+    SCOs = {
+        "file md5": "[file:hashes.md5 ='{}']",
+        "file sha1": "[file:hashes.sha1 = '{}']",
+        "file sha256": "[file:hashes.sha256 = '{}']",
+        "ssdeep": "[file:hashes.ssdeep = '']",
+        "ip": "[ipv4-addr:value = '{}']",
+        "cidr": "[ipv4-addr:value = '{}']",
+        "ipv6": "[ipv6-addr:value = '{}']",
+        "ipv6cidr": "[ipv6-addr:value = '{}']",
+        "url": "[url:value = '{}']",
+        "email": "[email-message:sender_ref.value = '{}']",
+        "username": "[user-account:account_login = '{}']",
+        "domain": "[domain-name:value = '{}']",
+        "hostname": "[domain-name:value = '{}']",
+        "registry key": "[windows-registry-key:key = '{}']"
+    }
 
-indicators = []
+    SDOs = {
+        "malware": Malware,
+        "attack pattern": AttackPattern,
+        "campaign": Campaign,
+        "infrastructure": Infrastructure,
+        "tool": Tool,
+        "intrusion set": IntrusionSet,
+        "report": Report,
+        "threat actor": ThreatActor,
+        "cve": Vulnerability,
+        "course of action": CourseOfAction
+    }
 
+    indicators = []
 
-userArgs = demisto.args().get('indicators', 'Unknown')
-doubleBackslash = demisto.args().get('doubleBackslash', True)
-all_args = {}
+    for indicator_fields in all_args:
+        kwargs = {"allow_custom": True}
 
-if isinstance(userArgs, dict):
-    all_args = json.loads(json.dumps(userArgs))
+        demisto_indicator_type = all_args[indicator_fields].get('indicator_type', 'Unknown')
 
-else:
-    try:
-        all_args = json.loads(demisto.args().get('indicators', 'Unknown'))
-    except:     # noqa: E722
-        return_error('indicators argument is invalid json object')
+        if doubleBackslash:
+            value = all_args[indicator_fields].get('value', '').replace('\\', r'\\')
+        else:
+            value = all_args[indicator_fields].get('value', '')
 
-for indicator_fields in all_args:
-    kwargs = {"allow_custom": True}
+        demisto_score = all_args[indicator_fields].get('score', '').lower()
 
-    demisto_indicator_type = all_args[indicator_fields].get('indicator_type', 'Unknown')
+        if demisto_score in ["bad", "malicious"]:
+            kwargs["score"] = "High"
 
-    if doubleBackslash:
-        value = all_args[indicator_fields].get('value', '').replace('\\', r'\\')
-    else:
-        value = all_args[indicator_fields].get('value', '')
+        elif demisto_score == "suspicious":
+            kwargs["score"] = "Medium"
 
-    demisto_score = all_args[indicator_fields].get('score', '').lower()
+        elif demisto_score in ["good", "benign"]:
+            kwargs["score"] = "None"
 
-    if demisto_score in ["bad", "malicious"]:
-        kwargs["score"] = "High"
+        else:
+            kwargs["score"] = "Not Specified"
 
-    elif demisto_score == "suspicious":
-        kwargs["score"] = "Medium"
+        kwargs["created"] = dateparser.parse(all_args[indicator_fields].get('timestamp', ''))
+        kwargs["modified"] = dateparser.parse(all_args[indicator_fields].get('lastSeen', f'{kwargs["created"]}'))
+        kwargs["id"] = all_args[indicator_fields].get('stixid', '')
+        kwargs["labels"] = [demisto_indicator_type.lower()]
+        kwargs["description"] = all_args[indicator_fields].get('description', '')
 
-    elif demisto_score in ["good", "benign"]:
-        kwargs["score"] = "None"
+        kwargs = {k: v for k, v in kwargs.items() if v}  # Removing keys with empty strings
 
-    else:
-        kwargs["score"] = "Not Specified"
-
-    kwargs["created"] = dateparser.parse(all_args[indicator_fields].get('timestamp', ''))
-    kwargs["modified"] = dateparser.parse(all_args[indicator_fields].get('lastSeen', f'{kwargs["created"]}'))
-    kwargs["id"] = all_args[indicator_fields].get('stixid', '')
-    kwargs["labels"] = [demisto_indicator_type.lower()]
-    kwargs["description"] = all_args[indicator_fields].get('description', '')
-
-    kwargs = {k: v for k, v in kwargs.items() if v}  # Removing keys with empty strings
-
-    try:
-        indicator_type = demisto_indicator_type.lower().replace("-", "")
-        indicator = Indicator(pattern=SCOs[indicator_type].format(value),
-                              pattern_type='stix',
-                              **kwargs)
-
-        indicators.append(indicator)
-
-    except KeyError:
         try:
-            indicator_type = demisto_indicator_type.lower()
-
-            if indicator_type == 'cve':
-                kwargs["external_references"] = [ExternalReference(source_name="cve", external_id=value)]
-
-            elif indicator_type == "attack pattern":
-                try:
-                    mitreid = all_args[indicator_fields].get('mitreid', '')
-                    if mitreid:
-                        kwargs["external_references"] = [ExternalReference(source_name="mitre", external_id=mitreid)]
-
-                except KeyError:
-                    pass
-
-            indicator = SDOs.get(indicator_type)(
-                name=value,
-                **kwargs
-            )
+            indicator_type = demisto_indicator_type.lower().replace("-", "")
+            indicator = Indicator(pattern=SCOs[indicator_type].format(value),
+                                  pattern_type='stix',
+                                  **kwargs)
 
             indicators.append(indicator)
 
-        except (KeyError, TypeError) as e:
-            demisto.info(
-                "Indicator type: {}, with the value: {} is not STIX compatible".format(demisto_indicator_type, value))
-            demisto.info("Export failure excpetion: {}".format(e))
-            continue
+        except KeyError:
+            try:
+                indicator_type = demisto_indicator_type.lower()
+
+                if indicator_type == 'cve':
+                    kwargs["external_references"] = [ExternalReference(source_name="cve", external_id=value)]
+
+                elif indicator_type == "attack pattern":
+                    try:
+                        mitreid = all_args[indicator_fields].get('mitreid', '')
+                        if mitreid:
+                            kwargs["external_references"] = [ExternalReference(source_name="mitre", external_id=mitreid)]
+
+                    except KeyError:
+                        pass
+
+                indicator = SDOs.get(indicator_type)(
+                    name=value,
+                    **kwargs
+                )
+
+                indicators.append(indicator)
+
+            except (KeyError, TypeError) as e:
+                demisto.info(
+                    "Indicator type: {}, with the value: {} is not STIX compatible".format(demisto_indicator_type, value))
+                demisto.info("Export failure excpetion: {}".format(e))
+                continue
+
+    if len(indicators) > 1:
+        bundle = Bundle(indicators)
+        context = {
+            'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(bundle))
+        }
+        res = (CommandResults(readable_output="",
+                              outputs=context,
+                              raw_response=str(bundle)))
+
+    elif len(indicators) == 1:
+        context = {
+            'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(indicators[0]))
+        }
+        res = (CommandResults(readable_output="",
+                              outputs=context,
+                              raw_response=str(indicators[0])))
+    else:
+        context = {
+            'StixExportedIndicators': {}
+        }
+        res = CommandResults(readable_output="",
+                             outputs=context,
+                             raw_response={})
+
+    return_results(res)
 
 
-if len(indicators) > 1:
-    bundle = Bundle(indicators)
-    context = {
-        'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(bundle))
-    }
-    return_outputs(readable_output="",
-                   outputs=context,
-                   raw_response=str(bundle))
-elif len(indicators) == 1:
-    context = {
-        'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(indicators[0]))
-    }
-    return_outputs(readable_output="",
-                   outputs=context,
-                   raw_response=str(indicators[0]))
-else:
-    context = {
-        'StixExportedIndicators': {}
-    }
-    return_outputs(readable_output="",
-                   outputs=context,
-                   raw_response={})
+if __name__ in ('__builtin__', 'builtins', '__main__'):
+    user_args = demisto.args().get('indicators', 'Unknown')
+    double_backslash = demisto.args().get('doubleBackslash', True)
+
+    if isinstance(user_args, dict):
+        user_args = json.loads(json.dumps(user_args))
+
+    else:
+        try:
+            user_args = json.loads(demisto.args().get('indicators', 'Unknown'))
+        except:     # noqa: E722
+            return_error('indicators argument is invalid json object')
+
+    extract_stix(user_args, double_backslash)
