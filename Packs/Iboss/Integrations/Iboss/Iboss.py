93c3a00e603d11adef7f302f459d6d873b77b843
Mike Forgione <mvforgione@gmail.com>
2022-06-20 20:36:27 -0400

    Update Packs/Iboss/Integrations/Iboss/Iboss.py
    Co-authored-by: Noy-Maimon <72340690+Noy-Maimon@users.noreply.github.com>

--- Packs/Iboss/Integrations/Iboss/Iboss.py
+++ Packs/Iboss/Integrations/Iboss/Iboss.py
@@ -406,7 +406,6 @@ class Client(BaseClient):
             json_data=data
         )
         return result
-    # TODO: ADD HERE THE FUNCTIONS TO INTERACT WITH YOUR PRODUCT API
 
 
 ''' HELPER FUNCTIONS '''
