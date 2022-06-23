fde3e22f4655d5f2c3b8b80e800342653c34fdaa
MLainer1 <maylainer96@gmail.com>
2022-06-23 14:23:31 +0300

    revert Cryptocurrency,CircleCI,Censys

--- Packs/Censys/Integrations/CensysV2/CensysV2.py
+++ Packs/Censys/Integrations/CensysV2/CensysV2.py
@@ -3,6 +3,7 @@ from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-impor
 from CommonServerUserPython import *  # noqa
 
 import requests
+import traceback
 from typing import Dict, Any
 
 # Disable insecure warnings
@@ -201,6 +202,7 @@ def main() -> None:
 
     # Log exceptions and return errors
     except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
         return_error(f'Failed to execute {command} command.\nError:\n{str(e)}')
 
 
