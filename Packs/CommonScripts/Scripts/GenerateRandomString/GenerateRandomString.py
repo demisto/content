0435f1e20a2d9696fa88679dfce52b353b92689b
BEAdi <72088126+BEAdi@users.noreply.github.com>
2022-06-23 15:02:19 +0300

    refactor

--- Packs/CommonScripts/Scripts/GenerateRandomString/GenerateRandomString.py
+++ Packs/CommonScripts/Scripts/GenerateRandomString/GenerateRandomString.py
@@ -1,7 +1,5 @@
-import json
 import random
 import string
-import time
 
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
@@ -9,59 +7,53 @@ from CommonServerPython import *  # noqa: F401
 MAX_LENGTH = 10000
 
 
-def create_entry(title, data, ec):
-    return {
-        'ContentsFormat': formats['json'],
-        'Type': entryTypes['note'],
-        'Contents': data,
-        'ReadableContentsFormat': formats['markdown'],
-        'HumanReadable': tableToMarkdown(title, data) if data else 'No result were found',
-        'EntryContext': ec
-    }
+def set_length(length):
+    if length <= 0:
+        return_error("Length must be greater than 0. Maximum value is {}.".format(MAX_LENGTH))
+
+    return min(length, MAX_LENGTH)
 
 
-def create_error_entry(error):
-    return {
-        'Type': entryTypes['error'],
-        'ContentsFormat': formats['text'],
-        'Contents': str(error)
-    }
+def set_characters(characters: str, digits: bool, lowercase: bool, punctuation: bool, uppercase: bool):
+    if not any([uppercase, lowercase, digits, punctuation]):
+        return_error("Punctuation, Digits, Uppercase or Lowercase must be True.")
+
+    characters += string.ascii_lowercase if lowercase else ''
+    characters += string.ascii_uppercase if uppercase else ''
+    characters += string.digits if digits else ''
+    characters += string.punctuation if punctuation else ''
+    return characters
 
 
-length = int(demisto.args()["Length"])
-punctuation = demisto.args()["Punctuation"]
-lowercase = demisto.args()["Lowercase"]
-uppercase = demisto.args()["Uppercase"]
-digits = demisto.args()["Digits"]
+def create_password(characters, length):
+    password = ""
+    for x in range(0, length):
+        password += random.SystemRandom(random.seed(time.time())).choice(characters)  # type: ignore
 
-characters = ""
-length = min(length, MAX_LENGTH)
-if length > 0:
-    if uppercase == "True" or lowercase == "True" or digits == "True" or punctuation == "True":
+    entry_context = {'RandomString': password}
+    raw = json.loads(json.dumps(entry_context))
+    results = CommandResults(content_format=EntryFormat.JSON,
+                             entry_type=EntryType.NOTE,
+                             outputs=entry_context,
+                             readable_output=tableToMarkdown('RandomString Generated.', raw) if raw else 'No result were found',
+                             raw_response=raw)
+    return results
 
-        if lowercase == "True":
-            characters += string.ascii_lowercase
 
-        if uppercase == "True":
-            characters += string.ascii_uppercase
+def main():
+    args = demisto.args()
 
-        if digits == "True":
-            characters += string.digits
+    punctuation = argToBoolean(args["Punctuation"])
+    lowercase = argToBoolean(args["Lowercase"])
+    uppercase = argToBoolean(args["Uppercase"])
+    digits = argToBoolean(args["Digits"])
+    length = set_length(arg_to_number(args["Length"], required=True))
 
-        if punctuation == "True":
-            characters += string.punctuation
+    characters = set_characters("", digits, lowercase, punctuation, uppercase)
 
-    else:
-        demisto.results(create_error_entry("Punctuation,Digits,Uppercase or Lowercase must be True."))
-        sys.exit(0)
-else:
-    demisto.results(create_error_entry("Length must be greater than 0. Maximum value is {}.".format(MAX_LENGTH)))
-    sys.exit(0)
+    results = create_password(characters, length)
+    return_results(results)
 
-password = ""
-for x in range(0, length):
-    password += random.SystemRandom(random.seed(time.time())).choice(characters)  # type: ignore
 
-ec = {'RandomString': password}
-raw = json.loads(json.dumps(ec))
-demisto.results(create_entry('RandomString Generated.', raw, ec))
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
