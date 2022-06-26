f18e90d7b54a84e7a7a6611c82749a0bc5ba2895
oauth2 <ypreisler@paloaltonetworks.com>
2022-06-26 18:59:05 +0300

    revert

--- Tests/Marketplace/marketplace_statistics.py
+++ Tests/Marketplace/marketplace_statistics.py
@@ -3,8 +3,8 @@ import warnings
 import google.auth
 from pprint import pformat
 from pandas.core.frame import DataFrame, Series
-# from google.cloud.bigquery.client import Client
-# from google.cloud import bigquery
+from google.cloud.bigquery.client import Client
+from google.cloud import bigquery
 from datetime import timedelta
 from typing import List, Dict
 
