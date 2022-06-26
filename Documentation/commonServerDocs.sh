c0a51dead75cde74eccda57447692d768c9133cb
aradcarmi <aradcarmi220@gmail.com>
2022-06-26 18:14:39 +0300

    Test

--- Documentation/commonServerDocs.sh
+++ Documentation/commonServerDocs.sh
@@ -1,5 +1,3 @@
 python ./Documentation/extract_common_server_js.py $ARTIFACTS_FOLDER/env_results.json
 jsdoc2md --json ./Documentation/commonServerJsDoc.js > ./Documentation/commonServerJsDoc.json
-TXT_RED="\e[31m" && TXT_CLEAR="\e[0m"
-echo -e "${TXT_RED}This text is red2,${TXT_CLEAR} but this part isn't${TXT_RED} however this part is again."
 python3 ./Documentation/common_server_docs.py
---
+++ Tests/scripts/Test2.py
@@ -0,0 +1,15 @@
+import logging
+import os
+
+from Tests.scripts.utils.log_util import install_logging
+
+
+def main():
+    os.
+    install_logging('Test_instances.log')
+    logging.info("Info")
+    logging.warning("Warning")
+    logging.error("Error")
+
+if __name__ == "__main__":
+    main()
