4c16aca4881d4700a83448d9d5ba79b6f6e29cac
israelpolishook <ipolishuk@paloaltonetworks.com>
2022-06-28 14:55:05 +0300

    commit

--- Packs/AWS-SQS/Integrations/AWS-SQS/AWS-SQS.py
+++ Packs/AWS-SQS/Integrations/AWS-SQS/AWS-SQS.py
@@ -157,7 +157,8 @@ def fetch_incidents(aws_client, aws_queue_url, max_fetch, parse_body_as_json):
         client = aws_client.aws_session(service='sqs')
         last_receipt_handles = demisto.getLastRun().get('lastReceiptHandles')
         if last_receipt_handles:
-            demisto.debug('last_receipt_handles before fetch occurred" -> {} {}'.format(len(last_receipt_handles), last_receipt_handles))
+            demisto.debug('last_receipt_handles before fetch occurred" -> {} {}'.format(len(last_receipt_handles),
+                                                                                            last_receipt_handles))
         incidents_created = 0  # type: int
         max_number_of_messages = min(max_fetch, 10)
         receipt_handles = set()  # type: set
