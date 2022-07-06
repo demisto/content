a69d866900eb33a142733284e484aa235dfec723
ilappe <ilappe@paloaltonetworks.com>
2022-07-06 15:39:00 +0300

    reuse cached token

--- Packs/ApiModules/Scripts/CrowdStrikeApiModule/CrowdStrikeApiModule_test.py
+++ Packs/ApiModules/Scripts/CrowdStrikeApiModule/CrowdStrikeApiModule_test.py
@@ -2,6 +2,9 @@ from CrowdStrikeApiModule import CrowdStrikeClient
 from TestsInput.http_responses import MULTI_ERRORS_HTTP_RESPONSE, NO_ERRORS_HTTP_RESPONSE
 from TestsInput.context import MULTIPLE_ERRORS_RESULT
 import pytest
+import demistomock as demisto
+from datetime import datetime
+from datetime import timedelta
 
 
 class ResMocker:
@@ -45,3 +48,22 @@ def test_handle_errors(http_response, output, mocker):
         _, output, _ = client.check_quota_status()
     except Exception as e:
         assert (str(e) == str(output))
+
+
+@pytest.mark.parametrize(argnames="context, expected_call_count", argvalues=[
+    ({}, 1),
+    ({'generation_time': datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), 'auth_token': 'test'}, 0),
+    ({'generation_time': (datetime.now() - timedelta(minutes=30)).strftime("%Y-%m-%dT%H:%M:%S")}, 1),
+])
+def test_get_token(mocker, context, expected_call_count):
+    """
+    Given - varios token generation time
+    When - try to get access token when init the client
+    Then - validate that token was generated only of required (after 28 min)
+    """
+    mocker.patch.object(demisto, 'getIntegrationContext', return_value=context)
+    mocker.patch.object(CrowdStrikeClient, '_generate_token', return_value="test_token_generated")
+
+    CrowdStrikeClient({})
+
+    assert CrowdStrikeClient._generate_token.call_count == expected_call_count
