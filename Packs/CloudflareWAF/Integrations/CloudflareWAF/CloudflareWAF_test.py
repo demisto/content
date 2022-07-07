40768d8591ae7f12ee2742c97034b6d17c9e7576
TalGumi <talg@qmasters.co>
2022-07-07 13:59:10 +0300

    Fix according CR

--- Packs/CloudflareWAF/Integrations/CloudflareWAF/CloudflareWAF_test.py
+++ Packs/CloudflareWAF/Integrations/CloudflareWAF/CloudflareWAF_test.py
@@ -373,7 +373,8 @@ def test_cloudflare_waf_ip_lists_list_command(requests_mock, mock_client):
     url = f'{BASE_URL}accounts/{ACCOUNT_ID}/rules/lists'
     requests_mock.get(url=url, json=mock_response)
 
-    result = cloudflare_waf_ip_lists_list_command(mock_client, {'limit': 5, 'item_ip': '120.2.2.8'})
+    result = cloudflare_waf_ip_lists_list_command(
+        mock_client, {'page': 1, 'page_size': 5, 'list_id': '123', 'item_ip': '120.2.2.8'})
 
     assert result.outputs_prefix == 'CloudflareWAF.IpList'
     assert len(result.outputs) == 2
