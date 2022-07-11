37984cbf7f802d1d31c04cffabfe11fa93f1c32a
Bishal Rai <bishal.rai@digitalshadows.com>
2022-07-11 10:05:15 +0100

    Fix flake 8 indentation error

--- Packs/DigitalShadows/Scripts/DsSearchQueryArray/DsSearchQueryTestData.py
+++ Packs/DigitalShadows/Scripts/DsSearchQueryArray/DsSearchQueryTestData.py
@@ -9,12 +9,12 @@ TEST_DATA_URL = {
             {"Data": "http://portal-digitalshadows.com.still.valid/learn/risk-profiles/MARKED_DOCUMENT"},
             {"Data": "https://http://microsoft365cpmsetup.com/triage/alerts/BGWZC"},
             {
-                "Data": "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb" +
-                        "-e0fd72944a11"
+                "Data": "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb"
+                        + "-e0fd72944a11"
             },
             {
-                "Data": "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb" +
-                        "-e0fd72944a11"
+                "Data": "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb"
+                        + "-e0fd72944a11"
             },
             {
                 "Data": "https://portal-digitalshadows.com/learn/risk-profiles/MARKED_DOCUMENT"
@@ -23,11 +23,11 @@ TEST_DATA_URL = {
     },
 }
 TEST_DATA_URL_EXPECTED = [
-    "http://microsoft365cpmsetup.com OR https://http://microsoft365cpmsetup.com/triage/alerts/BGWZC OR " +
-    "https://http://GermDetectives.com/learn/risk-profiles/IMPERSONATING_DOMAIN OR " +
-    "http://portal-digitalshadows.com.still.valid/learn/risk-profiles/MARKED_DOCUMENT",
-    "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb-e0fd72944a11 OR " +
-    "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb-e0fd72944a11",
+    "http://microsoft365cpmsetup.com OR https://http://microsoft365cpmsetup.com/triage/alerts/BGWZC OR "
+    + "https://http://GermDetectives.com/learn/risk-profiles/IMPERSONATING_DOMAIN OR "
+    + "http://portal-digitalshadows.com.still.valid/learn/risk-profiles/MARKED_DOCUMENT",
+    "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb-e0fd72944a11 OR "
+    + "https://http://microsoft365cpmsetup.com/api/external/resources/2de38dac-bba5-46c9-9ceb-e0fd72944a11",
 ]
 
 TEST_DATA_URL_SINGLE = {
@@ -87,8 +87,8 @@ TEST_DATA_DOMAIN = {
     },
 }
 TEST_DATA_DOMAIN_EXPECTED = [
-    "GermDetectives.com OR TutorialFriend.com OR LessIsFull.com OR SaveJam.com OR SaveItDay.com OR " +
-    "portal-digitalshadows.com.still.valid"
+    "GermDetectives.com OR TutorialFriend.com OR LessIsFull.com OR SaveJam.com OR SaveItDay.com OR "
+    + "portal-digitalshadows.com.still.valid"
 ]
 TEST_DATA_DOMAIN_SINGLE = {
     "field": "Domain.Name",
@@ -111,15 +111,14 @@ TEST_DATA_FILE_SHA1 = {
     },
 }
 TEST_DATA_FILE_SHA1_EXPECTED = [
-    "fd340743293eae593da4796e868ce57dfabf4147 OR fd340743293eae593da4796e868ce57dfabf4147 OR " +
-    "4eeff478822995573f4a5c45e746f01f93e23f28"
+    "fd340743293eae593da4796e868ce57dfabf4147 OR fd340743293eae593da4796e868ce57dfabf4147 OR "
+    + "4eeff478822995573f4a5c45e746f01f93e23f28"
 ]
 TEST_DATA_FILE_SHA1_SINGLE = {
     "field": "File.SHA1",
     "value": {"File": {"SHA1": "fd340743293eae593da4796e868ce57dfabf4147"}},
 }
 TEST_DATA_FILE_SHA1_SINGLE_EXPECTED = "fd340743293eae593da4796e868ce57dfabf4147"
-
 
 TEST_DATA_FILE_SHA256 = {
     "field": "File.SHA256",
@@ -138,8 +137,8 @@ TEST_DATA_FILE_SHA256 = {
     },
 }
 TEST_DATA_FILE_SHA256_EXPECTED = [
-    "57b0ed216fac388b6d99774d256fa5542d32cfdfe72a45b32fd7932ec154731d OR " +
-    "8dca20e35098837cc003b54f17688386aabd9a5640884f5e4397e755ca8bc606"
+    "57b0ed216fac388b6d99774d256fa5542d32cfdfe72a45b32fd7932ec154731d OR "
+    + "8dca20e35098837cc003b54f17688386aabd9a5640884f5e4397e755ca8bc606"
 ]
 TEST_DATA_FILE_SHA256_SINGLE = {
     "field": "File.SHA256",
