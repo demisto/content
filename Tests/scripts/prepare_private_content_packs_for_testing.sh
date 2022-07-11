daa4e910fab007ea902a4e7b474bd80c63843ab0
yaakovi <syaakovi@paloaltonetworks.com>
2022-07-11 22:01:13 +0300

    testing v4

--- Tests/scripts/prepare_private_content_packs_for_testing.sh
+++ Tests/scripts/prepare_private_content_packs_for_testing.sh
@@ -62,6 +62,7 @@ else
     python3 ./Tests/private_build/upload_packs_private.py -b $GCS_TESTING_BUCKET -pb $GCS_PRIVATE_TESTING_BUCKET -a $PACK_ARTIFACTS -d $ARTIFACTS_FOLDER/packs_dependencies.json -e $EXTRACT_FOLDER -s $KF -n $GITHUB_RUN_NUMBER -p $NEW_PACK_NAME -sb $PUBLIC_TARGET_PATH -k $PACK_SIGN_KEY -rt false -nek $PACK_ENCRYPTION_KEY_NEW -bn $GIT_BRANCH -ek $PACK_ENCRYPTION_KEY -inf $IS_INFRA_BUILD -o
     NEW_EXTRACT_FOLDER_FOR_INDEX=$(mktemp -d)
     NEW_EXTRACT_FOLDER_FOR_ARTIFACTS=$(mktemp -d)
+    echo "\nbetween the two scripts\n"
     python3 ./Tests/Marketplace/prepare_public_index_for_private_testing.py -b $GCS_TESTING_BUCKET -pb $GCS_PRIVATE_TESTING_BUCKET -n $GITHUB_RUN_NUMBER -e $NEW_EXTRACT_FOLDER_FOR_INDEX -sb $PUBLIC_TARGET_PATH -s $KF -p $NEW_PACK_NAME -a $PACK_ARTIFACTS -ea $NEW_EXTRACT_FOLDER_FOR_ARTIFACTS -di private/dummy_index
     echo "Finished updating content packs successfully."
   fi
