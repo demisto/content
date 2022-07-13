08e795cc359ea44329bf7083bf80e73bf2fbee1c
binalyze-murat <murat.alagoz@binalyze.com>
2022-07-13 07:19:10 +0000

    Test file reviewed. 	modified:   Packs/Binalyze/Integrations/BinalyzeAIR/BinalyzeAIR_test.py

--- Packs/Binalyze/Integrations/BinalyzeAIR/BinalyzeAIR_test.py
+++ Packs/Binalyze/Integrations/BinalyzeAIR/BinalyzeAIR_test.py
@@ -65,56 +65,13 @@ def test_air_acquire_command(requests_mock: Any) -> None:
     assert mocked_command_result.outputs == expected_mocked_command_result
 
 
-def test_air_isolate_command_enable(requests_mock: Any) -> None:
+def test_air_isolate_command(requests_mock: Any) -> None:
     from BinalyzeAIR import Client, air_isolate_command
 
     args: Dict[str, Any] = {
         'endpoint': 'endpointhostname',
         'organization_id': 0,
         'isolation': True
-    }
-
-    mock_response: Dict[str, Any] = {
-        "success": True,
-        "result": [
-            {
-                "_id": "12345678-90ab-cdef-1234-567890abcdef",
-                "name": "Isolation 001",
-                "organizationId": 0
-            }
-        ],
-        "statusCode": 200,
-        "errors": []
-    }
-
-    headers: Dict[str, Any] = {
-        'Authorization': 'Bearer api_key',
-        'User-Agent': 'Binalyze AIR',
-        'Content-type': 'application/json',
-        'Accept-Charset': 'UTF-8'
-    }
-    client: Client = Client(
-        base_url=MOCK_URL,
-        verify=False,
-        headers=headers
-    )
-    expected_mocked_command_result: List[Dict[str, Any]] = mock_response
-    requests_mock.post('https://nonexistent-domain.com/api/public/endpoints/tasks/isolation', json=mock_response)
-
-    mocked_command_result: CommandResults = air_isolate_command(client, args)
-
-    assert mocked_command_result.outputs_prefix == 'Binalyze.Air.Isolate'
-    assert mocked_command_result.outputs_key_field == 'endpoint'
-    assert mocked_command_result.outputs == expected_mocked_command_result
-
-
-def test_air_isolate_command_disable(requests_mock: Any) -> None:
-    from BinalyzeAIR import Client, air_isolate_command
-
-    args: Dict[str, Any] = {
-        'endpoint': 'endpointhostname',
-        'organization_id': 0,
-        'isolation': False
     }
 
     mock_response: Dict[str, Any] = {
