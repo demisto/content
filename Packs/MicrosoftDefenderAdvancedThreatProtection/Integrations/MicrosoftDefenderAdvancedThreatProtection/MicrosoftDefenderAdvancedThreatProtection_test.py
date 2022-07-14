19e344a18e4732b1ad5567b1aa33fede857b6a11
Michał Soboszek <michal.soboszek@group.issworld.com>
2022-07-14 05:41:01 -0400

    Integration updated

--- Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection_test.py
+++ Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection_test.py
@@ -134,7 +134,7 @@ def test_offboard_machine_command(mocker):
     from MicrosoftDefenderAdvancedThreatProtection import offboard_machine_command
     mocker.patch.object(client_mocker, 'offboard_machine', return_value=MACHINE_OFFBOARD_API_RESPONSE)
     result = offboard_machine_command(client_mocker, {'machine_id':'9b898e79b0ed2173cc87577a158d1dba5f61d7a7', 'comment':'Testing Offboarding'})
-    print(f'RESULTS: {result.outputs}')
+    # print(f'RESULTS: {result.outputs}')
     assert result.outputs['MicrosoftATP.MachineAction(val.ID === obj.ID)'][0]['ID'] == '947a677a-a11a-4240-ab6q-91277e2386b9'
     assert result.outputs['MicrosoftATP.MachineAction(val.ID === obj.ID)'][0]['Status'] == 'Pending'
     assert result.outputs['MicrosoftATP.MachineAction(val.ID === obj.ID)'][0]['Type'] == 'Offboard'
