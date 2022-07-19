f550915519b6bfac4d0fc48c6cc50e4e4c376f3e
beauchompers <beauchompers@gmail.com>
2022-07-18 18:42:29 -0600

    adding error handling to CaseMgmtIncidentTypesDisplay, and updating docker image for scripts

--- Packs/CaseManagement-Generic/Scripts/CaseMgmtIncidentTypesDisplay/CaseMgmtIncidentTypesDisplay.py
+++ Packs/CaseManagement-Generic/Scripts/CaseMgmtIncidentTypesDisplay/CaseMgmtIncidentTypesDisplay.py
@@ -2,22 +2,28 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 
-# Requirements - Create an XSOAR List called IncidentTypesFromList a list of comma separated Incident Types
-# Example: Incident Type 1,Incident Type 2, Incident Type 3
-
 # check if this is a new Incident or not
 incident = demisto.incident().get("id")
 
 # if new Incident, the ID will be empty:
 if not incident:
+
     # get the XSOAR IncidentTypesFromList XSOAR List, and split on the comma
-    types_list = demisto.executeCommand("getList", {"listName": "IncidentTypesFromList"})[0]["Contents"].split(",")
+    types_list = demisto.executeCommand("getList", {"listName": "IncidentTypesFromList"})[0]["Contents"]
 
-    # strip whitespace
-    types_list = [x.strip() for x in types_list]
+    # check if the list exists, if not, display the default options.
+    if "Item not found" in types_list:
+        # do nothing, return the original values from the field
+        pass
+    else:
+        # split the Incident Types based on the comma
+        types_list = types_list.split(",")
 
-    # return the options to display to the user
-    return_results({'hidden': False, 'options': types_list})
+        # strip whitespace
+        types_list = [x.strip() for x in types_list]
+
+        # return the options to display to the user
+        return_results({'hidden': False, 'options': types_list})
 
 # if it's an existing Incident, prevent changing the type from the UI.
 else:
