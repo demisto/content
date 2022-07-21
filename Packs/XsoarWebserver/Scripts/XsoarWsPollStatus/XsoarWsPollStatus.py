6f13ba938720ca785b69d4321b47b29aadebee25
arnarayanan <76943923+arnarayanan@users.noreply.github.com>
2022-07-21 16:26:15 +0200

    Update XsoarWsPollStatus.py
    removed demisto.log

--- Packs/XsoarWebserver/Scripts/XsoarWsPollStatus/XsoarWsPollStatus.py
+++ Packs/XsoarWebserver/Scripts/XsoarWsPollStatus/XsoarWsPollStatus.py
@@ -4,7 +4,6 @@ timeout = int(demisto.args().get('timeout'))
 action_status = demisto.executeCommand("xsoar-ws-get-action-status", {"uuid": uuid})[0]['Contents']
 
 if not action_status["response_received"]:
-    demisto.log("no response")
     scheduled_command = ScheduledCommand(command="xsoar-ws-poll-status", next_run_in_seconds=next_run,
                                          args={'uuid': uuid, 'timebetweenruns': next_run, 'timeout': timeout},
                                          timeout_in_seconds=timeout)
