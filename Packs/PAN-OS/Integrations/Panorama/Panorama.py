9ac9e03e78a9db548e22623d9dbf8f2c24636c99
GuyAfik <guyafik11@gmail.com>
2022-07-22 11:29:36 +0300

    empty commit

--- Packs/PAN-OS/Integrations/Panorama/Panorama.py
+++ Packs/PAN-OS/Integrations/Panorama/Panorama.py
@@ -665,7 +665,7 @@ def panorama_commit(args):
 )
 def panorama_commit_command(args: dict):
     """
-    Commit any configuration in PAN-OS.
+    Commit any configuration in PAN-OS
     This function implements the 'pan-os-commit' command.
     Supports polling as well.
     """
