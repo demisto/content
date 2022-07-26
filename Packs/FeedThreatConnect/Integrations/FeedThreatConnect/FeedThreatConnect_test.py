c5c78082a76697ad4f1c5fe51ebc0f8e87c24922
gal-forer <gforer@paloaltonetworks.com>
2022-07-26 17:34:31 +0300

    Build

--- Packs/FeedThreatConnect/Integrations/FeedThreatConnect/FeedThreatConnect_test.py
+++ Packs/FeedThreatConnect/Integrations/FeedThreatConnect/FeedThreatConnect_test.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 import demistomock as demisto
-from Packs.FeedThreatConnect.Integrations.FeedThreatConnect.FeedThreatConnect import create_or_query
+from FeedThreatConnect import create_or_query, parse_indicator
 
 
 def load_json_file(path):
@@ -23,10 +23,12 @@ def test_calculate_dbot_score(threatconnect_score, dbot_score):
     assert calculate_dbot_score(threatconnect_score) == dbot_score
 
 
-def test_parse_indicator(datadir):
-    from FeedThreatConnect import parse_indicator
-
-    assert load_json_file(datadir['parsed_indicator.json']) == parse_indicator(load_json_file(datadir['indicators.json']))
+def test_parse_indicator():
+    data_dir = {
+        'parsed_indicator.json': './FeedThreatConnect_test/parsed_indicator.json',  # type: ignore # noqa
+        'indicators.json': './FeedThreatConnect_test/indicators.json'}  # type: ignore # noqa
+    assert load_json_file(data_dir['parsed_indicator.json']) == parse_indicator(
+        load_json_file(data_dir['indicators.json']))
 
 
 def test_parse_indicator_with_tlp(mocker, datadir):
