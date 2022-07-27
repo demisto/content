683f4ee7596bdabf9301e185fc9f172249f54957
rshunim <rshunim@paloaltonetworks.com>
2022-07-27 14:53:06 +0300

    check test playbook

--- Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
+++ Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
@@ -2624,6 +2624,7 @@ def main():
         connection_args['username'] = username
         connection_args['password'] = password
         connection_args['autologin'] = True
+        # just for new pr
 
     if use_requests_handler:
         handle_proxy()
