07909424f68217cc816b39507f0ec43548e61eb6
okarkkatz <okarkkatz@paloaltonetworks.com>
2022-07-27 13:12:01 +0300

    commit

--- StringSifter/Scripts/StringSifter.py
+++ StringSifter/Scripts/StringSifter.py
@@ -3,11 +3,8 @@ import os
 import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
-import subprocess
 from subprocess import Popen, PIPE
-from typing import Dict, Any
 import re
-import tempfile
 from pathlib import Path
 
 WORDS_TEMP_DIR = 'words_temp_file.txt'
@@ -28,7 +25,6 @@ def create_rank_strings_args(args: dict) -> list:
 
     return args_rank_strings
 
-
 def handle_words_as_string(string_input):
     seperated_words = re.split('\n| ', string_input)
     words_set = set()
@@ -41,30 +37,39 @@ def handle_words_as_string(string_input):
     p = Path(os.getcwd(), WORDS_TEMP_DIR)
     return str(p)
 
+def handle_user_input(entry_id, string_text, file_name):
+    if entry_id and string_text:
+        raise ValueError('Use only one of the parameters entryID or string_text')
+    if not entry_id and not string_text:
+        raise ValueError('One of entryID of string_text must be specified')
+
+    path = ''
+    if entry_id:
+        file_info = demisto.getFilePath(entry_id)
+        path = file_info['path']
+    else:
+        if not file_name:
+            raise ValueError('When passing the parameter "string_text" please also specify a file name')
+        path = handle_words_as_string(string_text)
+    return path
 
 def main():
     try:
         args = demisto.args()
         entry_id = args.get('entryID')
         string_text = args.get('string_text', '')
+        file_name = args.get('file_name', '')
 
-        if entry_id and string_text:
-            raise ValueError('Use only one of the parameters entryID or string_text')
-        if not entry_id and not string_text:
-            raise ValueError('One of entryID of string_text must be specified')
-
-        path = ''
-        if entry_id:
-            file_info = demisto.getFilePath(entry_id)
-            path = file_info['path']
-        else:
-            path = handle_words_as_string(string_text)
+        path = handle_user_input(entry_id, string_text, file_name)
 
         args_rank_strings = create_rank_strings_args(args)
         p1 = Popen(["flarestrings", path], stdout=PIPE)
         p2 = Popen(args_rank_strings, stdin=p1.stdout, stdout=PIPE)
         p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
         output = p2.communicate()[0]    # gets the stdout from the pipe.
+
+        if string_text:
+            os.remove(path)
 
         output_data = output.decode("utf-8")
         regex = r"(?P<rating>.*),(?P<word>.*)"
@@ -81,7 +86,8 @@ def main():
 
         readable = tableToMarkdown('Stringsifter word ranking based on their relevance for malware analysis.',
                                    words_rating_list)
-        return CommandResults(readable_output=readable, outputs=words_rating_list, outputs_prefix='Stringsifter')
+        outputs = {'File_name' : }
+        return CommandResults(readable_output=readable, outputs=words_rating_list, outputs_prefix='Stringsifter', )
     except Exception as e:
         return_error(f'The script failed with the following error:\n {e}')
 
