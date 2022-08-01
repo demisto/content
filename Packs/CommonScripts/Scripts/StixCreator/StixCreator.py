d63b1dbaed88a384fa64db317f2ddb44be117afd
yucohen <yucohen@paloaltonetworks.com>
2022-08-01 17:02:38 +0300

    file issue

--- Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
+++ Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
@@ -13,9 +13,9 @@ from stix2 import Tool, CourseOfAction
 from typing import Any, Callable
 
 SCOs: dict[str, str] = {
-    "file md5": "file:hashes.md5",
-    "file sha1": "file:hashes.sha1",
-    "file sha256": "file:hashes.sha256",
+    "md5": "file:hashes.md5",
+    "sha1": "file:hashes.sha1",
+    "sha256": "file:hashes.sha256",
     "ssdeep": "file:hashes.ssdeep",
     "ip": "ipv4-addr:value",
     "cidr": "ipv4-addr:value",
@@ -28,7 +28,6 @@ SCOs: dict[str, str] = {
     "hostname": "domain-name:value",
     "registry key": "windows-registry-key:key"
 }
-
 SDOs: dict[str, Callable] = {
     "malware": Malware,
     "attack pattern": AttackPattern,
@@ -43,95 +42,85 @@ SDOs: dict[str, Callable] = {
 }
 
 
-def main():
+def hash_type(value: str) -> str:
+    length = len(value)
+    if length == 32:
+        return 'md5'
+    if length == 40:
+        return 'sha1'
+    if length == 64 and ":" in value:
+        return 'ssdeep'
+    elif length == 64:
+        return 'sha256'
+    if length == 128:
+        return 'sha512'
 
+
+def main():
     user_args = demisto.args().get('indicators', 'Unknown')
     doubleBackslash = demisto.args().get('doubleBackslash', True)
     all_args = {}
-
     if isinstance(user_args, dict):
         all_args = json.loads(json.dumps(user_args))
-
     else:
         try:
             all_args = json.loads(demisto.args().get('indicators', 'Unknown'))
         except:     # noqa: E722
             return_error('indicators argument is invalid json object')
-
     indicators = []
-
     for indicator_fields in all_args:
         kwargs: dict[str, Any] = {"allow_custom": True}
-
         demisto_indicator_type = all_args[indicator_fields].get('indicator_type', 'Unknown')
-
         if doubleBackslash:
             value = all_args[indicator_fields].get('value', '').replace('\\', r'\\')
         else:
             value = all_args[indicator_fields].get('value', '')
-
         demisto_score = all_args[indicator_fields].get('score', '').lower()
-
         if demisto_score in ["bad", "malicious"]:
             kwargs["score"] = "High"
-
         elif demisto_score == "suspicious":
             kwargs["score"] = "Medium"
-
         elif demisto_score in ["good", "benign"]:
             kwargs["score"] = "None"
-
         else:
             kwargs["score"] = "Not Specified"
-
         kwargs["created"] = dateparser.parse(all_args[indicator_fields].get('timestamp', ''))
         kwargs["modified"] = dateparser.parse(all_args[indicator_fields].get('lastSeen', f'{kwargs["created"]}'))
         kwargs["id"] = all_args[indicator_fields].get('stixid', '')
         kwargs["labels"] = [demisto_indicator_type.lower()]
         kwargs["description"] = all_args[indicator_fields].get('description', '')
-
         kwargs = {k: v for k, v in kwargs.items() if v}  # Removing keys with empty strings
-
         try:
             indicator_type = demisto_indicator_type.lower().replace("-", "")
+            if indicator_type == 'file':
+                indicator_type = hash_type(value)
             indicator = Indicator(pattern=f"[{SCOs[indicator_type]} = '{value}']",
                                   pattern_type='stix',
                                   **kwargs)
-
             indicators.append(indicator)
-
         except KeyError:
-
             demisto.debug(f"{demisto_indicator_type} isn't an SCO checking other IOC types")
-
             try:
                 indicator_type = demisto_indicator_type.lower()
-
                 if indicator_type == 'cve':
                     kwargs["external_references"] = [ExternalReference(source_name="cve", external_id=value)]
-
                 elif indicator_type == "attack pattern":
                     try:
                         mitreid = all_args[indicator_fields].get('mitreid', '')
                         if mitreid:
                             kwargs["external_references"] = [ExternalReference(source_name="mitre", external_id=mitreid)]
-
                     except KeyError:
                         pass
-
                 indicator = SDOs[indicator_type](
                     name=value,
                     **kwargs
                 )
-
                 indicators.append(indicator)
-
             except (KeyError, TypeError) as e:
                 demisto.info(
                     "Indicator type: {}, with the value: {} is not STIX compatible".format(demisto_indicator_type, value))
                 demisto.info("Export failure excpetion: {}".format(e))
                 continue
-
     if len(indicators) > 1:
         bundle = Bundle(indicators, allow_custom=True)
         context = {
@@ -140,7 +129,6 @@ def main():
         res = (CommandResults(readable_output="",
                               outputs=context,
                               raw_response=str(bundle)))
-
     elif len(indicators) == 1:
         context = {
             'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(indicators[0]))
@@ -155,7 +143,6 @@ def main():
         res = CommandResults(readable_output="",
                              outputs=context,
                              raw_response={})
-
     return_results(res)
 
 
