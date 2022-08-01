2b6b7eff2988fe060d9c0abb4ca68635b3c49839
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-01 12:01:54 +0300

    mypy

--- Tests/scripts/collect_tests/collect_tests.py
+++ Tests/scripts/collect_tests/collect_tests.py
@@ -383,8 +383,7 @@ class BranchTestCollector(TestCollector):
                 FileType.MAPPER: (self.conf.incoming_mapper_to_test, CollectionReason.MAPPER_CHANGED),
                 FileType.CLASSIFIER: (self.conf.classifier_to_test, CollectionReason.CLASSIFIER_CHANGED),
             }[file_type]
-            # type: ignore[call-overload]
-            if not (tests := source.get(content_item)):
+            if not (tests := source.get(content_item)):  # type: ignore[call-overload]
                 reason = CollectionReason.NON_CODE_FILE_CHANGED
                 reason_description = f'no specific tests for {relative_path} were found'
         elif path.suffix == '.yml':
