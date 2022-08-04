12f0ef72d393212351329a8a89881af23702a015
saipriya <saipriya@metronlabs.com>
2022-08-04 11:29:16 +0530

    addedunittests

---
+++ Packs/Cybereason/Integrations/Cybereason/test_files/responses_raw_data.py
@@ -0,0 +1,631 @@
+malop_processes_raw_response={
+  "data": {
+    "resultIdToElementDataMap": {
+      "1589170329.-285868401975015536": {
+        "simpleValues": {
+          "elementDisplayName": {
+            "totalValues": 1,
+            "values": [
+              "bdata.bin"
+            ]
+          },
+          "imageFile.signatureVerified": {
+            "totalValues": 1,
+            "values": [
+              "false"
+            ]
+          },
+          "calculatedName": {
+            "totalValues": 1,
+            "values": [
+              "bdata.bin"
+            ]
+          },
+          "endTime": {
+            "totalValues": 1,
+            "values": [
+            ]
+          },
+          "imageFile.md5String": {
+            "totalValues": 1,
+            "values": [
+            ]
+          },
+          "imageFile.isSigned": {
+            "totalValues": 1,
+            "values": [
+              "false"
+            ]
+          },
+          "markedForPrevention": {
+            "totalValues": 1,
+            "values": [
+              "false"
+            ]
+          },
+          "commandLine": {
+            "totalValues": 1,
+            "values": [
+            ]
+          },
+          "isLiveProcess": {
+            "totalValues": 1,
+            "values": [
+              "false"
+            ]
+          },
+          "creationTime": {
+            "totalValues": 1,
+            "values": [
+            ]
+          },
+          "pid": {
+            "totalValues": 1,
+            "values": [
+              "5180"
+            ]
+          },
+          "executionPrevented": {
+            "totalValues": 1,
+            "values": [
+              "false"
+            ]
+          },
+          "isChainOfInjections": {
+            "totalValues": 1,
+            "values": [
+              "false"
+            ]
+          },
+          "imageFile.maliciousClassificationType": {
+            "totalValues": 1,
+            "values": [
+              "av_detected"
+            ]
+          },
+          "imageFile.correctedPath": {
+            "totalValues": 1,
+            "values": [
+            ]
+          },
+          "imageFile.productType": {
+            "totalValues": 1,
+            "values": [
+              "NONE"
+            ]
+          },
+          "imageFile.sha1String": {
+            "totalValues": 1,
+            "values": [
+              "f56238da9fbfa3864d443a85bb80743bd2415682"
+            ]
+          },
+          "nonExecutableExtensionEvidence": {
+            "totalValues": 1,
+            "values": [
+              "APPLICATION_DATA"
+            ]
+          }
+        },
+        "elementValues": {
+          "imageFileUnsignedEvidence": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "File",
+                "guid": "",
+                "name": "bdata.bin",
+                "hasSuspicions": "true",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "calculatedUser": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "User",
+                "guid": "",
+                "name": "",
+                "hasSuspicions": "false",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 0,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "malwareModuleEvidence": {
+            "totalValues": 1,
+            "elementValues": "null",
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "parentProcess": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "Process",
+                "guid": "",
+                "name": "",
+                "hasSuspicions": "true",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "malwareByHashReputation": {
+            "totalValues": 1,
+            "elementValues": "null",
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "loadedModules": {
+            "totalValues": 2,
+            "elementValues": "null",
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+
+          "imageFileUnsignedHasSignedVersionEvidence": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "File",
+                "guid": "",
+                "name": "bdata.bin",
+                "hasSuspicions": "true",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "self": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "Process",
+                "guid": "",
+                "name": "bdata.bin",
+                "hasSuspicions": "true",
+                "hasMalops": "true"
+              }
+            ],
+            "totalSuspicious": 1,
+            "totalMalicious": 1,
+            "guessedTotal": 0
+          },
+          "ownerMachine": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "Machine",
+                "guid": "",
+                "name": "",
+                "hasSuspicions": "false",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 0,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "imageFile": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "File",
+                "guid": "",
+                "name": "bdata.bin",
+                "hasSuspicions": "true",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "systemUserEvidence": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "User",
+                "guid": "",
+                "name": "",
+                "hasSuspicions": "false",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 0,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          },
+          "malwareEvidence": {
+            "totalValues": 1,
+            "elementValues": [
+              {
+                "elementType": "File",
+                "guid": "",
+                "name": "bdata.bin",
+                "hasSuspicions": "true",
+                "hasMalops": "false"
+              }
+            ],
+            "totalSuspicious": 1,
+            "totalMalicious": 0,
+            "guessedTotal": 0
+          }
+        },
+        "suspicions": {
+          "knownMalwareSuspicion": 4798274876499,
+          "malwareModuleSuspicion": 1301477329815
+        },
+        "filterData": {
+          "sortInGroupValue": "",
+          "groupByValue": "bdata.bin"
+        },
+        "isMalicious": "true",
+        "suspicionCount": 2,
+        "guidString": "",
+        "labelsIds": "null",
+        "malopPriority": "null",
+        "malicious": "true",
+        "suspect": "true"
+      }
+    },
+    "suspicionsMap": {
+      "knownMalwareSuspicion": {
+        "potentialEvidence": [
+          "malwareEvidence"
+        ],
+        "firstTimestamp": 1598888976499,
+        "totalSuspicions": 1
+      },
+      "malwareModuleSuspicion": {
+        "potentialEvidence": [
+          "malwareModuleEvidence"
+        ],
+        "firstTimestamp": 1601434569815,
+        "totalSuspicions": 1
+      }
+    },
+    "evidenceMap": {
+      "nonExecutableExtensionEvidence": 1,
+      "imageFileUnsignedEvidence": 1,
+      "malwareEvidence": 1,
+      "malwareModuleEvidence": 1,
+      "imageFileUnsignedHasSignedVersionEvidence": 1,
+      "systemUserEvidence": 1
+    },
+    "totalPossibleResults": 1,
+    "guessedPossibleResults": 0,
+    "queryLimits": {
+      "totalResultLimit": 1000,
+      "perGroupLimit": 1200,
+      "perFeatureLimit": 1200,
+      "groupingFeature": {
+        "elementInstanceType": "Process",
+        "featureName": "imageFileHash"
+      },
+      "sortInGroupFeature": "null"
+    },
+    "queryTerminated": "false",
+    "pathResultCounts": [
+      {
+        "featureDescriptor": {
+          "elementInstanceType": "MalopProcess",
+          "featureName": "null"
+        },
+        "count": 1
+      },
+      {
+        "featureDescriptor": {
+          "elementInstanceType": "MalopProcess",
+          "featureName": "suspects"
+        },
+        "count": 1
+      }
+    ],
+    "guids": [
+
+    ]
+  },
+  "status": "SUCCESS",
+  "hidePartialSuccess": "false",
+  "message": "",
+  "expectedResults": 1,
+  "failures": 0
+}
+
+is_probe_connected_raw_response={
+    "Cybereason": {
+        "Machine": {
+            "Name": "desktop-vg9ke2u",
+            "isConnected": "true"
+        }
+    }
+}
+query_processes_raw_response={
+    "Process": [
+        {
+            "Children": "null",
+            "CommandLine": "C:\\WINDOWS\\system32\\svchost.exe -k LocalService -s W32Time",
+            "CompanyName": "Microsoft Corporation",
+            "CreationTime": "2022-05-06T04:15:33.939000",
+            "EndTime": "",
+            "ImageFile": "svchost.exe",
+            "MD5": "cd10cb894be2128fca0bf0e2b0c27c16",
+            "Malicious": "indifferent",
+            "Name": "svchost.exe",
+            "OwnerMachine": "desktop-vg9ke2u",
+            "Parent": "services.exe",
+            "ProductName": "Microsoft\u00ae Windows\u00ae Operating System",
+            "ProductType": "SVCHOST",
+            "SHA1": "1f912d4bec338ef10b7c9f19976286f8acc4eb97",
+            "SignedandVerified": "true",
+            "User": "desktop-vg9ke2u\\local service"
+        }
+    ]
+}
+
+query_connections_raw_response={
+    "Connection": [
+        {
+            "CreationTime": "2021-04-20T03:38:56.386000",
+            "Direction": "OUTGOING",
+            "EndTime": "2021-04-20T03:40:04.466000",
+            "Name": "<connection_ip_addresses>",
+            "OwnerMachine": "siemplify-cyber",
+            "OwnerProcess": "nbtscan.exe",
+            "PortType": "SERVICE_WINDOWS",
+            "ReceivedBytes": "0",
+            "RemoteCountry": "null",
+            "ServerAddress": "192.168.1.103",
+            "ServerPort": "137",
+            "TransmittedBytes": "50"
+        }
+    ]
+}
+
+isolate_machine_raw_response={
+    "Cybereason": {
+        "IsIsolated": "true",
+        "Machine": "desktop-vg9ke2u"
+    },
+    "Endpoint": {
+        "Hostname": "desktop-vg9ke2u"
+    }
+}
+
+unisolate_machine_raw_response={
+    "Cybereason": {
+        "IsIsolated": "false",
+        "Machine": "desktop-vg9ke2u"
+    },
+    "Endpoint": {
+        "Hostname": "desktop-vg9ke2u"
+    }
+}
+
+query_malops_raw_response={
+    "Cybereason": {
+        "Malops": [
+            {
+                "AffectedMachine": [
+                    "win10-cybereaso",
+                    "marketing"
+                ],
+                "CreationTime": "2021-07-12T09:11:42.641000",
+                "DecisionFailure": "blackListedFileHash",
+                "GUID": "11.3651150229438589171",
+                "InvolvedHash": [
+                    1
+                ],
+                "LastUpdateTime": "2021-08-28T23:19:12.430000",
+                "Link": "https://integration.cybereason.net:8443/#/malop/11.3651150229438589171",
+                "Status": "OPEN",
+                "Suspects": "Process: viagra_.exe"
+            }
+        ]
+    }
+}
+
+update_malop_status_raw_response={
+    "Cybereason": {
+        "Malops": {
+            "GUID": "11.-7780537507363356527",
+            "Status": "To Review"
+        }
+    }
+}
+
+prevent_output_raw_response={
+    "Process": {
+        "MD5": "fc61fdcad5a9d52a01bd2d596f2c92b9",
+        "Prevent": "true"
+    }
+}
+
+unprevent_output_raw_response={
+    "Process": {
+        "MD5": "fc61fdcad5a9d52a01bd2d596f2c92b9",
+        "Prevent": "false"
+    }
+}
+
+query_domain_raw_response={
+    "Cybereason": {
+        "Domain": {
+            "IsInternalDomain": "false",
+            "Malicious": "false",
+            "Name": "www2.bing.com",
+            "Reputation": "null",
+            "SuspicionsCount": 0,
+            "WasEverResolved": "false",
+            "WasEverResolvedAsASecondLevelDomain": "true"
+        }
+    },
+    "Domain": {
+        "Name": "www2.bing.com"
+    }
+}
+
+query_user_raw_response={
+    "Cybereason": {
+        "User": {
+            "Domain": "desktop-vg9ke2u",
+            "LastMachineLoggedInTo": "desktop-vg9ke2u",
+            "LocalSystem": "false",
+            "Organization": "INTEGRATION",
+            "Username": "desktop-vg9ke2u\\prase"
+        }
+    }
+}
+
+available_remediation_actions_raw_response={
+    "data": [
+        {
+            "machineConnected": "false",
+            "machineId": "-1845090846.1198775089551518743",
+            "machineName": "desktop-vg9ke2u",
+            "machinesCount": 1,
+            "malopId": "11.-7780537507363356527",
+            "malopType": "MalopProcess",
+            "remediationType": "BLOCK_FILE",
+            "targetId": "-1845090846.-1424333057657783286",
+            "targetName": "fc61fdcad5a9d52a01bd2d596f2c92b9",
+            "uniqueId": "BLOCK_FILE::-1845090846.-1424333057657783286"
+        },
+        {
+            "machineConnected": "false",
+            "machineId": "-1845090846.1198775089551518743",
+            "machineName": "desktop-vg9ke2u",
+            "machinesCount": 1,
+            "malopId": "11.-7780537507363356527",
+            "malopType": "MalopProcess",
+            "remediationType": "UNQUARANTINE_FILE",
+            "targetId": "-1845090846.-4034595808369608762",
+            "targetName": "<target_name>",
+            "uniqueId": "UNQUARANTINE_FILE::-1845090846.-4034595808369608762"
+        }
+    ],
+    "errorMessage": "",
+    "status": "SUCCESS"
+}
+
+fetch_file_progress_raw_response={
+    "Download": {
+        "progress": {
+            "MalopID": "11.-7780537507363356527",
+            "batchID": [
+                -796720096
+            ],
+            "fileName": [
+                "<file_name>"
+            ],
+            "status": [
+                "true"
+            ]
+        }
+    }
+}
+
+download_fetch_file_raw_response={
+    "File": {
+        "EntryID": "15836@1282f695-fa2d-4fdd-8c2a-965a7722044b",
+        "Extension": "zip",
+        "Info": "application/zip",
+        "MD5": "753ce5f6014c7cd549f751752978d4cf",
+        "Name": "download.zip",
+        "SHA1": "9d5ef11989f0294929b572fdd4be2aefae94810d",
+        "SHA256": "532fd3122f405471f48077bf0c24bfbd2b6fa13decb9916530b86f2f8802a827",
+        "SHA512": "59a9649736c464546cc582128a2694ec797b34d558b7e845485b7688f6a536d7acac3bf5912b0725a77c02177445ec90da9982d955e5d393ff40af7109586e3b",
+        "SSDeep": "<SSDeep_value>",
+        "Size": 3168792,
+        "Type": "Zip archive data, at least v2.0 to extract"
+    }
+}
+
+fetch_scan_status_raw_response={
+    "abortHttpStatusCode": "null",
+    "abortTime": 0,
+    "abortTimeout": "false",
+    "aborterUser": "null",
+    "actionArguments": [
+        "com.cybereason.configuration.models.ScheduleScanAction",
+        "FULL"
+    ],
+    "actionType": "SchedulerScan",
+    "batchId": -1112786456,
+    "creatorUser": "<user_name>",
+    "finalState": "true",
+    "globalStats": {
+        "stats": {
+            "AbortTimeout": 0,
+            "Aborted": 0,
+            "Aborting": 0,
+            "AlreadyUpdated": 0,
+            "BadArgument": 0,
+            "ChunksRequired": 0,
+            "Disconnected": 0,
+            "EndedWithInvalidParam": 0,
+            "EndedWithNoValidFolder": 0,
+            "EndedWithSensorTimeout": 0,
+            "EndedWithTooManyResults": 0,
+            "EndedWithTooManySearches": 0,
+            "EndedWithUnknownError": 0,
+            "EndedWithUnsupportedFilter": 0,
+            "EndedWithYaraCompileError": 0,
+            "Failed": 0,
+            "FailedSending": 0,
+            "FailedSendingToServer": 0,
+            "GettingChunks": 0,
+            "InProgress": 0,
+            "InvalidState": 0,
+            "MsiFileCorrupted": 0,
+            "MsiSendFail": 0,
+            "NewerInstalled": 0,
+            "None": 0,
+            "NotSupported": 0,
+            "Pending": 0,
+            "Primed": 0,
+            "ProbeRemoved": 0,
+            "SendingMsi": 0,
+            "SendingPlatform": 0,
+            "Started": 0,
+            "Succeeded": 1,
+            "Timeout": 0,
+            "TimeoutSending": 0,
+            "UnauthorizedUser": 0,
+            "UnknownProbe": 0,
+            "partialResponse": 0
+        }
+    },
+    "initiatorUser": "<user_name>",
+    "startTime": 1652279731232,
+    "totalNumberOfProbes": 1
+}
+
+malware_query_raw_data={
+    "data": {
+        "hasMoreResults": "false",
+        "malwares": [],
+        "totalResults": 0
+    },
+    "expectedResults": 0,
+    "failedServersInfo": "null",
+    "failures": 0,
+    "hidePartialSuccess": "false",
+    "message": "",
+    "status": "SUCCESS"
+}
