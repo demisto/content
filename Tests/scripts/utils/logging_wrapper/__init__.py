da528b13a272eefcce58da72fc56f419c5a66ddc
yaakovi <syaakovi@paloaltonetworks.com>
2022-08-04 18:55:25 +0300

    update logging wrapper

--- Tests/scripts/utils/logging_wrapper/__init__.py
+++ Tests/scripts/utils/logging_wrapper/__init__.py
@@ -1,10 +1,8 @@
-import logging
 from logging import *
 
 # add success level in addition to levels imported from logging
 SUCCESS = 25
 
-root = logging.root
 addLevelName(SUCCESS, 'SUCCESS')
 
 
@@ -12,4 +10,5 @@ def success(msg, *args, **kwargs):
     """
     Log a message with severity 'SUCCESS' on the root logger.
     """
+    global root
     root.log(SUCCESS, msg, *args, **kwargs)
