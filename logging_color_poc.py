37632d03e922f47940c427e4f1eb0bd0a1389432
yaakovi <syaakovi@paloaltonetworks.com>
2022-08-05 13:04:04 +0300

    test logging colors scenarios

---
+++ logging_color_poc.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) == 1:
+    import logging
+    from Tests.scripts.utils.log_util import install_logging
+    install_logging('Destroy_instances.log')
+else:
+    from Tests.scripts.utils import logging_wrapper as logging
+    from Tests.scripts.utils.log_util import install_logging
+    install_logging('Destroy_instances.log', logger=logging)
+
+logging.info(f'this is an info log: {sys.argv}')
+logging.debug(f'this is a debug log')
+logging.warning(f'this is a warning log')
+logging.success(f'this is a warning log')
---
+++ test_logging_colors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 logging_color_poc.py
+python3 logging_color_poc.py wrapper
