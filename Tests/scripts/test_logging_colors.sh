11a9fc4f4d3e0b1317e69336738dc8dcc11f28c7
yaakovi <syaakovi@paloaltonetworks.com>
2022-08-07 09:02:01 +0300

    move poc file to Tests/scripts

---
+++ Tests/scripts/test_logging_colors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 Tests/scripts/logging_color_poc.py
+python3 Tests/scripts/logging_color_poc.py wrapper
--- test_logging_colors.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 logging_color_poc.py
-python3 logging_color_poc.py wrapper
