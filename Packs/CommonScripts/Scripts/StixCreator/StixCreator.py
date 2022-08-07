798e2b873fccff5652afafa3592180989fec9eca
yucohen <yucohen@paloaltonetworks.com>
2022-08-07 14:36:43 +0300

    added deleted spaces

--- Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
+++ Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
@@ -28,6 +28,7 @@ SCOs: dict[str, str] = {
     "hostname": "domain-name:value",
     "registry key": "windows-registry-key:key"
 }
+
 SDOs: dict[str, Callable] = {
     "malware": Malware,
     "attack pattern": AttackPattern,
@@ -58,39 +59,54 @@ def hash_type(value: str) -> str:
 
 
 def main():
+
     user_args = demisto.args().get('indicators', 'Unknown')
     doubleBackslash = demisto.args().get('doubleBackslash', True)
     all_args = {}
+
     if isinstance(user_args, dict):
         all_args = json.loads(json.dumps(user_args))
+
     else:
         try:
             all_args = json.loads(demisto.args().get('indicators', 'Unknown'))
         except:     # noqa: E722
             return_error('indicators argument is invalid json object')
+
     indicators = []
+
     for indicator_fields in all_args:
         kwargs: dict[str, Any] = {"allow_custom": True}
+
         demisto_indicator_type = all_args[indicator_fields].get('indicator_type', 'Unknown')
+
         if doubleBackslash:
             value = all_args[indicator_fields].get('value', '').replace('\\', r'\\')
         else:
             value = all_args[indicator_fields].get('value', '')
+
         demisto_score = all_args[indicator_fields].get('score', '').lower()
+
         if demisto_score in ["bad", "malicious"]:
             kwargs["score"] = "High"
+
         elif demisto_score == "suspicious":
             kwargs["score"] = "Medium"
+
         elif demisto_score in ["good", "benign"]:
             kwargs["score"] = "None"
+
         else:
             kwargs["score"] = "Not Specified"
+
         kwargs["created"] = dateparser.parse(all_args[indicator_fields].get('timestamp', ''))
         kwargs["modified"] = dateparser.parse(all_args[indicator_fields].get('lastSeen', f'{kwargs["created"]}'))
         kwargs["id"] = all_args[indicator_fields].get('stixid', '')
         kwargs["labels"] = [demisto_indicator_type.lower()]
         kwargs["description"] = all_args[indicator_fields].get('description', '')
+
         kwargs = {k: v for k, v in kwargs.items() if v}  # Removing keys with empty strings
+
         try:
             indicator_type = demisto_indicator_type.lower().replace("-", "")
             if indicator_type == 'file':
@@ -98,30 +114,41 @@ def main():
             indicator = Indicator(pattern=f"[{SCOs[indicator_type]} = '{value}']",
                                   pattern_type='stix',
                                   **kwargs)
+
             indicators.append(indicator)
+
         except KeyError:
+
             demisto.debug(f"{demisto_indicator_type} isn't an SCO checking other IOC types")
+
             try:
                 indicator_type = demisto_indicator_type.lower()
+
                 if indicator_type == 'cve':
                     kwargs["external_references"] = [ExternalReference(source_name="cve", external_id=value)]
+
                 elif indicator_type == "attack pattern":
                     try:
                         mitreid = all_args[indicator_fields].get('mitreid', '')
                         if mitreid:
                             kwargs["external_references"] = [ExternalReference(source_name="mitre", external_id=mitreid)]
+
                     except KeyError:
                         pass
+
                 indicator = SDOs[indicator_type](
                     name=value,
                     **kwargs
                 )
+
                 indicators.append(indicator)
+
             except (KeyError, TypeError) as e:
                 demisto.info(
                     "Indicator type: {}, with the value: {} is not STIX compatible".format(demisto_indicator_type, value))
                 demisto.info("Export failure excpetion: {}".format(e))
                 continue
+
     if len(indicators) > 1:
         bundle = Bundle(indicators, allow_custom=True)
         context = {
@@ -130,6 +157,7 @@ def main():
         res = (CommandResults(readable_output="",
                               outputs=context,
                               raw_response=str(bundle)))
+
     elif len(indicators) == 1:
         context = {
             'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(indicators[0]))
@@ -144,6 +172,7 @@ def main():
         res = CommandResults(readable_output="",
                              outputs=context,
                              raw_response={})
+
     return_results(res)
 
 
