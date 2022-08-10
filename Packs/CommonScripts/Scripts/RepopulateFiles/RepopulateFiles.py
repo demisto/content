aa3d6fe165d196da4e3ceedfb6de3076d672fe7f
spearmin10 <minoue@paloaltonetworks.com>
2022-08-10 19:52:28 +0900

    Added features to collect file attributes of incident attachment files together with file entries in the War Room.

--- Packs/CommonScripts/Scripts/RepopulateFiles/RepopulateFiles.py
+++ Packs/CommonScripts/Scripts/RepopulateFiles/RepopulateFiles.py
@@ -1,6 +1,9 @@
+import hashlib
 import os
+from typing import Any, Dict, List, Optional
 
-from CommonServerPython import *
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
 
 
 def parse_attachment_entries(entries):
@@ -17,19 +20,58 @@ def parse_attachment_entries(entries):
     for entry in entries:
         if entry.get('File') and entry.get('FileMetadata'):
             name, ext = os.path.splitext(entry['File'])
-            entry_context.append({
-                'Name': entry['File'],
-                'MD5': entry['FileMetadata'].get('md5'),
-                'SHA1': entry['FileMetadata'].get('sha1'),
-                'SHA256': entry['FileMetadata'].get('sha256'),
-                'SSDeep': entry['FileMetadata'].get('ssdeep'),
-                'Size': entry['FileMetadata'].get('size'),
-                'Info': entry['FileMetadata'].get('info'),
-                'Type': entry['FileMetadata'].get('type'),
-                'Extension': ext[1:] if ext else '',
-                'EntryID': entry['ID']
-            })
+            entry_context.append(assign_params(
+                Name=entry['File'],
+                MD5=entry['FileMetadata'].get('md5'),
+                SHA1=entry['FileMetadata'].get('sha1'),
+                SHA256=entry['FileMetadata'].get('sha256'),
+                SHA512=entry['FileMetadata'].get('sha512'),
+                SSDeep=entry['FileMetadata'].get('ssdeep'),
+                Size=entry['FileMetadata'].get('size'),
+                Info=entry['FileMetadata'].get('info'),
+                Type=entry['FileMetadata'].get('type'),
+                Extension=ext[1:] if ext else '',
+                EntryID=entry['ID']
+            ))
     return entry_context
+
+
+def find_attachment_entry(file_ents: List[Dict[str, Any]], attachment_ent: Dict[str, Any]) -> Optional[Dict[str, Any]]:
+    """
+    Find an incident attachment entry
+
+    :param file_ents: A list of file entries
+    :param attachment_ent: An entry of an incident attachment file
+    :return: An file entry to which the attachment is correspond.
+    """
+    # Extract the File ID
+    path = attachment_ent.get('path')
+    if not path:
+        demisto.log('Key not found: path')
+        return None
+
+    m = re.search(r'[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', path)
+    if not m:
+        demisto.log(f'Failed to get file ID for {path}')
+        return None
+
+    # Read the file contents
+    try:
+        file = demisto.getFilePath(m.group())
+        with open(file['path'], 'rb') as f:
+            data = f.read()
+    except Exception as e:
+        demisto.log(str(e))
+        return None
+
+    # Find the attachment entry
+    for file_ent in file_ents:
+        for alg, func in {'SHA512': hashlib.sha512, 'SHA256': hashlib.sha256, 'SHA1': hashlib.sha1, 'MD5': hashlib.md5}.items():
+            if alg in file_ent:
+                if file_ent[alg].lower() == func(data).hexdigest().lower():
+                    return file_ent
+                break
+    return None
 
 
 def main():
@@ -49,8 +91,21 @@ def main():
     if is_error(entries):
         return_error(get_error(entries))
     if isinstance(entries, list):
-        entry_context = parse_attachment_entries(entries)
-        return_outputs('Done', {outputPaths['file']: entry_context}, entry_context)
+        war_attachment_ents = parse_attachment_entries(entries)
+        inc_attachment_ents = []
+        for ent in demisto.incident().get('attachment') or []:
+            inc_attachment_ent = find_attachment_entry(war_attachment_ents, ent)
+            if inc_attachment_ent:
+                inc_attachment_ent.update({'Attachment': ent})
+                inc_attachment_ents.append(inc_attachment_ent)
+
+        entry_context = {outputPaths['file']: war_attachment_ents}
+        if inc_attachment_ents:
+            entry_context['AttachmentFile(val.MD5 && val.MD5 == obj.MD5 || val.SHA1 && val.SHA1 == obj.SHA1 || '
+                          'val.SHA256 && val.SHA256 == obj.SHA256 || val.SHA512 && val.SHA512 == obj.SHA512 || '
+                          'val.SSDeep && val.SSDeep == obj.SSDeep)'] = inc_attachment_ents
+
+        return_outputs('Done', entry_context, war_attachment_ents)
     else:
         return_outputs('No attachments were found.')
 
--- Packs/CommonScripts/Scripts/RepopulateFiles/RepopulateFiles_test.py
+++ Packs/CommonScripts/Scripts/RepopulateFiles/RepopulateFiles_test.py
@@ -1,4 +1,5 @@
-from RepopulateFiles import parse_attachment_entries, main
+from RepopulateFiles import parse_attachment_entries, find_attachment_entry, main
+from unittest import mock
 
 
 class TestRepopulateFiles:
@@ -16,6 +17,49 @@ class TestRepopulateFiles:
         assert len(entry_context) == 2
         assert entry_context[0].get('Size') == '42'
         assert entry_context[1].get('Type') == 'PDF'
+
+    def test_find_attachment_entry(self, mocker):
+        file_ents = [
+            {
+                'Size': 4,
+                'SHA1': '70c881d4a26984ddce795f6f71817c9cf4480e79',
+                'SHA256': '61be55a8e2f6b4e172338bddf184d6dbee29c98853e0a0485ecee7f27b9af0b4',
+                'SHA512': '1b86355f13a7f0b90c8b6053c0254399994dfbb3843e08d603e292ca13b8f672'
+                          'ed5e58791c10f3e36daec9699cc2fbdc88b4fe116efa7fce016938b787043818',
+                'Name': 'test.txt',
+                'SSDeep': '3:tt:j',
+                'EntryID': '3@202',
+                'Info': 'ASCII text, with no line terminators',
+                'Type': 'txt',
+                'MD5': '74b87337454200d4d33f80c4663dc5e5',
+                'Extension': 'txt'
+            },
+            {
+                'Size': 7818,
+                'SHA1': '0cb2f5653c6c4fb6bc3235e1200af9126b9067b0',
+                'SHA256': '0207199f975738fe798d36263998563bdc52bd59b226730c0d5e481646247c9a',
+                'Name': 'test.yml',
+                'SSDeep': '192:Ik4PaoJcQfmBLTh7qX0QLAhl8G5wmIxtH5hnTG:IkKamFOBLTh4UHiNo',
+                'EntryID':'18@200',
+                'Info': 'Python script, ASCII text executable',
+                'Type': 'yml',
+                'MD5': '3e8ba0bbe4cba711550deeeb48812861'
+            }
+        ]
+
+        attachment_ent = {
+            'description': '',
+            'isTempPath': False,
+            'name': 'test.txt',
+            'path': '201_915f7adb-fcd8-4e09-81df-9353ae3858df_test.txt',
+            'showMediaFile': False,
+            'type': 'text/plain'
+        }
+
+        with mock.patch('builtins.open', mock.mock_open(read_data=b'aaaa')):
+            ent = find_attachment_entry(file_ents, attachment_ent)
+            assert ent.get('Size') == 4
+            assert ent.get('SHA256') == '61be55a8e2f6b4e172338bddf184d6dbee29c98853e0a0485ecee7f27b9af0b4'
 
     def test_main_no_entries(self, mocker):
         mocker.patch('RepopulateFiles.demisto.executeCommand', return_value=None)
