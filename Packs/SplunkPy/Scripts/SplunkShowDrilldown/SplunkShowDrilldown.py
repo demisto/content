76cb32d55cc4394db399a6633464c3f365acf937
bziser <bziser@paloaltonetworks.com>
2022-08-15 15:15:19 +0300

    drilldown errors

--- Packs/SplunkPy/Scripts/SplunkShowDrilldown/SplunkShowDrilldown.py
+++ Packs/SplunkPy/Scripts/SplunkShowDrilldown/SplunkShowDrilldown.py
@@ -5,17 +5,26 @@ from CommonServerPython import *  # noqa: F401
 
 
 def main():
+
     incident = demisto.incident()
     if not incident:
         raise ValueError("Error - demisto.incident() expected to return current incident "
                          "from context but returned None")
+
     custom_fields = incident.get('CustomFields', {})
-    drilldown_results_str = custom_fields.get('notabledrilldown', {})
+
+    drilldown_results_str = custom_fields.get('notabledrilldown', "")
+    if not drilldown_results_str:
+        return CommandResults(readable_output='Drilldown was not configured for notable.')
+
     is_successful = custom_fields.get('successfuldrilldownenrichment', '')
     if is_successful == 'false':
         return CommandResults(readable_output='Drilldown enrichment failed.')
 
-    drilldown_results = json.loads(drilldown_results_str)
+    try:
+        drilldown_results = json.loads(drilldown_results_str)
+    except json.decoder.JSONDecodeError as e:
+        raise ValueError(f'Drilldown is not in a valid JSON structure:\n{e}')
 
     if not drilldown_results:
         return CommandResults(readable_output='Drilldown was not configured for notable.')
@@ -25,7 +34,6 @@ def main():
         for event in drilldown_results:
             events_arr.append(event)
         markdown = tableToMarkdown("", events_arr, headers=events_arr[0].keys())
-
     else:
         markdown = tableToMarkdown("", drilldown_results)
 
