29a6b1d9192e1d356a7670e259bfb65849a0e441
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-17 16:12:42 +0300

    add pprof

--- Tests/configure_and_test_integration_instances.py
+++ Tests/configure_and_test_integration_instances.py
@@ -53,6 +53,7 @@ DOCKER_HARDENING_CONFIGURATION = {
     'limit.docker.cpu': 'true',
     'python.pass.extra.keys': f'--memory=1g##--memory-swap=-1##--pids-limit=256##--ulimit=nofile=1024:8192##--env##no_proxy={NO_PROXY}',  # noqa: E501
     'powershell.pass.extra.keys': f'--env##no_proxy={NO_PROXY}',
+    'monitoring.pprof': 'true',
 }
 DOCKER_HARDENING_CONFIGURATION_FOR_PODMAN = {
     'docker.run.internal.asuser': 'true'
