8959ebc1f486b5d25203bfc98b770aab76e28924
oauth2 <ypreisler@paloaltonetworks.com>
2022-08-18 18:59:41 +0300

    test fix

--- Tests/Marketplace/Tests/marketplace_services_test.py
+++ Tests/Marketplace/Tests/marketplace_services_test.py
@@ -1083,7 +1083,7 @@ This is visible
                                                                   build_number=build_number,
                                                                   modified_files_data=modified_data)
 
-        assert not version_changelog['releaseNotes']
+        assert version_changelog['releaseNotes'] == "No relevant release notes found."
 
     @pytest.mark.parametrize('release_notes, upload_marketplace, expected_result', [
         ('''
