95e66131df583163753e5ca16caf01031db5acaa
Ayman Mahmoud <57979775+ayman-m@users.noreply.github.com>
2022-08-22 16:25:35 +0400

    Update Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
    Co-authored-by: yuvalbenshalom <ybenshalom@paloaltonetworks.com>

--- Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
+++ Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
@@ -2633,6 +2633,7 @@ def main():
         connection_args['username'] = username
         connection_args['password'] = password
         connection_args['autologin'] = True
+        connection_args['basic'] = True
 
     if use_requests_handler:
         handle_proxy()
