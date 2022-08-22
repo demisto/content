a2d4559d56fd9d1f94b98967432b35718f9873aa
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-22 15:12:54 +0300

    fix

--- Tests/scripts/collect_tests/utils.py
+++ Tests/scripts/collect_tests/utils.py
@@ -8,8 +8,9 @@ from demisto_sdk.commands.common.constants import FileType, MarketplaceVersions
 from demisto_sdk.commands.common.tools import json, yaml
 from exceptions import (BlankPackNameException, DeprecatedPackException,
                         NonDictException, NonexistentPackException,
+                        NonXsoarSupportedPackException,
                         NoTestsConfiguredException, NotUnderPackException,
-                        SkippedPackException, UnsupportedPackException)
+                        SkippedPackException)
 from logger import logger
 from packaging import version
 from packaging._structures import InfinityType, NegativeInfinityType
@@ -301,7 +302,7 @@ class PackManager:
         if not (support_level := self[pack].get('support')):
             raise ValueError(f'pack {pack} has no support level (`support`) field or value')
         if support_level.lower() != 'xsoar':
-            raise UnsupportedPackException(pack)
+            raise NonXsoarSupportedPackException(pack)
 
 
 def to_tuple(value: Optional[str | list]) -> Optional[tuple]:
