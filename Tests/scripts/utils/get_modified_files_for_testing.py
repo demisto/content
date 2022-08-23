5cecde8a1b2bc6021da5d4d7aa6b92bfa1586602
ilaner <88267954+ilaner@users.noreply.github.com>
2022-08-23 14:37:06 +0300

    CONF_PATH converted to relative path (#20723)

--- Tests/scripts/utils/get_modified_files_for_testing.py
+++ Tests/scripts/utils/get_modified_files_for_testing.py
@@ -8,6 +8,7 @@ from Tests.scripts.utils.log_util import install_logging
 from Tests.scripts.utils import logging_wrapper as logging
 
 import demisto_sdk.commands.common.constants as constants
+from demisto_sdk.commands.common.tools import get_content_path
 # TODO: remove try except clause when demisto-sdk 1.7.3 is released
 try:
     import demisto_sdk.commands.common.content_constant_paths as content_constant_paths
@@ -69,8 +70,9 @@ def resolve_type(file_path: str) -> Optional[FileType]:
         FileType. Conf.json and Metadata files.
     """
     # if conf.json file
-    # TODO: remove if when demisto-sdk 1.7.3 is released
-    if checked_type(file_path, [content_constant_paths.CONF_PATH.as_posix() if IS_UP_TO_DATE else constants.CONF_PATH]):
+    # TODO: remove "if" statement when demisto-sdk 1.7.3 is released
+    if checked_type(file_path, [str(content_constant_paths.CONF_PATH.relative_to(get_content_path()))
+                                if IS_UP_TO_DATE else constants.CONF_PATH]):
         return FileType.CONF_JSON
     # landingPage_sections.json file
     if checked_type(file_path, [LANDING_PAGE_SECTIONS_JSON_PATH]):
