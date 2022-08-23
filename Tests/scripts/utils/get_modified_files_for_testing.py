cd77d10aea2e4c88a4f5611971079bbab34ec45c
ilan <ierukhimovic@paloaltonetworks.com>
2022-08-23 12:00:44 +0300

    change to str

--- Tests/scripts/utils/get_modified_files_for_testing.py
+++ Tests/scripts/utils/get_modified_files_for_testing.py
@@ -71,7 +71,7 @@ def resolve_type(file_path: str) -> Optional[FileType]:
     """
     # if conf.json file
     # TODO: remove if when demisto-sdk 1.7.3 is released
-    if checked_type(file_path, [content_constant_paths.CONF_PATH.relative_to(get_content_path()).as_posix()
+    if checked_type(file_path, [str(content_constant_paths.CONF_PATH.relative_to(get_content_path()))
                                 if IS_UP_TO_DATE else constants.CONF_PATH]):
         return FileType.CONF_JSON
     # landingPage_sections.json file
