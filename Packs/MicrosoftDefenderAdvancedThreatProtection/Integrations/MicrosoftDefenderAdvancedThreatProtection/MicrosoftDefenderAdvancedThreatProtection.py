53a488cdc2dab6a44af1655c8a4a0f27ac49e0b1
okarkkatz <okarkkatz@paloaltonetworks.com>
2022-08-24 15:22:37 +0300

    added the page_size default_value=50

--- Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection.py
+++ Packs/MicrosoftDefenderAdvancedThreatProtection/Integrations/MicrosoftDefenderAdvancedThreatProtection/MicrosoftDefenderAdvancedThreatProtection.py
@@ -1196,14 +1196,13 @@ class MsClient:
         cmd_url = '/machines'
         params = {'$filter': filter_req} if filter_req else {}
 
-        if page_size:
-            page_size = arg_to_number(page_size)
-            page_size = min(page_size, 10000)
-            page_num = arg_to_number(page_num)
-            page_num = 0 if not page_num else (page_num - 1)
-            skip = page_num * page_size
-            params['$skip'] = str(skip)
-            params['$top'] = str(page_size)
+        page_size = arg_to_number(page_size)
+        page_size = min(page_size, 10000)
+        page_num = arg_to_number(page_num)
+        page_num = 0 if not page_num else (page_num - 1)
+        skip = page_num * page_size
+        params['$skip'] = str(skip)
+        params['$top'] = str(page_size)
 
         return self.ms_client.http_request(method='GET', url_suffix=cmd_url, params=params)
 
