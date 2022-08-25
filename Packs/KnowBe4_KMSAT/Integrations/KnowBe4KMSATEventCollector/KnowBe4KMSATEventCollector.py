90cbea7504df0cff9e61e415bd8194ce99691456
YuvHayun <yhayun@paloaltonetworks.com>
2022-08-25 11:51:29 +0300

    changes

--- Packs/KnowBe4_KMSAT/Integrations/KnowBe4KMSATEventCollector/KnowBe4KMSATEventCollector.py
+++ Packs/KnowBe4_KMSAT/Integrations/KnowBe4KMSATEventCollector/KnowBe4KMSATEventCollector.py
@@ -29,7 +29,7 @@ class Client(BaseClient):
     :param headers (dict[str, str]): the requests header.
     """
     @logger
-    def __init__(self, base_url: str, verify: bool, proxy: bool, headers: dict[str, str]):
+    def __init__(self, base_url: str, verify: bool = False, proxy: bool = False, headers: dict[str, str] = {}):
         super().__init__(base_url, verify=verify, proxy=proxy)
         self.headers = headers
 
@@ -57,7 +57,20 @@ class Client(BaseClient):
 ''' HELPER FUNCTIONS '''
 
 
-def eliminate_duplicated_events(fetched_events: list[dict], last_run: date):
+def validate_limit(limit: Optional[int]):
+    """
+    Validate that the limit/max fetch is a number divisible by the MAX_EVENTS_PER_REQUEST (100) and that it is not
+    a negative number.
+    """
+    if limit:
+        if limit % MAX_EVENTS_PER_REQUEST != 0:
+            raise DemistoException(f'fetch limit parameter should be divisible by {MAX_EVENTS_PER_REQUEST}')
+
+        if limit <= 0:
+            raise DemistoException('fetch limit parameter cannot be negative number or zero')
+
+
+def eliminate_duplicated_events(fetched_events: list[dict], last_run: dict):
     """
     create a new list out of a given a list that include only events that occurred after that latest event from previous run.
 
@@ -68,10 +81,11 @@ def eliminate_duplicated_events(fetched_events: list[dict], last_run: date):
     Returns:
         list: A list containing only events that occurred after the last run.
     """
-    return [event for event in fetched_events if event.get('occurred_time', datetime.now()) > last_run]
+    last_run_time = last_run.get('latest_event_time', datetime.now())
+    return [event for event in fetched_events if event.get('occurred_date', datetime.now()) > last_run_time]
 
 
-def check_if_last_run_reached(last_run: date, earliest_fetched_event: dict[str, Any]):
+def check_if_last_run_reached(last_run: dict, earliest_fetched_event: dict[str, Any]):
     """
     Compare the latest event from previous fetch interval with the latest event in the page from the current fetch interval
     To check if the latest event was reached.
@@ -83,7 +97,8 @@ def check_if_last_run_reached(last_run: date, earliest_fetched_event: dict[str, 
     Returns:
         list: A list containing only events that occurred after the last run.
     """
-    return last_run <= earliest_fetched_event.get('occurred_date', datetime.now())
+    last_run_time = last_run.get('latest_event_time', datetime.now())
+    return last_run_time >= earliest_fetched_event.get('occurred_date', datetime.now())
 
 
 ''' COMMAND FUNCTIONS '''
@@ -96,25 +111,22 @@ def fetch_events(client: Client, first_fetch_time: Optional[datetime],
     """
     query_params = {'page': 1, 'per_page': 100}
     events: List[Dict] = []
-    if last_run:
-        #  if max fetch is None, all events will be fetched until there aren't anymore in the queue (until we get 204)
-        while True:
-            response = client.get_events_request(params=query_params)
-            # if response.status_code == 204:  # if we got 204, it means there aren't events in the queue, hence breaking.
-            #     break
-            fetched_events = response.json().get('data') or []
-            demisto.info(f'fetched events length: ({len(fetched_events)})')
-            demisto.debug(f'fetched events: ({fetched_events})')
-            events.extend(eliminate_duplicated_events(fetched_events, last_run.get('latest_event_time', datetime.now())))
-            is_last_run_reached = check_if_last_run_reached(last_run.get('latest_event_time', datetime.now()), events[0])
-            if not response.get('meta', {}).get('next_page') or is_last_run_reached:
-                break
-            else:
-                query_params['page'] = response.get('meta', {}).get('next_page', 1)
-
-    else:
-        a = 5
-        # to do: implement a mechanism to collect events from all days between first fetch and current date
+    if not last_run:
+        last_run = {}
+    #  if max fetch is None, all events will be fetched until there aren't anymore in the queue (until we get 204)
+    while True:
+        response = client.get_events_request(params=query_params)
+        # if response.status_code == 204:  # if we got 204, it means there aren't events in the queue, hence breaking.
+        #     break
+        fetched_events = response.json().get('data') or []
+        demisto.info(f'fetched events length: ({len(fetched_events)})')
+        demisto.debug(f'fetched events: ({fetched_events})')
+        events.extend(eliminate_duplicated_events(fetched_events, last_run))
+        is_last_run_reached = check_if_last_run_reached(last_run, events[-1])
+        if not response.get('meta', {}).get('next_page') or is_last_run_reached:
+            break
+        else:
+            query_params['page'] = response.get('meta', {}).get('next_page', 1)
     new_last_run: dict[str, date] = {'latest_event_time': events[0].get('occurred_date')}
     demisto.info(f'Done fetching {len(events)} events, Setting new_last_run = {new_last_run}.')
     return events, new_last_run
@@ -128,12 +140,62 @@ def test_module(client: Client) -> str:
     return 'ok'
 
 
+def get_events_command(client: Client, args: Dict, max_fetch: Optional[int], vendor: str,
+                       product: str) -> Union[str, CommandResults]:
+    """
+    Fetches events from the saas-security queue and return them to the war-room.
+    in case should_push_events is set to True, they will be also sent to XSIAM.
+    """
+    should_push_events = argToBoolean(args.get('should_push_events'))
+
+    if events := fetch_events_from_saas_security(client=client, max_fetch=max_fetch):
+        if should_push_events:
+            send_events_to_xsiam(events=events, vendor=vendor, product=product)
+        return CommandResults(
+            readable_output=tableToMarkdown(
+                'SaaS Security Logs',
+                events,
+                headers=['log_type', 'item_type', 'item_name', 'timestamp', 'serial'],
+                headerTransform=underscoreToCamelCase,
+                removeNull=True
+            ),
+            raw_response=events,
+            outputs=events,
+            outputs_key_field=['timestamp', 'log_type', 'item_name', 'item_type'],
+            outputs_prefix='SaasSecurity.Event'
+        )
+    return 'No events were found.'
+
+
+def fetch_events_from_saas_security(client: Client, max_fetch: Optional[int] = None) -> List[Dict]:
+    """
+    Fetches events from the saas-security queue.
+    """
+    events: List[Dict] = []
+    under_max_fetch = True
+
+    #  if max fetch is None, all events will be fetched until there aren't anymore in the queue (until we get 204)
+    while under_max_fetch:
+        response = client.get_events_request()
+        if response.status_code == 204:  # if we got 204, it means there aren't events in the queue, hence breaking.
+            break
+        fetched_events = response.json().get('events') or []
+        demisto.info(f'fetched events length: ({len(fetched_events)})')
+        demisto.debug(f'fetched events: ({fetched_events})')
+        events.extend(fetched_events)
+        if max_fetch:
+            under_max_fetch = len(events) < max_fetch
+
+    return events
+
+
 ''' MAIN FUNCTION '''
 
 
 def main() -> None:
     """main function, parses params and runs command functions
     """
+    args = demisto.args()
     params = demisto.params()
     command = demisto.command()
 
@@ -141,6 +203,8 @@ def main() -> None:
     api_key = params.get('credentials', {}).get('password')
     verify_certificate = not params.get('insecure', False)
     first_fetch_time = arg_to_datetime(params.get('first_fetch', '3 days'))
+    max_fetch = arg_to_number(args.get('limit') or params.get('max_fetch'))
+    validate_limit(max_fetch)
     proxy = demisto.params().get('proxy', False)
     vendor, product = params.get('vendor'), params.get('product')
     headers = {'Authorization': f'Bearer {api_key}'}
@@ -164,6 +228,10 @@ def main() -> None:
                 product=product
             )
             demisto.setLastRun(last_run)
+        elif command == 'kms-get-events':
+            return_results(get_events_command(
+                client=client, args=args, max_fetch=max_fetch, vendor=vendor, product=product)
+            )
         else:
             raise ValueError(f'Command {command} is not implemented in this integration')
     except Exception as e:
--- Packs/KnowBe4_KMSAT/Integrations/KnowBe4KMSATEventCollector/KnowBe4KMSATEventCollector_test.py
+++ Packs/KnowBe4_KMSAT/Integrations/KnowBe4KMSATEventCollector/KnowBe4KMSATEventCollector_test.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import io
 from KnowBe4KMSATEventCollector import Client
+from CommonServerPython import DemistoException
 
 
 def util_load_json(path):
@@ -59,18 +60,33 @@ def test_test_module(requests_mock):
 
 
 @pytest.mark.parametrize('last_run, fetched_events, expected_filtered_list_size, expected_filtered_list_elements', [
-    (),
+    ({'latest_event_time': datetime.datetime(2022, 5, 17, 10, 5, 3)},
+     [{'occurred_date': datetime.datetime(2022, 6, 17, 12, 3, 1)}], 1,
+     [{'occurred_date': datetime.datetime(2022, 6, 17, 12, 3, 1)}]),
+    ({'latest_event_time': datetime.datetime(2022, 5, 17, 10, 5, 3)},
+     [{'occurred_date': datetime.datetime(2022, 4, 17, 12, 3, 1)}], 0,
+     []), ({'latest_event_time': datetime.datetime(2022, 5, 17, 10, 5, 3)},
+           [{'occurred_date': datetime.datetime(2022, 6, 17, 12, 3, 1)},
+            {'occurred_date': datetime.datetime(2022, 4, 17, 12, 3, 1)}], 1,
+           [{'occurred_date': datetime.datetime(2022, 6, 17, 12, 3, 1)}]),
 ])
 def test_eliminate_duplicated_events(last_run, fetched_events, expected_filtered_list_size, expected_filtered_list_elements):
     """
     Given
     - A list of fetched events and a last_run date
+    - Case 1: last_run object and a list with 1 event that occurred after the last_run.
+    - Case 2: last_run object and a list with 1 event that occurred before the last_run.
+    - Case 3: last_run object and a list with 1 event that occurred after the last_run and 1 event that occurred before it.
 
     When
     - Running eliminate_duplicated_events helper function.
 
     Then
-    - Validate that all the events with the earlier than last_run 'occurred_time' are filtered out.
+    - Validate that all the events with the earlier than last_run 'occurred_date' are filtered out.
+    - Case 1: Ensure that the event wasn't filtered out of the events list.
+    - Case 2: Ensure that the event was filtered out of the events list.
+    - Case 3: Ensure that the event that occurred after that last run wasn't filtered out of the events list
+              and that the event that occurred before the last run was filtered.
     """
     from KnowBe4KMSATEventCollector import eliminate_duplicated_events
     filtered_events_list = eliminate_duplicated_events(fetched_events, last_run)
@@ -80,21 +96,42 @@ def test_eliminate_duplicated_events(last_run, fetched_events, expected_filtered
 
 
 @pytest.mark.parametrize('last_run, events, expected_results', [
-    ({'latest_event_time': datetime.datetime(2020, 5, 17, 10, 5, 3)},
-     [{'occurred_time': datetime.datetime(2020, 5, 17, 12, 3, 1)}, {'occurred_time': datetime.datetime(2020, 5, 17, 9, 3, 1)}],
-     True), ({'latest_event_time': datetime.datetime(2020, 5, 17, 10, 5, 3)},
-             [{'occurred_time': datetime.datetime(2020, 5, 17, 12, 3, 1)},
-              {'occurred_time': datetime.datetime(2020, 5, 17, 11, 3, 1)}], False)])
+    ({'latest_event_time': datetime.datetime(2022, 5, 17, 10, 5, 3)},
+     [{'occurred_date': datetime.datetime(2022, 6, 17, 12, 3, 1)}], False),
+    ({'latest_event_time': datetime.datetime(2022, 5, 17, 10, 5, 3)},
+     [{'occurred_date': datetime.datetime(2022, 5, 16, 9, 3, 1)}], True)])
 def test_check_if_last_run_reached(last_run, events, expected_results):
     """
     Given
-    - A list of fetched events and a last_run date
+    - A list of fetched events and a last_run date.
+    - Case 1: latest event that occurred before the first event in the events list.
+    - Case 2: latest event that occurred after the first event in the events list.
 
     When
     - Running check_if_last_run_reached helper function.
 
     Then
     - Validate that the answer returend from the function is correct.
+    - Case 1: Ensure that the function returned False.
+    - Case 2: Ensure that the function returend True.
     """
     from KnowBe4KMSATEventCollector import check_if_last_run_reached
     assert check_if_last_run_reached(last_run, events[0]) == expected_results
+
+
+@pytest.mark.parametrize('limit', [126, 1, 101, 235, -1, -100])
+def test_validate_limit(limit):
+    """
+    Given
+    - a limit parameter which is not divisible by 100/negative limit.
+
+    When
+    - executing the validate limit
+
+    Then
+    - make sure an exception is raised
+    """
+    from KnowBe4KMSATEventCollector import validate_limit
+
+    with pytest.raises(DemistoException):
+        validate_limit(limit)
