c615571f3fac8248685df6a831546b06a8736894
nmaimon <nmaimon@paloaltonetworks.com>
2022-08-29 15:03:48 +0300

    verify bucket

--- Utils/test_upload_flow/create_test_branch.py
+++ Utils/test_upload_flow/create_test_branch.py
@@ -236,20 +236,4 @@ if __name__ == "__main__":
             repo.delete_head(branch, force=True)
 
 
-if __name__ == "__main__":
-    # New pack - check that pack and all its content items are there
-    # Armis - check its dependency is also the new pack
-    #  ZeroFox - check there is a new version and RN
-    # Box - existing RN was updated
-    # BPA - 1.0.0 rn was added
-    # Microsoft365Defender set as hidden
-    # Maltiverse - readme updated
-    # MISP - update pack ignore
-    # Trello - landing page
-    # Absolute - failing pack
-    # Alexa - modified pack
-    # AlibabaActionTrail - modify item path
-    # index checks
-    # packs check
 
-    pass
---
+++ Utils/test_upload_flow/verify_bucket.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    # New pack - check that pack and all its content items are there
+    # Armis - check its dependency is also the new pack
+    #  ZeroFox - check there is a new version and RN
+    # Box - existing RN was updated
+    # BPA - 1.0.0 rn was added
+    # Microsoft365Defender set as hidden
+    # Maltiverse - readme updated
+    # MISP - update pack ignore
+    # Trello - landing page
+    # Absolute - failing pack
+    # Alexa - modified pack
+    # AlibabaActionTrail - modify item path
+    # index checks
+    # packs check
+
+    pass
