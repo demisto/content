8a1d643f85c25b3a85f7708921b296b9656e212f
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-29 18:02:32 +0300

    moar logs

--- Tests/scripts/collect_tests/id_set.py
+++ Tests/scripts/collect_tests/id_set.py
@@ -61,10 +61,12 @@ class IdSetItem(DictBased):
             return find_pack_folder(path)
 
         except NotUnderPackException:
+            logger.warning(f'{path=} is not under a pack')
             if path.name in SKIPPED_CONTENT_ITEMS__NOT_UNDER_PACK:
                 logger.warning(f'{path=} is not under a pack, but it is OK')
                 return None
             else:
+                logger.warning(f'{path=} is not under a pack, and it is NOT OK')
                 raise
 
 
