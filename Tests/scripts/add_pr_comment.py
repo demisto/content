c50d989d11692df907783e813e8253921aba1cb1
meichlerpanw <meichler@paloaltonetworks.com>
2022-08-29 11:15:10 +0300

    circle

--- Tests/scripts/add_pr_comment.py
+++ Tests/scripts/add_pr_comment.py
@@ -1,14 +1,10 @@
 import os
-import json
 from demisto_sdk.commands.test_content.execute_test_content import _add_pr_comment
 from demisto_sdk.commands.test_content.execute_test_content import ParallelLoggingManager
 
 
-# JOB_ID = os.environ.get("CI_JOB_ID")
 JOB_ID = os.environ.get("CIRCLE_WORKFLOW_ID")
 
-# COVERAGE_LINK = f'https://xsoar.docs.pan.run/-/content/-/jobs/{JOB_ID}/artifacts/artifacts/coverage_report/html/' \
-#                 f'index.html'
 COVERAGE_LINK = f'https://output.circle-artifacts.com/output/job/{JOB_ID}/artifacts/0/artifacts/coverage_report/html/' \
                 f'index.html'
 COVERAGE_REPORT_COMMENT = f'Link to the unit tests coverage report: \n {COVERAGE_LINK}'
@@ -16,4 +12,4 @@ COVERAGE_REPORT_COMMENT = f'Link to the unit tests coverage report: \n {COVERAGE
 
 if __name__ == "__main__":
     logging_manager = ParallelLoggingManager('UT_coverage_report.log')
-    _add_pr_comment(COVERAGE_REPORT_COMMENT, logging_manager)
+    _add_pr_comment(COVERAGE_REPORT_COMMENT, logging_manager, build='circleci')
