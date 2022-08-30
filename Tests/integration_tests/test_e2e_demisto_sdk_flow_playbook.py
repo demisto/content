66a708b710da64f4b36aba7433ec16a927aa2454
tkatzir <tkatzir@paloaltonetworks.com>
2022-08-30 11:12:41 +0300

    test_e2e_demisto_sdk_flow_playbook_client

--- Tests/integration_tests/test_e2e_demisto_sdk_flow_playbook.py
+++ Tests/integration_tests/test_e2e_demisto_sdk_flow_playbook.py
@@ -1,3 +1,9 @@
+from demisto_client.demisto_api.rest import ApiException
+
+import demisto_client.demisto_api as demisto_api
+from demisto_client.demisto_api import ApiClient
+
+
 
 from demisto_sdk.commands.download.downloader import Downloader
 from demisto_sdk.commands.format.format_module import format_manager
@@ -10,14 +16,12 @@ import random
 
 import e2e_tests_utils
 
-# TODO Create another test, with a playbook that was created in the server with demisto-py
-
 # TODO Create a new git Content branch, and work with that branch. And the end: git checkout master, git reset hard
 # TODO Review Ido's doc and see if we missed anything.
 
 
-def test_e2e_demisto_sdk_flow_playbook(tmpdir, insecure: bool = False):
-    # Importing TestSuite classes from Demisto-SDK
+def test_e2e_demisto_sdk_flow_playbook_testsuite(tmpdir, insecure: bool = False):
+    # Importing TestSuite classes from Demisto-SDK, as they are excluded when pip installing the SDK.
     e2e_tests_utils.cli(f'mkdir {tmpdir}/git')
     e2e_tests_utils.git_clone_demisto_sdk(destination_folder=f'{tmpdir}/git/demisto-sdk', sdk_git_branch='testsuite-playbook')
     from TestSuite.repo import Repo
@@ -37,22 +41,82 @@ def test_e2e_demisto_sdk_flow_playbook(tmpdir, insecure: bool = False):
     Uploader(input=f'{tmpdir}/Packs/{pack_name}/Playbooks/{playbook_name}.yml', insecure=True).upload()
 
     # Preparing updated pack folder
-    e2e_tests_utils.cli(f'mkdir {tmpdir}/Packs/{pack_name}_updated')
+    e2e_tests_utils.cli(f'mkdir {tmpdir}/Packs/{pack_name}_testsuite')
 
-    print(f'Trying to download the updated playbook from {playbook_name} to {tmpdir}/Packs/{pack_name}_updated/Playbooks')
-    Downloader(output=f'{tmpdir}/Packs/{pack_name}_updated', input=[playbook_name], insecure=True).download()
-    assert path.exists(f'{tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml')
+    print(f'Trying to download the updated playbook from {playbook_name} to {tmpdir}/Packs/{pack_name}_testsuite/Playbooks')
+    Downloader(output=f'{tmpdir}/Packs/{pack_name}_testsuite', input=[playbook_name], insecure=True).download()
+    assert path.exists(f'{tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml')
 
     print('Generating docs (creating a readme file)'
-          f' for the playbook {tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml'
+          f' for the playbook {tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml'
           )
-    generate_playbook_doc.generate_playbook_doc(input_path=f'{tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml')
-    assert path.exists(f'{tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}_README.md')
+    generate_playbook_doc.generate_playbook_doc(input_path=f'{tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml')
+    assert path.exists(f'{tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}_README.md')
 
-    print(f'Formating playbook {tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml')
-    format_manager(input=f'{tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml', assume_yes=True)
-    print(f'Validating playbook {tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml')
-    ValidateManager(file_path=f'{tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml').run_validation()
+    print(f'Formating playbook {tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml')
+    format_manager(input=f'{tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml', assume_yes=True)
+    print(f'Validating playbook {tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml')
+    ValidateManager(file_path=f'{tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml').run_validation()
 
-    print(f'Uploading updated playbook {tmpdir}/Packs/{pack_name}_updated/Playbooks/{playbook_name}.yml')
+    print(f'Uploading updated playbook {tmpdir}/Packs/{pack_name}_testsuite/Playbooks/{playbook_name}.yml')
+    Uploader(input=f'{tmpdir}/Packs/{pack_name}/Playbooks/{playbook_name}.yml', insecure=True).upload()
+
+def test_e2e_demisto_sdk_flow_playbook_client(tmpdir, insecure: bool = False):
+    # TODO Remove the random
+    pack_name = 'foo_' + str(random.randint(1, 1000))
+    playbook_name = 'pb_' + str(random.randint(1, 1000))
+
+    demisto_client = e2e_tests_utils.connect_to_server(insecure=insecure)
+    body = [
+        {
+            "name": playbook_name,
+            "propagationLabels": [
+                  "all"
+            ],
+            "tasks": {
+              "0": {
+                "id": "0",
+                "unqiueId": "0",
+                "type": "start",
+                "nextTasks": None,
+                "task": {}
+              }
+            }
+        }
+      ]
+
+    header_params = {}
+    header_params['Accept'] = 'application/json'  # noqa: E501
+    header_params['Accept-Encoding'] = 'gzip, deflate, br'
+    header_params['Content-Type'] = 'application/json'
+    header_params['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+
+    try:
+      demisto_client.api_client.call_api(resource_path='/playbook/save', method='POST',
+                                        header_params=header_params,
+                                        body=body,
+                                        )
+    except ApiException as ae:
+        print(f'*** Failed to create playbook {playbook_name}, reason: {ae}')
+        assert False
+
+    # Preparing updated pack folder
+    e2e_tests_utils.cli(f'mkdir -p {tmpdir}/Packs/{pack_name}_client')
+
+    print(f'Trying to download the updated playbook from {playbook_name} to {tmpdir}/Packs/{pack_name}_client/Playbooks')
+    Downloader(output=f'{tmpdir}/Packs/{pack_name}_client', input=[playbook_name], insecure=True).download()
+    assert path.exists(f'{tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml')
+
+    print('Generating docs (creating a readme file)'
+          f' for the playbook {tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml'
+          )
+    generate_playbook_doc.generate_playbook_doc(input_path=f'{tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml')
+    assert path.exists(f'{tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}_README.md')
+
+    print(f'Formating playbook {tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml')
+    format_manager(input=f'{tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml', assume_yes=True)
+    print(f'Validating playbook {tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml')
+    ValidateManager(file_path=f'{tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml').run_validation()
+
+    print(f'Uploading updated playbook {tmpdir}/Packs/{pack_name}_client/Playbooks/{playbook_name}.yml')
     Uploader(input=f'{tmpdir}/Packs/{pack_name}/Playbooks/{playbook_name}.yml', insecure=True).upload()
