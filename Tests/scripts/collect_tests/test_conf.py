e7d23d7cc654911604a292afd35ad522695c0efe
BEAdi <72088126+BEAdi@users.noreply.github.com>
2022-08-30 12:16:09 +0300

    remove nightly_integrations field

--- Tests/scripts/collect_tests/test_conf.py
+++ Tests/scripts/collect_tests/test_conf.py
@@ -22,7 +22,6 @@ class TestConf(DictFileBased):
         self.skipped_tests: dict[str, str] = self['skipped_tests']
         self.skipped_integrations: dict[str, str] = self['skipped_integrations']
         self.private_tests: set[str] = set(self['private_tests'])
-        self.nightly_integrations: set[str] = set(self['nightly_integrations'])
         self.nightly_packs: set[str] = set(self['nightly_packs'])
 
         self.classifier_to_test: dict[TestConfItem, str] = {
