a8c9aa5971e74330f64de95a6953ccdb93464967
Johnathan Wilkes <jwilkes@paloaltonetworks.com>
2022-08-30 08:59:07 -0400

    changed to the CORE built in Auth

--- Packs/CortexAttackSurfaceManagement/Integrations/CortexAttackSurfaceManagement/CortexAttackSurfaceManagement.py
+++ Packs/CortexAttackSurfaceManagement/Integrations/CortexAttackSurfaceManagement/CortexAttackSurfaceManagement.py
@@ -3,72 +3,60 @@ from CommonServerPython import *  # noqa: F401
 
 
 class Client(BaseClient):
-    def __init__(self, server_url, verify, proxy, headers, auth):
-        super().__init__(base_url=server_url, verify=verify, proxy=proxy, headers=headers, auth=auth)
+    def __init__(self, base_url, verify, proxy, headers, auth):
+        super().__init__(base_url, verify=verify, proxy=proxy, headers=headers, auth=auth)
 
     def getexternalservices_request(self, field, operator, value):
         data = {"request_data": {}}
         headers = self._headers
-        headers['Content-Type'] = 'application/json'
-        headers['x-xdr-auth-id'] = '2'
 
-        response = self._http_request('POST', 'public_api/v1/assets/get_external_services/',
+        response = self._http_request('POST', '/assets/get_external_services/',
                                       json_data=data, headers=headers)
 
         return response
 
     def getexternalservice_request(self, service_id_list):
-        data = {"request_data": {"service_id_list": service_id_list}}
+        data = {"request_data": {"service_id_list": ["94232f8a-f001-3292-aa65-63fa9d981427"]}}
         headers = self._headers
-        headers['Content-Type'] = 'application/json'
-        headers['x-xdr-auth-id'] = '2'
 
-        response = self._http_request('POST', 'public_api/v1/assets/get_external_service',
+        response = self._http_request('POST', '/assets/get_external_service',
                                       json_data=data, headers=headers)
 
         return response
 
     def getexternalipaddressranges_request(self):
+        data = {"request_data": {}}
         headers = self._headers
-        headers['Content-Type'] = 'application/json'
-        headers['x-xdr-auth-id'] = '2'
-        headers['accept'] = 'application/json'
 
-        response = self._http_request('POST', 'public_api/v1/assets/get_external_ip_address_ranges/', headers=headers)
+        response = self._http_request('POST', '/assets/get_external_ip_address_ranges/',
+                                      json_data=data, headers=headers)
 
         return response
 
     def getexternalipaddressrange_request(self, range_id_list):
-        data = {"request_data": {"range_id_list": range_id_list}}
+        data = {"request_data": {"range_id_list": ["1093124c-ce26-33ba-8fb8-937fecb4c7b6"]}}
         headers = self._headers
-        headers['Content-Type'] = 'application/json'
-        headers['x-xdr-auth-id'] = '2'
-        headers['accept'] = 'application/json'
 
-        response = self._http_request(
-            'POST', 'public_api/v1/assets/get_external_ip_address_range/', json_data=data, headers=headers)
+        response = self._http_request('POST', '/assets/get_external_ip_address_range/',
+                                      json_data=data, headers=headers)
 
         return response
 
     def getassetsinternetexposure_request(self):
+        data = {"request_data": {}}
         headers = self._headers
-        headers['Content-Type'] = 'application/json'
-        headers['x-xdr-auth-id'] = '2'
-        headers['accept'] = 'application/json'
 
-        response = self._http_request('POST', 'public_api/v1/assets/get_assets_internet_exposure/', headers=headers)
+        response = self._http_request('POST', '/assets/get_assets_internet_exposure/',
+                                      json_data=data, headers=headers)
 
         return response
 
     def getassetinternetexposure_request(self, asm_id_list):
-        data = {"request_data": {"asm_id_list": asm_id_list}}
+        data = {"request_data": {"asm_id_list": ["e06f15ce-9ab1-3460-8a31-000ac6d2d37e"]}}
         headers = self._headers
-        headers['Content-Type'] = 'application/json'
-        headers['x-xdr-auth-id'] = '2'
-        headers['accept'] = 'application/json'
 
-        response = self._http_request(
-            'POST', 'public_api/v1/assets/get_asset_internet_exposure/', json_data=data, headers=headers)
+        response = self._http_request('POST', '/assets/get_asset_internet_exposure/',
+                                      json_data=data, headers=headers)
 
         return response
 
@@ -166,19 +154,36 @@ def main() -> None:
 
     params: Dict[str, Any] = demisto.params()
     args: Dict[str, Any] = demisto.args()
-    url = params.get('url')
+    #url = params.get('url')
     verify_certificate: bool = not params.get('insecure', False)
     proxy = params.get('proxy', False)
 
-    headers = {}
-    headers['Authorization'] = f'{params["api_key"]}'
+    #headers = {}
+    #headers['Authorization'] = f'{params["api_key"]}'
 
     command = demisto.command()
     demisto.debug(f'Command being called is {command}')
 
     try:
         requests.packages.urllib3.disable_warnings()
-        client: Client = Client(urljoin(url, ''), verify_certificate, proxy, headers=headers, auth=None)
+        #client: Client = Client(urljoin(url, ''), verify_certificate, proxy, headers=headers, auth=None)
+        # ADDED
+        headers = {
+            "HOST": demisto.getLicenseCustomField("Core.ApiHostName"),
+            demisto.getLicenseCustomField("Core.ApiHeader"): demisto.getLicenseCustomField("Core.ApiKey"),
+            "Content-Type": "application/json"
+        }
+        url_suffix = "/public_api/v1"
+        url = "http://" + demisto.getLicenseCustomField("Core.ApiHost") + "/api/webapp/"
+        #url = "https://api-" + demisto.getLicenseCustomField("Core.ApiHostName")
+        add_sensitive_log_strs(demisto.getLicenseCustomField("Core.ApiKey"))
+        base_url = urljoin(url, url_suffix)
+        client = Client(
+            base_url=base_url,
+            verify=verify_certificate,
+            headers=headers,
+            proxy=proxy,
+            auth=None)
 
         commands = {
             'asm-getexternalservices': getexternalservices_command,
