8a66d00241954566d386b7ff7e8916ba95666a61
israelpolishook <ipolishuk@paloaltonetworks.com>
2022-08-31 12:14:31 +0300

    commit

--- Packs/ApiModules/Scripts/CrowdStrikeApiModule/CrowdStrikeApiModule.py
+++ Packs/ApiModules/Scripts/CrowdStrikeApiModule/CrowdStrikeApiModule.py
@@ -118,7 +118,6 @@ class CrowdStrikeClient(BaseClient):
             # new token is needed
             auth_token = self._generate_token()
         else:
-            demisto.info(ctx.get('generation_time'))
             time_passed = now - dateparser.parse(ctx.get('generation_time'))
             if time_passed < TOKEN_LIFE_TIME:
                 # token hasn't expired
