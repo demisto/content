682241e64a85185345f214a7bb4ba8e918fb17fc
dorschw <81086590+dorschw@users.noreply.github.com>
2022-09-01 16:08:49 +0300

    mypyyy

--- Tests/scripts/collect_tests/exceptions.py
+++ Tests/scripts/collect_tests/exceptions.py
@@ -21,7 +21,7 @@ class NonexistentPackException(UnsupportedPackException):
 
 
 class NonXsoarSupportedPackException(UnsupportedPackException):
-    def __init__(self, pack_name: str, support_level:str):
+    def __init__(self, pack_name: str, support_level: str):
         self.support_level = support_level
         super().__init__(pack_name, f'pack support level is not XSOAR (it is {support_level})')
 
