dcd6d09deb05cec85447946aa78c27f6be35d179
Judah Schwartz <juschwartz@paloaltonetworks.com>
2022-09-06 14:46:48 +0300

    moved to main method

--- Packs/CuckooSandbox/Scripts/CuckooGetReport/CuckooGetReport.py
+++ Packs/CuckooSandbox/Scripts/CuckooGetReport/CuckooGetReport.py
@@ -1,4 +1,11 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-demisto.results(demisto.executeCommand("cuckoo-get-task-report", {"id": demisto.args()["taskID"]}))
+
+def main():
+    demisto.results(demisto.executeCommand("cuckoo-get-task-report", {"id": demisto.args()["taskID"]}))
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
+    
--- Packs/CuckooSandbox/Scripts/CuckooGetScreenshot/CuckooGetScreenshot.py
+++ Packs/CuckooSandbox/Scripts/CuckooGetScreenshot/CuckooGetScreenshot.py
@@ -1,4 +1,10 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-demisto.results(demisto.executeCommand("cuckoo-task-screenshot", demisto.args()))
+
+def main():
+    demisto.results(demisto.executeCommand("cuckoo-task-screenshot", demisto.args()))
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
