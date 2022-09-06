5b4ce3cb7261c36dd3e568b309dc6984071749e4
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2022-09-06 17:16:22 +0300

    Update TextFromHTML to support python3

--- Packs/CommonScripts/Scripts/TextFromHTML/TextFromHTML.py
+++ Packs/CommonScripts/Scripts/TextFromHTML/TextFromHTML.py
@@ -5,10 +5,10 @@ import re
 
 
 def text_from_html(args):
-    html = args['html'].encode('utf-8')
-    body = re.search(ur'<body.*/body>', html, re.M + re.S + re.I + re.U)
+    html = args['html']
+    body = re.search(r'<body.*/body>', html, re.M + re.S + re.I + re.U)
     if body and body.group(0):
-        data = re.sub(ur'<.*?>', '', body.group(0))
+        data = re.sub(r'<.*?>', '', body.group(0))
         entities = {'quot': '"', 'amp': '&', 'apos': "'", 'lt': '<', 'gt': '>', 'nbsp': ' ',
                     'copy': '(C)', 'reg': '(R)', 'tilde': '~', 'ldquo': '"', 'rdquo': '"', 'hellip': '...'}
         for e in entities:
