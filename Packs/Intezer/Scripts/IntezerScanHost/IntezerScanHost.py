957aa5b041d53b11a2485e0c93d62d0eae7694ab
glicht <glicht@users.noreply.github.com>
2022-09-06 12:17:52 +0300

    use main

--- Packs/Intezer/Scripts/IntezerScanHost/IntezerScanHost.py
+++ Packs/Intezer/Scripts/IntezerScanHost/IntezerScanHost.py
@@ -3,19 +3,26 @@ from CommonServerPython import *
 from CommonServerUserPython import *
 import uuid
 
-demisto.executeCommand('createNewIndicator', {'value': demisto.args()['host'], 'type': 'hostname'})
-endpoint_analysis_id = str(uuid.uuid4())
-scanner_result = demisto.executeCommand('IntezerRunScanner',
-                                        {
-                                            'endpoint_analysis_id': endpoint_analysis_id,
-                                            'api_key': demisto.args()['intezer_api_key'],
-                                            'using': demisto.args()['host']
-                                        })
-if isError(scanner_result[0]):
-    demisto.results(scanner_result)
 
-context_json = {'Intezer.Analysis': {'ID': endpoint_analysis_id,
-                                     'Type': 'Endpoint',
-                                     'Status': 'Created'}}
+def main():
+    demisto.executeCommand('createNewIndicator', {'value': demisto.args()['host'], 'type': 'hostname'})
+    endpoint_analysis_id = str(uuid.uuid4())
+    scanner_result = demisto.executeCommand('IntezerRunScanner',
+                                            {
+                                                'endpoint_analysis_id': endpoint_analysis_id,
+                                                'api_key': demisto.args()['intezer_api_key'],
+                                                'using': demisto.args()['host']
+                                            })
+    if isError(scanner_result[0]):
+        demisto.results(scanner_result)
 
-return_outputs('Endpoint analysis created', context_json)
+    context_json = {'Intezer.Analysis': {'ID': endpoint_analysis_id,
+                                        'Type': 'Endpoint',
+                                        'Status': 'Created'}}
+
+    return_outputs('Endpoint analysis created', context_json)
+
+
+# python2 uses __builtin__ python3 uses builtins
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
