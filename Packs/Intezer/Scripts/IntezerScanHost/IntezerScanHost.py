b39aa418e673b89a0f1f52717d25914c07c907a7
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2022-09-06 15:18:05 +0300

    Update Packs/Intezer/Scripts/IntezerScanHost/IntezerScanHost.py
    Co-authored-by: tkatzir <tkatzir@paloaltonetworks.com>

--- Packs/Intezer/Scripts/IntezerScanHost/IntezerScanHost.py
+++ Packs/Intezer/Scripts/IntezerScanHost/IntezerScanHost.py
@@ -24,5 +24,5 @@ def main():
 
 
 # python2 uses __builtin__ python3 uses builtins
-if __name__ == "__builtin__" or __name__ == "builtins":
+if __name__ in ['__main__', 'builtin', 'builtins']:
     main()
