b10dcba8a0533fe7618b79df6ecf802259443a50
Darya Koval <dkoval@paloaltonetworks.com>
2022-09-06 19:11:53 +0300

    save

---
+++ Packs/CommonScripts/Scripts/RunDockerCommand/RunDockerCommand.py
@@ -0,0 +1,33 @@
+import subprocess
+
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+ret = []
+sysargs = None
+
+cmdarg = demisto.args()['cmd']
+if 'sysargs' in demisto.args():
+    sysargs = demisto.args()['sysargs']
+
+if sysargs is None:
+    output = None
+    try:
+        output = subprocess.check_output(cmdarg, shell=True)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+    ret.append(output)
+else:
+    ret.append(subprocess.check_output([cmdarg, sysargs]))
+
+ret = [r.decode() for r in ret]     # type:ignore[union-attr]
+ec = {'Command': cmdarg, 'Results': ret[0]}
+
+demisto.results({
+    'Type': entryTypes['note'],
+    'ContentsFormat': formats['json'],
+    'Contents': ret,
+    'HumanReadable': ret[0],
+    'EntryContext': {'CommandResults': ec}
+})
+sys.exit(0)
