a8edfde699ce3d94f6d8d23e988a3cfd017813cc
Judah Schwartz <juschwartz@paloaltonetworks.com>
2022-09-06 17:20:34 +0300

    added test file

---
+++ Packs/D2/Scripts/Autoruns/Autoruns_test.py
@@ -0,0 +1,32 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+respAR = demisto.executeCommand('D2Autoruns', {'using': demisto.args()['system']})
+if isError(respAR[0]):
+    demisto.results(respAR)
+else:
+    hashes = []
+    try:
+        try:
+            lines = respAR[0]['Contents'][2:].decode('utf-16').encode('ascii').split('\r\n')
+        except Exception:
+            lines = respAR[0]['Contents'].split('\r\n')
+        headers = lines[5].replace('\t', '|')
+        try:
+            hashCol = headers.split('|').index('MD5')
+        except ValueError:
+            hashCol = -1
+        mdTable = headers + '\n'
+        mdTable += '|'.join('---' * len(headers.split('|'))) + '\n'
+        for l in lines[6:]:
+            if hashCol > -1:
+                cells = l.split('\t')
+                if hashCol < len(cells) and cells[hashCol].strip():
+                    hashes.append(cells[hashCol])
+            mdTable += l.replace('\t', '|') + '\n'
+        if hashes:
+            appendContext('md5s', ', '.join(hashes), dedup=True)
+        demisto.results({'Type': entryTypes['note'], 'ContentsFormat': formats['markdown'], 'Contents': mdTable})
+    except Exception as ex:
+        demisto.results({'Type': entryTypes['error'], 'ContentsFormat': formats['text'],
+                        'Contents': "Error occurred while parsing output from D2Autoruns:\n" + str(ex) + '\n\nAutoruns output:\n' + respAR[0]['Contents']})
