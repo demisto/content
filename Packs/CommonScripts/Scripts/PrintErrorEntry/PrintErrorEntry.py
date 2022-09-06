752904def75a5834fa75b929e70a23ef5f79a553
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2022-09-06 12:35:18 +0300

    Update PrintErrorEntry.py

--- Packs/CommonScripts/Scripts/PrintErrorEntry/PrintErrorEntry.py
+++ Packs/CommonScripts/Scripts/PrintErrorEntry/PrintErrorEntry.py
@@ -3,7 +3,11 @@ from CommonServerPython import *  # noqa: F401
 
 
 def main():
-    return_error(demisto.getArg("message"))
+    return_results({
+        'Type' : EntryType.ERROR,
+        'ContentsFormat' : EntryFormat.TEXT,
+        'Contents' : demisto.getArg("message")
+    })
 
 
 if __name__ in ["__builtin__", "builtins"]:
