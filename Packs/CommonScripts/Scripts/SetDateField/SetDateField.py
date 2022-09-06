3d987d36e47f26d5343dfaa187fa308534050288
kgal-pan <kgal@paloaltonetworks.com>
2022-09-06 12:17:57 +0300

    added try/catch block

--- Packs/CommonScripts/Scripts/SetDateField/SetDateField.py
+++ Packs/CommonScripts/Scripts/SetDateField/SetDateField.py
@@ -3,10 +3,14 @@ from CommonServerPython import *  # noqa: F401
 from time import strftime
 
 def main():
-	field_name = demisto.args().get('fieldName')
-	t = strftime("%a, %d %b %Y %H:%M:%S %Z")
-	res = demisto.executeCommand("setIncident", {field_name: t})
-	return_results(res)
+
+	try:
+		field_name = demisto.args().get('fieldName')
+		t = strftime("%a, %d %b %Y %H:%M:%S %Z")
+		res = demisto.executeCommand("setIncident", {field_name: t})
+		return_results(res)
+	except Exception as e:
+		return_error(f"Failed to execute SetDateField error when running setIncident {field_name}={t}: {str(e)}")
 
 
 """ ENTRY POINT """
