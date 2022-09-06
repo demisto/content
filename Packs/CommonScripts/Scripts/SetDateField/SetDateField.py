f532c1bbf22a9aae6485b106eb9dfa6c61bbd7bd
kgal-pan <kgal@paloaltonetworks.com>
2022-09-06 12:19:16 +0300

    fixed flake8 issues

--- Packs/CommonScripts/Scripts/SetDateField/SetDateField.py
+++ Packs/CommonScripts/Scripts/SetDateField/SetDateField.py
@@ -3,17 +3,14 @@ from CommonServerPython import *  # noqa: F401
 from time import strftime
 
 def main():
+    try:
+        field_name = demisto.args().get('fieldName')
+        t = strftime("%a, %d %b %Y %H:%M:%S %Z")
+        res = demisto.executeCommand("setIncident", {field_name: t})
+        return_results(res)
+    except Exception as e:
+        return_error(f"Failed to execute SetDateField error when running setIncident {field_name}={t}: {str(e)}")
 
-	try:
-		field_name = demisto.args().get('fieldName')
-		t = strftime("%a, %d %b %Y %H:%M:%S %Z")
-		res = demisto.executeCommand("setIncident", {field_name: t})
-		return_results(res)
-	except Exception as e:
-		return_error(f"Failed to execute SetDateField error when running setIncident {field_name}={t}: {str(e)}")
-
-
-""" ENTRY POINT """
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
