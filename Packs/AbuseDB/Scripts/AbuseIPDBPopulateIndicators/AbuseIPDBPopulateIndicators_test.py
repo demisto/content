a436106615218e5ebe24ca8c3c5f1e9c14660a24
Shmuel Fainberg <sfainberg@paloaltonetworks.com>
2022-09-07 17:12:28 +0300

    UT added

---
+++ Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+"""HELPER FUNCTIONS"""
+
+
+def test_get_contents():
+    from AbuseIPDBPopulateIndicators import get_contents
+
+    assert get_contents({}) is None
+
+
+@pytest.mark.parametrize("input", ["Too many requests", None])
+def test_check_ips_fail(input):
+    from AbuseIPDBPopulateIndicators import check_ips
+
+    with pytest.raises(SystemExit) as e:
+        check_ips(input)
+    assert str(e.typename) == "SystemExit"
+
+
+def test_check_ips(input=[True]):
+    from AbuseIPDBPopulateIndicators import check_ips
+
+    assert check_ips(input) is None
