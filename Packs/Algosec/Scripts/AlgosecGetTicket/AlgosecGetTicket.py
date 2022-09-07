89ecb1798b54f6575818c5874a70ec777a080766
YuvHayun <yhayun@paloaltonetworks.com>
2022-09-07 10:34:06 +0300

    aa

--- Packs/Algosec/Scripts/AlgosecGetTicket/AlgosecGetTicket.py
+++ Packs/Algosec/Scripts/AlgosecGetTicket/AlgosecGetTicket.py
@@ -1,20 +1,36 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-resp = demisto.executeCommand("algosec-get-ticket", demisto.args())
 
-if isError(resp[0]):
-    demisto.results(resp)
-else:
-    data = demisto.get(resp[0], "Contents.getTicketResponse")
-    if data:
-        raiseTable(data, 'ticket')
-        for key in data:
-            if isinstance(data[key], dict):
-                if '-xmlns' in data[key]:
-                    del data[key]['-xmlns']
-                data[key] = zoomField(data[key], '#text')
-        data = flattenRow(data)
-        demisto.results({"ContentsFormat": formats["table"], "Type": entryTypes["note"], "Contents": data})
+def algosec_get_ticket():
+    resp = demisto.executeCommand("algosec-get-ticket", demisto.args())
+
+    if isError(resp[0]):
+        return CommandResults(raw_response=resp)
     else:
-        demisto.results("No results.")
+        data = demisto.get(resp[0], "Contents.getTicketResponse")
+        if data:
+            raiseTable(data, 'ticket')
+            for key in data:
+                if isinstance(data[key], dict):
+                    if '-xmlns' in data[key]:
+                        del data[key]['-xmlns']
+                    data[key] = zoomField(data[key], '#text')
+            data = flattenRow(data)
+            return CommandResults(content_format=formats["table"],
+                                  entry_type=entryTypes["note"],
+                                  raw_response=data)
+        else:
+            return "No results."
+
+
+def main():  # pragma: no cover
+    try:
+        return_results(algosec_get_ticket())
+    except Exception as e:
+        err_msg = f'Encountered an error while running the script: [{e}]'
+        return_error(err_msg, error=e)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
--- Packs/Algosec/Scripts/AlgosecGetTicket/AlgosecGetTicket_test.py
+++ Packs/Algosec/Scripts/AlgosecGetTicket/AlgosecGetTicket_test.py
@@ -1,7 +1,18 @@
 import demistomock as demisto
 
 
-def test_AlgosecGetTicket(mocker):
-    ticket = [{"Contents": {"getTicketResponse": "my_Data"}}]
+def test_AlgosecGetTicket_success(mocker):
+    """
+    Given:
+        - response mock.
+    When:
+        - running AlgosecGetTicket script.
+    Then:
+        - Ensure that the results were built correctly.
+    """
+    from AlgosecGetTicket import algosec_get_ticket
+    ticket = [{"Type": 3, "Contents": {"getTicketResponse": {"some_info": {"info": "test"}}}}]
     mocker.patch.object(demisto, 'executeCommand', return_value=ticket)
-    import AlgosecGetTicket
+    res = algosec_get_ticket()
+    content = res.raw_response
+    assert {'some_info': 'info: test'} == content
