a3e0d76c4ea710838e1868bc30f67ec1f6f8e308
okarkkatz <okarkkatz@paloaltonetworks.com>
2022-09-07 18:57:16 +0300

    added unit tests and test data:

---
+++ Packs/CommonScripts/Scripts/PcapHTTPExtractor/PcapHTTPExtractor_test.py
@@ -0,0 +1,21 @@
+import demistomock as demisto
+import PcapHTTPExtractor
+
+
+def open_file(path):
+    with open(path) as f:
+        return f.read()
+
+
+def test_PcapHTTPExtractor(mocker):
+
+    mocker.patch.object(demisto, 'args',
+                        return_value={"entryID": "6@1", "limit": "50", "start": "0", "limitData": "512"})
+    mocker.patch.object(demisto, 'executeCommand',
+                        return_value=[{'Contents': {'path': 'Test_data/TestPcapPost.pcap'}, 'Type': ''}])
+    demisto_res = mocker.patch.object(demisto, 'results')
+    PcapHTTPExtractor.main()
+
+    args_to_demisto_res = demisto_res.call_args.args[0]
+    assert args_to_demisto_res.get('Contents') == open_file('Test_data/readable_output.md')
+    assert len(args_to_demisto_res.get('EntryContext', {}).get('PcapHTTPFlows', {})) == 2
