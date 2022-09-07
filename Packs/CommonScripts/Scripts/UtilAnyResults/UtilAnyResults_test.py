3b24d86ce391e142226a4c734afcb1c219efbfa6
nmaimon <nmaimon@paloaltonetworks.com>
2022-09-07 17:22:12 +0300

    y

--- Packs/CommonScripts/Scripts/UtilAnyResults/UtilAnyResults_test.py
+++ Packs/CommonScripts/Scripts/UtilAnyResults/UtilAnyResults_test.py
@@ -1,27 +1,27 @@
 from UtilAnyResults import *
 
 
-def test_ustil_any_results_list(mocker):
+def test_util_any_results_list(mocker):
     mocker.patch.object(demisto, 'get', return_value=['a', 'b', 'c'])
     assert util_any_results({}) == ['yes']
 
 
-def test_ustil_any_results_str(mocker):
+def test_util_any_results_str(mocker):
     mocker.patch.object(demisto, 'get', return_value='abc')
     assert util_any_results({}) == ['yes']
 
 
-def test_ustil_any_results_str_lst(mocker):
+def test_util_any_results_str_lst(mocker):
     mocker.patch.object(demisto, 'get', return_value='[a, b, c]')
     assert util_any_results({}) == ['yes']
 
 
-def test_ustil_any_results_str_empty_lst(mocker):
+def test_util_any_results_str_empty_lst(mocker):
     mocker.patch.object(demisto, 'get', return_value='[]')
     assert util_any_results({}) == ['no']
 
 
-def test_ustil_any_results_str_empty_lst(mocker):
+def test_util_any_results_str_empty_lst(mocker):
     mocker.patch.object(demisto, 'get', return_value=1)
     assert util_any_results({}) == ['no']
 
