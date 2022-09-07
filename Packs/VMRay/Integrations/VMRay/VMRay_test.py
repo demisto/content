638156514cefdedb94839ca76ad9140873e4299f
gal-forer <gforer@paloaltonetworks.com>
2022-09-07 12:20:00 +0300

    Added unit tests

--- Packs/VMRay/Integrations/VMRay/VMRay_test.py
+++ Packs/VMRay/Integrations/VMRay/VMRay_test.py
@@ -49,3 +49,76 @@ def test_encoding_file_name(mocker):
     from VMRay import encode_file_name
 
     assert encode_file_name(file_name) == expected_output.encode('ascii', 'ignore')
+
+
+def test_is_json(mocker):
+    mocker.patch.object(demisto, 'params', return_value={"api_key": "123456", "server": "https://cloud.vmray.com/",
+                                                         'shareable': False, 'reanalyze': False})
+    from VMRay import is_json
+    from requests.models import Response
+
+    response = Response()
+    response._content = b'{ "key" : "a" }'
+    assert is_json(response)
+    response._content = b'{ "key" : a }'
+    assert not is_json(response)
+
+
+def test_check_id(mocker):
+    mocker.patch.object(demisto, 'params', return_value={"api_key": "123456", "server": "https://cloud.vmray.com/",
+                                                         'shareable': False, 'reanalyze': False})
+    from VMRay import check_id
+
+    assert check_id(5)
+
+
+def test_build_errors_string(mocker):
+    mocker.patch.object(demisto, 'params', return_value={"api_key": "123456", "server": "https://cloud.vmray.com/",
+                                                         'shareable': False, 'reanalyze': False})
+    from VMRay import build_errors_string
+    assert build_errors_string('Error') == 'Error'
+    assert build_errors_string([{'error_msg': 'Error'}, {'error_msg': 'Another error'}]) == 'Error.\nAnother error.\n'
+    assert build_errors_string({'error_msg': 'Error'}) == 'Error'
+
+
+def test_dbot_score_by_hash(mocker):
+    mocker.patch.object(demisto, 'params', return_value={"api_key": "123456", "server": "https://cloud.vmray.com/",
+                                                         'shareable': False, 'reanalyze': False})
+    from VMRay import dbot_score_by_hash
+
+    assert dbot_score_by_hash({'MD5': '0322ea0cb2fcfa4281cf7804c8f553d1'}) == [
+        {'Indicator': '0322ea0cb2fcfa4281cf7804c8f553d1',
+         'Reliability': 'C - Fairly reliable',
+         'Score': None,
+         'Type': 'hash',
+         'Vendor': 'VMRay'}]
+
+
+def test_build_job_data(mocker):
+    mocker.patch.object(demisto, 'params', return_value={"api_key": "123456", "server": "https://cloud.vmray.com/",
+                                                         'shareable': False, 'reanalyze': False})
+    from VMRay import build_job_data
+
+    entry = {'job_id': 'test', 'job_sample_id': 'test',
+             'job_submission_id': 'test', 'job_sample_md5': 'test',
+             'job_sample_sha1': 'test', 'job_sample_sha256': 'test',
+             'job_sample_ssdeep': 'test', 'job_vm_name': 'test',
+             'job_vm_id': 'test', 'job_status': 'test'}
+    assert build_job_data(entry) == {'JobID': 'test',
+                                     'MD5': 'test',
+                                     'SHA1': 'test',
+                                     'SHA256': 'test',
+                                     'SSDeep': 'test',
+                                     'SampleID': 'test',
+                                     'Status': 'test',
+                                     'SubmissionID': 'test',
+                                     'VMID': 'test',
+                                     'VMName': 'test'}
+
+
+def test_build_finished_job(mocker):
+    mocker.patch.object(demisto, 'params', return_value={"api_key": "123456", "server": "https://cloud.vmray.com/",
+                                                         'shareable': False, 'reanalyze': False})
+    from VMRay import build_finished_job
+
+    assert build_finished_job('test', 'test') == {'JobID': 'test', 'SampleID': 'test', 'Status': 'Finished/NotExists'}
