6f70843149f055b06bc5e2ba3fe1d9c6ba01bf69
Darya Koval <dkoval@paloaltonetworks.com>
2022-09-07 18:11:17 +0300

    moving the code incide main

--- Packs/CommonScripts/Scripts/RunDockerCommand/RunDockerCommand.py
+++ Packs/CommonScripts/Scripts/RunDockerCommand/RunDockerCommand.py
@@ -3,31 +3,37 @@ import subprocess
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-ret = []
-sysargs = None
 
-cmdarg = demisto.args()['cmd']
-if 'sysargs' in demisto.args():
-    sysargs = demisto.args()['sysargs']
+def main():
+    ret = []
+    sysargs = None
 
-if sysargs is None:
-    output = None
-    try:
-        output = subprocess.check_output(cmdarg, shell=True)
-    except subprocess.CalledProcessError as e:
-        output = e.output
-    ret.append(output)
-else:
-    ret.append(subprocess.check_output([cmdarg, sysargs]))
+    cmdarg = demisto.args()['cmd']
+    if 'sysargs' in demisto.args():
+        sysargs = demisto.args()['sysargs']
 
-ret = [r.decode() for r in ret]     # type:ignore[union-attr]
-ec = {'Command': cmdarg, 'Results': ret[0]}
+    if sysargs is None:
+        output = None
+        try:
+            output = subprocess.check_output(cmdarg, shell=True)
+        except subprocess.CalledProcessError as e:
+            output = e.output
+        ret.append(output)
+    else:
+        ret.append(subprocess.check_output([cmdarg, sysargs]))
 
-demisto.results({
-    'Type': entryTypes['note'],
-    'ContentsFormat': formats['json'],
-    'Contents': ret,
-    'HumanReadable': ret[0],
-    'EntryContext': {'CommandResults': ec}
-})
-sys.exit(0)
+    ret = [r.decode() for r in ret]     # type:ignore[union-attr]
+    ec = {'Command': cmdarg, 'Results': ret[0]}
+
+    demisto.results({
+        'Type': entryTypes['note'],
+        'ContentsFormat': formats['json'],
+        'Contents': ret,
+        'HumanReadable': ret[0],
+        'EntryContext': {'CommandResults': ec}
+    })
+    sys.exit(0)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
