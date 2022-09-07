80ad9716c31d4ed4c30caf48f8fd94093873f2ed
Shmuel Fainberg <sfainberg@paloaltonetworks.com>
2022-09-07 13:52:43 +0300

    changed to python3 + code conventions

--- Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators.py
+++ Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators.py
@@ -2,25 +2,47 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 BLACKLISTED = "Bad"
-res = demisto.executeCommand("abuseipdb-get-blacklist", {
-    "days": demisto.args().get("days"),
-    "limit": demisto.args().get("limit"),
-    "confidence": demisto.args().get("confidence")
-})
+args = demisto.args()
 
-ips = res[0]['Contents']
 
-if not ips or "Too many requests" in ips:
-    return_error("No Indicators were created (possibly bad API key)")
+def get_contents():
+    """Get IP addresses on block lists from AbuseIPDB
 
-# Extract IPs into new Indicators
-for ip in ips:
-    demisto.executeCommand("createNewIndicator", {
-        "type": 'ip',
-        "value": ip,
-        "source": 'AbuseIPDB',
-        "reputation": BLACKLISTED,
-        "seenNow": "true",
-    })
+    :return: dictionary containing block lists
+    :rtype: ``dict[str, Any]``
+    """
+    res = demisto.executeCommand(
+        "abuseipdb-get-blacklist",
+        {
+            "days": args.get("days"),
+            "limit": args.get("limit"),
+            "confidence": args.get("confidence"),
+        },
+    )
+    return res[0].get("Contents")
 
-demisto.results("All Indicators were created successfully")
+
+def main():
+    ips = get_contents()
+
+    if not ips or "Too many requests" in ips:
+        return_error("No Indicators were created (possibly bad API key)")
+
+    # Extract IPs into new Indicators
+    for ip in ips:
+        demisto.executeCommand(
+            "createNewIndicator",
+            {
+                "type": "ip",
+                "value": ip,
+                "source": "AbuseIPDB",
+                "reputation": BLACKLISTED,
+                "seenNow": "true",
+            },
+        )
+
+    demisto.results("All Indicators were created successfully")
+
+
+if __name__ == "__main__":
+    main()
