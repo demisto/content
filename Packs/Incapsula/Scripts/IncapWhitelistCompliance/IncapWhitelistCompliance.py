45670f52e15814e7055ef3d849ff8e83af829604
okarkkatz <okarkkatz@paloaltonetworks.com>
2022-09-07 12:08:24 +0300

    converted to python3 syntax

--- Packs/Incapsula/Scripts/IncapWhitelistCompliance/IncapWhitelistCompliance.py
+++ Packs/Incapsula/Scripts/IncapWhitelistCompliance/IncapWhitelistCompliance.py
@@ -24,20 +24,6 @@ please update your settings as soon as possible!
 
 --DBot
 '''
-URL_REGEX = '(?i)(?:\\([-A-Z0-9+&@#\\/%=~_|$?!:,.]*\\)|[-A-Z0-9+&@#\\/%=~_|$?!:,.])*(?:\\([-A-Z0-9+&@#\\/%=~_|$?!:,.]*\\)|[A-Z0-9+&@#\\/%=~_|$])'
-
-res = []
-res_errors = []
-
-ctx = demisto.context()
-compliance_table = demisto.get(ctx, COMPLIANCE_LABEL)
-if not compliance_table:
-    compliance_table = {}
-
-dArgs = {"ssh_server": demisto.args()["SSHValidationServer"]}
-
-# Calling a command - returns a list of one or more entries
-resp = demisto.executeCommand("incap-list-sites", {})
 
 
 def sendMail(to, subject, body="", bcc=""):
@@ -46,7 +32,7 @@ def sendMail(to, subject, body="", bcc=""):
 
 def escalation(url, severity, owner_mail):
     if isinstance(owner_mail, list):
-        owner_mail = ''.join(map(lambda x: str(x) + ', ', owner_mail[:-1]) + [str(owner_mail[-1])])
+        owner_mail = ''.join(list(map(lambda x: f'{x}, ', owner_mail[:-1])) + [str(owner_mail[-1])])
 
     if severity == 0:
         return None
@@ -56,46 +42,65 @@ def escalation(url, severity, owner_mail):
         return sendMail(MAIL_TO_RECIPIENT, INCAPSULA_WARNING, BASIC_WARNING_MAIL.format(url), owner_mail)
 
 
-if isError(resp[0]):
-    demisto.results(resp)
-else:
-    data = demisto.get(resp[0], "Contents.sites")
-    for site in data:
-        domain = site['domain']
-        if not re.match(URL_REGEX, str(domain)):
-            res_errors.append(str(domain) + ' - is not a valid url')
-        else:
-            site_id = site['site_id']
+def main():
+    URL_REGEX = '(?i)(?:\\([-A-Z0-9+&@#\\/%=~_|$?!:,.]*\\)|[-A-Z0-9+&@#\\/%=~_|$?!:,.])*' \
+                '(?:\\([-A-Z0-9+&@#\\/%=~_|$?!:,.]*\\)|[A-Z0-9+&@#\\/%=~_|$])'
 
-            temp_res = demisto.executeCommand(
-                "RemoteExec", {'cmd': CURL_GET_COMMAND.format(str(domain)), 'system': dArgs["ssh_server"]})
-            if not isError(temp_res[0]):
-                temp_data = demisto.get(temp_res[0], "Contents")
-                if temp_data.find(HTTP_OK) == -1:
-                    # remove from non copliance table
-                    pass
-                else:
-                    if compliance_table.has_key(domain):
-                        compliance_table[domain] += 1
+    res = []
+    res_errors = []
+
+    ctx = demisto.context()
+    compliance_table = demisto.get(ctx, COMPLIANCE_LABEL)
+    if not compliance_table:
+        compliance_table = {}
+
+    dArgs = {"ssh_server": demisto.args()["SSHValidationServer"]}
+
+    # Calling a command - returns a list of one or more entries
+    resp = demisto.executeCommand("incap-list-sites", {})
+
+    if isError(resp[0]):
+        demisto.results(resp)
+    else:
+        data = demisto.get(resp[0], "Contents.sites")
+        for site in data:
+            domain = site['domain']
+            if not re.match(URL_REGEX, str(domain)):
+                res_errors.append(str(domain) + ' - is not a valid url')
+
+                temp_res = demisto.executeCommand(
+                    "RemoteExec", {'cmd': CURL_GET_COMMAND.format(str(domain)), 'system': dArgs["ssh_server"]})
+                if not isError(temp_res[0]):
+                    temp_data = demisto.get(temp_res[0], "Contents")
+                    if temp_data.find(HTTP_OK) == -1:
+                        # remove from non copliance table
+                        pass
                     else:
-                        compliance_table[domain] = 1
-                    temp_res = demisto.executeCommand("incap-get-domain-approver-email", {'domain': domain})
-                    if isError(temp_res[0]):
-                        res_errors.append(str(temp_res))
-                    else:
-                        emails = demisto.get(temp_res[0], "Contents.domain_emails")
-                        if emails:
-                            temp_res = escalation(domain, compliance_table[domain], emails)
-                            if isError(temp_res[0]):
-                                res_errors.append(str(temp_res))
+                        if domain in compliance_table:
+                            compliance_table[domain] += 1
                         else:
-                            res_errors.append(str(temp_res[0]))
+                            compliance_table[domain] = 1
+                        temp_res = demisto.executeCommand("incap-get-domain-approver-email", {'domain': domain})
+                        if isError(temp_res[0]):
+                            res_errors.append(str(temp_res))
+                        else:
+                            emails = demisto.get(temp_res[0], "Contents.domain_emails")
+                            if emails:
+                                temp_res = escalation(domain, compliance_table[domain], emails)
+                                if isError(temp_res[0]):
+                                    res_errors.append(str(temp_res))
+                            else:
+                                res_errors.append(str(temp_res[0]))
 
-    demisto.setContext(COMPLIANCE_LABEL, compliance_table)
+        demisto.setContext(COMPLIANCE_LABEL, compliance_table)
 
-    markdownString = '## Incapsula Whitelist Compliance - validation results\n'
-    markdownString += tblToMd("Non-Compliant Sites - number of days not compliant", [compliance_table])
+        markdownString = '## Incapsula Whitelist Compliance - validation results\n'
+        markdownString += tblToMd("Non-Compliant Sites - number of days not compliant", [compliance_table])
 
-    res.append({'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': markdownString})
+        res.append({'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': markdownString})
 
-demisto.results(res)
+    demisto.results(res)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
