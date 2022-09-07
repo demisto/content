fc06b7106182e1a6bd433c460c04284f2bac204e
yucohen <yucohen@paloaltonetworks.com>
2022-09-07 11:01:23 +0300

    splited scripts and added main

---
+++ Packs/Algosec/Scripts/AlgosecCreateTicket/AlgosecCreateTicket.py
@@ -0,0 +1,34 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+
+def algosec_create_ticket():
+    resp = demisto.executeCommand("algosec-create-ticket", demisto.args())
+
+    if isError(resp[0]):
+        demisto.results(resp)
+    else:
+        data = demisto.get(resp[0], "Contents.createTicketResponse")
+        if data:
+            data = data if isinstance(data, list) else [data]
+            for item in data:
+                for row in item:
+                    if '#text' in item[row]:
+                        item[row] = item[row]['#text']
+
+            data = flattenTable(data)
+            demisto.results({"ContentsFormat": formats["table"], "Type": entryTypes["note"], "Contents": data})
+        else:
+            demisto.results("No results.")
+
+
+def main():  # pragma: no cover
+    try:
+        return_results(algosec_create_ticket())
+    except Exception as e:
+        err_msg = f'Encountered an error while running the script: [{e}]'
+        return_error(err_msg, error=e)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
---
+++ Packs/Algosec/Scripts/AlgosecQuery/AlgosecQuery.py
@@ -0,0 +1,29 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+
+def algosec_query():
+    resp = demisto.executeCommand("algosec-query", demisto.args())
+
+    if isError(resp[0]):
+        demisto.results(resp)
+    else:
+        data = demisto.get(resp[0], "Contents.QueryResponse.QueryResult")
+        if data:
+            data = data if isinstance(data, list) else [data]
+            data = flattenTable(data)
+            demisto.results({"ContentsFormat": formats["table"], "Type": entryTypes["note"], "Contents": data})
+        else:
+            demisto.results("No results.")
+
+
+def main():  # pragma: no cover
+    try:
+        return_results(algosec_query())
+    except Exception as e:
+        err_msg = f'Encountered an error while running the script: [{e}]'
+        return_error(err_msg, error=e)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
