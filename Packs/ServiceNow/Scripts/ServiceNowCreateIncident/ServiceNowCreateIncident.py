71f459ebd12157a9511bbe5667233199e72edde5
GuyAfik <guyafik11@gmail.com>
2022-09-08 16:50:33 +0300

    remove demisto.log

--- Packs/ServiceNow/Scripts/ServiceNowCreateIncident/ServiceNowCreateIncident.py
+++ Packs/ServiceNow/Scripts/ServiceNowCreateIncident/ServiceNowCreateIncident.py
@@ -157,7 +157,6 @@ def main():
     command_args['using'] = using
 
     command_res = demisto.executeCommand('servicenow-create-record', command_args)
-    demisto.log(command_res)
     result = {}
     try:
         entry = command_res[0]
