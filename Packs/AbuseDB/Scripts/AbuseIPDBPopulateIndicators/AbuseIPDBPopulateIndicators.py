0443cab7a6b908a434abd2fefb91d3765dce088c
Shmuel Fainberg <sfainberg@paloaltonetworks.com>
2022-09-08 11:43:43 +0300

    updated UT

--- Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators.py
+++ Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators.py
@@ -2,15 +2,22 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 BLACKLISTED = "Bad"
+<<<<<<< HEAD
 args = demisto.args()
 
 
 def get_contents():
+=======
+
+
+def get_contents(args: Dict[str, Any]):
+>>>>>>> 34f2d35b74 (updated UT)
     """Get IP addresses on block lists from AbuseIPDB
 
     :return: dictionary containing block lists
     :rtype: ``dict[str, Any]``
     """
+<<<<<<< HEAD
     res = demisto.executeCommand(
         "abuseipdb-get-blacklist",
         {
@@ -42,6 +49,57 @@ def main():
         )
 
     demisto.results("All Indicators were created successfully")
+=======
+
+    res = execute_command(
+        "abuseipdb-get-blacklist",
+        {
+            "days": args.get("days"),
+            "limit": args.get("limit"),
+            "confidence": args.get("confidence"),
+        },
+    )
+
+    if not res:
+        return None
+
+    return res[0].get("Contents", None)
+
+
+def check_ips(ips: dict):
+    """Check 'ips' dictionary validity
+
+    :return: None if valid, else execute return_error()
+    :rtype: ``None``
+    """
+    if not ips or "Too many requests" in ips:
+        raise DemistoException("No Indicators were created (possibly bad API key)")
+
+
+def main():
+    try:
+        args = demisto.args()
+        ips = get_contents(args)
+        check_ips(ips)
+
+        # Extract IPs into new Indicators
+        for ip in ips:
+            execute_command(
+                "createNewIndicator",
+                {
+                    "type": "ip",
+                    "value": ip,
+                    "source": "AbuseIPDB",
+                    "reputation": BLACKLISTED,
+                    "seenNow": "true",
+                },
+            )
+
+        return_results("All Indicators were created successfully")
+
+    except DemistoException as e:
+        return_error(e.message)
+>>>>>>> 34f2d35b74 (updated UT)
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
--- Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators_test.py
+++ Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators_test.py
@@ -1,6 +1,10 @@
 import pytest
 
+<<<<<<< HEAD
 """HELPER FUNCTIONS"""
+=======
+from CommonServerPython import DemistoException
+>>>>>>> 34f2d35b74 (updated UT)
 
 
 def test_get_contents():
@@ -13,9 +17,15 @@ def test_get_contents():
 def test_check_ips_fail(input):
     from AbuseIPDBPopulateIndicators import check_ips
 
+<<<<<<< HEAD
     with pytest.raises(SystemExit) as e:
         check_ips(input)
     assert str(e.typename) == "SystemExit"
+=======
+    with pytest.raises(DemistoException) as e:
+        check_ips(input)
+    assert str(e.value) == "No Indicators were created (possibly bad API key)"
+>>>>>>> 34f2d35b74 (updated UT)
 
 
 def test_check_ips(input=[True]):
