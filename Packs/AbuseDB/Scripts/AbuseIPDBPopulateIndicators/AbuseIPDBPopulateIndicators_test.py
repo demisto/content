085b4abf22ae7ef29851f2cf9e734c8a9290c286
Shmuel Fainberg <sfainberg@paloaltonetworks.com>
2022-09-08 17:52:06 +0300

    Added UT functions documentation

--- Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators_test.py
+++ Packs/AbuseDB/Scripts/AbuseIPDBPopulateIndicators/AbuseIPDBPopulateIndicators_test.py
@@ -8,16 +8,30 @@ import demistomock as demisto
     "return_val, expected", [(None, None), ([{"Contents": ["1.1.1.1"]}], ["1.1.1.1"])]
 )
 def test_get_contents(mocker, return_val, expected):
+    """
+    Given:
+        - All relevant arguments are passed
+    When:
+        - abuseipdb-get-blacklist command is executed in get_contents function
+    Then:
+        - Check if get_contents return value is valid
+    """
     from AbuseIPDBPopulateIndicators import get_contents
 
-    mocker.patch(
-        "AbuseIPDBPopulateIndicators.execute_command", return_value=return_val
-    )
+    mocker.patch("AbuseIPDBPopulateIndicators.execute_command", return_value=return_val)
     assert get_contents({}) == expected
 
 
 @pytest.mark.parametrize("input", ["Too many requests", None])
 def test_check_ips_fail(input):
+    """
+    Given:
+        - Invalid argument is passed
+    When:
+        - check_ips function is executed
+    Then:
+        - Raise DemistoException
+    """
     from AbuseIPDBPopulateIndicators import check_ips
 
     with pytest.raises(DemistoException) as e:
@@ -26,12 +40,28 @@ def test_check_ips_fail(input):
 
 
 def test_check_ips():
+    """
+    Given:
+        - Valid argument is passed
+    When:
+        - check_ips function is executed
+    Then:
+        - Return None
+    """
     from AbuseIPDBPopulateIndicators import check_ips
 
     assert check_ips(["1.1.1.1"]) is None
 
 
 def test_main(mocker):
+    """
+    Given:
+        - All return values from helper functions are valid
+    When:
+        - main function is executed
+    Then:
+        - Return results to War-Room
+    """
     from AbuseIPDBPopulateIndicators import main
 
     mocker.patch.object(
