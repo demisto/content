3e8f52c25e5375793be26e984d3622eaf643c3e1
GuyAfik <guyafik11@gmail.com>
2022-09-08 14:20:15 +0300

    fix uts

--- Packs/ServiceNow/Scripts/ServiceNowCreateIncident/ServiceNowCreateIncident_test.py
+++ Packs/ServiceNow/Scripts/ServiceNowCreateIncident/ServiceNowCreateIncident_test.py
@@ -1,43 +1,6 @@
 from CommonServerPython import *
 
 
-def test_get_incident(mocker):
-    """
-    Given:
-     - an incident data from service now command
-
-    When:
-     - executing get_incident function
-
-    Then:
-     - make sure the incident result key is being retrieved
-    """
-    from ServiceNowUpdateIncident import get_incident
-    mocker.patch.object(demisto, 'executeCommand')
-    mocker.patch.object(demisto, 'get', return_value={'result': 'test'})
-
-    result = get_incident('query')
-    assert result == 'test'
-
-
-def test_get_incident_id(mocker):
-    """
-    Given:
-     - an incident sys-id
-
-    When:
-     - executing get_incident_id function
-
-    Then:
-     - make sure that the incident sys ID is retrieved.
-    """
-    from ServiceNowUpdateIncident import get_incident_id
-    mocker.patch.object(demisto, 'executeCommand')
-    mocker.patch.object(demisto, 'get', return_value={'result': [{'sys_id': '123'}]})
-    incident_sys_id = get_incident_id('123')
-    assert incident_sys_id == '123'
-
-
 def test_get_user(mocker):
     """
     Given:
@@ -49,7 +12,7 @@ def test_get_user(mocker):
     Then:
      - make sure the incident result key is being retrieved
     """
-    from ServiceNowUpdateIncident import get_user
+    from ServiceNowCreateIncident import get_user
     mocker.patch.object(demisto, 'executeCommand')
     mocker.patch.object(demisto, 'get', return_value={'result': 'test'})
 
