ce67f5d7d48bddcaad130d06a6f998b17af2ed66
Menachem Weinfeld <mmhw770@gmail.com>
2022-09-11 15:35:57 +0300

    Update the test file

--- Packs/CommonScripts/Scripts/CheckSenderDomainDistance/CheckSenderDomainDistance_test.py
+++ Packs/CommonScripts/Scripts/CheckSenderDomainDistance/CheckSenderDomainDistance_test.py
@@ -22,9 +22,9 @@ def test_levenshtein(str1, str2, expected_results):
 
 
 @pytest.mark.parametrize('domain, sender, expected_results', [
-    ('example.com', 'mail@example1.com', 'yes'),
-    ('', 'mail@example1.com', 'no'),
-    ('example.com', '', 'no'),
+    ('example.com', 'mail@example1.com', 'Domain example1.com is suspiciously close to example.com'),
+    ('', 'mail@example1.com', 'Unable to extract domain from arguments'),
+    ('example.com', '', 'Unable to find sender in email'),
     ('example.com', 'mail@example.com', 'no')
 ])
 def test_main(mocker, domain, sender, expected_results):
@@ -45,6 +45,7 @@ def test_main(mocker, domain, sender, expected_results):
     main()
 
     try:
-        assert results_mock.call_args[0][0][1] == expected_results
-    except IndexError:  # In the fourth case
+        contents_results_mock = results_mock.call_args[0][0][0].get('Contents')
+        assert contents_results_mock == expected_results
+    except AttributeError:  # In the fourth case
         assert results_mock.call_args[0][0][0] == expected_results
