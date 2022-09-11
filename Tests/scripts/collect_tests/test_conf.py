8fe560eaaec7c854fb4b103c7ebd9551f20be5ad
BEAdi <72088126+BEAdi@users.noreply.github.com>
2022-09-11 08:23:20 +0300

    rename has_api

--- Tests/scripts/collect_tests/test_conf.py
+++ Tests/scripts/collect_tests/test_conf.py
@@ -17,10 +17,10 @@ class TestConfItem(DictBased):
         return to_tuple(self.get('integrations', (), warn_if_missing=False))
 
     @property
-    def is_api(self):
+    def has_api(self):
         # if there are no integrations- the test playbook is for a script and by default doesn't use api
         default_value = bool(self.integrations)
-        return self.get('is_api', default_value, warn_if_missing=False)
+        return self.get('has_api', default_value, warn_if_missing=False)
 
     @property
     def scripts(self) -> tuple[str, ...]:
@@ -67,7 +67,7 @@ class TestConf(DictFileBased):
             for test in self.tests if test.incoming_mapper
         }
 
-        self.non_api_tests = [test.playbook_id for test in self.tests if not test.is_api]
+        self.non_api_tests = [test.playbook_id for test in self.tests if not test.has_api]
 
     def _calculate_integration_to_tests(self) -> dict[str, list[str]]:
         result = defaultdict(list)
