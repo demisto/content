e4bc1b27725afc73d027ee0faef5210e83daee0c
Shai Yaakovi <30797606+yaakovi@users.noreply.github.com>
2022-09-12 21:20:22 +0300

    Update Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py

--- Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
+++ Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
@@ -353,7 +353,7 @@ def microsoft_365_defender_incidents_list_command(client: Client, args: Dict) ->
     if odata:
         try:
             odata = json.loads(odata)
-        except:
+        except JSONDecodeError:
             return_error(f"Can't parse odata argument as JSON array.\nvalue: {odata}")
 
     response = client.incidents_list(limit=limit, status=status, assigned_to=assigned_to,
