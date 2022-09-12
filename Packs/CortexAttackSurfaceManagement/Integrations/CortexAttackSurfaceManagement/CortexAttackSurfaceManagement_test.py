744ca06c8671bad3fc22885be9054290eca1977f
Johnathan Wilkes <jwilkes@paloaltonetworks.com>
2022-09-12 15:45:50 -0400

    first draft of unit tests

--- Packs/CortexAttackSurfaceManagement/Integrations/CortexAttackSurfaceManagement/CortexAttackSurfaceManagement_test.py
+++ Packs/CortexAttackSurfaceManagement/Integrations/CortexAttackSurfaceManagement/CortexAttackSurfaceManagement_test.py
@@ -72,7 +72,7 @@ def test_getexternalservices_command(requests_mock):
             auth=None)
 
     args = {
-        'domain': 'testdomain.com',
+        'domain': 'testdomain.com'
     }
 
     response = getexternalservices_command(client, args)
@@ -112,7 +112,7 @@ def test_getexternalservices_command(requests_mock):
             auth=None)
 
     args = {
-        'domain': 'testdomain.com',
+        'domain': 'testdomain.com'
     }
 
     response = getexternalservices_command(client, args)
@@ -152,7 +152,7 @@ def test_getexternalservice_command(requests_mock):
             auth=None)
 
     args = {
-        'service_id': '94232f8a-f001-3292-aa65-63fa9d981427',
+        'service_id': '94232f8a-f001-3292-aa65-63fa9d981427'
     }
 
     response = getexternalservice_command(client, args)
@@ -192,7 +192,7 @@ def test_getexternalservices_command(requests_mock):
             auth=None)
 
     args = {
-        'domain': 'testdomain.com',
+        'domain': 'testdomain.com'
     }
 
     response = getexternalservices_command(client, args)
@@ -202,18 +202,18 @@ def test_getexternalservices_command(requests_mock):
     assert response.outputs_key_field == 'service_id'
 
 
-def test_getexternalipaddressranges_command((requests_mock):
-    """Tests asm-getexternalservice_command command function.
+def test_getexternalipaddressranges_command(requests_mock):
+    """Tests getexternalipaddressranges_command function.
 
         Given:
             - requests_mock instance to generate the appropriate getexternalipaddressranges_command( API response,
               loaded from a local JSON file.
         When:
-            - Running the 'getexternalipaddressranges_command('.
+            - Running the 'getexternalipaddressranges_command'.
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, getexternalipaddressranges_command(
+    from CortexAttackSurfaceManagement import Client, getexternalipaddressranges_command
 
     from test_data.raw_response import EXTERNAL_RANGES_RESPONSE
     from test_data.expected_results import EXTERNAL_RANGES_RESULTS
@@ -230,10 +230,126 @@ def test_getexternalipaddressranges_command((requests_mock):
             },
             proxy=False,
             auth=None)
-        args = {}
+    args = {}
 
     response = getexternalipaddressranges_command(client,args)
 
     assert response.outputs == EXTERNAL_RANGES_RESULTS
     assert response.outputs_prefix == 'ASM.GetExternalIpAddressRanges'
     assert response.outputs_key_field == 'range_id'
+
+
+def test_getexternalipaddressrange_command(requests_mock):
+    """Tests getexternalipaddressrange_command function.
+
+        Given:
+            - requests_mock instance to generate the appropriate getexternalipaddressrange_command( API response,
+              loaded from a local JSON file.
+        When:
+            - Running the 'getexternalipaddressrange_command'.
+        Then:
+            - Checks the output of the command function with the expected output.
+    """
+    from CortexAttackSurfaceManagement import Client, getexternalipaddressrange_command
+
+    from test_data.raw_response import EXTERNAL_RANGE_RESPONSE
+    from test_data.expected_results import EXTERNAL_RANGE_RESULTS
+    requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_external_ip_address_range/',
+                      json=EXTERNAL_RANGE_RESPONSE)
+
+    client = Client(
+            base_url='https://test.com/api/webapp/public_api/v1',
+            verify=True,
+            headers = {
+            "HOST": "test.com",
+            "Authorizatio": "THISISAFAKEKEY",
+            "Content-Type": "application/json"
+            },
+            proxy=False,
+            auth=None)
+    args = {
+        'range_id': '1093124c-ce26-33ba-8fb8-937fecb4c7b6'
+    }
+
+    response = getexternalipaddressrange_command(client,args)
+
+    assert response.outputs == EXTERNAL_RANGE_RESULTS
+    assert response.outputs_prefix == 'ASM.GetExternalIpAddressRange'
+    assert response.outputs_key_field == 'range_id'
+
+
+def test_getassetsinternetexposure_command(requests_mock):
+    """Tests ggetassetsinternetexposure_command function.
+
+        Given:
+            - requests_mock instance to generate the appropriate getassetsinternetexposure_command( API response,
+              loaded from a local JSON file.
+        When:
+            - Running the 'getassetsinternetexposure_command'.
+        Then:
+            - Checks the output of the command function with the expected output.
+    """
+    from CortexAttackSurfaceManagement import Client, getassetsinternetexposure_command
+
+    from test_data.raw_response import EXTERNAL_EXPOSURES_RESPONSE
+    from test_data.expected_results import EXTERNAL_EXPOSURES_RESULTS
+    requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_assets_internet_exposure/',
+                      json=EXTERNAL_EXPOSURES_RESPONSE)
+
+    client = Client(
+            base_url='https://test.com/api/webapp/public_api/v1',
+            verify=True,
+            headers = {
+            "HOST": "test.com",
+            "Authorizatio": "THISISAFAKEKEY",
+            "Content-Type": "application/json"
+            },
+            proxy=False,
+            auth=None)
+    args = {
+         'name': 'testdomain.com'
+    }
+
+    response = getassetsinternetexposure_command(client,args)
+
+    assert response.outputs == EXTERNAL_EXPOSURES_RESULTS
+    assert response.outputs_prefix == 'ASM.GetAssetsInternetExposure'
+    assert response.outputs_key_field == 'asm_ids'
+
+def test_getassetinternetexposure_command(requests_mock):
+    """Tests ggetassetinternetexposure_command function.
+
+        Given:
+            - requests_mock instance to generate the appropriate getassetinternetexposure_command( API response,
+              loaded from a local JSON file.
+        When:
+            - Running the 'getassetinternetexposure_command'.
+        Then:
+            - Checks the output of the command function with the expected output.
+    """
+    from CortexAttackSurfaceManagement import Client, getassetinternetexposure_command
+
+    from test_data.raw_response import EXTERNAL_EXPOSURE_RESPONSE
+    from test_data.expected_results import EXTERNAL_EXPOSURE_RESULTS
+    requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_asset_internet_exposure/',
+                      json=EXTERNAL_EXPOSURE_RESPONSE)
+
+    client = Client(
+            base_url='https://test.com/api/webapp/public_api/v1',
+            verify=True,
+            headers = {
+            "HOST": "test.com",
+            "Authorizatio": "THISISAFAKEKEY",
+            "Content-Type": "application/json"
+            },
+            proxy=False,
+            auth=None)
+    args = {
+         'asm_id': 'testdomain.com'
+    }
+
+    response = getassetinternetexposure_command(client,args)
+
+    assert response.outputs == EXTERNAL_EXPOSURE_RESULTS
+    assert response.outputs_prefix == 'ASM.GetAssetInternetExposure'
+    assert response.outputs_key_field == 'asm_ids'
