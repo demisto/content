20d59007f06093479e0e2a6aad40dcdcfaa95efa
ShacharKidor <skidorball@paloaltonetworks.com>
2022-09-14 17:31:51 +0300

    Fixed mypy error

--- Packs/SnortIPBlocklistFeed/Integrations/SnortIPBlocklist/SnortIPBlocklist.py
+++ Packs/SnortIPBlocklistFeed/Integrations/SnortIPBlocklist/SnortIPBlocklist.py
@@ -134,7 +134,7 @@ def get_indicators_command(client: Client,
     Returns:
         Outputs.
     """
-    limit = arg_to_number(args.get('limit', '10'), arg_name='limit')
+    limit = arg_to_number(args.get('limit', '10'), arg_name='limit') or 10
     tlp_color = params.get('tlp_color')
     feed_tags = argToList(params.get('feedTags', ''))
     indicators = fetch_indicators(client, tlp_color, feed_tags, limit)
