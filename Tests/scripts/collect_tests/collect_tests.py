dd4b34ba4a28a2ea89817251e529d49f737d59ef
ilappe <ilappe@paloaltonetworks.com>
2022-09-15 17:56:34 +0300

    CR changes

--- Tests/scripts/collect_tests/collect_tests.py
+++ Tests/scripts/collect_tests/collect_tests.py
@@ -629,7 +629,7 @@ class BranchTestCollector(TestCollector):
             raise NothingToCollectException(path, f'ignored type {file_type}')
 
         if file_type is None and path.parent.name not in CONTENT_ENTITIES_DIRS:
-            raise NothingToCollectException(path, f'ignored non content directory {path.parent.name}')
+            raise NothingToCollectException(path, f'file of unknown type, and not directly under a content directory ({path.parent.name})')
 
         try:
             content_item = ContentItem(path)
