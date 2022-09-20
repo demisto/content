a4f69c012f099a9e1d881877aadd48c37a0715f9
ilan <ierukhimovic@paloaltonetworks.com>
2022-09-20 17:25:58 +0300

    enhanve compare zips

--- Tests/scripts/utils/compare_zips.py
+++ Tests/scripts/utils/compare_zips.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from zipfile import ZipFile
 
 import dictdiffer
+import yaml
 
 
 def compare_zips(zip1: Path, zip2: Path):
@@ -22,20 +23,26 @@ def compare_zips(zip1: Path, zip2: Path):
     dir_compare = filecmp.dircmp(zip1.stem, zip2.stem)
     dir_compare.report_full_closure()
     for file in dir_compare.common_files:
-        if file.endswith('json'):
+        if (is_json := file.endswith('json')) or file.endswith('yaml'):
             # compare the json files
+            parse_func = json.load if is_json else yaml.load
             with open(Path(zip1.stem) / file) as file1, open(Path(zip2.stem) / file) as file2:
-                json1 = json.load(file1)
-                json2 = json.load(file2)
-                if json1 != json2:
+                dct1 = parse_func(file1)
+                dct2 = parse_func(file2)
+                if dct1 != dct2:
                     print(f'JSON files {file} are different')
-                    print(dictdiffer.diff(json1, json2))
+                    print(dictdiffer.diff(dct1, dct2))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('zip1', help='First zip file to compare')
-    parser.add_argument('zip2', help='Second zip file to compare')
+    parser.add_argument('zip-id-set', help='id_set zip file to compare')
+    parser.add_argument('zip-graph', help='graph_id_set zip file to compare')
     args = parser.parse_args()
-    compare_zips(Path(args.zip1), Path(args.zip2))
-    # use argparse
+    zip_id_set = Path(args.zip_id_set)
+    zip_graph = Path(args.zip_graph)
+    # compare directories
+    dir_cmp = filecmp.dircmp(zip_id_set, zip_graph)
+    dir_cmp.report_full_closure()
+    for file in dir_cmp.common_files:
+        compare_zips(zip_id_set / file, zip_graph / file)
