fc75c27d494782bd0c98b8f88f5fb11f6acd1242
Craig Sands <craig.sands@gmail.com>
2022-09-20 10:39:53 -0400

    Add testing of file paths as Name

--- Packs/ContentManagement/Scripts/CustomPackInstaller/CustomPackInstaller_test.py
+++ Packs/ContentManagement/Scripts/CustomPackInstaller/CustomPackInstaller_test.py
@@ -57,6 +57,23 @@ def test_install_custom_pack_success(mocker, pack_id, context, err_massage, res)
     assert error_message == err_massage
 
 
+@pytest.mark.parametrize(
+    argnames='pack_id, context, err_massage, res',
+    argvalues=[
+        ('Pack1', {"File": [{"Name": "content/packs/Pack1/0.0.1/Pack1.zip", "EntryID": "1234"}]}, '', True),
+        ('Pack2', {"File": [{"Name": "content/packs/Pack2/0.1.0/Pack2.zip", "EntryID": "abcd"}]}, '', True)
+    ])
+def test_install_custom_pack_from_s3_success(mocker, pack_id, context, err_massage, res):
+    from CustomPackInstaller import install_custom_pack
+
+    mocker.patch.object(demisto, 'context', return_value=context)
+    mocker.patch('CustomPackInstaller.execute_command', return_value=('Ok', ''))
+
+    result, error_message = install_custom_pack(pack_id, 'true', 'true')
+    assert result == res
+    assert error_message == err_massage
+
+
 def test_install_custom_pack_specify_instance(mocker):
     from CustomPackInstaller import install_custom_pack
     context = {"File": [{"Name": "Pack1.zip", "EntryID": "1234"}]}
