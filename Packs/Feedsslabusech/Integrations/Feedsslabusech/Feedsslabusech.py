176961789b5660086e10b7fd0e1c84bae4dc198c
michal-dagan <mdagan@paloaltonetworks.com>
2022-09-21 15:04:50 +0300

    Add service: sslblacklist.csv

--- Packs/Feedsslabusech/Integrations/Feedsslabusech/Feedsslabusech.py
+++ Packs/Feedsslabusech/Integrations/Feedsslabusech/Feedsslabusech.py
@@ -1,5 +1,6 @@
 from CommonServerPython import *
 
+SSLBLACKLIST = "https://sslbl.abuse.ch/blacklist/sslblacklist.csv"
 
 def main():
     feed_url_to_config = {
@@ -18,6 +19,19 @@ def main():
                 'firstseenbysource': 'firstseenbysource',
                 'port': 'port'
             }
+        },
+        'https://sslbl.abuse.ch/blacklist/sslblacklist.csv': {
+            'fieldnames': ['Listingdate', 'value', 'Listingreason'],
+            'indicator_type': 'Certificate',
+            'mapping': {
+                'firstseenbysource': 'Listingdate',
+                'relationship_entity_b': 'Listingreason',
+                'Tags': 'Listingreason',
+            },
+            'relationship_entity_b_type': FeedIndicatorType.Malware,
+            'relationship_name': EntityRelationship.Relationships.INDICATOR_OF,
+            'auto_detect_type': False,
+
         }
     }
 
@@ -25,6 +39,7 @@ def main():
     params['feed_url_to_config'] = feed_url_to_config
     params['ignore_regex'] = r'^#'
     params['delimiter'] = ','
+    params['create_relationships'] = True
 
     # Main execution of the CSV API Module.
     # This function allows to add to or override this execution.
