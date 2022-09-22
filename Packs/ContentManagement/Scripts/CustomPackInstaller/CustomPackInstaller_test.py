a35679069babf74bc5d3a39e739da553210901bb
Craig Sands <craig.sands@upstart.com>
2022-09-22 05:53:22 -0400

    Compare only filename for S3-downloaded files (#21377)
    * Use endswith instead of equals for S3 paths
    
    * Use split instead of endswith
    
    * Add testing of file paths as Name
    
    * Update pack_metadata.json
    
    * Create 1_1_3.md
    
    * Update docker image
    
    * Update 1_1_3.md
    
    * Update CustomPackInstaller.yml
    
    * Update 1_1_3.md
    
    * Update Packs/ContentManagement/ReleaseNotes/1_1_3.md
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * Update Packs/ContentManagement/Scripts/CustomPackInstaller/CustomPackInstaller.py
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * Update Packs/ContentManagement/ReleaseNotes/1_1_3.md
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * Update Packs/ContentManagement/Scripts/CustomPackInstaller/CustomPackInstaller.py
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    
    * Update CustomPackInstaller_test.py
    
    * Update CustomPackInstaller_test.py
    
    * Fix formatting for flake8
    
    * unit tests fixes
    
    Co-authored-by: Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
    Co-authored-by: YuvHayun <yhayun@paloaltonetworks.com>

--- Packs/ContentManagement/Scripts/CustomPackInstaller/CustomPackInstaller_test.py
+++ Packs/ContentManagement/Scripts/CustomPackInstaller/CustomPackInstaller_test.py
@@ -41,20 +41,35 @@ def test_install_custom_pack_failed(mocker, pack_id, context, err_massage, res):
 
 
 @pytest.mark.parametrize(
-    argnames='pack_id, context, err_massage, res',
+    argnames='pack_id, context',
     argvalues=[
-        ('Pack1', {"File": [{"Name": "Pack1.zip", "EntryID": "1234"}]}, '', True),
-        ('Pack2', {"File": [{"Name": "Pack2.zip", "EntryID": "abcd"}]}, '', True)
+        ('Pack1', {"File": [{"Name": "Pack1.zip", "EntryID": "1234"}]}),
+        ('Pack2', {"File": [{"Name": "Pack2.zip", "EntryID": "abcd"}]}),
+        ('Pack1', {"File": [{"Name": "content/packs/Pack1/0.0.1/Pack1.zip", "EntryID": "1234"}]}),
+        ('Pack2', {"File": [{"Name": "content/packs/Pack2/0.1.0/Pack2.zip", "EntryID": "abcd"}]})
     ])
-def test_install_custom_pack_success(mocker, pack_id, context, err_massage, res):
+def test_install_custom_pack_success(mocker, pack_id, context,):
+    """
+    Given  pack_id and context mock.
+    - Case 1: given a pack with just a filename and numerical EntryID.
+    - Case 2: given a pack with just a filename and alphabetical EntryID.
+    - Case 3: given a pack with a full path name and numerical EntryID,
+    - Case 4: given a pack with a full path name and alphabetical EntryID,
+
+    When
+    - Running install_custom_pack function.
+
+    Then
+    - Ensure the pack id is recognized and the function succeed.
+    """
     from CustomPackInstaller import install_custom_pack
 
     mocker.patch.object(demisto, 'context', return_value=context)
     mocker.patch('CustomPackInstaller.execute_command', return_value=('Ok', ''))
 
     result, error_message = install_custom_pack(pack_id, 'true', 'true')
-    assert result == res
-    assert error_message == err_massage
+    assert result
+    assert error_message == ''
 
 
 def test_install_custom_pack_specify_instance(mocker):
