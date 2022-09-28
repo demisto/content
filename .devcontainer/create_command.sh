04747afe5fc21dd5123cb0c99f8334add3827498
ilan <ierukhimovic@paloaltonetworks.com>
2022-09-28 18:51:05 +0300

    fixes

---
+++ .devcontainer/create_command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo chown demisto .venv
+sudo chown demisto node_modules
+cp .devcontainer/settings.json .vscode/settings.json 
+touch CommonServerUserPython.py
+path=$(printf '%s:' Packs/ApiModules/Scripts/*)
+rm -f .env
+echo "PYTHONPATH=""$path"":$PYTHONPATH" >> .env
+echo "MYPYPATH=""$path"":$MYPYPATH" >> .env
+NO_HOOKS=1 .hooks/bootstrap
