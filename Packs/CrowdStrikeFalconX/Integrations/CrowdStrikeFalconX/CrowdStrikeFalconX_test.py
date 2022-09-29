2b69abf59c751c7a400dae162a3d329f26c4bd65
darkushin <darkushin@paloaltonetworks.com>
2022-09-29 23:02:42 +0300

    added UTs, before testing

--- Packs/CrowdStrikeFalconX/Integrations/CrowdStrikeFalconX/CrowdStrikeFalconX_test.py
+++ Packs/CrowdStrikeFalconX/Integrations/CrowdStrikeFalconX/CrowdStrikeFalconX_test.py
@@ -524,3 +524,29 @@ def test_file_command(requests_mock, mocker, file: str, mocked_address: str, moc
     assert id_query_mock.call_count == 1
     assert all(mocked_search.call_count == 1 for mocked_search in search_query_mocks)
     assert isinstance(command_results, list) and len(command_results) == len(file_ids)
+
+
+@pytest.mark.parametrize('mocked_address,ioc_id, mocked_response', (
+        ('https://api.crowdstrike.com/falconx/entities/artifacts/v1',  # todo: add all the path
+         '123',
+         {'headers': {'Content-Type': 'image/png', 'Content-Disposition': 'attachment; filename=screen_0.png', 'Content-Encoding': 'gzip'},
+          'content': 'png-image'})))
+def test_download_ioc_command(requests_mock, mocker, mocked_address: str, ioc_id: str, mocked_response: dict):
+    """
+    Given
+            IOCs to download
+    When
+            Calling the cs-fx-download-ioc command
+    Then
+            - Verify that when the response includes an image a file is returned
+            - Verify that when the response includes a json the output is a table
+    """
+    mocker.patch.object(Client, '_generate_token', return_value='token')
+    client = Client(server_url="https://api.crowdstrike.com/", username="user1", password="12345", use_ssl=False,
+                    proxy=False, reliability=DBotScoreReliability.B)
+    from CrowdStrikeFalconX import download_ioc_command
+    requests_mock.get(mocked_address, json=mocked_response)  # todo: check the exact api call and the possible response
+    command_results = download_ioc_command(client, ioc_id)
+    assert command_results['outputs'].get('File')  # todo: check how to access the file that was created
+
+
