953376e2c36e33e0518cf1d76c5e061c351758f1
avidan-H <>
2022-09-29 12:46:28 +0300

    test(CyberTriage): write UT for IS_2XX helper function

--- Packs/CyberTriage/Integrations/CyberTriage/CyberTriage_test.py
+++ Packs/CyberTriage/Integrations/CyberTriage/CyberTriage_test.py
@@ -1,13 +1,15 @@
+from inspect import isclass
 import pytest
 from unittest.mock import MagicMock
-from CyberTriage import CyberTriageClient
+from CyberTriage import CyberTriageClient, IS_2XX
 SERVER_URL = 'https://test_url.com'
 
 
 @pytest.fixture()
 def client():
     client = CyberTriageClient(
-        server=SERVER_URL, rest_port='test', api_key='test', user='test', password='test', verify_server_cert=True, ok_codes=(200,)
+        server=SERVER_URL, rest_port='test', api_key='test', user='test',
+        password='test', verify_server_cert=True, ok_codes=(200,)
     )
     return client
 
@@ -53,3 +55,30 @@ def test_triage_endpoint_command(client: CyberTriageClient):
     client.triage_endpoint = mock2
     triage_endpoint_command(client, args)
     client.triage_endpoint.assert_called_once_with(True, False, 'made-up-endpoint', '', 'MADEUP')
+
+
+is_2xx_test_data = [
+    (200, True),
+    (201, True),
+    (234, True),
+    (288, True),
+    (300, False),
+    (391, False),
+    (101, False),
+    (404, False),
+    (505, False),
+    (606, False),
+    (-200, False),
+    (-245, False),
+    ('200', TypeError),
+    ('help', TypeError)
+]
+
+
+@pytest.mark.parametrize('argument,expected', is_2xx_test_data)
+def test_IS_2XX(argument, expected):
+    if isclass(expected) and issubclass(expected, Exception):
+        with pytest.raises(expected_exception=expected):
+            IS_2XX(argument)
+    else:
+        assert IS_2XX(argument) == expected
