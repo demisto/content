8ef369a01ce2a735ef813232f262be2077d81e74
anas-yousef <44998563+anas-yousef@users.noreply.github.com>
2022-09-29 21:14:04 +0300

    Added padding to encoded string

--- Packs/CommonScripts/Scripts/Base64Decode/Base64Decode.py
+++ Packs/CommonScripts/Scripts/Base64Decode/Base64Decode.py
@@ -3,9 +3,16 @@ from CommonServerPython import *
 
 import base64
 
+MULTIPLIER = 4
 
-def decode(value):
-    decoded_bytes = base64.urlsafe_b64decode(str(value))
+
+def add_padding(value: str) -> str:
+    value_length = len(value)
+    return f'{value}{"="*((MULTIPLIER - value_length % MULTIPLIER) % MULTIPLIER)}'
+
+
+def decode(value: str):
+    decoded_bytes = base64.urlsafe_b64decode(add_padding(value))
     res = decoded_bytes.decode(errors='ignore')
 
     outputs = {
@@ -19,6 +26,14 @@ def decode(value):
     return res, outputs
 
 
-if __name__ in ('builtins', '__builtin__'):
-    value = demisto.args()["value"]
-    return_outputs(*decode(value))
+def main():
+    value = demisto.args().get('value')
+    try:
+        return_outputs(*decode(value))
+    except Exception as e:
+        return_error(f'Failed to execute command.'
+                     f'\nError:\n{str(e)}')
+
+
+if __name__ in ('__main__', 'builtins', '__builtin__'):
+    main()
