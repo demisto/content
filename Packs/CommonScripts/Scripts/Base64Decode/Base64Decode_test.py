f5a02dac8d458e40ebaab12e6052fa2c9837bd70
anas-yousef <44998563+anas-yousef@users.noreply.github.com>
2022-10-02 18:30:30 +0300

    Enhanced tests to validate padding logic

--- Packs/CommonScripts/Scripts/Base64Decode/Base64Decode_test.py
+++ Packs/CommonScripts/Scripts/Base64Decode/Base64Decode_test.py
@@ -1,9 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize('value,expected_result', [
+@pytest.mark.parametrize('value, expected_result', [
     ('aGVsbG8=', 'hello'),
-    ('dGhpcw==', 'this')
+    ('dGhpcw==', 'this'),
+    ('VGhpcyBpcyBhIHRlc3Q', 'This is a test')
 ])
 def test_decoding(value, expected_result):
     from Base64Decode import decode
@@ -17,3 +18,15 @@ def test_decoding(value, expected_result):
                 "decoded": expected_result
             }
     }
+
+@pytest.mark.parametrize('value, expected_result', [
+    ('aGVsbG80l', 'aGVsbG80l==='),
+    ('dGhpcw', 'dGhpcw=='),
+    ('VGhpcyBpcyBhIHRlc3Q', 'VGhpcyBpcyBhIHRlc3Q='),
+    ('ERVKHBEK0ejbce8IUBninli0', 'ERVKHBEK0ejbce8IUBninli0')
+])
+def test_padding(value, expected_result):
+    from Base64Decode import add_padding
+    padded_result = add_padding(value=value)
+
+    assert padded_result == expected_result
