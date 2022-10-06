008e2f3aeb24a15123c94f5d1bac49e3f3bf99b6
oauth2 <ypreisler@paloaltonetworks.com>
2022-10-06 23:23:21 +0300

    added test

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks_test.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks_test.py
@@ -1,7 +1,7 @@
 import demistomock as demisto
 import pytest
 import json
-from GetFailedTasks import main, get_failed_tasks_output
+from GetFailedTasks import main, get_failed_tasks_output, get_incident_tasks_using_internal_request
 from test_data.constants import INCIDENTS_RESULT, RESTAPI_TAS_RESULT, INTERNAL_TASKS_RESULT
 
 
@@ -72,3 +72,25 @@ def test_get_failed_tasks_output(tasks, expected_outputs):
 
     assert tasks_res == expected_outputs[0]
     assert num_tasks == expected_outputs[1]
+
+
+@pytest.mark.parametrize('response,expected_result', [
+    (INTERNAL_TASKS_RESULT, json.loads(INTERNAL_TASKS_RESULT.get('body'))),
+    ({}, [])
+])
+def test_get_incident_tasks_using_internal_request(mocker, response, expected_result):
+    """
+        Given:
+            - A response of an internal request to get failed tasks of an incident.
+
+        When:
+            - Running the get_incident_tasks_using_internal_request function
+
+        Then:
+            - Validates that the returned tasks is as expected.
+    """
+    mocker.patch.object(demisto, 'internalHttpRequest', return_value=response)
+    mocker.patch.object(demisto, 'error')
+
+    result = get_incident_tasks_using_internal_request(RESTAPI_TAS_RESULT[0]["Contents"]["response"][0])
+    assert result == expected_result
