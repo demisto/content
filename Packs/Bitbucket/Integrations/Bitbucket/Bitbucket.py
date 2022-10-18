8a117382d365c9d49a6c19d91bf0bbdb6513f92f
RotemAmit <ramit@paloaltonetworks.com>
2022-10-18 14:46:20 +0300

    fix Mypy problem

--- Packs/Bitbucket/Integrations/Bitbucket/Bitbucket.py
+++ Packs/Bitbucket/Integrations/Bitbucket/Bitbucket.py
@@ -3,11 +3,11 @@ from requests import Response
 from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
 from CommonServerUserPython import *  # noqa
 
-import requests
 from typing import Dict
+import urllib3
 
 # Disable insecure warnings
-requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
+urllib3.disable_warnings()  # pylint: disable=no-member
 
 ''' CONSTANTS '''
 
