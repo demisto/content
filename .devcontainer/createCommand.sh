418aba9fd9908c8598f69e5af23c3639761cbfdd
ilan <ierukhimovic@paloaltonetworks.com>
2022-10-18 16:23:48 +0000

    fix git config and no sdk

--- .devcontainer/createCommand.sh
+++ .devcontainer/createCommand.sh
@@ -5,7 +5,8 @@ set -e
 sudo chown demisto .venv
 sudo chown demisto node_modules
 sudo chown -R demisto $HOME
-sudo chown -R demisto /workspaces/demisto-sdk
+
+sudo git config --system http.sslCAInfo /usr/local/share/ca-certificates/certs.crt
 
 cp .devcontainer/settings.json .vscode/settings.json 
 touch CommonServerUserPython.py
