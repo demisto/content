76a13df879cbb6f43553ceba9893b57c0a971c76
okarkkatz <okarkkatz@paloaltonetworks.com>
2022-10-26 17:26:00 +0300

    added some try except for testing the prive build status code

--- Utils/get_private_build_status.py
+++ Utils/get_private_build_status.py
@@ -65,7 +65,14 @@ def get_workflow_status(github_token: str, workflow_id: str) -> Tuple[str, str, 
         return 'completed', 'failure', failure_steps[0].get('name')
 
     # if the job is still in progress - get the current step
-    curr_step = next(step for step in jobs[0].get('steps') if step.get('status') == 'in_progress')
+    # curr_step = next(step for step in jobs[0].get('steps') if step.get('status') == 'in_progress')
+
+    try:
+        # if the job is still in progress - get the current step
+        curr_step = next(step for step in jobs[0].get('steps') if step.get('status') == 'in_progress')
+    except Exception as e:
+        logging.info(jobs[0])
+        return job_status, job_conclusion, 'unknown'
 
     return job_status, job_conclusion, curr_step.get('name')
 
