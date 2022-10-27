74fa4b7b6a13308da9a2ba1fea5c45a87628c470
eli sharf <57587340+esharf@users.noreply.github.com>
2022-10-27 14:33:44 +0300

    GridFieldSetup Automation (#21898)
    * initial add and README Gen
    
    * added test playbook
    
    * first try at unittests
    
    * format/lint
    
    * updated IPs so no secrets
    
    * added test playbook
    
    * fixed conf.json
    
    * removed demisto.error
    
    Co-authored-by: eli sharf <57587340+esharf@users.noreply.github.com>
    
    * updated docker
    
    * add testplaybook to tests
    
    Co-authored-by: eli sharf <57587340+esharf@users.noreply.github.com>
    
    * switch to argToList
    
    Co-authored-by: eli sharf <57587340+esharf@users.noreply.github.com>
    
    * formatting and redo of unit tests
    
    * added command_examples
    
    * another README Update
    
    * update unit test
    
    * updated README again
    
    * readded 1_9_2 rel notes
    
    Co-authored-by: Johnathan Wilkes <jwilkes@paloaltonetworks.com>
    Co-authored-by: johnnywilkes <32227961+johnnywilkes@users.noreply.github.com>

---
+++ Packs/CommonScripts/Scripts/GridFieldSetup/GridFieldSetup.py
@@ -0,0 +1,81 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+
+from typing import Dict
+
+
+def grid_field_setup(keys: Optional[str], vals: dict, res_list: list) -> list[str]:
+    """Returns a list of dictionaries based on the key/values provided.
+    :type keys: ``str``
+    :type vals: ``dict``
+    :type res_list: ``list``
+    :param keys: comma separated values used for keys (columns)
+    :param vals: dictionary of the value assigned to keys
+    :param res_list: list of current entries in gridfield (or blank list)
+    :return: list of dictionaries
+    """
+    key_list = argToList(keys)
+    num_entries = len(key_list)
+    temp = {}
+    count = 0
+    while count < num_entries:
+        tkey = 'val' + str(count + 1)
+        temp[key_list[count]] = vals[tkey]
+        count += 1
+    res_list.append(temp)
+
+    return res_list
+
+
+''' COMMAND FUNCTION '''
+
+
+def grid_field_setup_command(args: Dict[str, str]) -> CommandResults:
+    keys = args.get('keys')
+    overwrite = args.get('overwrite')
+    gridfield = args.get('gridfield')
+
+    # logic here to check if empty result and set default list
+    fields = demisto.incidents()[0].get('CustomFields')
+    if not fields:
+        raise ValueError('no Custom Fields present')
+    orig = fields.get(gridfield)
+    if not orig or overwrite == "true":
+        orig = []
+
+    # list of all val keys
+    valkeys = sorted([k for k in args.keys() if k.startswith('val')])
+
+    # dictionary or all vals
+    vals = {}
+    for i, val in enumerate(valkeys, start=1):
+        vals[f'val{i}'] = args.get(f'val{i}')
+
+    # error is keys and value numbers don't align
+    if len(argToList(keys)) != len(vals.keys()):
+        raise ValueError('number of keys and values needs to be the same')
+
+    # Call the standalone function and get the raw response
+    result = grid_field_setup(keys, vals, orig)
+
+    results = demisto.executeCommand("setIncident", {gridfield: result})
+
+    return results
+
+
+''' MAIN FUNCTION '''
+
+
+def main():
+    try:
+        return_results(grid_field_setup_command(demisto.args()))
+    except Exception as ex:
+        return_error(f'Failed to execute GridFieldSetup. Error: {str(ex)}')
+
+
+''' ENTRY POINT '''
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
