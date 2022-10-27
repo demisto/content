d5e4408ee764a0f2fc4fcfa968def9b772592b38
oauth2 <ypreisler@paloaltonetworks.com>
2022-10-27 12:56:01 +0300

    adding event types request

--- Packs/OneLogin/Integrations/OneLoginEventCollector/OneLoginEventCollector.py
+++ Packs/OneLogin/Integrations/OneLoginEventCollector/OneLoginEventCollector.py
@@ -45,42 +45,128 @@ def prepare_query_params(params: dict, last_run: dict = {}) -> dict:
     return query_params
 
 
+def get_event_type_name(event_type_id, event_types):
+    """
+    Returns the event type name by the given event type ID.
+    """
+    if event_type_id in event_types:
+        return event_types[event_type_id]
+
+    return None
+
+
+def check_response(raw_response):
+    """
+    Checks the status of the API raw respones and raises an exceptionif it's an error.
+    """
+    status = raw_response.get('status')
+    if status and status.get('code') != 200 and status.get('error'):
+        demisto.error(f"Failed to get events from OneLogin API. Error message: {status.get('message')}")
+        raise Exception(f"Error code: {status.get('code')}. Error type: {status.get('type')}.\n"
+                        f"Error message: {status.get('message')}")
+
+
 ''' CLIENT CLASS '''
 
 
 class Client(BaseClient):
 
-    def get_access_token(self):
+    def get_access_token_request(self):
         """
         Send request to get an access token from OneLogin.
         """
-        res = self._http_request(method='POST', url_suffix='/auth/oauth2/v2/token',
-                                 params={'grant_type': 'client_credentials'})
-
-        if 'status' in res and res.get('status', {}).get('error'):
-            error = res.get('status', {})
-            return_error(f"Got error {error.get('code')} - {error.get('type')}.\nError message: {error.get('message')}")
+        raw_response = self._http_request(method='POST', url_suffix='/auth/oauth2/v2/token',
+                                          params={'grant_type': 'client_credentials'})
+        check_response(raw_response)
 
         demisto.debug('Succesfully got access token.')
-        return res.get('access_token')
+        return raw_response.get('access_token')
 
-    def get_events(self, access_token: str, query_params: dict) -> Tuple:
+    def get_events_request(self, access_token: str, query_params: dict) -> Tuple:
         """
         Send request to get events from OneLogin.
         """
         self._headers['Authorization'] = f'Bearer:{access_token}'
         raw_response = self._http_request(method='GET', url_suffix='/api/1/events', params=query_params)
-
-        status = raw_response.get('status')
-        if status.get('code') != 200 and status.get('error'):
-            demisto.error(f"Failed to get events from OneLogin API. Error message: {status.get('message')}")
-            raise Exception(f"Error code: {status.get('code')}. Error type: {status.get('type')}.\n"
-                            f"Error message: {status.get('message')}")
+        check_response(raw_response)
 
         events = raw_response.get('data', [])
         cursor = raw_response.get('pagination', {}).get('after_cursor')
         demisto.debug(f'Succesfully got {len(events)} events.')
         return raw_response, events, cursor
+
+    def get_event_types_request(self) -> list:
+        """
+        Send request to get event types from OneLogin.
+        """
+        raw_response = self._http_request(method='GET', url_suffix='/api/1/events/types')
+        check_response(raw_response)
+
+        demisto.debug('Succesfully got event types.')
+        return raw_response.get('data', [])
+
+    def get_event_types_from_last_run(self, last_run: dict):
+        """
+        Gets the event types from the LastRun obj.
+        For the first fetch, will send a call to get the event types from OneLogin and will update the LastRun as well.
+        """
+
+        if 'event_types' not in last_run:
+            event_types = self.get_event_types_request()
+            last_run['event_types'] = {str(event_type['id']): event_type['name'] for event_type in event_types}
+
+        return last_run.get('event_types')
+
+    def convert_type_id_to_name(self, event: dict, event_types: dict, last_run: dict) -> str:
+        """
+        Gets the event type name by the event type id.
+
+        Args:
+            event: The event getting from the OneLogin events API call.
+            event_types: Event types getting from the LastRun obj.
+            last_run: The LastRun obj.
+
+        Returns:
+            (str): The event type name.
+        """
+
+        event_type_name = ''
+        if event_type_name := get_event_type_name(str(event['event_type_id']), event_types):
+            return event_type_name
+
+        demisto.debug(f"Could not find the event type id {str(event['event_type_id'])}. "
+                      f"Trying to request the event types from OneLogin.")
+        event_types_res = self.get_event_types_request()
+        event_types = {str(event_type['id']): event_type['name'] for event_type in event_types_res}
+
+        if event_type_name := get_event_type_name(str(event['event_type_id']), event_types):
+            last_run['event_types'] = event_types
+            return event_type_name
+
+        demisto.debug(f"Could not find the event type id '{str(event['event_type_id'])}' "
+                      f"in the event types from OneLogin, returning an empty value")
+        return event_type_name
+
+    def get_access_token(self):
+        """
+        Gets the API access token.
+        """
+
+        int_context = get_integration_context()
+        if 'access_token' in int_context:
+            expired_token_date_time = parse(int_context['expired_token_time'], settings={'TIMEZONE': 'UTC'})
+            if expired_token_date_time and datetime.now(timezone.utc) - expired_token_date_time < timedelta(minutes=590):
+                demisto.debug("Found access token in integration context.")
+                return int_context.get('access_token')
+
+        demisto.debug("Access token has expired, sending request to get a new access token.")
+        access_token = self.get_access_token_request()
+        set_integration_context({
+            'access_token': access_token,
+            'expired_token_time': datetime.utcnow().strftime(DATE_FORMAT)
+        })
+
+        return access_token
 
     def handle_pagination_first_batch(self, access_token: str, query_params: dict, last_run: dict) -> Tuple:
         """
@@ -88,13 +174,16 @@ class Client(BaseClient):
         If `first_id` exists in the lastRun obj, finds it in the response and
         returns only the subsequent events (that weren't collected yet).
         """
-        _, events, cursor = self.get_events(access_token, query_params)
+        _, events, cursor = self.get_events_request(access_token, query_params)
+
         if last_run.get('first_id'):
             for idx, event in enumerate(events):
+
                 if event.get('id') == last_run['first_id']:
                     events = events[idx:]
                     break
             last_run.pop('first_id', None)  # removing to make sure it won't be used in future runs
+
         return events, cursor
 
     def fetch_events(self, access_token: str, query_params: dict, last_run: dict) -> List[dict]:
@@ -114,6 +203,8 @@ class Client(BaseClient):
         """
         aggregated_events: List[dict] = []
 
+        event_types = self.get_event_types_from_last_run(last_run)
+
         user_defined_limit = query_params.pop('limit')
         query_params['limit'] = 1000  # Constant limit value for the request
         try:
@@ -127,6 +218,7 @@ class Client(BaseClient):
                         cursor = query_params['after_cursor']
                         break
 
+                    event['event_type_name'] = self.convert_type_id_to_name(event, event_types, last_run)
                     aggregated_events.append(event)
 
                 else:
@@ -134,7 +226,7 @@ class Client(BaseClient):
                     if cursor:
                         demisto.debug('Using the cursor from the last API call to execute the next call.')
                         query_params['after_cursor'] = cursor
-                        _, events, cursor = self.get_events(access_token, query_params)
+                        _, events, cursor = self.get_events_request(access_token, query_params)
                         continue
 
                 demisto.debug('Finished iterating through all events in this fetch run.')
@@ -159,17 +251,19 @@ class Client(BaseClient):
 ''' COMMAND FUNCTIONS '''
 
 
-def test_module_command(client: Client) -> str:
+def test_module_command(client: Client, params: dict) -> str:
     """
     Tests connection to OneLogin.
     Args:
-        client (Client): the client implementing the API to OneLogin.
+        client (Client): The client implementing the API to OneLogin.
+        params (dict): The configuration parameters.
 
     Returns:
         (str) 'ok' if success.
     """
     access_token = client.get_access_token()
-    client.get_events(access_token, {})
+    params = prepare_query_params(params)
+    client.get_events_request(access_token, {})
     return 'ok'
 
 
@@ -186,7 +280,7 @@ def get_events_command(client: Client, args: dict) -> Tuple[list, CommandResults
     """
     access_token = client.get_access_token()
     query_params = prepare_query_params(args)
-    raw_response, events, cursor = client.get_events(access_token, query_params)
+    raw_response, events, cursor = client.get_events_request(access_token, query_params)
     results = CommandResults(
         raw_response=raw_response,
         readable_output=tableToMarkdown(
@@ -244,7 +338,7 @@ def main() -> None:
         )
 
         if command == 'test-module':
-            return_results(test_module_command(client))
+            return_results(test_module_command(client, params))
 
         else:
             if command == 'onelogin-get-events':
--- Packs/OneLogin/Integrations/OneLoginEventCollector/OneLoginEventCollector_description.md
+++ Packs/OneLogin/Integrations/OneLoginEventCollector/OneLoginEventCollector_description.md
@@ -1,8 +1,11 @@
-## BaseIntegration Help
+To start fetch events from OneLogin API you need to get an API Credential Pair (Client ID and Clien Secret) from the OneLogin UI, to do this please see the following section.
 
-Markdown file for integration configuration  help snippet. In this file add:
-- Brief information about how to retrieve the API key of your product
-- Other useful information on how to configure your integration in XSOAR
+### Creating an OneLogin API Credential Pair
+Creating the Client ID and Client Secret in the OneLogin UI:
+1. Access OneLogin as an account owner or administrator.
+2. Go to Developers > API Credentials.
+3. On the API Access page, click New Credential.
+4. Select a scope for the credentials. (Authentication Only, Read Users, Manage users etc.)
+5. Click Save.
 
-Since this is a Markdown file, we encourage you to use MD formatting for sections, sub-sections, lists, etc.
-
+For more information please see the [OneLogin documentation](https://developers.onelogin.com/api-docs/1/getting-started/working-with-api-credentials).
