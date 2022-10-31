8236366072d04fe29a9c31c03b7517ae284724dd
RotemAmit <ramit@paloaltonetworks.com>
2022-10-31 16:00:53 +0200

    CommitFilesBitbucket script ready, and Bitbucket CICD playbook

--- Packs/ContentManagement/Scripts/CommitFilesBitbucket/CommitFilesBitbucket.py
+++ Packs/ContentManagement/Scripts/CommitFilesBitbucket/CommitFilesBitbucket.py
@@ -18,53 +18,52 @@ PR_TEMPLATE = '### Pull Request created in Cortex XSOAR\n' \
               '{}\n\n' \
               '---'
 
-file_path_to_sha: Dict[str, str] = {}
+files_path = []
 
 new_files = []
 modified_files = []
 
 
-def get_file_sha(branch_name, content_file):
+def does_file_exist(branch_name, content_file):
     full_path = os.path.join(content_file.path_to_file, content_file.file_name)
 
-    sha = file_path_to_sha.get(full_path)
-    if sha:
-        return sha
+    if full_path in files_path:
+        return True
 
     # try to get the file from branch
-    status, list_files_res = execute_command('Github-list-files', {'branch': branch_name,
-                                                                   'path': content_file.path_to_file},
+    status, list_files_res = execute_command('bitbucket-commit-list', {'included_branches': branch_name,
+                                                                       'file_path': content_file.path_to_file},
                                              fail_on_error=False)
 
     if status:
-        for file in list_files_res:
-            file_path_to_sha[file['path']] = file['sha']
+        files_path.append(full_path)
+        return True
 
-    return file_path_to_sha.get(full_path)
+    return False
 
 
 def commit_content_item(branch_name, content_file):
-    commit_args = {'commit_message': f'Added {content_file.file_name}',
-                   'path_to_file': f'{content_file.path_to_file}/{content_file.file_name}',
-                   'branch_name': branch_name, 'file_text': content_file.file_text}
+    commit_args = {"message": f"Added {content_file.file_name}",
+                   "file_name": f"{content_file.path_to_file}/{content_file.file_name}",
+                   "branch": f"{branch_name}",
+                   "file_content": f"{content_file.file_text}"}
 
-    file_sha = get_file_sha(branch_name, content_file)
-
+    file_status = does_file_exist(branch_name, content_file)
     # dont commit pack_metadata.json if already exists in the branch
-    if file_sha and content_file.file_name == 'pack_metadata.json':
+    if file_status and content_file.file_name == 'pack_metadata.json':
         return
-    elif file_sha:
+    elif file_status:
         # update existing file
-        commit_args['file_sha'] = file_sha
-        commit_args['commit_message'] = f'Updated {content_file.file_name}'
+        commit_args['message'] = f'Updated {content_file.file_name}'
         modified_files.append(content_file.file_name)
     else:
         # new file added
         new_files.append(content_file.file_name)
-
-    status, commit_res = execute_command('Github-commit-file', commit_args)
-    if not status:
-        raise DemistoException(commit_res)
+    try:
+        demisto.executeCommand('bitbucket-commit-create', args=commit_args)
+    except DemistoException as e:
+        raise DemistoException(f'Failed to execute bitbucket-commit-create command. Error: {e}, '
+                               f'{traceback.format_exc()}')
 
 
 def split_yml_file(content_file):
@@ -82,7 +81,8 @@ def split_yml_file(content_file):
     output_capture = io.StringIO()
 
     # split the yml file
-    yml_splitter = YmlSplitter(content_file.file_name, base_name=base_name, output=base_name, file_type=content_file.content_type,
+    yml_splitter = YmlSplitter(content_file.file_name, base_name=base_name, output=base_name,
+                               file_type=content_file.content_type,
                                no_pipenv=True, no_basic_fmt=True, no_logging=True, no_readme=True)
 
     script_type = yml_splitter.yml_data.get('type')
@@ -184,6 +184,7 @@ def main():
         branch_name = demisto.getArg('branch')
         pack_name = demisto.getArg('pack')
         user = demisto.getArg('user')
+        email = demisto.getArg('email')
         comment = demisto.getArg('comment')
         template = demisto.getArg('template')
 
@@ -193,9 +194,10 @@ def main():
         if not comment:
             comment = ''
 
-        username = user.get('username')
-        if user.get('email'):
-            username = f'{username} ({user.get("email")})'
+        if user and email:
+            username = f'{user} <{email}>'
+        else:
+            username = ''
 
         # commit the files from the input
         for file in files:
@@ -233,11 +235,10 @@ def main():
         ))
 
     except Exception as ex:
-        return_error(f'Failed to execute CommitFiles script. Error: {ex}', error=ex)
+        return_error(f'Failed to execute CommitFiles script. Error: {ex}, {traceback.format_exc()}', error=ex)
 
 
 ''' ENTRY POINT '''
-
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
