62317c9de6e3eed3916121011a3932fbccec21a5
ishai <ishor@paloaltonetworks.com>
2022-10-31 11:56:26 +0200

    Bugfix - ansible linux parse error

--- Packs/ApiModules/Scripts/AnsibleApiModule/AnsibleApiModule_test.py
+++ Packs/ApiModules/Scripts/AnsibleApiModule/AnsibleApiModule_test.py
@@ -1,3 +1,5 @@
+import os
+
 from AnsibleApiModule import dict2md, rec_ansible_key_strip, generate_ansible_inventory, generic_ansible
 from TestsInput.markdown import MOCK_SINGLE_LEVEL_LIST, EXPECTED_MD_LIST, MOCK_SINGLE_LEVEL_DICT, EXPECTED_MD_DICT
 from TestsInput.markdown import MOCK_MULTI_LEVEL_DICT, EXPECTED_MD_MULTI_DICT, MOCK_MULTI_LEVEL_LIST
@@ -6,6 +8,8 @@ from TestsInput.ansible_keys import MOCK_ANSIBLE_DICT, EXPECTED_ANSIBLE_DICT, MO
 from TestsInput.ansible_inventory import ANSIBLE_INVENTORY_HOSTS_LIST, ANSIBLE_INVENTORY_HOSTS_CSV_LIST
 from TestsInput.ansible_inventory import ANSIBLE_INVENTORY_HOST_w_PORT, ANSIBLE_INVENTORY_INT_PARAMS
 from unittest.mock import patch
+
+fixture_path = os.path.join(os.path.dirname(__file__), "fixtures", "network")
 
 
 def test_dict2md_simple_lists():
@@ -204,6 +208,70 @@ def test_generic_ansible():
 """
     expected_outputs = [{'changed': False, 'current_audit_policy': {
         'file system': 'failure'}, 'host': '123.123.123.123', 'status': 'SUCCESS'}]
+
+    with patch('ansible_runner.run', return_value=mock_ansible_results):
+        CommandResults = generic_ansible('microsoftwindows', 'win_audit_policy_system', args, int_params, host_type)
+
+        assert CommandResults.readable_output == expected_readable
+        assert CommandResults.outputs == expected_outputs
+
+
+def test_generic_ansible_with_problematic_stdout():
+    """
+    Scenario: Given valid arguments, mock events from ansible-runner, ensure context/readable output matches expectations
+
+    Given:
+    - valid host args for single windows host 123.123.123.123
+    - valid username / password
+    - command win-audit-policy-system
+    - args subcategory="File System" audit_type="failure"
+
+    When:
+    - mock events from ansible-runner show that a change to policy was made successfully
+
+    Then:
+    - Valid context/readable output
+    """
+
+    # Inputs
+    args = {'host': "123.123.123.123", 'subcategory': 'File System', 'audit_type': 'failure'}
+    int_params = {'port': 5985, 'creds': {'identifier': 'bill', 'password': 'xyz321', 'credentials': {}}}
+    host_type = "winrm"
+
+    # Mock results from Ansible run
+    mock_ansible_results = Object()
+
+    with open(os.path.join(os.path.join("TestsInput", "stdout.txt")), encoding='unicode_escape') as f:
+        stdout = f.read()
+
+    mock_ansible_results.events = [{'uuid': 'cf26f7c4-6eca-48b2-8294-4bd263cfb2e0', 'counter': 1, 'stdout': '',
+                                    'start_line': 0, 'end_line': 0,
+                                    'runner_ident': 'd5a00f7c-7fb6-424a-a8f9-83556bdb2360',
+                                    'event': 'playbook_on_start', 'pid': 674619,
+                                    'created': '2021-06-01T15:57:37.638813',
+                                    'event_data': {}},
+                                   {'uuid': 'cc29d328-9d35-4193-ba18-e82e98eaf0c6', 'counter': 2, 'stdout': '',
+                                    'start_line': 0, 'end_line': 0,
+                                    'runner_ident': 'd5a00f7c-7fb6-424a-a8f9-83556bdb2360',
+                                       'event': 'runner_on_start', 'pid': 674619,
+                                    'created': '2021-06-01T15:57:37.668136',
+                                    'parent_uuid': 'a736a224-f5d0-0add-444b-000000000009', 'event_data': {}},
+                                   {'uuid': '4770effd-5088-430c-aee2-621bee4f3f00', 'counter': 3,
+                                    'stdout': stdout,
+                                    'start_line': 0, 'end_line': 6,
+                                              'runner_ident': 'd5a00f7c-7fb6-424a-a8f9-83556bdb2360',
+                                    'event': 'runner_on_ok',
+                                    'pid': 674619, 'created': '2021-06-01T15:57:40.592040',
+                                    'parent_uuid': 'a736a224-f5d0-0add-444b-000000000009', 'event_data': {}}]
+
+    # Expected results
+    expected_readable = """# \x1b[0;32m10.2.25.44 -  SUCCESS 
+  * changed: False
+  * ## Deprecations
+  * ## Warnings
+"""
+
+    expected_outputs = [{'changed': False, 'deprecations': [], 'warnings': [], 'host': '\x1b[0;32m10.2.25.44', 'status': 'SUCCESS'}]
 
     with patch('ansible_runner.run', return_value=mock_ansible_results):
         CommandResults = generic_ansible('microsoftwindows', 'win_audit_policy_system', args, int_params, host_type)
