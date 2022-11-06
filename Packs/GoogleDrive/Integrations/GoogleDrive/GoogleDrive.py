1cfd2f88f89b18d3e5a668ba1c779487bdee30ec
meichlerpanw <meichler@paloaltonetworks.com>
2022-11-06 12:33:43 +0200

    revert fix

--- Packs/GoogleDrive/Integrations/GoogleDrive/GoogleDrive.py
+++ Packs/GoogleDrive/Integrations/GoogleDrive/GoogleDrive.py
@@ -1098,12 +1098,7 @@ def prepare_single_file_human_readable(outputs_context: Dict[str, Any], args: Di
 
 def prepare_file_command_request(client: 'GSuiteClient', args: Dict[str, str], scopes: List[str]) -> Dict[str, Any]:
     # Prepare generic HTTP request params
-    if supports_all_drives := args.get('supports_all_drives'):
-        http_request_params: Dict[str, str] = assign_params(
-            supportsAllDrives=supports_all_drives
-        )
-    else:
-        http_request_params: Dict[str, str] = {}
+    http_request_params = assign_params(supportsAllDrives=args.get('supports_all_drives'))
 
     # user_id can be overridden in the args
     user_id = args.get('user_id') or client.user_id
