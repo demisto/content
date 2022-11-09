8f4913718d54af0776473fde26b23a738ad1d2a7
Evan Yandle <eyandle@paloaltonetworks.com>
2022-11-09 11:10:57 -0500

    Fixing Flake Errors SendFancyEmail.py:11:131: E501 line too long (140 > 130 characters)

--- Packs/FancyEmails/Scripts/SendFancyEmail/SendFancyEmail.py
+++ Packs/FancyEmails/Scripts/SendFancyEmail/SendFancyEmail.py
@@ -7,8 +7,13 @@ body_header = args.get('body_header')
 body_text = '<br/>' + args.get('htmlBody', args.get('body')) + '<br/>'
 custom_css = args.get('custom_css')
 
+params = {'body': body_text,
+          'header': body_header,
+          'banner': banner,
+          'custom_css': custom_css}
+
 email_html = demisto.executeCommand(
-    'fancy-email-make-email', {'body': body_text, 'header': body_header, 'banner': banner, 'custom_css': custom_css})[0]["Contents"]['html']
+    'fancy-email-make-email', params)[0]["Contents"]['html']
 args['htmlBody'] = email_html
 
 
