e49ab5905e5d8417b3949a6fe6e30db43aa7e8d3
MLainer1 <maylainer96@gmail.com>
2022-11-09 17:16:34 +0200

    added domain regex test

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -20,7 +20,7 @@ from CommonServerPython import xml2json, json2xml, entryTypes, formats, tableToM
     remove_nulls_from_dictionary, is_error, get_error, hash_djb2, fileResult, is_ip_valid, get_demisto_version, \
     IntegrationLogger, parse_date_string, IS_PY3, PY_VER_MINOR, DebugLogger, b64_encode, parse_date_range, \
     return_outputs, \
-    argToBoolean, ipv4Regex, ipv4cidrRegex, ipv6cidrRegex, urlRegex, ipv6Regex, batch, FeedIndicatorType, \
+    argToBoolean, ipv4Regex, ipv4cidrRegex, ipv6cidrRegex, urlRegex, ipv6Regex, domainRegex, batch, FeedIndicatorType, \
     encode_string_results, safe_load_json, remove_empty_elements, aws_table_to_markdown, is_demisto_version_ge, \
     appendContext, auto_detect_indicator_type, handle_proxy, get_demisto_version_as_str, get_x_content_info_headers, \
     url_to_clickable_markdown, WarningsHandler, DemistoException, SmartGetDict, JsonTransformer, \
@@ -3513,6 +3513,36 @@ def test_auto_detect_indicator_type_tldextract(mocker):
         assert 'cache_file' in res[1].keys()
 
 
+VALID_DOMAIN_INDICATORS = ['www.static.attackiqtes.com', 'test.com']
+
+@pytest.mark.parametrize('indicator_value', VALID_DOMAIN_INDICATORS)
+def test_valid_domain_indicator_types(indicator_value):
+    """
+    Given
+    - Valid Domain indicators.
+    When
+    - Trying to match those indicators with the Domain regex.
+    Then
+    - The indicators are classified as Domain indicators.
+    """
+    assert re.match(domainRegex, indicator_value)
+
+
+INVALID_DOMAIN_INDICATORS = ['aaa.2234', '1.1.1.1']
+
+@pytest.mark.parametrize('indicator_value', INVALID_DOMAIN_INDICATORS)
+def test_invalid_domain_indicator_types(indicator_value):
+    """
+    Given
+    - invalid Domain indicators.
+    When
+    - Trying to match those indicators with the Domain regex.
+    Then
+    - The indicators are not classified as Domain indicators.
+    """
+    assert not re.match(domainRegex, indicator_value)
+
+
 VALID_URL_INDICATORS = [
     '3.21.32.65/path',
     '19.117.63.253:28/other/path',
@@ -3712,7 +3742,6 @@ def test_invalid_url_indicator_types(indicator_value):
     - The indicators are not classified as URL indicators.
     """
     assert not re.match(urlRegex, indicator_value)
-
 
 def test_handle_proxy(mocker):
     os.environ['REQUESTS_CA_BUNDLE'] = '/test1.pem'
