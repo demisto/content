c7694fdef7e60367bb4728d7caebd858e5503612
sapirshuker <sshuker@paloaltonetworks.com>
2022-11-10 09:32:00 +0200

    fix playbook gmailtest

--- Packs/Gmail/Integrations/Gmail/Gmail.py
+++ Packs/Gmail/Integrations/Gmail/Gmail.py
@@ -2104,6 +2104,7 @@ def forwarding_address_add_command_with_disposition(user_id, forwarding_email_li
     return result_success, result_failure
 
 
+
 def forwarding_address_add_command():
     """
     Creates a forwarding address.
