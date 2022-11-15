f1117638b44392b916795a26be317f54f6271b64
ilan <ierukhimovic@paloaltonetworks.com>
2022-11-15 14:13:47 +0200

    no pytz

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -27,7 +27,6 @@ from abc import abstractmethod
 from distutils.version import LooseVersion
 from threading import Lock
 from inspect import currentframe
-import pytz
 
 import demistomock as demisto
 import warnings
@@ -40,7 +39,7 @@ def __line__():
 
 # 42 - The line offset from the beggining of the file.
 _MODULES_LINE_MAPPING = {
-    'CommonServerPython': {'start': __line__() - 43, 'end': float('inf')},
+    'CommonServerPython': {'start': __line__() - 42, 'end': float('inf')},
 }
 
 
@@ -10293,8 +10292,8 @@ def get_fetch_run_time_range(last_run, first_fetch, look_back=0, timezone=0, dat
         last_run_time = dateparser.parse(last_run_time, settings={'TIMEZONE': 'UTC'})
 
     # # Make sure both times are timezone aware
-    now = now.replace(tzinfo=pytz.UTC)
-    last_run_time = last_run_time.replace(tzinfo=pytz.UTC)
+    now = now.replace(tzinfo='UTC')
+    last_run_time = last_run_time.replace(tzinfo='UTC')
     if look_back > 0:
         if now - last_run_time < timedelta(minutes=look_back):
             last_run_time = now - timedelta(minutes=look_back)
