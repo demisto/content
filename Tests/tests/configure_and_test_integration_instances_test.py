6d627f3b4f71a9dbf305212a6c095f0262eb75d0
dmeylakh <dmeylakh@paloaltonetworks.com>
2022-11-20 15:52:27 +0200

    TDD: committing failing unittest

--- Tests/tests/configure_and_test_integration_instances_test.py
+++ Tests/tests/configure_and_test_integration_instances_test.py
@@ -1,9 +1,11 @@
 import pytest
 from unittest.mock import mock_open
+from TestSuite.test_tools import ChangeCWD
+from TestSuite.repo import Repo
 
 from Tests.configure_and_test_integration_instances import XSOARBuild, create_build_object, \
     options_handler, XSIAMBuild, get_turned_non_hidden_packs, update_integration_lists, \
-    get_packs_with_higher_min_version, filter_new_to_marketplace_packs
+    get_packs_with_higher_min_version, filter_new_to_marketplace_packs, packs_names_to_integrations_names
 
 XSIAM_SERVERS = {
     "qa2-test-111111": {
@@ -170,6 +172,15 @@ def test_update_integration_lists(mocker, new_integrations_names, turned_non_hid
                  return_value=turned_non_hidden_packs_id)
     returned_results = update_integration_lists(new_integrations_names, turned_non_hidden_packs_id, modified_integrations_names)
     assert the_expected_result(returned_results[0], returned_results[1])
+
+
+def test_pack_names_to_integration_names_no_integrations_folder(tmp_path):
+    repo = Repo(tmp_path)
+    pack = repo.create_pack('PackName')
+    pack._integrations_path.rmdir()
+
+    with ChangeCWD(pack.repo_path):
+        assert packs_names_to_integrations_names(['PackName']) == []
 
 
 def test_get_packs_with_higher_min_version(mocker):
