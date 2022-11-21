f1efe591d9035309b85dbd607853f5859fc12815
merit <meretmaayta@gmail.com>
2022-11-21 09:25:13 +0200

    added to readme

--- Packs/ArcherRSA/Integrations/ArcherV2/ArcherV2.py
+++ Packs/ArcherRSA/Integrations/ArcherV2/ArcherV2.py
@@ -661,9 +661,9 @@ def extract_from_xml(xml, path):
 def generate_field_contents(client, fields_values, level_fields):
     if fields_values and not isinstance(fields_values, dict):
         demisto.debug(f"fields values are: {fields_values}")
-        fields_values = fields_values.replace("\\", "\\\\")
-        fields_values = fields_values.replace('\\\\"', "")
-        demisto.debug(f"fields now: {fields_values}")
+        # fields_values = fields_values.replace('\\\\"', "")
+        # fields_values = fields_values.replace("\\", "\\\\")
+        # demisto.debug(f"fields now: {fields_values}")
         try:
             fields_values = json.loads(fields_values)
         except Exception:
