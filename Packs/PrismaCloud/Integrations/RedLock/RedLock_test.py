af9767642f644ffedbd126164f62355302671eb7
omerKarkKatz <95565843+omerKarkKatz@users.noreply.github.com>
2022-11-22 10:47:46 +0200

    Update Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py

--- Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
+++ Packs/PrismaCloud/Integrations/RedLock/RedLock_test.py
@@ -415,20 +415,15 @@ def test_redlock_search_network(mocker):
 )
 def test_alert_to_context(raw_alert, expected_result, set_args, mocker):
     """
-    Unit Test Scenario 1 - resource_keys not included
         Given
-        - Raw alert data
+            - Raw alert data
+            - Raw alert data
         When
-        - resource_keys argument is not set
+            - resource_keys argument is not set
+            - resource_keys argument is set to 'tags'
         Then
-        - Validate that the function returns the expected context JSON
-    Unit Test Scenario 2 - resource_keys argument included
-        Given
-        - Raw alert data
-        When
-        - resource_keys argument is set to 'tags'
-        Then
-        - Validate that the function returns the expected context JSON including the 'tags' element from the resource
+            - Validate that the function returns the expected context JSON
+            - Validate that the function returns the expected context JSON including the 'tags' element from the resource 
     """
     from RedLock import alert_to_context
     if set_args:
