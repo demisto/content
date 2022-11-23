d22cd0f229db5da854724af2f37f9e7ea8562d94
yaakovpraisler <ypreisler@paloaltonetworks.com>
2022-11-23 15:16:24 +0200

    clean private packs from index

--- Tests/Marketplace/upload_packs.py
+++ Tests/Marketplace/upload_packs.py
@@ -218,13 +218,9 @@ def clean_non_existing_packs(index_folder_path: str, private_packs: list, storag
         bool: whether cleanup was skipped or not.
     """
     if ('CI' not in os.environ) or (
-            os.environ.get('CI_COMMIT_BRANCH') != 'master' and storage_bucket.name == GCPConfig.PRODUCTION_BUCKET) or (
+            # os.environ.get('CI_COMMIT_BRANCH') != 'master' and storage_bucket.name == GCPConfig.PRODUCTION_BUCKET) or (
             os.environ.get('CI_COMMIT_BRANCH') == 'master' and storage_bucket.name not in
             (GCPConfig.PRODUCTION_BUCKET, GCPConfig.CI_BUILD_BUCKET)):
-        logging.info(f"[TEST] is CI not in os.environ: {'CI' not in os.environ}")
-        logging.info(f"[TEST] CI_COMMIT_BRANCH: {os.environ.get('CI_COMMIT_BRANCH')}")
-        logging.info(f"[TEST] storage_bucket.name: {storage_bucket.name}")
-        logging.info(f"[TEST] GCPConfig.PRODUCTION_BUCKET: {GCPConfig.PRODUCTION_BUCKET}")
         logging.info("Skipping cleanup of packs in gcs.")  # skipping execution of cleanup in gcs bucket
         return True
 
