6a416400af2b1c652e2a4963f1f516b7fb334c15
davrahami <davrahami@paloaltonetworks.com>
2022-11-24 14:38:58 +0200

    Added functions to formatter, updated regex in formatter and in the type, new tests.

--- Packs/CommonScripts/Scripts/ExtractEmailFormatting/ExtractEmailFormatting_test.py
+++ Packs/CommonScripts/Scripts/ExtractEmailFormatting/ExtractEmailFormatting_test.py
@@ -1,60 +1,53 @@
-from ExtractEmailFormatting import verify_is_email, main, remove_unicode_points
+from ExtractEmailFormatting import main, check_tld, extract_email, refang_email
 import pytest
 import demistomock as demisto
 
-testdata = [
+
+defang_data = [
+    ('xsoar@test[.]com', 'xsoar@test.com'),
+    ('xsoar[@]test[.]com', 'xsoar@test.com'),
+    ('xsoar[@]test.com', 'xsoar@test.com'),
+]
+@pytest.mark.parametrize('address,valid', defang_data)
+def test_check_defanging(address, valid):
+    assert refang_email(address) == valid
+
+
+tld_data = [
     ('Xsoar@test.org.de', True),
     ('xsoar@test.net.bla', True),
-    ('Xsoar@test.uk.Png', False),
+    ('Xsoar@test.uk.png', False),
     ('Xsoar@test.eml', False),
-    ('Xsoar@test.new.Docx', False),
+    ('Xsoar@test.new.docx', False),
     ('entry@id.com.gif', False),
     ('randomName@randomDomain.com', True),
     ('Xsoar@xsoar.xlsx', False),
-    ('Xsoa r@ test.BmP', False),
-    ('bt53h6htyj8j57k9k=organization.org@ozzy.qwer.de', True),
-    ('Xsoar[@]test.org.de', True),
-    ('xsoar[@]test.net.bla', True),
-    ('Xsoar[@]test.uk.Png', False),
-    ('Xsoar[@]test.eml', False),
-    ('Xsoar[@]test.new.Docx', False),
-    ('entry[@]id.com.gif', False),
-    ('randomName[@]randomDomain.com', True),
-    ('Xsoar[@]xsoar.xlsx', False),
-    ('Xsoa r[@] test.BmP', False),
-    ('bt53h6htyj8j57k9k=organization.org[@]ozzy.qwer.de', True),
-    ('xsoar@test[.]com', True),
-    ('xsoar[@]test[.]com', True),
 ]
-
-
-@pytest.mark.parametrize('address,valid', testdata)
-def test_verify_is_email(address, valid):
-    """Verifies that email address that was auto-extracted via the Email regex is valid.
-    Given
-    - address.
-    When
-    - When an email address was auto-extracted
-    Then
-    - Checks if it's an email address or not
-    """
-    assert verify_is_email(address) is valid
+@pytest.mark.parametrize('address,valid', tld_data)
+def test_check_tld(address, valid):
+    assert check_tld(address) is valid
 
 
 @pytest.mark.parametrize('input,output', [  # noqa: E501 disable-secrets-detection # no processing needed
     ('\\u003ctest@test.com', 'test@test.com'),
+    ('"test@test.com"', 'test@test.com'),
+    ('<test@test.com>', 'test@test.com'),
 ])  # noqa: E124
 def test_extract_email(input, output):
-    extracted_fqdn = remove_unicode_points(input)
-    # extracted_domain = extract_fqdn_or_domain(input, is_domain=True)
-
-    assert extracted_fqdn == output
-    # assert extracted_domain == domain
+    assert extract_email(input) == output
 
 
 ARGS = {
-    'input': 'Xsoar@test.org.de,Xsoar@test.eml, Xsoar@test.uk, Xsoar@xsoar.xlsx,Xsoar@xsoar.co.il'}
-EXPECTED_RESULTS = ['Xsoar@test.org.de', '', 'Xsoar@test.uk', '', 'Xsoar@xsoar.co.il']
+    'input': 'Xsoar@test.org.de,Xsoar@test.eml, Xsoar@test.uk, Xsoar@xsoar.xlsx,Xsoar@xsoar.co.il'
+}
+
+EXPECTED_RESULTS = [
+    'xsoar@test.org.de',
+    '',
+    'xsoar@test.uk',
+    '',
+    'xsoar@xsoar.co.il',
+]
 
 
 def test_main(mocker):
