3c730b806354e09ad9d890ba657a8239f2847ebe
ilan <ierukhimovic@paloaltonetworks.com>
2022-11-27 17:16:51 +0200

    enhance compare zips

--- Tests/scripts/utils/compare_staging_graph.py
+++ Tests/scripts/utils/compare_staging_graph.py
@@ -17,7 +17,7 @@ from slack_sdk import WebClient
 
 yaml = YAML()
 
-SKIPPED_FILES = {"signatures.sf", "script-CommonServerPython.yml"}
+SKIPPED_FILES = {"signatures.sf", "script-CommonServerPython.yml", "changelog.json"}
 
 
 def sort_dict(dct: dict):
@@ -28,7 +28,12 @@ def sort_dict(dct: dict):
             try:
                 v.sort()
             except TypeError:
-                v.sort(key=lambda item: item.get("name"))
+                if v and v[0].get("id"):
+                    v.sort(key=lambda x: x["id"])
+                elif v and v[0].get("name"):
+                    v.sort(key=lambda x: x["name"])
+                else:
+                    print("Could not sort list", v)
 
 
 def compare_indexes(index_id_set_path: Path, index_graph_path: Path, output_path: Path) -> bool:
@@ -100,7 +105,7 @@ def file_diff_text(file1_path: Path, file2_path: Path, output_path_file: Path):
     return False
 
 
-def remove_known_diffs(dct1: dict, dct2: dict, known_diff: list[str]):
+def remove_known_diffs(dct1: dict, dct2: dict, known_diff: list[str], changelog: bool = False):
     for diff in known_diff:
         dct1.pop(diff, None)
         dct2.pop(diff, None)
@@ -125,7 +130,7 @@ def file_diff(output_path: Path, zip1_files: str, zip2_files: str, file: str, di
             with open(file1_path) as f1, open(file2_path) as f2:
                 dct1 = load_func(f1)
                 dct2 = load_func(f2)
-                remove_known_diffs(dct1, dct2, ["updated", "downloads"])
+                remove_known_diffs(dct1, dct2, ["updated", "downloads", "created"])
                 if file == "metadata.json":
                     sort_dict(dct1)
                     sort_dict(dct2)
@@ -220,6 +225,7 @@ def main():
             message,
             output_path,
         )
+    print("\n".join(message))
     if slack_token:
         slack_client = WebClient(token=slack_token)
         diff_output = output_path / f"diff-{marketplace}.zip"
