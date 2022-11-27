e52f1f7513ca38bbf8fa8fe04b814477e01927c1
tkatzir <tkatzir@paloaltonetworks.com>
2022-11-27 16:52:22 +0200

    Fix conflicts

--- Packs/MicrosoftTeams/Integrations/MicrosoftTeamsManagement/MicrosoftTeamsManagement.py
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeamsManagement/MicrosoftTeamsManagement.py
@@ -26,7 +26,7 @@ class Client:
         self.ms_client = MicrosoftClient(
             self_deployed=True,
             auth_id=app_id,
-            token_retrieval_url='https://login.microsoftonline.com/organizations/oauth2/v2.0/token'
+            token_retrieval_url='https://login.microsoftonline.com/organizations/oauth2/v2.0/token' \
                 if 'Client' not in connection_type \
                 else None,
             grant_type=GRANT_BY_CONNECTION[connection_type],
