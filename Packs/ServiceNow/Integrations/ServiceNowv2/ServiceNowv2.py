9150edaceec4135bbd74a9067e560f8081415a95
ostolero <ostolero@paloaltonetworks.com>
2022-11-28 15:54:09 +0200

    remove new arg

--- Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
+++ Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
@@ -2669,7 +2669,6 @@ def generic_api_call_command(client: Client, args: Dict) -> Union[str, CommandRe
     method = str(args.get("method"))
     path = str(args.get("path"))
     headers = json.loads(str(args.get("headers", {})))
-    json_body = args.get("json_body", "true")
     try:
         body: Dict = json.loads(str(args.get("body", {})))
     except ValueError:
