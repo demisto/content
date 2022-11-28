5595788211e54c129f079f0abe333d656d59ff9b
Arad Carmi <62752352+AradCarmi@users.noreply.github.com>
2022-11-28 17:58:50 +0200

    Update Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py

--- Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py
+++ Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py
@@ -656,7 +656,7 @@ def test_get_attachment(client):
 def test_get_attachments_without_attachment_id(mocker, client):
     """
     Given:
-        - raw response returned from get_attachment_command
+        - Raw response returned from get_attachment_command
 
     When:
         - Calling 'get_attachment_command' method.
