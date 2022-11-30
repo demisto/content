a2d7ec361878bcbca1a19dda9436a5cfd23155fc
gal-forer <gforer@paloaltonetworks.com>
2022-12-01 01:12:12 +0200

    Replaced the test conf fo a file that is generated from GSM

---
+++ Tests/scripts/add_secret_file_to_build.py
@@ -0,0 +1,28 @@
+import json
+import os
+import sys
+
+from Tests.scripts.GoogleSecretManagerModule import GoogleSecreteManagerModule
+
+
+def run(json_path_file):
+    secret_conf = GoogleSecreteManagerModule(get_secret_service_account_config())
+    project_id = os.environ['GSM_PROJECT_ID']
+    secrets = secret_conf.list_secrets(project_id, with_secret=True)
+    print(f'secrets from API: {secrets[0:20]}')
+    with open(json_path_file, 'w') as secrets_out_file:
+        secrets_out_file.write(json.dump(secrets))
+
+
+def get_secret_service_account_config() -> str:
+    print('##################################')
+    print('##################################')
+    print('##################################')
+    print(f'GSM_SERVICE_ACCOUNT: {os.environ["GSM_SERVICE_ACCOUNT"][0:5]}')
+    with open(os.environ['GSM_SERVICE_ACCOUNT']) as f:
+        creds = json.load(f)
+        return json.dumps(creds)
+
+
+if __name__ == '__main__':
+    run(sys.argv[1])
