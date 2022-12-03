0ca182d83aaea54a7290483ebdf49d7f44047537
Shahaf Ben Yakir <sbenyakir@paloaltonetworks.com>
2022-12-03 15:50:36 +0200

    Fix valid preview image condition

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -3715,8 +3715,12 @@ class Pack(object):
             '_image' in os.path.basename(file_path.lower()),
             (PackFolders.XSIAM_DASHBOARDS.value in file_path or PackFolders.XSIAM_REPORTS.value in file_path)
         ])
+        if not valid_image:
+            return False
 
-        if valid_image and not os.path.exists(file_path):
+        # In cases where a preview image was deleted valid_image will be true but we don't want to upload it as it does
+        # not exist anymore
+        elif not os.path.exists(file_path):
             logging.warning(f'Image: {file_path} was deleted and therefore will not be uploaded')
             return False
 
