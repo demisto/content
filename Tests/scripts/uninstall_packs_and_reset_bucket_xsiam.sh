d1b542e6791a66b5d551d67afe7b121853518200
moishce <mgalitzki@paloaltonetworks.com>
2022-12-05 16:47:23 +0200

    testing

--- Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
+++ Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
@@ -3,12 +3,13 @@
 # exit on errors
 set -e
 
-CLOUD_SERVERS_PATH1=$(cat xsiam_servers_path)
+XSIAM_SERVERS_PATH=$(cat xsiam_servers_path)
 
 echo "XSIAM_CHOSEN_MACHINE_ID is: $XSIAM_CHOSEN_MACHINE_ID."
 echo "CLOUD_SERVERS_PATH1 is: $CLOUD_SERVERS_PATH1."
 echo "CLOUD_API_KEYS is: $CLOUD_API_KEYS."
 echo "CLOUD_SERVERS_PATH is: $CLOUD_SERVERS_PATH."
+echo "XSIAM_SERVERS_PATH is: $XSIAM_SERVERS_PATH."
 
 
 
@@ -18,6 +19,6 @@ else
   gcloud auth activate-service-account --key-file="$GCS_MARKET_KEY" > auth.out 2>&1
   echo "Copying prod bucket to $XSIAM_CHOSEN_MACHINE_ID bucket."
   gsutil -m cp -r "gs://$GCS_SOURCE_BUCKET/content" "$GCS_MACHINES_BUCKET/$XSIAM_CHOSEN_MACHINE_ID/" > "$ARTIFACTS_FOLDER/Copy_prod_bucket_to_xsiam_machine_cleanup.log" 2>&1
-  python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --xsiam_servers_path $CLOUD_SERVERS_PATH1 --xsiam_servers_api_keys $CLOUD_API_KEYS
+  python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --xsiam_servers_path $XSIAM_SERVERS_PATH --xsiam_servers_api_keys $CLOUD_API_KEYS
 fi
 
