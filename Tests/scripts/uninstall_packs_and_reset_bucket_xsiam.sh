6f91bcf98ec095c6e114a9202777ceba1925eef5
moishce <mgalitzki@paloaltonetworks.com>
2022-12-06 16:45:50 +0200

    test

--- Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
+++ Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
@@ -7,11 +7,13 @@ echo "CLOUD_SERVERS_FILE before cat is $CLOUD_API_KEYS"
 
 
 CLOUD_SERVERS_PATH=$(cat $CLOUD_SERVERS_FILE)
-echo ${CLOUD_API_KEYS} > CLOUD_API_KEYS_FILE
+echo ${CLOUD_API_KEYS} > "cloud_api_keys.json"
 
+echo "CLOUD_SERVERS_PATH is $CLOUD_SERVERS_PATH"
 echo "CLOUD_API_KEYS_FILE is $CLOUD_API_KEYS_FILE"
 echo "XSIAM_API_KEYS is $XSIAM_API_KEYS"
-
+echo "cloud_api_keys.json > cat file: "
+cat cloud_api_keys.json
 
 if [[ -z ${XSIAM_CHOSEN_MACHINE_ID} ]]; then
   echo "XSIAM_CHOSEN_MACHINE_ID is not defiened, exiting..."
@@ -19,6 +21,6 @@ else
   gcloud auth activate-service-account --key-file="$GCS_MARKET_KEY" > auth.out 2>&1
   echo "Copying prod bucket to $XSIAM_CHOSEN_MACHINE_ID bucket."
   gsutil -m cp -r "gs://$GCS_PRODUCTION_V2_BUCKET/content" "gs://marketplace-v2-dist-dev/upload-flow/builds-xsiam/$XSIAM_CHOSEN_MACHINE_ID/" > "$ARTIFACTS_FOLDER/Copy_prod_bucket_to_xsiam_machine_cleanup.log" 2>&1
-  python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --cloud_servers_path $CLOUD_SERVERS_PATH --cloud_servers_api_keys $CLOUD_API_KEYS_FILE
+  python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --cloud_servers_path $CLOUD_SERVERS_PATH --cloud_servers_api_keys "cloud_api_keys.json"
 fi
 
