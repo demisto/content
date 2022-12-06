fedcc8b9f51903578946a68bc0ce3289512f598b
moishce <mgalitzki@paloaltonetworks.com>
2022-12-06 12:07:12 +0200

    remove needs

--- Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
+++ Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
@@ -3,7 +3,13 @@
 # exit on errors
 set -e
 
-XSIAM_SERVERS_PATH=$(cat xsiam_servers_path)
+echo "CLOUD_SERVERS_PATH  before  cat is $CLOUD_SERVERS_PATH"
+
+
+CLOUD_SERVERS_PATH=$(cat CLOUD_SERVERS_PATH)
+
+echo "CLOUD_SERVERS_PATH is $CLOUD_SERVERS_PATH"
+
 
 if [[ -z ${XSIAM_CHOSEN_MACHINE_ID} ]]; then
   echo "XSIAM_CHOSEN_MACHINE_ID is not defiened, exiting..."
@@ -11,6 +17,6 @@ else
   gcloud auth activate-service-account --key-file="$GCS_MARKET_KEY" > auth.out 2>&1
   echo "Copying prod bucket to $XSIAM_CHOSEN_MACHINE_ID bucket."
   gsutil -m cp -r "gs://$GCS_PRODUCTION_V2_BUCKET/content" "gs://marketplace-v2-dist-dev/upload-flow/builds-xsiam/$XSIAM_CHOSEN_MACHINE_ID/" > "$ARTIFACTS_FOLDER/Copy_prod_bucket_to_xsiam_machine_cleanup.log" 2>&1
-  python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --xsiam_servers_path $XSIAM_SERVERS_PATH --xsiam_servers_api_keys $XSIAM_API_KEYS
+  python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --cloud_servers_path $CLOUD_SERVERS_PATH --cloud_servers_api_keys $CLOUD_API_KEYS
 fi
 
