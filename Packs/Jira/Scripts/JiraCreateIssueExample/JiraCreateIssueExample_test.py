36e438ffbc6b96e881998fe50eb3c97063137c4c
kgal-pan <kgal@paloaltonetworks.com>
2022-12-07 13:48:28 +0200

    added custom fields, began UT rewrite

--- Packs/Jira/Scripts/JiraCreateIssueExample/JiraCreateIssueExample_test.py
+++ Packs/Jira/Scripts/JiraCreateIssueExample/JiraCreateIssueExample_test.py
@@ -1,82 +1,32 @@
-from JiraCreateIssueExample import add_additional_args, get_known_args_from_input, get_additional_args_from_input, DEFAULT_ARGS
+from JiraCreateIssueExample import \
+    DATE_FORMAT \
+    , validate_date_field \
+    # , parse_custom_fields \
+    # , add_custom_fields \
+    # , rm_custom_field_from_args
 import pytest
 
 
-@pytest.mark.parametrize("input,expected", [
-    ({DEFAULT_ARGS[0]: "system down"}.items(), {"summary": "system down"}),
-    ({DEFAULT_ARGS[0]: "system down", "unknown_key": "some key"}.items(), {"summary": "system down"}),
-    ({"unknown_key": "some key"}.items(), {})
+@pytest.mark.parametrize("due_date", [
+    ("2022-01-01"),
+    ("2022-12-12T13:00:00")
 ])
-def test_get_known_args_from_input(input, expected):
+def test_validate_date_field_data_remains(due_date):
 
-    """
-    Given:
-        - A dictionary view with Jira fields
-    When:
-        - 1 known argument key is supplied
-        - 1 known argument key is supplied, 1 unknown argument key is supplied
-        - 0 known arguments are supplied,  1 unknown argument key is supplied
-
-    Then:
-        - dict with known argument key is returned
-        - dict with known argument key is returned
-        - empty dict
-    """
-
-    args = get_known_args_from_input(input)
-
-    assert isinstance(args, dict)
-    assert args == expected
+    try:
+        validate_date_field(due_date)
+    except ValueError as ve:
+        assert "unconverted data remains" in str(ve)
 
 
-@pytest.mark.parametrize("input,expected", [
-    ({DEFAULT_ARGS[0]: "system down"}.items(), {}),
-    ({DEFAULT_ARGS[0]: "system down", "unknown_key": "some key"}.items(), {"unknown_key": "some key"}),
-    ({"unknown_key": "some key"}.items(), {"unknown_key": "some key"})
+@pytest.mark.parametrize("due_date", [
+    ("2022-01-01"),
+    ("2022-31-31")
 ])
-def test_get_additional_args_from_input(input, expected):
-    """
-    Given:
-        - A dictionary view with Jira fields
-    When:
-        - 1 unknown argument key is supplied
-        - 1 unknown argument key is supplied, 1 known argument key is supplied
-        - 1 known arguments are supplied, 0 unknown argument key is supplied
+def test_validate_date_field_format(due_date):
 
-    Then:
-        - empty dict
-        - dict with unknown argument key is returned
-        - dict with unknown argument key is returned
-    """
+    try:
+        validate_date_field(due_date)
+    except ValueError as ve:
+        assert f"time data '{due_date}' does not match format '{DATE_FORMAT}'" in str(ve)
 
-    args = get_additional_args_from_input(input)
-
-    assert isinstance(args, dict)
-    assert args == expected
-
-
-@pytest.mark.parametrize("known,additional,expected", [
-    ({DEFAULT_ARGS[0]: "system down"}, None, {DEFAULT_ARGS[0]: "system down"}),
-    ({DEFAULT_ARGS[0]: "system down"}, {"unknown_key": "some key"},
-        {'issueJSON': '{"unknown_key": "some key"}', DEFAULT_ARGS[0]: "system down"}),
-])
-def test_add_additional_args(known, additional, expected):
-    """
-    Given:
-        - 2 dictionaries of known and unknown keys
-    When:
-        - 1 known dict, no unknown dicts
-        - 1 known dict and one unknown dict
-
-    Then:
-        - dict with known argument key is returned
-        - dict with known argument and unknown keys returned
-    """
-
-    args = add_additional_args(known, additional)
-
-    if additional:
-        assert "issueJSON" in args.keys()
-
-    assert isinstance(args, dict)
-    assert args == expected
