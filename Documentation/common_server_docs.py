b4f5b45f63614b48e8e91bc9b1ba41a9c9ff522a
ilan <ierukhimovic@paloaltonetworks.com>
2022-12-07 20:17:55 +0200

    fix import OOPS

--- Documentation/common_server_docs.py
+++ Documentation/common_server_docs.py
@@ -11,7 +11,7 @@ from Tests.scripts.utils.log_util import install_logging
 
 # temp handling of unifier import
 try:
-    from demisto_sdk.commands.unify.integration_script_unifier import YmlUnifier
+    from demisto_sdk.commands.unify.integration_script_unifier import IntegrationScriptUnifier as YmlUnifier
 except:  # noqa
     from demisto_sdk.commands.prepare_content.integration_script_unifier import IntegrationScriptUnifier as YmlUnifier
 
