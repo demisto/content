74e14b8e7dfb436d4b1ae2ddac00c0bf9e006b03
moishce <mgalitzki@paloaltonetworks.com>
2022-12-12 17:33:29 +0200

    revert

--- Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
+++ Tests/scripts/uninstall_packs_and_reset_bucket_xsiam.sh
@@ -11,6 +11,6 @@ if [[ -z ${XSIAM_CHOSEN_MACHINE_ID} ]]; then
 else
   gcloud auth activate-service-account --key-file="$GCS_MARKET_KEY" > auth.out 2>&1
   echo "Copying prod bucket to $XSIAM_CHOSEN_MACHINE_ID bucket."
-  gsutil -m cp -r "gs://$GCS_SOURCE_BUCKET/content" "$GCS_MACHINES_BUCKET/$XSIAM_CHOSEN_MACHINE_ID/" > "$ARTIFACTS_FOLDER/Copy_prod_bucket_to_xsiam_machine_cleanup.log" 2>&1
+  gsutil -m cp -r "gs://$GCS_SOURCE_BUCKET/content" "gs://$GCS_MACHINES_BUCKET/$XSIAM_CHOSEN_MACHINE_ID/" > "$ARTIFACTS_FOLDER/Copy_prod_bucket_to_xsiam_machine_cleanup.log" 2>&1
   python3 ./Tests/Marketplace/search_and_uninstall_pack.py --xsiam_machine $XSIAM_CHOSEN_MACHINE_ID --cloud_servers_path $CLOUD_SERVERS_PATH --cloud_servers_api_keys "cloud_api_keys.json"
 fi
