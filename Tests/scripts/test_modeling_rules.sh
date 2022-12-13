32a944d501574bc7d7bde6efdc736cf24635ef5c
avidan-H <>
2022-12-13 13:42:34 +0200

    fix: change mrs_to_test.txt --> modeling_rules_to_test.txt

--- Tests/scripts/test_modeling_rules.sh
+++ Tests/scripts/test_modeling_rules.sh
@@ -8,4 +8,4 @@ API_KEY=$(jq ".[\"$XSIAM_CHOSEN_MACHINE_ID\"]" < "$XSIAM_API_KEYS")
 XSIAM_TOKEN=$(echo "$XSIAM_TOKENS" | jq ".[\"$XSIAM_CHOSEN_MACHINE_ID\"]")
 
 echo "Testing Modeling Rules"
-demisto-sdk modeling-rules test --xsiam-url="$XSIAM_URL" --auth-id="$AUTH_ID" --api-key="$API_KEY" --xsiam-token="$XSIAM_TOKEN" --non-interactive "$(tr '\n' ' ' < \"$ARTIFACTS_FOLDER/mrs_to_test.txt\")"
+demisto-sdk modeling-rules test --xsiam-url="$XSIAM_URL" --auth-id="$AUTH_ID" --api-key="$API_KEY" --xsiam-token="$XSIAM_TOKEN" --non-interactive "$(tr '\n' ' ' < \"$ARTIFACTS_FOLDER/modeling_rules_to_test.txt\")"
