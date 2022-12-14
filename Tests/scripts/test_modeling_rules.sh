7fdc0553f17b9595f81a51f24afdd5d2c77296a6
avidan-H <>
2022-12-14 17:04:23 +0200

    fix(test_modeling_rules): correctly extract base_url from configs

--- Tests/scripts/test_modeling_rules.sh
+++ Tests/scripts/test_modeling_rules.sh
@@ -4,7 +4,7 @@ XSIAM_SERVERS_PATH=${XSIAM_SERVERS_PATH:-"xsiam_servers.json"}
 
 # Get XSIAM Tenant Config Details
 XSIAM_SERVER_CONFIG=$(jq ".[\"$XSIAM_CHOSEN_MACHINE_ID\"]" < "$XSIAM_SERVERS_PATH")
-XSIAM_URL=$(echo "$XSIAM_SERVER_CONFIG" | jq '.base_url')
+XSIAM_URL=$(echo "$XSIAM_SERVER_CONFIG" | jq ".[\"base_url\"]")
 AUTH_ID=$(echo "$XSIAM_SERVER_CONFIG" | jq ".[\"x-xdr-auth-id\"]")
 API_KEY=$(jq ".[\"$XSIAM_CHOSEN_MACHINE_ID\"]" < "$XSIAM_API_KEYS")
 XSIAM_TOKEN=$(echo "$XSIAM_TOKENS" | jq ".[\"$XSIAM_CHOSEN_MACHINE_ID\"]")
