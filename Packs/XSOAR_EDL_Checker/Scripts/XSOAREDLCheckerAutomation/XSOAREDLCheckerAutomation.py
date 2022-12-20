dc57d527597b3fbdb501d0b473528d156a28702d
okarkkatz <okarkkatz@paloaltonetworks.com>
2022-12-20 11:36:10 +0200

    Changed automation name

--- Packs/XSOAR_EDL_Checker/Scripts/XSOAREDLCheckerAutomation/XSOAREDLCheckerAutomation.py
+++ Packs/XSOAR_EDL_Checker/Scripts/XSOAREDLCheckerAutomation/XSOAREDLCheckerAutomation.py
@@ -8,6 +8,6 @@ results = demisto.executeCommand('xsoaredlchecker-get-edl', {})
 output = [x['Contents'] for x in results]
 
 # build and return the result consolidated result
-readable = tableToMarkdown(f"XSOAR EDL Checker Response", output, headers=['Name', 'Status', 'Response', 'ItemsOnList'])
+readable = tableToMarkdown("XSOAR EDL Checker Response", output, headers=['Name', 'Status', 'Response', 'ItemsOnList'])
 result = CommandResults(readable_output=readable, outputs_prefix='EDLChecker', outputs=output, ignore_auto_extract=True)
 return_results(result)
