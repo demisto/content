18c08a0c8f1eee6e91f1ef9897886063793be1c4
ilaner <88267954+ilaner@users.noreply.github.com>
2022-12-22 10:41:50 +0200

    fix devcontainer and introduced codespaces (#23169)

--- .devcontainer/createCommand.sh
+++ .devcontainer/createCommand.sh
@@ -6,8 +6,9 @@ echo "Fixing permissions"
 
 sudo chown demisto .venv
 sudo chown demisto node_modules
+sudo chown demisto /workspaces
 sudo chown -R demisto $HOME
-sudo chown -R demisto /workspaces
+
 
 echo "Setting up git certificate"
 
@@ -17,11 +18,12 @@ echo "Setting up VSCode paths"
 
 cp .devcontainer/settings.json .vscode/settings.json 
 touch CommonServerUserPython.py
+touch DemistoClassApiModule.py
 path=$(printf '%s:' Packs/ApiModules/Scripts/*)
 rm -f .env
 echo "PYTHONPATH=""$path"":$PYTHONPATH" >> .env
 echo "MYPYPATH=""$path"":$MYPYPATH" >> .env
 
 echo "Setting up content dependencies"
-
-NO_HOOKS=1 .hooks/bootstrap
+poetry install
+npm install
