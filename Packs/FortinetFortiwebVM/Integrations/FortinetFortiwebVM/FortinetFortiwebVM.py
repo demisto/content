d44305dccb7cab5b4b4a9ef71aea13523507921b
ronh1 <ronh@qmasters.co>
2022-12-26 17:52:54 +0200

    Improving code.

--- Packs/FortinetFortiwebVM/Integrations/FortinetFortiwebVM/FortinetFortiwebVM.py
+++ Packs/FortinetFortiwebVM/Integrations/FortinetFortiwebVM/FortinetFortiwebVM.py
@@ -1420,8 +1420,9 @@ class ClientV1(Client):
         Returns:
             Dict[str, Any]: API response from FortiwebVM V2.
         """
-        endpoint = \
-            f'ServerObjects/ProtectedHostnames/ProtectedHostnames/{group_name}/ProtectedHostnamesNewHost/{member_id}'
+        endpoint = f'ServerObjects/ProtectedHostnames/ProtectedHostnames/{group_name} \
+                /ProtectedHostnamesNewHost/{member_id}'
+
         data = remove_empty_elements({
             'host':
             host,
