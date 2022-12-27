c6ba6bcc509c9aa2303ee890a4848a46ecea0be5
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2022-12-27 18:26:22 +0200

    Add support to recursive unshortening, and self-resolving

--- Packs/CommonScripts/Scripts/ResolveShortenedURL/ResolveShortenedURL.py
+++ Packs/CommonScripts/Scripts/ResolveShortenedURL/ResolveShortenedURL.py
@@ -1,44 +1,236 @@
+from typing import NamedTuple
+
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+
 import requests
-# disable insecure warnings
+from requests import Response
+
+from abc import ABCMeta, abstractmethod
+
+# Disable insecure warnings
 requests.packages.urllib3.disable_warnings()
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive'
-}
+
+class URLUnshorteningData(NamedTuple):
+    """A tuple containing data for unshortend URLs."""
+    original_url: str
+    """The original URL."""
+
+    resolved_url: str
+    """The resolved URL."""
+
+    service_name: str
+    """The name of the service used to unshorten the URL."""
+
+    redirect_history: list[str]
+    """A list of redirection history of the URL."""
+
+    usage_count: int | None = None
+    """The number of times the URL was unshortened. None if the service does not provide this information."""
+
+    raw_data: dict | list[dict] | None = None
+    """The raw data returned by the API / service. None if not available."""
+
+    def to_context_dict(self) -> dict:
+        """
+        Converts the data to a dictionary that will be used as the context data.
+        Adds recursion data only if relevant.
+
+        Note: We subtract 1 from RedirectCount because the original URL is included in the recursion history.
+        """
+        return {
+            "OriginalURL": self.original_url,
+            "ResolvedURL": self.resolved_url,
+            "ServiceName": self.service_name,
+            "UsageCount": self.usage_count,
+            "RedirectCount": len(self.redirect_history) - 1 if len(self.redirect_history) > 1 else None,
+            "RedirectHistory": self.redirect_history if len(self.redirect_history) > 1 else None,
+        }
+
+    def to_hr_dict(self) -> dict:
+        """
+        Converts the data to a dictionary that will be used as the human-readable data.
+        """
+        return {
+            "Original URL": self.original_url,
+            "Resolved URL": self.resolved_url,
+            "Service Used": self.service_name,
+            "Usage Count": self.usage_count,
+            "Redirect History": self.redirect_history if len(self.redirect_history) > 1 else None,
+        }
 
 
-def main():
-    url = demisto.args().get('url')
-    verify = not argToBoolean(demisto.args().get('insecure', False))
-    req = requests.get('https://unshorten.me/json/' + url, headers=headers, verify=verify)
-    content = req.json()
-    if content['success']:
-        resolvedUrl = content['resolved_url']
-        shortenedUrl = content['requested_url']
-        usageCount = content['usage_count']
-        ec = {}
-        ec['URL.Data'] = [resolvedUrl]
-        demisto.results({
-            'Type': entryTypes['note'],
-            'Contents': [resolvedUrl],
-            'ContentsFormat': formats['json'],
-            'HumanReadable': tableToMarkdown('Shorten URL results', [{
-                'Shortened URL': shortenedUrl,
-                'Resolved URL': resolvedUrl,
-                'Usage count': usageCount
-            }]),
-            'EntryContext': ec
-        })
-    else:
-        demisto.results('Provided URL could not be un-shortened')
+class URLUnshortingService(BaseClient, metaclass=ABCMeta):  # pragma: no cover
+    """An abstract base class for URL unshorteners."""
+    @property
+    @abstractmethod
+    def base_url(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def service_name(self) -> str:
+        pass
+
+    def __init__(self, redirect_limit: int | None = None, *args, **kwargs):
+        super().__init__(base_url=self.base_url)
+        self.redirect_limit = redirect_limit if redirect_limit is not None else 0
+
+    @abstractmethod
+    def resolve_url(self, url: str) -> URLUnshorteningData:
+        """
+        Resolve a shortened URL.
+
+        Args:
+            url (str): The URL to resolve.
+        """
+        pass
 
 
-if __name__ == "__builtin__" or __name__ == "builtins":
+# --- Abstract stuff end here ---
+
+class UnshortenMeSservice(URLUnshortingService):
+    """A class for unshortening URLs using unshorten.me."""
+    base_url = 'https://unshorten.me/json/'
+    service_name = "unshorten.me"
+
+    def resolve_url(self, url: str) -> URLUnshorteningData:
+        response = self._http_request(
+            method='GET',
+            url_suffix=url,
+            resp_type="json",
+        )
+
+        usage_count = response.get('usage_count', 0)
+
+        raw_data = [response]
+        previous_resolved_url = url
+        resolved_url = response.get('resolved_url')
+        redirect_history = [url, resolved_url]
+
+        # If redirect_limit is 0, it should be unlimited.
+        # We add 1 to `redirect_limit` because the original URL is part of `redirect_history`.
+        while (previous_resolved_url != resolved_url) and \
+                (self.redirect_limit == 0 or len(redirect_history) < self.redirect_limit + 1):
+            response = self._http_request(
+                method='GET',
+                url_suffix=resolved_url,
+                resp_type="json",
+            )
+
+            raw_data.append(response)
+            previous_resolved_url = resolved_url
+            resolved_url = response.get('resolved_url')
+            usage_count += response.get('usage_count', 0)
+            redirect_history.append(resolved_url)
+
+        if len(redirect_history) >= 2 and redirect_history[-1] == redirect_history[-2]:
+            redirect_history.pop()
+
+        return URLUnshorteningData(
+            original_url=url,
+            resolved_url=resolved_url,
+            service_name=self.service_name,
+            usage_count=usage_count,
+            redirect_history=redirect_history,
+            raw_data=raw_data,
+        )
+
+
+class SelfShortener(URLUnshortingService):
+    """A class for unshortening URLs using Python requests."""
+    base_url = ''
+    service_name = "Self Unshortener"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.redirect_limit >= 2:
+            self._session.max_redirects = self.redirect_limit - 1
+
+    def resolve_url(self, url: str) -> URLUnshorteningData:
+        request_kwargs: dict[str, Any] = {
+            "method": "GET",
+            "full_url": url,
+            "resp_type": "response",
+        }
+
+        if self.redirect_limit == 1:
+            request_kwargs["allow_redirects"] = False
+
+        response: Response = self._http_request(**request_kwargs)
+
+        return URLUnshorteningData(
+            original_url=url,
+            resolved_url=response.url,
+            service_name=self.service_name,
+            redirect_history=[r.url for r in response.history + [response]],
+        )
+
+
+def unshorten_url(service: str, url: str, redirect_limit: int, session_verify: bool = True) -> CommandResults:
+    """
+    Unshorten a shortened URL.
+
+    Args:
+        service (str): The service to use for unshortening.
+        url (str): The URL to un-shorten.
+        session_verify (bool): Whether to verify the SSL certificate of the request.
+        redirect_limit (int): A maximum number of recursions to run. Use 0 for unlimited.
+    """
+    client: URLUnshortingService
+
+    match service.lower():
+        case "unshorten.me":
+            client = UnshortenMeSservice(redirect_limit=redirect_limit,
+                                         verify=session_verify)
+
+        case "self":
+            client = SelfShortener(redirect_limit=redirect_limit,
+                                   verify=session_verify)
+
+        case _:
+            raise ValueError(f"Unknown service: {service}")
+
+    returned_data = client.resolve_url(url=url)
+
+    return CommandResults(
+        outputs_prefix="ResolveShortenedURL",
+        outputs_key_field="OriginalURL",
+        outputs=returned_data.to_context_dict(),
+        readable_output=tableToMarkdown(name="URL Unshortening Results",
+                                        t=returned_data.to_hr_dict(),
+                                        headers=list(returned_data.to_hr_dict().keys()),
+                                        removeNull=True),
+        indicator=Common.URL(url=returned_data.resolved_url, dbot_score=0),
+        raw_response=returned_data.raw_data,
+    )
+
+
+def main():  # pragma: no cover
+    default_redirect_limit: int = 10  # Default value to use if `redirect_limit` is None
+    args = demisto.args()
+
+    try:
+        url: str = args['url']
+        service: str = args['service']
+
+        # If `redirect_limit` is 0, it means unlimited recursions. If it's 1, it means no recursions.
+        redirect_limit = arg_to_number(args.get('redirect_limit', str(default_redirect_limit)))
+
+        # `arg_to_number` returns `None` if int conversion was unsuccessful.
+        if redirect_limit is None:
+            raise ValueError("'redirect_limit' must be a number.")
+
+        session_verify = not argToBoolean(demisto.args().get('insecure', 'False'))
+
+        result = unshorten_url(service=service, url=url, redirect_limit=redirect_limit, session_verify=session_verify)
+        return_results(result)
+
+    except Exception as e:
+        return_error(f'Provided URL could not be un-shortened.\nError: {str(e)}')
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):  # pragma: no cover
     main()
--- Packs/CommonScripts/Scripts/ResolveShortenedURL/ResolveShortenedURL_test.py
+++ Packs/CommonScripts/Scripts/ResolveShortenedURL/ResolveShortenedURL_test.py
@@ -1,43 +1,128 @@
-import demistomock as demisto
-import ResolveShortenedURL
+import json
+from pathlib import Path
+
+import pytest
+from requests import Response
+
+from ResolveShortenedURL import *
 
 
-def test_resolve_un_shortened_url(mocker, requests_mock):
+def load_test_data(folder: str, file_name: str) -> dict:
     """
-     Given:
-         - The script args.
-     When:
-         - Running the main with a URL that can be unshortened.
-     Then:
-         - Validating calling to 'demisto.results' once with the right arguments.
-     """
-    args = {'url': 'https://test/example'}
-    mocker.patch.object(demisto, 'args', return_value=args)
-    content = {'success': True, 'resolved_url': 'test', 'requested_url': 'test', 'usage_count': 'test'}
-    excepted_args = {'Type': 1, 'Contents': ['test'], 'ContentsFormat': 'json',
-                     'HumanReadable': '### Shorten URL results\n|Resolved URL|Shortened URL|Usage count|\n'
-                                      '|---|---|---|\n| test | test | test |\n', 'EntryContext': {'URL.Data': ['test']}}
-    requests_mock.get('https://unshorten.me/json/' + args['url'], json=content)
-    execute_mock = mocker.patch.object(demisto, 'results')
-    ResolveShortenedURL.main()
-    assert execute_mock.call_count == 1
-    assert execute_mock.call_args[0][0] == excepted_args
+    A function for loading and returning data from json files within the "test_data" folder.
 
+    Args:
+        folder (str): Name of the parent folder of the file within `test_data`.
+        file_name (str): Name of a json file to load data from.
 
-def test_resolve_shortened_url(mocker, requests_mock):
+    Returns:
+        dict: Dictionary data loaded from the json file.
     """
-     Given:
-         - The script args.
-     When:
-         - Running the main with a URL that cannot be unshortened.
-     Then:
-         - Validating calling to 'demisto.results' once with the relevant massage.
-     """
-    args = {'url': 'https://test.com'}
-    mocker.patch.object(demisto, 'args', return_value=args)
-    content = {'success': False}
-    requests_mock.get('https://unshorten.me/json/' + args['url'], json=content)
-    execute_mock = mocker.patch.object(demisto, 'results')
-    ResolveShortenedURL.main()
-    assert execute_mock.call_count == 1
-    assert execute_mock.call_args[0][0] == 'Provided URL could not be un-shortened'
+    with open(Path("test_data") / folder / f"{file_name}.json", "r") as f:
+        return json.load(f)
+
+
+class TestUnshortenMeService:
+    @pytest.mark.parametrize("args, mock_files_prefix, mock_files_count, expected_output",
+                             [
+                                 (  # General test
+                                     {"url": "https://short.url/a", "redirect_limit": 6},
+                                     "nested_unshorten",
+                                     3,
+                                     load_test_data("unshorten.me", "nested_unshorten_expected_output"),
+                                 ),
+                                 (  # Test a case where redirect is stopped because of `redirect_limit`
+                                     {"url": "https://short.url/a", "redirect_limit": 1},
+                                     "nested_unshorten",
+                                     2,
+                                     load_test_data("unshorten.me", "limited_unshorten_expected_output"),
+                                 ),
+                             ])
+    def test_shortened_url(self, mocker, args: dict, mock_files_prefix: str,
+                           mock_files_count: int, expected_output: dict):
+        """
+        Given: Parameters for unshortening a URL using unshorten.me.
+        When: Calling the `unshorten_url` function.
+        Then: Ensure the context output is returned as expected, and that redirect_limit is working as expected.
+        """
+        mock_data = [load_test_data("unshorten.me", mock_files_prefix + f"_{i}")
+                     for i in range(mock_files_count)]
+
+        def recursion_side_effect():
+            for i in range(mock_files_count):
+                yield mock_data[i]
+
+        mocker.patch.object(BaseClient, "_http_request", side_effect=recursion_side_effect())
+
+        result = unshorten_url(service="unshorten.me",
+                               url=args["url"],
+                               redirect_limit=args["redirect_limit"])
+
+        assert result.outputs["RedirectCount"] <= args["redirect_limit"]
+        assert result.outputs == expected_output
+
+
+class TestSelfService:
+    @staticmethod
+    def get_response_mock(url: str, status_code: int, previous_response: Response | None = None) -> Response:
+        """
+        Create a mock requests.Response object.
+
+        Args:
+            url (str): URL to set as th `url` attribute (should be the last URL in the redirect chain).
+            status_code (int): Status code to set as the `status_code` attribute.
+            previous_response (Response | None, optional): Previous response in the redirect chain to use
+                for the `history` attribute. Defaults to None.
+
+        Returns:
+            Response: A requests.Response object to use as a mock.
+        """
+        response_mock = Response()
+        response_mock.url = url
+        response_mock.status_code = status_code,
+
+        response_mock.history = []
+
+        if previous_response is not None:
+            response_mock.history.append(previous_response)
+            response_mock.history.extend(previous_response.history)
+
+        return response_mock
+
+    @pytest.mark.parametrize("args, mock_response_obj, expected_output",
+                             [
+                                 (
+                                     {"url": "https://short.url/a", "redirect_limit": 6},
+                                     get_response_mock(
+                                         url='https://xsoar.pan.dev/',
+                                         status_code=200,
+                                         previous_response=get_response_mock(
+                                             url='https://short.url/b',
+                                             status_code=301,
+                                             previous_response=get_response_mock(
+                                                 url='https://short.url/a',
+                                                 status_code=301))),
+                                     load_test_data("self", "nested_unshorten_expected_output"),
+                                 ),
+                                 (
+                                     {"url": "https://short.url/a", "redirect_limit": 1},
+                                     get_response_mock(
+                                         url='https://xsoar.pan.dev/',
+                                         status_code=200,
+                                         previous_response=get_response_mock(
+                                             url='https://short.url/b',
+                                             status_code=301)),
+                                     load_test_data("self", "limited_unshorten_expected_output"),
+                                 ),
+                             ])
+    def test_shortened_url(self, mocker, args: dict, mock_response_obj: Response, expected_output: dict):
+        """
+        Given: Parameters for self unshortening a URL using the 'requests' library.
+        When: Calling the `unshorten_url` function.
+        Then: Ensure the context output is returned as expected, and that redirect_limit is working as expected.
+        """
+        mocker.patch.object(BaseClient, "_http_request", return_value=mock_response_obj)
+
+        assert unshorten_url(service="Self",
+                             url=args["url"],
+                             redirect_limit=args["redirect_limit"]).outputs == expected_output
