aa74d741790a415d7adc41770776c639b51f313a
Darya Koval <dkoval@paloaltonetworks.com>
2022-12-27 14:13:43 +0200

    save assign file type if not list

--- Packs/CommonScripts/Scripts/ParseEmailFilesV2/ParseEmailFilesV2.py
+++ Packs/CommonScripts/Scripts/ParseEmailFilesV2/ParseEmailFilesV2.py
@@ -91,6 +91,8 @@ def extract_file_info(entry_id: str) -> tuple:
         dt_file_type = demisto.dt(demisto.context(), f"File(val.EntryID=='{entry_id}').Type")
         if isinstance(dt_file_type, list):
             file_type = dt_file_type[0]
+        else:
+            file_type = dt_file_type
 
     except Exception as ex:
         return_error(
