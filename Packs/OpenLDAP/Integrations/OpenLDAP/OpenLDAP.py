1cb9ca24d373f7875dbc72ef66f8e7150a0997f2
ShacharKidor <skidorball@paloaltonetworks.com>
2022-12-29 19:14:40 +0200

    Editing a .py file for triggering lint - on a valid file

--- Packs/OpenLDAP/Integrations/OpenLDAP/OpenLDAP.py
+++ Packs/OpenLDAP/Integrations/OpenLDAP/OpenLDAP.py
@@ -687,8 +687,8 @@ def main():  # pragma: no coverage
             demisto.info(f'ad-authenticate-and-roles command - entry results: {entry_result}')
             return_results(entry_result)
         else:
+            demisto.debug('testtttttt')
             raise NotImplementedError(f'Command {command} is not implemented')
-            testttttt
 
     # Log exceptions
     except Exception as e:
