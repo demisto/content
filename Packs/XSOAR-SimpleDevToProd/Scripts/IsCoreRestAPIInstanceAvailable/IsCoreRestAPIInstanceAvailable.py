5f2d2091464df184cbb55bd042fdaa0d9107b82c
aradcarmi <aradcarmi220@gmail.com>
2023-01-01 13:42:49 +0200

    updated script

--- Packs/XSOAR-SimpleDevToProd/Scripts/IsCoreRestAPIInstanceAvailable/IsCoreRestAPIInstanceAvailable.py
+++ Packs/XSOAR-SimpleDevToProd/Scripts/IsCoreRestAPIInstanceAvailable/IsCoreRestAPIInstanceAvailable.py
@@ -1,14 +1,18 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-brandName = "Demisto REST API"
+brands_names = ["demisto rest api", "core rest api"]
+
 instanceName = demisto.args().get('instanceName')
 allInstances = demisto.getModules()
 brandInstances = [instanceName for instanceName in allInstances if allInstances[instanceName]['brand'].lower(
-) == brandName.lower() and demisto.get(allInstances[instanceName], 'state') and allInstances[instanceName]['state'] == 'active']
+) in brands_names and demisto.get(allInstances[instanceName], 'state') and allInstances[instanceName]['state'] == 'active']
 if brandInstances and instanceName in brandInstances:
     instance = allInstances.get(instanceName)
     instance['name'] = instanceName
-    demisto.setContext('DemsistoAPIInstances', instance)
+    if instance['brand'].lower() == 'demisto rest api':
+        demisto.setContext('DemistoAPIInstances', instance)
+    else:
+        demisto.setContext('CoreAPIInstances', instance)
     demisto.results('yes')
 else:
     demisto.results('no')
