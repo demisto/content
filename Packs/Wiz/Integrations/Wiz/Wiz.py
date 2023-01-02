162cbf17b18d7c64fb18feac84c560fd6abc9db7
Ariel <ariel.tobiana@wiz.io>
2023-01-02 15:27:46 +0200

    remove redundant parenthesis

--- Packs/Wiz/Integrations/Wiz/Wiz.py
+++ Packs/Wiz/Integrations/Wiz/Wiz.py
@@ -697,7 +697,7 @@ def reject_issue(issue_id, reject_reason, reject_note):
     if 'errors' in response:
         demisto.error(f"Could not find Issue with ID {issue_id}")
         demisto.error(f"Error: {response}")
-        return (f"Could not find Issue with ID {issue_id}")
+        return f"Could not find Issue with ID {issue_id}"
 
     return response
 
