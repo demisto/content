e407b9b16b9f4c1424095213453169773ccd2481
okarkkatz <okarkkatz@paloaltonetworks.com>
2023-01-02 13:13:51 +0200

    Added the base code

---
+++ Packs/AzureSecurityCenter/Integrations/MicrosoftDefenderForCloudEventCollector/MicrosoftDefenderForCloudEventCollector.py
@@ -0,0 +1,168 @@
+from requests import Response
+import demistomock as demisto
+from CommonServerPython import *
+import urllib3
+from typing import Any, Dict, Tuple, List, Optional, Union, cast
+from MicrosoftApiModule import *  # noqa: E402
+
+# Disable insecure warnings
+urllib3.disable_warnings()
+
+''' CONSTANTS '''
+
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+VENDOR = 'Micrsoft'
+PRODUCT = 'Microsoft_defender_for_cloud'
+API_VERSION = '2022-01-01'
+
+''' CLIENT CLASS '''
+
+
+class MsClient:
+    """
+    Microsoft Client enables authorized access to Azure Security Center.
+    """
+
+    def __init__(self, tenant_id, auth_id, enc_key, app_name, server, verify, proxy, self_deployed, subscription_id,
+                 ok_codes, certificate_thumbprint, private_key):
+        base_url_with_subscription = f"{server}subscriptions/{subscription_id}/"
+        self.ms_client = MicrosoftClient(
+            tenant_id=tenant_id, auth_id=auth_id, enc_key=enc_key, app_name=app_name,
+            base_url=base_url_with_subscription, verify=verify, proxy=proxy, self_deployed=self_deployed,
+            ok_codes=ok_codes, scope="https://management.azure.com/.default",
+            certificate_thumbprint=certificate_thumbprint, private_key=private_key)
+        self.server = server
+        self.subscription_id = subscription_id
+
+    
+    def get_event_list():
+        
+
+
+def test_module(client: MsClient):
+    """
+       Performs basic GET request to check if the API is reachable and authentication is successful.
+       Returns ok if successful.
+       """
+    if client.subscription_id:
+        client.list_locations()
+    else:
+        client.list_sc_subscriptions()
+    demisto.results('ok')
+
+
+def get_events(client, alert_status):
+    events = client.search_events(
+        prev_id=0,
+        alert_status=alert_status
+    )
+    hr = tableToMarkdown(name='Test Event', t=events)
+    return events, CommandResults(readable_output=hr)
+
+
+def fetch_events(client: Client, last_run: Dict[str, int],
+                 first_fetch_time: Optional[int], alert_status: Optional[str]
+                 ):
+    """
+    Args:
+        client (Client): HelloWorld client to use.
+        last_run (dict): A dict with a key containing the latest event created time we got from last fetch.
+        first_fetch_time(int): If last_run is None (first time we are fetching), it contains the timestamp in
+            milliseconds on when to start fetching events.
+        alert_status (str): status of the alert to search for. Options are: 'ACTIVE' or 'CLOSED'.
+    Returns:
+        dict: Next run dictionary containing the timestamp that will be used in ``last_run`` on the next fetch.
+        list: List of events that will be created in XSIAM.
+    """
+    prev_id = last_run.get('prev_id', None)
+    if not prev_id:
+        prev_id = 0
+
+    events = client.search_events(
+        prev_id=prev_id,
+        alert_status=alert_status
+    )
+    demisto.info(f'Fetched event with id: {prev_id + 1}.')
+
+    # Save the next_run as a dict with the last_fetch key to be stored
+    next_run = {'prev_id': prev_id + 1}
+    demisto.info(f'Setting next run {next_run}.')
+    return next_run, events
+
+
+''' MAIN FUNCTION '''
+
+
+def main() -> None:
+    """
+    main function, parses params and runs command functions
+    """
+
+    params = demisto.params()
+    args = demisto.args()
+    command = demisto.command()
+    api_key = params.get('apikey', {}).get('password')
+    base_url = urljoin(params.get('url'), '/api/v1')
+    verify_certificate = not params.get('insecure', False)
+
+    # How much time before the first fetch to retrieve events
+    first_fetch_time = arg_to_datetime(
+        arg=params.get('first_fetch', '3 days'),
+        arg_name='First fetch time',
+        required=True
+    )
+    first_fetch_timestamp = int(first_fetch_time.timestamp()) if first_fetch_time else None
+    assert isinstance(first_fetch_timestamp, int)
+    proxy = params.get('proxy', False)
+    alert_status = params.get('alert_status', None)
+
+    demisto.debug(f'Command being called is {command}')
+    try:
+        headers = {
+            'Authorization': f'Bearer {api_key}'
+        }
+        client = Client(
+            base_url=base_url,
+            verify=verify_certificate,
+            headers=headers,
+            proxy=proxy)
+
+        if command == 'test-module':
+            # This is the call made when pressing the integration Test button.
+            result = test_module(client, params, first_fetch_timestamp)
+            return_results(result)
+
+        elif command in ('hello-world-get-events', 'fetch-events'):
+            if command == 'hello-world-get-events':
+                should_push_events = argToBoolean(args.pop('should_push_events'))
+                events, results = get_events(client, alert_status)
+                return_results(results)
+
+            else:  # command == 'fetch-events':
+                should_push_events = True
+                last_run = demisto.getLastRun()
+                next_run, events = fetch_events(
+                    client=client,
+                    last_run=last_run,
+                    first_fetch_time=first_fetch_timestamp,
+                    alert_status=alert_status,
+                )
+                # saves next_run for the time fetch-events is invoked
+                demisto.setLastRun(next_run)
+
+            if should_push_events:
+                send_events_to_xsiam(
+                    events,
+                    vendor=VENDOR,
+                    product=PRODUCT
+                )
+
+    # Log exceptions and return errors
+    except Exception as e:
+        return_error(f'Failed to execute {command} command.\nError:\n{str(e)}')
+
+
+''' ENTRY POINT '''
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
