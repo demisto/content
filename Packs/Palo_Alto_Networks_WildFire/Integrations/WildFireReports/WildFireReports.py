51173f1ae8fd7e0657d1daffca91badb76783454
Menachem Weinfeld <mmhw770@gmail.com>
2023-01-02 14:18:35 +0200

    WildFire-Reports

--- Packs/Palo_Alto_Networks_WildFire/Integrations/WildFireReports/WildFireReports.py
+++ Packs/Palo_Alto_Networks_WildFire/Integrations/WildFireReports/WildFireReports.py
@@ -93,7 +93,7 @@ def main():
         base_url = base_url[:-1]
     if base_url and not base_url.endswith('/publicapi'):
         base_url += '/publicapi'
-    token = params.get('token')
+    token = params.get('credentials', {}).get('password') or params.get('token')
     if not token:
         token = demisto.getLicenseCustomField("WildFire-Reports.token")
     if not token:
