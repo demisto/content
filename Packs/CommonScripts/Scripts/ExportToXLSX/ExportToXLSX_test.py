2f5e5ad767a21d5624d14b64c0cc17fcf50f405b
maimorag <mmorag@paloaltonetworks.com>
2023-01-04 17:07:43 +0200

    adding tests

--- Packs/CommonScripts/Scripts/ExportToXLSX/ExportToXLSX_test.py
+++ Packs/CommonScripts/Scripts/ExportToXLSX/ExportToXLSX_test.py
@@ -1,4 +1,5 @@
 from ExportToXLSX import parse_data
+import pytest
 
 DATA_INPUT_SINGLE_DICT = {"key1": "val1", "key2": "val2"}
 DATA_INPUT_MULTIPLE_DICTS = '{\"key1\":\"val1\",\"key2\":\"val2\"},{\"key1\":\"val3\",\"key2\":\"val4\"}'
@@ -78,3 +79,35 @@ def test_parse_data_multiple_items_with_error():
         assert False
     except ValueError as err:
         assert 'Number of sheet names should be equal to the number of data items.' in err.args[0]
+        
+ARGS_VERIFY_ID = [
+    (None, None,
+     True),
+    (1, 1,
+     True)
+
+]
+
+
+@pytest.mark.parametrize('is_bold, is_border, is_none', ARGS_VERIFY_ID)
+def test_prepare_bold_and_border(is_bold: bool, is_border: bool, is_none: bool,  expected_results):
+    """
+    Given:
+        - optinal is_bold: bool, is_border: bool
+    When:
+        - running prepare_bold_and_border
+    Then:
+        - The http request is called with the right arguments, and returns the right command result.
+    """
+    from ExportToXLSX import prepare_bold_and_border
+    from xlsxwriter import Workbook
+    expected_workbook = Workbook()
+    bold_value = 1 if is_bold else 0
+    border_value = 1 if is_border else 0
+    expected_bold = expected_workbook.add_format({"bold": bold_value, "border": border_value})
+    expected_border = expected_workbook.add_format({"border": border_value})
+    result_bold, result_border = prepare_bold_and_border()
+        
+
+    
+def test_write_data():
