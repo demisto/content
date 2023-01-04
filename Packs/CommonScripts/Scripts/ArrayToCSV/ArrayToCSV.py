081ddfb1c1a17511fcc18058af4b11baa7d65542
Yehuda <yrosenberg@paloaltonetworks.com>
2023-01-04 17:07:23 +0200

    py

--- Packs/CommonScripts/Scripts/ArrayToCSV/ArrayToCSV.py
+++ Packs/CommonScripts/Scripts/ArrayToCSV/ArrayToCSV.py
@@ -1,17 +1,25 @@
+from typing import Iterable
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+from traceback import format_exc
 
 
-def main():
+def arr_to_csv_command(array: Iterable) -> CommandResults:
+    csv = ','.join(array)
+    return CommandResults(
+        readable_output=csv
+    )
+
+
+def main():  # pragma: no cover
     args = demisto.args()
-    the_val = args.get('value')
+    array = argToList(args.get('value'))
     try:
-        the_csv = ','.join(the_val)
-        return_results(the_csv)
-    except Exception as ex:
-        demisto.debug('Ooops, Something aint working!')
-        LOG.print_log()
-        demisto.debug(ex)
+        results = arr_to_csv_command(array=array)
+        return_results(results)
+    except Exception as e:
+        demisto.error(format_exc())
+        return_error(f'ArrToCSV command failed. Error: {e}')
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
