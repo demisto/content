4e561d2acee7d5067c20a4fdd33b36556aee5311
Shmuel Fainberg <sfainberg@paloaltonetworks.com>
2023-01-09 16:29:24 +0200

    Standardized CreateHash code

--- Packs/CommonScripts/Scripts/CreateHash/CreateHash.py
+++ Packs/CommonScripts/Scripts/CreateHash/CreateHash.py
@@ -1,32 +1,48 @@
-# Simple script to create a sha256 hash from agiven input
+
 import hashlib
 from hashlib import blake2b
 
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-text = demisto.args()['text']
-hashtype = demisto.args()['type']
 
-if hashtype == "sha512":
-    h = hashlib.sha512()
-    h.update(text.encode('utf-8'))
-elif hashtype == "sha256":
-    h = hashlib.sha256()
-    h.update(text.encode('utf-8'))
-elif hashtype == 'sha1':
-    h = hashlib.sha1()  # nosec
-    h.update(text.encode('utf-8'))
-elif hashtype == 'md5':
-    h = hashlib.md5()   # nosec
-    h.update(text.encode('utf-8'))
-else:
-    h = blake2b()
-    h.update(text.encode('utf-8'))
+def create_hash(text, hashtype):
+    """Create a sha256 hash from a given input
 
-context = {
-    "CreateHash": str(h.hexdigest())
-}
-command_results = CommandResults(outputs=context)
+    Args:
+        text (str): input to hash
+        hashtype (str): hash type
+    """
+    if hashtype == "sha512":
+        h = hashlib.sha512()
+        h.update(text.encode('utf-8'))
+    elif hashtype == "sha256":
+        h = hashlib.sha256()
+        h.update(text.encode('utf-8'))
+    elif hashtype == 'sha1':
+        h = hashlib.sha1()  # nosec
+        h.update(text.encode('utf-8'))
+    elif hashtype == 'md5':
+        h = hashlib.md5()   # nosec
+        h.update(text.encode('utf-8'))
+    else:
+        h = blake2b()
+        h.update(text.encode('utf-8'))
 
-return_results(command_results)
+    context = {
+        "CreateHash": str(h.hexdigest())
+    }
+    command_results = CommandResults(outputs=context)
+
+    return_results(command_results)
+
+
+def main():
+    args = demisto.args()
+    text = args.get('text')
+    hashtype = args.get('type')
+    create_hash(text, hashtype)
+
+
+if __name__ in ('__builtin__', 'builtins', '__main__'):
+    main()
