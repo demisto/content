4a12a16480075d715038c7f6b27433a6b6378f9c
Shmuel Fainberg <sfainberg@paloaltonetworks.com>
2023-01-09 14:01:39 +0200

    Added UT doc-strings

--- Packs/CommonScripts/Scripts/ServerLogs/ServerLogs_test.py
+++ Packs/CommonScripts/Scripts/ServerLogs/ServerLogs_test.py
@@ -3,6 +3,14 @@ import demistomock as demisto
 
 
 def test_main(mocker):
+    """
+    Given:
+        - Valid response from ssh command
+    When:
+        - Running the script
+    Then:
+        - Ensure results function is called with the expected output
+    """
     from ServerLogs import main
     mocker.patch.object(demisto, 'executeCommand', return_value=[{'Contents': {'output': 'output'}}])
     return_results = mocker.patch.object(demisto, 'results')
@@ -15,6 +23,14 @@ def raise_value_error():
 
 
 def test_main_fail(mocker):
+    """
+    Given:
+        - Invalid response from ssh command
+    When:
+        - Running the script
+    Then:
+        - Ensure return_error function is called with the expected output
+    """
     from ServerLogs import main
     mocker.patch.object(demisto, 'executeCommand', side_effect=raise_value_error)
     with pytest.raises(Exception):
--- Packs/CommonScripts/Scripts/ServerLogsDocker/ServerLogsDocker_test.py
+++ Packs/CommonScripts/Scripts/ServerLogsDocker/ServerLogsDocker_test.py
@@ -2,6 +2,14 @@ import demistomock as demisto
 
 
 def test_main(mocker):
+    """
+    Given:
+        - Valid response from ssh command
+    When:
+        - Running the script
+    Then:
+        - Ensure results function is called with the expected output
+    """
     from ServerLogsDocker import main
     mocker.patch.object(demisto, 'executeCommand', return_value=[{'Contents': {'output': 'output'}}])
     return_results = mocker.patch.object(demisto, 'results')
