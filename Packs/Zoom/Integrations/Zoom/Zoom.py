847e3b9f7e3ef3ea73d68c7fbc5c74b6c2d7bdcc
Yehuda <yrosenberg@paloaltonetworks.com>
2023-01-10 13:50:05 +0200

    fix

--- Packs/Zoom/Integrations/Zoom/Zoom.py
+++ Packs/Zoom/Integrations/Zoom/Zoom.py
@@ -606,7 +606,7 @@ def zoom_create_meeting_command(client, **args) -> CommandResults:
     )
 
 
-def zoom_fetch_recording_command(client: Client, **args) -> list:
+def zoom_fetch_recording_command(client: Client, **args) -> CommandResults:
     # preprocessing
     results = []
     meeting_id = args.get('meeting_id')
