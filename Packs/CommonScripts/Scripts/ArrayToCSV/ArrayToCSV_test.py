fe78aa00b414a75fbebd39b2dc7933a27aa8b516
Yehuda <yrosenberg@paloaltonetworks.com>
2023-01-11 13:06:13 +0200

    more

--- Packs/CommonScripts/Scripts/ArrayToCSV/ArrayToCSV_test.py
+++ Packs/CommonScripts/Scripts/ArrayToCSV/ArrayToCSV_test.py
@@ -20,10 +20,23 @@ def test_arr_to_csv_command__array_of_one_value():
         Given -
            an array
         When -
-            array contains a list of two str objects
+            array contains a list of one str objects
         Then -
-            the function will return the strings comma seperated
+            the function will return the string as is.
     """
     arr = ["mock"]
     command_result = arr_to_csv_command(arr)
     assert command_result.readable_output == "mock"
+
+def test_arr_to_csv_command__empty_array():
+    """
+        Given -
+           an array
+        When -
+            array contains nothing
+        Then -
+            the function will return ''
+    """
+    arr = ""
+    command_result = arr_to_csv_command(arr)
+    assert command_result.readable_output == ''
