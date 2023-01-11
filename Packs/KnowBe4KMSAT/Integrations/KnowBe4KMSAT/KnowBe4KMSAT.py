d5b426795cb8f72e6833cf5a2c19e120a921a9a9
kbreton <kbreton@knowbe4.com>
2023-01-11 18:41:54 +0000

    added get-user-events command

--- Packs/KnowBe4KMSAT/Integrations/KnowBe4KMSAT/KnowBe4KMSAT.py
+++ Packs/KnowBe4KMSAT/Integrations/KnowBe4KMSAT/KnowBe4KMSAT.py
@@ -51,6 +51,33 @@ class Client(BaseClient):
         return self._http_request(method='GET', url_suffix='/account/risk_score_history', resp_type='json',  ok_codes=(200,))
 
 
+class UserEventClient(BaseClient):
+    """Client class to interact with the KMSAT User EventAPI
+    """
+    def __init__(self, base_url, verify, proxy, headers=None, max_fetch=None):
+        self.max_fetch = max_fetch        
+
+        super().__init__(base_url=base_url, verify=verify, headers=headers, proxy=proxy)
+
+    def user_events(self, args: dict, page: int = None, page_size: int = None):
+        
+        params = remove_empty_elements({
+            'event_type': args.get('event_type'),
+            'target_user': args.get('target_user'),
+            'external_id': args.get('external_id'),
+            'source': args.get('source'),
+            'occurred_date': args.get('occurred_date'),
+            'risk_level': args.get('risk_level'),
+            'risk_decay_mode': args.get('risk_decay_mode'),
+            'risk_expire_date': args.get('risk_expire_date'),
+            'order_by': args.get('order_by'),
+            'order_direction': args.get('order_direction'),
+            'page': page,
+            'per_page': page_size
+        })
+        return self._http_request(method='GET', url_suffix='/events', resp_type='json', ok_codes=(200,), params=params)
+    
+
 ''' HELPER FUNCTIONS '''
 
 ''' COMMAND FUNCTIONS '''
@@ -76,6 +103,17 @@ def get_account_risk_score_history(client: Client) -> CommandResults:
                           outputs_key_field='',
                           raw_response=response,
                           readable_output=tableToMarkdown(name='Account_Risk_Score_History', t=response))
+
+
+def get_user_events(client: UserEventClient, args: dict) -> CommandResults:
+    response = client.user_events(args, 1, 100)
+    return_results(response)
+    if response is None:
+        raise DemistoException('Translation failed: the response from server did not include user event `data`.', res=response)
+    return CommandResults(outputs_prefix='KMSAT_User_Events_Returned',
+                          outputs_key_field='',
+                          raw_response=response,
+                          readable_output=tableToMarkdown(name='KMSAT_User_Events', t=response))
 
 
 def test_module(client: Client) -> str:
@@ -124,10 +162,15 @@ def main() -> None:
     
     # get the service API url
     base_url = urljoin(demisto.params()['url'], '/v1')
+    userEvents_base_url =demisto.params()['userEventsUrl']
 
     # verify api key or creds are specified
     if not params.get('apikey') or not (key := params.get('apikey', {}).get('password')):
-        raise DemistoException('Missing API Key. Fill in a valid key in the integration configuration.')
+        raise DemistoException('Missing Reporting API Key. Fill in a valid key in the integration configuration.')
+    
+    # verify User Events api key or creds are specified
+    if not params.get('userEventsApiKey') or not (userEventsApiKey := params.get('userEventsApiKey', {}).get('password')):
+        raise DemistoException('Missing User Events API Key. Fill in a valid key in the integration configuration.')
     
     # if your Client class inherits from BaseClient, SSL verification is
     # handled out of the box by it, just pass ``verify_certificate`` to
@@ -140,15 +183,21 @@ def main() -> None:
     
     try:
 
-        headers: Dict = {
-            'Authorization': 'Bearer ' + key,
-            'Content-Type': 'application/json'
-        }
-
         client = Client(
             base_url=base_url,
             verify=verify_certificate,
-            headers=headers,
+            headers= {
+            'Authorization': 'Bearer ' + key,
+            'Content-Type': 'application/json'
+            },
+            proxy=proxy)
+        
+        userEventClient = UserEventClient(base_url=userEvents_base_url,
+            verify=verify_certificate,
+            headers= {
+            'Authorization': 'Bearer ' + userEventsApiKey,
+            'Content-Type': 'application/json'
+            },
             proxy=proxy)
 
         if command == 'test-module':
@@ -158,7 +207,9 @@ def main() -> None:
         elif command == 'get-account-info':
             return_results(get_account_info(client, **args))
         elif command == 'get-account-risk-score-history':
-            return_results(get_account_risk_score_history(client, **args))            
+            return_results(get_account_risk_score_history(client, **args))
+        elif command == 'get-user-events':
+            return_results(get_user_events(userEventClient, **args))            
         else:
             raise NotImplementedError(f"command {command} is not implemented.")
 
