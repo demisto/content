ccb970ab8fa9a718fe8b0599552c1c3e8cb82566
Yehuda <yrosenberg@paloaltonetworks.com>
2023-01-12 11:16:34 +0200

    cr

--- Packs/CommonScripts/Scripts/ArrayToCSV/ArrayToCSV_test.py
+++ Packs/CommonScripts/Scripts/ArrayToCSV/ArrayToCSV_test.py
@@ -3,12 +3,12 @@ from ArrayToCSV import arr_to_csv_command
 
 def test_arr_to_csv_command__array_of_two_values():
     """
-        Given -
-           an array
-        When -
-            array contains a list of two str objects
-        Then -
-            the function will return the strings comma seperated
+        Given:
+           A list with 2 values
+        When:
+           converting list to a string representing a csv
+        Then:
+            the function will return the string as is (which is a valid csv format)
     """
     arr = ["mock", "mocker"]
     command_result = arr_to_csv_command(arr)
@@ -17,12 +17,12 @@ def test_arr_to_csv_command__array_of_two_values():
 
 def test_arr_to_csv_command__array_of_one_value():
     """
-        Given -
-           an array
-        When -
-            array contains a list of one str objects
-        Then -
-            the function will return the string as is.
+        Given:
+            A list with one value
+        When:
+            converting list to a string representing a csv
+        Then:
+            validate the result is valid csv
     """
     arr = ["mock"]
     command_result = arr_to_csv_command(arr)
@@ -31,12 +31,12 @@ def test_arr_to_csv_command__array_of_one_value():
 
 def test_arr_to_csv_command__empty_array():
     """
-        Given -
-           an array
-        When -
-            array contains nothing
-        Then -
-            the function will return ''
+        Given:
+           An empty list
+        When:
+           converting list to a string representing a csv
+        Then:
+            The result is an empty string (which is a valid csv format)
     """
     arr = ""
     command_result = arr_to_csv_command(arr)
--- Packs/CommonScripts/Scripts/ArrayToCSV/README.md
+++ Packs/CommonScripts/Scripts/ArrayToCSV/README.md
@@ -7,14 +7,39 @@ Converts a simple Array into a textual comma separated string
 | --- | --- |
 | Script Type | python3 |
 | Tags | transformer, general |
+| Cortex XSOAR Version | 6.0.0 |
 
 ## Inputs
 ---
 
 | **Argument Name** | **Description** |
 | --- | --- |
-| value |  An array of stringsfor example: `["abc","def"]` |
+| value | An array of strings input |
 
 ## Outputs
 ---
 There are no outputs for this script.
+
+
+## Script Examples
+### Example command
+```!ArrayToCSV value=`["example","example"]````
+### Context Example
+```json
+{}
+```
+
+### Human Readable Output
+
+>example,example
+
+### Example command
+```!ArrayToCSV value="example,example,example"```
+### Context Example
+```json
+{}
+```
+
+### Human Readable Output
+
+>example,example,example
