f0df03155a084784a64be828c126b8deb7aa52df
beauchompers <beauchompers@gmail.com>
2023-01-13 09:40:07 -0700

    updating integration to support baseclient

--- Packs/MicrosoftTeams/Integrations/MicrosoftTeamsWebhook/MicrosoftTeamsWebhook_test.py
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeamsWebhook/MicrosoftTeamsWebhook_test.py
@@ -1,4 +1,4 @@
-from MicrosoftTeamsWebhook import create_teams_message, send_teams_message_command
+from MicrosoftTeamsWebhook import (Client, send_teams_message_command)
 
 WEBHOOK = "https://readywebookone"
 MESSAGE = "Hello from XSOAR"
@@ -8,24 +8,24 @@ SERVERURLS = {
 }
 
 
-def test_create_teams_message():
-    message = create_teams_message(MESSAGE, TITLE, SERVERURLS["investigation"])
+def test_create_teams_message(Client):
+    message = Client.create_teams_message(MESSAGE, TITLE, SERVERURLS["investigation"])
     assert message
     assert message["sections"][0]["activitySubtitle"] == MESSAGE
     assert message["potentialAction"][0]["name"] == TITLE
     assert message["potentialAction"][0]["targets"][0]["uri"] == SERVERURLS["investigation"]
 
 
-def test_test_module(requests_mock):
+def test_test_module(requests_mock, Client):
     from MicrosoftTeamsWebhook import test_module
     requests_mock.post(WEBHOOK, status_code=200)
 
-    res = test_module(WEBHOOK, SERVERURLS["investigation"])
+    res = test_module(Client, SERVERURLS["investigation"])
     assert res == 'ok'
 
 
-def test_send_teams_message_command(requests_mock):
+def test_send_teams_message_command(requests_mock, Client):
     requests_mock.post(WEBHOOK, status_code=200)
 
-    res = send_teams_message_command(WEBHOOK, MESSAGE, TITLE, SERVERURLS["investigation"])
+    res = send_teams_message_command(Client, MESSAGE, TITLE, SERVERURLS["investigation"])
     assert res == 'message sent successfully'
--- Packs/MicrosoftTeams/Integrations/MicrosoftTeamsWebhook/README.md
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeamsWebhook/README.md
@@ -1,5 +1,5 @@
 Integration for sending notifications to a Microsoft Teams channel via Incoming Webhook.
-This integration was integrated and tested with version 6.8+ of Microsoft Teams via Webhook
+This integration was integrated and tested with version xx of Microsoft Teams via Webhook
 
 ## Configure Microsoft Teams via Webhook on Cortex XSOAR
 
@@ -10,6 +10,7 @@ This integration was integrated and tested with version 6.8+ of Microsoft Teams 
     | **Parameter** | **Description** | **Required** |
     | --- | --- | --- |
     | Microsoft Webhook URL | The webhook URL in the Teams channel. | True |
+    | Use system proxy settings |  | False |
 
 4. Click **Test** to validate the URLs, token, and connection.
 ## Commands
