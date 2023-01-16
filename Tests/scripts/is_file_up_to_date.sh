77734616105e92bf896a9569ae4c600ffb96117b
ilan <ierukhimovic@paloaltonetworks.com>
2023-01-16 15:53:29 +0200

    fix

--- Tests/scripts/is_file_up_to_date.sh
+++ Tests/scripts/is_file_up_to_date.sh
@@ -3,6 +3,9 @@ FILE_TO_CHECK=$1
 BRANCH=$2
 SHOULD_CHECKOUT=$3
 
+if [[ -n $BRANCH ]]; then
+    BRANCH=$(git branch --show-current)
+fi
 # Checks if there's any diff from master
 if [[ $(git diff origin/master -- ${FILE_TO_CHECK}) ]]; then
     # Checks if part of the branch's changes
@@ -13,7 +16,7 @@ if [[ $(git diff origin/master -- ${FILE_TO_CHECK}) ]]; then
             git checkout origin/master -- ${FILE_TO_CHECK}
             exit 0
         fi
-        if [[ -z "${CIRCLECI}" ]]; then
+        if [[ -z "${CIRCLECI}" && -f /usr/games/cowsay ]]; then
             # using printf & STDIN instead of command argument to support new lines in the message.
             # pick a ranadom cow-file
             printf "ERROR: %s has been changed.\nMerge from master" "${FILE_TO_CHECK}" | /usr/games/cowsay -n -f "$(ls /usr/share/cowsay/cows | shuf | head -1)"
