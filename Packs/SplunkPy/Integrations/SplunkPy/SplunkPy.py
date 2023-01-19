23e3a7fe7a51ff47814b8581b9ddb4f1b183267f
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2023-01-19 14:28:28 +0200

    Remove redundant line breaks

--- Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
+++ Packs/SplunkPy/Integrations/SplunkPy/SplunkPy.py
@@ -256,7 +256,6 @@ def enforce_look_behind_time(last_run_time, now, look_behind_time):
 
     Returns:
         last_run (str): The new start time for the fetch.
-
     """
     last_run_datetime = datetime.strptime(last_run_time, SPLUNK_TIME_FORMAT)
     now_datetime = datetime.strptime(now, SPLUNK_TIME_FORMAT)
@@ -443,7 +442,6 @@ class Enrichment:
         data (list): The enrichment's data list (events retrieved from the job's search).
         creation_time (str): The enrichment's creation time in ISO format.
         status (str): The enrichment's status.
-
     """
     FAILED = 'Enrichment failed'
     EXCEEDED_TIMEOUT = 'Enrichment exceed the given timeout'
@@ -468,7 +466,6 @@ class Enrichment:
 
         Returns:
             The created enrichment (Enrichment)
-
         """
         if job:
             return cls(enrichment_type=enrichment_type, enrichment_id=job["sid"])
@@ -484,7 +481,6 @@ class Enrichment:
 
         Returns:
             An instance of the Enrichment class constructed from JSON representation.
-
         """
         return cls(
             enrichment_type=enrichment_dict.get(TYPE),
@@ -507,7 +503,6 @@ class Notable:
         custom_id (str): The custom ID of the notable (used in the fetch function).
         time_is_missing (bool): Whether the `_time` field has an empty value or not.
         index_time (str): The time the notable have been indexed.
-
     """
 
     def __init__(self, data, enrichments=None, notable_id=None, occurred=None, custom_id=None, index_time=None,
@@ -650,7 +645,6 @@ class Notable:
             enrichment_timeout (int): The timeout for the enrichment process
 
         Returns (bool): True if the enrichment process exceeded the given timeout, False otherwise
-
         """
         now = datetime.utcnow()
         exceeding_timeout = False
@@ -673,7 +667,6 @@ class Notable:
 
         Returns:
             An instance of the Enrichment class constructed from JSON representation.
-
         """
         return cls(
             data=notable_dict.get(DATA),
@@ -693,9 +686,7 @@ class Cache:
     Attributes:
         not_yet_submitted_notables (list): The list of all notables that were fetched but not yet submitted.
         submitted_notables (list): The list of all submitted notables that needs to be handled.
-
     """
-
     def __init__(self, not_yet_submitted_notables=None, submitted_notables=None):
         self.not_yet_submitted_notables = not_yet_submitted_notables if not_yet_submitted_notables else []
         self.submitted_notables = submitted_notables if submitted_notables else []
@@ -716,7 +707,6 @@ class Cache:
         Returns:
             handled_not_created_incident (list): The list of all notables that have been handled but not created an
              incident.
-
         """
         not_yet_submitted, submitted, handled_not_created_incident = [], [], []
 
@@ -751,7 +741,6 @@ class Cache:
 
         Returns:
             An instance of the Cache class constructed from JSON representation.
-
         """
         return cls(
             not_yet_submitted_notables=list(map(Notable.from_json, cache_dict.get(NOT_YET_SUBMITTED_NOTABLES, []))),
@@ -780,7 +769,6 @@ def get_fields_query_part(notable_data, prefix, fields, raw_dict=None, add_backs
         add_backslash (bool): For users that contains single backslash, we add one more
 
     Returns: The query part
-
     """
     if not raw_dict:
         raw_dict = rawToDict(notable_data.get('_raw', ''))
@@ -832,7 +820,6 @@ def build_drilldown_search(notable_data, search, raw_dict):
         raw_dict (dict): The raw dict
 
     Returns (str): A searchable drilldown search query
-
     """
     searchable_search = []
     start = 0
@@ -866,7 +853,6 @@ def get_drilldown_timeframe(notable_data, raw):
         task_status: True if the timeframe was retrieved successfully, False otherwise.
         earliest_offset: The earliest time to query from.
         latest_offset: The latest time to query to.
-
     """
     task_status = True
     earliest_offset = notable_data.get("drilldown_earliest", "")
@@ -899,7 +885,6 @@ def drilldown_enrichment(service: client.Service, notable_data, num_enrichment_e
         num_enrichment_events (int): The maximal number of events to return per enrichment type.
 
     Returns: The Splunk Job
-
     """
     job = None
     search = notable_data.get("drilldown_search", "")
@@ -941,7 +926,6 @@ def identity_enrichment(service: client.Service, notable_data, num_enrichment_ev
         num_enrichment_events (int): The maximal number of events to return per enrichment type.
 
     Returns: The Splunk Job
-
     """
     job = None
     error_msg = "Failed submitting identity enrichment request to Splunk for notable {}".format(notable_data[EVENT_ID])
@@ -972,7 +956,6 @@ def asset_enrichment(service: client.Service, notable_data, num_enrichment_event
         num_enrichment_events (int): The maximal number of events to return per enrichment type.
 
     Returns: The Splunk Job
-
     """
     job = None
     error_msg = "Failed submitting asset enrichment request to Splunk for notable {}".format(notable_data[EVENT_ID])
@@ -1003,7 +986,6 @@ def handle_submitted_notables(service: client.Service, incidents, cache_object: 
         service (splunklib.client.Service): Splunk service object.
         incidents (list): The incident to be submitted at the end of the run.
         cache_object (Cache): The enrichment mechanism cache object
-
     """
     handled_notables = []
     enrichment_timeout = arg_to_number(str(demisto.params().get('enrichment_timeout', '5')))
@@ -1035,7 +1017,6 @@ def handle_submitted_notable(service: client.Service, notable: Notable, enrichme
 
     Returns:
         notable_status (str): The status of the notable
-
     """
     task_status = False
 
@@ -1076,7 +1057,6 @@ def submit_notables(service: client.Service, incidents: list, cache_object: Cach
         service (splunklib.client.Service): Splunk service object
         incidents (list): The incident to be submitted at the end of the run.
         cache_object (Cache): The enrichment mechanism cache object
-
     """
     failed_notables, submitted_notables = [], []
     num_enrichment_events = arg_to_number(str(demisto.params().get('num_enrichment_events', '20')))
@@ -1118,7 +1098,6 @@ def submit_notable(service: client.Service, notable: Notable, num_enrichment_eve
 
     Returns:
         task_status (bool): True if any of the enrichment's succeeded to be submitted to Splunk, False otherwise
-
     """
     submitted_drilldown, submitted_asset, submitted_identity = notable.get_submitted_enrichments()
 
@@ -1146,7 +1125,6 @@ def run_enrichment_mechanism(service: client.Service, integration_context, mappe
     Args:
         service (splunklib.client.Service): Splunk service object.
         integration_context (dict): The integration context
-
     """
     incidents = []  # type: list
     cache_object = Cache.load_from_integration_context(integration_context)
@@ -1179,7 +1157,6 @@ def store_incidents_for_mapping(incidents, integration_context):
     Args:
         incidents (list): The incidents
         integration_context (dict): The integration context
-
     """
     if incidents:
         integration_context[INCIDENTS] = incidents[:20]
@@ -1190,7 +1167,6 @@ def fetch_incidents_for_mapping(integration_context):
 
     Args:
         integration_context (dict): The integration context
-
     """
     incidents = integration_context.get(INCIDENTS, [])
     demisto.debug(
@@ -1222,7 +1198,6 @@ def get_last_update_in_splunk_time(last_update):
         last_update (str): The time to be transformed, E.g 2021-02-09T16:41:30.589575+02:00
 
     Returns (int): The corresponding timestamp on the Splunk server
-
     """
     last_update_utc_datetime = dateparser.parse(last_update, settings={'TIMEZONE': 'UTC'})
     if not last_update_utc_datetime:
@@ -1250,7 +1225,6 @@ def get_remote_data_command(service: client.Service, args: dict, close_incident,
 
     Returns:
         GetRemoteDataResponse: The Response containing the update notable to mirror and the entries
-
     """
     entries = []
     updated_notable = {}
@@ -1299,7 +1273,6 @@ def get_modified_remote_data_command(service: client.Service, args):
 
     Returns:
         GetModifiedRemoteDataResponse: The response containing the list of ids of notables changed
-
     """
     modified_notable_ids = []
     remote_args = GetModifiedRemoteDataArgs(args)
@@ -1329,7 +1302,6 @@ def update_remote_system_command(args, params, service: client.Service, auth_tok
 
     Returns:
         notable_id (str): The notable id
-
     """
     parsed_args = UpdateRemoteSystemArgs(args)
     delta = parsed_args.delta
@@ -1391,9 +1363,6 @@ def create_mapping_dict(total_parsed_results, type_field):
     Args:
         total_parsed_results: list. the results from the splunk search query
         type_field: str. the field that represents the type of the event or alert.
-
-    Returns:
-
     """
     types_map = {}
     for result in total_parsed_results:
@@ -1619,8 +1588,7 @@ def get_cim_mapping_field_command():
 # =========== Integration Functions & Classes ===========
 
 class ResponseReaderWrapper(io.RawIOBase):
-    """ This class was supplied as a solution for a bug in Splunk causing the search to run slowly.
-    """
+    """ This class was supplied as a solution for a bug in Splunk causing the search to run slowly."""
 
     def __init__(self, responseReader):
         self.responseReader = responseReader
@@ -1882,7 +1850,6 @@ def parse_notable(notable, to_dict=False):
         to_dict (bool): Whether to cast the notable to dict or not.
 
     Returns (OrderedDict or dict): The parsed notable
-
     """
     notable = replace_keys(notable) if REPLACE_FLAG else notable
     for key, val in list(notable.items()):
