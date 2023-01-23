5b947500010e77c1b28f7b9a192d8ec3d9b35d45
ilappe <ilappe@paloaltonetworks.com>
2023-01-23 13:08:11 +0200

    increase test coverage

--- Packs/MicrosoftGraphApplications/Integrations/MicrosoftGraphApplications/MicrosoftGraphApplications_test.py
+++ Packs/MicrosoftGraphApplications/Integrations/MicrosoftGraphApplications/MicrosoftGraphApplications_test.py
@@ -1,6 +1,7 @@
 from MicrosoftGraphApplications import main, MANAGED_IDENTITIES_TOKEN_URL, Resources
 import MicrosoftGraphApplications
 import demistomock as demisto
+import pytest
 
 
 def test_reset_auth_command(mocker, requests_mock):
@@ -20,7 +21,7 @@ def test_reset_auth_command(mocker, requests_mock):
 
     assert 'Authorization was reset successfully. Run **!msgraph-apps-start**' \
            in MicrosoftGraphApplications.return_results.call_args[0][0].readable_output
-    
+
 
 def test_auth_complete_with_managed_identities(mocker, requests_mock):
     """
@@ -49,7 +50,7 @@ def test_auth_complete_with_managed_identities(mocker, requests_mock):
     main()
 
     assert 'Authorization completed successfully' in MicrosoftGraphApplications.return_results.call_args[0][0]
-    
+
 
 def test_auth_test_with_managed_identities(mocker, requests_mock):
     """
@@ -109,3 +110,34 @@ def test_test_module_command_with_managed_identities(mocker, requests_mock):
     assert 'ok' in MicrosoftGraphApplications.return_results.call_args[0][0]
 
 
+@pytest.mark.parametrize('limit, list_url_count, next_url_count, top_param_in_url',
+                         [
+                             (10, 1, 0, True),
+                             (0, 1, 1, False)
+                         ])
+def test_service_principal_list_command(mocker, requests_mock,
+                                        limit, list_url_count, next_url_count, top_param_in_url):
+    """
+        Given:
+            -
+        When:
+            - principal_list_command.
+        Then:
+            - Ensure the expected requests was made.
+    """
+    import re
+    mock_token = {'access_token': 'test_token', 'expires_in': '86400'}
+    mock_res = {'value': [], '@odata.nextLink': 'https://test_next_link.test'}
+
+    get_mock = requests_mock.get(re.compile(f'^{Resources.graph}'), json=mock_res)
+    next_link_get_mock = requests_mock.get('https://test_next_link.test', json={'value': []})
+    requests_mock.post('https://login.microsoftonline.com/organizations/oauth2/v2.0/token', json=mock_token)
+
+    mocker.patch.object(demisto, 'args', return_value={'limit': limit})
+    mocker.patch.object(demisto, 'command', return_value='msgraph-apps-service-principal-list')
+
+    main()
+
+    assert get_mock.call_count == list_url_count
+    assert next_link_get_mock.call_count == next_url_count
+    assert ('?$top=' in str(get_mock.last_request)) == top_param_in_url
