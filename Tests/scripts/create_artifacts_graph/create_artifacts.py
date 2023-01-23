c7c524e2cccdd8808adcc53887cf4f841e7cfa69
ilan <ierukhimovic@paloaltonetworks.com>
2023-01-23 18:12:16 +0200

    fix

--- Tests/scripts/create_artifacts_graph/create_artifacts.py
+++ Tests/scripts/create_artifacts_graph/create_artifacts.py
@@ -6,25 +6,31 @@ from demisto_sdk.commands.content_graph.objects.repository import ContentDTO
 from Tests.scripts.utils.log_util import install_logging
 import logging as logger
 from demisto_sdk.commands.common.logger import logging_setup
-from demisto_sdk.commands.common.tools import get_content_path
+from demisto_sdk.commands.common.tools import get_content_path, str2bool
 
 import json
 
 logging_setup(3)
 install_logging("create_artifacts.log", logger=logger)
 
+def str2boolean(v: str):
+    if not v:
+        return False
+    return str2bool(v)
 
 def create_zips(content_dto: ContentDTO, output: Path, marketplace: str, zip: bool):
     content_dto.dump(output, marketplace, zip)
 
 
-def create_dependencies(content_dto: ContentDTO, output: Path):
+def create_dependencies(content_dto: ContentDTO, is_bucket_upload: bool, output: Path):
     pack_dependencies = {}
     for pack in content_dto.packs:
         dependencies = pack.depends_on
         first_level_dependencies = {}
         all_level_dependencies = {}
         for dependency in dependencies:
+            if is_bucket_upload and dependency.is_test:
+                continue
             if dependency.mandatorily:
                 all_level_dependencies[dependency.content_item.object_id] = {
                     "display_name": dependency.content_item.name,
@@ -34,6 +40,7 @@ def create_dependencies(content_dto: ContentDTO, output: Path):
                 first_level_dependencies[dependency.content_item.object_id] = {
                     "display_name": dependency.content_item.name,
                     "mandatory": dependency.mandatorily,
+                    "is_test": dependency.is_test,
                 }
         pack_dependencies[pack.object_id] = {
             "path": str(pack.path.relative_to(get_content_path())),
@@ -51,6 +58,7 @@ def main():
     parser.add_argument("-mp", "--marketplace", type=MarketplaceVersions, help="marketplace version", default="xsoar")
     parser.add_argument("-ao", "--artifacts-output", help="Artifacts output directory", required=True)
     parser.add_argument("-do", "--dependencies-output", help="Dependencies output directory", required=True)
+    parser.add_argument("-bu", "--bucket-upload", help="Upload to bucket", type=str2boolean, default=False)
     parser.add_argument("--zip", default=True, action="store_true")
     parser.add_argument("--no-zip", dest="zip", action="store_false")
     args = parser.parse_args()
