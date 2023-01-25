adf4ecc1569459296c5aec8a0d269aa77c44ac0a
tkatzir <tkatzir@paloaltonetworks.com>
2023-01-25 17:01:58 +0200

    Beautify

--- Tests/scripts/collect_tests/utils.py
+++ Tests/scripts/collect_tests/utils.py
@@ -176,8 +176,8 @@ class ContentItem(DictFileBased):
             or self.path.name.endswith('_schema.json') \
             or self.path.name.endswith('testdata.json') \
             or len(file_path_splitted) > 1 \
-                and file_path_splitted[-2] == 'ReleaseNotes' \
-                and self.path.suffix == '.json'
+            and file_path_splitted[-2] == 'ReleaseNotes' \
+            and self.path.suffix == '.json'
 
     @property
     def id_(self) -> Optional[str]:  # Optional as some content items don't have an id
