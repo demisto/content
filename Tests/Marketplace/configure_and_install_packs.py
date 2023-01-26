5ca195e858cd2f8bf31f5d2d19139dd799ccbf5a
Darya Koval <dkoval@paloaltonetworks.com>
2023-01-26 11:19:50 +0200

    installing packs one by one changes

--- Tests/Marketplace/configure_and_install_packs.py
+++ Tests/Marketplace/configure_and_install_packs.py
@@ -48,9 +48,9 @@ def install_packs_from_content_packs_to_install_path(servers, pack_ids, hostname
     """
     for server in servers:
         logging.info(f'Starting to install all content packs in {hostname if hostname else server.internal_ip}')
-        small_batches_install = True if (server.__class__ == XSIAMServer) else False
+        install_packs_one_by_one = True if (server.__class__ == CloudServer) else False
         _, success = search_and_install_packs_and_their_dependencies(pack_ids, server.client, hostname,
-                                                                     small_batches_install)
+                                                                     install_packs_one_by_one)
         if not success:
             raise Exception('Failed to search and install packs and their dependencies.')
 
