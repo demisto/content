df012281044540a809e84c0079d6ee41893e3efd
ido <43602124+idovandijk@users.noreply.github.com>
2023-01-29 19:35:35 +0200

    more linter stuff

--- Packs/Campaign/Scripts/ShowCampaignUniqueRecipients/ShowCampaignUniqueRecipients.py
+++ Packs/Campaign/Scripts/ShowCampaignUniqueRecipients/ShowCampaignUniqueRecipients.py
@@ -3,18 +3,18 @@ from CommonServerPython import *  # noqa: F401
 
 # Variable initialization:
 html = ""
-campaign_incidents = ""
+campaign_incidents: list[dict]
 
 try:
     incident_id = demisto.incidents()[0].get('id', {})
     context = demisto.executeCommand("getContext", {'id': incident_id})
-    campaign_incidents: list[dict] = demisto.get(context[0], "Contents.context.EmailCampaign.incidents")
+    campaign_incidents = demisto.get(context[0], "Contents.context.EmailCampaign.incidents")
     unique_recipients = set(recipient for incident in campaign_incidents for recipient in incident.get(
         "recipients", []) if isinstance(incident.get("recipients"), list))
     html = f"<div style='font-size:17px; text-align:center; padding-top: 20px;'> Unique Recipients " \
            f"<div style='font-size:32px;'> <div> {len(unique_recipients)} </div></div>"
-except Exception as ex:
-    html = f"<div style='text-align:center; padding-top: 20px;'> <div> No recipients </div>"
+except Exception:
+    html = "<div style='text-align:center; padding-top: 20px;'> <div> No recipients </div>"
 
 # Return the data to the layout:
 demisto.results({
