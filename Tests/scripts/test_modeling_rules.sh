67cfcbdb6588957af48554cdc9684612d3bc3a4b
Darya Koval <dkoval@paloaltonetworks.com>
2023-01-30 20:38:28 +0200

    no echo

--- Tests/scripts/test_modeling_rules.sh
+++ Tests/scripts/test_modeling_rules.sh
@@ -14,7 +14,6 @@ CURRENT_DIR=$(pwd)
 MODELING_RULES_ARRAY=($(cat "$ARTIFACTS_FOLDER"/modeling_rules_to_test.txt))
 for modeling_rule in "${MODELING_RULES_ARRAY[@]}"; do
   MODELING_RULE_TEST_FILE_PATTERN="$CURRENT_DIR/Packs/$modeling_rule/*_testdata.json"
-  echo "Pattern: $MODELING_RULE_TEST_FILE_PATTERN"
   # If it is nightly, run `test modeling rules` only on modeling rules that have `_testdata.json` file.
   if [ -z "$NIGHTLY" ] || [ -e $MODELING_RULE_TEST_FILE_PATTERN ]; then
     if [[ -n "$MODELING_RULES_TO_TEST" ]]; then
