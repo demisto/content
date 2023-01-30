9a4171a6963140c9e6d289772962f886839f8da5
ilaner <88267954+ilaner@users.noreply.github.com>
2023-01-30 16:54:30 +0200

    Automatically update devcontainer version and add native image dependencies (#24099)

--- .devcontainer/createCommand.sh
+++ .devcontainer/createCommand.sh
@@ -20,5 +20,5 @@ echo "PYTHONPATH=""$path"":$PYTHONPATH" >> .env
 echo "MYPYPATH=""$path"":$MYPYPATH" >> .env
 
 echo "Setting up content dependencies"
-poetry install
+poetry install --with native
 npm install
