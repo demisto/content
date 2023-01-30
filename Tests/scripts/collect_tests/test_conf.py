45f451e8da0d830f90fa9ba6d3e07cf89f80f7de
GuyAfik <guyafik11@gmail.com>
2023-01-30 19:26:32 +0200

    update TestConf constructor

--- Tests/scripts/collect_tests/test_conf.py
+++ Tests/scripts/collect_tests/test_conf.py
@@ -46,14 +46,11 @@ class TestConf(DictFileBased):
 
         tests_to_integration_set: dict[str, set[str, ...]] = defaultdict(set)
         for test in self.tests:
-            self.tests_to_integration_set[test.playbook_id].update(test.integrations)
-        self.tests_to_integrations: dict[str, tuple[str, ...]] = {test: tuple(sorted(test_integrations)) for test,test_integrations in tests_to_integration_set.items()}
+            tests_to_integration_set[test.playbook_id].update(test.integrations)
+        self.tests_to_integrations: dict[str, tuple[str, ...]] = {
+            test: tuple(sorted(test_integrations)) for test, test_integrations in tests_to_integration_set.items()
+        }
 
-        # self.tests_to_integrations: dict[str, tuple[str, ...]] = {
-        #     test.playbook_id: test.integrations
-        #     for test in self.tests
-        #     if test.integrations
-        # }
         logger.info(f'{self.tests_to_integrations=}')
         self.integrations_to_tests: dict[str, list[str]] = self._calculate_integration_to_tests()
 
