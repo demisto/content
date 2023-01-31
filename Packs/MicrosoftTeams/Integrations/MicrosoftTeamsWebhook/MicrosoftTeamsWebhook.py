1bdebf98a37594d6d8bbd1ec0481402b2e9edd2b
Mike Beauchamp <beauchompers@gmail.com>
2023-01-31 11:51:36 -0700

    Ms teams webhook update (#22759)
    * adding readme for webhook integration
    
    * updated test module
    
    * updated test for webhook integration
    
    * updating integration to support baseclient
    
    * removing MicrosoftTeamsviaWebhook folder
    
    * Updated readme
    
    * updated title arg to pull from args
    
    * Added the RN and updated the docker image
    
    * fixed tests
    
    * fixed some typing issues and removed a redundant try except
    
    * removed the ignore type
    
    ---------
    
    Co-authored-by: okarkkatz <okarkkatz@paloaltonetworks.com>

--- Packs/MicrosoftTeams/Integrations/MicrosoftTeamsWebhook/MicrosoftTeamsWebhook.py
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeamsWebhook/MicrosoftTeamsWebhook.py
@@ -1,85 +1,132 @@
+from typing import Any, Dict
+
 import demistomock as demisto  # noqa: F401
-import requests
+import urllib3
 from CommonServerPython import *  # noqa: F401
 
-
-def create_teams_message(message, serverurls):
-    """
-    Creates the Teams message using the messageCard format, and returns the card
-    """
-    messageCard = {
-        "@type": "MessageCard",
-        "@context": "http://schema.org/extensions",
-        "themeColor": "0076D7",
-        "summary": "Cortex XSOAR Notification",
-        "sections": [{
-            "activityTitle": "Cortex XSOAR Notification",
-            "activitySubtitle": message,
-            "markdown": True
-        }],
-        "potentialAction": [{
-            "@type": "OpenUri",
-            "name": "Cortex XSOAR URL",
-            "targets": [{"os": "default", "uri": serverurls['investigation']}]
-        }]
-    }
-
-    return messageCard
+# Disable insecure warnings
+urllib3.disable_warnings()
 
 
-def test_module(webhook):
-    """
-    Test command, will send a notification with a static message, and check we got a 200 OK back
-    """
-    try:
-        message = {
+class Client(BaseClient):
+
+    def create_teams_message(self, message: str, title: str, serverurls: str) -> dict:
+        """
+        Creates the Teams message using the messageCard format, and returns the card
+
+        Args:
+            message (str): The message to send in the message card to Teams.
+            title (str): The title of the message card.
+            serverurls (str): The URL to send in the message card.
+
+         Returns:
+            messagecard (dict): dict the adaptive card to send to Teams.
+        """
+        messagecard = {
             "@type": "MessageCard",
             "@context": "http://schema.org/extensions",
             "themeColor": "0076D7",
             "summary": "Cortex XSOAR Notification",
             "sections": [{
                 "activityTitle": "Cortex XSOAR Notification",
-                "activitySubtitle": "Successful test message from Cortex XSOAR",
+                "activitySubtitle": message,
                 "markdown": True
+            }],
+            "potentialAction": [{
+                "@type": "OpenUri",
+                "name": title,
+                "targets": [{"os": "default", "uri": serverurls}]
             }]
         }
-        res = requests.post(webhook, json=message)
-        res.raise_for_status()
-        return 'ok'
-    except Exception as e:
-        return_error(e)
+
+        return messagecard
+
+    def send_teams_message(self, messagecard: dict) -> Dict[str, Any]:
+        """
+        Sends the Teams Message to the provided webhook.
+
+        Args:
+            messagecard (dict): dict the adaptive card to send to Teams.
+        """
+
+        return self._http_request(
+            method='POST',
+            json_data=messagecard,
+            raise_on_status=True
+        )
 
 
-def send_teams_message_command(webhook, message, serverurls):
+def test_module(client: Client, serverurls: str) -> str:
     """
-    Sends the Teams Message to the provided webhook.
+    Test command, will send a notification with a static message.
+
+    Args:
+        client (Client): HelloWorld client to use.
+        serverurls (str): The URL to send in the message card.
+
+    Returns:
+        str: 'ok' if test passed, anything else will raise an exception and will fail the test.
     """
     try:
-        message = create_teams_message(message, serverurls)
-        res = requests.post(webhook, json=message)
-        res.raise_for_status()
-        return 'message sent successfully'
-    except Exception as e:
-        return_error(e)
+        message = "Successful test message from Cortex XSOAR"
+        title = "Cortex XSOAR Notification"
+        test_message = client.create_teams_message(message, title, serverurls)
+        client.send_teams_message(test_message)
+    except DemistoException as e:
+        return f'Error: {e}'
+    return 'ok'
 
 
-def main():  # pragma: no cover
+def send_teams_message_command(client: Client, message: str, title: str, serverurls: str) -> CommandResults:
     """
-    Grab the params and the server urls, and send the message, or test message.
+    send_teams_message command: Sends the Teams Message to the provided webhook.
+
+    Args:
+        client (Client): Teams client to use.
+        message (str): The message to send in the message card to Teams.
+        title (str): The title of the message card.
+        serverurls (str): The URL to send in the message card.
+
+    Returns:
+        CommandResults/dict: A ``CommandResults`` compatible to return ``return_results()``,
+        which contains the readable_output indicating the message was sent.
     """
-    webhook = demisto.params().get('webhookurl')
-    serverurls = demisto.demistoUrls()
+
+    messagecard = client.create_teams_message(message, title, serverurls)
+    client.send_teams_message(messagecard)
+    return CommandResults(readable_output='message sent successfully')
+
+
+def main() -> None:
+    """
+    main function, parses params and runs command functions
+    grab the params and the server urls, and send the message, or test message.
+    """
+    params = demisto.params()
     args = demisto.args()
+
+    title = args.get('url_title', 'Cortex XSOAR URL')
+    webhook = args.get('team_webhook', params.get('webhookurl'))
+    serverurls = demisto.demistoUrls()
+
+    if args.get('alternative_url'):
+        serverurls = args.get('alternative_url')
+    else:
+        serverurls = serverurls.get("investigation", serverurls["server"])
+
     command = demisto.command()
+
     try:
+        client = Client(
+            base_url=webhook,
+            verify=True,
+            proxy=params.get('proxy', False))
+
         if command == 'test-module':
-            return_results(test_module(webhook))
+            return_results(test_module(client, serverurls))
         elif command == 'ms-teams-message':
             message = args.get("message", "")
-            if args.get('team_webhook', False):
-                return_results(send_teams_message_command(args.get('team_webhook'), message, serverurls))
-            else:
-                return_results(send_teams_message_command(webhook, message, serverurls))
+            return_results(send_teams_message_command(client, message, title, serverurls))
         else:
             raise NotImplementedError(f"command {command} is not implemented.")
 
