3f984da9b1a6ce2607d67d2abd2ea677ab2a1095
MLainer1 <mlainer@paloaltonetworks.com>
2023-02-02 17:03:04 +0200

    make validate with -a

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -6,9 +6,9 @@ echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DE
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
     python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
-    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
 elif [[ $CI_COMMIT_BRANCH = demisto/python3 ]]; then
-    python3 -m demisto_sdk validate -g --post-commit --no-conf-json --allow-skipped --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -a --post-commit --no-conf-json --allow-skipped --graph --skip-pack-dependencies
 else
-    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
 fi
