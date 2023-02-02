5170210953f5c4371b2288d501086b362734d7eb
MLainer1 <mlainer@paloaltonetworks.com>
2023-02-02 15:58:40 +0200

    test - validate with graph

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -4,11 +4,11 @@ set -e
 echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_README_VALIDATION"
 
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
-    python3 -m demisto_sdk validate -a --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json"
+    python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
-    python3 -m demisto_sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/id_set.json"
+    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
 elif [[ $CI_COMMIT_BRANCH = demisto/python3 ]]; then
-    python3 -m demisto_sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json" --no-conf-json --allow-skipped
+    python3 -m demisto_sdk validate -g --post-commit --no-conf-json --allow-skipped --graph --skip-pack-dependencies
 else
-    python3 -m demisto_sdk validate -g --post-commit --id-set --id-set-path "$ARTIFACTS_FOLDER/unified_id_set.json"
+    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
 fi
