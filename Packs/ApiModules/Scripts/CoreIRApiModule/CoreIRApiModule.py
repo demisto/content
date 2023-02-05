6fb94bf0cd6404a43f2ac26b815b51f9ca588e7f
Binat Ziser <89336697+bziser@users.noreply.github.com>
2023-02-05 11:54:21 +0200

    Update Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
    Co-authored-by: ShirleyDenkberg <62508050+ShirleyDenkberg@users.noreply.github.com>

--- Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
+++ Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
@@ -1316,7 +1316,7 @@ def run_polling_command(client: CoreClient,
     results_function: the pythonic result function which we want to poll on.
     polling_field: the field that is retrieved from the results_function's response and indicates the polling status.
     polling_value: list of values of the polling_field we want to check. The list can contain values to stop or continue polling on, not both.
-    stop_polling: True - polling_value is stopping the polling, False - polling_value not stopping the polling
+    stop_polling: True - polling_value stops the polling. False - polling_value does not stop the polling.
 
     Return:
     command_results(CommandResults)
