a2d39eabb0894238911e136a39bb89dc3b3d538f
bziser <bziser@paloaltonetworks.com>
2023-02-05 13:15:25 +0200

    flake8

--- Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
+++ Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
@@ -1312,10 +1312,12 @@ def run_polling_command(client: CoreClient,
     args: args
     cmd: the scheduled command's name (as appears in the yml file) to run in the following polling.
     command_function: the pythonic function that executes the command.
-    command_decision_field: the field that is retrieved from the command_function's response that indicates the command_function status.
+    command_decision_field: the field that is retrieved from the command_function's response that indicates
+    the command_function status.
     results_function: the pythonic result function which we want to poll on.
     polling_field: the field that is retrieved from the results_function's response and indicates the polling status.
-    polling_value: list of values of the polling_field we want to check. The list can contain values to stop or continue polling on, not both.
+    polling_value: list of values of the polling_field we want to check. The list can contain values to stop or
+    continue polling on, not both.
     stop_polling: True - polling_value stops the polling. False - polling_value does not stop the polling.
 
     Return:
