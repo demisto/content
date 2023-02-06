2ca3a036d3130e63cab83d6e814ea59888073d95
rshunim <102469772+rshunim@users.noreply.github.com>
2023-02-06 14:50:46 +0200

    Microsoft graph mail single user fetch bug (#24353)
    * fix the decoding bug
    
    * RN
    
    * RN
    
    * docker image
    
    * Update Packs/MicrosoftGraphListener/Integrations/MicrosoftGraphListener/MicrosoftGraphListener.py
    
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>
    
    * function name was changed
    
    * RN was updated
    
    * unit test was added
    
    * Update Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphListener/MicrosoftGraphListener.py
    
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>
    
    * static method of the client was moved out
    
    * fix import
    
    * flake8 errors
    
    ---------
    
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>

--- Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphListener/MicrosoftGraphListener_test.py
+++ Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphListener/MicrosoftGraphListener_test.py
@@ -1,7 +1,7 @@
 import pytest
 import demistomock as demisto
 import json
-from MicrosoftGraphListener import MsGraphClient
+from MicrosoftGraphListener import MsGraphClient, is_only_ascii
 from MicrosoftGraphListener import add_second_to_str_date
 import requests_mock
 from unittest.mock import mock_open
@@ -1002,3 +1002,37 @@ def test_special_chars_in_attachment_name(mocker):
     res = client._get_email_attachments('message_id')
 
     assert res[0].get('name') == attachment_file_name
+
+
+@pytest.mark.parametrize('attachment_file_name', ['1.png', 'file_example_JPG_100kB.jpg', 'sdsdagdsga.png'])
+def test_regular_chars_in_attachment_name(mocker, attachment_file_name):
+    """
+    Given: A attachment file name containing Latin alphabet + some other characters but not from some other alphabet.
+    When: Running the `_get_email_attachments` function.
+    Then: Ensure the file name remains the same (without decoding).
+    """
+    client = oproxy_client()
+    mocker.patch.object(client.ms_client, 'http_request', return_value={'value': [{
+        '@odata.type': '#microsoft.graph.fileAttachment',
+        'name': attachment_file_name,
+        'contentBytes': 'contentBytes'}]})
+    mocker.patch.object(demisto, 'uniqueFile')
+    mocker.patch("builtins.open", mock_open())
+
+    res = client._get_email_attachments('message_id')
+
+    assert res[0].get('name') == attachment_file_name
+
+
+@pytest.mark.parametrize('str_to_check, expected_result', [('slabiky, ale liší se podle významu', False),
+                                                           ('English', True), ('ގެ ފުރަތަމަ ދެ އަކުރު ކަ', False),
+                                                           ('how about this one : 通 asfަ', False),
+                                                           ('?fd4))45s&', True)])
+def test_is_only_ascii(str_to_check, expected_result):
+    """
+    Given: A string which contains Latin alphabet + some other characters or some other alphabet.
+    When: Running the `is_only_ascii` function.
+    Then: Ensure the function works and returns true for English strings and false for everything else.
+    """
+    result = is_only_ascii(str_to_check)
+    assert expected_result == result
