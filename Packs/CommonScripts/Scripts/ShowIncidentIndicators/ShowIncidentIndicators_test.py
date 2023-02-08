e3d45521ef09e78925b237d85bbfeb946e37918d
ido <43602124+idovandijk@users.noreply.github.com>
2023-02-08 16:54:45 +0200

    Changed to use pytest

--- Packs/CommonScripts/Scripts/ShowIncidentIndicators/ShowIncidentIndicators_test.py
+++ Packs/CommonScripts/Scripts/ShowIncidentIndicators/ShowIncidentIndicators_test.py
@@ -1,31 +1,28 @@
-import unittest
+import pytest
 from ShowIncidentIndicators import group_by_type, get_indicators_from_incident
 
 
-class TestGroupByTypeAndGetIndicatorsFromIncident(unittest.TestCase):
-    def test_group_by_type(self):
-        indicators = [{"indicator_type": "IP", "value": "1.1.1.1"},
-                      {"indicator_type": "IP", "value": "2.2.2.2"},
-                      {"indicator_type": "Domain", "value": "test.com"}]
-        expected = ["--- IP ---", "1.1.1.1", "2.2.2.2", "", "--- Domain ---", "test.com", ""]
-        result = group_by_type(indicators)
-        self.assertEqual(result, expected)
+def test_group_by_type():
+    indicators = [{"indicator_type": "IP", "value": "1.1.1.1"},
+                  {"indicator_type": "IP", "value": "2.2.2.2"},
+                  {"indicator_type": "Domain", "value": "test.com"}]
+    expected = ["--- IP ---", "1.1.1.1", "2.2.2.2", "", "--- Domain ---", "test.com", ""]
+    result = group_by_type(indicators)
+    assert result == expected
 
-    def test_get_indicators_from_incident(self):
-        def mock_execute_command(command, args):
-            return [{"indicator_type": "IP", "value": "1.1.1.1"},
-                      {"indicator_type": "IP", "value": "2.2.2.2"},
-                      {"indicator_type": "Domain", "value": "test.com"}]
 
-        def mock_incident():
-            return {"id": 123}
+def test_get_indicators_from_incident():
+    def mock_execute_command(command, args):
+        return [{"indicator_type": "IP", "value": "1.1.1.1"},
+                  {"indicator_type": "IP", "value": "2.2.2.2"},
+                  {"indicator_type": "Domain", "value": "test.com"}]
 
-        demisto.execute_command = mock_execute_command
-        demisto.incident = mock_incident
+    def mock_incident():
+        return {"id": 123}
 
-        expected = {"hidden": False, "options": ["--- IP ---", "1.1.1.1", "2.2.2.2", "", "--- Domain ---", "test.com", ""]}
-        result = get_indicators_from_incident()
-        self.assertEqual(result, expected)
+    demisto.execute_command = mock_execute_command
+    demisto.incident = mock_incident
 
-if __name__ == '__main__':
-    unittest.main()
+    expected = {"hidden": False, "options": ["--- IP ---", "1.1.1.1", "2.2.2.2", "", "--- Domain ---", "test.com", ""]}
+    result = get_indicators_from_incident()
+    assert result == expected
