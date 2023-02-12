213db7a5ad1e5f9a46c50ea3651647f49f4d1add
okarkkatz <okarkkatz@paloaltonetworks.com>
2023-02-12 17:26:54 +0200

    updated docker image

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -2679,7 +2679,7 @@ class Pack(object):
         self._all_levels_dependencies = all_levels_dependencies
         self._displayed_images_dependent_on_packs = displayed_images_dependent_on_packs
 
-    def prepare_for_index_upload(self):
+    def prepare_for_index_upload(self, index_folder_path, diff_files_list, override_all_packs):
         """ Removes and leaves only necessary files in pack folder.
 
         Returns:
@@ -2688,6 +2688,9 @@ class Pack(object):
         """
         task_status = False
         files_to_leave = [Pack.METADATA, Pack.CHANGELOG_JSON, Pack.README]
+
+        detect_changes = False if override_all_packs else os.path.exists(os.path.join(index_folder_path, self.name,
+                                                                                      Pack.METADATA)) or self.hidden
 
         try:
             for file_or_folder in os.listdir(self._pack_path):
