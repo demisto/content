feee22fad6fe4d8955c154f3940f8fbb2985966f
yaakovpraisler <ypreisler@paloaltonetworks.com>
2023-02-13 12:56:18 +0200

    debugging

--- Utils/should_trigger_test_upload.sh
+++ Utils/should_trigger_test_upload.sh
@@ -5,13 +5,20 @@ IGNORED_FILES=(
     "Tests/conf.json"
     "Tests/known_words.txt"
 )
-
+echo "before for loop"
+echo "${DIFF_FILES_LIST[*]}"
+echo
 for i in "${IGNORED_FILES[@]}"; do
+    echo $i
+    echo
     DIFF_FILES_LIST=${DIFF_FILES_LIST[*]/$i} 
 done
-
+echo "after for loop"
+echo "${DIFF_FILES_LIST[*]}"
+echo
 SHOULD_SKIP_TEST=$(echo "${DIFF_FILES_LIST[*]}" | grep "Tests/\|Utils/")
-
+echo "SHOULD_SKIP_TEST=$SHOULD_SKIP_TEST"
+echo
 if $SHOULD_SKIP_TEST ; then
     echo "No upload-flow related files were modified, skipping upload test"
     exit 0
