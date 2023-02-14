e8cb4f6ba5f5608a3943cf1146dbc395660d5d0b
Yaakov Praisler <59408745+yaakovpraisler@users.noreply.github.com>
2023-02-14 18:43:42 +0200

    Fixed should_trigger_test_upload script (#24609)
    * fixed should_trigger_test_upload
    
    * another fix

--- Utils/should_trigger_test_upload.sh
+++ Utils/should_trigger_test_upload.sh
@@ -4,17 +4,12 @@ DIFF_FILES_LIST=$(git diff origin/master...$CI_COMMIT_BRANCH --name-only)
 IGNORED_FILES=(
     "Tests/conf.json"
     "Tests/known_words.txt"
+    "Utils/should_trigger_test_upload.sh"
 )
 
 for i in "${IGNORED_FILES[@]}"; do
     DIFF_FILES_LIST=${DIFF_FILES_LIST[*]/$i} 
 done
 
-SHOULD_SKIP_TEST=$(echo "${DIFF_FILES_LIST[*]}" | grep "Tests/\|Utils/")
-
-if [ -z "$SHOULD_SKIP_TEST" ] ; then
-    echo "No upload-flow related files were modified, skipping upload test"
-    exit 0
-fi
-
-echo "Found modified files that should be tested in upload-flow"
+echo "${DIFF_FILES_LIST[*]}" | grep "Tests/\|Utils/"
+exit 0
