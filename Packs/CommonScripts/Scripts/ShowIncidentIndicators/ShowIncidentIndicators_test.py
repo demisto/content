aed3078134e8f1833eaeffa2b37633e0db72467b
Ido van Dijk <43602124+idovandijk@users.noreply.github.com>
2023-02-16 17:46:07 +0200

    unit test fix
    Co-authored-by: Israel Lappe <79846863+ilappe@users.noreply.github.com>

--- Packs/CommonScripts/Scripts/ShowIncidentIndicators/ShowIncidentIndicators_test.py
+++ Packs/CommonScripts/Scripts/ShowIncidentIndicators/ShowIncidentIndicators_test.py
@@ -12,10 +12,11 @@ def test_group_by_type():
 
 
 def test_get_indicators_from_incident(mocker):
+    import ShowIncidentIndicators
     execute_command_output = [{"indicator_type": "IP", "value": "1.1.1.1"},
                               {"indicator_type": "IP", "value": "2.2.2.2"},
                               {"indicator_type": "Domain", "value": "test.com"}]
-    mocker.patch.object(demisto, 'executeCommand', return_value=execute_command_output)
+    mocker.patch.object(ShowIncidentIndicators, 'execute_commands', return_value=execute_command_output)
     mocker.patch.object(demisto, 'incidents', return_value=[{"id": 123}])
 
     expected = {"hidden": False, "options": ["--- IP ---", "1.1.1.1", "2.2.2.2", "", "--- Domain ---", "test.com", ""]}
