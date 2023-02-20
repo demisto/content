8fb3f9f6de9b7f32105dda2d1375868a6517b8f5
Dan Tavori <dtavori@paloaltonetworks.com>
2023-02-20 13:41:56 +0200

    testdan

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_README_VALIDATION"
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
-    python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies -sv GR,PA124
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
-    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies -sv GR,PA124
 elif [[ $CI_COMMIT_BRANCH = demisto/python3 ]]; then
-    python3 -m demisto_sdk validate -g --post-commit --no-conf-json --allow-skipped --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -g --post-commit --no-conf-json --allow-skipped --graph --skip-pack-dependencies -sv GR,PA124
 else
-    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
+    python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies -sv GR,PA124
 fi
