538ef7e806b9b63923d4264e1a38b00b8c5a2884
YuvHayun <yhayun@paloaltonetworks.com>
2023-02-26 16:27:43 +0200

    fixes

--- Tests/tests/configure_and_test_integration_instances_test.py
+++ Tests/tests/configure_and_test_integration_instances_test.py
@@ -204,10 +204,10 @@ def test_get_packs_with_higher_min_version(mocker):
     Then:
         - Assert the returned packs are with higher min version than the server version.
     """
-    import Tests.private_build.upload_packs_private as  upload_packs_private
+    import Tests.private_build.upload_packs_private as upload_packs_private
     import Tests.configure_and_test_integration_instances as configure_and_test_integration_instances
 
-    mocker.patch(upload_packs_private, "extract_packs_artifacts", mock_open(read_data='{"serverMinVersion": "6.6.0"}'))
+    mocker.patch.object(upload_packs_private, "extract_packs_artifacts", mock_open(read_data='{"serverMinVersion": "6.6.0"}'))
     mocker.patch.object(configure_and_test_integration_instances, "get_json_file",
                         mock_open(read_data='{"serverMinVersion": "6.6.0"}'))
 
