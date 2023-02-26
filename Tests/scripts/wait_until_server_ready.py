1f88f64ab8e709815646aa798f5d3b75ddc54ef1
YuvHayun <yhayun@paloaltonetworks.com>
2023-02-26 10:55:06 +0200

    testing

--- Tests/scripts/wait_until_server_ready.py
+++ Tests/scripts/wait_until_server_ready.py
@@ -45,7 +45,8 @@ def download_cloud_init_logs_from_server(ip: str) -> None:
         check_output(f'scp {SSH_USER}@{ip}:{cloud_init_log_path} '
                      f'{ARTIFACTS_FOLDER}/{ip}-cloud_init.log'.split())
     except Exception as e:
-        logging.exception(f'Could not download cloud-init file from server {ip}:\nerr:{e.stderr}\nstdout:{e.output}\nreturncode:{e.returncode}\ncmd:{e.cmd}.')
+        logging.exception(f'Could not download cloud-init file from server {ip}:\n{e.stderr=}\n{e.output=}\n'
+                          f'{e.returncode=}\n{e.cmd=}.')
 
 
 def docker_login(ip: str) -> None:
@@ -63,7 +64,8 @@ def docker_login(ip: str) -> None:
             f'login --username {docker_username} --password-stdin'.split(),
             input=docker_password.encode())
     except Exception as e:
-        logging.exception(f'Could not login to {container_engine_type} on server {ip}:\nerr:{e.stderr}\nstdout:{e.output}\nreturncode:{e.returncode}\ncmd:{e.cmd}.')
+        logging.exception(f'Could not login to {container_engine_type} on server {ip}:\n{e.stderr=}\n{e.output=}\n'
+                          f'{e.returncode=}\n{e.cmd=}.')
 
 
 def main():
