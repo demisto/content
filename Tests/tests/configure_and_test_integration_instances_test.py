a48367bafa9b30609793381434e28312adc515e9
GuyAfik <guyafik11@gmail.com>
2023-02-27 11:02:48 +0200

    update mock

--- Tests/tests/configure_and_test_integration_instances_test.py
+++ Tests/tests/configure_and_test_integration_instances_test.py
@@ -207,7 +207,7 @@ def test_get_packs_with_higher_min_version(mocker):
     # import Tests.Marketplace.upload_packs as upload_packs
     import Tests.configure_and_test_integration_instances as configure_and_test_integration_instances
 
-    mocker.patch("Tests.Marketplace.upload_packs.extract_packs_artifacts", mock_open(read_data='{"serverMinVersion": "6.6.0"}'))
+    mocker.patch("Tests.configure_and_test_integration_instances.extract_packs_artifacts")
     mocker.patch.object(configure_and_test_integration_instances, "get_json_file",
                         mock_open(read_data='{"serverMinVersion": "6.6.0"}'))
 
