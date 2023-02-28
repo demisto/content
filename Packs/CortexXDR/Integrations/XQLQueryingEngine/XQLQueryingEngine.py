764df38695c028be8b2a1d17de6addadf43d7632
Jas Beilin <jgranot@paloaltonetworks.com>
2023-02-28 13:13:38 +0200

    added sections

--- Packs/CortexXDR/Integrations/XQLQueryingEngine/XQLQueryingEngine.py
+++ Packs/CortexXDR/Integrations/XQLQueryingEngine/XQLQueryingEngine.py
@@ -5,13 +5,13 @@ import secrets
 import string
 from typing import Any, Dict, Tuple
 
-import requests
+import urllib3
 
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 # Disable insecure warnings
-requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
+urllib3.disable_warnings()  # pylint: disable=no-member
 
 ''' CONSTANTS '''
 
