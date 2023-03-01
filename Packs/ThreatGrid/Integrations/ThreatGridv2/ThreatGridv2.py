d10081cec71891e1f3d0aee80537cf07d54323da
Darya Koval <dkoval@paloaltonetworks.com>
2023-03-01 18:37:37 +0200

    fix format issues

--- Packs/ThreatGrid/Integrations/ThreatGridv2/ThreatGridv2.py
+++ Packs/ThreatGrid/Integrations/ThreatGridv2/ThreatGridv2.py
@@ -1,5 +1,3 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
 """
 Cisco ThreatGird integration
 """
@@ -18,7 +16,8 @@ from typing import (
     Union,
     Set,
 )
-
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
 
 DEFAULT_INTERVAL = 90
 DEFAULT_TIMEOUT = 600
