de3298318261529edd84bb5c009cdf6591065885
Menachem Weinfeld <mmhw770@gmail.com>
2023-03-01 09:47:38 +0200

    Added script - RelationsToTable

---
+++ Packs/AzureSentinel/Scripts/MicrosoftSentinelConvertRelationsToTable/MicrosoftSentinelConvertRelationsToTable.py
@@ -0,0 +1,64 @@
+import demistomock as demisto
+from CommonServerPython import *
+from itertools import chain
+import ast
+
+
+def format_relation(relation: dict) -> dict:
+    """
+    Converts an relation to a dictionary with the relevant fields.
+    """
+    return {
+        'name': relation.get('name'),
+        'relatedResourceKind': relation.get('properties', {}).get('relatedResourceKind'),
+        'relatedResourceType': relation.get('properties', {}).get('relatedResourceType'),
+        'relatedResourceName': relation.get('properties', {}).get('relatedResourceName'),
+        'relatedResourceId': relation.get('properties', {}).get('relatedResourceId')
+    }
+
+
+def convert_to_table(context_results: str) -> CommandResults:
+    """
+    Args:
+        context_results (str): list of dicts or a single dict but in string format
+
+    Returns:
+        CommandResults: CommandResults object with readable_output
+    """
+    context_results = ast.literal_eval(context_results)
+    if not isinstance(context_results, list):
+        context_results = [context_results]
+
+    context_formatted = [
+        format_relation(relation) for relation in context_results
+    ]
+
+    md = tableToMarkdown(
+        '',
+        context_formatted,
+        headers=[*dict.fromkeys(chain.from_iterable(context_formatted))],
+        removeNull=True,
+        sort_headers=False,
+        headerTransform=pascalToSpace
+    )
+
+    return CommandResults(
+        readable_output=md
+    )
+
+
+def main():
+    context = dict_safe_get(
+        demisto.callingContext,
+        ['context', 'Incidents', 0, 'CustomFields', 'microsoftsentinelrelations'],
+        {}
+    )
+
+    if not context:
+        return_error('No data to present')
+
+    return_results(convert_to_table(context))
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
