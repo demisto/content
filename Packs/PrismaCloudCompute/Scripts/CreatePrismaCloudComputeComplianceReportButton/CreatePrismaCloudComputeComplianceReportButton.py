e8f5c68a223142f337feaa077e755f9ef2225c0f
nericksen <nericksen@paloaltonetworks.com>
2023-03-01 15:29:45 -0500

    Fix try/except block; Add README for TagIndicatorButton

--- Packs/PrismaCloudCompute/Scripts/CreatePrismaCloudComputeComplianceReportButton/CreatePrismaCloudComputeComplianceReportButton.py
+++ Packs/PrismaCloudCompute/Scripts/CreatePrismaCloudComputeComplianceReportButton/CreatePrismaCloudComputeComplianceReportButton.py
@@ -7,28 +7,24 @@ def main():
         res = demisto.executeCommand("ConvertTableToHTML", {"table": args.get("table"), "title": args.get("title")})
 
         if is_error(res):
-            raise DemistoException(f'Failed to set indicators: {str(get_error(res))}')
+            raise DemistoException(f'Failed to create compliance report: {str(get_error(res))}')
 
-    except Exception as e:
-        return_error(e)
-
-    html = res[0]["EntryContext"]["HTMLTable"]
-
-    body = f"""
-    Hello,
-
-    Please see below the details for the compliance report from Prisma Cloud Compute
-
-    {html}
-
-    - DBot
-    """
-
-    try:
+        html = res[0]["EntryContext"]["HTMLTable"]
+    
+        body = f"""
+        Hello,
+    
+        Please see below the details for the compliance report from Prisma Cloud Compute
+    
+        {html}
+    
+        - DBot
+        """
+    
         res = demisto.executeCommand("send-mail", {"to": args.get("to"), "subject": "IMPORTANT: Prisma Cloud Compute Compliance", "body": body})
 
         if is_error(res):
-            raise DemistoException(f'Failed to set indicators: {str(get_error(res))}')
+            raise DemistoException(f'Failed to create compliance report: {str(get_error(res))}')
 
         demisto.results(res)
         return_results(CommandResults(
