3629fa616001c6090d2b8495002bb8f294b79a08
kgal-pan <kgal@paloaltonetworks.com>
2023-03-06 16:17:37 +0200

    pr review: rm conditionals when detections are empty

--- Packs/Vectra/Integrations/VectraEventCollector/VectraEventCollector.py
+++ Packs/Vectra/Integrations/VectraEventCollector/VectraEventCollector.py
@@ -417,14 +417,12 @@ def main() -> None:  # pragma: no cover
                 demisto.setLastRun(next_fetch)
 
             if should_push_events:
-                if detections:
-                    demisto.info(f"Sending {len(detections)} detections to XSIAM...")
-                    send_events_to_xsiam(detections, vendor=VENDOR, product=client.endpoints[0])
-                    demisto.info(f"{len(detections)} detections sent to XSIAM.")
-                if audits:
-                    demisto.info(f"Sending {len(audits)} audits to XSIAM...")
-                    send_events_to_xsiam(audits, vendor=VENDOR, product=client.endpoints[1])
-                    demisto.info(f"{len(audits)} audits sent to XSIAM.")
+                demisto.info(f"Sending {len(detections)} detections to XSIAM...")
+                send_events_to_xsiam(detections, vendor=VENDOR, product=client.endpoints[0])
+                demisto.info(f"{len(detections)} detections sent to XSIAM.")
+                demisto.info(f"Sending {len(audits)} audits to XSIAM...")
+                send_events_to_xsiam(audits, vendor=VENDOR, product=client.endpoints[1])
+                demisto.info(f"{len(audits)} audits sent to XSIAM.")
 
         else:
             raise NotImplementedError(f"command '{cmd}' is not implemented.")
