3adb404adb0fea0a1e26a797eda8635e925dea3a
GuyAfik <guyafik11@gmail.com>
2023-03-07 13:38:24 +0200

    change to STARTING_PAGE_NUMBER

--- Packs/CommonScripts/Scripts/SearchIncidentsV2/SearchIncidentsV2.py
+++ Packs/CommonScripts/Scripts/SearchIncidentsV2/SearchIncidentsV2.py
@@ -1,12 +1,11 @@
 from enum import Enum
-from typing import Dict, List
 import demistomock as demisto
 from CommonServerPython import *
 
 special = ['n', 't', '\\', '"', '\'', '7', 'r']
 DEFAULT_LIMIT = 100
 DEFAULT_PAGE_SIZE = 100
-DEFAULT_STARTING_PAGE_NUMBER = 1
+STARTING_PAGE_NUMBER = 1
 
 
 class AlertSeverity(Enum):
@@ -158,7 +157,7 @@ def search_incidents(args: Dict):   # pragma: no cover
     # adding 1 here because the default page number start from 0
     max_page = (res[0]["Contents"]["total"] // DEFAULT_PAGE_SIZE) + 1
 
-    page = DEFAULT_STARTING_PAGE_NUMBER
+    page = STARTING_PAGE_NUMBER
     while len(result_data_list) < limit and page < max_page:
         args['page'] = page
         result_data_list.extend(execute_command('getIncidents', args).get('data') or [])
