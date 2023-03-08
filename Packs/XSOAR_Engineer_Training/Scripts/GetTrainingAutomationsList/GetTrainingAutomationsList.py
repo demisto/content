e6db1a4ec376e89392cdd24c55d157ddf396732c
beauchompers <beauchompers@gmail.com>
2023-03-08 14:01:50 -0700

    adding secrets

--- Packs/XSOAR_Engineer_Training/Scripts/GetTrainingAutomationsList/GetTrainingAutomationsList.py
+++ Packs/XSOAR_Engineer_Training/Scripts/GetTrainingAutomationsList/GetTrainingAutomationsList.py
@@ -1,9 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-result = demisto.executeCommand("demisto-api-post", {"uri": "/automation/search",
-                                "body": {"query": "tags:training AND system:F"}})
-scripts = [{"Name": x["name"], "Description":x["comment"], "Tags":x["tags"]}
-           for x in result[0]["Contents"]["response"]["scripts"]]
+result = demisto.executeCommand("demisto-api-post", {"uri":"/automation/search","body":{"query":"tags:training"}})
+scripts = [{ "Name":x["name"],"Description":x["comment"],"Tags":x["tags"]} for x in result[0]["Contents"]["response"]["scripts"]]
 
 # remove the Training tag
 for script in scripts:
@@ -12,6 +10,5 @@ for script in scripts:
     script["Tags"] = tags
 
 # return a MD entry for the dynamic section
-results = CommandResults(readable_output=tableToMarkdown(
-    'Training Automation Scripts', scripts, headers=["Name", "Description", "Tags"]))
+results = CommandResults(readable_output=tableToMarkdown('Training Automation Scripts', scripts, headers=["Name","Description","Tags"]))
 return_results(results)
--- Packs/XSOAR_Engineer_Training/Scripts/GetTrainingPlaybooksList/GetTrainingPlaybooksList.py
+++ Packs/XSOAR_Engineer_Training/Scripts/GetTrainingPlaybooksList/GetTrainingPlaybooksList.py
@@ -1,9 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-result = demisto.executeCommand("demisto-api-post", {"uri": "/playbook/search",
-                                "body": {"query": "system:F"}})[0]["Contents"]["response"]
-playbooks = [{"Name": x.get("name", ""), "Description": x.get("comment", "")}
-             for x in result["playbooks"] if "XSOAR Engineer Training" in x.get("name")]
+result = demisto.executeCommand("demisto-api-post", {"uri":"/playbook/search","body":{"query":"tags:training"}})[0]["Contents"]["response"]
+playbooks = [ {"Name":x.get("name",""),"Description":x.get("comment","")} for x in result["playbooks"] if "XSOAR Engineer Training" in x.get("name")]
 
 # make the playbook clickable.
 data = []
@@ -16,5 +14,5 @@ for playbook in playbooks:
     data.append(fancy)
 
 # return a MD entry for the dynamic section
-results = CommandResults(readable_output=tableToMarkdown('Training Playbooks', data, headers=["Command", "Description"]))
+results = CommandResults(readable_output=tableToMarkdown('Training Playbooks', data, headers=["Command","Description"]))
 return_results(results)
