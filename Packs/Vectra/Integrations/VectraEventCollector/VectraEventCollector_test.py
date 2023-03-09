926de0c49ca571b08d7f4dd9be0dab11287256ab
kgal-pan <kgal@paloaltonetworks.com>
2023-03-09 11:56:06 +0200

    pr review: rm UT test_auth

--- Packs/Vectra/Integrations/VectraEventCollector/VectraEventCollector_test.py
+++ Packs/Vectra/Integrations/VectraEventCollector/VectraEventCollector_test.py
@@ -45,38 +45,6 @@ no_access_endpoints = load_json(Path("./test_data/endpoints_no_detection_audits.
 """ VectraClient Tests """
 
 
-@pytest.mark.parametrize(
-    "endpoints,expected",
-    [
-        ({"detections", f"{BASE_URL}/detections", "audits", f"{BASE_URL}/audits"}, True),
-        ({"detections", f"{BASE_URL}/detections"}, False),
-        ({"ep1", f"{BASE_URL}/ep1"}, False),
-        ({}, False),
-    ],
-)
-def test_auth(mocker: MockerFixture, endpoints: Dict[str, str], expected: bool):
-
-    """
-    Given:
-        - A Vectra client.
-    When:
-        - Case A: The returned endpoints from the API root are the required ones.
-        - Case B: The returned endpoints from the API root are missing 'audits'.
-        - Case C: The returned endpoints from the API root are missing 'audits' and 'detections'.
-        - Case D: The returned endpoints from the API root is empty.
-    Then:
-        - Case A: The authentication should succeed
-        - Case B: The authentication should fail
-        - Case C: The authentication should fail
-        - Case D: The authentication should fail
-    """
-
-    mocker.patch.object(client, "get_endpoints", return_value=endpoints)
-    endpoints = client.get_endpoints()
-
-    assert all(ep in endpoints for ep in client.endpoints) == expected
-
-
 @given(st.text())
 def test_create_headers(token: str):
 
