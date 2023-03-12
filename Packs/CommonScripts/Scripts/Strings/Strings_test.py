492278bbd09653b7f5a932a6d17e2e392d8513f6
epintzov <epintzov@paloaltonetworks.com>
2023-03-12 17:28:07 +0200

    bug fix

--- Packs/CommonScripts/Scripts/Strings/Strings_test.py
+++ Packs/CommonScripts/Scripts/Strings/Strings_test.py
@@ -32,9 +32,10 @@ def test_strings_regex(mocker):
     entry = strings({'chars': 1, 'size': 1024, 'entry': '123', 'filter': '.*'})
     assert entry == 'abcabc'
 
+
 def test_pdf_file(mocker):
     mocker.patch.object(demisto, 'executeCommand',
                         return_value=[{'path': './test_data/pdf_file.pdf', 'name': 'pdf_file.pdf', 'Type': ''}])
     mocker.patch.object(demisto, 'get', return_value='./test_data/pdf_file.pdf')
     entry = strings({'chars': 1, 'size': 1024, 'entry': '123', 'filter': '.*'})
-    assert entry is not ''
+    assert entry != ''
