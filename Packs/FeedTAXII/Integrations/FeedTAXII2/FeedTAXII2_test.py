ff47d535d3b52e0e27470315239e3d92a178ae9d
ostolero <ostolero@paloaltonetworks.com>
2023-03-14 18:00:03 +0200

    add failing UT

--- Packs/FeedTAXII/Integrations/FeedTAXII2/FeedTAXII2_test.py
+++ Packs/FeedTAXII/Integrations/FeedTAXII2/FeedTAXII2_test.py
@@ -104,6 +104,33 @@ class TestFetchIndicators:
         assert len(indicators) == 14
         assert mock_client.collection_to_fetch.id in last_run
 
+    def test_empty_string_collection_to_fetch(self, mocker):
+        """
+        Scenario: Test multi collection fetch with an empty string as collection_to_fetch
+
+        Given:
+        - collection to fetch is set to None
+        - there is no integration context
+        - limit is -1
+        - initial interval is `1 day`
+
+        When:
+        - fetch_indicators_command is called
+
+        Then:
+        - fetch 14 indicators
+        - update last run with latest collection fetch time
+        """
+        mock_client = Taxii2FeedClient(url='', collection_to_fetch="", proxies=[], verify=False, objects_to_fetch=[])
+        default_id = 1
+        nondefault_id = 2
+        mock_client.collections = [MockCollection(default_id, 'default'), MockCollection(nondefault_id, 'not_default')]
+
+        mocker.patch.object(mock_client, 'build_iterator', side_effect=[CORTEX_IOCS_1, CORTEX_IOCS_2])
+        indicators, last_run = fetch_indicators_command(mock_client, '1 day', -1, {})
+        assert len(indicators) == 14
+        assert mock_client.collection_to_fetch.id in last_run
+
     def test_multi_with_context(self, mocker):
         """
         Scenario: Test multi collection fetch with no last run
