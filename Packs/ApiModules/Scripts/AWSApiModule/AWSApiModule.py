b6fee93b53715cbee3945aee2633f52dd8665c2c
GuyAfik <guyafik11@gmail.com>
2023-03-23 18:46:10 +0200

    fix the bug

--- Packs/ApiModules/Scripts/AWSApiModule/AWSApiModule.py
+++ Packs/ApiModules/Scripts/AWSApiModule/AWSApiModule.py
@@ -74,6 +74,8 @@ class AWSClient:
                     role_policy=None):
         kwargs = {}
 
+        demisto.debug(f'{region=}, {role_arn=}, {role_session_name=}, {role_session_duration=}')
+
         self.update_config()
 
         if role_arn and role_session_name is not None:
@@ -97,8 +99,10 @@ class AWSClient:
         elif self.aws_role_policy is not None:
             kwargs.update({'Policy': self.aws_role_policy})
 
-        if kwargs and not self.aws_access_key_id:  # login with Role ARN
+        demisto.debug(f'{kwargs=}')
 
+        if kwargs and not self.aws_access_key_id:  # login with Role ARN
+            demisto.debug(f'entered kwargs and not self.aws_access_key_id')
             if not self.aws_access_key_id:
                 sts_client = boto3.client('sts', config=self.config, verify=self.verify_certificate,
                                           region_name=region if region else self.aws_default_region)
@@ -112,7 +116,8 @@ class AWSClient:
                     verify=self.verify_certificate,
                     config=self.config
                 )
-        elif self.aws_access_key_id and self.aws_role_arn:  # login with Access Key ID and Role ARN
+        elif self.aws_access_key_id and (self.aws_role_arn or role_arn):  # login with Access Key ID and Role ARN
+            demisto.debug(f'entered self.aws_access_key_id and self.aws_role_arn')
             sts_client = boto3.client(
                 service_name='sts',
                 aws_access_key_id=self.aws_access_key_id,
@@ -121,9 +126,10 @@ class AWSClient:
                 config=self.config
             )
             kwargs.update({
-                'RoleArn': self.aws_role_arn,
+                'RoleArn': self.aws_role_arn or role_arn,
                 'RoleSessionName': self.aws_role_session_name,
             })
+            demisto.debug(f'{kwargs=}')
             sts_response = sts_client.assume_role(**kwargs)
             client = boto3.client(
                 service_name=service,
@@ -135,6 +141,7 @@ class AWSClient:
                 config=self.config
             )
         elif self.aws_session_token and not self.aws_role_arn:  # login with session token
+            demisto.debug(f'entered self.aws_session_token and not self.aws_role_arn')
             client = boto3.client(
                 service_name=service,
                 region_name=region if region else self.aws_default_region,
@@ -145,6 +152,7 @@ class AWSClient:
                 config=self.config
             )
         elif self.aws_access_key_id and not self.aws_role_arn:  # login with access key id
+            demisto.debug(f'entered self.aws_access_key_id and not self.aws_role_arn')
             client = boto3.client(
                 service_name=service,
                 region_name=region if region else self.aws_default_region,
@@ -154,6 +162,7 @@ class AWSClient:
                 config=self.config
             )
         else:  # login with default permissions, permissions pulled from the ec2 metadata
+            demisto.debug(f'entered else statement')
             client = boto3.client(service_name=service,
                                   region_name=region if region else self.aws_default_region)
 
