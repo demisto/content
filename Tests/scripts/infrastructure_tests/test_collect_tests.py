27dfc31d8c01cda6f2d5877fce7e1ff9602f69bb
israelpolishook <ipolishuk@paloaltonetworks.com>
2023-03-24 03:07:52 +0200

    update poetry and fixed test_collect_tests.py

--- Tests/scripts/infrastructure_tests/test_collect_tests.py
+++ Tests/scripts/infrastructure_tests/test_collect_tests.py
@@ -618,7 +618,7 @@ def test_number_of_file_types():
 
         - Removed type:    Decrease the number here.
     """
-    assert len(FileType) == 76
+    assert len(FileType) == 77
 
 
 @pytest.mark.parametrize(
