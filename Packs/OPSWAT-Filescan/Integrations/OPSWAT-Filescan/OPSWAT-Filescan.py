a66c25969ab237fdf13ed75a1099cc7f3520e2a2
Aniko Bartos <aniko.bartos@opswat.com>
2023-03-26 04:12:27 +0200

    Add OPSWAT Filescan integration

---
+++ Packs/OPSWAT-Filescan/Integrations/OPSWAT-Filescan/OPSWAT-Filescan.py
@@ -0,0 +1,390 @@
+from CommonServerPython import *
+from CommonServerUserPython import *
+
+""" IMPORTS """
+from typing import Any, Dict, List
+import urllib3
+
+# Disable insecure warnings
+urllib3.disable_warnings()
+
+"""GLOBALS/PARAMS """
+INTEGRATION_NAME = "OPSWAT Filescan Integration"
+INTEGRATION_CONTEXT_NAME = "OPSWAT.Filescan"
+
+
+class Client(BaseClient):
+    def __init__(self, api_key: str, base_url: str, proxy: bool, verify: bool):
+        super().__init__(base_url=base_url, proxy=proxy, verify=verify)
+        self.api_key = api_key
+
+        if self.api_key:
+            self._headers = {"X-Api-Key": self.api_key}
+
+    def test_module(self) -> Dict:
+        """
+        Return information about the user. (Need API key)
+        """
+        request_result = self._http_request(
+            method="GET",
+            ok_codes=([200]),
+            url_suffix="/users/me",
+        )
+        return request_result
+
+    def post_sample(self, args: Dict[str, Any]) -> Dict[str, Any]:
+        data = {}
+
+        if description := args.get("description"):
+            data["description"] = description
+        if tags := args.get("tags"):
+            data["tags"] = tags
+        if password := args.get("password"):
+            data["password"] = password
+        if is_private := args.get("is_private"):
+            data["is_private"] = is_private
+
+        if url := args.get("url"):
+            data["url"] = url
+
+            return self._http_request(
+                method="POST", url_suffix="/scan/url", ok_codes=([200]), data=data
+            )
+
+        elif entry_id := args.get("entry_id"):
+            try:
+                file_entry = demisto.getFilePath(entry_id)
+            except Exception as e:
+                raise DemistoException(
+                    f'Failed to find file entry with id:"{entry_id}". got error: {e}'
+                )
+
+            with open(file_entry["path"], "rb") as file:
+                return self._http_request(
+                    method="POST",
+                    url_suffix="/scan/file",
+                    ok_codes=([200]),
+                    data=data,
+                    files={"file": file},
+                )
+        else:
+            raise DemistoException("No file or URL was provided.")
+
+    def get_scan_result(self, flow_id: str) -> Dict[str, Any]:
+
+        filters = [
+            "filter=general",
+            "filter=finalVerdict",
+            "filter=allTags",
+            "filter=overallState",
+            "filter=taskReference",
+            "filter=subtaskReferences",
+            "filter=allSignalGroups",
+        ]
+
+        postfix = "&".join(filters)
+
+        url_suffix = f"/scan/{flow_id}/report?{postfix}"
+
+        response = self._http_request(
+            method="GET",
+            ok_codes=([200]),
+            url_suffix=url_suffix,
+        )
+
+        return response
+
+    def get_search_query(self, query_str: str) -> Dict[str, Any]:
+        return self._http_request(
+            method="GET",
+            ok_codes=([200]),
+            params={"query": query_str, "page_size": 10, "page": 1},
+            url_suffix="/reports/search",
+        )
+
+
+""" HELPER FUNCTIONS """
+
+
+def build_one_reputation_result(report: Dict[str, Any]):
+    score = Common.DBotScore.NONE
+
+    final_verdict = report.get("finalVerdict", {})
+    verdict = final_verdict.get("verdict")
+    if verdict == "BENIGN" or verdict == "INFORMATIONAL":
+        score = Common.DBotScore.GOOD
+    elif verdict == "MALICIOUS" or verdict == "LIKELY_MALICIOUS":
+        score = Common.DBotScore.BAD
+    elif verdict == "SUSPICIOUS":
+        score = Common.DBotScore.SUSPICIOUS
+    else:
+        score = Common.DBotScore.NONE
+
+    report_file = report.get("file", {})
+    report_hash = report_file.get("hash", None)
+
+    dbot_score = Common.DBotScore(
+        indicator=report_hash,
+        indicator_type=DBotScoreType.FILE,
+        integration_name="OPSWAT Filescan",
+        score=score,
+    )
+
+    file = Common.File(
+        name=report_file.get("name"), sha256=report_hash, dbot_score=dbot_score
+    )
+
+    tags = [tag.get("tag", {}).get("name") for tag in report.get("allTags", [])]
+    subtasks = [subtask.get("name") for subtask in report.get("subtaskReferences", [])]
+    human_readable = {
+        "FileName": report_file.get("name"),
+        "FileHash": report_hash,
+        "FileType": report_file.get("type"),
+        "FinalVerdict": report.get("finalVerdict", {}).get("verdict"),
+        "Tags": tags,
+        "SubtaskReferences": subtasks,
+    }
+    readable_output = tableToMarkdown("Scan Result (digest):", human_readable)
+
+    results = CommandResults(
+        outputs_prefix=f"{INTEGRATION_CONTEXT_NAME}.Analysis",
+        outputs_key_field="sha256",
+        outputs=report,
+        raw_response=report,
+        readable_output=readable_output,
+        indicator=file,
+    )
+    return results
+
+
+def build_serach_query_result(
+    query_str: str, raw_result: Dict[str, Any]
+) -> List[CommandResults]:
+    def build_analysis_hr(analysis: Dict[str, Any]) -> Dict[str, Any]:
+        file_result = analysis.get("file", {})
+        hr_analysis = {
+            "Id": analysis.get("id"),
+            "SampleName": file_result.get("name"),
+            "SHA256": file_result.get("sha256"),
+            "Verdict": analysis.get("verdict"),
+            "State": analysis.get("state"),
+            "Date": analysis.get("date"),
+            "MIMEType": file_result.get("mime_type"),
+            "Type": file_result.get("short_type"),
+            "Tags": analysis.get("tags"),
+        }
+        return hr_analysis
+
+    def build_indicator_object(analysis: Dict[str, Any]):
+        score = Common.DBotScore.NONE
+
+        verdict = analysis.get("verdict", "UNKNOWN")
+        if verdict.upper() == "BENIGN":
+            score = Common.DBotScore.GOOD
+        elif verdict.upper() == "UNKNOWN":
+            score = Common.DBotScore.NONE
+        elif verdict.upper() == "INFORMATIONAL" or verdict.upper() == "SUSPICIOUS":
+            score = Common.DBotScore.SUSPICIOUS
+        else:
+            score = Common.DBotScore.BAD
+
+        analysis_file = analysis.get("file", {})
+        dbot_score = Common.DBotScore(
+            indicator=analysis_file.get("sha256"),
+            indicator_type=DBotScoreType.FILE,
+            integration_name="OPSWAT Filescan",
+            score=score,
+        )
+
+        file = Common.File(
+            name=analysis_file.get("name"),
+            sha256=analysis_file.get("sha256"),
+            dbot_score=dbot_score,
+        )
+
+        headers = ["File Name", "SHA256", "Verdict"]
+        hr = {
+            "File Name": analysis_file.get("name"),
+            "SHA256": analysis_file.get("sha256"),
+            "Verdict": verdict,
+        }
+        results = CommandResults(
+            outputs_prefix=f"{INTEGRATION_CONTEXT_NAME}.Analysis",
+            outputs_key_field="sha256",
+            outputs=hr,
+            readable_output=tableToMarkdown("File Result:", hr, headers),
+            indicator=file,
+        )
+        return results
+
+    hr_headers = [
+        "Id",
+        "SampleName",
+        "SHA256",
+        "Verdict",
+        "State",
+        "Date",
+        "MIMEType",
+        "Type",
+        "Tags",
+    ]
+
+    command_res_ls = []
+    hr_analysis_ls = []
+
+    analyses = raw_result.get("items", {})
+
+    for analysis in analyses:
+        hr_analysis_ls.append(build_analysis_hr(analysis))
+        command_res_ls.append(build_indicator_object(analysis))
+
+    command_result = CommandResults(
+        outputs=analyses,
+        readable_output=tableToMarkdown("Analysis Result:", hr_analysis_ls, hr_headers),
+        outputs_prefix=f"{INTEGRATION_CONTEXT_NAME}.Analysis",
+    )
+
+    command_res_ls.append(command_result)
+    return command_res_ls
+
+
+def sample_submission(client: Client, args: Dict[str, Any]) -> PollResult:
+    res = client.post_sample(args)
+    partial_res = CommandResults(
+        readable_output=f'Waiting for submission "{res.get("flow_id")}" to finish...'
+    )
+    return PollResult(
+        response=CommandResults(
+            outputs=res,
+            outputs_prefix=f"{INTEGRATION_CONTEXT_NAME}.Submission",
+            outputs_key_field="flow_id",
+        ),
+        args_for_next_run={"flow_id": res.get("flow_id"), **args},
+        continue_to_poll=True,
+        partial_result=partial_res,
+    )
+
+
+def build_reputation_result(api_reponse: Dict[str, Any]):
+    reports = api_reponse.get("reports", {})
+
+    if len(reports) == 1:
+        first_report = next(iter((api_reponse.get("reports", {}).items())))[1]
+        return build_one_reputation_result(first_report)
+
+    elif len(reports) > 1:
+        command_res_ls = []
+        for report in reports:
+            command_res_ls.append(build_one_reputation_result(reports[report]))
+        return command_res_ls
+
+
+def is_valid_pass(api_response: Dict[str, Any]):
+    if "rejected_files" not in api_response:
+        return True
+    for reject in api_response["rejected_files"]:
+        if reject.get("rejected_reason") == "INVALID_PASSWORD":
+            return False
+    return True
+
+
+@polling_function(
+    name=demisto.command(),
+    timeout=arg_to_number(demisto.args().get("timeout", 600)),
+    interval=1,
+    poll_message="Polling result",
+    requires_polling_arg=False,
+)
+def polling_submit_command(args: Dict[str, Any], client: Client):
+
+    if flow_id := args.get("flow_id"):
+        api_response = client.get_scan_result(flow_id)
+        successful_response = False
+
+        if api_response.get("allFinished", False):
+            successful_response = True
+
+        if successful_response:
+            if not is_valid_pass(api_response):
+                raise DemistoException("Invalid password!")
+
+            return PollResult(
+                response=build_reputation_result(api_response), continue_to_poll=False
+            )
+
+        return PollResult(
+            response=[
+                CommandResults(
+                    outputs=api_response,
+                    outputs_prefix=f"{INTEGRATION_CONTEXT_NAME}.Submission",
+                    outputs_key_field="flow_id",
+                    readable_output=f'Waiting for submission "{api_response.get("flow_id")}" to finish...',
+                ),
+            ],
+            continue_to_poll=True,
+            args_for_next_run={"flow_id": args.get("flow_id"), **args},
+        )
+    else:
+        return sample_submission(client, args)
+
+
+""" COMMANDS """
+
+
+def test_module_command(client: Client, *_) -> str:
+    """Performs a basic GET request to check if the API is reachable and authentication is successful."""
+    results = client.test_module()
+    if "accountId" in results:
+        return "ok"
+    raise DemistoException("\nTest module failed, {}".format(results))
+
+
+def scan_command(client: Client, args: Dict[str, Any]):
+    return polling_submit_command(args=args, client=client)
+
+
+def search_query_command(client: Client, args: Dict[str, Any]):
+    query_string = args.get("query", "")
+    result = client.get_search_query(query_string)
+    if result:
+        return build_serach_query_result(query_string, result)
+    return CommandResults(readable_output="No Results were found.")
+
+
+""" COMMANDS MANAGER / SWITCH PANEL """
+
+
+def main():
+
+    params = demisto.params()
+    base_url = params.get("url", "")
+    api_key = params.get("api_key", "")
+    verify = not params.get("insecure", False)
+    proxy = params.get("proxy", False)
+
+    command = demisto.command()
+    demisto.info(f"Command being called is {command}")
+    args = demisto.args()
+
+    try:
+        client = Client(api_key=api_key, base_url=base_url, verify=verify, proxy=proxy)
+        handle_proxy()
+
+        if command == "test-module":
+            return_results(test_module_command(client))
+        elif command == "filescan-scan-url":
+            return_results(scan_command(client, args))
+        elif command == "filescan-scan-file":
+            return_results(scan_command(client, args))
+        elif command == "filescan-search-query":
+            return_results(search_query_command(client, args))
+        else:
+            raise NotImplementedError(f"{command} command is not implemented.")
+    except Exception as e:
+        err_msg = f"Exception in {INTEGRATION_NAME} : Failed to execute {command} command: [{e!r}]"
+        return_error(err_msg, error=e)
+
+
+""" ENTRY POINT """
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main()
---
+++ Packs/OPSWAT-Filescan/Integrations/OPSWAT-Filescan/OPSWAT-Filescan_test.py
@@ -0,0 +1,161 @@
+from typing import Any
+from CommonServerPython import Common, DemistoException, ExecutionMetrics
+import json
+import pytest
+import io
+import importlib
+
+OPSWAT_Filescan = importlib.import_module("OPSWAT-Filescan")
+
+def util_load_json(path: str) -> Any:
+    with io.open(path, mode="r", encoding="utf-8") as f:
+        return json.loads(f.read())
+
+
+@pytest.fixture()
+def client():
+    return OPSWAT_Filescan.Client(
+        base_url="https://test.com", api_key="mockkey", proxy=False, verify=False
+    )
+
+
+APIKEY_VALIDATION_SUCCESS = {
+    "accountId": "1234",
+    "username": "Aniko",
+    "email": "aniko@o.com",
+}
+
+@pytest.mark.parametrize("result, expected", [(APIKEY_VALIDATION_SUCCESS, "ok")])
+def test_test_module_positive(mocker, client, result, expected):
+    mocker.patch.object(client, "test_module", return_value=result)
+    response = OPSWAT_Filescan.test_module_command(client)
+    assert response == expected
+
+
+APIKEY_VALIDATION_FAILURE = {"detail": "Could not validate credentials"}
+
+@pytest.mark.parametrize(
+    "result, expected", [(APIKEY_VALIDATION_FAILURE, DemistoException)]
+)
+def test_test_module_negative(mocker, client, result, expected):
+    mocker.patch.object(client, "test_module", return_value=result)
+    with pytest.raises(Exception) as e:
+        response = OPSWAT_Filescan.test_module_command(client)
+    assert isinstance(e.value, expected)
+
+
+def test_search_query_command_hash_badfile(mocker, client):
+    raw_response = util_load_json("test_data/query_hash_badfile.json")
+
+    mocker.patch.object(client, "get_search_query", return_value=raw_response)
+    response = OPSWAT_Filescan.search_query_command(client, {})
+
+    assert response[0].indicator.dbot_score.indicator == "834d1dbfab8330ea5f1844f6e905ed0ac19d1033ee9a9f1122ad2051c56783dc"
+    assert response[0].indicator.dbot_score.score == 3
+    assert response[0].indicator.dbot_score.integration_name == "OPSWAT Filescan"
+    assert response[0].indicator.name == "bad_file.exe"
+    assert response[0].indicator.sha256 == "834d1dbfab8330ea5f1844f6e905ed0ac19d1033ee9a9f1122ad2051c56783dc"
+    assert response[0].outputs['SHA256'] == "834d1dbfab8330ea5f1844f6e905ed0ac19d1033ee9a9f1122ad2051c56783dc"
+    assert response[0].outputs['Verdict'] == "malicious"
+
+
+def test_search_query_command_hash_cleanfile(mocker, client):
+    raw_response = util_load_json("test_data/query_hash_cleanfile.json")
+
+    mocker.patch.object(client, "get_search_query", return_value=raw_response)
+    response = OPSWAT_Filescan.search_query_command(client, {})
+
+    assert response[0].indicator.dbot_score.indicator == "a33a6ee82144b97bf75728a7ec302dd88ae2fa54389caeb8442839580b259b85"
+    assert response[0].indicator.dbot_score.score == 2
+    assert response[0].indicator.dbot_score.integration_name == "OPSWAT Filescan"
+    assert response[0].indicator.name == "e0e1a581-1c69-414f-8c50-22fa3afc34ae_1010%40e0e1a581-1c69-414f-8c50-22fa3afc34ae"
+    assert response[0].indicator.sha256 == "a33a6ee82144b97bf75728a7ec302dd88ae2fa54389caeb8442839580b259b85"
+    assert response[0].outputs['SHA256'] == "a33a6ee82144b97bf75728a7ec302dd88ae2fa54389caeb8442839580b259b85"
+    assert response[0].outputs['Verdict'] == "informational"
+
+
+def test_search_query_command_url(mocker, client):
+    raw_response = util_load_json("test_data/query_url_google.json")
+
+    mocker.patch.object(client, "get_search_query", return_value=raw_response)
+    response = OPSWAT_Filescan.search_query_command(client, {})
+
+    assert response[0].indicator.dbot_score.indicator == "5302e0de83c841169f0543eaf5f9a2b7313d49d35d9f3ecbeed4e6b353b5a2c8"
+    assert response[0].indicator.dbot_score.score == 2
+    assert response[0].indicator.dbot_score.integration_name == "OPSWAT Filescan"
+    assert response[0].indicator.name == "UNKNOW.EXE"
+    assert response[0].indicator.sha256 == "5302e0de83c841169f0543eaf5f9a2b7313d49d35d9f3ecbeed4e6b353b5a2c8"
+    assert response[0].outputs['SHA256'] == "5302e0de83c841169f0543eaf5f9a2b7313d49d35d9f3ecbeed4e6b353b5a2c8"
+    assert response[0].outputs['Verdict'] == "informational"
+
+
+def test_scan_command_url_polling_waiting(requests_mock, mocker, client):
+    from CommonServerPython import ScheduledCommand
+
+    mocker.patch.object(client, "post_sample", return_value={"flow_id": "1234"})
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported', return_value=None)
+    mocker.patch("builtins.open", create=True)
+
+    response = OPSWAT_Filescan.scan_command(client, {'entry_id': 'test_entry_id'})
+    assert response.readable_output == 'Waiting for submission "1234" to finish...'
+
+
+def test_scan_command_url_polling(mocker, client):
+    import requests
+    from CommonServerPython import ScheduledCommand
+
+    args = {'url': 'https://www.google.com'}
+    raw_response = util_load_json("test_data/scan_command_url_response.json")
+    mocker.patch.object(client, "post_sample", return_value={"flow_id": "1234"})
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported', return_value=None)
+
+    response = OPSWAT_Filescan.scan_command(client, args)
+    assert response.readable_output == 'Waiting for submission "1234" to finish...'
+
+    polling_args = {"flow_id": "1234", "hide_polling_output": True, "continue_to_poll": True, "url": "test.com"}
+    mocker.patch.object(client, "get_scan_result", return_value=raw_response)
+    response = OPSWAT_Filescan.scan_command(client, polling_args)
+
+    assert response.indicator.dbot_score.indicator == "ac6bb669e40e44a8d9f8f0c94dfc63734049dcf6219aac77f02edf94b9162c09"
+    assert response.indicator.dbot_score.score == 1
+    assert response.indicator.dbot_score.integration_name == "OPSWAT Filescan"
+    assert response.indicator.name == "https://www.google.com"
+    assert response.indicator.sha256 == "ac6bb669e40e44a8d9f8f0c94dfc63734049dcf6219aac77f02edf94b9162c09"
+    assert response.outputs['finalVerdict']['verdict'] == "BENIGN"
+    assert len(response.outputs['allTags']) == 2
+    assert response.outputs['overallState'] == "success_partial"
+    assert response.outputs['taskReference']['name'] == "transform-file"
+    assert response.outputs['file']['name'] == "https://www.google.com"
+    assert response.outputs['file']['hash'] == "ac6bb669e40e44a8d9f8f0c94dfc63734049dcf6219aac77f02edf94b9162c09"
+    assert response.outputs['file']['type'] == "other"
+
+
+def test_scan_command_file_polling(mocker, client):
+    import requests
+    from CommonServerPython import ScheduledCommand
+
+    args = {'entry_id': 'test_entry_id'}
+    raw_response = util_load_json("test_data/scan_command_zip_response.json")
+    mocker.patch.object(client, "post_sample", return_value={"flow_id": "1234"})
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported', return_value=None)
+
+    response = OPSWAT_Filescan.scan_command(client, args)
+    assert response.readable_output == 'Waiting for submission "1234" to finish...'
+
+    polling_args = {"flow_id": "1234", "hide_polling_output": True, "continue_to_poll": True, "url": "test.com"}
+    mocker.patch.object(client, "get_scan_result", return_value=raw_response)
+    response = OPSWAT_Filescan.scan_command(client, polling_args)
+
+    assert len(response) == 3
+    assert response[0].indicator.dbot_score.indicator == "834d1dbfab8330ea5f1844f6e905ed0ac19d1033ee9a9f1122ad2051c56783dc"
+    assert response[1].indicator.dbot_score.score == 1
+    assert response[2].indicator.dbot_score.integration_name == "OPSWAT Filescan"
+    assert response[0].indicator.name == "bad_file.exe"
+    assert response[1].indicator.sha256 == "ede5221225a03b12d11df11f4babf24e9c4a55e05aff27612813dd44876a71c2"
+    assert response[2].outputs['finalVerdict']['verdict'] == "INFORMATIONAL"
+    assert len(response[0].outputs['allTags']) == 5
+    assert response[1].outputs['overallState'] == "success"
+    assert response[2].outputs['taskReference']['name'] == "transform-file"
+    assert response[1].outputs['file']['name'] == "munkaltatoi.docx"
+    assert response[2].outputs['file']['hash'] == "2ee79f9a52e660f2322985c72c9dffefdfb5a3c302576d61b4e629d049098cb5"
+    assert response[0].outputs['file']['type'] == "pe"
