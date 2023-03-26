7cb9b8c9e73ad76796ba302e3f1d3ceb70e8219e
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2023-03-26 18:15:44 +0300

    Fix unit-test for `nexpose-start-site-scan` command

--- Packs/Rapid7_Nexpose/Integrations/Rapid7_Nexpose/Rapid7_Nexpose_test.py
+++ Packs/Rapid7_Nexpose/Integrations/Rapid7_Nexpose/Rapid7_Nexpose_test.py
@@ -1636,24 +1636,20 @@ def test_update_vulnerability_exception_status_command(mocker, mock_client: Clie
     assert result.outputs is None
 
 
-@pytest.mark.parametrize("site_id, site_assets_mock_file, expected_post_data_file",
+@pytest.mark.parametrize("site_id, hosts, expected_post_data",
                          [
-                             ("1", "client_get_site_assets", "start_site_scan_command")
+                             ("1", None, {"name": "Test Scan"}),
+                             ("1", ["192.0.2.0"], {"hosts": ["192.0.2.0"], "name": "Test Scan"})
                          ])
-def test_start_site_scan_command(mocker, mock_client: Client, site_id: str,
-                                 site_assets_mock_file: str, expected_post_data_file: str):
+def test_start_site_scan_command(mocker, mock_client: Client, site_id: str,  hosts: list[str] | None,
+                                 expected_post_data: dict):
     """
     Given: Valid parameters for the start_site_scan_command function.
     When: Calling the start_site_scan_command function.
     Then: Ensure a valid API call is made and no context output is returned.
     """
-    site_assets_data = load_test_data("api_mock", site_assets_mock_file)
-    mocker.patch.object(Client, "get_site_assets", return_value=site_assets_data)
-
     http_request = mocker.patch.object(BaseClient, "_http_request", return_value={})
-    start_site_scan_command(client=mock_client, site_id=site_id, name="Test Scan")
-
-    expected_post_data = load_test_data("expected_post_data", expected_post_data_file)
+    start_site_scan_command(client=mock_client, site_id=site_id, name="Test Scan", hosts=hosts)
 
     http_request.assert_called_with(
         url_suffix=f"/sites/{site_id}/scans",
