41e213bb6ad6ee965251bfcaae2df6194d70d90d
sepaprivate <113604678+sepaprivate@users.noreply.github.com>
2023-03-27 14:31:50 +0400

    Added ChatGPTv3 Integration Manually

---
+++ Packs/OpenAI/Integrations/OpenAI/OpenAiChatGPTV3/OpenAiChatGPTV3.py
@@ -0,0 +1,153 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+import requests
+import json
+
+
+''' CLIENT CLASS '''
+
+
+class Client(BaseClient):
+    """ Client class to interact with the OpenAI ChatGPT API v3
+    """
+
+    def __init__(self, api_key: str, base_url: str, proxy: bool, verify: bool):
+        super().__init__(base_url=base_url, proxy=proxy, verify=verify)
+        self.api_key = api_key
+        self.base_url = base_url
+
+    def chatgpt(self, prompt: str):
+        if self.api_key:
+            self._headers = {'Authorization': f"Bearer {self.api_key}", "Content-Type": "application/json"}
+            options = {"max_tokens": 1000, "model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": prompt}]}
+        return requests.post(self.base_url, headers=self._headers, json=options)
+
+
+''' COMMAND FUNCTIONS '''
+
+
+def test_module(client: Client) -> str:
+    """
+    Tests API connectivity and authentication'
+    Returning 'ok' indicates that connection to the service is successful.
+    Raises exceptions if something goes wrong.
+    """
+    response = client.chatgpt('Hello!')
+    status = ''
+    try:
+        if response:
+            status = 'ok'
+            return status
+    except Exception as e:
+        exception_text = str(e).lower()
+        if 'forbidden' in exception_text or 'authorization' in exception_text:
+            status = 'Authorization Error: make sure API Key is correctly set'
+            return status
+        else:
+            raise e
+
+    return status
+
+
+def chatgpt_send_prompt_command(client: Client, prompt: str) -> CommandResults:
+    """
+    Command to send prompts to OpenAI ChatGPT API
+    and receive a response converted into json then
+    returned to Output function to convert it to markdown table
+
+    :type client: ``Client``
+    :param prompt:  arguments
+    """
+    if not prompt:
+        raise DemistoException('the prompt argument cannot be empty.')
+
+    response = client.chatgpt(prompt)
+
+    chatgpt_response = response.json()
+
+    return chatgpt_output(chatgpt_response)
+
+
+def chatgpt_output(response) -> CommandResults:
+    """
+    Convert response from ChatGPT to a human readable format in markdown table
+
+    :return: CommandResults return output of ChatGPT response
+    :rtype: ``CommandResults``
+    """
+    if response and isinstance(response, dict):
+
+        rep = json.dumps(response)
+        repJSON = json.loads(rep)
+        model = repJSON['model']
+        createdTime = repJSON['created']
+        id = repJSON['id']
+        choices = repJSON['choices'][0]['message']['content'].strip('\n')
+        promptTokens = repJSON['usage']['prompt_tokens']
+        completionTokens = repJSON['usage']['completion_tokens']
+        totalTokens = repJSON['usage']['total_tokens']
+        context = [{'ID': id, 'Model': model,
+                    'ChatGPT Response': choices, 'Created Time': createdTime,
+                    'Number of Prompt Tokens': promptTokens,
+                    'Number of Completion Tokens': completionTokens,
+                    'Number of Total Tokens': totalTokens
+                    }]
+
+    markdown = tableToMarkdown(
+        '### ChatGPT API Response ###',
+        context,
+        date_fields=['Created Time'],
+    )
+
+    results = CommandResults(
+        readable_output=markdown,
+        outputs_prefix='ChatGPTResponse',
+        outputs_key_field='id',
+        outputs=context
+    )
+
+    return results
+
+
+''' MAIN FUNCTION '''
+
+
+def main() -> None:
+    """main function, runs command functions
+    """
+    params = demisto.params()
+    args = demisto.args()
+    command = demisto.command()
+
+    api_key = params.get('apikey', {}).get('password')
+    base_url = params.get('url', '')
+    verify = params.get('insecure', False)
+    proxy = params.get('proxy', False)
+
+    demisto.debug(f'Command being called is {command}')
+    try:
+        client = Client(api_key=api_key, base_url=base_url, verify=verify, proxy=proxy)
+
+        if command == 'test-module':
+            # This is the call made when clicking the integration Test button.
+            return_results(test_module(client))
+
+        elif command == 'chatgpt-send-prompt':
+            return_results(chatgpt_send_prompt_command(client, **args))
+
+        else:
+            raise NotImplementedError(f"command {command} is not implemented.")
+
+    # Log exceptions and return errors
+    except Exception as e:
+        demisto.error(traceback.format_exc())  # print the traceback
+        return_error("\n".join(("Failed to execute {command} command.",
+                                "Error:",
+                                str(e))))
+
+
+''' ENTRY POINT '''
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
