78663f583247d44d3431bbbb571827e8de963013
YuvHayun <yhayun011@gmail.com>
2023-03-30 09:45:28 +0300

    changes

--- Packs/FeedTAXII/Integrations/FeedTAXII2/FeedTAXII2.py
+++ Packs/FeedTAXII/Integrations/FeedTAXII2/FeedTAXII2.py
@@ -158,20 +158,18 @@ def get_indicators_command(
     else:
         indicators = client.build_iterator(limit=limit, added_after=added_after)
     relationships_list: list = []
+    parsed_relationships: str = ""
     if indicators and indicators[-1].get('value', ) == "$$DummyIndicator$$":
         relationships_list = indicators[-1].get('relationships', )
+        parsed_relationships = f"\n\n\nRelations ships:\n{tableToMarkdown('', relationships_list)}"
         indicators.pop()
 
     if raw:
         demisto.results({"indicators": [x.get("rawJSON") for x in indicators]})
         return
 
-    md = f"Found {len(indicators)} results:\n" + tableToMarkdown(
-        "", indicators, ["value", "type"]
-    )
-    if relationships_list:
-        parsed_relationships = tableToMarkdown("", relationships_list)
-        md = f"{md}\n\n\nRelations ships:\n{parsed_relationships}"
+    md = f"Found {len(indicators)} results:\n{tableToMarkdown('', indicators, ['value', 'type'])}{parsed_relationships}"
+
     if indicators:
         return CommandResults(
             outputs_prefix=CONTEXT_PREFIX + ".Indicators",
