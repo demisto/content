3a9b106c831295737f954d1ded107460ecc75840
dansterenson <dansterenson@gmail.com>
2023-04-02 14:40:24 +0300

    update docker image

--- Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule.py
+++ Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule.py
@@ -7,7 +7,13 @@ from CommonServerUserPython import *
 import requests
 import re
 import base64
-import defusedxml.ElementTree as defused_ET
+
+try:
+    import defusedxml.ElementTree as ET
+except ImportError:
+    demisto.debug('defused_ET is not supported, using ET instead.')
+    import xml.etree.cElementTree as ET
+
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from typing import Dict, Tuple, List, Optional
 
@@ -264,7 +270,7 @@ class MicrosoftClient(BaseClient):
             if resp_type == 'content':
                 return response.content
             if resp_type == 'xml':
-                defused_ET.parse(response.text)
+                ET.parse(response.text)
             return response
         except ValueError as exception:
             raise DemistoException('Failed to parse json object from response: {}'.format(response.content), exception)
