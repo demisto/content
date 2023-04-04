24e2cb0a12384dfc072022c2830d13175e6fcf80
bigeasyj <janny@paloaltonetworks.com>
2023-04-04 14:21:31 -0500

    Refactor client code to fixture

--- Packs/CortexAttackSurfaceManagement/Integrations/CortexAttackSurfaceManagement/CortexAttackSurfaceManagement_test.py
+++ Packs/CortexAttackSurfaceManagement/Integrations/CortexAttackSurfaceManagement/CortexAttackSurfaceManagement_test.py
@@ -1,9 +1,27 @@
-"""
-Tests module for Cortex Attack Surface Management integration.
-"""
+import pytest
+
+"""Fixtures for the test cases"""
 
 
-def test_format_asm_id_func(requests_mock):
+@pytest.fixture(scope='module')
+def client():
+    from CortexAttackSurfaceManagement import Client
+    client = Client(
+        base_url='https://test.com/api/webapp/public_api/v1',
+        verify=True,
+        headers={
+            "HOST": "test.com",
+            "Authorizatio": "THISISAFAKEKEY",
+            "Content-Type": "application/json"
+        },
+        proxy=False)
+    return client
+
+
+"""Test cases"""
+
+
+def test_format_asm_id_func():
     """Tests format_asm_id helper function.
 
         Given:
@@ -23,7 +41,7 @@ def test_format_asm_id_func(requests_mock):
     assert response == INTERNET_EXPOSURE_POST_FORMAT
 
 
-def test_list_external_service_command(requests_mock):
+def test_list_external_service_command(client, requests_mock):
     """Tests list_external_service_command command function.
 
         Given:
@@ -34,22 +52,12 @@ def test_list_external_service_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, list_external_service_command
+    from CortexAttackSurfaceManagement import list_external_service_command
 
     from test_data.raw_response import EXTERNAL_SERVICES_RESPONSE
     from test_data.expected_results import EXTERNAL_SERVICES_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_external_services/',
                        json=EXTERNAL_SERVICES_RESPONSE)
-
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
 
     args = {
         'domain': 'testdomain.com',
@@ -59,10 +67,10 @@ def test_list_external_service_command(requests_mock):
 
     assert response.outputs == EXTERNAL_SERVICES_RESULTS
     assert response.outputs_prefix == 'ASM.ExternalService'
-    assert response.outputs_key_field == 'service_id'
+    assert response.outputs_key_field == "service_id"
 
 
-def test_get_external_service_command(requests_mock):
+def test_get_external_service_command(client, requests_mock):
     """Tests get_external_service_command command function.
 
         Given:
@@ -73,22 +81,12 @@ def test_get_external_service_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, get_external_service_command
+    from CortexAttackSurfaceManagement import get_external_service_command
 
     from test_data.raw_response import EXTERNAL_SERVICE_RESPONSE
     from test_data.expected_results import EXTERNAL_SERVICE_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_external_service',
                        json=EXTERNAL_SERVICE_RESPONSE)
-
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
 
     args = {
         'service_id': '94232f8a-f001-3292-aa65-63fa9d981427'
@@ -101,7 +99,7 @@ def test_get_external_service_command(requests_mock):
     assert response.outputs_key_field == 'service_id'
 
 
-def test_list_external_ip_address_range_command(requests_mock):
+def test_list_external_ip_address_range_command(client, requests_mock):
     """Tests list_external_ip_address_range_command function.
 
         Given:
@@ -112,22 +110,13 @@ def test_list_external_ip_address_range_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, list_external_ip_address_range_command
+    from CortexAttackSurfaceManagement import list_external_ip_address_range_command
 
     from test_data.raw_response import EXTERNAL_RANGES_RESPONSE
     from test_data.expected_results import EXTERNAL_RANGES_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_external_ip_address_ranges/',
                        json=EXTERNAL_RANGES_RESPONSE)
 
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
     args = {}
 
     response = list_external_ip_address_range_command(client, args)
@@ -137,7 +126,7 @@ def test_list_external_ip_address_range_command(requests_mock):
     assert response.outputs_key_field == 'range_id'
 
 
-def test_get_external_ip_address_range_command(requests_mock):
+def test_get_external_ip_address_range_command(client, requests_mock):
     """Tests get_external_ip_address_range_command function.
 
         Given:
@@ -148,22 +137,13 @@ def test_get_external_ip_address_range_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, get_external_ip_address_range_command
+    from CortexAttackSurfaceManagement import get_external_ip_address_range_command
 
     from test_data.raw_response import EXTERNAL_RANGE_RESPONSE
     from test_data.expected_results import EXTERNAL_RANGE_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_external_ip_address_range/',
                        json=EXTERNAL_RANGE_RESPONSE)
 
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
     args = {
         'range_id': '1093124c-ce26-33ba-8fb8-937fecb4c7b6'
     }
@@ -175,7 +155,7 @@ def test_get_external_ip_address_range_command(requests_mock):
     assert response.outputs_key_field == 'range_id'
 
 
-def test_list_asset_internet_exposure_command(requests_mock):
+def test_list_asset_internet_exposure_command(client, requests_mock):
     """Tests list_asset_internet_exposure_command function.
 
         Given:
@@ -186,22 +166,13 @@ def test_list_asset_internet_exposure_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, list_asset_internet_exposure_command
+    from CortexAttackSurfaceManagement import list_asset_internet_exposure_command
 
     from test_data.raw_response import EXTERNAL_EXPOSURES_RESPONSE
     from test_data.expected_results import EXTERNAL_EXPOSURES_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_assets_internet_exposure/',
                        json=EXTERNAL_EXPOSURES_RESPONSE)
 
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
     args = {
         'name': 'testdomain.com'
     }
@@ -213,7 +184,7 @@ def test_list_asset_internet_exposure_command(requests_mock):
     assert response.outputs_key_field == 'asm_ids'
 
 
-def test_get_asset_internet_exposure_command(requests_mock):
+def test_get_asset_internet_exposure_command(client, requests_mock):
     """Tests get_asset_internet_exposure_command function.
 
         Given:
@@ -224,22 +195,13 @@ def test_get_asset_internet_exposure_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, get_asset_internet_exposure_command
+    from CortexAttackSurfaceManagement import get_asset_internet_exposure_command
 
     from test_data.raw_response import EXTERNAL_EXPOSURE_RESPONSE
     from test_data.expected_results import EXTERNAL_EXPOSURE_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/assets/get_asset_internet_exposure/',
                        json=EXTERNAL_EXPOSURE_RESPONSE)
 
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
     args = {
         'asm_id': 'testdomain.com'
     }
@@ -251,7 +213,7 @@ def test_get_asset_internet_exposure_command(requests_mock):
     assert response.outputs_key_field == 'asm_ids'
 
 
-def test_list_remediation_rule_command(requests_mock):
+def test_list_remediation_rule_command(client, requests_mock):
     """Tests list_remediation_rule_command function.
 
         Given:
@@ -262,22 +224,13 @@ def test_list_remediation_rule_command(requests_mock):
         Then:
             - Checks the output of the command function with the expected output.
     """
-    from CortexAttackSurfaceManagement import Client, list_remediation_rule_command
+    from CortexAttackSurfaceManagement import list_remediation_rule_command
 
     from test_data.raw_response import REMEDIATION_RULES_RESPONSE
     from test_data.expected_results import REMEDIATION_RULES_RESULTS
     requests_mock.post('https://test.com/api/webapp/public_api/v1/xpanse_remediation_rules/rules/',
                        json=REMEDIATION_RULES_RESPONSE)
 
-    client = Client(
-        base_url='https://test.com/api/webapp/public_api/v1',
-        verify=True,
-        headers={
-            "HOST": "test.com",
-            "Authorizatio": "THISISAFAKEKEY",
-            "Content-Type": "application/json"
-        },
-        proxy=False)
     args = {
         'asm_rule_id': 'RdpServer'
     }
