1feaba5f279e896da6cab2d24594dea06c693489
Enes Özdemir <49711791+ennozdd@users.noreply.github.com>
2023-04-13 16:40:15 +0300

    Update Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
    Co-authored-by: samuelFain <65926551+samuelFain@users.noreply.github.com>

--- Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
+++ Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
@@ -1074,9 +1074,7 @@ def fetch_incidents(client, first_fetch_time, integration_instance, last_run: di
 def get_endpoints_by_status_command(client: Client, args: Dict) -> CommandResults:
     status = args.get('status')
 
-    if ',' in status:  # type: ignore
-        status = status.split(',')  # type: ignore
-
+   status = argToList(status)
     last_seen_gte = arg_to_timestamp(
         arg=args.get('last_seen_gte'),
         arg_name='last_seen_gte'
