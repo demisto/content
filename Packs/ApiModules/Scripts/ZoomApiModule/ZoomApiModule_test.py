cbbbd38bce96b3c9615f4e8f015fe118f35d593a
Yehuda <yrosenberg@paloaltonetworks.com>
2023-04-17 12:35:42 +0300

    reorgenize

--- Packs/ApiModules/Scripts/ZoomApiModule/ZoomApiModule_test.py
+++ Packs/ApiModules/Scripts/ZoomApiModule/ZoomApiModule_test.py
@@ -146,44 +146,6 @@ def test_http_request___when_raising_invalid_token_message(mocker):
     assert generate_token_mock.called
 
 
-def test_check_authentication_type_parameters_with_extra_jwt_member(mocker):
-    """
-        Given -
-           client
-        When -
-            creating a client with an extra authentication type argument
-        Then -
-            Validate that the error wil raise as excepted
-    """
-    import ZoomApiModule
-    with pytest.raises(DemistoException) as e:
-        ZoomApiModule.check_authentication_type_parameters(account_id="mockaccount",
-                                                           client_id="mockclient", client_secret="mocksecret",
-                                                           api_key="blabla", api_secret="")
-    assert e.value.message == """Too many fields were filled.
-You should fill the Account ID, Client ID, and Client Secret fields (OAuth),
-OR the API Key and API Secret fields (JWT - Deprecated)."""
-
-
-def test_check_authentication_type_parameters__with_extra_AOuth_member():
-    """
-        Given -
-
-        When -
-            creating a client with an extra authentication type argument
-        Then -
-            Validate that the error wil raise as excepted
-    """
-    import ZoomApiModule
-    with pytest.raises(DemistoException) as e:
-        ZoomApiModule.check_authentication_type_parameters(account_id="",
-                                                           client_id="", client_secret="mocksecret",
-                                                           api_key="blabla", api_secret="ertert")
-    assert e.value.message == """Too many fields were filled.
-You should fill the Account ID, Client ID, and Client Secret fields (OAuth),
-OR the API Key and API Secret fields (JWT - Deprecated)."""
-
-
 @freeze_time("1988-03-03T11:00:00")
 def test_get_jwt_token__encoding_format_check():
     """
