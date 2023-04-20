5a96a08f835a1fcc38048aa421dd4ef6643114ad
RotemAmit <ramit@paloaltonetworks.com>
2023-04-20 19:48:51 +0300

    Added a flag for sco indicators, and updated the creation process of scos. Updated the sdos IDs. Updated the docker image. Added release notes

--- Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
+++ Packs/CommonScripts/Scripts/StixCreator/StixCreator.py
@@ -6,7 +6,7 @@ from CommonServerPython import *  # noqa: F401
 
 ''' IMPORTS '''
 import json
-
+import uuid
 from stix2 import Bundle, ExternalReference, Indicator, Vulnerability
 from stix2 import AttackPattern, Campaign, Malware, Infrastructure, IntrusionSet, Report, ThreatActor
 from stix2 import Tool, CourseOfAction
@@ -23,9 +23,9 @@ SCOs: dict[str, str] = {  # pragma: no cover
     "ipv6cidr": "ipv6-addr:value",
     "url": "url:value",
     "email": "email-message:sender_ref.value",
-    "username": "user-account:account_login",
+    "account": "user-account:account_login",
     "domain": "domain-name:value",
-    "hostname": "domain-name:value",
+    "host": "domain-name:value",
     "registry key": "windows-registry-key:key"
 }
 
@@ -40,6 +40,38 @@ SDOs: dict[str, Callable] = {  # pragma: no cover
     "threat actor": ThreatActor,
     "cve": Vulnerability,
     "course of action": CourseOfAction
+}
+
+SCO_DET_ID_NAMESPACE = uuid.UUID('00abedb4-aa42-466c-9c01-fed23315a9b7')
+PAWN_UUID = uuid.uuid5(uuid.NAMESPACE_URL, 'https://www.paloaltonetworks.com')
+
+XSOAR_TYPES_TO_STIX_SCO = {
+    FeedIndicatorType.CIDR: 'ipv4-addr',
+    FeedIndicatorType.DomainGlob: 'domain-name',
+    FeedIndicatorType.IPv6: 'ipv6-addr',
+    FeedIndicatorType.IPv6CIDR: 'ipv6-addr',
+    FeedIndicatorType.Account: 'user-account',
+    FeedIndicatorType.Domain: 'domain-name',
+    FeedIndicatorType.Email: 'email-addr',
+    FeedIndicatorType.IP: 'ipv4-addr',
+    FeedIndicatorType.Registry: 'windows-registry-key',
+    FeedIndicatorType.File: 'file',
+    FeedIndicatorType.URL: 'url',
+    FeedIndicatorType.Software: 'software',
+    FeedIndicatorType.AS: 'asn',
+}
+
+XSOAR_TYPES_TO_STIX_SDO = {
+    ThreatIntel.ObjectsNames.ATTACK_PATTERN: 'attack-pattern',
+    ThreatIntel.ObjectsNames.CAMPAIGN: 'campaign',
+    ThreatIntel.ObjectsNames.COURSE_OF_ACTION: 'course-of-action',
+    ThreatIntel.ObjectsNames.INFRASTRUCTURE: 'infrastructure',
+    ThreatIntel.ObjectsNames.INTRUSION_SET: 'instruction-set',
+    ThreatIntel.ObjectsNames.REPORT: 'report',
+    ThreatIntel.ObjectsNames.THREAT_ACTOR: 'threat-actor',
+    ThreatIntel.ObjectsNames.TOOL: 'tool',
+    ThreatIntel.ObjectsNames.MALWARE: 'malware',
+    FeedIndicatorType.CVE: 'vulnerability',
 }
 
 
@@ -68,10 +100,58 @@ def guess_indicator_type(type_: str, val: str) -> str:
     return (auto_detect_indicator_type(val) or type_).lower()
 
 
+def create_sco_stix_uuid(xsoar_indicator: dict, stix_type: Optional[str], value: str) -> str:
+    """
+    Create uuid for sco objects.
+    """
+    if stixid := xsoar_indicator.get('CustomFields', {}).get('stixid'):
+        return stixid
+    if stix_type == 'user-account':
+        account_type = xsoar_indicator.get('CustomFields', {}).get('accounttype')
+        user_id = xsoar_indicator.get('CustomFields', {}).get('userid')
+        unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE,
+                               f'{{"account_login":"{value}","account_type":"{account_type}","user_id":"{user_id}"}}')
+    elif stix_type == 'windows-registry-key':
+        unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"key":"{value}"}}')
+    elif stix_type == 'file':
+        if 'md5' == get_hash_type(value):
+            unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"hashes":{{"MD5":"{value}"}}}}')
+        elif 'sha1' == get_hash_type(value):
+            unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"hashes":{{"SHA-1":"{value}"}}}}')
+        elif 'sha256' == get_hash_type(value):
+            unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"hashes":{{"SHA-256":"{value}"}}}}')
+        elif 'sha512' == get_hash_type(value):
+            unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"hashes":{{"SHA-512":"{value}"}}}}')
+        else:
+            unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"value":"{value}"}}')
+    else:
+        unique_id = uuid.uuid5(SCO_DET_ID_NAMESPACE, f'{{"value":"{value}"}}')
+
+    return f'{stix_type}--{unique_id}'
+
+
+def create_sdo_stix_uuid(xsoar_indicator: dict, stix_type: Optional[str], value: str) -> str:
+    """
+    Create uuid for sdo objects.
+    """
+    if stixid := xsoar_indicator.get('CustomFields', {}).get('stixid'):
+        return stixid
+    if stix_type == 'attack-pattern':
+        if mitre_id := xsoar_indicator.get('CustomFields', {}).get('mitreid'):
+            unique_id = uuid.uuid5(PAWN_UUID, f'{stix_type}:{mitre_id}')
+        else:
+            unique_id = uuid.uuid5(PAWN_UUID, f'{stix_type}:{value}')
+    else:
+        unique_id = uuid.uuid5(PAWN_UUID, f'{stix_type}:{value}')
+
+    return f'{stix_type}--{unique_id}'
+
+
 def main():
 
     user_args = demisto.args().get('indicators', 'Unknown')
     doubleBackslash = demisto.args().get('doubleBackslash', True)
+    is_sco = argToBoolean(demisto.args().get('sco_flag', False))
     all_args = {}
 
     if isinstance(user_args, dict):
@@ -83,6 +163,7 @@ def main():
         except:     # noqa: E722
             return_error('indicators argument is invalid json object')
 
+    demisto.debug(f'StixCreator {demisto.args()=}\n{user_args=}\n{all_args=}')
     indicators = []
 
     for indicator_fields in all_args:
@@ -95,75 +176,88 @@ def main():
         else:
             value = all_args[indicator_fields].get('value', '')
 
-        demisto_score = all_args[indicator_fields].get('score', '').lower()
-
-        if demisto_score in ["bad", "malicious"]:
-            kwargs["score"] = "High"
-
-        elif demisto_score == "suspicious":
-            kwargs["score"] = "Medium"
-
-        elif demisto_score in ["good", "benign"]:
-            kwargs["score"] = "None"
-
-        else:
-            kwargs["score"] = "Not Specified"
-
-        kwargs["created"] = dateparser.parse(all_args[indicator_fields].get('timestamp', ''))
-        kwargs["modified"] = dateparser.parse(all_args[indicator_fields].get('lastSeen', f'{kwargs["created"]}'))
-        kwargs["id"] = all_args[indicator_fields].get('stixid', '')
-        kwargs["labels"] = [demisto_indicator_type.lower()]
-        kwargs["description"] = all_args[indicator_fields].get('description', '')
-
-        kwargs = {k: v for k, v in kwargs.items() if v}  # Removing keys with empty strings
-
-        try:
-            indicator_type = demisto_indicator_type.lower().replace("-", "")
-            if indicator_type == 'file':
-                indicator_type = hash_type(value)
-            if indicator_type not in SCOs and indicator_type not in SDOs:
-                indicator_type = guess_indicator_type(indicator_type, value)
-            indicator = Indicator(pattern=f"[{SCOs[indicator_type]} = '{value}']",
-                                  pattern_type='stix',
-                                  **kwargs)
-
+        if demisto_indicator_type in XSOAR_TYPES_TO_STIX_SCO and is_sco:
+            stix_type = XSOAR_TYPES_TO_STIX_SCO.get(demisto_indicator_type)
+            stix_id = create_sco_stix_uuid(all_args[indicator_fields], stix_type, value)
+            indicator = {
+                "type": stix_type,
+                "spec_version": "2.1",
+                "value": value,
+                "id": stix_id
+            }
             indicators.append(indicator)
+        else:
+            demisto_score = all_args[indicator_fields].get('score', '').lower()
 
-        except KeyError:
+            if demisto_score in ["bad", "malicious"]:
+                kwargs["score"] = "High"
 
-            demisto.debug(f"{demisto_indicator_type} isn't an SCO checking other IOC types")
+            elif demisto_score == "suspicious":
+                kwargs["score"] = "Medium"
+
+            elif demisto_score in ["good", "benign"]:
+                kwargs["score"] = "None"
+
+            else:
+                kwargs["score"] = "Not Specified"
+
+            stix_type = XSOAR_TYPES_TO_STIX_SDO.get(demisto_indicator_type, 'indicator')
+            stix_id = create_sdo_stix_uuid(all_args[indicator_fields], stix_type, value)
+            kwargs["id"] = stix_id
+
+            kwargs["created"] = dateparser.parse(all_args[indicator_fields].get('timestamp', ''))
+            kwargs["modified"] = dateparser.parse(all_args[indicator_fields].get('lastSeen', f'{kwargs["created"]}'))
+            kwargs["labels"] = [demisto_indicator_type.lower()]
+            kwargs["description"] = all_args[indicator_fields].get('description', '')
+
+            kwargs = {k: v for k, v in kwargs.items() if v}  # Removing keys with empty strings
 
             try:
-                indicator_type = demisto_indicator_type.lower()
-
-                if indicator_type == 'cve':
-                    kwargs["external_references"] = [ExternalReference(source_name="cve", external_id=value)]
-
-                elif indicator_type == "attack pattern":
-                    try:
-                        mitreid = all_args[indicator_fields].get('mitreid', '')
-                        if mitreid:
-                            kwargs["external_references"] = [ExternalReference(source_name="mitre", external_id=mitreid)]
-
-                    except KeyError:
-                        pass
-
-                elif indicator_type == 'malware':
-
-                    kwargs['is_family'] = argToBoolean(all_args[indicator_fields].get('ismalwarefamily', '').lower())
-
-                indicator = SDOs[indicator_type](
-                    name=value,
-                    **kwargs
-                )
-
+                indicator_type = demisto_indicator_type.lower().replace("-", "")
+                if indicator_type == 'file':
+                    indicator_type = hash_type(value)
+                if indicator_type not in SCOs and indicator_type not in SDOs:
+                    indicator_type = guess_indicator_type(indicator_type, value)
+                indicator = Indicator(pattern=f"[{SCOs[indicator_type]} = '{value}']",
+                                      pattern_type='stix',
+                                      **kwargs)
                 indicators.append(indicator)
 
-            except (KeyError, TypeError) as e:
-                demisto.info(
-                    "Indicator type: {}, with the value: {} is not STIX compatible".format(demisto_indicator_type, value))
-                demisto.info("Export failure excpetion: {}".format(e))
-                continue
+            except KeyError:
+
+                demisto.debug(f"{demisto_indicator_type} isn't an SCO checking other IOC types")
+
+                try:
+                    indicator_type = demisto_indicator_type.lower()
+
+                    if indicator_type == 'cve':
+                        kwargs["external_references"] = [ExternalReference(source_name="cve", external_id=value)]
+
+                    elif indicator_type == "attack pattern":
+                        try:
+                            mitreid = all_args[indicator_fields].get('mitreid', '')
+                            if mitreid:
+                                kwargs["external_references"] = [ExternalReference(source_name="mitre", external_id=mitreid)]
+
+                        except KeyError:
+                            pass
+
+                    elif indicator_type == 'malware':
+
+                        kwargs['is_family'] = argToBoolean(all_args[indicator_fields].get('ismalwarefamily', 'False').lower())
+
+                    indicator = SDOs[indicator_type](
+                        name=value,
+                        **kwargs
+                    )
+
+                    indicators.append(indicator)
+
+                except (KeyError, TypeError) as e:
+                    demisto.info(
+                        "Indicator type: {}, with the value: {} is not STIX compatible".format(demisto_indicator_type, value))
+                    demisto.info("Export failure excpetion: {}".format(e))
+                    continue
 
     if len(indicators) > 1:
         bundle = Bundle(indicators, allow_custom=True)
@@ -175,12 +269,14 @@ def main():
                               raw_response=str(bundle)))
 
     elif len(indicators) == 1:
+        bundle = Bundle(indicators, allow_custom=True)
+        bundle_obj = bundle.get('objects', [])[0]
         context = {
-            'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(indicators[0]))
+            'StixExportedIndicators(val.pattern && val.pattern == obj.pattern)': json.loads(str(bundle_obj))
         }
         res = (CommandResults(readable_output="",
                               outputs=context,
-                              raw_response=str(indicators[0])))
+                              raw_response=str(bundle_obj)))
     else:
         context = {
             'StixExportedIndicators': {}
--- Packs/CommonScripts/Scripts/StixCreator/StixCreator_test.py
+++ Packs/CommonScripts/Scripts/StixCreator/StixCreator_test.py
@@ -1,7 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 import pytest
-from StixCreator import main, guess_indicator_type
+from StixCreator import main, guess_indicator_type, create_sco_stix_uuid, create_sdo_stix_uuid
 
 FILE_INDICATOR = \
     {
@@ -49,13 +49,13 @@ DOMAIN_INDICATORS = \
         'indicators':
             {
                 '0': {'expirationStatus': 'active', 'firstSeen': '2022-07-31T13:24:44Z',
-                      'indicator_type': 'cve',
+                      'indicator_type': 'CVE',
                       'lastSeen': '2022-07-31T13:24:44Z', 'score': 'Unknown',
                       'timestamp': '2022-07-31T13:24:44Z',
                       'value': 'test.com'
                       },
                 '1': {'expirationStatus': 'active', 'firstSeen': '2022-07-31T13:24:40Z',
-                      'indicator_type': 'attack pattern',
+                      'indicator_type': 'Attack Pattern',
                       'lastSeen': '2022-07-31T13:24:40Z', 'score': 'suspicious',
                       'timestamp': '2022-07-31T13:24:40Z',
                       'value': 'bad.com'
@@ -63,11 +63,43 @@ DOMAIN_INDICATORS = \
             }
     }
 
+IP_INDICATOR_SCO = {  # checking the new logic
+    "indicators": {
+        "0": {
+            "expirationStatus": "active",
+            "firstSeen": "2023-04-18T12:17:38+03:00",
+            "indicator_type": "IP",
+            "lastSeen": "2023-04-18T12:17:38+03:00",
+            "score": "Unknown",
+            "timestamp": "2023-04-18T12:17:38+03:00",
+            "value": "8.8.8.8",
+        }
+    },
+    "sco_flag": "true",
+}
+
+IP_INDICATOR_SDO = {  # checking bc
+    "indicators": {
+        "0": {
+            "expirationStatus": "active",
+            "firstSeen": "2023-04-18T12:17:38+03:00",
+            "indicator_type": "IP",
+            "lastSeen": "2023-04-18T12:17:38+03:00",
+            "score": "Unknown",
+            "timestamp": "2023-04-18T12:17:38+03:00",
+            "value": "8.8.8.8",
+        }
+    },
+    "sco_flag": "false",
+}
+
 
 @pytest.mark.parametrize('indicators, stix_type', [(DOMAIN_INDICATORS, 'bundle'),
                                                    (FILE_INDICATOR, 'indicator'),
                                                    (MALWARE_INDICATOR, 'malware'),
-                                                   (ATTACK_PATTERN_INDICATOR, 'attack-pattern')])
+                                                   (ATTACK_PATTERN_INDICATOR, 'attack-pattern'),
+                                                   (IP_INDICATOR_SCO, 'ipv4-addr'),
+                                                   (IP_INDICATOR_SDO, 'indicator')])
 def test_stixCreator_with_indicators(mocker, indicators, stix_type):
     mocker.patch.object(demisto, 'args', return_value=indicators)
     mocker.patch.object(demisto, 'results')
@@ -85,3 +117,102 @@ def test_stixCreator_with_indicators(mocker, indicators, stix_type):
 def test_guess_indicator_type(k, v, exp):
     a = guess_indicator_type(k, v)
     assert a == exp
+
+
+xsoar_indicator_1 = {'expirationStatus': 'active',
+                     'firstSeen': '2023-04-19T17:43:07+03:00',
+                     'indicator_type': 'Account',
+                     'lastSeen': '2023-04-19T17:43:07+03:00',
+                     'score': 'Unknown',
+                     'timestamp': '2023-04-19T17:43:07+03:00',
+                     'value': 'test@test.com'}
+stix_type_1 = "user-account"
+value_1 = 'test@test.com'
+expected_stix_id_1 = "user-account--783b9e67-d7b0-58f3-b566-58ac7881a3bc"
+
+xsoar_indicator_2 = {'expirationStatus': 'active',
+                     'firstSeen': '2023-04-20T10:20:04+03:00',
+                     'indicator_type': 'File',
+                     'lastSeen': '2023-04-20T10:20:04+03:00',
+                     'score': 'Unknown', 'sourceBrands': 'VirusTotal',
+                     'sourceInstances': 'VirusTotal',
+                     'timestamp': '2023-04-20T10:20:04+03:00',
+                     'value': '701393b3b8e6ae6e70effcda7598a8cf92d0adb1aaeb5aa91c73004519644801'}
+stix_type_2 = "file"
+value_2 = '701393b3b8e6ae6e70effcda7598a8cf92d0adb1aaeb5aa91c73004519644801'
+expected_stix_id_2 = "file--3e26aab3-dfc3-57c5-8fe2-45cfde8fe7c8"
+
+xsoar_indicator_3 = {'expirationStatus': 'active',
+                     'firstSeen': '2023-04-18T12:17:38+03:00',
+                     'indicator_type': 'IP',
+                     'lastSeen': '2023-04-18T12:17:38+03:00',
+                     'score': 'Unknown',
+                     'timestamp': '2023-04-18T12:17:38+03:00',
+                     'value': '8.8.8.8'}
+stix_type_3 = "ipv4-addr"
+value_3 = '8.8.8.8'
+expected_stix_id_3 = "ipv4-addr--2f689bf9-0ff2-545f-aa61-e495eb8cecc7"
+
+test_test_create_sco_stix_uuid_params = [(xsoar_indicator_1, stix_type_1, value_1, expected_stix_id_1),
+                                         (xsoar_indicator_2, stix_type_2, value_2, expected_stix_id_2),
+                                         (xsoar_indicator_3, stix_type_3, value_3, expected_stix_id_3)]
+
+
+@pytest.mark.parametrize('xsoar_indicator, stix_type, value, expected_stix_id', test_test_create_sco_stix_uuid_params)
+def test_create_sco_stix_uuid(xsoar_indicator, stix_type, value, expected_stix_id):
+    """
+    Given:
+        - An indicator from XSOAR, it's stix type and the value of the indicator.
+    When:
+        - Creating a SCO inducator and calling create_sco_stix_uuid
+    Then:
+     - Return the indicator id.
+    """
+    stix_id = create_sco_stix_uuid(xsoar_indicator, stix_type, value)
+    assert expected_stix_id == stix_id
+
+
+sdo_xsoar_indicator_1 = {
+    "expirationStatus": "active",
+    "firstSeen": "2023-04-19T13:05:01+03:00",
+    "indicator_type": "Attack Pattern",
+    "lastSeen": "2023-04-19T13:05:01+03:00",
+    "score": "Unknown",
+    "timestamp": "2023-04-19T13:05:01+03:00",
+    "value": "T111",
+}
+sdo_stix_type_1 = 'attack-pattern'
+sdo_value_1 = 'T111'
+sdo_expected_stix_id_1 = 'attack-pattern--116d410f-50f9-5f0d-b677-2a9b95812a3e'
+
+sdo_xsoar_indicator_2 = {
+    "expirationStatus": "active",
+    "firstSeen": "2023-04-20T17:20:10+03:00",
+    "indicator_type": "Malware",
+    "lastSeen": "2023-04-20T17:20:10+03:00",
+    "score": "Unknown",
+    "timestamp": "2023-04-20T17:20:10+03:00",
+    "value": "bad malware",
+    "ismalwarefamily": "True",
+}
+sdo_stix_type_2 = 'malware'
+sdo_value_2 = 'bad malware'
+sdo_expected_stix_id_2 = 'malware--bddcf01f-9fd0-5107-a013-4b174285babc'
+
+test_test_create_sdo_stix_uuid_params = [(sdo_xsoar_indicator_1, sdo_stix_type_1, sdo_value_1, sdo_expected_stix_id_1),
+                                         (sdo_xsoar_indicator_2, sdo_stix_type_2, sdo_value_2, sdo_expected_stix_id_2)]
+
+
+@pytest.mark.parametrize('xsoar_indicator, stix_type, value, expected_stix_id', test_test_create_sdo_stix_uuid_params)
+def test_create_sdo_stix_uuid(xsoar_indicator, stix_type, value, expected_stix_id):
+    """
+    Given:
+        - An indicator from XSOAR, it's stix type and the value of the indicator.
+    When:
+        - Creating a SDO inducator and calling create_sdo_stix_uuid
+    Then:
+     - Return the indicator id.
+    """
+
+    stix_id = create_sdo_stix_uuid(xsoar_indicator, stix_type, value)
+    assert expected_stix_id == stix_id
