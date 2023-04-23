03ec463e584bce33743904bfcae57ad9aa78956a
spearmin10 <minoue@paloaltonetworks.com>
2023-04-23 21:23:37 +0900

    Added SortBy, MakePair and DedupBy.

---
+++ Packs/FiltersAndTransformers/Scripts/DedupBy/DedupBy.py
@@ -0,0 +1,85 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+import json
+from typing import Any, List, Union
+
+
+def demisto_get(obj: Any, path: Any) -> Any:
+    """
+    demisto.get(), this supports a syntax of path escaped with backslash.
+    """
+    def split_context_path(path: str) -> List[str]:
+        nodes = []
+        node = []
+        itr = iter(path)
+        for c in itr:
+            if c == '\\':
+                try:
+                    node.append(next(itr))
+                except StopIteration:
+                    node.append('\\')
+            elif c == '.':
+                nodes.append(''.join(node))
+                node = []
+            else:
+                node.append(c)
+        nodes.append(''.join(node))
+        return nodes
+
+    if not isinstance(obj, dict):
+        return None
+
+    for part in split_context_path(path):
+        if obj and part in obj:
+            obj = obj[part]
+        else:
+            return None
+    return obj
+
+
+class Key(object):
+    def __init__(self, value: Any, path: Optional[str] = None) -> None:
+        self.__value = value if path is None else demisto_get(value, path)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Key):
+            return False
+        elif type(self.__value) != type(other.__value):  # noqa: E721
+            return False
+        elif isinstance(self.__value, (bool, int, float, str)) or self.__value is None:
+            return self.__value == other.__value
+        else:
+            return json.dumps(self.__value) == json.dumps(other.__value)
+
+    def __hash__(self) -> int:
+        if isinstance(self.__value, (bool, int, float, str)) or self.__value is None:
+            return hash((type(self.__value), self.__value))
+        else:
+            return hash((type(self.__value), json.dumps(self.__value)))
+
+
+def main():
+    try:
+        args = assign_params(**demisto.args())
+        if value := args.get('value', []):
+            temp = {}
+            if paths := argToList(args.get('keys')):
+                for v in value:
+                    k: Union[tuple, Key] = tuple([Key(v, path) for path in paths])
+                    if k not in temp:
+                        temp[k] = v
+            else:
+                for v in value:
+                    k = Key(v)  # noqa: F812
+                    if k not in temp:
+                        temp[k] = v
+            value = list(temp.values())
+
+        return_results(value)
+    except Exception as err:
+        # Don't return an error by return_error() as this is transformer.
+        raise DemistoException(str(err))
+
+
+if __name__ in ('__builtin__', 'builtins', '__main__'):
+    main()
---
+++ Packs/FiltersAndTransformers/Scripts/SortBy/SortBy.py
@@ -0,0 +1,88 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+import json
+from typing import Any, List, Tuple
+
+
+def demisto_get(obj: Any, path: Any) -> Any:
+    """
+    demisto.get(), this supports a syntax of path escaped with backslash.
+    """
+    def split_context_path(path: str) -> List[str]:
+        nodes = []
+        node = []
+        itr = iter(path)
+        for c in itr:
+            if c == '\\':
+                try:
+                    node.append(next(itr))
+                except StopIteration:
+                    node.append('\\')
+            elif c == '.':
+                nodes.append(''.join(node))
+                node = []
+            else:
+                node.append(c)
+        nodes.append(''.join(node))
+        return nodes
+
+    if not isinstance(obj, dict):
+        return None
+
+    for part in split_context_path(path):
+        if obj and part in obj:
+            obj = obj[part]
+        else:
+            return None
+    return obj
+
+
+class Key:
+    def __init__(self, value: Any, path: Optional[str]) -> None:
+        self.__value = value if path is None else demisto_get(value, path)
+
+    def __get_type_order(self) -> int:
+        if self.__value is None:
+            return 0
+        elif isinstance(self.__value, bool):
+            return 1
+        elif isinstance(self.__value, (int, float)):
+            return 2
+        elif isinstance(self.__value, str):
+            return 3
+        else:
+            return 4
+
+    def __get_key(self) -> Any:
+        if self.__value is None:
+            return 0
+        elif isinstance(self.__value, (bool, int, float, str)):
+            return self.__value
+        else:
+            return json.dumps(self.__value)
+
+    def get(self) -> Tuple[int, Any]:
+        return self.__get_type_order(), self.__get_key()
+
+
+def main():
+    try:
+        args = assign_params(**demisto.args())
+        if value := args.get('value', []):
+            descending_keys = argToList(args.get('descending_keys'))
+            if paths := argToList(args.get('keys')):
+                for path in reversed(paths):
+                    value.sort(key=lambda x: Key(x, path).get(),
+                               reverse=path in descending_keys)
+            else:
+                descending = len(descending_keys) == 1 and descending_keys[0] == '*'
+                value.sort(key=lambda x: Key(x, None).get(), reverse=descending)
+
+        return_results(value)
+    except Exception as err:
+        # Don't return an error by return_error() as this is transformer.
+        raise DemistoException(str(err))
+
+
+if __name__ in ('__builtin__', 'builtins', '__main__'):
+    main()
