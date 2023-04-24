05581c92dde1c2a9684d24788a1736da59b93239
ilappe <ilappe@paloaltonetworks.com>
2023-04-24 13:32:20 +0300

    disable secrets

--- Packs/CommonScripts/Scripts/ExtractDomainAndFQDNFromUrlAndEmail/ExtractDomainAndFQDNFromUrlAndEmail_test.py
+++ Packs/CommonScripts/Scripts/ExtractDomainAndFQDNFromUrlAndEmail/ExtractDomainAndFQDNFromUrlAndEmail_test.py
@@ -56,7 +56,7 @@ import pytest
     ('/evil3.com', 'evil3.com'),  # noqa: E501 disable-secrets-detection
     ('<br>kasai.qlmsourcing.com', 'kasai.qlmsourcing.com'),  # disable-secrets-detection
     ('test.com@', ''),  # disable-secrets-detection
-    ('user.name%40subdomain.domain.com', 'subdomain.domain.com')
+    ('user.name%40subdomain.domain.com', 'subdomain.domain.com'),  # disable-secrets-detection
 ])  # noqa: E124
 def test_extract_fqdn_or_domain(input, fqdn):
     extracted_fqdn = extract_fqdn(input)
