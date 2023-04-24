dee325d81d573e70040d6a24fc64aee6b6595b6c
ilan <ierukhimovic@paloaltonetworks.com>
2023-04-24 11:56:06 +0300

    content test conf extract

---
+++ Tests/scripts/extract_content_test_conf.py
@@ -0,0 +1,34 @@
+
+
+import argparse
+from pathlib import Path
+import logging
+import shutil
+import slack_sdk
+
+def extract(content_path: Path, content_test_conf_path: Path, slack_token: str):
+    missing_content_packs = []
+    content_packs = set((content_path / "Packs").iterdir())
+    for pack in (content_test_conf_path / "content" / "Packs").iterdir():
+        if pack.name not in content_packs:
+            missing_content_packs.append(pack.name)
+            logging.warning(f"Pack {pack.name} is in content-test-conf but not in content")
+            continue
+        shutil.copytree(pack, content_path / "Packs" / pack.name)
+    if missing_content_packs:
+        slack_sdk.WebClient(token=slack_token).chat_postMessage(
+            channel="dmst-build",
+            text=f"The following packs were merged to content-test-conf, but does not to content: {missing_content_packs}"
+        )
+
+def main():
+    parser = argparse.ArgumentParser(description='Compare content and content-test-conf')
+    parser.add_argument('--content-path', required=True, help='Path to content repo')
+    parser.add_argument('--content-test-conf-path', required=True, help='Path to content-test-conf repo')
+    parser.add_argument('-s', '--slack_token', help='The token for slack', required=True)
+    args = parser.parse_args()
+    
+    extract(Path(args.content_path), Path(args.content_test_conf_path))
+    
+if __name__ == "__main__":
+    main()
