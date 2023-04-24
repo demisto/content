04d2abc3fece3d12f69136973e32eb95e6fbdc82
ilan <ierukhimovic@paloaltonetworks.com>
2023-04-24 16:02:17 +0300

    fix

--- Tests/scripts/extract_content_test_conf.py
+++ Tests/scripts/extract_content_test_conf.py
@@ -10,10 +10,17 @@ from Tests.scripts.utils.log_util import install_logging
 
 install_logging('collect_tests.log', logger=logger)
 
+CONTENT_CHANNEL = 'dmst-build-test'
 
-def extract(content_path: Path, content_test_conf_path: Path, slack_token: str | None = None):
+
+def extract(
+    content_path: Path,
+    content_test_conf_path: Path,
+    slack_token: str | None = None,
+    slack_channel: str = CONTENT_CHANNEL
+):
     missing_content_packs = []
-    content_packs = set((content_path / "Packs").iterdir())
+    content_packs = {pack.name for pack in (content_path / "Packs").iterdir()}
     for pack in (content_test_conf_path / "content" / "Packs").iterdir():
         if pack.name not in content_packs:
             missing_content_packs.append(pack.name)
@@ -23,7 +30,7 @@ def extract(content_path: Path, content_test_conf_path: Path, slack_token: str |
         shutil.copytree(pack, content_path / "Packs" / pack.name)
     if slack_token and missing_content_packs:
         slack_sdk.WebClient(token=slack_token).chat_postMessage(
-            channel="dmst-build",
+            channel=slack_channel,
             text=f"The following packs were merged to content-test-conf, but not to content: {missing_content_packs}"
         )
 
@@ -33,9 +40,12 @@ def main():
     parser.add_argument('--content-path', required=True, help='Path to content repo')
     parser.add_argument('--content-test-conf-path', required=True, help='Path to content-test-conf repo')
     parser.add_argument('-s', '--slack-token', help='The token for slack')
+    parser.add_argument(
+        '-ch', '--slack_channel', help='The slack channel in which to send the notification', default=CONTENT_CHANNEL
+    )
     args = parser.parse_args()
 
-    extract(Path(args.content_path), Path(args.content_test_conf_path), args.slack_token)
+    extract(Path(args.content_path), Path(args.content_test_conf_path), args.slack_token, args.slack_channel)
 
 
 if __name__ == "__main__":
