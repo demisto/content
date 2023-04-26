e5de6af275b80a0436e24bec1dbcf6dcc598078c
Yehuda <yrosenberg@paloaltonetworks.com>
2023-04-27 01:55:12 +0300

    lazy on one func

--- Packs/Zoom/Integrations/Zoom/Zoom.py
+++ Packs/Zoom/Integrations/Zoom/Zoom.py
@@ -130,7 +130,7 @@ class Client(Zoom_Client):
                 'page_number': page_number
             })
 
-    def zoom_fetch_recording(self, method: str, url_suffix: str = None, full_url: str = None,
+    def zoom_fetch_recording(self, method: str, url_suffix: str = None, full_url: str = None,  # pragma no cover
                              stream: bool = False, resp_type: str = 'json'):
         return self.error_handled_http_request(
             method=method,
