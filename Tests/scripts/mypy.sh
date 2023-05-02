a28fb0e5d69db7d251754450b32be19fcf1db3f6
ilan <ierukhimovic@paloaltonetworks.com>
2023-05-02 20:17:59 +0300

    fix mypy.sh

--- Tests/scripts/mypy.sh
+++ Tests/scripts/mypy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# ignored_messages_file contains patterns of mypy messages we want to ignore
+ignored_messages_file="./Tests/scripts/.mypy_ignored_messages"
+
 errors=0
 
 echo "Starting mypy run"
@@ -7,10 +11,17 @@ for dir in $*; do
   if [[ $dir == "." ]]; then
     continue
   fi
+  # check if there are python files in the directory 
+  if [[ $(find $dir -name "*.py" -maxdepth 1 | wc -l) -eq 0 ]]; then
+    continue
+  fi
   echo "Running mypy on $dir"
-  python3 -m mypy $dir 2>&1
-  if [[ $? -ne 0 ]]; then
-    errors=1
+  mypy_out=$(python3 -m mypy $dir 2>&1)
+  if [[ $? -ne 0 && $? -ne 2 ]]; then
+    echo -e "$mypy_out" | sort | uniq | grep -v -f $ignored_messages_file
+    if [[ $? -eq 0 ]]; then
+      errors=1 # some errors founded by grep
+    fi
   fi
 done
 
