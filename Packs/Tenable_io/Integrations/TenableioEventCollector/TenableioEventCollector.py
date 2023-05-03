cd53c63e7d57b73dae04219f2c03df29fe18fa1f
yaakovpraisler <ypreisler@paloaltonetworks.com>
2023-05-03 11:47:10 +0300

    mypy

--- Packs/Tenable_io/Integrations/TenableioEventCollector/TenableioEventCollector.py
+++ Packs/Tenable_io/Integrations/TenableioEventCollector/TenableioEventCollector.py
@@ -2,11 +2,10 @@ import time
 from typing import Dict
 
 import demistomock as demisto
-import requests
 from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
+import urllib3
 
-# Disable insecure warnings
-requests.packages.urllib3.disable_warnings()  # pylint: disable=no-member
+urllib3.disable_warnings()
 
 ''' CONSTANTS '''
 
