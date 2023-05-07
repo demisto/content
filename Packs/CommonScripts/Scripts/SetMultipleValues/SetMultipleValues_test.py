ca5cad72f1b3c296b078581c5408585933aa9752
michal-dagan <mdagan@paloaltonetworks.com>
2023-05-07 12:04:12 +0300

    added case

--- Packs/CommonScripts/Scripts/SetMultipleValues/SetMultipleValues_test.py
+++ Packs/CommonScripts/Scripts/SetMultipleValues/SetMultipleValues_test.py
@@ -1,6 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 from SetMultipleValues import main
+import pytest
 
 
 def test_main(mocker):
@@ -23,7 +24,18 @@ def test_main(mocker):
     assert results_mock.call_args[0][0]['Contents'] == {'Test(true)': {'a': '1', 'b': '2', 'c': '3'}}
 
 
-def test_arrays_as_values(mocker):
+@pytest.mark.parametrize("keys, values, expected_result", [
+    (
+            "array1,array2",
+            '["1.1.1.1","8.8.8.8"],["test@gmail.com", "google.com"]',
+            {'array1': ['1.1.1.1', '8.8.8.8'], 'array2': ['test@gmail.com', 'google.com']}
+    ),
+    (
+            "array,val",
+            '["1.1.1.1","8.8.8.8"], "test_val"',
+            {'array': ['1.1.1.1', '8.8.8.8'], 'val': "test_val"}),
+])
+def test_arrays_as_values(mocker, keys, values, expected_result):
     """
     Given:
         -  The script args (values = string contains arrays)
@@ -34,11 +46,10 @@ def test_arrays_as_values(mocker):
     """
     results_mock = mocker.patch.object(demisto, 'results')
     args = {
-        'keys': 'array1,array2',
-        'values': '["1.1.1.1","8.8.8.8"],["test@gmail.com", "google.com"]',
+        'keys': keys,
+        'values': values,
         'parent': 'Test'
     }
     mocker.patch.object(demisto, 'args', return_value=args)
     main()
-    assert results_mock.call_args[0][0]['Contents'] == {'Test(true)': {'array1': ['1.1.1.1', '8.8.8.8'],
-                                                                       'array2': ['test@gmail.com', 'google.com']}}
+    assert results_mock.call_args[0][0]['Contents'] == {'Test(true)': expected_result}
