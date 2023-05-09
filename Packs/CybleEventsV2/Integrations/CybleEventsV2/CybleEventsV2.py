92ecc1a795567f99436fc8022fe70ab2fcff7f68
Rajesh Ramamoorthy <rajesh.ramamoorthy@Rajeshs-MacBook-Pro.local>
2023-05-09 17:13:15 +0530

    Code changes for improving coverage

--- Packs/CybleEventsV2/Integrations/CybleEventsV2/CybleEventsV2.py
+++ Packs/CybleEventsV2/Integrations/CybleEventsV2/CybleEventsV2.py
@@ -105,6 +105,34 @@ def test_response(client, method, base_url, token):
         return 'fail'
 
 
+def fetch_subscribed_services_alert(client, method, base_url, token):
+    """
+    Fetch cyble subscribed services
+    Args:
+        client: instance of client to communicate with server
+        method: Requests method to be used
+        base_url: base url for the server
+        token: server access token
+
+    Returns: subscribed service list
+
+    """
+    get_subscribed_service_url = base_url + str(ROUTES[COMMAND['subscribed-services']])
+    subscribed_services = set_request(client, method, token, {}, get_subscribed_service_url)
+    service_name_list = []
+
+    for subscribed_service in subscribed_services:
+        service_name_list.append({"name": subscribed_service['name']})
+
+    markdown = tableToMarkdown('Alerts Group Details:', service_name_list, )
+    return CommandResults(
+        readable_output=markdown,
+        outputs_prefix='CybleEventsV2.ServiceList',
+        raw_response=service_name_list,
+        outputs=service_name_list
+    )
+
+
 def fetch_subscribed_services(client, method, base_url, token):
     """
     Fetch cyble subscribed services
@@ -127,7 +155,7 @@ def fetch_subscribed_services(client, method, base_url, token):
     return service_name_list
 
 
-def cyble_alert_group(client, method, token, url, input_params):
+def cyble_alert_group(client, method, token, url, args):
     """
     Call the client module to fetch alert group using the input parameters
     Args:
@@ -141,7 +169,32 @@ def cyble_alert_group(client, method, token, url, input_params):
 
     """
 
-    alert_groups = set_request(client, method, token, input_params, url)
+    input_params_alerts_group: dict[str, Any] = {
+        "orderBy": [
+            {
+                "created_at": args.get('orderBy', "desc")
+            }
+        ],
+        "skip": arg_to_number(args.get('from', 0)),
+        "take": arg_to_number(args.get('limit', 10)),
+        "include": {
+            "tags": True
+        }
+    }
+
+    if args.get('startDate', '') and args.get('endDate', ''):
+        input_params_alerts_group['where'] = {}
+        input_params_alerts_group['where']['created_at'] = {}
+
+    if args.get('startDate', ''):
+        input_params_alerts_group['where']['created_at']['gte'] = datetime.strptime(
+            args.get('startDate', ''), '%Y-%m-%dT%H:%M:%S%z').astimezone().isoformat()
+
+    if args.get('endDate', ''):
+        input_params_alerts_group['where']['created_at']['lte'] = \
+            datetime.strptime(args.get('endDate', ''), '%Y-%m-%dT%H:%M:%S%z').astimezone().isoformat()
+
+    alert_groups = set_request(client, method, token, input_params_alerts_group, url)
     lst_alert_group = []
 
     if alert_groups:
@@ -165,7 +218,7 @@ def cyble_alert_group(client, method, token, url, input_params):
         )
 
 
-def cyble_fetch_iocs(client, method, token, input_params, url):
+def cyble_fetch_iocs(client, method, token, args, url):
     """
     Call the client module to fetch IOCs using the input parameters
     Args:
@@ -173,12 +226,32 @@ def cyble_fetch_iocs(client, method, token, input_params, url):
         method: Requests method to be used
         token: API access token
         url: url for end point
-        input_params: input parameter for api
+        args: input parameter for api
 
     Returns: indicators from server
 
     """
-    iocs = set_request(client, method, token, input_params, url)
+
+    input_params_alerts_iocs = {}
+    if args.get('iocType', ''):
+        input_params_alerts_iocs['iocType'] = args.get('iocType', '')
+
+    if args.get('startDate', ''):
+        input_params_alerts_iocs['startDate'] = args.get('startDate', '')
+
+    if args.get('endDate', ''):
+        input_params_alerts_iocs['endDate'] = args.get('endDate', '')
+
+    input_params_alerts_iocs = {
+        'ioc': args.get('ioc', ''),
+        'page': args.get('from', ''),
+        'limit': args.get('limit', ''),
+        'sortBy': args.get('sortBy', ''),
+        'order': args.get('order', ''),
+        'tags': args.get('tags')
+    }
+
+    iocs = set_request(client, method, token, input_params_alerts_iocs, url)
 
     try:
         lst_iocs = []
@@ -243,26 +316,53 @@ def format_incidents(alerts):
         return []
 
 
-def cyble_events(client, method, token, url, input_params, args, skip=True):
+def cyble_events(client, method, token, url, args, base_url, last_run, skip=True):
     """
     Fetch alert details from server for creating incidents in XSOAR
     Args:
+        last_run: get last run details
+        base_url: base url for subscribed services
         client: instance of client to communicate with server
         method: Requests method to be used
         token: API access token
         url: end point URL
-        input_params: input parameter for api,
         args: input args
         skip: skip the validation for fetch incidnet
 
     Returns: events from the server
 
     """
+
+    input_params = {}
+
+    input_params['orderBy'] = args.get('orderBy', "desc")
+    input_params['from_da'] = arg_to_number(args.get('from', 0))
+
     if skip:
         validate_input(args, False)
 
-    alerts = set_request(client, method, token, input_params, url)
+        input_params['limit'] = arg_to_number(args.get('limit', 0))
+        input_params['startDate'] = args.get('startDate', 0)
+        input_params['endDate'] = args.get('endDate', 0)
 
+    else:
+
+        if 'event_pull_start_date' not in last_run.keys():
+            last_run['event_pull_start_date'] = datetime.now().astimezone().replace(microsecond=0).isoformat()
+
+        # Convert the argument to an int using helper function or set to MAX_INCIDENTS_TO_FETCH
+        max_results = arg_to_number(demisto.params().get('max_fetch', 0))
+
+        if not max_results or max_results > MAX_ALERTS:
+            input_params['limit'] = MAX_ALERTS
+        else:
+            input_params['limit'] = max_results
+
+        input_params['startDate'] = last_run['event_pull_start_date']
+        input_params['endDate'] = datetime.now().astimezone().replace(microsecond=0).isoformat()
+
+    final_input_structure = alert_input_structure(client, base_url, token, input_params)
+    alerts = set_request(client, method, token, final_input_structure, url)
     incidents = []
 
     try:
@@ -454,36 +554,15 @@ def main():
             args = demisto.args()
 
             if demisto.command() == "test-module":
-                fetch_subscribed = test_response(client, "GET", base_url, token)
                 # request was successful
-                return_results(fetch_subscribed)
+                return_results(test_response(client, "GET", base_url, token))
 
             elif demisto.command() == 'fetch-incidents':
-
                 # This is the call made when cyble-fetch-events command.
                 last_run = demisto.getLastRun()
-                if 'event_pull_start_date' not in last_run.keys():
-                    last_run['event_pull_start_date'] = datetime.now().astimezone().replace(microsecond=0).isoformat()
-
-                input_params = {}
-                input_params['orderBy'] = args.get('orderBy', "desc")
-                input_params['from_da'] = arg_to_number(args.get('from', 0))
-
-                # Convert the argument to an int using helper function or set to MAX_INCIDENTS_TO_FETCH
-                max_results = arg_to_number(params.get('max_fetch', 0))
-
-                if not max_results or max_results > MAX_ALERTS:
-                    input_params['limit'] = MAX_ALERTS
-                else:
-                    input_params['limit'] = max_results
-
-                input_params['startDate'] = last_run['event_pull_start_date']
-                input_params['endDate'] = datetime.now().astimezone().replace(microsecond=0).isoformat()
-
-                final_input_structure = alert_input_structure(client, base_url, token, input_params)
 
                 url = base_url + str(ROUTES[COMMAND[demisto.command()]])
-                data = cyble_events(client, 'POST', token, url, final_input_structure, args, False)
+                data = cyble_events(client, 'POST', token, url, args, base_url, last_run, False)
 
                 if last_run['event_pull_start_date'] < datetime.now().astimezone().replace(microsecond=0).isoformat():
                     last_run['event_pull_start_date'] = datetime.now().astimezone().replace(microsecond=0).isoformat()
@@ -493,92 +572,29 @@ def main():
 
             elif demisto.command() == "subscribed-services":
                 # This is the call made when subscribed-services command.
-                subscribed_service_list = fetch_subscribed_services(client, "GET", base_url, token)
-
-                markdown = tableToMarkdown('Alerts Group Details:', subscribed_service_list, )
-                return_results(CommandResults(
-                    readable_output=markdown,
-                    outputs_prefix='CybleEventsV2.ServiceList',
-                    raw_response=subscribed_service_list,
-                    outputs=subscribed_service_list
-                ))
+                return_results(fetch_subscribed_services_alert(client, "GET", base_url, token))
 
             elif demisto.command() == "fetch-alert-groups":
                 # Fetch alert group.
 
                 validate_input(args, False)
-
-                input_params_alerts_group: dict[str, Any] = {
-                    "orderBy": [
-                        {
-                            "created_at": args.get('orderBy', "desc")
-                        }
-                    ],
-                    "skip": arg_to_number(args.get('from', 0)),
-                    "take": arg_to_number(args.get('limit', 10)),
-                    "include": {
-                        "tags": True
-                    }
-                }
-
-                if args.get('startDate', '') and args.get('endDate', ''):
-                    input_params_alerts_group['where'] = {}
-                    input_params_alerts_group['where']['created_at'] = {}
-
-                if args.get('startDate', ''):
-                    input_params_alerts_group['where']['created_at']['gte'] = datetime.strptime(
-                        args.get('startDate', ''), '%Y-%m-%dT%H:%M:%S%z').astimezone().isoformat()
-
-                if args.get('endDate', ''):
-                    input_params_alerts_group['where']['created_at']['lte'] = \
-                        datetime.strptime(args.get('endDate', ''), '%Y-%m-%dT%H:%M:%S%z').astimezone().isoformat()
-
                 url = base_url + str(ROUTES[COMMAND[demisto.command()]])
-
-                return_results(cyble_alert_group(client, 'POST', token, url, input_params_alerts_group))
+                return_results(cyble_alert_group(client, 'POST', token, url, args))
 
             elif demisto.command() == 'cyble-vision-v2-fetch-iocs':
                 # This is the call made when cyble-vision-v2-fetch-iocs command.
 
                 validate_input(args, True)
-
-                input_params_alerts_iocs = {}
-                if args.get('iocType', ''):
-                    input_params_alerts_iocs['iocType'] = args.get('iocType', '')
-
-                if args.get('startDate', ''):
-                    input_params_alerts_iocs['startDate'] = args.get('startDate', '')
-
-                if args.get('endDate', ''):
-                    input_params_alerts_iocs['endDate'] = args.get('endDate', '')
-
-                input_params_alerts_iocs = {
-                    'ioc': args.get('ioc', ''),
-                    'page': args.get('from', ''),
-                    'limit': args.get('limit', ''),
-                    'sortBy': args.get('sortBy', ''),
-                    'order': args.get('order', ''),
-                    'tags': args.get('tags')
-                }
-
                 url = base_url + str(ROUTES[COMMAND[demisto.command()]])
-                command_results = cyble_fetch_iocs(client, 'GET', token, input_params_alerts_iocs, url)
+                command_results = cyble_fetch_iocs(client, 'GET', token, args, url)
+
                 return_results(command_results)
 
             elif demisto.command() == 'cyble-vision-v2-fetch-alerts':
                 # This is the call made when cyble-vision-v2-fetch-alerts command.
-                input_params = {}
-                input_params['orderBy'] = args.get('orderBy', "desc")
-                input_params['from_da'] = arg_to_number(args.get('from', 0))
-                input_params['limit'] = arg_to_number(args.get('limit', 0))
-                input_params['startDate'] = args.get('startDate', 0)
-                input_params['endDate'] = args.get('endDate', 0)
 
-                final_input_structure = alert_input_structure(client, base_url, token, input_params)
-
-                # print(final_input_structure)
                 url = base_url + str(ROUTES[COMMAND[demisto.command()]])
-                lst_alerts = cyble_events(client, 'POST', token, url, final_input_structure, args, True)
+                lst_alerts = cyble_events(client, 'POST', token, url, args, base_url, {}, True)
 
                 markdown = tableToMarkdown('Alerts Details:', lst_alerts)
 
