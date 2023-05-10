b8001728fe8acc1c4588eb5df24a7acf70bdc514
darkushin <darkushin@paloaltonetworks.com>
2023-05-10 17:50:53 +0300

    Added UTs for the hotfix part

--- Tests/Marketplace/Tests/upload_packs_test.py
+++ Tests/Marketplace/Tests/upload_packs_test.py
@@ -10,6 +10,7 @@ from Tests.Marketplace.upload_packs import get_packs_names, get_updated_private_
 
 from Tests.Marketplace.marketplace_services import Pack
 
+
 # disable-secrets-detection-start
 
 
@@ -567,6 +568,124 @@ class TestCorepacksFiles:
         # Remove the temp artifacts dir that was created for testing:
         shutil.rmtree(artifacts_dir)
 
+    def test_should_override_locked_corepacks_file(self, mocker):
+        """
+        When:
+            - running the should_override_locked_corepacks_file command
+        Given:
+            1. A server version in the corepacks_override file that does not exist in the versions-metadata file
+            2. A valid server version in the corepacks_override file, but a file version that is not greater than the
+                file version in the versions-metadata file.
+            3. A valid server version and a file version greater than the file version in the versions-metadata file.
+        Then:
+            1. Assert that the result returned from the function is False
+            2. Assert that the result returned from the function is False
+            3. Assert that the result returned from the function is True
+        """
+
+        from Tests.Marketplace.upload_packs import should_override_locked_corepacks_file
+        from Tests.Marketplace.marketplace_constants import GCPConfig
+
+        versions_metadata = {
+            "8.2.0": {
+                "core_packs_file": "corepacks-8.2.0.json",
+                "core_packs_file_is_locked": True,
+                "file_version": "1"
+            }
+        }
+        mocker.patch.object(GCPConfig, "CORE_PACKS_FILE_VERSIONS", versions_metadata)
+
+        # Case 1
+        corepacks_override = {
+            "server_version": "8.3.0",
+            "file_version": "1",
+            "updated_corepacks_content":
+                {
+                    "corePacks": [],
+                    "upgradeCorePacks": [],
+                    "buildNumber": "123"
+                }
+        }
+        mocker.patch.object(GCPConfig, "COREPACKS_OVERRIDE_CONTENTS", corepacks_override)
+        assert not should_override_locked_corepacks_file()
+
+        # Case 2
+        corepacks_override = {
+            "server_version": "8.2.0",
+            "file_version": "1",
+            "updated_corepacks_content":
+                {
+                    "corePacks": [],
+                    "upgradeCorePacks": [],
+                    "buildNumber": "123"
+                }
+        }
+        mocker.patch.object(GCPConfig, "COREPACKS_OVERRIDE_CONTENTS", corepacks_override)
+        assert not should_override_locked_corepacks_file()
+
+        # Case 3
+        corepacks_override = {
+            "server_version": "8.2.0",
+            "file_version": "2",
+            "updated_corepacks_content":
+                {
+                    "corePacks": [],
+                    "upgradeCorePacks": [],
+                    "buildNumber": "123"
+                }
+        }
+        mocker.patch.object(GCPConfig, "COREPACKS_OVERRIDE_CONTENTS", corepacks_override)
+        assert should_override_locked_corepacks_file()
+
+    def test_override_locked_corepacks_file(self, mocker):
+        """
+        Test the override_locked_corepacks_file function.
+        """
+        from Tests.Marketplace.upload_packs import override_locked_corepacks_file
+        from Tests.Marketplace.marketplace_constants import GCPConfig
+        import shutil
+
+        # Create a temp artifacts dir for the corepacks files:
+        artifacts_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tmp')
+        os.makedirs(artifacts_dir, exist_ok=True)
+
+        corepacks_override = {
+            "server_version": "8.2.0",
+            "file_version": "2",
+            "updated_corepacks_content":
+                {
+                    "corePacks": ['pack1', 'pack2'],
+                    "upgradeCorePacks": ['pack3'],
+                    "buildNumber": "123"
+                }
+        }
+        mocker.patch.object(GCPConfig, "COREPACKS_OVERRIDE_CONTENTS", corepacks_override)
+
+        versions_metadata_content = {
+            "version_map": {
+                "8.2.0": {
+                    "core_packs_file": "corepacks-8.2.0.json",
+                    "core_packs_file_is_locked": True,
+                    "file_version": "1"
+                }
+            }
+        }
+        mocker.patch.object(GCPConfig, "VERSIONS_METADATA_CONTENTS", versions_metadata_content)
+
+        override_locked_corepacks_file(build_number='456', artifacts_dir=artifacts_dir)
+
+        # Assert that the file was created in the artifacts folder with the build number as expected:
+        with open(os.path.join(artifacts_dir, 'corepacks-8.2.0.json'), 'r') as corepacks_file:
+            corepacks_file_contents = json.load(corepacks_file)
+            assert corepacks_file_contents.get('buildNumber') == '456'
+            assert corepacks_file_contents.get('corePacks') == ['pack1', 'pack2']
+
+        # Assert that the versions-metadata file was updated with the required file version:
+        assert GCPConfig.VERSIONS_METADATA_CONTENTS.get('version_map').get('8.2.0').get('file_version') == '2'
+
+        # Remove the temp artifacts dir that was created for testing:
+        shutil.rmtree(artifacts_dir)
+
 
 class TestUpdatedPrivatePacks:
 
@@ -624,7 +743,7 @@ class TestUpdatedPrivatePacks:
         updated_private_packs = get_updated_private_packs(private_packs, index_folder_path)
         assert len(updated_private_packs) == 1
         assert updated_private_packs[0] == "updated_pack" and updated_private_packs[0] != "first_non_updated_pack" and \
-            updated_private_packs[0] != "second_non_updated_pack"
+               updated_private_packs[0] != "second_non_updated_pack"
 
     def test_is_private_packs_updated(self, mocker):
         """
