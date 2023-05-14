15bb93f57bf98d4188b06e53154ef40206139a4e
davrahami <davrahami@paloaltonetworks.com>
2023-05-14 15:17:57 +0300

    New script to support a new dynamic section in the CVE layout.

---
+++ Packs/CommonScripts/Scripts/CVECVSSColor/CVECVSSColor.py
@@ -0,0 +1,40 @@
+
+from CommonServerPython import *
+import json
+
+colors = {
+    'Green1': '#50C878',
+    'Green2': '#6CB65B',
+    'Green3': '#89C35B',
+    'Green4': '#A3C157',
+    'Amber1': '#FFB347',
+    'Amber2': '#FFA07A',
+    'Amber3': '#FF7F50',
+    'Red1': '#FF6347',
+    'Red2': '#FF4500',
+    'Red3': '#FF4040'
+}
+
+theme = demisto.callingContext['context']['User']['theme']
+indicator = demisto.callingContext['args']['indicator']
+
+try:
+    cvss = indicator.get('CustomFields').get('cvss', '')
+    cvss = json.loads(cvss)
+    cvss = float(cvss['Score'])
+
+
+except (ValueError, AttributeError):
+    cvss = 0
+
+if not 0 < cvss <= 10:
+    color = "#000000"
+    cvss = ""
+elif cvss <= 4:
+    color = colors[f"Green{int(cvss)}"]
+elif cvss <= 7:
+    color = colors[f"Amber{int(cvss) - 4}"]
+else:
+    color = colors[f"Red{int(cvss) - 7}"]
+
+return_results(CommandResults(readable_output=f"# <-:->{{{{color:{color}}}}}(**{cvss}**)"))
---
+++ Packs/CommonScripts/Scripts/CVECVSSColor/README.md
@@ -0,0 +1,25 @@
+## CVECVSSColor
+
+---
+This dynamic automation parses the CVSS score of a CVE and presents it in the layout in color according to its score.
+
+
+### Script Data
+
+---
+
+| **Name** | **Description** |
+| --- | --- |
+| Script Type | python3 |
+| Tags | dynamic-indicator-section |
+| Cortex XSOAR Version | 6.8.0 |
+
+### Inputs
+
+---
+There are no inputs for this script.
+
+### Outputs
+
+---
+There are no outputs for this script.
