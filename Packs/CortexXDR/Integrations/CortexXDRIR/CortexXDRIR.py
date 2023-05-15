ccdbca05b7e8232f471f4e03a24606406bc971d2
shmuel44 <skroizer@paloaltonetworks.com>
2023-05-15 15:29:03 +0300

    fix syntax yml

--- Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
+++ Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
@@ -1612,10 +1612,10 @@ def main():  # pragma: no cover
             return_results(get_list_roles_command(client, args))
 
         elif command == 'xdr-set-user-role':
-            return_results(set_user_role_command(client, args))
+            return_results(change_user_role_command(client, args))
 
         elif command == 'xdr-remove-user-role':
-            return_results(remove_user_role_command(client, args))
+            return_results(change_user_role_command(client, args))
 
     except Exception as err:
         return_error(str(err))
