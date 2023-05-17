4c91cd0722d40c4716e5b8fbd1bf7cfa086f81c6
rshunim <rshunim@paloaltonetworks.com>
2023-05-17 15:20:29 +0300

    change condition

--- Packs/Jira/Integrations/JiraV2/JiraV2.py
+++ Packs/Jira/Integrations/JiraV2/JiraV2.py
@@ -551,7 +551,7 @@ def get_project_id(project_key='', project_name=''):
         result = jira_req('GET', 'rest/api/latest/issue/createmeta', resp_type='json')
     except DemistoException as de:
 
-        if not de.message.startswith('Status code: 404'):
+        if de.message.find('Status code: 404') < 0:
             raise de
 
         demisto.debug(f'The error message we got after first api call: {de.message}')
