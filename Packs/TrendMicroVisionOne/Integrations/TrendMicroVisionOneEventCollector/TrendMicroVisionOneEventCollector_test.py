480d2ab3f6ada114cd5b28ea1988e6ef4e8153a7
GuyAfik <guyafik11@gmail.com>
2023-05-23 10:07:14 +0300

    update unit-tests

--- Packs/TrendMicroVisionOne/Integrations/TrendMicroVisionOneEventCollector/TrendMicroVisionOneEventCollector_test.py
+++ Packs/TrendMicroVisionOne/Integrations/TrendMicroVisionOneEventCollector/TrendMicroVisionOneEventCollector_test.py
@@ -65,14 +65,19 @@ def create_logs_mocks(url: str, num_of_events: int, created_time_field: str, url
         }
 
 
-def _http_request_side_effect_decorator(num_of_events):
+def _http_request_side_effect_decorator(
+    num_of_workbench_logs: int = 0,
+    num_of_oat_logs: int = 0,
+    num_of_search_detection_logs: int = 0,
+    num_of_audit_logs: int = 0
+):
     def _http_request_side_effect(**kwargs):
         full_url = kwargs.get('full_url') or ''
         params = kwargs.get('params') or {}
         if 'workbench/alerts' in full_url:
             return create_logs_mocks(
                 url=full_url,
-                num_of_events=num_of_events,
+                num_of_events=num_of_workbench_logs,
                 url_suffix='workbench/alerts',
                 created_time_field='createdDateTime',
                 top=10
@@ -80,7 +85,7 @@ def _http_request_side_effect_decorator(num_of_events):
         if 'oat/detections' in full_url:
             return create_logs_mocks(
                 url=full_url,
-                num_of_events=num_of_events,
+                num_of_events=num_of_oat_logs,
                 url_suffix='oat/detections',
                 created_time_field='detectedDateTime',
                 top=params.get('top') or 200
@@ -88,7 +93,7 @@ def _http_request_side_effect_decorator(num_of_events):
         if 'search/detections' in full_url:
             return create_logs_mocks(
                 url=full_url,
-                num_of_events=num_of_events,
+                num_of_events=num_of_search_detection_logs,
                 url_suffix='search/detections',
                 created_time_field='eventTime',
                 top=params.get('top') or DEFAULT_MAX_LIMIT
@@ -96,7 +101,7 @@ def _http_request_side_effect_decorator(num_of_events):
         else:
             return create_logs_mocks(
                 url=full_url,
-                num_of_events=num_of_events,
+                num_of_events=num_of_audit_logs,
                 url_suffix='audit/logs',
                 created_time_field='loggedDateTime',
                 top=params.get('top') or 200
@@ -113,8 +118,8 @@ def start_freeze_time(timestamp):
 class TestFetchEvents:
 
     @pytest.mark.parametrize(
-        "last_run, integration_params, expected_updated_last_run, "
-        "num_of_events_for_each_log_type, expected_events_length, datetime_string_freeze_time",
+        "last_run, integration_params, expected_updated_last_run, datetime_string_freeze_time, "
+        "num_of_workbench_logs, num_of_oat_logs, num_of_search_detection_logs, num_of_audit_logs",
         [
             (
                 {},
@@ -125,9 +130,11 @@ class TestFetchEvents:
                     'search_detection_logs_time': '2023-01-01T15:00:00Z',
                     'audit_logs_time': '2023-01-01T14:59:59Z'
                 },
+                '2023-01-01T15:00:00Z',
                 50,
-                200,
-                '2023-01-01T15:00:00Z'
+                50,
+                50,
+                50
             ),
             (
                 {
@@ -143,33 +150,41 @@ class TestFetchEvents:
                     'search_detection_logs_time': '2023-01-01T15:05:00Z',
                     'audit_logs_time': '2023-01-01T15:04:59Z'
                 },
+                '2023-01-01T15:05:00Z',
                 50,
-                200,
-                '2023-01-01T15:05:00Z'
+                50,
+                50,
+                50
             ),
         ],
     )
     def test_fetch_events_main(
         self,
-        mocker, client: Client,
+        mocker,
+        client: Client,
         last_run: Dict,
         integration_params: Dict,
         expected_updated_last_run: Dict,
-        num_of_events_for_each_log_type: int,
-        expected_events_length: int,
-        datetime_string_freeze_time: str
+        datetime_string_freeze_time: str,
+        num_of_workbench_logs: int,
+        num_of_oat_logs: int,
+        num_of_search_detection_logs: int,
+        num_of_audit_logs: int
     ):
         """
+        Note: the limit is per single log!
+
         Given:
-            - Case A: last_run={}, limit=100, num_of_events_for_each_log_type=50
-            - Case B: last_run=previous fetch, limit=100, num_of_events_for_each_log_type=50
+            - Case A: last_run={}, limit=100, num_of_workbench_logs=50, num_of_oat_logs=50,
+                      num_of_search_detection_logs=50, num_of_audit_logs=50
+            - Case B: last_run=last run from Case A, limit=100, num_of_workbench_logs=50, num_of_oat_logs=50,
+                      num_of_search_detection_logs=50, num_of_audit_logs=50
         When:
             - fetch-events through the main flow
         Then:
-            - Case A + B:
-                1. workbench, oat and search detection logs last run time is the last log + 1 second added to it.
-                2. make sure the audit log last time is the last log without +1 second to it.
-                3. make sure the expected_events_length = 200
+            - workbench, oat and search detection logs last run time is the last log time + 1 second added to it.
+            - make sure the audit log last time is the last log without +1 second to it.
+            - make sure the expected_events_length is the sum of all the logs.
 
         """
         from TrendMicroVisionOneEventCollector import main
@@ -181,7 +196,12 @@ class TestFetchEvents:
         mocker.patch.object(
             BaseClient,
             '_http_request',
-            side_effect=_http_request_side_effect_decorator(num_of_events=num_of_events_for_each_log_type)
+            side_effect=_http_request_side_effect_decorator(
+                num_of_workbench_logs=num_of_workbench_logs,
+                num_of_oat_logs=num_of_oat_logs,
+                num_of_search_detection_logs=num_of_search_detection_logs,
+                num_of_audit_logs=num_of_audit_logs
+            )
         )
 
         set_last_run_mocker = mocker.patch.object(demisto, 'setLastRun', return_value=last_run)
@@ -189,4 +209,5 @@ class TestFetchEvents:
         main()
 
         assert set_last_run_mocker.call_args.args[0] == expected_updated_last_run
-        assert len(send_events_to_xsiam_mocker.call_args.kwargs['events']) == expected_events_length
+        assert len(send_events_to_xsiam_mocker.call_args.kwargs['events']) == \
+               num_of_workbench_logs + num_of_oat_logs + num_of_search_detection_logs + num_of_audit_logs
