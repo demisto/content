5eb2f881c5a41bc7d26b8700fde5f064959f4141
efelmandar <efelmandar@paloaltonetworks.com>
2023-05-24 13:30:48 +0000

    Fix validation and linting errors

--- Packs/RDPCacheHunting/Scripts/SetIndicatorstype/SetIndicatorstype_test.py
+++ Packs/RDPCacheHunting/Scripts/SetIndicatorstype/SetIndicatorstype_test.py
@@ -1,13 +1,10 @@
-from typing import List
-import pytest
-from unittest.mock import patch
-from SetIndicatorstype import parse_data, main
+from SetIndicatorstype import parse_data
 
 expected_results = {
-        "Type": 17,
-        "ContentsFormat": "pie",
-        "Contents": {
-            "stats": [
+    "Type": 17,
+    "ContentsFormat": "pie",
+    "Contents": {
+        "stats": [
                 {
                     "data": [2],
                     "groups": None,
@@ -15,77 +12,79 @@ expected_results = {
                     "label": "a",
                     "color": "#5057db"
                 },
-                {
+            {
                     "data": [2],
                     "groups": None,
                     "name": "b",
                     "label": "b",
                     "color": "#6802ec"
                 },
-                {
+            {
                     "data": [2],
                     "groups": None,
                     "name": "c",
                     "label": "c",
                     "color": "#a0b203"
                 },
-                {
+            {
                     "data": [2],
                     "groups": None,
                     "name": "d",
                     "label": "d",
                     "color": "#15e9ac"
                 },
-                {
+            {
                     "data": [2],
                     "groups": None,
                     "name": "e",
                     "label": "e",
                     "color": "#e0d010"
                 },
-                {
+            {
                     "data": [1],
                     "groups": None,
                     "name": "f",
                     "label": "f",
                     "color": "#7f0679"
                 },
-                {
+            {
                     "data": [1],
                     "groups": None,
                     "name": "g",
                     "label": "g",
                     "color": "#b3e83b"
                 },
-                {
+            {
                     "data": [1],
                     "groups": None,
                     "name": "h",
                     "label": "h",
                     "color": "#5ee71a"
                 },
-                {
+            {
                     "data": [1],
                     "groups": None,
                     "name": "i",
                     "label": "i",
                     "color": "#6654c5"
                 },
-                {
+            {
                     "data": [1],
                     "groups": None,
                     "name": "j",
                     "label": "j",
                     "color": "#12b826"
                 }
-            ],
-            "params": {
-                "layout": "horizontal"
-            }
+        ],
+        "params": {
+            "layout": "horizontal"
         }
     }
+}
 
 # Tests that the function returns the expected dictionary when given valid data.
+
+
 def test_parse_data_happy_path():
     # Arrange
     list_content = ['apple', 'banana', 'orange', 'apple', 'banana', 'grape', 'kiwi', 'pear', 'peach', 'watermelon']
@@ -96,8 +95,8 @@ def test_parse_data_happy_path():
     assert result['ContentsFormat'] == 'pie'
     assert len(result['Contents']['stats']) == 8
     assert result['Contents']['params']['layout'] == 'horizontal'
-    
-    
+
+
 # Tests that the function returns an empty dictionary when given an empty list.
 def test_parse_data_empty_list():
     # Arrange
@@ -109,9 +108,9 @@ def test_parse_data_empty_list():
     assert result['ContentsFormat'] == 'pie'
     assert len(result['Contents']['stats']) == 0
     assert result['Contents']['params']['layout'] == 'horizontal'
-    
 
-# Tests that the function correctly handles duplicate values in the list_content parameter.    
+
+# Tests that the function correctly handles duplicate values in the list_content parameter.
 def test_parse_data_duplicate_values():
     # Arrange
     list_content = ['apple', 'banana', 'orange', 'apple', 'banana', 'grape', 'kiwi', 'pear', 'peach', 'watermelon', 'watermelon']
@@ -122,8 +121,8 @@ def test_parse_data_duplicate_values():
     assert result['ContentsFormat'] == 'pie'
     assert len(result['Contents']['stats']) == 8
     assert result['Contents']['params']['layout'] == 'horizontal'
-    
-    
+
+
 # Tests that the function returns an empty dictionary when the top_lists parameter is empty.
 def test_parse_data_empty_top_lists():
     # Arrange
@@ -135,8 +134,8 @@ def test_parse_data_empty_top_lists():
     assert result['ContentsFormat'] == 'pie'
     assert len(result['Contents']['stats']) == 3
     assert result['Contents']['params']['layout'] == 'horizontal'
-    
-    
+
+
 # Tests that the function correctly handles cases where there are less than 10 unique values in list_content.
 def test_parse_data_less_than_10_unique_values():
     # Arrange
--- Packs/RDPCacheHunting/Scripts/SetRDPOverallScore/SetRDPOverallScore_test.py
+++ Packs/RDPCacheHunting/Scripts/SetRDPOverallScore/SetRDPOverallScore_test.py
@@ -1,9 +1,9 @@
-import pytest
 import demistomock as demisto
-from unittest.mock import patch
 from SetRDPOverallScore import main
 
 # Tests that the function returns the correct HTML string for an OverallScore of 0.
+
+
 def test_happy_path_score_zero(mocker):
     mocker.patch.object(demisto, 'context', return_value={'OverallScore': '0'})
     expected_html = "<div style='color:#1DB846;font-size:32px;padding: 60px; text-align:center;padding-left: 70px'>0/100<br><br>No suspicious strings found</div>"
