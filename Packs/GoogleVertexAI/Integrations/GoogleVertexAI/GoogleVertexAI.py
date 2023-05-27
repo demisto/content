5b95de7bee0e3d3b52df16494bba2df9cd1b51c0
sepaprivate <113604678+sepaprivate@users.noreply.github.com>
2023-05-28 03:20:44 +0400

    Update GoogleVertexAI.py

--- Packs/GoogleVertexAI/Integrations/GoogleVertexAI/GoogleVertexAI.py
+++ Packs/GoogleVertexAI/Integrations/GoogleVertexAI/GoogleVertexAI.py
@@ -302,5 +302,3 @@ def main():
 # python2 uses __builtin__ python3 uses builtins
 if __name__ == '__builtin__' or __name__ == 'builtins':
     main()
-
-register_module_line('Google Vertex AI', 'end', __line__())
