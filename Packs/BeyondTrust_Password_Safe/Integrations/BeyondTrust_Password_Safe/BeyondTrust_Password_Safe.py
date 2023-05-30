6466f1f9d818c8b6a77b95a13822c70b73251721
maimorag <mmorag@paloaltonetworks.com>
2023-05-30 11:35:54 +0300

    cr

--- Packs/BeyondTrust_Password_Safe/Integrations/BeyondTrust_Password_Safe/BeyondTrust_Password_Safe.py
+++ Packs/BeyondTrust_Password_Safe/Integrations/BeyondTrust_Password_Safe/BeyondTrust_Password_Safe.py
@@ -17,8 +17,8 @@ urllib3.disable_warnings()
 USERNAME = demisto.params().get('credentials', {}).get('identifier')
 PASSWORD = demisto.params().get('credentials', {}).get('password')
 API_KEY = demisto.params().get('credentials_key', {}).get('password') or demisto.params().get('key')
-if not API_KEY or not USERNAME or not PASSWORD:
-    raise DemistoException('API Key must be required')
+if not API_KEY or not(USERNAME and PASSWORD):
+    raise DemistoException('The API Key or username and password must be provided')
 SYSTEM_NAME = demisto.params().get('system_name')
 # Remove trailing slash to prevent wrong URL path to service
 SERVER = demisto.params()['url'][:-1] \
