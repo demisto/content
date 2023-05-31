57a3c40af6055c28095ac5d09146d327d79b513b
Michael Yochpaz <8832013+MichaelYochpaz@users.noreply.github.com>
2023-05-31 13:56:45 +0300

    Cleanup

--- Tests/scripts/download_demisto_conf.sh
+++ Tests/scripts/download_demisto_conf.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 set +e
 
-# replace slashes ('/') in the branch name, if exist, with underscores ('_')
-UNDERSCORE_BRANCH=${CI_COMMIT_BRANCH//\//_}
+# Replace slashes ('/') in the branch name, if exist, with underscores ('_')
+UNDERSCORE_BRANCH="${CI_COMMIT_BRANCH//\//_}"
 
-#download awsinstancetool
-echo "Getting conf from branch $UNDERSCORE_BRANCH (fallback to master)"
+echo "Getting conf from branch $UNDERSCORE_BRANCH (with master fallback)"
 
 SECRET_CONF_PATH="./conf_secret.json"
 echo ${SECRET_CONF_PATH} > secret_conf_path
@@ -22,14 +21,12 @@ echo ${DEMISTO_LIC_PATH} > demisto_lic_path
 DEMISTO_PACK_SIGNATURE_UTIL_PATH="./signDirectory"
 echo ${DEMISTO_PACK_SIGNATURE_UTIL_PATH} > demisto_pack_sig_util_path
 
-TEST_CONF_BRANCH="$UNDERSCORE_BRANCH"
 
-# download configuration files from Gitlab repo
+# Download content-test-conf from GitLab repository
 echo "clone content-test-conf from branch: $UNDERSCORE_BRANCH in content-test-conf"
 git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@code.pan.run/xsoar/content-test-conf.git --branch $UNDERSCORE_BRANCH
 if [ "$?" != "0" ]; then
     echo "No such branch in content-test-conf: $UNDERSCORE_BRANCH , falling back to master"
-    TEST_CONF_BRANCH="master"
     git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@code.pan.run/xsoar/content-test-conf.git
 fi
 cp -r ./content-test-conf/awsinstancetool ./Tests/scripts/awsinstancetool
