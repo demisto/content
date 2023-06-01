4cdd530d187dafd38aa8568c47ca048263e00c28
Dan Tavori <dtavori@paloaltonetworks.com>
2023-06-01 17:17:07 +0300

    second commit

--- Packs/CommonScripts/Scripts/GeneratePassword/GeneratePassword.py
+++ Packs/CommonScripts/Scripts/GeneratePassword/GeneratePassword.py
@@ -2,11 +2,22 @@ import demistomock as demisto
 from CommonServerPython import *
 from CommonServerUserPython import *
 
+import random
 import secrets
 import string
 
 DEFAULT_MIN = 0
 DEFAULT_MAX = 10
+
+LOWERCASE_CHARS = string.ascii_lowercase
+UPPERCASE_CHARS = string.ascii_uppercase
+DIGITS = string.digits
+SYMBOLS = "!@#$%^&*()[]+:\"?_><=';/-.,\\|"
+
+
+def randomize_number_in_range(min_val: int, max_val: int) -> int:
+    n = max(max_val - min_val, 0)
+    return secrets.randbelow(n + 1) + min_val
 
 
 def print_char_values(pw):
@@ -17,59 +28,43 @@ def print_char_values(pw):
 
 def generate_password(args: Dict[str, Any]) -> CommandResults:
     is_debug = argToBoolean(args.get('debug'))
-    min_lcase = arg_to_number(args.get('min_lcase')) or DEFAULT_MIN
-    max_lcase = arg_to_number(args.get('max_lcase')) or DEFAULT_MAX
-    min_ucase = arg_to_number(args.get('min_ucase')) or DEFAULT_MIN
-    max_ucase = arg_to_number(args.get('max_ucase')) or DEFAULT_MAX
+    min_lowercase = arg_to_number(args.get('min_lcase')) or DEFAULT_MIN
+    max_lowercase = arg_to_number(args.get('max_lcase')) or DEFAULT_MAX
+    min_uppercase = arg_to_number(args.get('min_ucase')) or DEFAULT_MIN
+    max_uppercase = arg_to_number(args.get('max_ucase')) or DEFAULT_MAX
     min_digits = arg_to_number(args.get('min_digits')) or DEFAULT_MIN
     max_digits = arg_to_number(args.get('max_digits')) or DEFAULT_MAX
     min_symbols = arg_to_number(args.get('min_symbols')) or DEFAULT_MIN
     max_symbols = arg_to_number(args.get('max_symbols')) or DEFAULT_MAX
 
-    # Define the characters of our classes
-    lcase = string.ascii_lowercase
-    ucase = string.ascii_uppercase
-    n = string.digits
-    s = "!@#$%^&*()[]+:\"?_><=';/-.,\\|"
-
     # randomize the amount of characters we get as per parameters
-    numu = max_ucase - min_ucase
-    numu = max(numu, 0)
-    numu = secrets.randbelow(numu + 1) + min_ucase
+    num_upper = randomize_number_in_range(min_uppercase, max_uppercase)
+    num_lower = randomize_number_in_range(min_lowercase, max_lowercase)
+    num_digits = randomize_number_in_range(min_digits, max_digits)
+    num_symbols = randomize_number_in_range(min_symbols, max_symbols)
 
-    numl = max_lcase - min_lcase
-    numl = max(numl, 0)
-    numl = secrets.randbelow(numl + 1) + min_lcase
-
-    numn = max_digits - min_digits
-    numn = max(numn, 0)
-    numn = secrets.randbelow(numn + 1) + min_digits
-
-    nums = max_symbols - min_symbols
-    nums = max(nums, 0)
-    nums = secrets.randbelow(nums + 1) + min_symbols
-
-    if numu + numl + numn + nums == 0:
+    if num_upper + num_lower + num_digits + num_symbols == 0:
         raise DemistoException("error: insane password. No character length.")
 
     # start with a blank password
     pw = []
 
     # iterate through each character class and add
-    for _ in range(numu):
-        pw.append(secrets.choice(ucase))
-    for _ in range(numl):
-        pw.append(secrets.choice(lcase))
-    for _ in range(numn):
-        pw.append(secrets.choice(n))
-    for _ in range(nums):
-        pw.append(secrets.choice(s))
+    for _ in range(num_upper):
+        pw.append(secrets.choice(UPPERCASE_CHARS))
+    for _ in range(num_lower):
+        pw.append(secrets.choice(LOWERCASE_CHARS))
+    for _ in range(num_digits):
+        pw.append(secrets.choice(DIGITS))
+    for _ in range(num_symbols):
+        pw.append(secrets.choice(SYMBOLS))
 
     # randomize our new password string
-    rpw = ''.join(secrets.choice(pw) for _ in range(len(pw)))
+    rpw = ''.join(random.sample(pw, len(pw)))
 
     if is_debug:
         print_char_values(rpw)
+
     return CommandResults(
         outputs_prefix="NEW_PASSWORD",
         outputs=rpw,
@@ -85,5 +80,5 @@ def main():  # pragma: no cover
         return_error(str(e))
 
 
-if __name__ in ('__builtin__', 'builtins'):
+if __name__ in ('__builtin__', 'builtins'):  # pragma: no cover
     main()
