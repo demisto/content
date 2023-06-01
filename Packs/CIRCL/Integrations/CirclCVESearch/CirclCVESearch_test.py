d14d11c6b9fd481a6617d0e51205f03987bd6817
Dror Avrahami <davrahami@paloaltonetworks.com>
2023-06-01 07:36:37 +0000

    pip8 issues

--- Packs/CIRCL/Integrations/CirclCVESearch/CirclCVESearch_test.py
+++ Packs/CIRCL/Integrations/CirclCVESearch/CirclCVESearch_test.py
@@ -1,7 +1,8 @@
 import json
 import os
 from pathlib import Path
-from Packs.CIRCL.Integrations.CirclCVESearch.CirclCVESearch import cve_command, valid_cve_id_format, Client, generate_indicator, parse_cpe
+from Packs.CIRCL.Integrations.CirclCVESearch.CirclCVESearch import (cve_command, valid_cve_id_format, Client,
+                                                                    generate_indicator, parse_cpe)
 from CommonServerPython import DemistoException, argToList, EntityRelationship
 import pytest
 
