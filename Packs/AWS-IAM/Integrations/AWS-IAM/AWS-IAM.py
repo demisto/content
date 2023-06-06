306609db0394bb1e54adc1e76592ac944535e7b6
YuvHayun <yhayun011@gmail.com>
2023-06-06 12:22:32 +0300

    added put_user_policy_command

--- Packs/AWS-IAM/Integrations/AWS-IAM/AWS-IAM.py
+++ Packs/AWS-IAM/Integrations/AWS-IAM/AWS-IAM.py
@@ -1236,31 +1236,183 @@ def put_role_policy_command(args, aws_client):
 
 
 def put_user_policy_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    user_name = args.get('userName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'UserName': user_name
+    }
+
+    response = client.put_user_policy(**kwargs)
+    print(response)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to user {user_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to user {user_name}"
+                               f"\nencountered the following exception: {str(e)}")
+
+
+
+
 
 
 def put_group_policy_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    role_name = args.get('roleName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'RoleName': role_name
+    }
+
+    response = client.put_role_policy(**kwargs)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to role {role_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to role {role_name}"
+                               f"\nencountered the following exception: {str(e)}")
 
 
 def tag_role_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    role_name = args.get('roleName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'RoleName': role_name
+    }
+
+    response = client.put_role_policy(**kwargs)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to role {role_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to role {role_name}"
+                               f"\nencountered the following exception: {str(e)}")
 
 
 def tag_user_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    role_name = args.get('roleName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'RoleName': role_name
+    }
+
+    response = client.put_role_policy(**kwargs)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to role {role_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to role {role_name}"
+                               f"\nencountered the following exception: {str(e)}")
 
 
 def untag_user_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    role_name = args.get('roleName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'RoleName': role_name
+    }
+
+    response = client.put_role_policy(**kwargs)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to role {role_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to role {role_name}"
+                               f"\nencountered the following exception: {str(e)}")
 
 
 def untag_role_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    role_name = args.get('roleName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'RoleName': role_name
+    }
+
+    response = client.put_role_policy(**kwargs)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to role {role_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to role {role_name}"
+                               f"\nencountered the following exception: {str(e)}")
 
 
 def get_access_key_last_used_command(args, aws_client):
-    pass
+    client = aws_client.aws_session(
+        service=SERVICE
+    )
+
+    policy_document = args.get('policyDocument')
+    policy_name = args.get('policyName')
+    role_name = args.get('roleName')
+
+    kwargs = {
+        'PolicyDocument': policy_document,
+        'PolicyName': policy_name,
+        'RoleName': role_name
+    }
+
+    response = client.put_role_policy(**kwargs)
+    try:
+        if response.get('ResponseMetadata', {}).get('HTTPStatusCode', 400) == 200:
+            human_readable = f"Policy {policy_name} was added to role {role_name}"
+            return_outputs(human_readable, {}, response)
+    except Exception as e:
+        raise DemistoException(f"Couldn't add policy {policy_name} was added to role {role_name}"
+                               f"\nencountered the following exception: {str(e)}")
 
 
 def test_function(aws_client):
