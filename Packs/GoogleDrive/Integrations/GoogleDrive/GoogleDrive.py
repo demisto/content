fbd0e121b38804a82b00378e70298ceff05095f3
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-06-08 10:10:57 +0300

    possible solution to server error

--- Packs/GoogleDrive/Integrations/GoogleDrive/GoogleDrive.py
+++ Packs/GoogleDrive/Integrations/GoogleDrive/GoogleDrive.py
@@ -1690,6 +1690,7 @@ def main() -> None:
         account_json = params.get('user_creds', {}).get('password') or params.get('user_service_account_json')
         user_id = params.get('user_creds', {}).get('identifier') or params.get('user_id', '')
         params['user_id'] = user_id
+        params['user_service_account_json'] = account_json
 
         if not account_json:
             raise DemistoException('Please fill out the User\'s Service Account JSON field.')
