a7cebf3d008383552d0b60344139e88c9d00a058
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-06-12 12:19:19 +0300

    fixed import bug

--- Packs/IvantiHeat/Integrations/IvantiHeat/IvantiHeat.py
+++ Packs/IvantiHeat/Integrations/IvantiHeat/IvantiHeat.py
@@ -4,11 +4,10 @@ from CommonServerUserPython import *
 ''' IMPORTS '''
 
 import json
-import requests
 import dateparser
+import urllib3
 
-# Disable insecure warnings
-requests.packages.urllib3.disable_warnings()
+urllib3.disable_warnings()
 
 ''' CONSTANTS '''
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
