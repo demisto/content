ee7e845a19db46ff5a619c0ff7b4b2b1b8431f9a
efelmandar <efelmandar@paloaltonetworks.com>
2023-06-14 10:05:16 +0000

    Add unitests

--- Packs/CommonScripts/Scripts/BMCTool/BMCTool_test.py
+++ Packs/CommonScripts/Scripts/BMCTool/BMCTool_test.py
@@ -4,8 +4,7 @@ import pytest
 from BMCTool import BMCContainer
 
 
-@pytest.mark.parametrize(
-    "container, expected_size",
+@pytest.mark.parametrize("container, expected_size",
     [
         (BMCContainer.BMC_CONTAINER, 0x14),
         (BMCContainer.BIN_CONTAINER, 0xC)
@@ -97,30 +96,121 @@ def test_color_black():
     """
     assert BMCContainer.COLOR_BLACK == b"\x00"
 
-
-def test_b_import_func(capfd):
-    """    
-    Given:
-        - BMCContainer object
-        - Empty file
-    When:
-        - Calling the b_import function
-    Then:
-        - Ensure that the result is False
-    """
-    container = BMCContainer()
-    with capfd.disabled():
-        assert container.b_import("Packs/CommonScripts/Scripts/BMCTool/test_data/empty_file") is False
-
-
-def test_b_process_func():
+def test_b_process():
     """    
     Given:
         - BMCContainer object
     When:
-        - Calling the b_process_func function
+        - Calling the b_process function
     Then:
         - Ensure that the return value is not None and returned properly
     """
     container = BMCContainer()
-    assert container.b_process
+    container.b_process()
+    assert container.bdat is not None
+    
+
+@pytest.mark.parametrize("data, expected_output", [
+    (b"\xFF\xE0\x07\x1F\xF8\x00", b"\xff\x1c\xe7\xff9\xe3\x18\xff\xc6\x1c\x00\xff"),
+    (b"\x00\x00\x00\x00", b"\x00\x00\x00\xff\x00\x00\x00\xff"),
+])
+def test_b_parse_rgb565(data, expected_output):
+    """    
+    Given:
+        - BMCContainer object
+    When:
+        - Calling the b_process function
+    Then:
+        - Ensure that the return value is not None and returned properly
+    """
+    container = BMCContainer()
+    result = container.b_parse_rgb565(data)
+    
+    # Compare the result with the expected output
+    assert result == expected_output
+    
+    
+@pytest.mark.parametrize("data, expected_output, btype", [
+    (b"\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00", b"", BMCContainer.BIN_CONTAINER),
+    (b"\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00", b"\xff\x00\x00\xff\x00\x00\xff\xff\x00\xff\x00\xff", BMCContainer.BMC_CONTAINER),
+    (b"\xFF\x00\x00", b"", BMCContainer.BIN_CONTAINER),
+    (b"\xFF\x00\x00", b"\xFF\x00\x00\xFF", BMCContainer.BMC_CONTAINER),
+])
+def test_b_parse_rgb32b(data, expected_output, btype):
+    container = BMCContainer()
+    
+    container.btype = btype
+    
+    result = container.b_parse_rgb32b(data)
+    
+    # Compare the result with the expected output
+    assert result == expected_output
+
+
+@pytest.mark.parametrize("data, expected_output, btype", [
+    (b"\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00", b"", BMCContainer.BIN_CONTAINER),
+    (b"\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00", b"\xff\x00\x00\xff\xff\x00\x00\xff\xff\x00\x00\xff\xff\x00\x00\xff", BMCContainer.BMC_CONTAINER),
+    (b"\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00", b"", BMCContainer.BIN_CONTAINER),
+    (b"\xFF\x00\x00\xFF\x00\x00\xFF\x00\x00", b"\xff\x00\x00\xff\xff\x00\x00\xff\xff\x00\x00\xff", BMCContainer.BMC_CONTAINER),    
+])
+def test_b_parse_rgb24b(data, expected_output, btype):
+    container = BMCContainer()
+    
+    container.btype = btype
+    
+    result = container.b_parse_rgb24b(data)
+    
+    # Compare the result with the expected output
+    assert result == expected_output
+    
+    
+@pytest.mark.parametrize("data, expected_output", [
+    (b"", (-1, 1, 0)),
+    (b"\xF5", (-1, 2, 0xF5)),
+    (b"\xFD", (0xFD, 0, 1)),
+    (b"\xF9", (0xF9, 8, 1)),
+    (b"\xF0\x00\x01", (0xF0, 256, 3)),
+    (b"\xA0", (-1, 2, 0xA0)),
+    (b"\x80", (-1, 1, 0x00)),
+    (b"\x00", (-1, 1, 0x00)),
+    (b"\x10", (0x00, 16, 1)),
+    (b"\x40", (-1, 1, 0x00)),
+    (b"\xD0", (-1, 1, 0x00)),
+])
+def test_b_unrle(data, expected_output):
+    container = BMCContainer()  
+    result = container.b_unrle(data)
+    
+    # Compare the result with the expected output
+    assert result == expected_output
+    
+
+@pytest.mark.parametrize("data, bbp, expected_output", [
+    (b"", 3, b""),
+    (b"\xF5", 3, b""),
+    (b"\xFD", 3, b"\xFF\xFF\xFF"),
+     (b"\xA0", 3, b""),
+     (b"\x80", 3, b""),
+     (b"\x00", 3, b""),
+     (b"\x40", 3, b""),
+     (b"\xD0", 3, b""),
+])
+def test_b_uncompress(data, bbp, expected_output):
+    container = BMCContainer() 
+    result = container.b_uncompress(data, bbp)
+    
+    # Compare the result with the expected output
+    assert result == expected_output
+    
+    
+@pytest.mark.parametrize("width, height, data, pal, expected_output", [
+    (10, 10, b"\x00" * 100, False, b'BM\xde\x00\x00\x00\x00z\x00\x00\x00l\x00\x00\x00\x0a\x00\x00\x00\x0a\x00\x00\x00\x01\x00 \x00\x03\x00\x00\x00d\x00\x00\x00\x13\x0b\x00\x00\x13\x0b\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\x00\x00\xff\x00\x00\xff\x00\x00\x00\x00\x00\x00\xff niW\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+),
+])
+def test_b_export_bmp(width, height, data, pal, expected_output):
+    container = BMCContainer()  
+    container.pal = pal
+    result = container.b_export_bmp(width, height, data)
+    
+    # Compare the result with the expected output
+    assert result == expected_output
