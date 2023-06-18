b34178644097aaba1ae936a1e51a682f73f43748
tkatzir <tkatzir@paloaltonetworks.com>
2023-06-18 17:30:07 +0300

    custom_fields only when needed

--- Packs/XSOARmirroring/Integrations/XSOARmirroring/XSOARmirroring.py
+++ Packs/XSOARmirroring/Integrations/XSOARmirroring/XSOARmirroring.py
@@ -678,7 +678,7 @@ def get_remote_data_command(client: Client, args: Dict[str, Any], params: Dict[s
         )
 
 
-def update_remote_system_command(client: Client, args: Dict[str, Any], mirror_tags: Set[str], custom_fields: List[str]) -> str:
+def update_remote_system_command(client: Client, args: Dict[str, Any], mirror_tags: Set[str]) -> str:
     """update-remote-system command: pushes local changes to the remote system
 
     :type client: ``Client``
@@ -695,10 +695,6 @@ def update_remote_system_command(client: Client, args: Dict[str, Any], mirror_ta
     :type mirror_tags: ``Optional[str]``
     :param mirror_tags:
         The tag that you will mirror out of the incident.
-
-    :type custom_fields: ``List[str]``
-    :param custom_fields:
-        The custom fields that you will mirror out of the incident even when empty in remote system.
 
     :return:
         ``str`` containing the remote incident id - really important if the incident is newly created remotely
@@ -717,6 +713,7 @@ def update_remote_system_command(client: Client, args: Dict[str, Any], mirror_ta
         if parsed_args.remote_incident_id:
             # First, get the incident as we need the version
             old_incident = client.get_incident(incident_id=parsed_args.remote_incident_id)
+            custom_fields = argToList(demisto.params().get('custom_fields'))
             for changed_key in parsed_args.delta.keys():
                 old_incident[changed_key] = parsed_args.delta[changed_key]  # type: ignore
                 if changed_key in old_incident.get('CustomFields', {}).keys() or changed_key in custom_fields:
@@ -772,7 +769,6 @@ def main() -> None:
     demisto.debug(f'Command being called is {demisto.command()}')
     mirror_tags = set(demisto.params().get('mirror_tag', '').split(',')) \
         if demisto.params().get('mirror_tag') else set([])
-    custom_fields = argToList(demisto.params().get('custom_fields'))
 
     query = demisto.params().get('query', '') or ''
     disable_from_same_integration = demisto.params().get('disable_from_same_integration')
@@ -839,7 +835,7 @@ def main() -> None:
             return_results(get_remote_data_command(client, demisto.args(), demisto.params()))
 
         elif demisto.command() == 'update-remote-system':
-            return_results(update_remote_system_command(client, demisto.args(), mirror_tags, custom_fields))
+            return_results(update_remote_system_command(client, demisto.args(), mirror_tags))
 
         else:
             raise NotImplementedError('Command not implemented')
