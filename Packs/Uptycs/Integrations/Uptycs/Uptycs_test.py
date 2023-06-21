024e722e116e81acc3a4966ecd30d4b9299e2d95
Chandrasekhar Gadde <cgadde@uptycs.com>
2023-06-22 00:31:49 +0530

    fix lint check failure

--- Packs/Uptycs/Integrations/Uptycs/Uptycs_test.py
+++ Packs/Uptycs/Integrations/Uptycs/Uptycs_test.py
@@ -1,5 +1,6 @@
 import demistomock as demisto
 
+
 def test_uptycs_get_carves(mocker, requests_mock):
     """
     Tests uptycs-get-carves command function.
@@ -22,22 +23,22 @@ def test_uptycs_get_carves(mocker, requests_mock):
         'secret': 'testsecret',
         'domain': 'testdomain.com',
         'customer_id': 'testcustomer',
+        'proxy': False,
         'fetch_time': '7 days'
     })
 
-    mock_response = [{
-            "id": "e037cb0b-e9b0-4061-8966-5d3404cef9f6",
-            "assetId": "2fb29ec9-5c16-4021-af7c-65528fead280",
-            "path": "/etc/hosts",
-            "createdAt": "2023-05-19T06:58:12.304Z",
-            "updatedAt": "2023-05-19T06:58:13.576Z",
-            "status": "FINISHED",
-            "assetHostName": "uptycs-testhost",
-            "offset": 0,
-            "length": 197,
-            "deletedUserName": '',
-            "deletedAt": ''
-    }]
+    mock_response = [{"id": "e037cb0b-e9b0-4061-8966-5d3404cef9f6",
+                      "assetId": "2fb29ec9-5c16-4021-af7c-65528fead280",
+                      "path": "/etc/hosts",
+                      "createdAt": "2023-05-19T06:58:12.304Z",
+                      "updatedAt": "2023-05-19T06:58:13.576Z",
+                      "status": "FINISHED",
+                      "assetHostName": "uptycs-testhost",
+                      "offset": 0,
+                      "length": 197,
+                      "deletedUserName": '',
+                      "deletedAt": ''
+                    }]
     requests_mock.get('https://testdomain.com/public/api/customers/testcustomer/carves', json=mock_response)
 
     response = uptycs_get_carves_source_command()
