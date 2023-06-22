5684ace69c0272550e4a3cfbe11d1bfb3eedfa50
mtraceable <mayuresh@traceable.ai>
2023-06-22 00:30:27 -0700

    Use mocks for tests.

--- Packs/Traceable/Integrations/Traceable/Traceable_test.py
+++ Packs/Traceable/Integrations/Traceable/Traceable_test.py
@@ -12,50 +12,211 @@ You must add at least a Unit Test function for every XSOAR command
 you are implementing with your integration
 """
 
-import json
-import io
 import demistomock as demisto
-import logging
 import pytest
-import warnings
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-logging.captureWarnings(False)
-
-logging.basicConfig(level=logging.INFO,
-                    format="[%(filename)s:%(lineno)s - %(funcName)15s() ] %(asctime)s [%(levelname)s] [%(name)s] [%(threadName)s] %(message)s",
-                    handlers=[logging.StreamHandler()])
+# warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
-def util_load_json(path):
-    with io.open(path, mode='r', encoding='utf-8') as f:
-        return json.loads(f.read())
+def test_fetch_incidents(mocker):
+    def response_handler(*args, **kwargs):
+        data: str = kwargs["json"]["query"]
 
+        r = Response()
+        if "DOMAIN_EVENT" in data:
+            r.text = """{
+  "data": {
+    "explore": {
+      "results": [
+        {
+          "threatCategory": {
+            "value": "null"
+          },
+          "id": {
+            "value": "9dd9261a-23db-472e-9d2a-a4c3227d6502"
+          },
+          "name": {
+            "value": "XSS Filter - Category 1: Script Tag Vector"
+          },
+          "type": {
+            "value": "Cross Site Scripting (XSS)"
+          },
+          "environment": {
+            "value": "Fintech_app"
+          },
+          "serviceName": {
+            "value": "frontend"
+          },
+          "apiName": {
+            "value": "POST /get_user"
+          },
+          "apiId": {
+            "value": "067bb0d7-3740-3ba6-89eb-c457491fbc53"
+          },
+          "serviceId": {
+            "value": "3d67aadf-4605-385d-bd3a-b297789046fd"
+          },
+          "threatActorScore": {
+            "value": -2147483648
+          },
+          "anomalousAttribute": {
+            "value": "default.password"
+          },
+          "eventDescription": {
+            "value": "Matched Data: <script alert(1) /> found within ARGS:password: ${<script alert(1) />}"
+          },
+          "actorId": {
+            "value": "Kacey_Kub@outlook.com"
+          },
+          "actorCountry": {
+            "value": "United States"
+          },
+          "actorIpAddress": {
+            "value": "23.128.248.23"
+          },
+          "actorDevice": {
+            "value": "null"
+          },
+          "apiUri": {
+            "value": "http://frontend:8784/get_user?forwardUrl=http%3A%2F%2Fdummyjon.com"
+          },
+          "traceId": {
+            "value": "a1f93e44b31be69835cfeeac4f181869"
+          },
+          "statusCode": {
+            "value": "200"
+          },
+          "actorEntityId": {
+            "value": "null"
+          },
+          "actorScoreCategory": {
+            "value": "null"
+          },
+          "securityScoreCategory": {
+            "value": "LOW"
+          },
+          "securityScore": {
+            "value": 0
+          },
+          "category": {
+            "value": "SECURITY"
+          },
+          "securityEventType": {
+            "value": "MODSEC"
+          },
+          "ipCategories": {
+            "value": [
+              "IP_LOCATION_TYPE_PUBLIC_PROXY",
+              "IP_LOCATION_TYPE_BOT"
+            ]
+          },
+          "ipReputationLevel": {
+            "value": "CRITICAL"
+          },
+          "ipAbuseVelocity": {
+            "value": "HIGH"
+          },
+          "spanId": {
+            "value": "f7dded93dc8b49c7"
+          },
+          "actorSession": {
+            "value": "00b79cf7-f47a-7903-2b72-f6c3c65ae04e"
+          },
+          "timestamp": {
+            "value": 1687388516786
+          }
+        }
+      ]
+    }
+  }
+}"""
+            return r
+        elif "spans(" in data:
+            r.text = """{
+  "data": {
+    "spans": {
+      "results": [
+        {
+          "id": "f7dded93dc8b49c7",
+          "protocolName": "HTTP",
+          "serviceName": "frontend",
+          "displaySpanName": "POST /get_user",
+          "userIdentifier": "Kacey_Kub@outlook.com",
+          "sessionId": "00b79cf7-f47a-7903-2b72-f6c3c65ae04e",
+          "ipAddress": "23.128.248.23",
+          "userCountry": "United States",
+          "userCity": "Houston",
+          "userRoles": [
+            "customer"
+          ],
+          "statusCode": "200",
+          "errorCount": 0,
+          "duration": 38,
+          "startTime": 1687388481641,
+          "endTime": 1687388481679,
+          "traceId": "a1f93e44b31be69835cfeeac4f181869",
+          "spanTags": {
+            "net.peer.port": "56453",
+            "http.url": "http://frontend:8784/get_user?forwardUrl=http%3A%2F%2Fdummyjon.com",
+            "enduser.role": "customer",
+            "net.peer.ip": "10.18.147.28",
+            "net.host.ip": "10.18.147.30",
+            "traceableai.enriched.api_type": "HTTP",
+            "http.status_code": "200",
+            "enduser.id": "Kacey_Kub@outlook.com",
+            "enduser.id.rule": "0394b434-1def-4b4b-8aa7-c7d03fb8dd57",
+            "span.kind": "server",
+            "traceableai.module.version": "1.0.5",
+            "enduser.role.rule": "0394b434-1def-4b4b-8aa7-c7d03fb8dd57",
+            "servicename": "frontend",
+            "http.method": "POST",
+            "deployment.environment": "Fintech_app",
+            "session.id": "00b79cf7-f47a-7903-2b72-f6c3c65ae04e",
+            "traceableai.module.name": "proxy"
+          },
+          "spanResponseHeaders": {
+            "content-type": "application/json"
+          },
+          "spanResponseCookies": {},
+          "spanRequestBody": "email=Kacey_Kub@outlook.com&password=${<script alert(1) />}",
+          "spanRequestHeaders": {
+            "content-type": "application/json",
+            "x-forwarded-for": "23.128.248.23"
+          },
+          "spanRequestCookies": {}
+        }
+      ]
+    }
+  }
+}"""
+            return r
 
-def test_fetch_incidents(caplog):
-    caplog.set_level(logging.INFO)
+    class Response:
+        def __init__(self) -> None:
+            pass
+
+        status_code = 200
+        text = None
+
     from Traceable import Client, fetch_incidents
     import urllib3
+
     urllib3.disable_warnings()
     headers = {}
-    headers["Authorization"] = 'a8947809ba12872d81c8f38e0531997c523ed84d989c793e7dd0bd33c7fc944d7f382c21881780cd25830c0ad66c0f8a20fe1237495d1e64abb1533c60f10e2a'
     headers["Content-Type"] = "application/json"
     headers["Accept"] = "application/json"
 
-    client = Client(base_url='https://api.eu.traceable.ai', verify=False, headers=headers)
+    client = Client(
+        base_url="https://api.eu.traceable.ai", verify=False, headers=headers
+    )
     client.set_security_score_category_list(["CRITICAL", "HIGH", "MEDIUM", "LOW"])
     # client.set_threat_category_list(threatCategoryList)
     client.set_ip_reputation_level_list(["CRITICAL", "HIGH", "MEDIUM", "LOW"])
     client.set_ip_abuse_velocity_list(["CRITICAL", "HIGH", "MEDIUM", "LOW"])
     client.set_limit(100)
 
-    next_run, incidents = fetch_incidents(client, {"last_fetch": None}, '3 days')
+    mocked_post = mocker.patch("requests.post")
+    mocked_post.side_effect = response_handler
 
-    for i in range(0, 5):
-        logging.info(json.dumps(incidents[i], indent=3))
-
-    demisto.setLastRun(next_run)
-    demisto.incidents(incidents)
-    # next_run, incidents = fetch_incidents(client, next_run, '3 days')
-    a = 123
-    assert a == 123
+    next_run, incidents = fetch_incidents(client, {"last_fetch": None}, "3 days")
+    assert len(incidents) == 1
