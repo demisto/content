0ffd1e9368d0fa615d87c72e1be064441daa22e3
Enes Ozdemir <enes_ozdmr@outlook.com>
2023-06-23 10:59:03 +0300

    drop redirected incidents

--- Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
+++ Packs/Microsoft365Defender/Integrations/Microsoft365Defender/Microsoft365Defender.py
@@ -510,7 +510,7 @@ def fetch_incidents(client: Client, first_fetch_time: str, fetch_limit: int, tim
                 "name": f"Microsoft 365 Defender {incident.get('incidentId')}",
                 "occurred": incident.get('createdTime'),
                 "rawJSON": json.dumps(incident)
-            } for incident in raw_incidents]
+            } for incident in raw_incidents if incident.get("status") != "Redirected" ]
 
             # raw_incidents length is less than MAX_ENTRIES than we fetch all the relevant incidents
             if len(raw_incidents) < int(MAX_ENTRIES):
