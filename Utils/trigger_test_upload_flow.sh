dc9e617910fdb2720a1fa91f6de6f5cd788c8a7c
okarkkatz <okarkkatz@paloaltonetworks.com>
2023-06-27 10:28:49 +0300

    commit

--- Utils/trigger_test_upload_flow.sh
+++ Utils/trigger_test_upload_flow.sh
@@ -70,7 +70,7 @@ while [[ "$#" -gt 0 ]]; do
     shift;;
 
   -gb4|--bucket_xsoar_saas)
-  if [ "$(echo "$2" | tr '[:upper:]' '[:lower:]')" == "marketplace-xsoar-prod-us" ]; then
+  if [ "$(echo "$2" | tr '[:upper:]' '[:lower:]')" == "marketplace-saas-dist" ]; then
     echo "Only test buckets are allowed to use. Using marketplace-saas-dist-dev instead."
   else
     _bucket_xsoar_saas=$2
