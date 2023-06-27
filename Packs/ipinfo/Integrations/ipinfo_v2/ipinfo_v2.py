8917e8f2de754d7caa0d6dd7be9b79eaaa4ce2c5
ilan <ierukhimovic@paloaltonetworks.com>
2023-06-27 16:57:33 +0300

    commit

--- Packs/ipinfo/Integrations/ipinfo_v2/ipinfo_v2.py
+++ Packs/ipinfo/Integrations/ipinfo_v2/ipinfo_v2.py
@@ -3,7 +3,7 @@ from CommonServerUserPython import *  # noqa
 
 import urllib3
 import traceback
-from typing import Dict, Any
+from typing import Any
 
 # Disable insecure warnings
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -20,10 +20,10 @@ class Client(BaseClient):
         self.api_key = api_key
         self.reliability = reliability
 
-    def ipinfo_ip(self, ip: str) -> Dict[str, Any]:
+    def ipinfo_ip(self, ip: str) -> dict[str, Any]:
         return self.http_request(ip)
 
-    def http_request(self, ip: str) -> Dict[str, Any]:
+    def http_request(self, ip: str) -> dict[str, Any]:
         """ constructs url with token (if existent), then returns request """
         return self._http_request(method='GET',
                                   url_suffix=f'{ip}/json',
@@ -48,7 +48,7 @@ def ipinfo_ip_command(client: Client, ip: str) -> List[List[CommandResults]]:
     return ip_results
 
 
-def parse_results(ip: str, raw_result: Dict[str, Any], reliability: str) -> List[CommandResults]:
+def parse_results(ip: str, raw_result: dict[str, Any], reliability: str) -> List[CommandResults]:
     command_results: List[CommandResults] = []
 
     # default values
@@ -169,6 +169,7 @@ def parse_results(ip: str, raw_result: Dict[str, Any], reliability: str) -> List
                                     outputs_key_field=outputs_key_field,
                                     indicator=indicator)
         command_results.append(map_output)
+        
 
     # do not change the order of the calls for CommandResults due to an issue where the ip command would not
     # present all of the information returned from the API.
