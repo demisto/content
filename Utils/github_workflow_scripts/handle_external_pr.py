945e5446e67f7e91ce85dee6e5a7a5527d5dd12c
dorschw <81086590+dorschw@users.noreply.github.com>
2023-06-28 17:49:45 +0300

    fix type hint

--- Utils/github_workflow_scripts/handle_external_pr.py
+++ Utils/github_workflow_scripts/handle_external_pr.py
@@ -142,7 +142,7 @@ def get_packs_support_level_label(file_paths: list[str], external_pr_branch: str
     return get_highest_support_label(packs_support_levels) if packs_support_levels else ''
 
 
-def get_highest_support_label(packs_support_levels: set[str]):
+def get_highest_support_label(packs_support_levels: set[str]) -> str:
     """
     Get the highest support level.
 
