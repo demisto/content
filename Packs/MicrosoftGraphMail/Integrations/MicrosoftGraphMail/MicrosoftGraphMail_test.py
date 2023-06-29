3ebc59d6064068d8079a17d9c8e576c602c588b4
michal-dagan <mdagan@paloaltonetworks.com>
2023-06-29 17:35:50 +0300

    fixes

--- Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py
+++ Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py
@@ -82,9 +82,7 @@ def test_params_working(mocker, params, expected_results):
                                                       emails_fetch_limit=50, timeout=10, endpoint='com',
                                                       certificate_thumbprint='', private_key='',
                                                       display_full_email_body=False, mark_fetched_read=False, look_back=0,
-                                                      managed_identities_client_id=None,
-                                                      command_prefix="msgraph-mail"
-                                                      )
+                                                      managed_identities_client_id=None)
 
 
 def test_build_mail_object():
