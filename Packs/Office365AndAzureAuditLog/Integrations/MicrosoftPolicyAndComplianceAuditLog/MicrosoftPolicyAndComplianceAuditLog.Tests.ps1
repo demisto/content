335ef28a85859811b0a7f349ee32ff60bdb046dd
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-11 23:08:14 +0300

    trial test

--- Packs/Office365AndAzureAuditLog/Integrations/MicrosoftPolicyAndComplianceAuditLog/MicrosoftPolicyAndComplianceAuditLog.Tests.ps1
+++ Packs/Office365AndAzureAuditLog/Integrations/MicrosoftPolicyAndComplianceAuditLog/MicrosoftPolicyAndComplianceAuditLog.Tests.ps1
@@ -1,1 +1,24 @@
-<# I'm here so build will pass #>
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Justification="Known issue in pester")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidTrailingWhitespace", "", Justification="Ignore")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification="Ignore")]
+Param()
+
+BeforeAll {
+    . "$PSScriptRoot/MicrosoftPolicyAndComplianceAuditLog.ps1"
+}
+
+class MockClient : ExchangeOnlinePowershellV3Client {
+    CreateSession() {}
+    DisconnectSession() {}
+}
+
+Mock Search-UnifiedAuditLog {return Get-Content "test_data/audit_log.json" -Raw | ConvertFrom-Json} -Verifiable
+
+$temp = Get-Content "test_data/audit_log.json" -Raw | ConvertFrom-Json
+Describe "SearchAuditLogCommand" {
+    It "Correct params" {
+        $client = MockClient "url" "app_id" "organization" "aGVsbG8=" "password"
+        ($human_readable, $entry_context, $raw_response) = SearchAuditLogCommand $client
+        ($human_readable, $entry_context, $raw_response) | Should -Be ($temp, $null, $null)
+    }
+}
