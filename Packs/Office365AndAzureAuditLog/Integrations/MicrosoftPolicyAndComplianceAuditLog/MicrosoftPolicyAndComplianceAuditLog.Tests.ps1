e86e1b0884ba88a67324402746a64d9f9d510e03
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-12 11:47:58 +0300

    trial test

--- Packs/Office365AndAzureAuditLog/Integrations/MicrosoftPolicyAndComplianceAuditLog/MicrosoftPolicyAndComplianceAuditLog.Tests.ps1
+++ Packs/Office365AndAzureAuditLog/Integrations/MicrosoftPolicyAndComplianceAuditLog/MicrosoftPolicyAndComplianceAuditLog.Tests.ps1
@@ -14,7 +14,19 @@ BeforeAll {
 $temp = Get-Content "test_data/audit_log.json" -Raw | ConvertFrom-Json
 Describe "SearchAuditLogCommand" {
     It "Correct params" {
-        Mock Search-UnifiedAuditLog {return $AUDIT_LOG_JSON} -Verifiable
+        function Search-UnifiedAuditLog {
+            param (
+                [string]$start_date,
+                [string]$end_date,
+                [string]$free_text,
+                [string]$record_type,
+                [String[]]$ip_addresses,
+                [String[]]$operations,
+                [String[]]$user_ids,
+                [int]$result_size
+            )
+            return $AUDIT_LOG_JSON
+        }
         Mock Get-Date {return "Wednesday, July 12, 2023 9:58:19 AM"}
         $client = [ExchangeOnlinePowershellV3Client]::new("url", "app_id", "organization", "aGVsbG8=", "password")
         ($human_readable, $entry_context, $raw_response) = SearchAuditLogCommand $client
