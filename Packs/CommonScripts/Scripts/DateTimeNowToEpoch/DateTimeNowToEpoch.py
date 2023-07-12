fcecdfaf367b75def68734338ea80211a4061308
TerminalFin <github@cantreach.me>
2023-07-12 10:47:43 -0400

    Fix unit tests

--- Packs/CommonScripts/Scripts/DateTimeNowToEpoch/DateTimeNowToEpoch.py
+++ Packs/CommonScripts/Scripts/DateTimeNowToEpoch/DateTimeNowToEpoch.py
@@ -1,5 +1,15 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-# Return current epoch value for datetime.now
-demisto.results(int(time.time()))
+
+def retrieve_epoch() -> int:
+    # Return current epoch value for datetime.now
+    return int(time.time())
+
+
+def main():
+    demisto.results(retrieve_epoch)
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
