577db7ac8ddf5ae107c1af8eff7b1fd2bee97d9f
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-07-12 10:10:23 +0300

    trial test

--- Packs/Office365AndAzureAuditLog/Integrations/MicrosoftPolicyAndComplianceAuditLog/MicrosoftPolicyAndComplianceAuditLog.Tests.ps1
+++ Packs/Office365AndAzureAuditLog/Integrations/MicrosoftPolicyAndComplianceAuditLog/MicrosoftPolicyAndComplianceAuditLog.Tests.ps1
@@ -3,21 +3,20 @@
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification="Ignore")]
 Param()
 
+$AUDIT_LOG_JSON = Get-Content "test_data/audit_log.json" -Raw | ConvertFrom-Json
+
 BeforeAll {
     . "$PSScriptRoot/MicrosoftPolicyAndComplianceAuditLog.ps1"
+    Mock Connect-ExchangeOnline
+    Mock Disconnect-ExchangeOnline
 }
-. "$PSScriptRoot/MicrosoftPolicyAndComplianceAuditLog.ps1"  # temporary
-class MockClient : ExchangeOnlinePowershellV3Client {
-    CreateSession() {}
-    DisconnectSession() {}
-}
-
-Mock Search-UnifiedAuditLog {return Get-Content "test_data/audit_log.json" -Raw | ConvertFrom-Json} -Verifiable
 
 $temp = Get-Content "test_data/audit_log.json" -Raw | ConvertFrom-Json
 Describe "SearchAuditLogCommand" {
     It "Correct params" {
-        $client = MockClient "url" "app_id" "organization" "aGVsbG8=" "password"
+        Mock Search-UnifiedAuditLog {return $AUDIT_LOG_JSON} -Verifiable
+        Mock Get-Date {return "Wednesday, July 12, 2023 9:58:19 AM"}
+        $client = [ExchangeOnlinePowershellV3Client]::new("url", "app_id", "organization", "aGVsbG8=", "password")
         ($human_readable, $entry_context, $raw_response) = SearchAuditLogCommand $client
         ($human_readable, $entry_context, $raw_response) | Should -Be ($temp, $null, $null)
     }
