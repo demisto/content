79e35e4a15c3d4f87b28bab39f31d814d5796940
TerminalFin <github@cantreach.me>
2023-07-11 20:07:36 -0400

    Comments

--- Packs/CommonScripts/Scripts/DateTimeNowToEpoch/DateTimeNowToEpoch.py
+++ Packs/CommonScripts/Scripts/DateTimeNowToEpoch/DateTimeNowToEpoch.py
@@ -3,4 +3,5 @@ from CommonServerPython import *  # noqa: F401
 from datetime import datetime
 import calendar
 
+# Return current epoch value for datetime.now()
 demisto.results(calendar.timegm(datetime.now().timetuple()))
