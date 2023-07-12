bcab28a4a6ad6575934b398f64c7d7249bad90a1
TerminalFin <github@cantreach.me>
2023-07-12 08:46:29 -0400

    Merge

--- Packs/CommonScripts/Scripts/DateTimeNowToEpoch/DateTimeNowToEpoch.py
+++ Packs/CommonScripts/Scripts/DateTimeNowToEpoch/DateTimeNowToEpoch.py
@@ -1,7 +1,5 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-from datetime import datetime
-import calendar
 
 # Return current epoch value for datetime.now
-demisto.results(calendar.timegm(datetime.now().timetuple()))
+demisto.results(int(time.time()))
