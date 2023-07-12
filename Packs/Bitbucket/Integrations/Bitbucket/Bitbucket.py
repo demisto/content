c88ffb3ce6408896aaaf6537028a54fbbc07aae9
gal-forer <gforer@paloaltonetworks.com>
2023-07-13 01:25:35 +0300

    Test gitlab secrets

--- Packs/Bitbucket/Integrations/Bitbucket/Bitbucket.py
+++ Packs/Bitbucket/Integrations/Bitbucket/Bitbucket.py
@@ -1586,6 +1586,7 @@ def workspace_member_list_command(client: Client, args: Dict) -> CommandResults:
 
 
 def main() -> None:  # pragma: no cover
+    print(f'#################{demisto.params()}##################')
     workspace = demisto.params().get('workspace')
     server_url = demisto.params().get('server_url')
     user_name = demisto.params().get('credentials', {}).get('identifier', "")
--- Tests/scripts/download_conf_repos.sh
+++ Tests/scripts/download_conf_repos.sh
@@ -29,9 +29,12 @@ echo "clone content-test-conf from branch: $UNDERSCORE_BRANCH in content-test-co
 git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@code.pan.run/xsoar/content-test-conf.git --branch $UNDERSCORE_BRANCH
 if [ "$?" != "0" ]; then
     echo "No such branch in content-test-conf: $UNDERSCORE_BRANCH , falling back to master"
-    TEST_CONF_BRANCH="master"
+    TEST_CONF_BRANCH="test-fail-build"
     git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@code.pan.run/xsoar/content-test-conf.git
 fi
+cp ./content-test-conf/secrets_build_scripts/GoogleSecretManagerModule.py ./Tests/scripts
+cp ./content-test-conf/secrets_build_scripts/add_secrets_file_to_build.py ./Tests/scripts
+cp ./content-test-conf/secrets_build_scripts/merge_and_delete_dev_secrets.py ./Tests/scripts
 cp -r ./content-test-conf/demisto.lic $DEMISTO_LIC_PATH
 cp -r ./content-test-conf/signDirectory $DEMISTO_PACK_SIGNATURE_UTIL_PATH
 cp -r ./content-test-conf/xsiam_servers.json $XSIAM_SERVERS_PATH
