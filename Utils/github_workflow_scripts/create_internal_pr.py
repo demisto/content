14b44c8c61754dbcdad4f280c1304d67fb1aca95
kgal-pan <kgal@paloaltonetworks.com>
2023-07-12 11:34:07 +0300

    added labels from consts

--- Utils/github_workflow_scripts/create_internal_pr.py
+++ Utils/github_workflow_scripts/create_internal_pr.py
@@ -5,12 +5,13 @@ import json
 import urllib3
 from blessings import Terminal
 from github import Github
-from handle_external_pr import SECURITY_LABEL
+from handle_external_pr import EXTERNAL_LABEL
 
 from utils import get_env_var, timestamped_print
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 print = timestamped_print
+INTERNAL_LABEL = "Internal PR"
 
 
 def main():
@@ -55,8 +56,8 @@ def main():
     print(f'{t.cyan}Internal PR Created - {pr.html_url}{t.normal}')
 
     # labels should already contain the contribution label from the external PR.
-    # We want to replace the 'External' with 'Internal' label
-    labels = [label.name.replace("External PR", "Internal PR") for label in merged_pr.labels]
+    # We want to replace the 'Externa PRl' with 'Internal PR' label
+    labels = [label.name.replace(EXTERNAL_LABEL, INTERNAL_LABEL) for label in merged_pr.labels]
     for label in labels:
         pr.add_to_labels(label)
         print(f'{t.cyan}"{label}" label added to the Internal PR{t.normal}')
