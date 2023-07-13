71a162c53399f4ef624f5233e2295d0a24ba2cad
yucohen <yucohen@paloaltonetworks.com>
2023-07-13 17:04:45 +0300

    added docstring

--- Packs/CommonScripts/Scripts/ConvertXmlFileToJson/ConvertXmlFileToJson.py
+++ Packs/CommonScripts/Scripts/ConvertXmlFileToJson/ConvertXmlFileToJson.py
@@ -3,7 +3,14 @@ from CommonServerPython import *
 import xmltodict
 
 
-def convert_file(entry_id: str, verbose: bool, context_key:str) ->  None:
+def convert_file(entry_id: str, verbose: bool, context_key:str | None) ->  None:
+    """
+    Converts file content crom XML to Json
+    Args:
+        entry_id: The entry id represents the file.
+        verbose: Whether to print the json result to the warroom.
+        context_key: The key to insert the json data to.
+    """
     xml_file = demisto.getFilePath(entry_id).get("path", "")
     with open(xml_file, 'rb') as xml:
         xml_json = xmltodict.parse(xml)
@@ -17,7 +24,7 @@ def main():
     args = demisto.args()
     entry_id = args.get("entryID", "")
     verbose = argToBoolean(args.get("verbose", False))
-    context_key = args.get("contextKey", "")
+    context_key = args.get("contextKey")
     try:
         convert_file(entry_id, verbose, context_key)
     except Exception as e:
