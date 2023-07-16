60b7aea6898f9d256e43cb6ab15e4048c188adcc
gal-forer <gforer@paloaltonetworks.com>
2023-07-17 01:39:40 +0300

    Test

--- Packs/AbuseDB/Integrations/AbuseDB/AbuseDB.py
+++ Packs/AbuseDB/Integrations/AbuseDB/AbuseDB.py
@@ -10,7 +10,7 @@ import requests
 # disable insecure warnings
 urllib3.disable_warnings()
 
-''' GLOBALS v '''
+''' GLOBALS vv '''
 VERBOSE = True
 SERVER = demisto.params().get('server')
 if not SERVER.endswith('/'):
