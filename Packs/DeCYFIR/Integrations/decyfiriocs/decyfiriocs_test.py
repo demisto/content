8d236370d1d0b3199c71922db32d75f87fd0a0f0
mohan <mohan.m@cyfirma.com>
2023-07-17 20:08:38 +0530

    updated decyfir & decyfiriocs integration

--- Packs/DeCYFIR/Integrations/decyfiriocs/decyfiriocs_test.py
+++ Packs/DeCYFIR/Integrations/decyfiriocs/decyfiriocs_test.py
@@ -1,11 +1,75 @@
 from decyfiriocs import Client
 from CommonServerPython import ThreatIntel
 import json
+from requests import Response
 
 
 def util_load_json(path):
     with open(path, encoding='utf-8') as f:
         return json.loads(f.read())
+
+
+def test_get_indicator_or_threatintel_type(mocker):
+    client = Client(
+        base_url='test_url',
+        verify=False,
+        proxy=False,
+    )
+    da = client.get_indicator_or_threatintel_type("[ipv4-addr:value = '0.0.0.0']")
+    assert "IP" == da
+
+
+def test_build_ioc_relationship_obj(mocker):
+    raw_iocs_ti_data = util_load_json('test_data/iocs.json')
+    ta_data = raw_iocs_ti_data['IN_DATA_3']
+    iocs_data = raw_iocs_ti_data['IN_DATA_1']
+
+    client = Client(
+        base_url='test_url',
+        verify=False,
+        proxy=False,
+    )
+    da = client.build_ioc_relationship_obj(iocs_data[0], ta_data[0])
+    assert raw_iocs_ti_data["IN_DATA_5"][0] == da
+
+
+def test_build_threat_actor_relationship_obj(mocker):
+    raw_iocs_ti_data = util_load_json('test_data/iocs_ti.json')
+    ins_data_in = raw_iocs_ti_data['intrusion_set']
+    raw_iocs_data = util_load_json('test_data/iocs.json')
+    ta_data = raw_iocs_data['IN_DATA_3']
+
+    client = Client(
+        base_url='test_url',
+        verify=False,
+        proxy=False,
+    )
+
+    ins_data_out = client.build_threat_intel_indicator_obj(data=ins_data_in[0],
+                                                           tlp_color='tlp_color',
+                                                           feed_tags=['feedTags'],
+                                                           )
+
+    da_re = client.build_threat_actor_relationship_obj(ta_data[0], ins_data_out)
+    assert raw_iocs_data["IN_DATA_6"][0] == da_re
+
+
+def test_convert_decyfir_ti_to_indicator_format(mocker):
+    raw_iocs_ti_data = util_load_json('test_data/iocs_ti.json')
+    raw_ti_data = util_load_json('test_data/iocs.json')
+    ti_data = raw_iocs_ti_data['threat_actors']
+
+    client = Client(
+        base_url='test_url',
+        verify=False,
+    )
+    ti_data_out = client.convert_decyfir_ti_to_indicator_format(decyfir_api_key='api_key', data=ti_data[0],
+                                                                tlp_color='tlp_color',
+                                                                feed_tags=['feedTags'],
+                                                                threat_intel_type=ThreatIntel.ObjectsNames.THREAT_ACTOR,
+                                                                is_data_save=False)
+
+    assert ti_data_out[0] == raw_ti_data['IN_DATA_3'][0]
 
 
 def test_convert_decyfir_ti_to_indicators_formats(mocker):
