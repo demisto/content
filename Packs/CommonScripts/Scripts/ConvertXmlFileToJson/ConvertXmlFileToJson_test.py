a7da8010b36beca374b125a2e7acfc206b6b8644
yucohen <yucohen@paloaltonetworks.com>
2023-07-17 11:44:34 +0300

    fixed lint

--- Packs/CommonScripts/Scripts/ConvertXmlFileToJson/ConvertXmlFileToJson_test.py
+++ Packs/CommonScripts/Scripts/ConvertXmlFileToJson/ConvertXmlFileToJson_test.py
@@ -1,7 +1,7 @@
 import demistomock as demisto
 def test_convert_file(mocker):
     from ConvertXmlFileToJson import convert_file
-    mocker.patch.object(demisto, 'getFilePath', return_value={'path': './TestData/input.xml'})
+    mocker.patch.object(demisto, 'getFilePath', return_value={'path': './test_data/input.xml'})
     res = mocker.patch.object(demisto, 'setContext')
     convert_file(entry_id="mock_entry", verbose=False, context_key="Test")
     expected_result = {"note": {"to": "Tove", "from": "Jani", "heading": "Reminder", "body": "Don't forget me this weekend!"}}
