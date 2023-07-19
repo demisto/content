0a6b6af70f83eff3cf304f9561f5a41111027a0b
ostolero <ostolero@paloaltonetworks.com>
2023-07-19 12:36:42 +0300

    cr fixes

--- Packs/MicrosoftExchangeOnPremise/Integrations/EWSv2/EWSv2_test.py
+++ Packs/MicrosoftExchangeOnPremise/Integrations/EWSv2/EWSv2_test.py
@@ -497,28 +497,31 @@ def test_list_parse_item_as_dict():
 def test_parse_item_as_dict_with_empty_field():
     """
     Given -
-        a Message where effective rights is None.
+        a Message where effective rights is None and other fields are false\empty strings.
 
     When -
         running the parse_item_as_dict function.
 
     Then -
-        effective rights field was removed from response.
+        effective rights field was removed from response other empty\negative fields aren't.
     """
     from EWSv2 import parse_item_as_dict
 
     message = Message(subject='message4',
                       message_id='message4',
                       text_body='Hello World',
-                      body='message4',
+                      body='',
                       datetime_received=EWSDateTime(2021, 7, 14, 13, 10, 00, tzinfo=EWSTimeZone('UTC')),
                       datetime_sent=EWSDateTime(2021, 7, 14, 13, 9, 00, tzinfo=EWSTimeZone('UTC')),
                       datetime_created=EWSDateTime(2021, 7, 14, 13, 11, 00, tzinfo=EWSTimeZone('UTC')),
-                      effective_rights=None
+                      effective_rights=None,
+                      is_read=False
                       )
 
     return_value = parse_item_as_dict(message, False)
     assert 'effective_rights' not in return_value
+    assert return_value['body'] == ''
+    assert return_value['is_read'] is False
 
 
 def test_get_entry_for_object_empty():
