fca5e1d1f9a92822928844aab7d15bed8445288c
efelmandar <efelmandar@paloaltonetworks.com>
2023-07-20 10:27:56 +0000

    Fix review comments

--- Packs/CommonScripts/Scripts/StringSimilarity/StringSimilarity_test.py
+++ Packs/CommonScripts/Scripts/StringSimilarity/StringSimilarity_test.py
@@ -4,15 +4,6 @@ from unittest.mock import patch, MagicMock, call
 from CommonServerPython import CommandResults
 from StringSimilarity import stringSimilarity
 
-
-# @pytest.mark.parametrize(
-#     "similarity_threshold, string_A, string_B, expected_output",
-#     [
-#         (0.5, "Hello", "Hello", True),  # Same strings, should return True
-#         # (0.8, "Hello", "Hola", False),  # Different strings, should return False
-#         # (0.5, "Hello", "Helloworld", True),  # Partial similarity, should return True
-#     ],
-# )
 @pytest.mark.parametrize("first_string, second_string, similarity_threshold, expected_result", [
     ("hello", "hello", 0.9, {
         "StringA": "hello",
