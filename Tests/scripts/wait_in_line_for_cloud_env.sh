3a9a9ff59d43c52f10e6f6058e491677d603016b
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-07-24 16:09:00 +0300

    test

--- Tests/scripts/wait_in_line_for_cloud_env.sh
+++ Tests/scripts/wait_in_line_for_cloud_env.sh
@@ -15,8 +15,11 @@ touch CloudEnvVariables
 #gsutil cp gs://xsoar-ci-artifacts/$GCS_LOCKS_PATH/$TEST_MACHINES_LIST $TEST_MACHINES_LIST # copy TestMachines locally for faster perf
 #XSIAM_SERVERS_PATH=${XSIAM_SERVERS_PATH:-"xsiam_servers.json"}
 #XSIAM_SERVERS_LIST=$(<XSIAM_SERVERS_PATH)
+echo here
 echo $XSIAM_SERVERS_PATH
+echo here2
 echo $XSIAM_SERVERS_LIST
+echo here3
 export NUM_OF_TEST_MACHINES=`sed -n '$=' $TEST_MACHINES_LIST`# reads num of lines in file (this is the num of machines)
 
 TEST_MACHINES_LIST_STRING=`cat $TEST_MACHINES_LIST`
