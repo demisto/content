6f044aadb36ab5f07776f246e87309bfd6038bb0
edik24 <ekatsenelson@paloaltonetworks.com>
2023-07-24 17:18:45 +0300

    Update

--- Utils/github_workflow_scripts/check_if_partner_pack_contributed_by_others.py
+++ Utils/github_workflow_scripts/check_if_partner_pack_contributed_by_others.py
@@ -13,43 +13,67 @@ from github.Repository import Repository
 from utils import get_env_var, timestamped_print, Checkout
 from demisto_sdk.commands.common.tools import get_pack_metadata, get_pack_name
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+print = timestamped_print
+
+PARTNER_APPROVED_LABEL = 'Partner-Approved'
+#COMMUNITY_LABEL = 'Community'
+#PARTNER_LABEL = 'Partner'
+#INTERNAL_LABEL = 'Internal'
+
+def arguments_handler():
+    """ Validates and parses script arguments.
+
+     Returns:
+        Namespace: Parsed arguments object.
+
+     """
+    parser = argparse.ArgumentParser(description='Check if the contribution form needs to be filled.')
+    parser.add_argument('-p', '--pr_number', help='The PR number to check if the contribution form needs to be filled.')
+    parser.add_argument('-g', '--github_token', help='The GitHub token to authenticate the GitHub client.')
+    return parser.parse_args()
 
 def main():
-    """Handles External PRs (PRs from forks)
-
-    Performs the following operations:
-    1. If the external PR's base branch is master we create a new branch and set it as the base branch of the PR.
-    2. Labels the PR with the "Contribution" label. (Adds the "Hackathon" label where applicable.)
-    3. Assigns a Reviewer.
-    4. Creates a welcome comment
-
-    Will use the following env vars:
-    - CONTENTBOT_GH_ADMIN_TOKEN: token to use to update the PR
-    - EVENT_PAYLOAD: json data from the pull_request event
-    """
-    t = Terminal()
-
-    payload_str = get_env_var('EVENT_PAYLOAD')
-    if not payload_str:
-        raise ValueError('EVENT_PAYLOAD env variable not set or empty')
-    payload = json.loads(payload_str)
-    print(f'{t.cyan}Processing PR started{t.normal}')
+    options = arguments_handler()
+    pr_number = options.pr_number
+    github_token = options.github_token
 
     org_name = 'demisto'
     repo_name = 'content'
-    gh = Github(get_env_var('CONTENTBOT_GH_ADMIN_TOKEN'), verify=False)
-    content_repo = gh.get_repo(f'{org_name}/{repo_name}')
 
-    pr_number = payload.get('pull_request', {}).get('number')
-    pr = content_repo.get_pull(pr_number)
+    github_client: Github = Github(github_token, verify=False)
+    content_repo: Repository = github_client.get_repo(f'{org_name}/{repo_name}')
+    pr: PullRequest = content_repo.get_pull(int(pr_number))
+    t = Terminal()
 
-    pr_files = [file.filename for file in pr.get_files()]
-    print(f'{pr_files=} for {pr_number=}')
+    pr_label_names = [label.name for label in pr.labels]
 
-    fork_owner = os.getenv('GITHUB_ACTOR')
-    print(f'fork owner is {fork_owner}')
+    is_contribution_form_filled_label_exist = PARTNER_APPROVED_LABEL in pr_label_names
+    #is_community_label_exist = COMMUNITY_LABEL in pr_label_names
+    #is_partner_label_exist = PARTNER_LABEL in pr_label_names
+    #is_internal_label_exist = INTERNAL_LABEL in pr_label_names
 
-    sys.exit(1)
+    print(f'{t.cyan}Checking if {PARTNER_APPROVED_LABEL} label exist in PR {pr_number}')
+    if not is_contribution_form_filled_label_exist:
+        print(
+            f'{t.red}ERROR: Contribution form was not filled for PR: {pr_number}.\nMake sure to register your'
+            f' contribution by filling the contribution registration form in - https://forms.gle/XDfxU4E61ZwEESSMA'
+        )
+        sys.exit(1)
+
+    print(
+        f'{t.cyan}Checking if one of {COMMUNITY_LABEL}/{PARTNER_LABEL}/{INTERNAL_LABEL} labels exist in PR {pr_number}'
+    )
+    # if not (is_community_label_exist ^ is_partner_label_exist ^ is_internal_label_exist):
+    #     print(
+    #         f'{t.red}ERROR: PR labels {pr_label_names} '
+    #         f'must contain one of {COMMUNITY_LABEL}/{PARTNER_LABEL}/{INTERNAL_LABEL} labels'
+    #     )
+    #     sys.exit(1)
+
+    print(f'{t.cyan}PR labels {pr_label_names} are valid')
+    print(f'{t.cyan} Contribution form was filled successfully for PR: {pr_number}')
+    sys.exit(0)
 
 
 if __name__ == "__main__":
