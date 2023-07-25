ebb2f2f3adb7d140b2f111b89cc46af9d7ea168a
maimorag <mmorag@paloaltonetworks.com>
2023-07-25 12:35:06 +0300

    removing typing.Tuple

--- Packs/CommonScripts/Scripts/IPNetwork/IPNetwork.py
+++ Packs/CommonScripts/Scripts/IPNetwork/IPNetwork.py
@@ -2,7 +2,7 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 # STD imports
 import ipaddress
-from typing import Dict, Tuple
+from typing import Dict
 
 
 # Local imports
