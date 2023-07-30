c66d93c08d4880dbfea521491d6890ed00166b53
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-07-30 15:22:39 +0300

    test

--- Tests/scripts/wait_in_line_for_cloud_env.sh
+++ Tests/scripts/wait_in_line_for_cloud_env.sh
@@ -14,7 +14,8 @@ touch CloudEnvVariables
 
 # Filter out not enabled and unnecessary machines
 echo $CLOUD_SERVERS_FILE
-TEST_MACHINES_LIST=$(jq --arg flow_type "$1" 'to_entries | map(select(.value.enabled == true and .value.flow_type == $flow_type)) | from_entries' "$CLOUD_SERVERS_FILE")
+CLOUD_SERVERS_PATH=$(cat $CLOUD_SERVERS_FILE)
+TEST_MACHINES_LIST=$(jq --arg flow_type "$1" 'to_entries | map(select(.value.enabled == true and .value.flow_type == $flow_type)) | from_entries' "$CLOUD_SERVERS_PATH")
 
 # Get the number of existing machines
 export NUM_OF_TEST_MACHINES=$(echo "$TEST_MACHINES_LIST" | jq 'length')
