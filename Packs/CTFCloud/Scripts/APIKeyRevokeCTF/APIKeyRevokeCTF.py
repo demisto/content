03d4c0e32b7caf60dc2d7ab9d356213ca86384d9
ssokolovich <ssokolovich@paloaltonetworks.com>
2023-07-30 17:50:21 +0300

    Format to the integrations

---
+++ Packs/CTFCloud/Scripts/APIKeyRevokeCTF/APIKeyRevokeCTF.py
@@ -0,0 +1,26 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+import secrets
+
+
+def validate(username):
+    if (username.lower() == "svc_dev"):
+        return "You got it right.Removed previous access keys to this user.\nFlag: keepcalmandstaysecure"
+    else:
+        string="You got it wrong, the attacker still has an access to the organization.\nFlag:"
+        flag =  secrets.token_hex(21)
+        return string + " " + flag
+
+
+
+def main():
+    args = demisto.args()
+    username = args.get('username')
+    try:
+        return_results(validate(username))
+    except Exception as ex:
+        return_error(f'Failed to execute APIKeyRevokeCTF. Error: {str(ex)}')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
