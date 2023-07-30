9f49c92cc87ce3312b6b754ac96c07ecd0be6b4a
michal-dagan <109464765+michal-dagan@users.noreply.github.com>
2023-07-30 20:48:32 +0300

    [MicrosoftAPIModule] - support auth-reset command (#26482)
    * reverted changes from https://github.com/demisto/content/pull/24347
    
    * updated msgs
    
    * updated all the integrations that already have the 'auth-reset' command to use the MicrosoftApiModule.py function
    
    * update rn
    
    * pre commit
    
    * Bump pack from version Microsoft365Defender to 4.5.7.
    
    * updated docker&ruff
    
    * updated docker
    
    * Bump pack from version MicrosoftGraphIdentityandAccess to 1.2.22.
    
    * Bump pack from version MicrosoftTeams to 1.4.20.
    
    * Bump pack from version MicrosoftGraphMail to 1.5.4.
    
    * updated rn
    
    * added the command_prefix parameter
    
    * cr fixes + added integrations
    
    * solved conflicts
    
    * solved conflicts
    
    * solved conflicts API
    
    * added integrations
    
    * added integrations
    
    * fixes
    
    * pre-commit ruff
    
    * fixes
    
    * fixes
    
    * Bump pack from version MicrosoftDefenderAdvancedThreatProtection to 1.15.29.
    
    * Bump pack from version MicrosoftCloudAppSecurity to 2.1.34.
    
    * fixes + ruff
    
    * Bump pack from version MicrosoftDefenderAdvancedThreatProtection to 1.15.30.
    
    * added test + ruff
    
    * update
    
    * Bump pack from version MicrosoftDefenderAdvancedThreatProtection to 1.15.31.
    
    * update
    
    * Bump pack from version MicrosoftDefenderAdvancedThreatProtection to 1.15.32.
    
    * fixed mypy error
    
    * Bump pack from version MicrosoftTeams to 1.4.23.
    
    * added unitest
    
    * Bump pack from version MicrosoftTeams to 1.4.24.
    
    * update rn
    
    * cr fixes
    
    * mypy + resolve conflicts
    
    * mypy + resolve conflicts
    
    * mypy + resolve conflicts
    
    * pre-commit
    
    * Bump pack from version MicrosoftDefenderAdvancedThreatProtection to 1.15.33.
    
    * Bump pack from version MicrosoftExchangeOnline to 1.2.10.
    
    * Bump pack from version AzureSentinel to 1.5.13.
    
    * Bump pack from version Microsoft365Defender to 4.5.8.
    
    * Bump pack from version MicrosoftExchangeOnline to 1.2.11.
    
    * mypy errors
    
    * mypy errors
    
    * mypy errors
    
    * fixed
    
    * fixed
    
    * resolved conflicts + updated dockers
    
    * solved conflicts
    
    * updated tpb
    
    * updated docker + fixed mypy
    
    * revert + skip
    
    * resolved conflicts
    
    * cr fixes
    
    * increased memory_threshold
    
    * resolved conflicts
    
    * resolved conflicts
    
    * updated tpb
    
    * resolved conflicts
    
    * resolved conflicts + updated '_raise_authentication_error' after arad changes
    
    * fixed
    
    * fixed + added test
    
    * updated dockers
    
    * fixed
    
    * updated conf
    
    * Update conf.json
    
    * resolved conflicts
    
    * updated conf.json
    
    * Update conf.json
    
    * Update conf.json
    
    * Bump pack from version MicrosoftExchangeOnline to 1.2.12.
    
    * updated 1_2_12.md
    
    * Update conf.json
    
    * updated conf.json
    
    * Bump pack from version MicrosoftGraphSecurity to 2.1.25.
    
    * updated dockers and docs
    
    * Bump pack from version MicrosoftExchangeOnline to 1.2.13.
    
    * mypy + dockers
    
    * updated rn
    
    * Update Packs/AzureStorage/ReleaseNotes/1_2_16.md
    
    * Update Packs/AzureSentinel/ReleaseNotes/1_5_14.md
    
    * Update Packs/AzureSecurityCenter/ReleaseNotes/2_0_6.md
    
    * Update Packs/AzureNetworkSecurityGroups/ReleaseNotes/1_2_15.md
    
    * Update Packs/AzureKubernetesServices/ReleaseNotes/1_1_16.md
    
    * updated rn
    
    * updated mypy
    
    * mypy+updated conf.json
    
    * revert teams
    
    * resolved conflicts
    
    * Bump pack from version MicrosoftTeams to 1.4.26.
    
    * fixed README.md
    
    * updated docker
    
    * resolved conflicts
    
    * Bump pack from version MicrosoftExchangeOnline to 1.2.14.
    
    ---------
    
    Co-authored-by: Content Bot <bot@demisto.com>
    Co-authored-by: Binat Ziser <89336697+bziser@users.noreply.github.com>

--- Packs/AzureStorage/Integrations/AzureStorage/AzureStorage.py
+++ Packs/AzureStorage/Integrations/AzureStorage/AzureStorage.py
@@ -40,7 +40,8 @@ class ASClient:
             auth_code=auth_code,
             redirect_uri=redirect_uri,
             managed_identities_client_id=managed_identities_client_id,
-            managed_identities_resource_uri=Resources.management_azure
+            managed_identities_resource_uri=Resources.management_azure,
+            command_prefix="azure-storage",
         )
         self.ms_client = MicrosoftClient(**client_args)
         self.subscription_id = subscription_id
@@ -874,12 +875,6 @@ def complete_auth(client: ASClient) -> str:
 def test_connection(client: ASClient) -> str:
     client.ms_client.get_access_token()
     return '✅ Success!'
-
-
-def reset_auth() -> str:
-    set_integration_context({})
-    return 'Authorization was reset successfully. Run **!azure-storage-auth-start** to start the authentication \
-    process.'
 
 
 def test_module(client: ASClient) -> str:
