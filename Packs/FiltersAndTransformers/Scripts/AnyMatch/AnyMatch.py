3161301f6c4a4e4ad99005098b1cfc49ef294f4d
Yehuda <yrosenberg@paloaltonetworks.com>
2023-07-31 23:38:37 +0300

    fixes

--- Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
+++ Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
@@ -4,15 +4,15 @@ from CommonServerPython import *  # noqa: F401
 
 def is_substring_in_list(single_str: str, str_list: list[str]) -> bool:
     """
-    Checks if a string is in a list of strings, also as a substring. (case insensitive)
+    This function checks if a string is in a list of strings, fully or partly, case insensitive.
     Args:
-       single_str: A string to check if it is in the list.
-       str_list: A list of strings to check if the single_str is in it.
+       single_str: A string to check if it exists in the list.
+       str_list: A list of strings to check if the single_str is in it, fully or partly.
     Returns:
         True or False
     """
     lower_list = [x.lower() for x in str_list]
-    return any(single_str.lower() in i for i in lower_list)
+    return any(i in single_str.lower() for i in lower_list)
 
 
 def main():
--- Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch_test.py
+++ Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch_test.py
@@ -2,21 +2,24 @@ import pytest
 import demistomock as demisto
 from AnyMatch import main
 
+# Note: left and right can be all types, and in order to test list as an input we use a string that looks like a list,
+# a comma separated string that will be converted to a list in the script.
+
 
 @pytest.mark.parametrize('left,right, call_count,result', [
-    ("1,2,3", 1, 3, [True, False, False]),
-    ("1,2", "25,10", 2, [True, True]),
-    (1, "1,2,3", 1, [True]),
-    (1, "21", 1, [True]),
-    ("5,1,6,9,65,8", "1,6", 6, [False, True, True, False, False, False]),
-    ('a', "kfjua", 1, [True]),
-    (1, "1", 1, [True]),
-    ('A', "bca", 1, [True]),  # case insensitive
-    ("a", "ABC", 1, [True]),  # case insensitive
-    ("x", "{'alert' {'data': 'x'}}", 1, [True]),
-    ("{'a':1},{'b':2}", "{'a':1,'c':2}", 2, [False, False]),     # {'a':1} is not in {'a':1,'c':2}
-    ("{'a':1},{'b':2}", "{a:1}", 2, [False, False]),     # {'a':1} is not in {a:1}
-    ("'','", "{'a':1,'c':2}", 2, [False, True]),     # '' is not in {'a':1,'c':2}, ' is in {'a':1,'c':2}
+    (123, 1, 1, [True]),
+    ("2", "25,10", 1, [False]),
+    ("1, '2'", "1,2,3", 2, [True, True]),    # a part of '2' is in '1,2,3'
+    ('"abc", "ahah", "a"', "A", 3, [True, True, True]),
+    ("5,1,6,9,65,8,b", "1,'6'", 7, [False, True, False, False, False, False, False]),  # no part of 6 or 65 is in the list: 1,'6'
+    ('a', "kfjua", 1, [False]),
+    (1, "1", 1, [True]),       # int and str are equal
+    ("bca", "A", 1, [True]),  # case insensitive
+    ("ABC", "a", 1, [True]),  # case insensitive
+    ({"alert": {"data": "x"}}, "x", 1, [True]),
+    ("{'a':1,'c':2}", "{'a': 1}, {'b': 2}", 2, [False, False]),     # {'a':1} is not a part of {'a':1, or 'c':2}
+    ("{'a': 1}, {'b': 2}", "{a:1}", 2, [False, False]),  # {a:1} is not a part of {'a': 1} or {'b': 2}
+    ("{'a':1,'c':2}", "'', '", 2, [True, True]),  # although '' is not a part of {'a':1,'c':2}, but ' is in {'a': 1 and in  'c': 2}
 ])
 def test_main(mocker, left, right, call_count, result):
     mocker.patch.object(demisto, 'args', return_value={'left': left, 'right': right})
