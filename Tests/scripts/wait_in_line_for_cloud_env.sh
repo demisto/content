b016806c42f7c42bf1e70b7679c6885652032295
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-08-01 10:54:09 +0300

    changed machines path (#28439)
    * changed machines path
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * unit test
    
    * changed to flatten
    
    * changed to flatten
    
    * pre commit
    
    * changed to field in the machines path
    
    * removed prints
    
    * pulled from master
    
    * changed servers path
    
    * added cloud path to upload
    
    * set build type to build in test upload
    
    * set build type to build in test upload
    
    * test
    
    * changed servers path
    
    * changed servers path
    
    * changed servers path
    
    * test
    
    * test
    
    * removed prints
    
    * CR
    
    * changed copy
    
    * validate and bug fix
    
    * added comma as seperator
    
    * rmoved loggings
    
    * Apply suggestions from code review
    
    Co-authored-by: Darya Koval <72339940+daryakoval@users.noreply.github.com>
    
    * CR
    
    * fix
    
    ---------
    
    Co-authored-by: Darya Koval <72339940+daryakoval@users.noreply.github.com>

--- Tests/scripts/wait_in_line_for_cloud_env.sh
+++ Tests/scripts/wait_in_line_for_cloud_env.sh
@@ -12,10 +12,17 @@ set -e
 
 touch CloudEnvVariables
 
-gsutil cp gs://xsoar-ci-artifacts/$GCS_LOCKS_PATH/$TEST_MACHINES_LIST $TEST_MACHINES_LIST # copy TestMachines locally for faster perf
-export NUM_OF_TEST_MACHINES=`sed -n '$=' $TEST_MACHINES_LIST`	# reads num of lines in file (this is the num of machines)
+# Filter out not enabled and unnecessary machines
+CLOUD_SERVERS_FILE=$(cat $CLOUD_SERVERS_FILE)
 
-TEST_MACHINES_LIST_STRING=`cat $TEST_MACHINES_LIST`
+TEST_MACHINES_LIST=$(jq --arg flow_type "$1" 'to_entries | map(select(.value.enabled == true and .value.flow_type == $flow_type)) | from_entries' "$CLOUD_SERVERS_FILE")
+
+# Get the number of existing machines
+export NUM_OF_TEST_MACHINES=$(echo "$TEST_MACHINES_LIST" | jq 'length')
+echo "Number of available machines is: $NUM_OF_TEST_MACHINES"
+
+# Print all available machines
+TEST_MACHINES_LIST_STRING=$(echo "$TEST_MACHINES_LIST" | jq -r 'keys | join(",")')
 echo "All existing machines: $TEST_MACHINES_LIST_STRING"
 
 if [[ -z $TEST_MACHINES_LIST_STRING ]];
@@ -35,4 +42,4 @@ echo -e "We have $NUM_OF_TEST_MACHINES machines for testing and a lot more build
 echo -e "If we want to make sure our product stays amazing, we will have to work together and keep an orderly queue"
 echo -e "May the tests be in our favour. Good luck to us all"
 
-python3 ./Tests/scripts/lock_cloud_machines.py --service_account $GCS_ARTIFACTS_KEY --gcs_locks_path $GCS_LOCKS_PATH  --ci_job_id $CI_JOB_ID  --test_machines_path "test-machines-ga"  --gitlab_status_token $GITLAB_STATUS_TOKEN --lock_machine_name "$LOCK_MACHINE_NAME" --number_machines_to_lock 1 --response_machine CloudEnvVariables
+python3 ./Tests/scripts/lock_cloud_machines.py --service_account $GCS_ARTIFACTS_KEY --gcs_locks_path $GCS_LOCKS_PATH  --ci_job_id $CI_JOB_ID  --test_machines $TEST_MACHINES_LIST_STRING  --gitlab_status_token $GITLAB_STATUS_TOKEN --lock_machine_name "$LOCK_MACHINE_NAME" --number_machines_to_lock 1 --response_machine CloudEnvVariables
