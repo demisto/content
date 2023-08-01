17f9f05f99912118361815201827deb9e7816b05
edik24 <ekatsenelson@paloaltonetworks.com>
2023-08-01 13:55:38 +0300

    Fix all comments after demo

--- Utils/github_workflow_scripts/check_if_partner_approved_label.py
+++ Utils/github_workflow_scripts/check_if_partner_approved_label.py
@@ -63,25 +63,26 @@ def get_pack_support_level(file_paths: list[str], external_pr_branch: str) -> st
 
     # # we need to check out to the contributor branch in his forked repo in order to retrieve the files cause workflow
     # runs on demisto master while the contributions changes are on the contributors branch
-    print(
-        f'Trying to checkout to forked branch {external_pr_branch} '
-        f'to retrieve support level of {pack_dirs_to_check_support_levels_labels}'
-    )
-    try:
-        fork_owner = os.getenv('GITHUB_ACTOR')
-        with Checkout(
-            repo=Repo(Path().cwd(), search_parent_directories=True),
-            branch_to_checkout=external_pr_branch,
-            # in marketplace contributions the name of the owner should be xsoar-contrib
-            fork_owner=fork_owner if fork_owner != 'xsoar-bot' else 'xsoar-contrib'
-        ):
-            packs_support_levels = get_support_level(pack_dirs_to_check_support_levels_labels)
-    except Exception as error:
-        # in case we were not able to checkout correctly, fallback to the files in the master branch to retrieve support labels
-        # in case those files exist.
-        print(f'Received error when trying to checkout to {external_pr_branch} \n{error=}')
-        print('Trying to retrieve support levels from the master branch')
-        packs_support_levels = get_support_level(pack_dirs_to_check_support_levels_labels)
+    # print(
+    #     f'Trying to checkout to forked branch {external_pr_branch} '
+    #     f'to retrieve support level of {pack_dirs_to_check_support_levels_labels}'
+    # )
+    packs_support_levels = get_support_level(pack_dirs_to_check_support_levels_labels)
+    # try:
+    #     fork_owner = os.getenv('GITHUB_ACTOR')
+    #     with Checkout(
+    #         repo=Repo(Path().cwd(), search_parent_directories=True),
+    #         branch_to_checkout=external_pr_branch,
+    #         # in marketplace contributions the name of the owner should be xsoar-contrib
+    #         fork_owner=fork_owner if fork_owner != 'xsoar-bot' else 'xsoar-contrib'
+    #     ):
+    #         packs_support_levels = get_support_level(pack_dirs_to_check_support_levels_labels)
+    # except Exception as error:
+    #     # in case we were not able to checkout correctly, fallback to the files in the master branch to retrieve support labels
+    #     # in case those files exist.
+    #     print(f'Received error when trying to checkout to {external_pr_branch} \n{error=}')
+    #     print('Trying to retrieve support levels from the master branch')
+    #     packs_support_levels = get_support_level(pack_dirs_to_check_support_levels_labels)
 
     return packs_support_levels
 
@@ -105,14 +106,18 @@ def main():
     print (f'support level is: {support_level}')
     partner_approved = PARTNER_APPROVED_LABEL in pr_label_names
 
-    print(f'{t.cyan}Checking if {PARTNER_APPROVED_LABEL} label exist in PR {pr_number}')
-    if not partner_approved:
-        print(
-            f'{t.red}ERROR: Label Partner-Approved was not added to PR: {pr_number}')
-        sys.exit(1)
+    if "partner" not in support_level:
+        print("PR is not from partner, approving the flow and exiting")
+        sys.exit(0)
+    else:
+        print(f'{t.cyan}Checking if {PARTNER_APPROVED_LABEL} label exist in PR {pr_number}')
+        if not partner_approved:
+            print(
+                f'{t.red}ERROR: Label Partner-Approved was not added to PR: {pr_number}')
+            sys.exit(1)
 
-    print(f'{t.cyan}PR labels {pr_label_names} are valid for PR: {pr_number}')
-    sys.exit(0)
+        print(f'{t.cyan}PR labels {pr_label_names} are valid for PR: {pr_number}')
+        sys.exit(0)
 
 
 if __name__ == "__main__":
