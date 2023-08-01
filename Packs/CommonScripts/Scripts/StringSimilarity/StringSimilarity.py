bb49d31c031046ba208a81fee5348ba4652dbd8b
Erez FelmanDar <102903097+efelmandar@users.noreply.github.com>
2023-08-01 10:45:07 +0200

    Similarity list bugfix (#28598)
    * Similarity list bugfix
    
    * Remove unused variables
    
    * Fix review comments
    
    * Update release notes
    
    * Bump pack from version CommonScripts to 1.12.9.
    
    * Bump pack from version CommonScripts to 1.12.10.
    
    ---------
    
    Co-authored-by: Content Bot <bot@demisto.com>

--- Packs/CommonScripts/Scripts/StringSimilarity/StringSimilarity.py
+++ Packs/CommonScripts/Scripts/StringSimilarity/StringSimilarity.py
@@ -3,48 +3,51 @@ from CommonServerPython import *  # noqa: F401
 import difflib
 
 
-def stringSimilarity(firstString: str, second_string: str, similarityThreshold: float):
+def stringSimilarity(first_string: str, second_string: str, similarity_threshold: float):
     """
     Calculate the similarity score between two strings using the SequenceMatcher.
 
-    The function calculates the similarity ratio between the provided 'firstString' and 'secondString'
+    The function calculates the similarity ratio between the provided 'first_string' and 'second_string'
     using the SequenceMatcher from the 'difflib' library. If the similarity ratio is greater than or equal
-    to the specified 'similarityThreshold', a CommandResults object containing the similarity score
+    to the specified 'similarity_threshold', a CommandResults object containing the similarity score
     and the input strings is returned.
 
     Args:
-        firstString (str): The first string to compare.
+        first_string (str): The first string to compare.
         second_string (str): The second string to compare.
-        similarityThreshold (float): The minimum similarity threshold required for a match.
+        similarity_threshold (float): The minimum similarity threshold required for a match.
 
     Returns:
         CommandResults: A CommandResults object with the similarity score and input strings if the
-                        similarity ratio is greater than or equal to the 'similarityThreshold'.
+                        similarity ratio is greater than or equal to the 'similarity_threshold'.
 
     Raises:
-        ValueError: If the similarity ratio is below the 'similarityThreshold', a ValueError is raised
+        ValueError: If the similarity ratio is below the 'similarity_threshold', a ValueError is raised
                     with a message indicating that no similarity score is calculated.
     """
-    similarity_ratio = difflib.SequenceMatcher(None, firstString, second_string).ratio()
-    if similarity_ratio >= similarityThreshold:
-        commandResults = CommandResults("StringSimilarity", "SimilarityScore", {
-            "StringA": firstString,
+
+    similarity_ratio = difflib.SequenceMatcher(None, first_string, second_string).ratio()
+    if similarity_ratio >= float(similarity_threshold):
+        results = {
+            "StringA": first_string,
             "StringB": second_string,
             "SimilarityScore": similarity_ratio
-        })
-        return commandResults
+        }
 
-    # Raise an exception if none of the conditions are met.
-    return ValueError("No similarity score calculated. Check the similarityThreshold value.")
+    return CommandResults("StringSimilarity", ["StringA", "StringB"], results)
 
 
 def main():
-    similarity_threshold = float(demisto.getArg('similarity_threshold'))
+    similarity_threshold = demisto.getArg('similarity_threshold')
     first_string = demisto.getArg('string_A')
     second_string = demisto.getArg('string_B')
-    commandResults = stringSimilarity(first_string, second_string, similarity_threshold)
 
-    return_results(commandResults)
+    try:
+        results = stringSimilarity(first_string, second_string, similarity_threshold)
+
+        return_results(results)
+    except Exception as e:
+        return_error(f'Failed to check string similarity. Problem: {str(e)}')
 
 
 if __name__ in ["__builtin__", "builtins", '__main__']:
