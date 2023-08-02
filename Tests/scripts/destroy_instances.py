c744a0c774d42460f6200da64b2696481f17951d
kobymeir <ymeir@paloaltonetworks.com>
2023-08-02 07:58:07 +0300

    destroy instances fixes

--- Tests/scripts/destroy_instances.py
+++ Tests/scripts/destroy_instances.py
@@ -4,18 +4,18 @@ import logging
 import os
 from pathlib import Path
 import subprocess
-from typing import Optional
 from demisto_sdk.commands.test_content.constants import SSH_USER
 from Tests.scripts.utils.log_util import install_logging
 
 # Disable insecure warnings
 import urllib3
+
 urllib3.disable_warnings()
 
 
 def options_handler():
-    parser = argparse.ArgumentParser(description='Utility for instantiating and testing integration instances')
-    parser.add_argument('--aritfacts-dir', help='Path to the artifacts directory', required=True)
+    parser = argparse.ArgumentParser(description='Utility for destroying integration test instances')
+    parser.add_argument('--artifacts-dir', help='Path to the artifacts directory', required=True)
     parser.add_argument('--instance-role', help='The instance role', required=True)
     parser.add_argument('--env-file', help='The env_results.json file')
     return parser.parse_args()
@@ -24,28 +24,28 @@ def options_handler():
 def chmod_logs(server_ip: str):
     try:
         logging.debug(f'Changing permissions of folder /var/log/demisto on server {server_ip}')
-        subprocess.check_output(f'ssh {SSH_USER}@{server_ip} "sudo chmod -R 755 /var/log/demisto"', shell=True)
+        subprocess.check_output(f'ssh {SSH_USER}@{server_ip} "sudo chmod -R 755 /var/log/demisto"', shell=True)  # noqa: S602
 
     except subprocess.CalledProcessError:
         logging.exception(f'Failed changing permissions of folder /var/log/demisto on server {server_ip}')
 
 
-def download_logs(server_ip: str, aritfacts_dir: str, role: str):
+def download_logs(server_ip: str, artifacts_dir: str, role: str):
     scp_string = f"scp {SSH_USER}@{server_ip}:/var/log/demisto/server.log " \
-        f"{aritfacts_dir}/server_{role}_{server_ip}.log || echo 'WARN: Failed downloading server.log'"
+                 f"{artifacts_dir}/server_{role}_{server_ip}.log || echo 'WARN: Failed downloading server.log'"
     try:
         logging.debug(f'Downloading server logs from server {server_ip}')
-        subprocess.check_output(scp_string, shell=True)
+        subprocess.check_output(scp_string, shell=True)  # noqa: S602
 
     except subprocess.CalledProcessError:
         logging.exception(f'Failed downloading server logs from server {server_ip}')
 
 
-def shutdown(server_ip: str, ttl: Optional[int] = None):
+def shutdown(server_ip: str, ttl: int | None = None):
     try:
         logging.info(f'Destroying instance with IP - {server_ip}')
         shutdown_command = f'sudo shutdown +{ttl}' if ttl else 'sudo shutdown'
-        subprocess.check_output(f'ssh {SSH_USER}@{server_ip} "{shutdown_command}"', shell=True)
+        subprocess.check_output(f'ssh {SSH_USER}@{server_ip} "{shutdown_command}"', shell=True)  # noqa: S602
 
     except subprocess.CalledProcessError:
         logging.exception(f'Failed to shutdown server {server_ip}')
@@ -57,7 +57,7 @@ def main():
     time_to_live = os.getenv('TIME_TO_LIVE')
     tests_path = Path('./Tests')
 
-    with open(options.env_file, 'r') as json_file:
+    with open(options.env_file) as json_file:
         env_results = json.load(json_file)
 
     for env in filter(lambda x: x["Role"] == options.instance_role, env_results):
@@ -67,16 +67,16 @@ def main():
 
         logging.info(f'Downloading server log from {readable_role}')
         chmod_logs(server_ip)
-        download_logs(server_ip, options.aritfacts_dir, role)
+        download_logs(server_ip, options.artifacts_dir, role)
 
-    if time_to_live:
-        logging.info(f'Time to live was set to {time_to_live} minutes')
-        shutdown(server_ip, int(time_to_live))
-    elif (tests_path / f'is_build_passed_{role}.txt').exists() and \
-            (tests_path / f'is_post_update_passed_{role}.txt').exists():
-        shutdown(server_ip)
-    else:
-        logging.warning(f'Tests for some integration failed on {readable_role}, keeping instance alive')
+        if time_to_live:
+            logging.info(f'Time to live was set to {time_to_live} minutes')
+            shutdown(server_ip, int(time_to_live))
+        elif (tests_path / f'is_build_passed_{role}.txt').exists() and \
+                (tests_path / f'is_post_update_passed_{role}.txt').exists():
+            shutdown(server_ip)
+        else:
+            logging.warning(f'Tests for some integration failed on {readable_role}, keeping instance alive')
 
 
 if __name__ == "__main__":
