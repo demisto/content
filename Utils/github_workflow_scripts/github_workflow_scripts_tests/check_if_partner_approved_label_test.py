834499a36d60062f9d355858644e6e835971d8bd
edik24 <ekatsenelson@paloaltonetworks.com>
2023-08-02 15:04:55 +0300

    Fix all comments after demo

--- Utils/github_workflow_scripts/github_workflow_scripts_tests/check_if_partner_approved_label_test.py
+++ Utils/github_workflow_scripts/github_workflow_scripts_tests/check_if_partner_approved_label_test.py
@@ -1,19 +1,18 @@
 import pytest
 
+
 @pytest.mark.parametrize(
     'support_levels, expected_support_level', [
         (
             ['Utils/github_workflow_scripts/github_workflow_scripts_tests/test_files/Packs/Pack1/pack_metadata.json'],
             {'xsoar'}
         ),
-(
+        (
             ['Utils/github_workflow_scripts/github_workflow_scripts_tests/test_files/Packs/Pack2/pack_metadata.json'],
             {'partner'}
         ),
     ]
 )
-
-
 def test_get_support_level(support_levels, expected_support_level):
     """
         Given:
@@ -27,5 +26,3 @@ def test_get_support_level(support_levels, expected_support_level):
     """
     from Utils.github_workflow_scripts.check_if_partner_approved_label import get_support_level
     assert get_support_level(support_levels) == expected_support_level
-
-
