f78845e31e8dc3b57c96d910d287d02bce20978d
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-08-03 15:34:34 +0300

    test

--- Tests/scripts/validate_synced_buckets.sh
+++ Tests/scripts/validate_synced_buckets.sh
@@ -5,7 +5,6 @@ function get_revision_field() {
   bucket=$1
   json_file_path="/content/packs/index.json"
 
-  echo "gs://$bucket$json_file_path"
   # Download the JSON file from the bucket and extract the revision field using jq
 
   revision=$(gsutil cat "gs://$bucket$json_file_path" | jq -r '.revision')
@@ -15,6 +14,7 @@ function get_revision_field() {
 function compare_revision() {
   bucket_list_origin=("gs://marketplace-dist" "gs://marketplace-v2-dist" "gs://xpanse-dist")
   bucket_list_prod=("gs://marketplace-xsoar" "gs://marketplace-xsiam" "gs://marketplace-xpanse")
+  json_file_path="/content/packs/index.json"
 
   # Compare the revision fields for each pair of buckets
   for ((i = 0; i < ${#bucket_list_origin[@]}; i++)); do
@@ -22,10 +22,8 @@ function compare_revision() {
     bucket2="${bucket_list_prod[$i]}-$1"
 
     echo "Comparing revisions for $bucket1 and $bucket2"
-
-    # Get the revision fields from the JSON files in the buckets
-    revision1=$(get_revision_field "$bucket1")
-    revision2=$(get_revision_field "$bucket2")
+    revision1=$(gsutil cat "gs://$bucket1$json_file_path" | jq -r '.revision')
+    revision2=$(gsutil cat "gs://$bucket2$json_file_path" | jq -r '.revision')
 
     # Compare the revisions
     if [ "$revision1" = "$revision2" ]; then
