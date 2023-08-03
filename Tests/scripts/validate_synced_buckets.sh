ca84e2348566bf7eec9148d10fa4410f6e614ed3
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-08-03 15:51:10 +0300

    test

--- Tests/scripts/validate_synced_buckets.sh
+++ Tests/scripts/validate_synced_buckets.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-# Function to get the revision field from a JSON file in a bucket
-function get_revision_field() {
-  bucket=$1
-  json_file_path="/content/packs/index.json"
-
-  # Download the JSON file from the bucket and extract the revision field using jq
-
-  revision=$(gsutil cat "gs://$bucket$json_file_path" | jq -r '.revision')
-  echo "$revision"
-}
 
 function compare_revision() {
-  bucket_list_origin=("gs://marketplace-dist" "gs://marketplace-v2-dist" "gs://xpanse-dist")
-  bucket_list_prod=("gs://marketplace-xsoar" "gs://marketplace-xsiam" "gs://marketplace-xpanse")
+  bucket_list_origin=("marketplace-dist" "marketplace-v2-dist" "xpanse-dist")
+  bucket_list_prod=("marketplace-xsoar" "marketplace-xsiam" "marketplace-xpanse")
   json_file_path="/content/packs/index.json"
 
   # Compare the revision fields for each pair of buckets
@@ -22,6 +12,7 @@ function compare_revision() {
     bucket2="${bucket_list_prod[$i]}-$1"
 
     echo "Comparing revisions for $bucket1 and $bucket2"
+
     revision1=$(gsutil cat "gs://$bucket1$json_file_path" | jq -r '.revision')
     revision2=$(gsutil cat "gs://$bucket2$json_file_path" | jq -r '.revision')
 
