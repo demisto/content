2d9a014cb7f8e8efd64e13d8b6aee99656efa974
Gal Nakash <gal@recolabs.ai>
2023-08-03 13:35:28 +0300

    packs/Reco: added test
    Signed-off-by: Gal Nakash <gal@recolabs.ai>

--- Packs/Reco/Integrations/Reco/Reco.py
+++ Packs/Reco/Integrations/Reco/Reco.py
@@ -1130,7 +1130,7 @@ def fetch_incidents(
         incident
         for incident in incidents
         if (incident.get("severity", 0) > DEMISTO_INFORMATIONAL)
-           and (incident.get("dbotMirrorId", None) not in existing_incidents)
+        and (incident.get("dbotMirrorId", None) not in existing_incidents)
     ]  # type: ignore
 
     incidents_sorted = sorted(incidents, key=lambda k: k["occurred"])
