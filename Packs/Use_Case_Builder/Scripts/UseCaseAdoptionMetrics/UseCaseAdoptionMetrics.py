eaa64c02d671d2e5d69d3565bf905bd0b83b87e6
Joe Cosgrove <joecosgrove5@gmail.com>
2023-08-03 11:00:46 -0400

    changes

--- Packs/Use_Case_Builder/Scripts/UseCaseAdoptionMetrics/UseCaseAdoptionMetrics.py
+++ Packs/Use_Case_Builder/Scripts/UseCaseAdoptionMetrics/UseCaseAdoptionMetrics.py
@@ -1,8 +1,9 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-
 # Function to check if there is at least one incident with the "Phishing" type
+
+
 def check_phishing_incidents():
     incident_type = "Phishing"
     res = demisto.executeCommand("getIncidents", {"type": incident_type, "size": 1})
@@ -13,10 +14,7 @@ def check_phishing_incidents():
 def is_rapid_breach_response_installed():
     res = demisto.executeCommand("core-api-get", {"uri": "/contentpacks/metadata/installed"})
     installed_packs = res[0]["Contents"]["response"]
-    for pack in installed_packs:
-        if pack["name"] == "Rapid Breach Response":
-            return True
-    return False
+    return any(pack["name"] == "Rapid Breach Response" for pack in installed_packs)
 
 
 modules = demisto.getModules()
@@ -26,19 +24,15 @@ at_risk = []
 
 phishing_incidents = check_phishing_incidents()
 
-for module, details in modules.items():
-    if not details.get('brand').lower() == 'builtin' and details.get('state') == 'active' and details.get(
-            'category') != 'Utilities':
+for _module, details in modules.items():
+    if details.get("brand").lower() != "builtin" and details.get('state') == 'active' and details.get('category') != 'Utilities':
         if details.get('category') == 'Forensic & Malware Analysis' or details.get('category') == 'Endpoint':
             use_cases_in_production.add('Ransomware & Malware Coverage')
-        elif details.get('category') in ['Email', 'Messaging',
-                                         'Messaging and Conferencing'] and 'phishing' in details.get('incident_types',
-                                                                                                     []):
+        elif details.get('category') in ['Email', 'Messaging', 'Messaging and Conferencing'] and 'phishing' in details.get('incident_types', []):
             if phishing_incidents:
                 use_cases_in_production.add('Business Email Compromise Coverage')
             else:
-                at_risk.append('[Business Email Compromise Coverage]('
-                               'https://xsoar.pan.dev/marketplace/?category=Email%2C%20Messaging)')
+                at_risk.append('[Business Email Compromise Coverage](https://xsoar.pan.dev/marketplace/?category=Email%2C%20Messaging)')
         elif details.get('category') == 'Network Security':
             use_cases_in_production.add('Network Security')
         elif details.get('category') == 'Analytics & SIEM':
@@ -54,16 +48,14 @@ if is_rapid_breach_response_installed():
     use_cases_in_production.add('Rapid Breach Response')
 
 use_case_dict = {
-    'Ransomware & Malware Coverage': 'https://xsoar.pan.dev/marketplace/?category=Endpoint%2C%20Forensics%20%26'
-                                     '%20Malware%20Analysis',
-    'Business Email Compromise Coverage': 'https://xsoar.pan.dev/marketplace/?category=Email%2C%20Messaging',
+    'Ransomware & Malware Coverage': 'https://cortex.marketplace.pan.dev/marketplace/?useCase=Malware',
+    'Business Email Compromise Coverage': 'https://cortex.marketplace.pan.dev/marketplace/?useCase=Phishing',
     'Network Security': 'https://xsoar.pan.dev/marketplace/?category=Network%20Security',
     'Analytics & SIEM': 'https://cortex.marketplace.pan.dev/marketplace/?category=Analytics+%26+SIEM',
-    'Data Enrichment & Threat Intelligence': 'https://cortex.marketplace.pan.dev/marketplace/?category=Data'
-                                             '+Enrichment+%26+Threat+Intelligence',
+    'Data Enrichment & Threat Intelligence': 'https://cortex.marketplace.pan.dev/marketplace/?category=Data+Enrichment+%26+Threat+Intelligence',
     'Vulnerability Management': 'https://xsoar.pan.dev/marketplace/?category=Vulnerability%20Management',
     'Case Management': 'https://cortex.marketplace.pan.dev/marketplace/?category=Case+Management',
-    'Rapid Breach Response': 'https://xsoar.pan.dev/marketplace/?category=Security%20Operations'
+    'Rapid Breach Response': 'https://cortex.marketplace.pan.dev/marketplace/details/MajorBreachesInvestigationandResponse/'
 }
 
 for use_case, url in use_case_dict.items():
