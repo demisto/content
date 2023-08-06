87697e5999db67fc066eced3666697fcf524cea1
moishce <mgalitzki@paloaltonetworks.com>
2023-08-06 17:43:01 +0300

    changed FORCE_PACK_UPLOAD to FORCE_BUCKET_UPLOAD

--- Tests/scripts/prepare_content_packs_for_testing.sh
+++ Tests/scripts/prepare_content_packs_for_testing.sh
@@ -84,7 +84,7 @@ else
 fi
 echo "BUCKET_UPLOAD = $BUCKET_UPLOAD, FORCE_BUCKET_UPLOAD = $FORCE_BUCKET_UPLOAD"
 
-if [ ! -f "${BUCKET_UPLOAD}" ] && [ ! -f  "${FORCE_BUCKET_UPLOAD}" ]; then
+if [ -z "${BUCKET_UPLOAD}" ] && [ -z "${FORCE_BUCKET_UPLOAD}" ]; then
   echo "Updating the following content packs: $CONTENT_PACKS_TO_UPLOAD ..."
   python3 ./Tests/Marketplace/upload_packs.py -pa $PACK_ARTIFACTS -d $ARTIFACTS_FOLDER/packs_dependencies.json -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s "$GCS_MARKET_KEY" -n "$CI_PIPELINE_ID" -p $CONTENT_PACKS_TO_UPLOAD -o false -sb $BUILD_BUCKET_PACKS_DIR_PATH -k $PACK_SIGNING_KEY -rt true -bu false -c $CI_COMMIT_BRANCH -f false -dz "$CREATE_DEPENDENCIES_ZIP" -mp "$MARKETPLACE_TYPE"
   echo "Finished updating content packs successfully."
