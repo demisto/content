5f894549e7b3afe34c42bb3bbe8a57005a1d636b
ostolero <ostolero@paloaltonetworks.com>
2023-08-06 16:23:23 +0300

    validate pack

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -4,9 +4,13 @@ set -ex
 echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_README_VALIDATION"
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
     if [ -n "${PACKS_TO_UPLOAD}" ]
+        PACKS_LIST=""
+        for item in $PACKS_TO_UPLOAD; do
+        PACKS_LIST=$PACKS_LIST"Packs/"$item" "
+        done
         echo "Packs upload - Validating only the supplied packs"
-        var PACKS_PATHS 
-        python3 -m demisto_sdk validate -i  --post-commit --graph --skip-pack-dependencies
+        echo "Packs path "$PACKS_LIST
+        python3 -m demisto_sdk validate -i $PACKS_LIST --post-commit --graph --skip-pack-dependencies
     python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
     python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
