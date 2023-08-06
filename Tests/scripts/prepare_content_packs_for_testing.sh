47de1b262ca1c26836fea829eca3b50e714bcda0
moishce <mgalitzki@paloaltonetworks.com>
2023-08-06 16:51:53 +0300

    changed FORCE_PACK_UPLOAD to FORCE_BUCKET_UPLOAD

--- Tests/scripts/prepare_content_packs_for_testing.sh
+++ Tests/scripts/prepare_content_packs_for_testing.sh
@@ -82,9 +82,9 @@ else
   fi
 
 fi
-echo "BUCKET_UPLOAD = $BUCKET_UPLOAD, FORCE_PACK_UPLOAD = $FORCE_PACK_UPLOAD"
+echo "BUCKET_UPLOAD = $BUCKET_UPLOAD, FORCE_BUCKET_UPLOAD = $FORCE_BUCKET_UPLOAD"
 
-if [ ! -f "${BUCKET_UPLOAD}" ] && [ ! -f  "${FORCE_PACK_UPLOAD}" ]; then
+if [ ! -f "${BUCKET_UPLOAD}" ] && [ ! -f  "${FORCE_BUCKET_UPLOAD}" ]; then
   echo "Updating the following content packs: $CONTENT_PACKS_TO_UPLOAD ..."
   python3 ./Tests/Marketplace/upload_packs.py -pa $PACK_ARTIFACTS -d $ARTIFACTS_FOLDER/packs_dependencies.json -e $EXTRACT_FOLDER -b $GCS_BUILD_BUCKET -s "$GCS_MARKET_KEY" -n "$CI_PIPELINE_ID" -p $CONTENT_PACKS_TO_UPLOAD -o false -sb $BUILD_BUCKET_PACKS_DIR_PATH -k $PACK_SIGNING_KEY -rt true -bu false -c $CI_COMMIT_BRANCH -f false -dz "$CREATE_DEPENDENCIES_ZIP" -mp "$MARKETPLACE_TYPE"
   echo "Finished updating content packs successfully."
@@ -93,7 +93,7 @@ else
   REMOVE_PBS=true
   BUCKET_UPLOAD_FLOW=true
   GCS_PRIVATE_BUCKET="marketplace-dist-private"
-  if [ -n "${FORCE_PACK_UPLOAD}" ] && [ -n "${PACKS_TO_UPLOAD}" ]; then
+  if [ -n "${FORCE_BUCKET_UPLOAD}" ] && [ -n "${PACKS_TO_UPLOAD}" ]; then
     # In case the workflow is force upload, we override the forced packs
     echo "Force uploading to production the following packs: ${PACKS_TO_UPLOAD}"
     OVERRIDE_ALL_PACKS=true
