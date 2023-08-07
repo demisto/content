0e1b05f70e8c06319cfb1bb907df8809a4be39e4
arikday <aday@paloaltonetworks.com>
2023-08-07 17:37:30 +0300

    Validation Fixes

--- Packs/ProactiveThreatHunting/Scripts/HuntingFromIndicatorLayout/HuntingFromIndicatorLayout.py
+++ Packs/ProactiveThreatHunting/Scripts/HuntingFromIndicatorLayout/HuntingFromIndicatorLayout.py
@@ -1,25 +1,22 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-
-
-def main():
-    dArgs = demisto.args()
-    if "indicator" in dArgs:
-        sdo = dArgs.get("indicator").get("value")
-    else:
-        sdo = dArgs.get("sdo")
+def HuntingFromIndicatorLayout(sdo):
     try:
         results = demisto.executeCommand("createNewIncident", {"name": f"Threat Hunting Session - {sdo}",
                                                                "sdoname": f"{sdo}",
                                                                "type": "Proactive Threat Hunting"})
-    except Exception:
-        return_results("Error - Please install Proactive Threat Hunting pack to support this feature")
+    except Exception as e:
+        raise DemistoException(f'Failed to create hunting session: {str(e)}')
 
-    result = CommandResults(
+    return CommandResults(
         readable_output=f"Proactive Threat Hunting Incident Created: Threat Hunting Session - {sdo}"
     )
 
-    return_results(result)
+def main():
+    args = demisto.args()
+    if "indicator" not in args:
+        raise DemistoException("error")
+    return_results(HuntingFromIndicatorLayout(args.get("indicator", "").get("value")))
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
