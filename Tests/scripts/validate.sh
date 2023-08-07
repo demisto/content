c4ea1b2e32f3c7cff431bb0b3cdfd029eb63a39a
ostolero <ostolero@paloaltonetworks.com>
2023-08-07 17:26:44 +0300

    fix validate.sh

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -4,15 +4,11 @@ set -ex
 echo "CI_COMMIT_BRANCH: $CI_COMMIT_BRANCH CI: $CI DEMISTO_README_VALIDATION: $DEMISTO_README_VALIDATION"
 if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_UPLOAD}" ]] || [[ -n "${DEMISTO_SDK_NIGHTLY}" ]]; then
     if [[ -n "${PACKS_TO_UPLOAD}" ]]; then
-        PACKS_LIST=""
+        echo "Packs upload - Validating only the supplied packs"
         PACKS_TO_UPLOAD_SPACED=${PACKS_TO_UPLOAD//,/ }
         for item in $PACKS_TO_UPLOAD_SPACED; do
-            PACKS_LIST=$PACKS_LIST"Packs/"$item" "
-        done
-        PACKS_LIST=${PACKS_LIST%?} # remove last space
-        echo "Packs upload - Validating only the supplied packs"
-        echo "Packs path "$PACKS_LIST
-        python3 -m demisto_sdk validate -i $PACKS_LIST --post-commit --graph --skip-pack-dependencies
+            python3 -m demisto_sdk validate -i Packs/"$item" --post-commit --graph --skip-pack-dependencies
+        done       
     else 
         python3 -m demisto_sdk validate -a --post-commit --graph --skip-pack-dependencies
     fi
