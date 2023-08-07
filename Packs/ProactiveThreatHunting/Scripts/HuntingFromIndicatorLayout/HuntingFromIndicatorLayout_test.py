fe4ac3ddd2fef58dd2cf8e270ffa8a2bcca42a0f
arikday <aday@paloaltonetworks.com>
2023-08-07 22:19:40 +0300

    Script Test

--- Packs/ProactiveThreatHunting/Scripts/HuntingFromIndicatorLayout/HuntingFromIndicatorLayout_test.py
+++ Packs/ProactiveThreatHunting/Scripts/HuntingFromIndicatorLayout/HuntingFromIndicatorLayout_test.py
@@ -3,11 +3,15 @@ import pytest
 from HuntingFromIndicatorLayout import hunting_from_indicator_layout, CommandResults
 
 
-@pytest.mark.parametrize('sdo_name', 'sdoname')
 def test_hunting_from_indicator_layout(sdo_name):
     expected_output = CommandResults(
         readable_output=f"Proactive Threat Hunting Incident Created: Threat Hunting Session - {sdo_name}")
     assert hunting_from_indicator_layout(sdo_name) == expected_output
+
+
+def main():
+    sdo_name = "sdo"
+    test_hunting_from_indicator_layout(sdo_name)
 
 
 if __name__ == '__main__':
