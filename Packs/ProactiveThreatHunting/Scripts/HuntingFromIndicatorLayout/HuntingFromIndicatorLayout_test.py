51658a5c8350683c1ba093a9468d66c9788f6e76
arikday <aday@paloaltonetworks.com>
2023-08-08 10:22:24 +0300

    Script Test

--- Packs/ProactiveThreatHunting/Scripts/HuntingFromIndicatorLayout/HuntingFromIndicatorLayout_test.py
+++ Packs/ProactiveThreatHunting/Scripts/HuntingFromIndicatorLayout/HuntingFromIndicatorLayout_test.py
@@ -1,16 +1,28 @@
 import unittest
-from HuntingFromIndicatorLayout import hunting_from_indicator_layout, CommandResults
+from unittest.mock import patch
+from HuntingFromIndicatorLayout import hunting_from_indicator_layout
 
 
-def test_hunting_from_indicator_layout(sdo_name):
-    expected_output = CommandResults(
-        readable_output=f"Proactive Threat Hunting Incident Created: Threat Hunting Session - {sdo_name}")
-    assert hunting_from_indicator_layout(sdo_name) == expected_output
+class TestHuntingFromIndicatorLayout(unittest.TestCase):
+    @patch('demistomock.executecommand')
+    def test_hunting_from_indicator_layout_success(self, mock_executecommand):
+        mock_executecommand.return_value = [{'Type': 1, 'Contents': 'Incident created successfully'}]
 
+        sdo_value = 'indicator'
+        result = hunting_from_indicator_layout(sdo_value)
 
-def main():
-    sdo_name = "sdo"
-    test_hunting_from_indicator_layout(sdo_name)
+        expected_result = {
+            'readable_output': f"Proactive Threat Hunting Incident Created: Threat Hunting Session - {sdo_value}"
+        }
+        self.assertEqual(result.outputs, expected_result)
+
+    @patch('demistomock.executecommand')
+    def test_hunting_from_indicator_layout_failure(self, mock_executecommand):
+        mock_executecommand.side_effect = Exception('Test error')
+
+        sdo_value = 'indicator'
+        with self.assertRaises(DemistoException):
+            hunting_from_indicator_layout(sdo_value)
 
 
 if __name__ == '__main__':
