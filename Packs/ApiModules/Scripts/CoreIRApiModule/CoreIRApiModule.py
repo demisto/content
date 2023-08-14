99473b8a93e7aaf4f93d5a2683253412ac03349c
Shmuel Kroizer <69422117+shmuel44@users.noreply.github.com>
2023-08-14 17:15:33 +0300

    Apply suggestions from code review
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>

--- Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
+++ Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule.py
@@ -3890,7 +3890,7 @@ def list_risky_users_or_host_command(client: CoreClient, command: str, args: dic
         try:
             outputs = client.risk_score_user_or_host(id_).get('reply', {})
         except DemistoException as e:
-            if e is not None and e.res.status_code == 500 and 'No identity threat' in str(e):
+            if e is not None and and e.res and e.res.status_code == 500 and 'No identity threat' in str(e):
                 raise DemistoException(f'Please confirm the module is enabled. Full error message: {e}') from e
             if error_message := enrich_error_message_id_group_role(e=e, type_="id", custom_message=""):
                 raise DemistoException(error_message) from e
@@ -3904,7 +3904,7 @@ def list_risky_users_or_host_command(client: CoreClient, command: str, args: dic
         try:
             outputs = get_func().get('reply', [])[:list_limit]
         except DemistoException as e:
-            if e is not None and e.res.status_code == 500 and 'No identity threat' in str(e):
+            if e is not None and e.res and e.res.status_code == 500 and 'No identity threat' in str(e):
                 raise DemistoException(f'Please confirm the module is enabled. Full error message: {e}') from e
             raise
         table_for_markdown = [parse_risky_users_or_hosts(user, *table_headers) for user in outputs]
