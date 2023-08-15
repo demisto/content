731426945a49568c535cf936fc7ce6d66d884199
ilan <ierukhimovic@paloaltonetworks.com>
2023-08-15 17:09:22 +0300

    no proofpoint

--- Packs/ProofpointOnDemandEmailSecurity/Integrations/ProofpointOnDemandEmailSecurityEventCollector/ProofpointOnDemandEmailSecurity.py
+++ Packs/ProofpointOnDemandEmailSecurity/Integrations/ProofpointOnDemandEmailSecurityEventCollector/ProofpointOnDemandEmailSecurity.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from CommonServerPython import *  # noqa: F401
 from websockets.sync.client import connect
 from websockets.sync.connection import Connection
@@ -5,7 +6,25 @@ from websockets.sync.connection import Connection
 VENDOR = "proofpoint"
 PRODUCT = "on_demand_email_security"
 
+URL = "wss://{SERVER_URL}/v1/stream?cid={cluster_id}&type={type}&sinceTime={time}"
+
+
 EVENTS_TO_FETCH = 10
+
+
+@contextmanager
+def websocket_connections(server_url: str, cluster_id: str, api_key: str):
+    now = datetime.now().isoformat()
+    demisto.info("Starting websocket connection")
+    extra_headers = {"Authorization": f"Bearer {api_key}"}
+    with connect(
+        URL.format(server_url=server_url, cluster_id=cluster_id, type="message", time=now),
+        additional_headers=extra_headers,
+    ) as message_connection, connect(
+        URL.format(server_url=server_url, cluster_id=cluster_id, type="maillog", time=now),
+        additional_headers=extra_headers,
+    ) as maillog_connection:
+        yield message_connection, maillog_connection
 
 
 def fetch_events(connection: Connection) -> list[dict]:
@@ -26,18 +45,15 @@ def fetch_events(connection: Connection) -> list[dict]:
     return events
 
 
-def long_running_execution_command(cluster_id: str, api_key: str):
-    # start websocket connection
-    now = datetime.now().isoformat()
-    demisto.info("Starting websocket connection")
-    extra_headers = {"Authorization": f"Bearer {api_key}"}
-    with connect(
-        f"wss://logstream.proofpoint.com/v1/stream?cid={cluster_id}&type=message&sinceTime={now}",
-        additional_headers=extra_headers,
-    ) as message_connection, connect(
-        f"wss://logstream.proofpoint.com/v1/stream?cid={cluster_id}&type=maillog&sinceTime={now}",
-        additional_headers=extra_headers,
-    ) as maillog_connection:
+def test_module(server_url: str, cluster_id: str, api_key: str):
+    with websocket_connections(server_url, cluster_id, api_key) as (message_connection, maillog_connection):
+        fetch_events(message_connection)
+        fetch_events(maillog_connection)
+        return "ok"
+
+
+def long_running_execution_command(server_url: str, cluster_id: str, api_key: str):
+    with websocket_connections(server_url, cluster_id, api_key) as (message_connection, maillog_connection):
         demisto.info("Connected to websocket")
         while True:
             message_events = fetch_events(message_connection)
@@ -51,10 +67,13 @@ def long_running_execution_command(cluster_id: str, api_key: str):
 def main():
     command = demisto.command()
     params = demisto.params()
+    server_url = params.get("server_url", "")
     cluster_id = params.get("cluster_id", "")
     api_key = params.get("api_key", {}).get("password", "")
     if command == "long-running-execution":
-        long_running_execution_command(cluster_id, api_key)
+        long_running_execution_command(server_url, cluster_id, api_key)
+    if command == "test-module":
+        test_module(server_url, cluster_id, api_key)
 
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
