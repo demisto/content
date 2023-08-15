efd9ff27fe9ac2cc239830d6a8c52ca7d722a94f
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-08-15 22:54:08 +0300

    changed to class

--- Packs/FiltersAndTransformers/Scripts/IfElif/IfElif.py
+++ Packs/FiltersAndTransformers/Scripts/IfElif/IfElif.py
@@ -1,128 +1,130 @@
 import demistomock as demisto
 from CommonServerPython import *
-from collections.abc import Callable
+from collections.abc import Callable, Any
 from functools import reduce, partial
 import ast
 import re
 
-ARGS = demisto.args()
-FLAGS = argToList(ARGS.get('flags'))
 
-
-def load_variables():
-    variables = {}
-    assignments = ARGS.get('variables', '').splitlines()
-    for assign in assignments:
-        left, right = assign.strip().split('=', 1)
-        try:
-            right = ast.literal_eval(right)
-        except Exception:  # is a string
-            right = right.strip()
-        variables[left.strip()] = right
-    variables |= {
-        'true': True,
-        'false': False,
-        'null': None,
-        'VALUE': ARGS['value'],
+class IfElif:
+    operator_functions: dict[type, Callable] = {
+        # comparison operators:
+        ast.Eq: lambda x, y: x == y,
+        ast.NotEq: lambda x, y: x != y,
+        ast.Lt: lambda x, y: x < y,
+        ast.LtE: lambda x, y: x <= y,
+        ast.Gt: lambda x, y: x > y,
+        ast.GtE: lambda x, y: x >= y,
+        ast.In: lambda x, y: x in y,
+        ast.NotIn: lambda x, y: x not in y,
+        # boolean operators:
+        ast.And: lambda x, y: x and y,
+        ast.Or: lambda x, y: x or y,
+        # unary operators:
+        ast.Not: lambda x: not x,
+        ast.USub: lambda x: -x,
     }
-    return variables
 
+    def __init__(self, value, conditions, variables='', flags=None):
+        self.conditions = self.evaluate(conditions)
+        self.load_variables(variables, value)
+        self.handle_flags(argToList(flags))
 
-VARIABLES = load_variables()
-
-REGEX_FLAGS = (
-    re.DOTALL * ('regex_dot_all' in FLAGS)
-    | re.MULTILINE * ('regex_multiline' in FLAGS)
-    | re.IGNORECASE * ('case_insensitive' in FLAGS)
-)
-
-EQUAL_FUNC = (  # noqa: E731
-    (lambda x, y: str(x).lower() == str(y).lower())
-    if 'case_insensitive' in FLAGS
-    else (lambda x, y: x == y)
-)
-
-OPERATOR_FUNCTIONS: dict[type, Callable] = {
-    # comparison operators:
-    ast.Eq: EQUAL_FUNC,
-    ast.NotEq: lambda x, y: not EQUAL_FUNC(x, y),
-    ast.Lt: lambda x, y: x < y,
-    ast.LtE: lambda x, y: x <= y,
-    ast.Gt: lambda x, y: x > y,
-    ast.GtE: lambda x, y: x >= y,
-    ast.In: lambda x, y: x in y,
-    ast.NotIn: lambda x, y: x not in y,
-    # boolean operators:
-    ast.And: lambda x, y: x and y,
-    ast.Or: lambda x, y: x or y,
-    # unary operators:
-    ast.Not: lambda x: not x,
-    ast.USub: lambda x: -x,
-}
-
-FUNCTIONS = {
-    'regex_match': partial(
-        re.fullmatch if 'regex_full_match' in FLAGS else re.search,
-        flags=REGEX_FLAGS
-    )
-}
-
-
-def get_value(node):
-    match type(node):
-        case ast.Constant:
-            return node.value
-        case ast.List:
-            return [get_value(item) for item in node.elts]
-        case ast.Dict:
-            return {
-                get_value(key): get_value(value)
-                for key, value in zip(node.keys, node.values)
-            }
-        case ast.Name:
-            return VARIABLES[node.id]
-        case ast.Call:
-            return FUNCTIONS[node.func.id](*map(get_value, node.args))
-        case ast.Compare:
-            left = get_value(node.left)
-            return all(
-                OPERATOR_FUNCTIONS[type(op)](
-                    left, left := get_value(right)  # noqa: F841
+    def handle_flags(self, flags: list):
+        if flags:
+            self.regex_flags = (
+                re.DOTALL * ('regex_dot_all' in flags)
+                | re.MULTILINE * ('regex_multiline' in flags)
+                | re.IGNORECASE * ('case_insensitive' in flags)
+            )
+            if 'case_insensitive' in flags:
+                def eq(x, y):
+                    return str(x).lower() == str(y).lower()
+                self.operator_functions |= {
+                    ast.Eq: eq,
+                    ast.NotEq: lambda x, y: not eq(x, y),
+                }
+            self.functions = {
+                'regex_match': partial(
+                    re.fullmatch if 'regex_full_match' in flags else re.search,
+                    flags=self.regex_flags
                 )
-                for op, right in zip(node.ops, node.comparators)
-            )
-        case ast.BoolOp:
-            return reduce(
-                OPERATOR_FUNCTIONS[type(node.op)], map(get_value, node.values)
-            )
-        case ast.UnaryOp:
-            return OPERATOR_FUNCTIONS[type(node.op)](get_value(node.operand))
-        case _:
-            raise SyntaxError(
-                f'Unsupported expression type found: {node.__class__.__name__}'
-            )
+            }
 
+    def load_variables(self, variables: str, value: Any):
+        self.variables: dict = {}
+        for assign in variables.splitlines():
+            left, right = assign.strip().split('=', 1)
+            try:
+                right = ast.literal_eval(right)
+            except Exception:  # is a string
+                right = right.strip()
+            self.variables[left.strip()] = right
+        self.variables |= {
+            'true': True,
+            'false': False,
+            'null': None,
+            'VALUE': value,
+        }
 
-def evaluate(expression: str):
-    # sourcery skip: raise-from-previous-error
-    try:
-        parsed = ast.parse(expression, mode='eval')
-    except Exception:
-        raise SyntaxError(f'Cannot parse expression: {expression!r}')
-    return get_value(parsed.body)
+    def get_value(self, node):
+        match type(node):
+            case ast.Constant:
+                return node.value
+            case ast.List:
+                return [self.get_value(item) for item in node.elts]
+            case ast.Dict:
+                return {
+                    self.get_value(key): self.get_value(value)
+                    for key, value in zip(node.keys, node.values)
+                }
+            case ast.Name:
+                return self.variables[node.id]
+            case ast.Call:
+                return self.functions[node.func.id](*map(self.get_value, node.args))
+            case ast.Compare:
+                left = self.get_value(node.left)
+                return all(
+                    self.operator_functions[type(op)](
+                        left, left := self.get_value(right)  # noqa: F841
+                    )
+                    for op, right in zip(node.ops, node.comparators)
+                )
+            case ast.BoolOp:
+                return reduce(
+                    self.operator_functions[type(node.op)], map(self.get_value, node.values)
+                )
+            case ast.UnaryOp:
+                return self.operator_functions[type(node.op)](self.get_value(node.operand))
+            case _:
+                raise SyntaxError(
+                    f'Unsupported expression type found: {node.__class__.__name__}'
+                )
 
+    def evaluate(self, expression: str):
+        # sourcery skip: raise-from-previous-error
+        try:
+            parsed = ast.parse(expression, mode='eval')
+        except Exception:
+            raise SyntaxError(f'Cannot parse expression: {expression!r}')
+        return self.get_value(parsed.body)
 
-def main():
-    try:
-        *conditions, default = evaluate(ARGS['conditions'])
+    def parse_conditions(self):
+        *conditions, default = self.conditions
         result = next(
             (
                 condition['return']
                 for condition in conditions
-                if evaluate(condition['condition'])
+                if self.evaluate(condition['condition'])
             ),
             default['else']
         )
+        return result
+
+
+def main():
+    try:
+        result = IfElif(**demisto.args()).parse_conditions()
         return_results(result)
     except Exception as e:
         return_error(f'Error in If-Elif Transformer: {e}')
