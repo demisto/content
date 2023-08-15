d92394b79002e76f3369c44d8a7e054b30c681e2
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-08-15 22:17:21 +0300

    changed vars to upper

--- Packs/FiltersAndTransformers/Scripts/IfElif/IfElif.py
+++ Packs/FiltersAndTransformers/Scripts/IfElif/IfElif.py
@@ -13,7 +13,7 @@ def load_variables():
     variables = {}
     assignments = ARGS.get('variables', '').splitlines()
     for assign in assignments:
-        left, right = re.split('\s*=\s*', assign.strip(), 1)
+        left, right = assign.strip().split('=', 1)
         try:
             right = ast.literal_eval(right)
         except Exception:  # is a string
@@ -28,24 +28,24 @@ def load_variables():
     return variables
 
 
-variables = load_variables()
+VARIABLES = load_variables()
 
-regex_flags = (
+REGEX_FLAGS = (
     re.DOTALL * ('regex_dot_all' in FLAGS)
     | re.MULTILINE * ('regex_multiline' in FLAGS)
     | re.IGNORECASE * ('case_insensitive' in FLAGS)
 )
 
-equal_func = (  # noqa: E731
+EQUAL_FUNC = (  # noqa: E731
     (lambda x, y: str(x).lower() == str(y).lower())
     if 'case_insensitive' in FLAGS
     else (lambda x, y: x == y)
 )
 
-operator_functions: dict[type, Callable] = {
+OPERATOR_FUNCTIONS: dict[type, Callable] = {
     # comparison operators:
-    ast.Eq: equal_func,
-    ast.NotEq: lambda x, y: not equal_func(x, y),
+    ast.Eq: EQUAL_FUNC,
+    ast.NotEq: lambda x, y: not EQUAL_FUNC(x, y),
     ast.Lt: lambda x, y: x < y,
     ast.LtE: lambda x, y: x <= y,
     ast.Gt: lambda x, y: x > y,
@@ -60,10 +60,10 @@ operator_functions: dict[type, Callable] = {
     ast.USub: lambda x: -x,
 }
 
-functions = {
+FUNCTIONS = {
     'regex_match': partial(
         re.fullmatch if 'regex_full_match' in FLAGS else re.search,
-        flags=regex_flags
+        flags=REGEX_FLAGS
     )
 }
 
@@ -80,23 +80,23 @@ def get_value(node):
                 for key, value in zip(node.keys, node.values)
             }
         case ast.Name:
-            return variables[node.id]
+            return VARIABLES[node.id]
         case ast.Call:
-            return functions[node.func.id](*map(get_value, node.args))
+            return FUNCTIONS[node.func.id](*map(get_value, node.args))
         case ast.Compare:
             left = get_value(node.left)
             return all(
-                operator_functions[type(op)](
+                OPERATOR_FUNCTIONS[type(op)](
                     left, left := get_value(right)  # noqa: F841
                 )
                 for op, right in zip(node.ops, node.comparators)
             )
         case ast.BoolOp:
             return reduce(
-                operator_functions[type(node.op)], map(get_value, node.values)
+                OPERATOR_FUNCTIONS[type(node.op)], map(get_value, node.values)
             )
         case ast.UnaryOp:
-            return operator_functions[type(node.op)](get_value(node.operand))
+            return OPERATOR_FUNCTIONS[type(node.op)](get_value(node.operand))
         case _:
             raise SyntaxError(
                 f'Unsupported expression type found: {node.__class__.__name__}'
@@ -115,18 +115,15 @@ def evaluate(expression: str):
 def main():
     try:
         *conditions, default = evaluate(ARGS['conditions'])
-
         result = next(
             (
                 condition['return']
                 for condition in conditions
                 if evaluate(condition['condition'])
             ),
-            default['else'],
+            default['else']
         )
-
         return_results(result)
-
     except Exception as e:
         return_error(f'Error in If-Elif Transformer: {e}')
 
