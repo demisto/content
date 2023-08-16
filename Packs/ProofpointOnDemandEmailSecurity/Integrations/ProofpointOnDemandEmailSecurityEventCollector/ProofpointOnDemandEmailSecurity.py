e61a3156b0f8b8d9e522e044ed22d6e0eb772edb
ilan <ierukhimovic@paloaltonetworks.com>
2023-08-16 12:42:16 +0300

    updates

--- Packs/ProofpointOnDemandEmailSecurity/Integrations/ProofpointOnDemandEmailSecurityEventCollector/ProofpointOnDemandEmailSecurity.py
+++ Packs/ProofpointOnDemandEmailSecurity/Integrations/ProofpointOnDemandEmailSecurityEventCollector/ProofpointOnDemandEmailSecurity.py
@@ -2,26 +2,27 @@ from contextlib import contextmanager
 from CommonServerPython import *  # noqa: F401
 from websockets.sync.client import connect
 from websockets.sync.connection import Connection
+from dateutil import tz
 
 VENDOR = "proofpoint"
 PRODUCT = "on_demand_email_security"
 
-URL = "wss://{SERVER_URL}/v1/stream?cid={cluster_id}&type={type}&sinceTime={time}"
+URL = "wss://{host}/v1/stream?cid={cluster_id}&type={type}&sinceTime={time}"
 
 
 EVENTS_TO_FETCH = 10
 
 
 @contextmanager
-def websocket_connections(server_url: str, cluster_id: str, api_key: str):
-    now = datetime.now().isoformat()
+def websocket_connections(host: str, cluster_id: str, api_key: str):
+    now = datetime.utcnow().isoformat()
     demisto.info("Starting websocket connection")
     extra_headers = {"Authorization": f"Bearer {api_key}"}
     with connect(
-        URL.format(server_url=server_url, cluster_id=cluster_id, type="message", time=now),
+        URL.format(host=host, cluster_id=cluster_id, type="message", time=now),
         additional_headers=extra_headers,
     ) as message_connection, connect(
-        URL.format(server_url=server_url, cluster_id=cluster_id, type="maillog", time=now),
+        URL.format(host=host, cluster_id=cluster_id, type="maillog", time=now),
         additional_headers=extra_headers,
     ) as maillog_connection:
         yield message_connection, maillog_connection
@@ -35,7 +36,7 @@ def fetch_events(connection: Connection) -> list[dict]:
             date = dateparser.parse(event.get("ts"))
             if not date:
                 raise DemistoException(f"Failed to parse date: {event.get('ts')}")
-            event["_time"] = date.isoformat()
+            event["_time"] = date.astimezone(tz.tzutc()).isoformat()
             demisto.info(f"Received event: {event}")
             events.append(event)
             demisto.info(f"len of message_events: {len(events)}")
@@ -45,15 +46,15 @@ def fetch_events(connection: Connection) -> list[dict]:
     return events
 
 
-def test_module(server_url: str, cluster_id: str, api_key: str):
-    with websocket_connections(server_url, cluster_id, api_key) as (message_connection, maillog_connection):
+def test_module(host: str, cluster_id: str, api_key: str):
+    with websocket_connections(host, cluster_id, api_key) as (message_connection, maillog_connection):
         fetch_events(message_connection)
         fetch_events(maillog_connection)
         return "ok"
 
 
-def long_running_execution_command(server_url: str, cluster_id: str, api_key: str):
-    with websocket_connections(server_url, cluster_id, api_key) as (message_connection, maillog_connection):
+def long_running_execution_command(host: str, cluster_id: str, api_key: str):
+    with websocket_connections(host, cluster_id, api_key) as (message_connection, maillog_connection):
         demisto.info("Connected to websocket")
         while True:
             message_events = fetch_events(message_connection)
@@ -67,13 +68,13 @@ def long_running_execution_command(server_url: str, cluster_id: str, api_key: st
 def main():
     command = demisto.command()
     params = demisto.params()
-    server_url = params.get("server_url", "")
+    host = params.get("host", "")
     cluster_id = params.get("cluster_id", "")
     api_key = params.get("api_key", {}).get("password", "")
     if command == "long-running-execution":
-        long_running_execution_command(server_url, cluster_id, api_key)
+        long_running_execution_command(host, cluster_id, api_key)
     if command == "test-module":
-        test_module(server_url, cluster_id, api_key)
+        test_module(host, cluster_id, api_key)
 
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
