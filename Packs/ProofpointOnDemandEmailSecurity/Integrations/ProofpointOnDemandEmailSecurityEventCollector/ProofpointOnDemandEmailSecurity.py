ac81d6ee5fa59a7289c24549ca468fcf0726956d
ilan <ierukhimovic@paloaltonetworks.com>
2023-08-16 15:36:01 +0300

    fix test module

--- Packs/ProofpointOnDemandEmailSecurity/Integrations/ProofpointOnDemandEmailSecurityEventCollector/ProofpointOnDemandEmailSecurity.py
+++ Packs/ProofpointOnDemandEmailSecurity/Integrations/ProofpointOnDemandEmailSecurityEventCollector/ProofpointOnDemandEmailSecurity.py
@@ -10,7 +10,8 @@ PRODUCT = "on_demand_email_security"
 URL = "wss://{host}/v1/stream?cid={cluster_id}&type={type}&sinceTime={time}"
 
 
-EVENTS_TO_FETCH = 10
+EVENTS_TO_FETCH = 20
+TIMEOUT = 60
 
 
 @contextmanager
@@ -32,7 +33,7 @@ def fetch_events(connection: Connection) -> list[dict]:
     events: list[dict] = []
     while len(events) < EVENTS_TO_FETCH:
         try:
-            event = json.loads(connection.recv(timeout=60))
+            event = json.loads(connection.recv(timeout=TIMEOUT))
             event_ts = event.get("ts")
             if not event_ts:
                 raise DemistoException(f"Event does not contain a timestamp: {event}")
@@ -42,14 +43,18 @@ def fetch_events(connection: Connection) -> list[dict]:
             # the `ts` parameter is not always in UTC, so we need to convert it
             event["_time"] = date.astimezone(tz.tzutc()).isoformat()
             events.append(event)
-            demisto.debug(f"Received event. length of events from {connection.remote_address} is: {len(events)}")
+            demisto.debug(f"Received event. length of events from {connection.id} is: {len(events)}")
         except TimeoutError:
-            demisto.debug("Timeout reached when receiving events")
+            demisto.debug("Timeout reached when receiving events. Returning the events received so far.")
             break
     return events
 
 
 def test_module(host: str, cluster_id: str, api_key: str):
+    global EVENTS_TO_FETCH, TIMEOUT
+    # edit the global variables to make the test module faster
+    EVENTS_TO_FETCH = 1
+    TIMEOUT = 10
     with websocket_connections(host, cluster_id, api_key) as (message_connection, maillog_connection):
         fetch_events(message_connection)
         fetch_events(maillog_connection)
@@ -64,8 +69,7 @@ def long_running_execution_command(host: str, cluster_id: str, api_key: str):
             maillog_events = fetch_events(maillog_connection)
             demisto.info(f"Adding {len(message_events) + len(maillog_events)} events to XSIAM")
             # Send the events to the XSIAM
-            send_events_to_xsiam(message_events, vendor=VENDOR, product=PRODUCT)
-            send_events_to_xsiam(maillog_events, vendor=VENDOR, product=PRODUCT)
+            send_events_to_xsiam(message_events + maillog_events, vendor=VENDOR, product=PRODUCT)
 
 
 def main():
@@ -75,9 +79,9 @@ def main():
     cluster_id = params.get("cluster_id", "")
     api_key = params.get("api_key", {}).get("password", "")
     if command == "long-running-execution":
-        long_running_execution_command(host, cluster_id, api_key)
+        return_results(long_running_execution_command(host, cluster_id, api_key))
     if command == "test-module":
-        test_module(host, cluster_id, api_key)
+        return_results(test_module(host, cluster_id, api_key))
 
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
