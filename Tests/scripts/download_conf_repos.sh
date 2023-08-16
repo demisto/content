b4304520eec038d875bff4e8404e27f4b38956c5
kobymeir <ymeir@paloaltonetworks.com>
2023-08-16 12:40:19 +0300

    fixing git clone retry mechanism to allow retries. review fixes

--- Tests/scripts/download_conf_repos.sh
+++ Tests/scripts/download_conf_repos.sh
@@ -5,43 +5,47 @@ clone_repository() {
   local repo_name=$1
   local branch=$2
   local retry_count=$3
+  local sleep_time=$4
   local exit_code=0
   local i=1
-  echo "clone ${repo_name} from branch: ${branch} with ${retry_count} retries"
+  echo "cloning ${repo_name} from branch: ${branch} with ${retry_count} retries"
   for ((i=1; i <= retry_count; i++)); do
     git clone --depth=1 "https://gitlab-ci-token:${CI_JOB_TOKEN}@${repo_name}.git" --branch "${branch}" && exit_code=0 && break || exit_code=$?
     if [ ${i} -ne "${retry_count}" ]; then
-      echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, sleeping for 10 seconds and trying again"
-      sleep 10
+      echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, sleeping for ${sleep_time} seconds and trying again"
+      sleep "${sleep_time}"
     else
-      echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, exhausted all retries"
+      echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, exhausted all ${retry_count} retries"
       break
     fi
   done
+  return ${exit_code}
+}
+
+clone_repository_with_fallback_branch() {
+  local repo_name=$1
+  local branch=$2
+  local fallback_branch="${3:-master}"
+  local retry_count=$4
+  local sleep_time=${5:-10}  # default sleep time is 10 seconds.
+
+  clone_repository "${repo_name}" "${branch}" "${retry_count}" "${sleep_time}"
+  local exit_code=$?
   if [ ${exit_code} -ne 0 ]; then
-    branch="master"
-    echo "Failed to clone ${repo_name} with branch:${branch}, will try to clone with branch:${branch}"
-    for ((i=1; i <= retry_count; i++)); do
-      git clone --depth=1 "https://gitlab-ci-token:${CI_JOB_TOKEN}@${repo_name}.git" --branch "${branch}" && exit_code=0 && break || exit_code=$?
-      if [ ${i} -ne "${retry_count}" ]; then
-        echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, sleeping for 10 seconds and trying again"
-        sleep 10
-      else
-        echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, exhausted all retries"
-        break
-      fi
-    done
-
+    # Failed to clone with branch, try again with fallback_branch.
+    clone_repository "${repo_name}" "${fallback_branch}" "${retry_count}" "${sleep_time}"
+    local exit_code=$?
     if [ ${exit_code} -ne 0 ]; then
-      echo "Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, exiting!"
+      echo "Failed to clone ${repo_name} with branch:${fallback_branch}, exit code:${exit_code}, exiting!"
       exit ${exit_code}
+    else
+      echo "Successfully cloned ${repo_name} with branch:${fallback_branch}"
+      return 0
     fi
-
   else
-    echo "Successfully cloned ${repo_name} with branch:${branch}"
-    exit 0
+    echo "successfully cloned ${repo_name} with branch:${branch}"
+    return 0
   fi
-
 }
 
 # Replace slashes '/' in the branch name with underscores '_'.
@@ -64,7 +68,7 @@ echo ${DEMISTO_LIC_PATH} > demisto_lic_path
 DEMISTO_PACK_SIGNATURE_UTIL_PATH="./signDirectory"
 echo ${DEMISTO_PACK_SIGNATURE_UTIL_PATH} > demisto_pack_sig_util_path
 
-clone_repository "content-test-conf" "${UNDERSCORE_BRANCH}" 3
+clone_repository_with_fallback_branch "content-test-conf" "${UNDERSCORE_BRANCH}" "master" 3
 
 cp ./content-test-conf/secrets_build_scripts/google_secret_manager_handler.py ./Tests/scripts
 cp ./content-test-conf/secrets_build_scripts/add_secrets_file_to_build.py ./Tests/scripts
@@ -77,7 +81,7 @@ if [[ "${NIGHTLY}" == "true" || "${EXTRACT_PRIVATE_TESTDATA}" == "true" ]]; then
 fi
 rm -rf ./content-test-conf
 
-clone_repository "infra" "${UNDERSCORE_BRANCH}" 3
+clone_repository_with_fallback_branch "infra" "${UNDERSCORE_BRANCH}" "master" 3
 
 cp -r ./infra/xsiam_servers.json $XSIAM_SERVERS_PATH
 cp -r ./infra/xsoar_ng_servers.json $XSOAR_NG_SERVERS_PATH
