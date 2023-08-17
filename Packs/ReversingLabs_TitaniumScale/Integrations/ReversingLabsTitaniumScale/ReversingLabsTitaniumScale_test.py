906356fa5c3edec445a63fa223a086bba48c49e4
msever <mislav.sever@reversinglabs.com>
2023-08-18 00:02:19 +0200

    Update unit tests

--- Packs/ReversingLabs_TitaniumScale/Integrations/ReversingLabsTitaniumScale/ReversingLabsTitaniumScale_test.py
+++ Packs/ReversingLabs_TitaniumScale/Integrations/ReversingLabsTitaniumScale/ReversingLabsTitaniumScale_test.py
@@ -4,8 +4,8 @@ from ReversingLabsTitaniumScale import parse_report_and_return_results, parse_up
 
 
 def test_parse_report_and_return_results():
-    test_response = util_load_json('TestData/tiscale_response.json')
-    test_context = util_load_json('TestData/tiscale_context.json')
+    test_response = util_load_json('test_data/tiscale_response.json')
+    test_context = util_load_json('test_data/tiscale_context.json')
 
     result = parse_report_and_return_results(title='## ReversingLabs TitaniumScale get results\n',
                                              response_json=test_response)
@@ -14,8 +14,8 @@ def test_parse_report_and_return_results():
 
 
 def test_parse_upload_report_and_return_results():
-    test_response = util_load_json('TestData/tiscale_upload_response.json')
-    test_context = util_load_json('TestData/tiscale_upload_context.json')
+    test_response = util_load_json('test_data/tiscale_upload_response.json')
+    test_context = util_load_json('test_data/tiscale_upload_context.json')
 
     result = parse_upload_report_and_return_results(response_json=test_response)
 
