f1c3ee565aa7f2eec8c00d66232ccebf5c4280cb
MosheEichler <meichler@paloaltonetworks.com>
2023-08-20 12:18:21 +0300

    UT

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -5,6 +5,7 @@ import json
 import os
 import re
 import sys
+import urllib
 import warnings
 
 import dateparser
@@ -3012,6 +3013,19 @@ class TestBaseClient:
         response = Response()
         response.status_code = 400
         assert not self.client._is_status_code_valid(response)
+
+    @pytest.mark.parametrize('quote_params, expected_result', [
+        (None, 'key=value+with+space'),
+        (urllib.parse.quote, 'key=value%20with%20space'),
+    ])
+    def test_http_request_quote_params_via(self, quote_params, expected_result, requests_mock):
+        mock_request = requests_mock.get('http://example.com/api/v2/', json={})
+        from CommonServerPython import BaseClient
+        mock_client = BaseClient('http://example.com/api/v2/')
+
+        mock_client._http_request('get', params={'key': 'value with space'}, quote_params_via=quote_params)
+
+        assert mock_request.last_request.query == expected_result
 
 
 def test_parse_date_string():
