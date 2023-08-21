66c5adf09e5af70c6dc84b3324030568ad422148
Moshe Eichler <78307768+MosheEichler@users.noreply.github.com>
2023-08-21 12:45:26 +0300

    SecurityAndCompliance improved access token expiration error message (#28960)
    * debug logs
    
    * improve error message
    
    * improve access token error message
    
    * RN

--- Packs/MicrosoftExchangeOnline/Integrations/SecurityAndComplianceV2/SecurityAndComplianceV2.ps1
+++ Packs/MicrosoftExchangeOnline/Integrations/SecurityAndComplianceV2/SecurityAndComplianceV2.ps1
@@ -555,6 +555,7 @@ class OAuth2DeviceCodeClient {
             $client.RefreshTokenIfExpired()
         #>
     }
+
     ClearContext(){
         $this.access_token = $null
         $this.refresh_token = $null
@@ -622,7 +623,6 @@ class SecurityAndComplianceClient {
         }
         Connect-ExchangeOnline @cmd_params -CommandName $CommandName -WarningAction:SilentlyContinue -ShowBanner:$false | Out-Null
     }
-
 
     DisconnectSession(){
         Disconnect-ExchangeOnline -Confirm:$false -WarningAction:SilentlyContinue 6>$null | Out-Null
@@ -1905,7 +1905,9 @@ function Main {
 Command: $command
 Arguments: $($command_arguments | ConvertTo-Json)
 Error: $($_.Exception.Message)")
-        if ($command -ne "test-module") {
+        if ($_.Exception.Message -like "*Unable to open a web page using xdg-open*" ) {
+           Write-Host "It looks like the access token has expired. Please run the command !$script:COMMAND_PREFIX-auth-start, before running this command."
+        } elseif ($command -ne "test-module") {
             ReturnError "Error:
             Integration: $script:INTEGRATION_NAME
             Command: $command
