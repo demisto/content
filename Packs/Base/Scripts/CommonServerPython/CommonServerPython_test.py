18c5be79ea0287e7006f168d9cdd66dbcfe6abfe
MosheEichler <meichler@paloaltonetworks.com>
2023-08-22 14:51:56 +0300

    divide into two functions

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -3014,18 +3014,10 @@ class TestBaseClient:
         response.status_code = 400
         assert not self.client._is_status_code_valid(response)
 
-    @pytest.mark.parametrize('quote_params, expected_result', [
-        (None, 'key=value+with+spaces'),
-        pytest.param(urllib.parse.quote_plus, 'key=value+with+spaces',
-                     marks=pytest.mark.skipif(not IS_PY3, reason='test not supported in py2')),
-        pytest.param(urllib.parse.quote, 'key=value%20with%20spaces',
-                     marks=pytest.mark.skipif(not IS_PY3, reason='test not supported in py2')),
-    ])
-    def test_http_request_quote_params_via(self, quote_params, expected_result, requests_mock):
+    def test_http_request_quote_params_via_none(self, requests_mock):
         """
             Given
-                - query params with spaces.
-                - with/without specific quote function.
+                - query params with spaces without specific quote function.
 
             When
                 - Calling _https_request function.
@@ -3037,9 +3029,29 @@ class TestBaseClient:
         from CommonServerPython import BaseClient
         mock_client = BaseClient('http://example.com/api/v2/')
 
-        mock_client._http_request('get', params={'key': 'value with spaces'}, quote_params_via=quote_params)
+        mock_client._http_request('get', params={'key': 'value with spaces'})
 
-        assert mock_request.last_request.query == expected_result
+        assert mock_request.last_request.query == 'key=value+with+spaces'
+
+    @pytest.mark.skipif(not IS_PY3, reason='test not supported in py2')
+    def test_http_request_quote_params_via_quote(self, requests_mock):
+        """
+            Given
+                - query params with spaces with specific quote function.
+
+            When
+                - Calling _https_request function.
+
+            Then
+                - Verify the spaces in the result is as expected.
+        """
+        mock_request = requests_mock.get('http://example.com/api/v2/', json={})
+        from CommonServerPython import BaseClient
+        mock_client = BaseClient('http://example.com/api/v2/')
+
+        mock_client._http_request('get', params={'key': 'value with spaces'}, quote_params_via=urllib.parse.quote)
+
+        assert mock_request.last_request.query == 'key=value%20with%20spaces'
 
 
 def test_parse_date_string():
