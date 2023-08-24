734f9316adfb0b5b00cc069d01bbec2500255992
Jacob Levy <129657918+jlevypaloalto@users.noreply.github.com>
2023-08-24 11:36:21 +0300

    Tenable Export Scan (#28146)
    * init
    
    * list-scan-filters init
    
    * list-scan-history init
    
    * export-scan init
    
    * added yml for list-filters
    
    * refactoring
    
    * yml version 1.0
    
    * fixed sortField bug
    
    * added defaultValue for filterSearchType
    
    * update output for export file
    
    * remove gitlab
    
    * improved yml
    
    * added test playbook
    
    * safe_get_json to get_json
    
    * added descriptions to yml
    
    * added user-agent to older commands
    
    * unit-tests init
    
    * RN + docs
    
    * unit-tests complete
    
    * fixed tests
    
    * updated docker
    
    * CR changes part 1
    
    * CR changes part 2
    
    * temp
    
    * temp
    
    * client: first test
    
    * client: working
    
    * paginate version 1
    
    * update readme,yml,tests
    
    * update readme,yml
    
    * tests almost complete
    
    * tests complete
    
    * added tests
    
    * added tests
    
    * added tec-docs to paginate
    
    * CR changes part 3
    
    * fixed tests
    
    * CR changes part 4
    
    * updated docker
    
    * Bump pack from version Tenable_io to 2.1.11.
    
    * reformatted paginate
    
    * Demo changes
    
    * merged test playbooks
    
    * Demo changes part 2
    
    * Demo changes part 3; added args to list-filters
    
    * remove args from list-filters
    
    * remove args from list-filters 2
    
    * added descriptions
    
    * fixed unit-tests KeyError
    
    * Bump pack from version Tenable_io to 2.1.12.
    
    * Demo changes part 4; added descrptions
    
    * Demo changes part 4; added filter arg
    
    * Demo changes part 5; added filter arg
    
    * Demo changes part 6; added filter arg in TPB
    
    * Demo changes part 7; added link for filter arg
    
    * updated docker
    
    * fixed build_filter's comment
    
    * improved filter parsing
    
    * add filter limitation to description
    
    * updated docker
    
    * fixed scan TPB
    
    * fixed tpb
    
    * removed flaky checks from TPB
    
    * updated docker
    
    * test: unmock TPBs
    
    * removed new TPB
    
    * revert: 'test: unmock TPBs'
    
    * fixed docs
    
    * test; remove demisto.results
    
    * Revert 'test; remove demisto.results'
    
    * fixed TPB
    
    * test; table in error response
    
    * revert: 'test; table in error response'
    
    * cosmetic changes
    
    * TPB on quick scan
    
    * TPB timeout fix
    
    * TPB launch in unallowed status fix
    
    * TPB launch in unallowed status fix
    
    * TPB
    
    * TPB
    
    * extended timeout
    
    ---------
    
    Co-authored-by: Content Bot <bot@demisto.com>

--- Packs/Tenable_io/Integrations/Tenable_io/Tenable_io_test.py
+++ Packs/Tenable_io/Integrations/Tenable_io/Tenable_io_test.py
@@ -2,6 +2,8 @@ import demistomock as demisto
 import pytest
 from freezegun import freeze_time
 from CommonServerPython import *
+import json
+# mypy: disable-error-code="operator"
 
 MOCK_PARAMS = {
     'access-key': 'fake_access_key',
@@ -42,12 +44,26 @@ EXPECTED_VULN_BY_ASSET_RESULTS = [
         'VulnerabilityState': 'Resurfaced'
     }
 ]
+MOCK_CLIENT_ARGS = {
+    'base_url': MOCK_PARAMS['url'],
+    'verify': True,
+    'proxy': True,
+    'ok_codes': (200,),
+}
 
 
-def mock_demisto(mocker, mock_args):
+def load_json(filename):
+    with open(f'test_data/{filename}.json') as f:
+        return json.load(f)
+
+
+def mock_demisto(mocker, mock_args=None):
     mocker.patch.object(demisto, 'params', return_value=MOCK_PARAMS)
     mocker.patch.object(demisto, 'args', return_value=mock_args)
+    mocker.patch.object(demisto, 'uniqueFile', return_value='file')
+    mocker.patch.object(demisto, 'investigation', return_value={'id': 'id'})
     mocker.patch.object(demisto, 'results')
+    mocker.patch.object(demisto, 'debug')
 
 
 def test_get_scan_status(mocker, requests_mock):
@@ -179,6 +195,8 @@ def test_get_asset_details_command(mocker, requests_mock):
     ('2.5-3.5', 2.5, 3.5),
     ('0.1-3', 0.1, 3),
     ('0.1 - 3', 0.1, 3),
+    ('0-1', 'exception', 'exception'),
+    ('3-100', 'exception', 'exception'),
     ('0', 'exception', 'exception'),
     ('3-0', 'exception', 'exception')
 ])
@@ -193,11 +211,10 @@ def test_validate_range(range_str, expected_lower_range_bound, expected_upper_ra
     """
     from Tenable_io import validate_range
 
-    if isinstance(expected_lower_range_bound, str) and isinstance(expected_upper_range_bound, str):
-        with pytest.raises(DemistoException) as de:
+    if expected_lower_range_bound == expected_upper_range_bound == 'exception':
+        err_msg = 'Please specify a valid vprScoreRange. The VPR values range is 0.1-10.0.'
+        with pytest.raises(DemistoException, match=err_msg):
             lower_range_bound, upper_range_bound = validate_range(range_str)
-
-        assert de.value.message == 'Please specify valid vprScoreRange. VPR values range are 0.1-10.0.'
     else:
         lower_range_bound, upper_range_bound = validate_range(range_str)
         assert lower_range_bound == expected_lower_range_bound
@@ -348,7 +365,7 @@ def test_export_assets_command(mocker, args, return_value_export_request_with_ex
     from Tenable_io import export_assets_command
     import Tenable_io
     from test_data.response_and_results import export_assets_response
-    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported', return_value=None)
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported')
     mocker.patch.object(Tenable_io, 'export_request', return_value={"export_uuid": 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'})
     mocker.patch.object(Tenable_io, 'export_request_with_export_uuid',
                         return_value={"export_uuid": 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'})
@@ -417,3 +434,259 @@ def test_export_vulnerabilities_command(mocker, args, return_value_export_reques
                                            ' 2023-08-15T15:56:18.852Z | 2023-08-15T15:56:18.852Z |' \
                                            ' some_description | solution. |\n'
         assert response.raw_response == export_vulnerabilities_response
+
+
+@pytest.mark.parametrize(
+    'args, expected_result',
+    [
+        # Test case 1: Basic input with limit and offset
+        (
+            {
+                'sortFields': '',
+                'sortOrder': 'asc',
+                'excludeRollover': False,
+                'limit': 10,
+                'offset': 0
+            },
+            {
+                'sort': '',
+                'exclude_rollover': False,
+                'limit': 10,
+                'offset': 0,
+            }
+        ),
+        # Test case 2: Sorting by a multiple fields in ascending order
+        (
+            {
+                'sortFields': 'name,date,status',
+                'sortOrder': 'asc',
+                'excludeRollover': False,
+            },
+            {
+                'sort': 'name:asc,date:asc,status:asc',
+                'exclude_rollover': False,
+                'limit': 50,
+                'offset': 0
+            }
+        ),
+        # Test case 3: Sorting by multiple fields in different orders
+        (
+            {
+                'sortFields': 'name,date,status',
+                'sortOrder': 'asc,desc,asc',
+                'excludeRollover': False,
+            },
+            {
+                'sort': 'name:asc,date:desc,status:asc',
+                'exclude_rollover': False,
+                'limit': 50,
+                'offset': 0
+            }
+        )
+    ]
+)
+def test_scan_history_params(args, expected_result):
+    """
+    Given:
+        Case 1: Only sortOrder is defined (Default).
+        Case 2: The sortFields has multiple values and sortOrder only one.
+        Case 3: Both sort lists have multiple values.
+
+    When:
+        - Running the tenable-io-get-scan-history command.
+
+    Then:
+        Case 1: Return empty sort.
+        Case 2: Sort all sortFields by sortOrder.
+        Case 3: Match sortFields and sortOrder's values by index.
+    """
+    from Tenable_io import scan_history_params
+
+    result = scan_history_params(args)
+
+    assert result == expected_result
+
+
+def test_list_scan_filters_command(mocker):
+    '''
+    Given:
+        -  A request to list Tenable IO scan filters.
+
+    When:
+        - Running the "list-scan-filters" command.
+
+    Then:
+        - Verify that tenable-io-list-scan-filters command works as expected.
+    '''
+    from Tenable_io import list_scan_filters_command, Client
+
+    test_data = load_json('list_scan_filters')
+
+    request = mocker.patch.object(BaseClient, '_http_request', return_value=test_data['response_json'])
+    mock_demisto(mocker)
+
+    results = list_scan_filters_command(Client(**MOCK_CLIENT_ARGS))
+
+    assert results.outputs == test_data['outputs']
+    assert results.readable_output == test_data['readable_output']
+    assert results.outputs_prefix == 'TenableIO.ScanFilter'
+    assert results.outputs_key_field == 'name'
+
+    request.assert_called_with(*test_data['called_with']['args'])
+
+
+def test_get_scan_history_command(mocker):
+    '''
+    Given:
+        -  A request to get Tenable IO scan history.
+
+    When:
+        - Running the "get-scan-history" command.
+
+    Then:
+        - Verify that tenable-io-get-scan-history command works as expected.
+    '''
+    from Tenable_io import get_scan_history_command, Client
+
+    test_data = load_json('get_scan_history')
+
+    request = mocker.patch.object(
+        BaseClient, '_http_request', return_value=test_data['response_json'])
+    mock_demisto(mocker)
+
+    results = get_scan_history_command(test_data['args'], Client(**MOCK_CLIENT_ARGS))
+
+    assert results.outputs_prefix == 'TenableIO.ScanHistory'
+    assert results.outputs_key_field == 'id'
+    assert results.outputs == test_data['outputs']
+    assert results.readable_output == test_data['readable_output']
+
+    request.assert_called_with(
+        *test_data['called_with']['args'],
+        **test_data['called_with']['kwargs'])
+
+
+def test_initiate_export_scan(mocker):
+    '''
+    Given:
+        - A request to export a scan report.
+
+    When:
+        - Running the "export-scan" command.
+
+    Then:
+        - Initiate an export scan request.
+    '''
+
+    from Tenable_io import initiate_export_scan, Client
+
+    test_data = load_json('initiate_export_scan')
+    mock_demisto(mocker)
+    request = mocker.patch.object(
+        BaseClient, '_http_request', return_value=test_data['response_json'])
+    file = initiate_export_scan(test_data['args'], Client(**MOCK_CLIENT_ARGS))
+
+    assert file == test_data['expected_file']
+    request.assert_called_with(
+        *test_data['called_with']['args'],
+        **test_data['called_with']['kwargs'])
+
+
+def test_download_export_scan(mocker):
+    '''
+    Given:
+        - A request to export a scan report.
+
+    When:
+        - Running the "export-scan" command.
+
+    Then:
+        - Initiate an export scan request.
+    '''
+    from Tenable_io import Client
+
+    mock_demisto(mocker)
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported')
+    request = mocker.patch.object(
+        BaseClient, '_http_request', return_value=b'')
+
+    result = Client(**MOCK_CLIENT_ARGS).download_export_scan('scan_id', 'file_id', 'HTML')
+
+    assert result == {
+        'Contents': '',
+        'ContentsFormat': 'text',
+        'Type': 9,
+        'File': 'scan_scan_id_file_id.html',
+        'FileID': 'file',
+    }
+    request.assert_called_with(
+        'GET', 'scans/scan_id/export/file_id/download', resp_type='content')
+
+
+@pytest.mark.parametrize(
+    'args, response_json, message',
+    [
+        ({'scanId': '', 'format': 'HTML', 'filterSearchType': ''}, {},
+         'The "chapters" field must be provided for PDF or HTML formats.'),
+        ({'scanId': '', 'format': '', 'filterSearchType': ''}, {'status': 'error'},
+         'Tenable IO encountered an error while exporting the scan report file.')
+    ]
+)
+def test_export_scan_command_errors(mocker, args, response_json, message):
+    '''
+    Given:
+        - A request to export a scan report.
+
+    When:
+        - Running the "export-scan" command in any of the following cases:
+            - Case A: The "format" arg is HTML or PDF but "chapters" is not defined.
+            - Case B: An export scan request has been made and the report status is "error" or unrecognized.
+
+    Then:
+        - Return an error.
+    '''
+
+    from Tenable_io import export_scan_command, Client
+
+    mock_demisto(mocker)
+    mocker.patch.object(ScheduledCommand, 'raise_error_if_not_supported')
+    mocker.patch.object(BaseClient, '_http_request', return_value=response_json)
+    mocker.patch.object(Client, 'initiate_export_scan', return_value={'file': 'file_id'})
+
+    with pytest.raises(DemistoException, match=message):
+        export_scan_command(args, Client(**MOCK_CLIENT_ARGS))
+
+
+@pytest.mark.parametrize(
+    'args, expected_result',
+    (
+        (
+            {
+                'page': '10',
+                'pageSize': '5',
+                'limit': '50'
+            },
+            {
+                'limit': 5,
+                'offset': 45
+            }
+        ),
+        (
+            {
+                'limit': '50',
+                'page': '23'
+            },
+            {
+                'limit': '50',
+                'offset': 0
+            }
+        )
+    )
+)
+def test_scan_history_pagination_params(args, expected_result):
+
+    from Tenable_io import scan_history_pagination_params
+
+    result = scan_history_pagination_params(args)
+
+    assert result == expected_result
