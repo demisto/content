2724f464aa77b683ecd3d524a4bcbe9698dcae77
kobymeir <ymeir@paloaltonetworks.com>
2023-08-28 11:43:02 +0300

    review fixes

--- Tests/scripts/convert_test_result_to_jira_issues.py
+++ Tests/scripts/convert_test_result_to_jira_issues.py
@@ -9,6 +9,7 @@ from tempfile import NamedTemporaryFile
 import urllib3
 from jira.client import ResultList, Issue, JIRA
 from junitparser import TestSuite, JUnitXml
+from tabulate import tabulate
 
 from Tests.scripts.utils import logging_wrapper as logging
 from Tests.scripts.utils.log_util import install_logging
@@ -25,7 +26,10 @@ JIRA_ISSUE_TYPE = os.environ["JIRA_ISSUE_TYPE"]
 JIRA_COMPONENT = os.environ["JIRA_COMPONENT"]
 JIRA_ISSUE_UNRESOLVED_TRANSITION_NAME = os.environ["JIRA_ISSUE_UNRESOLVED_TRANSITION_NAME"]
 JIRA_TIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
+# Jira additional fields are a json string that will be parsed into a dictionary containing the name of the field
+# as the key and the value as a dictionary containing the value of the field.
 JIRA_ADDITIONAL_FIELDS = json.loads(os.environ.get("JIRA_ADDITIONAL_FIELDS", "{}"))
+# Jira label are a json string that will be parsed into a list of labels.
 JIRA_LABELS = json.loads(os.environ.get("JIRA_LABELS", "[]"))
 
 
@@ -45,27 +49,48 @@ def create_jira_issue(jira_server: JIRA,
                       test_suite: TestSuite,
                       options: argparse.Namespace,
                       now: datetime) -> Issue:
-
     properties = {prop.name: prop.value for prop in test_suite.properties()}
     build_id = properties.get("ci_pipeline_id", "")
-    build_url = f"{GITLAB_SERVER_URL}/{GITLAB_PROJECT_ID}/-/pipelines/{build_id}" if build_id else ""
-    build_id_hash = f" #{build_id}" if build_id else ""
-    build_markdown_link = f"[Nightly{build_id_hash}|{build_url}]" if build_id else f"Nightly{build_id_hash}"
-    description = f"""
-        *{properties['pack_id']}* - *{properties['file_name']}* failed in {build_markdown_link}
-
-        ||Tests||Result||
-        |Total|{test_suite.tests}|
-        |Failed|{test_suite.failures}|
-        |Errors|{test_suite.errors}|
-        |Skipped|{test_suite.skipped}|
-        |Successful|{test_suite.tests - test_suite.failures - test_suite.errors - test_suite.skipped}|
-        |Duration|{test_suite.time}s|
-        """
+    description = generate_description(build_id, properties, test_suite)
     summary = f"{properties['pack_id']} - {properties['file_name']} failed nightly"
     jql_query = (f"project = \"{JIRA_PROJECT_ID}\" AND issuetype = \"{JIRA_ISSUE_TYPE}\" "
                  f"AND component = \"{JIRA_COMPONENT}\" AND summary ~ \"{summary}\" ORDER BY created DESC")
     search_issues: ResultList[Issue] = jira_server.search_issues(jql_query, maxResults=1)
+    jira_issue, link_to_issue, use_existing_issue = find_existing_jira_ticket(jira_server, now, options, search_issues)
+
+    if jira_issue is not None:
+        jira_server.add_comment(issue=jira_issue, body=description)
+    else:
+        jira_issue = jira_server.create_issue(project=JIRA_PROJECT_ID,
+                                              summary=summary,
+                                              description=description,
+                                              issuetype={'name': JIRA_ISSUE_TYPE},
+                                              components=[{'name': JIRA_COMPONENT}],
+                                              labels=['nightly'] + JIRA_LABELS,
+                                              **JIRA_ADDITIONAL_FIELDS
+                                              )
+        # Create a back link to the previous issue, which is resolved.
+        if link_to_issue:
+            jira_server.create_issue_link(type="Relates", inwardIssue=jira_issue.key,
+                                          outwardIssue=link_to_issue.key)
+
+    with NamedTemporaryFile() as attachment_file_name:
+        xml = JUnitXml()
+        xml.add_testsuite(test_suite)
+        xml.write(attachment_file_name.name, pretty=True)
+        build_id_dash = f"-{build_id}" if build_id else ""
+        junit_file_name = (f"unit-test{build_id_dash}-{properties['start_time']}-{properties['pack_id']}-"
+                           f"{properties['file_name']}.xml")
+        jira_server.add_attachment(issue=jira_issue.key, attachment=attachment_file_name.name, filename=junit_file_name)
+
+    back_link_to = f" with back link to {link_to_issue.key}" if link_to_issue else ""
+    logging.info(f"{'Updated' if use_existing_issue else 'Created'} Jira issue: {jira_issue.key} {back_link_to}"
+                 f"for {test_suite.name} with {test_suite.failures} failures and {test_suite.errors} errors")
+
+    return jira_issue
+
+
+def find_existing_jira_ticket(jira_server, now, options, search_issues):
     link_to_issue = None
     jira_issue = None
     if use_existing_issue := (len(search_issues) == 1):
@@ -99,37 +124,27 @@ def create_jira_issue(jira_server: JIRA,
                 link_to_issue = searched_issue
         else:
             jira_issue = searched_issue
+    return jira_issue, link_to_issue, use_existing_issue
 
-    if jira_issue is not None:
-        jira_server.add_comment(issue=jira_issue, body=description)
-    else:
-        jira_issue = jira_server.create_issue(project=JIRA_PROJECT_ID,
-                                              summary=summary,
-                                              description=description,
-                                              issuetype={'name': JIRA_ISSUE_TYPE},
-                                              components=[{'name': JIRA_COMPONENT}],
-                                              labels=['nightly'] + JIRA_LABELS,
-                                              **JIRA_ADDITIONAL_FIELDS
-                                              )
-        # Create a back link to the previous issue, which is resolved.
-        if link_to_issue:
-            jira_server.create_issue_link(type="Relates", inwardIssue=jira_issue.key,
-                                          outwardIssue=link_to_issue.key)
 
-    with NamedTemporaryFile() as attachment_file_name:
-        xml = JUnitXml()
-        xml.add_testsuite(test_suite)
-        xml.write(attachment_file_name.name, pretty=True)
-        build_id_dash = f"-{build_id}" if build_id else ""
-        junit_file_name = (f"unit-test{build_id_dash}-{properties['start_time']}-{properties['pack_id']}-"
-                           f"{properties['file_name']}.xml")
-        jira_server.add_attachment(issue=jira_issue.key, attachment=attachment_file_name.name, filename=junit_file_name)
+def generate_description(build_id: str, properties: dict[str, str], test_suite: TestSuite) -> str:
+    build_url = f"{GITLAB_SERVER_URL}/{GITLAB_PROJECT_ID}/-/pipelines/{build_id}" if build_id else ""
+    build_id_hash = f" #{build_id}" if build_id else ""
+    build_markdown_link = f"[Nightly{build_id_hash}|{build_url}]" if build_id else f"Nightly{build_id_hash}"
+    table = tabulate(tabular_data=[
+        ["Total", test_suite.tests],
+        ["Failed", test_suite.failures],
+        ["Errors", test_suite.errors],
+        ["Skipped", test_suite.skipped],
+        ["Successful", test_suite.tests - test_suite.failures - test_suite.errors - test_suite.skipped],
+        ["Duration", f"{test_suite.time}s"]
+    ], tablefmt="jira", headers=["Tests", "Result"])
+    description = f"""
+        *{properties['pack_id']}* - *{properties['file_name']}* failed in {build_markdown_link}
 
-    back_link_to = f" with back link to {link_to_issue.key}" if link_to_issue else ""
-    logging.info(f"{'Updated' if use_existing_issue else 'Created'} Jira issue: {jira_issue.key} {back_link_to}"
-                 f"for {test_suite.name} with {test_suite.failures} failures and {test_suite.errors} errors")
-
-    return jira_issue
+        {table}
+        """
+    return description
 
 
 def main():
