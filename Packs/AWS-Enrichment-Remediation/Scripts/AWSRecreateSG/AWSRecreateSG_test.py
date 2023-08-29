d4c6bedd96cce3a05d6d790e2a97d2214e2733ea
jwilkes <jwilkes@paloaltonetworks.com>
2023-08-29 14:29:59 -0400

    unit test coverage

--- Packs/AWS-Enrichment-Remediation/Scripts/AWSRecreateSG/AWSRecreateSG_test.py
+++ Packs/AWS-Enrichment-Remediation/Scripts/AWSRecreateSG/AWSRecreateSG_test.py
@@ -51,9 +51,9 @@ def test_instance_info(mocker):
     from AWSRecreateSG import instance_info
     from test_data.sample import INSTANCE_INFO
     mocker.patch.object(demisto, "executeCommand", return_value=INSTANCE_INFO)
-    args = {"instance_id": "fake-instance-id", "public_ip": "1.1.1.1"}
+    args = {"instance_id": "fake-instance-id", "public_ip": "1.1.1.1", "assume_role": "test_role"}
     result = instance_info(**args)
-    assert result == {'eni-00000000000000000': ['sg-00000000000000000']}
+    assert result == ({'eni-00000000000000000': ['sg-00000000000000000']}, 'AWS - EC2')
 
 
 def test_sg_fix(mocker):
@@ -70,7 +70,7 @@ def test_sg_fix(mocker):
     from test_data.sample import SG_INFO
     new_sg = [{'Type': 1, 'Contents': {'AWS.EC2.SecurityGroups': {'GroupId': 'sg-00000000000000001'}}}]
     mocker.patch.object(demisto, "executeCommand", return_value=new_sg)
-    args = {"sg_info": SG_INFO, "port": 22, "protocol": "tcp"}
+    args = {"sg_info": SG_INFO, "port": 22, "protocol": "tcp", "assume_role": "test_role", "integration_to_use": "AWS - EC2"}
     result = sg_fix(**args)
     assert result == {'new-sg': 'sg-00000000000000001'}
 
@@ -96,7 +96,8 @@ def test_determine_excessive_access(mocker):
             return new_sg
 
     mocker.patch.object(demisto, "executeCommand", side_effect=executeCommand)
-    args = {"int_sg_mapping": {'eni-00000000000000000': ['sg-00000000000000000']}, "port": 22, "protocol": "tcp"}
+    args = {"int_sg_mapping": {'eni-00000000000000000': ['sg-00000000000000000']}, "port": 22,
+            "protocol": "tcp", "assume_role": "test_role", "integration_to_use": "AWS - EC2"}
     result = determine_excessive_access(**args)
     assert result == [{'int': 'eni-00000000000000000', 'old-sg': 'sg-00000000000000000', 'new-sg': 'sg-00000000000000001'}]
 
