a5832432961600ec5633cd8aaf659015f554441b
dansterenson <dansterenson@gmail.com>
2023-08-29 22:48:39 +0300

    testing machine

--- Tests/scripts/wait_in_line_for_cloud_env.sh
+++ Tests/scripts/wait_in_line_for_cloud_env.sh
@@ -13,6 +13,7 @@ set -e
 touch CloudEnvVariables
 
 # Filter out not enabled and unnecessary machines
+echo "flow_type $1"
 CLOUD_SERVERS_FILE=$(cat $CLOUD_SERVERS_FILE)
 TEST_MACHINES_LIST=$(jq --arg flow_type "$1" 'to_entries | map(select(.value.enabled == true and .value.flow_type == $flow_type)) | from_entries' "$CLOUD_SERVERS_FILE")
 
