ab40df9f3154e16ba8a4361fa7d0dec40ce6e68e
Koby Meir <kobymeir@users.noreply.github.com>
2023-09-02 08:57:12 +0300

    Test modeling rules junit (#29085)
    Test modeling rules junit

--- Tests/scripts/print_cloud_machine_details.sh
+++ Tests/scripts/print_cloud_machine_details.sh
@@ -1,9 +1,9 @@
-CLOUD_SERVERS_PATH=$(cat $CLOUD_SERVERS_FILE)
+CLOUD_SERVERS_PATH=$(cat "${CLOUD_SERVERS_FILE}")
 
-if ! [ -z "$CLOUD_CHOSEN_MACHINE_ID" ]
+if [ -n "${CLOUD_CHOSEN_MACHINE_ID}" ]
 then
   echo "The tests run on machine: $CLOUD_CHOSEN_MACHINE_ID"
-  UI_URL=`cat $CLOUD_SERVERS_PATH | jq -c ". | .\"$CLOUD_CHOSEN_MACHINE_ID\" | .ui_url"`
+  UI_URL=$(jq -c ". | .\"$CLOUD_CHOSEN_MACHINE_ID\" | .ui_url" "${CLOUD_SERVERS_PATH}")
   BUCKET_URL="https://console.cloud.google.com/storage/browser/$GCS_MACHINES_BUCKET/$CLOUD_CHOSEN_MACHINE_ID/"
   echo "machine url: $UI_URL"
   echo "machine marketplace bucket: $BUCKET_URL"
