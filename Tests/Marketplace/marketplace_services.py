7ac47b0ed844a06576e4bbab8bed3577fc78f11c
omerKarkKatz <95565843+omerKarkKatz@users.noreply.github.com>
2023-09-04 11:39:37 +0300

    1.20.0 (#29407)
    * start implamenting content changes for xsoar_saas bukcet
    
    * changed the names of the xsoar saas
    
    * commit
    
    * commit
    
    * commit
    
    * added some functionality in upload_pack.py and on-push
    
    * some modifications in copy_and_upload
    
    * commit
    
    * trigger upload flow changes bucket upload and on-push
    
    * fixed the syntax of the bucket upload file
    
    * poetry lock
    
    * fixed the syntax of the on-push file
    
    * Changed the condifition to not parse xsoar and xsoar-saas inorder to remove xsoar name
    
    * Trigger-build
    
    * Trigger-build
    
    * removed a test dict that overide the readme images data
    
    * changed name of prod bucket
    
    * empty
    
    * commit
    
    * empty
    
    * poetry lock
    
    * added xsoar_saas to upload part so it wont be skipped
    
    * empty
    
    * updated the poetry.lock file
    
    * fixed a type in copy_and_upload.py marketplace parameter
    
    * fixed some ruff changes
    
    * added a test in google sheets and added the correct base_storage_path in upload_packs
    
    * bumped google sheets version and poetry lock for sdk changes
    
    * commit
    
    * fixed a test
    
    * poetry lock
    
    * change in collect tests
    
    * poetry lock
    
    * merge with master
    
    * added some tests in VT
    
    * added some tests in VT
    
    * changed logging level
    
    * fixed a small bug in get_images
    
    * fixed an issue that the copy_readme_images would exit before finish
    
    * ruff changes
    
    * fixed a test
    
    * merged with master
    
    * replace raedme with markdown
    
    * poetry lock
    
    * ruff changes
    
    * revert packs changes
    
    * revert packs changes
    
    * poetry lock after updating schemas in the sdk
    
    * merge with master
    
    * poetry lock
    
    * replaced some readme reference name with markdown
    
    * poetry lock
    
    * poetry lock
    
    * removed a todo
    
    * added some debug print and added and if for the uplod
    
    * fixed an merge issue in bucket-upload
    
    * commit
    
    * poetry lock
    
    * restored file name to avoid force merge
    
    * fixed some pre-commit fails
    
    * poetry lock
    
    * commit
    
    * changed the version of junit
    
    * updated sdk and junit
    
    * revert changed in pyproject and poetry lock
    
    * made the ..pre-commit-config_template.yaml like master
    
    * fixed a test
    
    * return the finally
    
    * revert double assert
    
    * make log debug

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -1134,7 +1134,10 @@ class Pack:
             task_status = True
             if pack_was_modified:
                 # Make sure the modification is not only of release notes files, if so count that as not modified
-                pack_was_modified = not all(self.RELEASE_NOTES in path for path in modified_rn_files_paths)
+                pack_was_modified = any(
+                    self.RELEASE_NOTES not in path
+                    for path in modified_rn_files_paths
+                )
                 # Filter modifications in release notes config JSON file - they will be handled later on.
                 modified_rn_files_paths = [path_ for path_ in modified_rn_files_paths if path_.endswith('.md')]
             return None
@@ -2514,6 +2517,9 @@ class Pack:
             self._eula_link = user_metadata.get(Metadata.EULA_LINK, Metadata.EULA_URL)
             self._marketplaces = user_metadata.get(Metadata.MARKETPLACES, ['xsoar', 'marketplacev2'])
             self._modules = user_metadata.get(Metadata.MODULES, [])
+
+            if 'xsoar' in self.marketplaces:
+                self.marketplaces.append('xsoar_saas')
 
             logging.info(f"Finished loading {self._pack_name} pack user metadata")
             task_status = True
@@ -3917,7 +3923,7 @@ def store_successful_and_failed_packs_in_ci_artifacts(packs_results_file_path: s
     if images_data:
         # adds a list with all the packs that were changed with images
         packs_results[stage].update({BucketUploadFlow.IMAGES: images_data})
-        logging.debug(f"Images data {images_data}")
+        logging.info(f"Images data {images_data}")
 
     if packs_results:
         if stage == BucketUploadFlow.PREPARE_CONTENT_FOR_TESTING:
