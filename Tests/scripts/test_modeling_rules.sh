141df9006614fdb2f5ec1e7fff0b6520415f5929
Koby Meir <kobymeir@users.noreply.github.com>
2023-09-04 08:39:03 +0300

    fix test modeling rule (#29416)
    Co-authored-by: kobymeir <ymeir@paloaltonetworks.com>

--- Tests/scripts/test_modeling_rules.sh
+++ Tests/scripts/test_modeling_rules.sh
@@ -40,34 +40,40 @@ if [[ -z "${MODELING_RULES_TO_TEST}" ]]; then
     exit 1
 fi
 
+MODELING_RULES_RESULTS_FILE_NAME="${ARTIFACTS_FOLDER}/modeling_rules_results.xml"
 if DEMISTO_SDK_SKIP_VERSION_CHECK=True demisto-sdk modeling-rules test --help 2>&1 | grep 'junit-path'; then
-  MODELING_RULES_RESULTS_FILE_NAME="${ARTIFACTS_FOLDER}/modeling_rules_results.xml"
   MODELING_RULES_RESULTS_ARG=(" --junit-path" "${MODELING_RULES_RESULTS_FILE_NAME}")
   echo "Testing Modeling Rules - Results will be saved to ${MODELING_RULES_RESULTS_FILE_NAME}"
 else
   echo "Testing Modeling Rules - demisto-sdk version is too old, creating empty junit file to ${MODELING_RULES_RESULTS_FILE_NAME}"
   MODELING_RULES_RESULTS_ARG=()
-  MODELING_RULES_RESULTS_FILE_NAME=""
   cat <<EOF > "${MODELING_RULES_RESULTS_FILE_NAME}"
 <?xml version='1.0' encoding='utf-8'?>
 <testsuites />
 EOF
+    MODELING_RULES_RESULTS_FILE_NAME=""  # Reset the file name so that the next command will not try to use it.
 fi
 
 demisto-sdk modeling-rules test --xsiam-url="$XSIAM_URL" --auth-id="$AUTH_ID" --api-key="$API_KEY" \
   --xsiam-token="$XSIAM_TOKEN" --non-interactive "${MODELING_RULES_RESULTS_ARG[@]}" \
   ${MODELING_RULES_TO_TEST}
 exit_code=$?
-  echo "Finished testing Modeling Rules, exit_code=$exit_code"
+echo "Finished testing Modeling Rules, exit_code=$exit_code"
 
-if [[ $exit_code -ne 0 ]]; then
+if [[ "${exit_code}" -ne 0 ]]; then
   echo "Failed to test modeling rules"
-  if [ -z "${NIGHTLY}" ] && [ -n "${MODELING_RULES_RESULTS_FILE_NAME}" ]; then
+  if [ -n "${NIGHTLY}" ] && [ -n "${MODELING_RULES_RESULTS_FILE_NAME}" ]; then
     echo "This is a nightly build, converting the results to Jira issues and exiting with 0"
     python3 "${CURRENT_DIR}/Tests/scripts/convert_test_result_to_jira_issues.py" --junit-path "${MODELING_RULES_RESULTS_FILE_NAME}"
-    exit $? # exit with the exit code of the python script
+    exit_code=$?
+    if [[ "${exit_code}" -ne 0 ]]; then
+      echo "Failed to convert the results to Jira issues"
+      exit ${exit_code}
+    fi
+    exit 0  # Exiting with 0 so that the build will not fail, because we successfully converted the results to Jira issues.
   else
-    exit $exit_code
+    exit ${exit_code}
   fi
 fi
 
+exit ${exit_code}
