48abf34352128cc032247f53fae00fd943b2a7b5
Sébastien Guisnet <sebastien.guisnet@nucleon-security.com>
2023-09-05 16:45:56 +0200

    Create test units for RSA Get Raw logs and display scripts

---
+++ Packs/RSANetWitness_v11_1/Scripts/RSADisplayMetasEvents/RSADisplayMetasEvents_test.py
@@ -0,0 +1,20 @@
+import pytest
+from RSADisplayMetasEvents import CamelCaseToDotCase, display_metas
+
+def test_CamelCaseToDotCase():
+    assert CamelCaseToDotCase("eventSource") == "event.source"
+
+@pytest.mark.parametrize(
+    "alerts_incident, expected_results",
+    [
+        ({"CustomFields": {"metasevents": [{"ip": "ip", "host": "host"}]}},
+         {'Contents': '|ip|host|\n|---|---|\n| ip | host |\n', 'ContentsFormat': 'markdown', 'Type': 1}),
+    ],
+)
+def test_display_metas(
+    mocker, alerts_incident, expected_results
+):
+    mocker.patch(
+        "RSADisplayMetasEvents.demisto.incident", return_value=alerts_incident
+    )
+    assert display_metas() == expected_results
