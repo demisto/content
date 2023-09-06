7fa3307f5f11d136b8f5ea7c981a5190ef5a260c
asimsarpkurt <79475614+asimsarpkurt@users.noreply.github.com>
2023-09-06 10:54:08 +0300

    Update RoksitDNSSecurityIntegrationSarp.py

--- Packs/RoksitDNSSecurityIntegration-Sarp/Integrations/RoksitDNSSecurityIntegrationSarp/RoksitDNSSecurityIntegrationSarp.py
+++ Packs/RoksitDNSSecurityIntegration-Sarp/Integrations/RoksitDNSSecurityIntegrationSarp/RoksitDNSSecurityIntegrationSarp.py
@@ -3,12 +3,16 @@ from CommonServerPython import *  # noqa: F401
 import requests
 
 url = demisto.params().get('url')
-yourkey = demisto.params().get("token")
+domaintobeblocked=demisto.getArg('domain')
+if domaintobeblocked:
+    blacklist = {'items':[domaintobeblocked]}
+else:
+    roksit_domain=(demisto.incident().get("CustomFields").get("roksitblockdomain"))
+    blacklist = {'items': [roksit_domain]}
 
+yourkey = demisto.params().get("token")
 headers = {'Content-Type': 'application/json',
            'ApiKey': yourkey}
-roksit_domain=(demisto.incident().get("CustomFields").get("roksitblockdomain"))
-blacklist = {'items': [roksit_domain]}
-x = requests.post(url, headers=headers, json=blacklist)
 
+x = requests.post(url, headers=headers, json=blacklist)
 print(x.text)
