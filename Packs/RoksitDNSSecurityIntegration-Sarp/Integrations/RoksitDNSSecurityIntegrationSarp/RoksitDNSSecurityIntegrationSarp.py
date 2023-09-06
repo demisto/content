381845df47ebb0fd000ceced8e97c775035ac1f3
Yehuda <yrosenberg@paloaltonetworks.com>
2023-09-06 11:41:43 +0300

    Yehuda's version

--- Packs/RoksitDNSSecurityIntegration-Sarp/Integrations/RoksitDNSSecurityIntegrationSarp/RoksitDNSSecurityIntegrationSarp.py
+++ Packs/RoksitDNSSecurityIntegration-Sarp/Integrations/RoksitDNSSecurityIntegrationSarp/RoksitDNSSecurityIntegrationSarp.py
@@ -2,13 +2,37 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 import requests
 
-url = demisto.params().get('url')
-yourkey = demisto.params().get("token")
 
-headers = {'Content-Type': 'application/json',
-           'ApiKey': yourkey}
-roksit_domain=(demisto.incident().get("CustomFields").get("roksitblockdomain"))
-blacklist = {'items': [roksit_domain]}
-x = requests.post(url, headers=headers, json=blacklist)
+def send_to_roksit_blacklist(url, yourkey, args):
+    domain = args.get('Domain')
+    headers = {'Content-Type': 'application/json',
+               'ApiKey': yourkey}
 
-print(x.text)
+    blacklist = {'items': [domain]}
+    x = requests.post(url, headers=headers, json=blacklist)
+    if x.status_code == 200:
+        return CommandResults(
+            readable_output="Domain added to blacklist"
+        )
+    else:
+        raise DemistoException(message="Failed to add domain to blacklist")
+
+
+def main():
+    try:
+        params = demisto.params()
+        args = demisto.args()
+        command = demisto.command()
+        url = params.get('url', 'https://portal.roksit.com/api/integration/blacklist')
+        yourkey = params.get('credentials', {}).get('password')
+
+        if command == 'test-module':
+            pass
+        elif command == 'Roksit-add-to-blacklist':
+            return_results(send_to_roksit_blacklist(url, yourkey, args))
+    except DemistoException as e:
+        return_error(f'Failed to execute {command} command. Error: {str(e)}.')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
