987448b0617e0d9c0090733983d919ea387eff1c
yucohen <yucohen@paloaltonetworks.com>
2023-09-07 12:33:10 +0300

    added test case

--- Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
+++ Packs/GenericSQL/Integrations/GenericSQL/GenericSQL_test.py
@@ -263,8 +263,12 @@ def test_mysql_integration():
 
 
 @pytest.mark.parametrize('connect_parameters, dialect, expected_response', [
-    ('arg1=value1&arg2=value2', 'MySQL', {'arg1': 'value1', 'arg2': 'value2'}),
-    ('arg1=value1&arg2=value2', 'Microsoft SQL Server', {'arg1': 'value1', 'arg2': 'value2', 'driver': 'FreeTDS'}),
+    ('arg1=value1&arg2=value2', 'MySQL',
+     {'arg1': 'value1', 'arg2': 'value2'}),
+    ('arg1=value1&arg2=value2', 'Microsoft SQL Server',
+     {'arg1': 'value1', 'arg2': 'value2', 'driver': 'FreeTDS', 'autocommit': 'True'}),
+    ('arg1=value1&arg2=value2&autocommit=False', 'Microsoft SQL Server',
+     {'arg1': 'value1', 'arg2': 'value2', 'driver': 'FreeTDS', 'autocommit': 'False'}),
     ('arg1=value1&arg2=value2', 'Microsoft SQL Server - MS ODBC Driver',
      {'arg1': 'value1', 'arg2': 'value2', 'driver': 'ODBC Driver 18 for SQL Server', 'TrustServerCertificate': 'yes'})])
 def test_parse_connect_parameters(connect_parameters, dialect, expected_response):
