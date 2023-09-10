e4883f16373a764f16581db3e6ebabc5bb191647
yaakovpraisler <ypreisler@paloaltonetworks.com>
2023-09-10 13:11:32 +0300

    Support background types for svg images

--- Tests/Marketplace/marketplace_services.py
+++ Tests/Marketplace/marketplace_services.py
@@ -3628,8 +3628,14 @@ class Pack:
             with open(integration_yaml_path[0]) as pack_file:
                 integration_yaml_content = yaml.safe_load(pack_file)
 
-            image_storage_path = os.path.join(pack_storage_root_path,
+            image_background = re.findall(r'_([^.]*)\.svg', dynamic_dashboard_image)
+            if not image_background:
+                raise f"Could not find background for image in path {dynamic_dashboard_image}.\n" \
+                      "The svg image file should be named either as `<ImageName>_dark.svg` or `<ImageName>_light.svg`"
+
+            image_storage_path = os.path.join(pack_storage_root_path, image_background[0],
                                               f"{integration_yaml_content.get('commonfields', {}).get('id', '')}.svg")
+            logging.debug(f"Uploading image in path '{dynamic_dashboard_image}' to bucket directory '{image_storage_path}'")
             pack_image_blob = storage_bucket.blob(image_storage_path)
 
             try:
