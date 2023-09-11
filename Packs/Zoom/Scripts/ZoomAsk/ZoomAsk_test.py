571a8bdcd3f8150c03a626781fd3e958fb11cfe2
jbabazadeh <jbabazadeh@paloaltonetworks.com>
2023-09-11 09:43:51 +0300

    zoomask tests

--- Packs/Zoom/Scripts/ZoomAsk/ZoomAsk_test.py
+++ Packs/Zoom/Scripts/ZoomAsk/ZoomAsk_test.py
@@ -1,52 +1,17 @@
 import demistomock as demisto
 from ZoomAsk import generate_json, main
 import dateparser
+from CommonServerPython import entryTypes
 import datetime
-import json
 
 
-def test_zoomask_user(mocker):
-    # Set
-    mocker.patch.object(demisto, 'executeCommand')
-    mocker.patch.object(demisto, 'investigation', return_value={'id': '22'})
-    mocker.patch.object(demisto, 'args', return_value={
-        'user': 'alexios',
-        'message': 'wat up',
-        'option1': 'yes#red',
-        'option2': 'no#red',
-        'reply': 'Thank you brother.',
-        'lifetime': '24 hours',
-        'defaultResponse': 'NoResponse',
-        'using-brand': 'ZoomAsk',
-        'zoomInstance': 'TestingInstance1'
-    })
-    mocker.patch.object(demisto, 'results')
-    mocker.patch.object(dateparser, 'parse', return_value=datetime.datetime(2019, 9, 26, 18, 38, 25))
+def test_generate_json_button():
+    text = "Test Text"
+    options = ["Option1#blue", "Option2#red"]
+    response_type = "button"
+    result = generate_json(text, options, response_type)
 
-    # Arrange
-    main()
-    call_args = demisto.executeCommand.call_args[0]
-
-    # Assert
-    assert call_args[1] == {
-        'ignoreAddURL': 'true',
-        'using-brand': 'ZoomAsk',
-        'using': 'TestingInstance1',
-        # Update the expected 'blocks' value to match your script's logic
-        'blocks': json.dumps({
-            # Update with your script's block structure
-        }),
-        'message': 'wat up',
-        'to': 'alexios'
-    }
-
-
-def test_generate_json_buttons():
-    # Test generate_json function for 'buttons' responseType
-    text = 'Test Message'
-    options = ['Option 1#blue', 'Option 2#red']
-    response_type = 'buttons'
-    expected_json = {
+    expected_result = {
         "head": {
             "type": "message",
             "text": text
@@ -56,38 +21,96 @@ def test_generate_json_buttons():
                 "type": "actions",
                 "items": [
                     {
-                        "value": "Option 1",
+                        "value": "Option1",
                         "style": "Primary",
-                        "text": "Option 1"
+                        "text": "Option1"
                     },
                     {
-                        "value": "Option 2",
+                        "value": "Option2",
                         "style": "Danger",
-                        "text": "Option 2"
+                        "text": "Option2"
                     }
                 ]
             }
         ]
     }
-    assert generate_json(text, options, response_type) == expected_json
+
+    assert result == expected_result
 
 
 def test_generate_json_dropdown():
-    # Test generate_json function for 'dropdown' responseType
-    text = 'Test Message'
-    options = ['Option 1#blue', 'Option 2#red']
-    response_type = 'dropdown'
-    expected_json = {
+    text = "Test Text"
+    options = ["Option1#blue", "Option2#red"]
+    response_type = "dropdown"
+    result = generate_json(text, options, response_type)
+
+    expected_result = {
         "body": [
             {
                 "select_items": [
-                    {"value": "Option 1", "text": "Option 1"},
-                    {"value": "Option 2", "text": "Option 2"}
+                    {
+                        "value": "Option1",
+                        "text": "Option1"
+                    },
+                    {
+                        "value": "Option2",
+                        "text": "Option2"
+                    }
                 ],
                 "text": text,
-                "selected_item": {"value": "Option 1"},
+                "selected_item": {
+                    "value": "Option1"
+                },
                 "type": "select"
             }
         ]
     }
-    assert generate_json(text, options, response_type) == expected_json
+
+    assert result == expected_result
+
+
+def execute_command(command, args):
+    if command == 'addEntitlement':
+        return [{
+            'Type': entryTypes['note'],
+            'Contents': '4404dae8-2d45-46bd-85fa-64779c12abe8'
+        }]
+
+    return []
+
+
+def test_main(mocker):
+
+    mocker.patch.object(demisto, 'executeCommand', side_effect=execute_command)
+    mocker.patch.object(demisto, 'investigation', return_value={'id': '22'})
+    mocker.patch.object(demisto, 'args', return_value={
+        'persistent': 'true',
+        'option1': 'Option1',
+        'option2': 'Option2',
+        'additionalOptions': '',
+        'reply': 'Test Reply',
+        'responseType': 'button',
+        'lifetime': '1 day',
+        'defaultResponse': 'Default Response',
+        'user': 'test@example.com',
+        'message': 'Test Message',
+        'channel_name': None,
+        'channel_id': None,
+        'task': None
+    })
+    mocker.patch.object(demisto, 'results')
+    mocker.patch.object(dateparser, 'parse', return_value=datetime.datetime(2019, 9, 26, 18, 38, 25))
+
+    # Call the main function
+    main()
+
+    call_args = demisto.executeCommand.call_args[0]
+    expected_args = {
+        'ignoreAddURL': 'true',
+        'message':
+            '{"blocks": "{\\"head\\": {\\"type\\": \\"message\\", \\"text\\": \\"Test Message\\"}, \\"body\\": [{\\"type\\": \\"actions\\", \\"items\\": [{\\"value\\": \\"Option1\\", \\"style\\": \\"Default\\", \\"text\\": \\"Option1\\"}, {\\"value\\": \\"Option2\\", \\"style\\": \\"Default\\", \\"text\\": \\"Option2\\"}]}]}", "entitlement": "4404dae8-2d45-46bd-85fa-64779c12abe8@22", "reply": "Test Reply", "expiry": "2019-09-26 18:38:25", "default_response": "Default Response"}',  # noqa: E501
+        'to': 'test@example.com',
+        'zoom_ask': 'true'
+    }
+
+    assert call_args[1] == expected_args
