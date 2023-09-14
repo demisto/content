c1283482223820e66e08d4180e9c04def245c539
michal-dagan <mdagan@paloaltonetworks.com>
2023-09-14 12:08:54 +0300

    add logs

--- Utils/gitlab_triggers/trigger_build_url.sh
+++ Utils/gitlab_triggers/trigger_build_url.sh
@@ -1,4 +1,6 @@
 CI_SERVER_URL=${CI_SERVER_URL:-https://code.pan.run}
 CI_PROJECT_ID=${CI_PROJECT_ID:-2596}
+echo "CI_SERVER_URL is set to: ${CI_SERVER_URL}"
+echo "CI_PROJECT_ID is set to: ${CI_PROJECT_ID}"
 export BUILD_TRIGGER_URL="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/trigger/pipeline"  # disable-secrets-detection
 echo "BUILD_TRIGGER_URL is set to: ${BUILD_TRIGGER_URL}"
