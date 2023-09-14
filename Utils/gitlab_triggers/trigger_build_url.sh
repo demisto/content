3f193b0973563d1c722bcf4dc0166074ab43406e
yucohen <yucohen@paloaltonetworks.com>
2023-09-14 15:04:19 +0300

    test

--- Utils/gitlab_triggers/trigger_build_url.sh
+++ Utils/gitlab_triggers/trigger_build_url.sh
@@ -1,3 +1,5 @@
 CI_SERVER_URL=${CI_SERVER_URL:-https://code.pan.run}
 CI_PROJECT_ID=${CI_PROJECT_ID:-2596}
 export BUILD_TRIGGER_URL="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/trigger/pipeline"  # disable-secrets-detection
+if [ -n "$BUILD_TRIGGER_URL" ]; then
+  echo "$BUILD_TRIGGER_URL"
