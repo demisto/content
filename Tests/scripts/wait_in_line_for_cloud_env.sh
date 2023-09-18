68a3514b4681448fe8f3337c23b7b8287bbed5a1
kobymeir <ymeir@paloaltonetworks.com>
2023-09-18 19:21:40 +0300

    build machine daily cleanup pipeline - support locking all available machines

--- Tests/scripts/wait_in_line_for_cloud_env.sh
+++ Tests/scripts/wait_in_line_for_cloud_env.sh
@@ -26,15 +26,33 @@ echo "requested flow type is: $1, Number of available machines is: $NUM_OF_TEST_
 TEST_MACHINES_LIST_STRING=$(echo "$TEST_MACHINES_LIST" | jq -r 'keys | join(",")')
 echo "All existing machines: $TEST_MACHINES_LIST_STRING"
 
-if [[ -z $TEST_MACHINES_LIST_STRING ]];
-then
+if [[ -z $TEST_MACHINES_LIST_STRING ]]; then
   echo "No machines in Test Machines List."
   exit 1
 fi
 
-if [[ $TEST_MACHINES_LIST_STRING != *"$LOCK_MACHINE_NAME"* ]];
-then
+if [[ $TEST_MACHINES_LIST_STRING != *"$LOCK_MACHINE_NAME"* ]]; then
   echo "Machine that you're trying to lock: '$LOCK_MACHINE_NAME' does not exist in Test Machines List."
+  exit 1
+fi
+
+if [[ "${NUM_OF_TEST_MACHINES}" -eq 0 ]]; then
+  echo "No machines are available for testing."
+  exit 1
+fi
+
+if [[ -z "${CLOUD_MACHINES_COUNT}" ]]; then
+  echo "Number of machines to lock is not set."
+  exit 1
+fi
+
+if [[ "${CLOUD_MACHINES_COUNT}" == "all" ]; then
+  echo "Got request to lock all available machines, locking: ${NUM_OF_TEST_MACHINES} machines"
+  CLOUD_MACHINES_COUNT="${NUM_OF_TEST_MACHINES}"
+fi
+
+if [[ "${CLOUD_MACHINES_COUNT}" -gt "${NUM_OF_TEST_MACHINES}" ]]; then
+  echo "Number of machines to lock is greater than the number of available machines."
   exit 1
 fi
 
@@ -43,4 +61,4 @@ echo -e "We have ${NUM_OF_TEST_MACHINES} machines for testing and a lot more bui
 echo -e "If we want to make sure our product stays amazing, we will have to work together and keep an orderly queue"
 echo -e "May the tests be in our favour. Good luck to us all"
 
-python3 ./Tests/scripts/lock_cloud_machines.py --service_account $GCS_ARTIFACTS_KEY --gcs_locks_path $GCS_LOCKS_PATH  --ci_job_id $CI_JOB_ID  --test_machines $TEST_MACHINES_LIST_STRING  --gitlab_status_token $GITLAB_STATUS_TOKEN --lock_machine_name "$LOCK_MACHINE_NAME" --number_machines_to_lock ${CLOUD_MACHINES_COUNT} --response_machine CloudEnvVariables
+python3 ./Tests/scripts/lock_cloud_machines.py --service_account $GCS_ARTIFACTS_KEY --gcs_locks_path $GCS_LOCKS_PATH  --ci_job_id $CI_JOB_ID  --test_machines $TEST_MACHINES_LIST_STRING  --gitlab_status_token $GITLAB_STATUS_TOKEN --lock_machine_name "${LOCK_MACHINE_NAME}" --number_machines_to_lock ${CLOUD_MACHINES_COUNT} --response_machine CloudEnvVariables
