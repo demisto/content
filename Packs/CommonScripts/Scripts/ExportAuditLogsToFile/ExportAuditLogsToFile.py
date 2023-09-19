46ba0a3add679007204a23de61dcbb3489705be5
GuyAfik <guyafik11@gmail.com>
2023-09-19 18:08:57 +0300

    ExportAuditLogsToFile - add support for xsoar-8

--- Packs/CommonScripts/Scripts/ExportAuditLogsToFile/ExportAuditLogsToFile.py
+++ Packs/CommonScripts/Scripts/ExportAuditLogsToFile/ExportAuditLogsToFile.py
@@ -6,51 +6,88 @@ import json
 from datetime import date, timedelta
 
 
+def get_audit_logs(res: Dict):
+
+    def get_xsoar6_audit_logs():
+        return res.get('audits') or []
+
+    def get_xsoar8_audit_logs():
+        return (res.get('reply') or {}).get('data') or []
+
+    return get_xsoar6_audit_logs() or get_xsoar8_audit_logs()
+
+
+def get_audit_logs_count(res: Dict):
+    def get_xsoar6_audit_logs_count():
+        return res.get('total', 0)
+
+    def get_xsoar8_audit_logs_count():
+        return res.get('total_count', 0)
+
+    return arg_to_number(get_xsoar6_audit_logs_count() or get_xsoar8_audit_logs_count())
+
+
 def main():   # pragma: no cover
     # get the file type
     file_type = demisto.args().get("output")
 
     # set time back for fetching
     fetch_back_date = date.today() - timedelta(days=int(demisto.args().get("days_back")))
+    fetch_from = fetch_back_date.strftime("%Y-%m-%dT00:00:00Z")
 
     demisto_version: str = get_demisto_version().get("version")
     demisto.debug(f'{demisto_version=}')
     if not demisto_version:
-        raise ValueError(f'Could not get the version of XSOAR.')
+        raise ValueError(f'Could not get the version of XSOAR')
+
+    page = 0
+    size = 100
 
     if demisto_version.startswith("6"):  # xsoar 6
         uri = "/settings/audits"
         body = {
-            'fromDate': fetch_back_date.strftime("%Y-%m-%dT00:00:00Z"),
+            'fromDate': fetch_from,
             'query': "",
-            'page': 0,
-            'size': 100
+            'page': page,
+            'size': size
         }
     else:  # xsoar 8
         uri = "/public_api/v1/audits/management_logs"
         body = {
             "request_data": {
-                "search_from": date_to_timestamp(fetch_back_date)
+                "search_from": page,
+                "search_to": size,
+                "filters": [
+                    {
+                        'field': 'timestamp',
+                        'operator': 'gte',
+                        'value': date_to_timestamp(fetch_back_date)
+                    },
+                ]
+                # "page": page
             }
         }
 
     args = {"uri": uri, "body": body}
     demisto.log(f'{args=}')
-    res = demisto.executeCommand("demisto-api-post", args)
-
-    demisto.log(f'{res=}')
+    res = demisto.executeCommand("demisto-api-post", args)[0]["Contents"]["response"]
+    if is_error(res):
+        raise DemistoException(f'error occurred when trying to retrieve the audit logs using {args=}, error: {res}')
 
     # set the initial counts
-    total = res.get('total', 0)
-    audits = res.get('audits', [])
+    total = get_audit_logs_count(res)
+    audits = get_audit_logs(res)
     count = 1
 
     # if there are more events than the default size, page through and get them all
     while len(audits) < total:
-        body['page'] = count
-        res = demisto.executeCommand("demisto-api-post", {"uri": "/settings/audits", "body": body})[0]["Contents"][
-            "response"]
-        audits.extend(res.get('audits', []))
+        if 'page' in body:  # pagination for xsoar-6
+            body['page'] = count
+        else:  # pagination for xsoar-8
+            body["request_data"]["search_from"] = count
+        args = {"uri": uri, "body": body}
+        res = demisto.executeCommand("demisto-api-post", args)[0]["Contents"]["response"]
+        audits.extend(get_audit_logs(res))
         count += 1
         # break if this goes crazy, if there are more than 100 pages of audit log entries.
         if count == 100:
