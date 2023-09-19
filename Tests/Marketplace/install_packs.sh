2e6f40754eb0b693f94febbb822f145fcf966579
Koby Meir <kobymeir@users.noreply.github.com>
2023-09-19 15:45:38 +0300

    XSIAM Build machines cleanup pipeline (#29694)
    * build machine daily cleanup pipeline

--- Tests/Marketplace/install_packs.sh
+++ Tests/Marketplace/install_packs.sh
@@ -7,14 +7,49 @@ XSIAM_SERVERS_PATH=$(cat xsiam_servers_path)
 
 EXTRACT_FOLDER=$(mktemp -d)
 
+INSTANCE_ROLE=${1}
+if [ -z "${INSTANCE_ROLE}" ]; then
+  echo "INSTANCE_ROLE not set aborting pack installation!"
+  exit 1
+fi
+
 if [[ ! -f "$GCS_MARKET_KEY" ]]; then
     echo "GCS_MARKET_KEY not set aborting pack installation!"
     exit 1
 fi
 
+echo "Trying to authenticate with GCS..."
 gcloud auth activate-service-account --key-file="$GCS_MARKET_KEY" > auth.out 2>&1
-echo "Auth loaded successfully."
+if [ $? -ne 0 ]; then
+  echo "Failed to authenticate with GCS, exiting..."
+  exit 1
+fi
+echo "Successfully authenticated with GCS."
 
-echo "starting configure_and_install_packs ..."
+echo "starting configure_and_install_packs instance role: ${INSTANCE_ROLE}"
+exit_code=0
+if [[ "${INSTANCE_ROLE}" == "XSIAM" ]]; then
 
-python3 ./Tests/Marketplace/configure_and_install_packs.py -s "$SECRET_CONF_PATH" --ami_env "$1" --branch "$CI_COMMIT_BRANCH" --build_number "$CI_PIPELINE_ID" --service_account $GCS_MARKET_KEY -e "$EXTRACT_FOLDER" --cloud_machine "$CLOUD_CHOSEN_MACHINE_ID" --cloud_servers_path $XSIAM_SERVERS_PATH --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt" --cloud_servers_api_keys $XSIAM_API_KEYS
+  if [ -n "${CLOUD_CHOSEN_MACHINE_IDS}" ]; then
+    IFS=', ' read -r -a CLOUD_CHOSEN_MACHINE_ID_ARRAY <<< "${CLOUD_CHOSEN_MACHINE_IDS}"
+    for CLOUD_CHOSEN_MACHINE_ID in "${CLOUD_CHOSEN_MACHINE_ID_ARRAY[@]}"; do
+      python3 ./Tests/Marketplace/configure_and_install_packs.py -s "$SECRET_CONF_PATH" --ami_env "${INSTANCE_ROLE}" --branch "$CI_COMMIT_BRANCH" --build_number "$CI_PIPELINE_ID" --service_account $GCS_MARKET_KEY -e "$EXTRACT_FOLDER" --cloud_machine "${CLOUD_CHOSEN_MACHINE_ID}" --cloud_servers_path ${XSIAM_SERVERS_PATH} --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt" --cloud_servers_api_keys $XSIAM_API_KEYS
+      if [ $? -ne 0 ]; then
+        exit_code=1
+      fi
+    done
+    echo "Finished configure_and_install_packs script with exit code ${exit_code}"
+    exit "${exit_code}"
+  else
+    echo "No machines were chosen, exiting with exit code 1"
+    exit 1
+  fi
+else
+  # Running on XSOAR instance roles
+  python3 ./Tests/Marketplace/configure_and_install_packs.py -s "$SECRET_CONF_PATH" --ami_env "${INSTANCE_ROLE}" --branch "$CI_COMMIT_BRANCH" --build_number "$CI_PIPELINE_ID" --service_account $GCS_MARKET_KEY -e "$EXTRACT_FOLDER" --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt"
+  if [ $? -ne 0 ]; then
+    exit_code=1
+  fi
+  echo "Finished configure_and_install_packs script with exit code ${exit_code}"
+  exit "${exit_code}"
+fi
--- Tests/scripts/install_content_and_test_integrations.sh
+++ Tests/scripts/install_content_and_test_integrations.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-# exit on errors
-set -e
-
 SECRET_CONF_PATH=$(cat secret_conf_path)
-CLOUD_SERVERS_PATH=$(cat $CLOUD_SERVERS_FILE)
-echo ${CLOUD_API_KEYS} > "cloud_api_keys.json"
+CLOUD_SERVERS_PATH=$(cat "${CLOUD_SERVERS_FILE}")
+echo "${CLOUD_API_KEYS}" > "cloud_api_keys.json"
 
 CONF_PATH="./Tests/conf.json"
-
 IS_NIGHTLY=false
 
 if [ -n "${NIGHTLY}" ]; then
@@ -18,14 +14,29 @@ fi
 if [ -f ./Tests/test_pack.zip ]; then
   echo "Copying test_pack.zip to artifacts folder:${ARTIFACTS_FOLDER}"
   cp ./Tests/test_pack.zip "$ARTIFACTS_FOLDER"
+  exit_code=$?
+  if [ ${exit_code} -ne 0 ]; then
+    echo "Failed to copy test_pack.zip to artifacts folder:${ARTIFACTS_FOLDER} - Exiting with code ${exit_code}"
+    exit ${exit_code}
+  fi
 else
   echo "test_pack.zip was not found in the build directory, skipping..."
 fi
 
 echo "Starting configure_and_test_integration_instances.sh script nightly=${IS_NIGHTLY}"
 
-python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" --tests_to_run "$ARTIFACTS_FOLDER/filter_file.txt"  --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$CI_COMMIT_BRANCH" --build-number "$CI_PIPELINE_ID" -sa "$GCS_MARKET_KEY" --build_object_type "$2" --cloud_machine "$CLOUD_CHOSEN_MACHINE_ID" --cloud_servers_path "$CLOUD_SERVERS_PATH" --cloud_servers_api_keys "cloud_api_keys.json" --marketplace_name "$MARKETPLACE_NAME" --artifacts_folder "$ARTIFACTS_FOLDER" --marketplace_buckets "$GCS_MACHINES_BUCKET"
-exit_code=$?
-
-echo "Finished configure_and_test_integration_instances.sh script with exit code ${exit_code}"
-exit ${exit_code}
+if [ -n "${CLOUD_CHOSEN_MACHINE_IDS}" ]; then
+  IFS=', ' read -r -a CLOUD_CHOSEN_MACHINE_ID_ARRAY <<< "${CLOUD_CHOSEN_MACHINE_IDS}"
+  exit_code=0
+  for CLOUD_CHOSEN_MACHINE_ID in "${CLOUD_CHOSEN_MACHINE_ID_ARRAY[@]}"; do
+    python3 ./Tests/configure_and_test_integration_instances.py -u "$USERNAME" -p "$PASSWORD" -c "$CONF_PATH" -s "$SECRET_CONF_PATH" --tests_to_run "$ARTIFACTS_FOLDER/filter_file.txt"  --pack_ids_to_install "$ARTIFACTS_FOLDER/content_packs_to_install.txt" -g "$GIT_SHA1" --ami_env "$1" -n $IS_NIGHTLY --branch "$CI_COMMIT_BRANCH" --build-number "$CI_PIPELINE_ID" -sa "$GCS_MARKET_KEY" --build_object_type "$2" --cloud_machine "${CLOUD_CHOSEN_MACHINE_ID}" --cloud_servers_path "$CLOUD_SERVERS_PATH" --cloud_servers_api_keys "cloud_api_keys.json" --marketplace_name "$MARKETPLACE_NAME" --artifacts_folder "$ARTIFACTS_FOLDER" --marketplace_buckets "$GCS_MACHINES_BUCKET"
+    if [ $? -ne 0 ]; then
+      exit_code=1
+        echo "Failed to configure_and_test_integration_instances.sh script on ${CLOUD_CHOSEN_MACHINE_ID}"
+    fi
+  done
+  echo "Finished configure_and_test_integration_instances.sh script with exit code ${exit_code}"
+  exit ${exit_code}
+else
+  echo "No machines were chosen"
+fi
