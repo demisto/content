9c637bc6af0da9d6337b4e3096e1c6113527cb6d
rshunim <102469772+rshunim@users.noreply.github.com>
2023-09-20 12:37:24 +0300

    Update Packs/GenericSQL/Integrations/GenericSQL/GenericSQL.py
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>

--- Packs/GenericSQL/Integrations/GenericSQL/GenericSQL.py
+++ Packs/GenericSQL/Integrations/GenericSQL/GenericSQL.py
@@ -215,10 +215,9 @@ def generate_variable_names_and_mapping(bind_variables_values_list: list, query:
         raise DemistoException(
             "Placeholder was not found for the given dialect"
         ) from e
-    count = len(re.findall(char_to_count, query))
 
     bind_variables_names_list = []
-    for i in range(count):
+    for i in range(len(re.findall(char_to_count, query))):
         query = query.replace(char_to_replace, f":bind_variable_{i+1}", 1)
         bind_variables_names_list.append(f"bind_variable_{i+1}")
     return dict(zip(bind_variables_names_list, bind_variables_values_list)), query
