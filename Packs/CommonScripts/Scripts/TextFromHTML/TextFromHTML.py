9f2f26f015c9a20e11b0a736d28716de75a68f42
rundssoar <rundssoar@gmail.com>
2023-09-22 16:33:24 +0200

    Allow optional fallback for body extraction.
    If the input html has no body tag, but represent the body
    (e.g. the content of an email), this commit add the option,
    to use the full html as a fallback if no body tag was found.
    
    The added argument is optional and defaults to 'false' to
    ensure backwards compability.
    
    The fallback only applies, if html_tag is 'body'

--- Packs/CommonScripts/Scripts/TextFromHTML/TextFromHTML.py
+++ Packs/CommonScripts/Scripts/TextFromHTML/TextFromHTML.py
@@ -5,10 +5,10 @@ from CommonServerPython import *  # noqa: F401
 import re
 
 
-def get_plain_text(html_regex):
+def get_plain_text(html):
     data = ''
-    if html_regex and html_regex.group(0):
-        data = re.sub(r'<.*?>', '', html_regex.group(0), flags=re.M + re.S + re.I + re.U)
+    if html:
+        data = re.sub(r'<.*?>', '', html, flags=re.M + re.S + re.I + re.U)
         entities = {'quot': '"', 'amp': '&', 'apos': "'", 'lt': '<', 'gt': '>', 'nbsp': ' ',
                     'copy': '(C)', 'reg': '(R)', 'tilde': '~', 'ldquo': '"', 'rdquo': '"', 'hellip': '...'}
         for e in entities:
@@ -18,12 +18,25 @@ def get_plain_text(html_regex):
     return data
 
 
+def get_body(html, html_tag, allow_fallback=False):
+    if html and html_tag:
+        body = re.search(fr'<{html_tag}.*/{html_tag}>', html, re.M + re.S + re.I + re.U)
+
+        if body and body.group(0):
+            return body.group(0)
+        elif allow_fallback and html_tag.lower() == 'body':
+            return html
+    return ''
+
+
 def text_from_html(args):
     html = args['html']
     html_tag = args.get('html_tag', 'body')
+    allow_fallback = str(args.get('allow_body_fallback', 'false')).lower() == 'true'
 
-    body = re.search(fr'<{html_tag}.*/{html_tag}>', html, re.M + re.S + re.I + re.U)
+    body = get_body(html, html_tag, allow_fallback)
     data = get_plain_text(body)
+
     return data if data != '' else 'Could not extract text'
 
 
