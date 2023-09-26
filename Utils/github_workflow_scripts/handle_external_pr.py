21307066705c0aa738c3695d2b70642718983e8e
edik24 <ekatsenelson@paloaltonetworks.com>
2023-09-26 17:12:53 +0300

    check contributions

--- Utils/github_workflow_scripts/handle_external_pr.py
+++ Utils/github_workflow_scripts/handle_external_pr.py
@@ -225,9 +225,9 @@ def main():
         print(f'{t.cyan}Added "{label}" label to the PR{t.normal}')
 
     print('contributors section')
-    pack_name = get_pack_name(pr_files[0])
-    print(f'pack name is: {pack_name}')
-    ver = get_pack_metadata(pack_name)
+    #pack_name = get_pack_name(pr_files[0])
+    print(f'pack path: {pr_files[0]}')
+    ver = get_pack_metadata(pr_files[0])
     print(f'version is: {ver}')
     if 'contributors' not in pr_files and XSOAR_SUPPORT_LEVEL_LABEL in labels_to_add and ver != '1.0.0':
         pr.create_issue_comment("EDI TEST")
