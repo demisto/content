1ef2dfe975fec6aa160e043c0586f5dc5e895b38
michal-dagan <mdagan@paloaltonetworks.com>
2023-09-27 18:14:07 +0300

    # disable-secrets-detection

--- Utils/gitlab/stop_running_pipelines.sh
+++ Utils/gitlab/stop_running_pipelines.sh
@@ -4,9 +4,9 @@
 set -e
 
 # set local vars
-CI_SERVER_URL=${CI_SERVER_URL:-https://code.pan.run}
+CI_SERVER_URL=${CI_SERVER_URL:-https://code.pan.run} # disable-secrets-detection
 CI_PROJECT_ID=${CI_PROJECT_ID:-2596}
-CONTENT_PIPELINES_API_URL=${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines # disable-secrets-detection
+CONTENT_PIPELINES_API_URL=${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/pipelines
 
 if [ -z "$1" ]; then
   echo "No commit branch. Aborting."
--- Utils/gitlab_triggers/trigger_build_url.sh
+++ Utils/gitlab_triggers/trigger_build_url.sh
@@ -1,3 +1,3 @@
-CI_SERVER_URL=${CI_SERVER_URL:-https://code.pan.run}
+CI_SERVER_URL=${CI_SERVER_URL:-https://code.pan.run} # disable-secrets-detection
 CI_PROJECT_ID=${CI_PROJECT_ID:-2596}
-export BUILD_TRIGGER_URL="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/trigger/pipeline"  # disable-secrets-detection
+export BUILD_TRIGGER_URL="${CI_SERVER_URL}/api/v4/projects/${CI_PROJECT_ID}/trigger/pipeline"
