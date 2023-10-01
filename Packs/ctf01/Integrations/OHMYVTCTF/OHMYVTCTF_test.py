14d62cfb4ef6ad4647ba248a4ae9205a96a458da
ssokolovich <ssokolovich@paloaltonetworks.com>
2023-10-01 14:56:04 +0300

    Format / Validations

---
+++ Packs/ctf01/Integrations/OHMYVTCTF/OHMYVTCTF_test.py
@@ -0,0 +1,10 @@
+import pytest
+from CommonServerPython import DemistoException
+import demistomock as demisto
+
+
+@pytest.mark.parametrize("question_id, secret, expected", [
+])
+def test_main(mocker, question_id, secret, expected):
+    assert "a"=="a"
+
