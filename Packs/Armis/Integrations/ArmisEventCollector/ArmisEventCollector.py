3f06470daed08a90be5683ba3cb978b05a3d25b0
samuelFain <65926551+samuelFain@users.noreply.github.com>
2023-10-04 15:52:26 +0300

    [Armis Event Collector] Update initial fetch handling (#30030)
    * Update initial fetch handling
    
    * Update docstring
    
    * Update Release Notes
    
    * Update fetch logic
    
    * Add test_set_last_run_with_current_time_initial UT
    
    * Add debug log for initial fetch
    
    * Add case to test_fetch_flow_cases UT
    
    * Update UT
    
    * Update UT

--- Packs/Armis/Integrations/ArmisEventCollector/ArmisEventCollector.py
+++ Packs/Armis/Integrations/ArmisEventCollector/ArmisEventCollector.py
@@ -65,7 +65,7 @@ class Client(BaseClient):
             list[dict]: List of events objects represented as dictionaries.
         """
         params: dict[str, Any] = {'aql': aql_query, 'includeTotal': 'true', 'length': max_fetch, 'orderBy': 'time'}
-        if not after:  # if this is the first fetch run of the instance, use current datetime as starting point
+        if not after:  # this should only happen when get-events command is used without from_date argument
             after = datetime.now()
         params['aql'] += f' after:{after.strftime(DATE_FORMAT)}'  # add 'after' date filter to AQL query in the desired format
         raw_response = self._http_request(url_suffix='/search/', method='GET', params=params, headers=self._headers)
@@ -380,9 +380,10 @@ def handle_fetched_events(events: list[dict[str, Any]], next_run: dict[str, str 
         )
         demisto.setLastRun(next_run)
         demisto.debug(f'debug-log: {len(events)} events were sent to XSIAM.')
-        demisto.debug(f'debug-log: {next_run=}')
     else:
-        demisto.debug('debug-log: No new events fetched, Last run was not updated.')
+        demisto.debug('debug-log: No new events fetched.')
+
+    demisto.debug(f'debug-log: {next_run=}')
 
 
 def events_to_command_results(events: list[dict[str, Any]]) -> CommandResults:
@@ -399,6 +400,21 @@ def events_to_command_results(events: list[dict[str, Any]]) -> CommandResults:
         readable_output=tableToMarkdown(name=f'{VENDOR} {PRODUCT} events',
                                         t=events,
                                         removeNull=True))
+
+
+def set_last_run_with_current_time(last_run: dict, event_types_to_fetch) -> None:
+    """ Set last fetch time values for all selected event types to current time.
+        This will set a fetch starting time until events are fetched for each event type.
+
+    Args:
+        last_run (dict): Last run dictionary.
+        event_types_to_fetch (list): List of event types to fetch.
+    """
+    now: datetime = datetime.now()
+    now_str: str = now.strftime(DATE_FORMAT)
+    for event_type in event_types_to_fetch:
+        last_fetch_time = f'{EVENT_TYPES[event_type].type}_last_fetch_time'
+        last_run[last_fetch_time] = now_str
 
 
 ''' MAIN FUNCTION '''
@@ -436,27 +452,33 @@ def main():  # pragma: no cover
         elif command in ('fetch-events', 'armis-get-events'):
             should_return_results = False
 
-            if command == 'armis-get-events':
-                last_run = {}
-                should_return_results = True
+            if not last_run:  # initial fetch - update last fetch time values to current time
+                set_last_run_with_current_time(last_run, event_types_to_fetch)
+                demisto.setLastRun(last_run)
+                demisto.debug('debug-log: Initial fetch - updating last fetch time value to current time for each event type.')
 
-            should_push_events = (command == 'fetch-events' or should_push_events)
+            else:
+                if command == 'armis-get-events':
+                    last_run = {}
+                    should_return_results = True
 
-            events, next_run = fetch_events(
-                client=client,
-                max_fetch=max_fetch,
-                last_run=last_run,
-                fetch_start_time=fetch_start_time,
-                event_types_to_fetch=event_types_to_fetch,
-            )
+                should_push_events = (command == 'fetch-events' or should_push_events)
 
-            demisto.debug(f'debug-log: {len(events)} events fetched from armis api')
+                events, next_run = fetch_events(
+                    client=client,
+                    max_fetch=max_fetch,
+                    last_run=last_run,
+                    fetch_start_time=fetch_start_time,
+                    event_types_to_fetch=event_types_to_fetch,
+                )
 
-            if should_push_events:
-                handle_fetched_events(events, next_run)
+                demisto.debug(f'debug-log: {len(events)} events fetched from armis api')
 
-            if should_return_results:
-                return_results(events_to_command_results(events))
+                if should_push_events:
+                    handle_fetched_events(events, next_run)
+
+                if should_return_results:
+                    return_results(events_to_command_results(events))
 
         else:
             raise NotImplementedError(f'Command {command} is not implemented')
