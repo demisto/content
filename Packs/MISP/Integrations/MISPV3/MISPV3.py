db599c4530d716271d8e16fbc827e79fc0041a6c
Martin Ohl <Martin.Ohl@ohl-net.eu>
2023-10-12 17:55:15 +0200

    Update Packs/MISP/Integrations/MISPV3/MISPV3.py
    Co-authored-by: Moshe Eichler <78307768+MosheEichler@users.noreply.github.com>

--- Packs/MISP/Integrations/MISPV3/MISPV3.py
+++ Packs/MISP/Integrations/MISPV3/MISPV3.py
@@ -1036,8 +1036,7 @@ def search_attributes(demisto_args: dict) -> CommandResults:
             if isinstance(response, list):
                 for attachment in response:
                     for objects in attachment.get('Event', {}).get('Object', []):
-                                if 'Attribute' in objects:
-                                    for object in objects.get('Attribute'):
+                                for object in objects.get('Attribute', []):
                                         if 'value' in object:
                                             if args.get('value') == object.get('value'):
                                                 for object in objects.get('Attribute'):
