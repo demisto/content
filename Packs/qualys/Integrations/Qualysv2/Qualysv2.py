595540441c7b8a946c0409ac58ba8ba6d19359f0
franciscojose.abellan.contractor <franciscojose.abellan.contractor@bbva.com>
2023-10-17 09:40:48 +0200

    fix added warning

--- Packs/qualys/Integrations/Qualysv2/Qualysv2.py
+++ Packs/qualys/Integrations/Qualysv2/Qualysv2.py
@@ -1488,7 +1488,10 @@ class Client(BaseClient):
 
     @staticmethod
     def error_handler(res):
-        err_msg = f"Error in API call [{res.status_code}] - {res.reason}"
+        err_msg = ""
+        if res.status_code == 414 or res.status_code == 520:
+            err_msg += "If this error was produced by a schedule-scan-create, please execute it again with IP list of less than 5000 characters\n\n"
+        err_msg += f"Error in API call [{res.status_code}] - {res.reason}"
         try:
             simple_response = get_simple_response_from_raw(parse_raw_response(res.text))
             err_msg = f'{err_msg}\nError Code: {simple_response.get("CODE")}\nError Message: {simple_response.get("TEXT")}'
