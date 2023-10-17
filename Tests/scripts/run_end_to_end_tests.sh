2ec578aade049d5c00b69b909720dc628848a5ea
GuyAfik <guyafik11@gmail.com>
2023-10-17 12:54:39 +0300

    add xsoar_ng_end_to_end_job

--- Tests/scripts/run_end_to_end_tests.sh
+++ Tests/scripts/run_end_to_end_tests.sh
@@ -3,8 +3,7 @@
 CLOUD_SERVERS_PATH=$(cat $CLOUD_SERVERS_FILE)
 echo ${CLOUD_API_KEYS} > "cloud_api_keys.json"
 
-if [[ "${INSTANCE_ROLE}" == "XSIAM" ]]; then
-
+if [[ "${INSTANCE_ROLE}" == "XSIAM" ]] || [[ "${SERVER_TYPE}" == "XSOAR_NG" ]]; then
   if [[ -z "${CLOUD_CHOSEN_MACHINE_IDS}" ]]; then
     echo "CLOUD_CHOSEN_MACHINE_IDS is not defined, exiting..."
     exit 1
@@ -13,7 +12,11 @@ if [[ "${INSTANCE_ROLE}" == "XSIAM" ]]; then
     IFS=', ' read -r -a CLOUD_CHOSEN_MACHINE_ID_ARRAY <<< "${CLOUD_CHOSEN_MACHINE_IDS}"
     for CLOUD_CHOSEN_MACHINE_ID in "${CLOUD_CHOSEN_MACHINE_ID_ARRAY[@]}"; do
       echo "Running end-to-end tests on ${CLOUD_CHOSEN_MACHINE_ID}"
-      python3 -m pytest ./Tests/tests_end_to_end_xsiam -v --cloud_machine "${CLOUD_CHOSEN_MACHINE_ID}" --cloud_servers_path "${CLOUD_SERVERS_PATH}" --cloud_servers_api_keys "cloud_api_keys.json" --disable-warnings
+      if [[ "${INSTANCE_ROLE}" == "XSIAM" ]]; then
+        python3 -m pytest ./Tests/tests_end_to_end_xsiam -v --cloud_machine "${CLOUD_CHOSEN_MACHINE_ID}" --cloud_servers_path "${CLOUD_SERVERS_PATH}" --cloud_servers_api_keys "cloud_api_keys.json" --disable-warnings
+      else
+        python3 -m pytest ./Tests/tests_end_to_end_xsoar_ng -v --cloud_machine "${CLOUD_CHOSEN_MACHINE_ID}" --cloud_servers_path "${CLOUD_SERVERS_PATH}" --cloud_servers_api_keys "cloud_api_keys.json" --disable-warnings
+      fi
       if [[ $? -ne 0 ]]; then
         exit_code=1
       fi
