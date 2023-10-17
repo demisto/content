ce9474e150c3ec986b1957dd6fafc224b65fd736
MosheEichler <meichler@paloaltonetworks.com>
2023-10-17 17:40:46 +0300

    pre-commit fixes

--- Packs/MISP/Integrations/MISPV3/MISPV3.py
+++ Packs/MISP/Integrations/MISPV3/MISPV3.py
@@ -1040,19 +1040,18 @@ def search_attributes(demisto_args: dict) -> CommandResults:
         response_for_context_list = []
         response_for_context_dict = {}
 
-        if args.get('with_attachments', 0) == 1:
-            if isinstance(response, list):
-                for attachment in response:
-                    for objects in attachment.get('Event', {}).get('Object', []):
-                        for object in objects.get('Attribute', []):
-                            if args.get('value') == object.get('value'):
-                                for object in objects.get('Attribute'):
-                                    if data := object.get('data'):
-                                        res = fileResult('{}.zip'.format(args.get('value')), b64decode(data))
-                                        demisto.results(res)
+        if args.get('with_attachments', 0) == 1 and isinstance(response, list):
+            for attachment in response:
+                for objects in attachment.get('Event', {}).get('Object', []):
+                    for object in objects.get('Attribute', []):
+                        if args.get('value') == object.get('value'):
+                            for object in objects.get('Attribute'):
+                                if data := object.get('data'):
+                                    res = fileResult('{}.zip'.format(args.get('value')), b64decode(data))
+                                    demisto.results(res)
 
-                demisto.args()['with_attachments'] = 'false'
-                return search_attributes(demisto_args)
+            demisto.args()['with_attachments'] = 'false'
+            return search_attributes(demisto_args)
 
         if outputs_should_include_only_values:
             response_for_context_list = build_attributes_search_response_return_only_values(response)
