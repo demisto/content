71ab3fcf6e0ae6a88af37b7303f163d273363794
Yehuda <yrosenberg@paloaltonetworks.com>
2023-10-17 21:30:34 +0300

    write to bucket

---
+++ .gitlab/ci/WriteToBucket.py
@@ -0,0 +1,18 @@
+from google.cloud import storage
+import threading
+
+mutex = threading.Lock()
+
+
+def write_to_gcs_with_mutex(data, bucket_name, file_name):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    with mutex:
+        with blob.open("w") as f:
+            f.write(data)
+
+
+if __name__ == "__main__":
+    write_to_gcs_with_mutex("Hello World!", "https://console.cloud.google.com/storage/browser/", "file.txt")
