e3010d7345d883da4df7b65ddfe4170c1db47419
msever <mislav.sever@reversinglabs.com>
2023-10-25 13:20:01 +0200

    Add ip urls

--- Packs/ReversingLabs_Titanium_Cloud/Integrations/ReversingLabsTitaniumCloudv2/ReversingLabsTitaniumCloudv2.py
+++ Packs/ReversingLabs_Titanium_Cloud/Integrations/ReversingLabsTitaniumCloudv2/ReversingLabsTitaniumCloudv2.py
@@ -1960,7 +1960,54 @@ def ip_downloaded_files_output(response, ip):
 
 
 def ip_urls_command():
-    pass
+    ip_ti = create_ip_ti_object()
+
+    ip = demisto.getArg("ip")
+    limit = int(demisto.getArg("result_limit"))
+    per_page = int(demisto.getArg("results_per_page"))
+
+    try:
+        response = ip_ti.urls_from_ip_aggregated(
+            ip_address=ip,
+            results_per_page=per_page,
+            max_results=limit
+        )
+    except Exception as e:
+        return_error(str(e))
+
+    results = ip_urls_output(response=response, ip=ip)
+    return_results(results)
+
+
+def ip_urls_output(response, ip):
+    urls_table = tableToMarkdown(
+        name="URL list",
+        t=response
+    )
+
+    markdown = f"## ReversingLabs URL-s associated with IP address {ip}\n {urls_table}"
+
+    dbot_score = Common.DBotScore(
+        indicator=ip,
+        indicator_type=DBotScoreType.IP,
+        integration_name="ReversingLabs TitaniumCloud v2",
+        score=0,
+        reliability=RELIABILITY
+    )
+
+    indicator = Common.IP(
+        ip=ip,
+        dbot_score=dbot_score
+    )
+
+    results = CommandResults(
+        outputs_prefix="ReversingLabs",
+        outputs={"ip_urls": response},
+        readable_output=markdown,
+        indicator=indicator
+    )
+
+    return results
 
 
 def ip_to_domain_command():
@@ -2080,6 +2127,9 @@ def main():
 
     elif command == "reversinglabs-titaniumcloud-ip-downloaded-files":
         ip_downloaded_files_command()
+
+    elif command == "reversinglabs-titaniumcloud-ip-urls":
+        ip_urls_command()
 
     else:
         return_error(f"Command {command} does not exist")
