d8e589d920f935dc3d0b53c358ae1bf828be5e08
israelpolishook <ipolishuk@paloaltonetworks.com>
2023-10-26 14:57:26 +0300

    commit

--- Packs/CommonScripts/Scripts/GetDataCollectionLink/GetDataCollectionLink.py
+++ Packs/CommonScripts/Scripts/GetDataCollectionLink/GetDataCollectionLink.py
@@ -37,15 +37,15 @@ def generate_url(server_url: str, encoded_task: str, encoded_user: str) -> str:
 
 
 def encode_string(value: str) -> str:
-    b64 = b64encode(value.encode("ascii"))
+    b64 = b64encode(value.encode('ascii'))
     return b64.hex()
 
 
 def get_data_collection_url(task_id: str, users: List[str]) -> List[Dict[str, str]]:
     demisto_urls = demisto.demistoUrls()
-    server_url = demisto_urls.get("server", "")
-    incident_id = demisto.incident().get("id")
-    task = f"{incident_id}@{task_id}"
+    server_url = demisto_urls.get('server', '')
+    incident_id = demisto.incident().get('id')
+    task = f'{incident_id}@{task_id}'
     encoded_task = encode_string(task)
     urls = []
 
@@ -61,24 +61,22 @@ def get_data_collection_url(task_id: str, users: List[str]) -> List[Dict[str, st
     return urls
 
 
-def get_data_collection_url_command(
-    args: Dict[str, Any]
-) -> CommandResults:  # pragma: no cover
-    task_id = args.get("task_id", None)
+def get_data_collection_url_command(args: Dict[str, Any]) -> CommandResults:  # pragma: no cover
+    task_id = args.get('task_id', None)
     if not task_id:
-        raise ValueError("task_id not specified")
+        raise ValueError('task_id not specified')
 
-    users = argToList(args.get("users", []))
+    users = argToList(args.get('users', []))
     if not users:
-        raise ValueError("users not specified")
+        raise ValueError('users not specified')
 
     result = get_data_collection_url(task_id, users)
 
     return CommandResults(
-        outputs_prefix="DataCollectionURL",
-        outputs_key_field="user",
+        outputs_prefix='DataCollectionURL',
+        outputs_key_field='user',
         outputs=result,
-        ignore_auto_extract=True,
+        ignore_auto_extract=True
     )
 
 
@@ -86,8 +84,8 @@ def main():  # pragma: no cover
     try:
         return_results(get_data_collection_url_command(demisto.args()))
     except Exception as ex:
-        return_error(f"Failed to execute GetDataCollectionLink. Error: {str(ex)}")
+        return_error(f'Failed to execute GetDataCollectionLink. Error: {str(ex)}')
 
 
-if __name__ in ("__main__", "__builtin__", "builtins"):
+if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
