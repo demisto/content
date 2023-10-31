56f933c48f8ac6f1b3f711bac76c4b0009d292ef
Nicole Casartelli <npcasartelli@gmail.com>
2023-10-31 11:40:35 -0700

    Changed username to identifier

--- Packs/RecordedFutureASI/Integrations/RecordedFutureASI/RecordedFutureASI.py
+++ Packs/RecordedFutureASI/Integrations/RecordedFutureASI/RecordedFutureASI.py
@@ -531,7 +531,7 @@ def fetch_incidents(client: Client, last_run: Dict[str, int], is_by_host: bool,
 
 def main() -> None:
     params = demisto.params()
-    project_id = params.get("credentials", {}).get("username") or params.get("project_id")
+    project_id = params.get("credentials", {}).get("identifier") or params.get("project_id")
     if not project_id:
         return_error('Please provide a valid Project ID')
     api_key = params.get("credentials", {}).get("password") or params.get("apikey")
