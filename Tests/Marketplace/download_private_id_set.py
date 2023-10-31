fd2665f1a772f831971e387d5aa4eaf56d83d25d
kobymeir <ymeir@paloaltonetworks.com>
2023-10-31 23:02:05 +0200

    Splitting prepare-testing-bucket output to be per server type - take 7

--- Tests/Marketplace/download_private_id_set.py
+++ Tests/Marketplace/download_private_id_set.py
@@ -1,6 +1,8 @@
 import os
 import json
 import argparse
+from pathlib import Path
+
 from Tests.Marketplace.marketplace_services import init_storage_client
 
 
@@ -45,17 +47,21 @@ def download_private_id_set_from_gcp(public_storage_bucket, storage_base_path):
         'content/private_id_set.json'
     private_artifacts_server_type_path = '/home/runner/work/content-private/content-private/content/artifacts/server_type_XSOAR'
     private_id_set_path = f'{private_artifacts_server_type_path}/private_id_set.json'
-
-    if not os.path.exists(private_artifacts_server_type_path):
-        os.mkdir(private_artifacts_server_type_path)
+    if os.path.exists(private_artifacts_server_type_path):
+        print(f'Directory: {private_artifacts_server_type_path} already exists.')  # noqa: T201
+    else:
+        print(f'Creating directory: {private_artifacts_server_type_path}')  # noqa: T201
+        Path(private_artifacts_server_type_path).mkdir(parents=True, exist_ok=True)
 
     is_private_id_set_file_exist = id_set_file_exists_in_bucket(public_storage_bucket, storage_id_set_path)
 
     if is_private_id_set_file_exist:
+        print(f'Downloading private_id_set from:{storage_id_set_path} to: {private_id_set_path}')  # noqa: T201
         index_blob = public_storage_bucket.blob(storage_id_set_path)
         index_blob.download_to_filename(private_id_set_path)
 
     else:
+        print(f'Private id set file does not exist in bucket: {public_storage_bucket.name}.')  # noqa: T201
         create_empty_id_set_in_artifacts(private_id_set_path)
 
     return private_id_set_path if os.path.exists(private_id_set_path) else ''
