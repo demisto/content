9a5f8b89881dcdbbcbf6879f6c9999dcaec36abd
adi88d <adaud@paloaltonetworks.com>
2023-10-31 15:56:30 +0200

    added test_get_request_command_valid_raw_response

--- Packs/OpsGenie/Integrations/OpsGenieV3/OpsGenieV3_test.py
+++ Packs/OpsGenie/Integrations/OpsGenieV3/OpsGenieV3_test.py
@@ -978,7 +978,7 @@ def test_responders_to_json_empty_value():
 def test_get_request_command(requests_mock, mocker):
     """
     Given:
-        - A call to get-retquest
+        - A call to get-request
     When:
         - response is successful
     Then:
@@ -995,7 +995,7 @@ def test_get_request_command(requests_mock, mocker):
 def test_get_request_command_404(requests_mock, mocker):
     """
       Given:
-          - A call to get-retquest
+          - A call to get-request
       When:
           - response is 404
       Then:
@@ -1008,6 +1008,22 @@ def test_get_request_command_404(requests_mock, mocker):
     assert response.scheduled_command._args == {**args, 'polled_once': True}
 
 
+def test_get_request_command_valid_raw_response(requests_mock, mocker):
+    """
+      Given:
+          - A call to get-request
+      When:
+          - raw response is valid json
+      Then:
+          - Scheduledcommand is returned
+      """
+    mocker.patch('CommonServerPython.ScheduledCommand.raise_error_if_not_supported')
+    requests_mock.get(url='http://example.com/v2/alert/requests/1', json={'data': {}}, status_code=404)
+    args = {'request_id': 1, 'request_type': 'alert'}
+    response = OpsGenieV3.get_request_command(OpsGenieV3.Client(base_url="http://example.com"), args)
+    assert response.raw_response == {'data': {}}
+    
+    
 def test_invite_user(mocker):
     """
     Given:
