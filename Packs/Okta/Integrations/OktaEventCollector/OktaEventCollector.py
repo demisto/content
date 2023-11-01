2660f4e7905f6b50a60ec55bb9b9248f887cf370
Danny_Fried <dfried@paloaltonetworks.com>
2023-11-01 23:21:06 +0200

    refacror

--- Packs/Okta/Integrations/OktaEventCollector/OktaEventCollector.py
+++ Packs/Okta/Integrations/OktaEventCollector/OktaEventCollector.py
@@ -1,25 +1,15 @@
-from enum import Enum
-from pydantic import BaseModel, AnyUrl, Json  # pylint: disable=no-name-in-module
 from CommonServerPython import *
-
+from http import HTTPStatus
 VENDOR = "okta"
 PRODUCT = "okta"
-
-
-# class Method(str, Enum):
-#     """
-#     A list that represent the types of http request available
-#     """
-#     GET = 'GET'
-#     POST = 'POST'
-#     PUT = 'PUT'
-#     HEAD = 'HEAD'
-#     PATCH = 'PATCH'
-    # DELETE = 'DELETE'
+FETCH_LIMIT = 1000
 
 
 class Client(BaseClient):
     next_run_filter: str
+    params = {
+        "sortOrder": "ASCENDING",
+    }
 
     def __init__(self, base_url, api_key, verify=True, proxy=False):
         headers = {"Accept": "application/json",
@@ -28,61 +18,42 @@ class Client(BaseClient):
                    }
         super().__init__(base_url=base_url, headers=headers, verify=verify, proxy=proxy)
 
-    def make_api_call(self, limit: int, since: int):
-        response = self.call(limit=limit, since=since)
-        events: list = response.json()
-        self.client.request.params.limit = str(limit_tmp - len(events))  # type: ignore
-        return events
-
-    def call(self, limit, since) -> requests.Response:  # pragma: no cover
-        params = {
-            "since": since,
-            "sortOrder": "ASCENDING",
-            "limit": limit
-        }
-        try:
-            response = self._http_request(url_suffix='/api/v1/logs', method='GET', params=params, headers=self._headers)
-            return response
-        except Exception as exc:
-            msg = f'something went wrong with the http call {exc}'
-            LOG(msg)
-            raise DemistoException(msg) from exc
+    def get_events(self, since: int, limit: int = FETCH_LIMIT):
+        self.params['since'] = since  # type: ignore
+        self.params['limit'] = limit  # type: ignore
+        return self._http_request(url_suffix='/api/v1/logs', method='GET', headers=self._headers, params=self.params)  # type: ignore
 
     def set_next_run_filter(self, after: str):
-        next_run_filter = after  # type: ignore
+        self.next_run_filter = after  # type: ignore
 
 
-
-def iter_events(self, client, limit, since, last_object_ids: list) -> None:  # type: ignore  # pragma: no cover
-        """
-        Function that responsible for the iteration over the events returned from the Okta api
-        """
-
-        events: list = client.make_api_call(limit, since)  # type: ignore
-        if last_object_ids:
-            events = remove_duplicates(events, last_object_ids)
-        while True:
-            yield events
-            last = events[-1]
-            client.next_run_filter = last['published']
-            events: list = client.make_api_call()  # type: ignore
-            try:
-                assert events
-            except (IndexError, AssertionError):
-                LOG('empty list, breaking')
-                break
-
-
-def aggregated_results(client, limit, since, last_object_ids: List[str] = None) -> List[dict]:  # pragma: no cover
+def get_events_command(client: Client, total_events_to_fetch, since, last_object_ids: List[str] = None) -> (List[dict], int):  # pragma: no cover
     """
     Function to group the events returned from the api
     """
-    stored_events = []
-    for events in iter_events(client, limit, since, last_object_ids):  # type: ignore
-        stored_events.extend(events)
-        if int(limit) == 0 or len(events) == 0:  # type: ignore
-            break
-    return stored_events
+    stored_events: list = []
+    while total_events_to_fetch > 0:
+        num_of_events_to_fetch = 1000 if total_events_to_fetch > 1000 else total_events_to_fetch
+        try:
+            events = client.get_events(since, num_of_events_to_fetch)  # type: ignore
+            if events:
+                total_events_to_fetch -= len(events)
+                if last_object_ids:
+                    events = remove_duplicates(events, last_object_ids)  # type: ignore
+                last = events[-1]
+                client.next_run_filter = last['published']
+                stored_events.extend(events)
+            else:
+                break
+        except Exception as exc:
+            msg = f'something went wrong with the http call {exc}'
+            demisto.debug(msg)
+            status_code: HTTPStatus = exc.res.status_code
+            if status_code == HTTPStatus.TOO_MANY_REQUESTS:
+                return stored_events, int(exc.res.headers['x-rate-limit-reset'])  # type: ignore
+            else:
+                return stored_events, 0
+    return stored_events, 0
 
 
 def remove_duplicates(events: list, ids: list) -> list:
@@ -90,123 +61,6 @@ def remove_duplicates(events: list, ids: list) -> list:
     Remove object duplicates by the uuid of the object
     """
     return [event for event in events if event['uuid'] not in ids]
-
-
-# class ReqParams(BaseModel):  # pragma: no cover
-#     """
-#     A class that stores the request query params
-#     """
-#     since: str
-#     sortOrder: Optional[str] = 'ASCENDING'
-#     limit: str = '1000'
-
-#     def set_since_value(self, since: str) -> None:  # pragma: no cover
-#         self.since = since
-
-
-# class Request(BaseModel):  # pragma: no cover
-#     """
-#     A class that stores a request configuration
-#     """
-#     method: Method = Method.GET
-#     url: AnyUrl
-#     headers: Optional[Union[Json[dict], dict]]
-#     params: Optional[ReqParams]
-#     verify: bool = True
-#     data: Optional[str] = None
-
-
-class deprecated_Client:
-    """
-    A class for the client request handling
-    """
-
-    def __init__(self, request: Request):  # pragma: no cover
-        self.request = request
-
-    def call(self, requests=requests) -> requests.Response:  # pragma: no cover
-        try:
-            response = requests.request(**self.request.dict())
-            response.raise_for_status()
-            return response
-        except Exception as exc:
-            msg = f'something went wrong with the http call {exc}'
-            LOG(msg)
-            raise DemistoException(msg) from exc
-
-    def set_next_run_filter(self, after: str):
-        self.request.params.set_since_value(after)  # type: ignore
-
-
-# class GetEvents:
-    # """
-    # A class to handle the flow of the integration
-    # """
-
-    # def __init__(self, client: Client) -> None:
-    #     self.client = client
-
-    # def make_api_call(self):
-    #     limit_tmp = int(self.client.request.params.limit)  # type: ignore
-    #     if limit_tmp > 1000:
-    #         self.client.request.params.limit = '1000'  # type: ignore
-    #     response = self.client.call()
-    #     events: list = response.json()
-    #     self.client.request.params.limit = str(limit_tmp - len(events))  # type: ignore
-    #     return events
-
-    # def _iter_events(self, last_object_ids: list) -> None:  # type: ignore  # pragma: no cover
-    #     """
-    #     Function that responsible for the iteration over the events returned from the Okta api
-    #     """
-
-    #     events: list = self.make_api_call()  # type: ignore
-    #     if last_object_ids:
-    #         events = GetEvents.remove_duplicates(events, last_object_ids)
-    #     while True:
-    #         yield events
-    #         last = events[-1]
-    #         self.client.set_next_run_filter(last['published'])
-    #         events: list = self.make_api_call()  # type: ignore
-    #         try:
-    #             assert events
-    #         except (IndexError, AssertionError):
-    #             LOG('empty list, breaking')
-    #             break
-
-    # def aggregated_results(self, last_object_ids: List[str] = None) -> List[dict]:  # pragma: no cover
-    #     """
-    #     Function to group the events returned from the api
-    #     """
-    #     stored_events = []
-    #     for events in self._iter_events(last_object_ids):  # type: ignore
-    #         stored_events.extend(events)
-    #         if int(self.client.request.params.limit) == 0 or len(events) == 0:  # type: ignore
-    #             break
-    #     return stored_events
-
-    # @staticmethod
-    # def get_last_run(events: List[dict], last_run_after) -> dict:
-    #     """
-    #     Get the info from the last run, it returns the time to query from and a list of ids to prevent duplications
-    #     """
-
-    #     ids = []
-    #     # gets the last event time
-    #     last_time = events[-1].get('published') if events else last_run_after
-    #     for event in reversed(events):
-    #         if event.get('published') != last_time:
-    #             break
-    #         ids.append(event.get('uuid'))
-    #     last_time = datetime.strptime(str(last_time).lower().replace('z', ''), '%Y-%m-%dt%H:%M:%S.%f')
-    #     return {'after': last_time.isoformat(), 'ids': ids}
-
-    # @staticmethod
-    # def remove_duplicates(events: list, ids: list) -> list:
-    #     """
-    #     Remove object duplicates by the uuid of the object
-    #     """
-    #     return [event for event in events if event['uuid'] not in ids]
 
 
 def get_last_run(events: List[dict], last_run_after) -> dict:
@@ -226,14 +80,13 @@ def get_last_run(events: List[dict], last_run_after) -> dict:
 
 def main():  # pragma: no cover
     try:
+        start_time_epoch = int(time.time())
         demisto_params = demisto.params() | demisto.args()
         events_limit = int(demisto_params.get('limit', 2000))
         should_push_events = argToBoolean(demisto_params.get('should_push_events', 'false'))
         after = dateparser.parse(demisto_params['after'].strip())
         api_key = demisto_params['api_key']['password']
         verify_certificate = not demisto_params.get('insecure', True)
-        # demisto_params['headers'] = {"Accept": "application/json", "Content-Type": "application/json",
-                                    #  "Authorization": f"SSWS {api_key}"}
         base_url = demisto_params['url']
         last_run = demisto.getLastRun()
         last_object_ids = last_run.get('ids')
@@ -241,21 +94,14 @@ def main():  # pragma: no cover
             last_run_after = after.isoformat()  # type: ignore
         else:
             last_run_after = last_run['after']
-        # demisto_params['params'] = ReqParams(**demisto_params, since=last_run_after)
-
-        # request = Request(**demisto_params)
-
-        # client = Client(request)
-
-        # get_events = GetEvents(client)
         client = Client(base_url=base_url, api_key=api_key, verify=verify_certificate)
         command = demisto.command()
         if command == 'test-module':
-            aggregated_results(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
+            get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
             demisto.results('ok')
 
         if command == 'okta-get-events':
-            events = aggregated_results(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
+            events, continue_fetch = get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
             command_results = CommandResults(
                 readable_output=tableToMarkdown('Okta Logs', events, headerTransform=pascalToSpace),
                 raw_response=events,
@@ -264,9 +110,12 @@ def main():  # pragma: no cover
 
             if should_push_events:
                 send_events_to_xsiam(events[:events_limit], vendor=VENDOR, product=PRODUCT)
-
         elif command == 'fetch-events':
-            events = aggregated_results(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
+            continue_fetch = True
+            while continue_fetch:
+                events, continue_fetch = get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
+                if continue_fetch:
+                    time.sleep(continue_fetch - start_time_epoch)  # TODO calculate sleep time
             send_events_to_xsiam(events[:events_limit], vendor=VENDOR, product=PRODUCT)
             demisto.setLastRun(get_last_run(events, last_run_after))
 
