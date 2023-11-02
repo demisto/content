df504b1815486807dce00c7766904eacd92c6a79
yucohen <yucohen@paloaltonetworks.com>
2023-11-02 12:28:18 +0200

    fix

--- Tests/test_content.py
+++ Tests/test_content.py
@@ -358,7 +358,7 @@ def extract_server_numeric_version(instances_ami_name, default_version):
 
     if server_numeric_version == 'master':
         logging.info('Server version: Master')
-        return default_version
+        return "6.99.99"
     else:
         server_numeric_version = server_numeric_version.replace('-', '.')
 
