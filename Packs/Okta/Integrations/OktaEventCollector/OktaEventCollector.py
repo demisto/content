95c89cb0d25250d5526d9120d57021f9d3121660
Danny_Fried <dfried@paloaltonetworks.com>
2023-11-02 11:53:24 +0200

    Update version. Update RN. Handle 429

--- Packs/Okta/Integrations/OktaEventCollector/OktaEventCollector.py
+++ Packs/Okta/Integrations/OktaEventCollector/OktaEventCollector.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 VENDOR = "okta"
 PRODUCT = "okta"
 FETCH_LIMIT = 1000
+FETCH_TIME_LIMIT = 60
 
 
 class Client(BaseClient):
@@ -24,6 +25,7 @@ class Client(BaseClient):
         return self._http_request(url_suffix='/api/v1/logs', method='GET', headers=self._headers, params=self.params)  # type: ignore
 
     def set_next_run_filter(self, after: str):
+        demisto.debug(f'setting next run since value to: {after}')
         self.next_run_filter = after  # type: ignore
 
 
@@ -32,27 +34,37 @@ def get_events_command(client: Client, total_events_to_fetch, since, last_object
     Function to group the events returned from the api
     """
     stored_events: list = []
-    while total_events_to_fetch > 0:
-        num_of_events_to_fetch = 1000 if total_events_to_fetch > 1000 else total_events_to_fetch
+    demisto.debug(f'max_events_to_fetch={total_events_to_fetch}')
+    while len(stored_events) < total_events_to_fetch:
+        demisto.debug(f'stored_events={len(stored_events)}')
+        num_of_events_to_fetch = FETCH_LIMIT if total_events_to_fetch > FETCH_LIMIT else total_events_to_fetch
         try:
-            events = client.get_events(since, num_of_events_to_fetch)  # type: ignore
+            events = client.get_events(since, num_of_events_to_fetch)  # type: ignore            
             if events:
-                total_events_to_fetch -= len(events)
+                demisto.debug(f'received {len(events)} number of events.')
                 if last_object_ids:
                     events = remove_duplicates(events, last_object_ids)  # type: ignore
                 last = events[-1]
                 client.next_run_filter = last['published']
                 stored_events.extend(events)
             else:
-                break
+                demisto.error('Didnt receive any events from the api.')
+                raise Exception('Didnt receive any events from the api.')
         except Exception as exc:
             msg = f'something went wrong with the http call {exc}'
             demisto.debug(msg)
-            status_code: HTTPStatus = exc.res.status_code
-            if status_code == HTTPStatus.TOO_MANY_REQUESTS:
-                return stored_events, int(exc.res.headers['x-rate-limit-reset'])  # type: ignore
+            if exc.res is not None:
+                status_code: HTTPStatus = exc.res.status_code
+                if status_code == HTTPStatus.TOO_MANY_REQUESTS:
+                    demisto.debug(f'okta rate limit headers:\n \
+                        x-rate-limit-remaining: {exc.res.headers["x-rate-limit-remaining"]}\n \
+                            x-rate-limit-limit: {exc.res.headers["x-rate-limit-limit"]}\n \
+                                x-rate-limit-reset: {exc.res.headers["x-rate-limit-reset"]}\n')  # type: ignore
+                    return stored_events, int(exc.res.headers['x-rate-limit-reset'])  # type: ignore
+                else:
+                    return stored_events, 0
             else:
-                return stored_events, 0
+                raise Exception(msg)
     return stored_events, 0
 
 
@@ -96,12 +108,13 @@ def main():  # pragma: no cover
             last_run_after = last_run['after']
         client = Client(base_url=base_url, api_key=api_key, verify=verify_certificate)
         command = demisto.command()
+        demisto.debug(f'Command being called is {command}')
         if command == 'test-module':
             get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
             demisto.results('ok')
 
         if command == 'okta-get-events':
-            events, continue_fetch = get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
+            events, epoch_time_to_continue_fetch = get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
             command_results = CommandResults(
                 readable_output=tableToMarkdown('Okta Logs', events, headerTransform=pascalToSpace),
                 raw_response=events,
@@ -111,11 +124,14 @@ def main():  # pragma: no cover
             if should_push_events:
                 send_events_to_xsiam(events[:events_limit], vendor=VENDOR, product=PRODUCT)
         elif command == 'fetch-events':
-            continue_fetch = True
-            while continue_fetch:
-                events, continue_fetch = get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
-                if continue_fetch:
-                    time.sleep(continue_fetch - start_time_epoch)  # TODO calculate sleep time
+            epoch_time_to_continue_fetch = True
+            while epoch_time_to_continue_fetch:
+                events, epoch_time_to_continue_fetch = get_events_command(client, events_limit, since=last_run_after, last_object_ids=last_object_ids)
+                if epoch_time_to_continue_fetch:
+                    if abs(epoch_time_to_continue_fetch - start_time_epoch) < FETCH_TIME_LIMIT:
+                        time.sleep(epoch_time_to_continue_fetch - start_time_epoch)
+                    else:
+                        break
             send_events_to_xsiam(events[:events_limit], vendor=VENDOR, product=PRODUCT)
             demisto.setLastRun(get_last_run(events, last_run_after))
 
