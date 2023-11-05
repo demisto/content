92c417da5d12b329629ec90f4bb52e7077d8741e
darbel <darbel@paloaltonetworks.com>
2023-11-05 17:44:57 +0200

    add polling mechanism to sleep upon crossing polling_threshold

--- Packs/CommonScripts/Scripts/Sleep/Sleep.js
+++ Packs/CommonScripts/Scripts/Sleep/Sleep.js
@@ -1,1 +1,20 @@
+if (args.polling_callback) {
+    // Polling runs once
+        return 'Slept for ' + args.seconds + ' seconds';
+}
+
+if (args.polling_threshold && (parseInt(args.seconds) >= parseInt(args.polling_threshold))) {
+    // Polling implementation
+    args.polling_callback = true;
+    return {
+        Type: entryTypes.note,
+        Contents: 'Sleep will complete in ' + args.seconds + ' seconds',
+        PollingCommand: 'Sleepp',
+        NextRun: args.seconds,
+        PollingArgs: args,
+        Timeout: String(parseInt(args.seconds) + 10)
+    }
+}
+
+// Sleep for the given number of seconds
 wait(parseInt(args.seconds));
