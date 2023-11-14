31b5f0b67833ba7f1e6e0faec88954d7c4b14ff5
Dan Tavori <38749041+dantavori@users.noreply.github.com>
2023-11-14 19:32:19 +0200

    update GetIndicatorDBotScoreFromCache (#30865)

--- Packs/CommonScripts/Scripts/GetIndicatorDBotScoreFromCache/GetIndicatorDBotScoreFromCache.py
+++ Packs/CommonScripts/Scripts/GetIndicatorDBotScoreFromCache/GetIndicatorDBotScoreFromCache.py
@@ -1,45 +1,71 @@
+import contextlib
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 
 def main():
-    value = demisto.args().get("value", None)
+    values: list[str] = argToList(demisto.args().get("value", None))
+    unique_values: set[str] = {v.lower() for v in values}  # search query is case insensitive
 
-    res = demisto.executeCommand("getIndicator", {"value": value})
+    res = demisto.searchIndicators(
+        query=f'value:({" ".join(unique_values)})',
+        populateFields='name,score,aggregatedReliability,type,expirationStatus',
+    )
 
-    if len(res[0]["Contents"]) > 0:
-        data = res[0]["Contents"][0]
-        score = data["score"]
-        vendor = "XSOAR"
-        reliability = data.get("aggregatedReliability")
-        indicatorType = data["indicator_type"]
-        expirationStatus = False if data.get("expirationStatus") == "active" else True
+    return_entries = []
 
-        dbotscore = {
-            "Indicator": value,
-            "Type": indicatorType,
-            "Vendor": vendor,
-            "Score": score,
-            "Reliability": reliability,
-            "Expired": expirationStatus
-        }
+    if 'iocs' in res and len(res['iocs']) > 0:
+        for data in res['iocs']:
+            score = data["score"]
+            vendor = "XSOAR"
+            reliability = data.get("aggregatedReliability")
+            indicatorType = data["indicator_type"]
+            expirationStatus = data.get("expirationStatus") != "active"
+            value: str = data["value"]
 
-        md = tableToMarkdown("Indicator", dbotscore)
+            dbotscore = {
+                "Indicator": value,
+                "Type": indicatorType,
+                "Vendor": vendor,
+                "Score": score,
+                "Reliability": reliability,
+                "Expired": expirationStatus
+            }
+
+            return_entries.append(dbotscore)
+            with contextlib.suppress(KeyError):  # for multiple IOCs with same value but different casing
+                unique_values.remove(value.lower())
+
+    values_not_found = list({v for v in values if v.lower() in unique_values})  # return the values with the original casing
+
+    if len(return_entries) > 0:
+        md = tableToMarkdown("Indicator", return_entries)
 
         entry = {
             "Type": entryTypes["note"],
             "ReadableContentsFormat": formats['markdown'],
             "ContentsFormat": formats["json"],
-            "Contents": dbotscore,
-            "EntryContext": {"DBotScoreCache": dbotscore},
+            "Contents": return_entries,
+            "EntryContext": {"DBotScoreCache": return_entries},
             "HumanReadable": md
         }
 
         return_results(entry)
 
-    else:
-        return_results(f"Could not find {value} in cache")
+    if len(values_not_found) == 1:
+        return_results(f"Could not find {values_not_found[0]} in cache")
+
+    elif len(values_not_found) > 1:
+        md = tableToMarkdown("Could not find in cache", values_not_found, headers=['Values'])
+        not_found_values_entry = {
+            "Type": entryTypes["note"],
+            "ContentsFormat": formats["json"],
+            "ReadableContentsFormat": formats['markdown'],
+            "Contents": md,
+            "HumanReadable": md
+        }
+        return_results(not_found_values_entry)
 
 
-if __name__ == "__builtin__" or __name__ == "builtins":
+if __name__ == "__builtin__" or __name__ == "builtins":  # pragma: no cover
     main()
