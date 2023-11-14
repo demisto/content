17bf503fb9607be46c3289f2bd82768a5e6d5c14
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-11-14 09:14:35 +0200

    slight additions

--- Packs/AWS-EC2/Scripts/AwsEC2GetAccounts/AwsEC2GetAccounts.py
+++ Packs/AWS-EC2/Scripts/AwsEC2GetAccounts/AwsEC2GetAccounts.py
@@ -5,14 +5,26 @@ from CommonServerPython import *
 ACCOUNT_LIST_COMMAND = 'aws-org-account-list'
 
 
-def get_accounts():
-    if ACCOUNT_LIST_COMMAND in demisto.getAllSupportedCommands():  # is dict; check if needed
-        demisto.executeCommand(ACCOUNT_LIST_COMMAND, {})
-    else:
+def get_account_ids() -> list[str]:
+    try:
+        account_list_result = demisto.executeCommand(ACCOUNT_LIST_COMMAND, {})
+        return [
+            account.get('Id', '')
+            for account in dict_safe_get(
+                account_list_result,
+                [0, 'EntryContext', 'AWS.Organizations.Account(val.Id && val.Id == obj.Id)'],
+                default_return_value=[],
+                return_type=list,
+            )
+        ]
+    except ValueError as e:
+        demisto.debug(str(e))
         raise DemistoException(f'The command {ACCOUNT_LIST_COMMAND!r} must be operational to run this script.')
+
 
 def main():
     try:
+        accounts = get_account_ids()
         demisto.internalHttpRequest('PUT', 'uri', {})
     except Exception as e:
         return_error(f'Error in AwsEC2GetAccounts: {e}')
