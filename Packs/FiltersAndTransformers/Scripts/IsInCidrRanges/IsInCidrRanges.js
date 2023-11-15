0b0e3f314b0ab387a099557b025d88fa972e4838
Anar Azadaliyev <aazadaliyev@paloaltonetworks.com>
2023-11-15 18:39:49 +0200

    added test playbook

--- Packs/FiltersAndTransformers/Scripts/IsInCidrRanges/IsInCidrRanges.js
+++ Packs/FiltersAndTransformers/Scripts/IsInCidrRanges/IsInCidrRanges.js
@@ -2,14 +2,38 @@ function isIPv6(ip) {
   return ip.includes(':');
 }
 
+function ipv6ToBinary(ipv6) {
+  // Split the IPv6 address into its components
+  const components = ipv6.split(':');
+
+  // Handle zero compression (::)
+  let zeroCompressionIndex = components.indexOf('');
+  if (zeroCompressionIndex !== -1) {
+      const zeroCount = 8 - components.length + 1; // Calculate the number of missing components
+      components.splice(zeroCompressionIndex, 1, ...Array(zeroCount).fill('0000')); // Replace :: with zero components
+  }
+
+  // Convert each component to binary and pad to 16 bits
+  const binaryComponents = components.map(component => {
+      // Handle the case when component is an empty string
+      if (component === '') {
+          return '0000000000000000'; // 16 zeros for empty component
+      }
+      
+      const binary = parseInt(component, 16).toString(2);
+      return '0'.repeat(16 - binary.length) + binary;
+  });
+
+  // Concatenate the binary components
+  const binaryString = binaryComponents.join('');
+
+  return binaryString;
+}
+
 function ipToBinary(ip) {
   if (isIPv6(ip)) {
     // IPv6
-    const binarySegments = ip.split(':').map(segment => {
-      const hexValue = segment === '' ? 0 : parseInt(segment, 16);
-      return ('0000' + hexValue.toString(2)).slice(-16);
-    });
-    return binarySegments.join('');
+    return ipv6ToBinary(ip);
   } else {
     // IPv4
     return ip.split('.').map(octet => ('00000000' + parseInt(octet, 10).toString(2)).slice(-8)).join('');
@@ -17,48 +41,47 @@ function ipToBinary(ip) {
 }
 
 function validateCIDR(cidrRange) {
-    const cidrRegex = /^([0-9a-f:.]+)\/([0-9]{1,3})$/i; // Regex for IPv4 and IPv6 CIDR notation
+  const cidrRegex = /^([0-9a-f:.]+)\/([0-9]{1,3})$/i; // Regex for IPv4 and IPv6 CIDR notation
 
-    const match = cidrRange.match(cidrRegex);
+  const match = cidrRange.match(cidrRegex);
 
-    if (!match) {
-        return false; // CIDR range is not well-formed
-    }
+  if (!match) {
+      return false; // CIDR range is not well-formed
+  }
 
-    const subnetMask = parseInt(match[2], 10);
+  const subnetMask = parseInt(match[2], 10);
 
-    if (match[1].includes(':')) {
-        // IPv6 CIDR
-        if (subnetMask < 0 || subnetMask > 128) {
-            return false; // Invalid subnet mask for IPv6
-        }
-    } else {
-        // IPv4 CIDR
-        if (subnetMask < 0 || subnetMask > 32) {
-            return false; // Invalid subnet mask for IPv4
-        }
-    }
+  if (match[1].includes(':')) {
+      // IPv6 CIDR
+      if (subnetMask < 0 || subnetMask > 128) {
+          return false; // Invalid subnet mask for IPv6
+      }
+  } else {
+      // IPv4 CIDR
+      if (subnetMask < 0 || subnetMask > 32) {
+          return false; // Invalid subnet mask for IPv4
+      }
+  }
 
-    return true; // CIDR range is well-formed
+  return true; // CIDR range is well-formed
 }
 
 function isIPInCIDR(ipAddress, cidrRange) {
-    if (!validateCIDR(cidrRange)) {
-        logDebug(`Invalid CIDR range: ${cidrRange}`);
-        return false;
-    }
+  if (!validateCIDR(cidrRange)) {
+      return false;
+  }
 
-    const [networkAddress, subnetMask] = cidrRange.split('/');
+  const [networkAddress, subnetMask] = cidrRange.split('/');
 
-    // Convert IP address and network address to binary
-    const ipBinary = ipToBinary(ipAddress);
-    const networkBinary = ipToBinary(networkAddress);
+  // Convert IP address and network address to binary
+  const ipBinary = ipToBinary(ipAddress);
+  const networkBinary = ipToBinary(networkAddress);
 
-    // Get the network part of the IP address based on the subnet mask
-    const networkPart = ipBinary.slice(0, parseInt(subnetMask, 10));
+  // Get the network part of the IP address based on the subnet mask
+  const networkPart = ipBinary.slice(0, parseInt(subnetMask, 10));
 
-    // Check if the network parts match
-    return networkPart === networkBinary.slice(0, parseInt(subnetMask, 10));
+  // Check if the network parts match
+  return networkPart === networkBinary.slice(0, parseInt(subnetMask, 10));
 }
 
 
@@ -70,8 +93,6 @@ function isIPInAnyCIDR(ipAddresses, cidrRanges) {
     
     for (let j = 0; j < cidrRanges.length; j++) {
       if (isIPInCIDR(ipAddresses[i], cidrRanges[j])) {
-        logDebug(`${ipAddresses[i]} is in range ${cidrRanges[j]}`)
-
         isInRange = true;
         results[i] = 'True';
         break;
@@ -79,7 +100,6 @@ function isIPInAnyCIDR(ipAddresses, cidrRanges) {
     }
 
     if (!isInRange) {
-      logDebug(`${ipAddresses[i]} is NOT in range.`)
       results[i] = 'False';
     }
   }
