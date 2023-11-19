1e4283aa02caa89a006d100e30de0505d4b9752f
Jas Beilin <jgranot@paloaltonetworks.com>
2023-11-19 11:58:22 +0200

    changed path for test playbook in xsoar

--- Tests/scripts/test_playbooks_results.sh
+++ Tests/scripts/test_playbooks_results.sh
@@ -9,7 +9,7 @@ function exit_on_error {
 
 echo "Starting the print test playbook summary script - Server type: ${SERVER_TYPE}, Product type: ${PRODUCT_TYPE}"
 
-python3 ./Tests/Marketplace/print_test_playbook_summary.py --artifacts-path "${ARTIFACTS_FOLDER}"
+python3 ./Tests/Marketplace/print_test_playbook_summary.py --artifacts-path "${ARTIFACTS_FOLDER_INSTANCE}"
 summary_exit_code=$?
 
 if [ -n "${NIGHTLY}" ]; then
