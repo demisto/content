70a4133cf40825faa5833551c540d9f897dc7b2f
michal-dagan <mdagan@paloaltonetworks.com>
2023-11-21 13:38:04 +0200

    add echo Common Ancestor

--- Tests/scripts/fetch_through_merge_base.sh
+++ Tests/scripts/fetch_through_merge_base.sh
@@ -7,6 +7,5 @@ BASE_REF=${2:-origin/master}
 while [ -z "$(git merge-base "$BASE_REF" "$HEAD_REF" 2>/dev/null)" ]; do
   echo "Continuing fetch with a depth of $DEEPEN_LENGTH commits..."
   git fetch -q --deepen="$DEEPEN_LENGTH" origin;
-  COMMON_ANCESTOR=$(git merge-base "$BASE_REF" "$HEAD_REF")
-  echo "Common Ancestor: $COMMON_ANCESTOR"
+  echo "git merge-base $BASE_REF $HEAD_REF"
 done
