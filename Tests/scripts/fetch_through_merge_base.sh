d754d6a4224a6dce829a692c17a26472e682077d
michal-dagan <mdagan@paloaltonetworks.com>
2023-11-21 12:10:55 +0200

    testing

--- Tests/scripts/fetch_through_merge_base.sh
+++ Tests/scripts/fetch_through_merge_base.sh
@@ -4,8 +4,8 @@ DEEPEN_LENGTH=${DEEPEN_LENGTH:-$GIT_DEPTH}
 
 HEAD_REF=${1:-$(git rev-parse HEAD)}
 BASE_REF=${2:-origin/master}
-
 while [ -z "$(git merge-base "$BASE_REF" "$HEAD_REF" 2>/dev/null)" ]; do
   echo "Continuing fetch with a depth of $DEEPEN_LENGTH commits..."
   git fetch -q --deepen="$DEEPEN_LENGTH" origin;
+  git merge-base "$BASE_REF" "$HEAD_REF";
 done
