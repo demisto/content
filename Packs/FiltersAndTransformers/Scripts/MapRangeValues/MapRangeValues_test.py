ce08a3d283648175a0a443f8ef0260ad0fc57da0
EyalPintzov <91007713+eyalpalo@users.noreply.github.com>
2023-11-23 16:47:53 +0200

    fix and ut

--- Packs/FiltersAndTransformers/Scripts/MapRangeValues/MapRangeValues_test.py
+++ Packs/FiltersAndTransformers/Scripts/MapRangeValues/MapRangeValues_test.py
@@ -101,6 +101,7 @@ def test_get_replace_list(map_from, map_to, sep, expected_replace_list):
 data_test_replace_values = [
     (['1'], [Replace('1', '2')], [2]),
     (['3', '4', '5'], [RangeReplace('3', '5', '4')], [4] * 3),
+    (['0.5', '2'], [RangeReplace('0.3', '0.6', 'test'), Replace('2', 'test2')], ['test', 'test2']),
     ([str(i) for i in range(1, 6)], [Replace('1', '2'), RangeReplace('3', '5', '4')], [2, 2, 4, 4, 4]),
 ]
 
