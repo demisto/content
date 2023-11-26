4a33bb4cd9cb22c3549da16de9aca7fcca6356d2
YuvHayun <yhayun011@gmail.com>
2023-11-26 15:32:20 +0200

    update test_data

--- Packs/FeedTAXII/Integrations/FeedTAXII/FeedTAXII_test.py
+++ Packs/FeedTAXII/Integrations/FeedTAXII/FeedTAXII_test.py
@@ -89,11 +89,11 @@ class TestUtilFunctions:
 class TestCommands:
     def test_fetch_indicators(self, mocker):
         client = TAXIIClient(collection='a collection')
-        with open('FeedTAXII_test/TestCommands/raw_indicators.json') as f:
+        with open('test_data/raw_indicators.json') as f:
             raw_indicators = json.load(f)
             mocker.patch.object(client, 'build_iterator', return_value=raw_indicators)
             res = fetch_indicators_command(client)
-            with open('FeedTAXII_test/TestCommands/indicators_results.json') as exp_f:
+            with open('test_data/indicators_results.json') as exp_f:
                 expected = json.load(exp_f)
                 assert res == expected
 
@@ -113,14 +113,14 @@ def test_poll_collection(mocker):
     from FeedTAXII import fetch_indicators_command
     client = TAXIIClient(collection='a collection', poll_service='http://example/taxii-data')
 
-    with open('FeedTAXII_test/TestCommands/collection_example.xml', 'rb') as xml_f:
+    with open('test_data/collection_example.xml', 'rb') as xml_f:
         stix_content = xml_f.read()
 
     with requests_mock.Mocker() as m:
         m.post('http://example/taxii-data', content=stix_content)
         res = fetch_indicators_command(client)
 
-    with open('FeedTAXII_test/TestCommands/indicators_example.json') as json_f:
+    with open('test_data/indicators_example.json') as json_f:
         expected_result = json.load(json_f)
 
     assert res == expected_result
@@ -137,7 +137,7 @@ def test_tags_parameter(mocker, tags):
     - Validate the tags supplied exists in the indicators
     """
     client = TAXIIClient(collection='a collection', feedTags=json.dumps(tags))
-    with open('FeedTAXII_test/TestCommands/raw_indicators.json') as f:
+    with open('test_data/raw_indicators.json') as f:
         raw_indicators = json.load(f)
         mocker.patch.object(client, 'build_iterator', return_value=raw_indicators)
         res = fetch_indicators_command(client)
