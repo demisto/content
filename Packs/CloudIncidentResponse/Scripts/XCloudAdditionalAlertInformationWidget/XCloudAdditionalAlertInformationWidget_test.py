2e9c7db135f7183afd289335e427dd50d3bdeac1
ssokolovich <ssokolovich@paloaltonetworks.com>
2023-11-26 14:53:13 +0200

    Added new layout rule Added new layout updated scripts

--- Packs/CloudIncidentResponse/Scripts/XCloudAdditionalAlertInformationWidget/XCloudAdditionalAlertInformationWidget_test.py
+++ Packs/CloudIncidentResponse/Scripts/XCloudAdditionalAlertInformationWidget/XCloudAdditionalAlertInformationWidget_test.py
@@ -3,19 +3,20 @@ from unittest.mock import patch
 import pytest
 from XCloudAdditionalAlertInformationWidget import get_additonal_info
 
+
 class TestXCloudAdditionalAlertInformationWidget(unittest.TestCase):
 
     @patch('demistomock.context', return_value={'Core': {'OriginalAlert': [{'event': {'vendor': 'Vendor1',
-                                                                                   'cloud_provider': 'AWS',
-                                                                                   'log_name': 'SecurityLog',
-                                                                                   'raw_log': {'eventType': 'Event1'},
-                                                                                   'caller_ip': '192.168.1.1',
-                                                                                   'caller_ip_geolocation': 'Location1',
-                                                                                   'resource_type': 'ResourceType1',
-                                                                                   'identity_name': 'User1',
-                                                                                   'operation_name': 'Operation1',
-                                                                                   'operation_status': 'Success',
-                                                                                   'user_agent': 'Browser1'}}]}})
+                                                                                      'cloud_provider': 'AWS',
+                                                                                      'log_name': 'SecurityLog',
+                                                                                      'raw_log': {'eventType': 'Event1'},
+                                                                                      'caller_ip': '192.168.1.1',
+                                                                                      'caller_ip_geolocation': 'Location1',
+                                                                                      'resource_type': 'ResourceType1',
+                                                                                      'identity_name': 'User1',
+                                                                                      'operation_name': 'Operation1',
+                                                                                      'operation_status': 'Success',
+                                                                                      'user_agent': 'Browser1'}}]}})
     def test_get_additonal_info(self, mock_context):
         # Test with a mock context containing one original alert
         expected_result = [{'Alert Full Description': None,
@@ -36,6 +37,7 @@ class TestXCloudAdditionalAlertInformationWidget(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
     # Add more test cases as needed
+
 
 if __name__ == '__main__':
     unittest.main()
