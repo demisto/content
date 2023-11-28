2071000795b7ec308fe382013940233ef0e2c2dd
ashamah <ashamah@paloaltonetworks.com>
2023-11-28 16:11:59 +0200

    - Add UT to increase coverage - Change support type

--- Packs/FeedORKL/Integrations/FeedORKL/FeedORKL_test.py
+++ Packs/FeedORKL/Integrations/FeedORKL/FeedORKL_test.py
@@ -1,7 +1,7 @@
 import json
 from importlib import import_module
 from test_data.feed_data import RESPONSE_DATA
-from FeedORKL import Client, fetch_indicator_command
+from FeedORKL import Client, fetch_indicator_command, get_reports_command
 
 FeedORKL = import_module('FeedORKL')
 main = FeedORKL.main
@@ -40,3 +40,29 @@ def test_fetch_integration(requests_mock):
         if indicator.get('value') == 'ALPHVM':
             assert indicator.get('source') == 'ORKL Feed'
             break
+
+
+def test_get_reports_command(requests_mock):
+    requests_mock.get('https://orkl.eu/api/v1/library/entries?order_by=created_at&limit=1&offset=0&order=desc',
+                      json=RESPONSE_DATA)
+
+    expected_human_readable = (
+        "### ORKL Reports\n"
+        "|Created At|Report Name|Source|References|Threat Actors|\n"
+        "|---|---|---|---|---|\n"
+        "| 2023-11-18T02:07:23.236896Z | Scattered Spider | Malpedia | "
+        "https://www.cisa.gov/sites/default/files/2023-11/aa23-320a_scattered_spider.pdf | "
+        "ETDA:ALPHV,<br>ETDA:Muddled Libra,<br>ETDA:Lead,<br>MITRE:Scattered Spider,<br>"
+        "MISPGALAXY:Scattered Spider,<br>ETDA:Scattered Spider,<br>Secureworks:GOLD HARVEST |\n"
+    )
+
+    client = Client(verify=False)
+    params = {
+        'feedTags': 'Test Tag',
+        'tlp_color': 'RED',
+        'limit': 1,
+        'createRelationships': 'true',
+        'feedReliability': 'C - Fairly reliable'
+    }
+    reports = get_reports_command(client=client, limit=params.get('limit'), order_by='created_at', order='desc')
+    assert reports.readable_output == expected_human_readable
