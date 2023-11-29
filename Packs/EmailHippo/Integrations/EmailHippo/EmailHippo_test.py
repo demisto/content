afb3847f4aa6439f44f622c31f5a204eef244487
ilappe <ilappe@paloaltonetworks.com>
2023-11-29 12:16:01 +0200

    A stricter test

--- Packs/EmailHippo/Integrations/EmailHippo/EmailHippo_test.py
+++ Packs/EmailHippo/Integrations/EmailHippo/EmailHippo_test.py
@@ -1,7 +1,8 @@
 import pytest
 import json
+import dictdiffer
 import EmailHippo
-from EmailHippo import Client, DemistoException, Common
+from EmailHippo import Client, DemistoException
 
 
 @pytest.fixture
@@ -15,6 +16,28 @@ def load_test_data(path):
         return json.load(f)
 
 
+def compare_dicts(title, actual, expected):
+    """compare two dicts and return the diff if they don't.
+
+    Args:
+        actual (dict): The actual dict.
+        expected (dict): The expected dict.
+
+    Returns:
+        Tuple: True or False indicating if dicts match, message if they don't.
+    """
+    diff = list(dictdiffer.diff(expected, actual))
+    if diff:
+        msg = f"Actual {title} are different from the expected.\nDifferences found in the following keys:"
+        for (action, path_list, key) in diff:
+            path = f'{".".join((str(x) for x in path_list))}'
+            if action != 'change':
+                path += f'.{key[0]}'
+            msg += f'\n{path} - was {action}'
+        return False, msg
+    return True, ''
+
+
 class TestHappyPath:
     def test_get_email_reputation_success(self, requests_mock, client):
         """
@@ -26,16 +49,18 @@ class TestHappyPath:
             result returned as expected
         """
         requests_mock.get('https://test.com/v3/more/json/test/test@example.com',
-                          json=load_test_data('test_data/get_email_output.json'))
+                          json=load_test_data('test_data/get_email_output.json')['api_result'])
+        expected_entry_context = load_test_data('test_data/get_email_output.json')['expected_context']
 
         command_res = EmailHippo.email_reputation_command(client, {'email': 'test@example.com'}, 0)
 
         hr_keys = ['Result', 'Hippo Trust Score', 'Inbox quality score', 'Spam risk score']
-        output_keys = [Common.EMAIL.CONTEXT_PATH, Common.DBotScore.CONTEXT_PATH]
 
         assert command_res
+        actual_entry_context = command_res[0].to_context()['EntryContext']
+        dict_diff, msg = compare_dicts('email_reputation results', expected_entry_context, actual_entry_context)
+        assert dict_diff, msg
         assert all(key in command_res[0].readable_output for key in hr_keys)
-        assert all(key in command_res[0].to_context()['EntryContext'] for key in output_keys)
 
     def test_domain_reputation_command_success(self, requests_mock, client):
         """
@@ -47,7 +72,8 @@ class TestHappyPath:
             - result returned as expected
         """
         requests_mock.get('https://test.com/v1/test/example.com',
-                          json=load_test_data('test_data/get_domain_output.json'))
+                          json=load_test_data('test_data/get_domain_output.json')['api_result'])
+        expected_entry_context = load_test_data('test_data/get_domain_output.json')['expected_context']
 
         command_res = EmailHippo.domain_reputation_command(client, {'domain': 'example.com'}, 0)
 
@@ -57,16 +83,12 @@ class TestHappyPath:
             'Time To Expiry', 'Updated On',
             'Status', 'Name servers'
         ]
-        output_keys = [Common.Domain.CONTEXT_PATH, Common.DBotScore.CONTEXT_PATH]
 
         assert command_res
-        entry_context = command_res[0].to_context()['EntryContext']
+        actual_entry_context = command_res[0].to_context()['EntryContext']
+        dict_diff, msg = compare_dicts('domain_reputation results', expected_entry_context, actual_entry_context)
+        assert dict_diff, msg
         assert all(key in command_res[0].readable_output for key in hr_keys)
-        assert all(key in entry_context for key in output_keys)
-
-        # test that the date parsed correct
-        assert entry_context[Common.Domain.CONTEXT_PATH][0]['CreationDate'] == '2018-07-20T13:27:00.000Z'
-        assert entry_context[Common.Domain.CONTEXT_PATH][0]['UpdatedDate'] == '2023-11-05T14:28:35.000Z'
 
     def test_quota_command_success(self, requests_mock, client):
         """
@@ -103,7 +125,10 @@ class TestFailure:
             - a DemistoException is raised
             - matrix quota_error increased
         """
-        requests_mock.get('https://test.com/v3/more/json/test/test@example.com', status_code=429)
+        requests_mock.get(
+            'https://test.com/v3/more/json/test/test@example.com',
+            status_code=401,
+            text='Insufficient quota')
 
         with pytest.raises(DemistoException):
             client.get_email_reputation('test@example.com')
