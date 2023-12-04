4db37ffcfbb13d824d16a341a6f2288d0dca9f3c
GuyAfik <guyafik11@gmail.com>
2023-12-04 09:39:27 +0200

    revert infra used for testing

--- Tests/scripts/test_e2e_results.sh
+++ Tests/scripts/test_e2e_results.sh
@@ -2,7 +2,7 @@
 
 echo "Starting the e2e test summary script - Server type: ${SERVER_TYPE}, Product type: ${PRODUCT_TYPE}"
 
-if [[ $CI_COMMIT_BRANCH = "coverage_xsoar8_refactors" ]]; then
+if [[ -n "${NIGHTLY}" ]]; then
   python3 ./Tests/Marketplace/print_e2e_test_summary.py --artifacts-path "${ARTIFACTS_FOLDER}"
   summary_exit_code=$?
 else
