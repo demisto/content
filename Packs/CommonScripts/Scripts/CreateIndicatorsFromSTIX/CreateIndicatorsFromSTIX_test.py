f7c0de6b9402edf9a4519fa9f000e77d4be8717b
Martin Ohl <Martin.Ohl@ohl-net.eu>
2023-12-07 11:57:04 +0100

    Updated CreateIndicatorsFromSTIX_test.py

--- Packs/CommonScripts/Scripts/CreateIndicatorsFromSTIX/CreateIndicatorsFromSTIX_test.py
+++ Packs/CommonScripts/Scripts/CreateIndicatorsFromSTIX/CreateIndicatorsFromSTIX_test.py
@@ -1,7 +1,7 @@
 from CreateIndicatorsFromSTIX import *
 
 
-def test_create_indicators_loop(mocker):
+def test_create_indicators_loop_wo_args(mocker):
     """
     Given:
         - A collection of indicators in XSOAR Format
@@ -13,6 +13,30 @@ def test_create_indicators_loop(mocker):
         - Validate the indicators extract without errors.
     """
     args = {}
+    with open('test_data/expected_result_example3.json') as json_f:
+        indicators = json.load(json_f)
+    mocker.patch.object(demisto, 'executeCommand', return_value=[None])
+    results, errors = create_indicators_loop(args, indicators)
+    assert errors == []
+    assert results.readable_output == 'Create Indicators From STIX: 2 indicators were created.'
+
+
+def test_create_indicators_loop_w_args(mocker):
+    """
+    Given:
+        - A collection of indicators in XSOAR Format
+
+    When:
+        - Parsing stix indicators.
+
+    Then:
+        - Validate the indicators extract without errors.
+    """
+    args = {
+        'add_context': 'true',
+        'tags': 'tag1,tag2'
+    }
+
     with open('test_data/expected_result_example3.json') as json_f:
         indicators = json.load(json_f)
     mocker.patch.object(demisto, 'executeCommand', return_value=[None])
