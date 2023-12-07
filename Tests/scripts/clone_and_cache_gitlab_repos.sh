e8cc0d255813eab7a6c7f6567395fb6f3e5d765f
michal-dagan <mdagan@paloaltonetworks.com>
2023-12-07 12:35:56 +0200

    new

--- Tests/scripts/clone_and_cache_gitlab_repos.sh
+++ Tests/scripts/clone_and_cache_gitlab_repos.sh
@@ -3,32 +3,41 @@ set +e
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
+
+get_user_info() {
+  local user=$1
+  local token=$2
+  if [ -z "${user}" ] && [ -z "${token}" ]; then
+    echo ""
+  else
+    # If either user or token is not empty, then we need to add them to the url.
+    echo "${user}:${token}@"
+  fi
+}
 
 clone_repository() {
   local host=$1
   local user=$2
   local token=$3
-  local repo_name=$4
+  local full_repo_path=$4
   local branch=$5
   local retry_count=$6
   local sleep_time=${7:-10}  # default sleep time is 10 seconds.
   local exit_code=0
   local i=1
-  echo -e "${GREEN}Cloning ${repo_name} from ${host} branch:${branch} with ${retry_count} retries${NC}"
-  if [ -z "${user}" ] && [ -z "${token}" ]; then
-    user_info=""
-  else
-    user_info="${user}:${token}@"
-    # If either user or token is not empty, then we need to add them to the url.
-  fi
+  echo -e "${GREEN}Cloning ${full_repo_path} from ${host} branch:${branch} with ${retry_count} retries${NC}"
+
+  user_info=$(get_user_info "${user}" "${token}")
+
   for ((i=1; i <= retry_count; i++)); do
-    git clone --depth=1 "https://${user_info}${host}/${repo_name}.git" --branch "${branch}" && exit_code=0 && break || exit_code=$?
+    git clone --depth=1 "https://${user_info}${host}/${full_repo_path}.git" --branch "${branch}" && exit_code=0 && break || exit_code=$?
     if [ ${i} -ne "${retry_count}" ]; then
-      echo -e "${RED}Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, sleeping for ${sleep_time} seconds and trying again${NC}"
+      echo -e "${YELLOW}Failed to clone ${full_repo_path} with branch:${branch}, exit code:${exit_code}, sleeping for ${sleep_time} seconds and trying again${NC}"
       sleep "${sleep_time}"
     else
-      echo -e "${RED}Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}, exhausted all ${retry_count} retries${NC}"
+      echo -e "${RED}Failed to clone ${full_repo_path} with branch:${branch}, exit code:${exit_code}, exhausted all ${retry_count} retries${NC}"
       break
     fi
   done
@@ -39,50 +48,98 @@ clone_repository_with_fallback_branch() {
   local host=$1
   local user=$2
   local token=$3
-  local repo_name=$4
+  local full_repo_path=$4
   local branch=$5
   local retry_count=$6
   local sleep_time=${7:-10}  # default sleep time is 10 seconds.
   local fallback_branch="${8:-master}"
+  local repo_name=$9
 
   # Check if branch exists in the repository.
-  echo -e "${GREEN}Checking if branch ${branch} exists in ${repo_name}${NC}"
-  if [ -z "${user}" ] && [ -z "${token}" ]; then
-    user_info=""
-  else
-    # If either user or token is not empty, then we need to add them to the url.
-    user_info="${user}:${token}@"
-  fi
-  git ls-remote --exit-code --quiet --heads "https://${user_info}${host}/${repo_name}.git" "refs/heads/${branch}" 1>/dev/null 2>&1
+  echo -e "${GREEN}Checking if branch ${branch} exists in ${full_repo_path}${NC}"
+
+  user_info=$(get_user_info "${user}" "${token}")
+
+  git ls-remote --exit-code --quiet --heads "https://${user_info}${host}/${full_repo_path}.git" "refs/heads/${branch}" 1>/dev/null 2>&1
   local branch_exists=$?
 
   if [ "${branch_exists}" -ne 0 ]; then
-    echo -e "${RED}Branch ${branch} does not exist in ${repo_name}, defaulting to ${fallback_branch}${NC}"
+    echo -e "${YELLOW}Branch ${branch} does not exist in ${full_repo_path}, defaulting to ${fallback_branch}${NC}"
     local exit_code=1
   else
-    echo -e "${GREEN}Branch ${branch} exists in ${repo_name}, trying to clone${NC}"
-    clone_repository "${host}" "${user}" "${token}" "${repo_name}" "${branch}" "${retry_count}" "${sleep_time}"
+    echo -e "${GREEN}Branch ${branch} exists in ${full_repo_path}, trying to clone${NC}"
+    clone_repository "${host}" "${user}" "${token}" "${full_repo_path}" "${branch}" "${retry_count}" "${sleep_time}"
     local exit_code=$?
     if [ "${exit_code}" -ne 0 ]; then
-      echo -e "${RED}Failed to clone ${repo_name} with branch:${branch}, exit code:${exit_code}${NC}"
+      echo -e "${RED}Failed to clone ${full_repo_path} with branch:${branch}, exit code:${exit_code}${NC}"
     fi
   fi
   if [ "${exit_code}" -ne 0 ]; then
     # Trying to clone from fallback branch.
-    echo -e "${RED}Trying to clone repository:${repo_name} with fallback branch ${fallback_branch}!${NC}"
-    clone_repository "${host}" "${user}" "${token}" "${repo_name}" "${fallback_branch}" "${retry_count}" "${sleep_time}"
+    echo -e "${YELLOW}Trying to clone repository:${full_repo_path} with fallback branch ${fallback_branch}!${NC}"
+    clone_repository "${host}" "${user}" "${token}" "${full_repo_path}" "${fallback_branch}" "${retry_count}" "${sleep_time}"
     local exit_code=$?
     if [ ${exit_code} -ne 0 ]; then
-      echo -e "${RED}ERROR: Failed to clone ${repo_name} with fallback branch:${fallback_branch}, exit code:${exit_code}, exiting!${NC}"
+      echo -e "${RED}ERROR: Failed to clone ${full_repo_path} with fallback branch:${fallback_branch}, exit code:${exit_code}, exiting!${NC}"
       exit ${exit_code}
     else
-      echo -e "${GREEN}Successfully cloned ${repo_name} with fallback branch:${fallback_branch}${NC}"
+      echo "${fallback_branch}" > "${repo_name}".txt
+      echo -e "${GREEN}Successfully cloned ${full_repo_path} with fallback branch:${fallback_branch}${NC}"
       return 0
     fi
   else
-    echo -e "${GREEN}Successfully cloned ${repo_name} with branch:${branch}${NC}"
+    echo "${branch}" > "${repo_name}".txt
+    echo -e "${GREEN}Successfully cloned ${full_repo_path} with branch:${branch}${NC}"
     return 0
   fi
+}
+
+remove_cached_gitlab_repo() {
+    local repo_name=$1
+    echo "Removing $repo_name from the cache"
+    rm -rf "./$repo_name"
+}
+
+clone_and_cache_gitlab_repos() {
+  local host=$1
+  local user=$2
+  local token=$3
+  local project_namespace=$4
+  local branch=$5
+  local retry_count=$6
+  local sleep_time=${7:-10}  # default sleep time is 10 seconds.
+  local fallback_branch="${8:-master}"
+  local repo_name=$9
+  local full_repo_path="${project_namespace}/${repo_name}"
+
+  user_info=$(get_user_info "${user}" "${token}")
+
+  if [ -f "${repo_name}.txt" ]; then
+    cached_branch_name=$(cat "${repo_name}.txt")
+    git ls-remote --exit-code --quiet --heads "https://${user_info}${host}/${full_repo_path}.git" "refs/heads/${branch}" 1>/dev/null 2>&1
+    local branch_exists=$?
+    if [ "${branch_exists}" -eq 0 ]; then
+        if [ "${cached_branch_name}" != "${branch}" ]; then
+          remove_cached_gitlab_repo "${repo_name}"
+        fi
+      else
+        if [ "${cached_branch_name}" != "${fallback_branch}" ]; then
+          remove_cached_gitlab_repo "${repo_name}"
+        fi
+      fi
+  fi
+
+  if [ -d "./${repo_name}" ] ; then
+    echo "Fetching ${full_repo_path} repository with branch:${SEARCHED_BRANCH_NAME}"
+    cd ./"${repo_name}"
+    git remote set-url origin "https://${user_info}${host}/${full_repo_path}.git"
+    git fetch -p -P
+    cd ..
+  else
+    echo "Getting ${full_repo_path} repository with branch:${SEARCHED_BRANCH_NAME}, with fallback to master"
+    clone_repository_with_fallback_branch "${host}" "${user}" "${token}" "${full_repo_path}" "${branch}" "${retry_count}" "${sleep_time}" "${fallback_branch}" "${repo_name}"
+  fi
+
 }
 
 TEST_UPLOAD_BRANCH_SUFFIX="-upload_test_branch-"
@@ -96,49 +153,13 @@ else
   echo "Didn't find a branch with suffix ${TEST_UPLOAD_BRANCH_SUFFIX} in branch name, using the branch ${CI_COMMIT_BRANCH} to clone content-test-conf and infra repositories, with fallback to master"
   SEARCHED_BRANCH_NAME="${CI_COMMIT_BRANCH}"
 fi
-echo "Getting content-test-conf and infra repositories with branch:${SEARCHED_BRANCH_NAME}, with fallback to master"
-
-SECRET_CONF_PATH="./conf_secret.json"
-echo ${SECRET_CONF_PATH} > secret_conf_path
-
-XSIAM_SERVERS_PATH="./xsiam_servers.json"
-echo ${XSIAM_SERVERS_PATH} > xsiam_servers_path
-
-XSOAR_NG_SERVERS_PATH="./xsoar_ng_servers.json"
-echo ${XSOAR_NG_SERVERS_PATH} > xsoar_ng_servers_path
-
-DEMISTO_LIC_PATH="./demisto.lic"
-echo ${DEMISTO_LIC_PATH} > demisto_lic_path
-
-DEMISTO_PACK_SIGNATURE_UTIL_PATH="./signDirectory"
-echo ${DEMISTO_PACK_SIGNATURE_UTIL_PATH} > demisto_pack_sig_util_path
 
 CI_SERVER_HOST=${CI_SERVER_HOST:-code.pan.run}
 
-clone_repository_with_fallback_branch "${CI_SERVER_HOST}" "gitlab-ci-token" "${CI_JOB_TOKEN}" "${CI_PROJECT_NAMESPACE}/content-test-conf" "${SEARCHED_BRANCH_NAME}" 3 10 "master"
+echo "Getting content-test-conf and infra repositories with branch:${SEARCHED_BRANCH_NAME}"
 
-cp ./content-test-conf/secrets_build_scripts/google_secret_manager_handler.py ./Tests/scripts
-cp ./content-test-conf/secrets_build_scripts/add_secrets_file_to_build.py ./Tests/scripts
-cp ./content-test-conf/secrets_build_scripts/merge_and_delete_dev_secrets.py ./Tests/scripts
-cp -r ./content-test-conf/demisto.lic ${DEMISTO_LIC_PATH}
-cp -r ./content-test-conf/signDirectory ${DEMISTO_PACK_SIGNATURE_UTIL_PATH}
-
-echo "Cloning PrivatePacks"
-cp -r ./content-test-conf/content/PrivatePacks/* ./Packs
-echo "Cloned PrivatePacks"
-
-if [[ "${NIGHTLY}" == "true" || "${EXTRACT_PRIVATE_TESTDATA}" == "true" ]]; then
-    python ./Tests/scripts/extract_content_test_conf.py --content-path . --content-test-conf-path ./content-test-conf --missing-content-packs-test-conf "${ARTIFACTS_FOLDER_SERVER_TYPE}/missing_content_packs_test_conf.txt"
-fi
-rm -rf ./content-test-conf
-
-clone_repository_with_fallback_branch "${CI_SERVER_HOST}" "gitlab-ci-token" "${CI_JOB_TOKEN}" "${CI_PROJECT_NAMESPACE}/infra" "${SEARCHED_BRANCH_NAME}" 3 10 "master"
-
-cp -r ./infra/xsiam_servers.json $XSIAM_SERVERS_PATH
-cp -r ./infra/xsoar_ng_servers.json $XSOAR_NG_SERVERS_PATH
-
-mv ./infra/gcp ./gcp
-rm -rf ./infra
+clone_and_cache_gitlab_repos "${CI_SERVER_HOST}" "gitlab-ci-token" "${CI_JOB_TOKEN}" "${CI_PROJECT_NAMESPACE}" "${SEARCHED_BRANCH_NAME}" 3 10 "master" "content-test-conf"
+clone_and_cache_gitlab_repos "${CI_SERVER_HOST}" "gitlab-ci-token" "${CI_JOB_TOKEN}" "${CI_PROJECT_NAMESPACE}" "${SEARCHED_BRANCH_NAME}" 3 10 "master" "infra"
 
 set -e
 echo "Successfully cloned content-test-conf and infra repositories"
