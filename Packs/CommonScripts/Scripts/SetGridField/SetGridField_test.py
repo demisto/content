b84d097d5dc2f66c8b47a3cd1cddd10cf5e3eb99
sapirshuker <sshuker@paloaltonetworks.com>
2023-12-07 12:37:44 +0200

    add tests

--- Packs/CommonScripts/Scripts/SetGridField/SetGridField_test.py
+++ Packs/CommonScripts/Scripts/SetGridField/SetGridField_test.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+import demistomock as demisto
 
 
 def util_load_json(path):
@@ -189,3 +190,53 @@ def test_build_grid_command_with_multi_sort_by(mocker, keys: list[str], columns:
 
     expected_results = util_load_json(expected_results_path)
     assert json.dumps(results) == json.dumps(expected_results)
+
+
+def side_effect_for_execute_command_in_playground(command: str, arguments: dict):
+    if command == "getIncidents":
+        return [{'Type': 1, 'Contents': {'ErrorsPrivateDoNotUse': None, 'data': [], 'total': 0}}]
+    if command == 'setIncident':
+        return None
+    return {}
+
+
+def test_main_does_not_raises_list_index_in_playground(mocker):
+    """Unit test
+     Given
+    - An output from executeCommand in the playground.
+     When
+    - Execute SetGridField from the playground.
+    Then
+    - Verify that no error message was raised..
+    """
+    import SetGridField
+    mocker.patch.object(demisto, 'args', return_value={"grid_id": "grid_id", "keys": "key1,key2", 'columns': "col1,col2", "sort_by": "col1",
+                                                       "overwrite": "True", "unpack_nested_elements": "False"})
+    mocker.patch.object(demisto, 'incident', return_value={"CustomFields": None, "isPlayground": True})
+    mocker.patch.object(SetGridField, 'get_current_table', return_value=[])
+    mocker.patch.object(SetGridField, 'build_grid_command', return_value=[{"name": "name", "readable_name": "readable_name"}])
+    mocker.patch.object(demisto, 'executeCommand', side_effect=side_effect_for_execute_command_in_playground)
+    mocked_return_err = mocker.patch.object(SetGridField, "return_error")
+    SetGridField.main()
+    assert not mocked_return_err.called
+
+
+def test_main_raises_list_index_not_in_playground(mocker):
+    """Unit test
+     Given
+    - An output from executeCommand in the playground.
+     When
+    - Execute SetGridField from the playground.
+    Then
+    - Verify that no error message was raised..
+    """
+    import SetGridField
+    mocker.patch.object(demisto, 'args', return_value={"grid_id": "grid_id", "keys": "key1,key2", 'columns': "col1,col2", "sort_by": "col1",
+                                                       "overwrite": "True", "unpack_nested_elements": "False"})
+    mocker.patch.object(demisto, 'incident', return_value={"CustomFields": None, "isPlayground": False})
+    mocker.patch.object(SetGridField, 'get_current_table', return_value=[])
+    mocker.patch.object(SetGridField, 'build_grid_command', return_value=[{"name": "name", "readable_name": "readable_name"}])
+    mocker.patch.object(demisto, 'executeCommand', side_effect=side_effect_for_execute_command_in_playground)
+    with pytest.raises(ValueError) as e:
+        SetGridField.main()
+    assert "Failed to execute setGridField. Error: The following grid id was not found: gridid." in str(e.value)
