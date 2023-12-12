18845be4a4a0909c1cc8b5fbb6584b1d2a4905f7
samuelFain <65926551+samuelFain@users.noreply.github.com>
2023-12-12 11:58:23 +0200

    Fix microsocks_darwin path

--- Tests/scripts/test.sh
+++ Tests/scripts/test.sh
@@ -4,9 +4,14 @@ pwd
 ls -la
 echo "-----------------------------------------"
 chmod +x darwin # grant permissions to execute
-./microsocks_darwin -p 9980 &pid=$!
+./Packs/Whois/Integrations/Whois/test_data/microsocks_darwin -p 9980 &pid=$!
 echo "running darwin on pid: $pid"
 sleep 5
+echo "running command: netstat -p tcp -l -n | grep 9980"
 netstat -p tcp -l -n | grep 9980
+echo "running command: ss -tln | grep 9980"
+ss -tln | grep 9980
+echo "running command: lsof -i :9980"
+lsof -i :9980
 kill $pid
 echo "killed darwin on pid: $pid"
