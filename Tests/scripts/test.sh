d200d840303f71231143c2ac0c04ede811b42105
samuelFain <65926551+samuelFain@users.noreply.github.com>
2023-12-12 12:57:08 +0200

    Add microsocks executable and enable netstat

--- Tests/scripts/test.sh
+++ Tests/scripts/test.sh
@@ -3,11 +3,12 @@ echo "-----------------------------------------"
 pwd
 ls -la
 echo "-----------------------------------------"
-chmod +x darwin # grant permissions to execute
+chmod +x ./Packs/Whois/Integrations/Whois/test_data/microsocks_darwin # grant permissions to execute
 ./Packs/Whois/Integrations/Whois/test_data/microsocks_darwin -p 9980 &pid=$!
 echo "running darwin on pid: $pid"
 sleep 5
 echo "running command: netstat -p tcp -l -n | grep 9980"
+sudo apt-get install net-tools
 netstat -p tcp -l -n | grep 9980
 echo "running command: ss -tln | grep 9980"
 ss -tln | grep 9980
