f5cc1ab793db855faf3c4a443094adbb579084cf
Danny_Fried <dfried@paloaltonetworks.com>
2023-12-15 16:56:53 +0200

    init commit

---
+++ Packs/AWS-SNS-Listener/Integrations/AWSSNSListener/AWSSNSListener.py
@@ -0,0 +1,116 @@
+from CommonServerPython import *  # noqa: F401
+from tempfile import NamedTemporaryFile
+from traceback import format_exc
+from collections import deque
+import uvicorn
+# from secrets import compare_digest
+from fastapi import FastAPI, Request
+from fastapi.security import HTTPBasic
+from fastapi.security.api_key import APIKeyHeader
+from CommonServerUserPython import *
+from AWSApiModule import *  # noqa: E402
+import requests
+
+
+sample_events_to_store = deque(maxlen=20)  # type: ignore[var-annotated]
+
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+basic_auth = HTTPBasic(auto_error=False)
+token_auth = APIKeyHeader(auto_error=False, name='Authorization')
+
+@app.post('/incident/aws/sns')
+async def handle_post(request: Request):
+    data = ''
+    try:
+        payload = await request.json()
+        dump = json.dumps(payload)
+    except Exception as e:
+        demisto.error(f'Failed to extract request {e}')
+        return
+    if payload['Type'] == 'SubscriptionConfirmation':
+        demisto.debug('SubscriptionConfirmation request')
+        subscribe_url = payload['SubscribeURL']
+        response = requests.get(subscribe_url)
+        demisto.debug(f'Response from subscribe url: {response}')
+
+    elif payload['Type'] == 'Notification':
+        message = payload['Message']
+        demisto.debug(f'Notification request msg: {message}')
+        incident = {
+            'name': payload['Subject'],
+            'labels': [],
+            'rawJSON': dump,
+            'occurred': payload['Timestamp'],
+            'details': message,
+            'type': 'AWS-SNS'
+        }
+        demisto.debug(f'demisto.createIncidents with:{incident}')
+        data = demisto.createIncidents(incidents=[incident])
+        if not data:
+            demisto.error('Failed creating incident')
+
+    else:
+        demisto.error(f"Failed handling AWS SNS request, unknown type: {payload['Type']}")
+
+''' MAIN FUNCTION '''
+
+
+def main():
+    params = demisto.params()
+    demisto.debug(f'Command being called is {demisto.command()}')
+    try:
+        try:
+            port = int(params.get('longRunningPort'))
+        except ValueError as e:
+            raise ValueError(f'Invalid listen port - {e}')
+        if demisto.command() == 'test-module':
+            return_results("ok")
+        elif demisto.command() == 'long-running-execution':
+            demisto.debug('Started long-running-execution.')
+            while True:
+                certificate = params.get('certificate', '')
+                private_key = params.get('key', '')
+
+                certificate_path = ''
+                private_key_path = ''
+                try:
+                    ssl_args = {}
+                    if certificate and private_key:
+                        certificate_file = NamedTemporaryFile(delete=False)
+                        certificate_path = certificate_file.name
+                        certificate_file.write(bytes(certificate, 'utf-8'))
+                        certificate_file.close()
+                        ssl_args['ssl_certfile'] = certificate_path
+
+                        private_key_file = NamedTemporaryFile(delete=False)
+                        private_key_path = private_key_file.name
+                        private_key_file.write(bytes(private_key, 'utf-8'))
+                        private_key_file.close()
+                        ssl_args['ssl_keyfile'] = private_key_path
+
+                        demisto.debug('Starting HTTPS Server')
+                    else:
+                        demisto.debug('Starting HTTP Server')
+
+                    integration_logger = IntegrationLogger()
+                    integration_logger.buffering = False
+                    log_config = dict(uvicorn.config.LOGGING_CONFIG)
+                    log_config['handlers']['default']['stream'] = integration_logger
+                    log_config['handlers']['access']['stream'] = integration_logger
+                    uvicorn.run(app, host='0.0.0.0', port=port, log_config=log_config, **ssl_args)
+                except Exception as e:
+                    demisto.error(f'An error occurred in the long running loop: {str(e)} - {format_exc()}')
+                    demisto.updateModuleHealth(f'An error occurred: {str(e)}')
+                finally:
+                    if certificate_path:
+                        os.unlink(certificate_path)
+                    if private_key_path:
+                        os.unlink(private_key_path)
+                    time.sleep(5)
+    except Exception as e:
+        demisto.error(format_exc())
+        return_error(f'Failed to execute {demisto.command()} command. Error: {e}')
+
+
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
---
+++ Packs/AWS-SNS-Listener/Integrations/AWSSNSListener/README.md
@@ -0,0 +1,25 @@
+AWS-SNS-Listener description.
+This integration was integrated and tested with version xx of AWS-SNS-Listener.
+
+## Configure AWS-SNS-Listener on Cortex XSOAR
+
+1. Navigate to **Settings** > **Integrations** > **Servers & Services**.
+2. Search for AWS-SNS-Listener.
+3. Click **Add instance** to create and configure a new integration instance.
+
+    | **Parameter** | **Description** | **Required** |
+    | --- | --- | --- |
+    | Long running instance |  | False |
+    | Listen Port | Runs the service on this port from within Cortex XSOAR. Requires a unique port for each long-running integration instance. Do not use the same port for multiple instances. Note: If you click the test button more than once, a failure may occur mistakenly indicating that the port is already in use. \(For Cortex XSOAR 8 and Cortex XSIAM\) If you do not enter a Listen Port, an unused port for AWS SNS Listener will automatically be generated when the instance is saved. However, if using an engine, you must enter a Listen Port. | True |
+    | Username | \(For Cortex XSOAR 6.x\) False \(For Cortex XSOAR 8 and Cortex XSIAM\) Optional for engines, otherwise mandatory. | False |
+    | Password |  | False |
+    | Certificate (Required for HTTPS) | \(For Cortex XSOAR 6.x\) For use with HTTPS - the certificate that the service should use. \(For Cortex XSOAR 8 and Cortex XSIAM\) Custom certificates are not supported. | False |
+    | Private Key (Required for HTTPS) | \(For Cortex XSOAR 6.x\) For use with HTTPS - the certificate that the service should use. \(For Cortex XSOAR 8 and Cortex XSIAM\) Custom certificates are not supported. | False |
+    | Store sample events for mapping | Because this is a push-based integration, it cannot fetch sample events in the mapping wizard. After you finish mapping, it is recommended to turn off the sample events storage to reduce performance overhead. | False |
+
+4. Click **Test** to validate the URLs, token, and connection.
+
+## Commands
+
+You can execute these commands from the Cortex XSOAR CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
