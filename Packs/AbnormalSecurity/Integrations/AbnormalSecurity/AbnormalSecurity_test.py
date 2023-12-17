19392bc833bd437289eda98caae57f243d56bf30
adi88d <adaud@paloaltonetworks.com>
2023-12-17 16:45:01 +0200

    remove whitespace from blank line

--- Packs/AbnormalSecurity/Integrations/AbnormalSecurity/AbnormalSecurity_test.py
+++ Packs/AbnormalSecurity/Integrations/AbnormalSecurity/AbnormalSecurity_test.py
@@ -501,7 +501,7 @@ def test_fetch_cases_incidents(mocker, mock_get_a_list_of_abnormal_cases_identif
         fetch_threats=False
     )
     assert len(incidents) == 1
-    
+
 
 def test_fetch_abuse_campaign_incidents(mocker, mock_get_a_list_of_campaigns_submitted_to_abuse_mailbox_request):
     client = mock_client(mocker, util_load_json('test_data/test_get_details_of_abuse_campaign.json'))
