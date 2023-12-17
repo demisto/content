b21262194a60ca0f1db9fe2130625d1eefb51411
jlevypaloalto <jlevy@paloaltonetworks.com>
2023-12-17 18:14:09 +0200

    init

--- Packs/AWS-EC2/Scripts/AwsEC2GetAccounts/AwsEC2GetAccounts.py
+++ Packs/AWS-EC2/Scripts/AwsEC2GetAccounts/AwsEC2GetAccounts.py
@@ -5,6 +5,10 @@ from CommonServerPython import *
 ACCOUNT_LIST_COMMAND = 'aws-org-account-list'
 
 
+def http_request(method: str, uri: str, body: dict = {}) -> dict:
+    return json.loads(demisto.internalHttpRequest(method, uri, body)['body'])
+
+
 def get_account_ids() -> list[str]:
     try:
         account_list_result = demisto.executeCommand(ACCOUNT_LIST_COMMAND, {})
@@ -24,18 +28,32 @@ def get_account_ids() -> list[str]:
         raise DemistoException(f'Unexpected error while fetching accounts:\n{e}')
 
 
-def configure_ec2_instance(account_ids: list[str]):
+def update_ec2_instance(account_ids: list[str], instance_name: str):
     accounts_as_str = ','.join(account_ids)
     try:
-        demisto.internalHttpRequest('PUT', 'uri', {})
+        response = http_request('POST', '/settings/integration/search')
+        demisto.debug(str(response))  # remove
+        instance = next(inst for inst in response['instances'] if inst['name'] == instance_name)
+        if instance['configvalues']['accounts_to_access'] != accounts_as_str:
+            demisto.debug(f'Updating {instance_name!r} with accounts: {accounts_as_str}')
+            # instance['version'] += 1
+            instance['configvalues']['accounts_to_access'] = accounts_as_str
+            response = http_request('PUT', '/settings/integration', instance)
+            return_results(str(response))  # remove
+        else:
+            demisto.debug(f'Not updating {instance_name!r}. Account list is up to date.')
+    except StopIteration:
+        raise DemistoException(f'AWS - EC2 instance {instance_name!r} was not found.')
     except Exception as e:
         raise DemistoException(f'Unexpected error while configuring AWS - EC2 instance with accounts {accounts_as_str!r}:\n{e}')
 
 
 def main():
     try:
+        instance_name = str(demisto.getArg('instanceName'))
         account_ids = get_account_ids()
-        configure_ec2_instance(account_ids)
+        update_ec2_instance(account_ids, instance_name)
+        return_results(f'Successfully updated {instance_name!r} with accounts: {", ".join(account_ids)}')
     except Exception as e:
         return_error(f'Error in AwsEC2GetAccounts: {e}')
 
