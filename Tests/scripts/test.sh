ffe1214e2f2ba21faaf716e68b7f374d2362a374
Koby Meir <kobymeir@users.noreply.github.com>
2023-12-18 14:05:05 +0200

    Update Tests/scripts/test.sh

--- Tests/scripts/test.sh
+++ Tests/scripts/test.sh
@@ -3,15 +3,17 @@ echo "-----------------------------------------"
 pwd
 ls -la
 echo "-----------------------------------------"
-chmod +x ./Packs/Whois/Integrations/Whois/test_data/microsocks_darwin # grant permissions to execute
-./Packs/Whois/Integrations/Whois/test_data/microsocks -p 9980 &pid=$!
-echo "running darwin on pid: $pid"
+chmod +x ./Packs/Whois/Integrations/Whois/test_data/microsocks # grant permissions to execute
+./Packs/Whois/Integrations/Whois/test_data/microsocks -p 9980 &
+pid=$!
+echo "running microsocks on pid: $pid"
 sleep 5
 echo "running command: netstat -p tcp -l -n | grep 9980"
-sudo apt-get install net-tools
+apt-get install net-tools -y
+apt-get install lsof -y
 netstat -p tcp -l -n | grep 9980
 echo "running command: ss -tln | grep 9980"
-ss -tln | grep 9980
+#ss -tln | grep 9980
 echo "running command: lsof -i :9980"
 lsof -i :9980
 kill $pid
