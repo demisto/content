65becfb053536474abfac2b8dbc18acce79e4e5f
samuelFain <65926551+samuelFain@users.noreply.github.com>
2023-12-19 11:49:32 +0200

    Update test.sh with whois commands

--- Tests/scripts/test.sh
+++ Tests/scripts/test.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 echo "-----------------------------------------"
+echo "running: pwd and ls -la"
 pwd
 ls -la
 echo "-----------------------------------------"
+echo "running: apt install whois"
+sudo apt-get install whois
+echo "running: apt install whois"
+echo "running: whois google.co.uk (wihtout proxy)"
+whois google.co.uk
+
+
+echo "running: chmod +x ./Packs/Whois/Integrations/Whois/test_data/microsocks"
 chmod +x ./Packs/Whois/Integrations/Whois/test_data/microsocks # grant permissions to execute
 ./Packs/Whois/Integrations/Whois/test_data/microsocks -p 9980 &
 pid=$!
-echo "running microsocks on pid: $pid"
+echo "running: microsocks on pid: $pid"
+
 sleep 5
-echo "running command: netstat -p tcp -l -n | grep 9980"
+echo "running: netstat -p tcp -l -n | grep 9980"
 apt-get install net-tools -y
-apt-get install lsof -y
 netstat -p tcp -l -n | grep 9980
-echo "running command: ss -tln | grep 9980"
+
+echo "running: whois google.co.uk (with proxy)"
+whois google.co.uk
+
+#echo "running command: ss -tln | grep 9980"
 #ss -tln | grep 9980
-echo "running command: lsof -i :9980"
-lsof -i :9980
+# apt-get install lsof -y
+#echo "running command: lsof -i :9980"
+# lsof -i :9980
+
+
 kill $pid
 echo "killed darwin on pid: $pid"
