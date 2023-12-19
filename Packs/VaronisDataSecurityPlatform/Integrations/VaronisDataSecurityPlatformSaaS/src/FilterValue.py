f389758ef6ea8d500a1b7f0636de59e6e3cc14c9
VARONIS\bdudnyk <bdudnyk@varonis.com>
2023-12-19 12:16:22 +0200

    removing src files

--- Packs/VaronisDataSecurityPlatform/Integrations/VaronisDataSecurityPlatformSaaS/src/FilterValue.py
+++
@@ -1,7 +0,0 @@
-class FilterValue:
-    def __init__(self, value):
-        self = value
-        # self.displayValue = value.get("displayValue", None)
-
-    def __repr__(self):
-        return f"{self.value}"
--- Packs/VaronisDataSecurityPlatform/Integrations/VaronisDataSecurityPlatformSaaS/src/ThreatModelAttributes.py
+++
@@ -1,8 +0,0 @@
-class ThreatModelAttributes:
-    Id = "ruleID"
-    Name = "ruleName"
-    Category = "ruleArea"
-    Source = "ruleSource"
-    Severity = "severity"
-
-    Columns = [Id, Name, Category, Source, Severity]
