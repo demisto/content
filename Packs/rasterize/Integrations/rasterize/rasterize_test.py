423ec8148e280829452796392dbc4fc7e2240f00
tkatzir <tkatzir@paloaltonetworks.com>
2023-12-19 11:41:34 +0200

    import mock_open

--- Packs/rasterize/Integrations/rasterize/rasterize_test.py
+++ Packs/rasterize/Integrations/rasterize/rasterize_test.py
@@ -11,6 +11,7 @@ import http.server
 import time
 import threading
 import pytest
+from unittest.mock import mock_open
 
 # disable warning from urllib3. these are emitted when python driver can't connect to chrome yet
 logging.getLogger("urllib3").setLevel(logging.ERROR)
