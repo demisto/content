516bc310db4eb2f1785800597e69d70f718c0d1f
sapirshuker <sshuker@paloaltonetworks.com>
2023-12-25 17:16:46 +0200

    add use case

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -9205,10 +9205,13 @@ def test_has_passed_time_threshold__different_timestamps(timestamp_str, seconds_
     ("e61fcc6a06420106fa6642ef833b9c38", "md5"), 
     ("12323c96a2ba1a4d7cd8d179641aac32f0d","md5")
     ("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", "sha256"), 
-    ("bb8098f4627441f6a29c31757c45339c74b2712b92783173df9ab58d47ae3bfa", "sha256")
+    ("bb8098f4627441f6a29c31757c45339c74b2712b92783173df9ab58d47ae3bfa", "sha256"),
     ("193:iAklVz3fzvBk5oFblLPBN1iXf2bCRErwyN4aEbwyiNwyiQwNeDAi4XMG:iAklVzfzvBTFblLpN1iXOYpyuapyiWym", "ssdeep"),
+    ("3:Wg8oEIjOH9+KS3qvRBTdRi690oVqzBUGyT0/n:Vx0HgKnTdE6eoVafY8", "ssdeep"),
     ("1ff8be1766d9e16b0b651f89001e8e7375c9e71f", "sha1"),
+    ("6c5360d41bd2b14b1565f5b18e5c203cf512e493", "sha1"),
     ("eaf7542ade2c338d8d2cc76fcbf883e62c31336e60cb236f86ed66c8154ea9fb836fd88367880911529bdafed0e76cd34272123a4d656db61b120b95eaa3e069","sha512"),
+    ("a7c19471fb4f2b752024246c28a37127ea7475148c04ace743392334d0ecc4762baf30b892d6a24b335e1065b254166f905fc46cc3ba5dba89e757bb7023a211","sha512" ),
     ("invalid", None)
 ])
 def test_detect_file_indicator_type(indicator,expected_result):
