b52dcaabae1a5fa2f54e21be9c3aac3e56e4c5a3
kobymeir <kmeir@paloaltonetworks.com>
2023-12-25 13:09:53 +0200

    CRTX-100700 - take 1

--- Packs/CommonScripts/Scripts/diplayMappedFields/diplayMappedFields.py
+++ Packs/CommonScripts/Scripts/diplayMappedFields/diplayMappedFields.py
@@ -2,8 +2,15 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 
+# List of empty values that we want to filter out.
+EMPTY_VALUES = ["{}", "[{}]", "[{}, {}]", "[{}, {}, {}]", "0001-01-01T00:00:00Z", "containmentsla", "remediationsla",
+                "detectionsla", "triagesla"]
+
+
 def extract_keys_with_values(obj, parent_key=''):
-    """Extracts the keys with values in the JSON object"""
+    """
+    Extracts the keys with values in the JSON object
+    """
     items = []
     for k, v in obj.items():
         new_key = f'{parent_key}.{k}' if parent_key else k
@@ -14,50 +21,30 @@ def extract_keys_with_values(obj, parent_key=''):
     return items
 
 
-def format_data_to_table(items):
-    """Formats the extracted data into a table format"""
-    table = '| Field Name | Value |\n'
-    for key, value in items:
-        table += f'| {key} | {value} |\n'
-    return table
+def format_data_to_rows(items):
+    """
+    Formats the extracted data into rows.
+    """
+    return [f'{key}|{value}' for key, value in items]
 
 
-def convert_to_html(markdown):
-    html = "<table style='border-collapse: collapse;'>"
-    lines = markdown.split("\n")
-    for line in lines[1:]:
-        html += "<tr>"
-        columns = line.split("|")
-        for i, column in enumerate(columns):
-            if column.strip():
-                if i != 1:
-                    html += f"<td style='font-family: Lato, Assistant, sans-serif; font-weight: 600; \
-                    font-size: 12px; text-align: left;  \
-                    padding: 1px 0px 0px; margin: 0px 5px 0px 0px; contrast: 4.95; ֿֿֿ\
-                    color: var(--xdr-on-background) ;'>{column.strip()}</td>"
-                else:
-                    html += f"<td style='font-family: Lato, Assistant, sans-serif; font-weight: 600; \
-                    font-size: 12px; text-align: left; \
-                    padding: 1px 0px 0px; margin: 0px 5px 0px 0px; contrast: 4.95; \
-                    color: var(--xdr-on-background-secondary);'>{column.strip()}</td>"
-        html += "</tr>"
-    html += "</table>"
-    return html
+def convert_to_html(rows):
+    html = ["""<table style="border-collapse:collapse;"><tbody style="font-family:Lato,Assistant,sans-serif;font-weight:600;font-size:12px;text-align:left;padding: 1px 0px 0px;margin:0px 5px 0px 0px;contrast:4.95">"""]  # noqa: E501
+    for row in rows:
+        html.append("<tr>")
+        columns = row.split("|")
+        for i, column in enumerate(map(str.strip, columns)):
+            if column:
+                style = "color:var(--xdr-on-background-secondary)" if i == 0 else "color:var(--xdr-on-background)"
+                html.append(f"<td style=\"{style}\">{column}</td>")
+        html.append("</tr>")
+    html.append("</tbody></table>")
+    return "".join(html)
 
 
-def remove_empty_rows(table):
-    # Split the table into rows
-    rows = table.split("\n")
-
+def remove_empty_rows(rows):
     # Filter out the rows that contain empty dictionaries
-    filtered_rows = [row for row in rows if not (
-        "{}" in row or "[{}]" in row or "[{}, {}]" in row or "[{}, {}, {}]" in row or "0001-01-01T00:00:00Z" in row or " \
-        containmentsla" in row or "remediationsla" in row or "detectionsla" in row or "triagesla" in row)]
-
-    # Join the filtered rows back into a table
-    filtered_table = "\n".join(filtered_rows)
-
-    return filtered_table
+    return [row for row in rows if not any(empty_value in row for empty_value in EMPTY_VALUES)]
 
 
 def main():
@@ -67,12 +54,12 @@ def main():
 
     # Extract the keys with values
     items = extract_keys_with_values(fields)
-    # Format the data into a table
-    table = format_data_to_table(items)
+    # Format the data into a rows
+    rows = format_data_to_rows(items)
     # Remove keys with empty dictionaries
-    filtered_markdown = remove_empty_rows(table)
-    # Convert the markdown to HTML
-    html = convert_to_html(filtered_markdown)
+    filtered_rows = remove_empty_rows(rows)
+    # Convert the rows to HTML
+    html = convert_to_html(filtered_rows)
 
     demisto.results({
         'ContentsFormat': formats['html'],
