1bdf7316b7d7aa30e2c89360befe746615d61bef
michal-dagan <mdagan@paloaltonetworks.com>
2023-12-31 12:49:30 +0200

    fixed

--- Utils/should_trigger_test_upload.sh
+++ Utils/should_trigger_test_upload.sh
@@ -7,9 +7,7 @@ if [[ $DEMISTO_SDK_NIGHTLY == "true" ]]; then
     echo "DEMISTO_SDK_NIGHTLY is set to true, Will run test-upload-flow"
     exit 0
 fi
-
-DIFF_FILES_LIST=$(git diff origin/master..."XSUP-29866" --name-only)
-
+DIFF_FILES_LIST=$(git diff origin/master...$CI_COMMIT_BRANCH --name-only)
 IGNORED_FILES=(
     "Tests/conf.json"
     "Tests/known_words.txt"
