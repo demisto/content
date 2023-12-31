6ee9bfb369b1440cc6ddea3e401eaa753c76bee8
michal-dagan <mdagan@paloaltonetworks.com>
2023-12-31 13:27:20 +0200

    test

--- Utils/should_trigger_test_upload.sh
+++ Utils/should_trigger_test_upload.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-# exit on errors
-set -e
 
 if [[ $DEMISTO_SDK_NIGHTLY == "true" ]]; then
     echo "DEMISTO_SDK_NIGHTLY is set to true, Will run test-upload-flow"
     exit 0
 fi
 DIFF_FILES_LIST=$(git diff origin/master...$CI_COMMIT_BRANCH --name-only)
+GIT_DIFF_STATUS=$?
+
+if [ $GIT_DIFF_STATUS -ne 0 ]; then
+    print_error "git diff command failed with exit status $GIT_DIFF_STATUS"
+    exit 1
+fi
+
 IGNORED_FILES=(
     "Tests/conf.json"
     "Tests/known_words.txt"
