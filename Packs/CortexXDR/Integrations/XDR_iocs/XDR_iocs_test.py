eeac2ac1b24146b451ec1f39ca4657e4c3635480
GuyAfik <guyafik11@gmail.com>
2024-01-02 07:19:49 +0200

    fix xdr_iocs_test

--- Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs_test.py
+++ Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs_test.py
@@ -219,7 +219,7 @@ class TestCreateFile:
             Then:
                 - Verify sync file data.
         """
-        mocker.patch.object(demisto, 'searchIndicators', return_value={})
+        mocker.patch.object(demisto, 'searchIndicators', return_value={"total": 0})
         create_file_sync(TestCreateFile.path)
         data = self.get_file(TestCreateFile.path)
         expected_data = ''
@@ -290,7 +290,7 @@ class TestCreateFile:
                 - Verify iocs to keep file data.
         """
 
-        mocker.patch.object(demisto, 'searchIndicators', return_value={})
+        mocker.patch.object(demisto, 'searchIndicators', return_value={"total": 0})
         create_file_iocs_to_keep(TestCreateFile.path)
         data = self.get_file(TestCreateFile.path)
         expected_data = ' '
@@ -580,7 +580,7 @@ class TestXDRIOCToDemisto:
             Then:
                 - IOC in demisto format.
         """
-        mocker.patch.object(demisto, 'searchIndicators', return_value={})
+        mocker.patch.object(demisto, 'searchIndicators', return_value={"total": 0})
         output = xdr_ioc_to_demisto(xdr_ioc)
         del output['rawJSON']
         assert output == demisto_ioc, f'xdr_ioc_to_demisto({xdr_ioc})\n\treturns: {d_sort(output)}\n\tinstead: {d_sort(demisto_ioc)}'    # noqa: E501
@@ -704,10 +704,10 @@ class TestParams:
             Then:
                 - IOC in demisto format.
         """
-        mocker.patch.object(demisto, 'searchIndicators', return_value={})
+        mocker.patch.object(demisto, 'searchIndicators', return_value={"total": 0})
         mocker.patch.object(demisto, 'params', return_value=param_value)
         mocker.patch.object(demisto, 'getIntegrationContext', return_value={'ts': 1591142400000})
-        mocker.patch.object(demisto, 'searchIndicators', return_value={})
+        mocker.patch.object(demisto, 'searchIndicators', return_value={"total": 0})
         outputs = mocker.patch.object(demisto, 'createIndicators')
         Client.tag = demisto.params().get('feedTags', demisto.params().get('tag', Client.tag))
         Client.tlp_color = demisto.params().get('tlp_color')
