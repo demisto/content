c025e1ad2019a2fd5904116cda810b18bba75fc8
jwilkes <jwilkes@paloaltonetworks.com>
2024-01-03 12:08:12 -0500

    updates

--- Packs/AWS-Enrichment-Remediation/Scripts/AWSAccountHierarchy/AWSAccountHierarchy.py
+++ Packs/AWS-Enrichment-Remediation/Scripts/AWSAccountHierarchy/AWSAccountHierarchy.py
@@ -4,7 +4,7 @@ from typing import Dict, Any
 import traceback
 
 
-''' STANDALONE FUNCTION '''
+""" STANDALONE FUNCTION """
 
 
 def lookup(parent_obj: str, level: int, instance_to_use: str) -> tuple[str, dict]:
@@ -24,37 +24,44 @@ def lookup(parent_obj: str, level: int, instance_to_use: str) -> tuple[str, dict
     temp: Dict[str, str] = {}
     try:
         if "ou-" in parent_obj:
-            ou_info = execute_command("aws-org-organization-unit-get",
-                                      {"organization_unit_id": parent_obj, "using": instance_to_use})
+            ou_info = execute_command(
+                "aws-org-organization-unit-get",
+                {"organization_unit_id": parent_obj, "using": instance_to_use},
+            )
             if not ou_info:
                 return "NONE", temp
             temp["level"] = str(level)
-            temp["id"] = ou_info.get('Id', '')
-            temp["name"] = ou_info.get('Name', '')
-            temp["arn"] = ou_info.get('Arn', '')
-            ou_parent = execute_command("aws-org-parent-list", {"child_id": parent_obj, "using": instance_to_use})
+            temp["id"] = ou_info.get("Id", "")
+            temp["name"] = ou_info.get("Name", "")
+            temp["arn"] = ou_info.get("Arn", "")
+            ou_parent = execute_command(
+                "aws-org-parent-list",
+                {"child_id": parent_obj, "using": instance_to_use},
+            )
             if not ou_parent:
                 return "NONE", temp
-            next_one = ou_parent[0].get('Id', '')
+            next_one = ou_parent[0].get("Id", "")
         elif "r-" in parent_obj:
             next_one = "stop"
             root_info = execute_command("aws-org-root-list", {"using": instance_to_use})
             if not root_info:
                 return "NONE", temp
-            root = root_info.get('AWS.Organizations.Root(val.Id && val.Id == obj.Id)')[0]
+            root = root_info.get("AWS.Organizations.Root(val.Id && val.Id == obj.Id)")[
+                0
+            ]
             temp["level"] = str(level)
-            temp["id"] = root.get('Id', '')
-            temp["name"] = root.get('Name', '')
-            temp["arn"] = root.get('Arn', '')
+            temp["id"] = root.get("Id", "")
+            temp["name"] = root.get("Name", "")
+            temp["arn"] = root.get("Arn", "")
         else:
-            raise ValueError('unexpected object type')
+            raise ValueError("unexpected object type")
     except TypeError:
         return "NONE", temp
     else:
         return next_one, temp
 
 
-''' COMMAND FUNCTION '''
+""" COMMAND FUNCTION """
 
 
 def aws_account_heirarchy(args: Dict[str, Any]) -> CommandResults:
@@ -68,46 +75,65 @@ def aws_account_heirarchy(args: Dict[str, Any]) -> CommandResults:
 
     """
 
-    account_id = args.get('account_id')
+    account_id = args.get("account_id")
 
     if not account_id:
-        raise ValueError('account_id not specified')
-    # Using `demisto.executeCommand` instead of `execute_command` because for multiple integration instances we can expect one too error out.
-    account_info = demisto.executeCommand("aws-org-account-list", {"account_id": account_id})
-    account_returned = [account for account in account_info if (not isError(account) and account.get('Contents').get('Arn'))]
+        raise ValueError("account_id not specified")
+    # Using `demisto.executeCommand` instead of `execute_command` because for
+    # multiple integration instances we can expect one too error out.
+    account_info = demisto.executeCommand(
+        "aws-org-account-list", {"account_id": account_id}
+    )
+    account_returned = [
+        account
+        for account in account_info
+        if (not isError(account) and account.get("Contents").get("Arn"))
+    ]
     if not account_returned:
-        return CommandResults(readable_output = "could not find specified account info")
+        return CommandResults(readable_output="could not find specified account info")
     else:
-        match_account = account_returned[0].get('Contents')
-        instance_to_use = account_returned[0]['Metadata']['instance']
+        match_account = account_returned[0].get("Contents")
+        instance_to_use = account_returned[0]["Metadata"]["instance"]
     level = 1
-    hierarchy = [{"level": "account", "id": match_account.get(
-        'Id', ''), "name": match_account.get('Name', ''), "arn": match_account.get('Arn', '')}]
-    account_parent = demisto.executeCommand("aws-org-parent-list", {"child_id": account_id, "using": instance_to_use})
+    hierarchy = [
+        {
+            "level": "account",
+            "id": match_account.get("Id", ""),
+            "name": match_account.get("Name", ""),
+            "arn": match_account.get("Arn", ""),
+        }
+    ]
+    account_parent = demisto.executeCommand(
+        "aws-org-parent-list", {"child_id": account_id, "using": instance_to_use}
+    )
     if isError(account_parent):
-        return CommandResults(readable_output =  "could not find specified account parent")
-    next_one, to_append = lookup(account_parent[0].get('Contents', {}).get('Id', ''), level, instance_to_use)
+        return CommandResults(readable_output="could not find specified account parent")
+    next_one, to_append = lookup(
+        account_parent[0].get("Contents", {})[0].get("Id", ""), level, instance_to_use
+    )
     if next_one == "NONE":
-        return CommandResults(readable_output = "could not find specified ou/root info")
+        return CommandResults(readable_output="could not find specified ou/root info")
     hierarchy.append(to_append)
     try:
-        while 'stop' not in next_one:
+        while "stop" not in next_one:
             level += 1
             next_one, to_append = lookup(next_one, level, instance_to_use)
             if next_one == "NONE" or next_one is None:
-                return CommandResults(readable_output = "could not find specified ou/root info")
+                return CommandResults(
+                    readable_output="could not find specified ou/root info"
+                )
             hierarchy.append(to_append)
     except TypeError:
-        return CommandResults(readable_output = "could not find specified ou/root info")
+        return CommandResults(readable_output="could not find specified ou/root info")
 
     return CommandResults(
-        outputs_prefix='AWSHierarchy',
-        outputs_key_field='level',
+        outputs_prefix="AWSHierarchy",
+        outputs_key_field="level",
         outputs=hierarchy,
     )
 
 
-''' MAIN FUNCTION '''
+""" MAIN FUNCTION """
 
 
 def main():
@@ -115,11 +141,11 @@ def main():
         return_results(aws_account_heirarchy(demisto.args()))
     except Exception as ex:
         demisto.error(traceback.format_exc())  # print the traceback
-        return_error(f'Failed to execute AWSAccountHierarchy. Error: {str(ex)}')
+        return_error(f"Failed to execute AWSAccountHierarchy. Error: {str(ex)}")
 
 
-''' ENTRY POINT '''
+""" ENTRY POINT """
 
 
-if __name__ in ('__main__', '__builtin__', 'builtins'):
+if __name__ in ("__main__", "__builtin__", "builtins"):
     main()
--- Packs/AWS-Enrichment-Remediation/Scripts/AWSAccountHierarchy/AWSAccountHierarchy_test.py
+++ Packs/AWS-Enrichment-Remediation/Scripts/AWSAccountHierarchy/AWSAccountHierarchy_test.py
@@ -1,78 +1,137 @@
 import demistomock as demisto  # noqa: F401
-import pytest
 from CommonServerPython import CommandResults
 
 
 def test_lookup_func(mocker):
     """Tests lookup helper function.
 
-        Given:
-            - Mocked arguments
-        When:
-            - Sending args to lookup helper function.
-        Then:
-            - Checks the output of the helper function with the expected output.
+    Given:
+        - Mocked arguments
+    When:
+        - Sending args to lookup helper function.
+    Then:
+        - Checks the output of the helper function with the expected output.
     """
     from AWSAccountHierarchy import lookup
-    folder_lookup = [{'Type': 1, 'Contents': {'AWS.Organizations.Root(val.Id && val.Id == obj.Id)': [{'Arn': 'arn:aws:organizations::111111111111:root/o-2222222222/r-3333', 'Id': 'r-3333', 'Name': 'Root'}]}}]
+
+    folder_lookup = [
+        {
+            "Type": 1,
+            "Contents": {
+                "AWS.Organizations.Root(val.Id && val.Id == obj.Id)": [
+                    {
+                        "Arn": "arn:aws:organizations::111111111111:root/o-2222222222/r-3333",
+                        "Id": "r-3333",
+                        "Name": "Root",
+                    }
+                ]
+            },
+        }
+    ]
 
     mocker.patch.object(demisto, "executeCommand", return_value=folder_lookup)
     args = {"parent_obj": "r-3333", "level": 2, "instance_to_use": "fake-instance-name"}
     result = lookup(**args)
-    assert result == ('stop', {'id': 'r-3333', 'level': '2', "name": "Root", 'arn': 'arn:aws:organizations::111111111111:root/o-2222222222/r-3333'})
+    assert result == (
+        "stop",
+        {
+            "id": "r-3333",
+            "level": "2",
+            "name": "Root",
+            "arn": "arn:aws:organizations::111111111111:root/o-2222222222/r-3333",
+        },
+    )
 
 
 def test_aws_account_heirarchy_command(mocker):
     """Tests aws_account_heirarchy function.
 
-        Given:
-            - Mocked arguments
-        When:
-            - Sending args to aws_account_heirarchy function.
-        Then:
-            - Checks the output of the function with the expected output.
+    Given:
+        - Mocked arguments
+    When:
+        - Sending args to aws_account_heirarchy function.
+    Then:
+        - Checks the output of the function with the expected output.
     """
     from AWSAccountHierarchy import aws_account_heirarchy
 
     def executeCommand(name, args):
         if name == "aws-org-account-list":
-            return [{'Type': 1, 'Contents': {'Name': "master-account", 'Id': '111111111111', 'Arn': "arn:aws:organizations::111111111111:root/o-2222222222/111111111111"}, 'Metadata': {'instance': 'fake-instance-name'}}]
+            return [
+                {
+                    "Type": 1,
+                    "Contents": {
+                        "Name": "master-account",
+                        "Id": "111111111111",
+                        "Arn": "arn:aws:organizations::111111111111:root/o-2222222222/111111111111",
+                    },
+                    "Metadata": {"instance": "fake-instance-name"},
+                }
+            ]
         elif name == "aws-org-parent-list":
-            return [{'Type': 1, 'Contents': {'Id': 'r-3333'}}]
+            return [{"Type": 1, "Contents": [{"Id": "r-3333"}]}]
         elif name == "aws-org-root-list":
-            return [{'Type': 1, 'Contents': {'AWS.Organizations.Root(val.Id && val.Id == obj.Id)': [{'Arn': 'arn:aws:organizations::111111111111:root/o-2222222222/r-3333', 'Id': 'r-3333', 'Name': 'Root'}]}}]
+            return [
+                {
+                    "Type": 1,
+                    "Contents": {
+                        "AWS.Organizations.Root(val.Id && val.Id == obj.Id)": [
+                            {
+                                "Arn": "arn:aws:organizations::111111111111:root/o-2222222222/r-3333",
+                                "Id": "r-3333",
+                                "Name": "Root",
+                            }
+                        ]
+                    },
+                }
+            ]
 
     mocker.patch.object(demisto, "executeCommand", side_effect=executeCommand)
     args = {"account_id": "111111111111"}
     result = aws_account_heirarchy(args)
-    expected_hierachy = [{'id': '111111111111', 'level': 'account', 'name': 'master-account', 'arn': "arn:aws:organizations::111111111111:root/o-2222222222/111111111111"}, {'id': 'r-3333', 'level': '1', 'name': 'Root', 'arn': 'arn:aws:organizations::111111111111:root/o-2222222222/r-3333'}]
-    expected_result = CommandResults(outputs_prefix='AWSHierarchy',
-                                     outputs_key_field='level',
-                                     outputs=expected_hierachy)
+    expected_hierachy = [
+        {
+            "id": "111111111111",
+            "level": "account",
+            "name": "master-account",
+            "arn": "arn:aws:organizations::111111111111:root/o-2222222222/111111111111",
+        },
+        {
+            "id": "r-3333",
+            "level": "1",
+            "name": "Root",
+            "arn": "arn:aws:organizations::111111111111:root/o-2222222222/r-3333",
+        },
+    ]
+    expected_result = CommandResults(
+        outputs_prefix="AWSHierarchy",
+        outputs_key_field="level",
+        outputs=expected_hierachy,
+    )
     assert result.outputs == expected_result.outputs
-    #assert result.readable_output == 2
 
 
 def test_aws_account_heirarchy_command_no_account(mocker):
     """Tests aws_account_heirarchy function.
 
-        Given:
-            - Null mocked arguments
-        When:
-            - Sending args to aws_account_heirarchy function.
-        Then:
-            - Checks the output of the function with the expected output.
+    Given:
+        - Null mocked arguments
+    When:
+        - Sending args to aws_account_heirarchy function.
+    Then:
+        - Checks the output of the function with the expected output.
     """
     from AWSAccountHierarchy import aws_account_heirarchy
 
     def executeCommand(name, args):
         if name == "aws-org-account-list":
-            return [{'Type': 4, 'Contents': {'bad command'}}]
+            return [{"Type": 4, "Contents": {"bad command"}}]
 
     mocker.patch.object(demisto, "executeCommand", side_effect=executeCommand)
     args = {"account_id": "111111111111"}
     result = aws_account_heirarchy(args)
-    expected_result = CommandResults(readable_output = "could not find specified account info")
+    expected_result = CommandResults(
+        readable_output="could not find specified account info"
+    )
 
     assert result.readable_output == expected_result.readable_output
-
