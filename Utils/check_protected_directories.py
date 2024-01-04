99f9a1b54ffd25de670bfc1e3a91caada4f88f75
jwilkes <jwilkes@paloaltonetworks.com>
2024-01-04 08:05:19 -0500

    undo check_protected change

--- Utils/check_protected_directories.py
+++ Utils/check_protected_directories.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 CONTENT_ROOT = Path(__file__).parents[1]
-assert CONTENT_ROOT.name == "content", f"Excpected content root dir to be named `content`, got ({CONTENT_ROOT.name}). Full path: {CONTENT_ROOT}"
+assert CONTENT_ROOT.name == "content"
 
 PROTECTED_DIRECTORY_PATHS: set[Path] = {
     Path(CONTENT_ROOT, dir_name)
