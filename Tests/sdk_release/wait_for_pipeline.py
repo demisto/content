934d141a4d582063d02c80d34fec9b2adf61bb51
adi88d <adaud@paloaltonetworks.com>
2024-01-08 14:59:22 +0200

    ad wait_for_pipeline.py

---
+++ Tests/sdk_release/wait_for_pipeline.py
@@ -0,0 +1,85 @@
+import argparse
+import json
+from Tests.scripts.utils import logging_wrapper as logging
+import sys
+import time
+import requests
+from Tests.scripts.utils.log_util import install_logging
+
+
+GITLAB_SERVER_URL = 'https://gitlab.xdr.pan.local'
+TIMEOUT = 60 * 60 * 6  # 6 hours - TODO - Decrease after replacing id-set with graph
+
+
+def get_pipeline_status(pipeline_id, project_id, token):
+    url = f'{GITLAB_SERVER_URL}/api/v4/projects/{project_id}/pipelines/{pipeline_id}/jobs'
+    res = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    if res.status_code != 200:
+        logging.error(f'Failed to get status of pipeline {pipeline_id}, request failed with error: {str(res.content)}')
+        sys.exit(1)
+
+    try:
+        jobs_info = json.loads(res.content)
+        pipeline_status = jobs_info[0].get('pipeline', {}).get('status')
+
+    except Exception as e:
+        logging.error(f'Unable to parse pipeline status response: {e}')
+        sys.exit(1)
+
+    return pipeline_status
+
+
+def get_pipeline_info(pipeline_id, project_id, token):
+    url = f'{GITLAB_SERVER_URL}/api/v4/projects/{project_id}/pipelines/{pipeline_id}'
+    res = requests.get(url, headers={'Authorization': f'Bearer {token}'})
+    if res.status_code != 200:
+        logging.error(f'Failed to get status of pipeline {pipeline_id}, request failed with error: {str(res.content)}')
+        sys.exit(1)
+
+    try:
+        pipeline_info = json.loads(res.content)
+    except Exception as e:
+        logging.error(f'Unable to parse pipeline status response: {e}')
+        sys.exit(1)
+
+    return pipeline_info
+
+
+def main():
+    install_logging('wait_for_pipeline.log', logger=logging)
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-g', '--gitlab-api-token', help='Gitlab api token')
+    arg_parser.add_argument('-p', '--pipeline-id', help='Pipeline id')
+    arg_parser.add_argument('-pid', '--project-id', help='Project id')
+
+    args = arg_parser.parse_args()
+
+    token = args.gitlab_api_token
+    pipeline_id = args.pipeline_id
+    project_id = args.project_id
+
+    pipeline_status = 'running'  # pipeline status when start to run
+
+    # initialize timer
+    start = time.time()
+    elapsed: float = 0
+
+    while pipeline_status not in ['failed', 'success', 'canceled'] and elapsed < TIMEOUT:
+        logging.info(f'Pipeline {pipeline_id} status is {pipeline_status}')
+        time.sleep(300)
+        pipeline_status = get_pipeline_status(pipeline_id, project_id, token)
+
+        elapsed = time.time() - start
+
+    if elapsed >= TIMEOUT:
+        logging.critical(f'Timeout reached while waiting for the pipeline to complete, pipeline number: {pipeline_id}')
+        sys.exit(1)
+
+    pipeline_url = get_pipeline_info(pipeline_id, project_id, token).get('web_url')
+    print(pipeline_url)
+    logging.info(f'The pipeline has finished. See pipeline here: {pipeline_url}')
+
+
+if __name__ == "__main__":
+    main()
