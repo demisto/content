2dcd6763cb7cfd97a7eea32447988fb8fbd77ee5
arad carmi <acarmi@paloaltonetworks.com>
2024-01-08 11:57:24 +0200

    Test New Changes

--- Utils/check_protected_directories.py
+++ Utils/check_protected_directories.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 CONTENT_ROOT = Path(__file__).parents[1]
-assert CONTENT_ROOT.name == "content"
+assert CONTENT_ROOT.name == "content" or (os.getenv("CIRCLECI") and CONTENT_ROOT.name == "Project")
 
 PROTECTED_DIRECTORY_PATHS: set[Path] = {
     Path(CONTENT_ROOT, dir_name)
