4a8efe268c375b52c4eb4496e2f5d2f4460ff455
manas-metron <manas@metronlabs.com>
2024-01-10 12:37:58 +0530

    fix : Removed redundant code block.

--- Packs/Devo/Integrations/Devo_v2/Devo_v2.py
+++ Packs/Devo/Integrations/Devo_v2/Devo_v2.py
@@ -534,6 +534,9 @@ def filter_results_by_fields(results, fields_to_view_string):
     Raises:
         ValueError: If the fields_to_view_string contains invalid column names.
     """
+    if fields_to_view_string == "":
+        raise ValueError("fields_to_view cannot be an empty.")
+
     if not fields_to_view_string:
         return results
 
@@ -552,8 +555,7 @@ def filter_results_by_fields(results, fields_to_view_string):
 
     for result in results:
         filtered_result = {
-            column: tuple(result.get(column)) if isinstance(result.get(column), list) else result.get(column)
-            for column in fields_to_view_list
+            column: result.get(column) for column in fields_to_view_list
         }
         filtered_results.append(filtered_result)
 
@@ -570,8 +572,6 @@ def run_query_command(offset, items):
     time_range = get_time_range(timestamp_from, timestamp_to)
     to_query = f"{to_query} offset {offset} limit {items}"
     fields_to_view = demisto.args().get("fields_to_view", None)
-    if fields_to_view == "":
-        raise ValueError("fields_to_view cannot be an empty string!")
     results = list(
         ds.Reader(
             oauth_token=READER_OAUTH_TOKEN,
@@ -597,8 +597,7 @@ def run_query_command(offset, items):
         )
     }
 
-    if fields_to_view:
-        results = filter_results_by_fields(results, fields_to_view)
+    results = filter_results_by_fields(results, fields_to_view)
 
     entry = {
         "Type": entryTypes["note"],
@@ -645,8 +644,6 @@ def get_alerts_command(offset, items):
     user_alert_table = demisto.args().get("table_name", None)
     user_prefix = demisto.args().get("prefix", "")
     fields_to_view = demisto.args().get("fields_to_view", None)
-    if fields_to_view == "":
-        raise ValueError("fields_to_view cannot be an empty string!")
     user_alert_table = user_alert_table if user_alert_table else DEFAULT_ALERT_TABLE
     if user_prefix:
         user_prefix = f"{user_prefix}_"
@@ -711,8 +708,7 @@ def get_alerts_command(offset, items):
         for ed in res[extra_data]:
             res[extra_data][ed] = urllib.parse.unquote_plus(res[extra_data][ed])
 
-    if fields_to_view:
-        results = filter_results_by_fields(results, fields_to_view)
+    results = filter_results_by_fields(results, fields_to_view)
 
     entry = {
         "Type": entryTypes["note"],
@@ -760,8 +756,6 @@ def multi_table_query_command(offset, items):
     write_context = demisto.args()["writeToContext"].lower()
     query_timeout = int(demisto.args().get("queryTimeout", TIMEOUT))
     fields_to_view = demisto.args().get("fields_to_view", None)
-    if fields_to_view == "":
-        raise ValueError("fields_to_view cannot be an empty string!")
     global COUNT_MULTI_TABLE
     time_range = get_time_range(timestamp_from, timestamp_to)
 
@@ -805,8 +799,7 @@ def multi_table_query_command(offset, items):
 
     concurrent.futures.wait(futures)
 
-    if fields_to_view:
-        all_results = filter_results_by_fields(all_results, fields_to_view)
+    all_results = filter_results_by_fields(all_results, fields_to_view)
 
     entry = {
         "Type": entryTypes["note"],
