cb11860b8fad806408e0fddfe4ec5dbe905ad9cc
Shmuel Kroizer <skroizer@paloaltonetworks.com>
2024-01-11 14:02:02 +0200

    revert the last commit

--- Packs/AWS-Enrichment-Remediation/Scripts/AWSRecreateSG/test_data/sample.py
+++ Packs/AWS-Enrichment-Remediation/Scripts/AWSRecreateSG/test_data/sample.py
@@ -1,9 +1,8 @@
-{
-    "SG_INFO": [
-        {
-            "Type": 1,
-            "Contents": {
-                "AWS.EC2.SecurityGroups(val.GroupId === obj.GroupId)": [
+SG_INFO = [
+    {
+        "Type": 1,
+        "Contents": {
+            "AWS.EC2.SecurityGroups(val.GroupId === obj.GroupId)": [
                     {
                         "Description": "sldkjdlskfjs",
                         "GroupId": "sg-00000000000000001",
@@ -55,62 +54,62 @@
                         "Region": "us-east-1",
                         "VpcId": "vpc-061c242911e464170"
                     }
-                ]
-            },
-            "HumanReadable": "### AWS EC2 SecurityGroups\n|Description|GroupId|GroupName|OwnerId|Region|VpcId|\n|---|---|---|---|---|---|\n| sldkjdlskfjs | sg-0408c2745d3d13b15 | demo-sg | 717007404259 | us-east-1 | vpc-061c242911e464170 |\n",
-            "ImportantEntryContext": "None",
-            "EntryContext": {
-                "AWS.EC2.SecurityGroups(val.GroupId === obj.GroupId)": [
-                    {
-                        "Description": "sldkjdlskfjs",
-                        "GroupId": "sg-0408c2745d3d13b15",
-                        "GroupName": "demo-sg",
-                        "IpPermissions": [
-                            {
-                                "FromPort": 0,
-                                "IpProtocol": "tcp",
-                                "IpRanges": [
-                                    {
-                                        "CidrIp": "0.0.0.0/0"
-                                    }
-                                ],
-                                "Ipv6Ranges": "None",
-                                "PrefixListIds": "None",
-                                "ToPort": 65535,
-                                "UserIdGroupPairs": "None"
-                            }
-                        ],
-                        "IpPermissionsEgress": [
-                            {
-                                "FromPort": 0,
-                                "IpProtocol": "tcp",
-                                "IpRanges": [
-                                    {
-                                        "CidrIp": "0.0.0.0/0"
-                                    }
-                                ],
-                                "Ipv6Ranges": "None",
-                                "PrefixListIds": "None",
-                                "ToPort": 65535,
-                                "UserIdGroupPairs": "None"
-                            }
-                        ],
-                        "OwnerId": "717007404259",
-                        "Region": "us-east-1",
-                        "VpcId": "vpc-061c242911e464170"
-                    }
-                ]
-            }
+            ]
+        },
+        "HumanReadable": "### AWS EC2 SecurityGroups\n|Description|GroupId|GroupName|OwnerId|Region|VpcId|\n|---|---|---|---|---|---|\n| sldkjdlskfjs | sg-0408c2745d3d13b15 | demo-sg | 717007404259 | us-east-1 | vpc-061c242911e464170 |\n",
+        "ImportantEntryContext": "None",
+        "EntryContext": {
+            "AWS.EC2.SecurityGroups(val.GroupId === obj.GroupId)": [
+                {
+                    "Description": "sldkjdlskfjs",
+                    "GroupId": "sg-0408c2745d3d13b15",
+                    "GroupName": "demo-sg",
+                    "IpPermissions": [
+                        {
+                            "FromPort": 0,
+                            "IpProtocol": "tcp",
+                            "IpRanges": [
+                                {
+                                    "CidrIp": "0.0.0.0/0"
+                                }
+                            ],
+                            "Ipv6Ranges": "None",
+                            "PrefixListIds": "None",
+                            "ToPort": 65535,
+                            "UserIdGroupPairs": "None"
+                        }
+                    ],
+                    "IpPermissionsEgress": [
+                        {
+                            "FromPort": 0,
+                            "IpProtocol": "tcp",
+                            "IpRanges": [
+                                {
+                                    "CidrIp": "0.0.0.0/0"
+                                }
+                            ],
+                            "Ipv6Ranges": "None",
+                            "PrefixListIds": "None",
+                            "ToPort": 65535,
+                            "UserIdGroupPairs": "None"
+                        }
+                    ],
+                    "OwnerId": "717007404259",
+                    "Region": "us-east-1",
+                    "VpcId": "vpc-061c242911e464170"
+                }
+            ]
         }
-    ],
-    "INSTANCE_INFO": [
-        {
-            "Metadata": {
-                "instance": "AWS - EC2"
-            },
-            "Type": 1,
-            "Contents": {
-                "AWS.EC2.Instances(val.InstanceId === obj.InstanceId)": [
+    }
+]
+INSTANCE_INFO = [
+    {
+        "Metadata": {
+            "instance": "AWS - EC2"
+        },
+        "Type": 1,
+        "Contents": {
+            "AWS.EC2.Instances(val.InstanceId === obj.InstanceId)": [
                     {
                         "NetworkInterfaces": [
                             {
@@ -127,8 +126,7 @@
                             }
                         ]
                     }
-                ]
-            }
+            ]
         }
-    ]
-}
+    }
+]
