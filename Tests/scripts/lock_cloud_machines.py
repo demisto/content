7dcdb9e4417ad3cd938e91e60c015f78302a9db8
michal-dagan <mdagan@paloaltonetworks.com>
2024-01-15 11:13:27 +0200

    fixed

--- Tests/scripts/lock_cloud_machines.py
+++ Tests/scripts/lock_cloud_machines.py
@@ -181,7 +181,7 @@ def adding_build_to_the_queue(storage_bucket: Any, lock_repository_name: str, jo
     blob.upload_from_string('')
 
 
-def get_my_place_in_the_queue(storage_client: storage.Client, gcs_locks_path: str, job_id: str, my_places: list) \
+def get_my_place_in_the_queue(storage_client: storage.Client, gcs_locks_path: str, job_id: str) \
         -> tuple[int, str]:
     """
     get the place in the queue for job-id by the time-created of lock-file time-created.
@@ -189,7 +189,6 @@ def get_my_place_in_the_queue(storage_client: storage.Client, gcs_locks_path: st
         storage_client(storage.Client): The GCP storage client.
         gcs_locks_path(str): the lock repository name.
         job_id(str): the job id to check.
-        my_places(list): saves the job places in the queue
 
     Returns: the place in the queue.
 
@@ -208,13 +207,10 @@ def get_my_place_in_the_queue(storage_client: storage.Client, gcs_locks_path: st
         previous_build_in_queue = sorted_builds_in_queue[my_place_in_the_queue - 1].get('name')  # type: ignore[assignment]
 
     try:
-        if my_place_in_the_queue != my_places[-1]:
-            send_slack_notification([f"{gcs_locks_path}",
-                                     f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}",
-                                     f"Job ID: {job_id}",
-                                     f"{len(builds_in_queue)}",
-                                     f"{my_place_in_the_queue}"])
-        my_places.append(my_place_in_the_queue)
+        send_slack_notification([f"{gcs_locks_path}",
+                                 f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}",
+                                 f"Job ID: {job_id}",
+                                 f"{len(builds_in_queue)}"])
     except Exception as e:
         logging.info(f"Failed to send Slack notification. Reason: {str(e)}")
 
@@ -362,9 +358,8 @@ def wait_for_build_to_be_first_in_queue(storage_client: storage.Client,
         gitlab_status_token(str): the gitlab token.
     """
     sleep(random.randint(1, 3))
-    my_places = [None]
     while True:
-        my_place_in_the_queue, previous_build = get_my_place_in_the_queue(storage_client, gcs_locks_path, job_id, my_places)
+        my_place_in_the_queue, previous_build = get_my_place_in_the_queue(storage_client, gcs_locks_path, job_id)
         logging.info(f'My place in the queue is: {my_place_in_the_queue}')
 
         if my_place_in_the_queue == 0:
@@ -415,8 +410,8 @@ def main():
                                  f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%f')}",
                                  f"{options.gcs_locks_path}",
                                  f"Job ID: {options.ci_job_id}",
-                                 f"Duration:",
-                                 f"{duration}"])
+                                 "Duration:",
+                                 f"{duration_sec}"])
     except Exception as e:
         logging.info(f"Failed to send Slack notification. Reason: {str(e)}")
 
