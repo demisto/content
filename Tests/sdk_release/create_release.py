93f1f56f41c69eae9194c5747518c3249aff0171
adi88d <adaud@paloaltonetworks.com>
2024-01-17 11:47:57 +0200

    update create_release.py

--- Tests/sdk_release/create_release.py
+++ Tests/sdk_release/create_release.py
@@ -2,6 +2,11 @@ import requests
 import json
 import sys
 import argparse
+import re
+
+
+REGEX = re.compile(r"^(.*) \[(#\d+)\]\((http.*)\)")
+
 
 
 def options_handler():
@@ -9,7 +14,6 @@ def options_handler():
 
     parser.add_argument('-t', '--access_token', help='Github access token', required=True)
     parser.add_argument('-b', '--release_branch_name', help='The name of the release branch', required=True)
-    parser.add_argument('-t', '--release_text', help='Text describing the contents of the release', required=True)
 
     options = parser.parse_args()
     return options
@@ -17,9 +21,35 @@ def options_handler():
 
 def main():
     options = options_handler()
-    release_text = options.release_text
     release_branch_name = options.release_branch_name
     access_token = options.access_token
+
+    # get release changelog
+    url = f"https://raw.githubusercontent.com/demisto/demisto-sdk/{release_branch_name}/CHANGELOG.md"
+    response = requests.request("GET", url, verify=False)
+    if response.status_code != 200:
+        print(f'Failed to get the CHANGELOG.md file from branch {release_branch_name}')
+        print(response.text)
+        sys.exit(1)
+    file_text = response.text
+    release_changes = (
+        file_text.split(f"## {release_branch_name}\n")[1].split("\n\n")[0].split("\n")
+    )
+
+    # Converting release changes to markdown links
+    releases = []
+    for change in release_changes:
+        try:
+            # Ignoring the mypy error because the regex must match
+            change_parts = REGEX.match(change).groups()  # type: ignore[union-attr]
+            releases.append(
+                f"{change_parts[0]} <{change_parts[2]}|{change_parts[1]}>"
+            )
+        except Exception as e:
+            print(f'Error parsing change: {e}')
+            exit(1)
+
+    release_text = "\n".join(releases)
 
     print(f"Preparing to release Demisto SDK version {release_branch_name}")
 
