673ce7bfbf3bc95a675f890e1c74ad643a34864e
GuyAfik <guyafik11@gmail.com>
2024-01-17 12:55:04 +0200

    create test infra

--- Packs/DigitalShadows/Integrations/ReliaQuestGreyMatterDRPEventCollector/ReliaQuestGreyMatterDRPEventCollector_test.py
+++ Packs/DigitalShadows/Integrations/ReliaQuestGreyMatterDRPEventCollector/ReliaQuestGreyMatterDRPEventCollector_test.py
@@ -23,12 +23,12 @@ def util_load_json(path):
         return json.loads(f.read())
 
 
-def create_triage_items_events(num_of_events: int, start_time: str):
+def create_triage_items_events(num_of_events: int, start_time: str, offset: int = 3):
     start_datetime = dateparser.parse(start_time)
     events = []
 
     for event_num in range(1, num_of_events + 1):
-        seconds_offset = (event_num - 1) // 3
+        seconds_offset = (event_num - 1) // offset
         event_created = start_datetime + timedelta(seconds=seconds_offset)
         event_created_str = event_created.strftime(DATE_FORMAT)
 
@@ -42,6 +42,39 @@ def create_triage_items_events(num_of_events: int, start_time: str):
     return events
 
 
+def create_triage_items_from_events(events: List[Dict], item_type: str):
+    if item_type not in {"incident-id", "alert-d"}:
+        raise ValueError(f'item-type {item_type} must be one of incident-id/alert-id')
+    triaged_items = []
+    for item_type_id, event in enumerate(events):
+        triaged_items.append(
+            {
+                "id": event.get("triage-item-id"),
+                "title": "title",
+                "source": {
+                    item_type: item_type_id
+                }
+            }
+        )
+    return triaged_items
+
+
+def create_incidents_and_alerts_from_triaged_items(triaged_alert_ids: List[Dict], item_type: str, amount_of_assets: int = 0):
+    if item_type not in {"incident-id", "alert-d"}:
+        raise ValueError(f'item-type {item_type} must be one of incident-id/alert-id')
+    alerts = []
+    for triaged_alert in triaged_alert_ids:
+        _id = triaged_alert.get("source", {}).get(item_type)
+        alerts.append(
+            {
+                "id": _id,
+                "title": f'{item_type}-{_id}',
+                "assets": [{"id": i} for i in amount_of_assets]
+            }
+        )
+    return alerts
+
+
 def test_the_test_module(requests_mock, client: ReilaQuestClient):
     from ReliaQuestGreyMatterDRPEventCollector import test_module
     requests_mock.get(
@@ -49,3 +82,13 @@ def test_the_test_module(requests_mock, client: ReilaQuestClient):
         json=create_triage_items_events(num_of_events=1, start_time="2020-09-24T16:30:10.016Z")
     )
     assert test_module(client) == "ok"
+
+
+class TestFetchEvents:
+
+    def test_fetch_events_no_last_run_single_iteration(self, client: ReilaQuestClient, mocker, requests_mock):
+        from ReliaQuestGreyMatterDRPEventCollector import fetch_events
+        events = create_triage_items_events(100, start_time="020-09-24T16:30:10.016Z")
+        triaged_alerts = create_triage_items_from_events(events[0:50], item_type="alert-id")
+        triaged_incidents = create_triage_items_from_events(events[50:100], item_type="incident-id")
+        mocker.patch.object(client, "_http_request", side_effect=[events, triaged_alerts, triaged_incidents])
