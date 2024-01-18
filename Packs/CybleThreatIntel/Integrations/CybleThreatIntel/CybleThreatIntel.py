0bd25f839a99aed0321e89b55b549cafb1f1c75f
cyble-dev <developers@cyble.com>
2024-01-18 14:47:22 +0530

    save_fetch_time logic update

--- Packs/CybleThreatIntel/Integrations/CybleThreatIntel/CybleThreatIntel.py
+++ Packs/CybleThreatIntel/Integrations/CybleThreatIntel/CybleThreatIntel.py
@@ -142,17 +142,21 @@ class Client(object):
         else:
             return {}
 
-    def get_taxii(self, args: Dict[str, Any]):
+    def get_taxii(self, args: Dict[str, Any], is_first_fetch: [bool] = True, limit: [int] = 10):
         """
         Fetch Taxii events for the given parameters
         :param args: arguments which would be used to fetch feed
+        :param is_first_fetch: indicates whether this is the first run or a subsequent run
+        :param limit: number of indicators to pull in one request
         :return:
         """
         taxii_data = []
-        save_fetch_time = None
+        save_fetch_time = args.get('begin')
         count = 0
+
         try:
             for data in self.fetch(args.get('begin'), args.get('end'), args.get('collection')):
+                skip = False
                 response = self.parse_to_json(data)
 
                 if response.get('indicators') or False:
@@ -164,14 +168,19 @@ class Client(object):
 
                 for eachone in content:
                     if eachone.get('confidence'):
-                        save_fetch_time = (parser.parse(eachone['confidence']['timestamp']) + timedelta(microseconds=1)).replace(tzinfo=pytz.UTC).strftime(
-                            DATETIME_FORMAT)
+                        current_timestamp = parser.parse(eachone['confidence']['timestamp']).replace(tzinfo=pytz.UTC).strftime(DATETIME_FORMAT)
+                        if is_first_fetch or current_timestamp > save_fetch_time:
+                            save_fetch_time = current_timestamp
+                        else:
+                            skip = True
 
-                taxii_data.append(response)
+                if not skip:
+                    taxii_data.append(response)
 
-                count += 1
-                if count >= 100:
-                    break
+                    count += 1
+                    if count >= limit:
+                        break
+
         except Exception as e:
             demisto.error("Failed to fetch feed details, exception:{}".format(e))
             raise e
@@ -276,20 +285,24 @@ def fetch_indicators(client: Client):
     '''
     args = {}
     last_run = demisto.getLastRun()
+    is_first_fetch = True
+    limit = 10
     if isinstance(last_run, dict):
         last_fetch_time = last_run.get('lastRun_{}'.format(client.collection_name), None)
 
     if last_fetch_time:
         args['begin'] = str(parser.parse(last_fetch_time).replace(tzinfo=pytz.UTC))
+        is_first_fetch = False
     else:
         last_fetch_time = datetime.utcnow() - timedelta(days=client.initial_interval)      # type: ignore
         args['begin'] = str(last_fetch_time.replace(tzinfo=pytz.UTC))
+        limit = 100
 
     args['end'] = str(datetime.utcnow().replace(tzinfo=pytz.UTC))
 
     args['collection'] = client.collection_name
     args['limit'] = client.limit       # type: ignore
-    indicator, save_fetch_time = client.get_taxii(args)
+    indicator, save_fetch_time = client.get_taxii(args, is_first_fetch, limit)
     indicators = client.build_indicators(args, indicator)
 
     if save_fetch_time:
