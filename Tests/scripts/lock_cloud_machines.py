45d7d945324144bc9c1caeaee879f840da7dffaf
michal-dagan <mdagan@paloaltonetworks.com>
2024-01-18 11:55:46 +0200

    fixed

--- Tests/scripts/lock_cloud_machines.py
+++ Tests/scripts/lock_cloud_machines.py
@@ -396,6 +396,7 @@ def main():
 
     list_machines = create_list_of_machines_to_run(options.lock_machine_name, options.test_machines,
                                                    options.number_machines_to_lock)
+
     lock_machine_list = get_and_lock_all_needed_machines(storage_client, storage_bucket, list_machines,
                                                          options.gcs_locks_path, options.number_machines_to_lock,
                                                          options.ci_job_id, options.gitlab_status_token)
