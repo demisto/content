0fc3c64b41a0bbb8cebdf73b4b3f614c0ceacfe5
ilan <ierukhimovic@paloaltonetworks.com>
2024-01-22 13:38:27 +0200

    add autoextract

--- Packs/CommonScripts/Scripts/CopyNotesToIncident/CopyNotesToIncident.py
+++ Packs/CommonScripts/Scripts/CopyNotesToIncident/CopyNotesToIncident.py
@@ -2,7 +2,7 @@ import demistomock as demisto  # noqa # pylint: disable=unused-wildcard-import
 from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
 from CommonServerUserPython import *  # noqa # pylint: disable=unused-wildcard-import
 
-from typing import Dict, Any, List
+from typing import Any
 import traceback
 
 ''' STANDALONE FUNCTION '''
@@ -13,17 +13,17 @@ def remove_id_and_version_from_entry(entry):
     entry.pop('Version', None)
 
 
-def copy_notes_to_target_incident(args: Dict[str, Any]) -> CommandResults:
+def copy_notes_to_target_incident(args: dict[str, Any]) -> CommandResults:
 
     target_incident = args.get('target_incident', None)
     if not target_incident:
         raise ValueError('Target Incident ID not specified')
 
     tags = argToList(args.get('tags'))
+    auto_extract = args.get('auto_extract', False)
+    entries = demisto.executeCommand('getEntries', {'filter': {'tags': tags, "ignoreAutoExtract": not auto_extract}})
 
-    entries = demisto.executeCommand('getEntries', {'filter': {'tags': tags}})
-
-    note_entries: List = []
+    note_entries: list = []
     md: str = ''
 
     if isinstance(entries, list) and len(entries) > 0:
@@ -33,7 +33,9 @@ def copy_notes_to_target_incident(args: Dict[str, Any]) -> CommandResults:
                 note_entries.append(entry)
 
         if len(note_entries) > 0:
-            demisto.executeCommand("addEntries", {"id": target_incident, "entries": note_entries})
+            demisto.executeCommand("addEntries", {"id": target_incident,
+                                                  "entries": note_entries,
+                                                  "ignoreAutoExtract": not auto_extract})
             md = f'## {len(note_entries)} notes copied'
         else:
             md = '## No notes found'
