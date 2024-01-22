b51d5865909f371dd9eefc571aee58b49a7576a6
arad carmi <acarmi@paloaltonetworks.com>
2024-01-22 16:07:31 +0200

    Test

--- Utils/github_workflow_scripts/trigger_contribution_build.py
+++ Utils/github_workflow_scripts/trigger_contribution_build.py
@@ -22,6 +22,7 @@ def arguments_handler():
     parser.add_argument('-b', '--base_branch', help='The Base branch name.')
     parser.add_argument('-c', '--contrib_branch', help='The contribution branch name.')
     parser.add_argument('-cr', '--contrib_repo', help='The contribution repo name.')
+    parser.add_argument('-cr', '--contrib_repo', help='The contribution repo name.')
     parser.add_argument('-u', '--username', help='The instance username.')
     parser.add_argument('-s', '--password', help='The instance password.')
     parser.add_argument('-gs', '--gold_server_url', help='The content gold instance url.')
@@ -33,6 +34,7 @@ def trigger_generic_webhook(options):
     base_branch = options.base_branch
     contrib_branch = options.contrib_branch
     contrib_repo = options.contrib_repo
+    contrib_repo = options.contrib_repo
     username = options.username
     password = options.password
     gold_server_url = options.gold_server_url
@@ -42,7 +44,7 @@ def trigger_generic_webhook(options):
     body = {
         "name": "GenericWebhook_trigger_contribution_build",
         "raw_json": {"BaseBranch": base_branch, "PullRequestNumber": pr_number, "ContribBranch": contrib_branch,
-                     "ProjectID": GITLAB_PROJECT_ID, "ContribRepo": contrib_repo},
+                     "ContribRepo": contrib_repo, "ProjectID": GITLAB_PROJECT_ID},
     }
     # post to Content Gold
     res = requests.post(contribution_build_instance_url, json=body, auth=(username, password))
