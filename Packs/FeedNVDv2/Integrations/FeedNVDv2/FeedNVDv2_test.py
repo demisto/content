908baea2af40927b1c860ea95ac72cb7a1cdfb51
Josh Levine <github@cantreach.me>
2024-01-23 09:27:11 -0500

    Linting

--- Packs/FeedNVDv2/Integrations/FeedNVDv2/FeedNVDv2_test.py
+++ Packs/FeedNVDv2/Integrations/FeedNVDv2/FeedNVDv2_test.py
@@ -20,17 +20,17 @@ def get_test_data(command_name):
     """
     # Retrieve the raw CPE sample data
     if command_name == "parse_cpe_command":
-        # with open('./Packs/FeedNVDv2/Integrations/FeedNVDv2/test_data/test_cpe_data.txt') as f:
+        # with open('./test_data/test_cpe_data.txt') as f:
         with open('./test_data/test_cpe_data.txt') as f:
             return f.read()
     # Retrieve the expected response to the sample CPE data
     elif command_name == "parse_cpe_command_response":
-        # with open('./Packs/FeedNVDv2/Integrations/FeedNVDv2/test_data/test_cpe_data_response.txt') as f:
+        # with open('./test_data/test_cpe_data_response.txt') as f:
         with open('./test_data/test_cpe_data_response.txt') as f:
             return f.read()
     # Otherwise this is testing the CVE processing chain (retrieve_cves_command which calls process_cves_command)
     else:
-        # with open('./Packs/FeedNVDv2/Integrations/FeedNVDv2/test_data/test_cve_data_response.json') as f:
+        # with open('./test_data/test_cve_data_response.json') as f:
         with open('./test_data/test_cve_data_response.json') as f:
             return json.loads(f.read())
 
