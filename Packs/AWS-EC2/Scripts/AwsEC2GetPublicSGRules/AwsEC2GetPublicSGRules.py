60644e27bdefdeb7e43cf07d9723bbdcab17737a
ilappe <ilappe@paloaltonetworks.com>
2024-01-23 11:08:04 +0200

    update other packs

--- Packs/AWS-EC2/Scripts/AwsEC2GetPublicSGRules/AwsEC2GetPublicSGRules.py
+++ Packs/AWS-EC2/Scripts/AwsEC2GetPublicSGRules/AwsEC2GetPublicSGRules.py
@@ -7,7 +7,7 @@ import copy
 
 def get_dict_value(data, key):
     """ Returns dict value for a given key (case insensitive) """
-    for key_name in data.keys():
+    for key_name in data:
         if key_name.lower() == key.lower():
             return data[key_name]
 
@@ -30,9 +30,8 @@ def get_ec2_sg_public_rules(group_id, ip_permissions, checked_protocol=None, che
     for rule in ip_permissions:
         # Check protocol
         protocol = get_dict_value(rule, 'IpProtocol')
-        if protocol != '-1':
-            if checked_protocol.lower() != protocol.lower():
-                continue
+        if protocol != '-1' and checked_protocol.lower() != protocol.lower():
+            continue
 
         bad_rule = {
             'groupId': group_id,
