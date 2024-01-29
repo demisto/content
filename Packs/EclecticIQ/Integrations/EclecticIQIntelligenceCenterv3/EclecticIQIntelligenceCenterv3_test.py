6fbfc501eec2e375a8f44b3fda75506989ca51b6
mikhva-eiq <vami473@gmail.com>
2024-01-29 16:20:18 +0100

    test added
    - test for get_source_group_uid

--- Packs/EclecticIQ/Integrations/EclecticIQIntelligenceCenterv3/EclecticIQIntelligenceCenterv3_test.py
+++ Packs/EclecticIQ/Integrations/EclecticIQIntelligenceCenterv3/EclecticIQIntelligenceCenterv3_test.py
@@ -9,6 +9,39 @@ EIQ_FEED_IDs = "12"
 USE_SSL = "false"
 API_VERSION = "v2"
 
+@pytest.fixture
+def mock_api_request(mocker):
+    return mocker.patch.object(EclecticIQ_api, 'send_api_request', autospec=True)
+
+def test_get_source_group_uid_success(mock_api_request, mocker):
+    mock_api_request.return_value.json.return_value = {
+        "data": [{"source": "mocked_source_id"}]
+    }
+    mocker.patch.object(EclecticIQ_api, 'eiq_logging')
+
+    eiq_instance = EclecticIQ_api()
+
+    result = eiq_instance.get_source_group_uid("mocked_group_name")
+    assert result == "mocked_source_id"
+    EclecticIQ_api.eiq_logging.debug.assert_called_with("Requesting source id for specified group, name=[mocked_group_name]")
+    EclecticIQ_api.eiq_logging.debug.assert_any_call("Source group id received")
+    EclecticIQ_api.eiq_logging.debug.assert_any_call("Source group id is: mocked_source_id")
+
+def test_get_source_group_uid_failure(mock_api_request, mocker):
+    mock_api_request.return_value.json.return_value = {"data": []}
+    mocker.patch.object(EclecticIQ_api, 'eiq_logging')
+    eiq_instance = EclecticIQ_api()
+    result = eiq_instance.get_source_group_uid("nonexistent_group")
+    assert result == "error_in_fetching_group_id"
+    EclecticIQ_api.eiq_logging.error.assert_called_with(
+        "Something went wrong fetching the group id. "
+        "Please note the source group name is case sensitive! "
+        "Received response: {'data': []}"
+    )
+
+
+
+
 # Mock function for sighting
 
 
@@ -72,7 +105,6 @@ def test_sighting(mocker):
     )
 
     assert isinstance(response, dict)
-    # print(response)
     assert response["data"]["data"]["confidence"] == "medium"
     assert response["data"]["data"]["description"] == "test_desc"
     assert response["data"]["data"]["type"] == " EclecticIQ-sighting"
