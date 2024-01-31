a7fedf89d2c0be2bce94731e62a0de1f5ef422e2
yucohen <yucohen@paloaltonetworks.com>
2024-01-31 14:09:35 +0200

    rollbacked changes for testing

--- Tests/scripts/validate.sh
+++ Tests/scripts/validate.sh
@@ -9,7 +9,7 @@ if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_
         PACKS_TO_UPLOAD_SPACED=${PACKS_TO_UPLOAD//,/ }
         for item in $PACKS_TO_UPLOAD_SPACED; do
             python3 -m demisto_sdk validate -i Packs/"$item" --post-commit --graph --skip-pack-dependencies
-        done       
+        done
     else
         if [[ -n "${NIGHTLY}" && "${CI_COMMIT_BRANCH}" == "master" ]]; then
             PREV_VER=$LAST_UPLOAD_COMMIT
@@ -19,18 +19,9 @@ if [[ $CI_COMMIT_BRANCH = master ]] || [[ -n "${NIGHTLY}" ]] || [[ -n "${BUCKET_
         python3 -m demisto_sdk validate -a --graph --skip-pack-dependencies --prev-ver $PREV_VER
     fi
 elif [[ $CI_COMMIT_BRANCH =~ pull/[0-9]+ ]]; then
-    echo "validate all for test start"
-    python3 -m demisto_sdk validate -a --graph --skip-pack-dependencies
-    echo "validate all for test end"
     python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
 elif [[ $CI_COMMIT_BRANCH = demisto/python3 ]]; then
-    echo "validate all for test start"
-    python3 -m demisto_sdk validate -a --graph --skip-pack-dependencies
-    echo "validate all for test end"
     python3 -m demisto_sdk validate -g --post-commit --no-conf-json --allow-skipped --graph --skip-pack-dependencies
 else
-    echo "validate all for test start"
-    python3 -m demisto_sdk validate -a --graph --skip-pack-dependencies
-    echo "validate all for test end"
     python3 -m demisto_sdk validate -g --post-commit --graph --skip-pack-dependencies
 fi
