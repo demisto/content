59af25eccfc4e2e1324847d6d9f3d4b7237c4ddc
Menachem Weinfeld <mmhw770@gmail.com>
2024-02-01 13:14:39 +0200

    Add demisto-sdk rev

--- Tests/test_content.py
+++ Tests/test_content.py
@@ -543,11 +543,10 @@ def lock_integrations(integrations_details: list,
         # Each file has content in the form of <circleci-build-number>:<timeout in seconds>
         # If it has not expired - it means the integration is currently locked by another test.
         workflow_id, build_number, lock_timeout = lock_file.download_as_string().decode().split(':')
-        logging_manager.warning(f'\n\n{lock_expired(lock_file, lock_timeout)=}, {workflow_still_running(workflow_id)=}\n\n')
         if not lock_expired(lock_file, lock_timeout) and workflow_still_running(workflow_id):
             # there is a locked integration for which the lock is not expired - test cannot be executed at the moment
             logging_manager.warning(
-                f'test Could not lock integration {integration}, another lock file was exist with '
+                f'Could not lock integration {integration}, another lock file was exist with '
                 f'build number: {build_number}, timeout: {lock_timeout}, last update at {lock_file.updated}.\n'
                 f'Delaying test execution')
             return False
