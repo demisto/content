c326b9bca3ee582eb6ff2ff2d19816b54c8a33e8
mikhva-eiq <vami473@gmail.com>
2024-02-02 16:35:08 +0100

    test updates

--- Packs/EclecticIQ/Integrations/EclecticIQIntelligenceCenterv3/EclecticIQIntelligenceCenterv3_test.py
+++ Packs/EclecticIQ/Integrations/EclecticIQIntelligenceCenterv3/EclecticIQIntelligenceCenterv3_test.py
@@ -1,6 +1,6 @@
 import pytest  # noqa: F401
 import demistomock as demisto  # noqa: F401
-from EclecticIQIntelligenceCenterv3 import EclecticIQ_api, create_sighting
+from EclecticIQIntelligenceCenterv3 import EclecticIQ_api, create_sighting, create_indicator, prepare_entity_observables
 
 
 SERVER = "https://test.eclecticiq.com"
@@ -72,7 +72,7 @@ def sighting_mock_response(*args, **kwargs):
                 "description": "test_desc",
                 "type": " EclecticIQ-sighting",
                 "timestamp": "2022-03-10T05:37:42Z",
-                "title": "title1",
+                "title": "EIQ-title",
                 "security_control": {
                     "type": "information-source",
                     "identity": {
@@ -92,13 +92,11 @@ def sighting_mock_response(*args, **kwargs):
     return return_value
 
 
-
 def test_create_sighting(mocker):
     """Test for sighting."""
-    #mocker.patch("EclecticIQIntelligenceCenterv3.create_sighting", sighting_mock_response)
     mocker.patch("EclecticIQIntelligenceCenterv3.EclecticIQ_api.create_entity", entity_create_response)
     mocker.patch("EclecticIQIntelligenceCenterv3.EclecticIQ_api.get_outh_token", platform_auth_mock_response)
-    mocker.patch.object(demisto, 'args', return_value={"observable_type": "ipv4", "observable_value": "1.1.1.1", "sighting_title": "EIQ",
+    mocker.patch.object(demisto, 'args', return_value={"observable_type": "ipv4", "observable_value": "1.1.1.1", "sighting_title": "EIQ-title",
             "sighting_description": "sighting", "observable_maliciousness": "Malicious (Medium confidence)"})
 
     client = EclecticIQ_api(
@@ -112,4 +110,48 @@ def test_create_sighting(mocker):
     response = create_sighting(client)
 
     assert isinstance(response.raw_response, dict)
+    assert response.outputs_prefix == "EclecticIQ.Sightings"
+    assert response.raw_response["sighting_details"]["observable_maliciousness"] == 'Malicious (Medium confidence)'
+    assert response.outputs["SightingId"] == "123-123-123"
+
+
+def test_create_indicator(mocker):
+    """Test for sighting."""
+    mocker.patch("EclecticIQIntelligenceCenterv3.EclecticIQ_api.create_entity", entity_create_response)
+    mocker.patch("EclecticIQIntelligenceCenterv3.EclecticIQ_api.get_outh_token", platform_auth_mock_response)
+    mocker.patch.object(demisto, 'args', return_value={"observable_type": "ipv4", "observable_value": "1.1.1.1", "indicator_title": "EIQ-title",
+            "indicator_description": "indicator", "observable_maliciousness": "Malicious (Medium confidence)"})
+
+    client = EclecticIQ_api(
+        baseurl=SERVER,
+        eiq_api_version=API_VERSION,
+        username="",
+        password=PASSWORD,
+        verify_ssl=USE_SSL,
+    )
+
+    response = create_indicator(client)
+
+    assert isinstance(response.raw_response, dict)
+    assert response.outputs_prefix == "EclecticIQ.Indicators"
+    assert response.raw_response["indicator_title"] == "EIQ-title"
+    assert response.outputs["IndicatorId"] == "123-123-123"
+
+
+
+def test_prepare_entity_observables(mocker):
+    response = prepare_entity_observables("1.1.1.1","ipv4", "Malicious (High confidence)", '[{"type":"ipv4", "value": "2.2.2.2", "maliciousness": "medium"}]')
+    assert isinstance(response, list)
+    assert isinstance(response[0], dict)
+    assert response[1]["observable_classification"] == "bad"
+
+
+
+
+
+
+
+
+
+
 
