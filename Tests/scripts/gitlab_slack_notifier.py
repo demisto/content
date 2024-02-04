49eeb986a27c861f180ecccb3f2e4cf95f92cc26
Yehuda <yrosenberg@paloaltonetworks.com>
2024-02-04 02:23:19 +0200

    fixes

--- Tests/scripts/gitlab_slack_notifier.py
+++ Tests/scripts/gitlab_slack_notifier.py
@@ -586,8 +586,9 @@ def main():
                     pipeline_changed_status = is_pivot(current_pipeline=current_pipeline, pipeline_to_compare=previous_pipeline)
 
                     logging.info(
-                        f"Checking pipeline id: {current_pipeline.id}, of commit: {current_commit.title}, "         # type: ignore[attr-defined]
-                        f"after comparing with pipeline id: {previous_pipeline.id}, the change status is: {pipeline_changed_status}")
+                        f"Checking pipeline id: {current_pipeline.id}, of commit: {current_commit.title}, "     # type: ignore[attr-defined]        
+                        f"after comparing with pipeline id: {previous_pipeline.id},"    # type: ignore[attr-defined]
+                        f"the change status is: {pipeline_changed_status}")
 
                     if pipeline_changed_status is None and current_commit_index > 0:
                         # looking_forward until we find a commit with a pipeline to compare with
@@ -595,13 +596,15 @@ def main():
                             list_of_pipelines, list_of_commits, current_commit_index, direction="newer")
 
                         if next_pipeline and suspicious_commits:
-                            pipeline_changed_status = is_pivot(current_pipeline=next_pipeline, pipeline_to_compare=current_pipeline)
+                            pipeline_changed_status = is_pivot(current_pipeline=next_pipeline,
+                                                               pipeline_to_compare=current_pipeline)
                             logging.info(
                                 f" after comparing with pipeline id: {next_pipeline.id},"
                                 f"the change status is: {pipeline_changed_status}")
 
                     if pipeline_changed_status is not None:
-                        shame_message = create_shame_message(suspicious_commits, pipeline_changed_status, options.name_mapping_path)
+                        shame_message = create_shame_message(suspicious_commits, pipeline_changed_status,
+                                                             options.name_mapping_path)
                         computed_slack_channel = "test_slack_notifier_when_master_is_broken"
 
     slack_msg_data, threaded_messages = construct_slack_msg(triggering_workflow, pipeline_url, pipeline_failed_jobs, pull_request,
