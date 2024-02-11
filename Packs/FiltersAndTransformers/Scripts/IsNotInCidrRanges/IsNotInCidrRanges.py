bfdcad668ae46f74b813e8b8603fd5bb7527c625
Shmuel Kroizer <skroizer@paloaltonetworks.com>
2024-02-11 12:38:09 +0200

    added UT

--- Packs/FiltersAndTransformers/Scripts/IsNotInCidrRanges/IsNotInCidrRanges.py
+++ Packs/FiltersAndTransformers/Scripts/IsNotInCidrRanges/IsNotInCidrRanges.py
@@ -28,37 +28,44 @@ def validate_cidr(cidr: str) -> bool:
     return True
 
 
-def main():
-    """Check if given IP (or IPs) address is part of a given CIDR (or a list of CIDRs).
+def process_ips(ip_addresses: list[str], cidr_range_list: list[str]) -> bool:
+    """
+    Check if given IP (or IPs) address is part of a given CIDR (or a list of CIDRs).
 
     Args:
-        ip_addresses (str): A list of IPs/IPv6s
-        cidr_range_list (str): A list of CIDRs to be checked against.
+        ip_addresses (List[str]): A list of IPs/IPv6s
+        cidr_range_list (List[str]): A list of CIDRs to be checked against.
 
     Returns:
         bool: True if given IP is part of given CIDR range.
     """
+    not_in_range_list = []
+    for ip in ip_addresses:
+        try:
+            ip_address = ipaddress.ip_address(ip)
+        except ValueError as e:
+            demisto.debug(f'Skipping "{ip}": {e}')
+            continue
 
-    ip_addresses = argToList(demisto.args()['left'])
-    cidr_range_list = argToList(demisto.args()['right'])
+        not_in_range_list.append(
+            all(
+                ip_address not in ipaddress.ip_network(cidr)
+                for cidr in cidr_range_list
+                if validate_cidr(cidr)
+            )
+        )
+
+    return all(not_in_range_list)
+
+
+def main():  # pragma: no cover
+    ip_addresses = argToList(demisto.args()["left"])
+    cidr_range_list = argToList(demisto.args()["right"])
 
     try:
-        not_in_range_list = []
-        for ip in ip_addresses:
-
-            try:
-                ip = ipaddress.ip_address(ip)
-
-            except ValueError as e:
-                demisto.debug(f'Skipping "{ip}": {e}')
-                continue
-
-            not_in_range_list.append(all(ip not in ipaddress.ip_network(cidr) for cidr in cidr_range_list if validate_cidr(cidr)))
-
-        return_results(all(not_in_range_list))
-
+        return_results(process_ips(ip_addresses, cidr_range_list))
     except Exception as e:
-        return_error(f'Failed to execute IsNotInCidrRange. Error: {str(e)}')
+        return_error(f"Failed to execute IsNotInCidrRange. Error: {str(e)}")
 
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
