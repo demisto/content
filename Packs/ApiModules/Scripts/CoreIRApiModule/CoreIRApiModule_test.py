842e1cb53dde7e41685dac0c32bc8a761cf97975
MLainer1 <mlainer@paloaltonetworks.com>
2024-02-12 13:35:04 +0200

    bug - Cortex IR resolved incidents not mirrored correctly

--- Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule_test.py
+++ Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule_test.py
@@ -846,6 +846,24 @@ def test_get_update_args_unassgning_user(mocker):
     assert update_args.get('assigned_user_pretty_name') is None
     assert update_args.get('unassign_user') == 'true'
 
+def test_handle_outgoing_issue_closure(mocker):
+    """
+    Given:
+        -  a dict indicating changed fields (delta) with assigned_user_mail set to "None"
+        - the incident status - set to 1 == Active
+    When
+        - running get_update_args
+    Then
+        - update_args have assigned_user_mail and assigned_user_pretty_name set to None and unassign_user set to 'true'
+    """
+    from CoreIRApiModule import handle_outgoing_issue_closure
+    from CommonServerPython import UpdateRemoteSystemArgs
+    remote_args = UpdateRemoteSystemArgs({'delta': {'assigned_user_mail': 'None', 'closeReason': 'Resolved - Security Testing'}, 'status': 2, 'inc_status': 2, 'closeNotes': 'yayy', 'data': {'status': 'other'}})
+    request_data_log = mocker.patch.object(demisto, 'debug')
+    handle_outgoing_issue_closure(remote_args)
+    
+    assert "Closing Remote incident with status resolved_security_testing" in request_data_log.call_args[0][0]
+
 
 def test_get_update_args_close_incident():
     """
