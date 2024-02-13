eff2ae4ae5091083d078ee145d129206b5af08bd
yucohen <yucohen@paloaltonetworks.com>
2024-02-13 17:18:16 +0200

    test

--- Tests/scripts/run_tests.sh
+++ Tests/scripts/run_tests.sh
@@ -72,7 +72,7 @@ if [[ "${SERVER_TYPE}" == "XSIAM" ]]; then
 elif [[ "${SERVER_TYPE}" == "XSOAR" ]] || [[ "${SERVER_TYPE}" == "XSOAR SAAS" ]]; then
     demisto-sdk test-content -k "$DEMISTO_API_KEY" -c "$CONF_PATH" -e "$SECRET_CONF_PATH" -n "${IS_NIGHTLY}" -t "$SLACK_TOKEN" \
       -a "$CIRCLECI_TOKEN" -b "${CI_PIPELINE_ID}" -g "$CI_COMMIT_BRANCH" -m "${MEM_CHECK}" --is-ami "${IS_AMI_RUN}" -d "${INSTANCE_ROLE}" \
-      --xsiam-machine "test_opp" --xsiam-servers-path "$CLOUD_SERVERS_PATH" --server-type "${SERVER_TYPE}" \
+      --xsiam-machine "test_opp" --xsiam-servers-path "$CLOUD_SERVERS_PATH" --server-type "XSOAR SAAS" \
       --use-retries --xsiam-servers-api-keys-path "cloud_api_keys.json" --artifacts-path="${ARTIFACTS_FOLDER_INSTANCE}" --product-type="${PRODUCT_TYPE}"
     exit_code=$?
     echo "Failed to run test content with exit code:${exit_code}"
