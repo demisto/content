4c9b4898fd6ce56e77d81723fd3bf152fbc2ec66
tomer-pan <thaimof@paloaltonetworks.com>
2024-02-14 16:38:33 +0200

    add test_data

--- Packs/PrismaCloudCompute/Scripts/PCComputeHostComplianceIssuesButton/PCComputeHostComplianceIssuesButton_test.py
+++ Packs/PrismaCloudCompute/Scripts/PCComputeHostComplianceIssuesButton/PCComputeHostComplianceIssuesButton_test.py
@@ -1,5 +1,7 @@
 from PCComputeHostComplianceIssuesButton import run_prisma_cloud_compute_hosts_scan_list
 import pytest
+import json
+import io
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
@@ -35,62 +37,19 @@ TEST_CASES = [
 ]
 
 
+def util_load_json(path):
+    with io.open(path, mode='r', encoding='utf-8') as f:
+        return json.loads(f.read())
+
+
+compliance_issues = util_load_json('test_data/compliance_issues.json')
+
+
 @pytest.mark.parametrize('args, expected', TEST_CASES)
 def test_run_prisma_cloud_compute_hosts_scan_list(mocker, args, expected):
     # Mock the executeCommand function
     mocker.patch.object(demisto, 'executeCommand', return_value=[{'Type': EntryType.NOTE,
-                                                                  'Contents': [{'complianceIssues': [
-                                                                      {"text": "", "id": 6112, "severity": "high", "cvss": 0, "status": "",
-                                                                       "cause": "The directory /tmp should be mounted. File: /proc/mounts",
-                                                                       "description": "The /tmp directory is a world-writable directory used for temporary storage by all users\nand some "
-                                                                       "applications.",
-                                                                       "title": "(CIS_Linux_2.0.0 - 1.1.2) Ensure /tmp is configured",
-                                                                       "vecStr": "", "exploit": "", "riskFactors": None, "link": "", "type": "linux", "packageName": "",
-                                                                       "packageVersion": "", "layerTime": 0, "templates": None, "twistlock": False, "cri": False,
-                                                                       "published": 0, "fixDate": 0, "discovered": "0001-01-01T00:00:00Z", "functionLayer": "",
-                                                                       "wildfireMalware": {}, "secret": {}},
-                                                                      {"text": "", "id": 6116, "severity": "medium", "cvss": 0, "status": "",
-                                                                       "cause": "The directory /var should be mounted. File: /proc/mounts",
-                                                                       "description": "Description for compliance ID 6116",
-                                                                       "title": "(CIS_Linux_2.0.0 - 1.1.6) Ensure separate partition exists for /var",
-                                                                       "vecStr": "", "exploit": "", "riskFactors": None, "link": "", "type": "linux", "packageName": "",
-                                                                       "packageVersion": "", "layerTime": 0, "templates": None, "twistlock": False, "cri": False,
-                                                                       "published": 0, "fixDate": 0, "discovered": "0001-01-01T00:00:00Z", "functionLayer": "",
-                                                                       "wildfireMalware": {}, "secret": {}},
-                                                                      {"text": "", "id": 6117, "severity": "medium", "cvss": 0, "status": "",
-                                                                       "cause": "The directory /var/tmp should be mounted. File: /proc/mounts",
-                                                                       "description": "Description for compliance ID 6117",
-                                                                       "title": "(CIS_Linux_2.0.0 - 1.1.7) Ensure separate partition exists for /var/tmp",
-                                                                       "vecStr": "", "exploit": "", "riskFactors": None, "link": "", "type": "linux", "packageName": "",
-                                                                       "packageVersion": "", "layerTime": 0, "templates": None, "twistlock": False, "cri": False,
-                                                                       "published": 0, "fixDate": 0, "discovered": "0001-01-01T00:00:00Z", "functionLayer": "",
-                                                                       "wildfireMalware": {}, "secret": {}},
-                                                                      {"text": "", "id": 61115, "severity": "medium", "cvss": 0, "status": "",
-                                                                       "cause": "The directory /var/log should be mounted. File: /proc/mounts",
-                                                                       "description": "Description for compliance ID 61115",
-                                                                       "title": "(CIS_Linux_2.0.0 - 1.1.11) Ensure separate partition exists for /var/log",
-                                                                       "vecStr": "", "exploit": "", "riskFactors": None, "link": "", "type": "linux", "packageName": "",
-                                                                       "packageVersion": "", "layerTime": 0, "templates": None, "twistlock": False, "cri": False,
-                                                                       "published": 0, "fixDate": 0, "discovered": "0001-01-01T00:00:00Z", "functionLayer": "",
-                                                                       "wildfireMalware": {}, "secret": {}},
-                                                                      {"text": "", "id": 61116, "severity": "medium", "cvss": 0, "status": "",
-                                                                       "cause": "The directory /var/log/audit should be mounted. File: /proc/mounts",
-                                                                       "description": "Description for compliance ID 61116",
-                                                                       "title": "(CIS_Linux_2.0.0 - 1.1.12) Ensure separate partition exists for /var/log/audit",
-                                                                       "vecStr": "", "exploit": "", "riskFactors": None, "link": "", "type": "linux", "packageName": "",
-                                                                       "packageVersion": "", "layerTime": 0, "templates": None, "twistlock": False, "cri": False,
-                                                                       "published": 0, "fixDate": 0, "discovered": "0001-01-01T00:00:00Z", "functionLayer": "",
-                                                                       "wildfireMalware": {}, "secret": {}},
-                                                                      {"text": "", "id": 602214, "severity": "low", "cvss": 0, "status": "",
-                                                                       "cause": "The service systemd-timesyncd.service should be enabled",
-                                                                       "description": "Description for compliance ID 602214",
-                                                                       "title": "(CIS_Linux_2.0.0 - 2.2.1.4) Ensure systemd-timesyncd is configured",
-                                                                       "vecStr": "", "exploit": "", "riskFactors": None, "link": "", "type": "linux", "packageName": "",
-                                                                       "packageVersion": "", "layerTime": 0, "templates": None,
-                                                                       "twistlock": False, "cri": False,
-                                                                       "published": 0, "fixDate": 0, "discovered": "0001-01-01T00:00:00Z", "functionLayer": "",
-                                                                       "wildfireMalware": {}, "secret": {}},
-                                                                  ]}]
+                                                                  'Contents': [{'complianceIssues': compliance_issues}]
                                                                   }])
 
     # Run the function
@@ -107,6 +66,5 @@ def test_run_prisma_cloud_compute_hosts_scan_list(mocker, args, expected):
 
     readable_output = results['HumanReadable']
     assert f'Compliance Issues of host {args.get("hostname")}' in readable_output
-
 
 # Add more tests as needed
