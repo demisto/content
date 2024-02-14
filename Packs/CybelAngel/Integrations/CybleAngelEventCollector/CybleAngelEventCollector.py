41d8366c78b3986c0ace778b41e592b8035a4c7a
GuyAfik <guyafik11@gmail.com>
2024-02-14 18:46:31 +0200

    add client class

--- Packs/CybelAngel/Integrations/CybleAngelEventCollector/CybleAngelEventCollector.py
+++ Packs/CybelAngel/Integrations/CybleAngelEventCollector/CybleAngelEventCollector.py
@@ -19,7 +19,7 @@ https://github.com/demisto/content/blob/master/Packs/HelloWorld/Integrations/Hel
 from CommonServerUserPython import *  # noqa
 
 import urllib3
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 
 # Disable insecure warnings
 urllib3.disable_warnings()
@@ -28,6 +28,7 @@ urllib3.disable_warnings()
 ''' CONSTANTS '''
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
+DEFAULT_MAX_FETCH = 5000
 
 ''' CLIENT CLASS '''
 
@@ -35,32 +36,108 @@ DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
 class Client(BaseClient):
     """Client class to interact with the service API
 
-    This Client implements API calls, and does not contain any XSOAR logic.
-    Should only do requests and return data.
-    It inherits from BaseClient defined in CommonServer Python.
-    Most calls use _http_request() that handles proxy, SSL verification, etc.
-    For this  implementation, no special attributes defined
+    This Client implements API calls to the Saas Security platform, and does not contain any XSOAR logic.
+    Handles the token retrieval.
+
+    :param base_url (str): Saas Security server url.
+    :param client_id (str): client ID.
+    :param client_secret (str): client secret.
+    :param verify (bool): specifies whether to verify the SSL certificate or not.
+    :param proxy (bool): specifies if to use XSOAR proxy settings.
     """
 
-    # TODO: REMOVE the following dummy function:
-    def baseintegration_dummy(self, dummy: str) -> Dict[str, str]:
-        """Returns a simple python dict with the information provided
-        in the input (dummy).
+    def __init__(self, base_url: str, client_id: str, client_secret: str, verify: bool, proxy: bool, **kwargs):
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-        :type dummy: ``str``
-        :param dummy: string to add in the dummy dict that is returned
+        super().__init__(base_url=base_url, verify=verify, proxy=proxy, **kwargs)
 
-        :return: dict as {"dummy": dummy}
-        :rtype: ``str``
+    def http_request(self, *args, **kwargs):
+        """
+        Overrides Base client request function, retrieves and adds to headers access token before sending the request.
+        """
+        token = self.get_access_token()
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json',
+        }
+        return super()._http_request(*args, headers=headers, **kwargs)  # type: ignore[misc]
+
+    def get_access_token(self) -> str:
+        """
+       Obtains access and refresh token from server.
+       Access token is used and stored in the integration context until expiration time.
+       After expiration, new refresh token and access token are obtained and stored in the
+       integration context.
+
+        Returns:
+            str: the access token.
+       """
+        integration_context = get_integration_context()
+        access_token = integration_context.get('access_token')
+        token_initiate_time = integration_context.get('token_initiate_time')
+        token_expiration_seconds = integration_context.get('token_expiration_seconds')
+
+        if access_token and not is_token_expired(
+            token_initiate_time=float(token_initiate_time),
+            token_expiration_seconds=float(token_expiration_seconds)
+        ):
+            return access_token
+
+        # there's no token or it is expired
+        access_token, token_expiration_seconds = self.get_token_request()
+        integration_context = {
+            'access_token': access_token,
+            'token_expiration_seconds': token_expiration_seconds,
+            'token_initiate_time': time.time()
+        }
+        demisto.debug(f'updating access token - {integration_context}')
+        set_integration_context(context=integration_context)
+
+        return access_token
+
+    def get_token_request(self) -> Tuple[str, str]:
+        """
+        Sends request to retrieve token.
+
+       Returns:
+           tuple[str, str]: token and its expiration date
         """
 
-        return {"dummy": dummy}
-    # TODO: ADD HERE THE FUNCTIONS TO INTERACT WITH YOUR PRODUCT API
+        token_response = self._http_request(
+            'POST',
+            full_url='https://auth.cybelangel.com/oauth/token',
+            json_data={
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+                "audience": "https://platform.cybelangel.com/",
+                "grant_type": "client_credentials"
+            }
+        )
+        return token_response.get('access_token'), token_response.get('expires_in')
 
 
 ''' HELPER FUNCTIONS '''
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
+
+def is_token_expired(token_initiate_time: float, token_expiration_seconds: float) -> bool:
+    """
+    Check whether a token has expired. a token considered expired if it has been reached to its expiration date in
+    seconds minus a minute.
+
+    for example ---> time.time() = 300, token_initiate_time = 240, token_expiration_seconds = 120
+
+    300.0001 - 240 < 120 - 60
+
+    Args:
+        token_initiate_time (float): the time in which the token was initiated in seconds.
+        token_expiration_seconds (float): the time in which the token should be expired in seconds.
+
+    Returns:
+        bool: True if token has expired, False if not.
+    """
+    return time.time() - token_initiate_time >= token_expiration_seconds - 60
+
 
 ''' COMMAND FUNCTIONS '''
 
@@ -120,34 +197,24 @@ def main() -> None:
     :return:
     :rtype:
     """
+    params = demisto.params()
+    client_id: str = params.get('credentials', {}).get('identifier', '')
+    client_secret: str = params.get('credentials', {}).get('password', '')
+    base_url: str = params.get('url', '').rstrip('/')
+    verify_certificate = not params.get('insecure', False)
+    proxy = params.get('proxy', False)
+    max_fetch = arg_to_number(params.get("max_fetch")) or DEFAULT_MAX_FETCH
 
-    # TODO: make sure you properly handle authentication
-    # api_key = demisto.params().get('credentials', {}).get('password')
-
-    # get the service API url
-    base_url = urljoin(demisto.params()['url'], '/api/v1')
-
-    # if your Client class inherits from BaseClient, SSL verification is
-    # handled out of the box by it, just pass ``verify_certificate`` to
-    # the Client constructor
-    verify_certificate = not demisto.params().get('insecure', False)
-
-    # if your Client class inherits from BaseClient, system proxy is handled
-    # out of the box by it, just pass ``proxy`` to the Client constructor
-    proxy = demisto.params().get('proxy', False)
-
-    demisto.debug(f'Command being called is {demisto.command()}')
+    command = demisto.command()
+    demisto.info(f'Command being called is {command}')
     try:
-
-        # TODO: Make sure you add the proper headers for authentication
-        # (i.e. "Authorization": {api key})
-        headers: Dict = {}
-
         client = Client(
+            client_id=client_id,
+            client_secret=client_secret,
             base_url=base_url,
             verify=verify_certificate,
-            headers=headers,
-            proxy=proxy)
+            proxy=proxy
+        )
 
         if demisto.command() == 'test-module':
             # This is the call made when pressing the integration Test button.
