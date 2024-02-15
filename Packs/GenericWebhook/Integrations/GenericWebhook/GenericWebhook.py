8deaa9580d1a55cbfeeb38d970c5d7e193830169
Judah Schwartz <juschwartz@paloaltonetworks.com>
2024-02-15 14:38:28 +0200

    init commit with logs

--- Packs/GenericWebhook/Integrations/GenericWebhook/GenericWebhook.py
+++ Packs/GenericWebhook/Integrations/GenericWebhook/GenericWebhook.py
@@ -1,12 +1,9 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
-import json
 from collections import deque
 from copy import copy
 from secrets import compare_digest
 from tempfile import NamedTemporaryFile
 from traceback import format_exc
-from typing import Dict
+from typing import Dict, List
 
 import uvicorn
 from fastapi import Depends, FastAPI, Request, Response, status
@@ -24,7 +21,7 @@ class Incident(BaseModel):
     name: Optional[str] = None
     type: Optional[str] = None
     occurred: Optional[str] = None
-    raw_json: Optional[Dict] = None
+    raw_json: Optional[Union[str, List, Dict]] = None  # Optional[Union[Dict, List]] = None
 
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
@@ -57,14 +54,13 @@ async def handle_post(
         credentials: HTTPBasicCredentials = Depends(basic_auth),
         token: APIKey = Depends(token_auth)
 ):
-    header_name = None
-    request_headers = dict(request.headers)
-
+    demisto.debug('handling request')
+    demisto.info(get_message_memory_dump(None, None))
     credentials_param = demisto.params().get('credentials')
-
     if credentials_param and (username := credentials_param.get('identifier')):
         password = credentials_param.get('password', '')
         auth_failed = False
+        header_name = None
         if username.startswith('_header'):
             header_name = username.split(':')[1]
             token_auth.model.name = header_name
@@ -74,37 +70,72 @@ async def handle_post(
                                    and compare_digest(credentials.password, password))):
             auth_failed = True
         if auth_failed:
+            request_headers = dict(request.headers)
             secret_header = (header_name or 'Authorization').lower()
             if secret_header in request_headers:
                 request_headers[secret_header] = '***'
             demisto.debug(f'Authorization failed - request headers {request_headers}')
             return Response(status_code=status.HTTP_401_UNAUTHORIZED, content='Authorization failed.')
-
-    secret_header = (header_name or 'Authorization').lower()
-    request_headers.pop(secret_header, None)
-
+    demisto.debug('finished auth')
+    # nmartini change
+    incidents = []
     raw_json = incident.raw_json or await request.json()
-    raw_json['headers'] = request_headers
+    demisto.debug('parsed json')
+    demisto.debug(f'{sys.getsizeof(raw_json)=}')
+    # unfortunately I have to do this to account for elastic's stupidity
+    if isinstance(raw_json, str):
+        raw_json = json.loads(raw_json)
+    if not isinstance(raw_json, list):
+        raw_json = [raw_json]
+    demisto.debug('starting loop')
+    for event in raw_json:
+        if len(event.keys()) == 0:
+            continue
 
-    incident = {
-        'name': incident.name or 'Generic webhook triggered incident',
-        'type': incident.type or demisto.params().get('incidentType'),
-        'occurred': incident.occurred,
-        'rawJSON': json.dumps(raw_json)
-    }
+        data = {
+            'name': incident.name or 'Generic webhook triggered incident',
+            'type': incident.type or demisto.params().get('incidentType'),
+            'rawJSON': json.dumps(event),
+            'labels': incident_label_maker(event)
+        }
 
-    if demisto.params().get('store_samples'):
-        try:
-            sample_events_to_store.append(incident)
-            integration_context = get_integration_context()
-            sample_events = deque(json.loads(integration_context.get('sample_events', '[]')), maxlen=20)
-            sample_events += sample_events_to_store
-            integration_context['sample_events'] = list(sample_events)
-            set_to_integration_context_with_retries(integration_context)
-        except Exception as e:
-            demisto.error(f'Failed storing sample events - {e}')
+        demisto.debug(json.dumps(data['labels']))
 
-    return demisto.createIncidents([incident])
+        incidents.append(data)
+
+        if demisto.params().get('store_samples'):
+            demisto.debug('in store samples')
+            try:
+                context = get_integration_context()
+                sample_events_to_store.append(data)
+                sample_events = deque(json.loads(context.get('sample_events', '[]')), maxlen=20)
+                sample_events += sample_events_to_store
+                context['sample_events'] = list(sample_events)
+                set_to_integration_context_with_retries(context)
+            except Exception as e:
+                demisto.error(f'Failed storing sample events - {e}')
+    demisto.debug("-->Incidents were: {}".format(incidents))
+    incidents =  demisto.createIncidents(incidents)
+    demisto.debug('created incidents')
+    return incidents
+    # end change
+
+
+def incident_label_maker(source):
+    """Creates labels for the created incident.
+
+    Args:
+        source(dict): the entire event document.
+
+    Returns:
+        (list).The labels.
+    """
+    labels = []
+    for field, value in source.items():
+        encoded_value = value if isinstance(value, str) else json.dumps(value)
+        labels.append({'type': str(field), 'value': encoded_value})
+
+    return labels
 
 
 def fetch_samples() -> None:
@@ -118,6 +149,27 @@ def fetch_samples() -> None:
     demisto.incidents(sample_events)
 
 
+# nmartini change
+def delete_cache():
+    try:
+        context = get_integration_context()
+        result = 'The sample cache is currently empty'
+        if len(json.loads(context.get('sample_events', '[]'))) > 0:
+            context['sample_events'] = []
+            set_to_integration_context_with_retries(context)
+            result = 'All samples have been removed from the cache'
+
+        return_results(
+            CommandResults(
+                readable_output=result
+            )
+        )
+
+    except Exception as e:
+        demisto.error(f'Failed removing sample events - {e}')
+# end change
+
+
 def main() -> None:
     demisto.debug(f'Command being called is {demisto.command()}')
     try:
@@ -129,6 +181,12 @@ def main() -> None:
             return_results('ok')
         elif demisto.command() == 'fetch-incidents':
             fetch_samples()
+        # nmartini custom change
+        elif demisto.command() == 'webhook_clear_cache':
+            delete_cache()
+        elif demisto.command() == 'webhook_get_cache':
+            fetch_samples()
+        # end change
         elif demisto.command() == 'long-running-execution':
             while True:
                 certificate = demisto.params().get('certificate', '')
@@ -181,4 +239,4 @@ def main() -> None:
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
-    main()
+        main()
