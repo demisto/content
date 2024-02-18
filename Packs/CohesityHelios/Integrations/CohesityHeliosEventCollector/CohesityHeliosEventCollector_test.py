c48587838b1e4d1d1139c7fcdd60cb2ced145311
sbenyakir <shahaf.benyakir@demisto.com>
2024-02-18 10:09:45 +0200

    code works

---
+++ Packs/CohesityHelios/Integrations/CohesityHeliosEventCollector/CohesityHeliosEventCollector_test.py
@@ -0,0 +1,5 @@
+
+ # Fetch should test 3 case:
+ # 1: There are less events than page_size on first request
+ # 2: There are more than page_size events but there are less than max_fetch events
+ # 3: There are more than max_fetch events
