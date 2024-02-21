00d0d9a3dba83e1ab87d82171864fa506420a7d5
Tal <tcarmeli@paloaltonetworks.com>
2024-02-21 17:07:34 +0200

    need to fix image

--- Packs/Redmine/Integrations/Redmine/Redmine.py
+++ Packs/Redmine/Integrations/Redmine/Redmine.py
@@ -37,8 +37,15 @@ class Client(BaseClient):
         return response
     def update_issue_request(self,args):
         issue_id = args.pop('issue_id')
+        file_token = args.pop('token', None)
+        file_name = args.pop('file_name', '')
+        description = args.pop('description', '')
+        content_type = args.pop('content_type', '')
+        if file_token:
+            args['uploads'] = [{'token':file_token,'file_name':file_name,'description':description, 'content_type': content_type}]
         response = self._http_request('PUT', f'/issues/{issue_id}.json', json_data={"issue":args}, headers=POST_PUT_HEADER)
         return response
+    
     def get_issues_list_request(self, args: dict[str, Any]):
         response = self._http_request('GET', '/issues.json', params=args, headers=GET_HEADER)
         return response
@@ -73,7 +80,25 @@ class Client(BaseClient):
         return response
 ''' HELPER FUNCTIONS '''
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
+def create_paging_header(page_size: int , page_number: int):
+    return '#### Showing' + (f' {page_size}') + ' results' + (f' from page {page_number}') + ':\n'
+
+# def create_readable_response (response : dict[str, Any]):
+#     issues = response.get('issues')
+#     if issues:
+#         for issue in issues:
+#             if (helper_variable := issue.pop('project', None)) is not None:
+#                 issue['project_id'] = helper_variable['id']
+#             if (helper_variable := issue.pop('tracker', None)) is not None:
+#                 issue['tracker_id'] = helper_variable['id']
+#             if (helper_variable := issue.pop('project', None)) is not None:
+#                 issue['project_id'] = helper_variable['id']
+#             if (helper_variable := issue.pop('project', None)) is not None:
+#                 issue['project_id'] = helper_variable['id']
+#             if (helper_variable := issue.pop('project', None)) is not None:
+#                 issue['project_id'] = helper_variable['id']
+#             if (helper_variable := issue.pop('project', None)) is not None:
+#                 issue['project_id'] = helper_variable['id']
 
 ''' COMMAND FUNCTIONS '''
 
@@ -92,65 +117,103 @@ def test_module(client: Client) -> None:
 
 
 def create_issue_command(client: Client, args: dict[str, Any]) -> CommandResults:
-    status_id = args.get('status_id') 
+    status_id = args.get('status_id')
     priority_id = args.get('priority_id')
     subject = args.get('subject')
     project_id = args.get('project_id')
-
     if not status_id or not priority_id or not subject or not project_id:
         raise DemistoException('One or more required arguments not specified')
     
     entry_id = args.get('entry_id')
-    file_name = args.get('file_name')
-    file_description = args.get('file_description')
+    file_name = args.get('file_name','')
+    file_description = args.get('file_description','')
+    content_type = args.get('file_content_type','')
     if (entry_id): #to ask about file_name
-        args_for_file = assign_params(file_name=file_name, file_description=file_description)
-        response = client.create_file_token_request(args_for_file, {'entry_id':entry_id})
+        args_for_file = assign_params(file_name=file_name)
+        response = client.create_file_token_request(args_for_file, entry_id)
         if (response['upload']):
-            args['uploads'] = response['upload']
-            args['uploads']['file_name'] = file_name
-            _ = args['uploads'].pop('id')
+            uploads = assign_params(token=response['upload']['token'],content_type=content_type,file_name=file_name,
+                                    description=file_description)
+            args['uploads'] = [uploads]
         else:
             raise DemistoException(f"Could not upload file with entry id {entry_id}")
-    response = client.create_issue_request(args)
+    response = client.create_issue_request(args)['issue']
 
-    headers = ['id', 'project id', 'tracker id', 'status id', 'priority id',
-               'subject', 'description', 'estimated_hours', 'custom fields']
-
-    return CommandResults(
+    headers = ['id', 'project', 'tracker', 'status', 'priority','author', 'estimated_hours', 'created_on',
+               'subject', 'description', 'start_date', 'estimated_hours', 'custom fields']
+    response['id'] = str(response['id'])
+    command_results = CommandResults(
         outputs_prefix='Redmine.Issue',
         outputs_key_field='id',
         outputs=response,
         raw_response=response,
-        readable_output=tableToMarkdown('The issue you created:', response, headers={},
-                                        removeNull=True, headerTransform=pascalToSpace)
+        readable_output=tableToMarkdown('The issue you created:', response, headers=headers,
+                                        removeNull=True,is_auto_json_transform=True, headerTransform=pascalToSpace)
     )
+    print(command_results.readable_output)
+    return command_results
+    
 
 def update_issue_command(client: Client, args: dict[str, Any]):
     #need to deal with watchers,customfields,attachments
-    if args.get('issue_id'):
-        watchers = args.pop('watcher_user_ids', None)
-        custom_fields = args.pop('custom_fields', None)
-        attachments = args.pop('attachments', None)
-        return client.update_issue_request(args)
+    issue_id = args.get('issue_id')
+    if issue_id:
+        entry_id = args.pop('entry_id', None)
+        file_name = args.pop('entry_id', '')
+        if (entry_id):
+            file_token = client.create_file_token_request(assign_params(file_name=file_name),entry_id)['upload']['token']
+            args = client.update_issue_request(assign_params(token=file_token, **args))
+        command_results = CommandResults(
+            readable_output=f'Issue with id {issue_id} was successfully updated.')
+        print(command_results.readable_output)
+        return (command_results)
     else:
         raise DemistoException('Issue_id is missing- in order to update this issue')
 
 def get_issues_list_command(client: Client, args: dict[str, Any]):
-    offset = (int(args.pop('page_number',1))-1)*int(args.pop('page_size',50))
-    args_to_request = assign_params(offset=offset, limit=,**args)
-    response = client.get_issues_list_request(args)
-    readable_response = create_readable_response(response, asset_list_handler, 'value')
+    page_number = args.pop('page_number',None)
+    page_size = args.pop('page_size', None)
+    limit = args.pop('limit', None)
+    offset_to_dict = None
+    limit_to_dict = None
+    page_header = None
+    if page_number or page_size:
+        if page_size:
+            page_size = int(page_size)
+        else:
+            page_size = 50
+        if page_number:
+            page_number = int(page_number)
+        else:
+            page_number = 1
+        offset_to_dict = (page_number-1) * page_size
+        limit_to_dict = page_size
+        page_header = create_paging_header (page_size, page_number)
+    elif limit:
+        offset_to_dict = 0
+        limit_to_dict = limit
+        page_header = create_paging_header (limit_to_dict, 1)
+    args = assign_params(offset=offset_to_dict, limit=limit_to_dict, **args)
+    response = client.get_issues_list_request(args)['issues']
+    if not page_header:
+        page_header = create_paging_header(len(response),1)
+    # readable_response = create_readable_response(response)
+    headers = ['id', 'tracker', 'status', 'priority', 'author', 'subject', 'description', 'start_date', 'due_date', 'done_ratio',
+               'is_private', 'estimated_hours', 'custom_fields', 'created_on', 'updated_on',
+               'closed_on', 'attachments', 'relations']
     command_results = CommandResults(
-        outputs_prefix='SysAid.Asset',
+        outputs_prefix='Redmine.Issue',
         outputs_key_field='id',
         outputs=response,
         raw_response=response,
-        readable_output=create_paging_header(limit, page_number) + tableToMarkdown('Asset Results:',
-                                                                                   readable_response,
-                                                                                   headers=headers,
-                                                                                   removeNull=True,
-                                                                                   headerTransform=pascalToSpace))
+        readable_output= page_header + tableToMarkdown('Issues Results:',
+                                                        response,
+                                                        headers=headers,
+                                                        removeNull=True,
+                                                        headerTransform=pascalToSpace,
+                                                        is_auto_json_transform=True))
+    print(command_results.readable_output)
+    return command_results
 
 def get_issue_by_id_command(client: Client, args: dict[str, Any]):
     include_possible_values = {'children', 'attachments', 'relations', 'changesets', 'journals', 'watchers', 'allowed_statuses'}
@@ -158,7 +221,7 @@ def get_issue_by_id_command(client: Client, args: dict[str, Any]):
     issue_id = args.pop('issue_id', None)
     if issue_id:
         if included_fields and not all(field_value in include_possible_values for field_value in included_fields):
-                raise DemistoException(f"You can only include the following values {include_possible_values}")        
+                raise DemistoException(f"You can only include the following values {include_possible_values}")
         elif (included_fields):
             array_of_include_to_string = ','.join(included_fields)
             args['include'] = array_of_include_to_string
@@ -175,7 +238,7 @@ def get_issue_by_id_command(client: Client, args: dict[str, Any]):
                                                                 removeNull=True, 
                                                                 headerTransform=underscoreToCamelCase))
         print(command_results.readable_output)
-        return command_results  
+        return command_results
     else:
         raise DemistoException('Issue_id is missing- in order to get this issue')
     
@@ -235,7 +298,7 @@ def get_project_list_command(client: Client, args: dict[str, Any]):
                                                                     removeNull=True, 
                                                                     headerTransform=underscoreToCamelCase))
     print(command_results.readable_output)
-    return command_results  
+    return command_results
         
 def get_custom_fields_command(client: Client):
     #didnt put all fields here- i think it is not user visible
@@ -303,8 +366,9 @@ def main() -> None:
             verify_certificate,
             proxy,
             auth=("", api_key))
-        #response = get_issues_list_command(client,{'limit':'10','offset':'9'})
-        # response = create_issue_command(client,{'project_id':'1','status_id':'1', 'priority_id':'1', 'subject':'wdq', 'entry_id':'/Users/sdepaz/Desktop/all/example.png'})
+        # return_results(get_issues_list_command(client,{'sort':'priority:desc', 'limit':'20'}))
+        return_results(create_issue_command(client,{'project_id':'1','status_id':'1', 'priority_id':'1', 'file_content_type': 'image/png',
+                                        'subject':'createfromcode', 'file_name':'tal.png','entry_id':'/Users/tcarmeli/Downloads/for_test.png'}))
         #return_results(get_users_command(client, {'status':'1'}))
         # return_results(get_users_command(client, {'name':'Redmine'}))
         # return_results(get_custom_fields_command(client))
@@ -312,8 +376,10 @@ def main() -> None:
         # return_results(remove_issue_watcher_command(client,{'issue_id':'44','watcher_id':'1'}))
         # return_results(add_issue_watcher_command(client,{'issue_id':'44','watcher_id':'1'}))
         # return_results(delete_issue_by_id_command(client, {'issue_id':'41'}))
-        return_results(get_issue_by_id_command(client, {'issue_id' : '44', 'include':['watchers', 'attachments']}))
-        # response = update_issue_command(client, {'issue_id' : '44', 'subject':'changeFromCode','tracker_id':'1'})
+        # return_results(get_issue_by_id_command(client, {'issue_id' : '44', 'include':['watchers', 'attachments']}))
+        # return_results(update_issue_command(client, {'issue_id' : '49', 'subject':'changeFromCode','tracker_id':'1',
+        #                                              'content_type':'image/png', 'entry_id':'/Users/tcarmeli/Downloads/for_test11.png',
+        #                                              'watcher_user_ids':'[1]'}))
         if command == 'test-module':
             test_module(client)
         # elif command in commands:
