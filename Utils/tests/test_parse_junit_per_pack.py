d872fb1388c83f2c94614e9b8213ab0a8912da31
dorschw <81086590+dorschw@users.noreply.github.com>
2024-02-21 13:38:10 +0200

    fix

--- Utils/tests/test_parse_junit_per_pack.py
+++ Utils/tests/test_parse_junit_per_pack.py
@@ -15,11 +15,11 @@ def test_parse_xml(tmp_path):
     """
     (xml_path := Path(tmp_path, "foo.xml")).write_text(
         "<?xml version='1.0' encoding='UTF-8'?>"
-        '<testsuites><testsuite name="pytest" errors="0" failures="0" skipped="0" tests="3" time="2.158" timestamp="2024-02-20T15:25:31.306846" hostname="0d480f757bd2">'
+        '<testsuites><testsuite name="pytest" errors="0" failures="0" skipped="0" tests="3" time="2.158" timestamp="2024-02-20T15:25:31.306846" hostname="0d480f757bd2">'  # noqa: E501
         '<testcase classname="Packs.ipinfo.Integrations.ipinfo_v2.ipinfo_v2_test" name="test_ipinfo_ip_command" time="0.010"/>'
-        '<testcase classname="Packs.ipinfo.Integrations.ipinfo_v2.ipinfo_v2_test" name="test_ipinfo_nultiple_ips_command" time="0.008"/>'
-        '<testcase classname="Packs.ipinfo.Integrations.ipinfo_v2.ipinfo_v2_test" name="test_check_columns_exists" time="0.005"/></testsuite>'
-        '<testsuite name="pytest" errors="0" failures="0" skipped="0" tests="3" time="1.767" timestamp="2024-02-20T15:25:25.799231" hostname="d190b19a16e8">'
+        '<testcase classname="Packs.ipinfo.Integrations.ipinfo_v2.ipinfo_v2_test" name="test_ipinfo_nultiple_ips_command" time="0.008"/>'  # noqa: E501
+        '<testcase classname="Packs.ipinfo.Integrations.ipinfo_v2.ipinfo_v2_test" name="test_check_columns_exists" time="0.005"/></testsuite>'  # noqa: E501
+        '<testsuite name="pytest" errors="0" failures="0" skipped="0" tests="3" time="1.767" timestamp="2024-02-20T15:25:25.799231" hostname="d190b19a16e8">'  # noqa: E501
         '<testcase classname="Packs.qradar.Integrations.qradar.qradar_test" name="test_ipinfo_ip_command" time="0.010"/>'
         '<testcase classname="Packs.ipinfo.Integrations.qradar.qradar_test" name="test_ipinfo_nultiple_ips_command" time="0.007"/>'
         '<testcase classname="Packs.ipinfo.Integrations.qradar.qradar_test" name="test_check_columns_exists" time="0.005"/>'
@@ -45,7 +45,10 @@ def test_parse_pack_name(value: str, pack_name: str):
     assert parse_pack_name(value) == pack_name
 
 
-@pytest.mark.parametrize("value", ("", " ", "foo", "Packs.", "Tests.pack1",'Utils.pack1','Packs.Pack', 'Packs..'))
+@pytest.mark.parametrize(
+    "value",
+    ("", " ", "foo", "Packs.", "Tests.pack1", "Utils.pack1", "Packs.Pack", "Packs.."),
+)
 def test_parse_pack_name_error(value: str):
     """
     Given   a string representing an INvalid junit class_name of a content pack
