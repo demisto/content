28d84b6c8c561569d7261268d29f1aaa59810e61
dorschw <81086590+dorschw@users.noreply.github.com>
2024-02-21 16:51:47 +0200

    autopep8

--- Utils/tests/test_parse_junit_per_pack.py
+++ Utils/tests/test_parse_junit_per_pack.py
@@ -21,12 +21,12 @@ def test_parse_xml(tmp_path):
         '<testcase classname="Packs.ipinfo.Integrations.ipinfo_v2.ipinfo_v2_test" name="test_check_columns_exists" time="0.005"/></testsuite>'  # noqa: E501
         '<testsuite name="pytest" errors="0" failures="0" skipped="0" tests="3" time="1.967" timestamp="2024-02-20T15:25:25.799231" hostname="d190b19a16e8">'  # noqa: E501
         '<testcase classname="Packs.qradar.Integrations.qradar.qradar_test" name="test_ipinfo_ip_command" time="0.210"/>'
-        '<testcase classname="Packs.ipinfo.Integrations.qradar.qradar_test" name="test_ipinfo_nultiple_ips_command" time="0.007"/>'
+        '<testcase classname="Packs.ipinfo.Integrations.qradar.qradar_test" name="test_ipinfo_nultiple_ips_command" time="0.007"/>'  # noqa: E501
         '<testcase classname="Packs.ipinfo.Integrations.qradar.qradar_test" name="test_check_columns_exists" time="0.005"/>'
         "</testsuite></testsuites>"
     )
     result = parse_xml(xml_path)
-    assert tuple(result.keys())[0] == "qradar" # test sort
+    assert tuple(result.keys())[0] == "qradar"  # test sort
     assert parse_xml(xml_path) == {"ipinfo": 0.035, "qradar": 0.21}
 
 
