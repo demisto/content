3490a8d1345456bacdac12a5397afca97657db2d
RotemAmit <ramit@paloaltonetworks.com>
2024-02-26 09:48:27 +0200

    removed extra spaces

--- Packs/GenericSQL/Integrations/GenericSQL/GenericSQL.py
+++ Packs/GenericSQL/Integrations/GenericSQL/GenericSQL.py
@@ -223,8 +223,8 @@ def generate_variable_names_and_mapping(bind_variables_values_list: list, query:
 
     bind_variables_names_list = []
     for i in range(len(re.findall(char_to_count, query))):
-        query = query.replace(char_to_replace, f":bind_variable_{i + 1}", 1)
-        bind_variables_names_list.append(f"bind_variable_{i + 1}")
+        query = query.replace(char_to_replace, f":bind_variable_{i+1}", 1)
+        bind_variables_names_list.append(f"bind_variable_{i+1}")
     return dict(zip(bind_variables_names_list, bind_variables_values_list)), query
 
 
