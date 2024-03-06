317ea5c51d540ec00a953725c9fa2540d68b9ca3
Tal <tcarmeli@paloaltonetworks.com>
2024-03-06 13:58:01 +0200

    no pre-commit- fix checkcreate

--- Packs/Redmine/Integrations/Redmine/Redmine.py
+++ Packs/Redmine/Integrations/Redmine/Redmine.py
@@ -43,7 +43,7 @@ ISSUE_PRIORITY_DICT = {
 class Client(BaseClient):
     def __init__(self, server_url, api_key, verify=True, proxy=False, headers=None, auth=None, project_id=None):
         super().__init__(base_url=server_url, verify=verify, proxy=proxy, headers=headers, auth=auth)
-        
+
         self._post_put_header = {'Content-Type': 'application/json', 'X-Redmine-API-Key': api_key}
         self._upload_file_header = {'Content-Type': 'application/octet-stream', 'X-Redmine-API-Key': api_key}
         self._get_header = {'X-Redmine-API-Key': api_key}
@@ -51,7 +51,7 @@ class Client(BaseClient):
 
     def create_issue_request(self, args, project_id=None):
         remove_nulls_from_dictionary(args)
-        uploads = args.pop('uploads', None) ## to remove the out and in of uploads
+        uploads = args.pop('uploads', None)  # to remove the out and in of uploads
         body_for_request = {'issue': args}
         if uploads:
             body_for_request['issue']['uploads'] = uploads
@@ -187,7 +187,7 @@ def map_predefined_values_to_id(predefined_value, converter_dict, error_message)
     return None
 
 
-def convert_args_to_request_format(args): #put pop in line, remove exceptions
+def convert_args_to_request_format(args):  # put pop in line, remove exceptions
     tracker_id = args.pop('tracker_id', None)
     status_id = args.pop('status_id', None)
     priority_id = args.pop('priority_id', None)
