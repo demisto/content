40da3feaa132e762ada7f06729472c9e2e3d17ce
Tal <tcarmeli@paloaltonetworks.com>
2024-03-12 14:15:46 +0200

    no pre-commit- fixing precommit

--- Packs/Redmine/Integrations/Redmine/Redmine.py
+++ Packs/Redmine/Integrations/Redmine/Redmine.py
@@ -463,15 +463,20 @@ def get_issue_by_id_command(client: Client, args: dict[str, Any]):
                                                                          is_auto_json_transform=True,
                                                                          json_transform_mapping={
                                                                              "tracker": JsonTransformer(keys=['name'],
-                                                                                                        func=lambda hdr: hdr.get('name', '')),
+                                                                                                        func=lambda hdr:
+                                                                                                            hdr.get('name', '')),
                                                                              "project": JsonTransformer(keys=['name'],
-                                                                                                        func=lambda hdr: hdr.get('name', '')),
+                                                                                                        func=lambda hdr:
+                                                                                                            hdr.get('name', '')),
                                                                              "status": JsonTransformer(keys=['name'],
-                                                                                                       func=lambda hdr: hdr.get('name', '')),
+                                                                                                       func=lambda hdr:
+                                                                                                           hdr.get('name', '')),
                                                                              "priority": JsonTransformer(keys=['name'],
-                                                                                                         func=lambda hdr: hdr.get('name', '')),
+                                                                                                         func=lambda hdr:
+                                                                                                             hdr.get('name', '')),
                                                                              "author": JsonTransformer(keys=['name'],
-                                                                                                       func=lambda hdr: hdr.get('name', '')),
+                                                                                                       func=lambda hdr:
+                                                                                                           hdr.get('name', '')),
                                                                              "custom_fields":
                                                                                  JsonTransformer(keys=["name", "value"]),
                                                                              "watchers": JsonTransformer(keys=["name"]),
