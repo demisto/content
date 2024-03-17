0e2e3cecfdc5af64491eb66fb10c15657b320bc0
tkatzir <tkatzir@paloaltonetworks.com>
2024-03-17 20:37:26 +0200

    Fix playbook

--- Packs/CommonScripts/Scripts/GenericPollingScheduledTask/GenericPollingScheduledTask.js
+++ Packs/CommonScripts/Scripts/GenericPollingScheduledTask/GenericPollingScheduledTask.js
@@ -75,6 +75,7 @@ function checkCommandSanitized(cmd) {
         if ((cmd_lower.match(regex) || []).length > 1) {
             throw new Error('Error, The value of ' + SANITIZED_ARG_NAMES[i] + ' is malformed.');
         }
+        cmd_lower.replace(current_arg_name_lower, '')
     }
 }
 
