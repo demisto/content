530589e40d7aeae938097a18991077c51f93614e
rshunim <rshunim@paloaltonetworks.com>
2024-03-17 16:55:51 +0200

    collect_from is needed so save it as a parameter

--- Tests/scripts/collect_tests/collect_tests.py
+++ Tests/scripts/collect_tests/collect_tests.py
@@ -827,7 +827,9 @@ class BranchTestCollector(TestCollector):
         collected_packs: list[CollectionResult | None] = []
 
         # diff between master and the last upload
-        for file_path in self._get_git_diff(upload_delta_from_last_upload=True).changed_files:
+        collect_from = self._get_git_diff(upload_delta_from_last_upload=True)
+
+        for file_path in collect_from.changed_files:
 
             full_path = PATHS.content_path / file_path
 
