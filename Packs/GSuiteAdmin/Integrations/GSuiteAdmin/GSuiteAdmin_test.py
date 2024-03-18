530cfa07f9769116cd1b0d5f5b96eb910fdbd825
edik24 <ekatsenelson@paloaltonetworks.com>
2024-03-18 18:15:17 +0200

    CIAC-8731 Enhance G Suite Admin Integration to Support Chrome Policy API and Google Workspace Admin API

--- Packs/GSuiteAdmin/Integrations/GSuiteAdmin/GSuiteAdmin_test.py
+++ Packs/GSuiteAdmin/Integrations/GSuiteAdmin/GSuiteAdmin_test.py
@@ -1279,6 +1279,34 @@ def test_chromebrowser_list_command(gsuite_client, mocker):
     assert command_result.outputs_prefix == 'GSuite.ChromeBrowserDevices'
 
 
+def test_chromebrowser_list_command_no_limit(gsuite_client, mocker):
+    """
+        Scenario: chromebrowserdevice list successful execution.
+
+        Given:
+        - Working API integration and correct parameters
+
+        When:
+        - Calling command chromebrowser_list_command
+
+        Then:
+        - Ensure no error returns
+    """
+    from GSuiteAdmin import chromebrowser_list_command
+    args = {"customer_id": "test"}
+    with open('test_data/chromebrowser_list_response.json') as file:
+        api_response = json.load(file)
+    with open('test_data/chromebrowser_list_context.json') as file:
+        expected_entry_context = json.load(file)
+    mocker.patch('GSuiteAdmin.GSuiteClient.http_request', return_value=api_response)
+    command_result = chromebrowser_list_command(gsuite_client, args)
+    assert command_result.readable_output == expected_entry_context['readable_output']
+    assert command_result.outputs == expected_entry_context['outputs']
+    assert command_result.raw_response == expected_entry_context['raw_response']
+    assert command_result.outputs_key_field == ['deviceId']
+    assert command_result.outputs_prefix == 'GSuite.ChromeBrowserDevices'
+
+
 def test_chromebrowser_list_command_high_limit(gsuite_client, mocker):
     """
         Scenario: chromebrowserdevice list successful execution.
@@ -1357,6 +1385,34 @@ def test_policy_schemas_command(gsuite_client, mocker):
     assert command_result.outputs_prefix == 'GSuite.PolicySchema'
 
 
+def test_policy_schemas_command_no_limit(gsuite_client, mocker):
+    """
+        Scenario: Policy Schema list command successful execution.
+
+        Given:
+        - Working API integration and correct parameters
+
+        When:
+        - Calling command policy_schemas_command
+
+        Then:
+        - Ensure no error returns
+    """
+    from GSuiteAdmin import policy_schemas_command
+    args = {"customer_id": "test"}
+    with open('test_data/policy_schemas_list_reponse.json') as file:
+        api_response = json.load(file)
+    with open('test_data/policy_schemas_list_context.json') as file:
+        expected_entry_context = json.load(file)
+    mocker.patch('GSuiteAdmin.GSuiteClient.http_request', return_value=api_response)
+    command_result = policy_schemas_command(gsuite_client, args)
+    assert command_result.readable_output == expected_entry_context['readable_output']
+    assert command_result.outputs == expected_entry_context['outputs']
+    assert command_result.raw_response == expected_entry_context['raw_response']
+    assert command_result.outputs_key_field == ['name']
+    assert command_result.outputs_prefix == 'GSuite.PolicySchema'
+
+
 def test_policy_schemas_command_high_limit(gsuite_client, mocker):
     """
         Scenario: Policy Schema list command successful execution.
@@ -1412,6 +1468,33 @@ def test_policy_resolve_command(gsuite_client, mocker):
     assert command_result.outputs_key_field == ['deviceId']
     assert command_result.outputs_prefix == 'GSuite.Policy'
 
+
+def test_policy_resolve_command_no_limit(gsuite_client, mocker):
+    """
+        Scenario: Policy resolve command successful execution.
+
+        Given:
+        - Working API integration and correct parameters
+
+        When:
+        - Calling command policy_resolve_command
+
+        Then:
+        - Ensure no error returns
+    """
+    from GSuiteAdmin import policy_resolve_command
+    args = {"customer_id": "test"}
+    with open('test_data/policy_resolve_response.json') as file:
+        api_response = json.load(file)
+    with open('test_data/policy_resolve_context.json') as file:
+        expected_entry_context = json.load(file)
+    mocker.patch('GSuiteAdmin.GSuiteClient.http_request', return_value=api_response)
+    command_result = policy_resolve_command(gsuite_client, args)
+    assert command_result.readable_output == expected_entry_context['readable_output']
+    assert command_result.outputs == expected_entry_context['outputs']
+    assert command_result.raw_response == expected_entry_context['raw_response']
+    assert command_result.outputs_key_field == ['deviceId']
+    assert command_result.outputs_prefix == 'GSuite.Policy'
 
 def test_policy_resolve_command_high_limit(gsuite_client, mocker):
     """
