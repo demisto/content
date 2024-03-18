401f4f9e02bd1dde61f9a80d8160ae11ea317519
GuyAfik <guyafik11@gmail.com>
2024-03-18 09:12:15 +0200

    description and code

--- Packs/Code42/Integrations/Code42EventCollector/Code42EventCollector/Code42EventCollector.py
+++ Packs/Code42/Integrations/Code42EventCollector/Code42EventCollector/Code42EventCollector.py
@@ -1,28 +1,20 @@
+import py42.sdk
+import py42.settings as settings
+
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-"""Base Integration for Cortex XSOAR (aka Demisto)
-
-This is an empty Integration with some basic structure according
-to the code conventions.
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-Developer Documentation: https://xsoar.pan.dev/docs/welcome
-Code Conventions: https://xsoar.pan.dev/docs/integrations/code-conventions
-Linting: https://xsoar.pan.dev/docs/integrations/linting
-
-This is an empty structure file. Check an example at;
-https://github.com/demisto/content/blob/master/Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.py
-
-"""
 
 from CommonServerUserPython import *  # noqa
 
 import urllib3
 from typing import Dict, Any
 
+
 # Disable insecure warnings
 urllib3.disable_warnings()
+
+DEFAULT_FILE_EVENTS_MAX_FETCH = 50000
+DEFAULT_AUDIT_EVENTS_MAX_FETCH = 100000
 
 
 ''' CONSTANTS '''
@@ -32,15 +24,15 @@ DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
 ''' CLIENT CLASS '''
 
 
-class Client(BaseClient):
-    """Client class to interact with the service API
+class Client:
 
-    This Client implements API calls, and does not contain any XSOAR logic.
-    Should only do requests and return data.
-    It inherits from BaseClient defined in CommonServer Python.
-    Most calls use _http_request() that handles proxy, SSL verification, etc.
-    For this  implementation, no special attributes defined
-    """
+    def __init__(self, base_url: str, client_id: str, client_secret: str, verify: bool, **kwargs):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.client = py42.sdk.from_api_client(base_url, client_id=client_id, secret=client_secret)
+        settings.verify_ssl_certs = verify
+        self.client.alerts.
+
 
     # TODO: REMOVE the following dummy function:
     def baseintegration_dummy(self, dummy: str) -> Dict[str, str]:
@@ -115,53 +107,35 @@ def baseintegration_dummy_command(client: Client, args: Dict[str, Any]) -> Comma
 
 
 def main() -> None:
-    """main function, parses params and runs command functions
+    params = demisto.params()
+    client_id: str = params.get('credentials', {}).get('identifier', '')
+    client_secret: str = params.get('credentials', {}).get('password', '')
+    base_url: str = params.get('url', '').rstrip('/')
+    verify_certificate = not params.get('insecure', False)
+    max_fetch_file_events = arg_to_number(params.get("max_file_events_per_fetch")) or DEFAULT_FILE_EVENTS_MAX_FETCH
+    max_fetch_audit_events = params.get("max_audit_events_per_fetch") or DEFAULT_AUDIT_EVENTS_MAX_FETCH
 
-    :return:
-    :rtype:
-    """
-
-    # TODO: make sure you properly handle authentication
-    # api_key = demisto.params().get('credentials', {}).get('password')
-
-    # get the service API url
-    base_url = urljoin(demisto.params()['url'], '/api/v1')
-
-    # if your Client class inherits from BaseClient, SSL verification is
-    # handled out of the box by it, just pass ``verify_certificate`` to
-    # the Client constructor
-    verify_certificate = not demisto.params().get('insecure', False)
-
-    # if your Client class inherits from BaseClient, system proxy is handled
-    # out of the box by it, just pass ``proxy`` to the Client constructor
-    proxy = demisto.params().get('proxy', False)
-
-    demisto.debug(f'Command being called is {demisto.command()}')
+    command = demisto.command()
+    demisto.info(f'Command being called is {command}')
     try:
-
-        # TODO: Make sure you add the proper headers for authentication
-        # (i.e. "Authorization": {api key})
-        headers: Dict = {}
-
         client = Client(
+            client_id=client_id,
+            client_secret=client_secret,
             base_url=base_url,
             verify=verify_certificate,
-            headers=headers,
-            proxy=proxy)
-
-        if demisto.command() == 'test-module':
-            # This is the call made when pressing the integration Test button.
-            result = test_module(client)
-            return_results(result)
-
-        # TODO: REMOVE the following dummy command case:
-        elif demisto.command() == 'baseintegration-dummy':
-            return_results(baseintegration_dummy_command(client, demisto.args()))
-        # TODO: ADD command cases for the commands you will implement
-
-    # Log exceptions and return errors
+        )
+        if command == 'test-module':
+            return_results(test_module(client))
+        elif command == 'fetch-events':
+            events, last_run = fetch_events(client, first_fetch=max_fetch_audit_events, last_run=demisto.getLastRun(), max_fetch=max_fetch_file_events)
+            send_events_to_xsiam(events, vendor=VENDOR, product=PRODUCT)
+            demisto.debug(f'Successfully sent event {[event.get("id") for event in events]} IDs to XSIAM')
+            demisto.setLastRun(last_run)
+        elif command == "cybelangel-get-events":
+            return_results(get_events_command(client, demisto.args()))
     except Exception as e:
-        return_error(f'Failed to execute {demisto.command()} command.\nError:\n{str(e)}')
+        demisto.error(traceback.format_exc())
+        return_error(f"Failed to execute {command} command.\nError:\ntype:{type(e)}, error:{str(e)}")
 
 
 ''' ENTRY POINT '''
