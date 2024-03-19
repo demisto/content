89f2f5463b6afa92ac81c270add6a5ee4c3c14ba
Dan Tavori <dtavori@paloaltonetworks.com>
2024-03-20 00:23:57 +0200

    fix cut

--- Packs/FiltersAndTransformers/Scripts/Cut/Cut.js
+++ Packs/FiltersAndTransformers/Scripts/Cut/Cut.js
@@ -1,4 +1,30 @@
+function parseValue(value) {
+    /* This function is a workaround for an issue where passing a date field from the context
+       to the "value" argument, in JS it is accepted as an object, while in python it is a string.
+       To solve this discrapancy, we ensure the value is stringified correctly before running
+       the cut() function. */
+    if (typeof value === "string") {
+        return value;
+    } else if (typeof value === "number") {
+        return "" + value;
+    }
+    value = JSON.stringify(value);
+    if (value.startsWith('"') && value.endsWith('"')) {
+        // potentially a date object, wrapped with quotes
+        value = value.slice(1, -1);
+        try {
+            Date(value);
+            return value;
+        } catch(e) {
+            // value type should be supported for Cut
+        }
+    }
+    // value type should be supported for Cut (either a boolean or a dict)
+    throw "Unsupported value for Cut: " + value;
+}
+
 function cut(value, fields, delim) {
+    value = parseValue(value);
     if (delim === "''") {
         delim = "";
     }
