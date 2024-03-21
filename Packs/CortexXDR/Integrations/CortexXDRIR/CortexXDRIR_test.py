cd46a43f2ca35409fd4917dd4aa686a127010de6
maimorag <mmorag@paloaltonetworks.com>
2024-03-21 10:09:14 +0200

    docker

--- Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR_test.py
+++ Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR_test.py
@@ -1267,5 +1267,3 @@ def test_get_incident_extra_data_incident_not_exist(mocker):
     with pytest.raises(DemistoException) as e:
         _, outputs, raw_incident = get_incident_extra_data_command(client, args)
     assert str(e.value) == 'Incident 1 is not found'
-    
-
