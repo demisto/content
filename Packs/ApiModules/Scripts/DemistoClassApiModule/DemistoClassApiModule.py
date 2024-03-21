dde84f6f2128010a1d703d1e6500bc37296426e7
Dan Tavori <dtavori@paloaltonetworks.com>
2024-03-21 15:40:09 +0200

    comments, logs and other minor changes

--- Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
+++ Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
@@ -1,32 +1,30 @@
 import demistomock as demisto  # noqa: F401
+import contextlib
 
+# Only the following integrations and scripts are using the Demisto class wrapper
 DEMISTO_WRAPPER_INTEGRATIONS = ['Cortex XDR - IR', 'QRadar v3', 'SlackV3', 'ServiceNow v2']
 DEMISTO_WRAPPER_SCRIPTS = ['UnzipFile', 'DBotFindSimilarIncidents', 'ParseCSV']
 
 
-try:
-    class DemistoWrapper(Demisto):   # type:ignore  [name-defined] # noqa: F821 # pylint: disable=E0602
-        """Wrapper class to interface with the Demisto server via stdin, stdout"""
+# NameError will be raised only in tests, where a Demisto class isn't defined.
+with contextlib.suppress(NameError):
+    class DemistoWrapper(Demisto):  # type:ignore  [name-defined] # noqa: F821 # pylint: disable=E0602
+        """A content-side wrapper to the builtin Demisto class.
+        All methods of this class can be executed in both scripts and integrations
+        (E.g., self.results). """
 
-        def __init__(self, context):
-            super().__init__(context)
-
-    class DemistoWrapperScript(DemistoWrapper):
+    class DemistoScript(DemistoWrapper):
         def getFilePath(self, id):
-            self.debug("Wrapper Class: Getting file path with id: {id}.".format(id=id))
+            self.debug("Getting path of file entry with ID {}".format(id))
             return super().getFilePath(id)
 
-    class DemistoWrapperIntegration(DemistoWrapper):
+    class DemistoIntegration(DemistoWrapper):
         def incidents(self, incidents):
-            if incidents:
-                self.debug("Wrapper Class: Creating {num_incidents} incidents.".format(num_incidents=len(incidents)))
+            if isinstance(incidents, list):
+                self.debug("[fetch-incidents] Creating {} incidents".format(len(incidents)))
             super().incidents(incidents)
 
     if demisto.callingContext.get('context', {}).get('IntegrationBrand', '') in DEMISTO_WRAPPER_INTEGRATIONS:
-        demisto.__class__ = DemistoWrapperIntegration
+        demisto.__class__ = DemistoIntegration
     elif demisto.callingContext.get('context', {}).get('ScriptName', '') in DEMISTO_WRAPPER_SCRIPTS:
-        demisto.__class__ = DemistoWrapperScript
-
-except NameError:
-    # try except for CommonServerPython tests.
-    pass
+        demisto.__class__ = DemistoScript
