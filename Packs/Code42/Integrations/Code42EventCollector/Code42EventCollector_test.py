a10915e2c65038d62d0666f707dda68791af2a43
GuyAfik <guyafik11@gmail.com>
2024-03-21 16:30:04 +0200

    readme

--- Packs/Code42/Integrations/Code42EventCollector/Code42EventCollector_test.py
+++ Packs/Code42/Integrations/Code42EventCollector/Code42EventCollector_test.py
@@ -1,18 +1,63 @@
+from unittest.mock import MagicMock
+
+import requests_toolbelt.sessions
 
 from CommonServerPython import *
 
 
-class HttpRequestsMocker:
+TEST_URL = "https://test.com"
 
-    def __init__(self, num_of_events: int):
-        self.num_of_events = num_of_events
-        self.num_of_calls = 0
 
-    def valid_http_request_side_effect(self, method: str, url_suffix: str = "", params: Dict | None = None, **kwargs):
-        if method == "GET" and url_suffix == "/api/v2/reports":
-            start_date = params.get("start-date")
-            events = create_events(1, amount_of_events=self.num_of_events, start_date=start_date)
-            return create_mocked_response(events)
-        if method == "POST" and kwargs.get("full_url") == "https://auth.cybelangel.com/oauth/token":
-            return {"access_token": "new_access_token"}
-        return None
+def create_mocked_response(response: List[Dict] | Dict, status_code: int = 200) -> requests.Response:
+    mocked_response = requests.Response()
+    mocked_response._content = json.dumps(response).encode('utf-8')
+    mocked_response.status_code = status_code
+    return mocked_response
+
+
+# class HttpRequestsMocker:
+#
+#     def __init__(self, num_of_events: int):
+#         self.num_of_events = num_of_events
+#         self.num_of_calls = 0
+#
+#     def valid_http_request_side_effect(self, method: str, url_suffix: str = "", params: Dict | None = None, **kwargs):
+#         mocker.patch.object(Session, "get", return_value="bla")
+#         if method == "GET" and url_suffix == "/api/v2/reports":
+#             start_date = params.get("start-date")
+#             events = create_events(1, amount_of_events=self.num_of_events, start_date=start_date)
+#             return create_mocked_response(events)
+#         if method == "POST" and kwargs.get("full_url") == "https://auth.cybelangel.com/oauth/token":
+#             return {"access_token": "new_access_token"}
+#         return None
+
+
+def test_the_test_module(mocker):
+    from requests import Session
+    import Code42EventCollector
+
+    return_results_mocker: MagicMock = mocker.patch.object(Code42EventCollector, 'return_results')
+    mocker.patch.object(
+        demisto,
+        'params',
+        return_value={
+            "url": TEST_URL,
+            "credentials": {
+                "identifier": "1234",
+                "password": "1234",
+            },
+        }
+    )
+    mocker.patch.object(demisto, 'command', return_value='test-module')
+
+    mocker.patch.object(requests_toolbelt.sessions.BaseUrlSession, "request", return_value=create_mocked_response(response=[]))
+
+    mocker.patch.object(
+        Session,
+        "get",
+        return_value="ok"
+    )
+
+    Code42EventCollector.main()
+    assert return_results_mocker.called
+    assert return_results_mocker.call_args[0][0] == "ok"
--- Packs/Code42/Integrations/Code42EventCollector/README.md
+++ Packs/Code42/Integrations/Code42EventCollector/README.md
@@ -1,3 +1,46 @@
-Code42 Insider Risk software solutions provide the right balance of transparency, technology and training to detect and appropriately respond to data risk.
+Code42 Insider Risk software solutions provide the right balance of transparency, technology and training to detect and appropriately respond to data risk. Use the Code42EventCollector integration to fetch file events and audit logs.
+This integration was integrated and tested with version xx of Code42EventCollector.
 
-Use the Code42EventCollector integration to fetch file events and audit logs.
+## Configure Code42EventCollector on Cortex XSOAR
+
+1. Navigate to **Settings** > **Integrations** > **Servers & Services**.
+2. Search for Code42EventCollector.
+3. Click **Add instance** to create and configure a new integration instance.
+
+    | **Parameter** | **Required** |
+    | --- | --- |
+    | Server URL (e.g. https://api.us.code42.com, see help section) | True |
+    | API Client ID | True |
+    | API Client Secret | True |
+    | Maximum number of file events per fetch | True |
+    | Maximum number of audit events per fetch | True |
+    | Trust any certificate (not secure) | False |
+
+4. Click **Test** to validate the URLs, token, and connection.
+
+## Commands
+
+You can execute these commands from the Cortex XSOAR CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
+
+### code42-get-events
+
+***
+Manual command to get events, used mainly for debugging
+
+#### Base Command
+
+`code42-get-events`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| start_date | from which time to get events. | Required | 
+| end_date | until which time to get events. | Required | 
+| limit | the maximum number of events to return. Default is 100. | Required | 
+| event_type | the type of event to return. Possible values are: audit-logs, file-events. | Required | 
+
+#### Context Output
+
+There is no context output for this command.
