619d6d942cdc7b852afefad6325a0281d2edf2ac
efelmandar <efelmandar@paloaltonetworks.com>
2024-03-24 22:29:41 +0000

    Add unitests

--- Packs/CertStream/Integrations/CertStream/CertStream_test.py
+++ Packs/CertStream/Integrations/CertStream/CertStream_test.py
@@ -1,15 +1,33 @@
-import unittest
-from CertStream import websocket_connections
+import pytest
+import demistomock as demisto
+from CertStream import *
 
 
-class TestCertStream(unittest.TestCase):
+@pytest.mark.parametrize("list_name", [
+    ("testdomainlist")])
+def test_get_homographs_list(list_name: str, mocker):
+    mocker.patch.object(
+        demisto,
+        'internalHttpRequest',
+        return_value={
+            'body': json.dumps([{'id': 'testdomainlist', 'data': '{"test.com": "test.domain.com"}'}]),
+            'headers': json.dumps({'statusCode': '200'})
+        })
+    result = get_homographs_list(list_name)
 
-    def test_websocket_connections_success(self):
-        host = "wss://certstream.calidog.io"
-        with websocket_connections(host) as ws:
-            assert ws is not None
+    assert result
 
-    def test_websocket_connections_invalid_host(self):
-        host = "invalidhost"
-        with self.assertRaises(ConnectionError), websocket_connections(host):
-            pass
+
+@pytest.mark.parametrize("original_string,reference_string,expected_result", [
+    ("test.domain.com", "test.domain.com", 0),
+    ("test.domain.com", "", 15),
+    ("paypal.com", "paypàl.com", 1)
+])
+def test_levenshtein_distance(original_string: str, reference_string: str, expected_result: int):
+
+    result = levenshtein_distance(original_string, reference_string)
+
+    assert result == expected_result
+
+
+# Additional tests for other commands
