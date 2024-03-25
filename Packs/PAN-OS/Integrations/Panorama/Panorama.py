43e3985baf381096d8b43a39ae297a42b3538280
michal-dagan <mdagan@paloaltonetworks.com>
2024-03-25 17:28:06 +0200

    update the corr

--- Packs/PAN-OS/Integrations/Panorama/Panorama.py
+++ Packs/PAN-OS/Integrations/Panorama/Panorama.py
@@ -55,7 +55,7 @@ DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
 QUERY_DATE_FORMAT = '%Y/%m/%d %H:%M:%S'
 FETCH_DEFAULT_TIME = '1 day'
 MAX_INCIDENTS_TO_FETCH = 100
-FETCH_INCIDENTS_LOG_TYPES = ['Traffic', 'Threat', 'URL', 'Data', 'Correlation', 'System', 'Wildfire', 'Decryption']
+FETCH_INCIDENTS_LOG_TYPES = ['Traffic', 'Threat', 'URL', 'Data', 'Corr', 'System', 'Wildfire', 'Decryption']
 GET_LOG_JOB_ID_MAX_RETRIES = 10
 
 XPATH_SECURITY_RULES = ''
@@ -14304,6 +14304,7 @@ def log_types_queries_to_dict(params: Dict[str, str]) -> Dict[str, str]:
         # if 'All' is chosen in Log Type (log_types) parameter then all query parameters are used, else only the chosen query parameters are used.
         active_log_type_queries = FETCH_INCIDENTS_LOG_TYPES if 'All' in log_types else log_types
         for log_type in active_log_type_queries:
+            log_type = "corr" if log_type == "Correlation" else log_type
             log_type_query = params.get(f'{log_type.lower()}_query', "")
             if log_type_query:
                 queries_dict[log_type.capitalize()] = log_type_query
@@ -14390,6 +14391,7 @@ def test_fetch_incidents_parameters(fetch_params):
         # if 'All' is chosen in Log Type (log_types) parameter then all query parameters are used, else only the chosen query parameters are used.
         active_log_type_queries = FETCH_INCIDENTS_LOG_TYPES if 'All' in log_types else log_types
         for log_type in active_log_type_queries:
+            log_type = "corr" if log_type == "Correlation" else log_type
             log_type_query = fetch_params.get(f'{log_type.lower()}_query', "")
             if not log_type_query:
                 raise DemistoException(f"{log_type} Log Type Query parameter is empty. Please enter a valid query.")
