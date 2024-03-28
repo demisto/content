2ca09c06d25a52259d3fbdd716ba6bc5ad34e964
Shmuel Kroizer <skroizer@paloaltonetworks.com>
2024-03-28 14:46:57 +0200

    fix ruff

--- Packs/CommonScripts/Scripts/FileCreateAndUploadV2/FileCreateAndUploadV2.py
+++ Packs/CommonScripts/Scripts/FileCreateAndUploadV2/FileCreateAndUploadV2.py
@@ -1,39 +1,36 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
 import base64
 from pathlib import Path
 from typing import Any
 
+import demistomock as demisto
+from CommonServerPython import *
 
-def get_data_from_file(entry_id: str):
-    """
-    This function reads the file associated with the entry_id and returns its data as bytes.
-    """
+
+def get_data_from_file(entry_id: str) -> bytes:
+    """Reads the file associated with the entry_id and returns its data as bytes."""
     try:
-        return Path(demisto.getFilePath(entry_id)['path']).read_bytes()
+        return Path(demisto.getFilePath(entry_id)["path"]).read_bytes()
     except Exception as e:
         raise DemistoException(f'There was a problem opening or reading the file.\nError is: {e}')
 
 
 def decode_data(data: Any, data_encoding: str) -> bytes | Any:
-    """
-    Given data and its encoding, this function decodes the data according to the provided encoding and returns it.
-    """
+    """Given data and its encoding, this function decodes the data according to the provided encoding and returns it."""
     match data_encoding:
-        case 'base64':
+        case "base64":
             return base64.b64decode(data)
-        case 'raw':
+        case "raw":
             return data
         case _:
             raise ValueError(f'Invalid data encoding value: {data_encoding}, must be either `base64` or `raw`')
 
 
-def main():
+def main() -> None:
     args = demisto.args()
-    filename = args.get('filename', '')
-    data = args.get('data', '')
-    data_encoding = args.get('data_encoding', 'raw')
-    entry_id = args.get('entryId')
+    filename = args["filename"]
+    data = args.get("data")
+    data_encoding = args.get("data_encoding", "raw")
+    entry_id = args.get("entryId")
 
     try:
         if entry_id:
@@ -46,5 +43,5 @@ def main():
         return_error(str(e) + "\n\nTrace:\n" + traceback.format_exc())
 
 
-if __name__ in ('__builtin__', 'builtins', '__main__'):
+if __name__ in ("__builtin__", "builtins", "__main__"):
     main()
--- Packs/CommonScripts/Scripts/FileCreateAndUploadV2/FileCreateAndUploadV2_test.py
+++ Packs/CommonScripts/Scripts/FileCreateAndUploadV2/FileCreateAndUploadV2_test.py
@@ -1,11 +1,13 @@
-import demistomock as demisto
 import json
 import sys
-import pytest
 from pathlib import Path
-from CommonServerPython import DemistoException
+
+import pytest
+from FileCreateAndUploadV2 import decode_data, get_data_from_file, main
 from pytest_mock import MockerFixture
-from FileCreateAndUploadV2 import get_data_from_file, decode_data, main
+
+import demistomock as demisto
+from CommonServerPython import DemistoException
 
 
 def side_effect_sys_exit(code):
@@ -13,7 +15,6 @@ def side_effect_sys_exit(code):
 
 
 def test_main(mocker):
-    from FileCreateAndUploadV2 import main
 
     mocker.patch.object(sys, 'exit', side_effect=side_effect_sys_exit)
 
@@ -34,95 +35,88 @@ def test_main(mocker):
         assert (eval['ok'] and results['Type'] == 3) or ((not eval['ok']) and results['Type'] != 3)
 
 
-def test_main_with_entry_id(mocker: MockerFixture):
-    """
-    Given valid arguments including an entry_id,
+def test_main_with_entry_id(mocker: MockerFixture) -> None:
+    """Given valid arguments including an entry_id,
     When the main function is called,
     Then it should not raise any exceptions.
     """
-    mocker.patch.object(demisto, 'args', return_value={
-        'filename': 'test_file',
-        'data': 'test_data',
-        'data_encoding': 'raw',
-        'entryId': 'test_entry_id'
+    mocker.patch.object(demisto, "args", return_value={
+        "filename": "test_file",
+        "data": "test_data",
+        "data_encoding": "raw",
+        "entryId": "test_entry_id",
     })
-    mocker.patch('FileCreateAndUploadV2.get_data_from_file', return_value=b'test_data')
-    mocker.patch('FileCreateAndUploadV2.decode_data', return_value=b'test_data')
-    mocker.patch.object(demisto, 'results')
+    mocker.patch("FileCreateAndUploadV2.get_data_from_file", return_value=b"test_data")
+    mocker.patch("FileCreateAndUploadV2.decode_data", return_value=b"test_data")
+    mocker.patch.object(demisto, "results")
 
     main()
     results = demisto.results.call_args[0][0]
 
-    assert results["File"] == 'test_file'
-    assert results["ContentsFormat"] == 'text'
+    assert results["File"] == "test_file"
+    assert results["ContentsFormat"] == "text"
 
 
-def test_main_without_entry_id(mocker: MockerFixture):
-    """
-    Given valid arguments without an entry_id,
+def test_main_without_entry_id(mocker: MockerFixture) -> None:
+    """Given valid arguments without an entry_id,
     When the main function is called,
     Then it should not raise any exceptions.
     """
-    mocker.patch.object(demisto, 'args', return_value={
-        'filename': 'test_file',
-        'data': 'test_data',
-        'data_encoding': 'raw'
+    mocker.patch.object(demisto, "args", return_value={
+        "filename": "test_file",
+        "data": "test_data",
+        "data_encoding": "raw",
     })
-    mocker.patch('FileCreateAndUploadV2.decode_data', return_value=b'test_data')
-    mocker.patch.object(demisto, 'results')
+    mocker.patch("FileCreateAndUploadV2.decode_data", return_value=b"test_data")
+    mocker.patch.object(demisto, "results")
     main()
     results = demisto.results.call_args[0][0]
 
-    assert results["File"] == 'test_file'
-    assert results["ContentsFormat"] == 'text'
+    assert results["File"] == "test_file"
+    assert results["ContentsFormat"] == "text"
 
 
-def test_get_data_from_file(mocker: MockerFixture):
-    """
-    Given a valid entry_id,
+def test_get_data_from_file(mocker: MockerFixture) -> None:
+    """Given a valid entry_id,
     When the get_data_from_file function is called,
     Then it should return the data read from the file.
     """
-    mocker.patch.object(Path, 'read_bytes', return_value=b'test_data')
-    mocker.patch.object(demisto, 'getFilePath', return_value={'path': 'test_path'})
-    assert get_data_from_file('test_entry_id') == b'test_data'
+    mocker.patch.object(Path, "read_bytes", return_value=b"test_data")
+    mocker.patch.object(demisto, "getFilePath", return_value={"path": "test_path"})
+    assert get_data_from_file("test_entry_id") == b"test_data"
 
 
-def test_get_data_from_file_exception(mocker: MockerFixture):
-    """
-    Given an invalid entry_id,
+def test_get_data_from_file_exception(mocker: MockerFixture) -> None:
+    """Given an invalid entry_id,
     When the get_data_from_file function is called,
     Then it should raise a DemistoException.
     """
-    mocker.patch.object(Path, 'read_bytes', side_effect=Exception('Error'))
-    mocker.patch.object(demisto, 'getFilePath', return_value={'path': 'test_path'})
-    with pytest.raises(DemistoException, match='There was a problem opening or reading the file.\nError is: Error'):
-        get_data_from_file('test_entry_id')
+    mocker.patch.object(Path, "read_bytes", side_effect=Exception("Error"))
+    mocker.patch.object(demisto, "getFilePath", return_value={"path": "test_path"})
+    with pytest.raises(DemistoException, match="There was a problem opening or reading the file.\nError is: Error"):
+        get_data_from_file("test_entry_id")
 
 
-def test_decode_data_base64():
-    """
-    Given a base64 encoded data,
+def test_decode_data_base64() -> None:
+    """Given a base64 encoded data,
     When the decode_data function is called with data_encoding as 'base64',
     Then it should return the decoded data.
     """
-    assert decode_data(b'dGVzdF9kYXRh', 'base64') == b'test_data'
+    assert decode_data(b"dGVzdF9kYXRh", "base64") == b"test_data"
 
 
-def test_decode_data_raw():
-    """
-    Given a raw data,
+def test_decode_data_raw() -> None:
+    """Given a raw data,
     When the decode_data function is called with data_encoding as 'raw',
     Then it should return the same data.
     """
-    assert decode_data(b'test_data', 'raw') == b'test_data'
+    assert decode_data(b"test_data", "raw") == b"test_data"
 
 
-def test_decode_data_invalid():
-    """
-    Given a data,
+def test_decode_data_invalid() -> None:
+    """Given a data,
     When the decode_data function is called with an invalid data_encoding,
     Then it should raise a ValueError.
     """
-    with pytest.raises(ValueError, match='Invalid data encoding value: invalid, must be either `base64` or `raw`'):
-        decode_data(b'test_data', 'invalid')
+    with pytest.raises(ValueError, match="Invalid data encoding value: invalid, must be either `base64` or `raw`"):
+        decode_data(b"test_data", "invalid")
