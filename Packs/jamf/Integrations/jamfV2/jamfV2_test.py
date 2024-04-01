b590d4d27bea45e99f716875fc06dc63fc879132
Yehuda <yrosenberg@paloaltonetworks.com>
2024-04-01 11:18:19 +0300

    pre commit

--- Packs/jamf/Integrations/jamfV2/jamfV2_test.py
+++ Packs/jamf/Integrations/jamfV2/jamfV2_test.py
@@ -536,7 +536,7 @@ def test_generate_token__basic_auth_no_token(mocker):
     Then:
         - Ensure the http_request will use the username and password for authentication (basic auth) since
         their is no token, and the basic_auth_flag is set to True
-        
+
     """
 
     from jamfV2 import Client
