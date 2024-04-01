aca9931d0a783be1ff3b7dd0520675100359d2ef
Yuval Hayun <70104171+YuvHayun@users.noreply.github.com>
2024-04-01 20:17:32 +0300

    Add gentec security center (#33324)
    * init pack
    
    * work in progress
    
    * testing
    
    * testing
    
    * fixes
    
    * mid save
    
    * mid save
    
    * test
    
    * test
    
    * temp save
    
    * in progress
    
    * added tests
    
    * update docs
    
    * ready
    
    * pre commit fixes
    
    * pre commit fixes
    
    * cr fixes
    
    * add response example
    
    * modeling-rules
    
    * in progress
    
    * in progress
    
    * add dedup system
    
    * modeling-rules-fix
    
    * remove print
    
    * tests fix
    
    * fixes
    
    * in block
    
    * fixes
    
    * fixes
    
    * added rn
    
    * changes
    
    * docs fix
    
    * Bump pack from version Base to 1.33.46.
    
    * mid save
    
    * added args
    
    * fixes
    
    * modeling-rule-minor-fixes
    
    * fix
    
    * fix
    
    * Bump pack from version Base to 1.33.47.
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    ---------
    
    Co-authored-by: Chanan Welt <cwelt@paloaltonetworks.com>
    Co-authored-by: Content Bot <bot@demisto.com>

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -11506,7 +11506,8 @@ def split_data_to_chunks(data, target_chunk_size):
 
 
 def send_events_to_xsiam(events, vendor, product, data_format=None, url_key='url', num_of_attempts=3,
-                         chunk_size=XSIAM_EVENT_CHUNK_SIZE, should_update_health_module=True):
+                         chunk_size=XSIAM_EVENT_CHUNK_SIZE, should_update_health_module=True,
+                         add_proxy_to_request=False):
     """
     Send the fetched events into the XDR data-collector private api.
 
@@ -11537,6 +11538,9 @@ def send_events_to_xsiam(events, vendor, product, data_format=None, url_key='url
     :type should_update_health_module: ``bool``
     :param should_update_health_module: whether to trigger the health module showing how many events were sent to xsiam
 
+    :type add_proxy_to_request :``bool``
+    :param add_proxy_to_request: whether to add proxy to the send evnets request.
+
     :return: None
     :rtype: ``None``
     """
@@ -11549,7 +11553,8 @@ def send_events_to_xsiam(events, vendor, product, data_format=None, url_key='url
         num_of_attempts,
         chunk_size,
         data_type="events",
-        should_update_health_module=should_update_health_module
+        should_update_health_module=should_update_health_module,
+        add_proxy_to_request=add_proxy_to_request,
     )
 
 
@@ -11660,7 +11665,8 @@ def has_passed_time_threshold(timestamp_str, seconds_threshold):
 
 
 def send_data_to_xsiam(data, vendor, product, data_format=None, url_key='url', num_of_attempts=3,
-                       chunk_size=XSIAM_EVENT_CHUNK_SIZE, data_type=EVENTS, should_update_health_module=True):
+                       chunk_size=XSIAM_EVENT_CHUNK_SIZE, data_type=EVENTS, should_update_health_module=True,
+                       add_proxy_to_request=False):
     """
     Send the supported fetched data types into the XDR data-collector private api.
 
@@ -11694,6 +11700,9 @@ def send_data_to_xsiam(data, vendor, product, data_format=None, url_key='url', n
     :type should_update_health_module: ``bool``
     :param should_update_health_module: whether to trigger the health module showing how many events were sent to xsiam
         This can be useful when using send_data_to_xsiam in batches for the same fetch.
+
+    :type add_proxy_to_request: ``bool``
+    :param add_proxy_to_request: whether to add proxy to the send evnets request.
 
     :return: None
     :rtype: ``None``
@@ -11778,7 +11787,7 @@ def send_data_to_xsiam(data, vendor, product, data_format=None, url_key='url', n
         demisto.error(header_msg + api_call_info)
         raise DemistoException(header_msg + error, DemistoException)
 
-    client = BaseClient(base_url=xsiam_url)
+    client = BaseClient(base_url=xsiam_url, proxy=add_proxy_to_request)
     data_chunks = split_data_to_chunks(data, chunk_size)
     for data_chunk in data_chunks:
         data_size += len(data_chunk)
