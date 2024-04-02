e11e0f9a38e9f22612ccbff49bf4a0585e1a45ed
ilan <ierukhimovic@paloaltonetworks.com>
2024-04-02 09:40:07 +0300

    dont override os.cpu_count in gitlab

--- .gitlab/ci/override_os.py
+++
@@ -1,5 +0,0 @@
-from demisto_sdk.commands.common.cpu_count import cpu_count
-import os
-
-# This will set the `os.cpu_count` to the `cpu_count` function, which is the real CPUs we use in the build
-os.cpu_count = cpu_count
