644e68c5ad57448c4885451fdf62b13058cf4cad
Tal <tcarmeli@paloaltonetworks.com>
2024-04-02 18:48:12 +0300

    fix pre

--- Packs/FeedCrowdstrikeFalconIntel/Integrations/CrowdStrikeIndicatorFeed/CrowdStrikeIndicatorFeed.py
+++ Packs/FeedCrowdstrikeFalconIntel/Integrations/CrowdStrikeIndicatorFeed/CrowdStrikeIndicatorFeed.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 import copy
@@ -413,7 +414,9 @@ def create_relationships(
     return relationships
 
 
-def change_actors_from_id_to_name(indicator_actors_array: List[str], get_name_of_actors__func):
+def change_actors_from_id_to_name(
+    indicator_actors_array: List[str], get_name_of_actors__func: Callable[[str], List[Dict[Any, Any]]]
+):
     integration_context = get_integration_context()
     actors_to_convert = []
     converted_actors_array = []
