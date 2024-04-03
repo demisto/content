e72b649c8edd77f46f4fd748c85efb9a283069ff
andrew <andrew.simons@endace.com>
2024-04-03 11:27:13 -0500

    Updating to only include v3

--- Packs/Endace/Integrations/Endace/Endace.py
+++ Packs/Endace/Integrations/Endace/Endace.py
@@ -1,3 +1,29 @@
+# --------------------------------------------------------------------------------------------------------------------
+# MIT
+# Copyright 2020 to 2024 Endace Technology Limited
+# LEGAL NOTICE: This source code:
+# a) is copyright (C) to Endace Technology Limited or any of its related or affiliated companies, a New Zealand company,
+# and its suppliers and licensors ("Endace"), 2020 to 2024.
+# All rights reserved (except as provided in the license terms referred to in clause (b) below); and
+# b) is licensed to you on the terms of the MIT license, detailed below.
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# --------------------------------------------------------------------------------------------------------------------
+
 from CommonServerPython import *
 from CommonServerUserPython import *
 import time
@@ -447,6 +473,7 @@ class EndaceApp(object):
 
     #  search
     def create_search_task(self, args=None):
+
         """ create a search task on Endace Probe
         Args: dict
         Returns:
@@ -517,6 +544,7 @@ class EndaceApp(object):
         return result
 
     def get_search_status(self, args=None):
+
         """ get status of the search task on Endace Probe
            Args: string - Job ID
            Returns:
@@ -664,6 +692,7 @@ class EndaceApp(object):
 
     #  archive
     def create_archive_task(self, args=None):
+
         """ create an archive task on Endace Probe
         Args: dict
         Returns:
@@ -787,6 +816,7 @@ class EndaceApp(object):
         return result
 
     def get_archive_status(self, args=None):
+
         """ get status of the archive task on Endace Probe
            Args: string - Archived File Name
            Returns:
@@ -895,6 +925,7 @@ class EndaceApp(object):
         return result
 
     def delete_archived_file(self, args=None):
+
         """ Delete archived file on Endace Probe
            Args: string - Archived File Name
            Returns:
--- Packs/Endace/Integrations/Endace/Endace_test.py
+++ Packs/Endace/Integrations/Endace/Endace_test.py
@@ -1,3 +1,29 @@
+# --------------------------------------------------------------------------------------------------------------------
+# MIT
+# Copyright 2020 to 2024 Endace Technology Limited
+# LEGAL NOTICE: This source code:
+# a) is copyright (C) to Endace Technology Limited or any of its related or affiliated companies, a New Zealand company,
+# and its suppliers and licensors ("Endace"), 2020 to 2024.
+# All rights reserved (except as provided in the license terms referred to in clause (b) below); and
+# b) is licensed to you on the terms of the MIT license, detailed below.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# --------------------------------------------------------------------------------------------------------------------
+
+
 import pytest
 import Endace
 from CommonServerPython import *
