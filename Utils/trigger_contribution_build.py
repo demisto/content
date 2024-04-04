e3ff1d86e7f4ccea98650dc43b3d7c0e3f455686
Jasmine Beilin <71636766+JasBeilin@users.noreply.github.com>
2024-04-04 11:48:04 +0300

    Revert "[Trigger contribution build] Add missing ENV variable in trigger pipe…" (#33759)
    This reverts commit 03aeafebbf0cdbaf8726b50f13cd5b70256754da.

--- Utils/trigger_contribution_build.py
+++ Utils/trigger_contribution_build.py
@@ -78,22 +78,15 @@ def handle_contribution_prs(args, github_issues: PaginatedList[Issue], gitlab_pr
 
             # get the GitLab branch object corresponding to the GitHub branch
             if branch := gitlab_project.branches.get(github_branch_name):
-                logging.info(
-                    f"Trigger contribution build for PR: {pull_request.number}, \
-                        with base branch: {pull_request.base.ref}, contrib branch: {pull_request.head.label}"
-                )
+                logging.info(f"--- Handling branch: {branch.name}. ---")
 
                 cancel_active_pipelines(gitlab_project, branch)
 
                 variables = {
-                    "CONTRIB_BRANCH": pull_request.head.label,
+                    "CONTRIB_BRANCH": branch.name,
                     "PULL_REQUEST_NUMBER": str(pull_request.number),
-                    "PR_NUMBER": str(pull_request.number),
                     "CI_COMMIT_BRANCH": branch.name,
                     "CI_PIPELINE_SOURCE": "contrib",
-                    "CONTRIB_REPO": pull_request.head.repo.name,
-                    "BASE_BRANCH": pull_request.base.ref,
-                    "USERNAME": pull_request.head.user.login
                 }
                 new_pipeline = gitlab_project.trigger_pipeline(
                     ref=branch.name,
