222b34f11683e51c2cda4546c022534f9c23cd9e
epintzov <epintzov@paloaltonetworks.com>
2024-04-07 13:18:17 +0300

    test module

--- Packs/PhishLabs/Integrations/PhishLabsIOC_EIR/PhishLabsIOC_EIR.py
+++ Packs/PhishLabs/Integrations/PhishLabsIOC_EIR/PhishLabsIOC_EIR.py
@@ -41,7 +41,7 @@ class Client(BaseClient):
         Returns:
             Response json
         """
-        return self.get_incidents(limit=2)
+        return self.get_incidents(limit=1)
 
     def get_incidents(self, status: Optional[str] = None, created_after: Optional[str] = None,
                       created_before: Optional[str] = None, closed_before: Optional[str] = None,
