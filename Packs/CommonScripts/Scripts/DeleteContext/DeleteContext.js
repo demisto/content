0dba47dccdd747b43227529f3d76c123202c94ce
barryyosi-panw <158817412+barryyosi-panw@users.noreply.github.com>
2024-04-08 10:47:59 +0300

    fixing all keys deletion (#33760)
    * fixing all keys deletion
    
    * Bump pack from version CommonScripts to 1.14.27.
    
    * fixing all keys deletions
    
    * lint
    
    * TPB format
    
    ---------
    
    Co-authored-by: Content Bot <bot@demisto.com>

--- Packs/CommonScripts/Scripts/DeleteContext/DeleteContext.js
+++ Packs/CommonScripts/Scripts/DeleteContext/DeleteContext.js
@@ -1,3 +1,5 @@
+LINE_SEPARATOR = '\n';
+
 function errorEntry(text) {
     return  {
         ContentsFormat: formats.text,
@@ -14,36 +16,36 @@ function hasDuplicates(arr) {
 
 /**
  * Deletes keys from the context and handles errors.
- * @param {Array<string>} keys - An array of keys to delete.
+ * @param {Array<string>} keysToDelete - An array of keys to delete.
+ * @param {Array<string>} _keysToKeep - An array of keys to keep. (If a key exists in both, it won't be deleted)
  * @returns {string} A message summarizing the outcome of the delete operation.
  */
-function deleteKeys(keysToDelete) {
+function deleteKeys(keysToDelete = [], _keysToKeep = []) {
     var deletedKeys = []
     var errors = []
-    var message = "";
+    var message = '';
     for (var key of keysToDelete) {
         const originalKey = typeof key === "string" ? key.trim() : key;
-        if (!dq(invContext, originalKey)) {
-            errors.push(`key does not exist: ${originalKey}`);
-            continue;
-        }
         const keyToDelete = isSubPlaybookKey ? 'subplaybook-${currentPlaybookID}.' + originalKey: originalKey;
-        const result = executeCommand('delContext', { key: keyToDelete });
-    
-        if (!result || result.type === entryTypes.error) {
-            errors.push(result.Contents);
-        } else {
-            deletedKeys.push(key);
+        if (_keysToKeep.indexOf(keyToDelete) === -1) {
+            const result = executeCommand('delContext', { key: keyToDelete });
+            if (!result || result.type === entryTypes.error ) {
+                errors.push(result.Contents);
+            } else {
+                deletedKeys.push(key);
+            }
         }
     }
     if (deletedKeys.length > 0) {
-        message += `\nSuccessfully deleted keys '${deletedKeys.join("', '")}' from context.`;
+        message += LINE_SEPARATOR + `Successfully deleted keys '${deletedKeys.join("', '")}' from context.`;
     }
-    return message;
+
+    return errors.join(LINE_SEPARATOR) + LINE_SEPARATOR + message;
 }
 
 var shouldDeleteAll = (args.all === 'yes');
 var isSubPlaybookKey = (args.subplaybook === 'yes');
+var keysToKeep = (args.keysToKeep) ? args.keysToKeep.split(',').map(item => item.trim()) : [];
 
 if (args.subplaybook === 'auto') {
     var res = executeCommand('Print', { value: 'id=${currentPlaybookID}' });
@@ -62,7 +64,6 @@ if (!shouldDeleteAll && !args.key) {
 }
 
 if (shouldDeleteAll) {
-    var keysToKeep = (args.keysToKeep) ? args.keysToKeep.split(',').map(item => item.trim()) : [];
     var keysToKeepObj = {};
     var KeepDBotScoreKey = false;
     index = keysToKeep.indexOf("DBotScore");
@@ -94,7 +95,7 @@ if (shouldDeleteAll) {
     }
     var keysToDelete = Object.keys(invContext);
 
-    var message = deleteKeys(keysToDelete, isSubPlaybookKey)
+    var message = deleteKeys(keysToDelete, keysToKeep)
 
     return {
         Type: entryTypes.note,
@@ -146,8 +147,7 @@ if (shouldDeleteAll) {
 } else {
     // Supporting comma separated list of keys to be deleted.
     var keysToDelete = (typeof args.key === "string") ? args.key.split(',') : [args.key]
-
-    var message = deleteKeys(keysToDelete, isSubPlaybookKey)
+    var message = deleteKeys(keysToDelete, keysToKeep)
     return {
         Type: entryTypes.note,
         Contents: message,
@@ -156,5 +156,4 @@ if (shouldDeleteAll) {
         ReadableContentsFormat: formats.markdown,
         EntryContext: keysToKeepObj
     };
-
 }
