5966e791ed7d31b947b84770ebb24698dcd07cb6
Ni-Knight <davrahami@paloaltonetworks.com>
2024-04-09 10:45:31 +0300

    YARA Indicator

---
+++ Packs/Yara/Scripts/FormatterYARA/Formatter-YARA.py
@@ -0,0 +1,44 @@
+import demistomock as demisto
+from CommonServerPython import *  # lgtm [py/polluting-import]
+
+def main():
+    try:
+        the_input = demisto.args().get('input')
+
+        # argToList returns the argument as is if it's already a list so no need to check here
+        the_input = argToList(the_input)
+        entries_list = []
+        rule_name_regex = re.compile(r'\brule\s+(?P<name>\w+?)\s')
+        
+        # Otherwise assumes it's already an array
+        for item in the_input:
+            rule_name = rule_name_regex.search(item).group("name")
+            
+            if not item:
+                input_entry = ''
+            else:
+                input_entry = {
+                    "Type": entryTypes["note"],
+                    "ContentsFormat": formats["json"],
+                    "Contents": [rule_name],
+                    "EntryContext": {"YARA": rule_name}
+                }
+            
+            entries_list.append(input_entry)
+        
+        if entries_list:
+            demisto.results(entries_list)
+        
+        else:
+            # Return empty string so it wouldn't create an empty domain indicator.
+            demisto.results('')
+
+    except Exception as e:
+        return_error(
+            f'Failed to execute the automation. Error: \n{str(e)}'
+        )
+
+
+# python2 uses __builtin__ python3 uses builtins
+if __name__ == "__builtin__" or __name__ == "builtins":
+    main()
