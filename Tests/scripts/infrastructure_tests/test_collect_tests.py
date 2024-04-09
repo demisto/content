c61369da6d01cc81e7a02e6adc01616fc3705935
michal-dagan <mdagan@paloaltonetworks.com>
2024-04-09 17:07:32 +0300

    fix

--- Tests/scripts/infrastructure_tests/test_collect_tests.py
+++ Tests/scripts/infrastructure_tests/test_collect_tests.py
@@ -806,11 +806,12 @@ def test_sort_packs_to_upload(mocker):
     from Tests.scripts.collect_tests.collect_tests import PACK_MANAGER
 
     input_files = {Path("Packs/myPack/pack_metadata.json"),
-                   Path("Packs/myPack2/ReleaseNotes/1_0_0.md"),
+                   Path("Packs/myPack2/ReleaseNotes/1_0_1.md"),
                    Path("Packs/myPack2/pack_metadata.json")}
 
     mocker.patch.object(GitUtil, "_get_all_changed_files", return_value=input_files)
-    mocker.patch.object(PACK_MANAGER, "get_current_version", return_value="1.0.0")
+    mocker.patch.object(GitUtil, "added_files", return_value={})
+    mocker.patch.object(PACK_MANAGER, "get_current_version", return_value="1.0.1")
     packs_to_upload, packs_to_update_metadata = sort_packs_to_upload({"myPack", "myPack2"})
 
     assert packs_to_upload == ["myPack2"], "myPack should be marked for hard upload"
