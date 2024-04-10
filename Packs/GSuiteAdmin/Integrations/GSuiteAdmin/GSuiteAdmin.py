7ee25523499c06c32b786108633e49ef1a8adf9e
edik24 <ekatsenelson@paloaltonetworks.com>
2024-04-10 14:03:57 +0300

    Test

--- Packs/GSuiteAdmin/Integrations/GSuiteAdmin/GSuiteAdmin.py
+++ Packs/GSuiteAdmin/Integrations/GSuiteAdmin/GSuiteAdmin.py
@@ -1597,6 +1597,7 @@ def chromebrowser_list_command(client: Client, args: dict[str, str]) -> CommandR
         :return: Command Result.
     '''
     API_LIMIT = 100
+    b=4
     client.set_authorized_http(scopes=SCOPES['CHROME_BROWSERS'])
     customer_id = return_customer_id(args)
     device_id = args.get('device_id', '')
