9084c5c4164db50ab8cbb9646e3d91aa624c0b43
edik24 <ekatsenelson@paloaltonetworks.com>
2024-04-10 14:42:56 +0300

    CIAC-8731 Enhance G Suite Admin Integration to Support - Fixing after demo and tests

--- Utils/github_workflow_scripts/handle_external_pr.py
+++ Utils/github_workflow_scripts/handle_external_pr.py
@@ -290,8 +290,10 @@ def get_pr_comments(pr_number: str) -> str:
         The comments URL for the PR.
     """
     pr_url = f'https://api.github.com/repos/demisto/content/pulls/{pr_number}'
+    print(f'the pr url is: "{pr_url}')
     response = requests.get(pr_url)
-    response.raise_for_status()
+    print(f'the response for the pr request{response}')
+    #response.raise_for_status()
     pr = response.json()
     if not pr:
         print('Could not find the pull request to reply on.')
