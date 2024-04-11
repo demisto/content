16ae3a8e0052a82cbe4a3ccc983910533fa0b882
Yehuda <yrosenberg@paloaltonetworks.com>
2024-04-11 18:27:16 +0300

    t

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -1770,6 +1770,7 @@ class IntegrationLogger(object):
                 if is_debug_mode():
                     if text.startswith(('send:', 'header:')):
                         try:
+                            text= text.replace("send: b\"", "send: b'")
                             text = censor_request_logs(text)
                         except Exception as e:  # should fail silently
                             demisto.debug('Failed censoring request logs - {}'.format(str(e)))
