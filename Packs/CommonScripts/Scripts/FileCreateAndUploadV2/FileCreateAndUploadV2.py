c77ac2cd1dfedc10f8d21f333e3ee589d2a5392b
Shmuel Kroizer <69422117+shmuel44@users.noreply.github.com>
2024-04-11 19:12:48 +0300

    [FileCreateAndUploadV2] Added Support For `EntryNote` Type (#33920)
    * Fixed
    
    * Update Packs/CommonScripts/ReleaseNotes/1_14_33.md
    
    ---------
    
    Co-authored-by: yuvalbenshalom <ybenshalom@paloaltonetworks.com>

--- Packs/CommonScripts/Scripts/FileCreateAndUploadV2/FileCreateAndUploadV2.py
+++ Packs/CommonScripts/Scripts/FileCreateAndUploadV2/FileCreateAndUploadV2.py
@@ -14,6 +14,28 @@ def get_data_from_file(entry_id: str) -> bytes:
         raise DemistoException(f'There was a problem opening or reading the file.\nError is: {e}')
 
 
+def get_entry_metadata(entry_id: str) -> dict:  # pragma: no cover
+    res = demisto.executeCommand("getEntry", {"id": entry_id})
+    if is_error(res):
+        raise DemistoException(get_error(res))
+    return res[0]
+
+
+def get_data_entry(entry_metadata: dict) -> Any:
+    """Retrieves the data associated with an entry based on its type."""
+    entry_type: int = entry_metadata["Type"]
+    match entry_type:
+        case (
+            EntryType.FILE
+            | EntryType.IMAGE
+            | EntryType.ENTRY_INFO_FILE
+            | EntryType.VIDEO_FILE
+        ):
+            return get_data_from_file(entry_metadata["ID"])
+        case _:
+            return entry_metadata["Contents"]
+
+
 def decode_data(data: Any, data_encoding: str) -> bytes | Any:
     """Given data and its encoding, this function decodes the data according to the provided encoding and returns it."""
     match data_encoding:
@@ -34,8 +56,8 @@ def main() -> None:
 
     try:
         if entry_id:
-            data = get_data_from_file(entry_id)
-
+            entry_metadata = get_entry_metadata(entry_id)
+            data = get_data_entry(entry_metadata)
         data = decode_data(data, data_encoding)
 
         return_results(fileResult(filename, data))
