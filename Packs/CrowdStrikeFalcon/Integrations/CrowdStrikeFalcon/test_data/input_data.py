e86d7b5d8f1a753eb0b4bf308903808d8c60d164
RotemAmit <ramit@paloaltonetworks.com>
2024-04-11 09:36:16 +0300

    XSUP-35612/ Fixing mirroring issues (#33806)
    * added the needed fields to CS_FALCON_DETECTION_INCOMING_ARGS and CS_FALCON_INCIDENT_INCOMING_ARGS
    
    * updated the unit test test_get_remote_data_command
    
    * updated the mapper
    
    * RN
    
    * update RN
    
    * update RN after CR

--- Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/test_data/input_data.py
+++ Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/test_data/input_data.py
@@ -193,8 +193,10 @@ response_idp_detection = {
             "updated_timestamp": "2023-06-27T09:29:52.448779709Z"
 }
 
-context_idp_detection = {'name': 'IDP Detection ID: 20879a8064904ecfbb62c118a6a19411:ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56',
-                         'occurred': '2023-04-20T11:12:03.089Z', 'last_updated': '2023-06-27T09:29:52.448779709Z', 'rawJSON': '{"added_privileges": ["AdministratorsRole"], "aggregate_id": "aggind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "cid": "20879a8064904ecfbb62c118a6a19411", "comment": "new test comment new test comment2 new test comment2 new test comment2 new test comment2 new test comment new test comment new test comment new test comment new test comment2 new test comment2 new test comment new test comment2 new test comment2 comment", "composite_id": "20879a8064904ecfbb62c118a6a19411:ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "confidence": 20, "context_timestamp": "2023-04-20T11:12:03.089Z", "crawl_edge_ids": {"Sensor": ["XNWu1KJ3f7ck@.W>%?R;<dP:4XWOiGq9#dnSpb\\"l0^f#1Kl9\'<k^`t9\\"ptE?07V_G^*\'_EU\'/Ch6&[Xsfl<UI$RnhG;AQa[gb#+-\\\\+J1O?GF\\\\U^<^9bluf^^X`dYoqOIQpM,@C%pV[2A%9a\\"T6O4b1:B1@ps8N", "N6GX$`\'=_9i\\"H:bRq0rXhf`Vd$[@1Wr?Lr\'`EGQh3P42Up,g(aNSe7C38V)J@NV=)Rg/2m^+P>?(%>fETtmdN.<_m*o\'\'\\"CCUmBn.;18rN6.!:g%ohR0te,H;Z\\\\DK\\"=MJe1?:_Y=XZj>E=nHY5ge>3^9:\'(g:)A\'RG0W,kPj.CNpo<Vk/RE^G9E!b\'?=G[!!*\'!", "XNXPaKHLg+i\\"HEWkr@-r>$W@\\"o+ta@8q\'lE4T!!e@D;nls7!2S0cEcXKeuua2Q+<<8!<pD:k1.5(j-*D`ECSL7qH1t\'ZZKh\'%UJG\'SaS8QVr:\\"4jTCn[!Z]eCQhZa>bpJ`SjuN\'Y.FcK0JOE\\"K_hb8DEP5rc6I]<!!*\'!"]}, "crawl_vertex_ids": {"Sensor": ["idpind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "uid:20879a8064904ecfbb62c118a6a19411:S-1-5-21-4043902054-3757442694-3243833439-1141", "ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "aggind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56"]}, "crawled_timestamp": "2023-04-20T12:12:10.427005199Z", "created_timestamp": "2023-04-20T11:13:10.424647194Z", "description": "A user received new privileges", "display_name": "Privilege escalation (user)", "end_time": "2023-04-20T11:12:03.089Z", "falcon_host_link": "https://falcon.crowdstrike.com/identity-protection/detections/20879a8064904ecfbb62c118a6a19411:ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56?cid=20879a8064904ecfbb62c118a6a19411", "id": "ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "name": "IdpEntityPrivilegeEscalationUser", "objective": "Gain Access", "pattern_id": 51113, "platform": "Windows", "previous_privileges": "0", "privileges": "2177", "product": "idp", "scenario": "privilege_escalation", "seconds_to_resolved": 5869002, "seconds_to_triaged": 2250786, "severity": 2, "show_in_ui": true, "source_account_domain": "ENV11.FERRUM-TECHS.LOCAL", "source_account_name": "morganf", "source_account_object_sid": "S-1-5-21-4043902054-3757442694-3243833439-1141", "source_account_sam_account_name": "morganf", "source_account_upn": "test@test.com", "start_time": "2023-04-20T11:12:03.089Z", "status": "closed", "tactic": "Privilege Escalation", "tactic_id": "TA0004", "tags": [], "technique": "Valid Accounts", "technique_id": "T1078", "timestamp": "2023-04-20T11:12:05.686Z", "type": "idp-user-endpoint-app-info", "updated_timestamp": "2023-06-27T09:29:52.448779709Z", "mirror_direction": null, "mirror_instance": ""}'}
+context_idp_detection = {
+    'name': 'IDP Detection ID: 20879a8064904ecfbb62c118a6a19411:ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56',
+    'occurred': '2023-04-20T11:12:03.089Z', 'last_updated': '2023-06-27T09:29:52.448779709Z',
+    'rawJSON': '{"added_privileges": ["AdministratorsRole"], "aggregate_id": "aggind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "cid": "20879a8064904ecfbb62c118a6a19411", "comment": "new test comment new test comment2 new test comment2 new test comment2 new test comment2 new test comment new test comment new test comment new test comment new test comment2 new test comment2 new test comment new test comment2 new test comment2 comment", "composite_id": "20879a8064904ecfbb62c118a6a19411:ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "confidence": 20, "context_timestamp": "2023-04-20T11:12:03.089Z", "crawl_edge_ids": {"Sensor": ["XNWu1KJ3f7ck@.W>%?R;<dP:4XWOiGq9#dnSpb\\"l0^f#1Kl9\'<k^`t9\\"ptE?07V_G^*\'_EU\'/Ch6&[Xsfl<UI$RnhG;AQa[gb#+-\\\\+J1O?GF\\\\U^<^9bluf^^X`dYoqOIQpM,@C%pV[2A%9a\\"T6O4b1:B1@ps8N", "N6GX$`\'=_9i\\"H:bRq0rXhf`Vd$[@1Wr?Lr\'`EGQh3P42Up,g(aNSe7C38V)J@NV=)Rg/2m^+P>?(%>fETtmdN.<_m*o\'\'\\"CCUmBn.;18rN6.!:g%ohR0te,H;Z\\\\DK\\"=MJe1?:_Y=XZj>E=nHY5ge>3^9:\'(g:)A\'RG0W,kPj.CNpo<Vk/RE^G9E!b\'?=G[!!*\'!", "XNXPaKHLg+i\\"HEWkr@-r>$W@\\"o+ta@8q\'lE4T!!e@D;nls7!2S0cEcXKeuua2Q+<<8!<pD:k1.5(j-*D`ECSL7qH1t\'ZZKh\'%UJG\'SaS8QVr:\\"4jTCn[!Z]eCQhZa>bpJ`SjuN\'Y.FcK0JOE\\"K_hb8DEP5rc6I]<!!*\'!"]}, "crawl_vertex_ids": {"Sensor": ["idpind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "uid:20879a8064904ecfbb62c118a6a19411:S-1-5-21-4043902054-3757442694-3243833439-1141", "ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "aggind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56"]}, "crawled_timestamp": "2023-04-20T12:12:10.427005199Z", "created_timestamp": "2023-04-20T11:13:10.424647194Z", "description": "A user received new privileges", "display_name": "Privilege escalation (user)", "end_time": "2023-04-20T11:12:03.089Z", "falcon_host_link": "https://falcon.crowdstrike.com/identity-protection/detections/20879a8064904ecfbb62c118a6a19411:ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56?cid=20879a8064904ecfbb62c118a6a19411", "id": "ind:20879a8064904ecfbb62c118a6a19411:C0BB6ACD-8FDC-4CBA-9CF9-EBF3E28B3E56", "name": "IdpEntityPrivilegeEscalationUser", "objective": "Gain Access", "pattern_id": 51113, "platform": "Windows", "previous_privileges": "0", "privileges": "2177", "product": "idp", "scenario": "privilege_escalation", "seconds_to_resolved": 5869002, "seconds_to_triaged": 2250786, "severity": 2, "show_in_ui": true, "source_account_domain": "ENV11.FERRUM-TECHS.LOCAL", "source_account_name": "morganf", "source_account_object_sid": "S-1-5-21-4043902054-3757442694-3243833439-1141", "source_account_sam_account_name": "morganf", "source_account_upn": "test@test.com", "start_time": "2023-04-20T11:12:03.089Z", "status": "closed", "tactic": "Privilege Escalation", "tactic_id": "TA0004", "tags": [], "technique": "Valid Accounts", "technique_id": "T1078", "timestamp": "2023-04-20T11:12:05.686Z", "type": "idp-user-endpoint-app-info", "updated_timestamp": "2023-06-27T09:29:52.448779709Z", "mirror_direction": null, "mirror_instance": ""}'}
 
 remote_incident_id = 'inc:afb5d1512a00480f53e9ad91dc3e4b55:1cf23a95678a421db810e11b5db693bd'
 remote_detection_id = 'ldt:15dbb9d8f06b89fe9f61eb46e829d986:528715079668'
@@ -206,21 +208,23 @@ get_remote_incident = (remote_incident_id,
                        30,
                        None,
                        {'hosts.hostname': 'SFO-M-Y81WHJ', 'incident_type': 'incident', 'state': 'closed', 'status': 'In Progress',
-                        'tags': ['Objective/Keep Access'], 'fine_score': 38},
+                        'tags': ['Objective/Keep Access'], 'fine_score': 38, 'incident_id': remote_incident_id},
                        [])
 get_remote_incident_update = (remote_incident_id,
                               True,
                               25,
                               None,
                               {'hosts.hostname': 'SFO-M-Y81WHJ', 'incident_type': 'incident', 'state': 'closed',
-                               'status': 'Reopened', 'tags': ['Objective/Keep Access'], 'fine_score': 38},
+                               'status': 'Reopened', 'tags': ['Objective/Keep Access'], 'fine_score': 38,
+                               'incident_id': remote_incident_id},
                               [{'Contents': {'dbotIncidentReopen': True}, 'ContentsFormat': 'json', 'Type': EntryType.NOTE}])
 get_remote_incident_close = (remote_incident_id,
                              True,
                              40,
                              None,
                              {'hosts.hostname': 'SFO-M-Y81WHJ', 'incident_type': 'incident', 'state': 'closed',
-                              'status': 'Closed', 'tags': ['Objective/Keep Access'], 'fine_score': 38},
+                              'status': 'Closed', 'tags': ['Objective/Keep Access'], 'fine_score': 38,
+                              'incident_id': remote_incident_id},
                              [{'Contents': {'closeReason': 'Incident was closed on CrowdStrike Falcon',
                                             'dbotIncidentClose': True}, 'ContentsFormat': 'json', 'Type': EntryType.NOTE}])
 get_remote_incident_no_close = (remote_incident_id,
@@ -228,7 +232,8 @@ get_remote_incident_no_close = (remote_incident_id,
                                 40,
                                 None,
                                 {'hosts.hostname': 'SFO-M-Y81WHJ', 'incident_type': 'incident', 'state': 'closed',
-                                 'status': 'Closed', 'tags': ['Objective/Keep Access'], 'fine_score': 38},
+                                 'status': 'Closed', 'tags': ['Objective/Keep Access'], 'fine_score': 38,
+                                 'incident_id': remote_incident_id},
                                 [])
 get_remote_detection = (remote_detection_id,
                         False,
@@ -237,7 +242,8 @@ get_remote_detection = (remote_detection_id,
                         {'behaviors.objective': 'Falcon Detection Method', 'behaviors.scenario': 'suspicious_activity',
                          'behaviors.tactic': 'Malware', 'behaviors.technique': 'Malicious File',
                          'device.hostname': 'FALCON-CROWDSTR', 'incident_type': 'detection', 'severity': 2,
-                         'status': 'in_progress'},
+                         'status': 'in_progress', 'detection_id': remote_detection_id,
+                         'behaviors.display_name': 'SampleTemplateDetection'},
                         [])
 get_remote_detection_update = (remote_detection_id,
                                True,
@@ -246,7 +252,8 @@ get_remote_detection_update = (remote_detection_id,
                                {'behaviors.objective': 'Falcon Detection Method', 'behaviors.scenario': 'suspicious_activity',
                                 'behaviors.tactic': 'Malware', 'behaviors.technique': 'Malicious File',
                                 'device.hostname': 'FALCON-CROWDSTR', 'incident_type': 'detection', 'severity': 2,
-                                'status': 'reopened'},
+                                'status': 'reopened', 'detection_id': remote_detection_id,
+                                'behaviors.display_name': 'SampleTemplateDetection'},
                                [{'Contents': {'dbotIncidentReopen': True}, 'ContentsFormat': 'json', 'Type': EntryType.NOTE}])
 get_remote_detection_close = (remote_detection_id,
                               True,
@@ -255,7 +262,8 @@ get_remote_detection_close = (remote_detection_id,
                               {'behaviors.objective': 'Falcon Detection Method', 'behaviors.scenario': 'suspicious_activity',
                                'behaviors.tactic': 'Malware', 'behaviors.technique': 'Malicious File',
                                'device.hostname': 'FALCON-CROWDSTR', 'incident_type': 'detection', 'severity': 2,
-                               'status': 'closed'},
+                               'status': 'closed', 'detection_id': remote_detection_id,
+                               'behaviors.display_name': 'SampleTemplateDetection'},
                               [{'Contents': {'closeReason': 'Detection was closed on CrowdStrike Falcon',
                                              'dbotIncidentClose': True}, 'ContentsFormat': 'json', 'Type': EntryType.NOTE}])
 get_remote_detection_no_close = (remote_detection_id,
@@ -265,7 +273,8 @@ get_remote_detection_no_close = (remote_detection_id,
                                  {'behaviors.objective': 'Falcon Detection Method', 'behaviors.scenario': 'suspicious_activity',
                                   'behaviors.tactic': 'Malware', 'behaviors.technique': 'Malicious File',
                                   'device.hostname': 'FALCON-CROWDSTR', 'incident_type': 'detection', 'severity': 2,
-                                  'status': 'closed'},
+                                  'status': 'closed', 'detection_id': remote_detection_id,
+                                  'behaviors.display_name': 'SampleTemplateDetection'},
                                  [])
 
 get_remote_data_command_args = [get_remote_incident,
