e182f0f8e7f143b66dbd78067403736baf4b9fe1
ilaredo <ilaredo@paloaltonetworks.com>
2024-04-17 09:31:47 +0300

    client authentication

--- Packs/Venafi/Integrations/VenafiV2/VenafiV2.py
+++ Packs/Venafi/Integrations/VenafiV2/VenafiV2.py
@@ -1,5 +1,6 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+
 """Base Integration for Cortex XSOAR (aka Demisto)
 
 This is an empty Integration with some basic structure according
@@ -24,45 +25,103 @@ from typing import Dict, Any
 # Disable insecure warnings
 urllib3.disable_warnings()
 
-
 ''' CONSTANTS '''
 
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
+MINUTES_BEFORE_TOKEN_EXPIRED = 2
 
 ''' CLIENT CLASS '''
 
 
 class Client(BaseClient):
     """Client class to interact with the service API
-
-    This Client implements API calls, and does not contain any XSOAR logic.
-    Should only do requests and return data.
-    It inherits from BaseClient defined in CommonServer Python.
-    Most calls use _http_request() that handles proxy, SSL verification, etc.
-    For this  implementation, no special attributes defined
     """
 
-    # TODO: REMOVE the following dummy function:
-    def baseintegration_dummy(self, dummy: str) -> Dict[str, str]:
-        """Returns a simple python dict with the information provided
-        in the input (dummy).
+    def __init__(self, base_url: str, verify: bool, username: str, password: str, client_id: str, proxy: bool = False):
+        super().__init__(base_url=base_url, verify=verify, proxy=proxy)
+        self.token = self._login(client_id, username, password)
 
-        :type dummy: ``str``
-        :param dummy: string to add in the dummy dict that is returned
+    def _login(self, client_id: str, username: str, password: str) -> str:
+        integration_context = get_integration_context()
+        if token := integration_context.get('token'):
+            expires_date = integration_context.get('expires')
+            if expires_date and not self._is_token_expired(expires_date):
+                return token
+            else:
+                refresh_token = integration_context.get('refresh_token')
+                url_suffix = "/vedauth/authorize/token"
+                json_data = {
+                    "client_id": client_id,
+                    "refresh_token": refresh_token
+                }
 
-        :return: dict as {"dummy": dummy}
-        :rtype: ``str``
+                return self._create_new_token(url_suffix, json_data)
+
+        url_suffix = "/vedauth/authorize/oauth"
+        json_data = {
+            "username": username,
+            "password": password,
+            "client_id": client_id,
+            "scope": "certificate"
+        }
+
+        return self._create_new_token(url_suffix, json_data)
+
+    def _is_token_expired(self, expires_date: str) -> bool:
         """
+        This method checks if the token is expired.
 
-        return {"dummy": dummy}
-    # TODO: ADD HERE THE FUNCTIONS TO INTERACT WITH YOUR PRODUCT API
+        Args:
+            expires_date (str): The expiration date of the token.
 
+        Returns:
+            bool: True if the token is expired, False otherwise.
+        """
+        utc_now = get_current_time()
+        expires_datetime = arg_to_datetime(expires_date)
+        return utc_now < expires_datetime
 
-''' HELPER FUNCTIONS '''
+    def _create_new_token(self, url_suffix: str, json_data: dict) -> str:
+        try:
+            access_token_obj = self._http_request(
+                method="POST",
+                url_suffix=url_suffix,
+                json_data=json_data,
+            )
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
+        except DemistoException as e:
+            if "Unauthorized" in str(e):
+                raise DemistoException("Failed to generate a token. Credentials are incorrect.")
+            raise e
 
-''' COMMAND FUNCTIONS '''
+        new_token = access_token_obj.get("access_token", "")
+        expire_in = arg_to_number(access_token_obj.get("expires_in")) or 1
+        refresh_token = access_token_obj.get("refresh_token", "")
+        self._store_token_in_context(new_token, refresh_token, expire_in)
+        return new_token
+
+    def _store_token_in_context(self, token: str, refresh_token: str, expire_in: int) -> None:
+        """
+        This method stores the generated token and its expiration date in the integration context.
+
+        Args:
+            token (str): The generated authentication token.
+            expire_in (int): The number of seconds until the token expires.
+
+        Returns:
+            None
+        """
+        expire_date = get_current_time() + timedelta(seconds=expire_in) - timedelta(minutes=MINUTES_BEFORE_TOKEN_EXPIRED)
+        set_integration_context({"token": token, "refresh_token": refresh_token, "expire_date": str(expire_date)})
+
+    def _get_certificates(self):
+        json_data = {}
+
+        certificates = self._http_request(
+            method="GET",
+            url_suffix="/vedsdk/certificates/",
+            json_data=json_data,
+        )
 
 
 def test_module(client: Client) -> str:
@@ -95,7 +154,6 @@ def test_module(client: Client) -> str:
 
 # TODO: REMOVE the following dummy command function
 def baseintegration_dummy_command(client: Client, args: Dict[str, Any]) -> CommandResults:
-
     dummy = args.get('dummy', None)
     if not dummy:
         raise ValueError('dummy not specified')
@@ -108,6 +166,8 @@ def baseintegration_dummy_command(client: Client, args: Dict[str, Any]) -> Comma
         outputs_key_field='',
         outputs=result,
     )
+
+
 # TODO: ADD additional command functions that translate XSOAR inputs/outputs to Client
 
 
@@ -165,7 +225,6 @@ def main() -> None:
 
 
 ''' ENTRY POINT '''
-
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
