ebb6a89aaeb5470a0f0f0bfaed05a80a98536fcd
edik24 <ekatsenelson@paloaltonetworks.com>
2024-04-17 18:04:30 +0300

    Test

--- Packs/Accessdata/Integrations/Accessdata/Accessdata.py
+++ Packs/Accessdata/Integrations/Accessdata/Accessdata.py
@@ -17,7 +17,7 @@ disable_warnings()
 
 ''' CONSTANTS '''
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-
+kj=2
 ACCESS_DATA = 'Accessdata'
 JOBID_KEY = 'Accessdata.Job.ID'
 JOBRESULT_KEY = 'Accessdata.Job.Result'
