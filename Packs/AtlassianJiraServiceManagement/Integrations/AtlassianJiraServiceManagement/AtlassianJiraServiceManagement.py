604c31a1d04980e8ce5631f2751add41c2e1705d
TalZich <tzichlinsky@paloaltonetworks.com>
2024-04-25 16:59:48 +0300

    Add jira-asset-object-schema-list command to Atlassian JSM integration

--- Packs/AtlassianJiraServiceManagement/Integrations/AtlassianJiraServiceManagement/AtlassianJiraServiceManagement.py
+++ Packs/AtlassianJiraServiceManagement/Integrations/AtlassianJiraServiceManagement/AtlassianJiraServiceManagement.py
@@ -30,7 +30,7 @@ DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'  # ISO8601 format with UTC, default in XSOAR
 CLOUD_URL_STRUCTURE = 'https://api.atlassian.com/jsm/assets/workspace/'
 ON_PREM_URL_STRUCTURE = 'https://{}/rest/assets/1.0/'
 GETֹֹֹֹ_WORKSPACE_URL_STRUCTURE = 'https://{}.atlassian.net/rest/servicedeskapi/assets/workspace'
-
+INTEGRATION_OUTPUTS_BASE_PATH = 'JiraAsset'
 ''' CLIENT CLASS '''
 
 
@@ -47,10 +47,10 @@ class Client(BaseClient):
         headers = {'Authorization': f'Bearer {api_key}', 'Accept': 'application/json'}
         super().__init__(base_url=base_url, verify=verify, proxy=proxy, headers=headers)
 
-    def get_object_schema_list(self, url_suffix: str):
+    def get_object_schema_list(self):
         return self._http_request(
             method='GET',
-            url_suffix=url_suffix
+            url_suffix='objectschema/list'
         )
 
     def get_workspace(self, jsm_premium_site_name) -> Dict[str, Any]:
@@ -64,7 +64,41 @@ class Client(BaseClient):
 
 ''' HELPER FUNCTIONS '''
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
+
+def pascal_case(s: str) -> str:
+    """ Convert a string to PascalCase. """
+    words = s.split('_')
+    return ''.join(word[:1].upper() + word[1:] for word in words)
+
+
+def convert_keys_to_pascal(objects: List[Dict[str, Any]], key_mapping: Optional[Dict[str, str]] = None) -> List[Dict[str, str]]:
+    """
+    Convert keys of objects in a list to PascalCase, with optional key mapping.
+
+    Args:
+        objects (list): List of dictionaries (objects) whose keys need conversion.
+        key_mapping (dict): Dictionary containing original keys and expected output keys.
+
+    Returns:
+        list: List of dictionaries with keys converted to PascalCase or as per key_mapping.
+    """
+    if not key_mapping:
+        key_mapping = {}
+
+    converted_objects = []
+
+    for obj in objects:
+        converted_obj = {}
+        for key, value in obj.items():
+            if key in key_mapping:
+                new_key = key_mapping[key]
+            else:
+                new_key = pascal_case(key)
+            converted_obj[new_key] = value
+        converted_objects.append(converted_obj)
+
+    return converted_objects
+
 
 ''' COMMAND FUNCTIONS '''
 
@@ -82,9 +116,8 @@ def test_module(client: Client) -> str:
     :return: 'ok' if test passed, anything else will fail the test.
     :rtype: ``str``
     """
-    message: str = ''
     try:
-        result = client.get_object_schema_list('objectschema/list')
+        client.get_object_schema_list()
         return 'ok'
     except DemistoException as e:
         if 'Forbidden' in str(e) or 'Authorization' in str(e):  # TODO: make sure you capture authentication errors
@@ -103,13 +136,47 @@ def jira_asset_get_workspace_command(args: Dict[str, Any], params: Dict[str, Any
     result = client.get_workspace(jsm_premium_site_name)
     outputs = {'ID': result.get('values')[0].get('workspaceId')}
     return CommandResults(
-        outputs_prefix='JiraAsset.Workspace',
+        outputs_prefix=f'{INTEGRATION_OUTPUTS_BASE_PATH}.Workspace',
         outputs_key_field='ID',
         outputs=outputs,
     )
 
 
-# TODO: ADD additional command functions that translate XSOAR inputs/outputs to Client
+def jira_asset_object_schema_list_command(client: Client, args: Dict[str, Any]) -> CommandResults:
+    page = args.get('page')
+    page_size = int(args.get('page_size', 50))
+    limit = args.get('limit')
+    res = client.get_object_schema_list()
+    object_schemas = res.get('objectschemas', [])
+    key_mapping = {'id': 'ID', 'objectSchemaKey': 'Key'}
+
+    if page:
+        page = int(page)
+        if page < 1 or (page - 1) * page_size >= len(object_schemas):
+            raise ValueError("Invalid page_number. Page does not exist.")
+        start_index = (page - 1) * page_size
+        end_index = min(start_index + page_size, len(object_schemas))
+
+        # Retrieve elements for the specified page
+        object_schemas = object_schemas[start_index:end_index]
+
+    if limit:
+        limit = int(limit)
+        object_schemas = object_schemas[:limit]
+
+    outputs = convert_keys_to_pascal(object_schemas, key_mapping)
+    readable_outputs = [
+        {k: v for k, v in output.items() if
+         k != 'Updated' and
+         k != 'ObjectCount' and
+         k != 'ObjectTypeCount'} for output in outputs]
+    hr_headers = ['ID', 'Name', 'Key', 'Status', 'Description', 'Created']
+    return CommandResults(
+        outputs_prefix=f'{INTEGRATION_OUTPUTS_BASE_PATH}.Schema',
+        outputs_key_field='ID',
+        outputs=outputs,
+        readable_output=tableToMarkdown('Object Schemas', readable_outputs, headers=hr_headers)
+    )
 
 
 ''' MAIN FUNCTION '''
@@ -141,9 +208,13 @@ def main() -> None:
 
         client = Client(base_url=base_url, verify=verify_certificate, proxy=proxy, api_key=api_key)
 
-        if demisto.command() == 'test-module':
+        if command == 'test-module':
             # This is the call made when pressing the integration Test button.
             result = test_module(client)
+            return_results(result)
+
+        if command == 'jira-asset-object-schema-list':
+            result = jira_asset_object_schema_list_command(client, args)
             return_results(result)
         else:
             raise NotImplementedError(f'''Command '{command}' is not implemented.''')
