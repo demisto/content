dd1e53efd8d7d50bdc417787c09e09bf136dccff
sberman <sberman@paloaltonetworks.com>
2024-05-02 08:58:01 +0300

    Added a validation in the code as well

--- Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
+++ Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
@@ -476,6 +476,9 @@ def update_incident_command(client, args):
     resolve_comment = args.get('resolve_comment')
     add_comment = args.get('add_comment')
 
+    if assigned_user_pretty_name and not assigned_user_mail:
+        raise DemistoException('To supply a new value in this field, you must also supply a value for assigned_user_mail.')
+
     client.update_incident(
         incident_id=incident_id,
         assigned_user_mail=assigned_user_mail,
