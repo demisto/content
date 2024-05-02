ef39d02bef908d7b97de50997b085117891df77b
GuyAfik <guyafik11@gmail.com>
2024-05-02 12:03:57 +0300

    add test success with mock for coverage

--- Packs/CommonScripts/Scripts/Ping/Ping_test.py
+++ Packs/CommonScripts/Scripts/Ping/Ping_test.py
@@ -29,6 +29,31 @@ def test_ping(mocker, address):
     assert results[0]["EntryContext"]["Ping"]['mdev_rtt']
 
 
+def test_ping_mocked(mocker):
+    import subprocess
+    address = "8.8.8.8"
+    mocker.patch.object(demisto, 'args', return_value={'address': address})
+    mocker.patch.object(demisto, 'results')
+    mocker.patch.object(
+        subprocess,
+        "check_output",
+        return_value=f'PING {address} ({address}) 56(84) bytes of data.\n--- {address} ping statistics ---\n3 '
+                     f'packets transmitted, 3 received, 0% packet loss, time 2010ms'
+                     f'\nrtt min/avg/max/mdev = 12.392/16.995/22.088/3.973 ms'
+    )
+    # validate our mocks are good
+    assert demisto.args()['address'] == address
+    main()
+    assert demisto.results.call_count == 1
+    # call_args is tuple (args list, kwargs). we only need the first one
+    results = demisto.results.call_args[0]
+    assert len(results) == 1
+    assert results[0]['Type'] == entryTypes['note']
+    assert address in results[0]["EntryContext"]["Ping"]['destination']
+    assert re.fullmatch(r'\d+\.\d+\.\d+.\d+', results[0]["EntryContext"]["Ping"]['destination_ip']) is not None
+    assert results[0]["EntryContext"]["Ping"]['mdev_rtt']
+
+
 def test_fail_ping(mocker):
     mocker.patch.object(demisto, 'args', return_value={'address': 'nonExistingDomain45343.com'})  # disable-secrets-detection
     return_error_mock = mocker.patch(RETURN_ERROR_TARGET)
