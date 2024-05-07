f3d26de2d01b01ef213d543890cc401dad44d382
Dan Tavori <dtavori@paloaltonetworks.com>
2024-05-07 20:09:46 +0300

    suppress is python3

--- Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
+++ Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
@@ -1,13 +1,11 @@
 import demistomock as demisto  # noqa: F401
-import contextlib
 
 # Only the following integrations and scripts are using the Demisto class wrapper
 DEMISTO_WRAPPER_INTEGRATIONS = ['Cortex XDR - IR', 'QRadar v3', 'SlackV3', 'ServiceNow v2']
 DEMISTO_WRAPPER_SCRIPTS = ['UnzipFile', 'DBotFindSimilarIncidents', 'ParseCSV']
 
 
-# NameError will be raised only in tests, where a Demisto class isn't defined.
-with contextlib.suppress(NameError):
+try:
     class DemistoWrapper(Demisto):  # type:ignore  [name-defined] # noqa: F821 # pylint: disable=E0602
         """A content-side wrapper to the builtin Demisto class.
         All methods of this class can be executed in both scripts and integrations
@@ -28,3 +26,7 @@ with contextlib.suppress(NameError):
         demisto.__class__ = DemistoIntegration
     elif demisto.callingContext.get('context', {}).get('ScriptName', '') in DEMISTO_WRAPPER_SCRIPTS:
         demisto.__class__ = DemistoScript
+
+except NameError:
+    # NameError will be raised only in tests, where a Demisto class isn't defined.
+    pass
