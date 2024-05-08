30058faf59b45c670fb19e7832821a9cad69eb55
Tal <tcarmeli@paloaltonetworks.com>
2024-05-08 17:04:52 +0300

    preprocess

--- Packs/EmailCommunication/Scripts/PreprocessEmail/PreprocessEmail.py
+++ Packs/EmailCommunication/Scripts/PreprocessEmail/PreprocessEmail.py
@@ -245,7 +245,7 @@ def get_attachments_using_instance(email_related_incident, labels, email_to, ide
         demisto.debug('Attachments could only be retrieved from EWS v2 or Gmail')
 
 
-def find_attachments_to_download(attachments, email_html, labels):
+def find_attachments_to_download(attachments, email_html, labels, email_related_incident):
     """ Filter only new attachment.
 
     Args:
@@ -260,18 +260,18 @@ def find_attachments_to_download(attachments, email_html, labels):
     new_attachments = []
     demisto.debug(f"help_the_attachment {attachments}")
     demisto.debug(f"this_is_the_email {email_html}")
+    previous_files = get_incident_related_files(email_related_incident)
+    previous_files = [previous_files] if not isinstance(previous_files, list) else previous_files
+    previous_file_names = [ file.get("Name") for file in previous_files ]
     for attachment in attachments:
-        if '-' in attachment.get('description', ''):
-            attachment_id = attachment.get('description', '').split('-', 1)[1]
-            demisto.debug(f"this_is_the_id {attachment_id}")
-        content_id = "None"
-        if '-' in attachment.get('name', ''):
-            content_id = attachment.get('name', '')[::-1].split('-', 1)[1][::-1]
-        demisto.debug(f"this_is_the_content {content_id}")
-        if re.search(rf'(src="cid:{content_id}")', email_html):
-            if integration_name in ['Gmail dev', 'Gmail Single User dev', 'MicrosoftGraphMail dev', 'Microsoft Graph Mail Single User dev']:
+        if attachment.get('name', '') not in previous_file_names:
+            if (integration_name in ['Gmail dev', 'Gmail Single User dev',
+                                     'MicrosoftGraphMail dev', 'Microsoft Graph Mail Single User dev']
+                and '-' in attachment.get('name', '')):
+                content_id = attachment.get('name', '')[::-1].split('-', 1)[1][::-1]
                 new_attachment_identifiers_list.append(content_id)
-            else:
+            elif '-' in attachment.get('description', ''):
+                attachment_id = attachment.get('description', '').split('-', 1)[1]
                 new_attachment_identifiers_list.append(attachment_id)
             new_attachments.append(attachment)
     demisto.debug(f"this_is_the_attachment_ids {new_attachment_identifiers_list}")
@@ -470,7 +470,7 @@ def main():
         email_html = remove_html_conversation_history(email_html)
 
         #Get attachments IDs for new attacments
-        attachment_identifiers_array, attachments = find_attachments_to_download(attachments, email_html, incident.get('labels'))
+        attachment_identifiers_array, attachments = find_attachments_to_download(attachments, email_html, incident.get('labels'), email_related_incident)
         demisto.debug(f"{attachment_identifiers_array=}")
         get_attachments_using_instance(email_related_incident, incident.get('labels'), email_to, attachment_identifiers_array)
 
