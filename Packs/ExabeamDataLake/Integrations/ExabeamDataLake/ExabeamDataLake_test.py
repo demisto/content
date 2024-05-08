7a7be975d4748f183af4c112bb2fe41f6e816e02
inbalapt1 <iapt@paloaltonetworks.com>
2024-05-08 15:44:08 +0300

    tests

--- Packs/ExabeamDataLake/Integrations/ExabeamDataLake/ExabeamDataLake_test.py
+++ Packs/ExabeamDataLake/Integrations/ExabeamDataLake/ExabeamDataLake_test.py
@@ -12,7 +12,49 @@ class MockClient(Client):
         return
     
     
-def test_login()
+
+def test_query_datalake_command(mocker):
+    args = {
+        'page': 1,
+        'page_size': 50,
+        'limit': 50,
+        'start_time': '2024-05-01T00:00:00',
+        'end_time': '2024-05-08T00:00:00',
+        'query': '*'
+    }
+    mock_response = {
+        "responses": [
+            {
+                "hits": {
+                    "hits": [
+                        {"_source": {"@timestamp": "2024-05-01T12:00:00", "message": "example message 1", "another_values":"nothing"}},
+                        {"_source": {"@timestamp": "2024-05-02T12:00:00", "message": "example message 2", "not_relevant":"nothing"}}
+                    ]
+                }
+            }
+        ]
+    }
+    
+    mocker.patch.object(Client, "query_datalake_request", return_value=mock_response)
+    
+    client = MockClient("","","", False, False)
+    
+    response = query_datalake_command(client, args, cluster_name="local")
+
+    result = response.to_context().get('EntryContext',{}).get('ExabeamDataLake.Event',{})
+    
+    assert {'_source': {'@timestamp': '2024-05-01T12:00:00', 'message': 'example message 1',
+                        'another_values': 'nothing'}} in result
+    assert {'_source': {'@timestamp': '2024-05-02T12:00:00', 'message': 'example message 2',
+                        'not_relevant': 'nothing'}} in result
+    expected_result = (
+        "### Logs\n"
+        "|created_at|id|message|product|vendor|\n"
+        "|---|---|---|---|---|\n"
+        "| 2024-05-01T12:00:00 |  | example message 1 |  |  |\n"
+        "| 2024-05-02T12:00:00 |  | example message 2 |  |  |\n"
+    )
+    assert expected_result in response.readable_output
     
 
 
@@ -70,40 +112,40 @@ def test_handle_time_range_query_raise_error():
         _handle_time_range_query(start_time, end_time)
 
 
-def test_query_datalake_command(mocker):
-    """
-    GIVEN:
-        a mocked Client and test data,
-    WHEN:
-        'query_datalake_command' function is called with the provided arguments,
-        and the Client returns the test data.
-    THEN:
-        it should return the expected context and readable output.
-    """
-    mock_response = load_test_data("./test_data/response.json")
+# def test_query_datalake_command(mocker):
+#     """
+#     GIVEN:
+#         a mocked Client and test data,
+#     WHEN:
+#         'query_datalake_command' function is called with the provided arguments,
+#         and the Client returns the test data.
+#     THEN:
+#         it should return the expected context and readable output.
+#     """
+#     mock_response = load_test_data("./test_data/response.json")
 
-    mocker.patch.object(Client, "query_datalake_request", return_value=mock_response)
+#     mocker.patch.object(Client, "query_datalake_request", return_value=mock_response)
 
-    response: CommandResults = query_datalake_command(
-        MockClient("", "", "", False, False),
-        {
-            "query": "*",
-            "start_time": "2021-07-16T12:00:00",  # 1626382800000
-            "end_time": "2022-07-16T12:00:00",  # 1657918800000
-            "limit": 3,
-            "all_result": False,
-        },
-    )
-    outputs = response.to_context()["EntryContext"]['ExabeamDataLake.Log']
-    assert outputs == mock_response["responses"][0]["hits"]["hits"]
-    assert response.readable_output == (
-        "### Logs\n"
-        "|Action|Event Name|ID|Product|Time|Vendor|\n"
-        "|---|---|---|---|---|---|\n"
-        "| Accept | Accept | test_id_1 | test_product_1 | 2022-06-12T23:55:05.000Z | test_vendor_1 |\n"
-        "| Accept | Accept | test_id_2 | test_product_2 | 2022-06-12T23:55:05.000Z | test_vendor_2 |\n"
-        "| Accept | Accept | test_id_3 | test_product_3 | 2022-06-12T23:55:05.000Z | test_vendor_3 |\n"
-    )
+#     response: CommandResults = query_datalake_command(
+#         MockClient("", "", "", False, False),
+#         {
+#             "query": "*",
+#             "start_time": "2021-07-16T12:00:00",  # 1626382800000
+#             "end_time": "2022-07-16T12:00:00",  # 1657918800000
+#             "limit": 3,
+#             "all_result": False,
+#         },
+#     )
+#     outputs = response.to_context()["EntryContext"]['ExabeamDataLake.Log']
+#     assert outputs == mock_response["responses"][0]["hits"]["hits"]
+#     assert response.readable_output == (
+#         "### Logs\n"
+#         "|Action|Event Name|ID|Product|Time|Vendor|\n"
+#         "|---|---|---|---|---|---|\n"
+#         "| Accept | Accept | test_id_1 | test_product_1 | 2022-06-12T23:55:05.000Z | test_vendor_1 |\n"
+#         "| Accept | Accept | test_id_2 | test_product_2 | 2022-06-12T23:55:05.000Z | test_vendor_2 |\n"
+#         "| Accept | Accept | test_id_3 | test_product_3 | 2022-06-12T23:55:05.000Z | test_vendor_3 |\n"
+#     )
 
 
 def test_query_datalake_command_no_response(mocker):
@@ -116,10 +158,18 @@ def test_query_datalake_command_no_response(mocker):
         it should return a readable output indicating no results found.
 
     """
-
+    args = {
+        'page': 1,
+        'page_size': 50,
+        'limit': 50,
+        'start_time': '2024-05-01T00:00:00',
+        'end_time': '2024-05-08T00:00:00',
+        'query': '*'
+    }
+    
     mocker.patch.object(Client, "query_datalake_request", return_value={})
 
-    response = query_datalake_command(MockClient("", "", "", False, False), {"query": "*"})
+    response = query_datalake_command(MockClient("", "", "", False, False), args, "local")
 
     assert response.readable_output == '### Logs\n**No entries.**\n'
 
@@ -136,7 +186,14 @@ def test_query_datalake_command_raise_error(mocker):
         it should raise a DemistoException with the appropriate error message.
     """
 
-    args = {"query": "*", "limit": 50, "all_result": False}
+    args = {
+        'page': 1,
+        'page_size': 50,
+        'limit': 50,
+        'start_time': '2024-05-01T00:00:00',
+        'end_time': '2024-05-08T00:00:00',
+        'query': '*'
+    }
     mocker.patch.object(
         Client,
         "query_datalake_request",
@@ -145,4 +202,4 @@ def test_query_datalake_command_raise_error(mocker):
         },
     )
     with pytest.raises(DemistoException, match="Error in query: test response"):
-        query_datalake_command(MockClient("", "", "", False, False), args)
+        query_datalake_command(MockClient("", "", "", False, False), args, "local")
