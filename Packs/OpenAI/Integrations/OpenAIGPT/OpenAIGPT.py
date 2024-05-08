0a8dd355b40f1dbf6e0f7496759c9945b828b7e1
barryyosi-panw <byosilevich@paloaltonetworks.com>
2024-05-08 13:55:35 +0300

    validations

--- Packs/OpenAI/Integrations/OpenAIGPT/OpenAIGPT.py
+++ Packs/OpenAI/Integrations/OpenAIGPT/OpenAIGPT.py
@@ -247,7 +247,8 @@ def check_email_part(email_part: str, client: OpenAiClient, args: dict[str, Any]
     """
     entry_id: str = args.get(ArgAndParamNames.ENTRY_ID, '')
     email_headers, email_text_body, email_html_body, file_name = get_email_parts(entry_id)
-    additional_instructions = f'openai-gpt check_email_part Additional instructions: {ArgAndParamNames.ADDITIONAL_INSTRUCTIONS}\n'\
+    additional_instructions = (f'openai-gpt check_email_part '
+                               f'Additional instructions: {ArgAndParamNames.ADDITIONAL_INSTRUCTIONS}\n') \
         if args.get(ArgAndParamNames.ADDITIONAL_INSTRUCTIONS, "") else ''
 
     if email_part == EmailParts.HEADERS:
