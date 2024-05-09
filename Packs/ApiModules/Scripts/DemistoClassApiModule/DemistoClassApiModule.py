03882905b0cd169d05514dc2f4a9f57066a0942a
Dan Tavori <dtavori@paloaltonetworks.com>
2024-05-09 11:27:37 +0300

    exec id

--- Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
+++ Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
@@ -1,15 +1,26 @@
 import demistomock as demisto  # noqa: F401
+import random
+import string
 
 # Only the following integrations and scripts are using the Demisto class wrapper
 DEMISTO_WRAPPER_INTEGRATIONS = ['Cortex XDR - IR', 'QRadar v3', 'SlackV3', 'ServiceNow v2']
 DEMISTO_WRAPPER_SCRIPTS = ['UnzipFile', 'DBotFindSimilarIncidents', 'ParseCSV']
 
+EXEC_ID = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
 
 try:
     class DemistoWrapper(Demisto):  # type:ignore  [name-defined] # noqa: F821 # pylint: disable=E0602
         """A content-side wrapper to the builtin Demisto class.
         All methods of this class can be executed in both scripts and integrations
         (E.g., self.results). """
+        def info(self, msg, *args):
+            return super(DemistoWrapper, self).info("{} [Exec ID: {}]".format(msg, EXEC_ID), *args)
+
+        def debug(self, msg, *args):
+            return super(DemistoWrapper, self).debug("{} [Exec ID: {}]".format(msg, EXEC_ID), *args)
+
+        def error(self, msg, *args):
+            return super(DemistoWrapper, self).error("{} [Exec ID: {}]".format(msg, EXEC_ID), *args)
 
     class DemistoScript(DemistoWrapper):
         def getFilePath(self, id):
