55933fffbaf56b4b4c987ad623eadd4cdd9c8b97
sberman <sberman@paloaltonetworks.com>
2024-05-09 10:54:32 +0300

    Adding logs for debug

--- Packs/Zscaler/Integrations/Zscaler/Zscaler.py
+++ Packs/Zscaler/Integrations/Zscaler/Zscaler.py
@@ -195,7 +195,8 @@ def blacklist_url(url):
     cmd_url = "/security/advanced/blacklistUrls?action=ADD_TO_LIST"
     data = {"blacklistUrls": urls_to_blacklist}
     json_data = json.dumps(data)
-    http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    res = http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    demisto.debug(f'Blacklist URL response = {res}')
     list_of_urls = ""
     for url in urls_to_blacklist:
         list_of_urls += "- " + url + "\n"
@@ -218,7 +219,8 @@ def unblacklist_url(url):
 
     data = {"blacklistUrls": urls_to_unblacklist}
     json_data = json.dumps(data)
-    http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    res = http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    demisto.debug(f'Unblacklist URL response = {res}')
     list_of_urls = ""
     for url in urls_to_unblacklist:
         list_of_urls += "- " + url + "\n"
@@ -232,7 +234,8 @@ def blacklist_ip(ip):
     cmd_url = "/security/advanced/blacklistUrls?action=ADD_TO_LIST"
     data = {"blacklistUrls": ips_to_blacklist}
     json_data = json.dumps(data)
-    http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    res = http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    demisto.debug(f'Blacklist IP response = {res}')
     list_of_ips = ""
     for ip in ips_to_blacklist:
         list_of_ips += "- " + ip + "\n"
@@ -256,7 +259,8 @@ def unblacklist_ip(ip):
         raise Exception("Given IP addresses are not blacklisted.")
     data = {"blacklistUrls": ips_to_unblacklist}
     json_data = json.dumps(data)
-    http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    res = http_request("POST", cmd_url, json_data, DEFAULT_HEADERS, resp_type='response')
+    demisto.debug(f'Blacklist IP response = {res}')
     list_of_ips = ""
     for ip in ips_to_unblacklist:
         list_of_ips += "- " + ip + "\n"
