9f33deb2e7ddc379f106e34f4e1eaf25e2b1ce93
pabloperezj <pablopj@google.com>
2024-05-10 07:50:41 +0200

    Improve test

--- Packs/GoogleThreatIntelligence/Integrations/GoogleThreatIntelligence/GoogleThreatIntelligence_test.py
+++ Packs/GoogleThreatIntelligence/Integrations/GoogleThreatIntelligence/GoogleThreatIntelligence_test.py
@@ -271,33 +271,38 @@ def test_file_command(mocker, requests_mock):
     """
     from GoogleThreatIntelligence import file_command, ScoreCalculator, Client
     import CommonServerPython
-    # Setup Mocks
+
     file_hash = '699ec052ecc898bdbdafea0027c4ab44c3d01ae011c17745dd2b7fbddaa077f3'
-    mocker.patch.object(demisto, 'args', return_value={'file': file_hash, 'extended_data': 'false'})
     mocker.patch.object(demisto, 'params', return_value=DEFAULT_PARAMS)
     mocker.patch.object(CommonServerPython, 'is_demisto_version_ge', return_value=True)
 
-    # Assign arguments
     params = demisto.params()
     mocked_score_calculator = ScoreCalculator(params=params)
     file_relationships = (','.join(argToList(params.get('file_relationships')))).replace('* ', '').replace(' ', '_')
     client = Client(params=params)
 
-    # Load assertions and mocked request data
     mock_response = util_load_json('test_data/file.json')
-    expected_results = util_load_json('test_data/file_results.json')
     requests_mock.get(f'https://www.virustotal.com/api/v3/files/{file_hash}?relationships={file_relationships}',
                       json=mock_response)
 
-    # Run command and collect result array
-    results = file_command(
-        client=client, score_calculator=mocked_score_calculator,
-        args=demisto.args(), relationships=file_relationships)
+    for extended_data in [True, False]:
+        file_hash = '699ec052ecc898bdbdafea0027c4ab44c3d01ae011c17745dd2b7fbddaa077f3'
+        mocker.patch.object(demisto, 'args', return_value={'file': file_hash, 'extended_data': extended_data})
 
-    assert results[1].execution_metrics == [{'APICallsCount': 1, 'Type': 'Successful'}]
-    assert results[0].execution_metrics is None
-    assert results[0].outputs == expected_results
-    assert results[0].indicator.dbot_score.score == 3
+        if extended_data:
+            expected_results = util_load_json('test_data/file_sigma_analysis_results.json')
+        else:
+            expected_results = util_load_json('test_data/file_results.json')
+
+        # Run command and collect result array
+        results = file_command(
+            client=client, score_calculator=mocked_score_calculator,
+            args=demisto.args(), relationships=file_relationships)
+
+        assert results[1].execution_metrics == [{'APICallsCount': 1, 'Type': 'Successful'}]
+        assert results[0].execution_metrics is None
+        assert results[0].outputs == expected_results
+        assert results[0].indicator.dbot_score.score == 3
 
 
 def test_not_found_file_command(mocker, requests_mock):
@@ -322,16 +327,16 @@ def test_not_found_file_command(mocker, requests_mock):
     # Assign arguments
     params = demisto.params()
     mocked_score_calculator = ScoreCalculator(params=params)
-    domain_relationships = (','.join(argToList(params.get('file_relationships')))).replace('* ', '').replace(' ', '_')
+    file_relationships = (','.join(argToList(params.get('file_relationships')))).replace('* ', '').replace(' ', '_')
     client = Client(params=params)
 
     mock_response = {'error': {'code': 'NotFoundError'}}
-    requests_mock.get(f'https://www.virustotal.com/api/v3/files/{file_hash}?relationships={domain_relationships}',
+    requests_mock.get(f'https://www.virustotal.com/api/v3/files/{file_hash}?relationships={file_relationships}',
                       json=mock_response)
 
     results = file_command(
         client=client, score_calculator=mocked_score_calculator,
-        args=demisto.args(), relationships=domain_relationships)
+        args=demisto.args(), relationships=file_relationships)
 
     assert results[0].execution_metrics is None
     assert results[0].readable_output == f'File "{file_hash}" was not found in GoogleThreatIntelligence.'
