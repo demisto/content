4b27c7a8eb112557548d53de6fe59ddb1af33c02
Israel Lappe <79846863+ilappe@users.noreply.github.com>
2024-05-15 13:24:42 +0300

    Update lock_cloud_machines.py

--- Tests/scripts/lock_cloud_machines.py
+++ Tests/scripts/lock_cloud_machines.py
@@ -286,7 +286,7 @@ def get_and_lock_all_needed_machines(storage_client: storage.Client,
     logging.debug('getting all machines lock files')
     machines_locks = get_machines_locks_details(storage_client, LOCKS_BUCKET,
                                                 gcs_locks_path, MACHINES_LOCKS_REPO)
-    list_machines = ['9993056930520']
+    list_machines = ['9995080931197']
     locked_machine_list = []
     while number_machines_to_lock > 0:
         busy_machines = []
