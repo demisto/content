4125c1d74b7078f9768f3904b4c03db9e09f05d0
Shmuel Kroizer <skroizer@paloaltonetworks.com>
2024-05-15 18:47:36 +0300

    Campaign - ruff UP015

--- Packs/Campaign/Scripts/ShowCampaignHighestSeverity/ShowCampaignHighestSeverity_test.py
+++ Packs/Campaign/Scripts/ShowCampaignHighestSeverity/ShowCampaignHighestSeverity_test.py
@@ -9,7 +9,7 @@ data_test_get_incident_severity = [
 
 @pytest.mark.parametrize('incident_data_path, expected_severity', data_test_get_incident_severity)
 def test_get_incident_severity(mocker, incident_data_path, expected_severity):
-    with open(incident_data_path, 'r') as incident_data_file:
+    with open(incident_data_path) as incident_data_file:
         incident_data = json.load(incident_data_file)
     mocker.patch.object(demisto, 'executeCommand', return_value=incident_data)
     assert get_incident_severity('test') == expected_severity
@@ -22,7 +22,7 @@ data_test_incidents_id = [
 
 @pytest.mark.parametrize('context_data_path, expected_ids', data_test_incidents_id)
 def test_incidents_id(mocker, context_data_path, expected_ids):
-    with open(context_data_path, 'r') as context_data_file:
+    with open(context_data_path) as context_data_file:
         context_data = json.load(context_data_file)
     mocker.patch.object(demisto, 'context', return_value=context_data)
     assert list(incidents_id()) == expected_ids
