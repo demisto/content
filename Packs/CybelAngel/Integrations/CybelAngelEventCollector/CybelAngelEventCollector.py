020fda441a3969e91bd3f80fdf0bd84dc307b497
meichler <meichler@paloaltonetworks.com>
2024-05-16 13:34:51 +0300

    split cisco-meraki integration

--- Packs/CybelAngel/Integrations/CybelAngelEventCollector/CybelAngelEventCollector.py
+++ Packs/CybelAngel/Integrations/CybelAngelEventCollector/CybelAngelEventCollector.py
@@ -1,4 +1,4 @@
-import dateparser
+Removed all redundant empty values in selectValues field.import dateparser
 
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
