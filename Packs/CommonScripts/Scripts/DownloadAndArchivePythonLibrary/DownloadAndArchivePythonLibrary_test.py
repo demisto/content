fab3bbb277158283ae26382da017d4c655f94ced
melamedbn <bmelamed@paloaltonetworks.com>
2024-05-16 16:46:28 +0300

    fix unit-test

--- Packs/CommonScripts/Scripts/DownloadAndArchivePythonLibrary/DownloadAndArchivePythonLibrary_test.py
+++ Packs/CommonScripts/Scripts/DownloadAndArchivePythonLibrary/DownloadAndArchivePythonLibrary_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, MagicMock, mock_open
+from unittest.mock import patch, MagicMock, mock_open, call
 from pathlib import Path
 import shlex
 import subprocess
