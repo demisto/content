32b0c49d4af5c62fd79d5221db79810bab332da7
edik24 <ekatsenelson@paloaltonetworks.com>
2024-05-19 10:31:19 +0300

    Ciac 10185

--- Utils/github_workflow_scripts/handle_external_pr.py
+++ Utils/github_workflow_scripts/handle_external_pr.py
@@ -472,8 +472,7 @@ def main():
     print(f"Security Reviewer: {security_reviewer}")
     print(f"TIM Reviewer: {tim_reviewer}")
 
-    #content_reviewer = find_reviewer_to_assign(content_repo, pr, pr_number, content_reviewers)
-    content_reviewer = determine_random_reviewer(content_reviewers, content_repo)
+    content_reviewer = find_reviewer_to_assign(content_repo, pr, pr_number, content_reviewers)
 
     pr.add_to_assignees(content_reviewer)
     reviewers = [content_reviewer]
