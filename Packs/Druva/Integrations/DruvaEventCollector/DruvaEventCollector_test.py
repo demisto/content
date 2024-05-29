f845918ad617ede6a03143faa8cf2cdc59bb1904
rshunim <102469772+rshunim@users.noreply.github.com>
2024-05-29 15:07:40 +0200

    Update Packs/Druva/Integrations/DruvaEventCollector/DruvaEventCollector_test.py
    Co-authored-by: dorschw <81086590+dorschw@users.noreply.github.com>

--- Packs/Druva/Integrations/DruvaEventCollector/DruvaEventCollector_test.py
+++ Packs/Druva/Integrations/DruvaEventCollector/DruvaEventCollector_test.py
@@ -149,7 +149,7 @@ def test_fetch_events_command(mocker, mock_client):
     - Running fetch-events command
 
     Then:
-    - Ensure number of events fetched, and next run fields
+    - Ensure number of events fetched, and the next run, match the response data
     """
     # First fetch
     mocker.patch.object(mock_client, "search_events", return_value=EVENT_1)
