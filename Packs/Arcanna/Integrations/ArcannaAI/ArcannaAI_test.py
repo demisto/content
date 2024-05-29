903854a9787af632ef7135d54875b20b5f5ff9d0
Bogdan Perian <bperian@siscale.com>
2024-05-29 11:31:03 +0300

    Arcanna.ai v1.2 commands update

--- Packs/Arcanna/Integrations/ArcannaAI/ArcannaAI_test.py
+++ Packs/Arcanna/Integrations/ArcannaAI/ArcannaAI_test.py
@@ -15,6 +15,13 @@ def util_load_json(path):
 
 connection_json = {"connected": True}
 
+arcanna_get_decision_set_response = ['Drop', 'Escalate']
+
+arcanna_trigger_train_Response = {
+    "status": "OK",
+    "error_message": ""
+}
+
 arcanna_inference_response = {
     "event_id": "11013867751676",
     "ingest_timestamp": "2021-06-30T13:57:47.386775",
@@ -26,7 +33,7 @@ arcanna_inference_response = {
 }
 
 arcanna_ingest_response = {
-    "event_id": "11018019921461",
+    "event_id": "20198",
     "ingest_timestamp": "2021-07-01T07:58:03.801992",
     "status": "Pending inference",
     "error_message": ""
@@ -85,16 +92,39 @@ def test_arcanna_send_event_command(mocker):
         "job_id": 10,
         "event_json": "{\"offset\": 1739561255, \"destination\": \"127.0.0.1\"}",
         "severity": 3,
-        "title": "Incident id #20198"
+        "title": "Incident id #20198",
+        "id_value": "20198"
     }
     mocker.patch.object(demisto, 'args', return_value=command_args)
     command_result = post_event(client, command_args)
 
     assert command_result.outputs_prefix == "Arcanna.Event"
-    assert command_result.raw_response['event_id'] == "11018019921461"
+    assert command_result.raw_response['event_id'] == "20198"
     assert command_result.raw_response['error_message'] == ""
     assert command_result.raw_response['status'] == "Pending inference"
 
+
+def test_arcanna_get_decision_set(mocker):
+    mocker.patch.object(client,"get_decision_set", return_value=arcanna_get_decision_set_response)
+    command_args = {
+        "job_id": 10
+    }
+    mocker.patch.object(demisto, 'args', return_value=command_args)
+    assert command_result.outputs_prefix == "Arcanna.Event"
+    assert command_result.outputs_key_field == "decision_set"
+    assert isinstance(command_result.raw_response['decision_set'], list)
+
+def test_arcanna_trigger_train(mocker):
+    mocker.patch.object(client, "trigger_training", return_value=arcanna_trigger_train_response)
+    command_args = {
+       "job_id": 10,
+       "username": "myusername"
+    }
+    mocker.patch.object(demisto, 'args', return_value=command_args)
+    assert command_result.outputs_prefix == "Arcanna.Training"
+    assert command_result.outputs_key_field == "result"
+    assert command_result.raw_response["status"] == "OK"
+    assert command_result.raw_response["error_message"] == ""
 
 def test_arcanna_get_event_status_command(mocker):
     mocker.patch.object(client, "get_event_status", return_value=arcanna_inference_response)
@@ -115,13 +145,12 @@ def test_arcanna_send_event_feedback_command(mocker):
     command_args = {
         "job_id": 10,
         "event_id": 10110011,
-        "label": "Resolved",
-        "cortex_user": "dbot",
-        "closing_notes": "notes"
+        "close_reason": "Resolved",
+        "username": "dbot"
     }
     mocker.patch.object(demisto, 'args', return_value=command_args)
     command_result = send_event_feedback(client=client,
-                                         feature_mapping_field='"Resolved"="escalate_alert"',
+                                         close_reasons=["False Positive","Resolved"],
                                          args=command_args)
 
     assert command_result.outputs_prefix == "Arcanna.Event"
