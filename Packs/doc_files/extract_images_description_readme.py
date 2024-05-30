71813a22164fd94a50889e59373aeac1b84a1018
maimorag <mmorag@paloaltonetworks.com>
2024-05-30 11:54:38 +0300

    fixs

--- Packs/doc_files/extract_images_description_readme.py
+++ Packs/doc_files/extract_images_description_readme.py
@@ -4,6 +4,7 @@ import os
 import json
 import requests
 from pathlib import Path
+from urllib.parse import urlparse
 
 
 rootFolderPath = r'.'
@@ -12,6 +13,7 @@ SAVING_IMAGES_AT = "/Users/mmorag/dev/demisto/content/Packs/doc_files/images"
 
 HTML_IMAGE_LINK_REGEX_SDK = r'(<img.*?src\s*=\s*"(https://.*?)")'
 URL_IMAGE_LINK_REGEX = r'((https?|ftp)://.*?(png|jpe?g|gif|bmp|tiff|webp))'
+URL_IMAGE_LINK_REGEX_SDK = r"(\!\[.*?\])\((?P<url>https://[a-zA-Z_/\.0-9\- :%]*?)\)((].*)?)"
 
 
 def creating_info_jason(file_path: str, image_link: str, folder_path, pack):
@@ -20,17 +22,31 @@ def creating_info_jason(file_path: str, image_link: str, folder_path, pack):
         'image_link': image_link,
         'image_type': 'README' if 'README' in file_path else 'DESCRIPTION'
     }
-    json_path = file_path[file_path.rfind(pack) + len(pack):].rsplit('/', 1)[0]
+    json_path = file_path[file_path.rfind(pack) + len(pack)+1:-3]
     with open(f'{folder_path}/{json_path}.json', "a") as json_file:
         json_file.write(json.dumps(image_details))
 
 
-def extract_image_link(text):
+def extract_image_link(lines, final_dst_image_path, original_file_path):
     # Regular expression to match URLs ending with common image file extensions
-    url_match = re.findall(URL_IMAGE_LINK_REGEX, text)
-    html_match = re.findall(HTML_IMAGE_LINK_REGEX_SDK, text)
-    images_links = url_match + html_match
-    return images_links
+    urls_list = []
+    for i, line in enumerate(lines):
+        if res := re.search(URL_IMAGE_LINK_REGEX, line):
+            url = res["url"]
+            parse_url = urlparse(url)
+            url_path = Path(parse_url.path)
+            image_name = url_path.name
+            new_replace_url = os.path.join(final_dst_image_path, image_name)
+            urls_list.append(
+                {
+                    "original_url": url,
+                    "original_file_path": original_file_path,
+                    "final_dst_image_path": new_replace_url,
+                    "image_name": image_name,
+
+                }
+            )
+    return urls_list
 
 
 def download_image_to_folder(folder_path, url):
@@ -63,14 +79,14 @@ def search_image_links(file_path):
         The folder name is derived from the file name by removing the extension and appending it to the specified path 'SAVING_IMAGES_AT'.
     '''
     with open(file_path, encoding='utf-8') as file:
-        content = file.read()
-        if image_links := extract_image_link(content):
-            pack_name = re.search(r'/Packs/([^/]+)/', file_path).group(1)
-            folder_path = f'{SAVING_IMAGES_AT}/{pack_name}'
+        file_lines = file.readlines()
+        pack_name = re.search(r'/Packs/([^/]+)/', file_path).group(1)
+        folder_path = f'{SAVING_IMAGES_AT}/{pack_name}'
+        if image_links := extract_image_link(file_lines, folder_path, file_path):
             try:
                 os.mkdir(folder_path)
                 for image_link in image_links:
-                    creating_info_jason(file_path, image_link, folder_path)
+                    creating_info_jason(file_path, image_link["original_url"], folder_path, pack_name)
                     download_image_to_folder(folder_path, file_path)
             except OSError as error:
                 print(error)
