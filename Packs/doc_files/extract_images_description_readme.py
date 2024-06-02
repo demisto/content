14f1e2878f60d6e0193cf96658369c99c2f2ef1e
maimorag <mmorag@paloaltonetworks.com>
2024-06-02 15:21:20 +0300

    fix script after demo

--- Packs/doc_files/extract_images_description_readme.py
+++ Packs/doc_files/extract_images_description_readme.py
@@ -6,24 +6,10 @@ import requests
 from pathlib import Path
 from urllib.parse import urlparse
 
-
-rootFolderPath = r'.'
-PACKS_PATH = "/Users/mmorag/dev/demisto/content/Packs"
-SAVING_IMAGES_AT = "/Users/mmorag/dev/demisto/content/Packs/doc_files/Packs_Images"
-
+PACKS_PATH = '/Users/mmorag/dev/demisto/content/Packs'
+LOGS_IMAGES_PER_PACK = "/Users/mmorag/dev/demisto/content/Packs/doc_files"
 HTML_IMAGE_LINK_REGEX_SDK = r'(<img.*?src\s*=\s*"(https://.*?)")'
-URL_IMAGE_LINK_REGEX = r'((https?|ftp)://.*?(png|jpe?g|gif|bmp|tiff|webp))'
-URL_IMAGE_LINK_REGEX_SDK = r"(\!\[.*?\])\((?P<url>https://[a-zA-Z_/\.0-9\- :%]*?)\)((].*)?)"
-
-
-def creating_info_jason(file_path: str, image_link: str, folder_path, pack):
-    image_details = {
-        'file_path': file_path,
-        'image_link': image_link,
-    }
-    json_path = file_path[file_path.rfind(pack) + len(pack)+1:-3]
-    with open(f'{folder_path}/{json_path}.json', "a") as json_file:
-        json_file.write(json.dumps(image_details))
+URL_IMAGE_LINK_REGEX = r"(\!\[.*?\])\((?P<url>https://[a-zA-Z_/\.0-9\- :%]*?)\)((].*)?)"
 
 
 def extract_image_link(lines, final_dst_image_path, original_file_path):
@@ -31,27 +17,37 @@ def extract_image_link(lines, final_dst_image_path, original_file_path):
     urls_list = []
     for i, line in enumerate(lines):
         if res := re.search(URL_IMAGE_LINK_REGEX, line):
-            url = res.group(0)
+            url = res.group("url")
             # parse_url = urlparse(url)
-            # url_path = Path(parse_url.path)
             urls_list.append(url)
     return urls_list
 
 
 def download_image_to_folder(folder_path, url):
+    """
+    Downloads an image from a given URL and saves it to a specified folder.
+
+    Args:
+        folder_path (str): The path to the folder where the image will be saved.
+        url (str): The URL of the image to be downloaded.
+
+    Returns:
+        int: The HTTP status code of the response. 200 indicates success.
+    """
     response = requests.get(url, verify=False)
     if response.status_code == 200:
         filename = url.split('/')[-1]
         full_path = os.path.join(folder_path, filename)
         with open(full_path, 'wb') as f:
             f.write(response.content)
-        print(f"Image downloaded and saved to {full_path}")
+        print(f"Image downloaded and saved to {full_path}")  # for debugging, removing it later
     else:
-        print("Failed to download image")
+        print("Failed to download image")  # for debugging, removing it later
+    return response.status_code
 
 
 def search_image_links(file_path):
-    '''
+    """
         Searches for image links in the given file and downloads the images to a folder.
 
         Parameters:
@@ -62,53 +58,56 @@ def search_image_links(file_path):
 
         Raises:
             OSError: If there is an error creating the folder to save images or downloading images.
-
-        The function reads the content of the file located at 'file_path' and extracts image links from it.
-        Then, it creates a folder to save the images and downloads each image to the folder.
-        The folder name is derived from the file name by removing the extension and appending it to the specified path 'SAVING_IMAGES_AT'.
-    '''
-    images_counter = 0
-    with open(file_path, encoding='utf-8') as file:
+    """
+    images_information_dict = {'Image successfully downloaded': [],
+                               'Failed to download image': []}
+    with (open(file_path, encoding='utf-8') as file):
         file_lines = file.readlines()
         pack_name = re.search(r'/Packs/([^/]+)/', file_path).group(1)
-        folder_path = f'{SAVING_IMAGES_AT}/{pack_name}'
+        folder_path = f'/Users/mmorag/dev/demisto/content/Packs/{pack_name}/doc_files'
         if image_links := extract_image_link(file_lines, folder_path, file_path):
-            try:
-                os.mkdir(folder_path)
-                for image_link in image_links:
-                    creating_info_jason(file_path, image_link, folder_path, pack_name)
-                    download_image_to_folder(folder_path, image_link)
-                    images_counter += 1
-                return images_counter
-            except OSError as error:
-                print(error)
+            if not os.path.exists(folder_path):
+                try:
+                    os.makedirs(folder_path)
+                except OSError as error:
+                    print(error)
+            for image_link in image_links:
+                status = download_image_to_folder(folder_path, image_link)
+                if status == 200:
+                    images_information_dict['Image successfully downloaded'].append(image_link)
+                else:
+                    images_information_dict['Failed to download image'].append({'image_link': image_link,
+                                                                                'status': status})
+            images_information_dict['Total Downloaded'] = len(images_information_dict['Image successfully downloaded'])
+            return images_information_dict
 
 
-def search_files(root_path):
-    readme_paths_links = list(Path(root_path).rglob("README.md"))
-    description_paths_links = list(Path(root_path).rglob("*description.md"))
-    images_readme_information = {}
-    images_description_information = {}
-    for link in readme_paths_links:
-        counter_images_readme = search_image_links(str(link))
-        if counter_images_readme:
-            images_readme_information[link] = counter_images_readme
+def extract_image_links_from_files_and_save_to_json():
+    """
+    Searches for files matching a specified pattern within a directory and its subdirectories,
+    then extracts image links from those files and saves the information to a JSON file.
 
-    for link in description_paths_links:
-        counter_description_readme = search_image_links(str(link))
-        if counter_description_readme:
-            images_description_information[link] = counter_description_readme
+    Args:
+        pattern_to_search (str): The pattern to search for in file names.
+        json_file_name (str): The name for the JSON file to save the images information.
+    """
+    paths_links = list(Path(PACKS_PATH).rglob("*.md"))
+    images_information = {}
 
-    with open('/Users/mmorag/dev/demisto/content/Packs/doc_files/readme_images_info.json', "a") as json_file:
-        json_file.write(json.dumps(images_readme_information))
+    for link in paths_links:
+        images_information_log = search_image_links(str(link))
+        if images_information_log:
+            images_information[str(link)] = images_information_log
 
-    with open('/Users/mmorag/dev/demisto/content/Packs/doc_files/description_images_info.json', "a") as json_file:
-        json_file.write(json.dumps(images_description_information))
+    with open(f'/Users/mmorag/dev/demisto/content/Packs/doc_files/logs_images_info.json', "a") as file:
+        file.write(json.dumps(images_information))
 
 
 def main():
-    path = '/Users/mmorag/dev/demisto/content/Packs/CortexAttackSurfaceManagement'
-    search_files(path)
+    try:
+        extract_image_links_from_files_and_save_to_json()
+    except Exception as e:
+        print(e)
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
