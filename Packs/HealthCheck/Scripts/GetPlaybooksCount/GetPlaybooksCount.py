4b705bbe4895e96b25360b6a2fb91e7fa0a6ef25
sharatpatil7 <77386454+sharatpatil7@users.noreply.github.com>
2024-06-03 18:01:57 +0800

    Update GetPlaybooksCount.py
    Removed the demisto.args() which was used 3 times

--- Packs/HealthCheck/Scripts/GetPlaybooksCount/GetPlaybooksCount.py
+++ Packs/HealthCheck/Scripts/GetPlaybooksCount/GetPlaybooksCount.py
@@ -1,7 +1,7 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-payload1 = {'query': demisto.args().get('query', 'system:T')}
+payload1 = {'query': 'system:T'}
 res1 = demisto.executeCommand("core-api-post", {"uri": "playbook/search",
                               "body": json.dumps(payload1)})[0]["Contents"]["response"]
 
@@ -9,14 +9,14 @@ if not res1['playbooks']:
     res1['playbooks'] = []
 
 
-payload2 = {'query': demisto.args().get('query', 'system:F')}
+payload2 = {'query': 'system:F'}
 res2 = demisto.executeCommand("core-api-post", {"uri": "playbook/search",
                               "body": json.dumps(payload2)})[0]["Contents"]["response"]
 
 if not res2['playbooks']:
     res2['playbooks'] = []
 
-payload3 = {'query': demisto.args().get('query', 'deprecated:T')}
+payload3 = {'query': 'deprecated:T'}
 res3 = demisto.executeCommand("core-api-post", {"uri": "playbook/search",
                               "body": json.dumps(payload3)})[0]["Contents"]["response"]
 
