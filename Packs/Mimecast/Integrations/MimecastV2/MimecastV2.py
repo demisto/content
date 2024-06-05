1a50c894034a2c400b649a202cdaa7f2100a258c
YairGlik <yair.glik33@gmail.com>
2024-06-05 16:35:23 +0300

    fix CLIENT_SECRET

--- Packs/Mimecast/Integrations/MimecastV2/MimecastV2.py
+++ Packs/Mimecast/Integrations/MimecastV2/MimecastV2.py
@@ -48,11 +48,8 @@ FETCH_DELTA = int(demisto.params().get('fetchDelta', 24))
 
 
 CLIENT_ID = demisto.params().get('client_id')
-CLIENT_SECRET = demisto.params().get('client_secret')
+CLIENT_SECRET = demisto.params().get('client_secret', {}).get("password")
 USE_OAUTH2 = bool(CLIENT_ID and CLIENT_SECRET)
-demisto.info(f"{USE_OAUTH2=}")
-demisto.info(f"len CLIENT_ID :{len(str(CLIENT_ID))}")
-demisto.info(f"len CLIENT_SECRET:{len(str(CLIENT_SECRET))}")
 TOKEN_OAUTH2 = ""
 DEFAULT_POLICY_TYPE = 'blockedsenders'
 LOG(f"command is {demisto.command()}")
