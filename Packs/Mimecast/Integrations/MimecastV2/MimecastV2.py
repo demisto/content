c4b0e4443eb81ae7209750a487bbb873ae260c45
YairGlik <yair.glik33@gmail.com>
2024-06-06 15:28:10 +0300

    CLIENT_ID to str

--- Packs/Mimecast/Integrations/MimecastV2/MimecastV2.py
+++ Packs/Mimecast/Integrations/MimecastV2/MimecastV2.py
@@ -47,10 +47,12 @@ if isinstance(PASSWORD, dict):
 FETCH_DELTA = int(demisto.params().get('fetchDelta', 24))
 
 
-CLIENT_ID = demisto.params().get('client_id')
+CLIENT_ID = str(demisto.params().get('client_id'))
 CLIENT_SECRET = demisto.params().get('client_secret', {}).get("password")
 USE_OAUTH2 = bool(CLIENT_ID and CLIENT_SECRET)
 demisto.info(f"{USE_OAUTH2=}")
+demisto.info(f"{len(str(CLIENT_ID))}")
+demisto.info(f"{len(CLIENT_SECRET)}")
 TOKEN_OAUTH2 = ""
 DEFAULT_POLICY_TYPE = 'blockedsenders'
 LOG(f"command is {demisto.command()}")
