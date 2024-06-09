cb62a4ea16d118335bd31c13462ad5b67e630d41
TalZich <tzichlinsky@paloaltonetworks.com>
2024-06-09 11:39:18 +0300

    Add UT for Atlassian JSM

--- Packs/AtlassianJiraServiceManagement/Integrations/AtlassianJiraServiceManagement/AtlassianJiraServiceManagement_test.py
+++ Packs/AtlassianJiraServiceManagement/Integrations/AtlassianJiraServiceManagement/AtlassianJiraServiceManagement_test.py
@@ -3,15 +3,19 @@ from pytest_mock import MockerFixture
 from typing import Any
 import AtlassianJiraServiceManagement as JSM
 import json
+from requests import Response
+from CommonServerPython import DemistoException
 
 
 def util_load_json(path):
     with open(path, encoding='utf-8') as f:
         return json.loads(f.read())
 
+
 command_test_data = util_load_json('./test_data/command_test_data.json')
 
 client = JSM.Client(base_url='https://url.com', verify=False, proxy=False, api_key='key123')
+
 
 @pytest.mark.parametrize(
     "objects, key_mapping, expected_result",
@@ -62,14 +66,14 @@ def test_pascal_case(input_str, expected_output):
 
 def test_get_object_outputs():
     objects = [
-                {'ObjectType': {'name': 'typeA'}, 'Avatar': 'avatarA', 'ID': 123, 'key': 'value'},
-                {'ObjectType': {'name': 'typeB'}, 'Avatar': 'avatarB', 'ID': 456, 'key': 'value'}
-            ]
+        {'ObjectType': {'name': 'typeA'}, 'Avatar': 'avatarA', 'ID': 123, 'key': 'value'},
+        {'ObjectType': {'name': 'typeB'}, 'Avatar': 'avatarB', 'ID': 456, 'key': 'value'}
+    ]
 
     expected_output = (
-                [{'ID': 123, 'key': 'value'},{'ID': 456, 'key': 'value'}],
-                [{'Type': 'typeA', 'ID': 123, 'key': 'value'}, {'Type': 'typeB', 'ID': 456, 'key': 'value'}],
-            )
+        [{'ID': 123, 'key': 'value'}, {'ID': 456, 'key': 'value'}],
+        [{'Type': 'typeA', 'ID': 123, 'key': 'value'}, {'Type': 'typeB', 'ID': 456, 'key': 'value'}],
+    )
     assert JSM.get_object_outputs(objects) == expected_output
 
 
@@ -84,7 +88,8 @@ def test_clean_object_attributes(attributes, expected):
 
 
 @pytest.mark.parametrize("attributes, expected", [
-    ({"attr1": ["value1", "value2"]}, [{"objectTypeAttributeId": "attr1", "objectAttributeValues": [{"value": "value1"}, {"value": "value2"}]}]),
+    ({"attr1": ["value1", "value2"]},
+     [{"objectTypeAttributeId": "attr1", "objectAttributeValues": [{"value": "value1"}, {"value": "value2"}]}]),
     ({"attr2": ["hello"]}, [{"objectTypeAttributeId": "attr2", "objectAttributeValues": [{"value": "hello"}]}]),
     ({}, []),
 ])
@@ -257,7 +262,7 @@ def test_jira_asset_object_schema_list_command(mocker: MockerFixture, args: dict
     ({'all_results': 'true', 'schema_id': '1'}, 5),
     ({'all_results': 'true', 'limit': 2, 'schema_id': '1'}, 5),
 ])
-def test_jira_asset_object_type_list_command(mocker: MockerFixture, args: dict[str,Any], expected_len: int):
+def test_jira_asset_object_type_list_command(mocker: MockerFixture, args: dict[str, Any], expected_len: int):
     """
     Given: An args dict with limit and/or all_results
     When: Calling the jira_asset_object_type_list_command with the args dict
@@ -276,7 +281,7 @@ def test_jira_asset_object_type_list_command(mocker: MockerFixture, args: dict[s
     ({'all_results': 'true', 'object_type_id': '1'}, 6),
     ({'all_results': 'true', 'limit': 2, 'object_type_id': '1'}, 6),
 ])
-def test_jira_asset_object_type_attribute_list_command(mocker: MockerFixture, args: dict[str,Any], expected_len: int):
+def test_jira_asset_object_type_attribute_list_command(mocker: MockerFixture, args: dict[str, Any], expected_len: int):
     """
     Given: An args dict with limit and/or all_results
     When: Calling the jira_asset_object_type_list_command with the args dict
@@ -285,6 +290,111 @@ def test_jira_asset_object_type_attribute_list_command(mocker: MockerFixture, ar
     mocked_return_value = command_test_data['object_type_attributes']['response']
     mocked_client = mocker.patch.object(client, 'get_object_type_attributes', return_value=mocked_return_value)
     command_results = JSM.jira_asset_object_type_attribute_list_command(client, args)
-    mocked_client.assert_called_with(object_type_id=args['object_type_id'], order_by_name=False, query=None, include_value_exist=False, exclude_parent_attributes=False, include_children=False, order_by_required=False)
     assert len(command_results.outputs) == expected_len
+    mocked_client.assert_called_with(
+        object_type_id=args['object_type_id'],
+        order_by_name=False,
+        query=None,
+        include_value_exist=False,
+        exclude_parent_attributes=False,
+        include_children=False,
+        order_by_required=False
+    )
+
+
+def test_jira_asset_object_create_command(mocker: MockerFixture):
+    """
+    Given: An object_type_id and a string of attributes
+    When: The object_create command is called with said arguments
+    Then: The client's create_object function is called with said arguments in the format that the
+            get_attributes_json_data function returns
+    """
+    object_type_id = "1"
+    attributes = json.dumps({"1": ["value1"], "2": ["value1", "value2"]})
+    attributes_json_data = JSM.get_attributes_json_data(object_type_id, attributes)
+    mocked_client = mocker.patch.object(
+        client,
+        'create_object',
+        return_value=command_test_data['object_create']['response']
+    )
+    JSM.jira_asset_object_create_command(client, {"object_type_id": object_type_id, "attributes": attributes})
+    mocked_client.assert_called_with(attributes_json_data)
+
+
+def test_jira_asset_object_update_command(mocker: MockerFixture):
+    """
+    Given: An object_id and a string of attributes
+    When: The object_update command is called with said arguments
+    Then: The client's update_object function is called with said arguments in the format that the
+            get_attributes_json_data function returns
+    """
+    object_id = "1"
+    object_type_id = "3"
+    attributes = json.dumps({"1": ["value1"], "2": ["value1", "value2"]})
+    attributes_json_data = JSM.get_attributes_json_data(object_type_id, attributes)
+    mocker.patch.object(client, 'get_object', return_value=command_test_data['get_object']['response'])
+    mocked_update_object = mocker.patch.object(
+        client,
+        'update_object',
+        return_value=command_test_data['object_update']['response']
+    )
+    JSM.jira_asset_object_update_command(client, {"object_id": object_id, "attributes": attributes})
+    mocked_update_object.assert_called_with(object_id, attributes_json_data)
+
+
+def test_jira_asset_object_delete_command(mocker: MockerFixture):
+    """
+    Given: An object id
+    When: The jira_asset_object_delete_command function is called
+    Then: The client's delete_object function is called with said object id
+    """
+    object_id = "1"
+    mocked_client = mocker.patch.object(client, 'delete_object', return_value={})
+    JSM.jira_asset_object_delete_command(client, {"object_id": object_id})
+    mocked_client.assert_called_with(object_id)
+
+
+def test_jira_asset_object_delete_non_exising_object(mocker: MockerFixture):
+    """
+    Given: A non-existing object id
+    When: The client's delete_object function raises a 404 error
+    Then: The jira_asset_object_delete_command function returns a specific hr response
+    """
+    object_id = "-1"
+    res = Response()
+    res.status_code = 404
+    mock_delete = mocker.patch.object(client, 'delete_object', side_effect=DemistoException(message="", res=res))
+    command_result = JSM.jira_asset_object_delete_command(client, {"object_id": object_id})
+    mock_delete.assert_called_with(object_id)
+    assert command_result.readable_output == f'Object with id: {object_id} does not exist'
+
+
+def test_jira_object_get_command(mocker: MockerFixture):
+    """
+    Given: An object id
+    When: The jira_asset_object_get_command function is called
+    Then: The client's get_object function is called with said object id
+    """
+    object_id = "1"
+    mocked_client = mocker.patch.object(client, 'get_object', return_value=command_test_data['get_object']['response'])
+    JSM.jira_asset_object_get_command(client, {"object_id": object_id})
+    mocked_client.assert_called_with(object_id)
+
+
+def test_jira_asset_object_get_non_exising_object(mocker: MockerFixture):
+    """
+    Given: A non-existing object id
+    When: The client's get_object function raises a 404 error
+    Then: The jira_asset_object_get_command function returns a specific hr response
+    """
+    object_id = "-1"
+    res = Response()
+    res.status_code = 404
+    mock_delete = mocker.patch.object(client, 'get_object', side_effect=DemistoException(message="", res=res))
+    command_result = JSM.jira_asset_object_get_command(client, {"object_id": object_id})
+    mock_delete.assert_called_with(object_id)
+    assert command_result.readable_output == f'Object with id: {object_id} does not exist'
+
+
+# def test_jira_asset_object_search_command(mocker: MockerFixture):
 
