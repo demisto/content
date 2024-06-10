45725831b8e37408dbf2f0aec2babc3cf5de8b61
sharatpatil7 <77386454+sharatpatil7@users.noreply.github.com>
2024-06-10 15:10:42 +0800

    Update GetDockerContainersCount.py

--- Packs/HealthCheck/Scripts/GetDockerContainersCount/GetDockerContainersCount.py
+++ Packs/HealthCheck/Scripts/GetDockerContainersCount/GetDockerContainersCount.py
@@ -4,7 +4,7 @@ from CommonServerPython import *  # noqa: F401
 res = demisto.executeCommand("core-api-get", {"uri": "diagnostics/checks"})[0]["Contents"]["response"]
 
 wList = []
-wList.append({"Max Limit": res["checkRuns"]["diagnostic.docker.containers.count"]["result"]["metrics"]["issueLimit"],
+wList.append({"Max Limit": res["checkRuns"]["diagnostic.docker.containers.count"]["result"]["metrics"]["issueLimit"],\
              "Current Count": res["checkRuns"]["diagnostic.docker.containers.count"]["result"]["metrics"]["currentCount"]})
 
 demisto.results({"total": len(wList), "data": wList})
