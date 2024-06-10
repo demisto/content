528d734943a634b81727cbcaa032e429a0a29f38
dorschw <81086590+dorschw@users.noreply.github.com>
2024-06-10 14:04:19 +0300

    clarify filters

--- Packs/MicrosoftCloudAppSecurity/Integrations/MicrosoftDefenderEventCollector/MicrosoftDefenderEventCollector.py
+++ Packs/MicrosoftCloudAppSecurity/Integrations/MicrosoftDefenderEventCollector/MicrosoftDefenderEventCollector.py
@@ -5,7 +5,7 @@ from CommonServerPython import *  # noqa: F401
 
 from CommonServerUserPython import *  # noqa
 from abc import ABC
-from typing import Any
+from typing import Any, NamedTuple
 from collections.abc import Callable
 
 from enum import Enum
@@ -20,17 +20,25 @@ import urllib3
 # Disable insecure warnings
 urllib3.disable_warnings()
 
+
+class EventFilter(NamedTuple):
+    ui_name: str
+    name: str
+    attributes: dict
+
+
+ALERTS_FILTER = EventFilter('Alerts', 'alerts', {'type': 'alerts', 'filters': {}})
+ADMIN_ACTIVITIES_FILTER = EventFilter('Admin activities', 'activities_admin', {
+                                      'type': 'activities', 'filters': {"activity.type": {"eq": True}}})
+LOGIN_ACTIVITIES_FILTER = EventFilter('Login activities', 'activities_login', {'type': 'activities', 'filters': {
+    "activity.eventType": {"eq": ["EVENT_CATEGORY_LOGIN", "EVENT_CATEGORY_FAILED_LOGIN"]}}})
+
+ALL_EVENT_FILTERS: list[EventFilter] = [ALERTS_FILTER, ADMIN_ACTIVITIES_FILTER, LOGIN_ACTIVITIES_FILTER]
+
+UI_NAME_TO_EVENT_FILTERS = {event_filter.ui_name: event_filter for event_filter in ALL_EVENT_FILTERS}
+
 ''' CONSTANTS '''
 AUTH_ERROR_MSG = 'Authorization Error: make sure tenant id, client id and client secret is correctly set'
-ALL_EVENT_TYPES = 'Alerts,Admin activities,Login activities'
-MAPPING_EVENT_TYPES = {'Alerts': 'alerts',
-                       'Admin activities': 'activities_admin',
-                       'Login activities': 'activities_login'
-                       }
-FILTERS_EVENT_TYPES = {'alerts': {'type': 'alerts', 'filters': {}},
-                       'activities_admin': {'type': 'activities', 'filters': {"activity.type": {"eq": True}}},
-                       'activities_login': {'type': 'activities', 'filters': {
-                           "activity.eventType": {"eq": ["EVENT_CATEGORY_LOGIN", "EVENT_CATEGORY_FAILED_LOGIN"]}}}}
 VENDOR = "Microsoft"
 PRODUCT = "defender_cloud_apps"
 
@@ -158,13 +166,13 @@ class IntegrationEventsClient(ABC):
 
 class IntegrationGetEvents(ABC):
     def __init__(
-        self, client: IntegrationEventsClient, options: IntegrationOptions, event_types_to_fetch: list[str]
+        self, client: IntegrationEventsClient, options: IntegrationOptions, event_filters: list[EventFilter]
     ) -> None:
         self.client = client
         self.options = options
-        self.event_types_to_fetch = {
-            MAPPING_EVENT_TYPES[event_type]: FILTERS_EVENT_TYPES[MAPPING_EVENT_TYPES[event_type]]
-            for event_type in event_types_to_fetch
+        self.filter_name_to_attributes = {
+            event_filter.name: event_filter.attributes
+            for event_filter in event_filters
         }
 
     def run(self):
@@ -290,8 +298,8 @@ class DefenderGetEvents(IntegrationGetEvents):
         # - activities with filter to get the admin events
         # - activities with different filter to get the login events
         # - alerts with no filter
-        # event_types_to_fetch, a dictionary contains the filters and the endpoint according to the event type.
-        for event_type_name, endpoint_details in self.event_types_to_fetch.items():
+        # filter_name_to_attributes, a dictionary contains the filters and the endpoint according to the event type.
+        for event_type_name, endpoint_details in self.filter_name_to_attributes.items():
             self.client.request.params.pop('filters', None)
             self.client.request.url = parse_obj_as(HttpUrl, f'{base_url}{endpoint_details["type"]}')
 
@@ -393,7 +401,12 @@ def main(command: str, demisto_params: dict):
     try:
         demisto_params['client_secret'] = demisto_params['credentials']['password']
         push_to_xsiam = argToBoolean(demisto_params.get('should_push_events', 'false'))
-        event_types_to_fetch = argToList(demisto_params.get('event_types_to_fetch', ALL_EVENT_TYPES))
+
+        if user_requested_event_types := argToList(demisto_params.get('event_types_to_fetch', [])):
+            event_filters: list[EventFilter] = [event_filter for ui_name, event_filter in UI_NAME_TO_EVENT_FILTERS.items()
+                                                if ui_name in user_requested_event_types]
+        else:
+            event_filters = ALL_EVENT_FILTERS
 
         after = demisto_params.get('after')
         if after and not isinstance(after, int):
@@ -406,7 +419,7 @@ def main(command: str, demisto_params: dict):
 
         client = DefenderClient(request=request, options=options, authenticator=authenticator,
                                 after=after)
-        get_events = DefenderGetEvents(client=client, options=options, event_types_to_fetch=event_types_to_fetch)
+        get_events = DefenderGetEvents(client=client, options=options, event_filters=event_filters)
 
         if command == 'test-module':
             return_results(module_test(get_events=get_events))
