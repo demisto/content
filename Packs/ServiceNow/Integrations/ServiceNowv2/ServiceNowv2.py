b8af81cb2d982e3e01b58b017acbb53a617aa52d
merit-maita <meretmaayta@gmail.com>
2024-06-16 16:29:56 +0300

    fixed an issue where test-module failed with oauth process.

--- Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
+++ Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
@@ -2438,10 +2438,14 @@ def test_module(client: Client, *_) -> tuple[str, dict[Any, Any], dict[Any, Any]
     """
     Test the instance configurations when using basic authorization.
     """
-    # Notify the user that test button can't be used when using OAuth 2.0:
     if client.use_oauth:
-        raise Exception('Test button cannot be used when using OAuth 2.0. Please use the !servicenow-oauth-login '
-                        'command followed by the !servicenow-oauth-test command to test the instance.')
+        context = get_integration_context()
+        # servicenow-oauth-login command was never executed, and refresh_token was not stored in integration context.
+        if not context.get('refresh_token'):
+            raise Exception('Test button cannot be used when using OAuth 2.0. Please use the !servicenow-oauth-login '
+                            'command followed by the !servicenow-oauth-test command to test the instance.')
+
+        test_instance(client)
 
     if client._version == 'v2' and client.get_attachments:
         raise DemistoException('Retrieving incident attachments is not supported when using the V2 API.')
