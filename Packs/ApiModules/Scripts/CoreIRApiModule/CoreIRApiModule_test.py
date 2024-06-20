5d6c9c27d8904cfaeacd0418646fcbc7f6921714
rshunim <rshunim@paloaltonetworks.com>
2024-06-20 14:46:40 +0300

    UT

--- Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule_test.py
+++ Packs/ApiModules/Scripts/CoreIRApiModule/CoreIRApiModule_test.py
@@ -4053,3 +4053,26 @@ def test_xsoar_to_xdr_flexible_close_reason_mapping(capfd, mocker, custom_mappin
 
         assert remote_args.delta.get('status')
         assert remote_args.delta['status'] == expected_resolved_status[i]
+
+
+def test_http_request_demisto_call(mocker):
+    """
+    Given:
+        - An XSIAM machine with a build version that supports demisto._apiCall() with RBAC validations.
+    When:
+        - Calling the http_request method.
+    Then:
+        - Make sure demisto._apiCall() is being called and the method returns the expected result.
+    """
+    from CoreIRApiModule import CoreClient
+    client = CoreClient(
+        base_url=f'{Core_URL}/public_api/v1', headers={}
+    )
+    data = '{"reply": {"container": ["1.1.1.1"]}}}'
+    mocker.patch("CoreIRApiModule.FORWARD_USER_RUN_RBAC", new=True)
+    mocker.patch.object(demisto, "_apiCall", return_value={'name': '/api/webapp/public_api/v1/distributions/get_versions/',
+                                                           'status': 200,
+                                                           'data': '{"reply": {"container": ["1.1.1.1"]}}}'})
+    res = client._http_request(method="POST",
+                               url_suffix="/distributions/get_versions/")
+    assert res == {"data": {"container": ["1.1.1.1"]}}
