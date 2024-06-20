7b5d714c64029af9e92483297c0dd609fede446b
okarkkatz <okarkkatz@paloaltonetworks.com>
2024-06-20 14:51:18 +0300

    added a main function

--- Packs/ProofpointTAP/Scripts/ProofpointTAPMostAttackedUsers/ProofpointTAPMostAttackedUsers.py
+++ Packs/ProofpointTAP/Scripts/ProofpointTAPMostAttackedUsers/ProofpointTAPMostAttackedUsers.py
@@ -1,24 +1,27 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
+def main():
+    users = []
+    users_res_for_chart = []
+    most_attacked_user_list = demisto.executeCommand('proofpoint-list-most-attacked-users', {'window': 14})
+    results = most_attacked_user_list[0].get('Contents', {})    # type: ignore
 
-users = []
-users_res_for_chart = []
-most_attacked_user_list = demisto.executeCommand('proofpoint-list-most-attacked-users', {'window': 14})
-results = most_attacked_user_list[0].get('Contents', {})    # type: ignore
+    if isinstance(results, dict):
+        # In the case the integration is not configured, this value will return as str.
+        # Unsupported Command..
+        users = results.get('users', [])
 
-if isinstance(results, dict):
-	# In the case the integration is not configured, this value will return as str.
-	# Unsupported Command..
-	users = results.get('users', [])
+    for user in users:
+        users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
+                    "data": [user.get("threatStatistics").get("attackIndex")]})
 
-for user in users:
-    users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
-                  "data": [user.get("threatStatistics").get("attackIndex")]})
+    default_empty_chart_data = [
+        {"name": "", "data": [], "color": ""},
+    ]
 
-default_empty_chart_data = [
-    {"name": "", "data": [], "color": ""},
-]
+    final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
+    return_results(json.dumps(final_res))
 
-final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
-return_results(json.dumps(final_res))
+if __name__ in ['__main__', 'builtin', 'builtins']:
+    main()
--- Packs/ProofpointTAP/Scripts/ProofpointTapTopClickers/ProofpointTapTopClickers.py
+++ Packs/ProofpointTAP/Scripts/ProofpointTapTopClickers/ProofpointTapTopClickers.py
@@ -1,19 +1,23 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-users = []
-users_res_for_chart = []
-results: dict = demisto.executeCommand('proofpoint-get-top-clickers', {'window': 90})[0]  # type: ignore
-contents = results.get('Contents')
-if isinstance(contents, dict):
-	users = contents.get('users', [])
+def main():
+    users = []
+    users_res_for_chart = []
+    results: dict = demisto.executeCommand('proofpoint-get-top-clickers', {'window': 90})[0]  # type: ignore
+    contents = results.get('Contents')
+    if isinstance(contents, dict):
+        users = contents.get('users', [])
 
-for user in users:
-    users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
-                  "data": [user.get("clickStatistics").get("clickCount")]})
-default_empty_chart_data = [
-    {"name": "", "data": [], "color": ""},
-]
+    for user in users:
+        users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
+                    "data": [user.get("clickStatistics").get("clickCount")]})
+    default_empty_chart_data = [
+        {"name": "", "data": [], "color": ""},
+    ]
 
-final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
-return_results(json.dumps(final_res))
+    final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
+    return_results(json.dumps(final_res))
+    
+if __name__ in ['__main__', 'builtin', 'builtins']:
+    main()
