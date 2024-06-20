e790c6db75b397154898e0f84ea9fa761d0245bf
okarkkatz <okarkkatz@paloaltonetworks.com>
2024-06-20 14:31:58 +0300

    proofpoint tap most attcked users widget

--- Packs/ProofpointTAP/Scripts/ProofpointTAPMostAttackedUsers/ProofpointTAPMostAttackedUsers.py
+++ Packs/ProofpointTAP/Scripts/ProofpointTAPMostAttackedUsers/ProofpointTAPMostAttackedUsers.py
@@ -1,11 +1,23 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
-results = demisto.executeCommand('proofpoint-list-most-attacked-users', {'window': 14})[0]['Contents']['users']
-users = []
+most_attacked_user_list = demisto.executeCommand('proofpoint-list-most-attacked-users', {'window': 14})
+results = most_attacked_user_list[0].get('Contents', {})    # type: ignore
 
-for user in results:
+if isinstance(results, dict):
+	# In the case the integration is not configured, this value will return as str.
+	# Unsupported Command..
+	users = results.get('users', [])
+
+for user in users:
     users.append({"name": user.get("identity").get("emails", [""])[0],
                   "data": [user.get("threatStatistics").get("attackIndex")]})
+
+data = [
+    {"name": "", "data": [], "color": ""},
+]
+
+final = users if users else data
+demisto.results(json.dumps(final))
 
 return_results(json.dumps(users))
