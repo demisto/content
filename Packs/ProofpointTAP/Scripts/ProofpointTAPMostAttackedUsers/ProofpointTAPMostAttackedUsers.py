112a915ae0904eb99aedd8f17dd2a595d91766c5
okarkkatz <okarkkatz@paloaltonetworks.com>
2024-06-22 20:38:22 +0300

    pre commit fixes

--- Packs/ProofpointTAP/Scripts/ProofpointTAPMostAttackedUsers/ProofpointTAPMostAttackedUsers.py
+++ Packs/ProofpointTAP/Scripts/ProofpointTAPMostAttackedUsers/ProofpointTAPMostAttackedUsers.py
@@ -3,25 +3,29 @@ from CommonServerPython import *  # noqa: F401
 
 
 def main():
-    most_attacked_user_list: dict = demisto.executeCommand('proofpoint-list-most-attacked-users',
-                                                           {'window': 14})[0]  # type: ignore
-    contents = most_attacked_user_list.get('Contents')
+    try:
+        most_attacked_user_list: dict = demisto.executeCommand('proofpoint-list-most-attacked-users',
+                                                               {'window': 14})[0]  # type: ignore
+        contents = most_attacked_user_list.get('Contents')
 
-    users = []
-    if isinstance(contents, dict):
-        # In the case the command is not available, this value will return as str.
-        # Unsupported Command..
-        users = contents.get('users', [])
+        users = []
+        if isinstance(contents, dict):
+            # In the case the command is not available, this value will return as str.
+            # Unsupported Command..
+            users = contents.get('users', [])
 
-    users_res_for_chart = []
-    for user in users:
-        users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
-                                    "data": [user.get("threatStatistics").get("attackIndex")]})
+        users_res_for_chart = []
+        for user in users:
+            users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
+                                        "data": [user.get("threatStatistics").get("attackIndex")]})
 
-    default_empty_chart_data = [{"name": "", "data": [], "color": ""}]
+        default_empty_chart_data = [{"name": "", "data": [], "color": ""}]
 
-    final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
-    return_results(json.dumps(final_res))
+        final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
+        return_results(json.dumps(final_res))
+
+    except Exception as e:
+        raise DemistoException(f'Script failed with the following error: {e}')
 
 
 if __name__ in ['__main__', 'builtin', 'builtins']:
--- Packs/ProofpointTAP/Scripts/ProofpointTapTopClickers/ProofpointTapTopClickers.py
+++ Packs/ProofpointTAP/Scripts/ProofpointTapTopClickers/ProofpointTapTopClickers.py
@@ -3,25 +3,29 @@ from CommonServerPython import *  # noqa: F401
 
 
 def main():
-    results: dict = demisto.executeCommand('proofpoint-get-top-clickers', {'window': 90})[0]  # type: ignore
-    contents = results.get('Contents')
-    
-    users = []
-    if isinstance(contents, dict):
-        # In the case the command is not available, this value will return as str.
-        # Unsupported Command..
-        users = contents.get('users', [])
+    try:
+        results: dict = demisto.executeCommand('proofpoint-get-top-clickers', {'window': 90})[0]  # type: ignore
+        contents = results.get('Contents')
 
-    users_res_for_chart = []
-    for user in users:
-        users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
-                                    "data": [user.get("clickStatistics").get("clickCount")]})
-    default_empty_chart_data = [
-        {"name": "", "data": [], "color": ""},
-    ]
+        users = []
+        if isinstance(contents, dict):
+            # In the case the command is not available, this value will return as str.
+            # Unsupported Command..
+            users = contents.get('users', [])
 
-    final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
-    return_results(json.dumps(final_res))
+        users_res_for_chart = []
+        for user in users:
+            users_res_for_chart.append({"name": user.get("identity").get("emails", [""])[0],
+                                        "data": [user.get("clickStatistics").get("clickCount")]})
+        default_empty_chart_data = [
+            {"name": "", "data": [], "color": ""},
+        ]
+
+        final_res = users_res_for_chart if users_res_for_chart else default_empty_chart_data
+        return_results(json.dumps(final_res))
+
+    except Exception as e:
+        raise DemistoException(f'Script failed with the following error: {e}')
 
 
 if __name__ in ['__main__', 'builtin', 'builtins']:
