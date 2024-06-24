758d33e4c575e89b03548793b062ce8540eb5394
yrosenberg <yrosenberg@paloaltonetworks.com>
2024-06-24 16:40:53 +0300

    mirroring

--- Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/CrowdStrikeFalcon.py
+++ Packs/CrowdStrikeFalcon/Integrations/CrowdStrikeFalcon/CrowdStrikeFalcon.py
@@ -2410,8 +2410,8 @@ def get_remote_detection_data(remote_incident_id: str):
     """
     mirrored_data_list = get_detections_entities([remote_incident_id]).get('resources', [])  # a list with one dict in it
     mirrored_data = mirrored_data_list[0]
-
-    mirrored_data['severity'] = severity_string_to_int(mirrored_data.get('max_severity_displayname'))
+    severity = mirrored_data.get('max_severity_displayname') or mirrored_data.get('severity_name')
+    mirrored_data['severity'] = severity_string_to_int(severity)
     demisto.debug(f'In get_remote_detection_data {remote_incident_id=} {mirrored_data=}')
 
     updated_object: dict[str, Any] = {'incident_type': 'detection'}
