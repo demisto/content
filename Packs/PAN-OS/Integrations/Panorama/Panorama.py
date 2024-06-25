3293a0a162a975b924572dd33c6b591463eb565e
ilappe <ilappe@paloaltonetworks.com>
2024-06-25 17:47:22 +0300

    fix mypy

--- Packs/PAN-OS/Integrations/Panorama/Panorama.py
+++ Packs/PAN-OS/Integrations/Panorama/Panorama.py
@@ -14450,9 +14450,9 @@ def fetch_incidents(last_run: LastRun, first_fetch: str,
     parsed_incident_entries_list = get_parsed_incident_entries(
         unique_incident_entries_dict, last_fetch_dict, last_id_dict)  # type: ignore[arg-type]
 
-    next_max_fetch = update_max_fetch_dict(max_fetch_dict, last_fetch_dict,
+    next_max_fetch = update_max_fetch_dict(max_fetch_dict, last_fetch_dict,  # type: ignore[arg-type]
                                            unique_incident_entries_dict)  # type: ignore[arg-type]
-    new_last_run = LastRun(last_fetch_dict=last_fetch_dict, last_id_dict=last_id_dict, max_fetch_dict=next_max_fetch)
+    new_last_run = LastRun(last_fetch_dict=last_fetch_dict, last_id_dict=last_id_dict, max_fetch_dict=next_max_fetch)  # type: ignore[typeddict-item]
 
     return new_last_run, parsed_incident_entries_list  # type: ignore[return-value]
 
