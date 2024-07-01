e5864dbb409b185c16476d76462af4656394ba9f
Anas Yousef <44998563+anas-yousef@users.noreply.github.com>
2024-07-01 19:39:41 +0300

    Added docs, refresh token, updated fetch events

--- Packs/KeeperSecretsManager/Integrations/KeeperSecretsManagerEventCollector/KeeperSecretsManagerEventCollector.py
+++ Packs/KeeperSecretsManager/Integrations/KeeperSecretsManagerEventCollector/KeeperSecretsManagerEventCollector.py
@@ -13,6 +13,8 @@ VENDOR = "Keeper"
 PRODUCT = "Secrets Manager"
 LOG_LINE = f"{VENDOR}_{PRODUCT}:"
 DEFAULT_MAX_FETCH = 1000
+API_MAX_FETCH = 1000
+SESSION_TOKEN_TTL = 3600  # In seconds
 
 """ Fetch Events Classes"""
 LAST_RUN = "Last Run"
@@ -29,11 +31,50 @@ DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"  # ISO8601 format with UTC, default in XSOAR
 """ HELPER FUNCTIONS """
 
 
+def get_max_events_to_fetch(params_max_fetch: str | int) -> int:
+    """Gets the maximum number of events to fetch.
+
+    Args:
+        params_max_fetch (str): A limit for the log type.
+
+    Returns:
+        int: The maximum number of events to fetch.
+
+    Raises:
+        ValueError: If the limit is not a valid integer.
+    """
+    try:
+        return DEFAULT_MAX_FETCH if params_max_fetch in ["", None] else int(params_max_fetch)
+
+    except ValueError:
+        raise ValueError("Please provide a valid integer value for a fetch limit.")
+
+
+def get_current_time_in_seconds() -> int:
+    """A function to return time as an int number of seconds since the epoch
+
+    Returns:
+        int: Number of seconds since the epoch
+    """
+    return int(time.time())
+
+
 def load_integration_context_into_keeper_params(
     username: str,
     password: str,
     server_url: str,
 ):
+    """Load data from the integration context into a KeeperParams instance, which is used
+    to communicate with the product.
+
+    Args:
+        username (str): The username of the account.
+        password (str): The password of the account.
+        server_url (str): The server URL.
+
+    Returns:
+        KeeperParams: An instance that will be used to communicate with the product.
+    """
     integration_context = get_integration_context()
     keeper_params = KeeperParams()
     keeper_params.user = username
@@ -51,6 +92,11 @@ def load_integration_context_into_keeper_params(
 
 
 def append_to_integration_context(context_to_append: dict[str, Any]):
+    """Appends data to the integration context DB.
+
+    Args:
+        context_to_append (dict[str, Any]): Context data to append
+    """
     integration_context = get_integration_context()
     integration_context |= context_to_append
     set_integration_context(integration_context)
@@ -61,6 +107,10 @@ def append_to_integration_context(context_to_append: dict[str, Any]):
 
 class Client:
     class DeviceApproval(LoginStepDeviceApproval):
+        """
+        In charge of sending and verifying the code sent to the user's email when registering the device for the first time
+        """
+
         @property
         def username(self):
             pass
@@ -100,6 +150,10 @@ class Client:
             pass
 
     class PasswordStep(LoginStepPassword):
+        """
+        In charge of verifying the user's password after verifying the device registeration.
+        """
+
         def __init__(self, salt_bytes: bytes, salt_iterations: int):
             self.salt_bytes = salt_bytes
             self.salt_iterations = salt_iterations
@@ -133,18 +187,77 @@ class Client:
             server_url=server_url,
         )
 
-    def start_registering_device(self, device_approval: DeviceApproval, new_device: bool = False):
-        encryptedDeviceToken = LoginV3API.get_device_id(self.keeper_params, new_device)
-        resp: APIRequest_pb2.LoginResponse = LoginV3API.startLoginMessage(
-            self.keeper_params, encryptedDeviceToken, cloneCode=None, loginType="NORMAL"
-        )
+    def refresh_session_token_if_needed(
+        self,
+    ) -> None:
+        """Refresh the session token if needed.
 
+        Raises:
+            DemistoException: If saved the TTL of the session token, but it is not found in the
+            integration's context.
+        """
+        integration_context = get_integration_context()
+        valid_until = integration_context.get("valid_until", 0)
+        current_time = get_current_time_in_seconds()
+        if current_time >= valid_until - 10:
+            if self.keeper_params.session_token:
+                # First helper
+                encrypted_device_token = LoginV3API.get_device_id(self.keeper_params)
+                resp = self.save_device_tokens(
+                    encrypted_device_token=encrypted_device_token,
+                )
+                encrypted_login_token: bytes = resp.encryptedLoginToken  # type: ignore
+
+                self.validate_device_registration(
+                    encrypted_device_token=encrypted_device_token,
+                    encrypted_login_token=encrypted_login_token,
+                )
+                self.save_session_token()
+            else:
+                raise DemistoException(
+                    "No session token was configured, please complete the authorization process as shown in the documentation"
+                )
+
+    def save_device_tokens(self, encrypted_device_token: bytes) -> APIRequest_pb2.LoginResponse:
+        """Save the devices' tokens when starting to verify the device registration.
+
+        Args:
+            encrypted_device_token (bytes): The encrypted device token.
+
+        Returns:
+            APIRequest_pb2.LoginResponse: The response that holds data about the API call.
+        """
+        resp: APIRequest_pb2.LoginResponse = LoginV3API.startLoginMessage(
+            self.keeper_params, encrypted_device_token, cloneCode=None, loginType="NORMAL"
+        )
         append_to_integration_context(
             {
                 "device_private_key": self.keeper_params.device_private_key,
                 "device_token": self.keeper_params.device_token,
                 "login_token": utils.base64_url_encode(resp.encryptedLoginToken),  # type: ignore
             }
+        )
+        return resp
+
+    def start_registering_device(
+        self,
+        device_approval: DeviceApproval,
+        new_device: bool = False,
+    ):
+        """Start the registration process of a new or old device.
+
+        Args:
+            device_approval (DeviceApproval): DeviceApproval instance that is in charge of sending the code to the
+            user's email.
+            new_device (bool, optional): If we should configure a new device. Defaults to False.
+
+        Raises:
+            DemistoException: If we try registering an already registered and authenticated device.
+            DemistoException: If we get a response status that we don't know how to handle.
+        """
+        encryptedDeviceToken = LoginV3API.get_device_id(self.keeper_params, new_device)
+        resp = self.save_device_tokens(
+            encrypted_device_token=encryptedDeviceToken,
         )
 
         if resp.loginState == APIRequest_pb2.DEVICE_APPROVAL_REQUIRED:  # type: ignore # client goes to “standard device approval”.
@@ -158,16 +271,22 @@ class Client:
         else:
             raise DemistoException(f"Unknown login state {resp.loginState}")  # type: ignore
 
-    def finish_registering_device(self, device_approval: DeviceApproval, encrypted_login_token: bytes, code: str = ""):
-        encrypted_device_token = utils.base64_url_decode(params.device_token)  # type: ignore
-        if code:
-            device_approval.send_code(
-                self.keeper_params,
-                DeviceApprovalChannel.Email,
-                encrypted_device_token,
-                encrypted_login_token,
-                code,
-            )
+    def validate_device_registration(
+        self,
+        encrypted_device_token: bytes,
+        encrypted_login_token: bytes,
+    ):
+        """Verify the registration process of a new or old device. This method is also used as part of the
+        mechanism to refresh the session token.
+
+        Args:
+            encrypted_device_token (bytes): The encrypted device token.
+            encrypted_login_token (bytes): The encrypted login token.
+
+        Raises:
+            DemistoException: When trying to verify the user's password, and an error occurs.
+            DemistoException: When trying to verify the device registration, and an error occurs.
+        """
         resp = LoginV3API.startLoginMessage(self.keeper_params, encrypted_device_token)
         if resp.loginState == APIRequest_pb2.REQUIRES_AUTH_HASH:  # type: ignore
             salt = api.get_correct_salt(resp.salt)  # type: ignore
@@ -180,6 +299,35 @@ class Client:
         else:
             raise DemistoException(f"Unknown login state {resp.loginState}")  # type: ignore
 
+    def finish_registering_device(
+        self,
+        device_approval: DeviceApproval,
+        encrypted_login_token: bytes,
+        code: str = "",
+    ):
+        """Finish the registration process of a new or old device. If the code argument is given, then
+        we are verifying the newly registered device.
+
+        Args:
+            device_approval (DeviceApproval): DeviceApproval instance that is in charge of verifying the code sent
+            to user's email.
+            encrypted_login_token (bytes): The encrypted login token.
+            code (str, optional): The code sent to the user's email. Defaults to "".
+        """
+        encrypted_device_token = utils.base64_url_decode(self.keeper_params.device_token)  # type: ignore
+        if code:
+            device_approval.send_code(
+                self.keeper_params,
+                DeviceApprovalChannel.Email,
+                encrypted_device_token,
+                encrypted_login_token,
+                code,
+            )
+        self.validate_device_registration(
+            encrypted_device_token=encrypted_device_token,
+            encrypted_login_token=encrypted_login_token,
+        )
+
     def start_login(self):
         device_approval = self.DeviceApproval()
         try:
@@ -188,17 +336,23 @@ class Client:
             demisto.info("Registering new device")
             self.start_registering_device(device_approval, new_device=True)
 
+    def save_session_token(
+        self,
+    ):
+        append_to_integration_context(
+            {
+                "session_token": self.keeper_params.session_token,
+                "clone_code": self.keeper_params.clone_code,
+                "valid_until": get_current_time_in_seconds() + SESSION_TOKEN_TTL,
+            }
+        )
+
     def complete_login(self, code: str):
         device_approval = self.DeviceApproval()
         integration_context = get_integration_context()
         encrypted_login_token = utils.base64_url_decode(integration_context["login_token"])
         self.finish_registering_device(device_approval, encrypted_login_token, code)
-        append_to_integration_context(
-            {
-                "session_token": self.keeper_params.session_token,
-                "clone_code": self.keeper_params.clone_code,
-            }
-        )
+        self.save_session_token()
         if not self.keeper_params.session_token:
             raise DemistoException("Could not find session token")
 
@@ -209,7 +363,9 @@ class Client:
             "scope": "enterprise",
             "limit": limit,
             "order": "ascending",
-            "filter": {"min": start_event_time},
+            "filter": {
+                "created": {"min": start_event_time},
+            },
         }
         return api.communicate(self.keeper_params, request_query)
 
@@ -233,34 +389,27 @@ def load_json(path: str):
 def get_audit_logs(
     client: Client, start_event_time: int, max_fetch_limit: int, last_fetched_ids: set[str]
 ) -> list[dict[str, Any]]:
-    max_fetch_limit = 9
     continue_fetching = True
     events_to_return: list[dict[str, Any]] = []
     # start_event_time -> UNIX epoch time in seconds
     start_time_to_fetch = start_event_time
     fetched_ids = last_fetched_ids
-    limit = max_fetch_limit
-    while continue_fetching:
-        # query_response = client.query_audit_logs(limit=limit, start_event_time=start_time_to_fetch)
-        # audit_events = query_response["audit_event_overview_report_rows"]
-        audit_events: list[dict[str, Any]] = load_json("mocked_data_2.json")
+    res_count = 0
+    while continue_fetching and res_count < max_fetch_limit:
+        query_response = client.query_audit_logs(
+            limit=min(API_MAX_FETCH, max_fetch_limit - res_count), start_event_time=start_time_to_fetch
+        )
+        audit_events = query_response["audit_event_overview_report_rows"]
         audit_events_count = len(audit_events)
         demisto.debug(f"{LOG_LINE} got {audit_events_count} events from API")
         if audit_events:
             # dedup
             dedupped_audit_events = dedup_events(audit_events, fetched_ids)
-            if audit_events_count == limit and dedupped_audit_events:
-                # We fetched the maximum amount, and we fetched new events
-                # We need to make up for dedupped events by running another fetch
-                dedupped_events_count = len(dedupped_audit_events)
-                limit = audit_events_count - dedupped_events_count
-            else:
-                # We did not reach the limit, or all events have been dropped
-                # No need to continue fetching
-                continue_fetching = False
+            dedupped_events_count = len(dedupped_audit_events)
+            demisto.debug(f"{LOG_LINE} Events count after dedup {dedupped_events_count}")
             if dedupped_audit_events:
+                res_count += dedupped_events_count
                 events_to_return.extend(dedupped_audit_events)
-
                 # Getting last events's creation date, assuming asc order
                 start_time_to_fetch: int = int(dedupped_audit_events[-1]["created"])
                 # We get the event IDs that have the same creation time as the latest event in the response
@@ -270,6 +419,8 @@ def get_audit_logs(
                     for audit_event in dedupped_audit_events
                     if int(audit_event["created"]) == start_time_to_fetch
                 }
+            else:
+                continue_fetching = False
         else:
             continue_fetching = False
     demisto.setLastRun({"last_fetch_epoch_time": str(start_time_to_fetch), "last_fetch_ids": list(fetched_ids)})
@@ -286,10 +437,9 @@ def dedup_events(audit_events: list[dict[str, Any]], last_fetched_ids: set[str])
     return dedupped_audit_events
 
 
-def fetch_events(client: Client, last_run: dict[str, Any], max_fetch_limit: int):
+def fetch_events(client: Client, last_run: dict[str, Any], max_fetch_limit: int) -> list[dict[str, Any]]:
     demisto.debug(f"last_run: {last_run}" if last_run else "last_run is empty")
-    # We save the last_fetch_epoch_time in string format, to gracefully handle how the backend server handles
-    # data saved to the last run object
+    # SDK's query uses Epoch time to filter events
     last_fetch_epoch_time: int = int(last_run.get("last_fetch_epoch_time", "0"))
 
     # (if 0) returns False
@@ -301,6 +451,7 @@ def fetch_events(client: Client, last_run: dict[str, Any], max_fetch_limit: int)
         max_fetch_limit=max_fetch_limit,
         last_fetched_ids=last_fetched_ids,
     )
+    return audit_log
 
 
 def test_authorization(
@@ -327,8 +478,9 @@ def main() -> None:
         username=username,
         password=password,
     )
-    client.start_login()
-    # client.complete_login(device_approval, keeper_params, "")
+    client.refresh_session_token_if_needed()
+    # client.start_login()
+    # client.complete_login(code=args.get("code", ""))
     demisto.debug(f"Command being called is {demisto.command()}")
     try:
         if command == "test-module":
@@ -342,15 +494,13 @@ def main() -> None:
             return_results(test_authorization(client=client))
         elif command == "fetch-events":
             last_run = demisto.getLastRun()
-            fetch_events(
+            fetched_audit_logs = fetch_events(
                 client=client,
                 last_run=last_run,
-                max_fetch_limit=DEFAULT_MAX_FETCH,
+                max_fetch_limit=get_max_events_to_fetch(params.get("alerts_max_fetch", DEFAULT_MAX_FETCH)),
             )
-            # next_run, events = collector.fetch_command(demisto_last_run=last_run)
-            # demisto.debug(f"{events=}")
-            # send_events_to_xsiam(events, VENDOR, PRODUCT)
-            # demisto.setLastRun(next_run)
+            demisto.debug(f"Events to send to XSIAM {fetched_audit_logs=}")
+            send_events_to_xsiam(fetched_audit_logs, VENDOR, PRODUCT)
     # Log exceptions and return errors
     except Exception as e:
         return_error(f"Failed to execute {command} command.\nError:\n{str(e)}")
