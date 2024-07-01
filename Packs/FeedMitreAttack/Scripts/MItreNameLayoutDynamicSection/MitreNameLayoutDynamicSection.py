92fd465099d6609ded4066910b06f91f4015aa24
samuelFain <65926551+samuelFain@users.noreply.github.com>
2024-07-01 11:11:19 +0300

    staging_branch_6

--- Packs/FeedMitreAttack/Scripts/MItreNameLayoutDynamicSection/MitreNameLayoutDynamicSection.py
+++
@@ -1,8 +0,0 @@
-import demistomock as demisto
-from CommonServerPython import *
-from CommonServerUserPython import *
-
-mitre_name = demisto.get(demisto.args()['indicator'], 'CustomFields.mitrename') or ''
-
-demisto.results(
-    {'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': f'# {mitre_name}'})
