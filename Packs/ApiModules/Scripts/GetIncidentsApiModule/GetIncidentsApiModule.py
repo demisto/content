8f2a85429d0ec4b43b619ac1dc9d4c703b125523
sberman <sberman@paloaltonetworks.com>
2024-07-02 12:22:02 +0300

    Fixed an issue with populated fields

--- Packs/ApiModules/Scripts/GetIncidentsApiModule/GetIncidentsApiModule.py
+++ Packs/ApiModules/Scripts/GetIncidentsApiModule/GetIncidentsApiModule.py
@@ -205,7 +205,7 @@ def get_incidents_by_query(args: dict) -> list[dict]:
     """
     query = args.get("query")
     incident_types = argToList(args.get("incidentTypes"), transform=str.strip)
-    populate_fields = argToList(args.get("populateFields"), transform=str.strip)
+    populate_fields = argToList(args.get("populateFields").replace("|", ","), transform=str.strip)
     non_empty_fields = argToList(args.get("NonEmptyFields"), transform=str.strip)
     time_field = args.get("timeField") or DEFAULT_TIME_FIELD
     from_date = arg_to_datetime(args.get("fromDate"))
