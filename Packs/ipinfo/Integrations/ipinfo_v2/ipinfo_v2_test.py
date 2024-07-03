2bd4c2e6799a87d0d69587383b3f95e88b44c621
shmuel44 <skroizer@paloaltonetworks.com>
2024-07-03 16:28:42 +0300

    log_file

--- Packs/ipinfo/Integrations/ipinfo_v2/ipinfo_v2_test.py
+++ Packs/ipinfo/Integrations/ipinfo_v2/ipinfo_v2_test.py
@@ -47,7 +47,7 @@ def test_ipinfo_ip_command(mocker):
 
     expected_parsed_context = util_load_json('test_data/ip_1.1.1.1_command_results.json')
     assert command_results.call_args[1].get("readable_output") == expected_parsed_context[1].get("HumanReadable")
-    assert command_results.call_args[1].get("outputs").get("Address") == "1.1.1.1"
+    assert command_results.call_args[1].get("outputs").get("Address") == "1.1.1.2"
     assert command_results.call_args[1].get("outputs").get("Hostname") == "one.one.one.one"
 
 
