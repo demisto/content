498fdc661dc7009abbcef1d2bea46bd67cc5fe7f
maimorag <mmorag@paloaltonetworks.com>
2024-07-03 12:09:02 +0300

    fix testcommonserver pyton

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython_test.py
@@ -7527,12 +7527,16 @@ class TestIsDemistoServerGE:
             'demistoVersion',
             return_value={
                 'version': '5.0.0',
-                'buildNumber': '50000'
+                'buildNumber': '50000',
+                'platform': 'xsoar',
+                'engine': True
             }
         )
         assert get_demisto_version() == {
             'version': '5.0.0',
-            'buildNumber': '50000'
+            'buildNumber': '50000',
+            'platform': 'xsoar',
+            'engine': True            
         }
         # call again to check cache
         assert get_demisto_version() == {
@@ -7545,7 +7549,7 @@ class TestIsDemistoServerGE:
         assert is_demisto_version_ge('5.0.0')
         assert is_demisto_version_ge('4.5.0')
         assert not is_demisto_version_ge('5.5.0')
-        assert get_demisto_version_as_str() == '5.0.0-50000'
+        assert get_demisto_version_as_str() == '5.0.0-50000-xsoar-True'
 
     def test_get_demisto_version_2(self, mocker):
         mocker.patch.object(
@@ -7553,12 +7557,16 @@ class TestIsDemistoServerGE:
             'demistoVersion',
             return_value={
                 'version': '6.10.0',
-                'buildNumber': '50000'
+                'buildNumber': '50000',
+                'platform': 'xsoar',
+                'engine': True
             }
         )
         assert get_demisto_version() == {
             'version': '6.10.0',
-            'buildNumber': '50000'
+            'buildNumber': '50000',
+            'platform': 'xsoar',
+            'engine': True  
         }
         assert is_demisto_version_ge('6.5.0')
         assert is_demisto_version_ge('6.1.0')
@@ -7579,7 +7587,9 @@ class TestIsDemistoServerGE:
             'demistoVersion',
             return_value={
                 'version': '6.0.0',
-                'buildNumber': '50000'
+                'buildNumber': '50000',
+                'platform': 'xsoar',
+                'engine': True
             }
         )
         assert is_demisto_version_ge('6.0.0', '1-dev')
