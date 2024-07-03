26989a096e49565d6fc2656dd1e659c2836e8ced
ilaredo <ilaredo@paloaltonetworks.com>
2024-07-03 18:35:17 +0300

    auth, fetch events

--- Packs/BitwardenPasswordManager/Integrations/BitwardenPasswordManagerEventCollector/BitwardenPasswordManagerEventCollector.py
+++ Packs/BitwardenPasswordManager/Integrations/BitwardenPasswordManagerEventCollector/BitwardenPasswordManagerEventCollector.py
@@ -4,6 +4,11 @@ from CommonServerUserPython import *  # noqa
 
 MINUTES_BEFORE_TOKEN_EXPIRED = 2
 
+DEFAULT_MAX_FETCH = 500
+DEFAULT_FIRST_FETCH = '3 days'
+DEFAULT_STARTTIME = 10
+DEFAULT_ENDTIME = 10
+
 
 class Client(BaseClient):
     """
@@ -16,9 +21,11 @@ class Client(BaseClient):
 
     def login(self, client_id: str, client_secret: str) -> str:
         integration_context = get_integration_context()
+        demisto.log(f"{integration_context=}")
         if token := integration_context.get('token'):
             expires_date = integration_context.get('expires')
             if expires_date and not self.is_token_expired(expires_date):
+                demisto.log(f"Token is valid: {expires_date=}, {token=}")
                 return token
 
         json_data = {
@@ -35,63 +42,71 @@ class Client(BaseClient):
         return utc_now > expires_datetime
 
     def create_new_token(self, json_data: dict) -> str:
-        url = 'https://identity.bitwarden.com/connect/token'
+        full_url = 'https://identity.bitwarden.com/connect/token'
 
         access_token_obj = self._http_request(
             method='POST',
-            url=url,
+            full_url=full_url,
             headers={'Content-Type': 'application/x-www-form-urlencoded'},
-            data=json.dumps(json_data),
+            # data=json.dumps(json_data),
+            data=json_data
         )
 
-        new_token = access_token_obj.get('access_token', '')
+        new_access_token = access_token_obj.get('access_token', '')
         expire_in = arg_to_number(access_token_obj.get('expires_in')) or 1
-        self.store_token_in_context(new_token, expire_in)
+        self.store_token_in_context(new_access_token, expire_in)
 
-        return new_token
+        demisto.log(f"Created new token: {new_access_token=}, {expire_in=}")
+        return new_access_token
 
     def store_token_in_context(self, token: str, expire_in: int) -> None:
 
         expire_date = get_current_time() + timedelta(seconds=expire_in) - timedelta(minutes=MINUTES_BEFORE_TOKEN_EXPIRED)
-        set_integration_context({
+        set_integration_context(context={
             'token': token,
-            'expire_date': str(expire_date)
+            'expires': str(expire_date)
         })
 
-    def authenticate(self, args: dict[str, Any]) -> dict:
-        pass
+    def get_events(self, start_time: datetime = None, end_time: datetime = None, limit: int = DEFAULT_MAX_FETCH) -> Dict[
+        str, Any]:
+        # params = {'starttime': start_time, 'endtime': end_time, 'expandenums': "true", 'includeacknowledged': "true",
+        #           'minimal': "false", 'includebreachurl': "true"}
+        # params = {'starttime': start_time.strftime("%H:%M:%S"), 'endtime': end_time.strftime("%H:%M:%S")}
 
-    # def get_certificates(self, args: dict[str, Any]) -> dict:
-    #     """
-    #     This method creates the HTTP request to retrieve the certificates the user has.
-    #
-    #     Args:
-    #         args (dict): The arguments for the command passed to the request.
-    #
-    #     Returns:
-    #         dict: The response object.
-    #     """
-    #
-    #     headers = {
-    #         'Authorization': f'Bearer {self.token}'
-    #     }
-    #
-    #     return self._http_request(
-    #         method='GET',
-    #         url_suffix='/vedsdk/certificates/',
-    #         headers=headers,
-    #         params=args
-    #     )
+        headers = {
+            'Authorization': f'Bearer {self.token}'
+        }
+
+        return self._http_request(
+            method='GET',
+            url_suffix='/public/events',
+            headers=headers,
+            # params=params
+        )
 
 
 def test_module(client: Client) -> str:
+    demisto.log("in test module 2")
     try:
-        test_empty_args: Dict = {}
-        # client.get_certificates(test_empty_args)
+        response = client.get_events()
+        demisto.log(f"{response=}")
     except DemistoException as e:
         raise e
 
     return 'ok'
+
+
+def get_events_command(client: Client, args: Dict[str, Any]) -> CommandResults:
+    limit = arg_to_number(args.get('limit')) or DEFAULT_MAX_FETCH
+    start_time = None
+    end_time = None
+    events = client.get_events(start_time=start_time, end_time=end_time, limit=limit)
+    if events:
+        return CommandResults(
+            readable_output=tableToMarkdown("Open Incidents", events),
+            raw_response=events
+        )
+    return CommandResults(readable_output='No events found')
 
 
 def main() -> None:  # pragma: no cover
@@ -103,10 +118,9 @@ def main() -> None:  # pragma: no cover
     """
     print("in main")
     demisto_params = demisto.params()
-    base_url = demisto_params.get('server', 'api.bitwarden.com')
-    username = demisto_params.get('credentials', {}).get('identifier')
-    password = demisto_params.get('credentials', {}).get('password')
-    client_id = demisto_params.get('client_id')
+    base_url = demisto_params.get('url', 'https://api.bitwarden.com')
+    client_id = demisto_params.get('credentials', {}).get('identifier')
+    client_secret = demisto_params.get('credentials', {}).get('password')
     verify_certificate = not demisto_params.get('insecure', False)
     proxy = demisto_params.get('proxy', False)
     command = demisto.command()
@@ -115,18 +129,16 @@ def main() -> None:  # pragma: no cover
         client = Client(
             base_url=base_url,
             verify=verify_certificate,
-            username=username,
-            password=password,
             client_id=client_id,
+            client_secret=client_secret,
             proxy=proxy)
-
+        demisto.log(f"{client}, {client.token}")
         args = demisto.args()
         if command == 'test-module':
+            demisto.log("in test module")
             return_results(test_module(client))
-        # elif command == 'venafi-get-certificates':
-        #     return_results(get_certificates_command(client, args))
-        # elif command == 'venafi-get-certificate-details':
-        #     return_results(get_certificate_details_command(client, args))
+        elif command == 'get-events':
+            return_results(get_events_command(client, args))
         else:
             raise NotImplementedError(f'{command} command is not implemented.')
 
