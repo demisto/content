95db51561bef59f5c9618fe212e78a8c2dfe9ef1
Ben Melamed <bmelamed@paloaltonetworks.com>
2024-07-04 14:39:26 +0300

    Apply suggestions from code review
    Co-authored-by: Jasmine Beilin <71636766+JasBeilin@users.noreply.github.com>

--- Packs/CommonScripts/Scripts/DownloadAndArchivePythonLibrary/DownloadAndArchivePythonLibrary.py
+++ Packs/CommonScripts/Scripts/DownloadAndArchivePythonLibrary/DownloadAndArchivePythonLibrary.py
@@ -10,10 +10,11 @@ import zipfile
 
 def installLibrary(dir_path: Path, library_name: str) -> str:
     # Install the package using pip
+    demisto.debug(f"Running in {dir_path=}, {library_name=}")
     cmd = f'python3 -m pip install --target {dir_path} {library_name}'
     process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
-
+    demisto.debug(f"returned code: {process.returncode}")
     if process.returncode != 0:
         raise DemistoException(f"Failed to install the {library_name} library: {stderr.decode('utf-8')}")
 
@@ -22,10 +23,12 @@ def installLibrary(dir_path: Path, library_name: str) -> str:
     with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9) as zip_file:
         for root, _dirs, files in os.walk(dir_path):
             for file in files:
+                demisto.debug(f"{root=}, {file=}")
                 file_path = Path(root) / file
                 # Ensure the folder inside the ZIP file is named 'python'
                 arcname = Path('python') / file_path.relative_to(dir_path)
                 zip_file.write(file_path, arcname=arcname)
+                demisto.debug("Updated file")
 
     # Read the zip file contents
     with open(zip_path, 'rb') as zip_file:
@@ -39,6 +42,7 @@ def main():
     library_name = args.get('library_name')
     try:
         dir_path = Path(mkdtemp(prefix='python'))
+        demisto.debug("Starting installing library.")
         result = installLibrary(dir_path, library_name)
         return_results(result)
 
