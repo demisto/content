16eeb0649a9e98a168d1518b5c3fc575a3108286
maimorag <mmorag@paloaltonetworks.com>
2024-07-04 14:46:21 +0300

    adding the fix+precommit_rn

--- Packs/BmcITSM/Integrations/BmcITSM/BmcITSM_test.py
+++ Packs/BmcITSM/Integrations/BmcITSM/BmcITSM_test.py
@@ -24,7 +24,7 @@ def load_mock_response(file_name: str) -> str:
     Returns:
         str: Mock file content.
     """
-    with open(os.path.join("test_data", file_name), mode="r", encoding="utf-8") as mock_file:
+    with open(os.path.join("test_data", file_name), encoding="utf-8") as mock_file:
         return json.loads(mock_file.read())
 
 
@@ -1497,22 +1497,25 @@ def test_fetch_command(
     """
     import BmcITSM
     mock_response = load_mock_response("list_tickets_not_sorted.json")
-    mocker.patch.object(demisto, 'getLastRun', return_value={"SRM:Request": {"last_create_time":'2021-06-29T14:38:36.000+0000'}})
+    mocker.patch.object(demisto, 'getLastRun', return_value={"SRM:Request": {"last_create_time": '2021-06-29T14:38:36.000+0000'}})
     mocker.patch.object(BmcITSM, "fetch_relevant_tickets_by_ticket_type", return_value=mock_response)
     incidents_result, last_run_result = BmcITSM.fetch_incidents(mock_client,
-                                     max_fetch=2,
-                                     first_fetch="2022-06-29T14:38:36.000+0000",
-                                     last_run={"SRM:Request": {"last_create_time":'2021-06-29T14:38:36.000+0000'}},
-                                     ticket_type_filter=["SRM:Request"],
-                                     status_filter=[],
-                                     impact_filter=[],
-                                     urgency_filter=[],
-                                     custom_query=("'Submit Date' <= \"1657032797\" AND 'Submit Date'"
-                                                   ">\"1657032797\" AND 'Urgency' = \"4-Low\""),
-                                     mirror_direction="both",
-                                     )
-    max_last_time_result = max(
-        [BmcITSM.date_to_epoch_for_fetch(arg_to_datetime(ticket.get("occured")))
-         for ticket in incidents_result]
+                                                                max_fetch=2,
+                                                                first_fetch="2022-06-29T14:38:36.000+0000",
+                                                                last_run={"SRM:Request": {
+                                                                    "last_create_time": '2021-06-29T14:38:36.000+0000'}},
+                                                                ticket_type_filter=["SRM:Request"],
+                                                                status_filter=[],
+                                                                impact_filter=[],
+                                                                urgency_filter=[],
+                                                                custom_query=("'Submit Date' <= \"1657032797\" AND 'Submit Date'"
+                                                                              ">\"1657032797\" AND 'Urgency' = \"4-Low\""),
+                                                                mirror_direction="both",
+                                                                )
+    expected_result = BmcITSM.date_to_epoch_for_fetch(
+        arg_to_datetime(max(
+            [BmcITSM.date_to_epoch_for_fetch(arg_to_datetime(ticket.get("occured")))
+             for ticket in incidents_result]
+        ))
     )
-    assert last_run_result["SRM:Request"]["last_create_time"] == max_last_time_result
+    assert last_run_result["SRM:Request"]["last_create_time"] == expected_result
