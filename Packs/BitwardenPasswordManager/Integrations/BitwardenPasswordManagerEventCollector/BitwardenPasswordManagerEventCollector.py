6721a43e9102da4ae0e74f2536c055b21ea1c0f4
ilaredo <ilaredo@paloaltonetworks.com>
2024-07-04 17:01:35 +0300

    wip

--- Packs/BitwardenPasswordManager/Integrations/BitwardenPasswordManagerEventCollector/BitwardenPasswordManagerEventCollector.py
+++ Packs/BitwardenPasswordManager/Integrations/BitwardenPasswordManagerEventCollector/BitwardenPasswordManagerEventCollector.py
@@ -4,10 +4,16 @@ from CommonServerUserPython import *  # noqa
 
 MINUTES_BEFORE_TOKEN_EXPIRED = 2
 
+VENDOR = 'Bitwarden'
+PRODUCT = 'Password Manager'
+
 DEFAULT_MAX_FETCH = 500
 DEFAULT_FIRST_FETCH = '3 days'
 DEFAULT_STARTTIME = 10
 DEFAULT_ENDTIME = 10
+DEFAULT_LIMIT = 10
+
+AUTHENTICATION_FULL_URL = 'https://identity.bitwarden.com/connect/token'
 
 
 class Client(BaseClient):
@@ -42,13 +48,10 @@ class Client(BaseClient):
         return utc_now > expires_datetime
 
     def create_new_token(self, json_data: dict) -> str:
-        full_url = 'https://identity.bitwarden.com/connect/token'
-
         access_token_obj = self._http_request(
             method='POST',
-            full_url=full_url,
+            full_url=AUTHENTICATION_FULL_URL,
             headers={'Content-Type': 'application/x-www-form-urlencoded'},
-            # data=json.dumps(json_data),
             data=json_data
         )
 
@@ -67,8 +70,8 @@ class Client(BaseClient):
             'expires': str(expire_date)
         })
 
-    def get_events(self, start_time: datetime = None, end_time: datetime = None, limit: int = DEFAULT_MAX_FETCH) -> Dict[
-        str, Any]:
+    def get_events(self, start_time: datetime = None, end_time: datetime = None, limit: int = DEFAULT_MAX_FETCH) -> List[Dict[
+        str, Any]]:
         # params = {'starttime': start_time, 'endtime': end_time, 'expandenums': "true", 'includeacknowledged': "true",
         #           'minimal': "false", 'includebreachurl': "true"}
         # params = {'starttime': start_time.strftime("%H:%M:%S"), 'endtime': end_time.strftime("%H:%M:%S")}
@@ -77,16 +80,18 @@ class Client(BaseClient):
             'Authorization': f'Bearer {self.token}'
         }
 
-        return self._http_request(
+        res = self._http_request(
             method='GET',
             url_suffix='/public/events',
             headers=headers,
             # params=params
         )
 
+        return res.get('data')
+
 
 def test_module(client: Client) -> str:
-    demisto.log("in test module 2")
+    demisto.log("in test module")
     try:
         response = client.get_events()
         demisto.log(f"{response=}")
@@ -96,17 +101,57 @@ def test_module(client: Client) -> str:
     return 'ok'
 
 
-def get_events_command(client: Client, args: Dict[str, Any]) -> CommandResults:
-    limit = arg_to_number(args.get('limit')) or DEFAULT_MAX_FETCH
+def convert_to_timestamp(date: datetime | None) -> int:
+    """Converts datetime to timestamp"""
+    if date:
+        if isinstance(date, datetime):
+            return int(date.timestamp())
+        elif isinstance(date, int):
+            return int(date)
+    return 0
+
+
+def add_time_field(events: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Adds time field to the events"""
+    for event in events:
+        event['_time'] = event['date']
+        # event['_time'] = timestamp_to_datestring(event['date'])
+    return events
+
+
+def filter_events(events: List[Dict[str, Any]], last_fetched_pid: int, max_fetch: int) -> List[Dict[str, Any]]:
+    """Filters events by ascending pbid and max_fetch"""
+    for index, event in enumerate(events):
+        if event.get('pbid', 0) > last_fetched_pid:
+            return events[index:index + max_fetch]
+    return []
+
+
+def fetch_events(client: Client, max_fetch: int, last_run: Dict[str, Any], start_time: int, end_time: int):
+    start_time = demisto.getLastRun() if demisto.getLastRun() else start_time
+    retrieve_events = client.get_events(start_time, end_time)
+    retrieve_events = filter_events(retrieve_events, int(last_run.get('last_fetch_pid', 0)), max_fetch)
+    if retrieve_events:
+        # extracting last fetch time and last fetched events.
+        last_fetch_time = retrieve_events[-1].get('time')
+        last_fetched_pbid = retrieve_events[-1].get('pbid')
+        demisto.debug(f'Setting last run to pbid: {last_fetched_pbid} time:{timestamp_to_datestring(last_fetch_time)}')
+        last_run = {'last_fetch_time': retrieve_events[-1].get('date'),
+                    'last_fetch_pid': last_fetched_pbid}
+    return retrieve_events, last_run
+
+
+def get_events_command(client: Client, args: Dict[str, Any]) -> tuple[List[Dict[str, Any]], CommandResults]:
+    limit = arg_to_number(args.get('limit')) or DEFAULT_LIMIT
     start_time = None
     end_time = None
     events = client.get_events(start_time=start_time, end_time=end_time, limit=limit)
     if events:
-        return CommandResults(
+        return events, CommandResults(
             readable_output=tableToMarkdown("Open Incidents", events),
             raw_response=events
         )
-    return CommandResults(readable_output='No events found')
+    return [], CommandResults(readable_output='No events found')
 
 
 def main() -> None:  # pragma: no cover
@@ -121,6 +166,8 @@ def main() -> None:  # pragma: no cover
     base_url = demisto_params.get('url', 'https://api.bitwarden.com')
     client_id = demisto_params.get('credentials', {}).get('identifier')
     client_secret = demisto_params.get('credentials', {}).get('password')
+    max_events_per_fetch = arg_to_number(demisto_params.get('max_fetch')) or DEFAULT_MAX_FETCH
+    first_fetch_time_timestamp = convert_to_timestamp(arg_to_datetime(demisto_params.get('first_fetch', DEFAULT_FIRST_FETCH)))
     verify_certificate = not demisto_params.get('insecure', False)
     proxy = demisto_params.get('proxy', False)
     command = demisto.command()
@@ -132,13 +179,30 @@ def main() -> None:  # pragma: no cover
             client_id=client_id,
             client_secret=client_secret,
             proxy=proxy)
-        demisto.log(f"{client}, {client.token}")
         args = demisto.args()
         if command == 'test-module':
-            demisto.log("in test module")
-            return_results(test_module(client))
-        elif command == 'get-events':
+            # return_results(test_module(client))
+            last_run = demisto.getLastRun()
+            events, new_last_run = get_events_command(client=client, args={})
+            if events:
+                add_time_field(events)
+                send_events_to_xsiam(events=events, vendor=VENDOR, product=PRODUCT)  # type: ignore
+                if new_last_run:
+                    demisto.setLastRun(new_last_run)
+        elif command == 'bitwarden-get-events':  # rename with bitwarden
             return_results(get_events_command(client, args))
+        elif demisto.command() == 'fetch-events':
+            last_run = demisto.getLastRun()
+            events, new_last_run = fetch_events(client=client,
+                                                max_fetch=max_events_per_fetch,
+                                                start_time=first_fetch_time_timestamp,
+                                                end_time=int(datetime.now().timestamp()),
+                                                last_run=last_run)
+            if events:
+                add_time_field(events)
+                send_events_to_xsiam(events=events, vendor=VENDOR, product=PRODUCT)  # type: ignore
+                if new_last_run:
+                    demisto.setLastRun(new_last_run)
         else:
             raise NotImplementedError(f'{command} command is not implemented.')
 
