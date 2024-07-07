96530eebcca8be6af782d3b395f615bacd7ef508
RotemAmit <ramit@paloaltonetworks.com>
2024-07-07 17:31:33 +0300

    coverage

--- Packs/GitHub/Integrations/GitHubEventCollector/GitHubEventCollector.py
+++ Packs/GitHub/Integrations/GitHubEventCollector/GitHubEventCollector.py
@@ -39,7 +39,7 @@ def prepare_demisto_params(params: dict):
     del params['after']
 
 
-class GithubParams(BaseModel):
+class GithubParams(BaseModel):  # pragma: no cover
     """
     A class that stores the request query params
     """
@@ -52,13 +52,13 @@ class GithubParams(BaseModel):
     )
 
 
-class GithubEventsRequestConfig(IntegrationHTTPRequest):
+class GithubEventsRequestConfig(IntegrationHTTPRequest):  # pragma: no cover
     url: AnyUrl
     method: Method = Method.GET
     params: GithubParams  # type: ignore
 
 
-class GithubClient(IntegrationEventsClient):
+class GithubClient(IntegrationEventsClient):  # pragma: no cover
     def set_request_filter(self, after: str):
         if self.request.params:
             self.request.params.phrase = get_github_timestamp_format(after)  # type: ignore
