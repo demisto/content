1dcc10478a986467042dccdd8ab54f7815ee8386
jlevypaloalto <jlevy@paloaltonetworks.com>
2024-07-09 10:53:57 +0300

    more stuff

--- Packs/CommonScripts/Scripts/GenerateAsBuiltConfiguration/GenerateAsBuiltConfiguration_test.py
+++ Packs/CommonScripts/Scripts/GenerateAsBuiltConfiguration/GenerateAsBuiltConfiguration_test.py
@@ -1,8 +1,13 @@
 from CommonServerPython import *
 import demistomock as demisto
+from test_data.execute_command import execute_command
 
+demisto.args = lambda: {'playbook': 'AwsEC2SyncAccounts'}
+demisto.executeCommand = execute_command
 
-def test():
+return_results
+
+def test(mocker):
     from GetIncidentsApiModule import main
 
     try:
