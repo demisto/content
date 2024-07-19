29120a3a1d2fb709940913260fed3caadadc3f2b
Tal <tcarmeli@paloaltonetworks.com>
2024-07-20 01:17:53 +0300

    forth  commit

--- Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py
+++ Packs/MicrosoftGraphMail/Integrations/MicrosoftGraphMail/MicrosoftGraphMail_test.py
@@ -82,7 +82,8 @@ def test_params_working(mocker, params, expected_results):
                                                       emails_fetch_limit=50, timeout=10, endpoint='com',
                                                       certificate_thumbprint='', private_key='',
                                                       display_full_email_body=False, mark_fetched_read=False, look_back=0,
-                                                      managed_identities_client_id=None)
+                                                      managed_identities_client_id=None,
+                                                      legacy_name=False)
 
 
 def test_build_mail_object():
