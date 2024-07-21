3269285741b08a6f754c11bdc9b8b09d121210e7
samuelFain <65926551+samuelFain@users.noreply.github.com>
2024-07-21 16:12:11 +0300

    Handle line formatting error

--- Packs/AzureLogAnalytics/Integrations/AzureLogAnalytics/AzureLogAnalytics.py
+++ Packs/AzureLogAnalytics/Integrations/AzureLogAnalytics/AzureLogAnalytics.py
@@ -36,8 +36,10 @@ class Client:
             f"subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/"
             + f"providers/Microsoft.OperationalInsights/workspaces/{workspace_name}"
         )
-        auth_code_scope = f"{self.azure_cloud.endpoints.log_analytics_resource_id}/Data.Read%20\
-            {self.azure_cloud.endpoints.resource_manager}user_impersonation"
+        auth_code_scope = (
+            f"{self.azure_cloud.endpoints.log_analytics_resource_id}"
+            + f"/Data.Read%20{self.azure_cloud.endpoints.resource_manager}user_impersonation"
+        )
         resources_list = [self.azure_cloud.endpoints.resource_manager, self.azure_cloud.endpoints.log_analytics_resource_id]
         base_url = urljoin(url=self.azure_cloud.endpoints.resource_manager, suffix=suffix)
 
