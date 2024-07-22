0501bb4a4c23744320786860d218f363a906dbf3
edik24 <ekatsenelson@paloaltonetworks.com>
2024-07-22 21:00:02 +0300

    Impartner community pack

--- Packs/Impartner/Integrations/Impartner/Impartner_test.py
+++ Packs/Impartner/Integrations/Impartner/Impartner_test.py
@@ -1,5 +1,6 @@
 import json
 import io
+import pytest
 
 def util_load_json(path):
     with io.open(path, mode='r', encoding='utf-8') as f:
@@ -30,7 +31,28 @@ def test_list_command(mocker):
     assert response.outputs == api_response.get('data')
 
 
-def test_id_command(mocker):
+@pytest.mark.parametrize(
+    "args, res",
+    [
+        ({'id': '1111', 'all_fields': 'TRUE'},{'id': 11111111, 'isActive': True, 'mailingCity': 'Palo Alto',
+                                               'mailingCountry': 'United States', 'mailingPostalCode': '11111',
+                                               'mailingState': 'California', 'mailingStreet': '236 test Ave',
+                                               'name': 'test_account',
+                                               'recordLink': 'https://prod.impartner.live/load/ACT/11111111',
+                                               'website': 'https://www.test-account.ai/', 'mainProductToIntegrate': 'test',
+                                               'mutualCustomer': 'test', 'tpA_Product_s__cf': 'test',
+                                               'integration_Status__cf': 'Integration Approved',
+                                               'target_customers__cf': ['Large Enterprise', 'SMB', 'SME'],
+                                               'company_Main_Market_Segment__cf': ['Automation Orchestration & SOC tools',
+                                                                                   'Data Security Governance & Classification'],
+                                               'panW_Integration_Product__cf': ['test'],
+                                               'account_Integration_Status__cf': ['Integrations in Process'],
+                                               'accountTimeline': '2022-06-30T00:00:00'}),
+        ({'id': '1111', 'all_fields': 'FALSE'},{'PST Engineer': None, 'id': 11111111,
+                                                'link': 'https://prod.impartner.live/load/ACT/11111111', 'name': 'test_account'})
+    ]
+)
+def test_id_command(mocker, args, res):
     """
         Scenario: executing command impartner-get-account-id.
 
@@ -46,9 +68,8 @@ def test_id_command(mocker):
     from Impartner import Client, impartner_get_account_id_command
 
     client = Client(base_url='some_mock_url', verify=False)
-    args = {'id': '1111', 'all_fields': 'FALSE'}
     api_response = util_load_json('test_data/id_command_response.json')
     mocker.patch('Impartner.Client.get_accounts_id', return_value=api_response)
     response = impartner_get_account_id_command(client, args)
 
-    assert response.outputs == api_response.get('data')
+    assert response.outputs == res
