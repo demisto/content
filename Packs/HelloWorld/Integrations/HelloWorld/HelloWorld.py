93f53e8dbf93d1c0d2fdb1cbfc40bb151be217ea
Kobbi Gal <kgal@paloaltonetworks.com>
2024-07-22 12:55:41 +0300

    contrib test

--- Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.py
+++ Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.py
@@ -14,7 +14,6 @@ that allows you to capture Use Cases, Requirements and Inputs/Outputs.
 Example Design document for the this Integration (HelloWorld):
 https://docs.google.com/document/d/1wETtBEKg37PHNU8tYeB56M1LE314ux086z3HFeF_cX0
 
-
 HelloWorld API
 --------------
 
