ffe7488f3bff1fc8001694c3733f40b46cdafe06
jlevypaloalto <jlevy@paloaltonetworks.com>
2024-07-30 16:36:00 +0300

    init

--- Packs/PhishingURL/Scripts/DBotPredictURLPhishing/DBotPredictURLPhishing.py
+++ Packs/PhishingURL/Scripts/DBotPredictURLPhishing/DBotPredictURLPhishing.py
@@ -247,6 +247,7 @@ def prepend_protocol(url: str, protocol: str, www: bool = True) -> str:
     :param protocol: protocol we want to add (usually http or https)
     :return: str
     """
+    protocol = demisto.args().get('request_protocol', 'HTTP').lower()
     p = urllib.parse.urlparse(url, protocol)  # type: ignore
     netloc = p.netloc or p.path
     path = p.path if p.netloc else ''
