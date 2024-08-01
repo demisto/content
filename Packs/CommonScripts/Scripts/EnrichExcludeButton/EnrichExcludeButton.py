855c38c255fcd6d1d2d5fa052965a2e687298625
Ni-Knight <davrahami@paloaltonetworks.com>
2024-08-01 15:29:12 +0300

    Simple_unit_test

--- Packs/CommonScripts/Scripts/EnrichExcludeButton/EnrichExcludeButton.py
+++ Packs/CommonScripts/Scripts/EnrichExcludeButton/EnrichExcludeButton.py
@@ -1,3 +1,9 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
-return_error("\nThis indicator is enrich excluded")
+
+def main():
+    return_error("\nThis indicator is enrich excluded")
+
+''' ENTRY POINT '''
+if __name__ in ('__main__', '__builtin__', 'builtins'):
+    main()
