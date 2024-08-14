5f35c8261ff9f509b571b42048c231ca7e82d35c
YairGlik <yair.glik33@gmail.com>
2024-08-14 09:42:46 +0300

    fix build

--- Packs/ExabeamSecurityOperationsPlatform/Integrations/ExabeamSecOpsPlatform/ExabeamSecOpsPlatform_test.py
+++ Packs/ExabeamSecurityOperationsPlatform/Integrations/ExabeamSecOpsPlatform/ExabeamSecOpsPlatform_test.py
@@ -584,12 +584,12 @@ def test_generic_search_command(mocker, args, item_type, mock_response, expected
             {
                 "name": ["Alice", "Bob", "Charlie"],
                 "age": ["25", "30", "35"],
-                "city": ["New York", "Los Angeles", "Chicago"]
+                "city": ["city1", "city2", "city3"]
             },
             [
-                {"name": "Alice", "age": "25", "city": "New York"},
-                {"name": "Bob", "age": "30", "city": "Los Angeles"},
-                {"name": "Charlie", "age": "35", "city": "Chicago"}
+                {"name": "Alice", "age": "25", "city": "city1"},
+                {"name": "Bob", "age": "30", "city": "city2"},
+                {"name": "Charlie", "age": "35", "city": "city3"}
             ]
         ),
     ]
