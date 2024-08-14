c0454570b02963fc7b51a710281c3df25ad3ad70
YuvHayun <yhayun@paloaltonetworks.com>
2024-08-14 10:38:11 +0300

    add logs

--- Packs/Campaign/Scripts/FindEmailCampaign/FindEmailCampaign.py
+++ Packs/Campaign/Scripts/FindEmailCampaign/FindEmailCampaign.py
@@ -302,6 +302,7 @@ def calculate_campaign_details_table(incidents_df, fields_to_display):
             if len(field_values) > 0:
                 if field in RECIPIENTS_COLUMNS:
                     field_values = [item for sublist in field_values for item in sublist]
+                demisto.info(f"[test] Current field is {field=}, the field_values are {field_values=}")
                 field_values_counter = Counter(field_values).most_common()  # type: ignore
                 field_value_str = get_str_representation_top_n_values(field_values, field_values_counter, top_n)
                 headers.append(field)
