6caf1125bbdf043fcdd0bdc3d9d143eb61e966e5
meichler <meichler@paloaltonetworks.com>
2024-08-14 11:26:24 +0300

    fix type

--- Packs/Box/Integrations/BoxV2/BoxV2.py
+++ Packs/Box/Integrations/BoxV2/BoxV2.py
@@ -655,7 +655,7 @@ class Client(BaseClient):
         return parts, upload_url_suffix
 
     def commit_file(self, file_path: str, as_user: Optional[str], parts: List[Dict],
-                    upload_url_suffix: str) -> requests.Response:
+                    upload_url_suffix: str) -> Response:
         """
         Once a file has been uploaded, the file must be committed. This request requires the SHA1
         digest of the entire file to be sent in the header. We reread the file to ensure the SHA is
@@ -1521,9 +1521,11 @@ def upload_file_command(args: Dict[str, Any], client: Client) -> PollResult:
     if '.' not in file_name:  # type: ignore
         raise DemistoException('A file extension is required in the filename.')
 
+    response: dict | Response
+
     # A generic endpoint is used for files under 50 MB, while an upload session is required for files over 50 MB.
     if file_size < maximum_chunk_size:
-        response: dict = client.upload_file(file_path=file_path, file_name=file_name, folder_id=folder_id, as_user=as_user)
+        response = client.upload_file(file_path=file_path, file_name=file_name, folder_id=folder_id, as_user=as_user)
         command_results = CommandResults(
             readable_output="File was successfully uploaded",
             outputs_prefix='Box.File',
@@ -1534,7 +1536,7 @@ def upload_file_command(args: Dict[str, Any], client: Client) -> PollResult:
     else:
         if not (parts := argToList(args.get('parts'))) or not (upload_url_suffix := args.get('upload_url_suffix')):
             parts, upload_url_suffix = client.chunk_upload(file_name, file_size, file_path, folder_id, as_user)
-        response: requests.Response = client.commit_file(file_path, as_user, parts, upload_url_suffix)
+        response = client.commit_file(file_path, as_user, parts, upload_url_suffix)
 
         if response.status_code == 202:
             retry_after = response.headers['retry-after'] or 30
