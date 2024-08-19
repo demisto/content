2321b529539ac2469f181534e7e933093952508b
sapirshuker <sshuker@paloaltonetworks.com>
2024-08-19 21:02:31 +0300

    delete get_report_object duplicated function

--- Packs/FeedUnit42v2/Integrations/FeedUnit42v2/FeedUnit42v2.py
+++ Packs/FeedUnit42v2/Integrations/FeedUnit42v2/FeedUnit42v2.py
@@ -180,29 +180,6 @@ def parse_malware(client, malware_objects: list = [], feed_tags: list = [],
     return malware_list
 
 
-def get_report_object(self, obj_id: str):
-    """Get specific report object by id.
-    Args:
-    obj_id: The object ID.
-    id_to_object: a dict in the form of - id: stix_object.
-    Returns:
-        A sub report object.
-    """
-    sub_report_obj = self.id_to_object.get(obj_id, {})
-    if not sub_report_obj:
-        if report_from_api := self.fetch_stix_objects_from_api(
-            type="report", id=obj_id
-        ):
-            sub_report_obj = report_from_api[0] if len(report_from_api) == 1 else None
-            if not sub_report_obj:
-                demisto.debug(
-                    f"{INTEGRATION_NAME}: Found more then one object for report object {obj_id} skipping"
-                )
-        else:
-            demisto.debug(f"{INTEGRATION_NAME}: Could not find report object {obj_id}")
-    return sub_report_obj
-
-
 def is_atom42_main_report(report_obj):
     """ Our definition for main report is a report with 'object_refs'
     list that contains only objects of the type not a report and a report with
