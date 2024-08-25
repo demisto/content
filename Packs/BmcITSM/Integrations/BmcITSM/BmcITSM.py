852ecffe8ca162773926e1ac45da760b20b460eb
meichler <meichler@paloaltonetworks.com>
2024-08-25 15:15:37 +0300

    revert

--- Packs/BmcITSM/Integrations/BmcITSM/BmcITSM.py
+++ Packs/BmcITSM/Integrations/BmcITSM/BmcITSM.py
@@ -543,7 +543,7 @@ class Client(BaseClient):
         company: str,
         assigned_support_organization: str,
         assigned_support_company: str,
-        assigned_group: str,
+        assigned_support_group_name: str,
         assignee: str,
         assignee_login_id: str,
         site_group: str,
@@ -585,7 +585,7 @@ class Client(BaseClient):
             "Reported Source": reported_source,
             "Service_Type": service_type,
             "Assigned Support Organization": assigned_support_organization,
-            "Assigned Group": assigned_group,
+            "Assigned Group": assigned_support_group_name,
             "Assignee": assignee,
             "Assignee Login ID": assignee_login_id,
             "Assigned Support Company": assigned_support_company,
@@ -1900,7 +1900,7 @@ def incident_create_command(client: Client, args: Dict[str, Any]) -> CommandResu
     company = args.get("location_company")
     assigned_support_organization = args.get("assigned_support_organization")
     assigned_support_company = args.get("assigned_support_company")
-    assigned_group = args.get("assigned_group")
+    assigned_support_group_name = args.get("support_group_name")
     assignee_login_id = args.get("assignee_login_id")
     assignee = args.get("assignee")
     site_group = args.get("site_group")
@@ -1930,7 +1930,7 @@ def incident_create_command(client: Client, args: Dict[str, Any]) -> CommandResu
         company=company,  # type: ignore[arg-type]
         assigned_support_organization=assigned_support_organization,  # type: ignore[arg-type]
         assigned_support_company=assigned_support_company,  # type: ignore[arg-type]
-        assigned_group=assigned_group,  # type: ignore[arg-type]
+        assigned_support_group_name=assigned_support_group_name,  # type: ignore[arg-type]
         assignee=assignee,  # type: ignore[arg-type]
         assignee_login_id=assignee_login_id,  # type: ignore[arg-type]
         site_group=site_group,  # type: ignore[arg-type]
