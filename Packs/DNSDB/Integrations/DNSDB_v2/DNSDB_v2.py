e1f294212d44445d6a64a4a106ae36d637d39db7
shmuel44 <skroizer@paloaltonetworks.com>
2024-08-27 11:00:09 +0300

    fix CR

--- Packs/DNSDB/Integrations/DNSDB_v2/DNSDB_v2.py
+++ Packs/DNSDB/Integrations/DNSDB_v2/DNSDB_v2.py
@@ -1,12 +1,12 @@
 import inspect
 import locale
-from collections.abc import Iterator, Callable, Iterable  # noqa: F401
+from collections.abc import Iterator, Iterable
 import urllib
 import urllib.parse
 
 from CommonServerPython import *  # noqa: E402 lgtm [py/polluting-import]
 
-import datetime  # type: ignore[assignment]
+from datetime import datetime
 import json
 import re
 
@@ -602,7 +602,7 @@ def parse_rate_limit_int(i):
 
 def parse_unix_time(ts) -> str:
     try:
-        return datetime.datetime.utcfromtimestamp(ts).strftime("%Y-%m-%dT%H:%M:%SZ")  # type: ignore[attr-defined]
+        return datetime.utcfromtimestamp(ts).strftime("%Y-%m-%dT%H:%M:%SZ")  # type: ignore[attr-defined]
     except TypeError:
         return ts
 
