8b8fc076d89ec7b5025107e25f2f3d83333af255
merit-maita <meretmaayta@gmail.com>
2024-08-29 09:55:03 +0300

    applied fix

--- Packs/CyberArkEPM/Integrations/CyberArkEPMEventCollector/CyberArkEPMEventCollector.py
+++ Packs/CyberArkEPM/Integrations/CyberArkEPMEventCollector/CyberArkEPMEventCollector.py
@@ -69,8 +69,9 @@ class Client(BaseClient):
         full_url = f'{self.application_url}?onetimetoken={self.get_session_token()}'
         result = self._http_request('POST', full_url=full_url, resp_type='response')
         soup = BeautifulSoup(result.text, features='html.parser')
-        saml_response = soup.find("input", {'name': 'SAMLResponse'}).get('value')
-        return saml_response
+        saml_response = soup.find("input", {'name': 'SAMLResponse'}) or {}
+
+        return saml_response.get('value')
 
     def saml_auth_to_cyber_ark(self):
         # Reference: https://docs.cyberark.com/EPM/Latest/en/Content/WebServices/SAMLAuthentication.htm
