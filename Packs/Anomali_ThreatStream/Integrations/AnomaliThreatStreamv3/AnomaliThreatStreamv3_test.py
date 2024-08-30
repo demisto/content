50223e3622a76866f6c67be6078555a290c33e1d
epartington <eric.partington@gmail.com>
2024-08-30 10:28:43 -0400

    Update AnomaliThreatStreamv3_test.py
    resetting _test.py format

--- Packs/Anomali_ThreatStream/Integrations/AnomaliThreatStreamv3/AnomaliThreatStreamv3_test.py
+++ Packs/Anomali_ThreatStream/Integrations/AnomaliThreatStreamv3/AnomaliThreatStreamv3_test.py
@@ -1005,7 +1005,7 @@ class TestUpdateCommands:
         mocker.patch.object(Client, 'http_request', return_value={'success': False})
 
         # run & validate
-        msg = "Failed to add ['tag_1'] to Actor with test_actor_id"
+        msg = "Failed to add \['tag_1'\] to Actor with test_actor_id"
         with pytest.raises(DemistoException, match=msg):
             add_tag_to_model(mock_client(), model_id='test_actor_id', model='Actor', tags='tag_1')
 
