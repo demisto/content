6ceaa098a1a324ac1d4ca2751645138c43f453f6
sapirshuker <sshuker@paloaltonetworks.com>
2024-09-04 16:29:09 +0300

    add strip to the get_mitre_attack_id_and_value_from_name function

--- Packs/ApiModules/Scripts/TAXII2ApiModule/TAXII2ApiModule.py
+++ Packs/ApiModules/Scripts/TAXII2ApiModule/TAXII2ApiModule.py
@@ -965,7 +965,7 @@ class STIX2XSOARParser(BaseClient):
         except ValueError:
             raise DemistoException(f"Failed parsing attack indicator {ind_name}")
         ind_id = partition_result[0]
-        value = partition_result[2]
+        value = partition_result[2].strip()
 
         if attack_indicator.get('x_mitre_is_subtechnique'):
             value = attack_indicator.get('x_panw_parent_technique_subtechnique')
