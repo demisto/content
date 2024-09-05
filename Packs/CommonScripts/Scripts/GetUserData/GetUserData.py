712f36a6d8fc524e419da641f717f16c6a1cf5e4
Menachem <mwienfeld@paloaltonetworks.com>
2024-09-05 16:36:56 +0300

    Add docstring

--- Packs/CommonScripts/Scripts/GetUserData/GetUserData.py
+++ Packs/CommonScripts/Scripts/GetUserData/GetUserData.py
@@ -24,6 +24,27 @@ def create_account(
     manager_display_name: Optional[str] = None,
     risk_level: Optional[str] = None,
 ) -> dict[str, Any]:
+    """
+    Create an account dictionary with the provided user information.
+
+    Args:
+        id (Optional[str]): The unique identifier for the account.
+        username (Optional[str]): The username associated with the account.
+        display_name (Optional[str]): The display name for the account.
+        email_address (Optional[str]): The email address associated with the account.
+        groups (Optional[list[str]]): A list of groups the account belongs to.
+        type (Optional[str]): The type of the account.
+        job_title (Optional[str]): The job title of the account holder.
+        office (Optional[str]): The office location of the account holder.
+        telephone_number (Optional[str]): The telephone number associated with the account.
+        is_enabled (Optional[bool]): Whether the account is enabled or not.
+        manager_email (Optional[str]): The email address of the account holder's manager.
+        manager_display_name (Optional[str]): The display name of the account holder's manager.
+        risk_level (Optional[str]): The risk level associated with the account.
+
+    Returns:
+        dict[str, Any]: A dictionary containing the non-empty account information.
+    """
     account = {
         "id": id,
         "username": username,
@@ -58,6 +79,30 @@ class Command:
         is_generic: bool = False,
         is_enabled: bool = True,
     ):
+        """
+        Initialize a Command object.
+
+        Args:
+            name (str): The name of the command.
+            args (dict): The arguments for the command.
+            output_key (str): The key for the command output.
+            output_function (Callable[[dict[str, Any]], dict[str, Any]]): Function to process the output.
+            update_args_fun (Optional[Callable[[], dict[str, Any]]]): Function to update arguments before running.
+            brand (Optional[str]): The brand associated with the command.
+            is_generic (bool): Whether the command is generic or not.
+            is_enabled (bool): Whether the command is enabled or not.
+
+        Attributes:
+            name (str): The name of the command.
+            args (dict): The arguments for the command.
+            brand (Optional[str]): The brand associated with the command.
+            is_enabled (bool): Whether the command is enabled or not.
+            is_generic (bool): Whether the command is generic or not.
+            output_key (str): The key for the command output.
+            command (CommandRunner.Command): The command runner object.
+            output_function (Callable): Function to process the output.
+            update_args_fun (Optional[Callable]): Function to update arguments before running.
+        """
         self.name = name
         self.args = args
         self.brand = brand
@@ -72,9 +117,25 @@ class Command:
         self.update_args_fun = update_args_fun
 
     def _verify_args(self) -> bool:
+        """
+        Verify if any arguments are provided.
+
+        Returns:
+            bool: True if any argument has a value, or if no arguments are present.
+                  False if all arguments are empty.
+        """
         return any(self.args.values()) if self.args else True
 
     def _get_output_key(self, outputs: dict[str, Any]) -> str:
+        """
+        Get the output key from the outputs dictionary.
+
+        Args:
+            outputs (dict[str, Any]): The dictionary containing output keys and values.
+
+        Returns:
+            str: The output key if found, or an empty string if not found.
+        """
         output_key = ""
         if self.output_key in outputs:
             output_key = self.output_key
@@ -85,6 +146,19 @@ class Command:
         return output_key
 
     def _prepare_output(self, entry_context: dict[str, Any]) -> dict[str, Any]:
+        """
+        Prepares the output from the entry context.
+
+        Args:
+            entry_context (dict[str, Any]): The entry context containing the command output.
+
+        Returns:
+            dict[str, Any]: The processed output.
+
+        This method extracts the relevant data from the entry context using the output key,
+        processes it with the output function, and returns the result. If the output key
+        is not found or the entry context is empty, it returns an empty dictionary.
+        """
         output = {}
         if entry_context:
             output_key = self._get_output_key(entry_context)
@@ -103,29 +177,60 @@ class Command:
     def _prepare_readable_output(
         self, response: dict, is_error: bool = False
     ) -> CommandResults:
+        """
+        Prepare a readable output for the command result.
+
+        Args:
+            response (dict): The response dictionary containing the command result.
+            is_error (bool, optional): Flag indicating if the result is an error. Defaults to False.
+
+        Returns:
+            CommandResults: The prepared command results with readable output.
+
+        This method formats the command result into a readable output, including the command that was executed.
+        It handles both successful results and errors, creating appropriate CommandResults objects with
+        formatted messages.
+        """
         command = f'!{self.name} {" ".join([f"{arg}={value}" for arg, value in self.args.items() if value])}'
-        tag = ["get-user-data"]
         if not is_error:
             readable_output = response["HumanReadable"]
             result_message = f"#### Result for {command}\n{readable_output}"
-            result = CommandResults(
-                readable_output=result_message, mark_as_note=True, tags=tag
-            )
+            result = CommandResults(readable_output=result_message, mark_as_note=True)
         else:
             result_message = f"#### Error for {command}\n{response}"
             result = CommandResults(
                 readable_output=result_message,
                 entry_type=EntryType.ERROR,
                 mark_as_note=True,
-                tags=tag,
             )
         return result
 
     def update_command_args(self, args: dict):
+        """
+        Update the command arguments with the provided dictionary.
+
+        Args:
+            args (dict): A dictionary containing new arguments to update.
+
+        This method updates the existing arguments dictionary and replaces the
+        command's argument list with the new arguments.
+        """
         self.args.update(args)
         self.command.args_lst = [args]
 
     def execute(self) -> tuple[bool, list[CommandResults], list[dict]]:
+        """
+        Execute the command associated with this instance.
+
+        Returns:
+            tuple[bool, list[CommandResults], list[dict]]: A tuple containing:
+                - status (bool): True if the command execution was successful, False otherwise.
+                - return_results (list[CommandResults]): A list of CommandResults objects representing the command output.
+                - return_outputs (list[dict]): A list of dictionaries containing the command outputs.
+
+        This method checks if the command is enabled and if the required arguments are provided.
+        If conditions are met, it executes the command, processes the results, and prepares the output.
+        """
         status = False
         return_results = []
         return_outputs = []
@@ -159,12 +264,33 @@ class Command:
 
 class Task:
     def __init__(self, task_id: str, command: Optional[Command] = None):
-        """Base class for all tasks."""
+        """
+        Initialize a Task object.
+
+        Args:
+            task_id (str): A unique identifier for the task.
+            command (Optional[Command]): The command associated with this task, if any.
+
+        Attributes:
+            task_id (str): The unique identifier of the task.
+            command (Optional[Command]): The command associated with this task.
+            status (Optional[bool]): The execution status of the task, initially set to None.
+        """
         self.task_id = task_id
         self.command = command
         self.status: Optional[bool] = None
 
     def execute(self) -> tuple[list[CommandResults], list[dict]]:
+        """
+        Execute the task.
+
+        This method runs the associated command if one exists, otherwise it sets the status to True.
+
+        Returns:
+            tuple[list[CommandResults], list[dict]]: A tuple containing two lists:
+                - A list of CommandResults objects representing the results of the command execution.
+                - A list of dictionaries containing the outputs of the command execution.
+        """
         results: list[CommandResults] = []
         outputs: list[dict] = []
         if self.command:
@@ -175,12 +301,21 @@ class Task:
 
 
 class PlaybookGraph:
-    """
-    tasks: nodes
-    connections: edges
-    """
-
     def __init__(self):
+        """
+        Initialize the PlaybookGraph.
+        The tasks are the nodes in the graph, and the connections are the edges.
+
+        Attributes:
+            args (dict[str, Any]): Dictionary to store arguments.
+            tasks (dict[str, Task]): Dictionary to store tasks, with task_id as the key.
+            connections (dict[str, list[tuple[str, Optional[Callable[[], bool]]]]]):
+                Dictionary to store task connections, with task_id as the key and a list of
+                tuples containing the connected task_id and an optional condition function.
+            start_task_id (str): The ID of the starting task.
+            outputs (list): List to store outputs.
+            results (list): List to store results.
+        """
         self.args: dict[str, Any] = {}
         self.tasks: dict[str, Task] = {}
         self.connections: dict[str, list[tuple[str, Optional[Callable[[], bool]]]]] = {}
@@ -189,12 +324,17 @@ class PlaybookGraph:
         self.results = []
 
     def add_task(self, task: Task):
+        """
+        Add a task to the PlaybookGraph.
+
+        This method adds the given task to the tasks dictionary and initializes
+        an empty list for its connections in the connections dictionary.
+
+        Args:
+            task (Task): The task to be added to the PlaybookGraph.
+        """
         self.tasks[task.task_id] = task
         self.connections[task.task_id] = []
-
-    def add_tasks(self, tasks: list[Task]):
-        for task in tasks:
-            self.add_task(task)
 
     def add_connection(
         self,
@@ -202,9 +342,32 @@ class PlaybookGraph:
         to_task: Task,
         condition: Callable[[], bool] = lambda: True,
     ):
+        """
+        Add a connection between two tasks in the PlaybookGraph.
+
+        This method creates a connection from one task to another, with an optional condition.
+
+        Args:
+            from_task (Task): The task from which the connection originates.
+            to_task (Task): The task to which the connection leads.
+            condition (Callable[[], bool], optional): A function that returns a boolean
+                indicating whether the connection should be followed. Defaults to a function
+                that always returns True.
+        """
         self.connections[from_task.task_id].append((to_task.task_id, condition))
 
     def run(self):
+        """
+        Execute the tasks in the PlaybookGraph.
+
+        This method runs the tasks in the PlaybookGraph in a breadth-first manner,
+        starting from the start_task_id. It executes each task, collects the results
+        and outputs, and follows the connections to the next tasks based on their
+        conditions. The execution continues until all reachable tasks have been processed.
+
+        The method updates the PlaybookGraph's results and outputs lists with the
+        collected data from each executed task.
+        """
         current_task_ids = [self.start_task_id]
         executed_task_ids = set()
 
@@ -237,7 +400,17 @@ class PlaybookGraph:
 
 def update_enabled_commands(commands: dict[str, Command]) -> None:
     """
-    This function updates the is_enabled attribute of the commands based on the availability of the brand.
+    Updates the is_enabled attribute of the commands based on the availability of the brand.
+
+    This function checks the availability of integration brands using the IsIntegrationAvailable
+    script and updates the is_enabled attribute of each command accordingly.
+    Generic commands are always enabled.
+
+    Args:
+        commands (dict[str, Command]): A dictionary of Command objects to update.
+
+    Returns:
+        None
     """
     brands = [command.brand for command in commands.values() if not command.is_generic]
     res = execute_command(
@@ -259,6 +432,20 @@ def update_enabled_commands(commands: dict[str, Command]) -> None:
 
 
 def merge_accounts(accounts: list[dict[str, str]]) -> dict[str, Any]:
+    """
+    Merges multiple account dictionaries into a single Common.Account object.
+
+    This function takes a list of account dictionaries and combines them into a single
+    merged account. If there are conflicting values for the same key, it logs a debug
+    message and keeps the first encountered value.
+
+    Args:
+        accounts (list[dict[str, str]]): A list of account dictionaries to merge.
+
+    Returns:
+        dict[str, Any]: A dictionary representation of the merged Common.Account object,
+        or an empty dictionary if no accounts were provided.
+    """
     merged_account: dict[str, Any] = {}
     for account in accounts:
         for key, value in account.items():
@@ -280,6 +467,16 @@ def setup_commands(playbook: PlaybookGraph) -> dict[str, Command]:
     user_name = playbook.args["user_name"]
     user_email = playbook.args["user_email"]
     domain = playbook.args["domain"]
+    """
+    Sets up and returns a dictionary of Command objects.
+    
+    This function creates Command objects for different identity management systems and
+    services.
+    
+    Returns:
+        dict[str, Command]: A dictionary of Command objects, where the keys are task names
+        and the values are the corresponding Command objects.
+    """
     return {
         "identityiq_search_identities_task": Command(
             name="identityiq-search-identities",
@@ -462,12 +659,34 @@ def setup_commands(playbook: PlaybookGraph) -> dict[str, Command]:
 
 
 def setup_tasks(playbook: PlaybookGraph, commands: Dict[str, Command]):
+    """
+    Sets up tasks for the playbook.
+
+    Args:
+        playbook (PlaybookGraph): The playbook graph to add tasks to.
+        commands (Dict[str, Command]): A dictionary of command names to Command objects.
+
+    This function adds a start task and then creates tasks for each command in the provided dictionary.
+    """
     playbook.add_task(Task("start_task"))
     for task_name, command in commands.items():
         playbook.add_task(Task(task_name, command))
 
 
 def setup_connections(playbook: PlaybookGraph):
+    """
+    Sets up connections between tasks in the playbook graph.
+
+    This function establishes the flow of execution between different tasks in the playbook.
+    It adds connections from the start task to various user data retrieval tasks.
+    It also sets up connections for retrieving manager information when available.
+
+    Args:
+        playbook (PlaybookGraph): The playbook graph to add connections to.
+
+    Note:
+        The function uses a shared context (SHARED_CONTEXT) to determine certain conditions.
+    """
     is_domain_in_user_name = "\\" in playbook.args["user_name"]
     playbook.add_connection(
         playbook.tasks["start_task"],
@@ -532,6 +751,19 @@ def setup_connections(playbook: PlaybookGraph):
 def setup_playbook_graph(
     playbook: PlaybookGraph, user_id: str, user_name: str, user_email: str, domain: str
 ) -> None:
+    """
+    Sets up the playbook graph for user data retrieval.
+
+    Args:
+        playbook (PlaybookGraph): The playbook graph to set up.
+        user_id (str): The ID of the user.
+        user_name (str): The name of the user.
+        user_email (str): The email of the user.
+        domain (str): The domain of the user.
+
+    Returns:
+        None
+    """
     playbook.args = {
         "user_id": user_id,
         "user_name": user_name,
@@ -594,8 +826,10 @@ def main():
             except Exception as e:
                 return_results(
                     CommandResults(
-                        readable_output=("An error occurred while running the 'get-user-data' command with the arg values: "
-                                         f"{user_id=}, {user_name=}, {user_email=}, {domain=}. Error: {str(e)}"),
+                        readable_output=(
+                            "An error occurred while running the 'get-user-data' command with the arg values: "
+                            f"{user_id=}, {user_name=}, {user_email=}, {domain=}. Error: {str(e)}"
+                        ),
                         entry_type=EntryType.ERROR,
                     )
                 )
