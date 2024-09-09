786c727d80bcf3edbbfdf4dac7f508ec5f4a4292
Lizz <lizzbrown1089@gmail.com>
2024-09-09 09:56:50 -0400

    Update Packs/CommunityCommonScripts/Scripts/DateTimeToLDAPTime/DateTimeToLDAPTime.py
    Co-authored-by: rshunim <102469772+rshunim@users.noreply.github.com>

--- Packs/CommunityCommonScripts/Scripts/DateTimeToLDAPTime/DateTimeToLDAPTime.py
+++ Packs/CommunityCommonScripts/Scripts/DateTimeToLDAPTime/DateTimeToLDAPTime.py
@@ -6,7 +6,7 @@ import dateparser
 def convert_time_command(time):
     # ((current time - epoch start date) in seconds + seconds since 1/1/1601) * 10000000 (to get nanoseconds)
     ldap_time = ((time_to_convert - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() + 11644473600) * 10000000
-    return str(int(res))
+    return str(int(ldap_time))
 
 
 def main():
