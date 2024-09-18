67d13a16dc84de044331ca15fd4428591e4d2e81
YairGlik <yair.glik33@gmail.com>
2024-09-18 13:40:20 +0300

    fix test

--- Packs/Shodan/Integrations/Shodan_v2/Shodan_v2_test.py
+++ Packs/Shodan/Integrations/Shodan_v2/Shodan_v2_test.py
@@ -1,3 +1,5 @@
+# import pytest
+# import demistomock as demisto
 import json
 from datetime import datetime
 
@@ -8,19 +10,19 @@ with open('./test_data/respons.json') as f:
 
 
 def test_get_events_command(mocker):
-    mock_http_request = mocker.patch('http_request', return_value=RESPONSE)
-    hr, events = get_events_command({'max_fetch': 2, 'start_date': '2024-08-13T00:00:00.000'})
+    mock_http_request = mocker.patch('Shodan_v2.http_request', return_value=RESPONSE)
+    hr, events = get_events_command({'max_fetch': 2, 'start_date': '2024-08-10T12:46:18.012000'})
 
     assert len(events) == 2
-    assert events[0]["name"] == "test_alert"
+    assert events[0]["name"] == "test_alert2"
 
     mock_http_request.assert_called_once_with('GET', '/shodan/alert/info')
 
 
 def test_filter_events():
-    start_date = datetime.strptime('2024-08-13T00:00:00.000', "%Y-%m-%dT%H:%M:%S.%f")
+    start_date = datetime.strptime('2024-08-10T12:46:18.012000', "%Y-%m-%dT%H:%M:%S.%f")
 
     filtered_events = filter_events(events=RESPONSE, start_date=start_date, limit=3)
 
-    assert len(filtered_events) == 2
-    assert filtered_events[0]["name"] == "nat-alert"
+    assert len(filtered_events) == 3
+    assert filtered_events[0]["name"] == "test_alert2"
