1ed80453892eb12dd714f7219dc9451f6157929c
Danny_Fried <dfried@paloaltonetworks.com>
2024-10-04 14:20:13 +0300

    compare key to DBOT_SCORE_KEY

--- Packs/CommonScripts/Scripts/DeleteContext/DeleteContext.js
+++ Packs/CommonScripts/Scripts/DeleteContext/DeleteContext.js
@@ -20,7 +20,7 @@ function deleteKeys(keysToDelete = [], _keysToKeep = [], keepDBotScoreKey = fals
     var message = '';
     for (var key of keysToDelete) {
         // 'DBotScore' key shall not be deleted in order to prevent caching it repeatedly and impacting performance.
-        if (DBOT_SCORE_KEY in _keysToKeep && keepDBotScoreKey){
+        if (key == DBOT_SCORE_KEY && keepDBotScoreKey){
             continue;
         }
         const originalKey = typeof key === "string" ? key.trim() : key;
