2a6dfd57f2e18c404a4e19d1772af2d324f8e554
merit-maita <meretmaayta@gmail.com>
2024-10-07 17:36:01 +0300

    removed func

--- Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
+++ Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
@@ -2,18 +2,6 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 
-def is_substring_in_list(single_str: str, str_list: list[str]) -> list:
-    """
-    This function checks if a string is in a list of strings, fully or partially, case insensitive.
-    Args:
-       single_str: A string to check if it exists in the list.
-       str_list: A list of strings to check if the single_str is in it, fully or partially.
-    Returns:
-        True or False
-    """
-    return list(filter(lambda x: single_str in x.lower(), str_list))
-
-
 def main():
     args = demisto.args()
     leftArg = args.get("left")
