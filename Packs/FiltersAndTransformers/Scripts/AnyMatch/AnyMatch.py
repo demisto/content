f7248fc4ad99093adb4acd38e5e1776532997fe4
merit-maita <meretmaayta@gmail.com>
2024-10-09 12:34:28 +0300

    reverted to first pr

--- Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
+++ Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
@@ -2,20 +2,32 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
 
+def is_substring_in_list(single_str: str, str_list: list[str]) -> bool:
+    """
+    This function checks if a string is in a list of strings, fully or partially, case insensitive.
+    Args:
+       single_str: A string to check if it exists in the list.
+       str_list: A list of strings to check if the single_str is in it, fully or partially.
+    Returns:
+        True or False
+    """
+    lower_list = [x.lower() for x in str_list]
+    return any(i in single_str.lower() for i in lower_list)
+
+
 def main():
     args = demisto.args()
     leftArg = args.get("left")
     rightArg = args.get("right")
 
+    if not (leftArg and rightArg):
+        return_results(False)
+
     left_list = argToList(leftArg)
     right_list = argToList(rightArg)
 
-    if not (leftArg and rightArg):
-        return_results("No matches found.")
-    results: set[str] = set()
-    for right_val in right_list:
-        results = results.union(list(filter(lambda left_val: right_val.lower() in left_val.lower(), left_list)))
-    return return_results(list(results)) if results else return_results("No matches found.")
+    for left_val in left_list:
+        return_results(is_substring_in_list(left_val, right_list))
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
