cea8a57e679d7a968682e98e0963a04069cc2496
mayagoldman <mgoldman@paloaltonetworks.com>
2024-10-13 21:33:47 +0300

    לכן לדעתי האופציה של ה includeinformational צריכה להיות רק true, אם השתמשו בזה - נעשה סינון אם לא, לא ה false מבלבל וסתם יוצר לך יותר עבודה

--- Packs/CommonScripts/Scripts/SearchIncidentsV2/SearchIncidentsV2.py
+++ Packs/CommonScripts/Scripts/SearchIncidentsV2/SearchIncidentsV2.py
@@ -155,16 +155,17 @@ def search_incidents(args: Dict):  # pragma: no cover
         if args.get('trimevents') == '0':
             args.pop('trimevents')
 
-    if args.get('includeinformational'):
+    if includeinformational := argToBoolean(args.get('includeinformational', False)):
         if not (args.get('fromdate') and args.get('todate')):
             raise ValueError('The includeinformational argument requires fromdate and todate arguments.')
         if (datetime.utcnow() - fromdate).total_seconds() > 5 * 60 * 60:  # type: ignore
             args['fromdate'] = arg_to_datetime('5 hours ago').isoformat()  # type: ignore
             demisto.log(f'fromdate: {fromdate} is more than 5 hours from now: {datetime.utcnow()}. '
-                         f'Currently, we support querying informational incidents for up to the last 5 hours.'
-                         f' The fromdate has been adjusted to {args.get("fromdate")}')
-        args['includeinformational'] = argToBoolean(args.get('includeinformational', False))
-
+                        f'Currently, we support querying informational incidents for up to the last 5 hours.'
+                        f' The fromdate has been adjusted to {args.get("fromdate")}')
+        args['includeinformational'] = includeinformational
+    else:
+        args.pop('includeinformational', None)
 
     # handle list of ids
     if args.get('id'):
