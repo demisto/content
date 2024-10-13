c613f78b292576303eb62840808feef5eb72418c
merit-maita <meretmaayta@gmail.com>
2024-10-13 11:43:19 +0300

    reverted changes

--- Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
+++ Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch.py
@@ -23,12 +23,10 @@ def main():
     if not (leftArg and rightArg):
         return_results(False)
 
-    left_list = argToList(leftArg)
-    right_list = argToList(rightArg)
-    results = []
+    left_list = argToList(str(leftArg))
+    right_list = argToList(str(rightArg))
     for left_val in left_list:
-        results.append(is_substring_in_list(left_val, right_list))
-    return return_results(any(i for i in results))
+        return_results(is_substring_in_list(left_val, right_list))
 
 
 if __name__ in ('__main__', '__builtin__', 'builtins'):
--- Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch_test.py
+++ Packs/FiltersAndTransformers/Scripts/AnyMatch/AnyMatch_test.py
@@ -11,23 +11,24 @@ from AnyMatch import main
 # a JSON containing a comma will be separated into two strings.
 
 
-@pytest.mark.parametrize('left,right,expected_result', [
-    ("2", "25,10", False),
-    ("'2'", "1,2,3", True),    # 2 is part of '2'
-    ("'abc','aha','a'", "A", True),
-    ("5,1,6,9,65,8,b", "1,'6'", True),  # no part of 6 or 65 is in the list: 1,'6'
-    ('a', "kfjua", False),
-    ("bca", "A", True),  # case insensitive
-    ("ABC", "a", True),  # case insensitive
-    ('{"alert": {"data": "x"}}', "x", True),
-    ("{'a':1,'c':2}", "{'a': 1}, {'b': 2}", False),     # {'a':1} is not a part of {'a':1, or 'c':2}
-    ("{'a': 1}, {'b': 2}", "{a:1}", False),  # {a:1} is not a part of {'a': 1} or {'b': 2}
+@pytest.mark.parametrize('left,right, call_count,expected_result', [
+    (123, 1, 1, [True]),
+    ("2", "25,10", 1, [False]),
+    ("1, '2'", "1,2,3", 2, [True, True]),    # a part of '2' is in '1,2,3'
+    ('"abc", "ahah", "a"', "A", 3, [True, True, True]),
+    ("5,1,6,9,65,8,b", "1,'6'", 7, [False, True, False, False, False, False, False]),  # no part of 6 or 65 is in the list: 1,'6'
+    ('a', "kfjua", 1, [False]),
+    (1, "1", 1, [True]),       # int and str are equal
+    ("bca", "A", 1, [True]),  # case insensitive
+    ("ABC", "a", 1, [True]),  # case insensitive
+    ({"alert": {"data": "x"}}, "x", 1, [True]),
+    ("{'a':1,'c':2}", "{'a': 1}, {'b': 2}", 2, [False, False]),     # {'a':1} is not a part of {'a':1, or 'c':2}
+    ("{'a': 1}, {'b': 2}", "{a:1}", 2, [False, False]),  # {a:1} is not a part of {'a': 1} or {'b': 2}
     # although '' is not a part of {'a':1,'c':2}, but ' is in {'a': 1 and in  'c': 2}
-    ("{'a':1,'c':2}", "'', '", True),
-    # one of the arguments is missing -> return empty list
-    ("1,2", "", False)
+    ("{'a':1,'c':2}", "'', '", 2, [True, True]),
+    (None, "A", 1, [False])
 ])
-def test_main(mocker, left, right, expected_result):
+def test_main(mocker, left, right, call_count, expected_result):
     """
     Given:
         left and right arguments.
@@ -39,6 +40,7 @@ def test_main(mocker, left, right, expected_result):
     mocker.patch.object(demisto, 'args', return_value={'left': left, 'right': right})
     mocker.patch.object(demisto, 'results')
     main()
-    results = demisto.results.call_args_list[0][0][0]
-    assert results == expected_result
-
+    assert demisto.results.call_count == call_count
+    for i in range(len(expected_result)):
+        results = demisto.results.call_args_list[i][0][0]
+        assert results == expected_result[i]
