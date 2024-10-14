137530d04b228cd3b371899c18d0ee95e83f1d08
dmeylakh <dmeylakh@paloaltonetworks.com>
2024-10-14 14:17:03 +0300

    testing fixed script

--- Packs/MicrosoftExchangeOnPremise/Integrations/EWSv2/EWSv2.py
+++ Packs/MicrosoftExchangeOnPremise/Integrations/EWSv2/EWSv2.py
@@ -2319,7 +2319,7 @@ def process_main():  # pragma: no cover
 
 def main():  # pragma: no cover
     try:
-        raise Exception("TESTING AAA")
+        raise Exception("TESTING AAABBB")
         handle_proxy()
         # When running big queries, like 'ews-search-mailbox' the memory might not freed by the garbage
         # collector. `separate_process` flag will run the integration on a separate process that will prevent
