6585f5701e6f8cff1462a85225353b16155c1c73
Yaakov Praisler <59408745+yaakovpraisler@users.noreply.github.com>
2024-10-18 14:04:34 +0300

    Fix pre-commit

--- Packs/UrlScan/Integrations/UrlScan/UrlScan.py
+++ Packs/UrlScan/Integrations/UrlScan/UrlScan.py
@@ -252,7 +252,7 @@ def urlscan_submit_url(client, url):
         submission_dict['customagent'] = demisto.args().get('useragent')
     elif demisto.params().get('useragent'):
         submission_dict['customagent'] = demisto.params().get('useragent')
-        
+
     if client.country:
         submission_dict['country'] = client.country.split(' ')[0]
 
@@ -833,7 +833,7 @@ def main():
     use_ssl = not params.get('insecure', False)
     reliability = params.get('integrationReliability')
     reliability = reliability if reliability else DBotScoreReliability.C
-    country = params.get('country','')
+    country = params.get('country', '')
     
     if DBotScoreReliability.is_valid_type(reliability):
         reliability = DBotScoreReliability.get_dbot_score_reliability_from_str(reliability)
@@ -849,7 +849,7 @@ def main():
         threshold=threshold,
         use_ssl=use_ssl,
         reliability=reliability,
-        country = country
+        country=country
     )
 
     demisto.debug(f'Command being called is {demisto.command()}')
