f433ecb635a115a07111e4721663938410483f78
samuelFain <65926551+samuelFain@users.noreply.github.com>
2024-10-27 11:45:04 +0200

    Remove redundant brackets

--- Packs/MicrosoftExchangeOnPremise/Integrations/EWSv2/EWSv2.py
+++ Packs/MicrosoftExchangeOnPremise/Integrations/EWSv2/EWSv2.py
@@ -830,7 +830,7 @@ def search_mailboxes(protocol, filter, limit=100, mailbox_search_scope=None, ema
     mailbox_ids = []
     limit_argument = arg_to_number(limit)
     if not limit_argument:
-        raise (DemistoException(f"Invalid limit value: {limit}. Please provide a valid integer."))
+        raise DemistoException(f"Invalid limit value: {limit}. Please provide a valid integer.")
     if mailbox_search_scope is not None and email_addresses is not None:
         raise Exception("Use one of the arguments - mailbox-search-scope or email-addresses, not both")
     if email_addresses:
