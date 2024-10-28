0df20fc8a12fd4760e230c22ab53fa1dc3effd38
MLainer1 <mlainer@paloaltonetworks.com>
2024-10-28 16:37:13 +0200

    test for dev_task_override_content_pipeline

--- Packs/AzureSentinel/Integrations/AzureSentinel/AzureSentinel.py
+++ Packs/AzureSentinel/Integrations/AzureSentinel/AzureSentinel.py
@@ -202,6 +202,7 @@ def error_handler(response: requests.Response):
                     'message': response.text
                 }
             }
+
         error_kind = error_json.get('error', {}).get('code', 'BadRequest')
         error_msg = error_json.get('error', {}).get('message', error_json)
         raise ValueError(
@@ -212,6 +213,7 @@ def error_handler(response: requests.Response):
 def format_date(date):
     if not date:
         return None
+
     return dateparser.parse(date).strftime(DATE_FORMAT)  # type:ignore
 
 
