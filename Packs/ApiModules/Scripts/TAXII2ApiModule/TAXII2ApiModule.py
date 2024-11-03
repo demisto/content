65db9f81375dd278b7e8b00f3c907763418eab59
yaakovpraisler <ypreisler@paloaltonetworks.com>
2024-11-03 18:55:56 +0200

    reverted non-relevant changes

--- Packs/ApiModules/Scripts/TAXII2ApiModule/TAXII2ApiModule.py
+++ Packs/ApiModules/Scripts/TAXII2ApiModule/TAXII2ApiModule.py
@@ -1,8 +1,7 @@
-from CommonServerPython import *  # noqa: F401
 import demistomock as demisto  # noqa: F401
-
+from CommonServerPython import *  # noqa: F401
 # pylint: disable=E9010, E9011
-
+from CommonServerUserPython import *
 
 from typing import Optional, Tuple
 from requests.sessions import merge_setting, CaseInsensitiveDict
