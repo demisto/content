9cac1288679c87e7b42cb617c29be1f8c155ea9b
yash-metron <yash@metronlabs.com>
2024-11-04 16:46:22 +0530

    Fixed pre-commit errors

--- Packs/DSPM/Scripts/DSPMCreateRiskSlackBlocks/DSPMCreateRiskSlackBlocks.py
+++ Packs/DSPM/Scripts/DSPMCreateRiskSlackBlocks/DSPMCreateRiskSlackBlocks.py
@@ -3,7 +3,7 @@ from CommonServerPython import *  # noqa: F401
 from typing import List, Dict, Any
 
 
-def create_slack_block(incident: dict, rule_names_list: list, incidentLink: str) -> dict:
+def create_slack_block(incident: dict, rule_names_dict: dict, incidentLink: str) -> dict:
     """
     Creates a Slack block message structure for a DSPM incident.
 
@@ -14,12 +14,12 @@ def create_slack_block(incident: dict, rule_names_list: list, incidentLink: str)
 
     Args:
         incident (dict): A dictionary containing details of the DSPM incident.
-        rule_names_list (list): A list of rule names that trigger the addition of a 'Remediate a Risk' option.
+        rule_names_dict (dict): A dictonary of rule names that trigger the addition of a 'Remediate a Risk' option.
 
     Returns:
         block (dict): A structured Slack block message in JSON format to be sent to Slack.
     """
-    rule_name = incident.get('ruleName')
+    rule_name = incident.get("ruleName")
 
     # Slack block structure
     block: Dict[str, List[Dict[str, Any]]] = {
@@ -29,8 +29,8 @@ def create_slack_block(incident: dict, rule_names_list: list, incidentLink: str)
                 "text": {
                     "type": "plain_text",
                     "text": "THE FOLLOWING RISK HAS BEEN DETECTED BY THE DSPM :warning:",
-                    "emoji": True
-                }
+                    "emoji": True,
+                },
             },
             {
                 "type": "section",
@@ -38,22 +38,20 @@ def create_slack_block(incident: dict, rule_names_list: list, incidentLink: str)
                 "text": {
                     "type": "mrkdwn",
                     "text": f"*XSOAR Incident ID:* {incident.get('incidentId')}\n"
-                            f"*XSOAR Incident link:* {incidentLink}\n"
-                            f"*DSPM Risk ID:* {incident.get('riskFindingId')}\n"
-                            f"*Rule Name:* {incident.get('ruleName')}\n"
-                            f"*Severity:* {incident.get('severity')}\n"
-                            f"*Asset Name:* {incident.get('assetName')}\n"
-                            f"*Asset ID:* {incident.get('assetId')}\n"
-                            f"*Project ID:* {incident.get('projectId')}\n"
-                            f"*Cloud Provider:* {incident.get('cloudProvider')}\n"
-                            f"*Service Type:* {incident.get('serviceType')}\n"
-                            f"*First Discovered:* {incident.get('firstDetectedOn')}\n"
-                            f"*Remediate Instruction:* {incident.get('remediateInstruction')}\n"
-                }
+                    f"*XSOAR Incident link:* {incidentLink}\n"
+                    f"*DSPM Risk ID:* {incident.get('riskFindingId')}\n"
+                    f"*Rule Name:* {incident.get('ruleName')}\n"
+                    f"*Severity:* {incident.get('severity')}\n"
+                    f"*Asset Name:* {incident.get('assetName')}\n"
+                    f"*Asset ID:* {incident.get('assetId')}\n"
+                    f"*Project ID:* {incident.get('projectId')}\n"
+                    f"*Cloud Provider:* {incident.get('cloudProvider')}\n"
+                    f"*Service Type:* {incident.get('serviceType')}\n"
+                    f"*First Discovered:* {incident.get('firstDetectedOn')}\n"
+                    f"*Remediate Instruction:* {incident.get('remediateInstruction')}\n",
+                },
             },
-            {
-                "type": "divider"
-            },
+            {"type": "divider"},
             {
                 "type": "actions",
                 "elements": [
@@ -61,82 +59,63 @@ def create_slack_block(incident: dict, rule_names_list: list, incidentLink: str)
                         "type": "radio_buttons",
                         "options": [
                             {
-                                "text": {
-                                    "type": "plain_text",
-                                    "text": "Create a Jira ticket",
-                                    "emoji": True
-                                },
-                                "value": "Create a Jira ticket"
+                                "text": {"type": "plain_text", "text": "Create a Jira ticket", "emoji": True},
+                                "value": "Create a Jira ticket",
                             }
                         ],
-                        "action_id": "actionId-0"
+                        "action_id": "actionId-0",
                     }
-                ]
+                ],
             },
             {
                 "type": "input",
                 "element": {
                     "type": "plain_text_input",
-                    "placeholder": {
-                        "type": "plain_text",
-                        "text": "Please enter a valid Project Name",
-                        "emoji": True
-                    },
-                    "action_id": "project_name"
+                    "placeholder": {"type": "plain_text", "text": "Please enter a valid Project Name", "emoji": True},
+                    "action_id": "project_name",
                 },
-                "label": {
-                    "type": "plain_text",
-                    "text": "Enter Project Name",
-                    "emoji": True
-                }
+                "label": {"type": "plain_text", "text": "Enter Project Name", "emoji": True},
             },
             {
                 "type": "input",
                 "element": {
                     "type": "plain_text_input",
-                    "placeholder": {
-                        "type": "plain_text",
-                        "text": "Please enter a valid Issue type",
-                        "emoji": True
-                    },
-                    "action_id": "Issue_type"
+                    "placeholder": {"type": "plain_text", "text": "Please enter a valid Issue type", "emoji": True},
+                    "action_id": "Issue_type",
                 },
-                "label": {
-                    "type": "plain_text",
-                    "text": "Enter Issue Type",
-                    "emoji": True
-                }
+                "label": {"type": "plain_text", "text": "Enter Issue Type", "emoji": True},
             },
-            {
-                "type": "divider"
-            }
+            {"type": "divider"},
         ]
     }
 
     # Log the block structure to verify correctness
     demisto.info(f"Block structure before modification: {json.dumps(block, indent=2)}")
 
-    # Add the "Remediate a Risk" radio button option if ruleName exists in rule_names_list
-    if rule_name in rule_names_list:
+    # Add the "Remediate a Risk" radio button option if ruleName exists in rule_names_dict
+    if rule_name in rule_names_dict:
         try:
             # Ensure the 'actions' block exists at the correct index
             elements = block["blocks"][3].get("elements", [{}])[0].get("options", [])
-            elements.insert(0, {
-                "text": {
-                    "type": "plain_text",
-                    "text": f"Remediate a Risk - {rule_names_list.get(rule_name)}",
-                    "emoji": True
+            elements.insert(
+                0,
+                {
+                    "text": {
+                        "type": "plain_text",
+                        "text": f"Remediate a Risk - {rule_names_dict.get(rule_name)}",
+                        "emoji": True,
+                    },
+                    "value": "Remediate a Risk",
                 },
-                "value": "Remediate a Risk"
-            })
-        except (AttributeError, IndexError, TypeError)as e:
+            )
+        except (AttributeError, IndexError, TypeError) as e:
             demisto.error(f"Error inserting 'Remediate a Risk' option: {str(e)}")
             raise
     res = {"block": block}
     return res
 
 
-''' MAIN FUNCTION '''
+""" MAIN FUNCTION """
 
 
 def main():  # pragma: no cover
@@ -150,14 +129,20 @@ def main():  # pragma: no cover
     Returns:
         None: Results are returned via demisto.results() and CommandResults().
     """
-    rule_names_list = {
-        "Sensitive asset open to world": "This action will block the public access of the bucket or container",
-        "Empty storage asset": "This action will delete the bucket or storage account in case of Azure"
+    rule_names_dict = {
+        "Sensitive asset open to world": (
+            "This action would block the public access to specific containers in storage account "
+            "(Azure) or to the bucket (AWS/GCP) based on the cloud provider."
+        ),
+        "Empty storage asset": (
+            "This action would delete the storage account (Azure) or the storage bucket "
+            "(AWS/GCP) based on the cloud provider."
+        )
     }
     try:
         incident = demisto.args().get("dspmIncident")
         incidentLink = demisto.args().get("incidentLink")
-        slackBlock = create_slack_block(incident, rule_names_list, incidentLink)
+        slackBlock = create_slack_block(incident, rule_names_dict, incidentLink)
 
         return_results(
             CommandResults(
@@ -167,10 +152,10 @@ def main():  # pragma: no cover
         )
 
     except Exception as excep:
-        return_error(f'Failed to execute CreateDSPMRiskSlackBlocks. Error: {str(excep)}')
+        return_error(f"Failed to execute CreateDSPMRiskSlackBlocks. Error: {str(excep)}")
 
 
-''' ENTRY POINT '''
+""" ENTRY POINT """
 
-if __name__ in ('__main__', '__builtin__', 'builtins'):  # pragma: no cover
+if __name__ in ("__main__", "__builtin__", "builtins"):  # pragma: no cover
     main()
