4a3018bbb90de437aec0d4d3e625bf482ccdaf68
rshunim <rshunim@paloaltonetworks.com>
2024-11-05 12:54:09 +0200

    fix UT

--- Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs_test.py
+++ Packs/CortexXDR/Integrations/XDR_iocs/XDR_iocs_test.py
@@ -1329,7 +1329,7 @@ def test_parse_xsoar_field_name_and_link(xsoar_comment_field: list[str], expecte
                            "should only contain the field name,"
                            " or the field name with the phrase indicator_link, separated by a comma."),
                           (["a", "b", "c"], ("The parameter xsoar_comment_field=['a', 'b', 'c'] cannot contain more than "
-                                             'two fields'))))
+                                             'two values'))))
 def test_parse_xsoar_field_name_and_link_exceptions(xsoar_comment_field: list[str], informative_message: str):
     """
     Given:
