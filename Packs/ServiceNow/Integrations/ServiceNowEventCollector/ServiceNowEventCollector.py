dfd2c8193c4d255b888163e1d45ee50991d1bffb
iapt@paloaltonetworks.com <iapt@paloaltonetworks.com>
2024-11-10 15:09:17 +0200

    limit fix

--- Packs/ServiceNow/Integrations/ServiceNowEventCollector/ServiceNowEventCollector.py
+++ Packs/ServiceNow/Integrations/ServiceNowEventCollector/ServiceNowEventCollector.py
@@ -291,8 +291,8 @@ def main() -> None:  # pragma: no cover
     credentials = params.get("credentials", {})
     user_name = credentials.get("identifier")
     password = credentials.get("password")
-    max_fetch_audit = arg_to_number(params.get("max_fetch")) or 1000
-    max_fetch_syslog = arg_to_number(params.get("max_fetch_syslog_transactions")) or 1000
+    max_fetch_audit = arg_to_number(params.get("max_fetch")) or 10000
+    max_fetch_syslog = arg_to_number(params.get("max_fetch_syslog_transactions")) or 10000
     event_types_to_fetch = argToList(params.get('event_types_to_fetch', ['Audit']))
     log_types = handle_log_types(event_types_to_fetch)
 
