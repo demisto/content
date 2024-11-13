bc936ee1de533997fd36b4b25d4eca3aa7a9d21f
Dan Tavori <dtavori@paloaltonetworks.com>
2024-11-13 20:48:42 +0200

    command/script prefixes

--- Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
+++ Packs/ApiModules/Scripts/DemistoClassApiModule/DemistoClassApiModule.py
@@ -12,6 +12,18 @@ if sys.version_info[0] >= 3:
             (E.g., self.results). """
 
         class DemistoScript(DemistoWrapper):
+            def info(self, msg):
+                if "script being called is" not in msg.lower():
+                    script = demisto.callingContext['context']['ScriptName']
+                    msg = "[{}] {}".format(script, msg)
+                    super(DemistoScript, self).info(msg)
+
+            def debug(self, msg):
+                if "script being called is" not in msg.lower():
+                    script = demisto.callingContext['context']['ScriptName']
+                    msg = "[{}] {}".format(script, msg)
+                    super(DemistoScript, self).debug(msg)
+
             def getFilePath(self, id):
                 res = super(DemistoScript, self).getFilePath(id)
                 self.debug("File path of entry with ID [{}] is [{}]".format(id, json.dumps(res)))
@@ -36,19 +48,26 @@ if sys.version_info[0] >= 3:
         class DemistoIntegration(DemistoWrapper):
             def info(self, msg):
                 if "command being called is" not in msg.lower():
+                    integration = demisto.callingContext['context']['IntegrationBrand']
+                    instance = demisto.callingContext['context']['IntegrationInstance']
+                    command = self.command()
+                    msg = "[{} - {}] [{}] {}".format(integration, instance, command, msg)
                     super(DemistoIntegration, self).info(msg)
 
             def debug(self, msg):
                 if "command being called is" not in msg.lower():
+                    integration = demisto.callingContext['context']['IntegrationBrand']
+                    instance = demisto.callingContext['context']['IntegrationInstance']
+                    msg = "[{} - {}] {}".format(integration, instance, msg)
                     super(DemistoIntegration, self).debug(msg)
 
             def getLastRun(self):
                 res = super(DemistoIntegration, self).getLastRun()
-                self.debug("[fetch-incidents] LastRun is: {}".format(json.dumps(res, indent=4)))
+                self.debug("LastRun is: {}".format(json.dumps(res, indent=4)))
                 return res
 
             def setLastRun(self, obj):
-                self.debug("[fetch-incidents] Setting last run to: {}".format(json.dumps(obj, indent=4)))
+                self.debug("Setting last run to: {}".format(json.dumps(obj, indent=4)))
                 super(DemistoIntegration, self).setLastRun(obj)
 
             def incidents(self, incidents):
@@ -57,16 +76,29 @@ if sys.version_info[0] >= 3:
                     for inc in incidents:
                         if "dbotMirrorId" in inc:
                             source_ids.append(inc["dbotMirrorId"])
-                    msg = "[fetch-incidents] Creating {} incidents".format(len(incidents))
+                    msg = "Creating {} incidents".format(len(incidents))
                     if source_ids:
                         msg += " with source IDs [{}]".format(", ".join(source_ids))
                     self.debug(msg)
                 super(DemistoIntegration, self).incidents(incidents)
 
+            def createIndicators(self, indicators_batch, noUpdate=False):
+                self.debug("Creating {} indicators".format(len(indicators_batch)))
+                super(DemistoIntegration, self).createIndicators(indicators_batch, noUpdate)
+
+        args_str = json.dumps(demisto.args())
         if demisto.callingContext.get('context', {}).get('IntegrationBrand'):
-            demisto.debug("Command being called is {}".format(demisto.command()))
+            integration = demisto.callingContext['context']['IntegrationBrand']
+            instance = demisto.callingContext['context']['IntegrationInstance']
+            demisto.debug(
+                "Command being called is [{}] ({} - {}), args: [{}]".format(
+                    demisto.command(), integration, instance, args_str
+                )
+            )
             demisto.__class__ = DemistoIntegration
         elif demisto.callingContext.get('context', {}).get('ScriptName'):
+            script = demisto.callingContext['context']['ScriptName']
+            demisto.debug("Script being called is [{}], args: [{}]".format(script, args_str))
             demisto.__class__ = DemistoScript
 
     except NameError:
