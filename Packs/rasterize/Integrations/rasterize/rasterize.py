ba5a5bb165b810ed9cd529a9669cbe59eea6c327
merit-maita <meretmaayta@gmail.com>
2024-12-01 11:19:05 +0200

    fixed docs

--- Packs/rasterize/Integrations/rasterize/rasterize.py
+++ Packs/rasterize/Integrations/rasterize/rasterize.py
@@ -1127,7 +1127,7 @@ def add_filename_suffix(file_names: list, file_extension: str):
 
 def rasterize_command():  # pragma: no cover
     urls = demisto.getArg('url')
-    # Do not remove this line
+    # Do not remove this line, as rasterize does not support array in `url`.
     urls = [urls] if isinstance(urls, str) else urls
     width, height = get_width_height(demisto.args())
     full_screen = argToBoolean(demisto.args().get('full_screen', False))
