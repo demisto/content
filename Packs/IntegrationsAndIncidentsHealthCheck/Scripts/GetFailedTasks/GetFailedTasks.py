ccf8e17657f975130b04082b0cdef155a4df49ac
Menachem <mwienfeld@paloaltonetworks.com>
2024-12-02 15:04:18 +0200

    Add unit tests

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks.py
@@ -40,7 +40,7 @@ def get_tenant_name():
     return tenant_name
 
 
-def get_failed_tasks_output(tasks: list, incident: dict, custom_scripts_map_id_and_name: dict[str, str]):
+def get_failed_tasks_output(tasks: list, incident: dict, custom_scripts_map_id_and_name: dict[str, str] = {}):
     """
         Converts the failing task objects of an incident to context outputs.
 
