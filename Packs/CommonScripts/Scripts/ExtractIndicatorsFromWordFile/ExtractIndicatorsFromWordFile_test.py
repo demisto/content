72790c0fb6669509d0f5ae3595e104aec7e1697c
adi88d <adaud@paloaltonetworks.com>
2024-12-02 18:24:09 +0200

    update test_parse_word

--- Packs/CommonScripts/Scripts/ExtractIndicatorsFromWordFile/ExtractIndicatorsFromWordFile_test.py
+++ Packs/CommonScripts/Scripts/ExtractIndicatorsFromWordFile/ExtractIndicatorsFromWordFile_test.py
@@ -11,17 +11,9 @@ expected_partial_all_data = 'Lorem ipsum dolor sit amet, an quas nostro posidoni
     ('docwithindicators.doc', 'test_data/docwithindicators'),
     ('docxwithindicators.docx', 'test_data/docxwithindicators')
 ])
-def test_parse_word(file_name, file_path, request):
+def test_parse_word(file_name, file_path):
     basename = os.path.basename(file_path)
     shutil.copy(file_path, os.getcwd())
-
-    def cleanup():
-        try:
-            os.remove(basename + ".docx")
-            os.remove(basename)
-        except OSError:
-            pass
-
 
     if os.getcwd().endswith('test_data'):
         os.chdir('..')
@@ -29,7 +21,10 @@ def test_parse_word(file_name, file_path, request):
     parser.get_file_details = lambda: None
     parser.file_name = file_name
     parser.file_path = basename
+
+    os.remove(basename)
     parser.parse_word()
+
     # assert (expected_partial_all_data in parser.paragraphs)
     assert (expected_partial_all_data in parser.all_data)
 
