fc2d23dd5f7fbf1927a894c24b264bec84d5cf0d
mayagoldman <mgoldman@paloaltonetworks.com>
2024-12-08 15:45:56 +0200

    add comment script

---
+++ Packs/Microsoft365Defender/Scripts/MS365DefenderAddComment/MS365DefenderAddComment.py
@@ -0,0 +1,52 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+
+
+def add_comment(args: Dict[str, Any]) -> CommandResults:
+    incident_id = args.get('id', 'none')
+    comment = args.get('comment')
+    using = args.get('instance_name')
+
+    command_args = {}
+
+    if incident_id == 'none':
+        incident_id = demisto.incident()['CustomFields'].get('microsoft365defenderid')
+
+    command_args['id'] = incident_id
+    command_args['comment'] = comment
+    command_args['using'] = using
+
+    try:
+        demisto.debug(f'Calling microsoft-365-defender-incident-update , {command_args=}')
+        command_res = demisto.executeCommand("microsoft-365-defender-incident-update", command_args)
+        demisto.debug(f'After calling microsoft-365-defender-incident-update, {command_res=}')
+        # resp = command_res[0]
+        # if isError(resp):
+        #     raise Exception(resp['Contents'])
+        # else:
+        #     if 'result' not in resp['Contents'] or not resp['Contents']['result']:
+        #         message = "Empty result. Please check your input. e.g. the ticket_id, or table_name"
+        #         demisto.info(message)
+        #         return_error(message)
+        #
+        #     result = resp['Contents']['result']
+        #     output_results = {}
+        #     md = tableToMarkdown("ServiceNow Comment Added", [output_results])
+
+    except Exception as ex1:
+        demisto.info(f"Failed to add comment to incident. {type(ex1)}: {ex1}, Trace:\n{traceback.format_exc()}")
+        return_error(str(ex1))
+    # return CommandResults(readable_output=md)
+
+
+def main():
+    try:
+        res = add_comment(demisto.args())
+        return_results(res)
+
+    except Exception as ex2:
+        return_error(f'Failed to execute MS365DefenderAddComment. Error: {str(ex2)}')
+
+
+if __name__ in ["__builtin__", "builtins", '__main__']:
+    main()
