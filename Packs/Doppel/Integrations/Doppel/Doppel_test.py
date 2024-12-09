641daa01a765b823a344182e9dd9dd5287f2df8a
Kapil Bisen <kapil@metronlabs.com>
2024-12-09 21:13:57 +0530

    Assert for the content of the results after test

--- Packs/Doppel/Integrations/Doppel/Doppel_test.py
+++ Packs/Doppel/Integrations/Doppel/Doppel_test.py
@@ -30,16 +30,18 @@ def test_command_success(mocker, requests_mock, api_path, command, args, api_res
     mocker.patch.object(demisto, 'params', return_value={"url": "https://api.doppel.com", "credentials": {"password": "<API-KEY>"}})
     mocker.patch.object(demisto, 'command', return_value=command)
     mocker.patch.object(demisto, 'args', return_value=args)
+    results_checker = mocker.patch.object(demisto, 'results', return_value=None)
     
     adapter = requests_mock.get(api_path, status_code=200, json=api_response)
     
-    # call the main function so that the command will be called
+    # Call the main function so that the command will be called
     main()
     
     assert adapter.call_count == 1
     assert adapter.called
     assert api_response == json.loads(requests.get(api_path).text)
     assert 200 == requests.get(api_path).status_code
-
+    assert sorted(results_checker.call_args.args[0].get('Contents')) == sorted(dict(api_response))
+         
     
     
