9acea938c4b6df54162b2c17dd59c7feaeb75d94
Kapil Bisen <kapil@metronlabs.com>
2024-12-10 14:48:15 +0530

    Unit tests for API failures and app exception

--- Packs/Doppel/Integrations/Doppel/Doppel_test.py
+++ Packs/Doppel/Integrations/Doppel/Doppel_test.py
@@ -10,6 +10,7 @@ import requests
 import demistomock as demisto
 import requests_mock
 
+from CommonServerPython import DemistoException
 from Doppel import Client, main
 
 
@@ -18,13 +19,12 @@ def util_load_json(path):
         return json.loads(f.read())
 
 
-get_alert_mock_response_200 = util_load_json('test_data/get-alert-command-200.json')
-@pytest.mark.parametrize("command, api_path, args, api_response",
+@pytest.mark.parametrize("command, args, api_path, api_response",
                          [
-                             ("get-alert", "https://api.doppel.com/v1/alert?id=TST-31222", {"id": "TST-31222"}, get_alert_mock_response_200)
+                             ("get-alert", {"id": "TST-31222"}, "https://api.doppel.com/v1/alert?id=TST-31222", util_load_json('test_data/get-alert-success-200.json'))
                          ]
                          )
-def test_command_success(mocker, requests_mock, command, api_path, args, api_response):
+def test_command_success(mocker, requests_mock, command, args, api_path, api_response):
     """Tests the current command
     """
     mocker.patch.object(demisto, 'params', return_value={"url": "https://api.doppel.com", "credentials": {"password": "<API-KEY>"}})
@@ -38,10 +38,60 @@ def test_command_success(mocker, requests_mock, command, api_path, args, api_res
     main()
     
     assert adapter.call_count == 1
-    assert adapter.called
     assert api_response == json.loads(requests.get(api_path).text)
     assert 200 == requests.get(api_path).status_code
     assert sorted(results_checker.call_args.args[0].get('Contents')) == sorted(dict(api_response))
+    
+
+@pytest.mark.parametrize("command, args, api_path, status_code, api_response",
+                         [
+                             ("get-alert", {"entity": "123"}, "https://api.doppel.com/v1/alert?entity=123", 400, util_load_json('test_data/get-alert-failure-400-invalid-entity.json')),
+                             ("get-alert", {"id": "1234"}, "https://api.doppel.com/v1/alert?id=1234", 400, util_load_json('test_data/get-alert-failure-400-invalid-alert-id.json')),
+                         ]
+                         )
+def test_command_failure(mocker, requests_mock, command, args, api_path, status_code, api_response):
+    """Tests the current command
+    """
+    mocker.patch.object(demisto, 'params', return_value={"url": "https://api.doppel.com", "credentials": {"password": "<API-KEY>"}})
+    mocker.patch.object(demisto, 'command', return_value=command)
+    mocker.patch.object(demisto, 'args', return_value=args)
+    mocker.patch.object(demisto, 'results', return_value=None)
+    
+    adapter = requests_mock.get(api_path, status_code=status_code, json=api_response)
+    
+    # Call the main function so that the command will be called
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        main()
+        
+    assert pytest_wrapped_e.type is SystemExit
+    assert pytest_wrapped_e.value.code == 0
+    assert adapter.call_count == 1
+    assert api_response == json.loads(requests.get(api_path).text)
+    assert status_code == requests.get(api_path).status_code
          
     
+@pytest.mark.parametrize("command, args, api_path, status_code, exception_message",
+                         [
+                             ("get-alert", {"id": "TST-31", "entity": "http://dummyrul.com"}, "https://api.doppel.com/v1/alert?id=TST-31&entity=http://dummyrul.com", 400, "Failed to execute get-alert command.\nError:\nBoth id and entity is specified. We need exactly single input for this command")
+                         ]
+                         )
+def test_command_exception(mocker, requests_mock, command, args, api_path, status_code, exception_message):
+    """Tests the current command
+    """
+    mocker.patch.object(demisto, 'params', return_value={"url": "https://api.doppel.com", "credentials": {"password": "<API-KEY>"}})
+    mocker.patch.object(demisto, 'command', return_value=command)
+    mocker.patch.object(demisto, 'args', return_value=args)
+    results_checker = mocker.patch.object(demisto, 'results', return_value=None)
+    
+    adapter = requests_mock.get(api_path, status_code=status_code, json=None)
+    
+    # Call the main function so that the command will be called
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        main()
+        
+    assert pytest_wrapped_e.type is SystemExit
+    assert pytest_wrapped_e.value.code == 0
+    # Notice that the API was not called, but the app itself has raised an exception before making the API call
+    assert adapter.call_count == 0
+    assert results_checker.call_args.args[0].get('Contents') == exception_message
     
