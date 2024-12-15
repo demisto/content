0a825b09949943e8b4be9a0877e01e3d8810039f
mayagoldman <mgoldman@paloaltonetworks.com>
2024-12-15 15:39:16 +0200

    master

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -218,7 +218,7 @@ except Exception:
         sys.exc_clear()
 
 CONTENT_RELEASE_VERSION = '0.0.0'
-CONTENT_BRANCH_NAME = 'CIAC-9576-wo-passing-data'
+CONTENT_BRANCH_NAME = 'master'
 IS_PY3 = sys.version_info[0] == 3
 PY_VER_MINOR = sys.version_info[1]
 STIX_PREFIX = "STIX "
