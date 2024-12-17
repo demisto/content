d0df843c09cd19a050c4194e02a98af153ffe662
Sasha <ssokolovich@paloaltonetworks.com>
2024-12-17 18:47:24 +0200

    fix validations

--- Packs/CrowdStrikeFalcon/Scripts/Cspresentparentprocess/Cspresentparentprocess.py
+++ Packs/CrowdStrikeFalcon/Scripts/Cspresentparentprocess/Cspresentparentprocess.py
@@ -1,25 +1,27 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 
+
 def convert_json_to_markdown_table(json_data):
-      # Check if the input is valid JSON
-      if not isinstance(json_data, dict):
-          raise ValueError("Input data is not a valid JSON object")
+    # Check if the input is valid JSON
+    if not isinstance(json_data, dict):
+        raise ValueError("Input data is not a valid JSON object")
 
-      markdown = "| ***Parent Process Information*** |  ***Value*** |\n"
-      markdown += "| --- | ----- |\n"
+    markdown = "| ***Parent Process Information*** |  ***Value*** |\n"
+    markdown += "| --- | ----- |\n"
 
-      # Loop through the JSON and format it into a table
-      for key, value in json_data.items():
-          # If value is a dictionary, we will format it as a JSON block
-          if isinstance(value, dict):
-              value = f"```json\n{json.dumps(value, indent=4)}\n```"
-          # If it's a list, we join the list into a string for display
-          elif isinstance(value, list):
-              value = ', '.join(map(str, value))
-          markdown += f"| {key} | {value} |\n"
+    # Loop through the JSON and format it into a table
+    for key, value in json_data.items():
+        # If value is a dictionary, we will format it as a JSON block
+        if isinstance(value, dict):
+            value = f"```json\n{json.dumps(value, indent=4)}\n```"
+        # If it's a list, we join the list into a string for display
+        elif isinstance(value, list):
+            value = ', '.join(map(str, value))
+        markdown += f"| {key} | {value} |\n"
 
-      return markdown
+    return markdown
+
 
 def main():
     # Fetch the dynamic context key input from the demisto args
@@ -27,25 +29,25 @@ def main():
     try:
         context_value = demisto.context().get(context_key, None).get('Detection', None)
         results = []
-        for idx, detection in enumerate(context_value):
+        for _idx, detection in enumerate(context_value):
             if isinstance(detection, dict) and 'parentprocess' in detection:
                 results.append(detection)
 
         if not results:
             return_results("### No policyactions data available.")
 
-
         results = results[0].get('parentprocess', None)
         # Set the markdown in the outputs
         # Convert the dictionary to markdown JSON format
 
-
-        MD = convert_json_to_markdown_table (results)
+        MD = convert_json_to_markdown_table(results)
         return_results({
-              'Type': entryTypes['note'],
-              'Contents': MD,
-              'ContentsFormat': formats['markdown']})
+            'Type': entryTypes['note'],
+            'Contents': MD,
+            'ContentsFormat': formats['markdown']})
     except:
         print("No Policy Acttions were found on CrowdStrike.Detection key")
+
+
 if __name__ in ('__main__', '__builtin__', 'builtins'):
     main()
