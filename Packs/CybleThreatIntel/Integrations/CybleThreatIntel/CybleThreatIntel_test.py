ba55c8d070a7fb4084b3e3c09a622f1d0fe97564
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2024-12-18 12:45:22 +0200

    Update Packs/CybleThreatIntel/Integrations/CybleThreatIntel/CybleThreatIntel_test.py

--- Packs/CybleThreatIntel/Integrations/CybleThreatIntel/CybleThreatIntel_test.py
+++ Packs/CybleThreatIntel/Integrations/CybleThreatIntel/CybleThreatIntel_test.py
@@ -70,6 +70,7 @@ def test_get_taxii(mocker):
     assert mock_response_3 == val[0]
 
 
+@pytest.mark.skip(reason="Skipping this test since we are no longer returning an error.")
 def test_get_taxii_failure(mocker):
     from CybleThreatIntel import Client
     client = Client(params)
