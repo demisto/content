34a1dd1be4ed10b57f1276a10969b4a471196911
jeremyaf <51758297+jeremyaf@users.noreply.github.com>
2024-12-20 10:59:53 +1100

    Update ToCase.py

--- Packs/CommunityCommonScripts/Scripts/ToCase/ToCase.py
+++ Packs/CommunityCommonScripts/Scripts/ToCase/ToCase.py
@@ -2,10 +2,13 @@ import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 value = demisto.args()['value']
 case = demisto.args().get('case', 'capital')
-if case == 'swap':
-    value = value.swapcase()
-elif case == 'capital':
-    value = value.capitalize()
-elif case == 'title':
-    value = value.title()
+
+match case:
+	case 'swap':
+		value = value.swapcase()
+	case 'capital':
+		value = value.capitalize()
+	case 'title':
+		value = value.title()
+
 return_results(value)
