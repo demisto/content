572cabf570e03e0002528c180cdfb182d2373f64
Kamal Qarain <kqarain@paloaltonetworks.com>
2024-12-22 17:23:27 +0200

    Add a unit test for fetch_events

--- Packs/1Password/Integrations/OnePasswordEventCollector/OnePasswordEventCollector_test.py
+++ Packs/1Password/Integrations/OnePasswordEventCollector/OnePasswordEventCollector_test.py
@@ -1,10 +1,12 @@
 import json
 import pytest
+from pytest_mock import MockerFixture
+from datetime import datetime
 from OnePasswordEventCollector import Client
 
 
 BASE_URL = 'http://example.com'
-HEADERS = {'Authorization': 'Bearer MY-TOKEN-123', 'Content-Type': 'application/json'},
+HEADERS = {'Authorization': 'Bearer MY-TOKEN-123', 'Content-Type': 'application/json'}
 
 
 def util_load_json(path: str):
@@ -19,6 +21,16 @@ def authenticated_client() -> Client:
 
 
 def test_get_limit_param():
+    """
+    Given:
+        - Integration instance Configuration params containing 'limit' values.
+
+    When:
+        - Calling get_limit_param.
+
+    Assert:
+        - Ensure the 'limit' value of the specified event type is as expected.
+    """
     from OnePasswordEventCollector import get_limit_param
 
     expected_audit_events_limit = '500'
@@ -55,6 +67,16 @@ def test_get_unauthorized_event_types():
 
 
 def test_add_fields_event():
+    """
+    Given:
+        - A raw 1Password event of type 'audit event'.
+
+    When:
+        - Calling add_fields_to_event.
+
+    Assert:
+        - Ensure the '_time' and 'event_type' fields are added and correctly set.
+    """
     from OnePasswordEventCollector import add_fields_to_event, arg_to_datetime, DATE_FORMAT
 
     event_timestamp = '2024-12-02T11:54:19.710457472Z'
@@ -102,3 +124,41 @@ def test_client_get_events_invalid_inputs(authenticated_client: Client, event_ty
     """
     with pytest.raises(ValueError, match=error_message):
         authenticated_client.get_events(event_type)
+
+
+def test_fetch_events(authenticated_client: Client, mocker: MockerFixture):
+    """
+    Given:
+        - A 1Password event type, first fetch date, and the maximum number of events per fetch.
+
+    When:
+        - Calling fetch_events (which calls get_events_from_client).
+
+    Assert:
+        - Ensure correct inputs to get_events_from_client.
+        - Ensure correct fetch_events outputs (last_run, events).
+    """
+    from OnePasswordEventCollector import fetch_events
+
+    # Inputs
+    event_type = 'sign in attempts'
+    first_fetch_date = datetime(2024, 12, 2, 11, 50)
+
+    # Expected outputs
+    expected_last_run = {'from_date': '2024-12-02T11:59:40.510481428Z', 'ids': ['5678']}
+    mock_response = util_load_json('test_data/signinattempts_response.json')
+    expected_events = mock_response['items']
+
+    get_events_from_client = mocker.patch('OnePasswordEventCollector.get_events_from_client', return_value=expected_events)
+    last_run, events = fetch_events(authenticated_client, event_type=event_type,
+                                    first_fetch_date=first_fetch_date, event_last_run={}, max_events=1000)
+
+    get_events_from_client_kwargs = get_events_from_client.call_args.kwargs
+
+    # Assert correct inputs
+    assert get_events_from_client_kwargs['event_type'] == event_type
+    assert get_events_from_client_kwargs['from_date'] == first_fetch_date
+
+    # Assert correct outputs
+    assert last_run == expected_last_run
+    assert events == expected_events
