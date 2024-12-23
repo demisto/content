d032467f591bd9d913a574a6a8da61708101a49c
Menachem <mwienfeld@paloaltonetworks.com>
2024-12-23 15:52:46 +0200

    Fix unit tests

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks_test.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks_test.py
@@ -26,7 +26,7 @@ def test_get_failed_tasks(mocker, rest_api_instacne):
     main()
 
     human_readable = demisto.results.call_args[0][0].get('HumanReadable')
-    assert '|Incident Created Date|Incident ID|Task Name|Task ID|Playbook Name|Command Name|Error Entry ID|' in \
+    assert '|Incident Created Date|Incident ID|Task Name|Task ID|Playbook Name|Command Name|Brand Name|Error Entry ID|' in \
            human_readable
     assert human_readable.count('AutoFocusPolling') == 3
 
@@ -35,6 +35,7 @@ def test_get_failed_tasks(mocker, rest_api_instacne):
     assert entry_context.get('NumberofFailedIncidents', [])[0].get('total of failed incidents') == 3
 
     assert entry_context.get('GetFailedTasks', [])[1] == {'Command Name': '',
+                                                          'Brand Name': None,
                                                           'Error Entry ID': ['8@3', '9@3'],
                                                           'Incident Created Date': '2020-09-29T14:02:45.82647067Z',
                                                           'Incident ID': '3',
