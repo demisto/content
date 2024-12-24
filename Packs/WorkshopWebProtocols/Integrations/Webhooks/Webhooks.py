4652995be084133aac1444df6ceee07609db1306
MLainer1 <mlainer@paloaltonetworks.com>
2024-12-24 15:32:57 +0200

    endpoints

--- Packs/WorkshopWebProtocols/Integrations/Webhooks/Webhooks.py
+++ Packs/WorkshopWebProtocols/Integrations/Webhooks/Webhooks.py
@@ -22,7 +22,12 @@ from tempfile import NamedTemporaryFile
 app = FastAPI()
 # Disable insecure warnings
 urllib3.disable_warnings()
-
+JSON_STRUCTURE = {
+    "event": "answer",
+    "payload": {
+        "answer": "your answer"
+    }
+}
 
 ''' CONSTANTS '''
 
@@ -90,6 +95,23 @@ async def handle_listen_error(error: str):
     demisto.updateModuleHealth(error)
 
 ''' HELPER FUNCTIONS '''
+
+def validate_json_structure(json_data):
+    # Check the overall structure and specific 'event' and 'payload->answer' existence
+    if json_data.get('event') == 'answer' and isinstance(json_data.get('payload'), dict) and 'answer' in json_data['payload']:
+        return True
+    return False
+
+@app.options('/')
+async def handle_options_response():
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    return Response(status_code=status.HTTP_200_OK, content=f'Welcome to the riddle game!\n\n1. To connect and send your answers, use the first endpoint: "riddle_1".\n2. To answer the questions, use the following body structure: \n{JSON_STRUCTURE}\n3. Use the GET method to request the riddles, and the POST method to send your answer.')
+
 @app.get('/')
 async def handle_get_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
                                token: APIKey = Depends(token_auth)):
@@ -99,24 +121,7 @@ async def handle_get_response(request: Request, credentials: HTTPBasicCredential
     Returns:
         JSONResponse:response to zoom
     """
-    # request = await request.json()
-    # demisto.debug(f"WH: Got request; {request}")
-    # credentials_param = demisto.params().get('credentials')
-    # auth_failed = False
-    # v_token = demisto.params().get('verification_token', {}).get('password')
-    # if not str(token).startswith('Basic') and v_token:
-    #     if token != v_token:
-    #         auth_failed = True
-
-    # elif credentials and credentials_param and (username := credentials_param.get('identifier')):
-    #     password = credentials_param.get('password', '')
-    #     if not compare_digest(credentials.username, username) or not compare_digest(credentials.password, password):
-    #         auth_failed = True
-    # if auth_failed:
-    #     demisto.debug('Authorization failed')
-    #     return Response(status_code=status.HTTP_401_UNAUTHORIZED, content='Authorization failed. Try another route')
-    
-    return Response(status_code=status.HTTP_200_OK, content='Great that you conected, ')
+    return Response(status_code=status.HTTP_200_OK, content='Great that you connected, start with the "riddle_1" endpoint')
 
 @app.post('/')
 async def handle_post_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
@@ -129,24 +134,11 @@ async def handle_post_response(request: Request, credentials: HTTPBasicCredentia
     """
     request = await request.json()
     demisto.debug(f"WH: Got request; {request}")
-    # credentials_param = demisto.params().get('credentials')
-    # auth_failed = False
-    # v_token = demisto.params().get('verification_token', {}).get('password')
-    # if not str(token).startswith('Basic') and v_token:
-    #     if token != v_token:
-    #         auth_failed = True
-
-    # elif credentials and credentials_param and (username := credentials_param.get('identifier')):
-    #     password = credentials_param.get('password', '')
-    #     if not compare_digest(credentials.username, username) or not compare_digest(credentials.password, password):
-    #         auth_failed = True
-    # if auth_failed:
-    #     demisto.debug('Authorization failed')
-    #     return Response(status_code=status.HTTP_401_UNAUTHORIZED, content='Authorization failed.')
-
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
     event_type = request['event']
     payload = request['payload']
-    
+
     try:
         if payload:
             return Response(status_code=status.HTTP_200_OK, content=f'Yayy, got {event_type=} and {payload=}')
@@ -155,6 +147,260 @@ async def handle_post_response(request: Request, credentials: HTTPBasicCredentia
     except Exception as e:
         await handle_listen_error(f'An error occurred while handling a response: {e}')
 
+@app.get('/riddle_1')
+async def handle_post_part1_response():
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    return Response(status_code=status.HTTP_200_OK, content='Now answer the riddle:\n' \
+                    'I’m tall when I’m young, and I’m short when I’m old. What am I?')
+
+
+@app.post('/riddle_1')
+async def handle_get_part1_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    request = await request.json()
+    demisto.debug(f"WH: Got request; {request}")
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
+    event_type = request['event']
+    payload = request['payload']
+    answer = payload["answer"]
+    try:
+        if answer in ["candle", "Candle", "A candle", "a candle"]:
+            return Response(status_code=status.HTTP_200_OK, content='Correct! continue to the next riddle in the endpoint: riddle_2')
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND, content='Incorrect! try again')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.get('/riddle_2')
+async def handle_get_riddle_2_response():
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+
+    try:
+        return Response(status_code=status.HTTP_200_OK, content='Now answer the riddle:\n' \
+                        'What is always in front of you but can’t be seen?')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.post('/riddle_2')
+async def handle_post_riddle_2_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    request = await request.json()
+    demisto.debug(f"WH: Got request; {request}")
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
+    event_type = request['event']
+    payload = request['payload']
+    answer = payload["answer"]
+    try:
+        if answer in ["future", "Future", "The future", "the future"]:
+            return Response(status_code=status.HTTP_200_OK, content='Correct! continue to the next riddle in the endpoint: arad_3')
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND, content='Incorrect! try again')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.get('/arad_3')
+async def handle_get_arad_3_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+   
+    try:
+        return Response(status_code=status.HTTP_200_OK, content='Now answer the riddle:\n' \
+                        'What gets wet while drying?')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.post('/arad_3')
+async def handle_post_arad_3_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    request = await request.json()
+    demisto.debug(f"WH: Got request; {request}")
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
+    event_type = request['event']
+    payload = request['payload']
+    answer = payload["answer"]
+    try:
+        if answer in ["towel", "Towel", "a towel", "A towel"]:
+            return Response(status_code=status.HTTP_200_OK, content='Correct! continue to the next riddle in the endpoint: riddle_4')
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND, content='Incorrect! try again')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.get('/riddle_4')
+async def handle_get_riddle_4_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    try:
+        return Response(status_code=status.HTTP_200_OK, content='Now answer the riddle:\n' \
+                        'What can fill a room but takes up no space?')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.post('/riddle_4')
+async def handle_post_riddle_4_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    request = await request.json()
+    demisto.debug(f"WH: Got request; {request}")
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
+    event_type = request['event']
+    payload = request['payload']
+    answer = payload["answer"]
+    try:
+        if answer in ["Light", "light"]:
+            return Response(status_code=status.HTTP_200_OK, content='Correct! continue to the next riddle in the endpoint: riddle_5')
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND, content='Incorrect! try again')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+@app.get('/riddle_5')
+async def handle_get_riddle_5_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    try:
+        return Response(status_code=status.HTTP_200_OK, content='Now answer the riddle:\n' \
+                        'I am always hungry and will die if not fed, but whatever I touch will soon turn red. What am I?')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.post('/riddle_5')
+async def handle_post_riddle_5_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    request = await request.json()
+    demisto.debug(f"WH: Got request; {request}")
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
+    event_type = request['event']
+    payload = request['payload']
+    answer = payload["answer"]
+    try:
+        if answer in ["Fire", "fire", "a fire", "A fire"]:
+            return Response(status_code=status.HTTP_200_OK, content='Correct! continue to the next riddle in the endpoint: riddle_6')
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND, content='Incorrect! try again')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+@app.get('/riddle_6')
+async def handle_get_riddle_6_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    try:
+        return Response(status_code=status.HTTP_200_OK, content='Now answer the riddle:\n' \
+                        'Which fish costs the most?')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.post('/riddle_6')
+async def handle_post_riddle_6_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    request = await request.json()
+    demisto.debug(f"WH: Got request; {request}")
+    if not validate_json_structure(request):
+        return Response(status_code=status.HTTP_400_BAD_REQUEST, content='Received incorrect body.')
+    event_type = request['event']
+    payload = request['payload']
+    answer = payload["answer"]
+    try:
+        if answer in ["goldfish", "Goldfish", "a goldfish", "A goldfish"]:
+            return Response(status_code=status.HTTP_200_OK, content='Correct! continue to the final endpoint: final')
+        else:
+            return Response(status_code=status.HTTP_404_NOT_FOUND, content='Incorrect! try again')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
+
+
+@app.get('/final')
+async def handle_get_riddle_6_response(request: Request, credentials: HTTPBasicCredentials = Depends(basic_auth),
+                               token: APIKey = Depends(token_auth)):
+    """handle any response that came from Zoom app
+    Args:
+        request : zoom request
+    Returns:
+        JSONResponse:response to zoom
+    """
+    try:
+        return Response(status_code=status.HTTP_200_OK, content='Congrats! you finished the game, now say the magic word to win a price!\nThe magic word is: Hannuka')
+    except Exception as e:
+        await handle_listen_error(f'An error occurred while handling a response: {e}')
 ''' COMMAND FUNCTIONS '''
 
 
