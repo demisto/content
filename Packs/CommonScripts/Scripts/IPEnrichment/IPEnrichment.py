ee2ea397985a312ca1bf01639dc59b1390b00b7d
mayagoldman <mgoldman@paloaltonetworks.com>
2024-12-24 16:14:25 +0200

    implemnted up tp flows

--- Packs/CommonScripts/Scripts/IPEnrichment/IPEnrichment.py
+++ Packs/CommonScripts/Scripts/IPEnrichment/IPEnrichment.py
@@ -1,36 +1,92 @@
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
+# import demistomock as demisto  # noqa: F401
+# from CommonServerPython import *  # noqa: F401
 import socket
+from ipaddress import ip_network, ip_address
+
+# values taken from IsIPInRanges script
+PRIVATE_SUBNETS = [
+    '172.16.0.0/12',
+    '10.0.0.0/8',
+    '198.18.0.0/15',
+    '192.168.0.0/16',
+    '100.64.0.0/10',
+    '127.0.0.0/8',
+    '169.254.0.0/16',
+    '192.0.0.0/24',
+    '0.0.0.0/8',
+    '224.0.0.0/4',
+    '240.0.0.0/4',
+    '255.255.255.255/32'
+]
 
 
-def search_indicator(ip: str):
-    # search for indicators
-    indicators = demisto.executeCommand("findIndicators", {"query": ip, "size": 1})[0]["Contents"]  #todo: is size mandatory?
-
+def get_tim_indicator_hr(indicator: dict) -> str:
     # return specific information for found indicators
-    filtered_indicators = []
-    fields = ['id', 'indicator_type', 'value', 'score']
-    if args.get("add_fields_to_context"):
-        fields = fields + args.get("add_fields_to_context").split(",")
-        fields = [x.strip() for x in fields]  # clear out whitespace
-    for indicator in indicators:
-        style_indicator = {}
-        for field in fields:
-            style_indicator[field] = indicator.get(field, indicator.get("CustomFields", {}).get(field, "n/a"))
-        style_indicator["verdict"] = scoreToReputation(style_indicator['score'])
-        filtered_indicators.append(style_indicator)
+    # todo - handle a case of an empty indicator
+    fields = ['id', 'indicator_type', 'value',
+              'score']  # todo: which fields to return? maybe another command should be used per Yarden
 
-    # headers = fields + ["verdict"]
-    # markdown = tableToMarkdown("Indicators Found", filtered_indicators, headers)
-    return filtered_indicators
+    styled_indicator = {}
+    for field in fields:
+        styled_indicator[field] = indicator.get(field, indicator.get("CustomFields", {}).get(field, "n/a"))
+    styled_indicator["verdict"] = scoreToReputation(styled_indicator['score'])
+
+    headers = fields + ["verdict"]
+    hr = tableToMarkdown("IP Enrichment- indicator data from TIM", styled_indicator, headers)
+    return hr
+
+
+def get_private_ips() -> list:
+    private_ips_list = demisto.executeCommand("getList", {"listName": "PrivateIPs"})[0]["Contents"]
+    private_ips = re.findall(r"(\b(?:\d{1,3}\.){3}\d{1,3}\b/\d{1,2})", private_ips_list)
+    return privte_ips if private_ips else PRIVATE_SUBNETS
+
+
+def is_ip_internal(ip: str):
+    def is_ip_in_subnet(ip, subnet):
+        try:
+            return ip_address(ip) in ip_network(subnet, strict=False)
+        except ValueError:
+            return False
+
+    ip_ranges = get_private_ips()
+    return any(is_ip_in_subnet(ip, re.sub(r'\\s', '', iprange)) for iprange in ip_ranges)
+
+
+def internal_ip_flow():
+    # get-endpoint-data
+    print("internal_ip_flow")
+
+
+def external_ip_flow():
+    # Check prevalence only in xsiam
+    # IP
+
+    print("external_ip_flow")
 
 
 def ip_enrichment(ip: str, third_enrichment: bool, verbose: bool) -> CommandResults:
-    # Check if the indicator exist in TIM
-    #Internal \ external
-    #Check prevalence
-    #IP
-    #get-endpoint-data
+    # Check if the indicator exist in TIM - searchIndicator script functionality
+    tim_indicator = demisto.executeCommand("findIndicators", {"query": ip, "size": 1})[0][
+        "Contents"][0]  #todo: always one entity for ip right?
+    demisto.debug(f"IP Enrichment - tim indicator tim_indicator")
+    if verbose:
+        tim_indicator_hr = get_tim_indicator_hr(tim_indicator)
+    if not third_enrichment:
+        return CommandResults(
+            outputs=tim_indicator,
+            # outputs_prefix='Endpoint',
+            # outputs_key_field='Hostname',
+            # readable_output=
+        )
+
+    # Internal \ external
+    if is_ip_internal(ip):
+        internal_ip_flow()
+    else:
+        external_ip_flow()
 
     output = None
     md = tableToMarkdown('IP Enrichment', [output])
@@ -39,16 +95,17 @@ def ip_enrichment(ip: str, third_enrichment: bool, verbose: bool) -> CommandResu
         outputs=output,
         # outputs_prefix='Endpoint',
         # outputs_key_field='Hostname',
-        readable_output=md,
+        # readable_output=md,
     )
 
 
 def main():
     try:
-        ip = demisto.args().get('ip', '')
-        third_enrichment = demisto.args().get('3rd_enrichment', False)
-        verbose = demisto.args().get('verbose', False)
-        return_results(ip_enrichment(ip, third_enrichment, verbose))
+        ips = argToList(demisto.args().get('ip', ''))
+        third_enrichment = argToBoolean(demisto.args().get('third_enrichment', False))
+        verbose = argToBoolean(demisto.args().get('verbose', False))
+        print(ips, third_enrichment, verbose)
+        return_results([ip_enrichment(ip, third_enrichment, verbose) for ip in ips])
     except Exception as e:
         demisto.error(traceback.format_exc())
         return_error(f'IP Enrichment failed. Error information: {str(e)}')
