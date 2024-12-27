e1c73472f7b759e22ac80db63410487eef6b2c69
yedidyacohenpalo <yecohen@paloaltonetworks.com>
2024-12-27 14:18:06 +0200

    updated to use page param

--- Packs/CloudflareZeroTrust/Integrations/CloudflareZeroTrustEventCollector/CloudflareZeroTrustEventCollector.py
+++ Packs/CloudflareZeroTrust/Integrations/CloudflareZeroTrustEventCollector/CloudflareZeroTrustEventCollector.py
@@ -40,52 +40,25 @@ class Client(BaseClient):
         self.account_id = account_id
         self.headers = headers
 
-    def get_events(self, start_date):
+    def get_events(self, start_date, page_size, page, event_type):
+        endpoint_urls = {
+            ACCOUNT_AUDIT_TYPE: f"/client/v4/accounts/{self.account_id}/audit_logs",
+            USER_AUDIT_TYPE: "/client/v4/user/audit_logs",
+            ACCESS_AUTHENTICATION_TYPE: f"/client/v4/accounts/{self.account_id}/access/logs/access_requests"
+        }
         params = {
-            'per_page': ACCOUNT_AUDIT_PAGE_SIZE,
+            'per_page': page_size,
+            'page': page,
             'since': start_date,
             'direction': 'asc'
-            # 'before': ''
         }
         return self._http_request(
             method="GET",
-            url_suffix=f"/client/v4/accounts/{self.account_id}/audit_logs",
+            url_suffix=endpoint_urls[event_type],
             headers=self.headers,
             params=params,
             retries=3
         )
-
-
-#     def search_events(self, prev_id: int, alert_status: None | str, limit: int, from_date: str | None = None, default_Protocol: str = 'UDP') -> List[Dict]:  # noqa: E501
-#         """
-#         Searches for HelloWorld alerts using the '/get_alerts' API endpoint.
-#         All the parameters are passed directly to the API as HTTP POST parameters in the request
-
-#         Args:
-#             prev_id: previous id that was fetched.
-#             alert_status:
-#             limit: limit.
-#             from_date: get events from from_date.
-
-#         Returns:
-#             List[Dict]: the next event
-#         """
-#         # use limit & from date arguments to query the API
-#         return [{
-#             'id': prev_id + 1,
-#             'created_time': datetime.now().isoformat(),
-#             'description': f'This is test description {prev_id + 1}',
-#             'alert_status': alert_status,
-#             'protocol': default_Protocol,
-#             't_port': prev_id + 1,
-#             'custom_details': {
-#                 'triggered_by_name': f'Name for id: {prev_id + 1}',
-#                 'triggered_by_uuid': str(uuid.uuid4()),
-#                 'type': 'customType',
-#                 'requested_limit': limit,
-#                 'requested_From_date': from_date
-#             }
-#         }]
 
 
 def test_module(client: Client) -> str:
@@ -103,7 +76,6 @@ def test_module(client: Client) -> str:
     Returns:
         str: 'ok' if test passed, anything else will raise an exception and will fail the test.
     """
-
     try:
         fetch_events(
             client=client,
@@ -123,21 +95,31 @@ def test_module(client: Client) -> str:
     return 'ok'
 
 
-def get_events(client: Client, last_run, max_fetch, event_type):
-    events: list[dict] = []
-    last_fetched_events = last_run.get("events_ids", [])
+def get_events(client: Client, last_run, max_fetch, max_page_size, event_type):
+    events: List[Dict[str, Any]] = []
     start_date = calculate_fetch_dates(last_run)
 
-    while len(events) < max_fetch:
-        response = client.get_events(start_date)
-        filtered_events = handle_duplicates(response, last_fetched_events)
-        if not filtered_events:
-            break
-        events.extend(filtered_events)
-        events = events[:max_fetch]
-        start_date, last_fetched_events = prepare_next_run(events)
+    previous_event_ids = last_run.get("events_ids", [])
+    events_to_fetch = max_fetch + len(previous_event_ids)
 
-    new_last_run = {"last_fetch": start_date, "events_ids": last_fetched_events}
+    page_size = min(events_to_fetch, max_page_size)
+    page = 1
+
+    while len(events) < events_to_fetch:
+        response = client.get_events(start_date, page_size, page, event_type)
+        result = response.get("result", [])
+        events.extend(result)
+        if len(result) < page_size:
+            break
+        page += 1
+
+    unique_events = handle_duplicates(events, previous_event_ids)
+    events = unique_events[:max_fetch]
+
+    if events:
+        start_date, previous_event_ids = prepare_next_run(events)
+
+    new_last_run = {"last_fetch": start_date, "events_ids": previous_event_ids}
     return events, new_last_run
 
 
@@ -147,21 +129,42 @@ def fetch_events(client: Client, last_run: dict[str, Any], max_fetch_account_aud
     next_run = {}
 
     event_type_params = {
-        ACCOUNT_AUDIT_TYPE: {"client": client, "last_run": last_run.get(ACCOUNT_AUDIT_TYPE, {}),
-                             "max_fetch": max_fetch_account_audit, "event_type": ACCOUNT_AUDIT_TYPE},
-        USER_AUDIT_TYPE: {"client": client, "last_run": last_run.get(USER_AUDIT_TYPE, {}),
-                          "max_fetch": max_fetch_user_audit, "event_type": USER_AUDIT_TYPE},
-        ACCESS_AUTHENTICATION_TYPE: {"client": client, "last_run": last_run.get(ACCESS_AUTHENTICATION_TYPE, {}),
-                                     "max_fetch": max_fetch_authentication, "event_type": ACCESS_AUTHENTICATION_TYPE},
+        ACCOUNT_AUDIT_TYPE: {
+            "last_run": last_run.get(ACCOUNT_AUDIT_TYPE, {}),
+            "max_fetch": max_fetch_account_audit,
+            "event_type": ACCOUNT_AUDIT_TYPE,
+            "max_page_size": ACCOUNT_AUDIT_PAGE_SIZE,
+        },
+        USER_AUDIT_TYPE: {
+            "last_run": last_run.get(USER_AUDIT_TYPE, {}),
+            "max_fetch": max_fetch_user_audit,
+            "event_type": USER_AUDIT_TYPE,
+            "max_page_size": USER_AUDIT_PAGE_SIZE,
+        },
+        ACCESS_AUTHENTICATION_TYPE: {
+            "last_run": last_run.get(ACCESS_AUTHENTICATION_TYPE, {}),
+            "max_fetch": max_fetch_authentication,
+            "event_type": ACCESS_AUTHENTICATION_TYPE,
+            "max_page_size": ACCESS_AUTHENTICATION_PAGE_SIZE,
+        },
     }
 
     for event_type in event_types_to_fetch:
-        # if event_type in event_type_params:
-        fetched_events, updated_last_run = get_events(**event_type_params[event_type])
+        fetched_events, updated_last_run = get_events(client=client, **event_type_params[event_type])
         next_run[event_type] = updated_last_run
         events.extend(fetched_events)
 
     return next_run, events
+
+
+# def fetch_events_command(client, limit, start_date, end_date' event_to_fetch):
+#     for event in events_to_fetch:
+#         res = get_events(
+#             client=client,
+#             last_run={},
+#             max_fetch=limit,
+#             event_type=
+#         )
 
 
 def calculate_fetch_dates(next_run):
@@ -185,23 +188,26 @@ def calculate_fetch_dates(next_run):
             - The end date as a string in the format '%Y-%m-%dT%H:%M:%SZ'.
     """
     now_utc_time = get_current_time()
+    # start_date = next_run.get('last_fetch') or "2024-09-16T10:08:58Z"
     start_date = next_run.get('last_fetch') or (
         (now_utc_time - timedelta(minutes=1)).strftime(DATE_FORMAT))
     return start_date
 
 
 def prepare_next_run(events):
-    latest_time = events[-1].get('when') or events[-1].get('created_at')
+    latest_time = events[-1].get('when') or events[-1].get('created_at', "")
+    latest_time_rounded_seconds = latest_time[:19]  # Extract up to seconds (e.g., 2024-12-22T15:28:00)
 
-    # Collect IDs of all events with the latest time
-    latest_ids = [event['id'] for event in events if event.get('when') or event.get('created_at') == latest_time]
+    latest_ids = [
+        event['id']
+        for event in events
+        if (event.get('when') or event.get('created_at', ""))[:19] == latest_time_rounded_seconds
+    ]
+    return latest_time_rounded_seconds, latest_ids
 
-    return latest_time, latest_ids
-    # return {"last_fetch": latest_time, "events_ids": latest_ids}
 
-
-def handle_duplicates(response, last_fetched_events):
-    return [event for event in response.get('result') if event.get('id') not in last_fetched_events]
+def handle_duplicates(events, previous_event_ids):
+    return [event for event in events if event.get('id') not in previous_event_ids]
 
 
 def add_time_to_events(events: List[Dict] | None):
@@ -226,7 +232,6 @@ def main() -> None:  # pragma: no cover
     main function, parses params and runs command functions
     """
     params = demisto.params()
-    demisto.args()
     command = demisto.command()
     max_fetch_account_audit = arg_to_number(params.get('max_fetch_account_audit_logs') or DEFAULT_MAX_FETCH_ACCOUNT_AUDIT)
     max_fetch_user_audit = arg_to_number(params.get('max_fetch_user_audit_logs') or DEFAULT_MAX_FETCH_USER_AUDIT)
@@ -251,21 +256,16 @@ def main() -> None:  # pragma: no cover
         )
 
         if command == 'test-module':
-            # This is the call made when pressing the integration Test button.
             result = test_module(client)
             return_results(result)
 
-        # elif command == 'hello-world-get-events':
+        # elif command == 'cloudflare-zero-trust-get-events':
         #     should_push_events = argToBoolean(args.pop('should_push_events'))
         #     events, results = get_events(client, demisto.args())
         #     return_results(results)
         #     if should_push_events:
         #         add_time_to_events(events)
-        #         send_events_to_xsiam(
-        #             events,
-        #             vendor=VENDOR,
-        #             product=PRODUCT
-        #         )
+        #         send_events_to_xsiam(events, vendor=VENDOR, product=PRODUCT)
 
         elif command == 'fetch-events':
             last_run = demisto.getLastRun()
