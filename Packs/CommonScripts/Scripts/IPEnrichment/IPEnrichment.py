dbcf7807be7a496d48cdced8d91114585ad6ac69
mayagoldman <mgoldman@paloaltonetworks.com>
2024-12-30 18:06:28 +0200

    human readable works

--- Packs/CommonScripts/Scripts/IPEnrichment/IPEnrichment.py
+++ Packs/CommonScripts/Scripts/IPEnrichment/IPEnrichment.py
@@ -40,10 +40,8 @@ def get_tim_indicator_hr(indicator: dict) -> str:
 class Command:
     def __init__(
         self,
-        brand: Optional[str],
         name: str,
         output_keys: List[str],
-        args_mapping: dict,
         output_mapping: dict | Callable,
         post_processing: Callable = None,
     ):
@@ -55,54 +53,106 @@ class Command:
             name (str): The name of the command.
             args_mapping (dict): A dictionary containing the command arguments
         """
-        self.brand = brand
         self.name = name
         self.output_keys = output_keys
-        self.args_mapping = args_mapping
         self.output_mapping = output_mapping
         self.post_processing = post_processing
 
     def __repr__(self):
-        return f'{{ name: {self.name}, brand: {self.brand} }}'
+        return f'{{ name: {self.name} }}'
 
 
-class ModuleManager:
-    def __init__(self, modules: dict[str, Any]) -> None:
-        """Initialize ModuleManager."""
-        self.modules = modules
-        self.enabled_brands = {
-            module.get("brand")
-            for module in self.modules.values()
-            if module.get("state") == "active"
-        }
-
-    def is_brand_available(self, brand: str) -> bool:
-        """Check if a brand is active and available."""
-        return (brand in self.enabled_brands) or (brand is None)
-
-
-def prepare_args(command: Command, args: dict[str, Any]) -> dict[str, Any]:
+def hr_to_command_results(command_name: str, args: dict[str, Any], human_readable: str, is_error: bool = False
+                          ) -> CommandResults | None:
     """
-    Prepares the arguments dictionary for the command.
-    If the argument value is an empty string or None, the resulting dictionary will not include
-    the argument.
+    Prepares human-readable output for a command execution.
+
+    This function creates a formatted message containing the command details and its output.
+    It can handle both successful executions and errors.
 
     Args:
-        command (Command): The command to prepare for.
-        args (dict[str, Any]): The arguments received by this command.
-    Returns:
-        dict[str, Any]: The arguments dictionary that's right for the command.
-    """
-    command_args = {}
-    for command_arg_key, arg_key in command.args_mapping.items():
-        if command_arg_value := args.get(arg_key):
-            command_args[command_arg_key] = command_arg_value
+        command_name (str): The name of the executed command.
+        args (dict[str, Any]): A dictionary of command arguments and their values.
+        human_readable (str): The human-readable output of the command.
+        is_error (bool, optional): Flag indicating if the result is an error. Defaults to False.
 
-    return command_args
+    Returns:
+        CommandResults: A list containing a CommandResults object with the formatted output.
+    """
+    result = None
+    if human_readable:
+        command = f'!{command_name} {" ".join([f"{arg}={value}" for arg, value in args.items() if value])}'
+        result_type = EntryType.ERROR if is_error else None
+        result_message = f"#### {'Error' if is_error else 'Result'} for {command}\n{human_readable}"
+        result = CommandResults(readable_output=result_message, entry_type=result_type, mark_as_note=True)
+    return result
+
+
+def run_execute_command(command: Command, args: dict[str, Any]) -> list[dict[str, Any]]:
+    """
+    Executes a command if the brand is available and returns the results.
+
+    Args:
+        command (Command): An instance of MappedCommand that contains the command information.
+        args (Dict[str, Any]): A dictionary containing the specific arguments for the command.
+    Returns:
+        Dict[str, Any]: A dictionary containing the command and its results.
+    """
+
+    return demisto.executeCommand(command.name, args)
+
+
+def get_command_results(command: Command, results: list[dict[str, Any]], args: dict[str, Any], verbose) -> tuple[
+    list, list, list, list]:
+    """
+    Processes the results of a previously executed command and extracts relevant outputs.
+
+    Args:
+        command (Command): An instance of MappedCommand that contains the command information.
+        results (Dict[str, Any]): A dictionary containing the command results.
+        args (Dict[str, Any]): A dictionary containing the specific arguments for the command.
+
+    Returns:
+        Tuple[List[Dict[str, Any]], str, List[Dict[str, Any]]]:
+            A tuple containing:
+            - A list of command context outputs.
+            - A human-readable string of the results.
+            - A list of command error outputs.
+    """
+
+    command_error_outputs = []
+    command_context_outputs = []
+    command_contents = []
+    command_human_readable_outputs = []
+    demisto.debug(f'get_commands_outputs for command "{command}" with {len(results)} entry results')
+    for entry in results:
+        if is_error(entry):
+            print("ERROR")
+            command_error_outputs.append(hr_to_command_results(command.name, args, get_error(entry), is_error=True))
+        if entry.get("EntryContext"):
+            command_context_outputs.append(entry.get("EntryContext", {}))
+        else:
+            contents = entry.get("Contents")
+            if isinstance(contents, list):
+                command_contents.extend(contents)
+            else:
+                command_contents.append(contents)
+
+        if verbose:
+            command_human_readable_outputs.append(entry.get("HumanReadable") or "")
+    return command_context_outputs, command_contents, command_human_readable_outputs, command_error_outputs
+
+    # else:
+    #     command_context_outputs.append(entry.get("EntryContext", {}))
+    #     human_readable_outputs.append(entry.get("HumanReadable") or "")
+
+    # human_readable = "\n".join(human_readable_outputs)
+    # human_readable = [hr] if (hr := hr_to_command_results(command, args, human_readable)) else []
+    # return command_context_outputs, human_readable, command_error_outputs
 
 
 class IPCommandRunner:
-    def __init__(self, module_manager: ModuleManager, verbose: bool) -> None:
+    def __init__(self, verbose: bool) -> None:
         """
         Initializes the instance of EndpointCommandRunner.
 
@@ -112,133 +162,46 @@ class IPCommandRunner:
         Attributes:
             module_manager (ModuleManager): Stores the provided ModuleManager instance.
         """
-        self.module_manager = module_manager
-        self.ip_outputs_list: list[dict[str, Any]] = []
-        self.command_results_list: list[CommandResults] = []
+
+        self.enabled_brands = {
+            module.get("brand")
+            for module in demisto.getModules().values()
+            if module.get("state") == "active"
+        }
+        self.commands_context_outputs: list[dict[str, Any]] = []
+        self.commands_results_list: list[CommandResults] = []
         self.ips_not_found_list: list[dict] = []
         self.verbose = verbose
 
-    def run_command(self, command: Command, endpoint_args: dict[str, list[str] | str]) -> tuple[
-        list[CommandResults],
-        list[dict[str, dict]]
-    ]:
+    def is_brand_available(self, brand: str) -> bool:
+        """Check if a brand is active and available."""
+        return brand in self.enabled_brands
+
+    def run_command(self, command: Command, args: dict[str, list[str] | str]) -> tuple[list[CommandResults],list[dict[str, dict]]]:
         """
             Runs the given command with the provided arguments and returns the results.
             Args:
                 command (Command): An instance of the Command class containing the command details.
-                endpoint_args (dict[str, list[str] | str]): A dictionary containing the arguments for the endpoint script.
+                args (dict[str, list[str] | str]): A dictionary containing the arguments for the endpoint script.
 
             Returns:
                 tuple[list[CommandResults], list[dict[str, dict]]]:
                     - A list of CommandResults objects, which contain the results of the command execution.
                     - A list of dictionaries, where each dictionary represents an endpoint and contains the raw output.
             """
-        args = prepare_args(command, endpoint_args)
         demisto.debug(f'run command {command.name} with args={args}')
         print(f'run command {command.name} with args={args}')
 
-        if not self.is_command_runnable(command, args):
-            return [], []
+        raw_outputs = run_execute_command(command, args)
+        command_context_outputs, command_contents, command_human_readable_outputs, command_error_outputs = get_command_results(command, raw_outputs, args, self.verbose)
+        if self.verbose:
+            human_readable = "\n".join(command_human_readable_outputs)
+            human_readable = [hr] if (hr := hr_to_command_results(command.name, args, human_readable)) else []
+            self.commands_results_list.extend(human_readable)
+            self.commands_results_list.extend(command_error_outputs)
 
-        raw_outputs = self.run_execute_command(command, args)
-        print(f"raw_outputs {raw_outputs}")
-        self.get_command_results(command, raw_outputs, args)
 
-        # if not entry_context:
-        #     return readable_errors, []
-        #
-        # endpoints = entry_context_to_endpoints(command, entry_context)
-        # if command.post_processing:
-        #     demisto.debug(f'command with post processing: {command.name}')
-        #     endpoints = command.post_processing(endpoints, endpoint_args)
-        #
-        # return human_readable, endpoints
 
-    def is_command_runnable(self, command: Command, args: dict[str, Any]) -> bool:
-        """
-        Checks if the given command is runnable.
-        This function performs the following checks:
-        1. Checks if the integration required for the command is installed and active using the
-            module_manager.is_brand_available() method.
-        2. Checks if the command has argument mapping and if the provided arguments match the command's expected arguments.
-
-        Args:
-            command (Command): An instance of the Command class containing the command details.
-            args (dict[str, Any]): A dictionary containing the arguments for the command.
-
-        Returns:
-            bool: True if the command is runnable, False otherwise.
-        """
-        # checks if the integration required for the command is installed and active
-        if not self.module_manager.is_brand_available(command.brand):
-            demisto.debug(f'Skipping command "{command.name}" since the brand {command.brand} is not available.')
-            print(f'Skipping command "{command.name}" since the brand {command.brand} is not available.')
-            return False
-
-        # checks if the command has argument mapping
-        if command.args_mapping and not args.values():
-            demisto.debug(f'Skipping command "{command.name}" since the provided arguments does not match the command.')
-            print(f'Skipping command "{command.name}" since the provided arguments does not match the command.')
-            return False
-
-        return True
-
-    @staticmethod
-    def run_execute_command(command: Command, args: dict[str, Any]) -> list[dict[str, Any]]:
-        """
-        Executes a command if the brand is available and returns the results.
-
-        Args:
-            command (Command): An instance of MappedCommand that contains the command information.
-            args (Dict[str, Any]): A dictionary containing the specific arguments for the command.
-        Returns:
-            Dict[str, Any]: A dictionary containing the command and its results.
-        """
-
-        return demisto.executeCommand(command.name, args)
-
-    @staticmethod
-    def get_command_results(command: Command, results: list[dict[str, Any]], args: dict[str, Any]) -> tuple[list, list, list]:
-        """
-        Processes the results of a previously executed command and extracts relevant outputs.
-
-        Args:
-            command (Command): An instance of MappedCommand that contains the command information.
-            results (Dict[str, Any]): A dictionary containing the command results.
-            args (Dict[str, Any]): A dictionary containing the specific arguments for the command.
-
-        Returns:
-            Tuple[List[Dict[str, Any]], str, List[Dict[str, Any]]]:
-                A tuple containing:
-                - A list of command context outputs.
-                - A human-readable string of the results.
-                - A list of command error outputs.
-        """
-
-        command_context_outputs = []
-        human_readable_outputs = []
-        command_error_outputs = []
-        demisto.debug(f'get_commands_outputs for command "{command}" with {len(results)} entry results')
-        for entry in results:
-            print(f"ENTRY {entry}")
-            if is_error(entry):
-                print("ERROR")
-            #     command_error_outputs.append(hr_to_command_results(command, args, get_error(entry), is_error=True))
-            contents = entry.get("Contents")
-            print(f"CONTENT {contents}")
-            if isinstance(contents, list):
-                for content in contents:
-                    print(f"CONTENT {content}")
-                    # command_context_outputs.append(content)
-                    # human_readable_outputs.append(content.get("HumanReadable") or "")
-
-            # else:
-            #     command_context_outputs.append(entry.get("EntryContext", {}))
-            #     human_readable_outputs.append(entry.get("HumanReadable") or "")
-
-        # human_readable = "\n".join(human_readable_outputs)
-        # human_readable = [hr] if (hr := hr_to_command_results(command, args, human_readable)) else []
-        # return command_context_outputs, human_readable, command_error_outputs
 
 
 def to_list(var):
@@ -291,23 +254,20 @@ def enrich_internal_ip_address(ip_command_runner, ips: list[str]) -> CommandResu
     demisto.debug(f"Internal IP detected: {ips}")
     print("INTERNAL IP")
     joined_ips = ",".join(ips)
-    endpoint_data = demisto.executeCommand("get-endpoint-data", {"ip": ips})[0]["Contents"]
-    readable_output = tableToMarkdown("Internal IP Data", endpoint_data)
-    return CommandResults(
-        outputs=endpoint_data,
-        outputs_prefix="IPEnrichment.Internal",
-        readable_output=readable_output
+    get_endpoint_data_command = Command(
+        name="get-endpoint-data",
+        output_keys=["Contents"],
+        output_mapping=lambda x: x.get("Contents", [])
     )
+    ip_command_runner.run_command(get_endpoint_data_command, {"agent_ip": joined_ips})
 
 
 def check_reputation(ip_command_runner: IPCommandRunner, ips: str):
     """Check the reputation of an IP address."""
     print("REPUTATION")
     ip_command = Command(
-        brand=None,
         name="ip",
         output_keys=["Contents"],
-        args_mapping={"ip": "ip"},
         output_mapping=lambda x: x.get("Contents", [])
     )
     ip_command_runner.run_command(ip_command, {"ip": ips})
@@ -316,24 +276,23 @@ def check_reputation(ip_command_runner: IPCommandRunner, ips: str):
 def get_analytics_prevalence(ip_command_runner: IPCommandRunner, ips: str) -> dict:
     """Retrieve analytics prevalence data for IP indicators."""
     print("ANALYTICS PREVALENCE")
+    if not ip_command_runner.is_brand_available("Cortex Core - IR"):
+        demisto.debug(f'Skipping get_analytics_prevalence since the brand Cortex Core - IR is not available.')
+        print(f'Skipping get_analytics_prevalence since the brand Cortex Core - IR is not available.')
     prevalence_command = Command(
-        brand="Cortex Core - IR",
         name="core-get-IP-analytics-prevalence",
         output_keys=["Contents"],
-        args_mapping={"ip_address": "ip"},
         output_mapping=lambda x: x.get("Contents", [])
     )
-    ip_command_runner.run_command(prevalence_command, {"ip": ips})
+    ip_command_runner.run_command(prevalence_command, {"ip_address": ips})
 
 
 def get_indicator_tim_data(ip_command_runner: IPCommandRunner, ips: list[str]) -> tuple[dict[str, Any], CommandResults]:
     """Retrieve TIM data for IP indicators."""
     print("TIM DATA")
     find_indicators_command = Command(
-        brand=None,
         name="findIndicators",
         output_keys=["Contents"],
-        args_mapping={"query": "query"},
         output_mapping=lambda x: x.get("Contents", []),
     )
     ips_value_query = " or ".join([f"value:{ip}" for ip in ips])
@@ -349,7 +308,7 @@ def enrich_external_ip_address(ip_command_runner, ips: list[str]) -> CommandResu
     joined_ips = ",".join(ips)
     check_reputation(ip_command_runner, joined_ips)
     if is_xsiam():
-        get_analytics_prevalence(ip_command_runner,joined_ips)
+        get_analytics_prevalence(ip_command_runner, joined_ips)
 
 
 def ip_enrichment(ip_command_runner, ips: list[str], third_enrichment: bool):
@@ -378,8 +337,7 @@ def main():
         ips = argToList(args.get("ip", ""))
         third_enrichment = argToBoolean(args.get("third_enrichment", False))
         verbose = argToBoolean(args.get("verbose", False))
-        module_manager = ModuleManager(demisto.getModules())
-        ip_command_runner = IPCommandRunner(module_manager, verbose)
+        ip_command_runner = IPCommandRunner(verbose)
 
         if not ips:
             raise ValueError("No IPs provided for enrichment.")
@@ -391,7 +349,7 @@ def main():
             print(f"Failed to enrich IP: {e}")
             #ips_not_found_list.append({"ip": ip, "error": str(e)})
 
-        return_results(ip_command_runner.command_results_list)
+        return_results(ip_command_runner.commands_results_list)
 
     except Exception as e:
         demisto.error(traceback.format_exc())
