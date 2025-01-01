18c53c72f0a9f3b3dc81281e6333bd263238d029
yucohen <yucohen@paloaltonetworks.com>
2025-01-01 17:31:06 +0200

    InvalidIntegrationScriptFileName - CIAC-10841

--- Packs/FeedMitreAttack/Scripts/MitreIDLayoutDynamicSection/MitreIDLayoutDynamicSection.py
+++ Packs/FeedMitreAttack/Scripts/MitreIDLayoutDynamicSection/MitreIDLayoutDynamicSection.py
@@ -6,4 +6,3 @@ mitre_id = demisto.get(demisto.args()['indicator'], 'Name') or ''
 
 demisto.results(
     {'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': f'# {mitre_id}'})
-
