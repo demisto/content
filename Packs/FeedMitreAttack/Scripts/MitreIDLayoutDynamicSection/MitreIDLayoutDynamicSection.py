d0ac30105206bab69e70f894544775bb4566028c
yucohen <yucohen@paloaltonetworks.com>
2025-01-01 17:17:24 +0200

    test

--- Packs/FeedMitreAttack/Scripts/MitreIDLayoutDynamicSection/MitreIDLayoutDynamicSection.py
+++ Packs/FeedMitreAttack/Scripts/MitreIDLayoutDynamicSection/MitreIDLayoutDynamicSection.py
@@ -6,3 +6,4 @@ mitre_id = demisto.get(demisto.args()['indicator'], 'Name') or ''
 
 demisto.results(
     {'ContentsFormat': formats['markdown'], 'Type': entryTypes['note'], 'Contents': f'# {mitre_id}'})
+
