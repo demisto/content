18155e39effa8c0be89bb0858c0510f9242ab5d5
Kamal Qarain <kqarain@paloaltonetworks.com>
2025-01-05 16:50:40 +0200

    Delete falcon_upload.sh

--- falcon_upload.sh
+++
@@ -1,6 +0,0 @@
-# prepare & upload
-demisto-sdk prepare-content -i '/Users/kqarain/dev/demisto/content/Packs/CrowdStrikeFalcon' -mp marketplacev2 --force
-demisto-sdk upload -i '/Users/kqarain/dev/demisto/content/Packs/CrowdStrikeFalcon/CrowdStrikeFalcon.zip' -x
-
-# cleanup
-rm '/Users/kqarain/dev/demisto/content/Packs/CrowdStrikeFalcon/CrowdStrikeFalcon.zip'
