8798f2f2a5068ebc39779ec8cf9756d4fde70a74
adit-ghildiyal <156281269+adit-ghildiyal@users.noreply.github.com>
2025-01-05 15:07:53 +0530

    fetch incidents added

--- Packs/RSANetWitness_v11_1/Integrations/RSANetWitnessv115/RSANetWitnessv115.py
+++ Packs/RSANetWitness_v11_1/Integrations/RSANetWitnessv115/RSANetWitnessv115.py
@@ -67,7 +67,18 @@ class Client(BaseClient):
         return self._http_request('GET', 'rest/api/incidents', params=params)
 
     def get_incident_request(self, inc_id: str | None) -> dict:
-        return self._http_request('GET', f'rest/api/incidents/{inc_id}')
+        data = json.dumps({
+            'meta_name': 'incidentId',
+            'meta_value': inc_id,
+            'numberOfRecords': "0",
+        })
+        response = self._http_request('GET', 'rest/api/incidents/fetch',data=data)
+
+        # Ensure the response is a list
+        if not isinstance(response, list):
+            raise ValueError("Expected the response to be a list")
+
+        return response
 
     def update_incident_request(self, id_: Any | None, status: Any | None, assignee: Any | None) -> dict:
         data = assign_params(status=status, assignee=assignee)
@@ -90,7 +101,7 @@ class Client(BaseClient):
         )
 
     def incident_list_alerts_request(self, page_size: str | None, page_number: str | None, id_: str | None) -> dict:
-        params = assign_params(pageNumber=page_number, pageSize=page_size)
+        #params = assign_params(pageNumber=page_number, pageSize=page_size)
         data = json.dumps({
             'meta_name': 'incidentId',
             'meta_value': id_,
@@ -99,7 +110,7 @@ class Client(BaseClient):
         })
 
         response = self._http_request(
-            'GET', f'rest/api/alert/fetch', data=data
+            'GET', 'rest/api/alert/fetch', data=data
         )
 
         # Ensure the response is a list
