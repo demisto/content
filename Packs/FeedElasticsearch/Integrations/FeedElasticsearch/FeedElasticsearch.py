d4d442a4dbb8b05957f03f42e4a6f4212bb01170
shmuel44 <skroizer@paloaltonetworks.com>
2025-01-05 06:01:40 +0200

    mypy

--- Packs/FeedElasticsearch/Integrations/FeedElasticsearch/FeedElasticsearch.py
+++ Packs/FeedElasticsearch/Integrations/FeedElasticsearch/FeedElasticsearch.py
@@ -41,8 +41,8 @@ if ELASTIC_SEARCH_CLIENT == OPEN_SEARCH:
     from opensearch_dsl import Search
     from opensearch_dsl.query import QueryString
 elif ELASTIC_SEARCH_CLIENT == ELASTICSEARCH_V8:
-    from elasticsearch import Elasticsearch
-    from elasticsearch.helpers import scan
+    from elasticsearch import Elasticsearch  # type: ignore[assignment]
+    from elasticsearch.helpers import scan  # type: ignore[assignment]
     from elasticsearch_dsl import Search
     from elasticsearch_dsl.query import QueryString
 else:  # Elasticsearch (<= v7)
