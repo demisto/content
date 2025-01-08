013a0d7d5cc46b251217990f6593eeb813f271a5
RotemAmit <ramit@paloaltonetworks.com>
2025-01-08 21:19:19 +0200

    g + h

--- Packs/GoogleKeyManagementService/Integrations/GoogleKeyManagementService/GoogleKeyManagementService.py
+++ Packs/GoogleKeyManagementService/Integrations/GoogleKeyManagementService/GoogleKeyManagementService.py
@@ -606,6 +606,9 @@ def symmetric_decrypt_key_command(client: Client, args: dict[str, Any]) -> tuple
 
     elif args.get('simple_ciphertext') or args.get('entry_id'):
         plaintext = str(base64.b64decode(response.plaintext))[2:-1].replace('\\n', '\n')
+    else:
+        plaintext = ""
+        demisto.debug(f"The arguments didn't match any condition, {plaintext=}")
 
     if args.get('entry_id'):
         file_name = demisto.getFilePath(args.get('entry_id'))['name'] + '_decrypted.txt'
@@ -979,6 +982,9 @@ def asymmetric_decrypt_command(client: Client, args: dict[str, Any]) -> tuple[st
     if args.get('entry_id'):
         file_name = demisto.getFilePath(args.get('entry_id'))['name'] + '_decrypted.txt'
         demisto.results(fileResult(file_name, plaintext))
+    else:
+        plaintext = ""
+        demisto.debug(f"The arguments didn't match any condition, {plaintext=}")
 
     asymmetric_decrypt_context = {
         'CryptoKey': crypto_key_id,
