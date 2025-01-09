c4baaf711978b0161d5c1a418688cb70ed7150d5
Karan Deep <karan.deep@metronlabs.com>
2025-01-09 16:50:46 +0530

    updated the code

--- Packs/SilentPush/Integrations/SilentPush/SilentPush.py
+++ Packs/SilentPush/Integrations/SilentPush/SilentPush.py
@@ -11,7 +11,7 @@ def mock_debug(message):
     """Print debug messages to the XSOAR logs"""
     print(f"DEBUG: {message}")
 
-# Assign the mock debug function to demisto.debug for logging
+
 demisto.debug = mock_debug
 
 class Client:
