b271dec21d5c73d4e994f988729ebc54a11ac6c4
BigEasyJ <janny@paloaltonetworks.com>
2025-01-10 17:22:22 +0000

    Refactor variables and update ReadMe

--- Packs/CortexAttackSurfaceManagement/Scripts/RankServiceOwners/README.md
+++ Packs/CortexAttackSurfaceManagement/Scripts/RankServiceOwners/README.md
@@ -19,6 +19,7 @@ Recommend most likely service owners from those surfaced by Cortex ASM Enrichmen
 | asmsystemids | System IDs or names associated with the compute instance |
 | owners | List of potential service owners |
 | ownerrelatedfield | The field of the alert or issue that owners should be stored. |
+| tenantcommand | False will use !setAlert, True will use !setIssue. |
 
 ## Outputs
 
--- Packs/CortexAttackSurfaceManagement/Scripts/RankServiceOwners/RankServiceOwners.py
+++ Packs/CortexAttackSurfaceManagement/Scripts/RankServiceOwners/RankServiceOwners.py
@@ -632,7 +632,7 @@ def main():
             unranked = [unranked]
         asm_system_ids = demisto.args().get("asmsystemids", [])
         owner_related_field = demisto.args().get("ownerrelatedfield", "asmserviceowner")
-        platform_tenant_usage = demisto.args().get("platformtenantusage", False)
+        platform_tenant_usage = demisto.args().get("tenantcommand", False)
         # deduplicate/normalize, score, and rank owners
         normalized = aggregate(canonicalize(unranked))
         final_owners = justify(rank(score(owners=normalized, asm_system_ids=asm_system_ids)))
