27552a1581d145cc471a629f83c4bf3536336f44
noydavidi <nodavidi@paloaltonetworks.com>
2025-01-13 15:25:22 +0200

    Added description for unittests

--- Packs/ProofpointIsolation/Integrations/ProofpointIsolationEventCollector/ProofpointIsolationEventCollector_test.py
+++ Packs/ProofpointIsolation/Integrations/ProofpointIsolationEventCollector/ProofpointIsolationEventCollector_test.py
@@ -19,6 +19,12 @@ def util_load_json(path):
 
 
 def test_get_and_reorganize_events(mocker):
+    """
+    Given: A mock Proofpoint client with a set of raw events and a set of event IDs.
+    When: Reorganizing events based on date and excluding the last event ID.
+    Then: Ensure the events are sorted in chronological order,the number of reorganized events matches the expected count,
+     and the last event is correctly excluded from the result.
+    """
     from ProofpointIsolationEventCollector import get_and_reorganize_events, hash_user_name_and_url
     mocked_events = util_load_json('test_data/get_events_raw_response.json')
     mocker.patch('ProofpointIsolationEventCollector.Client.get_events', return_value=mocked_events)
@@ -45,6 +51,12 @@ def test_get_and_reorganize_events(mocker):
 
 
 def test_initialize_args_to_fetch_events(mocker):
+    """
+    Given: A mock `getLastRun` return value with a start date and event IDs, and a mock current time.
+    When: Initializing the arguments to fetch events.
+    Then: Ensure the start date, end date, and event IDs are correctly set based on the mock data,
+     and verify the behavior when no previous run data is available.
+    """
     from ProofpointIsolationEventCollector import initialize_args_to_fetch_events, DATE_FORMAT
     from datetime import datetime
 
@@ -69,6 +81,12 @@ def test_initialize_args_to_fetch_events(mocker):
 
 
 def test_initialize_args_to_get_events():
+    """
+    Given: A dictionary with start and end dates for event fetching.
+    When: Initializing the arguments to get events.
+    Then: Ensure the start date and end date are correctly set, and verify the behavior when no arguments are provided,
+     resulting in `None` values for start and end, and an empty set for IDs.
+    """
     from ProofpointIsolationEventCollector import initialize_args_to_get_events
     args = {
         'start_date': "2025-01-08T10:27:08",
@@ -89,7 +107,14 @@ def test_initialize_args_to_get_events():
 
 
 def test_remove_duplicate_events():
-    from ProofpointIsolationEventCollector import remove_duplicate_events,sort_events_by_date, hash_user_name_and_url, get_and_parse_date
+    """
+    Given: A list of events sorted by date, with a specified start date and event IDs.
+    When: Removing duplicate events for a given start date.
+    Then: Ensure the number of events is reduced by the expected amount,
+     and verify that no event with the same ID and start date remains after duplicates are removed.
+    """
+    from ProofpointIsolationEventCollector import (remove_duplicate_events,sort_events_by_date,
+                                                   hash_user_name_and_url, get_and_parse_date)
     mocked_events = util_load_json('test_data/get_events_raw_response.json')
 
     events = sort_events_by_date(mocked_events["data"])
@@ -109,7 +134,12 @@ def test_remove_duplicate_events():
 
 
 def test_get_and_parse_date():
-    from ProofpointIsolationEventCollector import get_and_parse_date, parse_date_string, DATE_FORMAT
+    """
+    Given: A valid event with a date in a specific format.
+    When: Parsing the event's date using the `get_and_parse_date` function.
+    Then: Ensure the date is correctly parsed and returned in the expected format.
+    """
+    from ProofpointIsolationEventCollector import get_and_parse_date
 
     valid_event = {
         "date": "2025-01-01T19:44:35.000+0000",
@@ -121,6 +151,12 @@ def test_get_and_parse_date():
 
 
 def test_no_more_events_after_second_call(mocker):
+    """
+    Given: A mock Proofpoint client with event data and a last run timestamp.
+    When: Fetching events with a specified limit across multiple calls.
+    Then: Ensure the correct number of events are fetched on the first call,
+     and no events are returned on the second call when there are no more events to fetch.
+    """
     from ProofpointIsolationEventCollector import fetch_events
 
     client = create_client()
@@ -146,6 +182,12 @@ def test_no_more_events_after_second_call(mocker):
 
 
 def test_fetch_limit_reached(mocker):
+    """
+    Given: A mock Proofpoint client with event data and a last run timestamp.
+    When: Fetching events with a specified fetch limit.
+    Then: Ensure the correct number of events are returned based on the fetch limit,
+     and that the new last run contains the expected IDs.
+    """
     from ProofpointIsolationEventCollector import fetch_events
 
     client = create_client()
@@ -165,6 +207,12 @@ def test_fetch_limit_reached(mocker):
 
 
 def test_first_time_fetch(mocker):
+    """
+    Given: A mock Proofpoint client with event data and no previous run data.
+    When: Fetching events for the first time with a specified limit.
+    Then: Ensure no events are fetched on the first call,
+     and the new last run contains an empty list for IDs and a `start_date` field.
+    """
     from ProofpointIsolationEventCollector import fetch_events
 
     client = create_client()
