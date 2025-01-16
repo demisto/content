0626d5ef9a14b695acdbf4f77da5b3cb4de4bf38
omerKarkKatz <okarkkatz@paloaltonetworks.com>
2025-01-16 09:24:41 +0200

    fix dynamic type check

--- Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks.py
+++ Packs/IntegrationsAndIncidentsHealthCheck/Scripts/GetFailedTasks/GetFailedTasks.py
@@ -263,7 +263,7 @@ def filter_playbooks_failures(response: list | None) -> list | None:
         The filtered list of tasks. Tasks of type "playbook" whose names appear in the ancestors of other tasks are removed.
     """
 
-    if type(response) != list:
+    if not isinstance(response, list):
         return response
 
     ancestors = set()
