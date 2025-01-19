489a47ebfa63f52f9ef25ffb57cce83b536d0ff0
Daniel Rezvani <drezvani@paloaltonetworks.com>
2025-01-19 15:00:32 +0200

    added fixes from pre-commit

--- Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance_test.py
+++ Packs/Core/Scripts/impossibleTravelerGetDistance/impossibleTravelerGetDistance_test.py
@@ -70,7 +70,7 @@ def test_impossible_traveler_det_distance_bad_dest_coords(mocker):
     with pytest.raises(ValueError) as e:
         verify_coords(BAD_ARGS)
         if not e:
-            assert False
+            raise AssertionError
 
 
 def test_generate_events_dict(mocker):
