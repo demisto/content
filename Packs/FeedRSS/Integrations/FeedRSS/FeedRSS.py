ffa664274b3f8797d1e108ddb9d4d5c7ccf86f03
adi88d <adaud@paloaltonetworks.com>
2025-01-20 17:21:31 +0200

    set USER_AGENT as json

--- Packs/FeedRSS/Integrations/FeedRSS/FeedRSS.py
+++ Packs/FeedRSS/Integrations/FeedRSS/FeedRSS.py
@@ -7,8 +7,8 @@ HTML_TAGS = ['p', 'table', 'ul', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 INTEGRATION_NAME = 'RSS Feed'
 
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0" \
-             " Safari/537.36"
+USER_AGENT = '{"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)'\
+              ' Chrome/127.0.0.0 Safari/537.36"}'
 
 
 class Client(BaseClient):
