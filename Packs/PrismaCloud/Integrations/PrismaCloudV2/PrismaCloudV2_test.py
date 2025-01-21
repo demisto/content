add33aa8fe6265744f109b1e1d2aa2e7552c9fb0
Paul <32433511+pdragoi@users.noreply.github.com>
2025-01-21 15:39:54 +0000

    add unit test

--- Packs/PrismaCloud/Integrations/PrismaCloudV2/PrismaCloudV2_test.py
+++ Packs/PrismaCloud/Integrations/PrismaCloudV2/PrismaCloudV2_test.py
@@ -760,6 +760,31 @@ def test_get_asset_vulnerabilities(mocker, prisma_cloud_v2_client):
             'prismaCloudFindingsOnly': False
         }
     )
+
+
+def test_get_asset_alerts(mocker, prisma_cloud_v2_client):
+    """
+    Given:
+        - All relevant arguments for the command that is executed
+    When:
+        - prisma-cloud-asset-alerts-get command is executed
+    Then:
+        - The http request is called with the right arguments
+    """
+    from PrismaCloudV2 import get_asset_alerts
+    http_request = mocker.patch.object(prisma_cloud_v2_client, '_http_request')
+    args = {'asset_id': 'rrn::name:place:111:a1b2:a%3Ajj55-2023-01-29-09-25'}
+    get_asset_alerts(prisma_cloud_v2_client, args)
+    http_request.assert_called_with(
+        method="POST",
+        url_suffix="/uai/v1/asset",
+        json_data={
+            'assetId': 'rrn::name:place:111:a1b2:a%3Ajj55-2023-01-29-09-25',
+            'type': 'alerts',
+            'limit': 50,
+            'prismaCloudFindingsOnly': False
+        }
+    )
 ''' HELPER FUNCTIONS TESTS '''
 
 
