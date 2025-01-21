bd2c11b9a1d5cd2d190c2b4e416a9d6deeac9205
adi88d <adaud@paloaltonetworks.com>
2025-01-21 12:49:24 +0200

    pre commit

--- Packs/FeedRSS/Integrations/FeedRSS/FeedRSS.py
+++ Packs/FeedRSS/Integrations/FeedRSS/FeedRSS.py
@@ -7,6 +7,7 @@ HTML_TAGS = ['p', 'table', 'ul', 'ol', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 
 INTEGRATION_NAME = 'RSS Feed'
 
+
 class Client(BaseClient):
     """Client for RSS Feed - gets Reports from the website
     Attributes:
