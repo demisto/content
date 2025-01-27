2ace5e1c2a646c7d41aea7028a980a9ee6fb0162
darbel <darbel@paloaltonetworks.com>
2025-01-27 16:17:11 +0200

    changed suffix removal to beginning of function

--- Packs/CommonScripts/Scripts/EmailAskUserResponse/EmailAskUserResponse.js
+++ Packs/CommonScripts/Scripts/EmailAskUserResponse/EmailAskUserResponse.js
@@ -7,6 +7,9 @@ if (res[0].Type==entryTypes.note || res[0].Type==entryTypes.file) {
     if (prefix && text.startsWith(prefix)) {
         text = text.slice(prefix.length);
     }
+    if (suffix && text.endsWith(suffix)) {
+        text = text.slice(0, -1 * suffix.length);
+    }
     text = text.replace(/<br\/?>/gi,'\n')
                 .replace(/\r/g,'')
                 .replace(/\n/g,'__NL__')
@@ -15,9 +18,6 @@ if (res[0].Type==entryTypes.note || res[0].Type==entryTypes.file) {
                 .replace(/__NL____NL__/g,'')
                 .replace(/__NL__/g,'\n');
     response = text.replace(/<(?:.|\n)*?>/gm, '').trim("\n").split("\n")[0].trim();
-    if (suffix && response.endsWith(suffix)) {
-        response = response.slice(0, -1 * suffix.length);
-    }
     setContext('EmailAskUserResponse', response);
     return response;
 } else {
