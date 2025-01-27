959674a1f98a3b3f633fe89066d77c4939d0a6f4
mayagoldman <mgoldman@paloaltonetworks.com>
2025-01-27 12:30:03 +0200

    modified script

--- Packs/Microsoft365Defender/Scripts/MS365DefenderCountIncidentCategories/MS365DefenderCountIncidentCategories.py
+++ Packs/Microsoft365Defender/Scripts/MS365DefenderCountIncidentCategories/MS365DefenderCountIncidentCategories.py
@@ -3,18 +3,24 @@ from CommonServerPython import *  # noqa: F401
 from collections import Counter
 
 
-def count_dict(value):
+def count_dict(value: str) -> List[dict]:
     """
     Count the number of occurrences of each category in the given string.
     :param value: The string containing the categories separated by commas.
     :return: A list of dictionaries containing the category and the number of occurrences.
     """
-    if not isinstance(value, str):
-        return value
+
     demisto.debug(f'Counting categories in string: {value}')
     categories = value.split(',')
     return [{'category': key, 'count': value} for key, value in dict(Counter(categories)).items()]
 
 
+def main():
+    try:
+        return_results(count_dict(**demisto.args()))
+    except Exception as e:
+        return_error(f'Failed to execute MS365DefenderCountIncidentCategories.py. Error: {str(e)}')
+
+
 if __name__ in ('__main__', '__builtin__', 'builtins'):
-    return_results(count_dict(**demisto.args()))
+    main()
