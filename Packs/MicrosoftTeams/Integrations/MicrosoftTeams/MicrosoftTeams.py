184ffde6b5b5d93ecbe12ad30668fc195fcf9772
Danny_Fried <dfried@paloaltonetworks.com>
2025-01-28 15:13:22 +0200

    Add support for microsoft-teams-get-team-members command.

--- Packs/MicrosoftTeams/Integrations/MicrosoftTeams/MicrosoftTeams.py
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeams/MicrosoftTeams.py
@@ -1929,6 +1929,31 @@ def get_team_members(service_url: str, team_id: str) -> list:
     response: list = cast(list[Any], http_request('GET', url, api='bot'))
     return response
 
+def manual_get_team_members(team_name: str) -> list:
+    """
+    Retrieves and updates in context the team members of given a team_name
+    :param team_name: ID of team to get team members of
+    :return: List of team members
+    """
+    integration_context: dict = get_integration_context()
+    service_url: str = integration_context.get('service_url', '')
+    team_id = ''
+    team = None
+    teams: list = json.loads(integration_context.get('teams', '[]'))
+    for team in teams:
+        if team.get('team_name', '') == team_name:
+            team_id = team.get('id','')
+            demisto.debug(f'Found the following ID: {team_id} for team name: {team_name}')
+            url = f'{service_url}/v3/conversations/{team_id}/members'
+            team_members: list = cast(list[Any], http_request('GET', url, api='bot'))
+            demisto.debug(f'team_name: {team_name} got the following members: {team_members}')
+            team['team_members'] = team_members
+            integration_context['teams'] = json.dumps(teams)
+            set_to_integration_context_with_retries(integration_context)
+    else:
+        demisto.debug(f'Did not find a team for team name: {team_name}')
+        return []
+    
 
 def get_channel_members(team_id: str, channel_id: str) -> list[dict[str, Any]]:
     """
@@ -3021,7 +3046,8 @@ def main():   # pragma: no cover
         'microsoft-teams-generate-login-url': generate_login_url_command,
         'microsoft-teams-auth-reset': reset_graph_auth_command,
         'microsoft-teams-token-permissions-list': token_permissions_list_command,
-        'microsoft-teams-create-messaging-endpoint': create_messaging_endpoint_command
+        'microsoft-teams-create-messaging-endpoint': create_messaging_endpoint_command,
+        'microsoft-teams-get-team-members': manual_get_team_members
     }
 
     commands_auth_code: dict = {
