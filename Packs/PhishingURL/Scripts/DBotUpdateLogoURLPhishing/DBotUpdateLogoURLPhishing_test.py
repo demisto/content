f6778270015d597fdc815ad27ad29ada8367ced9
jlevypaloalto <jlevy@paloaltonetworks.com>
2025-01-30 18:20:30 +0200

    MORE tests

--- Packs/PhishingURL/Scripts/DBotUpdateLogoURLPhishing/DBotUpdateLogoURLPhishing_test.py
+++ Packs/PhishingURL/Scripts/DBotUpdateLogoURLPhishing/DBotUpdateLogoURLPhishing_test.py
@@ -25,12 +25,15 @@ MODEL_DATA = {
 #     def update_domain_for_custom_logo(self, logo_name, associated_domains):
 #         return True, MSG_LOGO_AMENDED
 
+def load_image(path):
+    with open(path, "rb") as f:
+        return base64.b64encode(f.read()).decode('utf-8')
 
-def test_new_logo_added(mocker: MockerFixture):
+
+def test_add_new_logo(mocker: MockerFixture):
     from DBotUpdateLogoURLPhishing import add_new_logo
-    # add_new_logo(model_data: ModelData, logo_name, logo_image_id, associated_domains):
     
-    mocker.patch.object(demisto, 'getFilePath', return_value={"path": "test_data/logo.png"})
+    mocker.patch('demisto.getFilePath', return_value={"path": "test_data/logo.png"})
     
     model_data = MODEL_DATA.copy()
     logo_name = 'PANW'
@@ -40,20 +43,56 @@ def test_new_logo_added(mocker: MockerFixture):
     res = add_new_logo(model_data, logo_name, logo_image_id, associated_domains)
     
     assert res == "Logo 'PANW' successfully added."
-    assert model_data['top_domains'] == None  # TODO
-    assert model_data['logos_dict'] == None  # TODO
-    assert model_data['custom_logo_associated_domain'] == None  # TODO
+    assert model_data['top_domains'] == dict.fromkeys(associated_domains, 0)
+    assert model_data['logos_dict']['PANW'] == load_image("test_data/logo.png")
+    assert model_data['custom_logo_associated_domain']['PANW'] == associated_domains
 
 
-# def test_remove_logo():
-#     from DBotUpdateLogoURLPhishing import add_new_logo, remove_logo
-#     model_data = MODEL_DATA.copy()
-#     logo_name = 'PANW'
+def test_remove_logo(mocker: MockerFixture):
+    from DBotUpdateLogoURLPhishing import add_new_logo, remove_logo
+    
+    mocker.patch('demisto.getFilePath', return_value={"path": "test_data/logo.png"})
+    
+    model_data = MODEL_DATA.copy()
+    logo_name = 'PANW'
+    add_new_logo(model_data, logo_name, '<file_id>', ['domain1.com', 'domain2.com'])
 
-#     remove_logo(model_data, logo_name)
+    res = remove_logo(model_data, logo_name)
+    
+    assert res == "Logo 'PANW' successfully removed."
+    assert model_data == MODEL_DATA
 
-# def test_update_domain_for_custom_logo(): ...
-# def test_test_display_all_logos(): ...
+
+def test_update_domain_for_custom_logo():
+    from DBotUpdateLogoURLPhishing import update_domain_for_custom_logo
+
+    logo_name = 'PANW'
+    old_associated_domains = ['domain1.com']
+    new_associated_domains = ['domain2.com']
+    model_data = MODEL_DATA.copy()
+    model_data['top_domains'] = dict.fromkeys(old_associated_domains, 0)
+    model_data['custom_logo_associated_domain'][logo_name] = old_associated_domains
+    
+    res = update_domain_for_custom_logo(model_data, logo_name, new_associated_domains)
+    
+    assert res == "Logo 'PANW' successfully modified."
+    assert model_data['top_domains'] == dict.fromkeys(new_associated_domains, 0)
+    assert model_data['custom_logo_associated_domain'][logo_name] == new_associated_domains
+    
+    
+def test_test_display_all_logos():
+    from DBotUpdateLogoURLPhishing import display_all_logos
+    
+    model_data = MODEL_DATA.copy()
+    model_data['custom_logo_associated_domain'] = {1: 'domain1.com'}
+    model_data['logos_dict'] = dict.fromkeys(range(3), load_image("test_data/logo.png"))
+    
+    res = display_all_logos(model_data)
+    
+    assert res['ContentsFormat'] == 'text'
+    assert res['Type'] == 7
+    assert res['File'] == ''  # TODO
+    # TODO check the files were concatenated
 
 # def test_new_major_no_added_logo(mocker):
 #     current_major_version = 0
