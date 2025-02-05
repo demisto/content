c33e928fb6cf5227ce5b82b5cdf4b6040ae6b622
noydavidi <nodavidi@paloaltonetworks.com>
2025-02-05 14:30:06 +0200

    started with unittests

--- Packs/Celonis/Integrations/Celonis/CelonisEventCollector_test.py
+++ Packs/Celonis/Integrations/Celonis/CelonisEventCollector_test.py
@@ -1,41 +1,22 @@
-"""Base Integration for Cortex XSOAR - Unit Tests file
-
-Pytest Unit Tests: all funcion names must start with "test_"
-
-More details: https://xsoar.pan.dev/docs/integrations/unit-testing
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-You must add at least a Unit Test function for every XSOAR command
-you are implementing with your integration
-"""
-
-from demisto_sdk.commands.common.handlers import JSON_Handler
-
 import json
+
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+MOCK_BASEURL = "https://example.com"
+MOCK_API_KEY = "API_KEY"
+
+
+def create_client():
+    from CelonisEventCollector import Client
+    return Client(
+        base_url=MOCK_BASEURL, verify=False,
+        client_id=MOCK_API_KEY
+    )
 
 
 def util_load_json(path):
-    with open(path, encoding="utf-8") as f:
+    with open(path, encoding='utf-8') as f:
         return json.loads(f.read())
 
 
-# TODO: REMOVE the following dummy unit test function
-def test_baseintegration_dummy():
-    """Tests helloworld-say-hello command function.
-
-    Checks the output of the command function with the expected output.
-
-    No mock is needed here because the say_hello_command does not call
-    any external API.
-    """
-    from BaseIntegration import Client, baseintegration_dummy_command
-
-    client = Client(base_url="some_mock_url", verify=False)
-    args = {"dummy": "this is a dummy response", "dummy2": "a dummy value"}
-    response = baseintegration_dummy_command(client, args)
-
-    assert response.outputs == args
-
-
-# TODO: ADD HERE unit tests for every command
+def test_sort_events_by_timestamp():
+    pass
