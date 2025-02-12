a5421986e6eca2671ca97f190b7bf2d3397cd0b2
Tal <tcarmeli@paloaltonetworks.com>
2025-02-12 16:46:58 +0200

    first commit

--- Packs/URLHaus/Integrations/URLHaus/URLHaus.py
+++ Packs/URLHaus/Integrations/URLHaus/URLHaus.py
@@ -575,8 +575,10 @@ def run_file_command(hash: str, params: dict) -> CommandResults:
         hash_type = 'md5'
     elif len(hash) == 64:
         hash_type = 'sha256'
-    else:
+    elif params.get('should_error'):
         return_error('Only accepting MD5 (32 bytes) or SHA256 (64 bytes) hash types')
+    else:
+        return_warning('Only accepting MD5 (32 bytes) or SHA256 (64 bytes) hash types', True)
 
     file_information = query_payload_information(hash_type, params.get('api_url'), params.get('use_ssl'),
                                                  hash).json()
@@ -722,7 +724,7 @@ def main():
             'use_ssl': not demisto_params.get('insecure', False),
             'threshold': int(demisto_params.get('threshold', 1)),
             'create_relationships': demisto_params.get('create_relationships', True),
-            'max_num_of_relationships': min(1000, int(demisto_params.get('max_num_of_relationships', 10))),
+            'max_num_of_relationships': min(1000, int(demisto_params.get('max_num_of_relationships', 10)))
         }
 
         reliability = demisto_params.get('integrationReliability', DBotScoreReliability.C)
@@ -744,6 +746,7 @@ def main():
         elif command == 'domain':
             domain_command(params)
         elif command == 'file':
+            params['should_error'] = argToBoolean(demisto.params().get('should_error', True))
             file_command(params)
         elif command == 'urlhaus-download-sample':
             urlhaus_download_sample_command(**params)
