896044d3db9f6ce1664acf817f41221ce762d517
Aaron <4101535@gmail.com>
2025-02-19 11:06:57 +0200

    init new commands

--- Packs/CybelAngel/Integrations/CybelAngelEventCollector/CybelAngelEventCollector.py
+++ Packs/CybelAngel/Integrations/CybelAngelEventCollector/CybelAngelEventCollector.py
@@ -33,30 +33,40 @@ class Client(BaseClient):
 
         super().__init__(base_url=base_url, verify=verify, proxy=proxy, **kwargs)
 
-    def http_request(self, method: str, url_suffix: str, params: dict[str, Any] | None = None) -> dict[str, Any]:
+    def http_request(self, method: str, url_suffix: str, data: dict | None = None, params: dict[str, Any] | None = None, pdf: bool = False, cve: bool = False) -> dict[str, Any]:
         """
         Overrides Base client request function, retrieves and adds to headers access token before sending the request.
         """
         token = self.get_access_token()
         headers = {
-            'Authorization': f'Bearer {token}',
-            'Content-Type': 'application/json',
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/json",
+            "Content-Type": (
+                "application/json"
+                if not pdf
+                else "application/pdf, application/json"
+            ),
         }
 
         demisto.debug(f'Running http-request with URL {url_suffix} and {params=}')
 
         response = self._http_request(
-            method, url_suffix=url_suffix, headers=headers, params=params, resp_type="response", ok_codes=(401, 403, 200)
+            method, url_suffix=url_suffix, headers=headers, params=params, json_data=data, resp_type="response", ok_codes=(401, 403, 200, 201, 302)
         )
-        if response.status_code == 200:
-            return response.json()
+        if response.status_code == 200 or response.status_code == 201:
+            return response.json() if not pdf else response
+        
+        if response.status_code == 302 and cve:
+            cve_response = requests.get(url=response.text)
+            return cve_response
+        
         else:
             demisto.debug('Access token has expired, retrieving new access token')
 
         token = self.get_access_token(create_new_token=True)
         headers["Authorization"] = f'Bearer {token}'
 
-        return self._http_request(method, url_suffix=url_suffix, headers=headers, params=params)
+        return self._http_request(method, url_suffix=url_suffix, headers=headers, params=params, json_data=data)
 
     def get_reports(self, start_date: str, end_date: str, limit: int = DEFAULT_MAX_FETCH) -> List[dict[str, Any]]:
         """
@@ -244,12 +254,193 @@ def get_events_command(client: Client, args: dict[str, Any]) -> CommandResults:
     )
 
 
+def cybelangel_report_list_command(client: Client, args):
+
+    start_date = arg_to_datetime(args.get("start_date"))
+    end_date = arg_to_datetime(args.get("end_date"))
+
+    response = client.http_request(
+        "GET",
+        "/api/v2/reports",
+        params={"start-date": start_date, "end-date": end_date},
+    )
+    return CommandResults(
+        outputs_prefix="CybelAngel.Report",
+        outputs=response,
+        readable_output="All reports retrieved.",
+    )
+
+
+def cybelangel_report_get_command(client: Client, args):
+
+    report_id = args.get("report_id")
+    pdf = argToBoolean(args.get("pdf", "false"))
+
+    response = client.http_request("GET", f"/api/v2/reports/{report_id}")
+    if pdf:
+        pdf_response = client.http_request(
+            "GET", f"/api/v1/reports/{report_id}/pdf", pdf=True
+        )
+        if pdf_response:
+            return fileResult(
+                f"cybelangel_report_{report_id}.pdf",
+                pdf_response.content,
+                file_type=EntryType.ENTRY_INFO_FILE,
+            )
+    return CommandResults(
+        outputs_prefix="CybelAngel.Report",
+        outputs=response,
+        readable_output=f"Report ID {report_id} retrieved.",
+    )
+
+
+def cybelangel_mirror_report_get_command(client: Client, args):
+    report_id = args.get("report_id")
+    csv = argToBoolean(args.get("csv", "false"))
+
+    endpoint = (
+        f"/api/v1/reports/{report_id}/mirror/csv"
+        if csv
+        else f"/api/v1/reports/{report_id}/mirror"
+    )
+    response = client.http_request("GET", endpoint, cve=True)
+
+    if csv:
+        return fileResult(
+            f"cybelangel_mirror_report_{report_id}.csv",
+            response.content,
+            file_type=EntryType.ENTRY_INFO_FILE,
+        )
+
+    return CommandResults(
+        outputs_prefix="CybelAngel.ReportMirror",
+        outputs=response,
+        readable_output=f"Mirror details for Report ID {report_id} retrieved.",
+    )
+
+
+def cybelangel_archive_report_by_id_get_command(client: Client, args):
+    report_id = args.get("report_id")
+
+    endpoint = f"/api/v1/reports/{report_id}/mirror/archive"
+    response = client.http_request("GET", endpoint, cve=True)
+
+    return fileResult(
+            f"cybelangel_archive_report_{report_id}.zip",
+            response.content,
+            file_type=EntryType.ENTRY_INFO_FILE,
+        )
+
+
+def cybelangel_report_status_update_command(client: Client, args):
+    report_ids = argToList(args.get("report_ids"))
+    status = args.get("status")
+
+    if len(report_ids) == 1:
+        client.http_request(
+            "PUT", f"/api/v1/reports/{report_ids[0]}/status", data={"status": status}
+        )
+    else:
+        client.http_request(
+            "POST", "/api/v1/reports/status", data={"ids": report_ids, "status": status}
+        )
+
+    return CommandResults(
+        readable_output=f"Status of {', '.join(report_ids)} was successfully updated."
+    )
+
+
+def cybelangel_report_comments_get_command(client: Client, args):
+    report_id = args.get("report_id")
+
+    endpoint = f"/api/v1/reports/{report_id}/comments"
+    response = client.http_request("GET", endpoint)
+
+    return CommandResults(
+        outputs_prefix="CybelAngel.Report.Comments",
+        outputs=response,
+        readable_output=f"Comments for Report ID {report_id} retrieved.",
+    )
+
+
+def cybelangel_report_comment_create_command(client: Client, args):
+    report_id = args.get("report_id")
+    content = args.get("content")
+    parent_id = args.get("parent_id")
+    assigned = argToBoolean(args.get("assigned", "false"))
+
+    comments_response = client.http_request(
+        "GET", f"/api/v1/reports/{report_id}/comments"
+    )
+    discussion_id = comments_response.get("comments", [])[0].get("discussion_id")
+    if not discussion_id:
+        raise ValueError("Unable to retrieve tenant ID")
+
+    data = {
+        "content": content,
+        "discussion_id": discussion_id,
+        "parent_id": parent_id,
+        "assigned": assigned if assigned == "True" else "",
+    }
+    remove_nulls_from_dictionary(data)
+
+    response = client.http_request(
+        "POST", f"/api/v1/reports/{report_id}/comments", data=data
+    )
+
+    return CommandResults(
+        outputs_prefix="CybelAngel.Report.Comments",
+        outputs=response,
+        readable_output=f"Comment added to Report ID {report_id}.",
+    )
+
+
+def cybelangel_report_attachment_get_command(client: Client, args):
+    report_id = args.get("report_id")
+    attachment_id = args.get("attachment_id")
+
+    response = client.http_request(
+        "GET", f"/api/v1/reports/{report_id}/attachments/{attachment_id}", pdf=True
+    )
+
+    return fileResult(
+        f"cybelangel_report_{report_id}_attachment_{attachment_id}.csv",
+        response.text,
+        file_type=EntryType.ENTRY_INFO_FILE,
+    )
+    # return CommandResults(
+    #     outputs_prefix="CybelAngel.Report.Attachment",
+    #     outputs=response.text,
+    #     readable_output=f"Attachment ID {attachment_id} for Report ID {report_id} retrieved."
+    # )
+
+
+def cybelangel_report_remediation_request_create_command(client: Client, args):
+    report_id = args.get("report_id")
+    requestor_email = args.get("requestor_email")
+    requestor_fullname = args.get("requestor_fullname")
+
+    endpoint = "/api/v1/reports/remediation-request"
+    data = {
+        "report_id": report_id,
+        "requester_email": requestor_email,
+        "requester_fullname": requestor_fullname,
+    }
+
+    client.http_request("POST", endpoint, data=data)
+
+    return CommandResults(
+        readable_output=f"Remediation request was created for {report_id}."
+    )
+
+
 ''' MAIN FUNCTION '''
 
 
 def main() -> None:
 
     params = demisto.params()
+    args = demisto.args()
     client_id: str = params.get('credentials', {}).get('identifier', '')
     client_secret: str = params.get('credentials', {}).get('password', '')
     base_url: str = params.get('url', '').rstrip('/')
@@ -257,6 +448,18 @@ def main() -> None:
     proxy = params.get('proxy', False)
     max_fetch = arg_to_number(params.get("max_fetch")) or DEFAULT_MAX_FETCH
     first_fetch = params.get("first_fetch") or DEFAULT_FIRST_FETCH
+
+    commands = {
+        "cybelangel-report-list": cybelangel_report_list_command,
+        "cybelangel-report-get": cybelangel_report_get_command,
+        "cybelangel-mirror-report-get": cybelangel_mirror_report_get_command,
+        "cybelangel-archive-report-by-id-get": cybelangel_archive_report_by_id_get_command,
+        "cybelangel-report-status-update": cybelangel_report_status_update_command,
+        "cybelangel-report-comments-get": cybelangel_report_comments_get_command,
+        "cybelangel-report-comment-create": cybelangel_report_comment_create_command,
+        "cybelangel-report-attachment-get": cybelangel_report_attachment_get_command,
+        "cybelangel-report-remediation-request-create": cybelangel_report_remediation_request_create_command,
+    }
 
     command = demisto.command()
     demisto.info(f'Command being called is {command}')
@@ -277,6 +480,8 @@ def main() -> None:
             demisto.setLastRun(last_run)
         elif command == "cybelangel-get-events":
             return_results(get_events_command(client, demisto.args()))
+        elif command in commands:
+            return_results(commands[command](client, args))
     except Exception as e:
         demisto.error(traceback.format_exc())
         return_error(f"Failed to execute {command} command.\nError:\ntype:{type(e)}, error:{str(e)}")
