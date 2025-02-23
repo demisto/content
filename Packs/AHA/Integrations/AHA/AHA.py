89bc6b4c6bba3403132aa35496f94db026e29512
ilaredo <166304750+ilaredo@users.noreply.github.com>
2025-02-23 13:36:48 +0200

    Update AHA.py

--- Packs/AHA/Integrations/AHA/AHA.py
+++ Packs/AHA/Integrations/AHA/AHA.py
@@ -4,7 +4,7 @@ from CommonServerUserPython import *  # noqa
 import requests
 from enum import Enum
 
-
+# Itamar test
 # Disable insecure warnings
 requests.packages.urllib3.disable_warnings()  # type: ignore[attr-defined]  # pylint: disable=no-member
 
