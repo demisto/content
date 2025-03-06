466f57cb5281e8e082fd3268ea8914a48b835382
Ali Sawyer <asawyer@paloaltonetworks.com>
2025-03-06 11:34:39 -0700

    address review comments

--- Packs/CortexDataLake/Integrations/CortexDataLake/CortexDataLake.py
+++ Packs/CortexDataLake/Integrations/CortexDataLake/CortexDataLake.py
@@ -1310,8 +1310,7 @@ def main():
     enc_key = params.get('credentials_auth_key', {}).get('password') or params.get('auth_key')
     if not enc_key or not refresh_token or not registration_id_and_url:
         raise DemistoException('Key, Token and ID must be provided.')
-    # is_fr = ('federal.paloaltonetworks.com' in demisto.getLicenseCustomField('Http_Connector.url'))
-    is_fr = False
+    is_fr = ('federal.paloaltonetworks.com' in demisto.getLicenseCustomField('Http_Connector.url'))
     demisto.debug(f"working on tenant with {is_fr=}")
     registration_id_and_url = registration_id_and_url.split('@')
     if len(registration_id_and_url) != 2:
