2607c296d01f9817a36a03305beae66e572688a8
Kamal Qarain <kqarain@paloaltonetworks.com>
2025-03-10 13:07:09 +0200

    Add sectionOrder to AwsSecretsManager.yml

--- Packs/AWS-IAM/Integrations/AWS-IAM/AWS-IAM.py
+++ Packs/AWS-IAM/Integrations/AWS-IAM/AWS-IAM.py
@@ -1294,7 +1294,7 @@ def get_access_key_last_used_command(args, client):
 
 
 def update_assume_role_policy_command(args: dict[str, Any], client) -> CommandResults:
-    """Updates the policy that grants an IAM entity permission to assume a role. 
+    """Updates the policy that grants an IAM entity permission to assume a role.
 
     Args:
         args (dict): The command arguments.
