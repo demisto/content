28c1a60db68b435371ad12ca5195689487671f6e
YuvHayun <yhayun@paloaltonetworks.com>
2025-03-11 08:32:03 +0200

    mid save

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -7548,45 +7548,46 @@ def return_error(message, error='', outputs=None):
         :return: Error entry object
         :rtype: ``dict``
     """
-    is_command = hasattr(demisto, 'command')
-    try:
-        is_server_handled = is_command and demisto.command() in ('fetch-incidents',
-                                                                 'fetch-credentials',
-                                                                 'long-running-execution',
-                                                                 'fetch-indicators')
-    except Exception:
-        is_server_handled = False
-    message = LOG(message)
-    if error:
-        LOG(str(error))
-    if any(sys.exc_info()):  # Checking that an exception occurred
-        fixed_traceback = fix_traceback_line_numbers(traceback.format_exc())
-        LOG('\n{}'.format(fixed_traceback))
-        if is_debug_mode():
-            message = '{}\n\n{}'.format(message, fixed_traceback)
+    # is_command = hasattr(demisto, 'command')
+    # try:
+    #     is_server_handled = is_command and demisto.command() in ('fetch-incidents',
+    #                                                              'fetch-credentials',
+    #                                                              'long-running-execution',
+    #                                                              'fetch-indicators')
+    # except Exception:
+    #     is_server_handled = False
+    # message = LOG(message)
+    # if error:
+    #     LOG(str(error))
+    # if any(sys.exc_info()):  # Checking that an exception occurred
+    #     fixed_traceback = fix_traceback_line_numbers(traceback.format_exc())
+    #     LOG('\n{}'.format(fixed_traceback))
+    #     if is_debug_mode():
+    #         message = '{}\n\n{}'.format(message, fixed_traceback)
 
-    LOG.print_log()
-    if not isinstance(message, str):
-        message = message.encode('utf8') if hasattr(message, 'encode') else str(message)
+    # LOG.print_log()
+    # if not isinstance(message, str):
+    #     message = message.encode('utf8') if hasattr(message, 'encode') else str(message)
 
-    if is_command and demisto.command() == 'get-modified-remote-data':
-        if (error and not isinstance(error, NotImplementedError)) or sys.exc_info()[0] != NotImplementedError:
-            message = 'skip update. error: ' + message
+    # if is_command and demisto.command() == 'get-modified-remote-data':
+    #     if (error and not isinstance(error, NotImplementedError)) or sys.exc_info()[0] != NotImplementedError:
+    #         message = 'skip update. error: ' + message
 
-    if is_server_handled:
-        raise Exception(message)
-    else:
-        if len(message) > MAX_ERROR_MESSAGE_LENGTH:
-            half_length = MAX_ERROR_MESSAGE_LENGTH // 2
-            message = message[:half_length] + "...This error body was truncated..." + message[half_length * (-1):]
+    # if is_server_handled:
+    #     raise Exception(message)
+    # else:
+    #     if len(message) > MAX_ERROR_MESSAGE_LENGTH:
+    #         half_length = MAX_ERROR_MESSAGE_LENGTH // 2
+    #         message = message[:half_length] + "...This error body was truncated..." + message[half_length * (-1):]
 
-        demisto.results({
-            'Type': entryTypes['error'],
-            'ContentsFormat': formats['text'],
-            'Contents': message,
-            'EntryContext': outputs,
-        })
-        sys.exit(0)
+    #     demisto.results({
+    #         'Type': entryTypes['error'],
+    #         'ContentsFormat': formats['text'],
+    #         'Contents': message,
+    #         'EntryContext': outputs,
+    #     })
+    #     sys.exit(0)
+    sys.exit(0)
 
 
 def return_warning(message, exit=False, warning='', outputs=None, ignore_auto_extract=False):
@@ -9444,8 +9445,8 @@ if 'requests' in sys.modules:
                     timeout=timeout,
                     **kwargs
                 )
-                if not self._is_status_code_valid(res, ok_codes):
-                    self._handle_error(error_handler, res, with_metrics)
+                # if not self._is_status_code_valid(res, ok_codes):
+                self._handle_error(error_handler, res, with_metrics)
 
                 return self._handle_success(res, resp_type, empty_valid_codes, return_empty_response, with_metrics)
 
@@ -11990,6 +11991,7 @@ def xsiam_api_call_with_retries(
         )
         status_code = response.status_code
         demisto.debug('received status code: {status_code}'.format(status_code=status_code))
+        status_code = 429
         if status_code == 429:
             time.sleep(1)
         attempt_num += 1
@@ -12019,19 +12021,20 @@ def split_data_to_chunks(data, target_chunk_size):
     if isinstance(data, str):
         data = data.split('\n')
     for data_part in data:
-        if chunk_size >= target_chunk_size:
-            demisto.debug("reached max chunk size, sending chunk with size: {size}".format(size=chunk_size))
-            yield chunk
-            chunk = []
-            chunk_size = 0
-        data_part_size = sys.getsizeof(data_part)
-        if data_part_size >= MAX_ALLOWED_ENTRY_SIZE:
-            demisto.error(
-                "entry size {} is larger than the maximum allowed entry size {}, skipping this entry".format(data_part_size,
-                                                                                                             MAX_ALLOWED_ENTRY_SIZE))
-            continue
-        chunk.append(data_part)
-        chunk_size += data_part_size
+        # if chunk_size >= target_chunk_size:
+        #     demisto.debug("reached max chunk size, sending chunk with size: {size}".format(size=chunk_size))
+        #     yield chunk
+        #     chunk = []
+        #     chunk_size = 0
+        # data_part_size = sys.getsizeof(data_part)
+        # if data_part_size >= MAX_ALLOWED_ENTRY_SIZE:
+        #     demisto.error(
+        #         "entry size {} is larger than the maximum allowed entry size {}, skipping this entry".format(data_part_size,
+        #                                                                                                      MAX_ALLOWED_ENTRY_SIZE))
+        #     continue
+        # chunk.append(data_part)
+        # chunk_size += data_part_size
+        yield [data_part]
     if chunk_size != 0:
         demisto.debug("sending the remaining chunk with size: {size}".format(size=chunk_size))
         yield chunk
