cec7ad89c8be58ad6eb66ebe2fe1da825de3c443
klevitskiy <kirilll@checkpoint.com>
2025-03-12 15:40:15 +0100

    fixes

--- Packs/Cyberint/Integrations/FeedCyberint/FeedCyberint.py
+++ Packs/Cyberint/Integrations/FeedCyberint/FeedCyberint.py
@@ -367,7 +367,7 @@ def get_indicators_command(
 
     return CommandResults(
         readable_output=human_readable,
-        outputs_prefix="Cyberint.indicators",
+        outputs_prefix="Cyberint",
         outputs_key_field="value",
         raw_response=indicators,
         outputs=indicators,
