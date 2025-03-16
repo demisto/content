924d3d53281c9e67b5598db8615da9b7361d77ba
Sapir Shuker <49246861+sapirshuker@users.noreply.github.com>
2025-03-16 16:45:18 +0200

    Update VulnDB_test.py

--- Packs/VulnDB/Integrations/VulnDB/VulnDB_test.py
+++ Packs/VulnDB/Integrations/VulnDB/VulnDB_test.py
@@ -58,7 +58,6 @@ def test_vulndb_get_cpe_command(mocker, requests_mock):
     assert len(return_results_args.outputs) == 1
     assert return_results_args.raw_response == util_load_json('test_data/vulndb_get_cpe_command.json')
     assert return_results_args.outputs_prefix == 'VulnDB.CPE.Value'
-    assert return_results_args.outputs_key_field == 'Value'
 
 
 def test_vulndb_get_vuln_report_command(mocker, requests_mock):
