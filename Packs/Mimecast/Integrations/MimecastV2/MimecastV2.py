b46d7f17c3e229c1458795798c3736e14b2ce52b
Tal <tcarmeli@paloaltonetworks.com>
2025-03-25 10:49:32 +0200

    delete debug

--- Packs/Mimecast/Integrations/MimecastV2/MimecastV2.py
+++ Packs/Mimecast/Integrations/MimecastV2/MimecastV2.py
@@ -152,8 +152,6 @@ def fetch_held_messages_with_pagination(api_endpoint: str, data: list, limit: in
     len_of_results = 0
     results = []
     dropped = 0
-    if current_next_page:  # For debugging
-        demisto.debug(f"current_next_page exists with value {current_next_page}")
     next_page = current_next_page or ''
     while True:
         pagination = {'pageSize': limit}
