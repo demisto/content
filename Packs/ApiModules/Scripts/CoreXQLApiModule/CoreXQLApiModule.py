b3b904470e44c817c0244f6139e1871b603dd0ad
Tal <tcarmeli@paloaltonetworks.com>
2025-03-25 15:31:59 +0200

    first commit

--- Packs/ApiModules/Scripts/CoreXQLApiModule/CoreXQLApiModule.py
+++ Packs/ApiModules/Scripts/CoreXQLApiModule/CoreXQLApiModule.py
@@ -654,7 +654,7 @@ def start_xql_query_polling_command(client: CoreClient, args: dict) -> Union[Com
         # the 'start_xql_query' function failed because it reached the maximum allowed number of parallel running queries.
         # running the command again using polling with an interval of 'interval_in_secs' seconds.
         command_results = CommandResults()
-        interval_in_secs = int(args.get('interval_in_seconds', 5))
+        interval_in_secs = int(args.get('interval_in_seconds', 20))
         scheduled_command = ScheduledCommand(command='xdr-xql-generic-query', next_run_in_seconds=interval_in_secs,
                                              args=args, timeout_in_seconds=600)
         command_results.scheduled_command = scheduled_command
