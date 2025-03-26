f5c5533054935c10103f5356391801091918ae9f
ArikDay <aday@paloaltonetworks.com>
2025-03-26 16:52:54 +0200

    fix

--- Packs/CommonScripts/Scripts/JSONDiff/JSONDiff_test.py
+++ Packs/CommonScripts/Scripts/JSONDiff/JSONDiff_test.py
@@ -1,61 +1,50 @@
+import pytest
+from unittest.mock import MagicMock
 from JSONDiff import compare_jsons
 
+# Mock demisto
+demisto = MagicMock()
 
-def test_compare_jsons_changed():
+
+def test_compare_jsons_simple():
     json1 = {"a": 1, "b": 2}
-    json2 = {"a": 1, "b": 3}
-
-    expected_output = {
-        "changed": [
-            {"field": "b", "from": 2, "to": 3}
-        ],
-        "added": [],
-        "removed": []
-    }
-
-    assert compare_jsons(json1, json2) == expected_output
-
-
-def test_compare_jsons_added():
-    json1 = {"a": 1}
-    json2 = {"a": 1, "b": 2}
-
-    expected_output = {
+    json2 = {"a": 1, "c": 3}
+    result = compare_jsons(json1, json2)
+    assert result == {
         "changed": [],
-        "added": [
-            {"field": "b", "value": 2}
-        ],
-        "removed": []
+        "added": [{"field": "c", "value": 3}],
+        "removed": [{"field": "b", "value": 2}]
     }
-
-    assert compare_jsons(json1, json2) == expected_output
-
-
-def test_compare_jsons_removed():
-    json1 = {"a": 1, "b": 2}
-    json2 = {"a": 1}
-
-    expected_output = {
-        "changed": [],
-        "added": [],
-        "removed": [
-            {"field": "b", "value": 2}
-        ]
-    }
-
-    assert compare_jsons(json1, json2) == expected_output
 
 
 def test_compare_jsons_nested():
-    json1 = {"a": 1, "b": {"c": 2, "d": 3}}
-    json2 = {"a": 1, "b": {"c": 2, "d": 4}}
+    json1 = {"a": {"b": 1, "c": 2}}
+    json2 = {"a": {"b": 1, "d": 3}}
+    result = compare_jsons(json1, json2)
+    assert result == {
+        "changed": [],
+        "added": [{"field": "a.d", "value": 3}],
+        "removed": [{"field": "a.c", "value": 2}]
+    }
 
-    expected_output = {
+
+def test_compare_jsons_changed():
+    json1 = {"a": 1, "b": {"c": 2}}
+    json2 = {"a": 2, "b": {"c": 2}}
+    result = compare_jsons(json1, json2)
+    assert result == {
         "changed": [
-            {"field": "b.d", "from": 3, "to": 4}
+            {"field": "a", "from": 1, "to": 2}
         ],
         "added": [],
         "removed": []
     }
 
-    assert compare_jsons(json1, json2) == expected_output
+
+@pytest.mark.parametrize("json1,json2,expected", [
+    ({"a": [1, 2]}, {"a": [1, 3]}, {"changed": [{"field": "a", "from": [1, 2], "to": [1, 3]}], "added": [], "removed": []}),
+    ({"a": None}, {"a": 1}, {"changed": [{"field": "a", "from": None, "to": 1}], "added": [], "removed": []}),
+    ({"a": True}, {"a": False}, {"changed": [{"field": "a", "from": True, "to": False}], "added": [], "removed": []}),
+])
+def test_compare_jsons_various_types(json1, json2, expected):
+    assert compare_jsons(json1, json2) == expected
