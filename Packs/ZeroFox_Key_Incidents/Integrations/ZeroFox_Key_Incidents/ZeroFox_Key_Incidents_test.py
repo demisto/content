1d3cd0552241da5db19e6b86122168461f346e8e
Diego Ramirez R <dramirez@zerofox.com>
2025-03-27 11:11:28 -0300

    add key incident client (#145)

--- Packs/ZeroFox_Key_Incidents/Integrations/ZeroFox_Key_Incidents/ZeroFox_Key_Incidents_test.py
+++ Packs/ZeroFox_Key_Incidents/Integrations/ZeroFox_Key_Incidents/ZeroFox_Key_Incidents_test.py
@@ -1,12 +1,30 @@
 import json
-from ZeroFox_Key_Incidents import ZFClient
+from urllib.parse import urlencode
+
+import pytest
+from ZeroFox_Key_Incidents import KeyIncident, ZeroFox
 
 BASE_URL = "https://api.zerofox.com"
-OK_CODES = (200, 201)
+OK_CODES = (200,)
 TOKEN = "token"
 
 KEY_INCIDENTS_ENDPOINT = "/cti/key-incidents/"
 CTI_TOKEN_ENDPOINT = "/auth/token/"
+
+
+@pytest.fixture
+def zerofox() -> ZeroFox:
+    return ZeroFox(
+        base_url=BASE_URL,
+        ok_codes=OK_CODES,
+        username='',
+        token=TOKEN,
+    )
+
+
+def load_json(file: str):
+    with open(file) as f:
+        return json.load(f)
 
 
 def util_load_json(path):
@@ -14,43 +32,51 @@ def util_load_json(path):
         return json.loads(f.read())
 
 
-def build_zf_client(token=TOKEN) -> ZFClient:
-    return ZFClient(
-        base_url=BASE_URL,
-        ok_codes=OK_CODES,
-        username="",
-        token=token,
+def test_get_key_incidents(requests_mock, zerofox, mocker):
+    requests_mock.post("/auth/token/", json={"access": "token"})
+    start_time = "2022-05-24"
+    end_time = "2022-05-25"
+    first_page_response = load_json(
+        "test_data/key_incidents/key_incidents_response_first_page.json",
+    )
+    first_url_params = urlencode(
+        {
+            'Tags': 'Key Incident',
+            'updated_after': start_time,
+            'updated_before': end_time,
+            'ordering': 'updated'
+        }
+    )
+    requests_mock.get(
+        f"{KEY_INCIDENTS_ENDPOINT}?{first_url_params}",
+        json=first_page_response,
+    )
+    second_page_response = load_json(
+        "test_data/key_incidents/key_incidents_response_second_page.json"
+    )
+    second_url_params = urlencode(
+        {
+            'Tags': 'Key Incident',
+            'updated_after': start_time,
+            'updated_before': end_time,
+            'ordering': 'updated',
+            'cursor': 'nextPageCursor'
+        }
+    )
+    requests_mock.get(
+        f"{KEY_INCIDENTS_ENDPOINT}?{second_url_params}",
+        json=second_page_response,
     )
 
+    expected = [
+        KeyIncident.from_dict(ki)
+        for ki in load_json("test_data/key_incidents/parsed_key_incidents.json").get("key_incidents")
+    ]
+    ki = zerofox.get_key_incidents(
+        start_time=start_time, end_time=end_time)
 
-def test_toy(mocker):
-    """
-    Given:
-        - An valid token and user
-    When:
-        - Getting a CTI token
-    Then:
-        - It should return a CTI token
-    """
-    client = build_zf_client()
-    fake_token = "valid_token"
-
-    mock_post = mocker.patch.object(
-        client._session, "request", return_value=mocker.Mock(status_code=200, json=lambda: {"access": fake_token})
-    )
-
-    token = client.get_cti_authorization_token()
-
-    assert token == fake_token
-    mock_post.assert_called_once_with(
-        "POST",
-        f"{BASE_URL}{CTI_TOKEN_ENDPOINT}",
-        verify=True,
-        params=None,
-        data=None,
-        json={"username": "", "password": "token"},
-        files=None,
-        headers=None,
-        auth=None,
-        timeout=60.0,
+    assert len(ki) == len(expected)
+    assert (
+        sorted(ki, key=lambda x: x.incident_id)
+        == sorted(expected, key=lambda x: x.incident_id)
     )
