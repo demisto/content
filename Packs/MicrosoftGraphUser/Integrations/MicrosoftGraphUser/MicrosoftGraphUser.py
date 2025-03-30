d89fb20550271e1563db4c9aabcc784b4f1f6052
Shir2611 <smatathias@paloaltonetworks.com>
2025-03-30 17:59:13 +0300

    test 1.0

--- Packs/MicrosoftGraphUser/Integrations/MicrosoftGraphUser/MicrosoftGraphUser.py
+++ Packs/MicrosoftGraphUser/Integrations/MicrosoftGraphUser/MicrosoftGraphUser.py
@@ -277,7 +277,7 @@ def test_function(client, _):
         if demisto.command() == 'test-module':
             if client.ms_client.grant_type != CLIENT_CREDENTIALS:
                 # cannot use test module due to the lack of ability to set refresh token to integration context
-                # for self deployed app 
+                # for self deployed app  
                 raise Exception("When using a self-deployed configuration with authorization code and redirect uri, "
                                 "Please enable the integration and run the !msgraph-user-test command in order to test it")
         else:
