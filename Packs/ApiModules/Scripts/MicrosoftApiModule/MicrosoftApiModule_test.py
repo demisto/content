99b86eedab957e09548b18ed7b1dfaec6246aedc
merit-maita <meretmaayta@gmail.com>
2025-04-03 01:50:25 +0300

    pre-commit

--- Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule_test.py
+++ Packs/ApiModules/Scripts/MicrosoftApiModule/MicrosoftApiModule_test.py
@@ -246,6 +246,7 @@ def test_page_not_found_error(mocker):
 
 def test_epoch_seconds(mocker):
     from datetime import datetime as date_time
+
     mocker.patch.object(MicrosoftClient, "_get_utcnow", return_value=date_time(2019, 12, 24, 14, 12, 0, 586636))
     mocker.patch.object(MicrosoftClient, "_get_utc_from_timestamp", return_value=date_time(1970, 1, 1, 0, 0))
     integer = MicrosoftClient.epoch_seconds()
