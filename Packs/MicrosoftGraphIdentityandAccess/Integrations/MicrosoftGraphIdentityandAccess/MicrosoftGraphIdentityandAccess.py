e7049e3db3b4c32fc343d51a1219972d047fb7a4
hyaffe <hyaffe@paloaltonetworks.com>
2025-04-03 17:16:49 +0300

    first commit

--- Packs/MicrosoftGraphIdentityandAccess/Integrations/MicrosoftGraphIdentityandAccess/MicrosoftGraphIdentityandAccess.py
+++ Packs/MicrosoftGraphIdentityandAccess/Integrations/MicrosoftGraphIdentityandAccess/MicrosoftGraphIdentityandAccess.py
@@ -363,6 +363,7 @@ def test_connection(client: Client) -> str:  # pragma: no cover
     return "✅ Success!"
 
 
+
 def list_directory_roles(ms_client: Client, args: dict) -> CommandResults:  # pragma: no cover
     """Lists all service principals
 
