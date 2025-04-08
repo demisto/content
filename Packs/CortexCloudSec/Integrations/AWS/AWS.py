8fb62b78450ba420eaa18f917e8554c650329c78
Anmol Baansal <abaansal@paloaltonetworks.com>
2025-04-08 13:39:25 +0530

    New pack - Cortex CloudSec

---
+++ Packs/CortexCloudSec/Integrations/AWS/AWS.py
@@ -0,0 +1,198 @@
+import demistomock as demisto
+from AWSApiModule import *  # noqa: E402
+from CommonServerPython import *
+from http import HTTPStatus
+from datetime import date
+
+def test_module(client) -> str:
+    return "ok"
+
+# =================== #
+# Helpers
+# =================== #
+class DatetimeEncoder(json.JSONEncoder):
+    # pylint: disable=method-hidden
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%dT%H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+def get_client(params, command_args):
+    aws_role_name = params.get('roleName')
+    accountId = command_args.get('account_id')
+    aws_role_arn = f'arn:aws:iam::{accountId}:role/{aws_role_name}'
+    
+    aws_role_session_name = params.get('roleSessionName')
+    aws_role_session_duration = params.get('sessionDuration')
+    verify_certificate = not params.get('insecure', True)
+    timeout = params.get('timeout')
+    retries = params.get('retries', 5)
+    sts_endpoint_url = params.get('sts_endpoint_url', None)
+    endpoint_url = params.get('endpoint_url', None)
+    
+    aws_default_region = aws_role_policy = aws_access_key_id = aws_secret_access_key = aws_session_token = None
+  
+    return AWSClient(
+            aws_default_region, aws_role_arn, aws_role_session_name, aws_role_session_duration,
+            aws_role_policy, aws_access_key_id, aws_secret_access_key, verify_certificate, timeout,
+            retries, aws_session_token, sts_endpoint_url, endpoint_url
+        )
+
+# =================== #
+# S3 commands
+# =================== #
+
+def put_public_access_block(aws_client: AWSClient, args: Dict[str, Any]) -> CommandResults:
+    client_session = aws_client.aws_session(service='s3', region=args.get('region'))
+    try:
+        response = client_session.get_public_access_block(Bucket=args.get('bucket'))
+        public_access_block_configuration = response.get('PublicAccessBlockConfiguration')
+        kwargs = {
+            'BlockPublicAcls': public_access_block_configuration.get('BlockPublicAcls'),
+            'IgnorePublicAcls': public_access_block_configuration.get('IgnorePublicAcls'),
+            'BlockPublicPolicy': public_access_block_configuration.get('BlockPublicPolicy'),
+            'RestrictPublicBuckets': public_access_block_configuration.get('RestrictPublicBuckets')
+        }
+    except client_session.exceptions.NoSuchEntityException:
+        kwargs = {}
+        
+    if args.get('block_public_acls'):
+        kwargs.update({ 'BlockPublicAcls': argToBoolean(args.get('block_public_acls')) })
+    if args.get('ignore_public_acls'):
+        kwargs.update({ 'IgnorePublicAcls': argToBoolean(args.get('ignore_public_acls')) })
+    if args.get('block_public_policy'):
+        kwargs.update({ 'BlockPublicPolicy': argToBoolean(args.get('block_public_policy')) })
+    if args.get('restrict_public_buckets'):
+        kwargs.update({ 'RestrictPublicBuckets': argToBoolean(args.get('restrict_public_buckets')) })
+
+
+    response = client_session.put_public_access_block(Bucket=args.get('bucket'),
+                                                      PublicAccessBlockConfiguration=kwargs)
+
+    if response['ResponseMetadata']['HTTPStatusCode'] == HTTPStatus.OK:
+        return CommandResults(
+            readable_output=f"Successfully applied public access block to the {args.get('bucket')} bucket")
+    return CommandResults(readable_output=f"Couldn't apply public access block to the {args.get('bucket')} bucket")
+
+# =================== #
+# IAM commands
+# =================== #
+
+def get_account_password_policy(aws_client: AWSClient, args: Dict[str, Any]) -> CommandResults:
+    client_session = aws_client.aws_session(service='iam', region=args.get('region'))
+    response = client_session.get_account_password_policy()
+    data = json.loads(json.dumps(response['PasswordPolicy'], cls=DatetimeEncoder))
+    data.update({'AccountId': args.get('account_id')})
+    
+    human_readable = tableToMarkdown('AWS IAM Account Password Policy', data)
+    
+    return CommandResults(outputs=data, readable_output=human_readable, outputs_prefix='AWS.IAM.PasswordPolicy',
+                          outputs_key_field='AccountId')
+    
+def update_account_password_policy(aws_client: AWSClient, args: Dict[str, Any]) -> CommandResults:
+    client_session = aws_client.aws_session(service='iam', region=args.get('region'))
+    try:
+        response = client_session.get_account_password_policy()
+        kwargs = response['PasswordPolicy']
+    except client_session.exceptions.NoSuchEntityException:
+        kwargs = {}
+    # ExpirePasswords is part of the response but cannot be included
+    # in the request
+    if 'ExpirePasswords' in kwargs:
+        kwargs.pop('ExpirePasswords')
+    if args.get('minimum_password_length'):
+        kwargs.update({'MinimumPasswordLength': int(args.get('minimum_password_length'))})
+    if args.get('require_symbols'):
+        kwargs.update({'RequireSymbols': args.get('require_symbols') == 'True'})
+    if args.get('require_numbers'):
+        kwargs.update({'RequireNumbers': args.get('require_numbers') == 'True'})
+    if args.get('require_uppercase_characters'):
+        kwargs.update(
+            {'RequireUppercaseCharacters': args.get('require_uppercase_characters') == 'True'})
+    if args.get('require_lowercase_characters'):
+        kwargs.update(
+            {'RequireLowercaseCharacters': args.get('require_lowercase_characters') == 'True'})
+    if args.get('allow_users_to_change_password'):
+        kwargs.update(
+            {'AllowUsersToChangePassword': args.get('allow_users_to_change_password') == 'True'})
+    if args.get('max_password_age'):
+        kwargs.update({'MaxPasswordAge': int(args.get('max_password_age'))})
+    if args.get('password_reuse_prevention'):
+        kwargs.update({'PasswordReusePrevention': int(args.get('password_reuse_prevention'))})
+    if args.get('hard_expiry'):
+        kwargs.update({'HardExpiry': args.get('hard_expiry') == 'True'})
+    response = client_session.update_account_password_policy(**kwargs)
+    
+    if response['ResponseMetadata']['HTTPStatusCode'] == HTTPStatus.OK:
+        return CommandResults(readable_output=f"Successfully updated account password policy for account: {args.get('account_id')}")
+    else:
+        return CommandResults(readable_output=f"Couldn't updated account password policy for account: {args.get('account_id')}")
+        
+# =================== #
+# EC2 commands
+# =================== #
+def aws_ec2_instance_metadata_options_modify(aws_client: AWSClient, args: Dict[str, Any]) -> CommandResults:
+    client_session = aws_client.aws_session(service='ec2', region=args.get('region'))
+    
+    kwargs = {'InstanceId': args.get('instance_id')}
+    
+    if args.get('http_tokens'):
+        kwargs.update({'HttpTokens': args.get('http_tokens')})
+    if args.get('http_endpoint'):
+        kwargs.update({'HttpEndpoint': args.get('http_endpoint')})
+
+    response = client_session.modify_instance_metadata_options(**kwargs)
+    
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        demisto.results("The EC2 instance metadata was updated")
+        
+    if response['ResponseMetadata']['HTTPStatusCode'] == HTTPStatus.OK:
+        return CommandResults(readable_output=f"Successfully updated EC2 instance metadata for {args.get('instance_id')}")
+    else:
+        return CommandResults(readable_output=f"Couldn't updated public EC2 instance metadata for {args.get('instance_id')}")
+    
+
+# =================== #
+# MAIN
+# =================== #
+def main():
+
+    params = demisto.params()
+    demisto.debug(f"Params: {params}")
+
+    command = demisto.command()
+    command_args = demisto.args()
+    demisto.debug(f"Command: {command}")
+    demisto.debug(f"Args: {command_args}")
+    
+    aws_client = get_client(params, command_args)
+
+    result = ''
+    try:
+        match command:
+            case "aws-s3-public-access-block-update":
+                result = put_public_access_block(aws_client, command_args)
+            case 'aws-iam-account-password-policy-get':
+                result = get_account_password_policy(aws_client, command_args)
+            case 'aws-iam-account-password-policy-update':
+                result = update_account_password_policy(aws_client, command_args)
+            case 'aws-ec2-instance-metadata-options-modify':
+                result = aws_ec2_instance_metadata_options_modify(aws_client, command_args)
+                
+            case 'test-module':
+                result = test_module(aws_client)
+            case _:
+                raise NotImplementedError(f"Command {command} is not implemented")
+
+        
+        
+        return_results(result)
+    except Exception as e:
+        return_error(f"Failed to execute {command} command.\nError:\n{str(e)}")
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):  # pragma: no cover
+    main()
---
+++ Packs/CortexCloudSec/Integrations/AWS/README.md
@@ -0,0 +1,128 @@
+Support for AWS cloud
+This integration was integrated and tested with version xx of Cortex CloudSec - AWS.
+
+## Configure Cortex CloudSec - AWS in Cortex
+
+
+| **Parameter** | **Description** | **Required** |
+| --- | --- | --- |
+| Role Name |  | True |
+| Role Session Name |  | True |
+| Role Session Duration |  | False |
+| Timeout | The time in seconds till a timeout exception is reached. You can specify just the read timeout \(for example 60\) or also the connect timeout followed after a comma \(for example 60,10\). If a connect timeout is not specified, a default of 10 second will be used. | False |
+| Retries | The maximum number of retry attempts when connection or throttling errors are encountered. Set to 0 to disable retries. The default value is 5 and the limit is 10. Note: Increasing the number of retries will increase the execution time. | False |
+| PrivateLink service URL. |  | False |
+| STS PrivateLink URL. |  | False |
+| AWS STS Regional Endpoints | Sets the AWS_STS_REGIONAL_ENDPOINTS environment variable to specify the AWS STS endpoint resolution logic. By default, this option is set to “legacy” in AWS. Leave empty if the environment variable is already set using server configuration. | False |
+| Role name for cross-organization account access | The role name used to access accounts in the organization. This role name must exist in the accounts provided in "AWS Organization accounts" and be assumable with the credentials provided. | False |
+| Use system proxy settings |  | False |
+| Trust any certificate (not secure) |  | False |
+
+## Commands
+
+You can execute these commands from the CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
+
+### aws-s3-public-access-block-update
+
+***
+Creates or modifies the PublicAccessBlock configuration for an Amazon S3 bucket.
+
+#### Base Command
+
+`aws-s3-public-access-block-update`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| account_id | The AWS account ID. | Required | 
+| region | The AWS region. | Required | 
+| bucket | The name of the Amazon S3 bucket. | Required | 
+| block_public_acls | Specifies whether Amazon S3 should block public access control lists (ACLs) for this bucket and objects in this bucket. Possible values are: True, False. | Optional | 
+| ignore_public_acls | Specifies whether Amazon S3 should ignore public ACLs for this bucket and objects in this bucket. Possible values are: True, False. | Optional | 
+| block_public_policy | Specifies whether Amazon S3 should block public bucket policies for this bucket. Possible values are: True, False. | Optional | 
+| restrict_public_buckets | Specifies whether Amazon S3 should restrict public bucket policies for this bucket. Possible values are: True, False. | Optional | 
+
+#### Context Output
+
+There is no context output for this command.
+### aws-iam-account-password-policy-get
+
+***
+Get AWS account's password policy.
+
+#### Base Command
+
+`aws-iam-account-password-policy-get`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| account_id | The AWS account ID. | Required | 
+
+#### Context Output
+
+| **Path** | **Type** | **Description** |
+| --- | --- | --- |
+| AWS.IAM.PasswordPolicy.MinimumPasswordLength | number | Minimum length to require for IAM user passwords. | 
+| AWS.IAM.PasswordPolicy.RequireSymbols | boolean | Specifies whether IAM user passwords must contain at least one of the symbols. | 
+| AWS.IAM.PasswordPolicy.RequireNumbers | boolean | Specifies whether IAM user passwords must contain at least one numeric character \(0 to 9\). | 
+| AWS.IAM.PasswordPolicy.RequireUppercaseCharacters | boolean | Specifies whether IAM user passwords must contain at least one uppercase character \(A to Z\). | 
+| AWS.IAM.PasswordPolicy.RequireLowercaseCharacters | boolean | Specifies whether IAM user passwords must contain at least one lowercase character \(a to z\). | 
+| AWS.IAM.PasswordPolicy.AllowUsersToChangePassword | boolean | Specifies whether IAM users are allowed to change their own password. | 
+| AWS.IAM.PasswordPolicy.ExpirePasswords | boolean | Indicates whether passwords in the account expire. | 
+| AWS.IAM.PasswordPolicy.MaxPasswordAge | number | The number of days that an IAM user password is valid. | 
+| AWS.IAM.PasswordPolicy.PasswordReusePrevention | number | Specifies the number of previous passwords that IAM users are prevented from reusing. | 
+| AWS.IAM.PasswordPolicy.HardExpiry | boolean | Specifies whether IAM users are prevented from setting a new password via the Amazon Web Services Management Console after their password has expired. | 
+
+### aws-ec2-instance-metadata-options-modify
+
+***
+Modify the EC2 instance metadata parameters on a running or stopped instance.
+
+#### Base Command
+
+`aws-ec2-instance-metadata-options-modify`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| account_id | The AWS account ID. | Required | 
+| region | The AWS region. | Required | 
+| instance_id | The ID of the instance. | Required | 
+| http_tokens | Indicates whether IMDSv2 is required. Possible values are: optional, required. | Optional | 
+| http_endpoint | The AWS region. Possible values are: disabled, enabled. | Optional | 
+
+#### Context Output
+
+There is no context output for this command.
+### aws-iam-account-password-policy-update
+
+***
+Create/update password policy.
+
+#### Base Command
+
+`aws-iam-account-password-policy-update`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| account_id | The AWS account ID. | Required | 
+| minimum_password_length | The minimum number of characters allowed in an IAM user password. Possible values are: . | Optional | 
+| require_symbols | Specifies whether IAM user passwords must contain at least one of the non-alphanumeric characters. Can be "True" or "False". Possible values are: True, False. | Optional | 
+| require_numbers | Specifies whether IAM user passwords must contain at least one numeric character (0 to 9). Can be "True" or "False". Possible values are: True, False. | Optional | 
+| require_uppercase_characters | Specifies whether IAM user passwords must contain at least one uppercase character from the ISO basic Latin alphabet (A to Z). Can be "True" or "False". Possible values are: True, False. | Optional | 
+| require_lowercase_characters | Specifies whether IAM user passwords must contain at least one lowercase character from the ISO basic Latin alphabet (a to z). Can be "True" or "False". Possible values are: True, False. | Optional | 
+| allow_users_to_change_password | Allows all IAM users in your account to use the AWS Management Console to change their own passwords. Can be "True" or "False". Possible values are: True, False. | Optional | 
+| max_password_age | The number of days that an IAM user password is valid. Possible values are: . | Optional | 
+| password_reuse_prevention | Specifies the number of previous passwords that IAM users are prevented from reusing. Possible values are: . | Optional | 
+| hard_expiry | Prevents IAM users from setting a new password after their password has expired. Can be "True" or "False". Possible values are: True, False. | Optional | 
+
+#### Context Output
+
+There is no context output for this command.
