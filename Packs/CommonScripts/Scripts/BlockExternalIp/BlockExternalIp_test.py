fd43194d373faf69f9dc9f910d740b6200e00ace
RotemAmit <ramit@paloaltonetworks.com>
2025-04-09 11:20:10 +0300

    removed the checkpoint flow

--- Packs/CommonScripts/Scripts/BlockExternalIp/BlockExternalIp_test.py
+++ Packs/CommonScripts/Scripts/BlockExternalIp/BlockExternalIp_test.py
@@ -204,25 +204,6 @@ def test_update_brands_to_run(mocker):
    assert expected_updated_brands_to_run == result_updated_brands_to_run
 
 
-
-
-def test_checkpoint_object_names_to_members():
-   """
-   Given:
-      - The command context and the ips.
-   When:
-      - Running the script block-external-ip for the checkpoint brand, getting the names of the current ips objects names.
-   Then:
-      - The correct object names.
-   """
-   from BlockExternalIp import checkpoint_object_names_to_members
-   context = util_load_json('test_data/checkpoint_responses.json').get('show_object_name_to_members')
-   ip_list = ['1.1.1.1', '1.2.2.2']
-   expected_names = ['1.1.1.1', '1.2.2.2']
-   result = checkpoint_object_names_to_members(context, ip_list)
-   assert expected_names == result
-
-
 def test_prisma_sase_candidate_config_push_false():
     """
     Given:
