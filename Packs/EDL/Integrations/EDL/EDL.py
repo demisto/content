bec20476f62db3165b238885eb5f034dd4a34e26
MLainer1 <mlainer@paloaltonetworks.com>
2025-04-10 14:05:55 +0300

    EDL - remove excessive logs

--- Packs/EDL/Integrations/EDL/EDL.py
+++ Packs/EDL/Integrations/EDL/EDL.py
@@ -362,7 +362,6 @@ def get_indicators_to_format(indicator_searcher: IndicatorsSearcher, request_arg
         for ioc_res in indicator_searcher:
             fetched_iocs = ioc_res.get("iocs") or []
             for ioc in fetched_iocs:
-                demisto.debug(f"Parsing the following indicator: {ioc.get('value')}")
 
                 ioc_counter += 1
                 if request_args.out_format == FORMAT_PROXYSG:
