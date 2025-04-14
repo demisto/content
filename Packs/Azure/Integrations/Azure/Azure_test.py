13d68afd37fb3e6c4db30a6bb3207fd56ea1f90c
iapt@paloaltonetworks.com <iapt@paloaltonetworks.com>
2025-04-14 09:26:27 +0300

    pre-commit fixes

--- Packs/Azure/Integrations/Azure/Azure_test.py
+++ Packs/Azure/Integrations/Azure/Azure_test.py
@@ -10,7 +10,6 @@ You must add at least a Unit Test function for every XSOAR command
 you are implementing with your integration
 """
 
-from demisto_sdk.commands.common.handlers import JSON_Handler
 
 import json
 
@@ -19,23 +18,3 @@ def util_load_json(path):
     with open(path, encoding="utf-8") as f:
         return json.loads(f.read())
 
-
-# TODO: REMOVE the following dummy unit test function
-def test_baseintegration_dummy():
-    """Tests helloworld-say-hello command function.
-
-    Checks the output of the command function with the expected output.
-
-    No mock is needed here because the say_hello_command does not call
-    any external API.
-    """
-    from BaseIntegration import Client, baseintegration_dummy_command
-
-    client = Client(base_url="some_mock_url", verify=False)
-    args = {"dummy": "this is a dummy response", "dummy2": "a dummy value"}
-    response = baseintegration_dummy_command(client, args)
-
-    assert response.outputs == args
-
-
-# TODO: ADD HERE unit tests for every command
