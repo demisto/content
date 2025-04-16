d1997c2bb2fe2d201de7696eb198aac51d3c8c4a
MLainer1 <mlainer@paloaltonetworks.com>
2025-04-16 15:00:37 +0300

    fix test

--- Packs/PAN-OS/Integrations/Panorama/Panorama_test.py
+++ Packs/PAN-OS/Integrations/Panorama/Panorama_test.py
@@ -2750,7 +2750,6 @@ def test_get_url_category_multiple_categories_for_url(mocker):
             "shareware-and-freeware",
             "online-storage-and-backup",
             "low-risk",
-            "not-resolved",
         ]
 
     # category with highest dbot-score
