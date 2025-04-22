a6fb715aa48718c36bb460cdb30b151b45ad483d
TOUFIKI Zakarya <zakarya.toufiki@sekoia.io>
2025-04-22 10:25:01 +0000

    Add some tests

--- Packs/SekoiaXDR/Integrations/SekoiaXDR/SekoiaXDR.py
+++ Packs/SekoiaXDR/Integrations/SekoiaXDR/SekoiaXDR.py
@@ -1005,7 +1005,7 @@ def get_remote_data_command(
             and (investigation["status"] == 1)
         ):
             demisto.debug(
-                f"Alert {alert_short_id} with status {alert_status} was reopened in Sekoia, "\
+                f"Alert {alert_short_id} with status {alert_status} was reopened in Sekoia, "
                 f"reopening incident {incident_id} in XSOAR"
             )
             entries = [
