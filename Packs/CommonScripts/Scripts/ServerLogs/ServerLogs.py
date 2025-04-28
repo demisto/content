2a110b804cf38809b200bf61d7db46e66b5b17f3
noydavidi <nodavidi.paloaltonetworks.com>
2025-04-28 15:56:26 +0300

    small fixes

--- Packs/CommonScripts/Scripts/ServerLogs/ServerLogs.py
+++ Packs/CommonScripts/Scripts/ServerLogs/ServerLogs.py
@@ -28,8 +28,10 @@ def main():
         execute_ssh_command()
     except ValueError as e:
         demisto.error(str(e))
-        return_error("The script could not execute the `ssh` command. Please create an instance of the "
-                     "`RemoteAccess v2` integration and try to run the script again.")
+        return_error(
+            "The script could not execute the `ssh` command. Please create an instance of the "
+            "`RemoteAccess v2` integration and try to run the script again."
+        )
 
 
 if __name__ in ("__builtin__", "builtins", "__main__"):
--- Packs/CommonScripts/Scripts/ServerLogsDocker/ServerLogsDocker.py
+++ Packs/CommonScripts/Scripts/ServerLogsDocker/ServerLogsDocker.py
@@ -21,8 +21,10 @@ def main():
 
     except ValueError as e:
         demisto.error(str(e))
-        return_error("The script could not execute the `ssh` command. Please create an instance of the "
-                     "`RemoteAccess v2` integration and try to run the script again.")
+        return_error(
+            "The script could not execute the `ssh` command. Please create an instance of the "
+            "`RemoteAccess v2` integration and try to run the script again."
+        )
 
 
 if __name__ in ("__builtin__", "builtins", "__main__"):
