084aa076760ff63f6576203ed253533285efa27c
rshunim <rshunim@paloaltonetworks.com>
2025-04-29 14:05:56 +0300

    works no polling

--- Packs/Core/Scripts/AgentixRetrieveData/AgentixRetrieveData.py
+++ Packs/Core/Scripts/AgentixRetrieveData/AgentixRetrieveData.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 import urllib3
+from CoreXQLApiModule import *
 
 urllib3.disable_warnings()
 
@@ -12,21 +15,27 @@ def shorten_text(text: str) -> str:
     return text  # Return original if not exactly two words
 
 
-def search_for_indicator(args: dict) -> Union[CommandResults, list]:
+def search_for_indicator(args: dict):
     time_frame: str = args["time_frame"]
-    data_set: str = args["data_set"]
+    data_set: str = args.get("data_set", "xdr_data")
     indicator: str = args["indicator"]
+    query_name: str = args["query_name"]
 
     time_frame_for_query: str = shorten_text(time_frame)
+    query: str = f'config timeframe = {time_frame_for_query} |search "{indicator}" dataset = {data_set}'
 
-    return demisto.executeCommand(command="xdr-xql-generic-query",
-                                  args={"query": f"config timeframe = {time_frame_for_query} |"
-                                                 f" search {indicator} dataset = {data_set}"})
+    res = demisto.executeCommand(command="xdr-xql-generic-query", args={"query": query, "query_name": query_name})
+    query_id = res[0]["Metadata"]["pollingArgs"]["query_id"]
+    while res[0]["Contents"]["status"] == "PENDING":
+        demisto.results("Query is still running, it may take a little while...")
+        sleep(30)
+        res = demisto.executeCommand(command="xdr-xql-get-query-results", args={"query_id": query_id})
+    return_results(res)
 
 
 def main():
     try:
-        return search_for_indicator(demisto.args())
+        search_for_indicator(demisto.args())
     except Exception as e:
         return_error("Error occurred while executing search_for_indicator. Exception info:\n" + str(e))
 
