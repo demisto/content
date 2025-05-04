ae8c7fee357c0aa27b1cd47be36ac4386a5a550f
nbensalm-palo <nbensalmon@paloaltonetworks.com>
2025-05-04 15:54:46 +0300

    First Logic implementation

--- Packs/AdminByRequest/Integrations/AdminByRequest/AdminByRequest.py
+++ Packs/AdminByRequest/Integrations/AdminByRequest/AdminByRequest.py
@@ -1,30 +1,30 @@
-"""Base Integration for Cortex XSOAR (aka Demisto)
-
-This is an empty Integration with some basic structure according
-to the code conventions.
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-Developer Documentation: https://xsoar.pan.dev/docs/welcome
-Code Conventions: https://xsoar.pan.dev/docs/integrations/code-conventions
-Linting: https://xsoar.pan.dev/docs/integrations/linting
-
-This is an empty structure file. Check an example at;
-https://github.com/demisto/content/blob/master/Packs/HelloWorld/Integrations/HelloWorld/HelloWorld.py
-
-"""
-
+'''
+Event Collector Source file for AdminByRequest API.
+'''
+from enum import EnumMeta
 from typing import Any, Dict, Optional
 
 import demistomock as demisto
 import urllib3
-from CommonServerPython import *  # noqa # pylint: disable=unused-wildcard-import
-from CommonServerUserPython import *  # noqa
+from CommonServerPython import *
+from CommonServerUserPython import *
 
 # Disable insecure warnings
 urllib3.disable_warnings()
 
 """ CONSTANTS """
+
+VENDOR = 'Admin'
+PRODUCT = 'ByRequest'
+MAX_FETCH_AUDIT_LIMIT = 50000
+MAX_FETCH_EVENT_LIMIT = 50000
+MAX_FETCH_REQUEST_LIMIT = 5000
+DEFAULT_TAKE_AUDIT_LOGS = 10000
+DEFAULT_TAKE_REQUESTS = 1000
+DEFAULT_TAKE_EVENTS = 10000
+AUDIT_LOG_CALL_SUFFIX = "auditlog"
+REQUESTS_CALL_SUFFIX = "requests"
+EVENTS_CALL_SUFFIX = "events"
 
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"  # ISO8601 format with UTC, default in XSOAR
 
@@ -33,36 +33,192 @@ DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"  # ISO8601 format with UTC, default in XSOAR
 
 class Client(BaseClient):
     """Client class to interact with the service API
-
-    This Client implements API calls, and does not contain any XSOAR logic.
-    Should only do requests and return data.
-    It inherits from BaseClient defined in CommonServer Python.
-    Most calls use _http_request() that handles proxy, SSL verification, etc.
-    For this  implementation, no special attributes defined
     """
-
-    # TODO: REMOVE the following dummy function:
-    def baseintegration_dummy(
-        self, dummy: str, dummy2: Optional[int]
-    ) -> Dict[str, str]:
-        """Returns a simple python dict with the information provided
-        in the input (dummy).
-
-        Args:
-            dummy: string to add in the dummy dict that is returned. This is a required argument.
-            dummy2: int to limit the number of results. This is an optional argument.
-
-        Returns:
-            The dict with the arguments
+    def __init__(self, base_url: str, api_key: str, verify: bool, use_proxy: bool) -> None:
         """
-        return {"dummy": dummy, "dummy2": dummy2}
+          Prepare constructor for Client class.
 
-    # TODO: ADD HERE THE FUNCTIONS TO INTERACT WITH YOUR PRODUCT API
+          Calls the constructor of BaseClient class and updates the header with the authentication token.
 
+          Args:
+              # TODO: do I need this url? im not sure
+              base_url: The url of ExtraHop instance.
+              api_key: The Api key for AdminByRequest API - specific for every licensing.
+              verify: True if verify SSL certificate is checked in integration configuration, False otherwise.
+              use_proxy: True if the proxy server needs to be used, False otherwise.
+          """
+
+        super().__init__(base_url=base_url, verify=verify, proxy=use_proxy)
+
+        self._api_key = api_key
+        self._headers: dict[str, Any] = {
+            "apiKey": self._api_key
+        }
+
+        # TODO: Is there expiration for the API KEY? it dosent seem that way - make sure of that
+
+
+    def retrieve_audit_log(self, params: dict) -> dict:
+        """Retrieve audit log from AdminByRequest API.
+        """
+        demisto.debug(f"Fetching audit logs events from with params={params}.")
+        url_suffix = "/auditlog/delta"
+        return self._inner_http_request(url_suffix=url_suffix, params=params)
+
+    def retrieve_request(self, params: dict) -> dict:
+        """Retrieve requests from AdminByRequest API.
+        """
+        url_suffix = "/requests"
+        return self._inner_http_request(url_suffix=url_suffix, params=params)
+
+    def retrieve_events(self, params: dict) -> dict:
+        """Retrieve events from AdminByRequest API.
+        """
+        url_suffix = "/events"
+        return self._inner_http_request(url_suffix=url_suffix, params=params)
+
+    def inner_http_request(self, url_suffix: str, params:dict) -> dict:
+        """Retrieve the detections from Reveal(X).
+        """
+        return self._http_request(
+            "GET", url_suffix="/" + url_suffix, params=params, resp_type="json"
+        )
 
 """ HELPER FUNCTIONS """
 
-# TODO: ADD HERE ANY HELPER FUNCTION YOU MIGHT NEED (if any)
+def update_values_events(suffix: str) -> tuple[str, str]:
+
+    if suffix == AUDIT_LOG_CALL_SUFFIX:
+        source_log_type = "auditlog"
+        time_field = "startTimeUTC"
+    elif suffix == REQUESTS_CALL_SUFFIX:
+        source_log_type = "request"
+        time_field = "requestTime"
+    else:
+        time_field = "startTimeUTC"
+        source_log_type = "event"
+
+    return source_log_type, time_field
+
+
+
+def retrieve_from_api(client: Client, suffix: str, params:dict) -> List[dict[str, Any]]:
+    events = list(client.inner_http_request(url_suffix=suffix, params=params))
+    update_values_events(events=events, suffix=suffix)
+    return events
+
+def fetch_audit_log(client: Client, last_run: dict) -> tuple[list[Any], dict]:
+    """Retrieve full audit log entries from AdminByRequest API using delta logic."""
+    today = get_current_time().strftime("%Y-%m-%d")
+    all_entries = []
+
+    if "start_time_audit_logs" in last_run:
+        start_time = last_run["start_time"]
+    else:
+        # Phase 1: Initial request to get timeNow
+        params = {"startdate": today, "enddate": today}
+        response = client.retrieve_audit_log(params=params)
+        entries = response.get("entries", [])
+        all_entries.extend(entries)
+        start_time = response.get("timeNow")
+
+    # Phase 2: Poll with deltaTime until no more entries
+    while True:
+        #updated params
+        params = {
+            "deltaTime": start_time
+        }
+        # API call
+        response = client.retrieve_audit_log(params=params)
+
+        start_time = response.get("timeNow")
+        new_entries = response.get("entries", [])
+        if not new_entries:
+            break
+        all_entries.extend(new_entries)
+
+    last_run["start_time_audit_logs"] = start_time
+
+    return all_entries, last_run
+
+
+def validate_fetch_events_params(last_run : dict, call_type: str, fetch_limit:int) -> tuple[dict, str, str]:
+    today = get_current_time().strftime("%Y-%m-%d")
+    # phase 1 Params  - use today date to get the last ID in the first run
+    params = {"startdate": today, "enddate": today}
+
+    if call_type == AUDIT_LOG_CALL_SUFFIX:
+        suffix = "auditlog"
+        take = DEFAULT_TAKE_AUDIT_LOGS
+    elif call_type == REQUESTS_CALL_SUFFIX:
+        suffix = "requests"
+        take = DEFAULT_TAKE_REQUESTS
+    else:
+        take = DEFAULT_TAKE_EVENTS
+        suffix = "events"
+
+    key = "start_id_" + suffix
+    # Phase 2 Params: If a call has already been executed then use the last run params.
+    if last_run.get(key):
+        params = {"startid": last_run[key]}
+
+    take = min(take, fetch_limit)
+    params["take"] = take
+
+    return params, suffix, key
+
+
+def retrieve_from_api_loop(client: Client, last_run: dict, call_type: str, fetch_limit: int) ->  tuple[
+    list[dict[str, Any]], dict]:
+
+    params, suffix, last_run_key = validate_fetch_events_params(last_run, call_type, fetch_limit)
+    time_field, source_log_type = update_values_events(suffix=suffix)
+    last_id : int
+    output : list[dict[str, Any]] = []
+    while True:
+        try:
+            # API call
+            events = list(client.inner_http_request(url_suffix=suffix, params=params))
+        except DemistoException as e:
+            if e.res.status_code == 429:
+                retry_after = int(e.res.headers.get("x-ratelimit-reset", 2))
+                demisto.debug(f"Rate limit reached. Waiting {retry_after} seconds before retrying.")
+                time.sleep(retry_after)
+                continue
+            else:
+                raise e
+
+        if not events:
+            break
+
+        output : List[dict[str, Any]] = []
+
+        for event in events:
+            last_id = event["id"]
+            event["_TIME"] = time_field
+            event["source_log_type"] = source_log_type
+
+            output.append(event)
+
+            if len(output) >= fetch_limit:
+                # Safe to add a millisecond and fetch since no two events share the same timestamp.
+                last_run[last_run_key] = last_id
+                return output, last_run
+
+        # If it was the first run and we have a first run "params values"
+        remove_first_run_params(params)
+        params["startid"] = last_id
+
+    last_run[last_run_key] = last_id
+
+    return output, last_run
+
+
+def remove_first_run_params(params: dict) -> None:
+    if "startdate" in params:
+        params.pop("startdate")
+        params.pop("enddate")
+
 
 """ COMMAND FUNCTIONS """
 
