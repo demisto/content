6219d4d73cc8b1bc38a2c94553c02dbbd0a3f044
YuvHayun <yhayun@paloaltonetworks.com>
2025-05-05 10:36:39 +0300

    test

--- Packs/Zoom/Scripts/SlackAskV2/SlackAskV2.py
+++ Packs/Zoom/Scripts/SlackAskV2/SlackAskV2.py
@@ -4,198 +4,124 @@ from CommonServerPython import *
 
 from CommonServerUserPython import *
 
-STYLES_DICT = {"black": "", "green": "primary", "red": "danger"}
+STYLES_DICT = {
+    "white": "Default",
+    "blue": "Primary",
+    "red": "Danger",
+    "gray": "Disabled",
+}
 
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
-def create_blocks(text: str, entitlement: str, options: list, reply: str) -> list:
-    value = json.dumps({"entitlement": entitlement, "reply": reply})
-    blocks: list = [{"type": "section", "text": {"type": "mrkdwn", "text": text}}]
+def parse_option_text(option_text):
+    # Function to parse the option text with color information and extract the text and style
+    # Format of the option text: "<text>#<color>"
+    parts = option_text.split("#", 1)
+    if len(parts) == 2:
+        text, color = parts
+        style = STYLES_DICT.get(color.lower(), "Default")
+    else:
+        text = option_text
+        style = "Default"
+    return text, style
 
-    elements = []
-    for option in options:
-        element = {"type": "button", "text": {"type": "plain_text", "emoji": True, "text": option["text"]}, "value": value}
-        if "style" in option:
-            element["style"] = option["style"]
-        elements.append(element)
-    if elements:
-        actions = {"type": "actions", "elements": elements}
-        blocks.append(actions)
 
-    return blocks
+def generate_json(text, options, response_type):
+    if response_type == "button":
+        # Generate JSON for button type response
+        button_items = []
+        for _i, option in enumerate(options):
+            option_text, option_style = parse_option_text(option)
+            button_item = {"value": option_text, "style": option_style, "text": option_text}
+            button_items.append(button_item)
+
+        json_data = {"head": {"type": "message", "text": text}, "body": [{"type": "actions", "items": button_items}]}
+
+    elif response_type == "dropdown":
+        # Generate JSON for dropdown type response
+        select_items = []
+        for _i, option in enumerate(options):
+            option_text, _ = parse_option_text(option)
+            select_item = {"value": option_text, "text": option_text}
+            select_items.append(select_item)
+
+        json_data = {
+            "body": [
+                {
+                    "select_items": select_items,
+                    "text": text,
+                    "selected_item": {"value": select_items[0].get("value")},
+                    "type": "select",
+                }
+            ]
+        }
+
+    else:
+        raise ValueError("Invalid responseType. should be 'button' or 'dropdown'.")
+    return json_data
 
 
 def main():
-    res = demisto.executeCommand(
-        "addEntitlement",
-        {
-            "persistent": demisto.get(demisto.args(), "persistent"),
-            "replyEntriesTag": demisto.get(demisto.args(), "replyEntriesTag"),
-        },
-    )
+    demisto_args = demisto.args()
+    res = demisto.executeCommand("addEntitlement", {"persistent": demisto_args.get("persistent")})
     if isError(res[0]):
-        demisto.results(res)
-        sys.exit(0)
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
-    a = 4
+        return_results(res)
     entitlement = demisto.get(res[0], "Contents")
-    option1 = demisto.get(demisto.args(), "option1")
-    option2 = demisto.get(demisto.args(), "option2")
-    extra_options = argToList(demisto.args().get("additionalOptions", ""))
-    reply = demisto.get(demisto.args(), "reply")
-    response_type = demisto.get(demisto.args(), "responseType")
-    lifetime = demisto.get(demisto.args(), "lifetime")
-    slack_instance = demisto.get(demisto.args(), "slackInstance")
-    slack_version = demisto.args().get("slackVersion", "SlackV3")
+    option1 = demisto_args.get("option1")
+    option2 = demisto_args.get("option2")
+    extra_options = argToList(demisto_args.get("additionalOptions", ""))
+    reply = demisto_args.get("reply")
+    response_type = demisto_args.get("responseType", "buttons")
+    lifetime = demisto_args.get("lifetime", "1 day")
     try:
         parsed_date = dateparser.parse("in " + lifetime, settings={"TIMEZONE": "UTC"})
-        assert parsed_date is not None, f"could not parse in {lifetime}"
+        assert parsed_date is not None, f"Could not parse in {lifetime}"
         expiry = datetime.strftime(parsed_date, DATE_FORMAT)
     except Exception:
         parsed_date = dateparser.parse("in 1 day", settings={"TIMEZONE": "UTC"})
         assert parsed_date is not None
         expiry = datetime.strftime(parsed_date, DATE_FORMAT)
-    default_response = demisto.get(demisto.args(), "defaultResponse")
+    default_response = demisto_args.get("defaultResponse")
 
     entitlement_string = entitlement + "@" + demisto.investigation()["id"]
-    if demisto.get(demisto.args(), "task"):
-        entitlement_string += "|" + demisto.get(demisto.args(), "task")
+    if demisto_args.get("task"):
+        entitlement_string += "|" + demisto_args.get("task")
 
-    args = {"ignoreAddURL": "true", "using-brand": slack_version}
-    if slack_instance:
-        args.update({"using": slack_instance})
+    args = {"ignoreAddURL": "true", "using-brand": "Zoom"}
+
     user_options = [option1, option2]
-    options = []
     if extra_options:
         user_options += extra_options
-    if response_type == "thread":
-        for option in user_options:
-            options.append(option.split("#")[0])
-        string_options = " or ".join([f"`{o}`" for o in options])
-        message = "{} - Please reply to this thread with {}.".format(demisto.args()["message"], string_options)
-        args["message"] = json.dumps(
-            {
-                "message": message,
-                "entitlement": entitlement_string,
-                "reply": reply,
-                "expiry": expiry,
-                "default_response": default_response,
-            }
-        )
-    else:
-        for option in user_options:
-            option = option.split("#")
-            button = {"text": option[0]}
-            if len(option) > 1:
-                style = STYLES_DICT.get(option[1])
-                if style:
-                    button["style"] = style
-            options.append(button)
-        blocks = json.dumps(create_blocks(demisto.args()["message"], entitlement_string, options, reply))
-        args["blocks"] = json.dumps(
-            {
-                "blocks": blocks,
-                "entitlement": entitlement_string,
-                "reply": reply,
-                "expiry": expiry,
-                "default_response": default_response,
-            }
-        )
-        args["message"] = demisto.args()["message"]
-
-    to = demisto.get(demisto.args(), "user")
-    channel = demisto.get(demisto.args(), "channel")
-    channel_id = demisto.get(demisto.args(), "channel_id")
+    blocks = json.dumps(generate_json(demisto.args()["message"], user_options, response_type))
+    args["message"] = json.dumps(
+        {
+            "blocks": blocks,
+            "entitlement": entitlement_string,
+            "reply": reply,
+            "expiry": expiry,
+            "default_response": default_response,
+        }
+    )
+    to = demisto_args.get("user")
+    channel_name = demisto_args.get("channel_name")
+    channel = demisto_args.get("channel_id")
 
     if to:
         args["to"] = to
-    elif channel_id:
-        args["channel_id"] = channel_id
     elif channel:
-        args["channel"] = channel
+        args["channel_id"] = channel
+    elif channel_name:
+        args["channel"] = channel_name
     else:
-        return_error("Either a user or a channel must be provided.")
+        return_error("Either a user or a channel_id or channel_name must be specified")
 
+    args["zoom_ask"] = "true"
     try:
-        demisto.results(demisto.executeCommand("send-notification", args))
+        return_results(demisto.executeCommand("send-notification", args))
     except ValueError as e:
         if "Unsupported Command" in str(e):
-            return_error("The command is unsupported by this script. If you have SlackV2 enabled, please use SlackAsk instead.")
+            return_error(f"The command is unsupported by this script. {e}")
         else:
             return_error("An error has occurred while executing the send-notification command", error=e)
 
