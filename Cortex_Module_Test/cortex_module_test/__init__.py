850c4209a18ec25603826448ba0a965c503b83bb
ShacharKidor <skidorball@paloaltonetworks.com>
2025-05-07 13:30:18 +0300

    Fixed the arg to number func + expose imports in the init file

--- Cortex_Module_Test/cortex_module_test/__init__.py
+++ Cortex_Module_Test/cortex_module_test/__init__.py
@@ -1,2 +1,9 @@
 def hello():
     return "Hello from cortex module test!"
+
+
+# cortex-module-test imports:
+from .test_override_csp import arg_to_number
+
+
+__all__ = ["arg_to_number"]
--- Cortex_Module_Test/cortex_module_test/test_override_csp.py
+++ Cortex_Module_Test/cortex_module_test/test_override_csp.py
@@ -1,5 +1,4 @@
 
 def arg_to_number(arg, arg_name=None, required=False):
-    print("In arg_to_number from cortex module test!") # noqa: T201
-    return arg
-    
+
+    return "In arg_to_number from cortex module test!"
