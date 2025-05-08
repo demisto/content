8fd57de530b6521f3b03f1cb91f917555916b9dd
YuvHayun <yhayun@paloaltonetworks.com>
2025-05-08 10:10:25 +0300

    added doc strings

--- Packs/CommonScripts/Scripts/HTMLtoMD/HTMLtoMD_test.py
+++ Packs/CommonScripts/Scripts/HTMLtoMD/HTMLtoMD_test.py
@@ -19,11 +19,23 @@ def test_main(mocker):
 @pytest.mark.parametrize(
     'escape_misc, expected_results',
     [
-        (True, "**\\+**"),
-        (False, "**+**")
+        (True, "**\\+ \\- \\&**"),
+        (False, "**+ - &**")
     ]
 )
 def test_escape_miscs(escape_misc, expected_results):
-    args = {"html": '<b>+</b>', "escape_misc": escape_misc}
+    """
+    Given:
+        html string to cast to md and escape_misc value.
+        - Case 1: escape_misc=True.
+        - Case 2: escape_misc=False.
+    When:
+        Calling html_to_md_command.
+    Then:
+        Ensue the right results returned.
+        - Case 1: should escape miscellaneous punctuation characters.
+        - Case 2: should not escape miscellaneous punctuation characters.
+    """
+    args = {"html": '<b>+ - &</b>', "escape_misc": escape_misc}
     results = html_to_md_command(args)
     assert results[1] == expected_results
