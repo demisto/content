bd68e8d10134561e06e9424602629b3192763672
meichler <meichler@paloaltonetworks.com>
2025-05-14 13:02:16 +0300

    pre-commit fixes

--- Packs/CommonScripts/Scripts/ExtractHTMLTables/ExtractHTMLTables.py
+++ Packs/CommonScripts/Scripts/ExtractHTMLTables/ExtractHTMLTables.py
@@ -22,7 +22,8 @@ def extract_html_table(html, indexes):
                     # Single value in a table - just create an array of strings ignoring header
                     if len(tds) == 1:
                         table.append(tds[0].text)
-                    # If there are 2 columns and no headers, treat as key-value (might override values if same key in first column)
+                    # If there are 2 columns and no headers,
+                    # treat as key-value (might override values if same key in first column)
                     elif len(tds) == 2 and len(headers) == 0:
                         if type(table) is list:
                             table = {}  # type: ignore
@@ -37,7 +38,7 @@ def extract_html_table(html, indexes):
                                 row["cell" + str(i)] = td.text
                         table.append(row)
         except Exception as e:
-            demisto.debug(f'Failed to extract table: {e}')
+            demisto.debug(f"Failed to extract table: {e}")
         if len(table) > 0:
             tables.append(table)
     if len(tables) > 0:
