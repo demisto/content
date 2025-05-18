8fcf98f41261df77360dd8270f156c504213d567
Shir2611 <smatathias@paloaltonetworks.com>
2025-05-18 18:39:16 +0300

    anomali threat stream 1.0

--- Packs/AnomaliThreatStreamFeed/Integrations/AnomaliThreatStreamFeed/AnomaliThreatStreamFeed.py
+++ Packs/AnomaliThreatStreamFeed/Integrations/AnomaliThreatStreamFeed/AnomaliThreatStreamFeed.py
@@ -20,28 +20,68 @@ from CommonServerPython import *  # noqa: F401
 urllib3.disable_warnings()
 
 """ CONSTANTS """
-
 DEFAULT_MALICIOUS_THRESHOLD = 65
 DEFAULT_SUSPICIOUS_THRESHOLD = 25
 THREAT_STREAM = "ThreatStream"
 RETRY_COUNT = 2
 
-RELATIONSHIPS_MAPPING = {
-    "ip": [
-        {"name": EntityRelationship.Relationships.RESOLVES_TO, "raw_field": "rdns", "entity_b_type": FeedIndicatorType.Domain},
-        {"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"},
-    ],
-    "domain": [
-        {"name": EntityRelationship.Relationships.RESOLVED_FROM, "raw_field": "ip", "entity_b_type": FeedIndicatorType.IP},
-        {"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"},
-    ],
-    "url": [
-        {"name": EntityRelationship.Relationships.RESOLVED_FROM, "raw_field": "ip", "entity_b_type": FeedIndicatorType.IP},
-        {"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"},
-    ],
-    "file": [{"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"}],
-    "email": [{"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"}],
-}
+# RELATIONSHIPS_MAPPING = {
+#     "ip": [
+#         {"name": EntityRelationship.Relationships.RESOLVES_TO, "raw_field": "rdns", "entity_b_type": FeedIndicatorType.Domain},
+#         {"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"},
+#     ],
+#     "domain": [
+#         {"name": EntityRelationship.Relationships.RESOLVED_FROM, "raw_field": "ip", "entity_b_type": FeedIndicatorType.IP},
+#         {"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"},
+#     ],
+#     "url": [
+#         {"name": EntityRelationship.Relationships.RESOLVED_FROM, "raw_field": "ip", "entity_b_type": FeedIndicatorType.IP},
+#         {"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"},
+#     ],
+#     "file": [{"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"}],
+#     "email": [{"name": EntityRelationship.Relationships.INDICATOR_OF, "raw_field": "meta.maltype", "entity_b_type": "Malware"}],
+# }
+
+# IOC_ARGS_TO_INDICATOR_KEY_MAP = {
+#     "domain": {
+#         "domain": "value",
+#         "dns": "ip",
+#         "organization": "org",
+#         "traffic_light_protocol": "tlp",
+#         "geo_country": "country",
+#         "creation_date": "created_ts",
+#         "updated_date": "modified_ts",
+#         "registrant_name": "meta.registrant_name",
+#         "registrant_email": "meta.registrant_email",
+#         "registrant_phone": "meta.registrant_phone",
+#     },
+#     "url": {"url": "value", "asn": "asn", "organization": "org", "geo_country": "country", "traffic_light_protocol": "tlp"},
+#     "ip": {
+#         "ip": "value",
+#         "asn": "asn",
+#         "geo_latitude": "latitude",
+#         "geo_longitude": "longitude",
+#         "geo_country": "country",
+#         "traffic_light_protocol": "tlp",
+#     },
+#     "file": {"organization": "org", "traffic_light_protocol": "tlp"},
+# }
+
+# INDICATOR_EXTENDED_MAPPING = {
+#     "Value": "value",
+#     "ID": "id",
+#     "IType": "itype",
+#     "Confidence": "confidence",
+#     "Country": "country",
+#     "Organization": "org",
+#     "ASN": "asn",
+#     "Status": "status",
+#     "Tags": "tags",
+#     "Modified": "modified_ts",
+#     "Source": "source",
+#     "Type": "type",
+#     "Severity": "severity",
+# }
 
 """ CLIENT CLASS """
 class Client(BaseClient):
@@ -49,10 +89,8 @@ class Client(BaseClient):
     Client to use in the Anomali ThreatStream Feed integration. Overrides BaseClient
     """
 
-    def __init__(self, base_url, user_name, api_key, verify, reliability, should_create_relationships):
+    def __init__(self, base_url, user_name, api_key, verify):
         super().__init__(base_url=base_url, verify=verify, ok_codes=(200, 201, 202))
-        self.reliability = reliability
-        self.should_create_relationships = should_create_relationships
         self.credentials = {
             "Authorization": f"apikey {user_name}:{api_key}",
         }
@@ -103,61 +141,12 @@ class Client(BaseClient):
         raise DemistoException(f"{THREAT_STREAM} - Error in API call {res.status_code} - {res.text}")
 
 
-class DBotScoreCalculator:
-    """
-    Class for DBot score calculation based on thresholds and confidence
-    """
-
-    def __init__(self, params: Dict):
-        self.instance_defined_thresholds = {
-            DBotScoreType.IP: arg_to_number(params.get("ip_threshold")),
-            DBotScoreType.URL: arg_to_number(params.get("url_threshold")),
-            DBotScoreType.FILE: arg_to_number(params.get("file_threshold")),
-            DBotScoreType.DOMAIN: arg_to_number(params.get("domain_threshold")),
-            DBotScoreType.EMAIL: arg_to_number(params.get("email_threshold")),
-        }
-        fetch_by = params.get("fetchBy", "modified")
-        feed_fetch_interval = arg_to_number(params.get("feedFetchInterval", "240"))
-        confidence_threshold = arg_to_number(params.get('confidenceThreshold', -1))
-        reliability = params.get("sourceReliability", "C - Fairly reliable")
-        tlp_color = params.get("tlp_color", "WHITE")
-        reputation = params.get("feedReputation", "Unknown")
-        ExpirationMethod = params.get("indicatorExpirationMethod", "Indicator Type")
-
-        indicator_default_score = params.get("indicator_default_score")
-        if indicator_default_score and indicator_default_score == "Unknown":
-            self.default_score = Common.DBotScore.NONE
-        else:
-            self.default_score = Common.DBotScore.GOOD
-
-    def calculate_score(self, ioc_type: str, indicator, threshold=None):
-        """
-        Calculate the DBot score according the indicator's confidence and thresholds if exist
-        """
-        # in case threshold was defined in the instance or passed as argument
-        # we have only two scores levels - malicious or good
-        # if threshold wasn't defined we have three score levels malicious suspicious and good
-        confidence = indicator.get("confidence", Common.DBotScore.NONE)
-        defined_threshold = threshold or self.instance_defined_thresholds.get(ioc_type)
-        if defined_threshold:
-            return Common.DBotScore.BAD if confidence >= defined_threshold else self.default_score
-        else:
-            if confidence > DEFAULT_MALICIOUS_THRESHOLD:
-                return Common.DBotScore.BAD
-            if confidence > DEFAULT_SUSPICIOUS_THRESHOLD:
-                return Common.DBotScore.SUSPICIOUS
-            else:
-                return self.default_score
-
-
-""" COMMAND FUNCTIONS """
-
-
 def test_module(client: Client) -> str:
     """Tests API connectivity and authentication'
 
     Returning 'ok' indicates that the integration works like it is supposed to.
     Connection to the service is successful.
+    
     Raises:
      exceptions if something goes wrong.
 
@@ -172,25 +161,169 @@ def test_module(client: Client) -> str:
     return "ok"
 
 
-def get_indicators_command(client: Client, args: dict) -> CommandResults:
-    """
-    Fetch and process threat indicators from Anomali ThreatStream
+def get_indicators_command(client: Client, args: dict[str, Any]):# -> CommandResults:
+    # TODO HERE
+    """Wrapper for retrieving indicators from the feed to the war-room.
 
     Args:
-        client (Client): Configured API client for Anomali ThreatStream
-        args (dict):
-            - limit: Maximum number of indicators to retrieve (optional)
-            - type: Type of indicators to fetch (optional)
-            - confidence: Minimum confidence threshold (optional)
-            - since: Retrieve indicators updated since this timestamp (optional)
+        client: Client object with request
+        args: demisto.args()
 
     Returns:
-        CommandResults
+        Outputs.
     """
-    return CommandResults(
-        outputs_prefix="AnomaliThreatStream.Indicators",
-        outputs_key_field="id",
+  
+    indicator_type = str(args.get("indicator_type"))
+    limit = int(demisto.args().get("limit")) if "limit" in demisto.args() else 10
+    
+    params = {
+        "limit": limit
+    }
+    if indicator_type:
+        params["itype"] = f"apt_{indicator_type}"
+        
+    res = client.http_request(method="GET", url_suffix="v2/intelligence", params=params)
+ 
+    parsed_indicators = parse_indicators_for_get_command(res.get("objects"))
+    demisto.debug(f"Anomali ThreatStream Feed got {len(parsed_indicators)} indicators")
+
+    human_readable = tableToMarkdown(
+        name="Indicators from Anomali ThreatStream Feed:",
+        t=parsed_indicators,
+        headers=[
+            "TargetIndustries",
+            "Source",
+            "ThreatStreamID",
+            "Country Code",
+            "ip/domain/email", #TODO find solution
+            "Description",
+            "Modified",
+            "Organization",
+            "Confidence",
+            "Creation",
+            "Expiration",
+            "Tags",
+            "TrafficLightProtocol",
+            "Location",
+            "ASN"
+        ],
+        removeNull=True,
+        is_auto_json_transform=True,
     )
+
+    return CommandResults(readable_output=human_readable)
+    # return CommandResults(readable_output=human_readable, raw_response=indicators)
+    # return human_readable, {}, {"raw_response": indicators}
+
+
+def parse_indicators_for_get_command(indicators) -> List[dict[str, Any]]:
+    """Parses the list of indicators returned from the api to indicators that can be returned to the war room.
+
+    Args:
+        indicators (list): list of indicators from api raw response.
+
+    Returns:
+        List[dict[str, Any]]: List of indicators that can be returned to the war room.
+    """
+    res = []
+    # indicators = [indicators] if type(indicators) is not list else indicators
+    for indicator in indicators:
+        indicator_type = indicator.get("type")  # ip/domain/email
+        indicator_value = indicator.get("value") # type's value
+        
+        dynamic_field = {}
+        dynamic_field[indicator_type] = indicator_value
+        
+        res.append(
+            assign_params(
+                TargetIndustries=indicator.get("target_industry"),
+                Source=indicator.get("source"),
+                ThreatStreamID=indicator.get("id"),
+                CountryCode=indicator.get("country"),
+                **dynamic_field,  # Unpack the dynamic field into the params
+                Description=indicator.get("description"),
+                Modified=indicator.get("modified_ts"),
+                Organization=indicator.get("org"),
+                Confidence=indicator.get("confidence"),
+                Creation=indicator.get("created_ts"),
+                Expiration=indicator.get("expires_ts"),
+                Tags=indicator.get("tags"),
+                TrafficLightProtocol=indicator.get("tlp"),
+                Location=indicator.get("locations"),
+                ASN=indicator.get("asn")
+            )
+        )
+    return res
+
+
+def fetch_indicators_command(client: Client, params: dict, last_run: dict):# -> list[dict]:
+    """Wrapper for fetching indicators from the feed to the Indicators tab.
+
+    Args:
+        client: Client object with request
+
+    Returns:
+        Indicators.
+    """
+    feed_fetch_interval = arg_to_number(params.get("feedFetchInterval", "240"))
+    fetch_by = params.get("fetchBy", "modified")
+    status = "active"
+    order_by = "modified_ts"
+    # confidence_threshold = arg_to_number(params.get('confidenceThreshold', "-1"))
+    confidence_threshold = 75  # Default confidence threshold
+    tlp_color = params.get("tlp_color", "WHITE")
+    reputation = params.get("feedReputation", "Unknown")
+    expiration_method = params.get("indicatorExpirationMethod", "Indicator Type")
+    relationships = params.get("createRelationships", True)
+    reliability = params.get("integrationReliability", DBotScoreReliability.C)
+    if DBotScoreReliability.is_valid_type(reliability):
+        reliability = DBotScoreReliability.get_dbot_score_reliability_from_str(reliability)
+    else:
+        Exception("Please provide a valid value for the Source Reliability parameter.")
+
+    now = datetime.now(timezone.utc)
+    days_for_query = int(feed_fetch_interval / 1440)  # The interval is validated already in the main
+
+    if last_run:
+        last_successful_run = dateparser.parse(
+            last_run["last_successful_run"], settings={"TIMEZONE": "UTC", "RETURN_AS_TIMEZONE_AWARE": True}
+        )
+        if last_successful_run:
+            time_delta = now - last_successful_run
+            days_for_query = time_delta.days + 1
+        else:
+            raise DemistoException("failed to fetch indicators")
+        
+    # handling case of more than 7 days history, as the API fail longer-fetching queries.
+    if days_for_query > 7:  # api can get up to 7 days
+        days_for_query = 7
+
+
+    # TODO cont from here!!!!!!!
+    response = client.get_indicators_request({"query": "get_iocs", "days": days_for_query})
+
+    if response.get("query_status") != "ok":
+        raise DemistoException(f"couldn't fetch, {response.get('query_status')}")
+
+    indicators = response["data"]
+    demisto.debug(f"{THREAT_STREAM} got {len(indicators)}")
+
+    results = []
+
+
+    # TODO this is the filtering
+    for indicator in indicators:
+        if indicator.get("ioc_type") == "sha3_384_hash":
+            demisto.debug(f'{THREAT_STREAM} got indicator of indicator type "sha3" skipping it')
+            continue
+        if (arg_to_number(indicator.get("confidence_level")) or 75) < confidence_threshold:
+            demisto.debug(f"{THREAT_STREAM} got indicator with low confidence level, skipping it")
+            continue
+
+        results.append(parse_indicator_for_fetch(indicator, with_ports, create_relationship, tlp_color))
+
+    return now.strftime("%Y-%m-%dT%H:%M:%SZ"), results
+
 
 
 def main():
@@ -206,16 +339,12 @@ def main():
     user_name = params.get("credentials", {}).get("identifier")
     api_key = params.get("credentials", {}).get("password")
     server_url = params.get("url", "").strip("/")
-    reliability = params.get("integrationReliability", DBotScoreReliability.B)
-    if DBotScoreReliability.is_valid_type(reliability):
-        reliability = DBotScoreReliability.get_dbot_score_reliability_from_str(reliability)
-    else:
-        Exception("Please provide a valid value for the Source Reliability parameter.")
 
-    commands = {
-        # reputation commands
-        "threatstream-feed-get-indicators": get_indicators_command,
-    }
+    # commands = {
+    #     # reputation commands
+    #     "test-module": test_module,
+    #     "threatstream-feed-get-indicators": get_indicators_command,
+    # }
 
     try:
         client = Client(
@@ -223,19 +352,27 @@ def main():
             user_name=user_name,
             api_key=api_key,
             verify=not params.get("insecure", False),
-            reliability=reliability,
-            should_create_relationships=params.get("createRelationships", True),
+            # reliability=reliability,
+            # should_create_relationships=relationships,
         )
 
         if command == "test-module":
             # This is the call made when pressing the integration Test button.
-            result = test_module(client)
-        elif command in commands:
-            result = commands[command](client, demisto.args())
+            return_results(test_module(client))
+            
+        elif command == 'threatstream-feed-get-indicators':
+            # TODO add the arguments from the run line - demisto arg
+            return_results(get_indicators_command(client, demisto.args()))
+            
+        elif command == "fetch-indicators":
+            next_run, res = fetch_indicators_command(client, params, demisto.getLastRun())
+            for b in batch(res, batch_size=2000):
+                demisto.debug(f"{THREAT_STREAM} {b=}")
+                demisto.createIndicators(b)
+            demisto.setLastRun({"last_successful_run": next_run})
+
         else:
             raise NotImplementedError(f"Command {command} is not implemented")
-
-        return_results(result)
 
     except Exception as e:
         return_error(f"Failed to execute {command} command.\nError:\n{str(e)}")
@@ -243,3 +380,7 @@ def main():
 
 if __name__ in ("__main__", "__builtin__", "builtins"):  # pragma: no cover
     main()
+
+
+# https://svlpartner-optic-api.threatstream.com/api/
+# https://svlpartner-optic-api.threatstream.com/api/v2/intelligence?limit=1000&status=active&order_by=modified_ts&confidence__gt=75&modified_ts__gte=2023-08-04T11:57:00
