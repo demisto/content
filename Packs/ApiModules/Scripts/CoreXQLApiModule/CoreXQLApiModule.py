cb9e85e156ef65e78d0934bc791066c5c7815695
Danny_Fried <dfried@paloaltonetworks.com>
2025-05-18 15:32:06 +0300

    empty strings

--- Packs/ApiModules/Scripts/CoreXQLApiModule/CoreXQLApiModule.py
+++ Packs/ApiModules/Scripts/CoreXQLApiModule/CoreXQLApiModule.py
@@ -430,11 +430,11 @@ def add_playbook_metadata(data: dict, command: str):
     ctx_output: dict = demisto.callingContext or {}
     entry_task: dict = ctx_output.get("context", {}).get("ParentEntry", {}).get("entryTask", {})
     incidents: list = ctx_output.get("context", {}).get("Incidents", [])
-    playbook_id = incidents[0].get("playbookId", "missing_playbookId") if incidents else ""
-    playbook_name = entry_task.get("playbookName", "missing_playbook_name")
-    task_name = entry_task.get("taskName", "missing_task_name")
-    task_id = entry_task.get("taskId", "missing_task_id")
-    brand = ctx_output.get('context', {}).get('ExecutedCommands', [{}])[0].get('moduleBrand', '')
+    playbook_id = incidents[0].get("playbookId", "") if incidents else ""
+    playbook_name = entry_task.get("playbookName", "")
+    task_name = entry_task.get("taskName", "")
+    task_id = entry_task.get("taskId", "")
+    brand = ctx_output.get("context", {}).get("ExecutedCommands", [{}])[0].get("moduleBrand", "")
     playbook_metadata = {
         "playbook_name": playbook_name,
         "playbook_id": playbook_id,
@@ -443,6 +443,7 @@ def add_playbook_metadata(data: dict, command: str):
         "integration_name": brand,
         "command_name": command,
     }
+    demisto.error(f'DANF playbook {playbook_metadata=}')
     data["request_data"]["playbook_metadata"] = playbook_metadata
     demisto.debug(
         f"Added playbook metadata data['request_data']['playbook_metadata']: {data['request_data']['playbook_metadata']}"
