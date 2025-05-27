bc34b483cb3c4b3cf832b1c00741d326f83ad35b
yrosenberg <yrosenberg@paloaltonetworks.com>
2025-05-27 20:21:34 +0300

    init

--- .hooks/validate-mdx.js
+++ .hooks/validate-mdx.js
@@ -1,6 +1,6 @@
-const { readFile } = require('fs-extra');
+const { createReadStream } = require('fs');
+const { once } = require('events');
 const mdx = require('@mdx-js/mdx');
-
 
 const NO_HTML = "<!-- NOT_HTML_DOC -->";
 const YES_HTML = "<!-- HTML_DOC -->";
@@ -14,36 +14,46 @@ function isHtmlDoc(content) {
         ));
 }
 
-
-function fixMdx(readmeContent) {
-    // copied from: https://github.com/demisto/content-docs/blob/2402bd1ab1a71f5bf1a23e1028df6ce3b2729cbb/content-repo/mdx_utils.py#L11
-    // to use the same logic as we have in the content-docs build
-    let txt = readmeContent;
-
+function fixMdx(content) {
+    let txt = content;
     const replaceTuples = [
         [/<br>(?!<\/br>)/gi, "<br/>"],
         [/<hr>(?!<\/hr>)/gi, "<hr/>"],
         [/<pre>/gi, "<pre>{`"],
         [/<\/pre>/gi, "`}</pre>"]
     ];
-
     replaceTuples.forEach(([oldPattern, newValue]) => {
         txt = txt.replace(oldPattern, newValue);
     });
-
     txt = txt.replace(/<!--.*?-->/gs, "");
-
     return txt;
 }
 
+async function readInitialChunk(file, length = 1024) {
+    return new Promise((resolve, reject) => {
+        const stream = createReadStream(file, { encoding: 'utf8', highWaterMark: length });
+        let data = '';
+        stream.on('data', chunk => {
+            data += chunk;
+            stream.destroy(); // stop reading more
+        });
+        stream.on('close', () => resolve(data));
+        stream.on('error', reject);
+    });
+}
+
+const { readFile } = require('fs-extra');
+
 async function parseMDX(file) {
     try {
-
-        let contents = await readFile(file, 'utf8');
-
-        if (isHtmlDoc(contents)) {
-            return true;
+        // Read first 1KB to check if HTML
+        const initialContent = await readInitialChunk(file);
+        if (isHtmlDoc(initialContent)) {
+            return true; // Skip MDX parsing for HTML docs
         }
+
+        // Read full file content (now that we know it's MDX)
+        let contents = await readFile(file, 'utf8');
         contents = fixMdx(contents);
         await mdx(contents);
         return true;
@@ -53,15 +63,13 @@ async function parseMDX(file) {
     }
 }
 
+// For concurrency limiting
+const pLimit = require('p-limit');
+const limit = pLimit(4);
+
 const files = process.argv.slice(2);
 (async () => {
-    const promises = [];
-
-    for (let i = 0; i < files.length; i++) {
-        const promise = parseMDX(files[i]);
-        promises.push(promise);
-    }
-
+    const promises = files.map(file => limit(() => parseMDX(file)));
     const results = await Promise.all(promises);
 
     if (results.includes(false)) {
