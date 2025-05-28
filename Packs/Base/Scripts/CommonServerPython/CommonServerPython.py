f53ff914f13eef18b82dfe33f5ad69fb3b388b00
Amichai <ashamah@paloaltonetworks.com>
2025-05-28 12:55:57 +0300

    General housekeeping

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -325,45 +325,6 @@ class EntryFormat(object):
         )
 
 
-@dataclass
-class QuickActionPreview:
-    """
-        A container class for storing quick action data previews.
-        This class is intended to be populated by commands like `!get-remote-data-preview`
-        and placed directly into the root context under `QuickActionPreview`.
-
-        Fields:
-            id (Optional[str]): The ID of the ticket.
-            title (Optional[str]): The title or summary of the ticket or action.
-            description (Optional[str]): A brief description or details about the action.
-            status (Optional[str]): Current status (e.g., Open, In Progress, Closed).
-            assignee (Optional[str]): The user or entity assigned to the action.
-            creation_date (Optional[str]): The date and time when the item was created.
-            severity (Optional[str]): Indicates the priority or severity level.
-        """
-    id: Optional[str] = None
-    title: Optional[str] = None
-    description: Optional[str] = None
-    status: Optional[str] = None
-    assignee: Optional[str] = None
-    creation_date: Optional[str] = None
-    severity: Optional[str] = None
-
-    def __post_init__(self):
-        missing_fields = [field_name for field_name, value in self.__dict__.items() if value is None]
-
-        if missing_fields:
-            demisto.debug(f"Missing fields: {', '.join(missing_fields)}")
-
-    def to_context(self) -> Dict[str, Any]:
-        """
-        Converts the dataclass to a dict for placing into context.
-        Returns:
-            dict: Dictionary representation of the QuickActionPreview.
-        """
-        return asdict(self)
-
-
 class FileAttachmentType(object):
     """
     Enum: contains the file attachment types,
