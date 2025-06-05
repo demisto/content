3089a15fc57bd0c2ef8ca9a56ad7fd9da860e126
Kamal Qarain <kqarain@paloaltonetworks.com>
2025-06-05 15:25:41 +0300

    Create CoreIsolateEndpoint script

--- Packs/Core/Scripts/CoreIsolateEndpoint/CoreIsolateEndpoint_test.py
+++ Packs/Core/Scripts/CoreIsolateEndpoint/CoreIsolateEndpoint_test.py
@@ -10,13 +10,13 @@ def test_main(mocker: MockerFixture):
     Assert:
         Ensure `execute_polling_command` is called once with the correct command name and args.
     """
-    from CoreUnisolateEndpoint import main
+    from CoreIsolateEndpoint import main
 
     args = {"endpoint_id": "endpointA", "incident_id": "incidentB"}
-    mocker.patch("CoreUnisolateEndpoint.demisto.args", return_value=args)
-    mock_execute_polling_command = mocker.patch("CoreUnisolateEndpoint.execute_polling_command", return_value=[])
+    mocker.patch("CoreIsolateEndpoint.demisto.args", return_value=args)
+    mock_execute_polling_command = mocker.patch("CoreIsolateEndpoint.execute_polling_command", return_value=[])
 
     main()
 
     assert mock_execute_polling_command.call_count == 1
-    assert mock_execute_polling_command.call_args[0] == ("core-unisolate-endpoint", args)
+    assert mock_execute_polling_command.call_args[0] == ("core-isolate-endpoint", args)
