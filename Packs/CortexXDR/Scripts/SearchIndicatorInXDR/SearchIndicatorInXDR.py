4deefe88d081fe0017cf7a75ec45eda87c0851fe
rshunim <rshunim@paloaltonetworks.com>
2025-06-08 16:11:09 +0300

    fix XDR script with Kamal's func

--- Packs/CortexXDR/Scripts/SearchIndicatorInXDR/SearchIndicatorInXDR.py
+++ Packs/CortexXDR/Scripts/SearchIndicatorInXDR/SearchIndicatorInXDR.py
@@ -16,39 +16,6 @@ def shorten_text(text: str) -> str:
     return text  # Return original if not exactly two words
 
 
-def check_status(args: dict) -> PollResult:
-    """
-    This function executes the xdr-xql-get-query-results command and PollResult object accordingly.
-    """
-    demisto.debug("starting check_status function")
-
-    query_id = args["query_id"]
-    entry_result = demisto.executeCommand(command="xdr-xql-get-query-results", args={"query_id": query_id})
-    demisto.debug(f"This is the entry result from executing xdr-xql-get-query-results command:\n{entry_result} ")
-
-    continue_to_poll = entry_result[0]["Contents"]["status"] == "PENDING"
-
-    if continue_to_poll:
-        demisto.debug("continue_to_poll is True")
-        return PollResult(
-            response={},
-            continue_to_poll=continue_to_poll,
-            args_for_next_run=args,
-            partial_result=CommandResults(readable_output=f'Waiting for job ID {args["query_id"]} to finish...'),
-        )
-
-    demisto.debug("continue_to_poll is False")
-    return PollResult(
-        response=CommandResults(
-            readable_output=f'job ID {args["query_id"]} is finished!',
-            outputs=entry_result[0]["Contents"]["results"],
-            outputs_prefix="PaloAltoNetworksXQL",
-        ),
-        continue_to_poll=continue_to_poll,
-        args_for_next_run=args,
-    )
-
-
 def generate_xdr_query(time_frame_for_query: str, indicator: str, data_set: str = "xdr_data") -> str:
     """
     This function generates a query for xdr by the indicator's type.
@@ -89,9 +56,11 @@ def generate_xdr_query(time_frame_for_query: str, indicator: str, data_set: str 
         )
 
 
-def execute_query(args: dict) -> dict:
+def execute_query_in_xdr(args: dict) -> CommandResults:
     """
-    This function executes the xdr-xql-generic-query command and returns the args for the next run since it's a polling command.
+    This function executes the xdr-xql-generic-query command by execute_polling_command (from CSP),
+    and returns the results.
+    execute_polling_command wraps the xdr-xql-generic-query polling command and manages the polling by itself.
     """
     demisto.debug("starting execute_query")
     time_frame: str = args.get("time_frame", "7 days")
@@ -101,48 +70,16 @@ def execute_query(args: dict) -> dict:
 
     time_frame_for_query: str = shorten_text(time_frame)
     query: str = generate_xdr_query(time_frame_for_query=time_frame_for_query, indicator=indicator, data_set=data_set)
-    entry_result = demisto.executeCommand(command="xdr-xql-generic-query", args={"query": query, "query_name": query_name})
-    demisto.debug(f"This is the entry result from executing xdr-xql-generic-query command:\n{entry_result} ")
-    polling_args = entry_result[0]["Metadata"]["pollingArgs"]
-    args_for_next_run = {
-        "query_id": polling_args["query_id"],
-        "query_name": polling_args["query_name"],
-        "time_frame": time_frame_for_query,
-        "data_set": data_set,
-        "indicator": indicator,
-    }
-
-    return args_for_next_run
-
-
-@polling_function(
-    name="SearchIndicatorInXDR",
-    interval=arg_to_number(demisto.args().get("interval_in_seconds", DEFAULT_INTERVAL)),
-    timeout=arg_to_number(demisto.args().get("timeout_in_seconds", DEFAULT_TIMEOUT)),
-    requires_polling_arg=False,
-)
-def retrieve_data_from_xdr(args: dict) -> PollResult:
-    """
-    This is the main function, which manages the polling.
-    If the 'query_id' argument exists, we're after executing the query and checking for the results.
-    Otherwise, we're just starting the process and should execute the query.
-    """
-    if "query_id" not in args:  # first time executing query
-        demisto.debug("starting polling_command function")
-        args_for_next_run = execute_query(args=args)
-        return PollResult(
-            response={},
-            continue_to_poll=True,
-            args_for_next_run=args_for_next_run | {"polling": True},
-            partial_result=CommandResults(readable_output=f'Waiting for job ID {args_for_next_run["query_id"]} to finish...'),
-        )
-    else:  # check query's status
-        return check_status(args=args)
+    demisto.debug(f"This is the {query=}")
+    entry_result = execute_polling_command(
+        command_name="xdr-xql-generic-query", args={"query": query, "query_name": query_name, "max_fields": 1000}
+    )
+    return entry_result
 
 
 def main():  # pragma: no cover
     try:
-        return_results(retrieve_data_from_xdr(args=demisto.args()))
+        return_results(execute_query_in_xdr(args=demisto.args()))
     except Exception as e:
         return_error("Error occurred while retrieving data from XDR. Exception info:\n" + str(e))
 
