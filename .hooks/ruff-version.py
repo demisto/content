d55b49db74eb884983592387c039cc4e71af23df
sapirshuker <sshuker@paloaltonetworks.com>
2025-06-09 16:19:36 +0300

    fix hook

--- .hooks/ruff-version.py
+++ .hooks/ruff-version.py
@@ -1,5 +1,16 @@
 import subprocess
 import sys
+import shutil
+
+
+def check_which_ruff():
+    ruff_path = shutil.which("ruff")
+    if ruff_path:
+        sys.stdout.write("Ruff is installed\n")
+        return True
+    else:
+        sys.stdout.write("Ruff is NOT found in the system's PATH.\n")
+        return False
 
 
 def get_ruff_version():
@@ -7,16 +18,16 @@ def get_ruff_version():
         result = subprocess.run([sys.executable, "-m", "ruff", "--version"], capture_output=True, text=True, check=True)
         return result.stdout.strip()
     except subprocess.CalledProcessError:
-        return None
+        return "subprocess.CalledProcessError"
     except FileNotFoundError:
-        return None
+        return "FileNotFoundError"
     except Exception:
-        return None
+        return "Exception"
 
 
 if __name__ == "__main__":
-    ruff_version = get_ruff_version()
-    if ruff_version:
-        print(f"Ruff version: {ruff_version}")
-    else:
-        print("Could not determine Ruff version.")
+    which_ruff = check_which_ruff()
+    if which_ruff:
+        ruff_version = get_ruff_version()
+        if ruff_version:
+            sys.stdout.write(f"{ruff_version}\n")
