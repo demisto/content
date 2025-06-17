b305a55449f5d1082950078de54a9a2526bf6ea2
Ryan McVicar <rymcvicar@gmail.com>
2025-06-17 14:29:43 -0400

    Update context to match format for command, and updated command documentation

--- Packs/UrlScan/Integrations/UrlScan/UrlScan.py
+++ Packs/UrlScan/Integrations/UrlScan/UrlScan.py
@@ -704,7 +704,7 @@ def urlscan_search_command(client):
     if search_type == "raw":
         r = urlscan_search(client, search_type, raw_query)
         results = CommandResults(
-                outputs_prefix = "UrlScan.search.results",
+                outputs_prefix = "URLScan.Search.Results",
                 raw_response = r,
                 outputs = r['results'],
                 readable_output = f'{r["total"]} results found for {raw_query}',
