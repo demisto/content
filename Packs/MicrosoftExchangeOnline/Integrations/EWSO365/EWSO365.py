5e93b4f91624dff9743c3d01755c9551d01b3801
ilaredo <ilaredo@paloaltonetworks.com>
2025-06-18 21:22:49 +0300

    wip

--- Packs/MicrosoftExchangeOnline/Integrations/EWSO365/EWSO365.py
+++ Packs/MicrosoftExchangeOnline/Integrations/EWSO365/EWSO365.py
@@ -536,8 +536,8 @@ def get_entry_for_item_attachment(item_id, attachment, target_email):  # pragma:
     dict_result.update(parse_item_as_dict(item, target_email, camel_case=True, compact_fields=True))
     title = (
         f'EWS get attachment got item for "{target_email}", '
-        f'"{get_attachment_name(attachment_name=attachment.name, content_id=attachment.content_id, is_inline=attachment.is_inline)}"') # noqa: E501
-
+        f'"{get_attachment_name(attachment_name=attachment.name, content_id=attachment.content_id, is_inline=attachment.is_inline)}"'  # noqa: E501
+    )
     return get_entry_for_object(
         title,
         CONTEXT_UPDATE_EWS_ITEM_FOR_ATTACHMENT + CONTEXT_UPDATE_ITEM_ATTACHMENT,
