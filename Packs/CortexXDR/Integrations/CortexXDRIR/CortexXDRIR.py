38dd554564c6bf3ba665e878294d05bc75c85eb8
Tal <tcarmeli@paloaltonetworks.com>
2025-06-18 17:55:55 +0300

    first commit

--- Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
+++ Packs/CortexXDR/Integrations/CortexXDRIR/CortexXDRIR.py
@@ -510,11 +510,11 @@ class Client(CoreClient):
             url_suffix="/alerts/update_alerts",
             json_data=request_data,
             headers=self.headers,
-            timeout=self.timeout, 
+            timeout=self.timeout,
         )
         if "reply" not in response or "alerts_ids" not in response["reply"]:
             raise DemistoException(f"Parse Error. Response not in format, can't find reply key. The response {response}.")
-        return response["reply"]["alerts_ids"] 
+        return response["reply"]["alerts_ids"]
 
 
 def get_headers(params: dict) -> dict:
