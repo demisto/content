a319442b6aef0c900fe167fc43cf35576cc02b05
MLainer1 <mlainer@paloaltonetworks.com>
2025-06-23 15:28:18 +0300

    pc + added test

--- Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query.py
+++ Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query.py
@@ -573,8 +573,8 @@ def search_users(default_base_dn, page_size):
 
     attributes: list[str] = []
     custom_attributes: list[str] = []
-    
-    if argToBoolean(params.get('fail_on_empty_args', False)):
+
+    if argToBoolean(params.get("fail_on_empty_args", False)):
         check_if_empty_args(args)
 
     # zero is actually no limitation, default is 20
@@ -663,17 +663,20 @@ def search_users(default_base_dn, page_size):
     }
     demisto.results(demisto_entry)
 
+
 def check_if_empty_args(args: dict):
     """Checks if the args dictionary contains empty values.
-        If there are empty values, raises an error.
+    If there are empty values, raises an error.
     """
     empty_args = []
     for key, value in args.items():
-        if value == '':
+        if value == "":
             empty_args.append(key)
-    
+
     if empty_args:
-        raise DemistoException(f"Got empty values for arguments: {empty_args}. Remove those values or uncheck the *Error on empty arguments* parameter\n")  # noqa: E501
+        raise DemistoException(
+            f"Got empty values for arguments: {empty_args}. Remove those values or uncheck the *Error on empty arguments* parameter\n"  # noqa: E501
+        )  # noqa: E501
 
 
 def get_user_iam(default_base_dn, args, mapper_in, mapper_out):
