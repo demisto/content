62fd5cfa3d269c686dd137e51df34599f947d990
MLainer1 <mlainer@paloaltonetworks.com>
2025-06-23 16:11:12 +0300

    pc

--- Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query_test.py
+++ Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query_test.py
@@ -552,6 +552,7 @@ def test_search__no_control_exist(mocker):
 
     assert "**No entries.**" in demisto.results.call_args[0][0]["HumanReadable"]
 
+
 def test_search__fail_on_empty_args(mocker):
     """
     Given:
@@ -572,13 +573,17 @@ def test_search__fail_on_empty_args(mocker):
             return
 
     mocker.patch.object(demisto, "results")
-    mocker.patch.object(demisto, "args", return_value={'limit': '20', 'name': ''})
-    mocker.patch.object(demisto, "params", return_value={'fail_on_empty_args': 'true'})
+    mocker.patch.object(demisto, "args", return_value={"limit": "20", "name": ""})
+    mocker.patch.object(demisto, "params", return_value={"fail_on_empty_args": "true"})
     Active_Directory_Query.connection = ConnectionMocker()
 
     with pytest.raises(Exception) as err:
         Active_Directory_Query.search_users("dc=test,dc=test_1", page_size=20)
-    assert err.value.args[0] == "Got empty values for arguments: ['name']. Remove those values or uncheck the *Error on empty arguments* parameter\n"  # noqa: E501
+    assert (
+        err.value.args[0]
+        == "Got empty values for arguments: ['name']. Remove those values or uncheck the *Error on empty arguments* parameter\n"
+    )  # noqa: E501
+
 
 def test_search_attributes_to_exclude(mocker):
     """
