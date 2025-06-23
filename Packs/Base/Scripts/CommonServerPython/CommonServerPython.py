3d488d4ca2ed1c59dab8deea30c9ef18ee23f702
Moshe <mgalitzki@paloaltonetworks.com>
2025-06-23 11:01:42 +0300

    fix

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -2213,7 +2213,9 @@ class JsonTransformer:
         if isinstance(json_input, list):
             if not json_input or (not isinstance(json_input[0], list) and not isinstance(json_input[0], dict)):
                 # if the items of the lists are primitive, put the values in one line
+                # yield path, 'values', ', '.join(str(x) for x in json_input)
                 yield path, 'values', ', '.join(json_input)
+
             else:
                 for i, item in enumerate(json_input):
                     for res in self.json_to_path_generator(item, path + [i]):  # this is yield from for python2 BC
