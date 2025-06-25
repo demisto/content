d361fac2bb941c6afdaaa8262871681f94d5b82d
sapirshuker <sshuker@paloaltonetworks.com>
2025-06-25 14:16:01 +0300

    pretty

--- .github/github_workflow_scripts/code_owner_file_validation.py
+++ .github/github_workflow_scripts/code_owner_file_validation.py
@@ -1,4 +1,5 @@
 import requests
+from requests import RequestException
 from utils import timestamped_print
 import os
 import sys
@@ -6,6 +7,7 @@ import sys
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
 BRANCH_NAME = os.getenv("BRANCH_NAME", "main")
 print = timestamped_print
+
 
 def validate_codeowners_file(owner: str, repo: str) -> dict:
     """
@@ -32,28 +34,26 @@ def validate_codeowners_file(owner: str, repo: str) -> dict:
     try:
         response = requests.get(url, headers=headers, params={"ref": BRANCH_NAME})
         response.raise_for_status()
-        data = response.json()
-        return data
+        return response.json()
 
     except RequestException as e:
         print(f"RequestException: {e.response.status_code} - {e.response.text}")
         return {"error": True, "message": f"RequestException: {e.response.status_code} - {e.response.text}"}
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     repository_owner = "demisto"
     repository_name = "content"
 
     validation_result = validate_codeowners_file(repository_owner, repository_name)
 
-    if validation_result.get("error",False):
+    if validation_result.get("error", False):
         print(f"Validation failed: {validation_result['message']}")
     else:
         if errors := validation_result.get("errors", []):
-            print("CODEOWNERS file has errors:")
+            print("CODEOWNERS file has errors:\n")
             for error in errors:
                 print(f" - Line {error.get('line', 'N/A')}: {error.get('message', 'No message provided')}\n")
             sys.exit(1)
         else:
-            print("\nCODEOWNERS file is valid! No errors found.")
-
+            print("CODEOWNERS file is valid! No errors found.")
