c9538e01745a9bd65b1d973119cb4b2d6d8e46b9
Shir2611 <smatathias@paloaltonetworks.com>
2025-07-02 17:39:00 +0300

    sap cloud 4.0

--- Packs/SAPCloudForCustomerC4C/Integrations/SAPCloudForCustomerC4C/README.md
+++ Packs/SAPCloudForCustomerC4C/Integrations/SAPCloudForCustomerC4C/README.md
@@ -1,5 +1,41 @@
-This README contains the full documentation for your integration.
+The SAP Cloud for Customer solution helps you manage day-to-day sales and service interactions efficiently by sending and receiving signals between front- and back-office solutions and providing a single view of the customer.
+This integration was integrated and tested with version xx of SAPCloudForCustomerC4C.
 
-You auto-generate this README file from your integration YML file using the `demisto-sdk generate-docs` command.
+## Configure SAP Cloud For Customer C4C in Cortex
 
-For more information see the [integration documentation](https://xsoar.pan.dev/docs/integrations/integration-docs).
+| **Parameter** | **Required** |
+| --- | --- |
+| Your server URL | True |
+| Username | True |
+| Password | True |
+| Report ID | True |
+| Trust any certificate (not secure) | False |
+| Use system proxy settings | False |
+| Maximum number of audit events per fetch | False |
+
+## Commands
+
+You can execute these commands from the CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
+
+### sap-cloud-get-events
+
+***
+Retrieves events from the SAP Cloud for Customer API based on specified filters.
+
+#### Base Command
+
+`sap-cloud-get-events`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| start_date | The **start date** for filtering events. Events newer than or equal to this date will be retrieved.<br/>Must be in `DD-MM-YYYY HH:MM:SS` format (e.g., `10-07-2025 14:17:46`).<br/>. | Required |
+| days_from_start | The **number of days** to include events after the `start_date`. For example, if `start_date` is <br/>'10-07-2025 10:00:00' and `days_from_start` is '2', events will be retrieved up to '12-07-2025 10:00:00'.<br/>It's recommended to keep this value no more than 5 days to avoid very large result sets.<br/>. Default is 2. | Optional |
+| should_push_events | Set to **true** to create events in your system from the retrieved data. <br/>If **false** (default), the command will only display the events without creating them.<br/>. Possible values are: true, false. Default is false. | Optional |
+| limit | The **maximum number of events** to retrieve. If more events match the criteria, only this<br/>specified amount will be returned.<br/>. Default is 10. | Optional |
+
+#### Context Output
+
+There is no context output for this command.
--- Packs/SAPCloudForCustomerC4C/Integrations/SAPCloudForCustomerC4C/SAPCloudForCustomerC4C.py
+++ Packs/SAPCloudForCustomerC4C/Integrations/SAPCloudForCustomerC4C/SAPCloudForCustomerC4C.py
@@ -2,6 +2,7 @@ import demistomock as demisto  # noqa: F401
 import urllib3
 from CommonServerPython import *  # noqa: F401
 from typing import Any
+from datetime import datetime, timedelta
 
 # Disable insecure warnings
 urllib3.disable_warnings()
@@ -129,7 +130,31 @@ def get_current_utc_time():
     return datetime.now(timezone.utc)
 
 
-def get_events(client: Client, report_id: str, start_date: str, skip: int, top: int) -> Optional[List[Dict[str, Any]]]:
+def get_end_date(start_date_str: str, days: int = 2) -> str:
+    """
+    Calculates an end date by adding a specified number of days to a given start date.
+
+    The start date string is parsed, converted to UTC, and then the timedelta is applied.
+    The resulting end date is returned as a formatted string.
+
+    Args:
+        start_date_str (str): The start date as a string, expected to be in
+                              the format defined by `STRFTIME_FORMAT` (e.g., "DD-MM-YYYY HH:MM:SS").
+        days (int, optional): The number of days to add to the start date.
+                              Defaults to 2.
+
+    Returns:
+        str: The calculated end date as a string, formatted according to `STRFTIME_FORMAT`.
+    """
+    start_date = datetime.strptime(start_date_str, STRFTIME_FORMAT)
+    start_date = start_date.replace(tzinfo=timezone.utc)
+
+    end_date = start_date + timedelta(days=days)
+
+    return end_date.strftime(STRFTIME_FORMAT)
+
+
+def get_events_for_get(client: Client, report_id: str, start_date: str, skip: int, top: int) -> Optional[List[Dict[str, Any]]]:
     """
     Get a list of events from the SAP Cloud for Customer API.
 
@@ -159,6 +184,85 @@ def get_events(client: Client, report_id: str, start_date: str, skip: int, top: 
     return res.get("d", {}).get("results", [])
 
 
+def get_events_command(client: Client, report_id: str, args: dict) -> tuple[List[Dict], CommandResults]:
+    """
+    Retrieves events from the SAP Cloud for Customer API based on provided parameters.
+
+    Args:
+        client (Client): The API client to use for the request.
+        report_id (str): The ID of the report to fetch events from.
+        args (dict): Command arguments, including:
+            - 'start_date' (str): Start date for event retrieval (formatted as DD-MM-YYYY HH:MM:SS).
+            - 'days_from_start' (int, optional): Number of days from the start_date to define the end of the retrieval period.
+                                                 Defaults to 2 days.
+            - 'limit' (int, optional): Maximum number of events to retrieve. Defaults to 10.
+
+    Returns:
+        tuple[List[Dict], CommandResults]: A tuple containing:
+            - A list of events (List[Dict]): The raw list of retrieved events.
+            - CommandResults: The command results object with a human-readable output table and raw response.
+    """
+    limit: int = arg_to_number(args.get("limit")) or 10
+    start_date: Optional[str] = args.get("start_date")
+    if not start_date:
+        # Handle the case where start_date is missing, as it's required for get_events
+        demisto.debug("start_date argument is missing. Cannot retrieve events.")
+        return [], CommandResults(readable_output="Error: 'start_date' argument is required.", raw_response={})
+
+    days_from_start: int = arg_to_number(args.get("days_from_start")) or 2
+    end_date: str = get_end_date(start_date, days=days_from_start)
+
+    skip_count = 0
+    all_events: list[dict[str, Any]] = []
+
+    while limit > 0:
+        top = min(DEFAULT_TOP, limit)
+        response = get_events(client, report_id, skip=skip_count, top=top, start_date=start_date, end_date=end_date)
+        if response:
+            all_events.extend(response)
+            skip_count += DEFAULT_TOP
+            limit -= len(response)
+        else:
+            demisto.debug("No more events exist or no response received, breaking...")
+            break
+
+    hr = tableToMarkdown(name=f"Test Event for {SAP_CLOUD}", t=all_events, removeNull=True, is_auto_json_transform=True)
+    return all_events, CommandResults(readable_output=hr, raw_response=all_events)
+
+
+def get_events(
+    client: Client, report_id: str, skip: int, top: int, start_date: str, end_date: Optional[str] = None
+) -> Optional[List[Dict[str, Any]]]:
+    """
+    Get a list of events from the SAP Cloud for Customer API.
+
+    Args:
+        client (Client): The client object to use for API requests.
+        report_id (str): The ID of the report to fetch events from.
+        skip (int): Number of items to skip for pagination.
+        top (int): Maximum number of events to return in this request.
+        start_date (str): Fetch events that are newer than or equal to this time (formatted as DD-MM-YYYY HH:MM:SS).
+        end_date (Optional[str], optional): Fetch events that are older than or equal to this time
+        (formatted as DD-MM-YYYY HH:MM:SS). Defaults to None.
+
+    Returns:
+        Optional[List[Dict[str, Any]]]: A list of events, or None if an error occurs.
+    """
+
+    filter = f"CTIMESTAMP ge '{start_date} INDIA'"
+    if end_date:
+        filter += f" and CTIMESTAMP le '{end_date} INDIA'"
+    params = {"$filter": filter, "$skip": skip, "$top": top, "$format": "json", "$inlinecount": "allpages"}
+
+    res = client.http_request(
+        method="GET",
+        url_suffix=f"{URL_SUFFIX}{report_id}?",
+        params=params,
+    )
+
+    return res.get("d", {}).get("results", [])
+
+
 def fetch_events(client: Client, params: dict, last_run: dict) -> tuple[dict, list[Any]]:
     """
     Fetches events from SAP Cloud API.
@@ -178,7 +282,6 @@ def fetch_events(client: Client, params: dict, last_run: dict) -> tuple[dict, li
         raise DemistoException("Report ID must be provided in the integration parameters and must be a string.")
     all_events: list[dict[str, Any]] = []
     skip_count = INIT_SKIP
-    top_count = DEFAULT_TOP
     now = get_current_utc_time()
 
     demisto.debug(f"the last run is {last_run=}")
@@ -202,8 +305,8 @@ def fetch_events(client: Client, params: dict, last_run: dict) -> tuple[dict, li
     demisto.debug(f"Getting events from: {start_date_for_filter}")
 
     while max_events_per_fetch > 0:
-        top = min(top_count, max_events_per_fetch)
-        response = get_events(client, report_id, start_date=start_date_for_filter, skip=skip_count, top=top)
+        top = min(DEFAULT_TOP, max_events_per_fetch)
+        response = get_events(client, report_id, skip=skip_count, top=top, start_date=start_date_for_filter)
         if response:
             all_events.extend(response)
             skip_count += DEFAULT_TOP
@@ -225,7 +328,7 @@ def main():
     """
     command = demisto.command()
     demisto.debug(f"Command being called is {command}")
-
+    args = demisto.args()
     params = demisto.params()
 
     # init credentials
@@ -244,6 +347,14 @@ def main():
             # This call is made when clicking the integration 'Test' button.
             report_id = params.get("report_id")
             return_results(test_module(client, report_id))  # Let test_module handle validation
+
+        elif command == "sap-cloud-get-events":
+            report_id = params.get("report_id")
+            events, results = get_events_command(client, report_id, args)
+            should_push_events = argToBoolean(args.get("should_push_events", "false"))
+            if should_push_events:
+                send_events_to_xsiam(events, vendor=VENDOR, product=PRODUCT)
+            return_results(results)
 
         elif command == "fetch-events":
             last_run = demisto.getLastRun() or {}
