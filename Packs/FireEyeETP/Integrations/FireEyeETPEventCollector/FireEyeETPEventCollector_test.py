fae6b974cfaa1f295e121c60f3cc72da70a4413c
Kamal Qarain <kqarain@paloaltonetworks.com>
2025-07-07 15:47:25 +0300

    Fix API request for fetching user activity logs

--- Packs/FireEyeETP/Integrations/FireEyeETPEventCollector/FireEyeETPEventCollector_test.py
+++ Packs/FireEyeETP/Integrations/FireEyeETPEventCollector/FireEyeETPEventCollector_test.py
@@ -374,3 +374,28 @@ def test_limit_zero_skip_fetch_flow(mocker):
     get_events_mock.reset_mock()
     collector.get_events_command(start_time=datetime(2023, 1, 15, 14, 30, 45, 123000))
     get_events_mock.assert_called_once()
+
+
+@freeze_time("2025-07-07T17:00:00Z")
+def test_client_get_activity_log(mocker):
+    """
+    Given:
+        - "from" time and a "size".
+    When:
+        - Calling `Client.get_activity_log`.
+    Then:
+        - Assert "to" time is set as expected (one day later).
+    """
+    client = mock_client()
+    mock_http_request = mocker.patch.object(client, "_http_request")
+
+    from_time = "2025-07-07T08:22:22+0000Z"
+    size = 100
+    client.get_activity_log(from_LastModifiedOn=from_time, to_LastModifiedOn="", size=size)
+    
+    expected_to_time = "2025-07-08T17:00:00+0000Z"  # one day after frozen timestamp
+    assert mock_http_request.call_args.kwargs == {
+            "method": "POST",
+            "url_suffix": "/api/v1/users/activitylogs/search",
+            "json_data": {"size": size, "attributes": {"time": {"from": from_time, "to": expected_to_time}}},
+    }
