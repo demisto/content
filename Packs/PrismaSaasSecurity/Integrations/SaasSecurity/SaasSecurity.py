7901306a1b8f7694a420a20584bfec169d9589cb
RotemAmit <ramit@paloaltonetworks.com>
2025-07-27 16:09:01 +0300

    a small fix

--- Packs/PrismaSaasSecurity/Integrations/SaasSecurity/SaasSecurity.py
+++ Packs/PrismaSaasSecurity/Integrations/SaasSecurity/SaasSecurity.py
@@ -186,7 +186,7 @@ class Client(BaseClient):
         :param status: The status of the incidents to pull.
         :param next_page: For pagination purposes. If provided, params should be None.
         """
-        url_suffix = next_page or "incident/api/incidents/delta"
+        url_suffix = next_page or "/incident/api/incidents/delta"
         state = state if state != "All" else None
 
         params = (
