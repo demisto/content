12275fa3eac2b88f2a6b400faede7b175c84c5e4
noydavidi <nodavidi.paloaltonetworks.com>
2025-08-04 14:36:29 +0300

    run precommit

--- Packs/AggregatedScripts/Scripts/IsolateEndpoint/IsolateEndpoint.py
+++ Packs/AggregatedScripts/Scripts/IsolateEndpoint/IsolateEndpoint.py
@@ -316,7 +316,7 @@ def handle_raw_response_results(
     return None
 
 
-def find_command_by_brand(commands: list[Command], brand: str) -> Command:
+def find_command_by_brand(commands: list[Command], brand: str) -> Command | None:
     """
     Finds and returns the command from the list that matches the specified brand.
 
@@ -330,6 +330,7 @@ def find_command_by_brand(commands: list[Command], brand: str) -> Command:
     for command in commands:
         if command.brand == brand:
             return command
+    return None
 
 
 def run_commands_for_endpoint(commands: list, endpoint_args: dict, endpoint_output: dict, results: list, verbose) -> None:
