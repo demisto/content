ec7f66f8320983e90ad80503152828e956ff1f0a
aneeshamore <amore@paloaltonetworks.com>
2025-08-12 11:43:13 -0700

    Replaced query arg with url_category arg for a specific query logs use case.

--- Packs/PAN-OS/Scripts/PANOSQueryLogs/PANOSQueryLogs.py
+++ Packs/PAN-OS/Scripts/PANOSQueryLogs/PANOSQueryLogs.py
@@ -15,6 +15,15 @@ def main():
         args["port-dst"] = args.pop("port_dst", None)
         args["show-detail"] = args.pop("show_detail", None)
         args["polling"] = True
+        url_category = args.get("url_category")
+        if url_category is not None:
+            if url_category.startswith("AI"):
+                # Keep "AI" uppercase, convert rest to lowercase
+                url_category = ("AI" + url_category[2:].lower().replace(" ","-"))
+            else:
+                # Convert entire string to lowercase
+                url_category = url_category.lower().replace(" ","-")
+            args["query"] = f"url_category_list contains '{url_category}'"
         return_results(execute_polling_command("pan-os-query-logs", args))
     except Exception as e:
         return_error(f"Failed to execute script.\nError:\n{e!s}")
