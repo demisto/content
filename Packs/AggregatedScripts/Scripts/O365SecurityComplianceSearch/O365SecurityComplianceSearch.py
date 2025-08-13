46b5e84589c3aff172e61391f362502c5fc12578
Daniel Rezvani <drezvani@paloaltonetworks.com>
2025-08-13 15:10:11 +0300

    ruff format

--- Packs/AggregatedScripts/Scripts/O365SecurityComplianceSearch/O365SecurityComplianceSearch.py
+++ Packs/AggregatedScripts/Scripts/O365SecurityComplianceSearch/O365SecurityComplianceSearch.py
@@ -39,39 +39,39 @@ def parse_args(args: dict) -> dict:
     Returns:
         dict: Parsed args
     """
-    
+
     if "force" in args:
         args["force"] = argToBoolean(args["force"])
-        
+
     if "preview" in args:
         args["preview"] = argToBoolean(args["preview"])
-    
+
     if "include_mailboxes" in args:
         args["include_mailboxes"] = argToBoolean(args["include_mailboxes"])
-        
+
     if "exchange_location" in args:
         args["exchange_location"] = argToList(args["exchange_location"])
-        
+
     if "exchange_location_exclusion" in args:
         args["exchange_location_exclusion"] = argToList(args["exchange_location_exclusion"])
-        
+
     if "public_folder_location" in args:
         args["public_folder_location"] = argToList(args["public_folder_location"])
-    
+
     if "share_point_location" in args:
         args["share_point_location"] = argToList(args["share_point_location"])
-        
+
     if "share_point_location_exclusion" in args:
         args["share_point_location_exclusion"] = argToList(args["share_point_location_exclusion"])
-        
+
     if "polling_interval" in args:
         args["polling_interval"] = arg_to_number(args["polling_interval"])
-        
+
     if "polling_timeout" in args:
         args["polling_timeout"] = arg_to_number(args["polling_timeout"])
-        
+
     return args
-    
+
 
 def get_result_value(cmd_results: CommandResults, key: str) -> str:
     """
@@ -87,12 +87,12 @@ def get_result_value(cmd_results: CommandResults, key: str) -> str:
     try:
         context = cmd_results[0]  # type: ignore
         return demisto.get(context, f"Contents.{key}")  # type: ignore
-    
+
     except (KeyError, IndexError):
         return ""
 
 
-def add_to_context(context: dict, sub_key: str, new_key: str, new_value: str|list):
+def add_to_context(context: dict, sub_key: str, new_key: str, new_value: str | list):
     """
     Add the value to the context dictionary under the specified sub-key
 
@@ -102,15 +102,15 @@ def add_to_context(context: dict, sub_key: str, new_key: str, new_value: str|lis
         new_key (str): The key to add
         new_value (str | list): The value to add
     """
-    
+
     for key, val in context.items():
         if isinstance(val, dict):
             if key == sub_key:
                 val[new_key] = new_value
 
             add_to_context(val, sub_key, new_key, new_value)
-        
-            
+
+
 def parse_results(search_results: str) -> list[dict]:
     """
     Parse results into a structured list of dictionaries for the context
@@ -121,21 +121,21 @@ def parse_results(search_results: str) -> list[dict]:
     Returns:
         list[dict]: The parsed search results
     """
-    
+
     if not search_results:
         return [{}]
-    
+
     parsed_results = []
-    
+
     # remove brackets and carriage returns to normalize string
     search_results = search_results.replace("\r", "").replace("{", "").replace("}", "")
-    
+
     # split results into lines and parse into dict
     results_list = search_results.split("\n")
     for entry in results_list:
-        result_matches  = re.findall(SEARCH_RESULT_PATTERN, entry)
+        result_matches = re.findall(SEARCH_RESULT_PATTERN, entry)
         parsed_results.append({key.strip(): val.strip() for key, val in result_matches})
-        
+
     return parsed_results
 
 
@@ -151,29 +151,29 @@ def wait_for_results(args: dict, cmd: str, result_key: str) -> CommandResults:
     Returns:
         Union[dict, list]: Command execution results
     """
-    
+
     interval = args.get("polling_interval", DEFAULT_POLLING_INTERVAL)
     timeout = args.get("polling_timeout", DEFAULT_POLLING_TIMEOUT)
-    
+
     # verify the interval is not too small to avoid excessive API calls
     interval = interval if interval >= DEFAULT_POLLING_INTERVAL else DEFAULT_POLLING_INTERVAL
-    
+
     start_time = time.time()
     while True:
         # timeout reached
         passed_time = time.time() - start_time
         if passed_time > timeout:
             return_error(f"Polling timed out after {int(passed_time)} seconds")
-            
+
         # get search status and results
         results = demisto.executeCommand(cmd, args)
         search_status = get_result_value(results, "Status")  # type: ignore
         search_results = get_result_value(results, result_key)  # type: ignore
-    
+
         # if status and results show command finished, return
         if (search_status == "Completed") and (len(search_results) > 2):
             return results  # type: ignore
-        
+
         time.sleep(interval)
 
 
@@ -182,93 +182,90 @@ def main():
         # init variables
         args = parse_args(demisto.args())
         modules = demisto.getModules()
-        context = {
-            CONTEXT_SEARCH_KEY: {},
-            CONTEXT_PREV_KEY: {}
-            }
-        
+        context = {CONTEXT_SEARCH_KEY: {}, CONTEXT_PREV_KEY: {}}
+
         # check if relevant integrations are enabled
         module_enabled = False
         for module in modules:
             if modules[module].get("brand") in SEC_COMP_MODULES:
                 module_enabled = True
                 break
-        
+
         if not module_enabled:
             return_error("Security and Compliance module is not enabled")
-            
+
         # check if search exists
         search_cmd_results = demisto.executeCommand(CMD_GET_SEARCH, args)
         search_name = get_result_value(search_cmd_results, "Name")  # type: ignore
-        
+
         run_new_search = False
-        
+
         # if search does not exist - initiate a new search
         if not search_name:
             run_new_search = True
-        
+
         # if search exists and force flag is used, remove search and make new one
         elif args.get("force", False):
             demisto.executeCommand(CMD_DEL_SEARCH, args)
             run_new_search = True
-        
+
         # create and start a new search
         if run_new_search:
-            
             # validate arguments for new search
             if not args.get("kql_search", None):
                 return_error("Running a new search requires the argument 'kql_search'.")
-                
+
             demisto.executeCommand(CMD_NEW_SEARCH, args)
             demisto.executeCommand(CMD_START_SEARCH, args)
             search_cmd_results = wait_for_results(args=args, cmd=CMD_GET_SEARCH, result_key="SuccessResults")
-                
+
         # get updated search values
         search_name = get_result_value(search_cmd_results, "Name")  # type: ignore
         search_status = get_result_value(search_cmd_results, "Status")  # type: ignore
         search_results = parse_results(get_result_value(search_cmd_results, "SuccessResults"))  # type: ignore
-        
+
         # add search values to context
         add_to_context(context=context, sub_key=CONTEXT_SEARCH_KEY, new_key=CONTEXT_NAME_KEY, new_value=search_name)
         add_to_context(context=context, sub_key=CONTEXT_SEARCH_KEY, new_key=CONTEXT_STATUS_KEY, new_value=search_status)
         add_to_context(context=context, sub_key=CONTEXT_SEARCH_KEY, new_key=CONTEXT_RESULTS_KEY, new_value=search_results)
-        
+
         # if preview is False, return only search results
         if not args.get("preview", False):
             return_results(
                 CommandResults(
                     outputs_prefix=CONTEXT_MAIN_KEY,
                     outputs=context,
-                    readable_output=f"Search [{search_name}] returned with status [{search_status}]")
+                    readable_output=f"Search [{search_name}] returned with status [{search_status}]",
                 )
+            )
             return
-        
+
         # start search action
         demisto.executeCommand(CMD_NEW_SEARCH_ACTION, args)
-        
+
         # add search_action_name and get preview
         args["search_action_name"] = args["search_name"] + SEARCH_ACTION_SUFFIX
         preview_cmd_results = wait_for_results(args=args, cmd=CMD_GET_SEARCH_ACTION, result_key="Results")
-        
+
         # get preview result values
         preview_name = get_result_value(preview_cmd_results, "Name")
         preview_status = get_result_value(preview_cmd_results, "Status")
         preview_results = parse_results(get_result_value(preview_cmd_results, "Results"))
-        
+
         # add preview values to context
         add_to_context(context=context, sub_key=CONTEXT_PREV_KEY, new_key=CONTEXT_NAME_KEY, new_value=preview_name)
         add_to_context(context=context, sub_key=CONTEXT_PREV_KEY, new_key=CONTEXT_STATUS_KEY, new_value=preview_status)
         add_to_context(context=context, sub_key=CONTEXT_PREV_KEY, new_key=CONTEXT_RESULTS_KEY, new_value=preview_results)
-        
+
         # return search and preview results
         return_results(
             CommandResults(
                 outputs_prefix=CONTEXT_MAIN_KEY,
                 outputs=context,
-                readable_output=
-                f"Search [{search_name}] returned with status [{search_status}]\n" +
-                f"Preview [{preview_name}] returned with status [{preview_status}]")
+                readable_output=f"Search [{search_name}] returned with status [{search_status}]\n"
+                + f"Preview [{preview_name}] returned with status [{preview_status}]",
             )
+        )
 
     except Exception as e:
         return_error(f"Failed to execute {SCRIPT_NAME}. Error: {e!s}")
--- Packs/AggregatedScripts/Scripts/O365SecurityComplianceSearch/O365SecurityComplianceSearch_test.py
+++ Packs/AggregatedScripts/Scripts/O365SecurityComplianceSearch/O365SecurityComplianceSearch_test.py
@@ -10,11 +10,11 @@ def test_arg_parse():
     Then:
        - The arguments with correct python types
     """
-    
+
     from O365SecurityComplianceSearch import parse_args
-    
-    args ={
-        "search_name":"test search",
+
+    args = {
+        "search_name": "test search",
         "force": "true",
         "preview": "false",
         "case": "test case",
@@ -26,28 +26,28 @@ def test_arg_parse():
         "share_point_location": "sharepoint1,sharepoint2",
         "share_point_location_exclusion": "exclude1,exclude2",
         "polling_interval": "15",
-        "polling_timeout": "300"
-        }
-    
+        "polling_timeout": "300",
+    }
+
     expected_args = {
-        "search_name":"test search",
+        "search_name": "test search",
         "force": True,
         "preview": False,
         "case": "test case",
         "kql_search": "test search",
         "include_mailboxes": True,
-        "exchange_location": ["exchange1","exchange2"],
-        "exchange_location_exclusion": ["exclude1","exclude2"],
-        "public_folder_location": ["public1","public2"],
-        "share_point_location": ["sharepoint1","sharepoint2"],
-        "share_point_location_exclusion": ["exclude1","exclude2"],
+        "exchange_location": ["exchange1", "exchange2"],
+        "exchange_location_exclusion": ["exclude1", "exclude2"],
+        "public_folder_location": ["public1", "public2"],
+        "share_point_location": ["sharepoint1", "sharepoint2"],
+        "share_point_location_exclusion": ["exclude1", "exclude2"],
         "polling_interval": 15,
-        "polling_timeout": 300
+        "polling_timeout": 300,
     }
-    
+
     assert expected_args == parse_args(args)
-    
-    
+
+
 def test_get_result_value():
     """
     Given:
@@ -57,14 +57,14 @@ def test_get_result_value():
     Then:
        - The value for the given key
     """
-    
+
     from O365SecurityComplianceSearch import get_result_value
-    
+
     cmd_result = [{"Contents": {"OuterKey": {"InnerKey": "Value"}}}]
     expected_value = "Value"
-    
+
     assert get_result_value(cmd_result, "OuterKey.InnerKey") == expected_value  # type: ignore
-    
+
 
 def test_add_to_context():
     """
@@ -75,32 +75,23 @@ def test_add_to_context():
     Then:
        - The updated context
     """
-    
+
     from O365SecurityComplianceSearch import add_to_context
-    
-    context = {
-            "Search": {},
-            "Preview": {}
-            }
-    
+
+    context = {"Search": {}, "Preview": {}}
+
     expected_context = {
-            "Search": {
-                "Name": "search name",
-                "Results": "search results"
-                },
-            "Preview": {
-                "Name": "preview name",
-                "Results": "preview results"
-            }
-            }
-    
+        "Search": {"Name": "search name", "Results": "search results"},
+        "Preview": {"Name": "preview name", "Results": "preview results"},
+    }
+
     add_to_context(context=context, sub_key="Search", new_key="Name", new_value="search name")
     add_to_context(context=context, sub_key="Search", new_key="Results", new_value="search results")
     add_to_context(context=context, sub_key="Preview", new_key="Name", new_value="preview name")
     add_to_context(context=context, sub_key="Preview", new_key="Results", new_value="preview results")
-    
+
     assert expected_context == context
-    
+
 
 def test_wait_for_results(mocker):
     """
@@ -111,21 +102,14 @@ def test_wait_for_results(mocker):
     Then:
        - The search results
     """
-    
+
     from O365SecurityComplianceSearch import wait_for_results
-    
-    args = {
-        "polling_interval": 2,
-        "polling_timeout": 10
-    }
-    
-    expected_results = [{
-        "Contents":{
-            "Status": "Completed",
-            "SuccessResults": "{Location: \"location\",Count: 10}"}
-        }]
-    
+
+    args = {"polling_interval": 2, "polling_timeout": 10}
+
+    expected_results = [{"Contents": {"Status": "Completed", "SuccessResults": '{Location: "location",Count: 10}'}}]
+
     mocker.patch.object(demisto, "executeCommand", return_value=expected_results)
     mock_results = wait_for_results(args=args, cmd="o365-sc-get-search", result_key="SuccessResults")
-    
+
     assert mock_results == expected_results
