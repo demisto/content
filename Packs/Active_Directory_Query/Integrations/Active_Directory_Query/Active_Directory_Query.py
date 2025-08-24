11f2ca14760c5e28906140b8a59e4036077359ef
Bar <bar.9ali@gmail.com>
2025-08-24 15:09:36 +0300

    first commit

--- Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query.py
+++ Packs/Active_Directory_Query/Integrations/Active_Directory_Query/Active_Directory_Query.py
@@ -1341,7 +1341,9 @@ def restore_user(default_base_dn: str, page_size: int) -> int:
     attributes = list(set(DEFAULT_PERSON_ATTRIBUTES))
 
     entries = search_with_paging(query, default_base_dn, attributes=attributes, size_limit=0, page_size=page_size)
+    demisto.log(f"Active Directory restore_user {entries=}")
     if entries.get("flat"):
+        demisto.log(f"Active Directory restore_user flat={entries.get('flat')}")
         return entries.get("flat")[0].get("userAccountControl")[0]
     return 0
 
