6990471c9732e894533feaa12b5385a1597d744c
mayagoldman <mgoldman@paloaltonetworks.com>
2025-08-28 12:38:41 +0300

    pre commot

--- Packs/MicrosoftTeams/Integrations/MicrosoftTeams/MicrosoftTeams.py
+++ Packs/MicrosoftTeams/Integrations/MicrosoftTeams/MicrosoftTeams.py
@@ -1132,7 +1132,9 @@ def integration_health():
         mirrored_channels_human_readable = "No mirrored channels."
 
     res = api_health_human_readable + mirrored_channels_human_readable
-    return_results(CommandResults(raw_response=res, readable_output=res, entry_type=EntryType.NOTE, content_format=EntryFormat.MARKDOWN))
+    return_results(
+        CommandResults(raw_response=res, readable_output=res, entry_type=EntryType.NOTE, content_format=EntryFormat.MARKDOWN)
+    )
 
 
 def validate_auth_header(headers: dict) -> bool:
