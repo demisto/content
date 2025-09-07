278a8f090a5c962e95af8a047815e8b80a9c242b
bluza <bluza@domaintools.com>
2025-09-07 09:14:40 -0700

    Implement domainrisk,domainhotlist feeds; Update test cases

--- Packs/FeedDomainTools/Integrations/FeedDomainTools/test_data/feed_mock_response.py
+++ Packs/FeedDomainTools/Integrations/FeedDomainTools/test_data/feed_mock_response.py
@@ -54,6 +54,17 @@ DOMAINRDAP_RESPONSE = [
     '{"timestamp":"2025-03-10T16:25:34Z","domain":"parkcitieslincolnoffers.com","raw_record":{"first_request_timestamp":"2025-03-10T16:25:29Z","requests":[{"data":"{\\"objectClassName\\":\\"domain\\",\\"handle\\":\\"2071186162_DOMAIN_COM-VRSN\\",\\"ldhName\\":\\"PARKCITIESLINCOLNOFFERS.COM\\",\\"links\\":[{\\"value\\":\\"https:\\\\/\\\\/rdap.verisign.com\\\\/com\\\\/v1\\\\/domain\\\\/PARKCITIESLINCOLNOFFERS.COM\\",\\"rel\\":\\"self\\",\\"href\\":\\"https:\\\\/\\\\/rdap.verisign.com\\\\/com\\\\/v1\\\\/domain\\\\/PARKCITIESLINCOLNOFFERS.COM\\",\\"type\\":\\"application\\\\/rdap+json\\"},{\\"value\\":\\"https:\\\\/\\\\/rdap.squarespace.domains\\\\/domain\\\\/PARKCITIESLINCOLNOFFERS.COM\\",\\"rel\\":\\"related\\",\\"href\\":\\"https:\\\\/\\\\/rdap.squarespace.domains\\\\/domain\\\\/PARKCITIESLINCOLNOFFERS.COM\\",\\"type\\":\\"application\\\\/rdap+json\\"}],\\"status\\":[\\"client delete prohibited\\",\\"client transfer prohibited\\"],\\"entities\\":[{\\"objectClassName\\":\\"entity\\",\\"handle\\":\\"895\\",\\"roles\\":[\\"registrar\\"],\\"publicIds\\":[{\\"type\\":\\"IANA Registrar ID\\",\\"identifier\\":\\"895\\"}],\\"vcardArray\\":[\\"vcard\\",[[\\"version\\",{},\\"text\\",\\"4.0\\"],[\\"fn\\",{},\\"text\\",\\"Squarespace Domains II LLC\\"]]],\\"entities\\":[{\\"objectClassName\\":\\"entity\\",\\"roles\\":[\\"abuse\\"],\\"vcardArray\\":[\\"vcard\\",[[\\"version\\",{},\\"text\\",\\"4.0\\"],[\\"fn\\",{},\\"text\\",\\"\\"],[\\"tel\\",{\\"type\\":\\"voice\\"},\\"uri\\",\\"tel:+1.6466935324\\"],[\\"email\\",{},\\"text\\",\\"abuse-complaints@squarespace.com\\"]]]}]}],\\"events\\":[{\\"eventAction\\":\\"registration\\",\\"eventDate\\":\\"2016-11-03T02:54:47Z\\"},{\\"eventAction\\":\\"expiration\\",\\"eventDate\\":\\"2025-11-03T02:54:47Z\\"},{\\"eventAction\\":\\"last changed\\",\\"eventDate\\":\\"2024-10-19T06:10:25Z\\"},{\\"eventAction\\":\\"last update of RDAP database\\",\\"eventDate\\":\\"2025-03-10T16:25:18Z\\"}],\\"secureDNS\\":{\\"delegationSigned\\":false},\\"nameservers\\":[{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"NS-CLOUD-D1.GOOGLEDOMAINS.COM\\"},{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"NS-CLOUD-D2.GOOGLEDOMAINS.COM\\"},{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"NS-CLOUD-D3.GOOGLEDOMAINS.COM\\"},{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"NS-CLOUD-D4.GOOGLEDOMAINS.COM\\"}],\\"rdapConformance\\":[\\"rdap_level_0\\",\\"icann_rdap_technical_implementation_guide_0\\",\\"icann_rdap_response_profile_0\\"],\\"notices\\":[{\\"title\\":\\"Terms of Use\\",\\"description\\":[\\"Service subject to Terms of Use.\\"],\\"links\\":[{\\"href\\":\\"https:\\\\/\\\\/www.verisign.com\\\\/domain-names\\\\/registration-data-access-protocol\\\\/terms-service\\\\/index.xhtml\\",\\"type\\":\\"text\\\\/html\\"}]},{\\"title\\":\\"Status Codes\\",\\"description\\":[\\"For more information on domain status codes, please visit https:\\\\/\\\\/icann.org\\\\/epp\\"],\\"links\\":[{\\"href\\":\\"https:\\\\/\\\\/icann.org\\\\/epp\\",\\"type\\":\\"text\\\\/html\\"}]},{\\"title\\":\\"RDDS Inaccuracy Complaint Form\\",\\"description\\":[\\"URL of the ICANN RDDS Inaccuracy Complaint Form: https:\\\\/\\\\/icann.org\\\\/wicf\\"],\\"links\\":[{\\"href\\":\\"https:\\\\/\\\\/icann.org\\\\/wicf\\",\\"type\\":\\"text\\\\/html\\"}]}]}","source_type":"registry","timestamp":"2025-03-10T16:25:29Z","url":"https://rdap.verisign.com/com/v1/domain/parkcitieslincolnoffers.com"},{"data":"{\\"rdapConformance\\":[\\"rdap_level_0\\"],\\"objectClassName\\":\\"domain\\",\\"lang\\":\\"en-US\\",\\"events\\":[{\\"eventAction\\":\\"registration\\",\\"eventActor\\":\\"Squarespace Domains II LLC\\",\\"eventDate\\":\\"2016-11-03T02:54:47Z\\"},{\\"eventAction\\":\\"last changed\\",\\"eventActor\\":\\"Squarespace Domains II LLC\\",\\"eventDate\\":\\"2024-10-19T06:10:25Z\\"},{\\"eventAction\\":\\"expiration\\",\\"eventActor\\":\\"Squarespace Domains II LLC\\",\\"eventDate\\":\\"2025-11-03T02:54:47Z\\"}],\\"status\\":[\\"client transfer prohibited\\",\\"client delete prohibited\\"],\\"port43\\":\\"whois.squarespace.domains\\",\\"handle\\":\\"2071186162_DOMAIN_COM-VRSN\\",\\"ldhName\\":\\"parkcitieslincolnoffers.com\\",\\"unicodeName\\":\\"parkcitieslincolnoffers.com\\",\\"secureDNS\\":{\\"delegationSigned\\":false},\\"entities\\":[{\\"objectClassName\\":\\"entity\\",\\"vcardArray\\":[\\"vcard\\",[[\\"version\\",{},\\"text\\",\\"4.0\\"],[\\"fn\\",{},\\"text\\",\\"REDACTED FOR PRIVACY\\"],[\\"adr\\",{},\\"text\\",[\\"\\",\\"\\",\\"REDACTED FOR PRIVACY\\",\\"REDACTED FOR PRIVACY\\",\\"VA\\",\\"REDACTED FOR PRIVACY\\",\\"US\\"]],[\\"org\\",{},\\"text\\",\\"TVM\\"]]],\\"roles\\":[\\"administrative\\"]},{\\"objectClassName\\":\\"entity\\",\\"vcardArray\\":[\\"vcard\\",[[\\"version\\",{},\\"text\\",\\"4.0\\"],[\\"fn\\",{},\\"text\\",\\"REDACTED FOR PRIVACY\\"],[\\"adr\\",{},\\"text\\",[\\"\\",\\"\\",\\"REDACTED FOR PRIVACY\\",\\"REDACTED FOR PRIVACY\\",\\"VA\\",\\"REDACTED FOR PRIVACY\\",\\"US\\"]],[\\"org\\",{},\\"text\\",\\"TVM\\"]]],\\"roles\\":[\\"registrant\\"]},{\\"objectClassName\\":\\"entity\\",\\"vcardArray\\":[\\"vcard\\",[[\\"version\\",{},\\"text\\",\\"4.0\\"],[\\"fn\\",{},\\"text\\",\\"REDACTED FOR PRIVACY\\"],[\\"adr\\",{},\\"text\\",[\\"\\",\\"\\",\\"REDACTED FOR PRIVACY\\",\\"REDACTED FOR PRIVACY\\",\\"VA\\",\\"REDACTED FOR PRIVACY\\",\\"US\\"]],[\\"org\\",{},\\"text\\",\\"TVM\\"]]],\\"roles\\":[\\"technical\\"]}],\\"publicIds\\":[{\\"type\\":\\"IANA Registrar ID\\",\\"identifier\\":\\"895\\"}],\\"nameservers\\":[{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"ns-cloud-d2.googledomains.com\\",\\"unicodeName\\":\\"ns-cloud-d2.googledomains.com\\",\\"ipAddresses\\":{\\"v4\\":[],\\"v6\\":[]}},{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"ns-cloud-d3.googledomains.com\\",\\"unicodeName\\":\\"ns-cloud-d3.googledomains.com\\",\\"ipAddresses\\":{\\"v4\\":[],\\"v6\\":[]}},{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"ns-cloud-d1.googledomains.com\\",\\"unicodeName\\":\\"ns-cloud-d1.googledomains.com\\",\\"ipAddresses\\":{\\"v4\\":[],\\"v6\\":[]}},{\\"objectClassName\\":\\"nameserver\\",\\"ldhName\\":\\"ns-cloud-d4.googledomains.com\\",\\"unicodeName\\":\\"ns-cloud-d4.googledomains.com\\",\\"ipAddresses\\":{\\"v4\\":[],\\"v6\\":[]}}]}","source_type":"registrar","timestamp":"2025-03-10T16:25:32Z","url":"https://rdap.squarespace.domains/domain/PARKCITIESLINCOLNOFFERS.COM"}]},"parsed_record":{"parsed_fields":{"conformance":["rdap_level_0"],"contacts":[{"city":"REDACTED FOR PRIVACY","country":"US","name":"REDACTED FOR PRIVACY","org":"TVM","postal":"REDACTED FOR PRIVACY","region":"VA","roles":["administrative"],"street":"REDACTED FOR PRIVACY"},{"city":"REDACTED FOR PRIVACY","country":"US","name":"REDACTED FOR PRIVACY","org":"TVM","postal":"REDACTED FOR PRIVACY","region":"VA","roles":["registrant"],"street":"REDACTED FOR PRIVACY"},{"city":"REDACTED FOR PRIVACY","country":"US","name":"REDACTED FOR PRIVACY","org":"TVM","postal":"REDACTED FOR PRIVACY","region":"VA","roles":["technical"],"street":"REDACTED FOR PRIVACY"}],"creation_date":"2016-11-03T02:54:47+00:00","dnssec":{"signed":false},"domain":"parkcitieslincolnoffers.com","domain_statuses":["client transfer prohibited","client delete prohibited"],"email_domains":["squarespace.com"],"emails":["abuse-complaints@squarespace.com"],"expiration_date":"2025-11-03T02:54:47+00:00","handle":"2071186162_DOMAIN_COM-VRSN","last_changed_date":"2024-10-19T06:10:25+00:00","links":[{"href":"https://rdap.verisign.com/com/v1/domain/PARKCITIESLINCOLNOFFERS.COM","rel":"self"},{"href":"https://rdap.squarespace.domains/domain/PARKCITIESLINCOLNOFFERS.COM","rel":"related"}],"nameservers":["ns-cloud-d2.googledomains.com","ns-cloud-d3.googledomains.com","ns-cloud-d1.googledomains.com","ns-cloud-d4.googledomains.com"],"registrar":{"contacts":[{"email":"abuse-complaints@squarespace.com","name":"","phone":"tel:+1.6466935324","roles":["abuse"]}],"iana_id":"895","name":"Squarespace Domains II LLC"},"unclassified_emails":[]},"registrar_request_url":"https://rdap.squarespace.domains/domain/PARKCITIESLINCOLNOFFERS.COM","registry_request_url":"https://rdap.verisign.com/com/v1/domain/parkcitieslincolnoffers.com"}}'
 ]
 
+DOMAINRISK_RESPONSE = [
+    '{"timestamp":"2025-09-06T21:18:06Z","domain":"resmiguncelsite.com","phishing_risk":99,"malware_risk":99,"spam_risk":99,"proximity_risk":100,"overall_risk":100}',
+    '{"timestamp":"2025-09-06T21:42:06Z","domain":"bpi-cye.co","phishing_risk":99,"malware_risk":99,"spam_risk":99,"proximity_risk":100,"overall_risk":45}',
+    '{"timestamp":"2025-09-06T21:12:06Z","domain":"antthrobchannel.com","phishing_risk":99,"malware_risk":99,"spam_risk":99,"proximity_risk":100,"overall_risk":63}'
+]
+
+DOMAINHOTLIST_RESPONSE = [
+    '{"timestamp":"2025-09-06T22:27:06Z","domain":"scmipgf.icu","phishing_risk":99,"malware_risk":99,"spam_risk":99,"proximity_risk":100,"overall_risk":100,"expires":"2025-09-07T11:34:51Z"}',
+    '{"timestamp":"2025-09-06T22:26:49Z","domain":"growth-hacker-ppc.com","phishing_risk":99,"malware_risk":99,"spam_risk":99,"proximity_risk":100,"overall_risk":100,"expires":"2025-09-07T19:19:29Z"}',
+    '{"timestamp":"2025-09-06T22:26:55Z","domain":"gq5zn0u.top","phishing_risk":99,"malware_risk":99,"spam_risk":99,"proximity_risk":100,"overall_risk":100,"expires":"2025-09-07T11:53:17Z"}'
+]
 
 NOD_PARSED_INDICATOR_RESPONSE = [
     {
@@ -668,7 +679,6 @@ DOMAINDISCOVERY_PARSED_INDICATOR_RESPONSE = [
     },
 ]
 
-
 DOMAINRDAP_PARSED_INDICATOR_RESPONSE = [
     {
         "value": "parkcitieslincolnoffers.com",
@@ -764,5 +774,107 @@ DOMAINRDAP_PARSED_INDICATOR_RESPONSE = [
                 "registry_request_url": "https://rdap.verisign.com/com/v1/domain/parkcitieslincolnoffers.com",
             },
         },
+    }
+]
+
+DOMAINRISK_PARSED_INDICATOR_RESPONSE = [
+    {
+        "value": "resmiguncelsite.com",
+        "type": "Domain",
+        "fields": {
+            "tags": "DomainToolsFeeds,domainrisk",
+            "service": "DomainTools Feeds",
+            "firstseenbysource": "2025-09-06T21:18:06Z",
+            "sourcebrands": "FeedDomainTools",
+        },
+        "rawJSON": {
+            "value": "resmiguncelsite.com",
+            "type": "Domain",
+            "timestamp": "2025-09-06T21:18:06Z",
+        },
+        "score": 3
+    },
+    {
+        "value": "bpi-cye.co",
+        "type": "Domain",
+        "fields": {
+            "tags": "DomainToolsFeeds,domainrisk",
+            "service": "DomainTools Feeds",
+            "firstseenbysource": "2025-09-06T21:42:06Z",
+            "sourcebrands": "FeedDomainTools",
+        },
+        "rawJSON": {
+            "value": "bpi-cye.co",
+            "type": "Domain",
+            "timestamp": "2025-09-06T21:42:06Z",
+        },
+         "score": 1
+    },
+    {
+        "value": "antthrobchannel.com",
+        "type": "Domain",
+        "fields": {
+            "tags": "DomainToolsFeeds,domainrisk",
+            "service": "DomainTools Feeds",
+            "firstseenbysource": "2025-09-06T21:12:06Z",
+            "sourcebrands": "FeedDomainTools",
+        },
+        "rawJSON": {
+            "value": "antthrobchannel.com",
+            "type": "Domain",
+            "timestamp": "2025-09-06T21:12:06Z",
+        },
+         "score": 2
+    }
+]
+
+DOMAINHOTLIST_PARSED_INDICATOR_RESPONSE = [
+    {
+        "value": "scmipgf.icu",
+        "type": "Domain",
+        "fields": {
+            "tags": "DomainToolsFeeds,domainhotlist",
+            "service": "DomainTools Feeds",
+            "firstseenbysource": "2025-09-06T22:27:06Z",
+            "sourcebrands": "FeedDomainTools",
+        },
+        "rawJSON": {
+            "value": "scmipgf.icu",
+            "type": "Domain",
+            "timestamp": "2025-09-06T22:27:06Z",
+        },
+        "score": 3
+    },
+    {
+        "value": "growth-hacker-ppc.com",
+        "type": "Domain",
+        "fields": {
+            "tags": "DomainToolsFeeds,domainhotlist",
+            "service": "DomainTools Feeds",
+            "firstseenbysource": "2025-09-06T22:26:49Z",
+            "sourcebrands": "FeedDomainTools",
+        },
+        "rawJSON": {
+            "value": "growth-hacker-ppc.com",
+            "type": "Domain",
+            "timestamp": "2025-09-06T22:26:49Z",
+        },
+         "score": 1
+    },
+    {
+        "value": "gq5zn0u.top",
+        "type": "Domain",
+        "fields": {
+            "tags": "DomainToolsFeeds,domainhotlist",
+            "service": "DomainTools Feeds",
+            "firstseenbysource": "2025-09-06T22:26:55Z",
+            "sourcebrands": "FeedDomainTools",
+        },
+        "rawJSON": {
+            "value": "gq5zn0u.top",
+            "type": "Domain",
+            "timestamp": "2025-09-06T22:26:55Z",
+        },
+         "score": 2
     }
 ]
