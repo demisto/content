9b91b6e989b2e161029e18ffe0db047e2997c632
Sapir Malka <samalka@paloaltonetworks.com>
2025-09-30 15:51:14 +0300

    ruff

--- Packs/Base/Scripts/SendNotificationAgentix/SendNotificationAgentix.py
+++ Packs/Base/Scripts/SendNotificationAgentix/SendNotificationAgentix.py
@@ -6,25 +6,26 @@ BRAND_MAPPING = {
     "Microsoft Teams": {
         "brand": "Microsoft Teams",
         "valid_args": {"brand", "message", "to", "channel", "team"},
-        },
+    },
     "Slack": {
         "brand": "SlackV3",
         "valid_args": {"brand", "message", "to", "channel", "channel_id"},
-        },
+    },
     "Mattermost": {
         "brand": "MattermostV2",
         "valid_args": {"brand", "message", "to", "channel"},
-        },
+    },
     "Zoom": {
         "brand": "Zoom",
         "valid_args": {"brand", "message", "to", "channel", "channel_id"},
-        },
+    },
 }
+
 
 def send_notification_by_brand(brand: str, args: dict):
     if not (brand_map := BRAND_MAPPING.get(brand)):
         raise DemistoException(f"Brand - {brand} is not supported. Supported brands: {BRAND_MAPPING.keys()}")
-    
+
     valid_args = set(brand_map.get("valid_args", []))
     invalid_args = list(set(args.keys()) - valid_args)
     if invalid_args:
