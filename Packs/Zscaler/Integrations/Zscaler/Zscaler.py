07a16da38a2c31b6971d5311520a092f3acaf407
shmuel44 <skroizer@paloaltonetworks.com>
2025-10-16 14:42:55 +0300

    relevant changes only

--- Packs/Zscaler/Integrations/Zscaler/Zscaler.py
+++ Packs/Zscaler/Integrations/Zscaler/Zscaler.py
@@ -1275,10 +1275,10 @@ def main():  # pragma: no cover
         finally:
             try:
                 # activate changes only when required
-                if params.get("auto_activate") and command in AUTO_ACTIVATE_CHANGES_COMMANDS:
+                if demisto.params().get("auto_activate") and command in AUTO_ACTIVATE_CHANGES_COMMANDS:
                     demisto.debug(f"Activating changes for command {command}")
                     activate_changes()
-                if params.get("auto_logout"):
+                if demisto.params().get("auto_logout"):
                     demisto.debug("Logging out")
                     logout()
             except Exception as err:
