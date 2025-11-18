4d90445f477691aed0c543363a67a4662542ec1d
Tal <tcarmeli@paloaltonetworks.com>
2025-11-18 14:24:24 +0200

    first commit

--- Packs/ctf01/Integrations/CortexXDRIRCTF/CortexXDRIRCTF.py
+++ Packs/ctf01/Integrations/CortexXDRIRCTF/CortexXDRIRCTF.py
@@ -6,7 +6,7 @@ from itertools import zip_longest
 import demistomock as demisto  # noqa: F401
 from CommonServerPython import *  # noqa: F401
 from CoreIRApiModule import *
-
+ 
 # Disable insecure warnings
 urllib3.disable_warnings()
 
