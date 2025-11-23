c75dd1696ffe2295012c4b3a6f1c8131eed5774d
samuelFain <sfainberg@paloaltonetworks.com>
2025-11-23 20:16:55 +0200

    chore: return indent changes to original state

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -9459,7 +9459,7 @@ if 'requests' in sys.modules:
                 than :attr:`Retry.BACKOFF_MAX`.
 
                 By default, backoff_factor set to 5
-
+                
             :type backoff_jitter ``float``
             :param backoff_jitter: the sleep (backoff factor) is extended by
                 random.uniform(0, {backoff jitter})
