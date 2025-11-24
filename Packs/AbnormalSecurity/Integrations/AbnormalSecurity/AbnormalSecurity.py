56f34f78c777d39659fc20f10df5103ec63f6441
merit-maita <meretmaayta@gmail.com>
2025-11-24 12:50:25 +0200

    change

--- Packs/AbnormalSecurity/Integrations/AbnormalSecurity/AbnormalSecurity.py
+++ Packs/AbnormalSecurity/Integrations/AbnormalSecurity/AbnormalSecurity.py
@@ -225,7 +225,7 @@ class Client(BaseClient):
             page_number (int, optional): The page number (zero-based)
 
         Returns:
-            dict: The threat details with pagination
+            dict: The threat details with pagination tet
         """
         headers = self._headers
         params = assign_params(subtenant=subtenant, pageSize=page_size, pageNumber=page_number)
