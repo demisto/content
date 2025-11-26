72535d6f7e277a3f4b80ad038ce1a8d1edba2d6d
klevitskiy <kirilll@checkpoint.com>
2025-11-26 11:53:14 +0100

    bump version in headers

--- Packs/Cyberint/Integrations/Cyberint/Cyberint.py
+++ Packs/Cyberint/Integrations/Cyberint/Cyberint.py
@@ -63,7 +63,7 @@ class Client(BaseClient):
             "X-Integration-Instance-Name": demisto.integrationInstance(),
             "X-Integration-Instance-Id": "",
             "X-Integration-Customer-Name": params.get("client_name", ""),
-            "X-Integration-Version": "1.1.12",
+            "X-Integration-Version": "1.1.13",
         }
         super().__init__(base_url=base_url, verify=verify_ssl, proxy=proxy)
 
