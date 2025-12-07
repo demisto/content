8218dfafacb81060495ed43f1478a0abec141050
meichler <meichler@paloaltonetworks.com>
2025-12-07 11:10:38 +0200

    fix ruff

--- Packs/FeedElasticsearch/Integrations/FeedElasticsearch/FeedElasticsearch.py
+++ Packs/FeedElasticsearch/Integrations/FeedElasticsearch/FeedElasticsearch.py
@@ -46,7 +46,7 @@ if ELASTIC_SEARCH_CLIENT == OPEN_SEARCH:
 elif ELASTIC_SEARCH_CLIENT == ELASTICSEARCH_V8:
     from elasticsearch import Elasticsearch  # type: ignore[assignment]
     from elasticsearch.helpers import scan  # type: ignore[assignment]
-    from elasticsearch_dsl import Search # pylint: disable=E0611
+    from elasticsearch_dsl import Search  # pylint: disable=E0611
     from elasticsearch_dsl.query import QueryString  # pylint: disable=E0401,E0611
 else:  # Elasticsearch (<= v7)
     from elasticsearch7 import Elasticsearch, RequestsHttpConnection  # type: ignore[assignment]
