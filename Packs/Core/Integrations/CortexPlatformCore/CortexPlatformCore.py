b3faa3c48d956a78a4907d2b4ff74303e89af059
mayyagoldman <mayyagoldman@gmail.com>
2025-12-14 16:24:50 +0200

    start and end

--- Packs/Core/Integrations/CortexPlatformCore/CortexPlatformCore.py
+++ Packs/Core/Integrations/CortexPlatformCore/CortexPlatformCore.py
@@ -374,8 +374,9 @@ class FilterBuilder:
             end_time (str | None): The end time of the range.
         """
         start, end = self._prepare_time_range(start_time, end_time)
-        if start and end:
-            self.add_field(name, FilterType.RANGE, {"from": start, "to": end})
+        if start is None and end is None:
+            return
+        self.add_field(name, FilterType.RANGE, {"from": start, "to": end})
 
     def to_dict(self) -> dict[str, list]:
         """
