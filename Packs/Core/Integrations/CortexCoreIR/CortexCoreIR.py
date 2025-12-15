d840f80ae28d2f849c22199f3dc58ab01aaa4f43
hyaffe <hyaffe@paloaltonetworks.com>
2025-12-15 10:45:40 +0200

    success and failure messages

--- Packs/Core/Integrations/CortexCoreIR/CortexCoreIR.py
+++ Packs/Core/Integrations/CortexCoreIR/CortexCoreIR.py
@@ -899,19 +899,26 @@ def update_endpoints_tags_command(client: Client, args: dict) -> List[CommandRes
     command_results = []
 
     if tags_to_add or tags_to_remove:
+        success_message = (f"Successfully updated tags for endpoint(s) {endpoint_ids}."
+        + (f" Added tags: {tags_to_add}" if tags_to_add else "")
+        + (f" Removed tags: {tags_to_remove}" if tags_to_remove else "")
+        + ".")
         command_results.append(
             CommandResults(
-                readable_output=f"Successfully updated tags for endpoint(s) {endpoint_ids}."
-                + (f" Added tags: {tags_to_add}" if tags_to_add else "")
-                + (f" Removed tags: {tags_to_remove}" if tags_to_remove else "")
-                + ".",
+                readable_output=success_message,
+                outputs_prefix=f"{INTEGRATION_CONTEXT_BRAND}.EndpointTags.SuccessMessage",
+                outputs = success_message,
             )
         )
     if invalid_tags:
+        failure_message = f"Invalid tags detected: {', '.join(invalid_tags)}. Tags must be less than 64 characters long."
         command_results.append(
             CommandResults(
-                readable_output=f"Invalid tags detected: {', '.join(invalid_tags)}. Tags must be less than 64 characters long.",
+                readable_output=failure_message,
                 entry_type=4,
+                outputs_prefix=f"{INTEGRATION_CONTEXT_BRAND}.EndpointTags.FailureMessage",
+                outputs = failure_message,
+                
             )
         )
 
