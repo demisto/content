c396d42544a2b1ddb345a83e02ac4c95925302b0
hyaffe <hyaffe@paloaltonetworks.com>
2025-12-15 07:40:59 +0200

    rn and remove comments from test file

--- Packs/Base/Scripts/SearchIndicatorRelationshipsAgentix/SearchIndicatorRelationshipsAgentix_test.py
+++ Packs/Base/Scripts/SearchIndicatorRelationshipsAgentix/SearchIndicatorRelationshipsAgentix_test.py
@@ -1,13 +1,3 @@
-"""Base Script for Cortex XSOAR - Unit Tests file
-
-Pytest Unit Tests: all funcion names must start with "test_"
-
-More details: https://xsoar.pan.dev/docs/integrations/unit-testing
-
-MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
-
-"""
-
 import json
 
 
