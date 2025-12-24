2fdd29b7cd0759d43d99874c6d1f1581d4b44737
adi88d <adaud@paloaltonetworks.com>
2025-12-24 20:00:01 +0200

    add set_is_opp

--- Packs/QRadar/Integrations/QRadar_v3/QRadar_v3.py
+++ Packs/QRadar/Integrations/QRadar_v3/QRadar_v3.py
@@ -47,6 +47,7 @@ EVENTS_SEARCH_RETRY_SECONDS = 100  # seconds between retries to create a new sea
 CONNECTION_ERRORS_RETRIES = 5  # num of times to retry in case of connection-errors
 CONNECTION_ERRORS_INTERVAL = 1  # num of seconds between each time to send an http-request in case of a connection error.
 LAST_FETCHED_ID = None
+IS_OPP = None
 
 ADVANCED_PARAMETERS_STRING_NAMES = [
     "DOMAIN_ENRCH_FLG",
@@ -1303,6 +1304,18 @@ def qradar_set_integration_context(context_data):
         demisto.info(f"LAST_FETCHED_ID variable set to {LAST_FETCHED_ID}")
     else:
         demisto.info("LAST_FETCH_KEY not found in context")
+
+
+def set_is_opp():
+    global IS_OPP
+
+    if IS_OPP == None:
+        res = demisto.internalHttpRequest("GET", "/about")
+        if res and res.get('body') and json.loads(res.get('body', '{}')):
+            deployment_mode = json.loads(res.get('body', '{}')).get('deploymentMode').lower()
+            IS_OPP = True if deployment_mode == 'opp' else False
+            demisto.info(f"set IS_OPP value to {IS_OPP}")
+    return IS_OPP
 
 def safely_update_context_data_partial(
     changes: dict,
@@ -5528,6 +5541,8 @@ def main() -> None:  # pragma: no cover
         raise DemistoException(f"API version cannot be lower than {MINIMUM_API_VERSION}")
     credentials = params.get("credentials")
     timeout = arg_to_number(params.get("timeout"))
+
+    set_is_opp()
 
     try:
         client = Client(
