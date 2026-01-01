a209537500665c5f080a5c1c9f94651207a8ac0f
hyaffe <hyaffe@paloaltonetworks.com>
2026-01-01 13:04:27 +0200

    hirerchy

--- Packs/Gmail/Integrations/Gmail/Gmail.py
+++ Packs/Gmail/Integrations/Gmail/Gmail.py
@@ -1487,7 +1487,7 @@ def search_command(mailbox: str = None, only_return_account_names: bool = False)
         if nextPageToken:
             if 'EntryContext' not in res:
                 res['EntryContext'] = {}
-            res['EntryContext']['Gmail.PageToken'] = {'NextPageToken': nextPageToken}
+            res['EntryContext']['GmailPageToken'] = {'NextPageToken': nextPageToken}
         return res
     return None
 
