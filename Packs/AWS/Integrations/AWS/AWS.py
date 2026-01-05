f11ddaea13b1e3ec81553983c1324ec13e5bc351
noydavidi <nodavidi.paloaltonetworks.com>
2026-01-05 15:22:46 +0200

    added delete permission

--- Packs/AWS/Integrations/AWS/AWS.py
+++ Packs/AWS/Integrations/AWS/AWS.py
@@ -4205,6 +4205,7 @@ REQUIRED_ACTIONS: list[str] = [
     "s3:GetEncryptionConfiguration",
     "s3:DeleteBucketPolicy",
     "s3:ListObjects",
+    "s3:DeleteBucket",
     "acm:UpdateCertificateOptions",
     "cloudtrail:DescribeTrails",
     "lambda:GetFunctionConfiguration",
