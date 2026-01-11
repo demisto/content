d89ad9d7a9d6a22f1c4a8760039e8fea73f408e1
bowenaguero <bowen@example.com>
2026-01-11 13:31:42 +0000

    Remove action dict and instead update state object directly

--- Packs/Slack/Integrations/SlackV3/SlackV3.py
+++ Packs/Slack/Integrations/SlackV3/SlackV3.py
@@ -1610,12 +1610,10 @@ async def listen(client: SocketModeClient, req: SocketModeRequest):
                 if actions[0].get("action_id") == "xsoar-button-submit":
                     demisto.debug("Handling a SlackBlockBuilder response.")
                     if state:
-                        action_dict = {
-                            "xsoar-button-submit": "Successful",
-                            "submitting_user": user_profile
-                        }
-                        action_dict.update(state)
-                        action_text = json.dumps(action_dict)
+                        state.update(
+                            remove_empty_elements({"xsoar-button-submit": "Successful", "submitting_user": user_profile})
+                        )
+                        action_text = json.dumps(state)
                 else:
                     demisto.debug("Not handling a SlackBlockBuilder response.")
                     action_text = actions[0].get("text").get("text")
