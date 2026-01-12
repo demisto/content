88c115505d39741e6f91ab608365ba01ea2f9c8a
klevitskiy <155461095+klevitskiy@users.noreply.github.com>
2026-01-12 19:02:26 +0100

    Update the data residency field name (#42048)
    * update the Data Residency field name
    
    * update the Data Residency field name
    
    * bump version in headers
    
    * remove region field, fix headers
    
    * add pytest-asyncio
    
    * fix lib versions
    
    * add new fetch types
    
    * update demisto-sdk
    
    * update demisto-sdk
    
    * add new alert fetch types
    
    * update poetry.lock
    
    * update docs
    
    * update poetry.lock
    
    * update poetry.lock
    
    * update docs
    
    * minor update
    
    * update poetry.lock
    
    * Update Packs/Cyberint/ReleaseNotes/1_1_13.md
    
    Co-authored-by: talihaff <thaffner@paloaltonetworks.com>
    
    * Update Packs/Cyberint/ReleaseNotes/1_1_13.md
    
    Co-authored-by: talihaff <thaffner@paloaltonetworks.com>
    
    ---------
    
    Co-authored-by: talihaff <thaffner@paloaltonetworks.com>

--- Packs/Cyberint/Integrations/TakedownCyberint/TakedownCyberint.py
+++ Packs/Cyberint/Integrations/TakedownCyberint/TakedownCyberint.py
@@ -30,9 +30,9 @@ class Client(BaseClient):
             "X-Integration-Instance-Name": demisto.integrationInstance(),
             "X-Integration-Instance-Id": "",
             "X-Integration-Customer-Name": params.get("client_name", ""),
-            "X-Integration-Version": "1.1.9",
+            "X-Integration-Version": "1.1.13",
         }
-        super().__init__(base_url=base_url, verify=verify, proxy=proxy)
+        super().__init__(base_url=base_url, verify=verify, proxy=proxy, headers=self._headers)
 
     @logger
     def submit_takedown_request(
