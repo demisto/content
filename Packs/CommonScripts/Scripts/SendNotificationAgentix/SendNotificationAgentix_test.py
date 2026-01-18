d2c7dbfd957124b5a0890a57aa80eed24a9bf5c0
iapt@paloaltonetworks.com <iapt@paloaltonetworks.com>
2026-01-18 09:30:41 +0200

    pre-commit

--- Packs/CommonScripts/Scripts/SendNotificationAgentix/SendNotificationAgentix_test.py
+++ Packs/CommonScripts/Scripts/SendNotificationAgentix/SendNotificationAgentix_test.py
@@ -207,10 +207,7 @@ def test_send_notification_multiple_invalid_args():
     assert all(arg in error_message for arg in ["team", "channel_id", "invalid_arg"])
 
 
-@pytest.mark.parametrize("brand, mapped_arg", [
-    ("Microsoft Teams", "message_id"),
-    ("Slack", "threadID")
-])
+@pytest.mark.parametrize("brand, mapped_arg", [("Microsoft Teams", "message_id"), ("Slack", "threadID")])
 def test_send_notification_with_thread_id(mocker, brand, mapped_arg):
     """
     Given:
@@ -222,17 +219,14 @@ def test_send_notification_with_thread_id(mocker, brand, mapped_arg):
     """
     mock_execute_command = mocker.patch.object(demisto, "executeCommand")
 
-    args = {
-        "message": "Test notification",
-        "thread_id": "12345"
-    }
+    args = {"message": "Test notification", "thread_id": "12345"}
 
     send_notification_by_brand(brand, args)
 
     expected_args = {
         "using-brand": BRAND_MAPPING.get(brand, {}).get("brand"),
         "message": "Test notification",
-        mapped_arg: "12345"
+        mapped_arg: "12345",
     }
 
     mock_execute_command.assert_called_once_with(
