6f395d6ef50bd7400cd5156e2d999c746caf81e5
mayyagoldman <mayyagoldman@gmail.com>
2026-01-20 21:18:57 +0200

    fix test

--- Packs/Core/Integrations/CortexPlatformCore/CortexPlatformCore.py
+++ Packs/Core/Integrations/CortexPlatformCore/CortexPlatformCore.py
@@ -1959,7 +1959,7 @@ def get_cases_command(client, args):
     if (
         returned_count == 1 and data[0].get("issue_count", 0) > 1
     ):  # AI summary supported in cases of a single case query with more than one issue
-        case_id = data.get("case_id")
+        case_id = data[0].get("case_id")
         try:  # if functionality isn't supported exception is raised and should be handled
             response = client.get_case_ai_summary(case_id)
             if response:
