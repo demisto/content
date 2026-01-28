60e109c86e484ebf86f6c7f0f82dd8f31e307627
Dor Zion <dzion@paloaltonetworks.com>
2026-01-28 14:18:23 +0200

    add action and script

---
+++ Packs/CAS/Scripts/CreateAppsecRule/CreateAppsecRule.py
@@ -0,0 +1,102 @@
+import demistomock as demisto  # noqa: F401
+from CommonServerPython import *  # noqa: F401
+import json
+
+
+def main():
+    try:
+        args = demisto.args()
+        """
+        Creates a new rule in Cortex Platform with defined conditions, scope, and triggers.
+        Args:
+            client: The Cortex Platform client instance.
+            args: Dictionary containing rule configuration parameters including:
+                - rule_name: Required name for the new rule
+                - description: Optional rule description
+                - severity: Required severity for the new rule
+                - labels: Optional labels to be assigned to the rule
+                - scanner: Required The type of security scanner used to detect findings of this rule IaC/secret
+                - category: Required Custom rule IaC/secret category
+                - subCategory: Required for IaC scanner only 
+                - frameworks: Required rule frameworks configuration parameters name and definition
+
+        Returns:
+            CommandResults: Results object containing the created rule information with
+            readable output, outputs prefix, and raw response data.
+
+        Raises:
+            DemistoException: If rule name is missing.
+        """
+        rule_name = args.get("rule_name")
+        severity = args.get("severity")
+        scanner = args.get("scanner")
+        category = args.get("category")
+        subCategory = args.get("subCategory")
+        frameworks = argToList(args.get("frameworks"))
+
+        if not rule_name:
+            raise DemistoException("Rule name is required.")
+
+        if not severity:
+            raise DemistoException("Severity is required.")
+
+        if not scanner:
+            raise DemistoException("Scanner is required.")
+
+        if not category:
+            raise DemistoException("Category is required.")
+
+        if scanner is 'IaC' and not subCategory:
+            raise DemistoException("SubCategory is required for IaC scanner.")
+
+        if len(frameworks) == 0 :
+            raise DemistoException("Frameworks is required.")
+
+        description = args.get("description", "")
+        labels = argToList(args.get("labels"))
+
+        payload = {
+            "name": rule_name,
+            "description": description,
+            "severity": severity,
+            "labels": labels,
+            "scanner": scanner,
+            "category": category,
+            "subCategory": subCategory,
+            "frameworks": frameworks,
+        }
+        payload = json.dumps(payload)
+
+        res = demisto.executeCommand(
+            "core-generic-api-call",
+            {
+                "path": "/public_api/appsec/v1/rules",
+                "method": "POST",
+                "data": payload
+            },
+        )
+
+        if is_error(res):
+            return_error(res)
+
+        else:
+            context = res[0]["EntryContext"]
+            data = context.get("data")
+            data = json.loads(data)
+            reply = data.get("reply")
+            data = reply.get("DATA")
+
+            return_results(
+                CommandResults(
+                    outputs_prefix="Core.Case",
+                    outputs=data,
+                    readable_output=f"Cases {data}",
+                    raw_response=data,
+                )
+            )
+    except Exception as ex:
+        return_error(f"Failed to execute GetCases. Error:\n{str(ex)}")
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):  # pragma: no cover
+    main()
