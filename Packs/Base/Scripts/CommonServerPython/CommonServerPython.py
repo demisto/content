79781008adeebe3b8e4b8ab9ef3fd66eeea47fbb
adi88d <adaud@paloaltonetworks.com>
2026-02-01 14:41:01 +0200

    update get_integration_context

--- Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
+++ Packs/Base/Scripts/CommonServerPython/CommonServerPython.py
@@ -10516,7 +10516,9 @@ def get_integration_context(sync=True, with_version=False):
         if with_version:
             return integration_context
         else:
-            return integration_context.get('context', {})
+            if isinstance(integration_context, list):
+                demisto.error(f"The integration context is a list: {integration_context=}")
+            return integration_context.get("context", {})
     else:
         return demisto.getIntegrationContext()
 
