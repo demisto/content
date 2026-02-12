48138f6557067e122fa3c96736e913a42b8e4925
Alan Justino <alan.justino@yahoo.com.br>
2026-02-12 14:49:44 +0000

    Allow current & upcoming version of polyswarm_api
    Newer versions will default the user-agent to have
    `polyswarm_api` instead of `polyswarm-api`

--- Packs/PolySwarm/Integrations/PolySwarmV2/PolySwarmV2_test.py
+++ Packs/PolySwarm/Integrations/PolySwarmV2/PolySwarmV2_test.py
@@ -204,7 +204,7 @@ def test_file(mocker):
 
     # Verify custom User-Agent header
     ua = polyswarm.polyswarm_api.session.headers["User-Agent"]
-    assert "polyswarm_api/" in ua
+    assert "polyswarm_api/" in ua or "polyswarm-api/" in ua
     assert f"xsoar-PolySwarmV2/{PolySwarmV2.POLYSWARM_DEMISTO_VERSION}" in ua
 
     param = {"hash": TEST_HASH_FILE}
