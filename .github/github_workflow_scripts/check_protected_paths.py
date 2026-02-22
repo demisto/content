d02e6d275b481b6f721905caf9daed93d843a8e0
barryyosi-panw <byosilevich@paloaltonetworks.com>
2026-02-22 15:39:12 +0200

    Update check for protected paths to include directories.

--- .github/github_workflow_scripts/check_protected_paths.py
+++ .github/github_workflow_scripts/check_protected_paths.py
@@ -49,7 +49,7 @@ def is_path_change_allowed(path: Path) -> bool:
 
 
 def main(changed_files: list[str]):
-    if unsafe_changes := sorted(full_path for full_path in changed_files if not is_path_change_allowed(Path(full_path))):
+    if unsafe_changes := sorted(path for path in changed_files if not is_path_change_allowed(Path(path))):
         for path in unsafe_changes:
             print(  # noqa: T201
                 f"::error file={path},line=1,endLine=1,title=Protected folder::"
@@ -61,7 +61,7 @@ def main(changed_files: list[str]):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Check for changes in protected directories.")
+    parser = argparse.ArgumentParser(description="Check for changes in protected paths")
     parser.add_argument("changed_files", nargs="+", help="List of changed files.")
     args = parser.parse_args()
     main(args.changed_files)
