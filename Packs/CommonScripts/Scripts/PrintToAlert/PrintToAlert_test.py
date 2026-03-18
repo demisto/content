01a9b5ad06250684c684c7f82a40048c215f538d
Jaden Evanger <123699247+xsoarguy@users.noreply.github.com>
2026-03-18 06:59:18 -0500

    Added ability to mark resulting entry as a note in script PrintToAlert (#43371)
    * Added ability to mark resulting entry as a note in script PrintToAlert
    
    * Modified CONTRIBUTORS.json file
    
    * Improved input handling and docstrings for mark_as_note argument in Script PrintToAlert
    
    * Updated README.md for Script PrintToAlert
    
    * Fixed spelling error in Script PrintToAlert

--- Packs/CommonScripts/Scripts/PrintToAlert/PrintToAlert_test.py
+++ Packs/CommonScripts/Scripts/PrintToAlert/PrintToAlert_test.py
@@ -33,7 +33,41 @@ def test_print_to_alert(mocker: MockerFixture):
     assert execute_command_mocker.call_args[0][0] == "addEntries"
     # Right arguments are given
     assert execute_command_mocker.call_args[0][1] == {
-        "entries": '[{"Type": 1, "ContentsFormat": "markdown", "Contents": "Entry from alert #5:\\nHello"}]',
+        "entries": '[{"Type": 1, "ContentsFormat": "markdown", "Contents": "Entry from alert #5:\\nHello", "Note": false}]',
+        "id": "4",
+        "reputationCalcAsync": True,
+    }
+    assert demisto.results.call_args[0][0]["HumanReadable"] == "Successfully printed to alert 4."
+
+
+def test_print_to_alert_with_note(mocker: MockerFixture):
+    """Tests print_to_alert_command when the executeCommand command succeeds.
+
+    Checks that the addEntries command is called with the right arguments.
+
+    Also tests mark_as_note=True argument of print_to_alert_command
+    """
+    from PrintToAlert import print_to_alert_command
+
+    execute_command_mocker = mocker.patch.object(
+        demisto,
+        "executeCommand",
+        return_value=[
+            {
+                "Type": EntryType.NOTE,
+                "Contents": "done",
+                "HumanReadable": None,
+                "EntryContext": None,
+            }
+        ],
+    )
+    mocker.patch.object(demisto, "results")
+    print_to_alert_command(current_alert_id="5", value="Hello", alert_id="4", mark_as_note=True)
+    # Right command is called
+    assert execute_command_mocker.call_args[0][0] == "addEntries"
+    # Right arguments are given
+    assert execute_command_mocker.call_args[0][1] == {
+        "entries": '[{"Type": 1, "ContentsFormat": "markdown", "Contents": "Entry from alert #5:\\nHello", "Note": true}]',
         "id": "4",
         "reputationCalcAsync": True,
     }
