125c8f9f672f94b3567eadd3f0e1547dae19efbc
Micah Pegman <micah.pegman@sophos.com>
2026-03-19 22:11:07 +0000

    OSSCON-50 : add xsoar content pack
    (cherry picked from commit 057c491474c53cf28c7f8582eadcf0a7947fe048)
    Signed-off-by: Micah Pegman <micah.pegman@sophos.com>

---
+++ Packs/SophosTaegis/Integrations/Taegis/Taegis_test.py
@@ -0,0 +1,41 @@
+"""Base Integration for Cortex XSOAR - Unit Tests file
+
+Pytest Unit Tests: all funcion names must start with "test_"
+
+More details: https://xsoar.pan.dev/docs/integrations/unit-testing
+
+MAKE SURE YOU REVIEW/REPLACE ALL THE COMMENTS MARKED AS "TODO"
+
+You must add at least a Unit Test function for every XSOAR command
+you are implementing with your integration
+"""
+
+from demisto_sdk.commands.common.handlers import JSON_Handler
+
+import json
+
+
+def util_load_json(path):
+    with open(path, encoding="utf-8") as f:
+        return json.loads(f.read())
+
+
+# TODO: REMOVE the following dummy unit test function
+def test_baseintegration_dummy():
+    """Tests helloworld-say-hello command function.
+
+    Checks the output of the command function with the expected output.
+
+    No mock is needed here because the say_hello_command does not call
+    any external API.
+    """
+    from BaseIntegration import Client, baseintegration_dummy_command
+
+    client = Client(base_url="some_mock_url", verify=False)
+    args = {"dummy": "this is a dummy response", "dummy2": "a dummy value"}
+    response = baseintegration_dummy_command(client, args)
+
+    assert response.outputs == args
+
+
+# TODO: ADD HERE unit tests for every command
