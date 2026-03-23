4909a8271077d7408918d99d9477f9110dc6c973
Prasoon Agnihotri <prasu.agnihotri51@gmail.com>
2026-03-23 22:30:16 +0530

    Updated the automation

--- Packs/Gaiatop-ForwardAuditLogsToSplunkHEC/Scripts/GaiatopForwardAuditLogsToSplunkHEC/Gaiatop_ForwardAuditLogsToSplunkHEC.py
+++ Packs/Gaiatop-ForwardAuditLogsToSplunkHEC/Scripts/GaiatopForwardAuditLogsToSplunkHEC/Gaiatop_ForwardAuditLogsToSplunkHEC.py
@@ -1,30 +1,33 @@
-import demistomock as demisto  # noqa: F401
-from CommonServerPython import *  # noqa: F401
-API_ENDPOINT_URL = "/public_api/v1/audits/management_logs"
+register_module_line('Gaiatop_ForwardAuditLogsToSplunkHEC', 'start', __line__())
 
+API_ENDPOINT_URL = "/public_api/v1/audits/management_logs"
 
 def main():
 
-    logs_to_forward = []
     args = demisto.args()
     list_name_log_count = args.get('AuditLogCountList')
     instance_name = args.get('CoreRestInstanceName')
     splunk_instance_name = args.get('SplunkInstanceName')
 
-    last_fetched_index = int(demisto.executeCommand("getList", {
+    # Get offset
+    last_fetched_index = demisto.executeCommand("getList", {
         "listName": list_name_log_count
-    })[0]['Contents'])
+    })[0]['Contents']
+
+    last_fetched_index = int(last_fetched_index) if last_fetched_index else 0
 
     body = {
         "request_data": {
-            "search_from": last_fetched_index + 1,
-            "search_to": last_fetched_index + 100,
+            "search_from": last_fetched_index+1,
+            "search_to": last_fetched_index+100,
             "sort": {
-                "field": "timestamp",
-                "keyword": "asc"
+            "field": "timestamp",
+            "keyword": "asc"
             }
         }
     }
+
+    # Get audit logs
     all_audit_logs = demisto.dt(demisto.executeCommand("core-api-post", {
         "uri": API_ENDPOINT_URL,
         "body": body,
@@ -32,8 +35,10 @@ def main():
     }), "Contents.response.reply.data")
 
     if all_audit_logs:
+        # Get number of logs pulled
         total_logs = len(all_audit_logs)
 
+        # Submit each log as an even to HEC
         for log in all_audit_logs:
             submit_event = demisto.executeCommand("splunk-submit-event-hec", {
                 "event": log,
@@ -45,12 +50,17 @@ def main():
 
     else:
         return_results("No Logs Found")
+
+    # Update the new offset
     demisto.executeCommand("setList", {
         "listName": list_name_log_count,
-        "listData": last_fetched_index + total_logs
+        "listData": last_fetched_index+total_logs
     })
 
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
     main()
-    # print("Script Completed")
+
+
+register_module_line('Gaiatop_ForwardAuditLogsToSplunkHEC', 'end', __line__())
+
