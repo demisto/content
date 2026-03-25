23838609bf5ed2bbe1d837815d1674d458ff04ab
mayyagoldman <mayyagoldman@gmail.com>
2026-03-25 17:16:53 +0200

    pre commit

--- Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
+++ Packs/ServiceNow/Integrations/ServiceNowv2/ServiceNowv2.py
@@ -1914,10 +1914,10 @@ def get_entries_for_notes(notes: list[dict], params) -> list[dict]:
                 {
                     "Type": note.get("type", 1),
                     "Category": note.get("category"),
-                    "HumanReadable" : f"Type: {note.get('element')}\nCreated By: {note.get('sys_created_by')}\nCreated On: {note.get('sys_created_on')}\n{note.get('value')}",
+                    "HumanReadable": f"Type: {note.get('element')}\nCreated By: {note.get('sys_created_by')}\nCreated On: {note.get('sys_created_on')}\n{note.get('value')}",
                     "Contents": {
-                        "created" : note.get('sys_created_on'),
-                        "Contents" : f"Type: {note.get('element')}\nCreated By: {note.get('sys_created_by')}\nCreated On: {note.get('sys_created_on')}\n{note.get('value')}"
+                        "created": note.get("sys_created_on"),
+                        "Contents": f"Type: {note.get('element')}\nCreated By: {note.get('sys_created_by')}\nCreated On: {note.get('sys_created_on')}\n{note.get('value')}",
                     },
                     "ContentsFormat": note.get("format"),
                     "Tags": tags,
