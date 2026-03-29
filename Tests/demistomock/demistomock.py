b39de48bcd34858b7f805879fe81a8fb323bad48
Amichai <ashamah@paloaltonetworks.com>
2026-03-29 09:02:46 +0300

    added the _Demisto__do mock function to demistomock.py to prevent AttributeError when demisto._Demisto__do is accessed.

--- Tests/demistomock/demistomock.py
+++ Tests/demistomock/demistomock.py
@@ -1515,3 +1515,10 @@ def _platformAPICall(path=None, method=None, params=None, data=None, timeout=Non
         dict: The response of the api call
     """
     return {}
+
+
+def _Demisto__do(*args, **kwargs):
+    """
+    Mock for the internal _Demisto__do function.
+    """
+    pass
