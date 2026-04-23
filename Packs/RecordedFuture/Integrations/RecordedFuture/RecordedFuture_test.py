04f78ac89d0560c47cb2567a7ee8a1d3c14e9645
Dmytro B <dmytro.branitskyi__contractor@recordedfuture.com>
2026-04-23 11:58:32 +0200

    [Pack:RecordedFuture] Make threshold for suspicious score be configurable. (#43635)
    * [Pack:RecordedFuture] Make threshold for suspicious score be configurable.
    
    * [Pack:RecordedFuture] Refactor translation of risk score into dbot score.
    
    * build(RecordedFuture): bump version to 1.8.10
    
    ---------
    
    Co-authored-by: Niklas Logren <niklas@niklaslogren.com>

--- Packs/RecordedFuture/Integrations/RecordedFuture/RecordedFuture_test.py
+++ Packs/RecordedFuture/Integrations/RecordedFuture/RecordedFuture_test.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def create_client():
     import os
     from RecordedFuture import Client
@@ -19,20 +22,140 @@ class TestHelpers:
         from RecordedFuture import translate_score
         from CommonServerPython import Common
 
-        assert translate_score(score=10, threshold=0) == Common.DBotScore.BAD
-        assert translate_score(score=10, threshold=10) == Common.DBotScore.BAD
-        assert translate_score(score=10, threshold=11) == Common.DBotScore.NONE
-        assert translate_score(score=24, threshold=40) == Common.DBotScore.NONE
-        assert translate_score(score=25, threshold=40) == Common.DBotScore.SUSPICIOUS
-        assert translate_score(score=26, threshold=40) == Common.DBotScore.SUSPICIOUS
-        assert translate_score(score=40, threshold=40) == Common.DBotScore.BAD
-        assert translate_score(score=45, threshold=40) == Common.DBotScore.BAD
-        assert translate_score(score=10, threshold=-1) == Common.DBotScore.BAD
-        assert translate_score(score=10, threshold=0) == Common.DBotScore.BAD
-        assert translate_score(score=25, threshold=-1) == Common.DBotScore.BAD
-        assert translate_score(score=25, threshold=0) == Common.DBotScore.BAD
-        assert translate_score(score=26, threshold=-1) == Common.DBotScore.BAD
-        assert translate_score(score=26, threshold=0) == Common.DBotScore.BAD
+        test_cases = [
+            (10, 0, 25, Common.DBotScore.BAD, "Score above 0"),
+            (10, 10, 25, Common.DBotScore.BAD, "Score above 10"),
+            (40, 40, 25, Common.DBotScore.BAD, "Score above 40"),
+            (45, 40, 25, Common.DBotScore.BAD, "Score above 40"),
+            (10, -1, 25, Common.DBotScore.BAD, "Score above -1"),
+            (25, -1, 25, Common.DBotScore.BAD, "Score above -1"),
+            (26, -1, 25, Common.DBotScore.BAD, "Score above -1"),
+            (25, 40, 25, Common.DBotScore.SUSPICIOUS, "Score above 25"),
+            (26, 40, 25, Common.DBotScore.SUSPICIOUS, "Score above 25"),
+            (50, 65, 50, Common.DBotScore.SUSPICIOUS, "Score above 50"),
+            (10, 11, 25, Common.DBotScore.NONE, ""),
+            (24, 40, 25, Common.DBotScore.NONE, ""),
+            (45, 65, 50, Common.DBotScore.NONE, ""),
+        ]
+
+        for (
+            score,
+            threshold_bad,
+            threshold_suspicious,
+            expected_score,
+            expected_description,
+        ) in test_cases:
+            dbot_score_details = translate_score(
+                score=score,
+                threshold_bad=threshold_bad,
+                threshold_suspicious=threshold_suspicious,
+            )
+
+            assert dbot_score_details.score == expected_score
+            assert dbot_score_details.description == expected_description
+
+    @pytest.mark.parametrize(
+        "demisto_params,expected_bad,expected_suspicious",
+        [
+            (
+                {},
+                {
+                    "file": 65,
+                    "ip": 65,
+                    "domain": 65,
+                    "url": 65,
+                    "cve": 65,
+                },
+                {
+                    "file": 25,
+                    "ip": 25,
+                    "domain": 25,
+                    "url": 25,
+                    "cve": 25,
+                },
+            ),
+            (
+                {
+                    "file_threshold": "",
+                    "ip_threshold": "70",
+                    "domain_threshold": "65",
+                    "url_threshold": "65",
+                    "cve_threshold": "65",
+                    "file_threshold_suspicious": None,
+                    "ip_threshold_suspicious": "30",
+                    "domain_threshold_suspicious": "25",
+                    "url_threshold_suspicious": "25",
+                    "cve_threshold_suspicious": "25",
+                },
+                {
+                    "file": 65,
+                    "ip": 70,
+                    "domain": 65,
+                    "url": 65,
+                    "cve": 65,
+                },
+                {
+                    "file": 25,
+                    "ip": 30,
+                    "domain": 25,
+                    "url": 25,
+                    "cve": 25,
+                },
+            ),
+        ],
+    )
+    def test_get_indicator_thresholds_uses_defaults_for_missing_or_empty_values(
+        self, mocker, demisto_params, expected_bad, expected_suspicious
+    ):
+        import demistomock as demisto
+        from RecordedFuture import get_indicator_thresholds
+
+        mock_params = mocker.patch.object(demisto, "params")
+        mock_params.return_value = demisto_params
+
+        indicator_thresholds = get_indicator_thresholds(demisto.params())
+
+        assert indicator_thresholds.bad == expected_bad
+        assert indicator_thresholds.suspicious == expected_suspicious
+
+    @pytest.mark.parametrize(
+        "demisto_params",
+        [
+            {
+                "file_threshold": "abc",
+                "ip_threshold": "65",
+                "domain_threshold": "65",
+                "url_threshold": "65",
+                "cve_threshold": "65",
+                "file_threshold_suspicious": "25",
+                "ip_threshold_suspicious": "25",
+                "domain_threshold_suspicious": "25",
+                "url_threshold_suspicious": "25",
+                "cve_threshold_suspicious": "25",
+            },
+            {
+                "file_threshold": "65",
+                "ip_threshold": "65",
+                "domain_threshold": "65",
+                "url_threshold": "65",
+                "cve_threshold": "65",
+                "file_threshold_suspicious": "25",
+                "ip_threshold_suspicious": "25",
+                "domain_threshold_suspicious": "25",
+                "url_threshold_suspicious": "abc",
+                "cve_threshold_suspicious": "25",
+            },
+        ],
+    )
+    def test_get_indicator_thresholds_raises_for_invalid_values(self, mocker, demisto_params):
+        import demistomock as demisto
+        from CommonServerPython import DemistoException
+        from RecordedFuture import get_indicator_thresholds
+
+        mocker.patch.object(demisto, "params", return_value=demisto_params)
+
+        with pytest.raises(DemistoException, match="Invalid threshold configuration"):
+            get_indicator_thresholds(demisto.params())
 
     def test_determine_hash(self):
         from RecordedFuture import determine_hash
@@ -75,7 +198,7 @@ class TestHelpers:
             DBotScoreType.IP,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -119,7 +242,7 @@ class TestHelpers:
             DBotScoreType.DOMAIN,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -155,7 +278,7 @@ class TestHelpers:
             DBotScoreType.URL,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -220,7 +343,7 @@ class TestHelpers:
             DBotScoreType.FILE,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -245,7 +368,7 @@ class TestHelpers:
             DBotScoreType.FILE,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -270,7 +393,7 @@ class TestHelpers:
             DBotScoreType.FILE,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -295,7 +418,7 @@ class TestHelpers:
             DBotScoreType.FILE,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
@@ -320,7 +443,7 @@ class TestHelpers:
             DBotScoreType.FILE,
             "Recorded Future v2",
             Common.DBotScore.SUSPICIOUS,
-            "",
+            "Score above 25",
             # reliability=DBotScoreReliability.B
             reliability=None,
         )
