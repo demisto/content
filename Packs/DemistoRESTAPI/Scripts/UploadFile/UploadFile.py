d5d19612c05b81b49615701a215cf35bd2aef222
yshamai <yshamai@paloaltonetworks.com>
2026-05-10 14:07:52 +0300

    format

--- Packs/DemistoRESTAPI/Scripts/UploadFile/UploadFile.py
+++ Packs/DemistoRESTAPI/Scripts/UploadFile/UploadFile.py
@@ -12,14 +12,13 @@ def upload_file(incident_id: str, entry_id: str, body: str = "", using: str = ""
     return demisto.executeCommand(
         "core-api-multipart", {"uri": f"{service_name}/upload/{incident_id}", "entryID": entry_id, "body": body, "using": using}
     )
-    
-
 
 def validate_target(target: str) -> str:
     if not is_platform() and "case" in target:
         raise DemistoException("Case attachment is only available in PLATFORM")
     return target
-        
+
+
 def upload_file_command(args: dict) -> list[CommandResults]:
     command_results: list[CommandResults] = []
     incident_id = args.get("incID", "")
