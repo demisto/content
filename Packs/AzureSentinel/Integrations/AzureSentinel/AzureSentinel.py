e3a85cef74b290ae75fc6eed0001e6f9b472dc9d
noydavidi <nodavidi.paloaltonetworks.com>
2026-05-13 13:06:27 +0300

    added unittests

--- Packs/AzureSentinel/Integrations/AzureSentinel/AzureSentinel.py
+++ Packs/AzureSentinel/Integrations/AzureSentinel/AzureSentinel.py
@@ -1748,6 +1748,7 @@ def process_incidents(
     Args:
         raw_incidents: The incidents that were fetched from the API.
         latest_created_time: The latest created time.
+        last_incident_number: The last incident number that was fetched.
         lookback_fetch_ids: IDs from the current lookback cycle to store in next_run.
 
     Returns:
