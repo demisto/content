b08f87036ef5c6b48e347d2b4fed4c5be6bb6e2b
Kamal Qarain <kqarain@paloaltonetworks.com>
2026-05-26 22:05:57 +0300

    Reformat using RUFF

--- Packs/ForwardAuditLogsToSplunkHEC/Scripts/GaiatopForwardAuditLogsToSplunkHEC/GaiatopForwardAuditLogsToSplunkHEC.py
+++ Packs/ForwardAuditLogsToSplunkHEC/Scripts/GaiatopForwardAuditLogsToSplunkHEC/GaiatopForwardAuditLogsToSplunkHEC.py
@@ -4,37 +4,31 @@ from CommonServerUserPython import *
 
 API_ENDPOINT_URL = "/public_api/v1/audits/management_logs"
 
-def main():
 
+def main():
     args = demisto.args()
-    list_name_log_count = args.get('AuditLogCountList')
-    instance_name = args.get('CoreRestInstanceName')
-    splunk_instance_name = args.get('SplunkInstanceName')
+    list_name_log_count = args.get("AuditLogCountList")
+    instance_name = args.get("CoreRestInstanceName")
+    splunk_instance_name = args.get("SplunkInstanceName")
 
     # Get offset
-    last_fetched_index = demisto.executeCommand("getList", {
-        "listName": list_name_log_count
-    })[0]['Contents']
+    last_fetched_index = demisto.executeCommand("getList", {"listName": list_name_log_count})[0]["Contents"]
 
     last_fetched_index = int(last_fetched_index) if last_fetched_index else 0
 
     body = {
         "request_data": {
-            "search_from": last_fetched_index+1,
-            "search_to": last_fetched_index+100,
-            "sort": {
-            "field": "timestamp",
-            "keyword": "asc"
-            }
+            "search_from": last_fetched_index + 1,
+            "search_to": last_fetched_index + 100,
+            "sort": {"field": "timestamp", "keyword": "asc"},
         }
     }
 
     # Get audit logs
-    all_audit_logs = demisto.dt(demisto.executeCommand("core-api-post", {
-        "uri": API_ENDPOINT_URL,
-        "body": body,
-        "using": instance_name
-    }), "Contents.response.reply.data")
+    all_audit_logs = demisto.dt(
+        demisto.executeCommand("core-api-post", {"uri": API_ENDPOINT_URL, "body": body, "using": instance_name}),
+        "Contents.response.reply.data",
+    )
 
     total_logs = 0
     if all_audit_logs:
@@ -43,26 +37,17 @@ def main():
 
         # Submit each log as an even to HEC
         for log in all_audit_logs:
-            submit_event = demisto.executeCommand("splunk-submit-event-hec", {
-                "event": log,
-                "using": splunk_instance_name
-            })
-            if 'sent successfully' not in submit_event:
-                return_error(submit_event[0]['Contents'])
+            submit_event = demisto.executeCommand("splunk-submit-event-hec", {"event": log, "using": splunk_instance_name})
+            if "sent successfully" not in submit_event:
+                return_error(submit_event[0]["Contents"])
                 sys.exit(1)
 
     else:
         return_results("No Logs Found")
 
     # Update the new offset
-    demisto.executeCommand("setList", {
-        "listName": list_name_log_count,
-        "listData": last_fetched_index+total_logs
-    })
+    demisto.executeCommand("setList", {"listName": list_name_log_count, "listData": last_fetched_index + total_logs})
 
 
 if __name__ in ("__main__", "__builtin__", "builtins"):
     main()
-
-
-
