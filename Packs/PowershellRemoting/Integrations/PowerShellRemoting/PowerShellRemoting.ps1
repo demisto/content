6fa37a6ba5372b58252d9ccbc1c7d3d0cc797097
YuvHayun <yhayun@paloaltonetworks.com>
2026-06-18 16:43:28 +0300

    reverts

--- Packs/PowershellRemoting/Integrations/PowerShellRemoting/PowerShellRemoting.ps1
+++ Packs/PowershellRemoting/Integrations/PowerShellRemoting/PowerShellRemoting.ps1
@@ -452,23 +452,6 @@ function TestModuleCommand {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Scope='Function')]
     param([RemotingClient]$client)
 
-    # Override: params parity dump for test-module
-    try {
-        $pp_payload = @{
-            '__params_parity_dump__' = $true
-            'params' = $demisto.Params()
-        }
-        $pp_json = $pp_payload | ConvertTo-Json -Depth 10 -Compress
-        ReturnError "PARAMS_PARITY_DUMP::$pp_json"
-        return $null, $null, $null
-    }
-    catch [System.Management.Automation.MethodInvocationException] {
-        throw
-    }
-    catch {
-        # Probe must never break unrelated integrations. Swallow and continue.
-    }
-
     if ($client.ssl) {
         $res = Test-Connection -TcpPort 5986 -TargetName $client.fqdn -ErrorAction Stop
         if (-not $res) {
@@ -802,26 +785,6 @@ function ExportMFTCommand([RemotingClient]$client, [string]$volume, [string]$out
 function Main
 {
     $command = $Demisto.GetCommand()
-
-    # Override: params parity dump for test-module (before any setup that might fail)
-    if ($command -eq "test-module") {
-        try {
-            $pp_payload = @{
-                '__params_parity_dump__' = $true
-                'params' = $demisto.Params()
-            }
-            $pp_json = $pp_payload | ConvertTo-Json -Depth 10 -Compress
-            ReturnError "PARAMS_PARITY_DUMP::$pp_json"
-            return
-        }
-        catch [System.Management.Automation.MethodInvocationException] {
-            throw
-        }
-        catch {
-            # Probe must never break unrelated integrations. Swallow and continue.
-        }
-    }
-
     $command_args = $Demisto.Args()
     $params = $Demisto.Params()
     <#
