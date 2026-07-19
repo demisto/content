ac0a67be687fd0ec6ca85c26555a4392fd1f6125
Bar Gali <75535203+BarGali@users.noreply.github.com>
2026-07-19 13:27:37 +0300

    Update Brandefense.py

--- Packs/Brandefense/Integrations/Brandefense/Brandefense.py
+++ Packs/Brandefense/Integrations/Brandefense/Brandefense.py
@@ -1,4 +1,4 @@
-?import demistomock as demisto  # noqa: F401
+import demistomock as demisto  # noqa: F401
 import requests
 from CommonServerPython import *  # noqa: F401
 
@@ -13,8 +13,7 @@ def get_assets(api_token):
     allassets = {"assets": response.json()}
     result = CommandResults(outputs=allassets, outputs_prefix="branddefense_assets")
     return_results(result)
-!
-kdufhgks
+
 def get_specific_asset(api_token, args):
     asset_id = args.get("assetid")
     url = "https://api.brandefense.io/api/v1/assets/" + str(asset_id)
