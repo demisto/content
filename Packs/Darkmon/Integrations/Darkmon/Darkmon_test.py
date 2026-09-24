82943c20b9ad21b2a82a471d390b182ce6f22c84
Darkmon <code@darkmon.com>
2026-09-24 14:44:19 +0300

    Darkmon Pack - v1.0.2 (fix NRD and TBF classification filter) (#45999)
    * Darkmon 1.0.2: send the IOC classification as a query parameter in dmontip-get-nrd and dmontip-get-tbf
    
    * Darkmon 1.0.2: bump pack version and add release notes

--- Packs/Darkmon/Integrations/Darkmon/Darkmon_test.py
+++ Packs/Darkmon/Integrations/Darkmon/Darkmon_test.py
@@ -882,7 +882,8 @@ def test_nrd_endpoint_and_filter(monkeypatch):
     calls = patch_http(monkeypatch, NRD_RESPONSE)
     src.dmontip_get_nrd_command(make_client(), {})
     assert calls["url_suffix"] == "ioc"
-    assert calls["params"]["filter"] == '{"iocClassifications": ["NEWLY_REGISTERED_DOMAIN"]}'
+    assert calls["params"]["iocClassifications"] == "NEWLY_REGISTERED_DOMAIN"
+    assert "filter" not in calls["params"]
 
 
 def test_nrd_rendering(monkeypatch):
@@ -921,7 +922,8 @@ def test_tbf_endpoint_and_filter(monkeypatch):
     calls = patch_http(monkeypatch, TBF_RESPONSE)
     src.dmontip_get_tbf_command(make_client(), {})
     assert calls["url_suffix"] == "ioc"
-    assert calls["params"]["filter"] == '{"iocClassifications": ["TELNET_BRUTE_FORCE"]}'
+    assert calls["params"]["iocClassifications"] == "TELNET_BRUTE_FORCE"
+    assert "filter" not in calls["params"]
 
 
 def test_tbf_rendering(monkeypatch):
